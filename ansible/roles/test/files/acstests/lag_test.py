62da4889ffa2785f08236ad310b866733709eb3e
Anton Patenko <c_antonp@mellanox.com>
2017-03-15 17:39:13 +0200

    [LAG tests]: Added LACP timing test;
        * also changed var name from "ptf_docker" to "ptf_host".

--- ansible/roles/test/files/acstests/lag_test.py
+++ ansible/roles/test/files/acstests/lag_test.py
@@ -35,6 +35,7 @@ import ptf.dataplane as dataplane
 import ptf.testutils as testutils
 from router_utils import *
 import pprint
+from scapy.all import rdpcap
 
 class LagAllRoutes(BaseTest,RouterUtility):
     '''
@@ -531,3 +532,56 @@ class LagMembersTrafficTest(BaseTest,RouterUtility):
             send_packet(self, self.src_iface, pkt)
             verify_packet(self, masked_exp_pkt, self.check_pkts_iface)
             i += 1
+
+class LacpTimingTest(BaseTest,RouterUtility):
+    '''
+    @ summary: Verify LACP packets arrive with proper packet timing.
+
+    @ param: exp_iface      -   where to expect LACP packets.
+    @ param: timeout        -   time to expect the LACP packet.
+    @ param: packet_timing         -   time between two packets.
+    @ param: ether_type     -   Ethernet type of expected packet.
+    '''
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.test_params = testutils.test_params_get()
+
+    def setUp(self):
+        '''
+        @summary: Setup for the test
+        '''
+        self.dataplane = ptf.dataplane_instance
+
+    def runTest(self):
+
+        # Get test parameters
+        self.exp_iface = self.test_params['exp_iface']
+        self.timeout = self.test_params['timeout']
+        self.packet_timing = self.test_params['packet_timing']
+        self.ether_type = self.test_params['ether_type']
+
+        # Generate a packet.
+        exp_pkt = simple_eth_packet(eth_type=self.ether_type)
+        exp_pkt = exp_pkt/("0" * 64)
+
+        # Ignore fields with value unknown
+        masked_exp_pkt = Mask(exp_pkt)
+        masked_exp_pkt.set_do_not_care_scapy(scapy.Ether,"dst")
+        masked_exp_pkt.set_do_not_care_scapy(scapy.Ether,"src")
+        masked_exp_pkt.set_do_not_care(14 * 8, 110 * 8)
+
+        # Verify two LACP packets.
+        (rcv_device, rcv_port, rcv_pkt, first_pkt_time) = self.dataplane.poll(port_number=self.exp_iface, timeout=self.timeout, exp_pkt=masked_exp_pkt)
+        (rcv_device, rcv_port, rcv_pkt, last_pkt_time) = self.dataplane.poll(port_number=self.exp_iface, timeout=self.timeout, exp_pkt=masked_exp_pkt)
+
+        # Check the packet received.
+        self.assertTrue(rcv_pkt != None, "Failed to receive LACP packet\n")
+
+        # Get current packet timing
+        first_pkt_time = round(float(first_pkt_time), 2)
+        last_pkt_time = round(float(last_pkt_time), 2)
+        current_pkt_timing = last_pkt_time - first_pkt_time
+
+        # Check that packet timing matches the expected value.
+        self.assertTrue(abs(current_pkt_timing - float(self.packet_timing)) < 0.01, "Bad packet timing: %.2f seconds while expected timing is %d seconds" % (current_pkt_timing, self.packet_timing))
