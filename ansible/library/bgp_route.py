d5c59a2a9e83f79a720b9425b81524ed8f8a4643
REDMONDmasun <masun@microsoft.com>
2017-10-11 18:50:31 +0000

    improve module input and document section

--- ansible/library/bgp_route.py
+++ ansible/library/bgp_route.py
@@ -7,10 +7,28 @@ short_description: Retrieve BGP routing information from Quagga
 description:
     - Retrieve BGP routing information from Quagga, using the VTYSH command line
     - module argument is the property of "SHOW IP BGP"
-    - Current supported parsing commands:
+    - This is not full implementation of all "show ip bgp" fact retrieving, there are many options for this command.
+      This is the first stage. If we need more info in the future, will continue working on this module.
+      Currently supported parsing commands:
         * show ip bgp 100.0.0.1  ### show ip bgp prefix info
-        * show ip bgp neighbor 10.0.0.1 adv   ### show ip bgp neighbor address advertised routes
-    - Retrieved facts will be inserted into the 'bgp_rt'  or 'bgp_rt_neiadv'
+        * show ip bgp neighbor 10.0.0.1 adv   ### show ip bgp neighbor nei_address advertised routes
+    - Retrieved facts will be inserted into the 'bgp_route'  or 'bgp_route_neiadv'
+Options:
+    - option-name: prefix
+      description: bgp prefix to be retrieved from show ip bgp
+      required: False
+      Default: None
+
+    - option-name: neighbor
+      description: restirct retrieving routing information from bgp neighbor
+                   bgp neighbor address is expected to follow this option
+      required: False
+      Default: None
+
+    - option-name: direction
+      required: False (if neighbor is presented, then direction is required)
+      description: to restict retrieving bgp neighbor advertise or received routes
+      Choice:  [adv | rec]
 '''
 
 EXAMPLES = '''
@@ -21,87 +39,106 @@ EXAMPLES = '''
   bgp_route: neighbor='10.0.0.1' direction='adv'
 '''
 
-SAMPLE_COMMAND_DATA = '''
-vtysh -c "show ip bgp 192.168.10.1"
- BGP routing table entry for 192.168.10.1/32
- Paths: (8 available, best #8, table Default-IP-Routing-Table)
- Advertised to non peer-group peers:
-   10.0.0.1 10.0.0.5 10.0.0.13 10.0.0.17 10.0.0.21 10.0.0.25 10.0.0.29 10.0.0.51 10.0.0.53 10.0.0.55 10.0.0.57 10.0.0.59 10.0.0.61 10.0.0.63
-   65200 62011 65501
-   10.0.0.17 from 10.0.0.17 (100.1.0.9)
-      Origin incomplete, localpref 100, valid, external, multipath
-      Last update: Fri Sep 22 06:33:44 2017
-
-   65200 62011 65501
-   10.0.0.25 from 10.0.0.25 (100.1.0.13)
-      Origin incomplete, localpref 100, valid, external, multipath
-      Last update: Fri Sep 22 06:33:42 2017
-
-   65200 62011 65501
-   10.0.0.13 from 10.0.0.13 (100.1.0.7)
-      Origin incomplete, localpref 100, valid, external, multipath
-      Last update: Fri Sep 22 06:33:40 2017
-
-   65200 62011 65501
-   10.0.0.1 from 10.0.0.1 (100.1.0.1)
-      Origin incomplete, localpref 100, valid, external, multipath
-      Last update: Fri Sep 22 06:33:39 2017
-
-    65200 62011 65501
-    10.0.0.29 from 10.0.0.29 (100.1.0.15)
-       Origin incomplete, localpref 100, valid, external, multipath
-       Last update: Fri Sep 22 06:33:39 2017
-
-    65200 62011 65501
-    10.0.0.5 from 10.0.0.5 (100.1.0.3)
-       Origin incomplete, localpref 100, valid, external, multipath
-       Last update: Fri Sep 22 06:33:39 2017
-
-    65200 62011 65501
-    10.0.0.21 from 10.0.0.21 (100.1.0.11)
-       Origin incomplete, localpref 100, valid, external, multipath
-       Last update: Fri Sep 22 06:33:37 2017
-
-    65200 62011 65501
-    10.0.0.9 from 10.0.0.9 (100.1.0.5)
-       Origin incomplete, localpref 100, valid, external, multipath, best
-       Last update: Fri Sep 22 06:33:37 2017
-
-
-
- vtysh -c "show ip bgp neighbor 10.0.0.35 adv" 
-
-  BGP table version is 0, local router ID is 10.1.0.32
-  Status codes: s suppressed, d damped, h history, * valid, > best, = multipath,
-              i internal, r RIB-failure, S Stale, R Removed
-  Origin codes: i - IGP, e - EGP, ? - incomplete
-
-      Network          Next Hop            Metric LocPrf Weight Path
-   *> 10.1.0.32/32     10.0.0.34                 0         32768 i
-   *> 100.1.0.3/32     10.0.0.34                              0 65200 i
-   *> 100.1.0.5/32     10.0.0.34                              0 65200 i
-   *> 100.1.0.7/32     10.0.0.34                              0 65200 i
-   *> 100.1.0.9/32     10.0.0.34                              0 65200 i
-   *> 100.1.0.11/32    10.0.0.34                              0 65200 i
-   *> 100.1.0.13/32    10.0.0.34                              0 65200 i
-   *> 100.1.0.15/32    10.0.0.34                              0 65200 i
-   *> 100.1.0.17/32    10.0.0.34                              0 64001 i
-   *> 192.168.10.97/32 10.0.0.34                              0 65200 62011 65507 ?
-   *> 192.168.10.112/32
-                       10.0.0.34                              0 65200 62011 65508 ?
-   *> 192.168.10.113/32
-                       10.0.0.34                              0 65200 62011 65508 ?
-   *> 192.168.10.128/32
-                       10.0.0.34                              0 65200 62011 65509 ?
-   *> 192.168.10.129/32
-                       10.0.0.34                              0 65200 62011 65509 ?
-   *> 192.168.10.144/32
-                       10.0.0.34                              0 65200 62011 65510 ?
-
+RETURN = '''
+    return: ansible_facts
+        - ansible_facts.bgp_route:  "show ip bgp prefix" result
+          bgp_route:
+            "10.0.100.0/26": {"found": true,
+                              "path_num": "2",
+                              "paths": ["64001 64700", "64003 64700"]
+                             }
+        - ansible_facts.bgp_route_neiadv:
+          "bgp_route_neiadv": {"192.10.0.0/24":
+                                   {"next_hop": "10.0.0.20",
+                                     "paths": ["65200", "62101", "65516"]
+                                   },
+                               "192.168.99.81/32":
+                                   {"next_hop": "10.0.0.20",
+                                    "paths": ["65200", "62100", "65506"]
+                                   }
+                              }
 '''
 
 
-### TODO: ipv6 route entries parsing is not fully working, need continue working on ipv6 ###
+SAMPLE_COMMAND_DATA = '''
+* vtysh -c "show ip bgp 192.168.10.1"
+   BGP routing table entry for 192.168.10.1/32
+   Paths: (8 available, best #8, table Default-IP-Routing-Table)
+   Advertised to non peer-group peers:
+     10.0.0.1 10.0.0.5 10.0.0.13 10.0.0.17 10.0.0.21 10.0.0.25 10.0.0.29 10.0.0.51 10.0.0.53 10.0.0.55 10.0.0.57 10.0.0.59 10.0.0.61 10.0.0.63
+     65200 62011 65501
+     10.0.0.17 from 10.0.0.17 (100.1.0.9)
+        Origin incomplete, localpref 100, valid, external, multipath
+        Last update: Fri Sep 22 06:33:44 2017
+
+     65200 62011 65501
+     10.0.0.25 from 10.0.0.25 (100.1.0.13)
+        Origin incomplete, localpref 100, valid, external, multipath
+        Last update: Fri Sep 22 06:33:42 2017
+
+     65200 62011 65501
+     10.0.0.13 from 10.0.0.13 (100.1.0.7)
+        Origin incomplete, localpref 100, valid, external, multipath
+        Last update: Fri Sep 22 06:33:40 2017
+
+     65200 62011 65501
+     10.0.0.1 from 10.0.0.1 (100.1.0.1)
+        Origin incomplete, localpref 100, valid, external, multipath
+        Last update: Fri Sep 22 06:33:39 2017
+
+      65200 62011 65501
+      10.0.0.29 from 10.0.0.29 (100.1.0.15)
+         Origin incomplete, localpref 100, valid, external, multipath
+         Last update: Fri Sep 22 06:33:39 2017
+
+      65200 62011 65501
+      10.0.0.5 from 10.0.0.5 (100.1.0.3)
+         Origin incomplete, localpref 100, valid, external, multipath
+         Last update: Fri Sep 22 06:33:39 2017
+
+      65200 62011 65501
+      10.0.0.21 from 10.0.0.21 (100.1.0.11)
+         Origin incomplete, localpref 100, valid, external, multipath
+         Last update: Fri Sep 22 06:33:37 2017
+
+      65200 62011 65501
+      10.0.0.9 from 10.0.0.9 (100.1.0.5)
+         Origin incomplete, localpref 100, valid, external, multipath, best
+         Last update: Fri Sep 22 06:33:37 2017
+
+
+*  vtysh -c "show ip bgp neighbor 10.0.0.35 adv"
+
+    BGP table version is 0, local router ID is 10.1.0.32
+    Status codes: s suppressed, d damped, h history, * valid, > best, = multipath,
+                i internal, r RIB-failure, S Stale, R Removed
+    Origin codes: i - IGP, e - EGP, ? - incomplete
+
+        Network          Next Hop            Metric LocPrf Weight Path
+     *> 10.1.0.32/32     10.0.0.34                 0         32768 i
+     *> 100.1.0.3/32     10.0.0.34                              0 65200 i
+     *> 100.1.0.5/32     10.0.0.34                              0 65200 i
+     *> 100.1.0.7/32     10.0.0.34                              0 65200 i
+     *> 100.1.0.9/32     10.0.0.34                              0 65200 i
+     *> 100.1.0.11/32    10.0.0.34                              0 65200 i
+     *> 100.1.0.13/32    10.0.0.34                              0 65200 i
+     *> 100.1.0.15/32    10.0.0.34                              0 65200 i
+     *> 100.1.0.17/32    10.0.0.34                              0 64001 i
+     *> 192.168.10.97/32 10.0.0.34                              0 65200 62011 65507 ?
+     *> 192.168.10.112/32
+                         10.0.0.34                              0 65200 62011 65508 ?
+     *> 192.168.10.113/32
+                         10.0.0.34                              0 65200 62011 65508 ?
+     *> 192.168.10.128/32
+                         10.0.0.34                              0 65200 62011 65509 ?
+     *> 192.168.10.129/32
+                         10.0.0.34                              0 65200 62011 65509 ?
+     *> 192.168.10.144/32
+                         10.0.0.34                              0 65200 62011 65510 ?
+'''
+
+
+### TODO: No show ipv6 route entries parsing option, some show command will expose ipv6 routes, need continue working on ipv6 specific commands###
 
 
 class BgpRoutes(object):
@@ -115,7 +152,7 @@ class BgpRoutes(object):
     def get_facts(self):
         return self.facts
 
-    def parse_bgp_rt_adv(self, cmd, cmd_result):
+    def parse_bgp_route_adv(self, cmd, cmd_result):
         '''
         parse BGP routing facts of neighbor advertised routes
         '''
@@ -125,7 +162,7 @@ class BgpRoutes(object):
             neighbor = regex_neighbor.match(cmd).group(1)
         else:
             raise Exception('cannot find neighbor in "show ip  bgp ' + cmd + '"')
-        self.facts['bgp_rt_neiadv']['neighbor'] = neighbor
+        self.facts['bgp_route_neiadv']['neighbor'] = neighbor
         ### so far parsing by fixed length table
         ### will improve this if find better way to parse it
         header_position = [0, 3, 20, 40, 47, 54, 61]
@@ -139,6 +176,7 @@ class BgpRoutes(object):
                     table_start = True
                     continue
             else:
+                ## only parse valid route entry, ignore if it's not marked as valid
                 if not re.match('^\*', line):
                     continue
                 entry = {}
@@ -150,83 +188,95 @@ class BgpRoutes(object):
                 entry['next_hop'] = line[header_position[2]:header_position[3]].strip()
                 paths = line[header_position[6]:].strip()
                 entry['paths'] = re.findall('[0-9]+', paths)
-                self.facts['bgp_rt_neiadv'][prefix] = entry
+                self.facts['bgp_route_neiadv'][prefix] = entry
 
 
-    def parse_bgp_rt_prefix(self, prefix, cmd_result):
+    def parse_bgp_route_prefix(self, prefix, cmd_result):
         '''
         parse BGP facts for specific prefix
         '''
+        # BGP route preix line parsing state
+        # search_state = [HEADER, PATH_PROPERTIES, PEER_GROUP_HEADER, PEER_GROUPS, PREFIX_PATHS, PREFIX_PATHS_FROM, PREFIX_PATH_ORIGIN, PREFIX_PATH_TIMESTAMP, ERR]
+        HEADER  = 1
+        PATH_PROPERTIES = 2
+        PEER_GROUP_HEADER = 3
+        PEER_GROUPS = 4
+        PREFIX_PATHS = 5
+        PREFIX_PATHS_FROM = 6
+        PREFIX_PATH_ORIGIN = 7
+        PREFIX_PATH_TIMESTAMP = 8
+        ERR = 9
+
         regex_prefix_header = re.compile('BGP routing table entry for ')
         regex_prefix_avail_paths = re.compile('Paths:\s+\((\d+)\s+available')
-        prefix_temp_line1 = 'Advertised to non peer-group peers:'
-        regex_prefix_prop2 = re.compile('^[0-9a-fA-F.:\s]+$')
+        prefix_peer_group_header = 'Advertised to non peer-group peers:'
+        regex_prefix_peers = re.compile('^[0-9a-fA-F.:\s]+$')
         regex_prefix_paths = re.compile('^[0-9\s]+$')
-        regex_prefix_path_line1 = re.compile('.* from .*\([0-9a-fA-F.:]+\)')
-        regex_prefix_path_line2 = re.compile('\s+Origin')
-        regex_prefix_path_line3 = re.compile('\s+Last update:')
+        regex_prefix_path_p1_from = re.compile('.* from .*\([0-9a-fA-F.:]+\)')
+        regex_prefix_path_p2_origin = re.compile('\s+Origin')
+        regex_prefix_path_p3_timestamp = re.compile('\s+Last update:')
         cmd_err1 = 'Unknown command'
         cmd_err2 = 'Network not in table'
-        self.facts['bgp_rt'] = defaultdict(dict)
+        self.facts['bgp_route'] = defaultdict(dict)
         if cmd_err1 in cmd_result or cmd_err2 in cmd_result:
-            self.facts['bgp_rt'][prefix]['found'] = False
+            self.facts['bgp_route'][prefix]['found'] = False
             return
         result_lines = cmd_result.split('\n')
         ## search_state = ['header', 'path_prop', 'prefix_prop1', 'prefix_prop2', 'paths', 'path_l1', 'path_l2', 'path_l3', 'err' ]
-        state = 'header'
-        self.facts['bgp_rt'][prefix]['paths'] = []
+        state = HEADER
+        self.facts['bgp_route'][prefix]['paths'] = []
         while len(result_lines) != 0:
             line = result_lines.pop(0)
             if line == '':
                 continue
-            if state == 'header':
+            if state == HEADER:
                 if regex_prefix_header.match(line) and (prefix not in line):
-                    self.facts['bgp_rt'][prefix]['found'] = False
+                    self.facts['bgp_route'][prefix]['found'] = False
                     return
                 if regex_prefix_header.match(line) and prefix in line:
-                    self.facts['bgp_rt'][prefix]['found'] = True
-                    state = 'path_prop'
+                    self.facts['bgp_route'][prefix]['found'] = True
+                    state = PATH_PROPERTIES
                 else:
-                    state = 'err'
-            elif state == 'path_prop':
+                    state = ERR
+            elif state == PATH_PROPERTIES:
                 find_path = regex_prefix_avail_paths.match(line)
                 if find_path:
                     path_num = find_path.group(1)
-                    self.facts['bgp_rt'][prefix]['path_num'] = path_num
-                    state = 'path_prefix_prop1'
+                    self.facts['bgp_route'][prefix]['path_num'] = path_num
+                    state = PEER_GROUP_HEADER
                 else:
-                    state = 'err'
-            elif  state == 'path_prefix_prop1':
-                state = 'path_prefix_prop2'
-            elif  state == 'path_prefix_prop2':
-                if regex_prefix_prop2.match(line):
-                    state = 'paths'
+                    state = ERR
+            elif  state == PEER_GROUP_HEADER:
+                state = PEER_GROUPS
+            elif  state == PEER_GROUPS:
+                if regex_prefix_peers.match(line):
+                    state = PREFIX_PATHS
                 else:
-                    state = 'err'
-            elif state == 'paths':
+                    state = ERR
+            elif state == PREFIX_PATHS:
                 if regex_prefix_paths.match(line):
                     path = line.strip()
-                    self.facts['bgp_rt'][prefix]['paths'].append(path)
-                    state = 'path_l1'
+                    self.facts['bgp_route'][prefix]['paths'].append(path)
+                    state = PREFIX_PATHS_FROM
                 else:
-                    state = 'err'
-            elif state == 'path_l1':
-                if regex_prefix_path_line1.match(line):
-                    state = 'path_l2'
+                    state = ERR
+            elif state == PREFIX_PATHS_FROM:
+                if regex_prefix_path_p1_from.match(line):
+                    state = PREFIX_PATH_ORIGIN
                 else:
-                    state = 'err'
-            elif state == 'path_l2':
-                if regex_prefix_path_line2.match(line):
-                    state = 'path_l3'
+                    state = ERR
+            elif state == PREFIX_PATH_ORIGIN:
+                if regex_prefix_path_p2_origin.match(line):
+                    state = PREFIX_PATH_TIMESTAMP
                 else:
-                    state = 'err'
-            elif state == 'path_l3':
-                if regex_prefix_path_line3.match(line):
-                    state = 'paths'
+                    state = ERR
+            elif state == PREFIX_PATH_TIMESTAMP:
+                if regex_prefix_path_p3_timestamp.match(line):
+                    state = PREFIX_PATHS
                 else:
-                    state = 'err'
-            elif state == 'err':
-                raise Exception("cannot parse bgp prefix info correctly" + str(self.facts))
+                    state = ERR
+            elif state == ERR:
+                raise Exception("cannot parse bgp prefix info correctly " + str(state) + str(self.facts))
 
 
 def collect_data(module, command=None):
@@ -242,7 +292,7 @@ def main():
     module = AnsibleModule(
             argument_spec=dict(
                 neighbor=dict(required=False, default=None),
-                direction=dict(required=False, default=None),
+                direction=dict(required=False, choices=['adv', 'rec']),
                 prefix=dict(required=False, default=None)
                 ),
             supports_check_mode=False
@@ -262,11 +312,11 @@ def main():
         if neigh and 'adv' in direction.lower():
             show_cmd = "neighbor " + neigh + " adv"
             (rc, out, err) = collect_data(module, show_cmd)
-            bgproute.parse_bgp_rt_adv(show_cmd, out)
+            bgproute.parse_bgp_route_adv(show_cmd, out)
         elif prefix:
             if regex_ip.match(prefix) or regex_iprange.match(prefix):
                 (rc, out, err) = collect_data(module, prefix)
-                bgproute.parse_bgp_rt_prefix(prefix, out)
+                bgproute.parse_bgp_route_prefix(prefix, out)
             else:
                 raise Exception('No support of this command "show ip(v6) bgp %s " yet ' % prefix)
         else:
