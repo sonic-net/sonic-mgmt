24e4d5703e8b4e79a0118a26dc51ad8dccff942e
maggiemsft <masun@microsoft.com>
2017-10-20 09:46:46 -0700

    [test]add more ARP and neighbor-mac tests (#303)
    * add more arp and neighbor-mac tests
    
    * improve based on review feedback

--- ansible/library/switch_arptable.py
+++ ansible/library/switch_arptable.py
@@ -4,45 +4,88 @@ DOCUMENTATION = '''
 ---
 module: switch_arptble
 version_added: "1.9"
+short description:
+    Ansible module to retrieves arp table from SONiC switch
 description:
-    Ansible module retrieves arp table from SONiC switch
-    Depends on /sbin/ip neigh
-output:
+    gathing arp information using "/sbin/ip neigh" command
+    parsing the result of the shell command
+    return ansible_facts for arptable
+        - arptable groups in v4 and v6
+        - for each entry:
+            key: IP address (either V4 or V6)
+            values:
+                interface: interface name
+                state: STALE | REACHABLE | FAILED | DELAY
+                macaddress: 'None'  for failed
+                            '00:00:00:01:02:03' for actual learned mac
+option:
+    None
+'''
+
+EXAMPLES = '''
+    switch_arptable:
+'''
+
+RETURN = '''
+return: ansible_facts:
     arptable{
         "v4":{
             "10.10.1.3":{
                 "interface": "Ethernet68"
                 "state": "STALE"
                 "macaddress": "00:00:00:01:02:03"
-            },
-        },
+                },
+            "10.10.1.4":
+                 "interface": "Ethernet4"
+                "state": "REACHABLE"
+                "macaddress": "00:85:00:00:0a:70"
+            }
+        "v6":{
+            "fc00::6e": {
+                "interface": "Ethernet68"
+                "state": "STALE"
+                "macaddress": "00:00:00:01:02:03"
+                }
+             },
     }
-    TODO: IPV6 neighbor table when we test IPV6
-'''
+ '''
 
-EXAMPLES = '''
-    switch_arptable: 
+SAMPLE_COMMAND_DATA = '''
+fc00::5a dev Ethernet88 lladdr 52:54:00:56:3b:f0 router DELAY
+fc00::32 dev Ethernet48 lladdr 52:54:00:f7:42:72 router STALE
+fc00::4e dev Ethernet76  FAILED
+fe80::5054:ff:fe96:9857 dev Ethernet16 lladdr 52:54:00:96:98:57 router STALE
+fc00::52 dev Ethernet80 lladdr 52:54:00:1d:5e:ba router REACHABLE
+10.0.0.43 dev Ethernet84 lladdr 52:54:00:34:51:ee REACHABLE
+10.0.0.55 dev Ethernet108 lladdr 52:54:00:58:2d:4c REACHABLE
+10.0.0.61 dev Ethernet120 lladdr 52:54:00:b2:46:15 REACHABLE
+10.0.0.13 dev PortChannel24 lladdr 52:54:00:4e:a8:f4 STALE
 '''
 
 from ansible.module_utils.basic import *
 from collections import defaultdict
-import socket
-import struct
 import re
 import json
 
-v4host = re.compile('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 def parse_arptable(output):
-    v4tbl = {}
+    v4host = re.compile('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+    v6host = re.compile('[0-9a-fA-F:]+::[0-9a-fA-F:]+')
+    v4tbl = dict()
+    v6tbl = dict()
     for line in output.split('\n'):
         fields = line.split()
         if len(fields) != 0:
             if v4host.match(fields[0]):
                 if len(fields) == 4:
-                    v4tbl[fields[0]]={'interface': fields[2], 'state':fields[3], 'macaddress': 'None'}
-                if len(fields) > 4:
-                     v4tbl[fields[0]]={'interface': fields[2], 'state':fields[5], 'macaddress': fields[4]}
-    arps = {'v4':v4tbl}
+                    v4tbl[fields[0]] = {'interface': fields[2], 'state':fields[3], 'macaddress': 'None'}
+                else:
+                    v4tbl[fields[0]] = {'interface': fields[2], 'state':fields[-1], 'macaddress': fields[4]}
+            if v6host.match(fields[0]):
+                if len(fields) == 4:
+                    v6tbl[fields[0]] = {'interface': fields[2], 'state':fields[3], 'macaddress': 'None'}
+                else:
+                    v6tbl[fields[0]] = {'interface': fields[2], 'state':fields[-1], 'macaddress': fields[4]}
+    arps = {'v4':v4tbl, 'v6':v6tbl}
     return arps
 
 def main():
@@ -50,16 +93,16 @@ def main():
         argument_spec=dict(),
         supports_check_mode=False)
 
-    rt, out, err = module.run_command("ip neigh")
-    if rt != 0:
-        self.module.fail_json(msg="Command 'ip neigh' failed rc=%d, out=%s, err=%s" %(rt, out, err))
-
-    arp_tbl = parse_arptable(out)
-
-    if arp_tbl == None:
-        self.module.fail_json(msg="Parse arp table failed??")
-
-    module.exit_json(changed=False, arptable=arp_tbl)
+    try:
+        rt, out, err = module.run_command("ip neigh")
+        if rt != 0:
+            self.module.fail_json(msg="Command 'ip neigh' failed rc=%d, out=%s, err=%s" %(rt, out, err))
+            return
+        arp_tbl = parse_arptable(out)
+        module.exit_json(changed=False, ansible_facts={'arptable':arp_tbl})
+    except Exception as e:
+        err_msg = "Parse ip neigh table failed! " + str(e)
+        self.module.fail_json(msg=err_msg)
 
 if __name__ == "__main__":
     main()
--- ansible/roles/test/files/acstests/arptest.py
+++ ansible/roles/test/files/acstests/arptest.py
@@ -10,8 +10,8 @@ from ptf.mask import Mask
 
 class ExpectReply(acs_base_test.ACSDataplaneTest):
     '''
-    Test correct ARP behavior, make sure ACS is replying ARP request for local interface IP address
-    ACS switch should reply ARP and update ARP table entry to correct peer MAC address
+    Test correct ARP behavior, make sure SONiC is replying ARP request for local interface IP address
+    SONiC switch should reply ARP and update ARP table entry to correct peer MAC address
     '''
     def runTest(self):
             acs_mac = self.test_params['acs_mac']
@@ -37,10 +37,40 @@ class ExpectReply(acs_base_test.ACSDataplaneTest):
             send_packet(self, 1, pkt)
             verify_packet(self, exp_pkt, 1)
 
+class VerifyUnicastARPReply(acs_base_test.ACSDataplaneTest):
+    '''
+    Test correct ARP behavior, make sure SONiC is replying Unicast ARP request for local interface IP address
+    SONiC switch should reply ARP and update ARP table entry to correct peer MAC address
+    '''
+    def runTest(self):
+            acs_mac = self.test_params['acs_mac']
+            pkt = simple_arp_packet(pktlen=60,
+                      eth_dst=acs_mac,
+                      eth_src='00:06:07:08:09:00',
+                      vlan_vid=0,
+                      vlan_pcp=0,
+                      arp_op=1,
+                      ip_snd='10.10.1.3',
+                      ip_tgt='10.10.1.2',
+                      hw_snd='00:06:07:08:09:00',
+                      hw_tgt=acs_mac,
+                      )
+            exp_pkt = simple_arp_packet(eth_dst='00:06:07:08:09:00',
+                      eth_src=acs_mac,
+                      arp_op=2,
+                      ip_snd='10.10.1.2',
+                      ip_tgt='10.10.1.3',
+                      hw_tgt='00:06:07:08:09:00',
+                      hw_snd=acs_mac,
+                      )
+            send_packet(self, 1, pkt)
+            verify_packet(self, exp_pkt, 1)
+
+
 class WrongIntNoReply(acs_base_test.ACSDataplaneTest):
     '''
     Test ARP packet from other(wrong) interface with dest IP address as local interface IP address
-    ACS should not reply to such ARP request
+    SONiC should not reply to such ARP request
     '''
     def runTest(self):
             acs_mac = self.test_params['acs_mac']
@@ -70,7 +100,7 @@ class WrongIntNoReply(acs_base_test.ACSDataplaneTest):
 class SrcOutRangeNoReply(acs_base_test.ACSDataplaneTest):
     '''
     Test incoming ARP request src IP address is not within local interface subnet, even the destination address match
-    ACS should not reply such ARP request and should not add ARP table entry either
+    SONiC should not reply such ARP request and should not add ARP table entry either
     '''
     def runTest(self):
         acs_mac = self.test_params['acs_mac']
@@ -98,8 +128,8 @@ class SrcOutRangeNoReply(acs_base_test.ACSDataplaneTest):
 
 class GarpNoUpdate(acs_base_test.ACSDataplaneTest):
     '''
-    When receiving gratuitous ARP packet, if it was not resolved in ARP table before, 
-    ACS should discard the request and won't add ARP entry for the GARP 
+    When receiving gratuitous ARP packet, if it was not resolved in ARP table before,
+    SONiC should discard the request and won't add ARP entry for the GARP
     '''
     def runTest(self):
         acs_mac = self.test_params['acs_mac']
@@ -128,8 +158,8 @@ class GarpNoUpdate(acs_base_test.ACSDataplaneTest):
 
 class GarpUpdate(acs_base_test.ACSDataplaneTest):
     '''
-    When receiving gratuitous ARP packet, if it was resolved in ARP table before, 
-    ACS should update ARP entry with new mac 
+    When receiving gratuitous ARP packet, if it was resolved in ARP table before,
+    SONiC should update ARP entry with new mac
     '''
     def runTest(self):
         acs_mac = self.test_params['acs_mac']
