de32ccea0ab4d5622633f4c86315a7f2be1eb50b
Ying Xie <ying.xie@microsoft.com>
2017-10-21 03:18:05 +0000

    [Arista7260cx3] hard code port layout to enable fib test
    - Excluding port 18, 20 until new bcm config is in place.

--- ansible/roles/test/files/ptftests/fib_test.py
+++ ansible/roles/test/files/ptftests/fib_test.py
@@ -98,6 +98,8 @@ class FibTest(BaseTest):
             self.src_ports = range(1, 25) + range(28, 32)
         if self.test_params['testbed_type'] == 't0-64':
             self.src_ports = range(0, 2) + range(4, 18) + range(20, 33) + range(36, 43) + range(48, 49) + range(52, 59)
+        if self.test_params['testbed_type'] == 't0-116':
+            self.src_ports = range(0, 17) + [18] + range(20,44) + range(52, 64)
     #---------------------------------------------------------------------
 
     def check_ip_range(self, ipv4=True):
