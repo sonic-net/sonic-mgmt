e0e0d914d92b3064e73f7869fdb56d3f4148d357
Shuotian Cheng <shuche@microsoft.com>
2018-03-15 12:57:43 -0700

    [ip_decap_test]: Support t0-64 topology (#517)

--- ansible/roles/test/files/ptftests/IP_decap_test.py
+++ ansible/roles/test/files/ptftests/IP_decap_test.py
@@ -64,6 +64,8 @@ class DecapPacketTest(BaseTest):
             self.src_ports = [0, 1, 4, 5, 16, 17, 20, 21, 34, 36, 37, 38, 39, 42, 44, 45, 46, 47, 50, 52, 53, 54, 55, 58, 60, 61, 62, 63]
         if self.test_params['testbed_type'] == 't0':
             self.src_ports = range(1, 25) + range(28, 32)
+        if self.test_params['testbed_type'] == 't0-64':
+            self.src_ports = [0,  1,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 48, 52, 53, 54, 55, 56, 57, 58]
         if self.test_params['testbed_type'] == 't0-116':
             self.src_ports = range(0, 24) + range(32, 120)
     #-----------------------------------------------------------------
