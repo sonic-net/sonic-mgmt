ac38cdb0a7bb85820ff0fad344adf98fb08ce74b
Ying Xie <ying.xie@microsoft.com>
2019-01-25 23:56:13 +0000

    [warm/fast reboot] fix data plane down time calculation error
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- ansible/roles/test/files/ptftests/advanced-reboot.py
+++ ansible/roles/test/files/ptftests/advanced-reboot.py
@@ -854,14 +854,14 @@ class ReloadTest(BaseTest):
                     self.log("Data plane was stopped, Waiting until it's up. Stop time: %s" % str(no_routing_start))
                 except TimeoutError:
                     self.log("Data plane never stop")
-                    no_routing_start = datetime.min
+                    no_routing_start = datetime.datetime.min
 
                 if no_routing_start is not None:
                     self.timeout(self.task_timeout, "DUT hasn't started to work for %d seconds" % self.task_timeout)
                     no_routing_stop, _ = self.check_forwarding_resume()
                     self.cancel_timeout()
                 else:
-                    no_routing_stop = datetime.min
+                    no_routing_stop = datetime.datetime.min
 
                 # Stop watching DUT
                 self.watching = False
@@ -884,7 +884,8 @@ class ReloadTest(BaseTest):
                     self.log("Total disruptions count is %d. All disruptions lasted %.3f seconds. Total %d packet(s) lost" % \
                         (self.disrupts_count, self.total_disrupt_time, self.total_disrupt_packets))
                 else:
-                    no_routing_stop, no_routing_start = 0, 0
+                    no_routing_start = self.reboot_start
+                    no_routing_stop  = self.reboot_start
 
             # wait until all bgp session are established
             self.log("Wait until bgp routing is up on all devices")
