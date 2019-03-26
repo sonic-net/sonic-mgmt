sonic-net/sonic-mgmt

commit e1ce368ec9717d9a2f3f732628bff93fee4f3804

[logAnalyzer] Avoid logAnalyzer logs being overwritten (#822)
In logAnalyzer related scripts, ansible_date_time is used for generating
testname_unique which will be used as folder name for storing
logAnalyzer results. The drawback is that ansible_date_time is fixed
in single ansible playbook execution. If logAnalyzer is called
multiple times, the earlier results could be overwritten by the latest
results.

The fix is to replace ansible_date_time with:
{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}

There are two differences:
1. Each time the lookup plugin is called, the returned date and time
   would be different (interval of two calls longer than 1 second)
2. The lookup plugin gets date and time of the sonic-mgmt container,
   not date and time of DUT.

Signed-off-by: Xin Wang <xinw@mellanox.com>

ansible/vars/run_config_test_vars.yml
@@ -1,6 +1,6 @@
 ---
 
-testname_unique: "{{ testname }}.{{ ansible_date_time.date }}.{{ ansible_date_time.time }}"
+testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
 loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml

ansible/vars/run_loganalyzer_vars.yml
@@ -1,6 +1,6 @@
 ---
 
-testname_unique: "{{ testname }}.{{ ansible_date_time.date }}.{{ ansible_date_time.time }}"
+testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
 loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml

ansible/vars/run_ping_test_vars.yml
@@ -1,6 +1,6 @@
 ---
 
-testname_unique: "{{ testname }}.{{ ansible_date_time.date }}.{{ ansible_date_time.time }}"
+testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
 summary_file: "summary.loganalysis.{{ testname_unique }}.log"

ansible/vars/run_ptf_test_vars.yml
@@ -1,6 +1,6 @@
 ---
 
-testname_unique: "{{ testname }}.{{ ansible_date_time.date }}.{{ ansible_date_time.time }}"
+testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
 summary_file: "summary.loganalysis.{{ testname_unique }}.log"

ansible/roles/test/files/tools/loganalyzer/loganalyzer_init.yml
@@ -18,7 +18,7 @@
   when: expect_file is not defined
 
 - set_fact:
-    testname_unique: "{{ testname }}.{{ ansible_date_time.date }}.{{ ansible_date_time.time }}"
+    testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
   when: testname_unique is not defined
 
 - set_fact:

ansible/roles/test/tasks/acltb_ranges_test.yml
@@ -27,7 +27,7 @@
 
 # Separate set_fact is required to be able to use 'testname' fact.
 - set_fact:
-    testname_unique: "{{ testname }}.{{ ansible_date_time.date}}.{{ ansible_date_time.hour}}-{{ ansible_date_time.minute}}-{{ ansible_date_time.second}}"
+    testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 # Separate set_fact is required to be able to use 'testname_unique' fact.
 - set_fact:

ansible/roles/test/tasks/decap.yml
@@ -103,7 +103,7 @@
 
   # Separate set_fact is required to be able to use 'testname' fact.
   - set_fact:
-      testname_unique: "{{ testname }}.{{ ansible_date_time.date}}.{{ ansible_date_time.hour}}-{{ ansible_date_time.minute}}-{{ ansible_date_time.second}}"
+      testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
   - debug: msg="generated run id:{{testname_unique}}"
 

ansible/roles/test/tasks/lag.yml
@@ -33,7 +33,7 @@
 
 # Separate set_fact is required to be able to use 'testname' fact.
 - set_fact:
-    testname_unique: "{{ testname }}.{{ ansible_date_time.date}}.{{ ansible_date_time.hour}}-{{ ansible_date_time.minute}}-{{ ansible_date_time.second}}"
+    testname_unique: "{{ testname }}.{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
 
 # Separate set_fact is required to be able to use 'testname_unique' fact.
 - set_fact:

commit aff292f14a068f6ea3e19851962514b77d31e14f

[logAnalyzer] Fix the issue that variables are not overridden as expected
PR #831 does not fully fix the issue introduced by PR #822. Ansible's
include_vars module could not override variable value previous defined
by set_fact. Variables in vars/run_config_test_vars.yml may still have
old value.

The change is to avoid using include_vars. The variables defined in
run_config_test_vars.yml are moved into script
run_command_with_log_analyzer.yml. The vars files are deleted.

The same change is made to other scripts using the same pattern.

Signed-off-by: Xin Wang <xinw@mellanox.com>

ansible/vars/run_config_test_vars.yml
@@ -1,13 +1,0 @@
----
-
-testname_unique: "{{ testname }}.{{ unique_timestamp }}"
-
-test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
-loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml
-loganalyzer_analyze: roles/test/files/tools/loganalyzer/loganalyzer_analyze.yml
-
-match_file: loganalyzer_common_match.txt
-ignore_file: loganalyzer_common_ignore.txt
-
-summary_file: summary.loganalysis.{{ testname_unique }}.log
-result_file: result.loganalysis.{{ testname_unique }}.log

ansible/vars/run_loganalyzer_vars.yml
@@ -1,15 +1,0 @@
----
-
-testname_unique: "{{ testname }}.{{ unique_timestamp }}"
-
-test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
-loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml
-loganalyzer_analyze: roles/test/files/tools/loganalyzer/loganalyzer_analyze.yml
-
-match_file: loganalyzer_common_match.txt
-ignore_file: loganalyzer_common_ignore.txt
-
-summary_file: summary.loganalysis.{{ testname_unique }}.log
-result_file: result.loganalysis.{{ testname_unique }}.log
-
-run_analyze_and_check: "roles/test/tasks/run_analyze_and_check.yml"

ansible/vars/run_ping_test_vars.yml
@@ -1,7 +1,0 @@
----
-
-testname_unique: "{{ testname }}.{{ unique_timestamp }}"
-
-test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
-summary_file: "summary.loganalysis.{{ testname_unique }}.log"
-result_file: "result.loganalysis.{{ testname_unique }}.log"

ansible/vars/run_ptf_test_vars.yml
@@ -1,7 +1,0 @@
----
-
-testname_unique: "{{ testname }}.{{ unique_timestamp }}"
-
-test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
-summary_file: "summary.loganalysis.{{ testname_unique }}.log"
-result_file: "result.loganalysis.{{ testname_unique }}.log"

ansible/roles/test/tasks/acl/acl_traffic_test/run_ping_test.yml
@@ -2,10 +2,13 @@
 # Execute ping and check the log.
 #-----------------------------------------
 
-- name: Get an unique timestamp to feed to testname_unique
+- name: Initialize some variables for loganalyzer
   set_fact:
-    unique_timestamp: "{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
-- include_vars: "vars/run_ping_test_vars.yml"
+    testname_unique: "{{ testname }}.{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
+- set_fact:
+    test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
+    summary_file: "summary.loganalysis.{{ testname_unique }}.log"
+    result_file: "result.loganalysis.{{ testname_unique }}.log"
 
 - name: Start log analyser
   include: roles/test/files/tools/loganalyzer/loganalyzer_init.yml

ansible/roles/test/tasks/acl/acl_traffic_test/run_ptf_test.yml
@@ -2,10 +2,13 @@
 # Send some TCP packets.
 #-----------------------------------------
 
-- name: Get an unique timestamp to feed to testname_unique
+- name: Initialize some variables for loganalyzer
   set_fact:
-    unique_timestamp: "{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
-- include_vars: "vars/run_ptf_test_vars.yml"
+    testname_unique: "{{ testname }}.{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
+- set_fact:
+    test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
+    summary_file: "summary.loganalysis.{{ testname_unique }}.log"
+    result_file: "result.loganalysis.{{ testname_unique }}.log"
 
 - block:
     - name: Execute ping from host to switch to validate link

ansible/roles/test/tasks/run_command_with_log_analyzer.yml
@@ -5,10 +5,17 @@
 #   3) Run cleanup if needed.
 #-----------------------------------------
 
-- name: Get an unique timestamp to feed to testname_unique
+- name: Initialize some variables for loganalyzer
   set_fact:
-    unique_timestamp: "{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
-- include_vars: "vars/run_config_test_vars.yml"
+    testname_unique: "{{ testname }}.{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
+- set_fact:
+    test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
+    loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml
+    loganalyzer_analyze: roles/test/files/tools/loganalyzer/loganalyzer_analyze.yml
+    match_file: loganalyzer_common_match.txt
+    ignore_file: loganalyzer_common_ignore.txt
+    summary_file: summary.loganalysis.{{ testname_unique }}.log
+    result_file: result.loganalysis.{{ testname_unique }}.log
 
 - block:
     - name: Initialize loganalizer. Put start marker to log file.

ansible/roles/test/tasks/run_loganalyzer.yml
@@ -3,10 +3,17 @@
 # or analyze-phase of loganalyzer.
 #-----------------------------------------
 
-- name: Get an unique timestamp to feed to testname_unique
+- name: Initialize some variables for loganalyzer
   set_fact:
-    unique_timestamp: "{{lookup('pipe','date +%Y-%m-%d-%H:%M:%S')}}"
-- include_vars: "vars/run_loganalyzer_vars.yml"
+    testname_unique: "{{ testname }}.{{ lookup('pipe','date +%Y-%m-%d-%H:%M:%S') }}"
+- set_fact:
+    test_out_dir: "{{ out_dir }}/{{ testname_unique }}"
+    loganalyzer_init: roles/test/files/tools/loganalyzer/loganalyzer_init.yml
+    loganalyzer_analyze: roles/test/files/tools/loganalyzer/loganalyzer_analyze.yml
+    match_file: loganalyzer_common_match.txt
+    ignore_file: loganalyzer_common_ignore.txt
+    summary_file: summary.loganalysis.{{ testname_unique }}.log
+    result_file: result.loganalysis.{{ testname_unique }}.log
 
 - name: Initialize loganalizer. Put start marker to log file.
   include: "{{ loganalyzer_init }}"