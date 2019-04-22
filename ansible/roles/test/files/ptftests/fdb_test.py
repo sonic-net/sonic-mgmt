e7adbb1c351d296b495ca75a2b849f7d90f9842c
Xin Wang <xinw@mellanox.com>
2019-04-22 19:00:22 +0800

    [fdb] Add FDB learning during config reload test
    This case is to ensure that all the FDB entries
    learned by SDK are correctly reported to upper
    layer in the initialization process after config
    reload.
    
    Signed-off-by: Xin Wang <xinw@mellanox.com>

--- ansible/roles/test/files/ptftests/fdb_test.py
+++ ansible/roles/test/files/ptftests/fdb_test.py
@@ -1,12 +1,13 @@
-import fdb
+import time
 import subprocess
+import logging
 
 from ipaddress import ip_address
-
 import ptf
-
 from ptf.base_tests import BaseTest
 from ptf.testutils import *
+
+import fdb
 
 class FdbTest(BaseTest):
 
@@ -82,4 +83,53 @@ class FdbTest(BaseTest):
 
                     for dummy_mac in self.dummy_mac_table[dst]:
                         self.test_l2_forwarding(arp_table[src], dummy_mac, src, dst)
+    #--------------------------------------------------------------------------
+
+
+class FdbConfigReloadTest(BaseTest):
+
+    def __init__(self):
+        BaseTest.__init__(self)
+        self.test_params = test_params_get()
+    #--------------------------------------------------------------------------
+
+    def setUp(self):
+        self.dataplane = ptf.dataplane_instance
+        self.router_mac = self.test_params["router_mac"]
+        self.vlan_ports = self.test_params["vlan_ports"].split()
+        self.lag_ports = self.test_params["lag_ports"].split()
+        self.dummy_mac_prefix = self.test_params["dummy_mac_prefix"]
+        self.vlan_port_idx = 0
+        self.lag_port_idx = 0
+    #--------------------------------------------------------------------------
+
+    def runTest(self):
+        max_time = 200  # seconds
+        i = 0
+        start_time = time.time()
+        while i < 0xffff - 128 and time.time() - start_time < max_time:
+            # Send frame to vlan and lag port in interleaved way
+            if i % 2 == 0:
+                port = self.vlan_ports[self.vlan_port_idx]
+                self.vlan_port_idx += 1
+                if self.vlan_port_idx >= len(self.vlan_ports):
+                    self.vlan_port_idx = 0
+            else:
+                port = self.lag_ports[self.lag_port_idx]
+                self.lag_port_idx += 1
+                if self.lag_port_idx >= len(self.lag_ports):
+                    self.lag_port_idx = 0
+
+            field_0 = i % 256
+            field_1 = i / 256
+            dummy_mac = self.dummy_mac_prefix + ":{:02x}:{:02x}".format(field_1, field_0)
+            pkt = simple_eth_packet(eth_dst=self.router_mac,
+                                    eth_src=dummy_mac,
+                                    eth_type=0x1234)
+            logging.debug("Send packet to port %s, src mac: %s" % (str(port), dummy_mac))
+            send(self, port, pkt)
+            i += 1
+
+            if i % len(self.lag_ports)*2 == 0:
+                time.sleep(0.1)
     #--------------------------------------------------------------------------
