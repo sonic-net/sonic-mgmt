812a8eaf3b4589cfd25edb107db5a912d503fea6
Stepan Blyschak <stepanb@mellanox.com>
2019-07-25 15:45:20 +0300

    [tests/ptfadapter] non need for disable_* config keys for newer ptf
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- tests/ptfadapter/__init__.py
+++ tests/ptfadapter/__init__.py
@@ -1,15 +1,3 @@
-import ptf
-
-# config ptf with basic parameters before any other ptf module import
-ptf.config = {
-    'disable_ipv6': False,
-    'disable_vxlan': False,
-    'disable_erspan': False,
-    'disable_geneve': False,
-    'disable_mpls': False,
-    'disable_nvgre': False,
-}
-
 from ptfadapter import PtfTestAdapter
 
 __all__ = ['PtfTestAdapter']
