519b1140aadf7d23b43e0884375dca43ed7d4f85
dawnbeauty <18810562248@163.com>
2019-07-28 15:59:04 -0500

    [pytest] Improve ansible_host (#1006)
    [pytest] adding ability to use stdout callback plugin when running ansible module
    [pytest ansible_host] Support 'module_ignore_errors' option
    
    Signed-off-by: Zhiqian Wu zhiqian.wu@nephosinc.com
    
    Description of PR
    Summary:
    
    - stdout callback
    
    With stdout callback plugin, we could get pretty-printed output of ansible module results.
    It is simple and convenient to read the outputs or print dict/list objects.
    So i add a function dump_ansible_results to do that. 'yaml' is the default stdout callback plugin, back port from ansible 2.5 in PR #1005 , you may choose another plugin whenever calling dump_ansible_results.
    
    - module_ignore_errors
    
    We need to ignore errors when execute some cmds with non-zero return codes.
    
    Signed-off-by: Zhiqian Wu <zhiqian.wu@nephosinc.com>

--- tests/ansible_host.py
+++ tests/ansible_host.py
@@ -1,3 +1,21 @@
+from ansible.plugins import callback_loader
+from ansible.errors import AnsibleError
+
+def dump_ansible_results(results, stdout_callback='yaml'):
+    cb = callback_loader.get(stdout_callback)
+    return cb._dump_results(results) if cb else results
+
+class AnsibleModuleException(AnsibleError):
+
+    """Sub-class AnsibleError when module exceptions occur."""
+
+    def __init__(self, msg, results=None):
+        super(AnsibleModuleException, self).__init__(msg)
+        self.results = results
+
+    def __str__(self):
+        return "{}\nAnsible Results => {}".format(self.message, dump_ansible_results(self.results))
+
 class ansible_host():
     
     def __init__(self, ansible_adhoc, hostname, is_local = False):
@@ -15,8 +33,10 @@ class ansible_host():
 
     def _run(self, *module_args, **complex_args):
    
+        module_ignore_errors = complex_args.pop('module_ignore_errors', False)
+
         res = self.module(*module_args, **complex_args)[self.hostname]
-        if res.is_failed:
-            raise Exception("run module {} failed, errmsg {}".format(self.module_name, res))
+        if res.is_failed and not module_ignore_errors:
+            raise AnsibleModuleException("run module {} failed".format(self.module_name), res)
 
         return res
