41528b3160ff07c7a3cd75de56d379e1cc0b9180
iris <iris_hsu@edge-core.com>
2019-08-08 17:08:57 +0800

    modify t0-64-32 src_ports range.

--- ansible/roles/test/files/ptftests/fib_test.py
+++ ansible/roles/test/files/ptftests/fib_test.py
@@ -96,11 +96,11 @@ class FibTest(BaseTest):
         self.balancing_test_ratio = self.test_params.get('balancing_test_ratio', self.DEFAULT_BALANCING_TEST_RATIO)
 
         # Provide the list of all UP interfaces with index in sequence order starting from 0
-        if self.test_params['testbed_type'] == 't1' or self.test_params['testbed_type'] == 't1-lag':
+        if self.test_params['testbed_type'] == 't1' or self.test_params['testbed_type'] == 't1-lag' or self.test_params['testbed_type'] == 't0-64-32':
             self.src_ports = range(0, 32)
         if self.test_params['testbed_type'] == 't1-64-lag':
             self.src_ports = [0, 1, 4, 5, 16, 17, 20, 21, 34, 36, 37, 38, 39, 42, 44, 45, 46, 47, 50, 52, 53, 54, 55, 58, 60, 61, 62, 63]
-        if self.test_params['testbed_type'] == 't0' or self.test_params['testbed_type'] == 't0-64-32':
+        if self.test_params['testbed_type'] == 't0':
             self.src_ports = range(1, 25) + range(28, 32)
         if self.test_params['testbed_type'] == 't0-56':
             self.src_ports = [0, 1, 4, 5, 8, 9] + range(12, 18) + [20, 21, 24, 25, 28, 29, 32, 33, 36, 37] + range(40, 46) + [48, 49, 52, 53]
