dff7666c7095051d5e7fd95649fd83255493c751
Xin Wang <xinw@mellanox.com>
2019-09-27 14:49:14 +0300

    Improve the scripts for preparing PTF interfaces
    Signed-off-by: Xin Wang <xinw@mellanox.com>

--- tests/scripts/change_mac.sh
+++ tests/scripts/change_mac.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-INTF_LIST=$(ifconfig | grep eth | cut -f 1 -d ' ')
+INTF_LIST=$(ls /sys/class/net | grep eth)
 
-for i in ${INTF_LIST}; do
-  prefix=$(ifconfig $i | grep HWaddr | cut -c39-53)
-  suffix=$( printf "%02x" ${i##eth})
-  mac=$prefix$suffix
-  echo $i $mac
-  ifconfig $i hw ether $mac
+for INTF in ${INTF_LIST}; do
+    ADDR="$(ip link show ${INTF} | grep ether | awk '{print $2}')"
+    PREFIX="$(cut -c1-15 <<< ${ADDR})"
+    SUFFIX="$(printf "%02x" ${INTF##eth})"
+    MAC="${PREFIX}${SUFFIX}"
+
+    echo "Update ${INTF} MAC address: ${ADDR}->$MAC"
+    ip link set dev ${INTF} address ${MAC}
 done
