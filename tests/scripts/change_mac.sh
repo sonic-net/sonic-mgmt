f83edae0075df9426ac8cae7402d82e013b0ef57
Xin Wang <xinw@mellanox.com>
2019-10-10 10:03:05 +0000

    Update per review comments

--- tests/scripts/change_mac.sh
+++ tests/scripts/change_mac.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-set -euo pipefail
-
-INTF_LIST=$(ls /sys/class/net | grep -E "^eth[0-9]+$")
-
-for INTF in ${INTF_LIST}; do
-    ADDR="$(ip link show ${INTF} | grep ether | awk '{print $2}')"
-    PREFIX="$(cut -c1-15 <<< ${ADDR})"
-    SUFFIX="$(printf "%02x" ${INTF##eth})"
-    MAC="${PREFIX}${SUFFIX}"
-
-    echo "Update ${INTF} MAC address: ${ADDR}->$MAC"
-    ip link set dev ${INTF} address ${MAC}
-done
+../../ansible/roles/test/files/helpers/change_mac.sh
