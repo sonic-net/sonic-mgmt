2822dc052ed9575c938aa667bf9308863a9a5742
Xin Wang <xinw@mellanox.com>
2019-10-10 09:12:51 +0000

    Update per review comments

--- ansible/roles/test/files/helpers/change_mac.sh
+++ ansible/roles/test/files/helpers/change_mac.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 INTF_LIST=$(ls /sys/class/net | grep -E "^eth[0-9]+$")
 
 for INTF in ${INTF_LIST}; do
-    ADDR="$(ip link show ${INTF} | grep ether | awk '{print $2}')"
+    ADDR="$(cat /sys/class/net/${INTF}/address)"
     PREFIX="$(cut -c1-15 <<< ${ADDR})"
     SUFFIX="$(printf "%02x" ${INTF##eth})"
     MAC="${PREFIX}${SUFFIX}"
