5608f1f0c5dfff8c0934c9eb38c6f9b1ef6eb5db
Bhavani <bhavani@cisco.com>
2019-10-24 21:51:19 -0700

    Cafy integration for gNMI scripts

---
+++ gnmi/gnmi_ap.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import argparse
+import grpc
+import os
+import sys
+import json
+from time import sleep
+from topology.topo_mgr.topo_mgr import Topology
+from framework.pytest.cafy import Cafy
+import pytest
+from logger.cafylog import CafyLog
+from topology.zap.zap import Zap
+from utils.helper import Helper
+from utils.cafyexception import CafyException
+from gnmi_base_ap import ApData, GnmiApBase
+import marshal
+log = CafyLog("GNMI AP")
+
+
+# Import the required Proto files from lib dir
+# Probably there's a better way of doing this.
+# sys.path.append(
+#    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+#                 '../../lib/'))
+
+# Add 3rd party python packages' paths (instead of setting PYTHONPATH)
+TP_DIR = "./../../godiva-test/lib"
+tp_dirs = os.listdir(TP_DIR)
+for tp_dir in tp_dirs:
+    sys.path.append(os.path.join(TP_DIR,tp_dir))
+
+import gnmi_test_lib as gnmiTestLib
+
+
+
+SWITCH_TO_HOST_PORT = 1
+SWITCH_TO_SWITCH_PORT = 2
+
+
+
+class TestGnmi(GnmiApBase):
+
+    def test_gnmi_Capability(self,sw_conn):
+        gnmi_san_tc._test_gnmi_Capability(sw_conn)
+
+
+    @pytest.mark.last
+    def test_max_connections(self,sw_conn):
+        gnmi_san_tc._test_max_connections()    
+
+
---
+++ gnmi/gnmi_base_ap.py
@@ -0,0 +1,42 @@
+# Cafy based Godiva Test
+import pytest
+import os
+from framework.ap_base import ApBase
+from logger.cafylog import CafyLog
+from topology.topo_mgr.topo_mgr import Topology
+from framework.pytest.cafy import Cafy
+from topology.zap.zap import Zap
+
+log = CafyLog("Godiva GNMI AP")
+
+class ApData:
+    
+    """
+    Definition of ApData Class
+    """
+
+    testbed = None
+    prefix = os.path.dirname(os.path.abspath(__file__))
+    if CafyLog.topology_file:
+        test_bed = CafyLog.topology_file
+    else:
+        test_bed = os.path.join(prefix,"gd_ap_topo.json")
+
+    if CafyLog.test_input_file:
+        input_file = CafyLog.test_input_file
+    else:
+        input_file = os.path.join(prefix,"gd_ap_input_file.json")
+    testbed = Topology(topo_file=test_bed)
+    zap = Zap(test_input_file=input_file,topo_file=test_bed)
+    gnmi_feature_dict = zap.get_feature_configuration("p4")  
+    
+class GnmiApBase(ApBase):
+    # GNMI Base Variables
+    ApData.svr_addr = ApData.gnmi_feature_dict['R1']['svr_addr']
+    ApData.port_addr = ApData.gnmi_feature_dict['R1']['svr_port']
+    ApData.proto_dump_file = ApData.gnmi_feature_dict['R1']['proto_dump_file']
+    ApData.sw_name = ApData.gnmi_feature_dict['R1']['name']
+
+    # Port Configuration Variables
+    
+
