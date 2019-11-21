f7cc969d430a5e4744e4828bbc358e18fe841248
Guohan Lu <gulv@microsoft.com>
2019-11-21 00:17:44 +0000

    [pytest]: convert bgp speaker test to pytest
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- tests/ptf_runner.py
+++ tests/ptf_runner.py
@@ -1,7 +1,8 @@
 import pipes
 
 def ptf_runner(host, testdir, testname, platform_dir, params={}, \
-               platform="remote", qlen=0, relax=True, debug_level="info", log_file=None):
+               platform="remote", qlen=0, relax=True, debug_level="info", \
+               socket_recv_size=None, log_file=None):
 
     ptf_test_params = ";".join(["{}={}".format(k, repr(v)) for k, v in params.items()])
 
@@ -18,6 +19,8 @@ def ptf_runner(host, testdir, testname, platform_dir, params={}, \
         cmd += " --debug {}".format(debug_level)
     if log_file:
         cmd += " --log-file {}".format(log_file)
+    if socket_recv_size:
+        cmd += " --socket-recv-size {}".format(socket_recv_size)
 
     res = host.shell(cmd, chdir="/root")
 
