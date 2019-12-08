c96eaa2dbe1c149ecd8f0793e161ede435877e57
lguohan <lguohan@gmail.com>
2019-12-08 00:06:11 -0800

    [pytest]: use ip instead of hostname for snmp_facts module (#1266)
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- tests/snmp/test_snmp_queue.py
+++ tests/snmp/test_snmp_queue.py
@@ -6,8 +6,9 @@ def test_snmp_queues(ansible_adhoc, testbed, creds):
     hostname = testbed['dut']
     ans_host = AnsibleHost(ansible_adhoc, hostname)
     lhost = AnsibleHost(ansible_adhoc, 'localhost', True)
+    hostip = ans_host.host.options['inventory_manager'].get_host(hostname).vars['ansible_host']
 
-    snmp_facts = lhost.snmp_facts(host=hostname, version="v2c", community=creds["snmp_rocommunity"])['ansible_facts']
+    snmp_facts = lhost.snmp_facts(host=hostip, version="v2c", community=creds["snmp_rocommunity"])['ansible_facts']
 
     for k, v in snmp_facts['snmp_interfaces'].items():
         if "Ethernet" in v['description']:
