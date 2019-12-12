996edc1d86e3eb10a04b1d441005e44a6955ae9d
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2019-12-12 10:54:28 -0800

    Update topo_facts from configlet, if present. (#1270)
    * Update topo_facts from configlet, if present.
    
    * 1) Let any topology file for configlet has the toponame with suffix as "-clet"
    2) For every configlet topo, there be a corresponding  non-configlet version
       e.g. t1-64-lag & t1-64-lag-clet.
       The configlet version would be same as the other, except for few missing
       VMs that are added later through configlets.
       But for test data, like topo_facts, we need complete info, so we use the
       non-configlet version of the topo file.
    
    * Removed an unused code chunk and an import
    
    * Fixed few bugs.

--- ansible/vars/configlet/t1-64-lag-clet/apply_clet.sh
+++ ansible/vars/configlet/t1-64-lag-clet/apply_clet.sh
@@ -3,4 +3,4 @@
 # Sleep to let all BGP sessions go up & running before adding a T0
 sleep 1m
 /usr/bin/configlet -j /etc/sonic/clet-to_clear.json -d
-/usr/bin/configlet -j /etc/sonic/clet-add_20T0.json -u
+/usr/bin/configlet -j /etc/sonic/clet-add.json -u
