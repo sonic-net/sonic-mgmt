3f13445e0066859cbd87cfc37c04528cc6078510
Sujin Kang <sujkang@microsoft.com>
2019-12-23 12:22:42 -0800

    Add HW Sku for platform info pytest (#1301)

--- tests/common/mellanox_data.py
+++ tests/common/mellanox_data.py
@@ -1,10 +1,58 @@
 
-SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "ACS-MSN2740", "ACS-MSN2100", "ACS-MSN2410", "ACS-MSN2010"]
+SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "Mellanox-SN2700-D48C8", "ACS-MSN2740", "ACS-MSN2100", "ACS-MSN2410", "ACS-MSN2010"]
 SPC2_HWSKUS = ["ACS-MSN3700", "ACS-MSN3700C", "ACS-MSN3800"]
 SWITCH_HWSKUS = SPC1_HWSKUS + SPC2_HWSKUS
 
 SWITCH_MODELS = {
     "ACS-MSN2700": {
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 2
+        },
+        "ports": {
+            "number": 32
+        }
+    },
+    "Mellanox-SN2700": {
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 2
+        },
+        "ports": {
+            "number": 32
+        }
+    },
+    "Mellanox-SN2700-D48C8": {
         "reboot": {
             "cold_reboot": True,
             "fast_reboot": True,
