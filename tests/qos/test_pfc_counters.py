6d13238e576a13485c84c0cc1da09a1c4d5f77df
Wei Bai <baiwei0427@gmail.com>
2020-01-08 17:50:33 -0800

    Merge run_pfc and run_fc into run_test & Filter inactive interfaces

--- tests/qos/test_pfc_counters.py
+++ tests/qos/test_pfc_counters.py
@@ -40,9 +40,10 @@ def setup_testbed(ansible_adhoc, testbed, leaf_fanouts):
         file_src = os.path.join(os.path.dirname(__file__), PFC_GEN_FILE_RELATIVE_PATH)
         peerdev_ans.copy(src = file_src, dest = PFC_GEN_FILE_DEST, force = True)
 
-def run_pfc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, pause_time):
+def run_test(is_pfc, ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, pause_time):
     """
-    @Summary: Run the priority-based flow control (PFC) test case
+    @Summary: Run test for Ethernet flow control (FC) or priority-based flow control (PFC)
+    @param is_pfc: If this test is for PFC?
     @param ansible_adhoc: Fixture provided by the pytest-ansible package. Source of the various device objects. It is
     mandatory argument for the class constructors.
     @param testbed: Testbed information
@@ -52,79 +53,63 @@ def run_pfc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, pause_time):
     """
     setup_testbed(ansible_adhoc, testbed, leaf_fanouts)
     conn_facts = conn_graph_facts['device_conn']
-     
-    """ Generate PFC packets for all the priority queues of all the interfaces """
-    for intf in conn_facts:
+    
+    dut_hostname = testbed['dut']
+    dut_ans = AnsibleHost(ansible_adhoc, dut_hostname)
+    int_status = dut_ans.show_interface(command = "status")['ansible_facts']['int_status']
+    
+    """ We only test active interfaces """
+    active_intfs = []
+    for intf in int_status:
+        if int_status[intf]['admin_state'] == 'up' and \
+           int_status[intf]['oper_state'] == 'up':
+            active_intfs.append(intf)
+        
+    """ Generate PFC or FC packets for active interfaces """
+    for intf in active_intfs:
+        if intf not in conn_facts:
+            continue
+        
         peer_device = conn_facts[intf]['peerdevice']
         peer_port = conn_facts[intf]['peerport']
         peer_port_name = eos_to_linux_intf(peer_port)
 
         peerdev_ans = AnsibleHost(ansible_adhoc, peer_device)
-        for priority in range(PRIO_COUNT):
-            cmd = "sudo python %s -i %s -p %d -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, 2 ** priority, pause_time, PKT_COUNT)
+        if is_pfc:
+            for priority in range(PRIO_COUNT):
+                cmd = "sudo python %s -i %s -p %d -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, 2 ** priority, pause_time, PKT_COUNT)
+                peerdev_ans.command(cmd)
+        else:
+            cmd = "sudo python %s -i %s -g -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, pause_time, PKT_COUNT)
             peerdev_ans.command(cmd)
-	
+            
     """ SONiC takes some time to update counters in database """
     time.sleep(5)
 
     """ Check results """
-    dut_hostname = testbed['dut']
-    dut_ans = AnsibleHost(ansible_adhoc, dut_hostname)
     counter_facts = dut_ans.sonic_pfc_counters(method = "get")['ansible_facts']
 
-    for intf in conn_facts:
+    for intf in active_intfs:
         assert intf in counter_facts
         assert 'Rx' in counter_facts[intf]
-        assert counter_facts[intf]['Rx'] == [str(PKT_COUNT)] * PRIO_COUNT
-
-def run_fc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, pause_time):
-    """
-    @Summary: Run the flow control (FC) test case
-    @param ansible_adhoc: Fixture provided by the pytest-ansible package. Source of the various device objects. It is
-    mandatory argument for the class constructors.
-    @param testbed: Testbed information
-    @param conn_graph_facts: Testbed topology connectivity information
-    @param leaf_fanouts: Leaf fanout switches
-    @param pause_time: Pause time quanta (0-65535) in the frame. 0 means unpause.
-    """
-    setup_testbed(ansible_adhoc, testbed, leaf_fanouts)
-    conn_facts = conn_graph_facts['device_conn']
-
-    """ Generate flow control packets for all the interfaces """
-    for intf in conn_facts:
-        peer_device = conn_facts[intf]['peerdevice']
-        peer_port = conn_facts[intf]['peerport']
-        peer_port_name = eos_to_linux_intf(peer_port)
-
-        peerdev_ans = AnsibleHost(ansible_adhoc, peer_device)
-        cmd = "sudo python %s -i %s -g -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, pause_time, PKT_COUNT)
-        peerdev_ans.command(cmd)
-	
-    """ SONiC takes some time to update counters in database """
-    time.sleep(5)
-
-    """ Check resuls. SONiC should not update PFC counters when receiving global pause/unpause frames """
-    dut_hostname = testbed['dut']
-    dut_ans = AnsibleHost(ansible_adhoc, dut_hostname)
-    counter_facts = dut_ans.sonic_pfc_counters(method = "get")['ansible_facts']
-    
-    for intf in conn_facts:
-        assert intf in counter_facts
-        assert 'Rx' in counter_facts[intf]
-        assert counter_facts[intf]['Rx'] == ['0'] * PRIO_COUNT
+        
+        if is_pfc:
+            assert counter_facts[intf]['Rx'] == [str(PKT_COUNT)] * PRIO_COUNT
+        else:
+            assert counter_facts[intf]['Rx'] == ['0'] * PRIO_COUNT
 
 def test_pfc_pause(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts):
     """ @Summary: Run PFC pause frame (pause time quanta > 0) tests """
-    run_pfc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 65535)
-			
+    run_test(True, ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 65535)
+	
 def test_pfc_unpause(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts):
     """ @Summary: Run PFC unpause frame (pause time quanta = 0) tests """
-    run_pfc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 0)        
+    run_test(True, ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 0)        
 
 def test_fc_pause(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts):
     """ @Summary: Run FC pause frame (pause time quanta > 0) tests """
-    run_fc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 65535)
+    run_test(False, ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 65535)
 
 def test_fc_unpause(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts):
     """ @Summary: Run FC pause frame (pause time quanta = 0) tests """ 
-    run_fc(ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 0) 	
+    run_test(False, ansible_adhoc, testbed, conn_graph_facts, leaf_fanouts, 0)
