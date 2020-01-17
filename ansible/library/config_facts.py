f22288fc2ba21d423a136606f4dd0f722f8c825c
Mykola Faryma <mykolaf@mellanox.com>
2020-01-17 09:38:04 +0000

    [config_facts] return config w/o additional parsing
    Signed-off-by: Mykola Faryma <mykolaf@mellanox.com>

--- ansible/library/config_facts.py
+++ ansible/library/config_facts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import json
-import ipaddr as ipaddress
 from collections import defaultdict
 from natsort import natsorted
 
@@ -25,177 +24,46 @@ options:
 '''
 
 PERSISTENT_CONFIG_PATH = "/etc/sonic/config_db.json"
+TABLE_NAME_SEPARATOR = '|'
 
-def parse_meta_servers(config):
-    dhcp_servers = []
-    ntp_servers = []
-
-    if "DHCP_SERVER" in config:
-        dhcp_servers = config["DHCP_SERVER"].keys()
-
-    if "NTP_SERVER" in config:
-        dhcp_servers = config["NTP_SERVER"].keys()
-
-    return dhcp_servers, ntp_servers
-
-def parse_meta(config):
-
-    localhost_config = config['DEVICE_METADATA']['localhost']
-
-    hostname = localhost_config.get('hostname', None)
-    hwsku = localhost_config.get('hwsku', None)
-    deployment_id = localhost_config.get('deployment_id', None)
-    bgp_asn = localhost_config.get('bgp_asn', None)
-    mac = localhost_config.get('mac', None)
-    platform = localhost_config.get('platform', None)
-
-    return hostname, hwsku, deployment_id, bgp_asn, mac, platform
-
-def parse_bgp(config):
-
-    bgp_sessions = []
-    bgp_peers_with_range = []
-
-    try:
-        for addr in config["BGP_NEIGHBOR"]:
-            bgp_sessions.append({
-                'name': config["BGP_NEIGHBOR"][addr]['name'],
-                'addr': addr,
-                'peer_addr': config["BGP_NEIGHBOR"][addr]['local_addr'],
-                'asn' : int(config["BGP_NEIGHBOR"][addr]['asn'])
-            })
-
-        for peer_range in config["BGP_PEER_RANGE"].values():
-            bgp_peers_with_range.append({
-                'name': peer_range['name'],
-                'ip_range' : peer_range['ip_range']
-            })
-    except KeyError:
-        pass
-
-    return bgp_sessions, bgp_peers_with_range
-
-
-def parse_interfaces(config):
-    router_intfs = []
-    vlan_intfs = []
-    pc_intfs = []
-    lo_intfs = []
-    mgmt_intfs = {}
-
-    if "INTERFACE" in config:
-        for entry in config["INTERFACE"].keys():
-            if '|' not in entry: 
-                continue
-            interface_name, subnet = entry.split('|')
-            subnet = ipaddress.IPNetwork(subnet)
-            router_intfs.append({
-                'name' : interface_name,
-                'attachto' : interface_name,
-                'subnet' : str(subnet),
-                'addr' : str(subnet.ip),
-                'prefixlen' : subnet.prefixlen,
-                'mask' : str(subnet.netmask)
-            })
-    if "VLAN_INTERFACE" in config:
-        for entry in config["VLAN_INTERFACE"].keys():
-            if '|' not in entry: 
-                continue
-            vlan_name, subnet = entry.split('|')
-            subnet = ipaddress.IPNetwork(subnet)
-            vlan_intfs.append({
-                'name' : vlan_name,
-                'attachto' : vlan_name,
-                'subnet' : str(subnet),
-                'addr' : str(subnet.ip),
-                'prefixlen' : subnet.prefixlen,
-                'mask' : str(subnet.netmask)
-            })
-    if "PORTCHANNEL_INTERFACE" in config:
-        for entry in config["PORTCHANNEL_INTERFACE"].keys():
-            if '|' not in entry: 
-                continue
-            pc_name, subnet = entry.split('|')
-            subnet = ipaddress.IPNetwork(subnet)
-            pc_intfs.append({
-                'name' : pc_name,
-                'attachto' : pc_name,
-                'subnet' : str(subnet),
-                'addr' : str(subnet.ip),
-                'prefixlen' : subnet.prefixlen,
-                'mask' : str(subnet.netmask)
-            })
-
-    if "MGMT_INTERFACE" in config:
-        for entry, v in config["MGMT_INTERFACE"].iteritems():
-            if '|' not in entry: 
-                continue
-            interface_name, subnet = entry.split('|')
-            # Looking for IPv4 mgmt interface here
+def format_config(json_data):
+    """Format config data. 
+    Returns:
+        Config data in a dictionary form of TABLE, KEY, [ ENTRY ], FV
+    Example:
+    {
+    "VLAN_MEMBER": {
+        "Vlan1000|Ethernet2": {
+            "tagging_mode": "untagged"
+        },
+        "Vlan1000|Ethernet4": {
+            "tagging_mode": "untagged"
+        }
+    }
+    Is converted into 
+     'VLAN_MEMBER': {'Vlan1000': {'Ethernet10': {'tagging_mode': 'untagged'},
+                                  'Ethernet12': {'tagging_mode': 'untagged'}
+                    }
+    """
+    res = {}
+    for table, item in json_data.items():
+        data = {}
+        for key, entry in item.items():
             try:
-                subnet = ipaddress.IPNetwork(subnet, version=4)
+                (key_l1, key_l2)= key.split(TABLE_NAME_SEPARATOR, 1)
+                data.setdefault(key_l1, {})[key_l2] = entry
             except ValueError:
-                continue
-            mgmt_intfs = {
-                'name' : interface_name,
-                'alias' : interface_name,
-                'subnet' : str(subnet),
-                'addr' : str(subnet.ip),
-                'prefixlen' : subnet.prefixlen,
-                'mask' : str(subnet.netmask),
-                'gwaddr' : v['gwaddr']
-            }
+                # This is a single level key
+                data.setdefault(key, entry)
 
-    if "LOOPBACK_INTERFACE" in config:
-        for entry, v in config["LOOPBACK_INTERFACE"].iteritems():
-            if '|' not in entry: 
-                continue
-            interface_name, subnet = entry.split('|')
-            subnet = ipaddress.IPNetwork(subnet)
-            lo_intfs.append({
-                'name' : interface_name,
-                'subnet' : str(subnet),
-                'addr' : str(subnet.ip),
-                'prefixlen' : subnet.prefixlen,
-                'mask' : str(subnet.netmask),
-            })
+        res.setdefault(table, data)
 
-    return router_intfs, vlan_intfs, pc_intfs, lo_intfs, mgmt_intfs
+    return res
 
 
-def parse_ports(config):
-    ports = {}
-    portchannels = {}
-    vlans = {}
+def create_maps(config):
+    """ Create a map of SONiC port name to physical port index """
 
-    if "PORT" in config:
-        for k, v in config["PORT"].items():
-            ports[k] = {
-                'alias' : v['alias'],
-                'speed' : v['speed'],
-                'admin_status' : v.get('admin_status', 'down'),
-                'name' : k
-            }
-    if "VLAN" in config:
-        for k, v in config["VLAN"].items():
-            vlans[k] = {
-                'vlanid' : v['vlanid'],
-                'name' : k,
-                'members' : [ entry.split('|')[-1] for entry in config["VLAN_MEMBER"] if k in entry ]
-            }
-    if "PORTCHANNEL" in config:
-        for k, v in config["PORTCHANNEL"].items():
-            portchannels[k] = {
-                'admin_status' : v.get('admin_status', 'down'),
-                'name' : k,
-                'members' : v['members']
-            }
-
-    return ports, vlans, portchannels
-
-def create_alias_maps(config):
-    # Create a map of SONiC port name to physical port index
-    # Start by creating a list of all port names
     port_name_list = config["PORT"].keys()
     port_name_list_sorted = natsorted(port_name_list)
 
@@ -208,72 +76,32 @@ def create_alias_maps(config):
     # Create inverse mapping between port name and alias
     port_alias_to_name_map = {v: k for k, v in port_name_to_alias_map.iteritems()}
 
-    return port_name_to_alias_map, port_alias_to_name_map, port_index_map
-
-
-def parse_acl(config):
-    return config.get('ACL_TABLE', {})
-
-
-def parse_devices(config):
-
-    devices = config.get('DEVICE_NEIGHBOR', {})
-    neighbors = config.get('DEVICE_NEIGHBOR_METADATA', {})
-    return devices, neighbors
+    return {
+    'port_name_to_alias_map' : port_name_to_alias_map,
+    'port_alias_to_name_map' : port_alias_to_name_map,
+    'port_index_map' : port_index_map
+    }
 
 
 def get_running_config(module):
 
     rt, out, err = module.run_command("sonic-cfggen -d --print-data")
     if rt != 0:
-        module.fail_json(msg="failed to dump running config!")
+        module.fail_json(msg="Failed to dump running config! {}".format(err))
     json_info = json.loads(out)
     return json_info
 
 
-def parse_config(config):
+def get_facts(config):
+    """ Create the facts dict """
 
-    # Generate results
     Tree = lambda: defaultdict(Tree)
 
     results = Tree()
-    hostname, hwsku, deployment_id, bgp_asn, mac, platform = parse_meta(config)
-    results['config_hwsku'] = hwsku
-    results['deployment_id'] = deployment_id
-    results['config_hostname'] = hostname
-    results['inventory_hostname'] = hostname
-    results['config_bgp_asn'] = bgp_asn
 
-    bgp_sessions, bgp_peers_with_range = parse_bgp(config)
-    results['config_bgp'] = bgp_sessions
-    results['config_bgp_peers_with_range'] = bgp_peers_with_range
+    results.update(format_config(config))
 
-    router_intfs, vlan_intfs, pc_intfs, lo_intfs, mgmt_intfs = parse_interfaces(config)
-    results['config_interfaces'] = sorted(router_intfs, key=lambda x: x['name'])
-    results['config_vlan_interfaces'] = sorted(vlan_intfs, key=lambda x: x['name'])
-    results['config_portchannel_interfaces'] = sorted(pc_intfs, key=lambda x: x['name'])
-    results['config_mgmt_interface'] = mgmt_intfs
-    results['config_lo_interfaces'] = lo_intfs
-
-    ports, vlans, portchannels = parse_ports(config)
-    results['config_ports'] = ports
-    results['config_vlans'] = vlans
-    results['config_portchannels'] = portchannels
-
-    results['config_acls'] = parse_acl(config)
-
-    port_name_to_alias_map, port_alias_to_name_map, port_index_map = create_alias_maps(config)
-    results['config_port_indices'] = port_index_map
-    results['config_port_name_to_alias_map'] = port_name_to_alias_map
-    results['config_port_alias_to_name_map'] = port_alias_to_name_map
-
-    devices, neighbors = parse_devices(config)
-    results['config_neighbors'] = neighbors
-    results['config_devices'] = devices
-
-    dhcp_servers, ntp_servers = parse_meta_servers(config) 
-    results['dhcp_servers'] = dhcp_servers
-    results['ntp_servers'] = ntp_servers
+    results.update(create_maps(config))
 
     return results
 
@@ -295,14 +123,13 @@ def main():
                 config = json.load(f)
         elif m_args["source"] == "running":    
             config = get_running_config(module)
-
-        results = parse_config(config)
+        results = get_facts(config)
         module.exit_json(ansible_facts=results)
     except Exception as e:
         module.fail_json(msg=e.message)
 
 
-from ansible.module_utils.basic import *
+from ansible.module_utils.basic import AnsibleModule
 
 if __name__ == "__main__":
     main()
