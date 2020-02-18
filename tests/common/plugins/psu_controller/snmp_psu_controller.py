5fc583584a529ce5eeeef40732744b0bd5ad436a
Xin Wang <xinw@mellanox.com>
2020-02-18 15:30:27 +0800

    Refactor the psu_controller fixture
    * Move the psu_contoller fixture from the tests/platform folder to tests/common/plugins/psu_controller
    * Add PDU host information to inventory file
    * Move the mellanox specific psu controller to tests/common/plugins/psu_controller
    * Update the mellanox specific psu controller to a common snmp_psu_controller
    * Update the psu_controller fixture to support reading PDU information from host
    * Deprecate the requirement of aisc_type information for creating psu_controller
    * Update related scripts to use the new psu_controller fixture
    
    ******************************** ATTENTION PLEASE *******************************************
    After this commit is merged, please ensure to add pdu_host info into your inventory file. Please refer
    to sonic-mgmt/tests/common/plugins/psu_controller/README.md for how to add pdu_host
    information to inventory. Otherwise, the psu_controller fixture will be None in test scripts and
    the test cases using this fixture will be skipped.
    ************************************************************************************************
    
    Signed-off-by: Xin Wang <xinw@mellanox.com>

--- tests/common/plugins/psu_controller/snmp_psu_controller.py
+++ tests/common/plugins/psu_controller/snmp_psu_controller.py
@@ -1,92 +1,12 @@
 """
-Mellanox specific PSU controller
+This module contains classes for PSU controllers that supports the SNMP management protocol.
 
-This script contains illustrative functions and class for creating PSU controller based on Mellanox lab configuration.
-
-Some actual configurations were or replaced with dummy configurations.
+The classes must implement the PsuControllerBase interface defined in controller_base.py.
 """
 import logging
-import subprocess
 
-import paramiko
-
-from psu_controller import PsuControllerBase
-
-
-def run_local_cmd(cmd):
-    """
-    @summary: Helper function for run command on localhost -- the sonic-mgmt container
-    @param cmd: Command to be executed
-    @return: Returns whatever output to stdout by the command
-    @raise: Raise an exception if the command return code is not 0.
-    """
-    process = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    ret_code = process.returncode
-
-    if ret_code != 0:
-        raise Exception("ret_code=%d, error message=%s. cmd=%s" % (ret_code, stderr, ' '.join(cmd)))
-
-    return stdout
-
-
-def connect_mellanox_server():
-    """
-    @summary: Connect to a server on Mellanox lab network via SSH
-    @return: Returns a paramiko.client.SSHClient object which can be used for running commands
-    """
-    mellanox_server = None
-    try:
-        mellanox_server = paramiko.client.SSHClient()
-        mellanox_server.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
-        mellanox_server.connect("a_mellanox_server", username="username", password="password")
-    except Exception as e:
-        logging.debug("Failed to connect to mellanox server, exception: " + repr(e))
-    return mellanox_server
-
-
-def find_psu_controller_conf_file(server):
-    """
-    @summary: Find the exact location of the configuration file which contains mapping between PSU controllers and DUT
-              switches.
-    @param server: The paramiko.client.SSHClient object connected to a Mellanox server
-    @return: Returns the exact path of the configuration file
-    """
-    result = None
-    try:
-        locations = ("/path1", "/path2")
-        config_file_name = "psu_controller_configuration_file.txt"
-        for location in locations:
-            _, stdout, stderr = server.exec_command("find %s -name %s" % (location, config_file_name))
-
-            lines = stdout.readlines()
-            if len(lines) > 0:
-                result = lines[0].strip()
-                break
-    except paramiko.SSHException as e:
-        logging.debug("Failed to find psu controller configuration file location, exception: " + repr(e))
-    return result
-
-
-def get_psu_controller_host(hostname, server, conf_file_location):
-    """
-    @summary: Check the configuration file to find out IP address of the PDU controlling power to PSUs of DUT.
-    @param hostname: Hostname of the SONiC DUT
-    @param server: The paramiko.client.SSHClient object connected to a Mellanox server
-    @param conf_file_location: Exact path of the configuration file on the Mellanox server
-    @return: Returns IP address of the PDU controlling power to PSUs of DUT
-    """
-    result = None
-    try:
-        _, stdout, stderr = server.exec_command("grep %s %s" % (hostname, conf_file_location))
-        for line in stdout.readlines():
-            fields = line.strip().split()
-            if len(fields) == 2:
-                result = fields[1]
-                break
-    except paramiko.SSHException as e:
-        logging.debug("Failed to get psu controller host, exception: " + repr(e))
-    return result
+from controller_base import PsuControllerBase
+from controller_base import run_local_cmd
 
 
 def get_psu_controller_type(psu_controller_host):
@@ -124,40 +44,47 @@ class SentrySwitchedCDU(PsuControllerBase):
     CONTROL_ON = "1"
     CONTROL_OFF = "2"
 
-    def _get_psu_ports(self):
+    def _get_pdu_ports(self):
         """
         @summary: Helper method for getting PDU ports connected to PSUs of DUT
+
+        The PDU ports connected to DUT must have hostname of DUT configured in port name/description.
+        This method depends on this configuration to find out the PDU ports connected to PSUs of specific DUT.
         """
         try:
             cmd = "snmpwalk -v 1 -c public -Ofenq %s %s " % (self.controller, self.PORT_NAME_BASE_OID)
             stdout = run_local_cmd(cmd)
             for line in stdout.splitlines():
-                if self.hostname in line:
+                if self.hostname in line:  # PDU port name/description should have DUT hostname
                     fields = line.split()
                     if len(fields) == 2:
-                        # Remove the preceeding PORT_NAME_BASE_OID, remaining string is the PDU port ID
+                        # Remove the preceding PORT_NAME_BASE_OID, remaining string is the PDU port ID
                         self.pdu_ports.append(fields[0].replace(self.PORT_NAME_BASE_OID, ''))
         except Exception as e:
             logging.debug("Failed to get ports controlling PSUs of DUT, exception: " + repr(e))
 
     def __init__(self, hostname, controller):
+        logging.info("Initializing " + self.__class__.__name__)
         PsuControllerBase.__init__(self)
         self.hostname = hostname
         self.controller = controller
         self.pdu_ports = []
-        self._get_psu_ports()
+        self._get_pdu_ports()
         logging.info("Initialized " + self.__class__.__name__)
 
     def turn_on_psu(self, psu_id):
         """
         @summary: Use SNMP to turn on power to PSU of DUT specified by psu_id
 
-        There is a limitation in the Mellanox configuration. Currently we can just find out which PDU ports are
-        connected to PSUs of which DUT. But we cannot find out the exact mapping between PDU ports and PSUs of DUT.
+        DUT hostname must be configured in PDU port name/description. But it is hard to specify which PDU port is
+        connected to the first PSU of DUT and which port is connected to the second PSU.
+
+        Because of this, currently we just find out which PDU ports are connected to PSUs of which DUT. We cannot
+        find out the exact mapping between PDU ports and PSUs of DUT.
 
         To overcome this limitation, the trick is to convert the specified psu_id to integer, then calculate the mode
         upon the number of PSUs on DUT. The calculated mode is used as an index to get PDU ports ID stored in
-        self.pdu_ports.
+        self.pdu_ports. But still, we cannot gurante that psu_id 0 is first PSU of DUT, and so on.
 
         @param psu_id: ID of the PSU on SONiC DUT
         @return: Return true if successfully execute the command for turning on power. Otherwise return False.
@@ -177,12 +104,15 @@ class SentrySwitchedCDU(PsuControllerBase):
         """
         @summary: Use SNMP to turn off power to PSU of DUT specified by psu_id
 
-        There is a limitation in the Mellanox configuration. Currently we can just find out which PDU ports are
-        connected to PSUs of which DUT. But we cannot find out the exact mapping between PDU ports and PSUs of DUT.
+        DUT hostname must be configured in PDU port name/description. But it is hard to specify which PDU port is
+        connected to the first PSU of DUT and which port is connected to the second PSU.
+
+        Because of this, currently we just find out which PDU ports are connected to PSUs of which DUT. We cannot
+        find out the exact mapping between PDU ports and PSUs of DUT.
 
         To overcome this limitation, the trick is to convert the specified psu_id to integer, then calculate the mode
         upon the number of PSUs on DUT. The calculated mode is used as an index to get PDU ports ID stored in
-        self.pdu_ports.
+        self.pdu_ports. But still, we cannot gurante that psu_id 0 is first PSU of DUT, and so on.
 
         @param psu_id: ID of the PSU on SONiC DUT
         @return: Return true if successfully execute the command for turning off power. Otherwise return False.
@@ -202,12 +132,15 @@ class SentrySwitchedCDU(PsuControllerBase):
         """
         @summary: Use SNMP to get status of PDU ports supplying power to PSUs of DUT
 
-        There is a limitation in the Mellanox configuration. Currently we can just find out which PDU ports are
-        connected to PSUs of which DUT. But we cannot find out the exact mapping between PDU ports and PSUs of DUT.
+        DUT hostname must be configured in PDU port name/description. But it is hard to specify which PDU port is
+        connected to the first PSU of DUT and which port is connected to the second PSU.
+
+        Because of this, currently we just find out which PDU ports are connected to PSUs of which DUT. We cannot
+        find out the exact mapping between PDU ports and PSUs of DUT.
 
         To overcome this limitation, the trick is to convert the specified psu_id to integer, then calculate the mode
         upon the number of PSUs on DUT. The calculated mode is used as an index to get PDU ports ID stored in
-        self.pdu_ports.
+        self.pdu_ports. But still, we cannot gurante that psu_id 0 is first PSU of DUT, and so on.
 
         @param psu_id: Optional. If specified, only return status of PDU port connected to specified PSU of DUT. If
                        omitted, return status of all PDU ports connected to PSUs of DUT.
@@ -222,11 +155,10 @@ class SentrySwitchedCDU(PsuControllerBase):
             for line in stdout.splitlines():
                 for idx, port in enumerate(self.pdu_ports):
                     port_oid = self.PORT_STATUS_BASE_OID + port
-                    if port_oid in line:
-                        fields = line.strip().split()
-                        if len(fields) == 2:
-                            status = {"psu_id": idx, "psu_on": True if fields[1] == self.STATUS_ON else False}
-                            results.append(status)
+                    fields = line.strip().split()
+                    if len(fields) == 2 and fields[0] == port_oid:
+                        status = {"psu_id": idx, "psu_on": True if fields[1] == self.STATUS_ON else False}
+                        results.append(status)
             if psu_id is not None:
                 idx = int(psu_id) % len(self.pdu_ports)
                 results = results[idx:idx+1]
@@ -239,31 +171,18 @@ class SentrySwitchedCDU(PsuControllerBase):
         pass
 
 
-def make_mellanox_psu_controller(hostname):
+def get_psu_controller(controller_ip, dut_hostname):
     """
-    @summary: For creating different type of PSU controller based on Mellanox lab configuration.
-    @param hostname: Hostname of the SONiC DUT
-    @return: Returns an instance of PSU controller
+    @summary: Factory function to create the actual PSU controller object.
+    @return: The actual PSU controller object. Returns None if something went wrong.
     """
-    mellanox_server = connect_mellanox_server()
-    if not mellanox_server:
-        return None
 
-    conf_file_location = find_psu_controller_conf_file(mellanox_server)
-    logging.info("conf_file_location: %s" % conf_file_location)
-    if not conf_file_location:
-        return None
-
-    psu_controller_host = get_psu_controller_host(hostname, mellanox_server, conf_file_location)
-    logging.info("psu_controller_host: %s" % psu_controller_host)
-    if not psu_controller_host:
-        return None
-
-    psu_controller_type = get_psu_controller_type(psu_controller_host)
-    logging.info("psu_controller_type: %s" % psu_controller_type)
+    psu_controller_type = get_psu_controller_type(controller_ip)
     if not psu_controller_type:
         return None
 
     if "Sentry Switched CDU" in psu_controller_type:
-        logging.info("Initializing PSU controller instance")
-        return SentrySwitchedCDU(hostname, psu_controller_host)
+        logging.info("Initializing PSU controller")
+        return SentrySwitchedCDU(dut_hostname, controller_ip)
+
+    return None
