bad63f84d69c03a43fb8701eee1848f3b99ef161
Perumal Venkatesh <pevenkat@cisco.com>
2020-03-04 15:11:58 -0800

    infra changes

--- gnmi/gnmi_test_lib.py
+++ gnmi/gnmi_test_lib.py
@@ -695,9 +695,7 @@ def get_response_dict(get_value):
         for key_val in value['prefix']['elem']:
             try:
                 main_key = key_val['key']['name']
-                print("Before sub: {}".format(main_key))
-                main_key = re.sub(r'\W+', '',main_key)
-                print("After sub: {}".format(main_key))
+                #main_key = re.sub(r'\W+', '',main_key)
 
                 if old_key is None:
                     old_key = main_key
