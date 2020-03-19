85b5c1322eed35f7daacc1cf7aaa683b9807a237
Guohan Lu <gulv@microsoft.com>
2020-03-19 08:32:40 +0000

    [test_nbr_health]: add test to check neighbor device health
    it is important to ensure the neighbor device health before
    starting the test.
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

---
+++ tests/test_nbr_health.py
@@ -0,0 +1,30 @@
+import pytest
+import logging
+logger = logging.getLogger(__name__)
+
+pytestmark = [
+    pytest.mark.sanity_check(skip_sanity=True),
+    pytest.mark.disable_loganalyzer,
+]
+
+def test_neighbors_health(duthost, testbed_devices, eos):
+    """Check each neighbor device health"""
+
+    fails = []
+    localhost = testbed_devices['localhost']
+    config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    nei_meta = config_facts.get('DEVICE_NEIGHBOR_METADATA', {})
+    for k, v in nei_meta.items():
+        logger.info("Check neighbor {}, mgmt ip {} snmp".format(k, v['mgmt_addr']))
+        res = localhost.snmp_facts(host=v['mgmt_addr'], version='v2c', community=eos['snmp_rocommunity'])
+        try:
+            snmp_data = res['ansible_facts']
+        except:
+            fails.append("neighbor {} has no snmp data".format(k))
+            continue
+        logger.info("Neighbor {}, sysdescr {}".format(k, snmp_data['ansible_sysdescr']))
+
+    # TODO: check link, bgp, etc. on 
+
+    if len(fails) > 0:
+        pytest.fail("\n".join(fails))
