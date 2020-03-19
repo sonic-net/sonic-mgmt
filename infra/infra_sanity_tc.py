80d921e899a12b509a8ada8822fac399297cdfec
Perumal Venkatesh <pevenkat@cisco.com>
2020-03-18 18:16:27 -0700

    infra changes

--- infra/infra_sanity_tc.py
+++ infra/infra_sanity_tc.py
@@ -140,7 +140,7 @@ def _test_Optics_Laser_Status():
                 data[0:index+1] = []
                 for item in data:
                     if item.split()[0] == str(slot_num) and item.split()[1].lower() == status.lower():
-                        log.info("Laser Status is up for slot_num {}".format(slot_num))
+                        log.info("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
                     else:
                         log.error("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
             else:
@@ -216,7 +216,7 @@ def _test_Flap_Intf_LS():
                 data[0:index+1] = []
                 for item in data:
                     if item.split()[0] == str(slot_num) and item.split()[1].lower() == status.lower():
-                        log.info("Laser Status is up for slot_num {}".format(slot_num))
+                        log.info("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
                     else:
                         log.error("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
             else:
@@ -275,7 +275,7 @@ def _test_Flap_Intf_LS():
                 data[0:index+1] = []
                 for item in data:
                     if item.split()[0] == str(slot_num) and item.split()[1].lower() == status.lower():
-                        log.info("Laser Status is up for slot_num {}".format(slot_num))
+                        log.info("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
                     else:
                         log.error("Laser Status is {} for slot_num {}".format(item.split()[1],slot_num))
             else:
