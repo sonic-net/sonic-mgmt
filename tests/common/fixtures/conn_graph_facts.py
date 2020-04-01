efcd243cbfdeb2c0c3307f0cb002de1b1fb02710
Neetha John <nejo@microsoft.com>
2020-04-01 10:38:24 -0700

    Specify inventory mapping file and avoid using AnsibleHost
    Signed-off-by: Neetha John <nejo@microsoft.com>

--- tests/common/fixtures/conn_graph_facts.py
+++ tests/common/fixtures/conn_graph_facts.py
@@ -2,25 +2,21 @@ import pytest
 import os
 import json
 
-from ansible_host import AnsibleHost
-
 @pytest.fixture(scope="module")
-def conn_graph_facts(testbed_devices, ansible_adhoc):
+def conn_graph_facts(testbed_devices):
+    conn_graph_facts = dict()
     dut = testbed_devices["dut"]
     localhost = testbed_devices["localhost"]
 
     base_path = os.path.dirname(os.path.realpath(__file__))
-    # default lab graph file
-    lab_conn_graph_file = os.path.join(base_path, "../../../ansible/files/lab_connection_graph.xml")
     # json file contains mapping from inventory file name to its corresponding graph file
     inv_mapping_file = os.path.join(base_path, "../../../ansible/group_vars/all/inv_mapping.json")
     if os.path.exists(inv_mapping_file):
         with open(inv_mapping_file) as fd:
             inv_map = json.load(fd)
-        ans_host = AnsibleHost(ansible_adhoc, dut.hostname)
-        inv_file = ans_host.host.options['inventory'].split('/')[-1]
-        if inv_file in inv_map:
+        inv_file = dut.host.options['inventory'].split('/')[-1]
+        if inv_map and inv_file in inv_map:
             lab_conn_graph_file = os.path.join(base_path, "../../../ansible/files/{}".format(inv_map[inv_file]))
 
-    conn_graph_facts = localhost.conn_graph_facts(host=dut.hostname, filename=lab_conn_graph_file)['ansible_facts']
+            conn_graph_facts = localhost.conn_graph_facts(host=dut.hostname, filename=lab_conn_graph_file)['ansible_facts']
     return conn_graph_facts
