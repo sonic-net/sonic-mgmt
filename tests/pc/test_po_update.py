a1748b3a13ff8c0b2f4b5b1dc63e5dbe3c0bf50f
Guohan Lu <gulv@microsoft.com>
2020-04-02 09:35:11 +0000

    [pytest/pc]: rename test_config.py to test_po_update.py
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- tests/pc/test_po_update.py
+++ tests/pc/test_po_update.py
@@ -3,8 +3,10 @@ import time
 import pytest
 import logging
 
-def test_config(ansible_adhoc, testbed):
-    """compare the configuration between observed states and target state"""
+def test_po_update(ansible_adhoc, testbed):
+    """
+    test port channel add/deletion as well ip address configuration
+    """
 
     hostname = testbed['dut']
     ans_host = AnsibleHost(ansible_adhoc, hostname)
@@ -32,7 +34,7 @@ def test_config(ansible_adhoc, testbed):
     logging.info("portchannel=%s" % portchannel)
     logging.info("portchannel_ip=%s" % portchannel_ip)
     logging.info("portchannel_members=%s" % portchannel_members)
- 
+
     try:
         if len(portchannel_members) == 0:
             pytest.skip("Skip test due to there is no portchannel member exists in current topology.")
@@ -61,7 +63,7 @@ def test_config(ansible_adhoc, testbed):
             ans_host.shell("config portchannel member add %s %s" % (tmp_portchannel, member))
         add_tmp_portchannel_members = True
 
-        # Step 5: Add portchannel ip to tmp portchannel                                 
+        # Step 5: Add portchannel ip to tmp portchannel
         ans_host.shell("config interface ip add %s %s/31" % (tmp_portchannel, portchannel_ip))
         int_facts = ans_host.interface_facts()['ansible_facts']
         assert int_facts['ansible_interface_facts'][tmp_portchannel]['ipv4']['address'] == portchannel_ip
@@ -76,12 +78,12 @@ def test_config(ansible_adhoc, testbed):
         # Recover all states
         if add_tmp_portchannel_ip:
             ans_host.shell("config interface ip remove %s %s/31" % (tmp_portchannel, portchannel_ip))
-        
+
         time.sleep(5)
         if add_tmp_portchannel_members:
             for member in portchannel_members:
                 ans_host.shell("config portchannel member del %s %s" % (tmp_portchannel, member))
-        
+
         time.sleep(5)
         if create_tmp_portchannel:
             ans_host.shell("config portchannel del %s" % tmp_portchannel)
@@ -90,7 +92,7 @@ def test_config(ansible_adhoc, testbed):
         if remove_portchannel_members:
             for member in portchannel_members:
                 ans_host.shell("config portchannel member add %s %s" % (portchannel, member))
-        
+
         time.sleep(30)
         bgp_facts = ans_host.bgp_facts()['ansible_facts']
         assert bgp_facts['bgp_statistics']['ipv4_idle'] == 0
