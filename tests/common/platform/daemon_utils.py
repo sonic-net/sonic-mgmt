a4af86bbde2bf420a67bc555c7fa7d53d2ae8530
Ying Xie <yxieca@users.noreply.github.com>
2020-04-06 11:47:56 -0700

    [pmon daemon check] refactoring pmon daemon state check code (#1537)
    * [daemon utils] move platform/check_daemon_status.py to common/platform/daemon_utils.py
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>
    
    * [pmon daemon] refactoring pmon daemon state checking
    
    - Refactor get_pmon_daemon_list to get_pmon_daemon_states:
      returning list of daemon states.
      not returning daemons that are known short lived.
      not returning daemons disabled by configuration.
    - Refactor daemon state check to check the returned states.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

---
+++ tests/common/platform/daemon_utils.py
@@ -0,0 +1,23 @@
+"""
+Helper script for checking status of platform daemon status
+
+This script contains re-usable functions for checking status of platform daemon status.
+"""
+import logging
+
+
+def check_pmon_daemon_status(dut):
+    """
+    @summary: check daemon running status inside pmon docker.
+
+    This function use command "supervisorctl status" inside the container and check the status from the command output.
+    If the daemon status is "RUNNING" then return True, if daemon not exist or status is not "RUNNING", return false.
+    """
+    daemons = dut.get_pmon_daemon_states()
+    ret     = True
+    for daemon, state in daemons.items():
+        logging.debug("Daemon %s status is %s" % (daemon, state))
+        if state != 'RUNNING':
+            ret = False
+
+    return ret
