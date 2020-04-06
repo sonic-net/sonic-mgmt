4c1b4910c5d1f706d955b27c099d689ef357a70c
Pradnya Mohite <pramoh@microsoft.com>
2020-04-05 23:34:22 -0700

    resolve comments

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -4,23 +4,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Helper functions
-def get_telemetry_keyvalues(gnmi_out, certs_out):
-    gnmi_values = gnmi_out.split('\n')
-    cert_values = certs_out.split('\n')
+def get_dict_stdout(gnmi_out, certs_out):
+    result = ""
     gnmi_list = []
-    gnmi_list = gnmi_values + cert_values
+    gnmi_list = get_list_stdout(gnmi_out) + get_list_stdout(certs_out)
     params_dict= dict(itertools.izip_longest(*[iter(gnmi_list)] *2, fillvalue=""))
     return params_dict
+
+def get_list_stdout(cmd_out):
+    result = ""
+    out_list = []
+    for x in cmd_out:
+        result = x.encode('UTF-8')
+        out_list.append(result)
+    return out_list
 
 # Test functions
 def test_config_db_parameters(duthost):
     """Verifies required telemetry parameters from config_db.
+       This is scoped as module as it need to be run once before first test run.
     """
-    gnmi = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|gnmi"', module_ignore_errors=True)['stdout']
-    certs = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|certs"', module_ignore_errors=True)['stdout']
-    d = get_telemetry_keyvalues(gnmi, certs)
+    gnmi = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|gnmi"', module_ignore_errors=False)['stdout_lines']
+    certs = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|certs"', module_ignore_errors=False)['stdout_lines']
+    d = get_dict_stdout(gnmi, certs)
     for key, value in d.items():
-        logger.info(' testing {}={}'.format(key,value))
         if str(key) == "client_auth" and str(value) == "true":
             assert True, "Client_auth set to true"
         if str(key) == "port" and str(value) == "50051":
@@ -35,10 +42,9 @@ def test_config_db_parameters(duthost):
 def test_telemetry_enabledbydefault(duthost):
     """Verify telemetry should be enabled by default
     """
-    status = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "FEATURE|telemetry"', module_ignore_errors=True)['stdout']
-    status_value = status.split('\n')
-    if str(status_value[0]) == "status" and str(status_value[1]) == "enabled":
-        assert True, "Telemetry status is enabled"
-   
-     
-
+    status = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "FEATURE|telemetry"', module_ignore_errors=True)['stdout_lines']
+    status_list = get_list_stdout(status)
+    status_dict = dict(itertools.izip_longest(*[iter(status_list)] *2, fillvalue=""))
+    for k,v in status_dict.items():
+        if str(k) == "status" and str(v) == "enabled":
+            assert True, "Telemetry status is enabled"
