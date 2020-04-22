c43e31299aad071f64c0122e0d822f52e25146e5
Pradnya Mohite <pramoh@microsoft.com>
2020-04-21 18:40:41 -0700

    removing if statement ADDING MISISNG (

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -25,27 +25,25 @@ def test_config_db_parameters(duthost):
     """
     gnmi = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|gnmi"', module_ignore_errors=False)['stdout_lines']
     certs = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|certs"', module_ignore_errors=False)['stdout_lines']
-    if gnmi is None or certs is None:
-        pytest_assert(gnmi is not None, "TELEMETRY|gnmi does not exist in config_db")
-        pytest_assert(certs is not None, "TELEMETRY|certs does not exist in config_db")
-    else:
-        d = get_dict_stdout(gnmi, certs)
-        for key, value in d.items():
-            if str(key) == "client_auth":
-                client_auth_expected = "true"
-                pytest_assert(str(value) == client_auth_expected, "'client_auth' value is not '{}'".format(client_auth_expected))
-            if str(key) == "port":
-                port_expected = "50051"
-                pytest_assert(str(value) == port_expected, "'port' value is not '{}'".format(port_expected))
-            if str(key) == "ca_crt":
-                ca_crt_value_expected = "/etc/sonic/telemetry/dsmsroot.cer"
-                pytest_assert(str(value) == ca_crt_value_expected, "'ca_crt' value is not '{}'".format(ca_crt_value_expected))
-            if str(key) == "server_key":
-                server_key_expected = "/etc/sonic/telemetry/streamingtelemetryserver.key"
-                pytest_assert(str(value) == server_key_expected, "'server_key' value is not '{}'".format(server_key_expected))
-            if str(key) == "server_crt":
-                server_crt_expected = "/etc/sonic/telemetry/streamingtelemetryserver.cer"
-                pytest_assert str(value) == server_crt_expected, "'server_crt' value is not '{}'".format(server_crt_expected))
+    pytest_assert(gnmi is not None, "TELEMETRY|gnmi does not exist in config_db")
+    pytest_assert(certs is not None, "TELEMETRY|certs does not exist in config_db")
+    d = get_dict_stdout(gnmi, certs)
+    for key, value in d.items():
+        if str(key) == "client_auth":
+            client_auth_expected = "true"
+            pytest_assert(str(value) == client_auth_expected, "'client_auth' value is not '{}'".format(client_auth_expected))
+        if str(key) == "port":
+            port_expected = "50051"
+            pytest_assert(str(value) == port_expected, "'port' value is not '{}'".format(port_expected))
+        if str(key) == "ca_crt":
+            ca_crt_value_expected = "/etc/sonic/telemetry/dsmsroot.cer"
+            pytest_assert(str(value) == ca_crt_value_expected, "'ca_crt' value is not '{}'".format(ca_crt_value_expected))
+        if str(key) == "server_key":
+            server_key_expected = "/etc/sonic/telemetry/streamingtelemetryserver.key"
+            pytest_assert(str(value) == server_key_expected, "'server_key' value is not '{}'".format(server_key_expected))
+        if str(key) == "server_crt":
+            server_crt_expected = "/etc/sonic/telemetry/streamingtelemetryserver.cer"
+            pytest_assert(str(value) == server_crt_expected, "'server_crt' value is not '{}'".format(server_crt_expected))
 
 def test_telemetry_enabledbydefault(duthost):
     """Verify telemetry should be enabled by default
