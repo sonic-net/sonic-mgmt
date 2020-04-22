2b025b0fc9910167deb75f71892427c39e20f192
Guohan Lu <gulv@microsoft.com>
2020-04-22 11:35:11 +0000

    add ipv6 nexthop test
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- tests/route/test_default_route.py
+++ tests/route/test_default_route.py
@@ -1,4 +1,4 @@
-from netaddr import IPAddress, IPNetwork
+import ipaddress
 import re
 import pytest
 import logging
@@ -17,10 +17,10 @@ default proto bgp src 10.1.0.32 metric 20
 
 admin@vlab-01:~$ ip -6 route list match ::
 default proto bgp src fc00:1::32 metric 20
-        nexthop via fe80::831:6cff:fea5:d8eb dev PortChannel0004 weight 1
-        nexthop via fe80::10b5:4aff:fef2:6e37 dev PortChannel0002 weight 1
-        nexthop via fe80::1419:9ff:fe52:2db dev PortChannel0003 weight 1
-        nexthop via fe80::5cb4:7bff:fec8:148b dev PortChannel0001 weight 1 pref medium
+        nexthop via fc00::72 dev PortChannel0001 weight 1
+        nexthop via fc00::76 dev PortChannel0002 weight 1
+        nexthop via fc00::7a dev PortChannel0003 weight 1
+        nexthop via fc00::7e dev PortChannel0004 weight 1 pref medium
     """
 
     config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
@@ -32,7 +32,7 @@ default proto bgp src fc00:1::32 metric 20
     for k, v in los.items():
         if k == "Loopback0":
             for ipstr in v.keys():
-                ip = IPNetwork(ipstr)
+                ip = ipaddress.ip_interface(ipstr)
                 if ip.version == 4:
                     lo_ipv4 = ip
                 elif ip.version == 6:
@@ -46,7 +46,7 @@ default proto bgp src fc00:1::32 metric 20
     rt = duthost.command("ip route list match 0.0.0.0")['stdout_lines']
     m = re.match(r"^default proto bgp src (\S+)", rt[0])
     if m:
-        if IPAddress(m.group(1)) != lo_ipv4.ip:
+        if ipaddress.ip_address(m.group(1)) != lo_ipv4.ip:
             pytest.fail("default route set src to wrong IP {} != {}".format(m.group(1), lo_ipv4.ip))
     else:
         pytest.fail("default route do not have set src. {}".format(rt))
@@ -54,7 +54,27 @@ default proto bgp src fc00:1::32 metric 20
     rt = duthost.command("ip -6 route list match ::")['stdout_lines']
     m = re.match(r"^default proto bgp src (\S+)", rt[0])
     if m:
-        if IPAddress(m.group(1)) != lo_ipv6.ip:
+        if ipaddress.ip_address(m.group(1)) != lo_ipv6.ip:
             pytest.fail("default route set src to wrong IP {} != {}".format(m.group(1), lo_ipv6.ip))
     else:
         pytest.fail("default route do not have set src. {}".format(rt))
+
+def test_default_ipv6_route_next_hop_global_address(duthost):
+    """check if ipv6 default route nexthop address uses global address
+
+admin@vlab-01:~$ ip -6 route list match ::
+default proto bgp src fc00:1::32 metric 20
+        nexthop via fc00::72 dev PortChannel0001 weight 1
+        nexthop via fc00::76 dev PortChannel0002 weight 1
+        nexthop via fc00::7a dev PortChannel0003 weight 1
+        nexthop via fc00::7e dev PortChannel0004 weight 1 pref medium
+    """
+
+    rt = duthost.command("ip -6 route list match ::")['stdout_lines']
+    for l in rt[1:]:
+        m = re.search(r"nexthop via (\S+)", l)
+        if m:
+            if ipaddress.ip_address(m.group(1)).is_link_local:
+                pytest.fail("use link local address for for nexthop".format(m.group(1)))
+        else:
+            pytest.fail("cannot find ipv6 nexthop for default route {}".format(rt))
