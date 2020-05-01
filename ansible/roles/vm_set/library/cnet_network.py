85d5ca07f09dfc99f6c73ee1e23fdbad6ff050d0
Guohan Lu <gulv@microsoft.com>
2020-05-01 15:54:54 +0000

    csonic support
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- ansible/roles/vm_set/library/cnet_network.py
+++ ansible/roles/vm_set/library/cnet_network.py
@@ -12,10 +12,10 @@ from pprint import pprint
 
 DOCUMENTATION = '''
 ---
-module: ceos_network
+module: cnet_network
 version_added: "0.1"
 author: Guohan Lu (gulv@microsoft.com)
-short_description: Create network for ceos container
+short_description: Create network for container
 description:
     the module creates follow network interfaces
     - 1 management interface which is added to management bridge
@@ -30,7 +30,7 @@ Parameters:
 
 EXAMPLES = '''
 - name: Create VMs network
-  ceos_network:
+  cnet_network:
     name:        net_{{ vm_set_name }}_{{ vm_name }}
     vm_name:     "{{ vm_name }}"
     fp_mtu:      "{{ fp_mtu_size }}"
@@ -42,8 +42,8 @@ EXAMPLES = '''
 DEFAULT_MTU = 0
 NUM_FP_VLANS_PER_FP = 4
 VM_SET_NAME_MAX_LEN = 8  # used in interface names. So restricted
-CMD_DEBUG_FNAME = "/tmp/ceos_network.cmds.%s.txt"
-EXCEPTION_DEBUG_FNAME = "/tmp/ceos_network.exception.%s.txt"
+CMD_DEBUG_FNAME = "/tmp/cnet_network.cmds.%s.txt"
+EXCEPTION_DEBUG_FNAME = "/tmp/cnet_network.exception.%s.txt"
 
 OVS_FP_BRIDGE_REGEX = 'br-%s-\d+'
 OVS_FP_BRIDGE_TEMPLATE = 'br-%s-%d'
@@ -316,4 +316,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
