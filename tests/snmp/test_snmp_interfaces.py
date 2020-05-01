cb725e5fdf7ce1f2e412d0caaf7f3d6198fa3829
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-05-01 09:22:44 -0700

    [Multiasic]: Update Interfaces MIB testcase to test multiasic platform.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- tests/snmp/test_snmp_interfaces.py
+++ tests/snmp/test_snmp_interfaces.py
@@ -9,20 +9,30 @@ def test_snmp_interfaces(ansible_adhoc, duthost, creds):
 
     hostip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
 
+    npus =  duthost.num_npus()
+
     snmp_facts = lhost.snmp_facts(host=hostip, version="v2c", community=creds["snmp_rocommunity"])['ansible_facts']
     config_facts  = duthost.config_facts(host=duthost.hostname, source="persistent")['ansible_facts']
 
     snmp_ifnames = [ v['name'] for k, v in snmp_facts['snmp_interfaces'].items() ]
     print snmp_ifnames
 
-    # Verify all physical ports in snmp interface list
-    for _, alias in config_facts['port_name_to_alias_map'].items():
-        assert alias in snmp_ifnames
-
-    # Verify all port channels in snmp interface list
-    for po_name in config_facts.get('PORTCHANNEL', {}):
-        assert po_name in snmp_ifnames
-    
     # Verify management port in snmp interface list
     for name in config_facts.get('MGMT_INTERFACE', {}):
         assert name in snmp_ifnames
+
+    if (npus > 1):
+        for npu_inst in range(npus):
+            npu_config_path = "/etc/sonic/config_db" + str(npu_inst) + ".json"
+            config_facts  = duthost.config_facts(host=duthost.hostname, source="persistent", filename=npu_config_path)['ansible_facts']
+            pdb.set_trace()
+            for _, alias in config_facts['port_name_to_alias_map'].items():
+                assert alias in snmp_ifnames
+            for po_name in config_facts.get('PORTCHANNEL', {}):
+                assert po_name in snmp_ifnames
+    else:
+        config_facts  = duthost.config_facts(host=duthost.hostname, source="persistent")['ansible_facts']
+        for _, alias in config_facts['port_name_to_alias_map'].items():
+            assert alias in snmp_ifnames
+        for po_name in config_facts.get('PORTCHANNEL', {}):
+            assert po_name in snmp_ifnames
