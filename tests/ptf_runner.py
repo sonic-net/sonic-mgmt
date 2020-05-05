e3554323ef1f6ba1e0d2f5d338c8863907f427c8
lguohan <lguohan@gmail.com>
2020-05-05 12:52:27 -0700

    Revert "[copp] Convert COPP test to pytest (#1633)" (#1643)
    This reverts commit 9a8fc1ed62666f46b8f23b5587860799ace13704.

--- tests/ptf_runner.py
+++ tests/ptf_runner.py
@@ -1,37 +1,27 @@
 import pipes
 
-def ptf_runner(host, testdir, testname, platform_dir=None, params={},
-               platform="remote", qlen=0, relax=True, debug_level="info",
-               socket_recv_size=None, log_file=None, device_sockets=[]):
+def ptf_runner(host, testdir, testname, platform_dir, params={}, \
+               platform="remote", qlen=0, relax=True, debug_level="info", \
+               socket_recv_size=None, log_file=None):
 
-    cmd = "ptf --test-dir {} {}".format(testdir, testname)
+    ptf_test_params = ";".join(["{}={}".format(k, repr(v)) for k, v in params.items()])
 
-    if platform_dir:
-        cmd += "--platform-dir {}".format(platform_dir)
-
+    cmd = "ptf --test-dir {} {} --platform-dir {}".format(testdir, testname, platform_dir)
     if qlen:
         cmd += " --qlen={}".format(qlen)
-
     if platform:
         cmd += " --platform {}".format(platform)
-
-    if params:
-        ptf_test_params = ";".join(["{}={}".format(k, repr(v)) for k, v in params.items()])
+    if ptf_test_params:
         cmd += " -t {}".format(pipes.quote(ptf_test_params))
-
     if relax:
         cmd += " --relax"
-
     if debug_level:
         cmd += " --debug {}".format(debug_level)
-
     if log_file:
         cmd += " --log-file {}".format(log_file)
-
     if socket_recv_size:
         cmd += " --socket-recv-size {}".format(socket_recv_size)
 
-    if device_sockets:
-        cmd += " ".join(map(" --device-socket {}".format, device_sockets))
+    res = host.shell(cmd, chdir="/root")
 
-    host.shell(cmd, chdir="/root")
+
