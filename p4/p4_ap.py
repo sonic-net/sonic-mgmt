fd250380ad1c3bb916155d1e9b13134c31baa4c1
Perumal Venkatesh <pevenkat@cisco.com>
2020-05-11 19:53:46 -0700

    New data file for subnet testing

--- p4/p4_ap.py
+++ p4/p4_ap.py
@@ -216,7 +216,13 @@ class TestP4(P4ApBase):
         p4_san_tc._test_indirect_table_crudTests(self, tbl_name, tbl_ops,sw_conn)
 
     @pytest.mark.parametrize("tbl_ops", ["INSERT", "READ"])
+    #@pytest.mark.parametrize("tbl_ops", ["DELETE"])
     def test_traffic_l3_fwd_l3_ipv4_vrf_table(self, tbl_ops, sw_conn):
+        p4_traffic_tc._test_traffic_l3_fwd_l3_ipv4_vrf_table(self, tbl_ops,sw_conn)
+
+    @pytest.mark.parametrize("tbl_ops", ["INSERT", "READ"])
+    #@pytest.mark.parametrize("tbl_ops", ["DELETE"])
+    def test_subnet_traffic_l3_fwd_l3_ipv4_vrf_table(self, tbl_ops, sw_conn):
         p4_traffic_tc._test_traffic_l3_fwd_l3_ipv4_vrf_table(self, tbl_ops,sw_conn)
 
     def test_p4_sanity(self,sw_conn):
