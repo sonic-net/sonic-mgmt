1a0ae559894fa0dd2ebe0dbf9cd4b672be58baf3
Ying Xie <ying.xie@microsoft.com>
2020-05-14 02:03:15 +0000

    [reload config] redirect reload config output to /dev/null
    config reload command will restart interface-config service, which
    will reset eth0. Which will reset the connection issues config
    reload if the output is coming to stdio/stderr. It in turn could
    cause the pytest to stuck for long time.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- tests/common/config_reload.py
+++ tests/common/config_reload.py
@@ -24,10 +24,10 @@ def config_reload(duthost, config_source='config_db', wait=120):
     logger.info('reloading {}'.format(config_source))
 
     if config_source == 'minigraph':
-        duthost.command('config load_minigraph -y')
-        duthost.command('config save -y')
+        duthost.shell('config load_minigraph -y &>/dev/null')
+        duthost.shell('config save -y')
 
     if config_source == 'config_db':
-        duthost.command('config reload -y')
+        duthost.shell('config reload -y &>/dev/null')
 
     time.sleep(wait)
--- tests/vlan/test_vlan.py
+++ tests/vlan/test_vlan.py
@@ -184,7 +184,7 @@ def tearDown(vlan_ports_list, duthost, ptfhost, vlan_intfs_list, portchannel_int
     except RunAnsibleModuleFail as e:
         logger.error(e)
 
-    duthost.command("config reload -y")
+    duthost.shell("config reload -y &>/dev/null")
 
     # make sure Portchannels go up for post-test link sanity
     time.sleep(90)
