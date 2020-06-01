53c1f226a15960c91556a028770419dfcae16b7e
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-06-01 10:59:19 -0700

    [pytest] Convert Ansible  QoS SAI test cases
    This PR converts Ansible QoS SAI test cases to pytest framework. The
    expermental test cases under buff_wm.yml is not part of this conversion.
    
    signed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

---
+++ tests/qos/args/qos_sai_args.py
@@ -0,0 +1,54 @@
+# QoS-SAI Args file
+
+def add_qos_sai_args(parser):
+    """
+        Adding arguments required for QoS SAI test cases
+
+       Args:
+            parser: pytest parser object
+
+        Returns:
+            None
+    """
+    qos_group = parser.getgroup("QoS test suite options")
+
+    qos_group.addoption(
+        "--ptf_portmap",
+        action="store",
+        type=str,
+        default=None,
+        help="PTF port index to DUT port alias map",
+    )
+
+    qos_group.addoption(
+        "--disable_test",
+        action="store",
+        type=bool,
+        default=True,
+        help="Control execution of buffer watermark experimental tests",
+    )
+
+    qos_group.addoption(
+        "--qos_swap_syncd",
+        action="store",
+        type=bool,
+        default=True,
+        help="Swap syncd container with syncd-rpc container",
+    )
+
+    qos_group.addoption(
+        "--qos_dst_ports",
+        action="store",
+        type=lambda opt_value: [int(v) for v in opt_value.translate(None, "[]").split(',')],
+        default=[0, 1, 3],
+        help="QoS SAI comma separated list of destination ports. Test currently expects exactly 3 destination ports",
+    )
+
+    qos_group.addoption(
+        "--qos_src_ports",
+        action="store",
+        type=lambda opt_value: [int(v) for v in opt_value.translate(None, "[]").split(',')],
+        default=[2],
+        help="QoS SAI comma separated list of source ports. Test currently expects exactly 1 source port",
+    )
+
