c100e81834d657546eb00eb990fec04371865bfc
Yong Zhao <yozhao@microsoft.com>
2020-06-15 00:15:31 -0700

    Add test functions for all containers.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- tests/autorestart_test/test_container_autorestart.py
+++ tests/autorestart_test/test_container_autorestart.py
@@ -1,17 +1,15 @@
 """
-Check the auto-restart feature of syncd, swss, teamd, bgp and dhcp_relay
+Check the auto-restart feature of different containers 
 """
 import time
+import pytest
 import logging
 
 from common.utilities import wait_until
 
-CONTAINER_STOP_TEST_WAITING_TIME = 30
-CONTAINER_STOP_TEST_CHECK_INTERVAL = 1
-CONTAINER_RESTART_TEST_WAITING_TIME = 180
-CONTAINER_RESTART_TEST_CHECK_INTERVAL = 1
-CONTAINER_RUNNING_TEST_WAITING_TIME = 30
-CONTAINER_RUNNING_TEST_CHECK_INTERVAL = 1
+CONTAINER_CHECK_INTERVAL_SECS = 1
+CONTAINER_STOP_THRESHOLD_SECS = 30
+CONTAINER_RESTART_THRESHOLD_SECS = 180
 
 def get_process_id(duthost, container_name, process):
     """
@@ -44,7 +42,7 @@ def kill_process(duthost, container_name, process):
 
     running_status, process_id = get_process_id(duthost, container_name, process)
     if running_status:
-        assert False, "Failed to stop %s process" % process
+        assert False, "Failed to stop {} process before test.".format(process)
     else:
         logging.info("{} process is stopped successfully".format(process)) 
 
@@ -64,21 +62,18 @@ def verify_autorestart_with_critical_process(duthost, container_name, process):
     @summary: Killing a critical process in a container to verify whether the container 
               can be stopped and then restarted correctly 
     """
-    
     kill_process(duthost, container_name, process)
 
     logging.info("Waiting until {} is stopped...".format(container_name))
-    assert wait_until(CONTAINER_STOP_TEST_WAITING_TIME, 
-                      CONTAINER_STOP_TEST_CHECK_INTERVAL,
-                      check_container_status, duthost, 
-                      container_name, True), "Failed to stop %s" % container_name
+    assert wait_until(CONTAINER_STOP_THRESHOLD_SECS, 
+                      CONTAINER_CHECK_INTERVAL_SECS,
+                      check_container_status, duthost, container_name, True)
     logging.info("{} is stopped".format(container_name))
 
     logging.info("Waiting until {} is restarted...".format(container_name))
-    assert wait_until(CONTAINER_RESTART_TEST_WAITING_TIME, 
-                      CONTAINER_RESTART_TEST_CHECK_INTERVAL,
-                      check_container_status, duthost, 
-                      container_name, False), "Failed to restart %s" % container_name
+    assert wait_until(CONTAINER_RESTART_THRESHOLD_SECS, 
+                      CONTAINER_CHECK_INTERVAL_SECS,
+                      check_container_status, duthost, container_name, False)
     logging.info("{} is restarted".format(container_name))
 
 def verify_autorestart_with_non_critical_process(duthost, container_name, process):
@@ -86,16 +81,69 @@ def verify_autorestart_with_non_critical_process(duthost, container_name, proces
     @summary: Killing a non-critical process in a container to verify whether the container 
               is still in the running state 
     """
-    
     kill_process(duthost, container_name, process)
 
     logging.info("Checking whether the {} is still running...".format(container_name))
-    assert wait_until(CONTAINER_RUNNING_TEST_WAITING_TIME, 
-                      CONTAINER_RUNNING_TEST_CHECK_INTERVAL,
-                      check_container_status, duthost, 
-                      container_name, False), "%s is stopped unexpectedly" % container_name
+    assert wait_until(CONTAINER_STOP_THRESHOLD_SECS, 
+                      CONTAINER_CHECK_INTERVAL_SECS,
+                      check_container_status, duthost, container_name, False)
     logging.info("{} is running".format(container_name))
+
+@pytest.fixture(scope="module", autouse=True)
+def change_autorestart_status(duthost):
+    container_list = ["lldp", "radv", "pmon", "sflow", "database", "telemetry", "snmp",
+                      "bgp", "dhcp_relay", "nat", "teamd", "syncd", "swss"]
+    for container_name in container_list:
+        logging.info("Change {} auto-restart status to enabled".format(container_name))
+        duthost.shell("config container feature autorestart %s enabled" % container_name)
 
 def test_swss_autorestart(duthost):
     verify_autorestart_with_critical_process(duthost, "swss", "portsyncd")
     verify_autorestart_with_non_critical_process(duthost, "swss", "rsyslogd")
+
+def test_lldp_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "lldp", "lldpmgrd")
+    verify_autorestart_with_non_critical_process(duthost, "lldp", "rsyslogd")
+
+def test_sflow_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "sflow", "sflowmgrd")
+    verify_autorestart_with_non_critical_process(duthost, "sflow", "rsyslogd")
+
+def test_database_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "database", "redis")
+    verify_autorestart_with_non_critical_process(duthost, "database", "rsyslogd")
+
+def test_telemetry_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "telemetry", "telemetry")
+    verify_autorestart_with_non_critical_process(duthost, "telemetry", "rsyslogd")
+
+def test_snmp_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "snmp", "snmpd")
+    verify_autorestart_with_non_critical_process(duthost, "snmp", "rsyslogd")
+
+def test_bgp_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "bgp", "zebra")
+    verify_autorestart_with_non_critical_process(duthost, "bgp", "rsyslogd")
+
+def test_radv_autorestart(duthost):
+    verify_autorestart_with_non_critical_process(duthost, "radv", "rsyslogd")
+
+def test_dhcp_relay_autorestart(duthost):
+    #verify_autorestart_with_critical_process(duthost, "dhcp_relay", "")
+    verify_autorestart_with_non_critical_process(duthost, "dhcp_relay", "rsyslogd")
+
+def test_nat_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "nat", "natmgrd")
+    verify_autorestart_with_non_critical_process(duthost, "nat", "rsyslogd")
+
+def test_teamd_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "teamd", "teammgrd")
+    verify_autorestart_with_non_critical_process(duthost, "teamd", "rsyslogd")
+
+def test_syncd_autorestart(duthost):
+    verify_autorestart_with_critical_process(duthost, "syncd", "syncd")
+    verify_autorestart_with_non_critical_process(duthost, "syncd", "rsyslogd")
+
+def test_pmon_autorestart(duthost):
+    #verify_autorestart_with_critical_process(duthost, "pmon", "")
+    verify_autorestart_with_non_critical_process(duthost, "pmon", "rsyslogd")
