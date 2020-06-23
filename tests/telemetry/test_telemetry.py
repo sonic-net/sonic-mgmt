c59e4bc6443a1f8a286beb7a0f46c0c4d0530575
pramoh <pramoh@microsoft.com>
2020-06-23 20:03:29 +0000

    add gnmi_get output test

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -5,6 +5,8 @@ pytestmark = [
     pytest.mark.topology('any')
 ]
 
+logger = logging.getLogger(__name__)
+
 # Helper functions
 def get_dict_stdout(gnmi_out, certs_out):
     """ Extracts dictionary from redis output.
@@ -24,10 +26,22 @@ def get_list_stdout(cmd_out):
         out_list.append(result)
     return out_list
 
+def restart_telemetry(duthost):
+    """ Restart telemetry process
+    """
+    duthost.shell('sudo config feature telemetry disabled', module_ignore_errors=False)['stdout_lines']
+    status_out = duthost.shell('show features | grep telemetry', module_ignore_errors=False)['stdout_lines']
+    status_value = get_list_stdout(status_out)
+    status_value_list = status_value[0].split()
+    status_value = status_value_list[1]
+    if str(status_value) == "disabled":
+        logger.info(' telemetry is disabled. enabling it back...')
+        duthost.shell('sudo config feature telemetry enabled', module_ignore_errors=False)['stdout_lines']
+        return bool("true")
+    return bool(false)
+
 # Test functions
 def test_config_db_parameters(duthost):
-    """Verifies required telemetry parameters from config_db.
-    """
     gnmi = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "TELEMETRY|gnmi"', module_ignore_errors=False)['stdout_lines']
     pytest_assert(gnmi is not None, "TELEMETRY|gnmi does not exist in config_db")
 
@@ -53,8 +67,6 @@ def test_config_db_parameters(duthost):
             pytest_assert(str(value) == server_crt_expected, "'server_crt' value is not '{}'".format(server_crt_expected))
 
 def test_telemetry_enabledbydefault(duthost):
-    """Verify telemetry should be enabled by default
-    """
     status = duthost.shell('/usr/bin/redis-cli -n 4 hgetall "FEATURE|telemetry"', module_ignore_errors=False)['stdout_lines']
     status_list = get_list_stdout(status)
     # Elements in list alternate between key and value. Separate them and combine into a dict.
@@ -65,3 +77,26 @@ def test_telemetry_enabledbydefault(duthost):
         if str(k) == "status":
             status_expected = "enabled";
             pytest_assert(str(v) == status_expected, "Telemetry feature is not enabled")
+
+def test_telemetry_gnmi_get(duthost, ptfhost):
+    """Run gnmi_get from ptfdocker and verify data streaming
+    """
+    certs_del_status = duthost.shell('/usr/bin/redis-cli -n 4 del "TELEMETRY|certs"', module_ignore_errors=False)['stdout_lines']
+    set_client_auth = duthost.shell('/usr/bin/redis-cli -n 4 hset "TELEMETRY|gnmi" "client_auth" "false"', module_ignore_errors=False)['stdout_lines']
+    logger.info('start telemetry testing')
+    restart_status = restart_telemetry(duthost)
+    if restart_status == bool("true"):
+        logger.info('telemetry process restarted. Now run gnmi_get on ptfdocker')
+        #Now run gnmi_get on ptfdocker
+        dut_ip = duthost.setup()['ansible_facts']['ansible_eth0']['ipv4']['address']
+        logger.info("dut ip={}".format(dut_ip))
+        cmd = '''
+              cd ~/etc/go/bin &&
+             ./gnmi_get -xpath_target COUNTERS_DB COUNTERS/Ethernet0 -target_addr {0}:8080 -insecure
+             '''.format(dut_ip)
+        show_gnmi_get_out = ptfhost.shell(cmd)[stdout]
+        logger.info("gnmi get output \n {}".format(show_gnmi_get_out))
+    # Reset config back to original for telemetry process
+    duthost.shell('/usr/bin/redis-cli -n 4 hmset "TELEMETRY|certs" "ca_crt" "/etc/sonic/telemetry/dsmsroot.cer" "server_key" "/etc/sonic/telemetry/streamingtelemetryserver.key" "server_crt" "/etc/sonic/telemetry/streamingtelemetryserver.cer"', module_ignore_errors=False)['stdout_lines']
+    duthost.shell('/usr/bin/redis-cli -n 4 hset "TELEMETRY|gnmi" "client_auth" "true"', module_ignore_errors=False)['stdout_lines']
+    restart_telemetry(duthost) 
