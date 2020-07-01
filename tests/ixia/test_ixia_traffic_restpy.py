61ff293c3598c54d16e60f146f306049171b82bc
abhijit-dhar <67135817+abhijit-dhar@users.noreply.github.com>
2020-07-01 16:07:56 +0530

    Update test_ixia_traffic_restpy.py

--- tests/ixia/test_ixia_traffic_restpy.py
+++ tests/ixia/test_ixia_traffic_restpy.py
@@ -1,9 +1,9 @@
 ###############################################################################
-# This test cases demonstrates:
-#   * All the fixtures required for running ixia script (please see the
+# This test cases demonstrates: 
+#   * All the fixtures required for running ixia script (please see the 
 #     arguments of the test function)
 #   * How Ixia chassis card/ports are addressed
-#   * How you can configure/control ixia devices, start traffic and collect
+#   * How you can configure/control ixia devices, start traffic and collect 
 #     statistics using REST API
 #   * This simple sanity test cases can be used to check if testbed setup
 #     is correct or not - since it prints a lot of testbed data
@@ -12,107 +12,82 @@
 import logging
 import time
 import pytest
-import pprint
+import ipaddr
 from common.utilities import wait_until
-from common.fixtures.conn_graph_facts import conn_graph_facts
-from common.platform.interface_utils import check_interface_information
-from common.platform.daemon_utils import check_pmon_daemon_status
+from common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts 
 from common.reboot import *
-from common.platform.device_utils import fanout_switch_port_lookup
-from common.helpers import assertions
 from ixnetwork_restpy import SessionAssistant, Files
 
-from common.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_user,\
-     ixia_api_serv_passwd, ixia_dev, ixia_api_serv_port, ixia_api_serv_session_id
+from common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_user,\
+     ixia_api_serv_passwd, ixia_api_serv_port, ixia_api_serv_session_id, \
+     ixia_api_server_session  
 
-from common.ixia_helpers import get_neigh_ixia_mgmt_ip, get_neigh_ixia_card,\
-     get_neigh_ixia_port
+from common.ixia.ixia_helpers import get_neigh_ixia_mgmt_ip, get_neigh_ixia_card,\
+     get_neigh_ixia_port, IxiaFanoutManager
 
 import time
 
-#pytestmark = [pytest.mark.disable_loganalyzer]
+def create_ipv4_traffic_end_points (
+    src_start_port,
+    src_port_count,
+    src_first_route_index,
+    src_route_count,
+    dst_start_port,
+    dst_port_count,
+    dst_first_route_index,
+    dst_route_count) :
 
-def returnIxiaChassisIp (chassisDict, no) :
-    chList = []
-    for key in chassisDict.keys():
-        chList.append(chassisDict[key])
+    src = [{'arg1': '/api/v1/sessions/1/ixnetwork/topology/1/deviceGroup/1/ethernet/1/ipv4/1',
+             'arg2': src_start_port, 
+             'arg3': src_port_count, 
+             'arg4': src_first_route_index, 
+             'arg5': dst_route_count}
+    ]
 
-    return (chList[no - 1])
+    dst = [{'arg1': '/api/v1/sessions/1/ixnetwork/topology/1/deviceGroup/1/ethernet/1/ipv4/1', 
+             'arg2': dst_start_port, 
+             'arg3': dst_port_count, 
+             'arg4': dst_first_route_index,
+             'arg5': dst_route_count}
+    ]
 
-def parseDeviceConn (device_conn) :
-    retval = []
-    dive_con_dict = device_conn['device_conn']
-    for key in dive_con_dict.keys() :
-        pp =  dive_con_dict[key]['peerport']
-        string = pp + '/' + key
-        retval.append(string)
-    retval.sort()
-    return(retval)
+    return (src, dst)
 
-def getCard(ixiaCardPortList, num) :
-    card = ixiaCardPortList[num].split('/')[0]
-    cardNo = int(card.replace('Card', ''))
-    return(cardNo)
 
-def getPort(ixiaCardPortList, num) :
-    port = ixiaCardPortList[num].split('/')[1]
-    portNo = int(port.replace('Port', ''))
-    return(portNo)
+def test_testbed(testbed, conn_graph_facts, duthost, fanout_graph_facts,
+    ixia_api_server_session, fanouthosts):
 
-def test_testbed(testbed, conn_graph_facts, duthost, ixia_dev, ixia_api_serv_ip,
-                 ixia_api_serv_user, ixia_api_serv_passwd, ixia_api_serv_port,
-                 ixia_api_serv_session_id):
     logger.info("Connection Graph Facts = %s " %(conn_graph_facts))
+    logger.info("Fanout Graph facts = %s" %(fanout_graph_facts))
     logger.info("DUT hostname = %s" %(duthost.hostname))
-    ixiaports = parseDeviceConn(conn_graph_facts)
-    logger.info("Ixia ports = %s" %(ixiaports))
-    logger.info("Ixia chassis IP = %s" %(ixia_dev))
-    logger.info("Ixia API server IP = %s" %(ixia_api_serv_ip))
-    logger.info("Ixia API server user = %s" %(ixia_api_serv_user))
-    logger.info("Ixia API server password = %s" %(ixia_api_serv_passwd))
-    logger.info("Ixia API server port = %s" %(ixia_api_serv_port))
-    logger.info("Ixia API server sessionId = %s" %(ixia_api_serv_session_id))
+ 
+    mg_facts  = duthost.minigraph_facts(host=duthost.hostname)
+    gatewayIp = mg_facts['ansible_facts']['minigraph_vlan_interfaces'][0]['addr']
 
-    clientIp  = ixia_api_serv_ip
-    UserName  = ixia_api_serv_user
-    Password  = ixia_api_serv_passwd
-    RestPort  = ixia_api_serv_passwd
-    SessionId = ixia_api_serv_session_id
-    chassisIp = returnIxiaChassisIp(ixia_dev, 1)
+    ixiaFanoutHostList = IxiaFanoutManager(fanout_graph_facts) 
+    ixiaFanoutHostList.get_fanout_device_details(device_number = 0)
 
-    cardId    = getCard(ixiaports, 0)
-    PortId1   = getPort(ixiaports, 0)
-    PortId2   = getPort(ixiaports, 1)
-    PortId3   = getPort(ixiaports, 2)
-    PortId4   = getPort(ixiaports, 3)
-    PortId5   = getPort(ixiaports, 4)
-    PortId6   = getPort(ixiaports, 5)
+    # Build gateway valuelist. Same gateway IP for all interface
+    gateway_value_list = []
+    for i in ixiaFanoutHostList.ports() :
+        gateway_value_list.append(gatewayIp)
 
-    if (SessionId != "None") :
-        session = SessionAssistant(IpAddress = clientIp,
-                               UserName = UserName,
-                               Password = Password,
-                               RestPort = RestPort,
-                               SessionId = SessionId)
-    else :
-        session = SessionAssistant(IpAddress = clientIp,
-                               UserName = UserName,
-                               Password = Password,
-                               RestPort = RestPort)
+    # Create ixNetwork interface IP address list 
+    interface_ip_list = []
+    ipaddress = ipaddr.IPv4Address(gatewayIp)
+    for i in ixiaFanoutHostList.ports() :
+        ipaddress = ipaddress + 1
+        interface_ip_list.append(ipaddress._string_from_ip_int(ipaddress._ip))
 
-    sessionData = session.Session
-    ixNetwork   = session.Ixnetwork
-    ixNetwork.NewConfig()
-    portMap = session.PortMapAssistant()
+    session   = ixia_api_server_session
+    ixNetwork = session.Ixnetwork
+    portMap   = session.PortMapAssistant()
 
-    vPort1 = portMap.Map(chassisIp, cardId, PortId1)
-    vPort2 = portMap.Map(chassisIp, cardId, PortId2)
-    vPort3 = portMap.Map(chassisIp, cardId, PortId3)
-    vPort4 = portMap.Map(chassisIp, cardId, PortId4)
-    vPort5 = portMap.Map(chassisIp, cardId, PortId5)
-    vPort6 = portMap.Map(chassisIp, cardId, PortId6)
-    #print ('connecting to chassis %s' %(chassisIp))
-
+    vport_list = []
+    for i in ixiaFanoutHostList.ports() :
+        (chassisIp, cardId, portId) = ixiaFanoutHostList.getCardPort(i)
+        vport_list.append(portMap.Map(chassisIp, cardId, portId))
+       
     t1 = time.time()
     portMap.Connect(ChassisTimeout=1200, ForceOwnership=True)
     t2 = time.time()
@@ -120,64 +95,59 @@ def test_testbed(testbed, conn_graph_facts, duthost, ixia_dev, ixia_api_serv_ip,
     time_taken = t2 - t1
     logger.info("time-taken to connect = %s" %(time_taken))
 
-    vPort1.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort1.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort1.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort1.L1Config.NovusHundredGigLan.EnableRsFecStats = True
+    for vport in vport_list :
+        vport.L1Config.NovusHundredGigLan.IeeeL1Defaults        = False         
+        vport.L1Config.NovusHundredGigLan.EnableAutoNegotiation = False
+        vport.L1Config.NovusHundredGigLan.EnableRsFec           = True
+        vport.L1Config.NovusHundredGigLan.EnableRsFecStats      = True
 
-    vPort2.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort2.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort2.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort2.L1Config.NovusHundredGigLan.EnableRsFecStats = True
-
-    vPort3.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort3.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort3.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort3.L1Config.NovusHundredGigLan.EnableRsFecStats = True
-
-    vPort4.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort4.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort4.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort4.L1Config.NovusHundredGigLan.EnableRsFecStats = True
-
-    vPort5.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort5.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort5.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort5.L1Config.NovusHundredGigLan.EnableRsFecStats = True
-
-    vPort6.L1Config.NovusHundredGigLan.IeeeL1Defaults  =  False
-    vPort6.L1Config.NovusHundredGigLan.EnableAutoNegotiation =False
-    vPort6.L1Config.NovusHundredGigLan.EnableRsFec = True
-    vPort6.L1Config.NovusHundredGigLan.EnableRsFecStats = True
-
-    vPort1.Name = 'Tx1'
-    vPort4.Name = 'Rx1'
-    state = vPort1.State
-
-    topology1    = ixNetwork.Topology.add(Name='Topo1', Ports=[vPort1, vPort2, vPort3, vPort4, vPort5, vPort6])
+    topology1    = ixNetwork.Topology.add(Name='Topo1', Ports=vport_list)
     deviceGroup1 = topology1.DeviceGroup.add(Name='DG1', Multiplier='1')
     ethernet1    = deviceGroup1.Ethernet.add(Name='Eth1')
     ipv4         = ethernet1.Ipv4.add(Name='Ipv4')
 
-    ipv4.GatewayIp.ValueList(['192.168.1.1','192.168.1.1','192.168.1.1','192.168.1.1', '192.168.1.1', '192.168.1.1'])
-    ipv4.Address.ValueList(['192.168.1.2','192.168.1.3','192.168.1.4','192.168.1.5', '192.168.1.6', '192.168.1.7'])
+    ipv4.GatewayIp.ValueList(gateway_value_list)
+    ipv4.Address.ValueList(interface_ip_list)
 
     ixNetwork.StartAllProtocols()
-    time.sleep(60)
+    logger.info("Wait for 5 seconds for iv4 sessions to up")
+    time.sleep(5) 
 
-    # Traffic
-    traffic_item = ixNetwork.Traffic.TrafficItem.add(Name='Traffic Test', TrafficType='ipv4')
-    dest = [{'arg1': '/api/v1/sessions/1/ixnetwork/topology/1/deviceGroup/1/ethernet/1/ipv4/1', 'arg2': 1, 'arg3': 4, 'arg4': 1 ,'arg5': 1}]
-    src  = [{'arg1': '/api/v1/sessions/1/ixnetwork/topology/1/deviceGroup/1/ethernet/1/ipv4/1', 'arg2': 1, 'arg3': 1, 'arg4': 1, 'arg5': 1}]
-    endPoint = traffic_item.EndpointSet.add()
+    # Create a traffic item 
+    traffic_item = ixNetwork.Traffic.TrafficItem.add(
+                   Name        = 'Traffic Test',
+                   TrafficType = 'ipv4')
+
+    # Create a ipv4 source and destination for the endpoint of traffic item.
+    src_dst_ep = create_ipv4_traffic_end_points (
+                 src_start_port        = 1,
+                 src_port_count        = 1,
+                 src_first_route_index = 1,
+                 src_route_count       = 1,
+                 dst_start_port        = 2,
+                 dst_port_count        = 3,
+                 dst_first_route_index = 1,
+                 dst_route_count       = 1
+    )
+
+    # Create endpoint set and set source and destination.
+    endPoint                      = traffic_item.EndpointSet.add()
+    endPoint.ScalableSources      = src_dst_ep[0]
+    endPoint.ScalableDestinations = src_dst_ep[1]
+
+    # Enable tracking.
     traffic_item.Tracking.find().TrackBy = ['trackingenabled0']
-    endPoint.ScalableSources = src
-    endPoint.ScalableDestinations = dest
+
+    # Generate, apply and start traffic.
     traffic_item.Generate()
     ixNetwork.Traffic.Apply()
     ixNetwork.Traffic.Start()
-    time.sleep(10)
+
+    logger.info("run traffic for 5 seconds")
+    time.sleep(5)
+
+    # Fetch statistics.
     logger.info(session.StatViewAssistant('Traffic Item Statistics'))
     ixNetwork.Traffic.Stop()
-
     assert 1
+
