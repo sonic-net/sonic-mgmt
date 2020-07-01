dad33d68fe6cea2d31adef5c3f2c08f2011d0a34
sujinmkang <sujkang@microsoft.com>
2020-07-01 00:41:56 -0700

    Use PlatformApiTestBase to accumulate error messages

--- tests/platform_tests/api/test_psu.py
+++ tests/platform_tests/api/test_psu.py
@@ -5,12 +5,15 @@ import pytest
 import yaml
 
 from common.helpers.assertions import pytest_assert
-from common.helpers.platform_api import psu
-from common.helpers.platform_api import chassis
+from common.helpers.platform_api import chassis, psu
+
+from platform_api_test_base import PlatformApiTestBase
+
 
 logger = logging.getLogger(__name__)
 
 pytestmark = [
+    pytest.mark.topology('any'),
     pytest.mark.sanity_check(skip_sanity=True),
     pytest.mark.disable_loganalyzer  # disable automatic loganalyzer
 ]
@@ -20,69 +23,68 @@ STATUS_LED_COLOR_AMBER = "amber"
 STATUS_LED_COLOR_RED = "red"
 STATUS_LED_COLOR_OFF = "off"
 
-class TestPsuApi(object):
+class TestPsuApi(PlatformApiTestBase):
     ''' Platform API test cases for the PSU class'''
+
+    num_psus = None
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, platform_api_conn):
+        if self.num_psus is None:
+            try:
+                self.num_psus = int(chassis.get_num_psus(platform_api_conn))
+            except:
+                pytest.fail("num_psus is not an integer")
+
 
     def test_fans(self, duthost, localhost, platform_api_conn):
         ''' PSU fan test '''
-        try:
-            num_psus = int(chassis.get_num_psus(platform_api_conn))
-        except:
-            pytest_assert("num_psus is not integer!")
-
-        for psu_id in range(num_psus):
+        for psu_id in range(self.num_psus):
             try:
                 num_fans = int(psu.get_num_fans(platform_api_conn, psu_id))
             except:
                 pytest.fail("num_fans is not an integer!")
 
             fan_list = psu.get_all_fans(platform_api_conn, psu_id)
-            pytest_assert(fan_list is not None, "Failed to retrieve fans of PSU {}".format(psu_id))
-            pytest_assert(isinstance(fan_list, list) and len(fan_list) == num_fans, "Fans of PSU {} appear to be incorrect".format(psu_id))
+            if self.expect(fan_list is not None, "Failed to retrieve fans of PSU {}".format(psu_id)):
+                self.expect(isinstance(fan_list, list) and len(fan_list) == num_fans, "Fans of PSU {} appear to be incorrect".format(psu_id))
 
             for i in range(num_fans):
                 fan = psu.get_fan(platform_api_conn, psu_id, i)
-                pytest_assert(fan and fan == fan_list[i], "Fan {} of PSU {} is incorrect".format(i, psu_id))
+                self.expect(fan and fan == fan_list[i], "Fan {} of PSU {} is incorrect".format(i, psu_id))
+        self.assert_expectations()
 
 
     def test_power(self, duthost, localhost, platform_api_conn):
         ''' PSU power test '''
-        try:
-            num_psus = int(chassis.get_num_psus(platform_api_conn))
-        except:
-            pytest_assert("num_psus is not integer!")
-
-        for psu_id in range(num_psus):
+        for psu_id in range(self.num_psus):
             voltage = psu.get_voltage(platform_api_conn, psu_id)
             current = psu.get_current(platform_api_conn, psu_id)
             power = psu.get_power(platform_api_conn, psu_id)
-            pytest_assert(abs(power - (voltage*current)) < power*0.1, "PSU {} reading does not make sense \
+            self.expect(abs(power - (voltage*current)) < power*0.1, "PSU {} reading does not make sense \
                 (power:{}, voltage:{}, current:{})".format(psu_id, power, voltage, current))
 
             powergood_status = psu.get_powergood_status(platform_api_conn, psu_id)
-            pytest_assert(powergood_status is not None, "Failed to retrieve operational status of PSU {}".format(psu_id))
-            pytest_assert(powergood_status is True, "PSU {} is not operational".format(psu_id))
+            if self.expect(powergood_status is not None, "Failed to retrieve operational status of PSU {}".format(psu_id)):
+                self.expect(powergood_status is True, "PSU {} is not operational".format(psu_id))
 
             high_threshold = psu.get_voltage_high_threshold(platform_api_conn, psu_id)
             low_threshold = psu.get_voltage_low_threshold(platform_api_conn, psu_id)
-            pytest_assert(high_threshold is not None and low_threshold is not None, "Failed to retrieve the voltage threshold values of PSU {}".format(psu_id))
-            pytest_assert(voltage < high_threshold and voltage > low_threshold, "Voltage {} of PSU {} is not in between {} and {}".format(voltage, psu_id, low_threshold, high_threshold))
+            if self.expect(high_threshold is not None and low_threshold is not None, "Failed to retrieve the voltage threshold values of PSU {}".format(psu_id)):
+                self.expect(voltage < high_threshold and voltage > low_threshold, "Voltage {} of PSU {} is not in between {} and {}".format(voltage, psu_id, low_threshold, high_threshold))
+        self.assert_expectations()
 
 
     def test_temperature(self, duthost, localhost, platform_api_conn):
         ''' PSU temperature test '''
-        try:
-            num_psus = int(chassis.get_num_psus(platform_api_conn))
-        except:
-            pytest_assert("num_psus is not integer!")
-
-        for psu_id in range(num_psus):
+        for psu_id in range(self.num_psus):
             temperature = psu.get_temperature(platform_api_conn, psu_id)
-            pytest_assert(temperature is not None, "Failed to retrieve temperature of PSU {}".format(psu_id))
+            self.expect(temperature is not None, "Failed to retrieve temperature of PSU {}".format(psu_id))
 
             temp_threshold = psu.get_temperature_high_threshold(platform_api_conn, psu_id)
-            pytest_assert(temp_threshold is not None, "Failed to retrieve temperature threshold of PSU {}".format(psu_id))
-            pytest_assert(temperature < temp_threshold, "Temperature {} of PSU {} is over the threshold {}".format(temperature, psu_id, temp_threshold))
+            if self.expect(temp_threshold is not None, "Failed to retrieve temperature threshold of PSU {}".format(psu_id)):
+                self.expect(temperature < temp_threshold, "Temperature {} of PSU {} is over the threshold {}".format(temperature, psu_id, temp_threshold))
+        self.assert_expectations()
 
 
     def test_led(self, duthost, localhost, platform_api_conn):
@@ -93,17 +95,14 @@ class TestPsuApi(object):
             STATUS_LED_COLOR_RED,
             STATUS_LED_COLOR_OFF
         ]
-        try:
-            num_psus = int(chassis.get_num_psus(platform_api_conn))
-        except:
-            pytest_assert("num_psus is not integer!")
 
-        for psu_id in range(num_psus):
+        for psu_id in range(self.num_psus):
             for color in LED_COLOR_LIST:
                 result = psu.set_status_led(platform_api_conn, psu_id, color)
-                pytest_assert(result is not None, "Failed to perform set_status_led of PSU {}".format(psu_id))
-                pytest_assert(result is True, "Failed to set status_led of PSU {}".format(psu_id))
+                if self.expect(result is not None, "Failed to perform set_status_led of PSU {}".format(psu_id)):
+                    self.expect(result is True, "Failed to set status_led of PSU {}".format(psu_id))
 
                 color_status = psu.get_status_led(platform_api_conn, psu_id)
-                pytest_assert(color == color_status, "Retrived the status_led {} not {} from PSU {}".format(color_status, color, psu_id))
+                self.expect(color == color_status, "Retrived the status_led {} not {} from PSU {}".format(color_status, color, psu_id))
+        self.assert_expectations()
 
