22d2d682b5ffdd17828c43056c8b6578b48c78b3
sujinmkang <sujkang@microsoft.com>
2020-07-15 21:00:44 -0700

    review comments

--- tests/platform_tests/api/test_watchdog.py
+++ tests/platform_tests/api/test_watchdog.py
@@ -67,7 +67,7 @@ class TestWatchdogApi(PlatformApiTestBase):
         actual_timeout = watchdog.arm(platform_api_conn, watchdog_timeout)
 
         if self.expect(actual_timeout is not None, "Watchdog.arm is not supported"):
-            pytest_assert(isinstance(actual_timeout, int), actual_timeout appears incorrect)
+            pytest_assert(isinstance(actual_timeout, int), "actual_timeout appears incorrect")
             if self.expect(actual_timeout != -1, "Failed to arm the watchdog"):
                 self.expect(actual_timeout >= watchdog_timeout, "Actual watchdog {} seconds apears wrong, should be less than {} seconds".format(actual_timeout, watchdog_timeout))
 
@@ -78,7 +78,7 @@ class TestWatchdogApi(PlatformApiTestBase):
         remaining_time = watchdog.get_remaining_time(platform_api_conn)
 
         if self.expect(remaining_time is not None, "Failed to get the remaining time of watchdog"):
-            pytest_assert(isinstance(remaining_time, int), remaining_time appears incorrect)
+            pytest_assert(isinstance(remaining_time, int), "remaining_time appears incorrect")
             self.expect(remaining_time <= watchdog_timeout, "Watchdog remaining_time {} seconds is wrong compared to watchdog timeout {} seocnds".format(remaining_time))
 
         watchdog_status = watchdog.disarm(platform_api_conn)
