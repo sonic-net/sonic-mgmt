1fe6dcc61bb1e3cfa070cfa27c034cd5d00da072
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-07-17 15:23:46 -0700

    Fix function name.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- tests/tacacs/test_ro_user.py
+++ tests/tacacs/test_ro_user.py
@@ -20,7 +20,7 @@ def test_ro_user(localhost, duthost, creds, setup_tacacs):
         if fds[0] == "test":
             assert fds[4] == "remote_user"
 
-def test_ro_user(localhost, duthost, creds, setup_tacacs_v6):
+def test_ro_user_ipv6(localhost, duthost, creds, setup_tacacs_v6):
 
     dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
     res = localhost.shell("sshpass -p {} ssh "\
