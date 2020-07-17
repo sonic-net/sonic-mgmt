f0e46d56193f826f3149756d64f70e952f07af02
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-07-16 17:52:10 -0700

    [IPv6]: Add test case for IPv6 TACACS.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- tests/tacacs/test_ro_user.py
+++ tests/tacacs/test_ro_user.py
@@ -19,3 +19,16 @@ def test_ro_user(localhost, duthost, creds, setup_tacacs):
         fds = l.split(':')
         if fds[0] == "test":
             assert fds[4] == "remote_user"
+
+def test_ro_user(localhost, duthost, creds, setup_tacacs_v6):
+
+    dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    res = localhost.shell("sshpass -p {} ssh "\
+                          "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "\
+                          "{}@{} cat /etc/passwd".format(
+            creds['tacacs_ro_user_passwd'], creds['tacacs_ro_user'], dutip))
+
+    for l in res['stdout_lines']:
+        fds = l.split(':')
+        if fds[0] == "test":
+            assert fds[4] == "remote_user"
