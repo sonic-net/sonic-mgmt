ca0ffd00cd2faecf3d4004ed1ac49a29c2403e82
abhijit-dhar <67135817+abhijit-dhar@users.noreply.github.com>
2020-07-25 07:10:03 +0530

    [ixia/Keysight] sample test cases and fixtures (#1819)
    * [ixia/Keysight] sample test cases and fixtures
    
    This directory should contain the Ixia ECN and RDMA test cases.
    The lib folder contains
          * fixtures required to run ixia test cases
          *  helper function required to write ixia test cases
    
    * [ixia/Keysight] adding ixia libraries
    
    As per review comments given against the 1819 we have to keep the ixia libraries under sonic-mgmt/tests/common/ixia/
    
    * [ixia/Keysight] This file should be in common/ixia
    
    This file should be in tests/common/ixia folder.
    So deleting it from tests/ixia/lib/ folder
    
    * [ixia/Keysight] lib/ixia_fixtures.py
    
    This file should be under the folder tests/common/ixia
    Hence deleting it form tests/ixia/lib
    
    * [ixia/Keysight] deleting ixia_helpers.py
    
    This file should be under tests/common/ixia
    So deleting it from tests/ixia/lib
    
    * [ixia/Keysight] updated test_ixia_traffic_restpy.py
    
    There were many review comment against this file. Some of the comments I have implemented. But some of the comments may be implemented in future. Like "(Wei Bai) Can you move all the low level IXIA API functions to ixia_helpers.py?" or "I have a high level comment. can we design some IXIA wrapper functions (e.g., configure ports, create a PFC traffic item) and only call these high level wrapper functions in py.test scripts without exposing too many IXIA low level details? (Wei Bai)"
    
    This comment may be addressed in future PRs once we come up with the high level wrapper APIs.
    
    * Update test_ixia_traffic_restpy.py
    
    * [ixia/Keysight] Changed as per review comments
    
    Added a new fixture ixia_api_server_session
    
    * [ixia/Keysight] added new IxiaFanoutManager class
    
    This class is added to efficiently manage ixia fanout devices
    
    * [ixia/Keysight] Adding __init__().py
    
    Making ixia directory a package, to access ixia_fixtures.py and ixia_helpers.py
    
    * [ixia/Keysight] Adding IxiaHost class
    
    This class is a place holder right now. Ixia fanout devices (chassis) do not require this abstraction layer. Ixia PTF (ixia API server) takes care of all the operation that we can do on the ixia fanout devices (chassis) and hence we execute them via API server only. But it is still nice to have all abstractions in the same location. So future users knows where to find them.
    
    * [ixia/Keysight] corrected typo added a new class
    
    Added a new class that should make chassis port management easier
    
    * [ixia/Keysight] Deleted from the review
    
    There was an typo
    
    * [ixia/Keysight] adding ixia_fixtures
    
    Deletes accidentally
    
    * [ixia/Keysight] updated ixia_fixtures.py
    
    * [ixia/Keysight] updating the review comments PR1819
    
    * Implementing the comments of PR1819
    
    * Updating the files as per the comment made on PR1819

--- tests/common/devices.py
+++ tests/common/devices.py
@@ -906,6 +906,54 @@ class OnyxHost(AnsibleHostBase):
             raise Exception("Unable to execute template\n{}".format(res["localhost"]["stdout"]))
 
 
+class IxiaHost (AnsibleHostBase):
+    """ This class is a place-holder for running ansible module on Ixia
+    fanout devices in future (TBD).
+    """
+    def __init__ (self, ansible_adhoc, os, hostname, device_type) :
+        """ Initializing Ixia fanout host for using ansible modules.
+
+        Note: Right now, it is just a place holder.
+
+        Args:
+            ansible_adhoc :The pytest-ansible fixture
+            os (str): The os type of Ixia Fanout.
+            hostname (str): The Ixia fanout host-name
+            device_type (str): The Ixia fanout device type.
+        """ 
+
+        self.ansible_adhoc = ansible_adhoc
+        self.os            = os
+        self.hostname      = hostname
+        self.device_type   = device_type
+        super().__init__(IxiaHost, self)
+   
+    def get_host_name (self):
+        """Returns the Ixia hostname
+
+        Args:
+            This function takes no argument.
+        """    
+        return self.hostname
+
+    def get_os (self) :
+        """Returns the os type of the ixia device.
+
+        Args:
+            This function takes no argument.
+        """    
+        return self.os
+
+    def execute (self, cmd) :
+        """Execute a given command on ixia fanout host.
+         
+        Args: 
+           cmd (str): Command to be executed.
+        """ 
+        if (self.os == 'ixia') :
+            eval(cmd)
+
+
 class FanoutHost():
     """
     @summary: Class for Fanout switch
@@ -927,7 +975,7 @@ class FanoutHost():
         elif os == 'ixia':
             # TODO: add ixia chassis abstraction
             self.os = os
-            self.host = None
+            self.host = IxiaHost(ansible_adhoc, os, hostname, device_type)
         else:
             # Use eos host if the os type is unknown
             self.os = 'eos'
