e163bda5ade054acdab74cc39d3d5f1e9d3d8318
Yong Zhao <yozhao@microsoft.com>
2020-07-29 09:03:22 -0700

    [pytest] Delete the debug code.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- tests/autorestart_test/test_container_autorestart.py
+++ tests/autorestart_test/test_container_autorestart.py
@@ -271,8 +271,6 @@ def test_containers_autorestart(duthost):
     disabled_containers = get_disabled_container_list(duthost)
 
     for container_name in container_autorestart_states.keys():
-        if container_name in ["bgp", "database", "restapi", "pmon", "radv"]:
-            continue
         # Skip testing the containers/services which are disabled by default
         if container_name in disabled_containers:
             continue
