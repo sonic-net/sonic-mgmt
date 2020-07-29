3b957732e76706cdc096a9bfde990232160d3273
Vaibhav Hemant Dixit <vaibhav.dixit@microsoft.com>
2020-07-29 09:50:37 -0700

    Image installation during continuous warm reboot test (#1977)
    * Runtime image installation during continuous warm reboot test

--- tests/platform_tests/args/cont_warm_reboot_args.py
+++ tests/platform_tests/args/cont_warm_reboot_args.py
@@ -25,3 +25,17 @@ def add_cont_warm_reboot_args(parser):
         default=False,
         help="Enable continuous IO",
     )
+
+    parser.addoption(
+        "--image_location",
+        action="store",
+        default=None,
+        help="Path to image(s) to be installed",
+    )
+
+    parser.addoption(
+        "--image_list",
+        action="store",
+        default="current",
+        help="Comma separated list of images to be installed during continuous reboot test",
+    )
