2f7a77ce1f098e6b937447b1868f088636358b41
shlomibitton <60430976+shlomibitton@users.noreply.github.com>
2020-08-06 23:21:46 +0300

    Fix watchdog test case. (#2052)
    Change test_arm_disarm_states condition watchdog_timeout -> actual_timeout like it should be.
    Fix msg description.
    
    Signed-off-by: Shlomi Bitton <shlomibi@mellanox.com>

--- tests/platform_tests/api/test_watchdog.py
+++ tests/platform_tests/api/test_watchdog.py
@@ -75,7 +75,7 @@ class TestWatchdogApi(PlatformApiTestBase):
         if self.expect(actual_timeout is not None, "Watchdog.arm is not supported"):
             if self.expect(isinstance(actual_timeout, int), "actual_timeout appears incorrect"):
                 if self.expect(actual_timeout != -1, "Failed to arm the watchdog"):
-                    self.expect(actual_timeout >= watchdog_timeout, "Actual watchdog timeout {} seconds appears wrong, should be less than {} seconds".format(actual_timeout, watchdog_timeout))
+                    self.expect(actual_timeout >= watchdog_timeout, "Actual watchdog timeout {} seconds appears wrong, should be equal or greater than {} seconds".format(actual_timeout, watchdog_timeout))
 
         watchdog_status = watchdog.is_armed(platform_api_conn)
         if self.expect(watchdog_status is not None, "Failed to retrieve watchdog status"):
@@ -85,7 +85,7 @@ class TestWatchdogApi(PlatformApiTestBase):
 
         if self.expect(remaining_time is not None, "Failed to get the remaining time of watchdog"):
             if self.expect(isinstance(remaining_time, int), "remaining_time appears incorrect"):
-                self.expect(remaining_time <= watchdog_timeout, "Watchdog remaining_time {} seconds appears wrong compared to watchdog timeout {} seocnds".format(remaining_time, watchdog_timeout))
+                self.expect(remaining_time <= actual_timeout, "Watchdog remaining_time {} seconds appears wrong compared to watchdog timeout {} seocnds".format(remaining_time, actual_timeout))
 
         watchdog_status = watchdog.disarm(platform_api_conn)
         if self.expect(watchdog_status is not None, "Watchdog.disarm is not supported"):
