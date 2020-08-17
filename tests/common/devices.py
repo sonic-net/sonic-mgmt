31a075ae82f183cec43a90dfda631b056637b207
Xin Wang <xiwang5@microsoft.com>
2020-08-17 23:22:41 +0800

    Add a generic method for running and parsing output of show commands (#2080)
    * Add a generic method for parsing output of show commands
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>
    
    * Process header line before processing the content

--- tests/common/devices.py
+++ tests/common/devices.py
@@ -769,6 +769,139 @@ default via fc00::1a dev PortChannel0004 proto 186 src fc00:1::32 metric 20  pre
             feature_status[r[0]] = r[1]
         return feature_status, True
 
+    def _parse_column_positions(self, sep_line, sep_char='-'):
+        """Parse the position of each columns in the command output
+
+        Args:
+            sep_line: The output line separating actual data and column headers
+            sep_char: The character used in separation line. Defaults to '-'.
+
+        Returns:
+            Returns a list. Each item is a tuple with two elements. The first element is start position of a column. The
+            second element is the end position of the column.
+        """
+        prev = ' ',
+        positions = []
+        for pos, char in enumerate(sep_line + ' '):
+            if char == sep_char:
+                if char != prev:
+                    left = pos
+            else:
+                if char != prev:
+                    right = pos
+                    positions.append((left, right))
+            prev = char
+        return positions
+
+
+    def _parse_show(self, output_lines):
+
+        result = []
+
+        sep_line_pattern = re.compile(r"^( *-+ *)+$")
+        sep_line_found = False
+        for idx, line in enumerate(output_lines):
+            if sep_line_pattern.match(line):
+                sep_line_found = True
+                header_line = output_lines[idx-1]
+                sep_line = output_lines[idx]
+                content_lines = output_lines[idx+1:]
+                break
+
+        if not sep_line_found:
+            logging.error('Failed to find separation line in the show command output')
+            return result
+
+        try:
+            positions = self._parse_column_positions(sep_line)
+        except Exception as e:
+            logging.error('Possibly bad command output, exception: {}'.format(repr(e)))
+            return result
+
+        headers = []
+        for (left, right) in positions:
+            headers.append(header_line[left:right].strip().lower())
+
+        for content_line in content_lines:
+            item = {}
+            for idx, (left, right) in enumerate(positions):
+                k = headers[idx]
+                v = content_line[left:right].strip()
+                item[k] = v
+            result.append(item)
+
+        return result
+
+    def show_and_parse(self, show_cmd, **kwargs):
+        """Run a show command and parse the output using a generic pattern.
+
+        This method can adapt to the column changes as long as the output format follows the pattern of
+        'show interface status'.
+
+        The key is to have a line of headers. Then a separation line with '-' under each column header. Both header and
+        column content are within the width of '-' chars for that column.
+
+        For example, part of the output of command 'show interface status':
+
+        admin@str-msn2700-02:~$ show interface status
+              Interface            Lanes    Speed    MTU    FEC    Alias             Vlan    Oper    Admin             Type    Asym PFC
+        ---------------  ---------------  -------  -----  -----  -------  ---------------  ------  -------  ---------------  ----------
+              Ethernet0          0,1,2,3      40G   9100    N/A     etp1  PortChannel0002      up       up   QSFP+ or later         off
+              Ethernet4          4,5,6,7      40G   9100    N/A     etp2  PortChannel0002      up       up   QSFP+ or later         off
+              Ethernet8        8,9,10,11      40G   9100    N/A     etp3  PortChannel0005      up       up   QSFP+ or later         off
+        ...
+
+        The parsed example will be like:
+            [{
+                "oper": "up",
+                "lanes": "0,1,2,3",
+                "fec": "N/A",
+                "asym pfc": "off",
+                "admin": "up",
+                "type": "QSFP+ or later",
+                "vlan": "PortChannel0002",
+                "mtu": "9100",
+                "alias": "etp1",
+                "interface": "Ethernet0",
+                "speed": "40G"
+              },
+              {
+                "oper": "up",
+                "lanes": "4,5,6,7",
+                "fec": "N/A",
+                "asym pfc": "off",
+                "admin": "up",                                                                                                                                                                                                                             "type": "QSFP+ or later",                                                                                                                                                                                                                  "vlan": "PortChannel0002",                                                                                                                                                                                                                 "mtu": "9100",                                                                                                                                                                                                                             "alias": "etp2",
+                "interface": "Ethernet4",
+                "speed": "40G"
+              },
+              {
+                "oper": "up",
+                "lanes": "8,9,10,11",
+                "fec": "N/A",
+                "asym pfc": "off",
+                "admin": "up",
+                "type": "QSFP+ or later",
+                "vlan": "PortChannel0005",
+                "mtu": "9100",
+                "alias": "etp3",
+                "interface": "Ethernet8",
+                "speed": "40G"
+              },
+              ...
+            ]
+
+        Args:
+            show_cmd: The show command that will be executed.
+
+        Returns:
+            Return the parsed output of the show command in a list of dictionary. Each list item is a dictionary,
+            corresponding to one content line under the header in the output. Keys of the dictionary are the column
+            headers in lowercase.
+        """
+        output = self.shell(show_cmd, **kwargs)["stdout_lines"]
+        return self._parse_show(output)
+
+
 class EosHost(AnsibleHostBase):
     """
     @summary: Class for Eos switch
