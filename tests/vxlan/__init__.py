bc11655e346802e8e748feb08a4798f217e2a50a
Lawrence Lee <lfqlee314@gmail.com>
2020-08-21 13:02:36 -0700

    Fix typo in tests/vxlan/__init__.py
    Co-authored-by: Danny Allen <daall@microsoft.com>

--- tests/vxlan/__init__.py
+++ tests/vxlan/__init__.py
@@ -84,7 +84,7 @@ def vnet_test_params(request):
         request: Pytest fixture containing parsed CLI parameters
 
     Returns:
-        A dictienary holding each parameter with the parameter name as the key
+        A dictionary holding each parameter with the parameter name as the key
             * ipv6_vxlan_test - whether to include ipv6 functionality in testing
             * cleanup - whether to remove test data/configs after test in finished
     """
