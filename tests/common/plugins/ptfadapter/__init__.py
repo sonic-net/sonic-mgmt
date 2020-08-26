2e03d87de9238fba7c32698f0461a222ddc2a510
Xin Wang <xiwang5@microsoft.com>
2020-08-26 16:36:58 +0800

    Add an option to skip packet payload updating

--- tests/common/plugins/ptfadapter/__init__.py
+++ tests/common/plugins/ptfadapter/__init__.py
@@ -11,35 +11,41 @@ DEFAULT_DEVICE_NUM = 0
 ETH_PFX = 'eth'
 
 
-# Below code is to override the 'send' function in the ptf.testutils module. Purpose of this change is to insert
-# code for updating the packet pattern before send it out. Generally we want to make the payload part of injected
-# packet to have string of current test module and case name. While inspecting the captured packets, it is easier
-# to fiture out which packets are injected by which test case.
-def _send(test, port_id, pkt, count=1):
-    update_payload = getattr(test, "update_payload", None)
-    if update_payload and callable(update_payload):
-        pkt = test.update_payload(pkt)
-
-    return ptf.testutils.send_packet(test, port_id, pkt, count=count)
-setattr(ptf.testutils, "send", _send)
+def pytest_addoption(parser):
+    parser.addoption("--keep_payload", action="store_true", default=False,
+                     help="Keep the original packet payload, do not update payload to default pattern")
 
 
-# Below code is to override the 'dp_poll' function in the ptf.testutils module. This function is called by all
-# the other functions for receiving packets in the ptf.testutils module. Purpose of this overriding is to update
-# the payload of received packet using the same method to match the updated injected packets.
-def _dp_poll(test, device_number=0, port_number=None, timeout=-1, exp_pkt=None):
-    update_payload = getattr(test, "update_payload", None)
-    if update_payload and callable(update_payload):
-        exp_pkt = test.update_payload(exp_pkt)
+def override_ptf_functions():
+    # Below code is to override the 'send' function in the ptf.testutils module. Purpose of this change is to insert
+    # code for updating the packet pattern before send it out. Generally we want to make the payload part of injected
+    # packet to have string of current test module and case name. While inspecting the captured packets, it is easier
+    # to fiture out which packets are injected by which test case.
+    def _send(test, port_id, pkt, count=1):
+        update_payload = getattr(test, "update_payload", None)
+        if update_payload and callable(update_payload):
+            pkt = test.update_payload(pkt)
 
-    result = test.dataplane.poll(
-        device_number=device_number, port_number=port_number,
-        timeout=timeout, exp_pkt=exp_pkt, filters=ptf.testutils.FILTERS
-    )
-    if isinstance(result, test.dataplane.PollSuccess):
-        test.at_receive(result.packet, device_number=result.device, port_number=result.port)
-    return result
-setattr(ptf.testutils, "dp_poll", _dp_poll)
+        return ptf.testutils.send_packet(test, port_id, pkt, count=count)
+    setattr(ptf.testutils, "send", _send)
+
+
+    # Below code is to override the 'dp_poll' function in the ptf.testutils module. This function is called by all
+    # the other functions for receiving packets in the ptf.testutils module. Purpose of this overriding is to update
+    # the payload of received packet using the same method to match the updated injected packets.
+    def _dp_poll(test, device_number=0, port_number=None, timeout=-1, exp_pkt=None):
+        update_payload = getattr(test, "update_payload", None)
+        if update_payload and callable(update_payload):
+            exp_pkt = test.update_payload(exp_pkt)
+
+        result = test.dataplane.poll(
+            device_number=device_number, port_number=port_number,
+            timeout=timeout, exp_pkt=exp_pkt, filters=ptf.testutils.FILTERS
+        )
+        if isinstance(result, test.dataplane.PollSuccess):
+            test.at_receive(result.packet, device_number=result.device, port_number=result.port)
+        return result
+    setattr(ptf.testutils, "dp_poll", _dp_poll)
 
 
 def get_ifaces(netdev_output):
@@ -100,7 +106,9 @@ def ptfadapter(ptfhost, testbed, request):
     ptfhost.command('supervisorctl restart ptf_nn_agent')
 
     with PtfTestAdapter(testbed['ptf_ip'], DEFAULT_PTF_NN_PORT, 0, ifaces_map.keys()) as adapter:
-        node_id = request.module.__name__
-        adapter.payload_pattern = node_id + " "
+        if not request.config.option.keep_payload:
+            override_ptf_functions()
+            node_id = request.module.__name__
+            adapter.payload_pattern = node_id + " "
 
         yield adapter
