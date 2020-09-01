3779eb80273267d83bbd665d044776694dd5de6a
Lawrence Lee <lawlee@microsoft.com>
2020-09-01 14:05:37 -0700

    [vnet_route_leak]: Add reload scenarios
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/vxlan/test_vnet_route_leak.py
+++ tests/vxlan/test_vnet_route_leak.py
@@ -2,6 +2,7 @@ import logging
 import pytest
 import re
 
+from tests.common.utilities import wait_until
 from vnet_constants import *
 from vnet_utils import cleanup_vnet_routes, cleanup_dut_vnets, cleanup_vxlan_tunnels, \
                        apply_dut_config_files, generate_dut_config_files
@@ -12,11 +13,17 @@ pytestmark = [
     pytest.mark.topology("t0")
 ]
 
+BGP_WAIT_TIMEOUT = 120
+
 TESTING_STATUS = "Testing"
 CLEANUP_STATUS = "Cleanup"
+
 SHOW_VNET_ROUTES_CMD = "show vnet routes all"
 SHOW_BGP_SUMMARY_CMD = "show ip bgp summary"
 SHOW_BGP_ADV_ROUTES_CMD_TEMPLATE = "show ip bgp neighbor {} advertised-routes"
+RESTART_BGP_CMD = "sudo systemctl restart bgp"
+CONFIG_SAVE_CMD = "sudo config save -y"
+CONFIG_RELOAD_CMD = "sudo config reload -y"
 
 
 @pytest.fixture(scope="module")
@@ -45,19 +52,56 @@ def configure_dut(minigraph_facts, duthost, vnet_config, vnet_test_params):
         cleanup_vnet_routes(duthost, vnet_test_params)
         cleanup_dut_vnets(duthost, minigraph_facts, vnet_config)
         cleanup_vxlan_tunnels(duthost, vnet_test_params)
+
+        logger.info("Reloading CONFIG_DB from JSON")
+
+        duthost.shell(CONFIG_SAVE_CMD)
+        duthost.shell(RESTART_BGP_CMD)
     else:
         logger.info("Skipping cleanup")
 
 
-def test_vnet_route_leak(configure_dut, duthost):
+def bgp_connected(duthost):
     """
-    Test case for VNET route leak check
+    Checks if BGP connections are up
 
-    Gets a list of all VNET routes programmed to the DUT, and a list of all BGP neighbors
-    Verifies that no VNET routes are being advertised to BGP neighbors
+    BGP connections are "up" once they have received all prefixes (6400) from all neighbors
 
     Args:
-        configure_dut: Pytest fixture to prepare DUT for testing
+        duthost: DUT host object
+    """
+
+    bgp_summary = duthost.shell(SHOW_BGP_SUMMARY_CMD)["stdout"].split("\n")
+    logger.debug("BGP Summary: {}".format(bgp_summary))
+
+    # Match entire line containing BGP neighbor information
+    bgp_neighbor_regex = re.compile(r"^([0-9]{1,3}\.){3}[0-9]{1,3}.*")
+
+    bgp_neighbors = []
+
+    for line in bgp_summary:
+        matched = bgp_neighbor_regex.match(line)
+
+        if matched:
+            bgp_neighbors.append(str(matched.group(0)))
+
+    if not bgp_neighbors:
+        return False
+
+    for neighbor in bgp_neighbors:
+        if neighbor.split()[9] != "6400":
+            return False
+
+    logger.info("BGP sessions up")
+
+    return True
+
+
+def no_vnet_route_leak(duthost):
+    """
+    Gets all VNET routes and checks that they are not advertised to any BGP neighbors
+
+    Args:
         duthost: DUT host object
     """
 
@@ -75,18 +119,62 @@ def test_vnet_route_leak(configure_dut, duthost):
 
     # Match only IP addresses at the beginning of the line
     # Only IP addresses of neighbors should be matched by this
-    ip_addr_regex = re.compile(r"^([0-9]{1,3}\.){3}[0-9]{1,3}")
+    bgp_neighbor_addr_regex = re.compile(r"^([0-9]{1,3}\.){3}[0-9]{1,3}")
 
     bgp_neighbors = []
 
     for line in bgp_summary:
-        matched = ip_addr_regex.match(line)
+        matched = bgp_neighbor_addr_regex.match(line)
 
         if matched:
             bgp_neighbors.append(str(matched.group(0)))
+
+    leaked_routes = {}
 
     for neighbor in bgp_neighbors:
         adv_routes = duthost.shell(SHOW_BGP_ADV_ROUTES_CMD_TEMPLATE.format(neighbor))["stdout"]
 
         for prefix in vnet_prefixes:
-            assert prefix not in adv_routes
+            if prefix in adv_routes:
+                if neighbor not in leaked_routes:
+                    leaked_routes[neighbor] = []
+                leaked_routes[neighbor].append(str(prefix))
+
+    for neighbor, routes in leaked_routes.items():
+        logger.error("VNET routes leaked to neighbor {}: {}".format(neighbor, routes))
+
+    return not leaked_routes
+
+
+def test_vnet_route_leak(configure_dut, duthost):
+    """
+    Test case for VNET route leak check
+
+    Gets a list of all VNET routes programmed to the DUT, and a list of all BGP neighbors
+    Verifies that no VNET routes are being advertised to BGP neighbors
+
+    Args:
+        configure_dut: Pytest fixture to prepare DUT for testing
+        duthost: DUT host object
+    """
+
+    assert no_vnet_route_leak(duthost)
+
+    logger.info("Restarting BGP")
+    duthost.shell(RESTART_BGP_CMD)
+
+    if not wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost):
+        logger.error("BGP sessions not established after {} seconds".format(BGP_WAIT_TIMEOUT))
+        assert False
+
+    assert no_vnet_route_leak(duthost)
+
+    logger.info("Saving and reloading CONFIG_DB")
+    duthost.shell(CONFIG_SAVE_CMD)
+    duthost.shell(CONFIG_RELOAD_CMD)
+
+    if not wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost):
+        logger.error("BGP sessions not established after {} seconds".format(BGP_WAIT_TIMEOUT))
+        assert False
+
+    assert no_vnet_route_leak(duthost)
