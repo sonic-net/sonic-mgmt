7403f1b42ceb59a55bcd0f111f241f98a5b42ab5
Lawrence Lee <lawlee@microsoft.com>
2020-09-01 20:41:30 -0700

    [vnet_route_leak]: Address review comments
    * Implicitly yield `None`
    * Backup config_db and restore instead of using `config save`
    * Add example regex match
    * Use `defaultdict` to store leaked routes
    * Use `pytest_assert` in place of `assert`
    * Add clarifying comments
    
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/vxlan/test_vnet_route_leak.py
+++ tests/vxlan/test_vnet_route_leak.py
@@ -2,6 +2,7 @@ import logging
 import pytest
 import re
 
+from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import wait_until
 from vnet_constants import *
 from vnet_utils import cleanup_vnet_routes, cleanup_dut_vnets, cleanup_vxlan_tunnels, \
@@ -24,6 +25,12 @@ SHOW_BGP_ADV_ROUTES_CMD_TEMPLATE = "show ip bgp neighbor {} advertised-routes"
 RESTART_BGP_CMD = "sudo systemctl restart bgp"
 CONFIG_SAVE_CMD = "sudo config save -y"
 CONFIG_RELOAD_CMD = "sudo config reload -y"
+BACKUP_CONFIG_DB_CMD = "sudo cp /etc/sonic/config_db.json /etc/sonic/config_db.json.route_leak_orig"
+RESTORE_CONFIG_DB_CMD = "sudo cp /etc/sonic/config_db.json.route_leak_orig /etc/sonic/config_db.json"
+DELETE_BACKUP_CONFIG_DB_CMD = "sudo rm /etc/sonic/config_db.json.route_leak_orig"
+
+BGP_ERROR_TEMPLATE = "BGP sessions not established after {} seconds"
+LEAKED_ROUTES_TEMPLATE = "Leaked routes: {}"
 
 
 @pytest.fixture(scope="module")
@@ -41,21 +48,25 @@ def configure_dut(minigraph_facts, duthost, vnet_config, vnet_test_params):
         vnet_test_params: Dictionary containing VNET test parameters
     """
 
+    logger.info("Backing up config_db.json")
+    duthost.shell(BACKUP_CONFIG_DB_CMD)
+
     duthost.shell("sonic-clear fdb all")
     generate_dut_config_files(duthost, minigraph_facts, vnet_test_params, vnet_config)
     apply_dut_config_files(duthost, vnet_test_params)
 
     # In this case yield is used only to separate this fixture into setup and teardown portions
-    yield None
+    yield
 
     if vnet_test_params[CLEANUP_KEY]:
+        logger.info("Restoring config_db.json")
+        duthost.shell(RESTORE_CONFIG_DB_CMD)
+        duthost.shell(DELETE_BACKUP_CONFIG_DB_CMD)
+
         cleanup_vnet_routes(duthost, vnet_test_params)
         cleanup_dut_vnets(duthost, minigraph_facts, vnet_config)
         cleanup_vxlan_tunnels(duthost, vnet_test_params)
 
-        logger.info("Reloading CONFIG_DB from JSON")
-
-        duthost.shell(CONFIG_SAVE_CMD)
         duthost.shell(RESTART_BGP_CMD)
     else:
         logger.info("Skipping cleanup")
@@ -74,7 +85,8 @@ def bgp_connected(duthost):
     bgp_summary = duthost.shell(SHOW_BGP_SUMMARY_CMD)["stdout"].split("\n")
     logger.debug("BGP Summary: {}".format(bgp_summary))
 
-    # Match entire line containing BGP neighbor information
+    # Match entire line containing BGP neighbor information such as:
+    # 10.0.0.1        4      64802    6419   10273        0    0    0 00:09:36         6400   ARISTA01T1
     bgp_neighbor_regex = re.compile(r"^([0-9]{1,3}\.){3}[0-9]{1,3}.*")
 
     bgp_neighbors = []
@@ -97,7 +109,7 @@ def bgp_connected(duthost):
     return True
 
 
-def no_vnet_route_leak(duthost):
+def get_leaked_routes(duthost):
     """
     Gets all VNET routes and checks that they are not advertised to any BGP neighbors
 
@@ -111,6 +123,8 @@ def no_vnet_route_leak(duthost):
     vnet_prefixes = []
 
     for line in vnet_routes:
+        # Ignore header lines and separators
+        # All other lines will contain numbers in the form of an IP address/prefix, which is the information we want to extract
         if any(char.isdigit() for char in line):
             vnet_prefixes.append(line.split()[1])
 
@@ -129,21 +143,16 @@ def no_vnet_route_leak(duthost):
         if matched:
             bgp_neighbors.append(str(matched.group(0)))
 
-    leaked_routes = {}
+    leaked_routes = defaultdict(list)
 
     for neighbor in bgp_neighbors:
         adv_routes = duthost.shell(SHOW_BGP_ADV_ROUTES_CMD_TEMPLATE.format(neighbor))["stdout"]
 
         for prefix in vnet_prefixes:
             if prefix in adv_routes:
-                if neighbor not in leaked_routes:
-                    leaked_routes[neighbor] = []
-                leaked_routes[neighbor].append(str(prefix))
+                leaked_routes["Neighbor {}".format(neighbor)].append(str(prefix))
 
-    for neighbor, routes in leaked_routes.items():
-        logger.error("VNET routes leaked to neighbor {}: {}".format(neighbor, routes))
-
-    return not leaked_routes
+    return leaked_routes
 
 
 def test_vnet_route_leak(configure_dut, duthost):
@@ -158,23 +167,22 @@ def test_vnet_route_leak(configure_dut, duthost):
         duthost: DUT host object
     """
 
-    assert no_vnet_route_leak(duthost)
+    leaked_routes = get_leaked_routes(duthost)
+    pytest_assert(not leaked_routes, LEAKED_ROUTES_TEMPLATE.format(leaked_routes))
 
     logger.info("Restarting BGP")
     duthost.shell(RESTART_BGP_CMD)
 
-    if not wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost):
-        logger.error("BGP sessions not established after {} seconds".format(BGP_WAIT_TIMEOUT))
-        assert False
+    pytest_assert(wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost), BGP_ERROR_TEMPLATE.format(BGP_WAIT_TIMEOUT))
 
-    assert no_vnet_route_leak(duthost)
+    leaked_routes = get_leaked_routes(duthost)
+    pytest_assert(not leaked_routes, LEAKED_ROUTES_TEMPLATE.format(leaked_routes))
 
     logger.info("Saving and reloading CONFIG_DB")
     duthost.shell(CONFIG_SAVE_CMD)
     duthost.shell(CONFIG_RELOAD_CMD)
 
-    if not wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost):
-        logger.error("BGP sessions not established after {} seconds".format(BGP_WAIT_TIMEOUT))
-        assert False
+    pytest_assert(wait_until(BGP_WAIT_TIMEOUT, 5, bgp_connected, duthost), BGP_ERROR_TEMPLATE.format(BGP_WAIT_TIMEOUT))
 
-    assert no_vnet_route_leak(duthost)
+    leaked_routes = get_leaked_routes(duthost)
+    pytest_assert(not leaked_routes, LEAKED_ROUTES_TEMPLATE.format(leaked_routes))
