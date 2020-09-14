7186f3544fbcc4ec6a7cf08781c7b47a15d6754e
Oleksandr Kozodoi <68429300+OleksandrKozodoi@users.noreply.github.com>
2020-09-14 16:26:43 +0300

    Remove spaces

--- tests/common/plugins/loganalyzer/__init__.py
+++ tests/common/plugins/loganalyzer/__init__.py
@@ -23,9 +23,9 @@ def loganalyzer(duthost, request):
             "/usr/sbin/logrotate -f /etc/logrotate.conf > /dev/null 2>&1"
             )
     except RunAnsibleModuleFail as e:
-        logging.warning("logrotate is failed. Command returned:\n" 
-                        "Stdout: {}\n" 
-                        "Stderr: {}\n" 
+        logging.warning("logrotate is failed. Command returned:\n"
+                        "Stdout: {}\n"
+                        "Stderr: {}\n"
                         "Return code: {}".format(e.results["stdout"], e.results["stderr"], e.results["rc"]))
 
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix=request.node.name)
