57b81595c2bc1ea02c915b6aaa83225ca69d8a31
bingwang <bingwang@microsoft.com>
2020-09-14 19:01:14 -0700

    Add an argument for allow_module_level skip

--- tests/common/helpers/assertions.py
+++ tests/common/helpers/assertions.py
@@ -5,8 +5,8 @@ def pytest_assert(condition, message = None):
     if not condition:
         pytest.fail(message)
 
-def pytest_require(condition, skip_message=""):
+def pytest_require(condition, skip_message="", allow_module_level=True):
     if not condition:
         # We can't use pytest.skip here because pytest after 3.0
         # doesn't allow to call skip outside a test case or fixture.
-        raise pytest.skip.Exception(skip_message, allow_module_level=True)
+        raise pytest.skip.Exception(skip_message, allow_module_level=allow_module_level)
