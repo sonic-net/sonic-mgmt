4e31839e4ddced9505a64fc689e2bc395098b8f5
bingwang <bingwang@microsoft.com>
2020-09-16 21:58:47 -0700

    add a check for existing fixture

--- tests/common/plugins/custom_fixtures/__init__.py
+++ tests/common/plugins/custom_fixtures/__init__.py
@@ -6,6 +6,9 @@ def pytest_collection_modifyitems(items):
     for item in items:
         asic_marks = [mark for mark in item.iter_markers(name="asic")]
         if asic_marks:
+            if 'check_dut_asic_type' in item.fixturenames:
+                # Already added
+                return
             for pos, name in enumerate(item.fixturenames):
                 if item._fixtureinfo.name2fixturedefs[name][0].scope == 'module':
                     break
