476fef0bf1c1c1fdb0f25145645babfc15badb2f
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2020-09-17 14:16:04 +0800

    [framework]Update hook to ensure check_dut_asic_type runs before any other module-level fixtures (#2221)
    * Update hook for custom fixture.
    
    Fixture check_dut_asic_type is set to function level, as a result, it
    always runs after all module-level fixtures. This commit update the hook
    for custom fixture. The check_dut_asic_type fixture is inserted into
    fixturelist before all module-level fixture, so that these fixtures
    no longer runs if the case is to be skipped.
    
    * fix bug when iteration end
    
    * fix LGTM
    
    * add a check for existing fixture

--- tests/common/plugins/custom_fixtures/__init__.py
+++ tests/common/plugins/custom_fixtures/__init__.py
@@ -6,5 +6,13 @@ def pytest_collection_modifyitems(items):
     for item in items:
         asic_marks = [mark for mark in item.iter_markers(name="asic")]
         if asic_marks:
-            item.fixturenames.append("check_dut_asic_type")
+            if 'check_dut_asic_type' in item.fixturenames:
+                # Already added
+                return
+            for pos, name in enumerate(item.fixturenames):
+                if item._fixtureinfo.name2fixturedefs[name][0].scope == 'module':
+                    break
+            else:
+                pos = len(item.fixturenames)
+            item.fixturenames.insert(pos, "check_dut_asic_type")
 
