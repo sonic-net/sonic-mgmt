8cc57f9440910bf9c50b798f02eecfb847abc966
bingwang <bingwang@microsoft.com>
2020-09-29 00:15:52 -0700

    Add missing import for get_advanced_reboot

--- tests/platform_tests/conftest.py
+++ tests/platform_tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from tests.common.fixtures.advanced_reboot import get_advanced_reboot
 from .args.normal_reboot_args import add_normal_reboot_args
 from .args.advanced_reboot_args import add_advanced_reboot_args
 from .args.cont_warm_reboot_args import add_cont_warm_reboot_args
