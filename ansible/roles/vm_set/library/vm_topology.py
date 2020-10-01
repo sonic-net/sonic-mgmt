b5de6f55f48720602d6cdfe45f87012c92fe1dde
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-09-30 17:55:34 -0700

    Enable IPv6 packet forwarding by setting net.ipv6.conf.all.forwarding to 1.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- ansible/roles/vm_set/library/vm_topology.py
+++ ansible/roles/vm_set/library/vm_topology.py
@@ -301,6 +301,7 @@ class VMTopology(object):
             if mgmt_gw:
                 VMTopology.cmd("nsenter -t %s -n ip route add default via %s dev %s" % (self.pid, mgmt_gw, int_if))
             if mgmt_ipv6_addr and mgmt_gw_v6:
+                VMTopology.cmd("echo 1 > /proc/sys/net/ipv6/conf/all/forwarding")
                 VMTopology.cmd("nsenter -t %s -n ip -6 addr replace %s dev %s" % (self.pid, mgmt_ipv6_addr, int_if))
                 VMTopology.cmd("nsenter -t %s -n ip -6 route replace default via %s dev %s" % (self.pid, mgmt_gw_v6, int_if))
 
