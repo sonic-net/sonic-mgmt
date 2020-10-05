2e16676a146ca42e11ed8502843bbd60bfbe9354
Yong Zhao <yozhao@microsoft.com>
2020-10-05 15:48:01 -0700

    [pytest] Test the running status of Monit service.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

---
+++ tests/monit_test/test_monit_service_status.py
@@ -0,0 +1,26 @@
+"""
+Test the running status of Monit service
+"""
+import logging
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+
+def test_monit_service_status(duthost):
+    """
+    @summary: Test the running status of Monit service by analyzing the command
+              output of "sudo systemctl status monit.service | grep Active".
+    """
+    monit_service_status_info = duthost.shell("sudo systemctl status monit.service | grep Active")
+
+    status_line = monit_service_status_info["stdout_lines"][0].strip()
+    if "active" in status_line:
+        logger.info("Monit service is running.")
+    elif "failed" in status_line:
+        pytest.fail("Monit service failed to run.")
