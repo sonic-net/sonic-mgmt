cc6c755723d2f2c87c0ba27b394fbd078e7b77c6
Yong Zhao <yozhao@microsoft.com>
2020-10-08 11:24:14 -0700

    [pytest] Show possible reasons in the message why Monit service is not running.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- tests/monit_test/test_monit_service_status.py
+++ tests/monit_test/test_monit_service_status.py
@@ -23,4 +23,4 @@ def test_monit_service_status(duthost):
     if "active" in status_line:
         logger.info("Monit service is running.")
     else:
-        pytest.fail("Monit service is not running.")
+        pytest.fail("Monit service is in 'dead|failed' state and is not running.")
