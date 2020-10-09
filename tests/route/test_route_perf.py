300c9aecc0c88fb334c673eaa9ece95a5ff168f6
shi-su <ssu@microsoft.com>
2020-10-09 22:49:25 +0000

    Add performance test for set/delete routes

---
+++ tests/route/test_route_perf.py
@@ -0,0 +1,142 @@
+import pytest
+import ipaddress
+import json
+import logging
+import time
+from datetime import datetime
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.utilities import wait_until
+
+logger = logging.getLogger(__name__)
+
+ROUTE_JSON = 'route.json'
+NEIGH_JSON = 'neigh.json'
+ROUTE_TIMEOUT = 20 # Time limit for applying route changes
+ROUTE_TABLE_NAME = 'ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY'
+
+def prepare_dut(duthost):
+    # Set up interface
+    duthost.shell('sudo sonic-cfggen -a \'{\"INTERFACE\":{\"Ethernet72\":{\"NULL\":\"NULL\"}}}\' --write-to-db')
+    duthost.shell('sudo sonic-cfggen -a \'{\"INTERFACE\":{\"Ethernet72|10.1.0.54/31\":{\"NULL\":\"NULL\"}}}\' --write-to-db')
+    
+    # Set up neighbor
+    neighbor = [{
+        'NEIGH_TABLE:Ethernet72:10.1.0.37': {
+            'neigh': '55:54:00:ad:48:98',
+            'family': 'IPv4'
+        },
+        'OP': 'SET'
+    }]
+    duthost.copy(content=json.dumps(neighbor, indent=4), dest=NEIGH_JSON)
+    duthost.shell('docker cp {} swss:/'.format(NEIGH_JSON))
+    duthost.shell('docker exec -i swss swssconfig /{}'.format(NEIGH_JSON))
+
+    time.sleep(5)
+
+def cleanup_dut(duthost):
+    # Delete neighbor
+    neighbor = [{
+        'NEIGH_TABLE:Ethernet72:10.1.0.37': {
+            'neigh': '55:54:00:ad:48:98',
+            'family': 'IPv4'
+        },
+        'OP': 'SET'
+    }]
+    duthost.copy(content=json.dumps(neighbor, indent=4), dest='NEIGH_JSON')
+    duthost.shell('docker cp {} swss:/'.format(NEIGH_JSON))
+    duthost.shell('docker exec -i swss swssconfig /{}'.format(NEIGH_JSON))
+
+    # remove files
+    duthost.shell('rm {}'.format(ROUTE_JSON))
+    duthost.shell('rm {}'.format(NEIGH_JSON))
+    duthost.shell('docker exec -i swss rm {}'.format(ROUTE_JSON))
+    duthost.shell('docker exec -i swss rm {}'.format(NEIGH_JSON))
+
+    time.sleep(5)
+
+def generate_route_file(duthost, prefixes, op):
+    route_data = []
+    for prefix in prefixes:
+        key = 'ROUTE_TABLE:' + prefix
+        route = {}
+        route['ifname'] = 'Ethernet72'
+        route['nexthop'] = '10.1.0.37'
+        route_command = {}
+        route_command[key] = route
+        route_command['OP'] = op
+        route_data.append(route_command)
+
+    # Copy json file to DUT
+    duthost.copy(content=json.dumps(route_data, indent=4), dest=ROUTE_JSON)
+
+def exec_routes(duthost, num_routes, op):
+    # generate ip prefixes of routes
+    prefixes = ['%d.%d.%d.%d/%d' % (101 + int(idx_route / 256 ** 2), int(idx_route / 256) % 256, idx_route % 256, 0, 24)
+                for idx_route in range(num_routes)]
+
+    # Generate json file for routes
+    generate_route_file(duthost, prefixes, op)
+
+    def _check_num_routes(expected_num_routes):
+        # Check the number of routes in ASIC_DB
+        num_routes = int(duthost.shell('sonic-db-cli ASIC_DB keys \'{}*\' | wc -l'.format(ROUTE_TABLE_NAME))['stdout'])
+        return num_routes == expected_num_routes
+
+    # Copy route file to swss container
+    duthost.shell('docker cp {} swss:/'.format(ROUTE_JSON))
+    
+    # Check the number of routes in ASIC_DB
+    start_num_route = int(duthost.shell('sonic-db-cli ASIC_DB keys \'{}*\' | wc -l'.format(ROUTE_TABLE_NAME))['stdout'])
+    
+    # Calculate expected number of route and record start time
+    if op == 'SET':
+        expected_num_routes = start_num_route + num_routes
+    elif op == 'DEL':
+        expected_num_routes = start_num_route - num_routes
+    else:
+        pytest.fail('Operation {} not supported'.format(op))
+    start_time = datetime.now()
+
+    # Apply routes with swssconfig
+    duthost.shell('docker exec -i swss swssconfig /{}'.format(ROUTE_JSON))
+
+    # Wait until the routes set/del applys to ASIC_DB
+    if not wait_until(ROUTE_TIMEOUT, 0.1, _check_num_routes, expected_num_routes):
+        pytest.fail('failed to add routes within time limit')
+
+    # Record time when all routes show up in ASIC_DB
+    end_time = datetime.now()
+
+    # Check route entries are correct
+    asic_route_lines = duthost.shell('sonic-db-cli ASIC_DB keys \'{}*\''.format(ROUTE_TABLE_NAME))['stdout_lines']
+    asic_prefixes = []
+    for line in asic_route_lines:
+        json_obj = line[len(ROUTE_TABLE_NAME) + 1 : ]
+        asic_prefixes.append(json.loads(json_obj)['dest'])
+    if op == 'SET':
+        assert all(prefix in asic_prefixes for prefix in prefixes)
+    elif op == 'DEL':
+        assert all(prefix not in asic_prefixes for prefix in prefixes)
+    else:
+        pytest.fail('Operation {} not supported'.format(op))
+
+    # Retuen time used for set/del routes
+    return (end_time - start_time).total_seconds()
+
+def test_perf_add_remove_routes(duthost):
+    # Number of routes for test
+    num_routes = 10000
+
+    # Set up interface and interface for routes
+    prepare_dut(duthost)
+
+    # Add routes
+    time_set = exec_routes(duthost, num_routes, 'SET')
+    print("Time to set %d routes is %.2f seconds. " % (num_routes, time_set))
+
+    # Remove routes
+    time_del = exec_routes(duthost, num_routes, 'DEL')
+    print("Time to del %d routes is %.2f seconds. " % (num_routes, time_del))
+
+    # Cleanup DUT
+    cleanup_dut(duthost)
