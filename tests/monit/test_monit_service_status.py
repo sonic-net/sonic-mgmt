36c56cb3df293eab7a5b55dac48aab2a3579ae99
Yong Zhao <yozhao@microsoft.com>
2020-10-08 23:09:03 -0700

    [pytest] Change the folder name from monit_test to monit.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

---
+++ tests/monit/test_monit_service_status.py
@@ -0,0 +1,26 @@
+"""
+Test the running status of Monit service
+"""
+import logging
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+
+def test_monit_service_status(duthost):
+    """
+    @summary: Test the running status of Monit service by analyzing the command
+              output of "sudo systemctl status monit.service | grep Active".
+    """
+    monit_service_status_info = duthost.shell("sudo systemctl status monit.service | grep Active")
+
+    status_line = monit_service_status_info["stdout_lines"][0].strip()
+    if "active" in status_line:
+        logger.info("Monit service is running.")
+    else:
+        pytest.fail("Monit service is not running: '{}'".format(status_line))
