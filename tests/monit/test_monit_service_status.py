7b233be2c0cef2b6c0d1ea110e20c74887c71624
Yong Zhao <yozhao@microsoft.com>
2020-10-09 17:46:53 -0700

    [pytest] Use the return code of commnad `sudo monit status` to determinie whether the Monit is running or not.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- tests/monit/test_monit_service_status.py
+++ tests/monit/test_monit_service_status.py
@@ -17,10 +17,10 @@ def test_monit_service_status(duthost):
     @summary: Test the running status of Monit service by analyzing the command
               output of "sudo systemctl status monit.service | grep Active".
     """
-    monit_service_status_info = duthost.shell("sudo systemctl status monit.service | grep Active")
+    monit_service_status_info = duthost.shell("sudo monit status", module_ignore_erros=True)
 
-    status_line = monit_service_status_info["stdout_lines"][0].strip()
-    if "active" in status_line:
+    exit_code = monit_service_status_info["rc"]
+    if exit_code == 0:
         logger.info("Monit service is running.")
     else:
-        pytest.fail("Monit service is not running: '{}'".format(status_line))
+        pytest.fail("Monit service is not running.")
