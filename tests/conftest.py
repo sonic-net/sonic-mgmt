814f702b26ba8c5bf790506feb1dda705df7676b
Shi Su <67605788+shi-su@users.noreply.github.com>
2020-10-20 20:39:43 -0700

    Add performance test for set/delete routes (#2344)
    Add a test to quantify the performance of set/delete routes and make sure the operation could finish within the time limit.

--- tests/conftest.py
+++ tests/conftest.py
@@ -137,6 +137,7 @@ def pytest_addoption(parser):
                      help="Deep clean DUT before tests (remove old logs, cores, dumps)")
 
 
+
 @pytest.fixture(scope="session", autouse=True)
 def enhance_inventory(request):
     """
