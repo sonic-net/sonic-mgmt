127ec7ea59cc7ff65b39f09addb28c12286f34b5
Oleksandr Kozodoi <oleksandrx.kozodoi@intel.com>
2020-10-21 21:45:29 -0700

    Change port toggle tool based on port toggle test
    Signed-off-by: Oleksandr Kozodoi <oleksandrx.kozodoi@intel.com>

--- tests/common/port_toggle.py
+++ tests/common/port_toggle.py
@@ -1,18 +1,28 @@
+"""
+Tool used for shutdown/startup port on the DUT.
+"""
+
 import time
 import logging
 import pprint
 
+from tests.common.helpers.assertions import pytest_assert
+from tests.platform_tests.link_flap.link_flap_utils import watch_system_status
+
+
 logger = logging.getLogger(__name__)
 
 
-def port_toggle(duthost, ports=None, wait=60, wait_after_ports_up=60):
+def port_toggle(duthost, ports=None, wait=60, wait_after_ports_up=60, watch=False):
     """
-    Toggle ports on DUT
-    :param duthost: DUT host object
-    :param ports: specify list of ports, None if toggle all ports
-    :param wait: time to wait for interface to become up
-    :param wait_after_ports_up: time to wait after interfaces become up
-    :return:
+    Toggle ports on DUT.
+
+    Args:
+        duthost: DUT host object
+        ports: Specify list of ports, None if toggle all ports
+        wait: Time to wait for interface to become up
+        wait_after_ports_up: Time to wait after interfaces become up
+        watch: Logging system state
     """
 
     if ports is None:
@@ -20,14 +30,17 @@ def port_toggle(duthost, ports=None, wait=60, wait_after_ports_up=60):
         mg_facts = duthost.minigraph_facts(host=duthost.hostname)['ansible_facts']
         ports = mg_facts['minigraph_ports'].keys()
 
-    logger.info('toggling ports:\n{}'.format(pprint.pformat(ports)))
+    logger.info('toggling ports:\n%s', pprint.pformat(ports))
 
     for port in ports:
         duthost.command('config interface shutdown {}'.format(port))
+        if watch:
+            time.sleep(1)
+            watch_system_status(duthost)
 
-    # verify all interfaces are up
+    # verify all interfaces are down
     ports_down = duthost.interface_facts(up_ports=ports)['ansible_facts']['ansible_interface_link_down_ports']
-    assert len(ports_down) == len(ports)
+    pytest_assert(len(ports_down) == len(ports), "dut ports {} didn't go down as expected".format(list(set(ports).difference(set(ports_down)))))
 
     for port in ports:
         duthost.command('config interface startup {}'.format(port))
@@ -36,13 +49,13 @@ def port_toggle(duthost, ports=None, wait=60, wait_after_ports_up=60):
 
     start = time.time()
     ports_down = duthost.interface_facts(up_ports=ports)['ansible_facts']['ansible_interface_link_down_ports']
-    while time.time() - start < wait:
+    while ports_down and time.time() - start < wait:
         ports_down = duthost.interface_facts(up_ports=ports)['ansible_facts']['ansible_interface_link_down_ports']
-        logger.info('retry, down ports:\n{}'.format(pprint.pformat(ports_down)))
-        if len(ports_down) == 0:
+        logger.info('retry, down ports:\n%s', pprint.pformat(ports_down))
+        if not ports_down:
             break
 
-    assert len(ports_down) == 0
+    pytest_assert(not ports_down, "dut ports {} didn't go up as expected".format(ports_down))
 
-    logger.info('wait {} seconds for system to startup'.format(wait_after_ports_up))
+    logger.info('wait %d seconds for system to startup', wait_after_ports_up)
     time.sleep(wait_after_ports_up)
