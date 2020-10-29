9aa807e2db9c38c4173ecd9cc52d1d358f8d7466
Xin Wang <xiwang5@microsoft.com>
2020-10-29 23:24:01 +0800

    Stabilize the NTP test (#2426)
    Script test_ntp.py is failing and caused sonic-mgmt PR testing failed.
    This change is to stabilizie the ntp test to ensure that the NTP server
    on PTF is always restarted to take the configuration change in setup.
    
    This change will force the ntpd on PTF to always use local time. Then 
    the ntp server on PTF docker will always be "synchronized". This test 
    will be more stable. Since we are not updating time of the PTF container, 
    time of PTF will be same as host. Eventually, time of DUT will be kind 
    of synchronized with host.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- tests/ntp/test_ntp.py
+++ tests/ntp/test_ntp.py
@@ -11,15 +11,18 @@ pytestmark = [
     pytest.mark.device_type('vs')
 ]
 
+
 @pytest.fixture(scope="module")
 def setup_ntp(ptfhost, duthost, creds):
     """setup ntp client and server"""
-    if creds.get('proxy_env'):
-        # If testbed is behaind proxy then force ntpd inside ptf use local time
-        ptfhost.lineinfile(path="/etc/ntp.conf", line="server 127.127.1.0 prefer")
 
-    # enable ntp server
-    ntp_en_res = ptfhost.service(name="ntp", state="started")
+    ptfhost.lineinfile(path="/etc/ntp.conf", line="server 127.127.1.0 prefer")
+
+    # restart ntp server
+    ntp_en_res = ptfhost.service(name="ntp", state="restarted")
+
+    pytest_assert(wait_until(120, 5, check_ntp_status, ptfhost), \
+        "NTP server was not started in PTF container {}; NTP service start result {}".format(ptfhost.hostname, ntp_en_res))
 
     # setup ntp on dut to sync with ntp server
     config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
@@ -27,19 +30,17 @@ def setup_ntp(ptfhost, duthost, creds):
     for ntp_server in ntp_servers:
         duthost.command("config ntp del %s" % ntp_server)
 
-    ptfip = ptfhost.host.options['inventory_manager'].get_host(ptfhost.hostname).vars['ansible_host']
-    duthost.command("config ntp add %s" % ptfip)
-    pytest_assert(wait_until(120, 5, check_ntp_status, ptfhost), \
-        "NTP server was not started in PTF container {}; NTP service start result {}".format(ptfhost.hostname, ntp_en_res))
+    duthost.command("config ntp add %s" % ptfhost.mgmt_ip)
 
     yield
 
     # stop ntp server
     ptfhost.service(name="ntp", state="stopped")
     # reset ntp client configuration
-    duthost.command("config ntp del %s" % ptfip)
+    duthost.command("config ntp del %s" % ptfhost.mgmt_ip)
     for ntp_server in ntp_servers:
         duthost.command("config ntp add %s" % ntp_server)
+
 
 def check_ntp_status(host):
     res = host.command("ntpstat", module_ignore_errors=True)
@@ -47,8 +48,9 @@ def check_ntp_status(host):
        return False
     return True
 
+
 def test_ntp(duthost, setup_ntp):
-    """ verify the LLDP message on DUT """
+    """ Verify that DUT is synchronized with configured NTP server """
 
     duthost.service(name='ntp', state='stopped')
     duthost.command("ntpd -gq")
