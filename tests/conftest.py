14d56a9d66739619a69cc4fb9ee207e753b3bdfc
sanmalho-git <66454160+sanmalho-git@users.noreply.github.com>
2020-10-30 10:32:31 -0400

    Implementation of multi-DUT and multi-ASIC as per PR 2347 (#2417)
    PR Azure/SONiC#644 introduced the HLD to support multi ASIC. In the future, multi DUT or Chassis will be supported by SONiC as well. The test infrastructure and some of the customized ansible modules need to be updated to support testing of the upcoming new architectures. This PR is implementation of PR 2347 which tried to propose how to improve the current test infrastructure to support multi-DUT and multi-ASIC systems. The target is to ensure that the existing test scripts are not broken and we can update the tests in incremental way.
    
    This change is the implementation of PR 2347 - Add proposal for multi-DUT and multi-ASIC testing support
    - Added the classes described in the PR:
      - SonicAsic - represents an asic, and implements the asic/namespace related operations to hide the complexity of handling the asic/namespace specific details.
          - For now, have added bgp_facts as an example to add 'instance_id' to the bgp_facts module call on a SonicHost.
      - MutliAsicSonicHost - a host with one or more SonicAsics.
      - DutHosts - represents all the DUT's in a testbed.
          - has 'nodes' list to represent each DUT in the testbed.
    
    - Update duthosts fixture to return an instance of DutHosts instead of a list of SonicHosts
    - Modify duthost fixture to return a MultiAsicSonicHost from duthosts.nodes

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,11 @@ from ansible.inventory.manager import InventoryManager
 from collections import defaultdict
 from datetime import datetime
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts
-from tests.common.devices import SonicHost, Localhost
+from tests.common.devices import Localhost
 from tests.common.devices import PTFHost, EosHost, FanoutHost
 from tests.common.helpers.constants import ASIC_PARAM_TYPE_ALL, ASIC_PARAM_TYPE_FRONTEND, DEFAULT_ASIC_ID
 from tests.common.helpers.dut_ports import encode_dut_port_name
+from tests.common.devices import DutHosts
 
 logger = logging.getLogger(__name__)
 
@@ -214,7 +215,7 @@ def fixture_duthosts(ansible_adhoc, tbinfo):
         mandatory argument for the class constructors.
     @param tbinfo: fixture provides information about testbed.
     """
-    return [SonicHost(ansible_adhoc, dut) for dut in tbinfo["duts"]]
+    return DutHosts(ansible_adhoc, tbinfo)
 
 
 @pytest.fixture(scope="session")
