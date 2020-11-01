de6123ae55466c7ff8b2bdf735302db3cc6861ed
lguohan <lguohan@gmail.com>
2020-11-01 00:07:02 -0700

    [kvmtest]: add cpu/mem to kvmtest suite (#2441)
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- tests/platform_tests/test_cpu_memory_usage.py
+++ tests/platform_tests/test_cpu_memory_usage.py
@@ -14,12 +14,12 @@ pytestmark = [
 def setup_thresholds(duthost):
     cpu_threshold = 50
     memory_threshold = 60
-    if duthost.facts['platform'] in ('x86_64-arista_7050_qx32',):
+    if duthost.facts['platform'] in ('x86_64-arista_7050_qx32', 'x86_64-kvm_x86_64-r0'):
         memory_threshold = 80
     return memory_threshold, cpu_threshold
 
 
-def test_cpu_memory_usage(duthost, setup_threshold):
+def test_cpu_memory_usage(duthost, setup_thresholds):
     """Check DUT memory usage and process cpu usage are within threshold."""
     MonitResult = namedtuple('MonitResult', ['processes', 'memory'])
     monit_results = duthost.monit_process(iterations=12)['monit_results']
