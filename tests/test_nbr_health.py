bed9897101fe4dc413216e5067f9278dec4123cc
Ying Xie <yxieca@users.noreply.github.com>
2020-11-05 13:46:56 -0800

    [multi DUT] making test_nbr_health multi-DUT ready (#2469)
    Summary:
    making test_nbr_health multi-DUT ready.
    
    Type of change
     Bug fix
     Testbed and Framework(new/improvement)
     Test case(new/improvement)
    Approach
    What is the motivation for this PR?
    Making nbr health check ready for multi-DUT tests.
    
    How did you do it?
    Enumerate the dut hostnames and test them individually.
    
    Signed-off-by: Ying Xie ying.xie@microsoft.com
    
    How did you verify/test it?
    Run nbr health test case on multi-DUT testbed and single DUT testbed.

--- tests/test_nbr_health.py
+++ tests/test_nbr_health.py
@@ -1,6 +1,9 @@
 import json
 import pytest
 import logging
+
+from common.helpers.assertions import pytest_assert
+
 logger = logging.getLogger(__name__)
 
 pytestmark = [
@@ -49,10 +52,11 @@ def check_bgp_facts(hostname, host):
     if not res.has_key('stdout_lines') or u'BGP summary' not in res['stdout_lines'][0][0]:
         return "neighbor {} bgp not configured correctly".format(hostname)
 
-def test_neighbors_health(duthost, localhost, nbrhosts, eos):
+def test_neighbors_health(duthosts, localhost, nbrhosts, eos, dut_hostname):
     """Check each neighbor device health"""
 
     fails = []
+    duthost = duthosts[dut_hostname]
     config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
     nei_meta = config_facts.get('DEVICE_NEIGHBOR_METADATA', {})
     for k, v in nei_meta.items():
@@ -70,5 +74,5 @@ def test_neighbors_health(duthost, localhost, nbrhosts, eos):
             fails.append(failmsg)
 
     # TODO: check link, bgp, etc. on
-    if len(fails) > 0:
-        pytest.fail("\n".join(fails))
+
+    pytest_assert(len(fails) == 0, "\n".join(fails))
