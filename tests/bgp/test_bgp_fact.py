495dcbc5edc3ed38723f46531d316e8cdc92c67b
Xin Wang <xiwang5@microsoft.com>
2020-11-05 16:43:14 +0800

    Add support of parametrize by DUT hostname (#2453)
    * Add support of parametrize by DUT hostname
    
    Currently test cases can add fixture `dut_index` to support parametrize by
    DUT index for multi-DUT testing. It would be more convenient if the test
    cases can be parametrized by DUT hostname. This change added support
    of parametrize by DUT hostname. When fixture name `dut_hostname` is
    added to argument list of test case function, the test case will be
    parametrized by hostnames of DUTs. The test_bgp_fact.py script have been
    updated to use this new `dut_hostname` fixture as well.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- tests/bgp/test_bgp_fact.py
+++ tests/bgp/test_bgp_fact.py
@@ -5,10 +5,10 @@ pytestmark = [
     pytest.mark.device_type('vs')
 ]
 
-def test_bgp_facts(duthosts, dut_index, asic_index):
+def test_bgp_facts(duthosts, dut_hostname, asic_index):
     """compare the bgp facts between observed states and target state"""
 
-    duthost = duthosts[dut_index]
+    duthost = duthosts[dut_hostname]
     bgp_facts =duthost.bgp_facts(instance_id=asic_index)['ansible_facts']
     namespace = duthost.get_namespace_from_asic_id(asic_index)
     config_facts = duthost.config_facts(host=duthost.hostname, source="running",namespace=namespace)['ansible_facts']
@@ -16,12 +16,11 @@ def test_bgp_facts(duthosts, dut_index, asic_index):
     for k, v in bgp_facts['bgp_neighbors'].items():
         # Verify bgp sessions are established
         assert v['state'] == 'established'
-        # Verify locat ASNs in bgp sessions
+        # Verify local ASNs in bgp sessions
         assert v['local AS'] == int(config_facts['DEVICE_METADATA']['localhost']['bgp_asn'].decode("utf-8"))
 
     for k, v in config_facts['BGP_NEIGHBOR'].items():
-        # Compare the bgp neighbors name with config db bgp neigbhors name
+        # Compare the bgp neighbors name with config db bgp neighbors name
         assert v['name'] == bgp_facts['bgp_neighbors'][k]['description']
         # Compare the bgp neighbors ASN with config db
         assert int(v['asn'].decode("utf-8")) == bgp_facts['bgp_neighbors'][k]['remote AS']
-
