82ff35a612cce6edfa9141ab220f2c02f55e0c30
Ying Xie <yxieca@users.noreply.github.com>
2020-11-06 15:40:25 -0800

    [cont_link_flap] address issue in fixture bring_up_fanout_interfaces (#2481)
    Description of PR
    Summary:
    
    Type of change
     Bug fix
     Testbed and Framework(new/improvement)
     Test case(new/improvement)
    Approach
    What is the motivation for this PR?
    test_cont_link_flap was executed N times, where N is number of ports of the testbed.
    
    How did you do it?
    Fixture mistaenly uses all_ports enumerator, which causes test_cont_link_flap itself being parameterized with all available ports on DUT. So the test was executed N times instead of once. Where N is number of ports.
    
    Also the warning in candidate builder is intended to give warning when port enumeration failed. Add another explicit value to get all ports without triggering the warning.
    
    Signed-off-by: Ying Xie ying.xie@microsoft.com
    
    How did you verify/test it?
    Run link flap tests.

--- tests/platform_tests/link_flap/conftest.py
+++ tests/platform_tests/link_flap/conftest.py
@@ -9,7 +9,6 @@ import time
 import pytest
 
 from tests.platform_tests.link_flap.link_flap_utils import build_test_candidates
-from tests.common.helpers.dut_ports import decode_dut_port_name
 
 def pytest_addoption(parser):
     """
@@ -26,23 +25,20 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture()
-def bring_up_fanout_interfaces(request, all_ports, duthosts, fanouthosts):
+def bring_up_fanout_interfaces(request, duthosts, fanouthosts):
     """
     Bring up outer interfaces on the DUT.
 
     Args:
         request: pytest request object
-        duthost: Fixture for interacting with the DUT.
+        duthosts: Fixture for interacting with the DUT list.
         fanouthosts: Fixture for interacting with the fanouts.
     """
     yield
     if request.node.rep_call.failed:
-        dutname, portname = decode_dut_port_name(all_ports)
-
         for dut in duthosts:
-            if dutname == 'unknown' or dutname == dut.hostname:
-                candidates = build_test_candidates(dut, fanouthosts, portname)
-                for _, fanout, fanout_port in candidates:
-                    fanout.no_shutdown(fanout_port)
+            candidates = build_test_candidates(dut, fanouthosts, "all_ports")
+            for _, fanout, fanout_port in candidates:
+                fanout.no_shutdown(fanout_port)
 
         time.sleep(60)
