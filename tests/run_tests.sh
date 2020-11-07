750ba0e98cac7e07f151400c6135f2362222b4c3
isabelmsft <isabel.li@microsoft.com>
2020-11-07 08:37:26 +0000

    Update description for master identifier in run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,7 +6,7 @@ function show_help_and_exit()
     echo "    options with (*) must be provided"
     echo "    -h -?          : get this help"
     echo "    -a <True|False>: specify if autu-recover is allowed (default: True)"
-    echo "    -b <master_id> : kubernetes master identifier in the form {servernumber}_{mastersetnumber}"
+    echo "    -b <master_id> : specify name of k8s master group used in k8s inventory, format: k8s_vms{msetnumber}_{servernumber}"
     echo "    -c <testcases> : specify test cases to execute (default: none, executed all matched)"
     echo "    -d <dut name>  : specify DUT name (default: DUT name associated with testbed in testbed file)"
     echo "    -e <parameters>: specify extra parameter(s) (default: none)"
