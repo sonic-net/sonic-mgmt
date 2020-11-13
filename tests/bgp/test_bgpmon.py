acfea3e3e6db0f15d3a76fb27d5e48779ba50942
bingwang <wang.bing@microsoft.com>
2020-11-13 12:46:03 +0000

    Update after review.
    1.Move bgpmon.j2 to templates
    2.Generate random IP address for test
    3.Capture on all interfaces in default routes
    
    Signed-off-by: bingwang <wang.bing@microsoft.com>

--- tests/bgp/test_bgpmon.py
+++ tests/bgp/test_bgpmon.py
@@ -5,33 +5,70 @@ import ptf.testutils as testutils
 from jinja2 import Template
 import ptf.packet as scapy
 from ptf.mask import Mask
+import json
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses      # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import remove_ip_addresses       # lgtm[py/unused-import]
 from tests.common.helpers.generators import generate_ips as generate_ips
+from tests.common.helpers.assertions import pytest_assert
 
 pytestmark = [
     pytest.mark.topology('any'),
 ]
 
-BGPMON_TEMPLATE_FILE = 'bgp/bgpmon.j2'
+BGPMON_TEMPLATE_FILE = 'bgp/templates/bgpmon.j2'
 BGPMON_CONFIG_FILE = '/tmp/bgpmon.json'
 BGP_PORT = 179
-BGP_CONNECT_TIMEOUT = 120
+BGP_CONNECT_TIMEOUT = 121
+ZERO_ADDR = r'0.0.0.0/0'
 
 logger = logging.getLogger(__name__)
 
+def route_through_default_routes(host, ip_addr):
+    output = host.shell("show ip route {} json".format(ip_addr))['stdout']
+    routes_info = json.loads(output)
+    ret = True
+    for prefix in routes_info.keys():
+        if prefix != ZERO_ADDR:
+            ret = False
+            break
+    return ret
+
+def generate_ip_through_default_route(host):
+    # Generate an IP address routed through default routes
+    ip_addrs = generate_ips(200, "200.0.0.1/24", [])
+    for ip_addr in ip_addrs:
+        if route_through_default_routes(host, ip_addr):
+            return ip_addr
+    return None
+
+def get_default_route_ports(host):
+    mg_facts = host.minigraph_facts(host=host.hostname)['ansible_facts']
+    route_info = json.loads(host.shell("show ip route {} json".format(ZERO_ADDR))['stdout'])
+    ports = []
+    for route in route_info[ZERO_ADDR]:
+        if route['protocol'] != 'bgp':
+            continue
+        for itfs in route['nexthops']:
+            ports.append(itfs['interfaceName'])
+    port_indices = []
+    for port in ports:
+        if 'PortChannel' in port:
+            for member in mg_facts['minigraph_portchannels'][port]['members']:
+                port_indices.append(mg_facts['minigraph_port_indices'][member])
+        else:
+            port_indices.append(mg_facts['minigraph_port_indices'][port])
+    return port_indices
+
 @pytest.fixture
 def common_setup_teardown(duthost, ptfhost):
-    mg_facts = duthost.minigraph_facts(host=duthost.hostname)['ansible_facts']
-    peer_addr = generate_ips(1, "%s/%s" % (mg_facts['minigraph_vlan_interfaces'][0]['addr'],
-                                           mg_facts['minigraph_vlan_interfaces'][0]['prefixlen']),
-                             [IPAddress(mg_facts['minigraph_vlan_interfaces'][0]['addr'])])[0]
+    peer_addr = generate_ip_through_default_route(duthost)
+    pytest_assert(peer_addr, "Failed to generate ip address for test")
     peer_addr = str(IPNetwork(peer_addr).ip)
-    peer_port = mg_facts['minigraph_port_indices'][mg_facts['minigraph_vlans'][mg_facts['minigraph_vlan_interfaces'][0]['attachto']]['members'][0]]
+    peer_ports = get_default_route_ports(duthost)
+    mg_facts = duthost.minigraph_facts(host=duthost.hostname)['ansible_facts']
     local_addr = mg_facts['minigraph_lo_interfaces'][0]['addr']
     # Assign peer addr to an interface on ptf
-    ptfhost.shell("ifconfig eth{} {}".format(peer_port, peer_addr))
-    logger.info("Generated peer address {} and assigned to eth{} on ptf".format(peer_addr, peer_port))
+    logger.info("Generated peer address {}".format(peer_addr))
     bgpmon_args = {
         'peer_addr': peer_addr,
         'asn': mg_facts['minigraph_bgp_asn'],
@@ -41,12 +78,13 @@ def common_setup_teardown(duthost, ptfhost):
     bgpmon_template = Template(open(BGPMON_TEMPLATE_FILE).read())
     duthost.copy(content=bgpmon_template.render(**bgpmon_args),
                  dest=BGPMON_CONFIG_FILE)
-    yield local_addr, peer_addr, peer_port
+    yield local_addr, peer_addr, peer_ports
     # Cleanup bgp monitor
     duthost.shell("redis-cli -n 4 -c DEL 'BGP_MONITORS|{}'".format(peer_addr))
     duthost.file(path=BGPMON_CONFIG_FILE, state='absent')
 
-def build_syn_pkt(local_addr, peer_addr, peer_port):
+
+def build_syn_pkt(local_addr, peer_addr):
     pkt = testutils.simple_tcp_packet(
         pktlen=54,
         ip_src=local_addr,
@@ -85,11 +123,11 @@ def test_bgpmon(duthost, common_setup_teardown, ptfadapter):
     """
     Add a bgp monitor on ptf and verify that DUT is attempting to establish connection to it
     """
-    local_addr, peer_addr, peer_port = common_setup_teardown
-    exp_packet = build_syn_pkt(local_addr, peer_addr, peer_port)
+    local_addr, peer_addr, peer_ports = common_setup_teardown
+    exp_packet = build_syn_pkt(local_addr, peer_addr)
     # Load bgp monitor config
-    logger.info("Configured bgpmon and verifying packet")
+    logger.info("Configured bgpmon and verifying packet on {}".format(peer_ports))
     duthost.command("sonic-cfggen -j {} -w".format(BGPMON_CONFIG_FILE))
     # Verify syn packet on ptf
-    testutils.verify_packet(ptfadapter, exp_packet, peer_port, BGP_CONNECT_TIMEOUT)
+    testutils.verify_packet_any_port(test=ptfadapter, pkt=exp_packet, ports=peer_ports, timeout=BGP_CONNECT_TIMEOUT)
 
