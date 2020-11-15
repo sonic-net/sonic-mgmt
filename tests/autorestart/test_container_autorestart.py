2cae73dfbb093bc2d64bbec98758a1aceba33d16
yozhao101 <56170650+yozhao101@users.noreply.github.com>
2020-11-14 21:41:32 -0800

    [pytest] Skip ‘dsserve’ process in syncd when testing autorestart (#2530)
    Description of PR
    Summary:
    In the critical_processes file of syncd container, dsserve was the first critical process. The pytest script will read this file to get the first critical processes and check its PID, running status by retrieving the result of command docker exec syncd supervisorctl status. Since dsserve process was not managed by supervisord, the pytest script can not verify the PID and running status of dsserve process, then fail.
    
    Fixes # (issue)
    
    Type of change
    [x ] Bug fix
     Testbed and Framework(new/improvement)
     Test case(new/improvement)
    Approach
    What is the motivation for this PR?
    In the critical_processes file of syncd container, dsserve was the first critical process. The pytest script will read this file to get the first critical processes and check its PID, running status by retrieving the result of command docker exec syncd supervisorctl status. Since dsserve process was not managed by supervisord, the pytest script can not verify the PID and running status of dsserve process, then fail.
    
    How did you do it?
    The script will skip the dsserve process in syncd container during the testing the autorestart feature.
    
    How did you verify/test it?
    I tested this change on the virtual testbed and also against the lab device str-dx010-acs-1.

--- tests/autorestart/test_container_autorestart.py
+++ tests/autorestart/test_container_autorestart.py
@@ -346,6 +346,11 @@ def test_containers_autorestart(duthosts, rand_one_dut_hostname, tbinfo):
             pytest.fail("Failed to get critical group and process lists of container '{}'".format(container_name))
 
         for critical_process in critical_process_list:
+            # Skip 'dsserve' process since it was not managed by supervisord
+            # TODO: Should remove the following two lines once the issue was solved in the image.
+            if container_name == "syncd" and critical_process == "dsserve":
+                continue
+
             program_status, program_pid = get_program_info(duthost, container_name, critical_process)
             verify_autorestart_with_critical_process(duthost, container_name, critical_process,
                                                      program_status, program_pid)
