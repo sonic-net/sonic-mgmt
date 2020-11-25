7d0027b4224fb9a20644caf32fe973a15cefb56e
Wei Bai <baiwei0427@gmail.com>
2020-11-25 05:39:35 +0000

    Add PFC pause lossy test case

---
+++ tests/pfc/test_pfc_pause_lossy.py
@@ -0,0 +1,44 @@
+import logging
+import time
+import pytest
+
+from files.experiment import run_pfc_test
+from files.qos_fixtures import prio_dscp_map, all_prio_list
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
+    fanout_graph_facts
+from tests.common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_port,\
+    ixia_api_serv_user, ixia_api_serv_passwd, ixia_api
+from tests.common.ixia.common_helpers import ansible_stdout_to_str
+
+@pytest.mark.topology("tgen")
+@pytest.mark.disable_loganalyzer
+
+def test_pfc_pause_single_lossy_prio(ixia_api,
+                                     conn_graph_facts,
+                                     fanout_graph_facts,
+                                     duthosts, 
+                                     rand_one_dut_hostname,
+                                     enum_dut_portname_oper_up, 
+                                     enum_dut_lossy_prio, 
+                                     all_prio_list,
+                                     prio_dscp_map):
+
+    pause_prio_list = [enum_dut_lossy_prio]
+    test_prio_list = [enum_dut_lossy_prio]
+    bg_prio_list = [p for p in all_prio_list]
+    bg_prio_list.remove(enum_dut_lossy_prio)
+    
+    duthost = duthosts[rand_one_dut_hostname]
+
+    run_pfc_test(api=ixia_api,
+                 conn_data=conn_graph_facts,
+                 fanout_data=fanout_graph_facts,
+                 duthost=duthost,
+                 port=ansible_stdout_to_str(enum_dut_portname_oper_up),
+                 global_pause=False,
+                 pause_prio_list=pause_prio_list,
+                 test_prio_list=test_prio_list,
+                 bg_prio_list=bg_prio_list,
+                 prio_dscp_map=prio_dscp_map,
+                 test_traffic_pause=False)
+    
