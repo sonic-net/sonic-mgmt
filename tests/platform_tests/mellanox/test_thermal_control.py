65a6dca6e568572620197437a5c11d9787531334
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2020-11-25 15:16:53 +0800

    [Mellanox] Remove unsupported air flow direction from test_dynamic_minimum_table (#2571)
    * [Mellanox] Remove unsupported fan direction from test_dynamic_minimum_table
    
    * Enlarge wait time in test_set_psu_fan_speed to make sure kernel algorithm does not affect the test
    
    * Fix typo
    
    * Fix LGTM warning
    
    * Fix review comment

--- tests/platform_tests/mellanox/test_thermal_control.py
+++ tests/platform_tests/mellanox/test_thermal_control.py
@@ -4,17 +4,18 @@ import pytest
 import random
 import time
 from tests.common.mellanox_data import get_platform_data
-from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer
 from tests.common.utilities import wait_until
 from tests.platform_tests.thermal_control_test_helper import *
 from mellanox_thermal_control_test_helper import MockerHelper, AbnormalFanMocker
+from tabulate import tabulate
+import re
 
 pytestmark = [
     pytest.mark.asic('mellanox'),
     pytest.mark.topology('any')
 ]
 
-THERMAL_CONTROL_TEST_WAIT_TIME = 65
+THERMAL_CONTROL_TEST_WAIT_TIME = 75
 THERMAL_CONTROL_TEST_CHECK_INTERVAL = 5
 
 COOLING_CUR_STATE_PATH = '/run/hw-management/thermal/cooling_cur_state'
@@ -24,42 +25,35 @@ PSU_SPEED_PATH = '/run/hw-management/thermal/psu{}_fan1_speed_get'
 PSU_MAX_SPEED_PATH = '/run/hw-management/config/psu_fan_max'
 PSU_SPEED_TOLERANCE = 0.25
 
-LOG_EXPECT_CHANGE_MIN_COOLING_LEVEL_RE = '.*Changed minimum cooling level to {}.*'
+MAX_COOLING_LEVEL = 10
 
 
 @pytest.mark.disable_loganalyzer
 def test_dynamic_minimum_table(duthosts, rand_one_dut_hostname, mocker_factory):
     duthost = duthosts[rand_one_dut_hostname]
-    air_flow_dirs = ['p2c', 'c2p', 'unk']
     max_temperature = 45000 # 45 C
     cooling_cur_state = get_cooling_cur_state(duthost)
     if cooling_cur_state >= COOLING_CUR_STATE_THRESHOLD:
         pytest.skip('The cooling level {} is higher than threshold {}.'.format(cooling_cur_state, COOLING_CUR_STATE_THRESHOLD))
 
     mocker = mocker_factory(duthost, 'MinTableMocker')
-    loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix='thermal_control')
-    loganalyzer.load_common_config()
 
-    for index in range(len(air_flow_dirs)):
-        air_flow_index = random.randint(0, len(air_flow_dirs) - 1)
-        air_flow_dir = air_flow_dirs[air_flow_index]
-        air_flow_dirs.remove(air_flow_dir)
-        temperature = random.randint(0, max_temperature)
-        trust_state = True if random.randint(0, 1) else False
-        logging.info('Testing with air_flow_dir={}, temperature={}, trust_state={}'.format(air_flow_dir, temperature, trust_state))
-        expect_minimum_cooling_level = mocker.get_expect_cooling_level(air_flow_dir, temperature, trust_state)
-        loganalyzer.expect_regex = [LOG_EXPECT_CHANGE_MIN_COOLING_LEVEL_RE.format(expect_minimum_cooling_level)]
-        with loganalyzer:
-            mocker.mock_min_table(air_flow_dir, temperature, trust_state)
-            time.sleep(THERMAL_CONTROL_TEST_WAIT_TIME)
+    temperature = random.randint(0, max_temperature)
+    trust_state = True if random.randint(0, 1) else False
+    logging.info('Testing with temperature={}, trust_state={}'.format(temperature, trust_state))
+    expect_minimum_cooling_level = mocker.get_expect_cooling_level(temperature, trust_state)
+    mocker.mock_min_table(temperature, trust_state)
+    time.sleep(THERMAL_CONTROL_TEST_WAIT_TIME)
+    actual_cooling_level = get_cooling_cur_state(duthost)
+    assert actual_cooling_level >= expect_minimum_cooling_level, 'Cooling level {} is less than minimum allowed {}'.format(actual_cooling_level, expect_minimum_cooling_level)
 
-        temperature = random.randint(0, max_temperature)
-        logging.info('Testing with air_flow_dir={}, temperature={}, trust_state={}'.format(air_flow_dir, temperature, not trust_state))
-        expect_minimum_cooling_level = mocker.get_expect_cooling_level(air_flow_dir, temperature, not trust_state)
-        loganalyzer.expect_regex = [LOG_EXPECT_CHANGE_MIN_COOLING_LEVEL_RE.format(expect_minimum_cooling_level)]
-        with loganalyzer:
-            mocker.mock_min_table(air_flow_dir, temperature, not trust_state)
-            time.sleep(THERMAL_CONTROL_TEST_WAIT_TIME)
+    temperature = random.randint(0, max_temperature)
+    logging.info('Testing with temperature={}, trust_state={}'.format(temperature, not trust_state))
+    expect_minimum_cooling_level = mocker.get_expect_cooling_level(temperature, not trust_state)
+    mocker.mock_min_table(temperature, not trust_state)
+    time.sleep(THERMAL_CONTROL_TEST_WAIT_TIME)
+    actual_cooling_level = get_cooling_cur_state(duthost)
+    assert actual_cooling_level >= expect_minimum_cooling_level, 'Cooling level {} is less than minimum allowed {}'.format(actual_cooling_level, expect_minimum_cooling_level)
 
 
 @pytest.mark.disable_loganalyzer
@@ -75,7 +69,7 @@ def test_set_psu_fan_speed(duthosts, rand_one_dut_hostname, mocker_factory):
     single_fan_mocker = mocker_factory(duthost, 'SingleFanMocker')
     logging.info('Mock FAN absence...')
     single_fan_mocker.mock_absence()
-    assert wait_until(THERMAL_CONTROL_TEST_WAIT_TIME, THERMAL_CONTROL_TEST_CHECK_INTERVAL, check_cooling_cur_state, duthost, 10, operator.eq), \
+    assert wait_until(THERMAL_CONTROL_TEST_WAIT_TIME, THERMAL_CONTROL_TEST_CHECK_INTERVAL, check_cooling_cur_state, duthost, MAX_COOLING_LEVEL, operator.eq), \
         'Current cooling state is {}'.format(get_cooling_cur_state(duthost))
 
     logging.info('Wait {} seconds for the policy to take effect...'.format(THERMAL_CONTROL_TEST_WAIT_TIME))
@@ -85,15 +79,35 @@ def test_set_psu_fan_speed(duthosts, rand_one_dut_hostname, mocker_factory):
     for index in range(psu_num):
         speed = get_psu_speed(duthost, index)
         logging.info('Speed for PSU {} fan is {}'.format(index, speed))
-        _check_psu_fan_speed_in_range(speed, psu_max_speed, 10)
+        _check_psu_fan_speed_in_range(speed, psu_max_speed, MAX_COOLING_LEVEL)
 
     logging.info('Mock FAN presence...')
     single_fan_mocker.mock_presence()
-    assert wait_until(THERMAL_CONTROL_TEST_WAIT_TIME, THERMAL_CONTROL_TEST_CHECK_INTERVAL, check_cooling_cur_state, duthost, 10, operator.ne), \
-        'Current cooling state is {}'.format(get_cooling_cur_state(duthost))
-    logging.info('Wait {} seconds for the policy to take effect...'.format(THERMAL_CONTROL_TEST_CHECK_INTERVAL))
-    time.sleep(THERMAL_CONTROL_TEST_CHECK_INTERVAL)
+    wait_until(THERMAL_CONTROL_TEST_WAIT_TIME, THERMAL_CONTROL_TEST_CHECK_INTERVAL, check_cooling_cur_state, duthost, MAX_COOLING_LEVEL, operator.ne)
+    logging.info('Wait {} seconds for the policy to take effect...'.format(THERMAL_CONTROL_TEST_WAIT_TIME * 2))
+    # We have to wait THERMAL_CONTROL_TEST_WAIT_TIME * 2 seconds long here because:
+    #     Usually we only need wait THERMAL_CONTROL_TEST_WAIT_TIME seconds here to make sure thermal
+    #     control daemon change the cooling level to proper value, However,
+    #     there is chance that kernel might change cooling state back to MAX_COOLING_LEVEL after
+    #     user space thermal control adjust it to dynamic minimum value. So we have to wait longer for the
+    #     user space thermal control to set fan speed to dynamic minimum value again. It
+    #     means that we might need wait up to 2 thermal loops here.
+    time.sleep(THERMAL_CONTROL_TEST_WAIT_TIME * 2)
     cooling_cur_state = get_cooling_cur_state(duthost)
+    if cooling_cur_state == MAX_COOLING_LEVEL:
+        cmd_output = str(duthost.command('show platform temperature')['stdout_lines'])
+        cmd_output = cmd_output.replace("u'", "").replace(',', " ")
+        cmd_output = re.split(r'  +',cmd_output)
+        cmd_output.pop(0)
+        j = 0
+        table = []
+        while j != len(cmd_output):
+            entry = []
+            for i in range(8):
+                entry.append(cmd_output[j + i])
+            table.append(entry)
+            j += 8
+        pytest.skip('Cooling level is still 10, ignore the rest test.\nIt might happen because the asic temperature is still high.\nCurrent system temperature:\n{}'.format(tabulate(table)))
     logging.info('Cooling level changed to {}'.format(cooling_cur_state))
     if cooling_cur_state < 6: # PSU fan speed will never be less than 60%
         cooling_cur_state = 6
@@ -114,8 +128,8 @@ def _check_psu_fan_speed_in_range(actual_speed, max_speed, cooling_level):
 def get_psu_speed(dut, index):
     index = index + 1
     psu_speed_path = PSU_SPEED_PATH.format(index)
-    file_exists = dut.stat(path=psu_speed_path)
-    if not file_exists:
+    file_stat = dut.stat(path=psu_speed_path)
+    if not file_stat["stat"]["exists"]:
         return None
 
     cmd_output = dut.command('cat {}'.format(psu_speed_path))
