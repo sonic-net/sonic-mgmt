fc29770b62d02edf2ad0046ea2f4b7635d5c6fb4
Danny Allen <daall@microsoft.com>
2020-12-01 09:41:48 -0800

    [copp] Update COPP tests to handle coppmgr config format (#2606)
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- tests/copp/scripts/update_copp_config.py
+++ tests/copp/scripts/update_copp_config.py
@@ -1,36 +1,80 @@
 #!/usr/bin/python
-"""
-    Script to modify a COPP configuration to follow a specified rate limit.
+"""Script to modify a COPP configuration to follow a specified rate limit.
 
-    This is used by the COPP tests to reduce the rate limit below that of the
-    PTF host's sending rate.
+This is used by the COPP tests to reduce the rate limit below that of the
+PTF host's sending rate.
 
-    Example::
+Example:
+    python update_copp_config.py <rate limit in pps> <input file> <output file> <config_format>
+    python update_copp_config.py 600 /tmp/copp_config.json /tmp/new_copp_config.json app_db
 
-        $ python update_copp_config.py <rate limit in pps> <input file> <output file>
-        $ python update_copp_config.py 600 /tmp/copp_config.json /tmp/new_copp_config.json
+Note:
+    Historically, there was a 00-copp.config.json file in the SWSS docker that specified
+    the parameters for each trap group like so:
+
+    [
+        "COPP_TABLE:default": {
+            "cir": "600",
+            "cbs": "600",
+            ...
+        },
+        "COPP_TABLE:trap.group.bgp.lacp": {
+            ...
+        },
+        ...
+    ]
+
+    This is the "app_db" format used for 202006 images and earlier.
+
+    In newer SONiC versions, there is a copp_cfg.json file on the host that specifies the parameters
+    for each trap group like so:
+
+    {
+        "COPP_GROUP": {
+            "default": {
+                "cir":"600",
+                "cbs":"600",
+                ...
+            },
+            "queue4_group1": {
+                ...
+            },
+            ...
+        },
+        ...
+    }
+
+    This is the "config_db" format used for 202012 images and later (including the master branch).
 """
 import json
 import sys
 
-def generate_limited_pps_config(pps_limit, input_config_file, output_config_file):
+
+def generate_limited_pps_config(pps_limit, input_config_file, output_config_file, config_format="app_db"):
+    """Modifies a COPP config to use the specified rate limit.
+
+    Notes:
+        This only affects COPP policies that enforce a rate limit. Other
+        policies are left alone.
+
+    Args:
+        pps_limit (int): The rate limit to enforce expressed in PPS (packets-per-second)
+        input_config_file (str): The name of the file containing the initial config
+        output_config_file (str): The name of the file to output the modified config
+        config_format (str): The format of the input COPP config file
+
     """
-        Modifies a COPP config to use the specified rate limit.
-
-        Notes:
-            This only affects COPP policies that enforce a rate limit. Other
-            policies are left alone.
-
-        Args:
-            pps_limit (int): The rate limit to enforce expressed in PPS (packets-per-second)
-            input_config_file (str): The name of the file containing the initial config
-            output_config_file (str): The name of the file to output the modified config
-    """
-
     with open(input_config_file) as input_stream:
         copp_config = json.load(input_stream)
 
-    for trap_group in copp_config:
+    if config_format == "app_db":
+        trap_groups = copp_config
+    elif config_format == "config_db":
+        trap_groups = [{x: y} for x, y in copp_config["COPP_GROUP"].items()]
+    else:
+        raise ValueError("Invalid config format specified")
+
+    for trap_group in trap_groups:
         for _, group_config in trap_group.items():
             # Notes:
             # CIR (committed information rate) - bandwidth limit set by the policer
@@ -48,6 +92,13 @@ def generate_limited_pps_config(pps_limit, input_config_file, output_config_file
     with open(output_config_file, "w+") as output_stream:
         json.dump(copp_config, output_stream)
 
+
 if __name__ == "__main__":
     ARGS = sys.argv[1:]
-    generate_limited_pps_config(ARGS[0], ARGS[1], ARGS[2])
+
+    if len(ARGS) < 4:
+        config_format = "app_db"
+    else:
+        config_format = ARGS[3]
+
+    generate_limited_pps_config(ARGS[0], ARGS[1], ARGS[2], config_format)
