b230c4918fbb1bfdbf38a40617d121ceaae4f44d
Xin Wang <xiwang5@microsoft.com>
2020-12-01 15:30:41 +0800

    Make the minigraph_facts module backward compatible (#2597)
    What is the motivation for this PR?
    The minigraph_facts module uses library sonic_py_common.multi_asic.get_namespace_list
    to get the namespace list and store the value in variable namespace_list. On older SONiC
    image, sonic_py_common.multi_asic is not available. Running this module will fail with
    ImportError.
    
    How did you do it?
    This change improved this module to make it backward compatible with older SONiC
    images by protecting sonic_py_common.multi_asic importing using try...except. When
    ImportError is caught, just assign value [''] to variable namespace_list.
    
    How did you verify/test it?
    Test run the minigaph_facts module on image supports sonic_py_common.multi_asic and on 
    image does not support sonic_py_common.multi_asic.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- ansible/library/minigraph_facts.py
+++ ansible/library/minigraph_facts.py
@@ -9,7 +9,6 @@ import copy
 import ipaddr as ipaddress
 from collections import defaultdict
 from natsort import natsorted
-from sonic_py_common import multi_asic
 
 from lxml import etree as ET
 from lxml.etree import QName
@@ -61,7 +60,12 @@ def parse_png(png, hname):
     console_port = ''
     mgmt_dev = ''
     mgmt_port = ''
-    namespace_list = multi_asic.get_namespace_list()
+    try:
+        from sonic_py_common import multi_asic
+        namespace_list = multi_asic.get_namespace_list()
+    except ImportError:
+        namespace_list = ['']
+
     for child in png:
         if child.tag == str(QName(ns, "DeviceInterfaceLinks")):
             for link in child.findall(str(QName(ns, "DeviceLinkBase"))):
@@ -165,7 +169,7 @@ def parse_dpg(dpg, hname):
             else:
                 intf['mask'] = str(prefix_len)
             intf.update({'attachto': intfname, 'prefixlen': int(prefix_len)})
-                    
+
             # TODO: remove peer_addr after dependency removed
             ipaddr_val = int(ipn.ip)
             peer_addr_val = None
@@ -179,7 +183,7 @@ def parse_dpg(dpg, hname):
                     peer_addr_val = ipaddr_val + 1
                 else:
                     peer_addr_val = ipaddr_val - 1
-                    
+
             if peer_addr_val is not None:
                 intf['peer_addr'] = ipaddress.IPAddress(peer_addr_val)
             intfs.append(intf)
