646470ad42159f64670604dc3fd3cc6a8c7d5681
abdosi <58047199+abdosi@users.noreply.github.com>
2020-12-09 15:07:34 -0800

    [lldp/multi-asic] Update LLDP tests to support multi-asic (#2269)
    Made the following changes to LLDP pytest:
    a) Moved LLDP test case from using minigraph fact to config fact
    b) Moved LLDP from using standard LLDP fact ansible module to user-defined LLDP fact module. This user-defined module is needed because we will have to gather facts from multiple LLDP namespace instances for multi-asic platform
    
    Signed-off-by: Abhishek Dosi <abdosi@micrsoft.com>
    Co-authored-by: Danny Allen <daall@microsoft.com>

--- tests/lldp/test_lldp.py
+++ tests/lldp/test_lldp.py
@@ -17,32 +17,22 @@ def lldp_setup(duthosts, rand_one_dut_hostname, patch_lldpctl, unpatch_lldpctl, 
     unpatch_lldpctl(localhost, duthost)
 
 
-def test_lldp(duthosts, rand_one_dut_hostname, localhost, collect_techsupport, tbinfo):
+def test_lldp(duthosts, rand_one_dut_hostname, localhost, collect_techsupport):
     """ verify the LLDP message on DUT """
     duthost = duthosts[rand_one_dut_hostname]
 
-    mg_facts  = duthost.get_extended_minigraph_facts(tbinfo)
-    lldp_facts = duthost.lldp()['ansible_facts']
+    config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    lldpctl_facts = duthost.lldpctl_facts(skip_interface_pattern_list=["eth0"])['ansible_facts']
 
-    minigraph_lldp_nei = {}
-    for k, v in mg_facts['minigraph_neighbors'].items():
-        if 'server' not in v['name'].lower():
-            minigraph_lldp_nei[k] = v
-
-    # Verify LLDP information is available on most interfaces
-    assert len(lldp_facts['lldp']) > len(minigraph_lldp_nei) * 0.8
-
-    for k, v in lldp_facts['lldp'].items():
-        if k == 'eth0':
-            continue
+    for k, v in lldpctl_facts['lldpctl'].items():
         # Compare the LLDP neighbor name with minigraph neigbhor name (exclude the management port)
-        assert v['chassis']['name'] == minigraph_lldp_nei[k]['name']
+        assert v['chassis']['name'] == config_facts['DEVICE_NEIGHBOR'][k]['name']
         # Compare the LLDP neighbor interface with minigraph neigbhor interface (exclude the management port)
-        assert v['port']['ifname'] == mg_facts['minigraph_neighbors'][k]['port']
-
+        assert v['port']['ifname'] == config_facts['DEVICE_NEIGHBOR'][k]['port']
+   
 
 def test_lldp_neighbor(duthosts, rand_one_dut_hostname, localhost, eos,
-                       collect_techsupport, loganalyzer, tbinfo):
+                       collect_techsupport, loganalyzer):
     """ verify LLDP information on neighbors """
     duthost = duthosts[rand_one_dut_hostname]
 
@@ -55,20 +45,15 @@ def test_lldp_neighbor(duthosts, rand_one_dut_hostname, localhost, eos,
                 not sent since it contain invalid OIDs, bug.*",
         ])
 
-    mg_facts  = duthost.get_extended_minigraph_facts(tbinfo)
     res = duthost.shell("docker exec -i lldp lldpcli show chassis | grep \"SysDescr:\" | sed -e 's/^\\s*SysDescr:\\s*//g'")
     dut_system_description = res['stdout']
-    lldp_facts = duthost.lldp()['ansible_facts']
+    lldpctl_facts = duthost.lldpctl_facts(skip_interface_pattern_list=["eth0"])['ansible_facts']
     host_facts  = duthost.setup()['ansible_facts']
 
     config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
     nei_meta = config_facts.get('DEVICE_NEIGHBOR_METADATA', {})
 
-    for k, v in lldp_facts['lldp'].items():
-        if k == 'eth0':
-            # skip test on management interface
-            continue
-
+    for k, v in lldpctl_facts['lldpctl'].items():
         try:
             hostip = v['chassis']['mgmt-ip']
         except:
@@ -76,7 +61,6 @@ def test_lldp_neighbor(duthosts, rand_one_dut_hostname, localhost, eos,
             hostip = nei_meta[v['chassis']['name']]['mgmt_addr']
 
         nei_lldp_facts = localhost.lldp_facts(host=hostip, version='v2c', community=eos['snmp_rocommunity'])['ansible_facts']
-        print nei_lldp_facts
         neighbor_interface = v['port']['ifname']
         # Verify the published DUT system name field is correct
         assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_sys_name'] == duthost.hostname
@@ -86,7 +70,6 @@ def test_lldp_neighbor(duthosts, rand_one_dut_hostname, localhost, eos,
         # Verify the published DUT system description field is correct
         assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_sys_desc'] == dut_system_description
         # Verify the published DUT port id field is correct
-        assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_port_id'] == mg_facts['minigraph_ports'][k]['alias']
+        assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_port_id'] == config_facts['PORT'][k]['alias']
         # Verify the published DUT port description field is correct
-        assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_port_desc'] == \
-                "%s:%s" % (mg_facts['minigraph_neighbors'][k]['name'], mg_facts['minigraph_neighbors'][k]['port'])
+        assert nei_lldp_facts['ansible_lldp_facts'][neighbor_interface]['neighbor_port_desc'] == config_facts['PORT'][k]['description']
