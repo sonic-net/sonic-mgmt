19ecb5ab93ba3d3bdd13354bc132704aa3a8f71d
Danny Allen <daall@microsoft.com>
2020-12-11 10:28:58 -0800

    [copp] Send traffic over a set period of time for COPP testing (#2643)
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- tests/copp/conftest.py
+++ tests/copp/conftest.py
@@ -8,14 +8,6 @@ def pytest_addoption(parser):
     """
 
     parser.addoption(
-        "--pkt_tx_count",
-        action="store",
-        type=int,
-        default=100000,
-        help="How many packets to send to the DUT"
-    )
-
-    parser.addoption(
         "--copp_swap_syncd",
         action="store_true",
         default=False,
