sonic-net/sonic-mgmt

commit accb36dab58a6a03c462d3b92d887f24c1b99cab

[install image] work around for slow disk devices (#2715)
Approach
What is the motivation for this PR?
Some dut triggers kernel panic while installing new image due to harddrive is too slow and taking too long to sync contents to disk.

How did you do it?
Some devices has slow hard drive, installing image could trigger kernel crash due to task seemingly hang.

Increase the hung task timeout value to 600 seconds to work around this issue.

Signed-off-by: Ying Xie ying.xie@microsoft.com

How did you verify/test it?
Tested the configuration on dut triggers kernel panic consistently.

ansible/library/reduce_and_add_sonic_images.py
@@ -105,6 +105,13 @@ def install_new_sonic_image(module, new_image_url):
                      cmd="rm -f /host/old_config/config_db.json",
                      msg="Remove config_db.json in preference of minigraph.xml")
 
+
+def work_around_for_slow_disks():
+    # Increase hung task timeout to 600 seconds to avoid kernel panic
+    # while writing lots of data to a slow disk.
+    exec_command(module, cmd="sysctl -w kernel.hung_task_timeout_secs=600", ignore_error=True)
+
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
@@ -117,6 +124,7 @@ def main():
     new_image_url   = module.params['new_image_url']
 
     try:
+        work_around_for_slow_disks()
         reduce_installed_sonic_images(module, disk_used_pcent)
         install_new_sonic_image(module, new_image_url)
     except:

commit 20d729d3124030bf15196c69174901ccdd6cd655

[slow disk] pass module into the workaround function (#2717)
Summary:
Address regression caused by PR #2715 .

Approach
What is the motivation for this PR?
PR #2715 introduced an issue where the workaround function doesn't have module parameter to call exec_command with.

How did you do it?
Pass in the module parameter so that workaround function can execute command properly.

How did you verify/test it?
Run upgrade_sonic task against an DUT.

ansible/library/reduce_and_add_sonic_images.py
@@ -106,7 +106,7 @@ def install_new_sonic_image(module, new_image_url):
                      msg="Remove config_db.json in preference of minigraph.xml")
 
 
-def work_around_for_slow_disks():
+def work_around_for_slow_disks(module):
     # Increase hung task timeout to 600 seconds to avoid kernel panic
     # while writing lots of data to a slow disk.
     exec_command(module, cmd="sysctl -w kernel.hung_task_timeout_secs=600", ignore_error=True)
@@ -124,7 +124,7 @@ def main():
     new_image_url   = module.params['new_image_url']
 
     try:
-        work_around_for_slow_disks()
+        work_around_for_slow_disks(module)
         reduce_installed_sonic_images(module, disk_used_pcent)
         install_new_sonic_image(module, new_image_url)
     except: