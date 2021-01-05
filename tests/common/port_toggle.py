0c45c5bf30a3aa5d8949ab1368c7a3d9d89211a2
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2021-01-05 17:10:29 +0800

    [port_toggle] Calculate port toggle timeout based on dut type and port count (#2712)
    What is the motivation for this PR?
    Currently port toggle test case waits 20 seconds for all ports down and 60 seconds for all ports up. However, the wait time could be affected by dut type and port count. This case may fail on some platforms whose CPU is not so powerful.
    
    How did you do it?
    Add a function to calculate port toggle timeout based on dut type and port count

--- tests/common/port_toggle.py
+++ tests/common/port_toggle.py
@@ -12,34 +12,36 @@ from tests.common.helpers.assertions import pytest_assert
 logger = logging.getLogger(__name__)
 
 
-def port_toggle(duthost, tbinfo, ports=None, wait=60, wait_after_ports_up=60, watch=False):
+def port_toggle(duthost, tbinfo, ports=None, wait_time_getter=None, wait_after_ports_up=60, watch=False):
     """
     Toggle ports on DUT.
 
     Args:
         duthost: DUT host object
         ports: Specify list of ports, None if toggle all ports
-        wait: Time to wait for interface to become up
+        wait_time_getter: A call back function to get port toggle wait time.
         wait_after_ports_up: Time to wait after interfaces become up
         watch: Logging system state
     """
 
-    def __get_down_ports():
+    def __get_down_ports(expect_up=True):
         """Check interface status and return the down ports in a set
         """
-        total_down_ports = set()
         ports_down = duthost.interface_facts(up_ports=ports)['ansible_facts']['ansible_interface_link_down_ports']
         db_ports_down = duthost.show_interface(command='status', up_ports=ports)['ansible_facts']\
             ['ansible_interface_link_down_ports']
-        total_down_ports.update(ports_down)
-        total_down_ports.update(db_ports_down)
-        return total_down_ports
+        if expect_up:
+            return set(ports_down) | set(db_ports_down)
+        else:
+            return set(ports_down) & set(db_ports_down)
 
     if ports is None:
         logger.debug('ports is None, toggling all minigraph ports')
         mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
         ports = mg_facts['minigraph_ports'].keys()
-
+    if not wait_time_getter:
+        wait_time_getter = get_port_toggle_wait_time
+    port_down_wait_time, port_up_wait_time = wait_time_getter(duthost, len(ports))
     logger.info('toggling ports:\n%s', pprint.pformat(ports))
 
     cmds_down = []
@@ -70,12 +72,12 @@ def port_toggle(duthost, tbinfo, ports=None, wait=60, wait_after_ports_up=60, wa
         logger.info('Wait for ports to become down.')
         start_time = datetime.datetime.now()
         while True:
-            down_ports = __get_down_ports()
+            down_ports = __get_down_ports(expect_up=False)
             if len(down_ports) == len(ports):
                 shutdown_ok = True
                 break
             time.sleep(5)
-            if (datetime.datetime.now() - start_time).seconds > 20:
+            if (datetime.datetime.now() - start_time).seconds > port_down_wait_time:
                 break
 
         if not shutdown_ok:
@@ -96,7 +98,7 @@ def port_toggle(duthost, tbinfo, ports=None, wait=60, wait_after_ports_up=60, wa
                 startup_ok = True
                 break
             time.sleep(5)
-            if (datetime.datetime.now() - start_time).seconds > wait:
+            if (datetime.datetime.now() - start_time).seconds > port_up_wait_time:
                 break
 
         if not startup_ok:
@@ -110,3 +112,35 @@ def port_toggle(duthost, tbinfo, ports=None, wait=60, wait_after_ports_up=60, wa
 
     logger.info('wait %d seconds for system to startup', wait_after_ports_up)
     time.sleep(wait_after_ports_up)
+
+
+def get_port_toggle_wait_time(duthost, port_count):
+    """
+    Port toggle wait time depends on many factors: port count, cpu type. This function allow user to customize port
+    toggle wait time according to DUT host information and port count.
+    :param duthost: DUT host object
+    :param port_count: total port count to be toggled
+    :return: port down wait time and port up wait time
+    """
+    port_down_wait_time, port_up_wait_time = 20, 60
+    asic_type = duthost.facts["asic_type"]
+    if asic_type == 'mellanox':
+        # default t0 topology has 28 ports to toggle
+        base_port_count = 28.0
+        if port_count <= base_port_count:
+            port_count_factor = 1
+        else:
+            port_count_factor = port_count / base_port_count
+        from .mellanox_data import get_chip_type
+        chip_type = get_chip_type(duthost)
+        if chip_type == 'spectrum1':
+            # spectrum based switch usually has less powerful cpu
+            port_down_wait_time = 40
+            port_up_wait_time = 70
+        else:
+            port_down_wait_time = 30
+
+        port_down_wait_time = int(port_down_wait_time * port_count_factor)
+        port_up_wait_time = int(port_up_wait_time * port_count_factor)
+
+    return port_down_wait_time, port_up_wait_time
