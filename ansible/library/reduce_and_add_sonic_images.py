sonic-net/sonic-mgmt

commit d97d086ad97831b39f59361a5d3dc3c923a872b5

[test_upgrade_path] Support test_upgrade_path on KVM switch on VS testbed (#2661)
test_upgrade_path presently is not supported and enabled on KVM switch. Make changes to:

1. Support copying the base/target images from a path within sonic-mgmt container.
2. Support the image download from a remote URL.
3. Added VS setup marker in test_upgrade_path.
4. Detect path type - Hyperlink vs filesystem path. Transfer or download the images accordingly.
5. Add a default mgmt gw route so that switch can download the image from a remore URL. Remove this after download.

Executed test_upgrade_path using both remote public URLs and local file path.
Upgrade KVM switch using URL of public Jenkins images:

ansible/library/reduce_and_add_sonic_images.py
@@ -47,37 +47,25 @@ def get_disk_free_size(module, partition):
 def reduce_installed_sonic_images(module, disk_used_pcent):
     exec_command(module, cmd="sonic_installer cleanup -y", ignore_error=True)
 
+
 def download_new_sonic_image(module, new_image_url, save_as):
     global results
-    if not new_image_url:
-        return
-    exec_command(module,
-                 cmd="curl -o {} {}".format(save_as, new_image_url),
-                 msg="downloading new image")
+    if new_image_url:
+        exec_command(module,
+                    cmd="curl -o {} {}".format(save_as, new_image_url),
+                    msg="downloading new image")
     if path.exists(save_as):
-        _, out, _ = exec_command(module,
-                                                cmd="sonic_installer binary_version %s" % save_as
-                                                )
+        _, out, _ = exec_command(module,cmd="sonic_installer binary_version %s" % save_as)
         results['downloaded_image_version'] = out.rstrip('\n')
 
-def install_new_sonic_image(module, new_image_url):
-    if not new_image_url:
-        return
 
-    avail = get_disk_free_size(module, "/host")
-    if avail >= 2000:
-        # There is enough space to install directly
-        save_as = "/host/downloaded-sonic-image"
-        download_new_sonic_image(module, new_image_url, save_as)
-        rc, out, err = exec_command(module,
-                     cmd="sonic_installer install {} -y".format(save_as),
-                     msg="installing new image", ignore_error=True)
-        # Always remove the downloaded temp image inside /host/ before proceeding
-        exec_command(module, cmd="rm -f {}".format(save_as))
-        if rc != 0:
-            module.fail_json(msg="Image installation failed: rc=%d, out=%s, err=%s" %
-                         (rc, out, err))
-    else:
+def install_new_sonic_image(module, new_image_url, save_as=None):
+
+    if not save_as:
+        avail = get_disk_free_size(module, "/host")
+        save_as = "/host/downloaded-sonic-image" if avail >= 2000 else "/tmp/tmpfs/downloaded-sonic-image"
+
+    if save_as.startswith("/tmp/tmpfs"):
         # Create a tmpfs partition to download image to install
         exec_command(module, cmd="mkdir -p /tmp/tmpfs", ignore_error=True)
         exec_command(module, cmd="umount /tmp/tmpfs", ignore_error=True)
@@ -85,7 +73,6 @@ def install_new_sonic_image(module, new_image_url):
         exec_command(module,
                      cmd="mount -t tmpfs -o size=1300M tmpfs /tmp/tmpfs",
                      msg="mounting tmpfs")
-        save_as = "/tmp/tmpfs/downloaded-sonic-image"
         download_new_sonic_image(module, new_image_url, save_as)
         rc, out, err = exec_command(module,
                      cmd="sonic_installer install {} -y".format(save_as),
@@ -94,6 +81,17 @@ def install_new_sonic_image(module, new_image_url):
         exec_command(module, cmd="sync", ignore_error=True)
         exec_command(module, cmd="umount /tmp/tmpfs", ignore_error=True)
         exec_command(module, cmd="rm -rf /tmp/tmpfs", ignore_error=True)
+        if rc != 0:
+            module.fail_json(msg="Image installation failed: rc=%d, out=%s, err=%s" %
+                         (rc, out, err))
+    else:
+        # There is enough space to install directly
+        download_new_sonic_image(module, new_image_url, save_as)
+        rc, out, err = exec_command(module,
+                     cmd="sonic_installer install {} -y".format(save_as),
+                     msg="installing new image", ignore_error=True)
+        # Always remove the downloaded temp image inside /host/ before proceeding
+        exec_command(module, cmd="rm -f {}".format(save_as))
         if rc != 0:
             module.fail_json(msg="Image installation failed: rc=%d, out=%s, err=%s" %
                          (rc, out, err))
@@ -117,16 +115,18 @@ def main():
         argument_spec=dict(
             disk_used_pcent=dict(required=False, type='int', default=8),
             new_image_url=dict(required=False, type='str', default=None),
+            save_as=dict(required=False, type='str', default=None),
         ),
         supports_check_mode=False)
 
     disk_used_pcent = module.params['disk_used_pcent']
     new_image_url   = module.params['new_image_url']
+    save_as = module.params['save_as']
 
     try:
         work_around_for_slow_disks(module)
         reduce_installed_sonic_images(module, disk_used_pcent)
-        install_new_sonic_image(module, new_image_url)
+        install_new_sonic_image(module, new_image_url, save_as)
     except:
         err = str(sys.exc_info())
         module.fail_json(msg="Error: %s" % err)

tests/upgrade_path/test_upgrade_path.py
@@ -2,12 +2,19 @@ import pytest
 import logging
 import json
 import os
-from tests.common import reboot
-from jinja2 import Template
-import ipaddr as ipaddress
-from tests.ptf_runner import ptf_runner
-from tests.common.platform.ssh_utils import prepare_testbed_ssh_keys
+import time
+from urlparse import urlparse
 from datetime import datetime
+from jinja2 import Template
+import ipaddr
+import ipaddress
+from tests.ptf_runner import ptf_runner
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.platform.ssh_utils import prepare_testbed_ssh_keys
+from tests.common import reboot
+from tests.common.reboot import get_reboot_cause
+from tests.common.reboot import REBOOT_TYPE_WARM
+
 
 from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses      # lgtm[py/unused-import]
@@ -33,15 +40,15 @@ def setup(localhost, ptfhost, duthosts, rand_one_dut_hostname, upgrade_path_list
     duthost = duthosts[rand_one_dut_hostname]
     prepare_ptf(ptfhost, duthost, tbinfo)
     yield
-    cleanup(localhost, ptfhost, duthost, upgrade_path_lists)
+    cleanup(localhost, ptfhost, duthost, upgrade_path_lists, tbinfo)
 
 
-def cleanup(localhost, ptfhost, duthost, upgrade_path_lists):
+def cleanup(localhost, ptfhost, duthost, upgrade_path_lists, tbinfo):
     _, _, restore_to_image = upgrade_path_lists
     if restore_to_image:
         logger.info("Preparing to cleanup and restore to {}".format(restore_to_image))
         # restore orignial image
-        install_sonic(duthost, restore_to_image)
+        install_sonic(duthost, restore_to_image, tbinfo)
         # Perform a cold reboot
         reboot(duthost, localhost)
     # cleanup
@@ -85,12 +92,17 @@ def prepare_ptf(ptfhost, duthost, tbinfo):
 def ptf_params(duthosts, rand_one_dut_hostname, nbrhosts, creds, tbinfo):
     duthost = duthosts[rand_one_dut_hostname]
 
+    if duthost.facts['platform'] == 'x86_64-kvm_x86_64-r0':
+        reboot_limit_in_seconds = 150
+    else:
+        reboot_limit_in_seconds = 30
+
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
     lo_v6_prefix = ""
     for intf in mg_facts["minigraph_lo_interfaces"]:
-        ipn = ipaddress.IPNetwork(intf['addr'])
+        ipn = ipaddr.IPNetwork(intf['addr'])
         if ipn.version == 6:
-            lo_v6_prefix = str(ipaddress.IPNetwork(intf['addr'] + '/64').network) + '/64'
+            lo_v6_prefix = str(ipaddr.IPNetwork(intf['addr'] + '/64').network) + '/64'
             break
 
     vm_hosts = []
@@ -106,7 +118,7 @@ def ptf_params(duthosts, rand_one_dut_hostname, nbrhosts, creds, tbinfo):
         "dut_password": creds.get('sonicadmin_password'),
         "alt_password": sonicadmin_alt_password,
         "dut_hostname": duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host'],
-        "reboot_limit_in_seconds": 30,
+        "reboot_limit_in_seconds": reboot_limit_in_seconds,
         "reboot_type": "warm-reboot",
         "portchannel_ports_file": TMP_VLAN_PORTCHANNEL_FILE,
         "vlan_ports_file": TMP_VLAN_FILE,
@@ -122,6 +134,7 @@ def ptf_params(duthosts, rand_one_dut_hostname, nbrhosts, creds, tbinfo):
     }
     return ptf_params
 
+
 def get_reboot_type(duthost):
     next_os_version = duthost.shell('sonic_installer list | grep Next | cut -f2 -d " "')['stdout']
     current_os_version = duthost.shell('sonic_installer list | grep Current | cut -f2 -d " "')['stdout']
@@ -133,17 +146,72 @@ def get_reboot_type(duthost):
         reboot_type = "warm-reboot"
     return reboot_type
 
+
 def check_sonic_version(duthost, target_version):
     current_version = duthost.image_facts()['ansible_facts']['ansible_image_facts']['current']
     assert current_version == target_version, \
         "Upgrade sonic failed: target={} current={}".format(target_version, current_version)
 
 
-def install_sonic(duthost, image_url):
-    res = duthost.reduce_and_add_sonic_images(new_image_url=image_url)
+def install_sonic(duthost, image_url, tbinfo):
+    new_route_added = False
+    if urlparse(image_url).scheme in ('http', 'https',):
+        mg_gwaddr = duthost.get_extended_minigraph_facts(tbinfo).get("minigraph_mgmt_interface", {}).get("gwaddr")
+        mg_gwaddr = ipaddress.IPv4Address(mg_gwaddr)
+        rtinfo_v4 = duthost.get_ip_route_info(ipaddress.ip_network(u'0.0.0.0/0'))
+        for nexthop in rtinfo_v4['nexthops']:
+            if mg_gwaddr == nexthop[0]:
+                break
+        else:
+            # Temporarily change the default route to mgmt-gateway address. This is done so that
+            # DUT can download an image from a remote host over the mgmt network.
+            logger.info("Add default mgmt-gateway-route to the device via {}".format(mg_gwaddr))
+            duthost.shell("ip route add default via {}".format(mg_gwaddr), module_ignore_errors=True)
+            new_route_added = True
+        res = duthost.reduce_and_add_sonic_images(new_image_url=image_url)
+    else:
+        out = duthost.command("df -BM --output=avail /host",
+                        module_ignore_errors=True)["stdout"]
+        avail = int(out.split('\n')[1][:-1])
+        if avail >= 2000:
+            # There is enough space to install directly
+            save_as = "/host/downloaded-sonic-image"
+        else:
+            save_as = "/tmp/tmpfs/downloaded-sonic-image"
+            # Create a tmpfs partition to download image to install
+            duthost.shell("mkdir -p /tmp/tmpfs", module_ignore_errors=True)
+            duthost.shell("umount /tmp/tmpfs", module_ignore_errors=True)
+            duthost.shell("mount -t tmpfs -o size=1300M tmpfs /tmp/tmpfs", module_ignore_errors=True)
+        duthost.copy(src=image_url, dest=save_as)
+        res = duthost.reduce_and_add_sonic_images(save_as=save_as)
+
+    # if the new default mgmt-gateway route was added, remove it. This is done so that
+    # default route src address matches Loopback0 address
+    if new_route_added:
+        logger.info("Remove default mgmt-gateway-route earlier added")
+        duthost.shell("ip route del default via {}".format(mg_gwaddr), module_ignore_errors=True)
     return res['ansible_facts']['downloaded_image_version']
 
-def test_upgrade_path(localhost, duthosts, rand_one_dut_hostname, ptfhost, upgrade_path_lists, ptf_params, setup):
+
+def check_services(duthost):
+    """
+    Perform a health check of services
+    """
+    logging.info("Wait until DUT uptime reaches {}s".format(300))
+    while duthost.get_uptime().total_seconds() < 300:
+            time.sleep(1)
+    logging.info("Wait until all critical services are fully started")
+    logging.info("Check critical service status")
+    pytest_assert(duthost.critical_services_fully_started(), "dut.critical_services_fully_started is False")
+
+    for service in duthost.critical_services:
+        status = duthost.get_service_props(service)
+        pytest_assert(status["ActiveState"] == "active", "ActiveState of {} is {}, expected: active".format(service, status["ActiveState"]))
+        pytest_assert(status["SubState"] == "running", "SubState of {} is {}, expected: running".format(service, status["SubState"]))
+        
+
+@pytest.mark.device_type('vs')
+def test_upgrade_path(localhost, duthosts, rand_one_dut_hostname, ptfhost, upgrade_path_lists, ptf_params, setup, tbinfo):
     duthost = duthosts[rand_one_dut_hostname]
     from_list_images, to_list_images, _ = upgrade_path_lists
     from_list = from_list_images.split(',')
@@ -154,14 +222,15 @@ def test_upgrade_path(localhost, duthosts, rand_one_dut_hostname, ptfhost, upgra
             logger.info("Test upgrade path from {} to {}".format(from_image, to_image))
             # Install base image
             logger.info("Installing {}".format(from_image))
-            target_version = install_sonic(duthost, from_image)
+            target_version = install_sonic(duthost, from_image, tbinfo)
             # Perform a cold reboot
+            logger.info("Cold reboot the DUT to make the base image as current")
             reboot(duthost, localhost)
             check_sonic_version(duthost, target_version)
 
             # Install target image
             logger.info("Upgrading to {}".format(to_image))
-            target_version = install_sonic(duthost, to_image)
+            target_version = install_sonic(duthost, to_image, tbinfo)
             test_params = ptf_params
             test_params['target_version'] = target_version
             test_params['reboot_type'] = get_reboot_type(duthost)
@@ -176,4 +245,8 @@ def test_upgrade_path(localhost, duthosts, rand_one_dut_hostname, ptfhost, upgra
                        platform="remote",
                        qlen=10000,
                        log_file=log_file)
+            reboot_cause = get_reboot_cause(duthost)
+            logger.info("Check reboot cause. Expected cause {}".format(REBOOT_TYPE_WARM))
+            pytest_assert(reboot_cause == REBOOT_TYPE_WARM, "Reboot cause {} did not match the trigger - {}".format(reboot_cause, REBOOT_TYPE_WARM))
+            check_services(duthost)
 

commit 264820c04f4a5321d59f8be8264f2d697f917849

Bug fix - handle the cleanup only case without image URL (#2783)
Description of PR
Summary: Fix bug introduced by #2661 where if new_image_url is not provided, the installation logic still proceeds.
This leads to error in the Ansible module in upgrade_sonic.yml step for removing old sonic images.

Approach
What is the motivation for this PR?
How did you do it?
Added the check for no image URL. And do not execute installation logic if URL is None.

How did you verify/test it?
Tested on a physical lab device, and the Ansible execution of upgrade_sonic.yml passed.

ansible/library/reduce_and_add_sonic_images.py
@@ -50,17 +50,16 @@ def reduce_installed_sonic_images(module, disk_used_pcent):
 
 def download_new_sonic_image(module, new_image_url, save_as):
     global results
-    if new_image_url:
-        exec_command(module,
-                    cmd="curl -o {} {}".format(save_as, new_image_url),
-                    msg="downloading new image")
+    exec_command(module,
+                cmd="curl -o {} {}".format(save_as, new_image_url),
+                msg="downloading new image")
+
     if path.exists(save_as):
         _, out, _ = exec_command(module,cmd="sonic_installer binary_version %s" % save_as)
         results['downloaded_image_version'] = out.rstrip('\n')
 
 
 def install_new_sonic_image(module, new_image_url, save_as=None):
-
     if not save_as:
         avail = get_disk_free_size(module, "/host")
         save_as = "/host/downloaded-sonic-image" if avail >= 2000 else "/tmp/tmpfs/downloaded-sonic-image"
@@ -126,7 +125,8 @@ def main():
     try:
         work_around_for_slow_disks(module)
         reduce_installed_sonic_images(module, disk_used_pcent)
-        install_new_sonic_image(module, new_image_url, save_as)
+        if new_image_url:
+            install_new_sonic_image(module, new_image_url, save_as)
     except:
         err = str(sys.exc_info())
         module.fail_json(msg="Error: %s" % err)