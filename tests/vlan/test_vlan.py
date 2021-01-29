1304c0a78daeccc94725b284faefa2eae862a95f
Viktor Cheketa <74666177+vcheketx@users.noreply.github.com>
2021-01-29 11:36:41 +0200

    Use pytest.fail() for vlan non-broadcast tests (#2844)
    What is the motivation for this PR?
    test_vlan_tc4_tagged_non_broadcast and test_vlan_tc5_untagged_non_broadcast fail with incorrect error message if we do not receive expected packet.
    
    Expected result:
            if isinstance(result_dst_if, ptfadapter.dataplane.PollSuccess):
                logger.info ("One Way Tagged Packet Transmission Works")
                logger.info ("Tagged packet successfully sent from port {} to port {}".format(src_port, dst_port))
            else:
    >           test.fail("Expected packet was not received")
    E           Failed: Expected packet was not received
    
    Actual result:
            if isinstance(result_dst_if, ptfadapter.dataplane.PollSuccess):
                logger.info ("One Way Tagged Packet Transmission Works")
                logger.info ("Tagged packet successfully sent from port {} to port {}".format(src_port, dst_port))
            else:
    >           test.fail("Expected packet was not received")
    E           NameError: global name 'test' is not defined
    
    How did you do it?
    Use pytest.fail() instead of test.fail()

--- tests/vlan/test_vlan.py
+++ tests/vlan/test_vlan.py
@@ -386,7 +386,7 @@ def test_vlan_tc4_tagged_non_broadcast(ptfadapter, vlan_ports_list, duthost):
         logger.info ("One Way Tagged Packet Transmission Works")
         logger.info ("Tagged packet successfully sent from port {} to port {}".format(src_port, dst_port))
     else:
-        test.fail("Expected packet was not received")
+        pytest.fail("Expected packet was not received")
 
     logger.info ("Tagged packet to be sent from port {} to port {}".format(dst_port, src_port))
 
@@ -399,7 +399,7 @@ def test_vlan_tc4_tagged_non_broadcast(ptfadapter, vlan_ports_list, duthost):
         logger.info ("Two Way Tagged Packet Transmission Works")
         logger.info ("Tagged packet successfully sent from port {} to port {}".format(dst_port, src_port))
     else:
-        test.fail("Expected packet was not received")
+        pytest.fail("Expected packet was not received")
 
 @pytest.mark.bsl
 def test_vlan_tc5_untagged_non_broadcast(ptfadapter, vlan_ports_list, duthost):
@@ -439,7 +439,7 @@ def test_vlan_tc5_untagged_non_broadcast(ptfadapter, vlan_ports_list, duthost):
         logger.info ("One Way Untagged Packet Transmission Works")
         logger.info ("Untagged packet successfully sent from port {} to port {}".format(src_port, dst_port))
     else:
-        test.fail("Expected packet was not received")
+        pytest.fail("Expected packet was not received")
 
     logger.info ("Untagged packet to be sent from port {} to port {}".format(dst_port, src_port))  
 
@@ -453,4 +453,4 @@ def test_vlan_tc5_untagged_non_broadcast(ptfadapter, vlan_ports_list, duthost):
         logger.info ("Two Way Untagged Packet Transmission Works")
         logger.info ("Untagged packet successfully sent from port {} to port {}".format(dst_port, src_port))
     else:
-        test.fail("Expected packet was not received")
+        pytest.fail("Expected packet was not received")
