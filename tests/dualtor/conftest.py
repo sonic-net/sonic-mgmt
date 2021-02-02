901b38aacb418c42b132506611aa4bf20064b4b1
bingwang <bingwang@microsoft.com>
2021-02-02 01:22:18 -0800

    Use mux_simulator to toggle mux cable.
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/dualtor/conftest.py
+++ tests/dualtor/conftest.py
@@ -1,7 +1,3 @@
 import pytest
-from tests.common.dualtor.dual_tor_utils import force_active_tor
-
-@pytest.fixture(scope="module", autouse=True)
-def set_selected_tor_active(duthosts, rand_one_dut_hostname):
-    force_active_tor(duthosts[rand_one_dut_hostname], 'all')
+from tests.common.dualtor.mux_simulator_control import mux_server_url, toggle_all_simulator_ports_to_rand_selected_tor
 
