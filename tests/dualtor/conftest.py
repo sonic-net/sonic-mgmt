7ef409511b0034d50aff66290a6cef61510012ab
bingwang <bingwang@microsoft.com>
2021-02-02 17:13:55 -0800

    Fix LGTM.
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/dualtor/conftest.py
+++ tests/dualtor/conftest.py
@@ -1,3 +1,2 @@
-import pytest
-from tests.common.dualtor.mux_simulator_control import mux_server_url, toggle_all_simulator_ports_to_rand_selected_tor
+from tests.common.dualtor.mux_simulator_control import mux_server_url, toggle_all_simulator_ports_to_rand_selected_tor # lgtm[py/unused-import]
 
