2a2cef8cd5c5a710cba4e153876264d201b721ee
sanmalho-git <66454160+sanmalho-git@users.noreply.github.com>
2021-02-07 20:44:43 -0500

    [multi-dut] Support for gen-mg to work with multi-dut where VM's connect to only a single host and supervisor card (#2700)
    What is the motivation for this PR?
    For SONiC Chassis support gen-mg has the following limitations:
    * for multi-dut it assumes each VM is connected to all the DUT's (similar to the dualtor topology)
    * assumes that all interfaces connected to a PortChannel are sequential.
    
    It also needs the following enhancements:
    * In a SONiC Chassis we have a 'supervisor' card which has no ports and thus no BGP/loopback/IpInterfaces/ACL's etc. The minigraph should not include info about these.
    * We need to introduce 'T3' router type as 'CoreRouter'
    
    How did you do it?
    For VMs to connect to a single DUT:
    * In vm_topo_config, the fields like interface_indexes, intfs, ip_intf etc would be either empty or null. Add checks for these where they are being used in gen-mg (config_sonic_basedon_testbed.yml and the minigraph templates).
    
    For not all PortChannel ports being sequential:
    * In config_sonic_based_on_testbed.yml, changed interface_to_vms structure to be compatable to allow using ansible's 'with_subelements' loop to get the intf_names
    
    For SONiC Chassis support:
    * For 'supervisor' card, there are no BGP peers, interfaces etc. required.
      * A 'supervisor' card is identified by having 'type' field in the inventory with value 'supervisor'.
      * Add this check to skip generation of portions not valid for supervisor card
    * Added 'CoreRouter' as the DUT type for T3 VM's that connect to 'T2' DUT.
    
    How did you verify/test it?
    * Ran gen-mg against all the testbeds defined in vtestbed.csv with latest github master code, and then with our code changes. Verified that there are no differences in the generated minigraph files.
    * Ran against topo_t2.yml proposed in PR #2638 where we have 3 linecards with 24 VM's each (8 on 2 port LAG and 16 on single port) and 1 supervisor card.
      * Validated generated minigraph files for the linecards are the same as those generated by running gen-mg against individual linecard testbed (with only 1 DUT).
      * Validate the generated minigraph file for supervisor card is acceptable to sonic-cfggen and the generated config_db.json is valid for the supervisor card.

--- ansible/library/topo_facts.py
+++ ansible/library/topo_facts.py
@@ -153,15 +153,19 @@ class ParseTestbedTopoinfo():
                     ip = ipaddress.ip_address(ipstr.decode('utf8'))
                     for dut_index in range(0, dut_num):
                         if ip.version == 4:
-                            ipsubnet_str = vmconfig[vm]['peer_ipv4'][dut_index]+'/'+vmconfig[vm]['ipv4mask'][dut_index]
-                            ipsubnet = ipaddress.ip_interface(ipsubnet_str.decode('utf8'))
-                            if ip in ipsubnet.network:
-                                vmconfig[vm]['bgp_ipv4'][dut_index] = ipstr.upper()
+                            # Each VM might not be connected to all the DUT's, so check if this VM is a peer to DUT at dut_index
+                            if vmconfig[vm]['peer_ipv4'][dut_index]:
+                                ipsubnet_str = vmconfig[vm]['peer_ipv4'][dut_index]+'/'+vmconfig[vm]['ipv4mask'][dut_index]
+                                ipsubnet = ipaddress.ip_interface(ipsubnet_str.decode('utf8'))
+                                if ip in ipsubnet.network:
+                                    vmconfig[vm]['bgp_ipv4'][dut_index] = ipstr.upper()
                         elif ip.version == 6:
-                            ipsubnet_str = vmconfig[vm]['peer_ipv6'][dut_index]+'/'+vmconfig[vm]['ipv6mask'][dut_index]
-                            ipsubnet = ipaddress.ip_interface(ipsubnet_str.decode('utf8'))
-                            if ip in ipsubnet.network:
-                                vmconfig[vm]['bgp_ipv6'][dut_index] = ipstr.upper()
+                            # Each VM might not be connected to all the DUT's, so check if this VM is a peer to DUT at dut_index
+                            if vmconfig[vm]['peer_ipv6'][dut_index]:
+                                ipsubnet_str = vmconfig[vm]['peer_ipv6'][dut_index]+'/'+vmconfig[vm]['ipv6mask'][dut_index]
+                                ipsubnet = ipaddress.ip_interface(ipsubnet_str.decode('utf8'))
+                                if ip in ipsubnet.network:
+                                    vmconfig[vm]['bgp_ipv6'][dut_index] = ipstr.upper()
 
             vm_topo_config['vm'] = vmconfig
 
