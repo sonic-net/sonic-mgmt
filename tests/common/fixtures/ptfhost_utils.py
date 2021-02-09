9bd1fca53216242302eca35c8cdb443181d7fb2b
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2021-02-09 10:59:01 +0800

    [dualtor] Add icmp_responder (#2938)
    What is the motivation for this PR?
    Simulate the physical servers for dualtor testbeds that could reply ICMP echo-request(heatbeat).
    
    How did you do it?
    Add a service icmp_responder to ptf container.
    
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- tests/common/fixtures/ptfhost_utils.py
+++ tests/common/fixtures/ptfhost_utils.py
@@ -1,19 +1,27 @@
 import os
-from natsort import natsorted
 import pytest
 import logging
+
+from jinja2 import Template
+from natsort import natsorted
+
 
 logger = logging.getLogger(__name__)
 
 ROOT_DIR = "/root"
 OPT_DIR = "/opt"
+SUPERVISOR_CONFIG_DIR = "/etc/supervisor/conf.d/"
 SCRIPTS_SRC_DIR = "scripts/"
+TEMPLATES_DIR = "templates/"
 ACS_TESTS = "acstests"
 PTF_TESTS = "ptftests"
 SAI_TESTS = "saitests"
 ARP_RESPONDER_PY = "arp_responder.py"
+ICMP_RESPONDER_PY = "icmp_responder.py"
+ICMP_RESPONDER_CONF_TEMPL = "icmp_responder.conf.j2"
 CHANGE_MAC_ADDRESS_SCRIPT = "scripts/change_mac.sh"
 REMOVE_IP_ADDRESS_SCRIPT = "scripts/remove_ip.sh"
+
 
 @pytest.fixture(scope="session", autouse=True)
 def copy_acstests_directory(ptfhost):
@@ -34,6 +42,7 @@ def copy_acstests_directory(ptfhost):
     logger.info("Delete ACS test files from PTF host '{0}'".format(ptfhost.hostname))
     ptfhost.file(path=os.path.join(ROOT_DIR, ACS_TESTS), state="absent")
 
+
 @pytest.fixture(scope="session", autouse=True)
 def copy_ptftests_directory(ptfhost):
     """
@@ -52,6 +61,7 @@ def copy_ptftests_directory(ptfhost):
 
     logger.info("Delete PTF test files from PTF host '{0}'".format(ptfhost.hostname))
     ptfhost.file(path=os.path.join(ROOT_DIR, PTF_TESTS), state="absent")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def copy_saitests_directory(ptfhost):
@@ -72,6 +82,7 @@ def copy_saitests_directory(ptfhost):
     logger.info("Delete SAI test files from PTF host '{0}'".format(ptfhost.hostname))
     ptfhost.file(path=os.path.join(ROOT_DIR, SAI_TESTS), state="absent")
 
+
 @pytest.fixture(scope="session", autouse=True)
 def change_mac_addresses(ptfhost):
     """
@@ -85,6 +96,7 @@ def change_mac_addresses(ptfhost):
     """
     logger.info("Change interface MAC addresses on ptfhost '{0}'".format(ptfhost.hostname))
     ptfhost.script(CHANGE_MAC_ADDRESS_SCRIPT)
+
 
 @pytest.fixture(scope="session", autouse=True)
 def remove_ip_addresses(ptfhost):
@@ -103,7 +115,8 @@ def remove_ip_addresses(ptfhost):
 
     logger.info("Remove IPs to restore ptfhost '{0}'".format(ptfhost.hostname))
     ptfhost.script(REMOVE_IP_ADDRESS_SCRIPT)
-    
+
+
 @pytest.fixture(scope="session", autouse=True)
 def copy_arp_responder_py(ptfhost):
     """
@@ -121,6 +134,7 @@ def copy_arp_responder_py(ptfhost):
 
     logger.info("Delete arp_responder.py from ptfhost '{0}'".format(ptfhost.hostname))
     ptfhost.file(path=os.path.join(OPT_DIR, ARP_RESPONDER_PY), state="absent")
+
 
 @pytest.fixture(scope='class')
 def ptf_portmap_file(duthosts, rand_one_dut_hostname, ptfhost):
@@ -151,3 +165,27 @@ def ptf_portmap_file(duthosts, rand_one_dut_hostname, ptfhost):
     ptfhost.copy(src=portMapFile, dest="/root/")
 
     yield "/root/{}".format(portMapFile.split('/')[-1])
+
+
+@pytest.fixture(scope="session", autouse=True)
+def run_icmp_responder(duthost, ptfhost, tbinfo):
+    """Run icmp_responder.py over ptfhost."""
+    logger.debug("Copy icmp_responder.py to ptfhost '{0}'".format(ptfhost.hostname))
+    ptfhost.copy(src=os.path.join(SCRIPTS_SRC_DIR, ICMP_RESPONDER_PY), dest=OPT_DIR)
+
+    logging.debug("Start running icmp_responder")
+    templ = Template(open(os.path.join(TEMPLATES_DIR, ICMP_RESPONDER_CONF_TEMPL)).read())
+    ptf_indices = duthost.get_extended_minigraph_facts(tbinfo)["minigraph_ptf_indices"]
+    vlan_intfs = duthost.get_vlan_intfs()
+    icmp_responder_args = " ".join("-i eth%s" % ptf_indices[_] for _ in vlan_intfs)
+    ptfhost.copy(
+        content=templ.render(icmp_responder_args=icmp_responder_args),
+        dest=os.path.join(SUPERVISOR_CONFIG_DIR, "icmp_responder.conf")
+    )
+    ptfhost.shell("supervisorctl update")
+    ptfhost.shell("supervisorctl start icmp_responder")
+
+    yield
+
+    logging.debug("Stop running icmp_responder")
+    ptfhost.shell("supervisorctl stop icmp_responder")
