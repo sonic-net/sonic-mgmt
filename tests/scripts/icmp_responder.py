1044d2934773fe05966841384b2468db20435f73
Lawrence Lee <lawlee@microsoft.com>
2021-02-10 17:07:18 +0000

    [icmp responder]: Allow hardcoding of ICMP responder MAC
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/scripts/icmp_responder.py
+++ tests/scripts/icmp_responder.py
@@ -6,7 +6,7 @@ from scapy.data import ETH_P_IP
 from select import select
 
 
-def respond_to_icmp_request(socket, request):
+def respond_to_icmp_request(socket, request, src_mac=None):
     """Respond to ICMP request."""
     reply = request.copy()
     reply[ICMP].type = 0
@@ -14,7 +14,13 @@ def respond_to_icmp_request(socket, request):
     reply[ICMP].chksum = None
     reply[IP].src, reply[IP].dst = request[IP].dst, request[IP].src
     reply[IP].chksum = None
-    reply[Ether].src, reply[Ether].dst = request[Ether].dst, request[Ether].src
+
+    if src_mac is None:
+        icmp_reply_source_mac = request[Ether].src
+    else:
+        icmp_reply_source_mac = src_mac
+
+    reply[Ether].src, reply[Ether].dst = request[Ether].dst, icmp_reply_source_mac
     socket.send(reply)
 
 
@@ -23,7 +29,7 @@ class ICMPSniffer(object):
 
     TYPE_ECHO_REQUEST = 8
 
-    def __init__(self, ifaces, request_handler=None):
+    def __init__(self, ifaces, request_handler=None, src_mac=None):
         """
         Init ICMP sniffer.
 
@@ -37,6 +43,7 @@ class ICMPSniffer(object):
             self.sniff_sockets.append(conf.L2socket(type=ETH_P_IP, iface=iface, filter="icmp"))
             self.iface_hwaddr[iface] = get_if_hwaddr(iface)
         self.request_handler = request_handler
+        self.src_mac = src_mac
 
     def __call__(self):
         try:
@@ -46,7 +53,7 @@ class ICMPSniffer(object):
                     packet = s.recv()
                     if packet is not None:
                         if packet[ICMP].type == self.TYPE_ECHO_REQUEST and self.request_handler:
-                            self.request_handler(s, packet)
+                            self.request_handler(s, packet, self.src_mac)
         finally:
             for s in self.sniff_sockets:
                 s.close()
@@ -55,8 +62,10 @@ class ICMPSniffer(object):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ICMP responder")
     parser.add_argument("--intf", "-i", dest="ifaces", required=True, action="append", help="interface to listen for ICMP request")
+    parser.add_argument("--src_mac", "-m", dest="src_mac", required=False, action="store", help="The source MAC to use for ICMP replies")
     args = parser.parse_args()
     ifaces = args.ifaces
+    src_mac = args.src_mac
 
-    icmp_sniffer = ICMPSniffer(ifaces, request_handler=respond_to_icmp_request)
+    icmp_sniffer = ICMPSniffer(ifaces, request_handler=respond_to_icmp_request, src_mac=src_mac)
     icmp_sniffer()
