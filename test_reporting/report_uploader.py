6319e0cd4370dffad07b47cb30d9b62cdbf942ff
Danny Allen <daall@microsoft.com>
2021-02-10 17:20:07 -0800

    [report_uploader] Add support for uploading ICMP ping data from devutils (#2974)
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- test_reporting/report_uploader.py
+++ test_reporting/report_uploader.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import sys
 
 from junit_xml_parser import (
     validate_junit_json_file,
@@ -25,19 +27,31 @@ python3 report_uploader.py tests/files/sample_tr.xml -e TRACKING_ID#22
     parser.add_argument(
         "--json", "-j", action="store_true", help="Load an existing test result JSON file from path_name.",
     )
+    parser.add_argument(
+        "--category", "-c", type=str, help="Type of data to upload (i.e. test_result, reachability, etc.)"
+    )
 
     args = parser.parse_args()
-
-    if args.json:
-        test_result_json = validate_junit_json_file(args.path_name)
-    else:
-        roots = validate_junit_xml_path(args.path_name)
-        test_result_json = parse_test_result(roots)
-
-    tracking_id = args.external_id if args.external_id else ""
-
     kusto_db = KustoConnector(args.db_name)
-    kusto_db.upload_report(test_result_json, tracking_id)
+
+    if args.category == "test_result":
+        if args.json:
+            test_result_json = validate_junit_json_file(args.path_name)
+        else:
+            roots = validate_junit_xml_path(args.path_name)
+            test_result_json = parse_test_result(roots)
+
+        tracking_id = args.external_id if args.external_id else ""
+
+        kusto_db.upload_report(test_result_json, tracking_id)
+    elif args.category == "reachability":
+        with open(args.path_name) as f:
+            reachability_data = json.load(f)
+
+        kusto_db.upload_reachability_data(reachability_data)
+    else:
+        print('Unknown category "{}"'.format(args.category))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
