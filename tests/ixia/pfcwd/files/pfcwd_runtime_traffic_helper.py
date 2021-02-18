aa82911da204bbece5951769560b3346816db83b
Wei Bai <baiwei0427@gmail.com>
2021-02-18 09:23:35 -0800

    [rdma] Fix var name and add check for flow completion in max attempts (#2996)
    Some minor fixes of RDMA IXIA test cases
    
    Signed-off-by: Wei Bai webai@microsoft.com
    
    How did you do it?
    Check if flows complete in max_attempts rounds/seconds.
    Fix a variable name
    
    How did you verify/test it?
    I did test using Mellanox SN2700 and IXIA chassis

--- tests/ixia/pfcwd/files/pfcwd_runtime_traffic_helper.py
+++ tests/ixia/pfcwd/files/pfcwd_runtime_traffic_helper.py
@@ -184,6 +184,9 @@ def __run_traffic(api, config, duthost, all_flow_names, pfcwd_start_delay_sec, e
             time.sleep(1)
             attempts += 1
 
+    pytest_assert(attempts < max_attempts,
+                  "Flows do not stop in {} seconds".format(max_attempts))
+
     """ Dump per-flow statistics """
     rows = api.get_flow_results(FlowRequest(flow_names=all_flow_names))
     api.set_state(State(FlowTransmitState(state='stop')))
