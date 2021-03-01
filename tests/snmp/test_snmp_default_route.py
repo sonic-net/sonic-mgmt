9a5cec4d9fc417f392f98655e37580617bb95c5f
Suvarna Meenakshi <sumeenak@microsoft.com>
2021-03-01 00:35:41 +0000

    Add ipCidRouteTable MIB in snmp_facts. Make changes in test_snmp_default_route to use snmp_facts.
    Signed-off-by: Suvarna Meenakshi <sumeenak@microsoft.com>

--- tests/snmp/test_snmp_default_route.py
+++ tests/snmp/test_snmp_default_route.py
@@ -9,43 +9,39 @@ pytestmark = [
     pytest.mark.device_type('vs')
 ]
 
-
 @pytest.mark.bsl
 def test_snmp_default_route(duthosts, enum_dut_hostname, localhost, creds):
     """compare the snmp facts between observed states and target state"""
 
     duthost = duthosts[enum_dut_hostname]
     hostip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    snmp_facts = localhost.snmp_facts(host=hostip, version="v2c", community=creds["snmp_rocommunity"])['ansible_facts']
     dut_result = duthost.shell('show ip route 0.0.0.0/0 | grep "\*"')
-    route_count = 0
 
-    # Test to ensure show ip route 0.0.0.0/0 result matches with SNMP result
+    dut_result_nexthops = []
+    # ipCidrRouteEntry MIB for default route will have entries 
+    # where next hop are not eth0 interface.
     for line in dut_result['stdout_lines']:
         if 'via' in line:
             ip, interface = line.split('via')
             ip = ip.strip("*, ")
             interface = interface.strip("*, ")
             if interface != "eth0":
-                route_count = route_count + 1
-                snmp_result = localhost.shell(
-                        'snmpget -v2c -c {} {} 1.3.6.1.2.1.4.24.4.1.1.0.0.0.0.0.0.0.0.0.{}'
-                        .format(quote(creds['snmp_rocommunity']), hostip, ip))
-                assert snmp_result is not None, "snmpget failed for default route via {}".format(ip)
-                assert(snmp_result['stdout_lines'][0].split('=')[1].strip().lower() == "ipaddress: 0.0.0.0")
+                dut_result_nexthops.append(ip)
 
-                snmp_result = localhost.shell(
-                        'snmpget -v2c -c {} {} 1.3.6.1.2.1.4.24.4.1.16.0.0.0.0.0.0.0.0.0.{}'
-                        .format(quote(creds['snmp_rocommunity']), hostip, ip))
-                assert snmp_result is not None, "snmpget failed for default route via {}".format(ip)
-                assert(snmp_result['stdout_lines'][0].split('=')[1].strip().lower() == "integer: 1")
+    # If show ip route 0.0.0.0/0 has route only via eth0,
+    # or has no route snmp_facts for ip_cidr_route
+    # will be empty.
+    if len(dut_result_nexthops) == 0:
+        assert 'snmp_cidr_route' not in snmp_facts, 'snmp_cidr_route should not be present in snmp_facts'
 
-    # Test to ensure the number of lines in SNMP result matches the output from show ip route
-    snmp_result = localhost.shell(
-            'snmpwalk -v2c -c {} {} 1.3.6.1.2.1.4.24.4.1.1'.format(quote(creds['snmp_rocommunity']), hostip))
-    assert snmp_result is not None, "snmpwalk failed for default route via {}".format(ip)
-    assert len(snmp_result['stdout_lines']) == route_count
+    if len(dut_result_nexthops) != 0:
+        # Test to ensure show ip route 0.0.0.0/0 result matches with SNMP result
+        for ip in dut_result_ips:
+            assert ip in snmp_facts['snmp_cidr_route'], "{} ip not found in snmp_facts".format(ip)
+            assert snmp_facts['snmp_cidr_route'][ip]['route_dest'] == '0.0.0.0', "Incorrect route_dest for {} ip".format(ip)
+            assert snmp_facts['snmp_cidr_route'][ip]['status'] == '1', "Incorrect status for {} ip".format(ip)
 
-    snmp_result = localhost.shell(
-            'snmpwalk -v2c -c {} {} 1.3.6.1.2.1.4.24.4.1.16'.format(quote(creds['snmp_rocommunity']), hostip))
-    assert snmp_result is not None, "snmpwalk failed for default route via {}".format(ip)
-    assert len(snmp_result['stdout_lines']) == route_count
+        # Compare the length of routes in CLI output and SNMP facts 
+        assert len(snmp_facts['snmp_cidr_route'].keys()) == len(snmp_facts['snmp_cidr_route'].keys()), \
+                "Number or route entries in SNMP does not match with cli"
