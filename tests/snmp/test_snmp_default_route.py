f4fc9d22b2f07e8c3c3593f419aaf7f5d04d729b
Suvarna Meenakshi <sumeenak@microsoft.com>
2021-03-01 03:40:32 +0000

    Fix variable name.
    Signed-off-by: Suvarna Meenakshi <sumeenak@microsoft.com>

--- tests/snmp/test_snmp_default_route.py
+++ tests/snmp/test_snmp_default_route.py
@@ -37,7 +37,7 @@ def test_snmp_default_route(duthosts, enum_dut_hostname, localhost, creds):
 
     if len(dut_result_nexthops) != 0:
         # Test to ensure show ip route 0.0.0.0/0 result matches with SNMP result
-        for ip in dut_result_ips:
+        for ip in dut_result_nexthops:
             assert ip in snmp_facts['snmp_cidr_route'], "{} ip not found in snmp_facts".format(ip)
             assert snmp_facts['snmp_cidr_route'][ip]['route_dest'] == '0.0.0.0', "Incorrect route_dest for {} ip".format(ip)
             assert snmp_facts['snmp_cidr_route'][ip]['status'] == '1', "Incorrect status for {} ip".format(ip)
