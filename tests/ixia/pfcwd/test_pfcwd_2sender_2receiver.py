42ea213f871ea28dc2fbe922175bdc2a5f8866f6
Wei Bai <baiwei0427@gmail.com>
2021-03-05 10:46:14 -0800

    [rdma] Test PFC watchdog in a three-node topology with 2 senders and 2 receivers (#3082)
    * Add PFC watchdog two sender two receiver test case
    
    Signed-off-by: Wei Bai webai@microsoft.com
    
    How did you do it?
    Implement helper functions in tests/ixia/pfcwd/files/pfcwd_2sender_2receiver_helper.py
    Implement the test case in tests/ixia/pfcwd/test_pfcwd_2sender_2receiver.py
    
    How did you verify/test it?
    I did test using SONiC switches and IXIA chassis. The Tgen API version is 0.0.75. The IXIA Linux API server version is 9.10.
    
    Supported testbed topology if it's a new test case?
    T0 topology using IXIA chassis as the fanout

---
+++ tests/ixia/pfcwd/test_pfcwd_2sender_2receiver.py
@@ -0,0 +1,65 @@
+import pytest
+
+from tests.common.helpers.assertions import pytest_require, pytest_assert
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
+    fanout_graph_facts
+from tests.common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_port,\
+    ixia_api_serv_user, ixia_api_serv_passwd, ixia_api, ixia_testbed
+from tests.common.ixia.qos_fixtures import prio_dscp_map, all_prio_list,\
+    lossless_prio_list, lossy_prio_list
+
+from files.pfcwd_2sender_2receiver_helper import run_pfcwd_2sender_2receiver_test
+
+@pytest.mark.topology("tgen")
+
+@pytest.mark.parametrize("trigger_pfcwd", [True, False])
+def test_pfcwd_2sender_2receiver(ixia_api,
+                                 ixia_testbed,
+                                 conn_graph_facts,
+                                 fanout_graph_facts,
+                                 duthosts,
+                                 rand_one_dut_hostname,
+                                 rand_one_dut_portname_oper_up,
+                                 rand_one_dut_lossless_prio,
+                                 lossy_prio_list,
+                                 prio_dscp_map,
+                                 trigger_pfcwd):
+
+    """
+    Run PFC watchdog test in a 3-host topology with 2 senders and 2 receivers
+
+    Args:
+        ixia_api (pytest fixture): IXIA session
+        ixia_testbed (pytest fixture): L2/L3 config of a T0 testbed
+        conn_graph_facts (pytest fixture): connection graph
+        fanout_graph_facts (pytest fixture): fanout graph
+        duthosts (pytest fixture): list of DUTs
+        rand_one_dut_hostname (str): hostname of DUT
+        rand_one_dut_portname_oper_up (str): port to test, e.g., 's6100-1|Ethernet0'
+        rand_one_dut_lossless_prio (str): lossless priority to test, e.g., 's6100-1|3'
+        lossy_prio_list (pytest fixture): list of lossy priorities
+        prio_dscp_map (pytest fixture): priority vs. DSCP map (key = priority)
+        trigger_pfcwd (bool): if PFC watchdog is expected to be triggered
+
+    Returns:
+        N/A
+    """
+    dut_hostname, dut_port = rand_one_dut_portname_oper_up.split('|')
+    dut_hostname2, lossless_prio = rand_one_dut_lossless_prio.split('|')
+    pytest_require(rand_one_dut_hostname == dut_hostname == dut_hostname2,
+                   "Priority and port are not mapped to the expected DUT")
+
+    duthost = duthosts[rand_one_dut_hostname]
+    lossless_prio = int(lossless_prio)
+
+    run_pfcwd_2sender_2receiver_test(api=ixia_api,
+                                     testbed_config=ixia_testbed,
+                                     conn_data=conn_graph_facts,
+                                     fanout_data=fanout_graph_facts,
+                                     duthost=duthost,
+                                     dut_port=dut_port,
+                                     pause_prio_list=[lossless_prio],
+                                     test_prio_list=[lossless_prio],
+                                     bg_prio_list=lossy_prio_list,
+                                     prio_dscp_map=prio_dscp_map,
+                                     trigger_pfcwd=trigger_pfcwd)
