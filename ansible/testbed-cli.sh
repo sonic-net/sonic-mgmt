853c08dc9c0e970230c63f302d49eb1172c211bd
Xin Wang <xiwang5@microsoft.com>
2021-03-12 08:53:13 +0800

    Add 'testbed-cli.sh announce-routes' (#3127)
    In PR #2741, test script for announcing IP routes to PTF docker container was deprecated.
    The function of announcing IP routes is moved to 'testbed-cli.sh add-topo'. Then we do
    not need to run the test script to announce IP routes after a new topology is deployed.
    
    However, there are still occasions that people may want to re-announce the IP routes to
    PTF docker containers. Because announcing routes is part of add-topo, then the only
    possible way to re-announce routes is to run 'testbed-cli.sh remove-topo' and
    'testbed-cli.sh add-topo'. This is inconvenient and time consuming.
    
    This change added a new 'announce-routes' sub-command to the testbed-cli.sh tool.
    We can now just re-announce IP routes using command like below:
        ./testbed-cli.sh -t vtestbed.csv vms-kvm-t0 password.txt
    
    Other changes:
    * Corrected the inv_name field in vtestbed.csv file.
    * Moved the 'announce_routes.py' module from ansible/roles/vm_set/library to ansible/library
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- ansible/testbed-cli.sh
+++ ansible/testbed-cli.sh
@@ -12,6 +12,7 @@ function usage
   echo "    $0 [options] refresh-dut <topo-name> <vault-password-file>"
   echo "    $0 [options] (connect-vms | disconnect-vms) <topo-name> <vault-password-file>"
   echo "    $0 [options] config-vm <topo-name> <vm-name> <vault-password-file>"
+  echo "    $0 [options] announce-routes <topo-name> <vault-password-file>"
   echo "    $0 [options] (gen-mg | deploy-mg | test-mg) <topo-name> <inventory> <vault-password-file>"
   echo "    $0 [options] (create-master | destroy-master) <k8s-server-name> <vault-password-file>"
   echo
@@ -51,6 +52,7 @@ function usage
   echo "To connect a topology: $0 connect-topo 'topo-name' ~/.password"
   echo "To refresh DUT in a topology: $0 refresh-dut 'topo-name' ~/.password"
   echo "To configure a VM on a server: $0 config-vm 'topo-name' 'vm-name' ~/.password"
+  echo "To announce routes to DUT: $0 announce-routes 'topo-name' ~/.password"
   echo "To generate minigraph for DUT in a topology: $0 gen-mg 'topo-name' 'inventory' ~/.password"
   echo "To deploy minigraph to DUT in a topology: $0 deploy-mg 'topo-name' 'inventory' ~/.password"
   echo "    gen-mg, deploy-mg, test-mg supports enabling/disabling data ACL with parameter"
@@ -98,6 +100,7 @@ function read_csv
   vm_base=${line_arr[8]}
   dut=${line_arr[9]//;/,}
   duts=${dut//[\[\] ]/}
+  inventory=${line_arr[10]}
 }
 
 function read_yaml
@@ -138,6 +141,7 @@ function read_yaml
   vm_base=${line_arr[8]}
   dut=${line_arr[9]}
   duts=$(python -c "from __future__ import print_function; print(','.join(eval(\"$dut\")))")
+  inventory=${line_arr[10]}
 }
 
 function read_file
@@ -341,6 +345,23 @@ function disconnect_vms
   echo Done
 }
 
+function announce_routes
+{
+  topology=$1
+  passfile=$2
+  shift
+  shift
+
+  echo "Announce routes '$topology'"
+
+  read_file $topology
+
+  ANSIBLE_SCP_IF_SSH=y ansible-playbook -i "$inventory" testbed_announce_routes.yml --vault-password-file="$passfile" \
+      -l "$server" -e vm_set_name="$vm_set_name" -e topo="$topo" -e ptf_ip="$ptf_ip" $@
+
+  echo done
+}
+
 function generate_minigraph
 {
   topology=$1
@@ -520,6 +541,8 @@ case "${subcmd}" in
   disconnect-vms) disconnect_vms $@
                ;;
   config-vm)   config_vm $@
+               ;;
+  announce-routes) announce_routes $@
                ;;
   gen-mg)      generate_minigraph $@
                ;;
