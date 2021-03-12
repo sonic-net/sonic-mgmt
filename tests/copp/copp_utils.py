8a118edee7da9a1322090fe852bb9ba34cdec970
abdosi <58047199+abdosi@users.noreply.github.com>
2021-03-12 10:58:35 -0800

    multi-asic support for copp testcase (#3060)
    This is PR continuation of #2627 where infra changes were done to support copp on multi-asic platforms. In this PR test is enhanced to work on multi-asic platforms. Following are major changes:
    
    Multi-asic as of now will support installing of nanomsg and ptf_nn_agent in syncd directly mode. This way test case will randomly select any front-asic syncd for verification. To support this needed to add iptable rules for HTTP and PTF Traffic forwarding from host to asic namespace. swap_syncd mode is not supported as of now.
    
    All the docker name are translated to particular namespace/asic that is under test. (Added utility api for same).
    
    Remove disable_lldp_for_testing fixture as noted below since test case is not dependent on that.

--- tests/copp/copp_utils.py
+++ tests/copp/copp_utils.py
@@ -4,6 +4,7 @@
     Todo:
         Refactor ptfadapter so it can be leveraged in these test cases.
 """
+import re
 
 DEFAULT_NN_TARGET_PORT = 3
 
@@ -12,20 +13,21 @@ _ADD_IP_SCRIPT = "scripts/add_ip.sh"
 _UPDATE_COPP_SCRIPT = "copp/scripts/update_copp_config.py"
 
 _BASE_COPP_CONFIG = "/tmp/base_copp_config.json"
-_APP_DB_COPP_CONFIG = "swss:/etc/swss/config.d/00-copp.config.json"
+_APP_DB_COPP_CONFIG = ":/etc/swss/config.d/00-copp.config.json"
 _CONFIG_DB_COPP_CONFIG = "/etc/sonic/copp_cfg.json"
 _TEMP_COPP_CONFIG = "/tmp/copp_config.json"
 _TEMP_COPP_TEMPLATE = "/tmp/copp.json.j2"
 _COPP_TEMPLATE_PATH = "/usr/share/sonic/templates/copp.json.j2"
-_SWSS_COPP_TEMPLATE = "swss:" + _COPP_TEMPLATE_PATH
+_SWSS_COPP_TEMPLATE = ":" + _COPP_TEMPLATE_PATH
 
 _PTF_NN_TEMPLATE = "templates/ptf_nn_agent.conf.ptf.j2"
 _PTF_NN_DEST = "/etc/supervisor/conf.d/ptf_nn_agent.conf"
 
 _SYNCD_NN_TEMPLATE = "templates/ptf_nn_agent.conf.dut.j2"
 _SYNCD_NN_DEST = "/tmp/ptf_nn_agent.conf"
+_SYNCD_NN_FILE = "ptf_nn_agent.conf"
 
-def limit_policer(dut, pps_limit):
+def limit_policer(dut, pps_limit, nn_target_namespace):
     """
         Updates the COPP configuration in the SWSS container to respect a given rate limit.
 
@@ -37,8 +39,12 @@ def limit_policer(dut, pps_limit):
             pps_limit (int): The rate limit for COPP to enforce on ALL trap groups.
     """
 
+    asichost = dut.asic_instance_from_namespace(nn_target_namespace)
+
+    swss_docker_name = asichost.get_docker_name("swss")
+
     if "201811" in dut.os_version or "201911" in dut.os_version:
-        dut.command("docker cp {} {}".format(_APP_DB_COPP_CONFIG, _BASE_COPP_CONFIG))
+        dut.command("docker cp {} {}".format(swss_docker_name + _APP_DB_COPP_CONFIG, _BASE_COPP_CONFIG))
         config_format = "app_db"
     else:
         dut.command("cp {} {}".format(_CONFIG_DB_COPP_CONFIG, _BASE_COPP_CONFIG))
@@ -53,17 +59,17 @@ def limit_policer(dut, pps_limit):
     )
 
     if config_format == "app_db":
-        dut.command("docker cp {} {}".format(_TEMP_COPP_CONFIG, _APP_DB_COPP_CONFIG))
+        dut.command("docker cp {} {}".format(_TEMP_COPP_CONFIG, swss_docker_name + _APP_DB_COPP_CONFIG))
 
         # As copp config is regenerated each time swss starts need to replace the template with
         # config updated above. But before doing that need store the original template in a
         # temporary file for restore after test.
-        dut.command("docker cp {} {}".format(_SWSS_COPP_TEMPLATE, _TEMP_COPP_TEMPLATE))
-        dut.command("docker cp {} {}".format(_TEMP_COPP_CONFIG, _SWSS_COPP_TEMPLATE))
+        dut.command("docker cp {} {}".format(swss_docker_name + _SWSS_COPP_TEMPLATE, _TEMP_COPP_TEMPLATE))
+        dut.command("docker cp {} {}".format(_TEMP_COPP_CONFIG, swss_docker_name + _SWSS_COPP_TEMPLATE))
     else:
         dut.command("cp {} {}".format(_TEMP_COPP_CONFIG, _CONFIG_DB_COPP_CONFIG))
 
-def restore_policer(dut):
+def restore_policer(dut, nn_target_namespace):
     """
         Reloads the default COPP configuration in the SWSS container.
 
@@ -72,10 +78,14 @@ def restore_policer(dut):
 
             The SWSS container must be restarted for the config change to take effect.
     """
+    asichost = dut.asic_instance_from_namespace(nn_target_namespace)
+
+    swss_docker_name = asichost.get_docker_name("swss")
+
     # Restore the copp template in swss
     if "201811" in dut.os_version or "201911" in dut.os_version:
-        dut.command("docker cp {} {}".format(_BASE_COPP_CONFIG, _APP_DB_COPP_CONFIG))
-        dut.command("docker cp {} {}".format(_TEMP_COPP_TEMPLATE, _SWSS_COPP_TEMPLATE))
+        dut.command("docker cp {} {}".format(_BASE_COPP_CONFIG, swss_docker_name + _APP_DB_COPP_CONFIG))
+        dut.command("docker cp {} {}".format(_TEMP_COPP_TEMPLATE, swss_docker_name + _SWSS_COPP_TEMPLATE))
     else:
         dut.command("cp {} {}".format(_BASE_COPP_CONFIG, _CONFIG_DB_COPP_CONFIG))
 
@@ -114,7 +124,7 @@ def restore_ptf(ptf):
 
     ptf.supervisorctl(name="ptf_nn_agent", state="restarted")
 
-def configure_syncd(dut, nn_target_port, nn_target_interface, creds):
+def configure_syncd(dut, nn_target_port, nn_target_interface, nn_target_namespace, creds):
     """
         Configures syncd to run the NN agent on the specified port.
 
@@ -125,22 +135,38 @@ def configure_syncd(dut, nn_target_port, nn_target_interface, creds):
         Args:
             dut (SonicHost): The target device.
             nn_target_port (int): The port to run NN agent on.
-            nn_target_interface (str): The Interface remote NN agents listens.
+            nn_target_interface (str): The Interface remote NN agents listen to
+            nn_target_namespace (str): The namespace remote NN agents listens
             creds (dict): Credential information according to the dut inventory
     """
 
     facts = {"nn_target_port": nn_target_port, "nn_target_interface": nn_target_interface}
     dut.host.options["variable_manager"].extra_vars.update(facts)
 
-    _install_nano(dut, creds)
+    asichost = dut.asic_instance_from_namespace(nn_target_namespace)
+
+    syncd_docker_name = asichost.get_docker_name("syncd")
+
+    _install_nano(dut, creds, syncd_docker_name)
 
     dut.template(src=_SYNCD_NN_TEMPLATE, dest=_SYNCD_NN_DEST)
-    dut.command("docker cp {} syncd:/etc/supervisor/conf.d/".format(_SYNCD_NN_DEST))
 
-    dut.command("docker exec syncd supervisorctl reread")
-    dut.command("docker exec syncd supervisorctl update")
+    dut.command("docker cp {} {}:/etc/supervisor/conf.d/".format(_SYNCD_NN_DEST, syncd_docker_name))
 
-def _install_nano(dut, creds):
+    dut.command("docker exec {} supervisorctl reread".format(syncd_docker_name))
+    dut.command("docker exec {} supervisorctl update".format(syncd_docker_name))
+
+def restore_syncd(dut, nn_target_namespace):
+    asichost = dut.asic_instance_from_namespace(nn_target_namespace)
+
+    syncd_docker_name = asichost.get_docker_name("syncd")
+
+    dut.command("docker exec {} rm -rf /etc/supervisor/conf.d/{}".format(syncd_docker_name, _SYNCD_NN_FILE))
+    dut.command("docker exec {} supervisorctl reread".format(syncd_docker_name))
+    dut.command("docker exec {} supervisorctl update".format(syncd_docker_name))
+
+
+def _install_nano(dut, creds,  syncd_docker_name):
     """
         Install nanomsg package to syncd container.
 
@@ -149,13 +175,13 @@ def _install_nano(dut, creds):
             creds (dict): Credential information according to the dut inventory
     """
 
-    output = dut.command("docker exec syncd bash -c '[ -d /usr/local/include/nanomsg ] || echo copp'")
+    output = dut.command("docker exec {} bash -c '[ -d /usr/local/include/nanomsg ] || echo copp'".format(syncd_docker_name))
 
     if output["stdout"] == "copp":
         http_proxy = creds.get('proxy_env', {}).get('http_proxy', '')
         https_proxy = creds.get('proxy_env', {}).get('https_proxy', '')
 
-        cmd = '''docker exec -e http_proxy={} -e https_proxy={} syncd bash -c " \
+        cmd = '''docker exec -e http_proxy={} -e https_proxy={} {} bash -c " \
                 rm -rf /var/lib/apt/lists/* \
                 && apt-get update \
                 && apt-get install -y python-pip build-essential libssl-dev python-dev python-setuptools wget cmake \
@@ -165,7 +191,7 @@ def _install_nano(dut, creds):
                 && rm -f 1.0.0.tar.gz && pip2 install cffi==1.7.0 && pip2 install --upgrade cffi==1.7.0 && pip2 install nnpy \
                 && mkdir -p /opt && cd /opt && wget https://raw.githubusercontent.com/p4lang/ptf/master/ptf_nn/ptf_nn_agent.py \
                 && mkdir ptf && cd ptf && wget https://raw.githubusercontent.com/p4lang/ptf/master/src/ptf/afpacket.py && touch __init__.py \
-                " '''.format(http_proxy, https_proxy)
+                " '''.format(http_proxy, https_proxy, syncd_docker_name)
         dut.command(cmd)
 
 def _map_port_number_to_interface(dut, nn_target_port):
@@ -175,3 +201,14 @@ def _map_port_number_to_interface(dut, nn_target_port):
 
     interfaces = dut.command("portstat")["stdout_lines"][2:]
     return interfaces[nn_target_port].split()[0]
+
+def _get_http_and_https_proxy_ip(creds):
+    """
+       Get the http and https proxy ip.
+
+       Args:
+           creds (dict): Credential information according to the dut inventory
+    """
+
+    return (re.findall(r'[0-9]+(?:\.[0-9]+){3}', creds.get('proxy_env', {}).get('http_proxy', ''))[0],
+            re.findall(r'[0-9]+(?:\.[0-9]+){3}', creds.get('proxy_env', {}).get('https_proxy', ''))[0])
