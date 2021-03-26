2807ec09dea6fee0c976f896947cd5d51dde1c44
Danny Allen <daall@microsoft.com>
2021-03-25 23:42:44 -0700

    [tacacs] Address race conditions during TACACS server setup/teardown (#3191)
    - Wait 10 seconds to start the server after applying the config template
    - Do not change user credentials during rw_user test
    
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- tests/tacacs/test_rw_user.py
+++ tests/tacacs/test_rw_user.py
@@ -1,36 +1,33 @@
 import pytest
 import crypt
 
+from .test_ro_user import ssh_remote_run
+
 pytestmark = [
     pytest.mark.disable_loganalyzer,
     pytest.mark.topology('any'),
     pytest.mark.device_type('vs')
 ]
 
-def test_rw_user(duthosts, rand_one_dut_hostname, creds, test_tacacs):
+
+def test_rw_user(localhost, duthosts, rand_one_dut_hostname, creds, test_tacacs):
     """test tacacs rw user
     """
     duthost = duthosts[rand_one_dut_hostname]
-
-    duthost.host.options['variable_manager'].extra_vars.update(
-        {'ansible_user':creds['tacacs_rw_user'], 'ansible_password':creds['tacacs_rw_user_passwd']})
-
-    res = duthost.shell("cat /etc/passwd")
+    dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    res = ssh_remote_run(localhost, dutip, creds['tacacs_rw_user'], creds['tacacs_rw_user_passwd'], "cat /etc/passwd")
 
     for l in res['stdout_lines']:
         fds = l.split(':')
         if fds[0] == "testadmin":
             assert fds[4] == "remote_user_su"
 
-def test_rw_user_ipv6(duthosts, rand_one_dut_hostname, creds, test_tacacs_v6):
+def test_rw_user_ipv6(localhost, duthosts, rand_one_dut_hostname, creds, test_tacacs_v6):
     """test tacacs rw user
     """
     duthost = duthosts[rand_one_dut_hostname]
-
-    duthost.host.options['variable_manager'].extra_vars.update(
-        {'ansible_user':creds['tacacs_rw_user'], 'ansible_password':creds['tacacs_rw_user_passwd']})
-
-    res = duthost.shell("cat /etc/passwd")
+    dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    res = ssh_remote_run(localhost, dutip, creds['tacacs_rw_user'], creds['tacacs_rw_user_passwd'], "cat /etc/passwd")
 
     for l in res['stdout_lines']:
         fds = l.split(':')
