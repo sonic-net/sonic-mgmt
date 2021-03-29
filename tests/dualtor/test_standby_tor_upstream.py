9d32d6faaaa04531b0b8166afaea2b2adf3106f4
bingwang <bingwang@microsoft.com>
2021-03-29 01:13:30 -0700

    Fix comment
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/dualtor/test_standby_tor_upstream.py
+++ tests/dualtor/test_standby_tor_upstream.py
@@ -9,12 +9,14 @@ from tests.common.helpers.assertions import pytest_assert as pt_assert
 from tests.common.dualtor.dual_tor_utils import rand_selected_interface, verify_upstream_traffic
 from tests.common.utilities import compare_crm_facts
 from tests.common.config_reload import config_reload
+from tests.common.dualtor.mux_simulator_control import url, toggle_mux_simulator_to_tor
+from tests.common.fixtures.ptfhost_utils import change_mac_addresses, run_garp_service, run_icmp_responder
 
 logger = logging.getLogger(__file__)
 
 pytestmark = [
     pytest.mark.topology('t0'),
-    pytest.mark.usefixtures('apply_mock_dual_tor_tables', 'apply_mock_dual_tor_kernel_configs')
+    pytest.mark.usefixtures('apply_mock_dual_tor_tables', 'apply_mock_dual_tor_kernel_configs', 'run_garp_service', 'run_icmp_responder')
 ]
 
 PAUSE_TIME = 10
@@ -36,6 +38,10 @@ def clear_portstat(dut):
     dut.shell("portstat -c")
 
 
+def is_mocked_dualtor(tbinfo):
+    return 'dualtor' not in tbinfo['topo']['name']
+
+
 @pytest.fixture(scope='module', autouse=True)
 def test_cleanup(rand_selected_dut):
     """
@@ -44,12 +50,24 @@ def test_cleanup(rand_selected_dut):
     yield
     config_reload(rand_selected_dut)
 
-def test_standby_tor_upstream(rand_selected_dut, tbinfo, ptfadapter, rand_selected_interface):
+
+def set_mux_state(rand_selected_dut, rand_unselected_dut, tbinfo, mux_url, state, itfs):
+    if is_mocked_dualtor(tbinfo):
+        set_dual_tor_state_to_orchagent(rand_selected_dut, state, [itfs])
+    else:
+        if state == 'active':
+            toggle_mux_simulator_to_tor(rand_selected_dut, mux_url, tbinfo)
+        else:
+            toggle_mux_simulator_to_tor(rand_unselected_dut, mux_url, tbinfo)
+
+
+def test_standby_tor_upstream(rand_selected_dut, rand_unselected_dut, tbinfo, ptfadapter, rand_selected_interface, url):
     itfs, ip = rand_selected_interface
+    mux_url = url(itfs)
     crm_facts0 = rand_selected_dut.get_crm_facts()
     PKT_NUM = 100
     # Step 1. Set mux state to standby and verify traffic is dropped by ACL rule and drop counters incremented
-    set_dual_tor_state_to_orchagent(rand_selected_dut, 'standby', [itfs])
+    set_mux_state(rand_selected_dut, rand_unselected_dut, tbinfo, mux_url, 'standby', itfs)
     # Wait sometime for mux toggle
     time.sleep(PAUSE_TIME)
     # Verify packets are not go up
@@ -68,7 +86,7 @@ def test_standby_tor_upstream(rand_selected_dut, tbinfo, ptfadapter, rand_select
                 "RX_DRP for {} is expected to increase by {} actually {}".format(itfs, PKT_NUM, drop_counter))
 
     # Step 2. Toggle mux state to active, and verify traffic is not dropped by ACL and fwd-ed to uplinks; verify CRM show and no nexthop objects are stale
-    set_dual_tor_state_to_orchagent(rand_selected_dut, 'active', [itfs])
+    set_mux_state(rand_selected_dut, rand_unselected_dut, tbinfo, mux_url, 'active', itfs)
     # Wait sometime for mux toggle
     time.sleep(PAUSE_TIME)
     # Verify packets are not go up
@@ -84,7 +102,7 @@ def test_standby_tor_upstream(rand_selected_dut, tbinfo, ptfadapter, rand_select
     pt_assert(len(unmatched_crm_facts)==0, 'Unmatched CRM facts: {}'.format(json.dumps(unmatched_crm_facts, indent=4)))
 
     # Step 3. Toggle mux state to standby, and verify traffic is dropped by ACL; verify CRM show and no nexthop objects are stale
-    set_dual_tor_state_to_orchagent(rand_selected_dut, 'standby', [itfs])
+    set_mux_state(rand_selected_dut, rand_unselected_dut, tbinfo, mux_url, 'standby', itfs)
      # Wait sometime for mux toggle
     time.sleep(PAUSE_TIME)
     # Verify packets are not go up again
@@ -102,4 +120,5 @@ def test_standby_tor_upstream(rand_selected_dut, tbinfo, ptfadapter, rand_select
     crm_facts2 = rand_selected_dut.get_crm_facts()
     unmatched_crm_facts = compare_crm_facts(crm_facts0, crm_facts2)
     pt_assert(len(unmatched_crm_facts)==0, 'Unmatched CRM facts: {}'.format(json.dumps(unmatched_crm_facts, indent=4)))
+
 
