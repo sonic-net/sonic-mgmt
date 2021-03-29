4b860cdbfa6fda7c0cb892edbac686385ff1f809
Vivek Reddy Karri <vkarri@nvidia.com>
2021-03-29 16:13:42 +0000

    Refactor & Cleanup Fixes
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- tests/sflow/test_sflow.py
+++ tests/sflow/test_sflow.py
@@ -123,6 +123,7 @@ def config_sflow_agent(duthosts, rand_one_dut_hostname):
     # NOTE: When no agent-id is set, hsflowd chooses the agent-id based on simple heuristics
     # Hence, this fixture to keep the test stable
     duthost = duthosts[rand_one_dut_hostname]
+    duthost.shell("config sflow agent-id del") # Remove any existing agent-id
     duthost.shell("config sflow agent-id add Loopback0")
     yield   
     duthost.shell("config sflow agent-id del")
@@ -307,21 +308,22 @@ class TestSflowCollector():
 
 
 # ------------------------------------------------------------------------------
-
+@pytest.mark.usefixtures("sflowbase_config")
+@pytest.mark.usefixtures("config_sflow_agent")
 class TestSflowPolling():
     """
     Test Sflow polling with different polling interval and check whether the test interface sends one counter sample for every polling interval
     Disable polling and check the dut doesn't send counter samples .
     """
 
-    def testPolling(self, sflowbase_config, config_sflow_agent, duthost, partial_ptf_runner):
+    def testPolling(self, duthost, partial_ptf_runner):
         duthost.shell("config sflow polling-interval 20")
         verify_show_sflow(duthost,status='up',polling_int=20)
         partial_ptf_runner(
               polling_int=20,
               active_collectors="['collector0','collector1']" )
 
-    def testDisablePolling(self, sflowbase_config, config_sflow_agent, duthost, partial_ptf_runner):
+    def testDisablePolling(self, duthost, partial_ptf_runner):
         duthost.shell("config sflow polling-interval 0")
 
         verify_show_sflow(duthost,status='up',polling_int=0)
@@ -329,7 +331,7 @@ class TestSflowPolling():
               polling_int=0,
               active_collectors="['collector0','collector1']" )
 
-    def testDifferntPollingInt(self, sflowbase_config, config_sflow_agent, duthost, partial_ptf_runner):
+    def testDifferntPollingInt(self, duthost, partial_ptf_runner):
         duthost.shell("config sflow polling-interval 60")
 
         verify_show_sflow(duthost,status='up',polling_int=60)
@@ -398,7 +400,7 @@ class TestSflowInterface():
               active_collectors="['collector0','collector1']" )
 
 # ------------------------------------------------------------------------------
-
+@pytest.mark.usefixtures("sflowbase_config")
 class TestAgentId():
     """
     Add loopback0 ip as the agent id and check the samples are received with intended agent-id.
@@ -406,7 +408,7 @@ class TestAgentId():
     Add eth0 ip as the agent ip and check the samples are received with intended agent-id.
     """
 
-    def testNonDefaultAgent(self, sflowbase_config, duthost, partial_ptf_runner):
+    def testNonDefaultAgent(self, duthost, partial_ptf_runner):
         agent_ip = var['lo_ip']
         duthost.shell(" config sflow agent-id del")
         duthost.shell(" config sflow agent-id  add Loopback0")
@@ -417,7 +419,7 @@ class TestAgentId():
               active_collectors="['collector0','collector1']" )
 
 
-    def testDelAgent(self, sflowbase_config, duthost, partial_ptf_runner):
+    def testDelAgent(self, duthost, partial_ptf_runner):
         duthost.shell(" config sflow agent-id del")
         verify_show_sflow(duthost,status='up',agent_id='default')
         time.sleep(5)
@@ -427,7 +429,7 @@ class TestAgentId():
               agent_id=var['lo_ip'],
               active_collectors="['collector0','collector1']" )
 
-    def testAddAgent(self, sflowbase_config, duthost, partial_ptf_runner):
+    def testAddAgent(self, duthost, partial_ptf_runner):
         agent_ip = var['mgmt_ip']
         duthost.shell(" config sflow agent-id  add  eth0")
         verify_show_sflow(duthost,status='up',agent_id='eth0')
@@ -463,7 +465,7 @@ class TestReboot():
               active_collectors="['collector0','collector1']" )
 
 
-    def testRebootSflowDisable(self, sflowbase_config, config_sflow_agent, duthost, localhost, partial_ptf_runner, ptfhost):
+    def testRebootSflowDisable(self, sflowbase_config, duthost, localhost, partial_ptf_runner, ptfhost):
         config_sflow(duthost,sflow_status='disable')
         verify_show_sflow(duthost,status='down')
         partial_ptf_runner(
