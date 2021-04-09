8fb944a9ffec7a748caeac5df4ad4c22180d34a2
Lawrence Lee <lawlee@microsoft.com>
2021-04-09 22:30:08 +0000

    [dualtor]: Increase allowed disruption time
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/dualtor/test_tor_bgp_failure.py
+++ tests/dualtor/test_tor_bgp_failure.py
@@ -7,6 +7,7 @@ from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_port
 from tests.common.dualtor.tor_failure_utils import kill_bgpd                                                                                    # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import run_icmp_responder, run_garp_service, copy_ptftests_directory, change_mac_addresses             # lgtm[py/unused-import]
 from tests.common.dualtor.tunnel_traffic_utils import tunnel_traffic_monitor
+from tests.common.dualtor.constants import MUX_SIM_ALLOWED_DISRUPTION_SEC
 
 pytestmark = [
     pytest.mark.topology("dualtor")
