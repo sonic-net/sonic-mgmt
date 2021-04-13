cb401ec47297352f7b5d9213749290e638a9813a
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2021-04-13 10:06:18 +0800

    [dualtor] Implement dualtor T1 -> Standby Tor orchagent  test cases. (#3110)
    * Implement dualtor T1 -> Standby Tor test cases.
    
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/dualtor/conftest.py
+++ tests/dualtor/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import logging
 import time
 
-from tests.common.dualtor.dual_tor_utils import get_crm_nexthop_counter, lower_tor_host # lgtm[py/unused-import]
+from tests.common.dualtor.dual_tor_utils import get_crm_nexthop_counter # lgtm[py/unused-import]
 from tests.common.helpers.assertions import pytest_assert as py_assert
 from tests.common.fixtures.ptfhost_utils import run_garp_service
 
@@ -12,27 +12,32 @@ CRM_DEFAULT_POLL_INTERVAL = 300
 
 
 @pytest.fixture
-def set_crm_polling_interval(lower_tor_host):
+def set_crm_polling_interval(rand_selected_dut):
     """
     A function level fixture to set crm polling interval to 1 second
     """
     wait_time = 2
-    lower_tor_host.command("crm config polling interval {}".format(CRM_POLL_INTERVAL))
+    logging.info("Setting crm polling interval to {} seconds".format(CRM_POLL_INTERVAL))
+    rand_selected_dut.command("crm config polling interval {}".format(CRM_POLL_INTERVAL))
     logging.info("Waiting {} sec for CRM counters to become updated".format(wait_time))
     time.sleep(wait_time)
     yield
-    lower_tor_host.command("crm config polling interval {}".format(CRM_DEFAULT_POLL_INTERVAL))
+    logging.info("Setting crm polling interval to {} seconds".format(CRM_DEFAULT_POLL_INTERVAL))
+    rand_selected_dut.command("crm config polling interval {}".format(CRM_DEFAULT_POLL_INTERVAL))
 
 
 @pytest.fixture
-def verify_crm_nexthop_counter_not_increased(lower_tor_host):
+def verify_crm_nexthop_counter_not_increased(rand_selected_dut, set_crm_polling_interval):
     """
     A function level fixture to verify crm nexthop counter not increased
     """
-    original_counter = get_crm_nexthop_counter(lower_tor_host)
+    original_counter = get_crm_nexthop_counter(rand_selected_dut)
+    logging.info("Before test: crm nexthop counter = {}".format(original_counter))
     yield
-    diff = get_crm_nexthop_counter(lower_tor_host) - original_counter
-    py_assert(diff == 0, "crm nexthop counter is increased by {}.".format(diff))
+    time.sleep(CRM_POLL_INTERVAL)
+    diff = get_crm_nexthop_counter(rand_selected_dut) - original_counter
+    logging.info("Before test: crm nexthop counter = {}".format(original_counter + diff))
+    py_assert(diff <= 0, "crm nexthop counter is increased by {}.".format(diff))
 
 
 def pytest_addoption(parser):
@@ -49,6 +54,7 @@ def pytest_addoption(parser):
         type=int,
         help="The number of iterations for mux stress test"
     )
+
 
 @pytest.fixture(scope="module", autouse=True)
 def common_setup_teardown(request, tbinfo):
