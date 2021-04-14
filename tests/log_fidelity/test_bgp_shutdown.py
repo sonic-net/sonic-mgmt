8dabd1ee2507235dbcaba01db4456c3a4e6ac70f
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2021-04-14 15:38:43 -0700

    Update constants name and set expect_regex directly to expected log

--- tests/log_fidelity/test_bgp_shutdown.py
+++ tests/log_fidelity/test_bgp_shutdown.py
@@ -9,21 +9,21 @@ pytestmark = [
     pytest.mark.topology('any')
 ]
 
-expected_log = "admin state is set to 'down'"
-defined_action = "sudo config bgp shutdown all"
+EXPECTED_LOG = "admin state is set to 'down'"
+BGP_DOWN_EXPECTED_LOG_MESSAGE = "sudo config bgp shutdown all"
 
 def test_bgp_shutdown(duthosts, rand_one_dut_hostname):
     duthost=duthosts[rand_one_dut_hostname]
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="bgp_shutdown")
-    loganalyzer.expect_regex=[]
+    loganalyzer.expect_regex=[EXPECTED_LOG]
 
     try:
-        loganalyzer.expect_regex.append(expected_log)
         marker=loganalyzer.init()
-        duthost.command(defined_action)
+        duthost.command(BGP_DOWN_EXPECTED_LOG_MESSAGE)
         logger.info("check for expected log in syslog")
         loganalyzer.analyze(marker)
 
     except LogAnalyzerError as err:
         logger.error("Unable to find expected log in syslog")
         raise err
+        
