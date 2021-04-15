28586208e5c6df3bb375f17903016268443efb69
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2021-04-14 17:09:22 -0700

    Added BGP start up command to return DUT to pretest state

--- tests/log_fidelity/test_bgp_shutdown.py
+++ tests/log_fidelity/test_bgp_shutdown.py
@@ -9,21 +9,24 @@ pytestmark = [
     pytest.mark.topology('any')
 ]
 
-EXPECTED_LOG = "admin state is set to 'down'"
-BGP_DOWN_EXPECTED_LOG_MESSAGE = "sudo config bgp shutdown all"
+BGP_DOWN_EXPECTED_LOG_MESSAGE = "admin state is set to 'down'"
+BGP_DOWN_COMMAND = "sudo config bgp shutdown all"
+BGP_UP_COMMAND = "sudo config bgp shutdown all"
 
 def test_bgp_shutdown(duthosts, rand_one_dut_hostname):
     duthost=duthosts[rand_one_dut_hostname]
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="bgp_shutdown")
-    loganalyzer.expect_regex=[EXPECTED_LOG]
+    loganalyzer.expect_regex=[BGP_DOWN_EXPECTED_LOG_MESSAGE]
 
     try:
         marker=loganalyzer.init()
-        duthost.command(BGP_DOWN_EXPECTED_LOG_MESSAGE)
-        logger.info("Check for expected log {} in syslog".format(EXPECTED_LOG))
+        duthost.command(BGP_DOWN_COMMAND)
+        logger.info("Check for expected log {} in syslog".format(BGP_DOWN_EXPECTED_LOG_MESSAGE))
         loganalyzer.analyze(marker)
 
     except LogAnalyzerError as err:
         logger.error("Unable to find expected log in syslog")
         raise err
         
+    finally:
+        duthost.command(BGP_UP_COMMAND)
