00188a0670e9f8202cd246a4c6ef1a55e257bb7d
falodiya <renu.falodiy@nokia.com>
2021-04-15 21:49:12 -0400

    removed unused variable

--- tests/lldp/test_lldp.py
+++ tests/lldp/test_lldp.py
@@ -47,7 +47,6 @@ def test_lldp_neighbor(duthosts, enum_rand_one_per_hwsku_frontend_hostname, loca
     res = duthost.shell("docker exec -i lldp lldpcli show chassis | grep \"SysDescr:\" | sed -e 's/^\\s*SysDescr:\\s*//g'")
     dut_system_description = res['stdout']
     lldpctl_facts = duthost.lldpctl_facts(asic_instance_id=enum_frontend_asic_index, skip_interface_pattern_list=["eth0", "Ethernet-BP", "Ethernet-IB"])['ansible_facts']
-    host_facts  = duthost.setup()['ansible_facts']
     config_facts = duthost.asic_instance(enum_frontend_asic_index).config_facts(host=duthost.hostname, source="running")['ansible_facts']
  
     nei_meta = config_facts.get('DEVICE_NEIGHBOR_METADATA', {})
