ba85ea14d44ac23b376d5ab457abf51f6c4969f4
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2021-04-19 12:17:49 -0700

    Allow future validation by redefining the input elements

--- tests/log_fidelity/test_bgp_shutdown.py
+++ tests/log_fidelity/test_bgp_shutdown.py
@@ -9,24 +9,37 @@ pytestmark = [
     pytest.mark.topology('any')
 ]
 
-BGP_DOWN_EXPECTED_LOG_MESSAGE = "admin state is set to 'down'"
-BGP_DOWN_COMMAND = "sudo config bgp shutdown all"
-BGP_UP_COMMAND = "sudo config bgp startup all"
+@pytest.fixture
+def trigger_action():
+    BGP_DOWN_COMMAND = "sudo config bgp shutdown all"
+    return BGP_DOWN_COMMAND
 
-def test_bgp_shutdown(duthosts, rand_one_dut_hostname):
+@pytest.fixture
+def bgp_shutdown_log():
+    BGP_DOWN_EXPECTED_LOG_MESSAGE = "admin state is set to 'down'"
+    return BGP_DOWN_EXPECTED_LOG_MESSAGE
+
+@pytest.fixture
+def restore_action():
+    BGP_UP_COMMAND = "sudo config bgp startup all"
+    return BGP_UP_COMMAND
+
+def test_check_syslog(duthosts, rand_one_dut_hostname, bgp_shutdown_log, trigger_action, restore_action):
     duthost=duthosts[rand_one_dut_hostname]
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="bgp_shutdown")
-    loganalyzer.expect_regex=[BGP_DOWN_EXPECTED_LOG_MESSAGE]
+    loganalyzer.expect_regex=[]
 
     try:
+        loganalyzer.expect_regex.append(bgp_shutdown_log)
         marker=loganalyzer.init()
-        duthost.command(BGP_DOWN_COMMAND)
-        logger.info("Check for expected log {} in syslog".format(BGP_DOWN_EXPECTED_LOG_MESSAGE))
+        duthost.command(trigger_action)
+        logger.info("Check for expected log in syslog")
         loganalyzer.analyze(marker)
 
     except LogAnalyzerError as err:
         logger.error("Unable to find expected log in syslog")
         raise err
-        
+
     finally:
-        duthost.command(BGP_UP_COMMAND)
+        duthost.command(restore_action)
+        
