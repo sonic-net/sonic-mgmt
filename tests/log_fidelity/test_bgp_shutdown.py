4112d1075fcc3a1bba4d81d3f46c5b8d13f9ac4e
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2021-04-26 16:40:54 -0700

    Fixed typo and "sudo" is not needed for ansible

--- tests/log_fidelity/test_bgp_shutdown.py
+++ tests/log_fidelity/test_bgp_shutdown.py
@@ -21,7 +21,7 @@ def check_syslog(duthost, prefix, trigger_action, expected_log, restore_action):
 
     except LogAnalyzerError as err:
         logger.error("Unable to find expected log in syslog")
-        raise error
+        raise err
 
     finally:
         duthost.command(restore_action)
@@ -30,8 +30,8 @@ def test_bgp_shutdown(duthosts, rand_one_dut_hostname):
     duthost=duthosts[rand_one_dut_hostname]
 
     BGP_DOWN_EXPECTED_LOG_MESSAGE = "admin state is set to 'down'"
-    BGP_DOWN_COMMAND = "sudo config bgp shutdown all"
-    BGP_UP_COMMAND = "sudo config bgp startup all"
+    BGP_DOWN_COMMAND = "config bgp shutdown all"
+    BGP_UP_COMMAND = "config bgp startup all"
 
     check_syslog(duthost, "bgp_shutdown", BGP_DOWN_COMMAND, BGP_DOWN_EXPECTED_LOG_MESSAGE, BGP_UP_COMMAND)
     
