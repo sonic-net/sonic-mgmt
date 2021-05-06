43e5c53bc77d7a3d0a021ec8ec397d9259bf5466
Shi Su <ssu@microsoft.com>
2021-05-06 03:07:19 +0000

    Address review comments

--- tests/route/test_static_route.py
+++ tests/route/test_static_route.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import ipaddress
 from tests.common.utilities import wait_until
+from tests.common import config_reload
 import ptf.testutils as testutils
 import ptf.mask as mask
 import ptf.packet as packet
@@ -60,18 +61,24 @@ def generate_and_verify_traffic(duthost, ptfadapter, ip_dst, expected_ports, ipv
     testutils.send(ptfadapter, 5, pkt)
     testutils.verify_packet_any_port(ptfadapter, exp_pkt, ports=expected_ports)
 
-def run_static_route_test(duthost, ptfadapter, ptfhost, prefix, nexthop_addrs, prefix_len, nexthop_devs, ipv6=False):
+def run_static_route_test(duthost, ptfadapter, ptfhost, prefix, nexthop_addrs, prefix_len, nexthop_devs, ipv6=False, config_reload_test=False):
     # Add ipaddresses in ptf
     add_ipaddr(ptfhost, nexthop_addrs, prefix_len, nexthop_devs, ipv6=ipv6)
 
     try:
         # Add static route
         duthost.shell("sonic-db-cli CONFIG_DB hmset 'STATIC_ROUTE|{}' nexthop {}".format(prefix, ",".join(nexthop_addrs)))
-        time.sleep(2)
+        time.sleep(5)
 
         # Check traffic get forwarded to the nexthop
         ip_dst = str(ipaddress.ip_network(unicode(prefix))[1])
         generate_and_verify_traffic(duthost, ptfadapter, ip_dst, nexthop_devs, ipv6=ipv6)
+
+        # Config save and reload if specified
+        if config_reload_test:
+            duthost.shell('config save -y')
+            config_reload(duthost)
+            generate_and_verify_traffic(duthost, ptfadapter, ip_dst, nexthop_devs, ipv6=ipv6)
 
     finally:
         # Remove static route
@@ -80,29 +87,47 @@ def run_static_route_test(duthost, ptfadapter, ptfhost, prefix, nexthop_addrs, p
         # Delete ipaddresses in ptf
         del_ipaddr(ptfhost, nexthop_addrs, prefix_len, nexthop_devs, ipv6=ipv6)
 
+        # Config save if the saved config_db was updated
+        if config_reload_test:
+            duthost.shell('config save -y')
+
 def get_vlan_info(duthost, tbinfo, ipv6=False):
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
     vlan_intf = mg_facts['minigraph_vlan_interfaces'][1 if ipv6 else 0]
     prefix_len = vlan_intf['prefixlen']
     vlan_subnet = ipaddress.ip_network(vlan_intf['subnet'])
-    return prefix_len, vlan_subnet
+    vlan_ports = mg_facts['minigraph_vlans'][mg_facts['minigraph_vlan_interfaces'][1 if ipv6 else 0]['attachto']]['members']
+    vlan_ptf_ports = [mg_facts['minigraph_ptf_indices'][port] for port in vlan_ports]
+    return prefix_len, vlan_subnet, vlan_ptf_ports
 
 def test_static_route(duthost, ptfadapter, ptfhost, tbinfo):
-    prefix_len, vlan_subnet = get_vlan_info(duthost, tbinfo)
+    prefix_len, vlan_subnet, vlan_ptf_ports = get_vlan_info(duthost, tbinfo)
     run_static_route_test(duthost, ptfadapter, ptfhost, "1.1.1.0/24", 
-        [str(vlan_subnet[11])], prefix_len, [1])
+        [str(vlan_subnet[11])], prefix_len, [vlan_ptf_ports[0]])
 
 def test_static_route_ecmp(duthost, ptfadapter, ptfhost, tbinfo):
-    prefix_len, vlan_subnet = get_vlan_info(duthost, tbinfo)
+    prefix_len, vlan_subnet, vlan_ptf_ports = get_vlan_info(duthost, tbinfo)
+    if len(vlan_ptf_ports) >= 3:
+        nexthops = [str(vlan_subnet[20 + idx]) for idx in range(3)]
+        intfs = vlan_ptf_ports[0:3]
+    else:
+        nexthops = [str(vlan_subnet[20 + idx]) for idx in range(len(vlan_ptf_ports))]
+        intfs = vlan_ptf_ports[0:len(vlan_ptf_ports)]
     run_static_route_test(duthost, ptfadapter, ptfhost, "2.2.2.0/24", 
-        [str(vlan_subnet[21]), str(vlan_subnet[22]), str(vlan_subnet[23])], prefix_len, [1, 2, 3])
+        nexthops, prefix_len, intfs, config_reload_test=True)
 
 def test_static_route_ipv6(duthost, ptfadapter, ptfhost, tbinfo):
-    prefix_len, vlan_subnet = get_vlan_info(duthost, tbinfo, ipv6=True)
+    prefix_len, vlan_subnet, vlan_ptf_ports = get_vlan_info(duthost, tbinfo, ipv6=True)
     run_static_route_test(duthost, ptfadapter, ptfhost, "2000:1::/64", 
-        [str(vlan_subnet[11])], prefix_len, [1], ipv6=True)
+        [str(vlan_subnet[11])], prefix_len, [vlan_ptf_ports[0]], ipv6=True)
 
 def test_static_route_ecmp_ipv6(duthost, ptfadapter, ptfhost, tbinfo):
-    prefix_len, vlan_subnet = get_vlan_info(duthost, tbinfo, ipv6=True)
+    prefix_len, vlan_subnet, vlan_ptf_ports = get_vlan_info(duthost, tbinfo, ipv6=True)
+    if len(vlan_ptf_ports) >= 3:
+        nexthops = [str(vlan_subnet[20 + idx]) for idx in range(3)]
+        intfs = vlan_ptf_ports[0:3]
+    else:
+        nexthops = [str(vlan_subnet[20 + idx]) for idx in range(len(vlan_ptf_ports))]
+        intfs = vlan_ptf_ports[0:len(vlan_ptf_ports)]
     run_static_route_test(duthost, ptfadapter, ptfhost, "2000:2::/64", 
-        [str(vlan_subnet[21]), str(vlan_subnet[22]), str(vlan_subnet[23])], prefix_len, [1, 2, 3], ipv6=True)
+        nexthops, prefix_len, intfs, ipv6=True, config_reload_test=True)
