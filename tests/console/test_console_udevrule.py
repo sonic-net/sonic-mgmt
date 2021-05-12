93d0c3be1924603e54f51cf7bdb16c9797ffe3d4
Blueve <672454911@qq.com>
2021-05-13 07:59:29 +0800

    [console] Add udevrule port mapping test for console switch (#3468)
    * [console] Add udevrule port mapping test for console switch
    * Fix issue where the test failed unexpected if no device found
    
    Signed-off-by: Jing Kan jika@microsoft.com

---
+++ tests/console/test_console_udevrule.py
@@ -0,0 +1,22 @@
+import pytest
+
+from tests.common.helpers.assertions import pytest_assert
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+def test_console_port_mapping(duthost):
+    """
+    Test udev rule are working as expect.
+    Verify C0-(1-48) are presented in DUT
+    """
+    out = duthost.shell('ls /dev/C0-*', module_ignore_errors=True)['stdout']
+    ttys = set(out.split())
+    pytest_assert(len(ttys) > 0, "No console tty devices been created by udev rule")
+
+    for i in range(1, 49):
+        expected_console_tty = "/dev/C0-{}".format(i)
+        pytest_assert(
+            expected_console_tty in ttys,
+            "Expected console device [{}] not found.".format(expected_console_tty))
