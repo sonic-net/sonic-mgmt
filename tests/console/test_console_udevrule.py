31bc436df84ef556e9b361cb65f24356a57a190e
Blueve <672454911@qq.com>
2021-05-12 06:45:28 +0000

    Fix issue where the test failed unexpected if no device found

--- tests/console/test_console_udevrule.py
+++ tests/console/test_console_udevrule.py
@@ -11,14 +11,12 @@ def test_console_port_mapping(duthost):
     Test udev rule are working as expect.
     Verify C0-(1-48) are presented in DUT
     """
-    out = duthost.shell('ls /dev/C0-*')['stdout']
-    pytest_assert(
-        "No such file or directory" not in out,
-        "No console tty devices been created by udev rule")
-
+    out = duthost.shell('ls /dev/C0-*', module_ignore_errors=True)['stdout']
     ttys = set(out.split())
+    pytest_assert(len(ttys) > 0, "No console tty devices been created by udev rule")
+
     for i in range(1, 49):
-        expected_virtual_tty = "/dev/C0-{}".format(i)
+        expected_console_tty = "/dev/C0-{}".format(i)
         pytest_assert(
-            expected_virtual_tty in ttys,
-            "Expected console device [{}] not found.".format(expected_virtual_tty))
+            expected_console_tty in ttys,
+            "Expected console device [{}] not found.".format(expected_console_tty))
