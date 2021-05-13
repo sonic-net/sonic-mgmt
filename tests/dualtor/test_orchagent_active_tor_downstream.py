1562eb10c6e5889e0333491a916282b17e11c419
Vaibhav Hemant Dixit <vaibhav.dixit@microsoft.com>
2021-05-13 14:59:20 -0700

    [dualtor][orchagent] ECMP hashing with multiple nexthops and standby mux toggle (#3428)
    dualtor-orchagent testcase: ECMP hashing with multiple nexthops and standby mux toggle.
    Added a route with 4 servers as nexthop. Now switch (to standby mux) one-by-one every link connecting to these nexthops.
    After every mux toggle, check if the packets are evenly distributed towards downlinks (active muxes) and uplinks (standby muxes).
    The test is failing due to unequal hashing towards uplinks and downlinks.

--- tests/dualtor/test_orchagent_active_tor_downstream.py
+++ tests/dualtor/test_orchagent_active_tor_downstream.py
@@ -2,7 +2,6 @@ import contextlib
 import logging
 import pytest
 import random
-
 from ptf import testutils
 from tests.common.dualtor.dual_tor_mock import *
 from tests.common.dualtor.dual_tor_utils import dualtor_info
@@ -10,6 +9,10 @@ from tests.common.dualtor.dual_tor_utils import flush_neighbor
 from tests.common.dualtor.dual_tor_utils import get_t1_ptf_ports
 from tests.common.dualtor.dual_tor_utils import crm_neighbor_checker
 from tests.common.dualtor.dual_tor_utils import build_packet_to_server
+from tests.common.dualtor.dual_tor_utils import get_interface_server_map
+from tests.common.dualtor.dual_tor_utils import check_nexthops_balance
+from tests.common.dualtor.dual_tor_utils import add_nexthop_routes, remove_static_routes
+from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports
 from tests.common.dualtor.server_traffic_utils import ServerTrafficMonitor
 from tests.common.dualtor.tunnel_traffic_utils import tunnel_traffic_monitor
 from tests.common.fixtures.ptfhost_utils import run_icmp_responder
@@ -78,3 +81,51 @@ def test_active_tor_remove_neighbor_downstream_active(
     )
     with crm_neighbor_checker(tor), tunnel_monitor, server_traffic_monitor:
         testutils.send(ptfadapter, int(ptf_t1_intf.strip("eth")), pkt, count=10)
+
+
+def test_downstream_ecmp_nexthops(
+    ptfadapter,
+    rand_selected_dut, tbinfo,
+    toggle_all_simulator_ports,
+    tor_mux_intfs
+    ):
+    dst_server_ipv4 = "1.1.1.2"
+    nexthops_count = 4
+    set_mux_state(rand_selected_dut, tbinfo, 'active', tor_mux_intfs, toggle_all_simulator_ports)
+
+    iface_server_map = get_interface_server_map(rand_selected_dut, nexthops_count)
+    interface_to_server = dict()
+    for interface, servers in iface_server_map.items():
+        interface_to_server[interface] = servers['server_ipv4'].split("/")[0]
+
+    nexthop_servers = list(interface_to_server.values())
+    nexthop_interfaces = list(interface_to_server.keys())
+
+    logging.info("Add route with four nexthops, where four muxes are active")
+    add_nexthop_routes(rand_selected_dut, dst_server_ipv4, nexthops=nexthop_servers[0:nexthops_count])
+
+    logging.info("Verify traffic to this route destination is distributed to four server ports")
+    check_nexthops_balance(rand_selected_dut, ptfadapter, dst_server_ipv4, tbinfo,
+        nexthop_interfaces, nexthops_count)
+
+    ### Sequentially set four mux states to standby
+    for index, interface in enumerate(nexthop_interfaces):
+        uplink_ports_active = index + 1
+        logging.info("Simulate {} mux state change to Standby".format(nexthop_servers[index]))
+        set_mux_state(rand_selected_dut, tbinfo, 'standby', [interface], toggle_all_simulator_ports)
+        logging.info("Verify traffic to this route destination is distributed to"\
+            " {} server ports and {} tunnel nexthop".format(
+                nexthops_count-uplink_ports_active, uplink_ports_active))
+        check_nexthops_balance(rand_selected_dut, ptfadapter, dst_server_ipv4, tbinfo,
+            nexthop_interfaces[uplink_ports_active:nexthops_count], nexthops_count)
+
+    ### Revert two mux states to active
+    for index, interface in reversed(list(enumerate(nexthop_interfaces))):
+        logging.info("Simulate {} mux state change back to Active".format(nexthop_servers[index]))
+        set_mux_state(rand_selected_dut, tbinfo, 'active', [interface], toggle_all_simulator_ports)
+        logging.info("Verify traffic to this route destination is distributed to"\
+            " {} server ports and {} tunnel nexthop".format(nexthops_count-index, index))
+        check_nexthops_balance(rand_selected_dut, ptfadapter, dst_server_ipv4, tbinfo,
+            nexthop_interfaces[index:nexthops_count], nexthops_count)
+    ### Remove the nexthop route
+    remove_static_routes(rand_selected_dut, dst_server_ipv4)
