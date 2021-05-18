017dd2a0f5ee15f065979e9e1e453e3775fa3cc5
Blueve <672454911@qq.com>
2021-05-18 10:11:14 +0800

    [console][reverse ssh] Add reverse ssh test(conn) for console switch (#3487)
    * [console][reverse ssh] Add reverse ssh test(conn) for console switch
    * Use pexpect directly
    * Add parametrize
    
    Signed-off-by: Jing Kan jika@microsoft.com

---
+++ tests/console/test_console_reversessh.py
@@ -0,0 +1,49 @@
+import pytest
+import pexpect
+import logging
+
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.utilities import wait_until
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+@pytest.mark.parametrize("target_line", ["1", "2"])
+def test_console_reversessh_connectivity(duthost, creds, target_line):
+    """
+    Test reverse SSH are working as expect.
+    Verify serial session is available after connect DUT via reverse SSH
+    """
+    dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    dutuser = creds['sonicadmin_user']
+    dutpass = creds['sonicadmin_password']
+
+    pytest_assert(
+        check_target_line_status(duthost, target_line, "IDLE"),
+        "Target line {} is busy before reverse SSH session start".format(target_line))
+
+    ressh_user = "{}:{}".format(dutuser, target_line)
+    try:
+        client = pexpect.spawn('ssh {}@{}'.format(ressh_user, dutip))
+        client.expect('[Pp]assword:')
+        client.sendline(dutpass)
+
+        # Check the console line state again
+        pytest_assert(
+            check_target_line_status(duthost, target_line, "BUSY"),
+            "Target line {} is idle while reverse SSH session is up".format(target_line))
+
+        # Send escape sequence to exit reverse SSH session
+        client.sendcontrol('a')
+        client.sendcontrol('x')
+    except Exception as e:
+        pytest.fail("Not able to do reverse SSH to remote host via DUT")
+
+    pytest_assert(
+        wait_until(10, 1, check_target_line_status, duthost, target_line, "IDLE"),
+        "Target line {} is busy after exited reverse SSH session".format(target_line))
+
+def check_target_line_status(duthost, line, expect_status):
+    console_facts = duthost.console_facts()['ansible_facts']['console_facts']
+    return console_facts['lines'][line]['state'] == expect_status
