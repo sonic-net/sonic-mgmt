2a1d121e8d5921acf86b9f565b07354e24739ab0
rraghav <rraghav@sonic-ucs-m3-1.cisco.com>
2021-05-22 15:37:59 -0700

    git automatio

---
+++ gitAutomation/githubautomation.py
@@ -0,0 +1,51 @@
+import requests
+import csv
+from pprint import pprint
+import pandas as pd
+import json
+
+testlist = dict()
+for i in range(1,50):
+    params = {"state": "all","page": i}
+    token = "b046c73a5969f9fe3f5263a900e3c7c43bf6352d"
+    url="https://wwwin-github.cisco.com/api/v3/repos/whitebox/sonic-buildimage/issues"
+    headers = {'Authorization': token}
+    r = requests.get(url, headers=headers, params=params)
+    if r.status_code != 200:
+        raise Exception(r.text)
+    data = {}
+    for bug in r.json():
+        with open('bugslist.csv', 'a') as bugslist:
+            try:
+                fieldnames = ['Title', 'Created', 'Closed', 'State', 'BugID', 'RaisedBy', 'AssignedTo', 'Labels']
+                writer = csv.DictWriter(bugslist, fieldnames=fieldnames)
+                writer.writerow({'Title': bug['title'], 'Created': bug['created_at'], 'Closed':bug['closed_at'],'State': bug['state'],
+                                 'BugID': bug['number'], 'RaisedBy': bug['user']['login'],
+                                 'AssignedTo': bug['assignee']['login'], 'Labels':[ele['name'].encode('ascii') for ele in bug['labels']] })
+            except:
+                pass
+
+        # Creating JSON File
+        try:
+            bugDict = {"BugID":bug['number'], 'Title': bug['title'], 'Created at': bug['created_at'], 'Closed at':bug['closed_at'],'State': bug['state'],
+                                      'RaisedBy': bug['user']['login'],
+                                     'AssignedTo': bug['assignee']['login'], 'Labels':[ele['name'] for ele in bug['labels']] }
+        except:
+            pass
+
+        data.update(bugDict)
+    with open('bugslist.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+
+
+#Creating Headers
+file = pd.read_csv("bugslist.csv")
+headerList=['Title', 'Created', 'Closed', 'State', 'BugID', 'RaisedBy', 'AssignedTo', 'Labels']
+file.to_csv("bugslist.csv", header=headerList, index=False)
+file2 = pd.read_csv("bugslist.csv")
+print(file2)
+
+
+
