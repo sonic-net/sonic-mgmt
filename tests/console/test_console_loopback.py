63164d8268e57d74ff9f82a73c3354b9db5324f5
Blueve <672454911@qq.com>
2021-05-24 11:02:29 +0800

    [console][loopback] Add loopback test(ping-pong) for console switch (#3517)
    * [console][loopback] Add loopback test(ping-pong) for console switch
    
    Signed-off-by: Jing Kan jika@microsoft.com

--- tests/console/test_console_loopback.py
+++ tests/console/test_console_loopback.py
@@ -36,34 +36,67 @@ def test_console_loopback_echo(duthost, creds, target_line):
         # Generate a random strings to send
         text = generate_random_string(packet_size)
         client.sendline(text)
-        index = client.expect_exact([text, pexpect.EOF, pexpect.TIMEOUT], timeout=timeout_sec)
-        if index == 1:
-            pytest.fail("Encounter early EOF during testing line {}".format(target_line))
-        elif index == 2:
-            pytest.fail("Not able to get echo in {}s".format(timeout_sec))
+        assert_expect_text(client, text, target_line, timeout_sec)
+    except Exception as e:
+        pytest.fail("Not able to communicate DUT via reverse SSH")
+
+@pytest.mark.parametrize("src_line,dst_line", [('17', '19'),
+                                               ('18', '20'),
+                                               ('21', '27'),
+                                               ('22', '28'),
+                                               ('23', '25'),
+                                               ('24', '26'),
+                                               ('29', '35'),
+                                               ('30', '36'),
+                                               ('31', '33'),
+                                               ('32', '34')])
+def test_console_loopback_pingpong(duthost, creds, src_line, dst_line):
+    """
+    Test data transfer are working as expect.
+    Verify data can go out through the console switch and come back through the console switch
+    """
+    dutip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
+    dutuser = creds['sonicadmin_user']
+    dutpass = creds['sonicadmin_password']
+
+    console_facts = duthost.console_facts()['ansible_facts']['console_facts']
+
+    if src_line not in console_facts['lines']:
+        pytest.skip("Source line {} has not configured".format(src_line))
+    if dst_line not in console_facts['lines']:
+        pytest.skip("Destination line {} has not configured".format(dst_line))
+
+    try:
+        sender   = create_ssh_client(dutip, "{}:{}".format(dutuser, src_line), dutpass)
+        receiver = create_ssh_client(dutip, "{}:{}".format(dutuser, dst_line), dutpass)
+
+        ensure_console_session_up(sender,   src_line)
+        ensure_console_session_up(receiver, dst_line)
+
+        sender.sendline('ping')
+        assert_expect_text(receiver, 'ping', dst_line)
+        receiver.sendline('pong')
+        assert_expect_text(sender, 'pong', src_line)
     except Exception as e:
         pytest.fail("Not able to communicate DUT via reverse SSH")
 
 def create_ssh_client(ip, user, pwd):
     # Set 'echo=False' is very important since pexpect will echo back all inputs to buffer by default
-    client = pexpect.spawn('ssh {}@{}'.format(user, ip), echo=False)
-
-    while True:
-        index = client.expect([
-            '[Pp]assword:',
-            'Are you sure you want to continue connecting (yes/no)?'])
-
-        if index == 0:
-            client.sendline(pwd)
-            return client
-        elif index == 1:
-            client.sendline('yes')
-        else:
-            raise Exception("Unexpect pattern encountered")
+    client = pexpect.spawn('ssh {}@{} -o StrictHostKeyChecking=no'.format(user, ip), echo=False)
+    client.expect('[Pp]assword:')
+    client.sendline(pwd)
+    return client
 
 def ensure_console_session_up(client, line):
     client.expect_exact('Successful connection to line [{}]'.format(line))
     client.expect_exact('Press ^A ^X to disconnect')
+
+def assert_expect_text(client, text, target_line, timeout_sec=0.1):
+    index = client.expect_exact([text, pexpect.EOF, pexpect.TIMEOUT], timeout=timeout_sec)
+    if index == 1:
+        pytest.fail("Encounter early EOF during testing line {}".format(target_line))
+    elif index == 2:
+        pytest.fail("Not able to get expected text in {}s".format(timeout_sec))
 
 def generate_random_string(length):
     letters = string.ascii_lowercase
