457777bf670fb2d20868f0e6b076ba7d72ee8b8a
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2021-05-30 15:00:47 +0800

    Add test cases for port auto negotiation feature (#3376)
    - Summary:
    Add test cases for port auto negotiation feature
    
    - What is the motivation for this PR?
    Add test cases for port auto negotiation feature
    
    - How did you do it?
    Add test case to verify "config interface advertised-speeds <interface_name> all"
    Add test case to verify "config interface advertised-speeds <interface_name> "
    Add test case to verify "config interface speed <interface_name> "
    
    - How did you verify/test it?
    Manually run the test case on Mellanox platform
    
    - Any platform specific information?
    N/A
    
    - Supported testbed topology if it's a new test case?
    N/A

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -1383,6 +1383,84 @@ default via fc00::1a dev PortChannel0004 proto 186 src fc00:1::32 metric 20  pre
                 pass
         return up_ip_ports
 
+    def get_supported_speeds(self, interface_name):
+        """Get supported speeds for a given interface
+
+        Args:
+            interface_name (str): Interface name
+
+        Returns:
+            list: A list of supported speed strings or None
+        """
+        cmd = 'sonic-db-cli STATE_DB HGET \"PORT_TABLE|{}\" \"{}\"'.format(interface_name, 'supported_speeds')
+        supported_speeds = self.shell(cmd)['stdout'].strip()
+        return None if not supported_speeds else supported_speeds.split(',')
+
+    def set_auto_negotiation_mode(self, interface_name, mode):
+        """Set auto negotiation mode for a given interface
+
+        Args:
+            interface_name (str): Interface name
+            mode (boolean): True to enable auto negotiation else disable
+
+        Returns:
+            boolean: False if the operation is not supported else True
+        """
+        cmd = 'config interface autoneg {} {}'.format(interface_name, 'enabled' if mode else 'disabled')
+        self.shell(cmd)
+        return True
+
+    def get_auto_negotiation_mode(self, interface_name):
+        """Get auto negotiation mode for a given interface
+
+        Args:
+            interface_name (str): Interface name
+
+        Returns:
+            boolean: True if auto negotiation mode is enabled else False. Return None if 
+            the auto negotiation mode is unknown or unsupported.
+        """
+        cmd = 'sonic-db-cli APPL_DB HGET \"PORT_TABLE:{}\" \"{}\"'.format(interface_name, 'autoneg')
+        mode = self.shell(cmd)['stdout'].strip()
+        if not mode:
+            return None
+        return True if mode == 'on' else False
+
+    def set_speed(self, interface_name, speed):
+        """Set interface speed according to the auto negotiation mode. When auto negotiation mode
+        is enabled, set the advertised speeds; otherwise, set the force speed.
+
+        Args:
+            interface_name (str): Interface name
+            speed (str): SONiC style interface speed. E.g, 1G=1000, 10G=10000, 100G=100000. If the speed
+            is None and auto negotiation mode is enabled, it sets the advertised speeds to all supported
+            speeds.
+
+        Returns:
+            boolean: True if success. Usually, the method return False only if the operation
+            is not supported or failed.
+        """
+        auto_neg_mode = self.get_auto_negotiation_mode(interface_name)
+        if not auto_neg_mode:
+            cmd = 'config interface speed {} {}'.format(interface_name, speed)
+        else:
+            cmd = 'config interface advertised-speeds {} {}'.format(interface_name, speed)
+        self.shell(cmd)
+        return True
+
+    def get_speed(self, interface_name):
+        """Get interface speed
+
+        Args:
+            interface_name (str): Interface name
+
+        Returns:
+            str: SONiC style interface speed value. E.g, 1G=1000, 10G=10000, 100G=100000.
+        """
+        cmd = 'sonic-db-cli APPL_DB HGET \"PORT_TABLE:{}\" \"{}\"'.format(interface_name, 'speed')
+        speed = self.shell(cmd)['stdout'].strip()
+        return speed
+
     def get_rsyslog_ipv4(self):
         if not self.is_multi_asic:
             return "127.0.0.1"
