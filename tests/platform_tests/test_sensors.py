3aab77eb85d0414a168617b197910cc5d616f3a1
Kebo Liu <kebol@nvidia.com>
2021-05-30 16:23:56 +0800

    [Mellanox] update sensor test case for MSN4600 A0 and A1 system (#3547)
    - Summary:
    Update sku-sensors-data.yml and test_sensors.py to support Mellanox MSN4600 A0 and A1 system.
    
    - What is the motivation for this PR?
    To have sensor test support Mellanox MSN4600 A0 and A1 system.
    
    - How did you do it?
    Add MSN4600 A0 and A1 sensor data to sku-sensors-data.yml
    Update test_sensors.py to differentiate A0 and A1 system.
    
    - How did you verify/test it?
    Run sensor test against Mellanox 4600 A0 and A1 system.

--- tests/platform_tests/test_sensors.py
+++ tests/platform_tests/test_sensors.py
@@ -24,11 +24,12 @@ def test_sensors(duthosts, rand_one_dut_hostname, creds):
 
     logging.info("Sensor checks:\n{}".format(to_json(sensors_checks[platform])))
 
-    if platform == 'x86_64-mlnx_msn4700-r0':
+    # Special treatment for Mellanox platforms which have two different A0 and A1 types    
+    if platform in ['x86_64-mlnx_msn4700-r0', 'x86_64-mlnx_msn4600-r0']:
         # Check the hardware version and choose sensor conf data accordingly
         output = duthost.command('cat /run/hw-management/system/config1', module_ignore_errors=True)
         if output["rc"] == 0 and output["stdout"] == '1':
-            platform = 'x86_64-mlnx_msn4700-r0-a1'
+            platform = platform + '-a1'
 
     # Gather sensor facts
     sensors_facts = duthost.sensors_facts(checks=sensors_checks[platform])['ansible_facts']
