ed691f2086e031e2b363b0fe3ebb6bfd56611cdf
Petro Pikh <petrop@nvidia.com>
2021-06-02 14:06:40 +0300

    Added possibility to skip tests dynamically by GitHub issue
    Signed-off-by: Petro Pikh <petrop@nvidia.com>

--- tests/common/plugins/custom_skipif/__init__.py
+++ tests/common/plugins/custom_skipif/__init__.py
@@ -123,7 +123,7 @@ def prepare_checkers(skip_dict, current_platform):
     for skip_by in skip_dict:
         logger.debug('Importing dynamic skip module: {}'.format(skip_by))
         try:
-            skip_module = __import__(skip_by)
+            skip_module = __import__(skip_by)  # lgtm[py/unused-import]
             skip_module_obj = eval(
                 'skip_module.{}({}, {})'.format(skip_by, skip_dict[skip_by], extra_params))
             skip_checkers_list.append(skip_module_obj)
