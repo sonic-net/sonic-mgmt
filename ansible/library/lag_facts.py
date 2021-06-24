sonic-net/sonic-mgmt

commit 04e73e768e865a027bf48297573b5d5c95df40a5

[test_lag_2] Fix import error if multi_asic is not present (#3652)
If the switch is running an older image, and multi_asic is not present,
then use default values or don't call functions in that module

Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

How did you verify/test it?
Tested with an internal setup running a 201811 image, where multi_asic is not present.

ansible/library/lag_facts.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import json
+import sys
 from ansible.module_utils.basic import *
-from sonic_py_common import multi_asic
+try:
+    from sonic_py_common import multi_asic
+    NAMESPACE_LIST = multi_asic.get_namespace_list()
+except ImportError:
+    NAMESPACE_LIST = ['']
 DOCUMENTATION = '''
 ---
 module: lag_facts
@@ -43,7 +48,10 @@ class LagModule(object):
         self.get_po_names()
         for po,ns in self.lag_names.items():
             self.lags[po] = {}
-            ns_id = multi_asic.get_asic_id_from_name(ns) if ns else ''
+            if 'multi_asic' in sys.modules:
+                ns_id = multi_asic.get_asic_id_from_name(ns) if ns else ''
+            else:
+                ns_id = ''
             self.lags[po]['po_stats'] = self.get_po_status(po, ns_id)
             self.lags[po]['po_config'] = self.get_po_config(po, ns_id)
             self.lags[po]['po_intf_stat'] = self.get_po_intf_stat(po, ns)
@@ -55,15 +63,14 @@ class LagModule(object):
         '''
             Collect configured lag interface names
         '''
-        namespace_list = multi_asic.get_namespace_list()
-        for ns in namespace_list:
+        for ns in NAMESPACE_LIST:
             rt, out, err = self.module.run_command("sonic-cfggen -m /etc/sonic/minigraph.xml {} -v \"PORTCHANNEL.keys() | join(' ')\"".format('-n ' + ns if ns else ''))
             if rt != 0:
                 fail_msg="Command to retrieve portchannel names failed return=%d, out=%s, err=%s" %(rt, out, err)
                 self.module.fail_json(msg=fail_msg)
             else:
                 for po in out.split():
-                    if multi_asic.is_port_channel_internal(po):
+                    if 'multi_asic' in sys.modules and multi_asic.is_port_channel_internal(po):
                         continue
                     self.lag_names[po] = ns
         return

commit d39fe3a0a2858080f2bfee2755c6a6ca1783c565

[test_lag_2] Fix logic error in checking the wrong module name
This change is to fix a regression that got committed in
04e73e768e865a027bf48297573b5d5c95df40a5. It was checking to see that
the multi_asic python module was loaded or not (to determine if this is
running on an older image or current image. This is incorrect, and it
should have been checking for the sonic_py_common module instead.

Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

ansible/library/lag_facts.py
@@ -48,7 +48,7 @@ class LagModule(object):
         self.get_po_names()
         for po,ns in self.lag_names.items():
             self.lags[po] = {}
-            if 'multi_asic' in sys.modules:
+            if 'sonic_py_common' in sys.modules:
                 ns_id = multi_asic.get_asic_id_from_name(ns) if ns else ''
             else:
                 ns_id = ''
@@ -70,7 +70,7 @@ class LagModule(object):
                 self.module.fail_json(msg=fail_msg)
             else:
                 for po in out.split():
-                    if 'multi_asic' in sys.modules and multi_asic.is_port_channel_internal(po):
+                    if 'sonic_py_common' in sys.modules and multi_asic.is_port_channel_internal(po):
                         continue
                     self.lag_names[po] = ns
         return