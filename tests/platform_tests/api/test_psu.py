9a9565a5989f5fda0812ed682a3505fda67535c8
rawal <amol.rawal@nokia.com>
2021-07-08 22:39:40 -0400

    tests/platform_tests/api/test_chassis.py : modify regex for serial number to include '-'  tests/platform_tests/api/test_psu.py :  enhance test_led to skip test if psu does not support led based on information from platform.json  tests/platform_tests/cli/test_show_platform.py : change show_platform_psu_status to use enum_supervisor_dut_hostname instead of rand_one_dut_hostname

--- tests/platform_tests/api/test_psu.py
+++ tests/platform_tests/api/test_psu.py
@@ -298,7 +298,11 @@ class TestPsuApi(PlatformApiTestBase):
         psus_skipped = 0
         for psu_id in range(self.num_psus):
             name = psu.get_name(platform_api_conn, psu_id)
-            if name in self.psu_skip_list:
+            if duthost.facts.get("chassis").get("psus")[psu_id].get("led"):
+                led_support = duthost.facts.get("chassis").get("psus")[psu_id].get("led")
+                if led_support == "N/A":
+                    logger.info("led not supported for this psu {}".format(name))
+            elif name in self.psu_skip_list:
                 logger.info("skipping check for {}".format(name))
                 psus_skipped += 1
             else:
@@ -388,9 +392,16 @@ class TestPsuApi(PlatformApiTestBase):
             1: "normal",
             2: "off"
         }
-
+        supported_colors = []
         if self.num_psus == 0:
             pytest.skip("No psus found on device skipping for device {}".format(duthost))
+        
+        if duthost.facts.get("chassis").get("master_psu_led_color"):
+            supported_colors = duthost.facts.get("chassis").get("master_psu_led_color")
+            for index, colors in enumerate(LED_COLOR_TYPES):
+                for color in colors:
+                    if color not in supported_colors:
+                        LED_COLOR_TYPES[index].remove(color)
 
         for psu_id in range(self.num_psus):
             name = psu.get_name(platform_api_conn, psu_id)
