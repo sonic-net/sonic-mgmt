39fa8ffded6f2c5a6dbc47b0f38e07bdef534214
Andoni Sanguesa <t-asanguesa@microsoft.com>
2021-07-21 10:40:36 -0700

    fixed ptf down after http copy test

--- tests/http/test_http_copy.py
+++ tests/http/test_http_copy.py
@@ -21,8 +21,9 @@ def test_http_copy(duthosts, rand_one_dut_hostname, ptfhost):
 
     test_file_name = "test_file.bin"
 
-    # Copies http server file to ptf module_async=True
+    # Copies http server files to ptf
     ptfhost.copy(src="http/start_http_server.py", dest="/tmp/start_http_server.py")
+    ptfhost.copy(src="http/stop_http_server.py", dest="/tmp/stop_http_server.py")
     
     # Starts the http server on the ptf
     ptfhost.command("python /tmp/start_http_server.py", module_async=True)
@@ -42,9 +43,9 @@ def test_http_copy(duthosts, rand_one_dut_hostname, ptfhost):
     ptfhost.command(("dd if=/dev/urandom of=./{} count=1 bs=1000000000 iflag=fullblock".format(test_file_name)))
 
     # Ensure that file was downloaded
-    res = ptfhost.command("ls -ltr ./{}".format(test_file_name), module_ignore_errors=True)["rc"]
+    file_stat = ptfhost.stat(path="./{}".format(test_file_name))
 
-    pytest_assert(res==0, "Test file was not found on DUT after attempted http get")
+    pytest_assert(file_stat["stat"]["exists"], "Test file was not found on DUT after attempted http get")
 
     # Generate MD5 checksum to compare with the sent file
     output = ptfhost.command("md5sum ./{}".format(test_file_name))["stdout"]
@@ -54,9 +55,9 @@ def test_http_copy(duthosts, rand_one_dut_hostname, ptfhost):
     duthost.command("curl -O {}:{}/{}".format(ptf_ip, HTTP_PORT, test_file_name))
 
     # Validate file was received
-    res = duthost.command("ls -ltr ./{}".format(test_file_name), module_ignore_errors=True)["rc"]
+    file_stat = duthost.stat(path="./{}".format(test_file_name))
 
-    pytest_assert(res==0, "Test file was not found on DUT after attempted http get")
+    pytest_assert(file_stat["stat"]["exists"], "Test file was not found on DUT after attempted http get")
 
     # Get MD5 checksum of received file
     output = duthost.command("md5sum ./{}".format(test_file_name))["stdout"]
@@ -69,23 +70,20 @@ def test_http_copy(duthosts, rand_one_dut_hostname, ptfhost):
     duthost.command("sudo rm ./{}".format(test_file_name))
 
     # Confirm cleanup occured succesfuly
-    res = duthost.command("ls -ltr ./{}".format(test_file_name), module_ignore_errors=True)["rc"]
+    file_stat = duthost.stat(path="./{}".format(test_file_name))
 
-    pytest_assert(res!=0, "DUT container could not be cleaned.")
+    pytest_assert(not file_stat["stat"]["exists"], "DUT container could not be cleaned.")
 
     # Delete file off ptf
     ptfhost.command(("rm ./{}".format(test_file_name)))
 
     # Ensure that file was removed correctly
-    res = ptfhost.command("ls -ltr ./{}".format(test_file_name), module_ignore_errors=True)["rc"]
+    file_stat = ptfhost.stat(path="./{}".format(test_file_name))
 
-    pytest_assert(res!=0, "PTF container could not be cleaned.")
+    pytest_assert(not file_stat["stat"]["exists"], "PTF container could not be cleaned.")
 
     # Stops http server
-    output = ptfhost.command("netstat -nlp | grep :8080")["stdout"]
-    pid_and_program = output.split()[-2]
-    pid = pid_and_program.split("/")[0]
-    ptfhost.command("kill {}".format(pid))
+    ptfhost.command("python /tmp/stop_http_server.py")
 
     # Ensure that HTTP server was closed
     started = True
