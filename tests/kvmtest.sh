623c31611d90dd430487242fe5c1a73eb53c6f35
Guohan Lu <lguohan@gmail.com>
2021-08-08 10:07:05 -0700

    [kvmtest]: disable test_bgp_gr_helper_all_routes_preserved temporarily
    test seems flaky.
    
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -99,7 +99,7 @@ test_t0() {
     test_interfaces.py \
     arp/test_arp_dualtor.py \
     bgp/test_bgp_fact.py \
-    bgp/test_bgp_gr_helper.py \
+    bgp/test_bgp_gr_helper.py::test_bgp_gr_helper_routes_perserved \
     bgp/test_bgp_speaker.py \
     bgp/test_bgp_update_timer.py \
     cacl/test_ebtables_application.py \
@@ -247,7 +247,7 @@ if [ x$test_suite == x"t0" ]; then
 elif [ x$test_suite == x"t1-lag" ]; then
     test_t1_lag
 elif [ x$test_suite == x"multi-asic-t1-lag" ]; then
-    test_multi_asic_t1_lag 
+    test_multi_asic_t1_lag
 elif [ x$test_suite == x"t2" ]; then
     test_t2
 else
