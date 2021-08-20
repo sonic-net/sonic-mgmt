d9b9dd91ae5a62c7ce749ad0d2b84177415ec3fe
Blueve <672454911@qq.com>
2021-08-20 10:56:41 +0800

    [console] Fix issue where the reverse ssh failed due to invalid hostkey (#4077)
    Signed-off-by: Jing Kan jika@microsoft.com

--- tests/console/test_console_reversessh.py
+++ tests/console/test_console_reversessh.py
@@ -25,7 +25,7 @@ def test_console_reversessh_connectivity(duthost, creds, target_line):
 
     ressh_user = "{}:{}".format(dutuser, target_line)
     try:
-        client = pexpect.spawn('ssh {}@{}'.format(ressh_user, dutip))
+        client = pexpect.spawn('ssh {}@{} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'.format(ressh_user, dutip))
         client.expect('[Pp]assword:')
         client.sendline(dutpass)
 
