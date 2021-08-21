915e3a1c7c7099148ce085447131cd500ab15d14
Kevin Wang <65380078+kevinskwang@users.noreply.github.com>
2021-08-21 08:12:21 +0800

    [test_heartbeat_failure] skip the heartbeat test (#4095)
    What is the motivation for this PR?
    After this test is failed, then the subsequent dual-tor test cases will mostly fail as well. Need to figure out the reason why can't gurantee all interfaces to be toggled to active state. See detail on issue#8423.
    
    How did you do it?
    just skip this test before issue is addressed.
    
    Signed-off-by: Kevin Wang <shengkaiwang@microsoft.com>

--- tests/dualtor/test_heartbeat_failure.py
+++ tests/dualtor/test_heartbeat_failure.py
@@ -11,7 +11,8 @@ from tests.common.fixtures.ptfhost_utils import run_icmp_responder, run_garp_ser
 from tests.common.dualtor.constants import MUX_SIM_ALLOWED_DISRUPTION_SEC
 
 pytestmark = [
-    pytest.mark.topology("dualtor")
+    pytest.mark.topology("dualtor"),
+    pytest.mark.skip(reason="skipping until the mux toggle issue is addressed")
 ]
 
 
