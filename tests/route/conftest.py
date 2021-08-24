5848d8c67c756ba54448938012c75a0ab271abc6
Jibin Bao <jbao@nvidia.com>
2021-08-24 14:47:44 +0800

    remove change from conftest, since arp_responder is only used in static route tests.

--- tests/route/conftest.py
+++ tests/route/conftest.py
@@ -1,7 +1,3 @@
-import pytest
-from jinja2 import Template
-
-
 # Pytest configuration used by the route tests.
 def pytest_addoption(parser):
     # Add options to pytest that are used by route tests
@@ -10,10 +6,3 @@ def pytest_addoption(parser):
 
     route_group.addoption("--num_routes", action="store", default=10000, type=int,
                      help="Number of routes for add/delete")
-
-
-@pytest.fixture(scope='package', autouse=True)
-def prepare_arp_responder_conf(ptfhost):
-    arp_responder_conf = Template(open("../ansible/roles/test/templates/arp_responder.conf.j2").read())
-    ptfhost.copy(content=arp_responder_conf.render(arp_responder_args="--conf /tmp/from_t1.json"),
-                 dest="/etc/supervisor/conf.d/arp_responder.conf")
