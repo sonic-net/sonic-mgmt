e2a8f002fdfba89c791298b5c932c2fefa5f19a9
Alexander Allen <arallen@nvidia.com>
2021-08-23 19:06:13 -0700

    Implement FWUtil Test Plan (#3814)
    ### Description of PR
    
    Implemented the fwutil test plan here: 
    https://github.com/Azure/sonic-mgmt/blob/master/docs/testplan/FWUtil-test-plan.md
    
    ### Type of change
    
    - [ ] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [x] Test case(new/improvement)
    
    
    ### Approach
    #### What is the motivation for this PR?
    
    See HLD
    
    #### How did you do it?
    
    See HLD
    
    #### How did you verify/test it?
    
    Tested on CPLD / ONIE on Mellanox 2010 / 2100 / 2410 / 2700 / 3420 / 3700 / 3800 / 4600 / 4700
    Currently running in mellanox internal regression. 
    
    #### Any platform specific information?
    
    See HLD. Every platform will need to provide custom `firmware.tar.gz` file. Will update the HLD and a wiki page to have a very specific specification / instructions soon.
    
    #### Supported testbed topology if it's a new test case?
    
    N/A Platform Test
    
    ### Documentation 
    
    Pending, need to create wiki page and update HLD.

---
+++ tests/platform_tests/fwutil/conftest.py
@@ -0,0 +1,116 @@
+import tarfile
+import json
+import pytest
+import logging
+
+from random import randrange
+
+from fwutil_common import show_firmware
+
+logger = logging.getLogger(__name__)
+
+DUT_HOME="/home/admin"
+DEVICES_PATH="/usr/share/sonic/device"
+FS_PATH_TEMPLATE = "/host/image-{}/fs.squashfs"
+FS_RW_TEMPLATE = "/host/image-{}/rw"
+FS_WORK_TEMPLATE = "/host/image-{}/work"
+FS_MOUNTPOINT_TEMPLATE = "/tmp/image-{}-fs"
+OVERLAY_MOUNTPOINT_TEMPLATE = "/tmp/image-{}-overlay"
+
+def check_path_exists(path):
+    return duthost.stat(path = path)["stat"]["exists"] 
+
+def pytest_generate_tests(metafunc):
+    val = metafunc.config.getoption('--fw-pkg')
+    if 'fw_pkg_name' in metafunc.fixturenames and val is not None:
+        metafunc.parametrize('fw_pkg_name', [val], scope="module")
+
+@pytest.fixture(scope='module')
+def fw_pkg(fw_pkg_name):
+    if fw_pkg_name is None:
+        pytest.skip("No fw package specified.")
+    logger.info("Unpacking firmware package to ./firmware")
+    try:
+        os.mkdir("firmware")
+    except Exception as e:
+        pass # Already exists, thats fine
+    with tarfile.open(fw_pkg_name, "r:gz") as f:
+        f.extractall("./firmware/")
+        with open('./firmware/firmware.json', 'r') as fw:
+            fw_data = json.load(fw)
+            yield fw_data
+    subprocess.call("rm -rf firmware", shell=True)
+
+@pytest.fixture(scope='function')
+def random_component(duthost, fw_pkg):
+    chass = show_firmware(duthost)["chassis"].keys()[0]
+    components = fw_pkg["chassis"].get(chass, {}).get("component", []).keys()
+
+    if len(components) == 0:
+        pytest.skip("No suitable components found in config file for platform {}.".format(duthost.facts['platform']))
+
+    return components[randrange(len(components))] 
+
+@pytest.fixture(scope='function')
+def host_firmware(localhost, duthost):
+    logger.info("Starting local python server to test URL firmware update....")
+    comm = "python3 -m http.server --directory {}".format(os.path.join(DEVICES_PATH, 
+        duthost.facts['platform']))
+    duthost.command(comm, module_ignore_errors=True, module_async=True)
+    yield "http://localhost:8000/"
+    logger.info("Stopping local python server.")
+    duthost.command('pkill -f "{}"'.format(comm), module_ignore_errors=True)
+
+@pytest.fixture(scope='function')
+def next_image(duthost, fw_pkg):
+
+    # Install next version of sonic
+    current = duthost.shell('sonic_installer list | grep Current | cut -f2 -d " "')['stdout']
+
+    image = fw_pkg.get("images", {}).keys()
+    target = None
+
+    for i in image:
+        if "SONiC-OS-{}".format(i) != current:
+            target = i
+
+    if target is None:
+        pytest.skip("No suitable image definitions found in config")
+
+    logger.info("Installing new image {}".format(target))
+    duthost.copy(src=os.path.join("firmware", fw_pkg["images"][target]), dest=DUT_HOME)
+    remote_path = os.path.join(DUT_HOME, os.path.basename(fw_pkg["images"][target]))
+    duthost.command("sonic_installer install -y {}".format(remote_path), module_ignore_errors=True)
+
+    # Mount newly installed image
+    fs_path = FS_PATH_TEMPLATE.format(target)
+    fs_mountpoint = FS_MOUNTPOINT_TEMPLATE.format(target)
+    fs_rw = FS_RW_TEMPLATE.format(target)
+    fs_work = FS_WORK_TEMPLATE.format(target)
+    overlay_mountpoint = OVERLAY_MOUNTPOINT_TEMPLATE.format(target)
+
+    logger.info("Attempting to stage test firware onto newly-installed image.")
+    try:
+        wait_until(10, 1, check_path_exists, fs_rw)
+
+        duthost.command("mkdir -p {}".format(fs_mountpoint))
+        cmd = "mount -t squashfs {} {}".format(fs_path, fs_mountpoint)
+        duthost.command(cmd)
+
+        duthost.command("mkdir -p {}".format(overlay_mountpoint))
+        cmd = "mount -n -r -t overlay -o lowerdir={},upperdir={},workdir={},rw overlay {}".format(
+            fs_mountpoint,
+            fs_rw,
+            fs_work,
+            overlay_mountpoint
+        )
+        duthost.command(cmd)
+    except Exception as e:
+        pytest.fail("Failed to setup next-image.")
+        duthost.command("sonic_installer set-default {}".format(current))
+
+    yield overlay_mountpoint
+
+    logger.info("Ensuring correct image is set to default boot.")
+    duthost.command("sonic_installer set-default {}".format(current))
+
