bc591a6ac5a2363b45d670bd202e83fbc5090ee6
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2021-09-03 13:32:31 +0800

    Skip dualtor test on Mellanox platform (#4162)
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/dualtor/conftest.py
+++ tests/dualtor/conftest.py
@@ -4,8 +4,10 @@ import time
 
 from tests.common.dualtor.dual_tor_utils import get_crm_nexthop_counter # lgtm[py/unused-import]
 from tests.common.helpers.assertions import pytest_assert as py_assert
+from tests.common.helpers.assertions import pytest_require as py_require
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses, run_garp_service
 from tests.common.dualtor.dual_tor_mock import mock_server_base_ip_addr
+from tests.common.utilities import get_host_visible_vars
 
 CRM_POLL_INTERVAL = 1
 CRM_DEFAULT_POLL_INTERVAL = 300
@@ -57,6 +59,18 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="module", autouse=True)
-def common_setup_teardown(request, tbinfo, vmhost):
+def common_setup_teardown(rand_selected_dut, request, tbinfo, vmhost):
+    # Skip dualtor test cases on unsupported platform
+    supported_platforms = ['broadcom_td3_hwskus', 'broadcom_th2_hwskus']
+    hostvars = get_host_visible_vars(rand_selected_dut.host.options['inventory'], rand_selected_dut.hostname)
+    hwsku = rand_selected_dut.facts['hwsku']
+    skip = True
+    for platform in supported_platforms:
+        supported_skus = hostvars.get(platform, [])
+        if hwsku in supported_skus:
+            skip = False
+            break
+    py_require(not skip, "Skip on unsupported platform")
+
     if 'dualtor' in tbinfo['topo']['name']:
         request.getfixturevalue('run_garp_service')
