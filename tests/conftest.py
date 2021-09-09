0229a20fc4f034bdff70f0647b4af28f8b32eb7a
andriyz-nv <80095786+andriyz-nv@users.noreply.github.com>
2021-09-09 11:37:26 +0300

    Fixed RPC regression which is executed on ptf32 topology (#4083)
    Community introduced change to clean up topology #3656, which assumes
    that topo_ptf32.yml will have VMs, but it did not.

--- tests/conftest.py
+++ tests/conftest.py
@@ -398,7 +398,7 @@ def nbrhosts(ansible_adhoc, tbinfo, creds, request):
     """
 
     devices = {}
-    if not tbinfo['vm_base'] and 'tgen' in tbinfo['topo']['name']:
+    if (not tbinfo['vm_base'] and 'tgen' in tbinfo['topo']['name']) or 'ptf' in tbinfo['topo']['name']:
         logger.info("No VMs exist for this topology: {}".format(tbinfo['topo']['name']))
         return devices
 
