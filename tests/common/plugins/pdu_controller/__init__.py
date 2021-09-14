fa1ad9af99745f26a4c6945e8a63c4553a93cdce
Lawrence Lee <lawlee@microsoft.com>
2021-09-14 16:03:22 -0700

    [dualtor_io]: Improve ToR failure tests (#4183)
    - Use the PDU controller to cut power to the ToRs, instead of using the reboot command
    - Change control plane verification utils to poll DBs instead of checking one time
        - This allows the tests to be shorter, since previously they would fail since the DBs would be checked while the ToR was still booting up
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/common/plugins/pdu_controller/__init__.py
+++ tests/common/plugins/pdu_controller/__init__.py
@@ -6,16 +6,7 @@ from pdu_manager import pdu_manager_factory
 
 logger = logging.getLogger(__name__)
 
-
-@pytest.fixture(scope="module")
-def pdu_controller(duthosts, enum_rand_one_per_hwsku_hostname, conn_graph_facts, pdu):
-    """
-    @summary: Fixture for controlling power supply to PSUs of DUT
-    @param duthost: Fixture duthost defined in sonic-mgmt/tests/conftest.py
-    @returns: Returns a pdu controller object implementing the BasePduController interface defined in
-              controller_base.py.
-    """
-    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+def get_pdu_hosts(duthost):
     inv_mgr = duthost.host.options["inventory_manager"]
     pdu_host_list = inv_mgr.get_host(duthost.hostname).get_vars().get("pdu_host")
     pdu_hosts = {}
@@ -27,11 +18,42 @@ def pdu_controller(duthosts, enum_rand_one_per_hwsku_hostname, conn_graph_facts,
         logging.debug("No 'pdu_host' is defined in inventory file for '%s'." %
                      duthost.hostname)
 
+    return pdu_hosts
+
+
+@pytest.fixture(scope="module")
+def pdu_controller(duthosts, enum_rand_one_per_hwsku_hostname, conn_graph_facts, pdu):
+    """
+    @summary: Fixture for controlling power supply to PSUs of DUT
+    @param duthost: Fixture duthost defined in sonic-mgmt/tests/conftest.py
+    @returns: Returns a pdu controller object implementing the BasePduController interface defined in
+              controller_base.py.
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+    pdu_hosts = get_pdu_hosts(duthost) 
     controller = pdu_manager_factory(duthost.hostname, pdu_hosts, conn_graph_facts, pdu)
 
     yield controller
 
     logger.info("pdu_controller fixture teardown, ensure that all PDU outlets are turned on after test")
     if controller:
+        controller.turn_on_outlet()
+        controller.close()
+
+@pytest.fixture(scope="module")
+def get_pdu_controller(conn_graph_facts, pdu):
+    controller_map = {}
+
+    def pdu_controller_helper(duthost):
+        if duthost.hostname not in controller_map:
+            pdu_hosts = get_pdu_hosts(duthost)
+            controller = pdu_manager_factory(duthost.hostname, pdu_hosts, conn_graph_facts, pdu)
+            controller_map[duthost.hostname] = controller
+
+        return controller_map[duthost.hostname]
+
+    yield pdu_controller_helper
+
+    for controller in controller_map.values():
         controller.turn_on_outlet()
         controller.close()
