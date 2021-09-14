7d659a27b8cb9263e991c0f59be7f325f776d20e
Lawrence Lee <lawlee@microsoft.com>
2021-09-14 16:03:22 -0700

    [dualtor_io]: Improve ToR failure tests (#4183)
    - Use the PDU controller to cut power to the ToRs, instead of using the reboot command
    - Change control plane verification utils to poll DBs instead of checking one time
        - This allows the tests to be shorter, since previously they would fail since the DBs would be checked while the ToR was still booting up
    Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

--- tests/common/dualtor/control_plane_utils.py
+++ tests/common/dualtor/control_plane_utils.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 from tests.common.helpers.assertions import pytest_assert
+from tests.common.utilities import wait_until
 
 logger = logging.getLogger(__name__)
 
@@ -36,73 +37,88 @@ DB_CHECK_FIELD_MAP = {
 }
 
 
-def _dump_db(duthost, db, key_pattern):
-    """Dump redis database matching specificied key pattern"""
-    command = "redis-dump -d {db} -k \"{key_pattern}\"".format(
-        db=db, key_pattern=key_pattern)
-    lines = duthost.shell(command)["stdout_lines"]
-    return json.loads(lines[0])
+class DBChecker:
 
+    def __init__(self, duthost, state, health, intf_names='all'):
+        """
+        Create a DBChecker object
+        Args:
+            duthost:    DUT host object (needs to be passed by calling function
+                        from duthosts fixture)
+            state:      The expected value for each of the `state` fields in both
+                        tables
+            health:     The expected value for the `state` field in the
+                        MUX_LINKMGR_TABLE table (only needed for STATE_DB)
+            intf_names: A list of the PORTNAME to check in each table, or 'all'
+                        (by default) to check all MUX_CABLE interfaces
+        """
+        self.duthost = duthost
+        self.state = state
+        self.health = health
+        self.intf_names = intf_names
+        self.mismatch_ports = {}
 
-def expect_db_values(duthost, db, state, health=None, intf_names='all'):
-    """
-    Query db on `tor_host` and check if the mux-related fields match the
-    expected values.
+    def _dump_db(self, db, key_pattern):
+        """Dump redis database matching specificied key pattern"""
+        command = "redis-dump -d {db} -k \"{key_pattern}\"".format(
+            db=db, key_pattern=key_pattern)
+        lines = self.duthost.shell(command)["stdout_lines"]
+        return json.loads(lines[0])
 
-    The tables/fields checked are defined in DB_CHECK_FIELD_MAP
+    def verify_db(self, db):
+        pytest_assert(
+            wait_until(30, 10, self.get_mismatched_ports, db),
+            "Database states don't match expected state {state},"
+            "incorrect {db_name} values {db_states}"
+            .format(state=self.state, db_name=DB_NAME_MAP[db],
+                    db_states=json.dumps(self.mismatch_ports,
+                                        indent=4,
+                                        sort_keys=True)))
 
-    Args:
-        duthost:    DUT host object (needs to be passed by calling function
-                    from duthosts fixture)
-        db:         Database number to check. Should be either 0 for APP_DB or
-                    6 for STATE_DB
-        state:      The expected value for each of the `state` fields in both
-                    tables
-        health:     The expected value for the `state` field in the
-                    MUX_LINKMGR_TABLE table (only needed for STATE_DB)
-        intf_names: A list of the PORTNAME to check in each table, or 'all'
-                    (by default) to check all MUX_CABLE interfaces
+    def get_mismatched_ports(self, db):
+        """
+        Query db on `tor_host` and check if the mux-related fields match the
+        expected values.
 
-    Raises:
-        AssertionError if th mux cable fields don't match the given states.
-    """
-    logger.info("Verifying {} values on {}: "
-                "expected state = {}, expected health = {}".format(
-                            DB_NAME_MAP[db], duthost, state, health))
-    if intf_names == 'all':
-        mux_intfs = duthost.get_running_config_facts()['MUX_CABLE'].keys()
-    else:
-        mux_intfs = intf_names
+        The tables/fields checked are defined in DB_CHECK_FIELD_MAP
 
-    mismatch_ports = {}
-    separator = DB_SEPARATOR_MAP[db]
-
-    db_check_fields = DB_CHECK_FIELD_MAP[db]
-    for table, field in db_check_fields.items():
-        key_pattern = table + separator + "*"
-        db_dump = _dump_db(duthost, db, key_pattern)
-
-        if table == 'MUX_LINKMGR_TABLE':
-            pytest_assert(
-                health is not None,
-                "Must give a value for `health` when checking STATE_DB values")
-            target_value = health
+        Args:
+            db:         Database number to check. Should be either 0 for APP_DB or
+                        6 for STATE_DB
+        """
+        logger.info("Verifying {} values on {}: "
+                    "expected state = {}, expected health = {}".format(
+                                DB_NAME_MAP[db], self.duthost, self.state, self.health))
+        if self.intf_names == 'all':
+            mux_intfs = self.duthost.get_running_config_facts()['MUX_CABLE'].keys()
         else:
-            target_value = state
+            mux_intfs = self.intf_names
 
-        for intf_name in mux_intfs:
-            table_key = '{}{}{}'.format(table, separator, intf_name)
+        mismatch_ports = {}
+        separator = DB_SEPARATOR_MAP[db]
 
-            if db_dump[table_key]['value'][field] != target_value:
-                mismatch_ports[table_key] = db_dump[table_key]['value']
+        db_check_fields = DB_CHECK_FIELD_MAP[db]
+        for table, field in db_check_fields.items():
+            key_pattern = table + separator + "*"
+            db_dump = self._dump_db(db, key_pattern)
 
-    pytest_assert(not bool(mismatch_ports),
-                  "Database states don't match expected state {state},"
-                  "incorrect {db_name} values {db_states}"
-                  .format(state=state, db_name=DB_NAME_MAP[db],
-                          db_states=json.dumps(mismatch_ports,
-                                               indent=4,
-                                               sort_keys=True)))
+            if table == 'MUX_LINKMGR_TABLE':
+                pytest_assert(
+                    self.health is not None,
+                    "Must give a value for `health` when checking STATE_DB values")
+                target_value = self.health
+            else:
+                target_value = self.state
+
+            for intf_name in mux_intfs:
+                table_key = '{}{}{}'.format(table, separator, intf_name)
+
+                if db_dump[table_key]['value'][field] != target_value:
+                    mismatch_ports[table_key] = db_dump[table_key]['value']
+
+        self.mismtach_ports = mismatch_ports
+
+        return not bool(mismatch_ports)
 
 
 def verify_tor_states(expected_active_host, expected_standby_host,
@@ -112,11 +128,10 @@ def verify_tor_states(expected_active_host, expected_standby_host,
     reflected in APP_DB and STATE_DB on each device
     """
 
-    expect_db_values(expected_active_host, APP_DB,
-                     'active', intf_names=intf_names)
-    expect_db_values(expected_active_host, STATE_DB, 'active',
-                     'healthy', intf_names=intf_names)
-    expect_db_values(expected_standby_host, APP_DB,
-                     'standby', intf_names=intf_names)
-    expect_db_values(expected_standby_host, STATE_DB, 'standby',
-                     expected_standby_health, intf_names=intf_names)
+    active_db_checker = DBChecker(expected_active_host, 'active', 'healthy', intf_names=intf_names)
+    standby_db_checker = DBChecker(expected_standby_host, 'standby', expected_standby_health, intf_names=intf_names)
+
+    active_db_checker.verify_db(APP_DB)
+    active_db_checker.verify_db(STATE_DB)
+    standby_db_checker.verify_db(APP_DB)
+    standby_db_checker.verify_db(STATE_DB)
