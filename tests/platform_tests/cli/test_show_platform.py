50215ed20f4e8126a22b7aa0fa21eeeefd740b50
vdahiya12 <67608553+vdahiya12@users.noreply.github.com>
2021-09-15 14:47:53 -0700

    [Platform API][pytest] Fix test_chassis.py::test_get_system_eeprom_info; skip platform_tests/cli/test_show_platform.py::test_show_platform_syseeprom (#4168)
    What is the motivation for this PR?
    change the logic inside get_system_eeprom_info by changing the way dictionary values are compared,
    skip the test platform_tests/cli/test_show_platform.py::test_show_platform_syseeprom since for this Arista does not abide by the ONIE output format because our eeprom uses a different format and stores other information.
    
    How did you do it?
    made the changes
    
    How did you verify/test it?
    ran the test on platforms, Arista7050cx3-32s
    
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- tests/platform_tests/cli/test_show_platform.py
+++ tests/platform_tests/cli/test_show_platform.py
@@ -93,6 +93,7 @@ def test_show_platform_syseeprom(duthosts, enum_rand_one_per_hwsku_hostname, dut
     @summary: Verify output of `show platform syseeprom`
     """
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+    skip_release_for_platform(duthost, ["202012", "201911", "201811"], ["arista_7050"])
     cmd = " ".join([CMD_SHOW_PLATFORM, "syseeprom"])
 
     logging.info("Verifying output of '{}' on '{}' ...".format(cmd, duthost.hostname))
