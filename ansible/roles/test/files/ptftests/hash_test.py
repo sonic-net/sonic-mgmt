ef36c063847fad20db666c42a02c2df439640428
sanmalho-git <66454160+sanmalho-git@users.noreply.github.com>
2021-09-15 19:33:49 -0400

    Ignoring ip protocol 253 in IPv6 packets in the fib hash test. (#4274)
    What is the motivation for this PR?
    When a DNX chipset gets an IPv6 packet with ip protocol 253, it strips out 4 bytes from its payload
    before forwarding the packet out. This issue was raised against BCM
    
    https://github.com/Broadcom-Switch-Collab/Zeus_SAI/issues/100
    
    How did you do it?
    Recommendation of BCM and MSFT is to avoid ip protocol 253 (similar to protocol 2 and 254) in the fib hash test
    
    How did you verify/test it?
    Ran fib_hash test against linecards in a VoQ Chassis with DNX chipset.

--- ansible/roles/test/files/ptftests/hash_test.py
+++ ansible/roles/test/files/ptftests/hash_test.py
@@ -171,7 +171,7 @@ class HashTest(BaseTest):
         # ip_proto 2 is IGMP, should not be forwarded by router
         # ip_proto 254 is experimental
         # MLNX ASIC can't forward ip_proto 254, BRCM is OK, skip for all for simplicity
-        skip_ports = [2, 254]
+        skip_ports = [2, 253, 254]
         if ipv6:
             # Skip ip_proto 0 for IPv6
             skip_ports.append(0)
