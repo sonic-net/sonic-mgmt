f45b2325dd57c48fa4d5be79a317dd5f3611c700
Stephen Sun <stephens@nvidia.com>
2021-09-17 02:09:30 +0000

    Works great with APPL_DB tables
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- tests/qos/test_buffer_traditional.py
+++ tests/qos/test_buffer_traditional.py
@@ -36,7 +36,7 @@ def load_lossless_info_from_pg_profile_lookup(duthost):
         xoff = tokens[4]
         threshold = tokens[5]
         profile_info = {
-            'pool': '[BUFFER_POOL|ingress_lossless_pool]',
+            'pool': '[BUFFER_POOL_TABLE:ingress_lossless_pool]',
             'size': size,
             'xon': xon,
             'xoff': xoff,
@@ -143,12 +143,12 @@ def test_buffer_pg(duthosts, rand_one_dut_hostname, conn_graph_facts):
         Return:
             A tuple consisting of the OID of buffer profile and whether there is any check failed
         """
-        profile_in_pg = duthost.shell('redis-cli -n 4 hget "{}|{}|{}" profile'.format(table, port, ids))['stdout']
+        profile_in_pg = duthost.shell('redis-cli hget "{}:{}:{}" profile'.format(table, port, ids))['stdout']
         buffer_profile_oid = None
-        if table[:9] == 'BUFFER_PG':
+        if table == 'BUFFER_PG_TABLE':
             sai_field = 'SAI_INGRESS_PRIORITY_GROUP_ATTR_BUFFER_PROFILE'
             buffer_name_map = pg_name_map
-        elif table == 'BUFFER_QUEUE':
+        elif table == 'BUFFER_QUEUE_TABLE':
             sai_field = 'SAI_QUEUE_ATTR_BUFFER_PROFILE_ID'
             buffer_name_map = queue_name_map
 
@@ -161,7 +161,7 @@ def test_buffer_pg(duthosts, rand_one_dut_hostname, conn_graph_facts):
             if buffer_name_map:
                 buffer_profile_oid = None
                 for pg in id_list:
-                    logging.info("Checking {} {}|{} in ASIC_DB".format(table, port, pg))
+                    logging.info("Checking {}:{}:{} in ASIC_DB".format(table, port, pg))
                     buffer_profile_oid, success = _check_buffer_item_in_asic_db(duthost, port, pg, buffer_name_map, buffer_profile_oid, sai_field, True, use_assert)
                     if not success:
                         return None, False
@@ -170,7 +170,7 @@ def test_buffer_pg(duthosts, rand_one_dut_hostname, conn_graph_facts):
                 return None, False
             if buffer_name_map:
                 for pg in id_list:
-                    logging.info("Checking {} {}|{} in ASIC_DB".format(table, port, pg))
+                    logging.info("Checking {}:{}:{} in ASIC_DB".format(table, port, pg))
                     buffer_profile_oid, success = _check_buffer_item_in_asic_db(duthost, port, pg, buffer_name_map, None, sai_field, False, use_assert)
 
         return buffer_profile_oid, True
@@ -212,31 +212,31 @@ def test_buffer_pg(duthosts, rand_one_dut_hostname, conn_graph_facts):
             admin_up_ports.add(port)
             cable_length = cable_length_map[port]
             speed = port_config['speed']
-            lossless_profile = '[BUFFER_PROFILE|pg_lossless_{}_{}_profile]'.format(speed, cable_length)
+            lossless_profile = '[BUFFER_PROFILE_TABLE:pg_lossless_{}_{}_profile]'.format(speed, cable_length)
 
-            buffer_items_to_check = [('BUFFER_PG', '3-4', lossless_profile),
-                                     ('BUFFER_PG', '0', '[BUFFER_PROFILE|ingress_lossy_profile]'),
-                                     ('BUFFER_QUEUE', '0-2', '[BUFFER_PROFILE|q_lossy_profile]'),
-                                     ('BUFFER_QUEUE', '3-4', '[BUFFER_PROFILE|egress_lossless_profile]'),
-                                     ('BUFFER_QUEUE', '5-6', '[BUFFER_PROFILE|q_lossy_profile]')
+            buffer_items_to_check = [('BUFFER_PG_TABLE', '3-4', lossless_profile),
+                                     ('BUFFER_PG_TABLE', '0', '[BUFFER_PROFILE_TABLE:ingress_lossy_profile]'),
+                                     ('BUFFER_QUEUE_TABLE', '0-2', '[BUFFER_PROFILE_TABLE:q_lossy_profile]'),
+                                     ('BUFFER_QUEUE_TABLE', '3-4', '[BUFFER_PROFILE_TABLE:egress_lossless_profile]'),
+                                     ('BUFFER_QUEUE_TABLE', '5-6', '[BUFFER_PROFILE_TABLE:q_lossy_profile]')
                                      ]
         else:
-            buffer_items_to_check = [('BUFFER_PG', '0', '[BUFFER_PROFILE|ingress_lossy_pg_zero_profile]'),
-                                     ('BUFFER_PG', '3-4', None),
-                                     ('BUFFER_QUEUE', '0-2', '[BUFFER_PROFILE|egress_lossy_zero_profile]'),
-                                     ('BUFFER_QUEUE', '3-4', '[BUFFER_PROFILE|egress_lossless_zero_profile]'),
-                                     ('BUFFER_QUEUE', '5-6', '[BUFFER_PROFILE|egress_lossy_zero_profile]')
+            buffer_items_to_check = [('BUFFER_PG_TABLE', '0', '[BUFFER_PROFILE_TABLE:ingress_lossy_pg_zero_profile]'),
+                                     ('BUFFER_PG_TABLE', '3-4', None),
+                                     ('BUFFER_QUEUE_TABLE', '0-2', '[BUFFER_PROFILE_TABLE:egress_lossy_zero_profile]'),
+                                     ('BUFFER_QUEUE_TABLE', '3-4', '[BUFFER_PROFILE_TABLE:egress_lossless_zero_profile]'),
+                                     ('BUFFER_QUEUE_TABLE', '5-6', '[BUFFER_PROFILE_TABLE:egress_lossy_zero_profile]')
                                      ]
 
         for table, ids, expected_profile in buffer_items_to_check:
-            logging.info("Checking buffer item {}|{}|{}".format(table, port, ids))
+            logging.info("Checking buffer item {}:{}:{}".format(table, port, ids))
             buffer_profile_oid, _ = _check_port_buffer_info_and_get_profile_oid(duthost, table, ids, port, expected_profile)
 
             if not expected_profile:
                 continue
 
             if expected_profile not in profiles_checked:
-                profile_info = make_dict_from_output_lines(duthost.shell('redis-cli -n 4 hgetall "{}"'.format(expected_profile[1:-1]))['stdout'].split())
+                profile_info = make_dict_from_output_lines(duthost.shell('redis-cli hgetall "{}"'.format(expected_profile[1:-1]))['stdout'].split())
                 is_ingress_lossless = expected_profile[:12] == 'pg_lossless_'
                 if is_ingress_lossless:
                     pytest_assert(profile_info == default_lossless_profiles[(speed, cable_length)], "Buffer profile {} {} doesn't match default {}".format(expected_profile, profile_info, default_lossless_profiles[(speed, cable_length)]))
@@ -264,19 +264,19 @@ def test_buffer_pg(duthosts, rand_one_dut_hostname, conn_graph_facts):
                                       "Buffer profile {} has different buffer pool id {} from others {}".format(expected_profile, buffer_profile_asic_info['SAI_BUFFER_PROFILE_ATTR_POOL_ID'], lossless_pool_oid))
             else:
                 pytest_assert(profiles_checked[expected_profile] == buffer_profile_oid,
-                              "PG {}|3-4 has different OID of profile from other PGs sharing the same profile {}".format(port, expected_profile))
+                              "PG {}:3-4 has different OID of profile from other PGs sharing the same profile {}".format(port, expected_profile))
 
     port_to_shutdown = admin_up_ports.pop()
-    expected_profile = duthost.shell('redis-cli -n 4 hget "BUFFER_PG|{}|3-4" profile'.format(port))['stdout']
+    expected_profile = duthost.shell('redis-cli hget "BUFFER_PG_TABLE:{}:3-4" profile'.format(port))['stdout']
     try:
         # Shutdown the port and check whether the lossless PG has been remvoed
         logging.info("Shut down an admin-up port {} and check its buffer information".format(port_to_shutdown))
         duthost.shell('config interface shutdown {}'.format(port_to_shutdown))
-        wait_until(60, 5, _check_port_buffer_info_and_return, duthost, 'BUFFER_PG', '3-4', port_to_shutdown, None)
+        wait_until(60, 5, _check_port_buffer_info_and_return, duthost, 'BUFFER_PG_TABLE', '3-4', port_to_shutdown, None)
 
         # Startup the port and check whether the lossless PG has been reconfigured
         logging.info("Re-startup the port {} and check its buffer information".format(port_to_shutdown))
         duthost.shell('config interface startup {}'.format(port_to_shutdown))
-        wait_until(60, 5, _check_port_buffer_info_and_return, duthost, 'BUFFER_PG', '3-4', port_to_shutdown, expected_profile)
+        wait_until(60, 5, _check_port_buffer_info_and_return, duthost, 'BUFFER_PG_TABLE', '3-4', port_to_shutdown, expected_profile)
     finally:
         duthost.shell('config interface startup {}'.format(port_to_shutdown), module_ignore_errors=True)
