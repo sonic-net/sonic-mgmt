08bb83913f943d6043402822284e2aae9ae9a10f
Jibin Bao <jbao@nvidia.com>
2021-09-17 13:26:09 +0800

    integrated reboot-cause history into reboot tests in platfrom_tests/test_reboot.py (#4270)
    This is to add test case in sonic-mgmt/tests/platform_tests/test_reboot.py to cover new feature reboot-cause history introduced in Azure/SONiC#669. Example:
    
    show reboot-cause history
    Name                 Cause          Time                             User    Comment
    -------------------  -------------  -------------------------------  ------  ---------
    2021_09_09_14_15_13  Power Loss ()  N/A                              N/A     N/A
    2021_09_09_14_06_17  reboot         Thu 09 Sep 2021 02:05:17 PM UTC  admin   N/A
    2021_09_09_13_59_11  Watchdog ()    N/A                              N/A     N/A
    2021_09_09_13_52_13  Power Loss ()  N/A                              N/A     N/A
    2021_09_09_13_45_18  warm-reboot    Thu 09 Sep 2021 01:44:14 PM UTC  admin   N/A
    2021_09_09_13_37_58  fast-reboot    Thu 09 Sep 2021 01:37:09 PM UTC  admin   N/A
    2021_09_09_13_30_52  soft-reboot    Thu 09 Sep 2021 01:30:24 PM UTC  admin   N/A
    2021_09_09_13_24_17  reboot         Thu 09 Sep 2021 01:23:17 PM UTC  admin   N/A
    
    What is the motivation for this PR?
    In order to verify reboot-cause history function, add new checker for "show reboot-cause history" into reboot tests
    
    How did you do it?
    Add one new function of check_reboot_cause_history in sonic-mgmt/tests/platform_tests/test_reboot.py.
    In test_reboot.py, there are 8 different reboot tests. In order to save test execution time, we leverage the reboot-cause history generated by them to test it.
    1. Verify the reboot-cause history title
    2. Verify reboot-cause output are sorted in reverse chronological order

--- tests/platform_tests/test_reboot.py
+++ tests/platform_tests/test_reboot.py
@@ -58,6 +58,7 @@ def reboot_and_check(localhost, dut, interfaces, xcvr_skip_list, reboot_type=REB
     logging.info("Run %s reboot on DUT" % reboot_type)
 
     reboot(dut, localhost, reboot_type=reboot_type, reboot_helper=reboot_helper, reboot_kwargs=reboot_kwargs)
+    REBOOT_TYPE_HISTOYR_QUEUE.append(reboot_type)
 
     check_interfaces_and_services(dut, interfaces, xcvr_skip_list, reboot_type)
 
@@ -77,6 +78,12 @@ def check_interfaces_and_services(dut, interfaces, xcvr_skip_list, reboot_type =
         assert wait_until(MAX_WAIT_TIME_FOR_REBOOT_CAUSE, 20, check_reboot_cause, dut, reboot_type), \
             "got reboot-cause failed after rebooted by %s" % reboot_type
 
+        if "201811" in dut.os_version or "201911" in dut.os_version:
+            logging.info("Skip check reboot-cause history for version before 202012")
+        else:
+            logger.info("Check reboot-cause history")
+            assert wait_until(MAX_WAIT_TIME_FOR_REBOOT_CAUSE, 20, check_reboot_cause_history, dut,
+                              REBOOT_TYPE_HISTOYR_QUEUE), "Check reboot-cause history failed after rebooted by %s" % reboot_type
         if reboot_ctrl_dict[reboot_type]["test_reboot_cause_only"]:
             logging.info("Further checking skipped for %s test which intends to verify reboot-cause only" % reboot_type)
             return
