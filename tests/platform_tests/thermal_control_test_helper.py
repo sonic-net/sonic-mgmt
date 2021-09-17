dc4c53bc84c017f2ff02b0a5bfe82646a8764518
tjchadaga <tchadaga@microsoft.com>
2021-09-17 18:56:57 +0000

    Added warning if thermal policy file is missing

--- tests/platform_tests/thermal_control_test_helper.py
+++ tests/platform_tests/thermal_control_test_helper.py
@@ -305,6 +305,8 @@ class ThermalPolicyFileContext:
         """
         if os.path.exists(self.thermal_policy_file_path):
             self.dut.command('mv -f {} {}'.format(self.thermal_policy_file_path, self.thermal_policy_file_backup_path))
+        else:
+            logging.warning("Thermal Policy file {} not found".format(self.thermal_policy_file_path))
         self.dut.copy(src=os.path.join(FILES_DIR, self.src), dest=self.thermal_policy_file_path)
         restart_thermal_control_daemon(self.dut)
 
