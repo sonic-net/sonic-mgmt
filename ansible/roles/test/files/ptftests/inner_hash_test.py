67a741380502b18cabb8e1286defff99d05d3d55
Anton <antonh@nvidia.com>
2021-09-19 13:16:44 +0300

    CR fixes to PBH inner hashing
    Signed-off-by: Anton <antonh@nvidia.com>

--- ansible/roles/test/files/ptftests/inner_hash_test.py
+++ ansible/roles/test/files/ptftests/inner_hash_test.py
@@ -94,6 +94,8 @@ class InnerHashTest(BaseTest):
         logging.info("balancing_range:  {}".format(self.balancing_range))
         logging.info("balancing_test_times:  {}".format(self.balancing_test_times))
         logging.info("outer_encap_formats:  {}".format(self.outer_encap_formats))
+        logging.info("hash_keys:  {}".format(self.hash_keys))
+        logging.info("symmetric_hashing:  {}".format(self.symmetric_hashing))
 
 
     def check_hash(self, hash_key):
