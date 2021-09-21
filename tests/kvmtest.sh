d1e79187fa3af660fe33223ced8dee64b0db91fe
Aurgho Bhattacharjee <aurghob@gmail.com>
2021-09-21 06:30:10 -0700

    Add scp copy test (#4150)
    What is the motivation for this PR?
    Adds a test that checks that the DUT is able to access external objects via SCP
    
    How did you do it?
    Generate a file on the PTF. The DUT uses scp to fetch the file from the PTF. File integrity is checked by comparing checksums for the file before and after transport.
    The same file is then transferred back to the ptf via scp initiated from the DUT. File integrity is checked by again comparing checksums.
    
    How did you verify/test it?
    The test passed on my platform and confirmed that the file was copied to the host and the dut manually.
    Also made sure that the cleanup function works as expected.
    
    Any platform specific information?
    Ran this test with sonic based VMs. The interactions here are between the host, DUT and PTF. Assumptions are : There is enough memory on the DUT and PTF. Also assuming that the DUT has scp working since it is linux based.
    
    Supported testbed topology if it's a new test case?
    Tested on t0.
    But should work on any topology since this test is only using direct communication between the host, DUT and PTF.

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -200,7 +200,8 @@ test_t1_lag() {
     platform_tests/test_cpu_memory_usage.py \
     bgp/test_bgpmon.py \
     container_checker/test_container_checker.py \
-    process_monitoring/test_critical_process_monitoring.py"
+    process_monitoring/test_critical_process_monitoring.py \
+    scp/test_scp_copy.py"
 
     pushd $SONIC_MGMT_DIR/tests
     ./run_tests.sh $RUNTEST_CLI_COMMON_OPTS -c "$tests" -p logs/$tgname
