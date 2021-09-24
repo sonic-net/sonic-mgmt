e0c5c44733f18fb1127bebb603f48b89940d5e64
Perumal Venkatesh (pevenkat) <pevenkat@cisco.com>
2021-09-24 14:52:09 -0700

    Adding Cisco Sku in unsupported list

--- sonic-mgmt/tests/span/conftest.py
+++ sonic-mgmt/tests/span/conftest.py
@@ -55,7 +55,7 @@ def ports_for_test(cfg_facts):
 
 @pytest.fixture(scope='module', autouse=True)
 def skip_unsupported_asic_type(duthost):
-    SPAN_UNSUPPORTED_ASIC_TYPE = ["broadcom"]
+    SPAN_UNSUPPORTED_ASIC_TYPE = ["broadcom", "cisco-8000"]
     if duthost.facts["asic_type"] in SPAN_UNSUPPORTED_ASIC_TYPE:
         pytest.skip(
             "Skipping span test on {} platform".format(duthost.facts["asic_type"]))
