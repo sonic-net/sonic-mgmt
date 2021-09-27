4279a3a4a8b96f28cd84d6244fde898a2064fb19
liuh-80 <58683130+liuh-80@users.noreply.github.com>
2021-09-27 09:41:00 +0800

    Fix by disable warning.

--- tests/tacacs/test_ro_user.py
+++ tests/tacacs/test_ro_user.py
@@ -16,8 +16,8 @@ TIMEOUT_LIMIT   = 120
 
 def ssh_remote_run(localhost, remote_ip, username, password, cmd):
     res = localhost.shell("sshpass -p {} ssh "\
-                          "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "\
-                          "{}@{} {} > >(grep -v \"Permanently added\" 1>&2)".format(
+                          "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR "\
+                          "{}@{} {}".format(
             password, username, remote_ip, cmd), module_ignore_errors=True)
     return res
 
