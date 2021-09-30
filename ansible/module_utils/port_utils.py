35a72515788046d2504862ec408ae0f879365e92
Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>
2021-09-30 20:34:36 +0000

    Merged PR 188: add new topo vms26-t2-7800-1 for Arista T2 chassis
    The PR has following:
    
    Changes to bring up the T2 test topology vms25-t2-7800-1 for Arista Changes
    The inventory file changes required to generate the minigraph for the chassis

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -176,8 +176,8 @@ def get_port_alias_to_name_map(hwsku, asic_id=None):
             s100G_ports = [ x for x in range(45, 53) ] + [64]
 
         port_alias_to_name_map = _port_alias_to_name_map_50G(all_ports, s100G_ports)
-    elif hwsku == "Arista-7800R3-48CQ-LC" or\
-         hwsku == "Arista-7800R3K-48CQ-LC":
+    elif hwsku == "Arista-7800R3-48CQ2-C48" or\
+         hwsku == "Arista-7800R3-48CMQ2-C48":
          for i in range(1, 48):
              port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 4)
     elif hwsku == "INGRASYS-S9100-C32":
