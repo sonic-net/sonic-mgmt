750ba2135fd3314f71361202df26be9cbb4b33b1
Wei Bai <baiwei0427@gmail.com>
2021-10-04 08:43:10 -0700

    Add Arista-7050CX3-32S-C32 to port_utils.py (#4407)
    What is the motivation for this PR?
    Current port_utils.py does not have port mapping of Arista-7050CX3-32S-C32.
    
    How did you do it?
    Add port mapping based on https://github.com/Azure/sonic-buildimage/blob/master/device/arista/x86_64-arista_7050cx3_32s/Arista-7050CX3-32S-C32/port_config.ini
    
    How did you verify/test it?
    Test it using Arista-7050CX3-32S-C32

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -79,7 +79,8 @@ def get_port_alias_to_name_map(hwsku, asic_id=None):
     elif hwsku == "Arista-7260CX3-C64" or hwsku == "Arista-7170-64C":
         for i in range(1, 65):
             port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 4)
-    elif hwsku == "Arista-7060CX-32S-C32" or hwsku == "Arista-7060CX-32S-Q32" or hwsku == "Arista-7060CX-32S-C32-T1" or hwsku == "Arista-7170-32CD-C32":
+    elif hwsku == "Arista-7060CX-32S-C32" or hwsku == "Arista-7060CX-32S-Q32" or hwsku == "Arista-7060CX-32S-C32-T1" or hwsku == "Arista-7170-32CD-C32" \
+        or hwsku == "Arista-7050CX3-32S-C32":
         for i in range(1, 33):
             port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 4)
     elif hwsku == "Mellanox-SN2700-D40C8S8":
