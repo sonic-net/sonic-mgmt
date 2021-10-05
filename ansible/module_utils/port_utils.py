deefb4f3f2087699b9a0ca150637d7c90c712841
Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>
2021-10-05 22:14:29 +0000

    Merged PR 192: [chassis] correct the hwsku for Arista mac-sec linecard
    correct the hwsku for Arista mac-sec linecard
    
    Signed-off-by: Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -177,7 +177,7 @@ def get_port_alias_to_name_map(hwsku, asic_id=None):
 
         port_alias_to_name_map = _port_alias_to_name_map_50G(all_ports, s100G_ports)
     elif hwsku == "Arista-7800R3-48CQ2-C48" or\
-         hwsku == "Arista-7800R3-48CMQ2-C48":
+         hwsku == "Arista-7800R3-48CQM2-C48":
          for i in range(1, 48):
              port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 4)
     elif hwsku == "INGRASYS-S9100-C32":
