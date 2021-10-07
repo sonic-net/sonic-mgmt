cc91c1c4baa5a5f25c26e39f1cda8669fe2e7977
Ying Xie <yxieca@users.noreply.github.com>
2021-10-07 15:00:06 -0700

    [port util] update port mapping for Nokia 7215 to match the image change (#4437)
    What is the motivation for this PR?
    Nightly test was failing on Nokia 7215 since the image change.
    
    How did you do it?
    Update the port name mapping to match the image change.
    
    How did you verify/test it?
    Running previously error'ed out test cases.
    
    Signed-off-by: Ying Xie ying.xie@microsoft.com

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -164,10 +164,10 @@ def get_port_alias_to_name_map(hwsku, asic_id=None):
     elif hwsku == "Seastone-DX010":
         for i in range(1, 33):
             port_alias_to_name_map["Eth%d" % i] = "Ethernet%d" % ((i - 1) * 4)
-    elif hwsku == "Celestica-E1031-T48S4":
+    elif hwsku in ["Celestica-E1031-T48S4", "Nokia-7215", "Nokia-M0-7215"]:
         for i in range(1, 53):
             port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1))
-    elif hwsku == "et6448m" or hwsku == "Nokia-7215":
+    elif hwsku == "et6448m":
         for i in range(0, 52):
             port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
     elif hwsku == "newport":
