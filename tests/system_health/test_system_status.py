550c86419b107561de74938489d63bc168262b5b
Ze Gan <ganze718@gmail.com>
2021-10-19 17:32:23 +0000

    Add test_system_status to test_t0
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- tests/system_health/test_system_status.py
+++ tests/system_health/test_system_status.py
@@ -11,8 +11,8 @@ pytestmark = [
 
 def test_system_is_running(duthost):
     def is_system_ready(duthost):
-        status = duthost.shell('sudo systemctl is-system-running', module_ignore_errors=False)['stdout']
+        status = duthost.shell('sudo systemctl is-system-running', module_ignore_errors=True)['stdout']
         return status != "starting"
 
     if not wait_until(180, 10.0, is_system_ready, duthost):
-        pytest.fail('Failed to find routed interface in 120 s')
+        pytest.fail('Failed to find routed interface in 180 s')
