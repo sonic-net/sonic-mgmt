d65b1eedc3c582a80bc83923f7e81f618fd6de67
ppikh <70200079+ppikh@users.noreply.github.com>
2021-10-25 20:23:03 +0300

    Added info about new platform msn2201 to mellanox_data.py file (#4539)
    Description of PR
    Added info about new platform msn2201 to mellanox_data.py file
    
    Summary: Added info about new platform msn2201 to mellanox_data.py file
    Approach
    What is the motivation for this PR?
    Added info about new platform msn2201 to mellanox_data.py file
    
    How did you do it?
    Added platform related info into mellanox_data.py file
    
    How did you verify/test it?
    Executed platform tests which used this file
    
    Any platform specific information?
    msn2201 only
    
    Signed-off-by: Petro Pikh <petrop@nvidia.com>

--- tests/common/mellanox_data.py
+++ tests/common/mellanox_data.py
@@ -1,10 +1,62 @@
 
-SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "Mellanox-SN2700-D48C8", "ACS-MSN2740", "ACS-MSN2100", "ACS-MSN2410", "ACS-MSN2010"]
+SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "Mellanox-SN2700-D48C8", "ACS-MSN2740", "ACS-MSN2100", "ACS-MSN2410",
+               "ACS-MSN2010", "ACS-MSN2201"]
 SPC2_HWSKUS = ["ACS-MSN3700", "ACS-MSN3700C", "ACS-MSN3800", "Mellanox-SN3800-D112C8", "ACS-MSN3420"]
 SPC3_HWSKUS = ["ACS-MSN4700", "ACS-MSN4600C", "ACS-MSN4410", "ACS-MSN4600"]
 SWITCH_HWSKUS = SPC1_HWSKUS + SPC2_HWSKUS + SPC3_HWSKUS
 
 SWITCH_MODELS = {
+    "x86_64-mlnx_msn2201-r0": {
+        "chip_type": "spectrum1",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 2
+        },
+        "ports": {
+            "number": 52
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 2
+            },
+            "module": {
+                "start": 1,
+                "number": 52
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            }
+        }
+    },
     "x86_64-mlnx_msn2700-r0": {
         "chip_type": "spectrum1",
         "reboot": {
