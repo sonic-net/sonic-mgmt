474c3e9c912ea1c79717452a9d44f4fbe87ef3ac
Ze Gan <ganze718@gmail.com>
2021-10-26 09:02:00 +0800

    [test]: Add test to check whether the system is ready (#4507)
    * Add test to check the system status
    
    Signed-off-by: Ze Gan <ganze718@gmail.com>
    
    * Add test_system_status to test_t0
    
    Signed-off-by: Ze Gan <ganze718@gmail.com>

---
+++ tests/system_health/test_system_status.py
@@ -0,0 +1,18 @@
+import time
+
+import pytest
+
+from tests.common.utilities import wait_until
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+
+def test_system_is_running(duthost):
+    def is_system_ready(duthost):
+        status = duthost.shell('sudo systemctl is-system-running', module_ignore_errors=True)['stdout']
+        return status != "starting"
+
+    if not wait_until(180, 10.0, is_system_ready, duthost):
+        pytest.fail('Failed to find routed interface in 180 s')
