bc3a413d0bb0578eec7744edb3fc9cf01c04013b
Vaibhav Hemant Dixit <vadixit@microsoft.com>
2021-10-27 14:39:02 -0700

    More fixes - soft reboot handling, parameterized testcase

--- tests/metadata-scripts/test_metadata_upgrade_path.py
+++ tests/metadata-scripts/test_metadata_upgrade_path.py
@@ -28,15 +28,13 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
-def upgrade_path_lists(request):
-    upgrade_type = request.config.getoption('upgrade_type')
+def upgrade_path_lists(request, upgrade_type_params):
     from_list = request.config.getoption('base_image_list')
     to_list = request.config.getoption('target_image_list')
     restore_to_image = request.config.getoption('restore_to_image')
     if not from_list or not to_list:
         pytest.skip("base_image_list or target_image_list is empty")
-    return upgrade_type, from_list, to_list, restore_to_image
-
+    return upgrade_type_params, from_list, to_list, restore_to_image
 
 @pytest.fixture
 def skip_cancelled_case(request, upgrade_type_params):
