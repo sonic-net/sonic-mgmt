0f9afd6bc0572487099ff5d828f098ac073d8cb5
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2021-10-31 21:44:31 +0800

    [Mellanox] Check PSU capability against sysfs (#4551)
    Check the PSU sensors' capability against sysfs.
    There is a capability list for each PSU sensor. If a capability is in the list, there should be a sysfs file for that capability.
    This is to verify this.
    
    - How did you verify/test it?
    Run the test on Mellanox platforms.
    
    - Any platform specific information?
    Mellanox specific test.

--- tests/platform_tests/mellanox/check_sysfs.py
+++ tests/platform_tests/mellanox/check_sysfs.py
@@ -129,6 +129,17 @@ def check_sysfs(dut):
                 assert "Invalid PSU fan speed value {} for PSU {}, exception: {}".format(psu_info["fan_speed"],
                                                                                          psu_id, e)
 
+            # Check consistency between voltage capability and sysfs
+            all_capabilities = platform_data["psus"].get("capabilities")
+            if all_capabilities:
+                for capabilities in all_capabilities:
+                    psu_cmd_prefix = 'cat /var/run/hw-management/power/{}_'.format(capabilities.format(psu_id))
+                    psu_capability = dut.command(psu_cmd_prefix + 'capability')['stdout'].split()
+                    for capability in psu_capability:
+                        # Each capability should exist
+                        output = dut.command(psu_cmd_prefix + capability)['stdout']
+                        assert output, "PSU capability {} doesn't not exist".format(capability)
+
     logging.info("Check SFP related sysfs")
     for sfp_id, sfp_info in sysfs_facts['sfp_info'].items():
         assert sfp_info["temp_fault"] == '0', "SFP%d temp fault" % sfp_id
