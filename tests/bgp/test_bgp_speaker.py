f5772316c8ba9e9f8567650c570b9f1cd0d38d28
Andriy Zabavskyy <andriyz@nvidia.com>
2021-11-05 21:28:50 +0200

    Changed sleep for waiting till bgp learns neighbors to wait_until

--- tests/bgp/test_bgp_speaker.py
+++ tests/bgp/test_bgp_speaker.py
@@ -16,7 +16,7 @@ from tests.common.dualtor.mux_simulator_control import mux_server_url     # lgtm
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports
 from tests.common.dualtor.dual_tor_utils import map_hostname_to_tor_side
 from tests.common.helpers.assertions import pytest_require
-from tests.common import constants
+from tests.common.utilities import wait_until
 
 
 pytestmark = [
@@ -244,6 +244,14 @@ def get_dut_vlan_ptf_ports(mg_facts):
     return ports
 
 
+def is_all_neighbors_learned(duthost, speaker_ips):
+    bgp_facts = duthost.bgp_facts()['ansible_facts']
+    for ip in speaker_ips:
+        if not str(ip.ip) in bgp_facts['bgp_neighbors']:
+            return False
+    return True
+
+
 def bgp_speaker_announce_routes_common(common_setup_teardown,
                                        tbinfo, duthost, ptfhost, ipv4, ipv6, mtu,
                                        family, prefix, nexthop_ips, vlan_mac):
@@ -263,7 +271,7 @@ def bgp_speaker_announce_routes_common(common_setup_teardown,
     announce_route(ptfip, lo_addr, peer_range, vlan_ips[0].ip, port_num[2])
 
     logger.info("Wait some time to make sure routes announced to dynamic bgp neighbors")
-    time.sleep(30)
+    assert wait_until(90, 10, 0, is_all_neighbors_learned, duthost, speaker_ips), "Not all dynamic neighbors were learned"
 
     logger.info("Verify accepted prefixes of the dynamic neighbors are correct")
     bgp_facts = duthost.bgp_facts()['ansible_facts']
