35dc72fcc627a99bb9f64ee406e6df6eb8ff1925
stephengzh <87261920+stephengao-ragilenetworks@users.noreply.github.com>
2021-11-11 18:14:38 +0800

    Update test_vnet_route_leak.py

--- tests/vxlan/test_vnet_route_leak.py
+++ tests/vxlan/test_vnet_route_leak.py
@@ -12,7 +12,7 @@ from tests.common.config_reload import config_reload
 logger = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.topology("t0"),
+    pytest.mark.topology('t0'),
     pytest.mark.asic("mellanox")
 ]
 
