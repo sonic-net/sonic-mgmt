c998f70519976dd22cd8f31df23893f1c5f25221
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2021-11-16 12:05:25 -0800

    Add new test for PFC Rx frame counter (#4702)
    check pfc(priority flow control) continously that PFC frame are counted
        only for the respective priority(here we check priority 3 and 4), and
        all other priorities show pfc Rx frame count as 0
    
    Co-authored-by: Wenyi Zhang <wenyizhang@microsoft.com>

--- tests/qos/test_pfc_counters.py
+++ tests/qos/test_pfc_counters.py
@@ -60,7 +60,7 @@ def setup_testbed(fanouthosts, duthost, leaf_fanouts):
         file_src = os.path.join(os.path.dirname(__file__), PFC_GEN_FILE_RELATIVE_PATH)
         peerdev_ans.host.copy(src = file_src, dest = PFC_GEN_FILE_DEST, force = True)
 
-def run_test(fanouthosts, duthost, conn_graph_facts, leaf_fanouts, is_pfc=True, pause_time=65535):
+def run_test(fanouthosts, duthost, conn_graph_facts, leaf_fanouts, is_pfc=True, pause_time=65535, check_continous_pfc=False):
     """
     @Summary: Run test for Ethernet flow control (FC) or priority-based flow control (PFC)
     @param duthost: The object for interacting with DUT through ansible
@@ -79,33 +79,59 @@ def run_test(fanouthosts, duthost, conn_graph_facts, leaf_fanouts, is_pfc=True, 
         intf.startswith('Ethernet') and \
         int_status[intf]['admin_state'] == 'up' and \
         int_status[intf]['oper_state'] == 'up']
+    if not check_continous_pfc:
+        """ Generate PFC or FC packets for active physical interfaces """
+        for intf in active_phy_intfs:
+            peer_device = conn_facts[intf]['peerdevice']
+            peer_port = conn_facts[intf]['peerport']
+            peer_port_name = eos_to_linux_intf(peer_port)
 
-    """ Generate PFC or FC packets for active physical interfaces """
-    for intf in active_phy_intfs:
-        peer_device = conn_facts[intf]['peerdevice']
-        peer_port = conn_facts[intf]['peerport']
-        peer_port_name = eos_to_linux_intf(peer_port)
+            peerdev_ans = fanouthosts[peer_device]
+            if is_pfc:
+                for priority in range(PRIO_COUNT):
+                    cmd = "sudo python %s -i %s -p %d -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, 2 ** priority, pause_time, PKT_COUNT)
+                    peerdev_ans.host.command(cmd)
+            else:
+                cmd = "sudo python %s -i %s -g -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, pause_time, PKT_COUNT)
+                peerdev_ans.host.command(cmd)
 
-        peerdev_ans = fanouthosts[peer_device]
-        if is_pfc:
-            for priority in range(PRIO_COUNT):
+        """ SONiC takes some time to update counters in database """
+        time.sleep(5)
+
+        """ Check results """
+        counter_facts = duthost.sonic_pfc_counters(method = "get")['ansible_facts']
+
+        for intf in active_phy_intfs:
+            if is_pfc:
+                assert counter_facts[intf]['Rx'] == [str(PKT_COUNT)] * PRIO_COUNT
+            else:
+                assert counter_facts[intf]['Rx'] == ['0'] * PRIO_COUNT
+
+    else:
+        for intf in active_phy_intfs:
+            """only check priority 3 and 4: lossless priorities"""
+            for priority in range(3, 5):
+                """ Clear PFC counters """
+                duthost.sonic_pfc_counters(method = "clear")
+
+                peer_device = conn_facts[intf]['peerdevice']
+                peer_port = conn_facts[intf]['peerport']
+                peer_port_name = eos_to_linux_intf(peer_port)
+                peerdev_ans = fanouthosts[peer_device]
                 cmd = "sudo python %s -i %s -p %d -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, 2 ** priority, pause_time, PKT_COUNT)
                 peerdev_ans.host.command(cmd)
-        else:
-            cmd = "sudo python %s -i %s -g -t %d -n %d" % (PFC_GEN_FILE_DEST, peer_port_name, pause_time, PKT_COUNT)
-            peerdev_ans.host.command(cmd)
+                
+                time.sleep(5)
 
-    """ SONiC takes some time to update counters in database """
-    time.sleep(5)
-
-    """ Check results """
-    counter_facts = duthost.sonic_pfc_counters(method = "get")['ansible_facts']
-
-    for intf in active_phy_intfs:
-        if is_pfc:
-            assert counter_facts[intf]['Rx'] == [str(PKT_COUNT)] * PRIO_COUNT
-        else:
-            assert counter_facts[intf]['Rx'] == ['0'] * PRIO_COUNT
+                pfc_rx = duthost.sonic_pfc_counters(method = "get")['ansible_facts']
+                """check pfc Rx frame count on particular priority are increased"""
+                assert pfc_rx[intf]['Rx'][priority] == str(PKT_COUNT)
+                """check LHS priorities are 0 count"""
+                for i in range(priority):
+                    assert pfc_rx[intf]['Rx'][i] == '0'
+                """check RHS priorities are 0 count"""
+                for i in range(priority+1, PRIO_COUNT):
+                    assert pfc_rx[intf]['Rx'][i] == '0'
 
 def test_pfc_pause(fanouthosts, duthosts, rand_one_dut_hostname, conn_graph_facts, leaf_fanouts):
     """ @Summary: Run PFC pause frame (pause time quanta > 0) tests """
@@ -126,3 +152,8 @@ def test_fc_unpause(fanouthosts, duthosts, rand_one_dut_hostname, conn_graph_fac
     """ @Summary: Run FC pause frame (pause time quanta = 0) tests """
     duthost = duthosts[rand_one_dut_hostname]
     run_test(fanouthosts, duthost, conn_graph_facts, leaf_fanouts, is_pfc=False, pause_time=0)
+
+def test_continous_pfc(fanouthosts, duthosts, rand_one_dut_hostname, conn_graph_facts, leaf_fanouts):
+    duthost = duthosts[rand_one_dut_hostname]
+    run_test(fanouthosts, duthost, conn_graph_facts, leaf_fanouts, check_continous_pfc=True)
+
