1ca12fb055b3f1bcc16d9a2ed953223913161def
dennis0113 <stu90702003@gmail.com>
2021-11-17 16:13:53 +0800

    [test_read_mac_metadata] Fix check_interface failure and "PORTCHANNEL" keyerror in t1/ptf32 topology (#3759)
    What is the motivation for this PR?
    Script `test_read_mac_metadata` will be failing with the following error:
    Issue 1:
      FAILED                                                                   [100%]
      ------------------------------ live log teardown -------------------------------
      16:49:29 INFO test_read_mac_metadata.py:cleanup_read_mac:27: Remove temporary imagesFixture "check_interfaces" called directly. 
      Fixtures are not meant to be called directly,but are created automatically when test functions request them as parameters.
      See https://docs.pytest.org/en/latest/fixture.html for more information about fixtures, and
      https://docs.pytest.org/en/latest/deprecations.html#calling-fixtures-directly about how to update your code.
    
    Issue 2:
    >       non_default_portchannel = [k for k,v in cfg_facts["PORTCHANNEL"].items() if "mtu" in v and v["mtu"] != "9100" and "admin_status" in v and v["admin_status"] == "up" ]
    E       KeyError: 'PORTCHANNEL'read_mac/test_read_mac_metadata.py:122: KeyError
    
    How did you do it?
    - Refine test_read_mac_metadata while calling check_interfaces to do sanity checking.
    - Check the key: "PORTCHANNEL" exists before verify the status
    
    Co-authored-by: dennis_chiu <dennis_chiu@edge-core.com>

--- tests/read_mac/test_read_mac_metadata.py
+++ tests/read_mac/test_read_mac_metadata.py
@@ -3,7 +3,6 @@ import logging
 
 from tests.common.utilities import wait
 from tests.common.utilities import wait_until
-from tests.common.plugins.sanity_check import checks
 from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer
 from tests.common.helpers.assertions import pytest_assert
 from datetime import datetime
@@ -111,11 +110,19 @@ class ReadMACMetadata():
             pytest.fail("MAC entry does not exist")
 
         logger.info("Verify interfaces are UP and MTU == 9100")
-        checks.check_interfaces(duthost)
+        check_interfaces = self.request.getfixturevalue("check_interfaces")
+        results = check_interfaces()
+        failed = [result for result in results if "failed" in result and result["failed"]]
+        if failed:
+            pytest.fail("Interface check failed, not all interfaces are up. Failed: {}".format(failed))
 
         cfg_facts = duthost.config_facts(host=duthost.hostname, source="persistent")['ansible_facts']
         non_default_ports = [k for k,v in cfg_facts["PORT"].items() if "mtu" in v and v["mtu"] != "9100" and "admin_status" in v and v["admin_status"] == "up" ]
-        non_default_portchannel = [k for k,v in cfg_facts["PORTCHANNEL"].items() if "mtu" in v and v["mtu"] != "9100" and "admin_status" in v and v["admin_status"] == "up" ]
+
+        # Not all topology has portchannel in config, therefore, only verify the status if portchannel exists.
+        non_default_portchannel = []
+        if "PORTCHANNEL" in cfg_facts:
+            non_default_portchannel = [k for k,v in cfg_facts["PORTCHANNEL"].items() if "mtu" in v and v["mtu"] != "9100" and "admin_status" in v and v["admin_status"] == "up" ]
 
         if len(non_default_ports) != 0 or len(non_default_portchannel) != 0:
             pytest.fail("There are ports/portchannel with non default MTU:\nPorts: {}\nPortchannel: {}".format(non_default_ports,non_default_portchannel))
