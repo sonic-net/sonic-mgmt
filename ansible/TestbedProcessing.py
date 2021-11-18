bc6673f6ad7644b1bcad0dc8dad7914e6456f70a
Song Yuan <64041228+ysmanman@users.noreply.github.com>
2021-11-17 23:42:59 -0800

    Fix a typo that accidentally comments out the statement: num = dev.get("slot_num") (#4710)

--- ansible/TestbedProcessing.py
+++ ansible/TestbedProcessing.py
@@ -549,9 +549,10 @@ def makeLab(data, devices, testbed, outfile):
                         except AttributeError:
                             print("\t\t" + host + " switch_type not found")
 
-                        try: #get slot_num                                                                                                                                                                                           slot_num = dev.get("slot_num")                                           
-                            if slot_num is not None:                                                 
-                               entry += "\tslot_num=" + str( slot_num )                              
+                        try: #get slot_num
+                            slot_num = dev.get("slot_num")
+                            if slot_num is not None:
+                               entry += "\tslot_num=" + str( slot_num )
                         except AttributeError:
                             print("\t\t" + host + " slot_num not found")
 
