bb0d46189cef52b6736c4f29b073a599e1a4b8a4
yuxuanye <yuxuanye@microsoft.com>
2021-11-22 16:12:50 +0800

    add to kvm test

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -123,6 +123,7 @@ test_t0() {
     snmp/test_snmp_loopback.py \
     snmp/test_snmp_default_route.py \
     ssh/test_ssh_stress.py \
+    ssh/test_ssh_ciphers.py \
     syslog/test_syslog.py \
     tacacs/test_rw_user.py \
     tacacs/test_ro_user.py \
