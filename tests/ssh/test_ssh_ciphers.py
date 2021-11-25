efb5403f50880b3a4a29382c8c6dc2c0f12d0706
Yuxuan Ye <44050505+diaryevil@users.noreply.github.com>
2021-11-25 16:43:02 +0800

    [test gap] SSH using permitted Ciphers, Mac, Kex (#4707)
    What is the motivation for this PR?
    Add test case about SSH Ciphers/Macs/Kexs and protocol version.
    * Ensure SSH protocol version 1.x is disabled
    * Only preferred Ciphers/Mac/Kex should work without issues
      * Preferred Ciphers: ["aes256-gcm@openssh.com"], permitted Ciphers: ["aes256-ctr", "aes192-ctr"]
      * Preferred Macs: ['hmac-sha2-512-etm@openssh.com'], Permitted Mac: ['hmac-sha2-256-etm@openssh.com']
      * Preferred Kex: [ 'ecdh-sha2-nistp384'], Permitted Kex: [ 'ecdh-sha2-nistp521']
    
    How did you do it?
    * Check sshd version and check if sshd specially supports protocol version 1, because it's hard to guarantee that a ssh client support protocol version 1 will be found on test servers
    * Positive test cases expect preferred/permitted ciphers to connect successfully
    Negative test cases expect the ciphers not preferred/permitted ciphers to fail to connect(Maybe this is not good?)
    
    Co-authored-by: Yuxuan Ye <yuxuanye@microsoft.com>

---
+++ tests/ssh/test_ssh_ciphers.py
@@ -0,0 +1,61 @@
+import pexpect
+import logging
+import pytest
+
+from tests.common.helpers.assertions import pytest_assert
+
+logger = logging.getLogger(__name__)
+
+pytestmark = [
+    pytest.mark.topology('any'),
+    pytest.mark.device_type('vs')
+]
+
+def connect_with_specified_ciphers(duthosts, rand_one_dut_hostname, specified_cipher, creds, typename):
+    duthost = duthosts[rand_one_dut_hostname]
+    dutuser, dutpass = creds['sonicadmin_user'], creds['sonicadmin_password']
+    dutip = duthost.mgmt_ip
+
+    if typename == "enc":
+        ssh_cipher_option = "-c {}".format(specified_cipher)
+    elif typename == "mac":
+        ssh_cipher_option = "-m {}".format(specified_cipher)
+    elif typename == "kex":
+        ssh_cipher_option = "-o KexAlgorithms={}".format(specified_cipher)
+    else:
+        pytest.fail("typename only supports enc/mac/kex")
+
+    ssh_cmd = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no {} {}@{}".format(ssh_cipher_option, dutuser, dutip)
+
+    try:
+        connect = pexpect.spawn(ssh_cmd)
+        connect.expect('.*[Pp]assword:')
+        connect.sendline(dutpass)
+
+        i = connect.expect('{}@{}:'.format(dutuser, duthost.hostname), timeout=10)
+        pytest_assert(i == 0, "Failed to connect")
+    except pexpect.exceptions.EOF:
+        pytest.fail("EOF reached")
+    except pexpect.exceptions.TIMEOUT:
+        pytest.fail("Timeout reached")
+    except Exception as e:
+        pytest.fail("Cannot connect to DUT host via SSH: {}".format(e))
+
+def test_ssh_protocol_version(duthosts, rand_one_dut_hostname):
+    duthost = duthosts[rand_one_dut_hostname]
+    result = duthost.shell("sshd --error", module_ignore_errors=True)
+    major_version = result["stderr"].split("OpenSSH_", 1)[1].split(".", 1)[0]
+    if major_version < "7" or '[-1' in result["stderr"]:
+        pytest.fail("SSHD may support protocol version 1.x, only version 2.x will be passed")
+
+def test_ssh_enc_ciphers(duthosts, rand_one_dut_hostname, enum_dut_ssh_enc_cipher, creds):
+    typename = "enc"
+    connect_with_specified_ciphers(duthosts, rand_one_dut_hostname, enum_dut_ssh_enc_cipher, creds, typename)
+
+def test_ssh_macs(duthosts, rand_one_dut_hostname, enum_dut_ssh_mac, creds):
+    typename = "mac"
+    connect_with_specified_ciphers(duthosts, rand_one_dut_hostname, enum_dut_ssh_mac, creds, typename)
+
+def test_ssh_kex(duthosts, rand_one_dut_hostname, enum_dut_ssh_kex, creds):
+    typename = "kex"
+    connect_with_specified_ciphers(duthosts, rand_one_dut_hostname, enum_dut_ssh_kex, creds, typename)
