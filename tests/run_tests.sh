b97a4c7feecb00d44d7f2901b7ff0f0ae26cace9
Xin Wang <xiwang5@microsoft.com>
2021-12-01 08:54:27 +0800

    Improve the error message when non-exist testbed name is provided to run_tests.sh (#4770)
    The error message of run_tests.sh is confusing if:
    * non-exist testbed name is provided
    * Yaml format testbed file is used.
    
    For example:
    ```
    $ ./run_tests.sh -i veos_vtb -n 'vms-kvm-xxx' -c ./dhcp_relay/test_dhcp_relay.py -f vtestbed.yaml
    Traceback (most recent call last):
      File "<string>", line 1, in <module>
      File "<string>", line 0
    
        ^
    SyntaxError: unexpected EOF while parsing
    DUT name (-d) is not set..
    Usage ./run_tests.sh [options]
        options with (*) must be provided
        -h -?          : get this help
        ...
    ```
    This change added checking of existence of testbed name and improved
    the error message
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -37,11 +37,19 @@ function get_dut_from_testbed_file() {
         if [[ $TESTBED_FILE == *.csv ]];
         then
             LINE=`cat $TESTBED_FILE | grep "^$TESTBED_NAME"`
+            if [[ -z ${LINE} ]]; then
+                echo "Unable to find testbed '$TESTBED_NAME' in testbed file '$TESTBED_FILE'"
+                show_help_and_exit 4
+            fi
             IFS=',' read -ra ARRAY <<< "$LINE"
             DUT_NAME=${ARRAY[9]}
         elif [[ $TESTBED_FILE == *.yaml ]];
         then
             content=$(python -c "from __future__ import print_function; import yaml; print('+'.join(str(tb) for tb in yaml.safe_load(open('$TESTBED_FILE')) if '$TESTBED_NAME'==tb['conf-name']))")
+            if [[ -z ${content} ]]; then
+                echo "Unable to find testbed '$TESTBED_NAME' in testbed file '$TESTBED_FILE'"
+                show_help_and_exit 4
+            fi
             IFS=$'+' read -r -a tb_lines <<< $content
             tb_line=${tb_lines[0]}
             DUT_NAME=$(python -c "from __future__ import print_function; tb=eval(\"$tb_line\"); print(\",\".join(tb[\"dut\"]))")
