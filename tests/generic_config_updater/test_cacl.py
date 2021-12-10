d7321f049b83664e3a8437a359890935b78b2759
Jingwen Xie <jingwenxie@microsoft.com>
2021-12-10 05:56:28 +0000

    Combine to one test due to dependency among each test

--- tests/generic_config_updater/test_cacl.py
+++ tests/generic_config_updater/test_cacl.py
@@ -13,19 +13,14 @@ pytestmark = [
 logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope="module", autouse=True)
-def setup_env(duthosts, rand_one_dut_hostname, cfg_facts):
+def setup_env(duthosts, rand_one_dut_hostname):
     """
     Setup/teardown fixture for acl config
     Args:
         duthosts: list of DUTs.
         rand_selected_dut: The fixture returns a randomly selected DuT.
-        cfg_facts: config facts for selected DUT
     """
     duthost = duthosts[rand_one_dut_hostname]
-
-    config_tmpfile = generate_tmpfile(duthost)
-    logger.info("config_tmpfile {} Backing up config_db.json".format(config_tmpfile))
-    duthost.shell("sudo cp /etc/sonic/config_db.json {}".format(config_tmpfile))
 
     # Cleanup acl config
     duthost.shell('sonic-db-cli CONFIG_DB keys "ACL_RULE|*" | xargs --no-run-if-empty sonic-db-cli CONFIG_DB del')
@@ -34,10 +29,6 @@ def setup_env(duthosts, rand_one_dut_hostname, cfg_facts):
     yield
 
     logger.info("Restoring config_db.json")
-    duthost.shell("sudo cp {} /etc/sonic/config_db.json".format(config_tmpfile))
-    delete_tmpfile(duthost, config_tmpfile)
-
-     # Cleanup acl config
     config_reload(duthost)
 
 def expect_res_success_acl_table(duthost, expected_content_list, unexpected_content_list):
@@ -58,7 +49,7 @@ def expect_res_success_acl_rule(duthost, expected_content_list, unexpected_conte
 
     expect_res_success(duthost, output, expected_content_list, unexpected_content_list)
 
-def test_cacl_tc1_add_init_table(duthost):
+def cacl_add_init_table(duthost):
     """ Add acl table for test
 
     Sample output
@@ -87,15 +78,16 @@ def test_cacl_tc1_add_init_table(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    expected_content_list = ["TEST_1", "SNMP"]
-    expect_res_success_acl_table(duthost, expected_content_list, [])
+        expected_content_list = ["TEST_1", "SNMP"]
+        expect_res_success_acl_table(duthost, expected_content_list, [])
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc2_add_duplicate_table(duthost):
+def cacl_add_duplicate_table(duthost):
     """ Add duplicate acl table
     """
     json_patch = [
@@ -116,13 +108,20 @@ def test_cacl_tc2_add_duplicate_table(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc3_replace_table(duthost):
+def cacl_replace_table(duthost):
     """ Replace acl table with SSH service
+
+    Expected output
+    admin@vlab-01:~$ show acl table
+    Name    Type       Binding    Description    Stage
+    ------  ---------  ---------  -------------  -------
+    TEST_1  CTRLPLANE  SSH        Test Table 1   ingress
     """
     json_patch = [
         {
@@ -135,42 +134,62 @@ def test_cacl_tc3_replace_table(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    expected_content_list = ["TEST_1", "SSH"]
-    expect_res_success_acl_table(duthost, expected_content_list, [])
+        expected_content_list = ["TEST_1", "SSH"]
+        expect_res_success_acl_table(duthost, expected_content_list, [])
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc4_add_invalid_table(duthost):
-    """ Add invalid acl table with wrong type
+def cacl_add_invalid_table(duthost):
+    """ Add invalid acl table
     """
-    json_patch = [
-        {
-            "op": "add",
-            "path": "/ACL_TABLE/TEST_2",
-            "value": {
-                "policy_desc": "Test Table 2",
-                "services": [
-                 "SSH"
-                ],
-                "stage": "ingress",
-                "type": "CONTROLLING PLANE"
-            }
-        }
+    invalid_table = [
+        {"service":"SSH", "stage":"ogress", "type":"CTRLPLANE"}, # wrong stage
+        {"service":"SSH", "stage":"ingress", "type":"TRLPLANE"}  # wrong type
     ]
 
-    tmpfile = generate_tmpfile(duthost)
-    logger.info("tmpfile {}".format(tmpfile))
+    for ele in invalid_table:
+        json_patch = [
+            {
+                "op": "add",
+                "path": "/ACL_TABLE/TEST_2",
+                "value": {
+                    "policy_desc": "Test Table 2",
+                    "services": [
+                     "{}".format(ele["service"])
+                    ],
+                    "stage": "{}".format(ele["stage"]),
+                    "type": "{}".format(ele["type"])
+                }
+            }
+        ]
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_failure(output)
+        tmpfile = generate_tmpfile(duthost)
+        logger.info("tmpfile {}".format(tmpfile))
 
-    delete_tmpfile(duthost, tmpfile)
+        try:
+            output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+            expect_op_failure(output)
+        finally:
+            delete_tmpfile(duthost, tmpfile)
 
-def test_cacl_tc5_add_init_rule(duthost):
+
+def cacl_add_init_rule(duthost):
     """ Add acl rule for test
+
+    Check 'ip tables' to make sure rule is actually being applied
+    show command as below:
+    admin@vlab-01:~/test$ show acl rule
+    Table    Rule       Priority    Action    Match
+    -------  ---------  ----------  --------  ------------------
+    TEST_1   TEST_DROP  9998        DROP      IP_PROTOCOL: 6
+                                              IP_TYPE: IP
+                                              L4_DST_PORT: 22
+                                              SRC_IP: 9.9.9.9/32
+
     """
     json_patch = [
         {
@@ -192,15 +211,16 @@ def test_cacl_tc5_add_init_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    expected_content_list = ["-A INPUT -s 9.9.9.9/32 -p tcp -m tcp --dport 22 -j DROP"]
-    expect_res_success_acl_rule(duthost, expected_content_list, [])
+        expected_content_list = ["-A INPUT -s 9.9.9.9/32 -p tcp -m tcp --dport 22 -j DROP"]
+        expect_res_success_acl_rule(duthost, expected_content_list, [])
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc6_add_duplicate_rule(duthost):
+def cacl_add_duplicate_rule(duthost):
     """ Add duplicate acl rule for test
     """
     json_patch = [
@@ -221,13 +241,24 @@ def test_cacl_tc6_add_duplicate_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc7_replace_rule(duthost):
+def cacl_replace_rule(duthost):
     """ Replace a value from acl rule test
+
+    Check 'ip tables' to make sure rule is actually being applied
+    show command:
+    admin@vlab-01:~/test$ show acl rule
+    Table    Rule       Priority    Action    Match
+    -------  ---------  ----------  --------  ------------------
+    TEST_1   TEST_DROP  9998        DROP      IP_PROTOCOL: 6
+                                              IP_TYPE: IP
+                                              L4_DST_PORT: 22
+                                              SRC_IP: 8.8.8.8/32
     """
     json_patch = [
         {
@@ -240,17 +271,18 @@ def test_cacl_tc7_replace_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    expected_content_list = ["-A INPUT -s 8.8.8.8/32 -p tcp -m tcp --dport 22 -j DROP"]
-    unexpected_content_list = ["-A INPUT -s 9.9.9.9/32 -p tcp -m tcp --dport 22 -j DROP"]
-    expect_res_success_acl_rule(duthost, expected_content_list, unexpected_content_list)
+        expected_content_list = ["-A INPUT -s 8.8.8.8/32 -p tcp -m tcp --dport 22 -j DROP"]
+        unexpected_content_list = ["-A INPUT -s 9.9.9.9/32 -p tcp -m tcp --dport 22 -j DROP"]
+        expect_res_success_acl_rule(duthost, expected_content_list, unexpected_content_list)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc8_add_invalid_rule(duthost):
-    """ Add invalid acl rule for test
+def cacl_add_rule_to_unexisted_table(duthost):
+    """ Add acl rule to unexisted table
     """
     json_patch = [
         {
@@ -270,12 +302,13 @@ def test_cacl_tc8_add_invalid_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_failure(output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_failure(output)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc9_remove_table_before_rule(duthost):
+def cacl_remove_table_before_rule(duthost):
     """ Remove acl table before removing acl rule
     """
     json_patch = [
@@ -288,34 +321,38 @@ def test_cacl_tc9_remove_table_before_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_failure(output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_failure(output)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-@pytest.mark.parametrize("unexist_rule_or_table", [
-    ("/ACL_RULE/TEST_2|TEST_DROP"),
-    ("/ACL_TABLE/TEST_2")
-])
-def test_cacl_tc10_remove_unexist_rule_or_table(duthost, unexist_rule_or_table):
+def cacl_remove_unexist_rule_or_table(duthost):
     """ Remove unexisted acl rule or acl table
     """
-    json_patch = [
-        {
-            "op": "remove",
-            "path": "{}".format(unexist_rule_or_table)
-        }
+    unexist_rule_or_table = [
+        "/ACL_RULE/TEST_2|TEST_DROP", # unexisted rule
+        "/ACL_TABLE/TEST_2"           # unexisted table
     ]
 
-    tmpfile = generate_tmpfile(duthost)
-    logger.info("tmpfile {}".format(tmpfile))
+    for ele in unexist_rule_or_table:
+        json_patch = [
+            {
+                "op": "remove",
+                "path": "{}".format(ele)
+            }
+        ]
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_failure(output)
+        tmpfile = generate_tmpfile(duthost)
+        logger.info("tmpfile {}".format(tmpfile))
 
-    delete_tmpfile(duthost, tmpfile)
+        try:
+            output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+            expect_op_failure(output)
+        finally:
+            delete_tmpfile(duthost, tmpfile)
 
-def test_cacl_tc11_remove_rule(duthost):
+def cacl_remove_rule(duthost):
     """ Remove acl rule test
     """
     json_patch = [
@@ -328,15 +365,16 @@ def test_cacl_tc11_remove_rule(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    unexpected_content_list = ["-A INPUT -s 8.8.8.8/32 -p tcp -m tcp --dport 22 -j DROP"]
-    expect_res_success_acl_rule(duthost, [], unexpected_content_list)
+        unexpected_content_list = ["-A INPUT -s 8.8.8.8/32 -p tcp -m tcp --dport 22 -j DROP"]
+        expect_res_success_acl_rule(duthost, [], unexpected_content_list)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
-
-def test_cacl_tc12_remove_table(duthost):
+def cacl_remove_table(duthost):
     """ Remove acl table test
     """
     json_patch = [
@@ -349,10 +387,25 @@ def test_cacl_tc12_remove_table(duthost):
     tmpfile = generate_tmpfile(duthost)
     logger.info("tmpfile {}".format(tmpfile))
 
-    output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
-    expect_op_success(duthost, output)
+    try:
+        output = apply_patch(duthost, json_data=json_patch, dest_file=tmpfile)
+        expect_op_success(duthost, output)
 
-    unexpected_content_list = ["TEST_1"]
-    expect_res_success_acl_table(duthost, [], unexpected_content_list)
+        unexpected_content_list = ["TEST_1"]
+        expect_res_success_acl_table(duthost, [], unexpected_content_list)
+    finally:
+        delete_tmpfile(duthost, tmpfile)
 
-    delete_tmpfile(duthost, tmpfile)
+def test_cacl_tc1_suite(duthost):
+    cacl_add_init_table(duthost)
+    cacl_add_duplicate_table(duthost)
+    cacl_replace_table(duthost)
+    cacl_add_invalid_table(duthost)
+    cacl_add_init_rule(duthost)
+    cacl_add_duplicate_rule(duthost)
+    cacl_replace_rule(duthost)
+    cacl_add_rule_to_unexisted_table(duthost)
+    cacl_remove_table_before_rule(duthost)
+    cacl_remove_unexist_rule_or_table(duthost)
+    cacl_remove_rule(duthost)
+    cacl_remove_table(duthost)
