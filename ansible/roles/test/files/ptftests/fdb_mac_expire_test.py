ca1e63ca041f8e42ff904dc1977b0fdf50a7ef13
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2021-12-22 08:40:45 +0800

    Implement test case to verify dest MAC won't refresh FDB (#4815)
    Summary:
    Fixes #4281
    This PR is to add a test case to verify MAC aging will ignore dest MAC in packets.

--- ansible/roles/test/files/ptftests/fdb_mac_expire_test.py
+++ ansible/roles/test/files/ptftests/fdb_mac_expire_test.py
@@ -7,6 +7,11 @@ import ptf.dataplane as dataplane
 from ptf import config
 from ptf.base_tests import BaseTest
 from ptf.testutils import *
+import datetime
+import time
+
+DISABLE_REFRESH = "disable_refresh"
+REFRESH_DEST_MAC = "refresh_with_dest_mac"
 
 class FdbMacExpireTest(BaseTest):
     def __init__(self):
@@ -18,6 +23,9 @@ class FdbMacExpireTest(BaseTest):
         self.router_mac = self.test_params['router_mac']
         self.dummy_mac_prefix = self.test_params['dummy_mac_prefix']
         self.fdb_info = self.test_params['fdb_info']
+        self.refresh_type = self.test_params.get('refresh_type', DISABLE_REFRESH)
+        self.aging_time = self.test_params.get('aging_time', 0)
+        self.mac_table = []
     #--------------------------------------------------------------------------
     def populateFdb(self):
         self.fdb = fdb.Fdb(self.fdb_info)
@@ -30,8 +38,24 @@ class FdbMacExpireTest(BaseTest):
                                         eth_src=mac,
                                         eth_type=0x1234)
                 send(self, member, pkt)
+                # Save the MAC table for possible refresh
+                self.mac_table.append((member, mac))
+    
+    def refreshFdbWithInvalidPackets(self):
+        # Keep sending packets with Dest MAC address equals to router MAC for aging_time
+        # The FDB is not supposed to be refreshed on DUT
+        t1 = t0 = datetime.datetime.now()
+        while (t1 - t0).seconds < self.aging_time:
+            for member, mac in self.mac_table:
+                pkt = simple_eth_packet(eth_dst=mac, eth_type=0x1234)
+                send(self, member, pkt)
+            time.sleep(5)
+            t1 = datetime.datetime.now()
+            
     #--------------------------------------------------------------------------
     def runTest(self):
         self.populateFdb()
+        if self.refresh_type == REFRESH_DEST_MAC:
+            self.refreshFdbWithInvalidPackets()
 	return
     #--------------------------------------------------------------------------
