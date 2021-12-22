ed32c1bbd7a219fcb1d9b232944da4f37869afa7
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2021-12-22 14:21:49 +0800

    Add new test case to verify router mac is not learnt (#4813)
    Signed-off-by: bingwang <bingwang@microsoft.com>

--- tests/fdb/test_fdb.py
+++ tests/fdb/test_fdb.py
@@ -9,6 +9,7 @@ import itertools
 import logging
 import pprint
 import re
+import random
 
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses      # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import remove_ip_addresses       # lgtm[py/unused-import]
@@ -215,6 +216,7 @@ def setup_fdb(ptfadapter, vlan_table, router_mac, pkt_type, dummy_mac_count):
 
     return fdb
 
+
 def validate_mac(mac):
     if mac.find(':') != -1:
         pattern = re.compile(r"^([0-9a-fA-F]{2,2}:){5,5}[0-9a-fA-F]{2,2}$")
@@ -308,33 +310,52 @@ def test_fdb(ansible_adhoc, ptfadapter, duthosts, rand_one_dut_hostname, ptfhost
                 send_recv_eth(ptfadapter, src_ports, src_mac, dst_ports, dst_mac, src_vlan, dst_vlan)
 
     # Should we have fdb_facts ansible module for this test?
-    res = duthost.command('show mac')
-    logger.info('"show mac" output on DUT:\n{}'.format(pprint.pformat(res['stdout_lines'])))
+    fdb_fact = duthost.fdb_facts()['ansible_facts']
+    logger.info('fdb facts on DUT:\n{}'.format(pprint.pformat(fdb_fact)))
 
     dummy_mac_count = 0
     total_mac_count = 0
-    for l in res['stdout_lines']:
-        # No. Vlan MacAddress Port Type
-        items = l.split()
-        if len(items) != 5:
-            continue
-        # First item must be number
-        if not items[0].isdigit():
-            continue
-        vlan_id = int(items[1])
-        mac = items[2]
-        ifname = items[3]
-        fdb_type = items[4]
-        assert ifname in interface_table
-        assert vlan_id in interface_table[ifname]
-        assert validate_mac(mac) == True
-        assert fdb_type in ['Dynamic', 'Static']
-        if DUMMY_MAC_PREFIX in l.lower():
+    for k, v in fdb_fact.items():
+        assert v['port'] in interface_table
+        assert v['vlan'] in interface_table[ifname]
+        assert validate_mac(k) == True
+        assert v['type'] in ['Dynamic', 'Static']
+        if DUMMY_MAC_PREFIX in k.lower():
             dummy_mac_count += 1
-        if "dynamic" in l.lower():
+        if "dynamic" in k.lower():
             total_mac_count += 1
 
     assert vlan_member_count > 0
 
     # Verify that the number of dummy MAC entries is expected
     assert dummy_mac_count == configured_dummay_mac_count * vlan_member_count
+
+
+@pytest.mark.parametrize("pkt_type", PKT_TYPES)
+def test_self_mac_not_learnt(ptfadapter, rand_selected_dut, pkt_type, toggle_all_simulator_ports_to_rand_selected_tor_m, tbinfo):
+    """
+    Verify self mac will not be learnt.
+    """
+    # Clear existing FDB entry from DUT
+    rand_selected_dut.command('sonic-clear fdb all')
+    # Sleep some time to ensure clear is done
+    time.sleep(5)
+    if pkt_type == "cleanup":
+        return
+
+    mg_facts = rand_selected_dut.get_extended_minigraph_facts(tbinfo)
+    port_index = random.choice(list(mg_facts['minigraph_ptf_indices'].values()))
+    self_mac = rand_selected_dut.facts['router_mac']
+    dummy_mac = "00:22:33:44:33:22"
+    if pkt_type == "ethernet":
+        send_eth(ptfadapter, port_index, self_mac, dummy_mac, 0)
+    elif pkt_type == "arp_request":
+        send_arp_request(ptfadapter, port_index, self_mac, dummy_mac, 0)
+    elif pkt_type == "arp_reply":
+        send_arp_reply(ptfadapter, port_index, self_mac, dummy_mac, 0)
+    # Sleep some time to ensure FDB is populate
+    time.sleep(5)
+    # Verify that self mac is not learnt
+    fdb_facts = rand_selected_dut.fdb_facts()['ansible_facts']
+    pytest_assert(self_mac not in fdb_facts,
+                    "Self-mac {} is not supposed to be learnt".format(self_mac))
