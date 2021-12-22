fb8dbf7770fa54709c4430f62e3ffc5af99e470f
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2021-12-22 15:26:20 +0800

    [test_gap] Need 4 vlans for dhcp relay/dhcpv6 relay (#4859)
    What is the motivation for this PR?
    Need new test: DHCP Relay #3599
    
    How did you do it?
    Add four vlans config in related t0 topo xml files.
    dhcp relay service has only 3 times restart limit, for 4 vlan config, it should restart 4 times for test_dhcp_relay_start_with_uplinks_down case, so add 'systemctl reset-failed dhcp_relay' to avoid the restart failure.
    
    How did you verify/test it?
    Deploy updated t0 topo, run test_dhcp_relay or test_dhcpv6_relay to verify.
    
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

--- tests/dhcp_relay/test_dhcp_relay.py
+++ tests/dhcp_relay/test_dhcp_relay.py
@@ -351,7 +351,10 @@ def test_dhcp_relay_start_with_uplinks_down(ptfhost, dut_dhcp_relay_data, valida
                       "Not all uplinks go down")
 
         # Restart DHCP relay service on DUT
-        duthost.shell('systemctl restart dhcp_relay.service')
+        # dhcp_relay service has 3 times restart limit in 20 mins, for 4 vlans config it will hit the maximum limit
+        # reset-failed before restart service
+        cmds = ['systemctl reset-failed dhcp_relay', 'systemctl restart dhcp_relay']
+        duthost.shell_cmds(cmds=cmds)
 
         # Sleep to give the DHCP relay container time to start up and
         # allow the relay agent to begin listening on the down interfaces
