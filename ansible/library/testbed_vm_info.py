ba0ae2afbc6d6940c40c88edbee4e4f9d268a5c5
Xin Wang <xiwang5@microsoft.com>
2021-12-23 10:44:45 +0800

    Improve infrastructure to support over 100 VMs per server (#4862)
    What is the motivation for this PR?
    Currently name of VMs running in test server follows the pattern of "VMXXXX". Each "X" is a decimal digit. The first
    two digits usually are for server. The last 2 digits are for VMs. Because of this limitation, at most 100 VMs can
    be defined for a test server. After the T2 topology is introduced, a single testbed may need more than 100 VMs as
    neighbors. We need to get rid of this limitation to support over 100 VMs on single test server.
    
    How did you do it?
    This change updated the infrastructure to get rid of this limitation. Now the VM name can have 4 digits or more.
    
    How did you verify/test it?
    Run below tasks for both VMs with 4 digits and 5 digits.
    * Test run add-topo, remove-topo, deploy-mg
    * Test run scripts test_bgp_facts.py, test_nbr_health.py
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- ansible/library/testbed_vm_info.py
+++ ansible/library/testbed_vm_info.py
@@ -2,13 +2,8 @@
 
 import re
 import yaml
-import os
 import traceback
-import subprocess
 import ipaddr as ipaddress
-from operator import itemgetter
-from itertools import groupby
-from collections import defaultdict
 import re
 
 from ansible.parsing.dataloader import DataLoader
@@ -17,13 +12,13 @@ from ansible.inventory.manager import InventoryManager
 DOCUMENTATION = '''
 module: testbed_vm_info.py
 Ansible_version_added:  2.0.0.2
-short_description:   Gather all related VMs info
+short_description: Gather all related VMs info
 Description:
        When deploy testbed topology with VM connected to SONiC, gather neighbor VMs info for generating SONiC minigraph file
  options:
     base_vm:  base vm name defined in testbed.csv for the deployed topology; required: True
     topo:     topology name defined in testbed.csv for the deployed topology; required: True
-    vm_file:  the virtual machine file path ; default: 'veos'
+    vm_file:  the virtual machine file path; default: 'veos'
 
 Ansible_facts:
     'neighbor_eosvm_mgmt':  all VM hosts management IPs
@@ -36,7 +31,7 @@ EXAMPLES = '''
       testbed_vm_info: base_vm='VM0100' topo='t1' vm_file='veos'
 '''
 
-### Here are the assumption/expectation of files to gather VM informations, if the file location or name changes, please modify it here
+### Here are the assumption/expectation of files to gather VM information, if the file location or name changes, please modify it here
 TOPO_PATH = 'vars/'
 VM_INV_FILE = 'veos'
 TGEN_MGMT_NETWORK = '10.65.32.0/24'
@@ -48,28 +43,25 @@ class TestbedVMFacts():
 
     """
 
-    def __init__(self, toponame, vmbase, vmfile):
+    def __init__(self, toponame, base_vm, vm_file):
         CLET_SUFFIX = "-clet"
         toponame = re.sub(CLET_SUFFIX + "$", "", toponame)
         self.topofile = TOPO_PATH+'topo_'+toponame +'.yml'
-        if vmbase != '':
-            self.start_index = int(re.findall('VM(\d+)', vmbase)[0])
-        else:
-            self.start_index = 0
-        self.vmhosts = {}
-        self.vmfile = vmfile
-        self.inv_mgr = InventoryManager(loader=DataLoader(), sources=self.vmfile)
-        return
-
+        self.base_vm = base_vm
+        self.vm_file = vm_file
+        self.inv_mgr = InventoryManager(loader=DataLoader(), sources=self.vm_file)
 
     def get_neighbor_eos(self):
         eos = {}
         with open(self.topofile) as f:
             vm_topology = yaml.safe_load(f)
         self.topoall = vm_topology
-        for  vm in vm_topology['topology']['VMs']:
-            vm_index = int(vm_topology['topology']['VMs'][vm]['vm_offset'])+self.start_index
-            eos[vm] = vm_index
+
+        vm_base = int(self.base_vm[2:])
+        vm_name_fmt = 'VM%0{}d'.format(len(self.base_vm) - 2)
+        for eos_name, eos_value in vm_topology['topology']['VMs'].items():
+            vm_name = vm_name_fmt % (vm_base + eos_value['vm_offset'])
+            eos[eos_name] = vm_name
         return eos
 
 
@@ -86,26 +78,30 @@ def main():
     topo_type = m_args['topo']
     if 'ptf' in topo_type:
         module.exit_json(ansible_facts={'neighbor_eosvm_mgmt': {}})
+
+    vm_mgmt_ip = {}
     try:
-        vmsall = TestbedVMFacts(m_args['topo'], m_args['base_vm'], m_args['vm_file'])
-        neighbor_eos = vmsall.get_neighbor_eos()
+        vm_facts = TestbedVMFacts(m_args['topo'], m_args['base_vm'], m_args['vm_file'])
+        neighbor_eos = vm_facts.get_neighbor_eos()
+
         tgen_mgmt_ips = list(ipaddress.IPNetwork(unicode(TGEN_MGMT_NETWORK)))
         for index, eos in enumerate(neighbor_eos):
-            vmname = 'VM'+format(neighbor_eos[eos], '04d')
+            vm_name = neighbor_eos[eos]
             if 'tgen' in topo_type:
-                vmsall.vmhosts[eos] = str(tgen_mgmt_ips[index])
-            elif vmname in vmsall.inv_mgr.hosts:
-                vmsall.vmhosts[eos] = vmsall.inv_mgr.get_host(vmname).get_vars()['ansible_host']
+                vm_mgmt_ip[eos] = str(tgen_mgmt_ips[index])
+            elif vm_name in vm_facts.inv_mgr.hosts:
+                vm_mgmt_ip[eos] = vm_facts.inv_mgr.get_host(vm_name).get_vars()['ansible_host']
             else:
-                err_msg = "cannot find the vm " + vmname + " in VM inventory file, please make sure you have enough VMs for the topology you are using"
+                err_msg = "Cannot find the vm {} in VM inventory file {}, please make sure you have enough VMs" \
+                          "for the topology you are using."
+                err_msg.format(vm_name, vm_facts.vm_file)
                 module.fail_json(msg=err_msg)
-        module.exit_json(ansible_facts={'neighbor_eosvm_mgmt':vmsall.vmhosts, 'topoall': vmsall.topoall})
+        module.exit_json(ansible_facts={'neighbor_eosvm_mgmt':vm_mgmt_ip, 'topoall': vm_facts.topoall})
     except (IOError, OSError):
-        module.fail_json(msg="Can not find file "+vmsall.topofile+" or "+m_args['vm_file']+" or "+VM_INV_FILE)
+        module.fail_json(msg='Can not find VM file {} or {}'.format(m_args['vm_file'], VM_INV_FILE))
     except Exception as e:
         module.fail_json(msg=traceback.format_exc())
 
 from ansible.module_utils.basic import *
 if __name__ == "__main__":
     main()
-
