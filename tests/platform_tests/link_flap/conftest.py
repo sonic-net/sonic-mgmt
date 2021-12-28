7e5d45d904a0c7187c9c199c276f2f935d571958
Nana@Nvidia <78413612+nhe-NV@users.noreply.github.com>
2021-12-28 16:26:58 +0800

    Shutdown all bgp sessions before start the link flap test (#4874)
    The link flap test failed due to the oper_state of the interface will be change to UP after 30s.
    This happens due to 3 facts: orchagent being single threaded, SPC1 has weaker CPU and SDK time to configure routes.
    The port comes up in ~10 sec actually if you look at dmesg. But, when you shutdown the port orchagent starts to updated 6k ipv4 and 6k ipv6 routes with new ECMP without the neighbor behind the port that is shutdown - this takes a while, now we startup the port, frr starts sending updates to orchagent and now we are adding back the neighbor (6k ipv4 and 6k ipv6 routes), this takes a while and only then the orchagent queue is free to process a notification that the port is up.
    
    So shutdown the bgp sessions before this test to stabilize the test
    
    - What is the motivation for this PR?
    make the test_link_flap test case much more stable, the fix for #4873
    
    - How did you do it?
    Shutdown all bgp sessions before start the link flap test
    
    - How did you verify/test it?
    run the test_link_flap, it can pass on the testbed which got failure.
    
    - Any platform specific information?
    No

--- tests/platform_tests/link_flap/conftest.py
+++ tests/platform_tests/link_flap/conftest.py
@@ -4,6 +4,12 @@ Pytest configuration used by the link flap tests.
 Teardowns used by the link flap tests.
 """
 import pytest
+import logging
+from tests.common.utilities import wait_until
+
+logger = logging.getLogger()
+
+
 def pytest_addoption(parser):
     """
     Adds options to pytest that are used by the Link flap tests.
@@ -17,7 +23,24 @@ def pytest_addoption(parser):
         help="Orchagent CPU threshold",
     )
 
+
 @pytest.fixture(scope='module')
 def get_loop_times(pytestconfig):
     return pytestconfig.getoption("--loop_times")
 
+
+@pytest.fixture()
+def bgp_sessions_config(duthost):
+    logger.info("Stop all bgp sessions")
+    duthost.command('sudo config bgp shutdown all')
+    logger.info("Wait all bgp sessions are down")
+    wait_until(60, 10, 0, check_bgp_is_shutdown, duthost)
+    yield
+    logger.info("Start all bgp sessions")
+    duthost.command('sudo config bgp startup all')
+
+
+def check_bgp_is_shutdown(duthost):
+    logger.info("checking bgp status")
+    return not duthost.command("show ip route bgp")["stdout_lines"] and \
+           not duthost.command("show ipv6 route bgp")["stdout_lines"]
