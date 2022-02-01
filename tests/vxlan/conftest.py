910266eff524f6aec955bcc47b0d322cdb69d212
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2022-01-31 20:45:05 -0800

    Overlay ECMP VxLAN Test Cases for T1 and T1-Lag: TC1 (#4954)
    * Test script for VxLAN ECMP on T1/T1-Lag Topology.

--- tests/vxlan/conftest.py
+++ tests/vxlan/conftest.py
@@ -6,6 +6,14 @@ def pytest_addoption(parser):
     vxlan_group = parser.getgroup("VXLAN test suite options")
 
     vxlan_group.addoption(
+        "--vxlan_port",
+        action="store",
+        default=4789,
+        type=int,
+        help="The UDP port to use for VxLAN. It must be a viable UDP port - not one of the already used standard protocol ports"
+    )
+
+    vxlan_group.addoption(
         "--num_vnet",
         action="store",
         default=8,
@@ -71,3 +79,47 @@ def pytest_addoption(parser):
         help="Highest expected src port for VXLAN UPD packet"
     )
 
+    # ECMP options
+    vxlan_group.addoption(
+        "--total_number_of_endpoints",
+        action="store",
+        default=2,
+        type=int,
+        help="Total number of uniq endpoints that can be used in the DUT"
+    )
+
+    vxlan_group.addoption(
+        "--ecmp_nhs_per_destination",
+        action="store",
+        default=1,
+        type=int,
+        help="ECMP: Number of tunnel endpoints to provide for each tunnel destination"
+    )
+
+    vxlan_group.addoption(
+        "--debug_enabled",
+        action="store_true",
+        help="Enable debugging the script. The config file names will *not* be time-stamped, every run of the script will over-write the previously created config files."
+    )
+
+    vxlan_group.addoption(
+        "--keep_temp_files",
+        action="store_true",
+        help="This will keep the config files in the DUT and PTF."
+    )
+
+    vxlan_group.addoption(
+        "--dut_hostid",
+        default=1,
+        type=int,
+        help="This is the host part of the IP addresses for interfaces in the DUT to be used in this script."
+    )
+
+    # This will decide the number of destinations.
+    vxlan_group.addoption(
+        "--total_number_of_nexthops",
+        action="store",
+        default=2, # Max: 32k, 64K, or 128 K
+        type=int,
+        help="ECMP: Number of tunnel nexthops to be tested. (number of nhs_per_destination X number_of_destinations)"
+    )
