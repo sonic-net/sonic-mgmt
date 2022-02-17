42544e08f5c2ba83b619354eadb19a32d4b483d7
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2022-02-17 17:02:38 +0800

    Update test_escape_character.py

--- tests/dut_console/test_escape_character.py
+++ tests/dut_console/test_escape_character.py
@@ -9,18 +9,14 @@ from tests.common.helpers.assertions import pytest_assert
 TOTAL_PACKETS = 100
 logger = logging.getLogger(__name__)
 
-def test_console_escape(duthost_console):
-    """
-    Test reverse SSH are working as expect.
-    Verify serial session is available after connect DUT via reverse SSH
-    """
-    try:
-        child = pexpect.spawn("ping 127.0.0.1 -c {} -i 1".format(TOTAL_PACKETS))
-        time.sleep(5)
-        child.sendcontrol('C')
-        child.expect("\^C")
-        match = re.search(r'(\d) packets transmitted', child.read())
-        pytest_assert(int(match.group(1)) < TOTAL_PACKETS, "Escape Character does not work.")
-    except Exception as e:
-        pytest.fail("Not able to login DUT via console")
+def test_console_escape(duthost_console, duthost):
+    if duthost.facts["asic_type"] == "vs":
+        pytest.skip("Real console session is supported on physical testbed.")
+    child = pexpect.spawn("ping 127.0.0.1 -c {} -i 1".format(TOTAL_PACKETS))
+    time.sleep(5)
+    child.sendcontrol('C')
+    child.expect("\^C")
+    match = re.search(r'(\d) packets transmitted', child.read())
+    pytest_assert(int(match.group(1)) < TOTAL_PACKETS, "Escape Character does not work.")
+
 
