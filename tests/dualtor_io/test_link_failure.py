338739842b99503e95124c77e58700c0f8aa711d
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2022-02-22 20:49:04 -0800

    [dualToR] Add test to cover the scenario of a port being set to admin down (#5137)
    What is the motivation for this PR?
    Add test gap for #4910
    
    How did you do it?
    Add 2 new fixtures to shutdown downlink interfaces on upper tor or lower tor shutdown_upper_tor_downlink_intfs() and shutdown_lower_tor_downlink_intfs().
    Add 6 new test cases to cover different scenarios for upstream or downstream traffic.
    
    How did you verify/test it?
    Run the following test cases in dualtor_io/test_link_failure.py:
    
    test_active_tor_downlink_down_upstream
    test_active_tor_downlink_down_downstream_active
    test_active_tor_downlink_down_downstream_standby
    test_standby_tor_downlink_down_upstream
    test_standby_tor_downlink_down_downstream_active
    test_standby_tor_downlink_down_downstream_standby
    
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

--- tests/dualtor_io/test_link_failure.py
+++ tests/dualtor_io/test_link_failure.py
@@ -3,7 +3,8 @@ import pytest
 from tests.common.dualtor.control_plane_utils import verify_tor_states
 from tests.common.dualtor.data_plane_utils import send_t1_to_server_with_action, send_server_to_t1_with_action                                  # lgtm[py/unused-import]
 from tests.common.dualtor.dual_tor_utils import upper_tor_host, lower_tor_host, shutdown_fanout_upper_tor_intfs, \
-                                                shutdown_fanout_lower_tor_intfs, upper_tor_fanouthosts, lower_tor_fanouthosts                   # lgtm[py/unused-import]
+                                                shutdown_fanout_lower_tor_intfs, upper_tor_fanouthosts, lower_tor_fanouthosts, \
+                                                shutdown_upper_tor_downlink_intfs, shutdown_lower_tor_downlink_intfs                   # lgtm[py/unused-import]
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_upper_tor                                                  # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import run_icmp_responder, run_garp_service, copy_ptftests_directory, change_mac_addresses             # lgtm[py/unused-import]
 from tests.common.dualtor.constants import MUX_SIM_ALLOWED_DISRUPTION_SEC
@@ -125,6 +126,125 @@ def test_standby_link_down_downstream_standby(
     send_t1_to_server_with_action(
         lower_tor_host, verify=True, delay=MUX_SIM_ALLOWED_DISRUPTION_SEC,
         allowed_disruption=2, action=shutdown_fanout_lower_tor_intfs
+    )
+    verify_tor_states(
+        expected_active_host=upper_tor_host,
+        expected_standby_host=lower_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+
+def test_active_tor_downlink_down_upstream(
+    upper_tor_host, lower_tor_host, send_server_to_t1_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_upper_tor_downlink_intfs
+):
+    """
+    Send traffic from server to T1 and shutdown the active ToR downlink on DUT.
+    Verify switchover and disruption lasts < 1 second
+    """
+    send_server_to_t1_with_action(
+        upper_tor_host, verify=True, delay=MUX_SIM_ALLOWED_DISRUPTION_SEC,
+        allowed_disruption=1, action=shutdown_upper_tor_downlink_intfs
+    )
+    verify_tor_states(
+        expected_active_host=lower_tor_host,
+        expected_standby_host=upper_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+def test_active_tor_downlink_down_downstream_active(
+    upper_tor_host, lower_tor_host, send_t1_to_server_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_upper_tor_downlink_intfs
+):
+    """
+    Send traffic from T1 to active ToR and shutdown the active ToR downlink on DUT.
+    Verify switchover and disruption lasts < 1 second
+    """
+    send_t1_to_server_with_action(
+        upper_tor_host, verify=True, delay=MUX_SIM_ALLOWED_DISRUPTION_SEC,
+        allowed_disruption=1, action=shutdown_upper_tor_downlink_intfs
+    )
+    verify_tor_states(
+        expected_active_host=lower_tor_host,
+        expected_standby_host=upper_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+
+def test_active_tor_downlink_down_downstream_standby(
+    upper_tor_host, lower_tor_host, send_t1_to_server_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_upper_tor_downlink_intfs
+):
+    """
+    Send traffic from T1 to standby ToR and shutdown the active ToR downlink on DUT.
+    Verify switchover and disruption lasts < 1 second
+    """
+    send_t1_to_server_with_action(
+        lower_tor_host, verify=True, delay=MUX_SIM_ALLOWED_DISRUPTION_SEC,
+        allowed_disruption=1, action=shutdown_upper_tor_downlink_intfs
+    )
+    verify_tor_states(
+        expected_active_host=lower_tor_host,
+        expected_standby_host=upper_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+
+def test_standby_tor_downlink_down_upstream(
+    upper_tor_host, lower_tor_host, send_server_to_t1_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_lower_tor_downlink_intfs
+):
+    """
+    Send traffic from server to T1 and shutdown the standby ToR downlink on DUT.
+    Verify no switchover and no disruption
+    """
+    send_server_to_t1_with_action(
+        upper_tor_host, verify=True,
+        allowed_disruption=0, action=shutdown_lower_tor_downlink_intfs
+    )
+    verify_tor_states(
+        expected_active_host=upper_tor_host,
+        expected_standby_host=lower_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+
+def test_standby_tor_downlink_down_downstream_active(
+    upper_tor_host, lower_tor_host, send_t1_to_server_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_lower_tor_downlink_intfs
+):
+    """
+    Send traffic from T1 to active ToR and shutdown the standby ToR downlink on DUT.
+    Confirm no switchover and no disruption
+    """
+    send_t1_to_server_with_action(
+        upper_tor_host, verify=True,
+        allowed_disruption=0, action=shutdown_lower_tor_downlink_intfs
+    )
+    verify_tor_states(
+        expected_active_host=upper_tor_host,
+        expected_standby_host=lower_tor_host,
+        expected_standby_health='unhealthy'
+    )
+
+
+def test_standby_tor_downlink_down_downstream_standby(
+    upper_tor_host, lower_tor_host, send_t1_to_server_with_action,
+    toggle_all_simulator_ports_to_upper_tor,
+    shutdown_lower_tor_downlink_intfs
+):
+    """
+    Send traffic from T1 to standby ToR and shutdwon the standby ToR downlink on DUT.
+    Confirm no switchover and no disruption
+    """
+    send_t1_to_server_with_action(
+        lower_tor_host, verify=True,
+        allowed_disruption=0, action=shutdown_lower_tor_downlink_intfs
     )
     verify_tor_states(
         expected_active_host=upper_tor_host,
