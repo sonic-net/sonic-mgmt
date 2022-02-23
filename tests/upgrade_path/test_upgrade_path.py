032eac69cc47b5dc9cfa81e4d2d8bd789c79be9d
Vaibhav Hemant Dixit <vaibhav.dixit@microsoft.com>
2022-02-23 09:34:19 -0800

    [advanced-reboot] Add KVM warmboot test support for vlan oper down case (#5162)
    Test enahancements to cover warmboot with SAD case scenarios on KVM platform.
    These cases already work on physical testbed where there is fanout switch, and to simulate vlan-oper-down, mapped ports on fanout switch is shutdown. However, on virtual testbed, there is no fanout. Hence, to simulate vlan port oper-down condition, a different mechanism is needed.
    
    How did you do it?
    Check if DUT is KVM platform. If yes, shutdown virsh domain ports on vmhost (server hosting entire testbed).
    The port is found via the DUT port index which is selected for the test.
    The goal is to execute down/up command on vmhost for mapped ports -
    Example:
    `virsh domif-setlink vlab-01 vlab-01-3 down`
    `virsh domif-setlink vlab-01 vlab-01-3 up`
    
    How did you verify/test it?
    Tested on KVM testbed - vlan ports now can go oper-down and UP as part of the test.

--- tests/upgrade_path/test_upgrade_path.py
+++ tests/upgrade_path/test_upgrade_path.py
@@ -79,7 +79,7 @@ def test_upgrade_path(localhost, duthosts, ptfhost, rand_one_dut_hostname, nbrho
 
 
 @pytest.mark.device_type('vs')
-def test_warm_upgrade_sad_path(localhost, duthosts, ptfhost, rand_one_dut_hostname, nbrhosts, fanouthosts, tbinfo,
+def test_warm_upgrade_sad_path(localhost, duthosts, ptfhost, rand_one_dut_hostname, nbrhosts, fanouthosts, vmhost, tbinfo,
                         restore_image, get_advanced_reboot, verify_dut_health, advanceboot_loganalyzer,
                         upgrade_path_lists, backup_and_restore_config_db, advanceboot_neighbor_restore,
                         sad_case_type):
@@ -104,7 +104,7 @@ def test_warm_upgrade_sad_path(localhost, duthosts, ptfhost, rand_one_dut_hostna
             install_sonic(duthost, to_image, tbinfo)
             advancedReboot = get_advanced_reboot(rebootType=get_reboot_command(duthost, "warm"),\
                 advanceboot_loganalyzer=advanceboot_loganalyzer)
-            sad_preboot_list, sad_inboot_list = get_sad_case_list(duthost, nbrhosts, fanouthosts, tbinfo, sad_case_type)
+            sad_preboot_list, sad_inboot_list = get_sad_case_list(duthost, nbrhosts, fanouthosts, vmhost, tbinfo, sad_case_type)
             advancedReboot.runRebootTestcase(
                 prebootList=sad_preboot_list,
                 inbootList=sad_inboot_list
