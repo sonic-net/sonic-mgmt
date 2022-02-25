170da8b0adf578ba15bda3fdbeb911c6801a5feb
Jingwen Xie <jingwenxie@microsoft.com>
2022-02-25 07:47:59 +0000

    Clean up monitor config before test

--- tests/generic_config_updater/test_monitor_config.py
+++ tests/generic_config_updater/test_monitor_config.py
@@ -10,10 +10,10 @@ logger = logging.getLogger(__name__)
 
 MONITOR_CONFIG_TEST_CP        = "monitor_config_test"
 MONITOR_CONFIG_INITIAL_CP     = "monitor_config_initial"
-MONITOR_CONFIG_ACL_TABLE      = "EVERFLOW_DSCP_TEST"
+MONITOR_CONFIG_ACL_TABLE      = "EVERFLOW_DSCP"
 MONITOR_CONFIG_ACL_RULE       = "RULE_1"
-MONITOR_CONFIG_MIRROR_SESSION = "mirror_session_dscp_test"
-MONITOR_CONFIG_POLICER        = "everflow_policer_test"
+MONITOR_CONFIG_MIRROR_SESSION = "mirror_session_dscp"
+MONITOR_CONFIG_POLICER        = "policer_dscp"
 
 @pytest.fixture(scope='module')
 def get_valid_acl_ports(cfg_facts):
@@ -35,6 +35,22 @@ def get_valid_acl_ports(cfg_facts):
             ports.add(key)
 
     return list(ports)
+
+def bgp_monitor_config_cleanup(duthost):
+    """ Test requires no monitor config
+    Clean up current monitor config if existed
+    """
+    cmds = []
+    cmds.append('sonic-db-cli CONFIG_DB del "ACL_TABLE|{}"'.format(MONITOR_CONFIG_ACL_TABLE))
+    cmds.append('sonic-db-cli CONFIG_DB del "ACL_RULE|{}|{}"'.format(MONITOR_CONFIG_ACL_TABLE, MONITOR_CONFIG_ACL_RULE))
+    cmds.append('sonic-db-cli CONFIG_DB del "MIRROR_SESSION|{}"'.format(MONITOR_CONFIG_MIRROR_SESSION))
+    cmds.append('sonic-db-cli CONFIG_DB del "POLICER|everflow_static_policer"'.format(MONITOR_CONFIG_POLICER))
+
+    output = duthost.shell_cmds(cmds=cmds)['results']
+    for res in output:
+        pytest_assert(not res['rc'],
+            "bgp monitor config cleanup failed."
+        )
 
 @pytest.fixture(autouse=True)
 def setup_env(duthosts, rand_one_dut_hostname):
@@ -184,7 +200,8 @@ def monitor_config_add_config(duthost, get_valid_acl_ports):
 def test_monitor_config_tc1_suite(duthost, get_valid_acl_ports):
     """ Test enable/disable EverflowAlwaysOn config
     """
-    # Step 1: Create checkpoint at initial state
+    # Step 1: Create checkpoint at initial state where no monitor config exist
+    bgp_monitor_config_cleanup(duthost)
     create_checkpoint(duthost, MONITOR_CONFIG_INITIAL_CP)
 
     # Step 2: Add EverflowAlwaysOn config to duthost
