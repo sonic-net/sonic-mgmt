53408a3e1dd1c16ba6d14fef6660728cf62a1d7f
Jingwen Xie <jingwenxie@microsoft.com>
2022-03-02 07:25:36 +0000

    fix typo

--- tests/generic_config_updater/test_bgp_prefix.py
+++ tests/generic_config_updater/test_bgp_prefix.py
@@ -204,7 +204,7 @@ def bgp_prefix_tc1_remove(duthost, community):
     finally:
         delete_tmpfile(duthost, tmpfile)
 
-@pytest.mark.parametrize("commnunity", ["empty", "1010:1010"])
+@pytest.mark.parametrize("community", ["empty", "1010:1010"])
 def test_bgp_prefix_tc1_suite(duthost, community):
     """ Test suite for bgp prefix for v4 and v6 w/ and w/o community ID
 
