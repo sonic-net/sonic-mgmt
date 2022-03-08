bc44e54a0299a4bb806d0e82aacec39facad90fb
Stephen Sun <stephens@nvidia.com>
2022-03-08 11:13:03 +0000

    Adjust platform API test for SN2201
    On SN2201, RJ45 ports are also implemented by SFP API.
    Test case needs to be adjusted accordingly
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- tests/platform_tests/api/test_chassis.py
+++ tests/platform_tests/api/test_chassis.py
@@ -427,6 +427,10 @@ class TestChassisApi(PlatformApiTestBase):
 
         if duthost.facts.get("chassis"):
             expected_num_sfps = len(duthost.facts.get("chassis").get('sfps'))
+            if duthost.facts.get("platform") == 'x86_64-nvidia_sn2201-r0':
+                # On SN2201, there are 48 RJ45 ports which are also counted in SFP object lists
+                # So we need to adjust test case accordingly
+                expected_num_sfps += 48
             pytest_assert(num_sfps == expected_num_sfps,
                           "Number of sfps ({}) does not match expected number ({})"
                           .format(num_sfps, expected_num_sfps))
