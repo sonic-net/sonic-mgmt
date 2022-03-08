4ecf4eb68f553ccdb33dc924b72bb555df96fcec
Xin Wang <wangxin.wang@gmail.com>
2022-03-08 21:03:57 +0800

    Revert "[inner hahsing] extension of PBH tests (#4848)" (#5282)
    This reverts commit 9008381a43f3fc194662a04f8ef816771e831e26.
    
    Reverts #4848
    
    PR #4848 depends on pytest-allure package which is only available to user “AzDevOps” in sonic-mgmt docker. The reason is that the package is installed to local path of that user:
    
    AzDevOps@2fb15ed04ed5:~$ python
    Python 2.7.17 (default, Feb 27 2021, 15:10:58)
    [GCC 7.5.0] on linux2
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import allure
    >>> allure.__file__
    '/var/AzDevOps/.local/lib/python2.7/site-packages/allure.pyc'
    AzDevOps@2fb15ed04ed5:~$ sudo su
    root@2fb15ed04ed5:/var/AzDevOps# pip list | grep allure
    DEPRECATION: The default format will switch to columns in the future. You can use --format=(legacy|columns) (or define a format=(legacy|columns) in your pip.conf under the [list] section) to disable this warning.
    root@2fb15ed04ed5:/var/AzDevOps# python
    Python 2.7.17 (default, Feb 27 2021, 15:10:58)
    [GCC 7.5.0] on linux2
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import allure
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    ImportError: No module named allure
    >>> exit()
    
    If use “root” user to run the test, issue #5280 will be hit.
    
    I’ll revert #4848 for now. The sonic-mgmt docker file https://github.com/Azure/sonic-buildimage/blob/master/dockers/docker-sonic-mgmt/Dockerfile.j2 needs to be updated to ensure that package pytest-allure is installed to global directory.

--- tests/ecmp/inner_hashing/test_wr_inner_hashing.py
+++ tests/ecmp/inner_hashing/test_wr_inner_hashing.py
@@ -2,12 +2,11 @@ import logging
 import threading
 import pytest
 import random
-import allure
 
 from datetime import datetime
 from tests.common import reboot
 from tests.ecmp.inner_hashing.conftest import get_src_dst_ip_range, FIB_INFO_FILE_DST, VXLAN_PORT,\
-    PTF_QLEN, OUTER_ENCAP_FORMATS, NVGRE_TNI
+    PTF_QLEN, OUTER_ENCAP_FORMATS
 from tests.ptf_runner import ptf_runner
 
 logger = logging.getLogger(__name__)
@@ -18,64 +17,62 @@ pytestmark = [
 ]
 
 @pytest.mark.dynamic_config
-class TestWRDynamicInnerHashing():
+class TestDynamicInnerHashing():
 
     @pytest.fixture(scope="class", autouse=True)
     def setup_dynamic_pbh(self, request):
-        with allure.step('Config Dynamic PBH'):
-            request.getfixturevalue("config_pbh_table")
-            request.getfixturevalue("config_hash_fields")
-            request.getfixturevalue("config_hash")
-            request.getfixturevalue("config_rules")
+        request.getfixturevalue("config_pbh_table")
+        request.getfixturevalue("config_hash_fields")
+        request.getfixturevalue("config_hash")
+        request.getfixturevalue("config_rules")
 
     def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
                            vlan_ptf_ports, symmetric_hashing, localhost):
         logging.info("Executing warm boot dynamic inner hash test for outer {} and inner {} with symmetric_hashing"
                      " set to {}".format(outer_ipver, inner_ipver, str(symmetric_hashing)))
-        with allure.step('Run ptf test InnerHashTest and warm-reboot in parallel'):
-            timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-            log_file = "/tmp/wr_inner_hash_test.DynamicInnerHashTest.{}.{}.{}.log".format(outer_ipver, inner_ipver, timestamp)
-            logging.info("PTF log file: %s" % log_file)
+        timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+        log_file = "/tmp/wr_inner_hash_test.DynamicInnerHashTest.{}.{}.{}.log".format(outer_ipver, inner_ipver, timestamp)
+        logging.info("PTF log file: %s" % log_file)
 
-            # to reduce test run time, check one of encapsulation formats
-            outer_encap_format = random.choice(OUTER_ENCAP_FORMATS).split()
-            logging.info("Tested encapsulation format: {}".format(outer_encap_format[0]))
+        # to reduce test run time, check one of encapsulation formats
+        outer_encap_format = random.choice(OUTER_ENCAP_FORMATS).split()
+        logging.info("Tested encapsulation format: {}".format(outer_encap_format[0]))
 
-            outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
-            inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
+        outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
+        inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
 
-            balancing_test_times = 200
-            balancing_range = 0.3
+        balancing_test_times = 200
+        balancing_range = 0.3
 
-            reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm',))
-            reboot_thr.start()
+        duthost.command('sudo config save -y')
+        reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm',))
+        reboot_thr.start()
 
-            ptf_runner(ptfhost,
-                       "ptftests",
-                       "inner_hash_test.InnerHashTest",
-                       platform_dir="ptftests",
-                       params={"fib_info": FIB_INFO_FILE_DST,
-                               "router_mac": router_mac,
-                               "src_ports": vlan_ptf_ports,
-                               "hash_keys": hash_keys,
-                               "vxlan_port": VXLAN_PORT,
-                               "inner_src_ip_range": ",".join(inner_src_ip_range),
-                               "inner_dst_ip_range": ",".join(inner_dst_ip_range),
-                               "outer_src_ip_range": ",".join(outer_src_ip_range),
-                               "outer_dst_ip_range": ",".join(outer_dst_ip_range),
-                               "balancing_test_times": balancing_test_times,
-                               "balancing_range": balancing_range,
-                               "outer_encap_formats": outer_encap_format,
-                               "nvgre_tni": NVGRE_TNI,
-                               "symmetric_hashing": symmetric_hashing},
-                       log_file=log_file,
-                       qlen=PTF_QLEN,
-                       socket_recv_size=16384)
-            reboot_thr.join()
+        ptf_runner(ptfhost,
+                   "ptftests",
+                   "inner_hash_test.InnerHashTest",
+                   platform_dir="ptftests",
+                   params={"fib_info": FIB_INFO_FILE_DST,
+                           "router_mac": router_mac,
+                           "src_ports": vlan_ptf_ports,
+                           "hash_keys": hash_keys,
+                           "vxlan_port": VXLAN_PORT,
+                           "inner_src_ip_range": ",".join(inner_src_ip_range),
+                           "inner_dst_ip_range": ",".join(inner_dst_ip_range),
+                           "outer_src_ip_range": ",".join(outer_src_ip_range),
+                           "outer_dst_ip_range": ",".join(outer_dst_ip_range),
+                           "balancing_test_times": balancing_test_times,
+                           "balancing_range": balancing_range,
+                           "outer_encap_formats": outer_encap_format,
+                           "symmetric_hashing": symmetric_hashing},
+                   log_file=log_file,
+                   qlen=PTF_QLEN,
+                   socket_recv_size=16384)
+        reboot_thr.join()
 
 
 @pytest.mark.static_config
-class TestWRStaticInnerHashing():
+class TestStaticInnerHashing():
 
     def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
                            vlan_ptf_ports, symmetric_hashing, localhost):
@@ -88,6 +85,7 @@ class TestWRStaticInnerHashing():
         outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
         inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
 
+        duthost.command('sudo config save -y')
         reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm',))
         reboot_thr.start()
 
