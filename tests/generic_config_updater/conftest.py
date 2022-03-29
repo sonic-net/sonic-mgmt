4bfcfd31a51a194fe4a89736fb2fbe31de746e5f
jingwenxie <jingwenxie@microsoft.com>
2022-03-28 21:17:09 -0700

    Add loganalyzer ignore regex for GCU (#5391)
    Summary: 
    Add LogAnalyzer ignored regex for GCU as it will report failure on incorrect ordering which is discarded by GCU.
    
    What is the motivation for this PR?
    Recent LogAnalyzer fix report failure on discarded ordering in GCU jsonpatch. However, GCU doesn't fail actually as it will explore a correct ordering and apply.
    There are some real issue in ignored regex. Will fix in future PR. Currently add it for PR check pass.
    
    How did you do it?
    Add LogAnalyzer ignored regex for GCU tests.
    
    How did you verify/test it?
    Run GCU test without flag --disable_loganalyzer and see if it works.

--- tests/generic_config_updater/conftest.py
+++ tests/generic_config_updater/conftest.py
@@ -2,6 +2,35 @@ import pytest
 
 from tests.common.utilities import skip_release
 
+@pytest.fixture(autouse=True)
+def ignore_expected_loganalyzer_exceptions(duthost, loganalyzer):
+    """
+       Ignore expected yang validation failure during test execution
+
+       GCU will try several sortings of JsonPatch until the sorting passes yang validation
+
+       Args:
+           loganalyzer: Loganalyzer utility fixture
+    """
+    # When loganalyzer is disabled, the object could be None
+    if loganalyzer:
+         ignoreRegex = [
+             ".*ERR sonic_yang.*",
+             ".*ERR bgp#bgpcfgd.*Can't update the peer. Only 'admin_status' attribute is supported.*", # test_bgpl
+             ".*ERR bgp#bgpcfgd: BGPAllowListMgr::Received BGP ALLOWED 'SET' message with no prefixes specified: {'NULL': 'NULL'}.*", # test_bgp_prefix
+             ".*ERR.*Failed to start dhcp_relay container.*", # test_dhcp_relay
+             ".*ERR GenericConfigUpdater: Service Validator: Service has been reset.*", # test_dhcp_relay test_syslog
+             ".*Same listen range is attached to peer-group.*", # test_bgp_speaker -> real issue
+             ".*ERR swss[0-9]*#orchagent.*removeLag.*", # autorestart/test_container_autorestart.py test_portchannel_interface
+             ".*ERR swss[0-9]*#intfmgrd: :- setIntfVrf:.*", # test_portchannel_interface
+             ".*ERR teamd.*get_dump: Can't get dump for LAG.*", # test_portchannel_interface
+             ".*ERR kernel.*Reset adapter.*", # test_portchannel_interface replace mtu
+             ".*ERR swss[0-9]*#orchagent: :- getPortOperSpeed.*", # test_portchannel_interface replace mtu
+             ".*ERR.*Failed to apply Json change.*", # validator need updater submodule
+             ".*ERR GenericConfigUpdater: Change Applier: service invoked.*", # validator need updater submodule
+         ]
+         loganalyzer[duthost.hostname].ignore_regex.extend(ignoreRegex)
+
 @pytest.fixture(scope="module", autouse=True)
 def check_image_version(duthost):
     """Skips this test if the SONiC image installed on DUT is older than 202112
