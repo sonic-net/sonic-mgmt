058c0b0c7f9109e47c2ac498693316657b0d4219
Antonh <76687950+AntonHryshchuk@users.noreply.github.com>
2022-03-31 15:58:39 +0300

    [pbh edit flow] add pbh edit flow validation to test (#5263)
    Added pbh edit flow validation to the test test_inner_hashing.py.
    Also added: 1) warmboot finalizer checker to pbh tests with WR. 2)removed acl dependency in WR lag test
    
    Signed-off-by: Anton <antonh@nvidia.com>

--- tests/ecmp/inner_hashing/test_inner_hashing.py
+++ tests/ecmp/inner_hashing/test_inner_hashing.py
@@ -5,12 +5,13 @@
 import logging
 import pytest
 import allure
+import random
 
 from datetime import datetime
 from retry.api import retry_call
 from tests.ptf_runner import ptf_runner
 from tests.ecmp.inner_hashing.conftest import get_src_dst_ip_range, FIB_INFO_FILE_DST,\
-    VXLAN_PORT, PTF_QLEN, check_pbh_counters, OUTER_ENCAP_FORMATS, NVGRE_TNI
+    VXLAN_PORT, PTF_QLEN, check_pbh_counters, OUTER_ENCAP_FORMATS, NVGRE_TNI, IP_VERSIONS_LIST
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,9 @@ pytestmark = [
     pytest.mark.topology('t0'),
     pytest.mark.asic('mellanox')
 ]
+
+update_outer_ipver = random.choice(IP_VERSIONS_LIST)
+update_inner_ipver = random.choice(IP_VERSIONS_LIST)
 
 
 @pytest.mark.dynamic_config
@@ -31,7 +35,7 @@ class TestDynamicInnerHashing():
             request.getfixturevalue("config_hash")
             request.getfixturevalue("config_rules")
 
-    def test_inner_hashing(self, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac, vlan_ptf_ports, symmetric_hashing, duthost):
+    def test_inner_hashing(self, request, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac, vlan_ptf_ports, symmetric_hashing, duthost):
         logging.info("Executing dynamic inner hash test for outer {} and inner {} with symmetric_hashing set to {}"
                      .format(outer_ipver, inner_ipver, str(symmetric_hashing)))
         with allure.step('Run ptf test InnerHashTest'):
@@ -45,32 +49,64 @@ class TestDynamicInnerHashing():
             balancing_test_times = 150
             balancing_range = 0.3
 
+            ptf_params = {"fib_info": FIB_INFO_FILE_DST,
+                          "router_mac": router_mac,
+                          "src_ports": vlan_ptf_ports,
+                          "hash_keys": hash_keys,
+                          "vxlan_port": VXLAN_PORT,
+                          "inner_src_ip_range": ",".join(inner_src_ip_range),
+                          "inner_dst_ip_range": ",".join(inner_dst_ip_range),
+                          "outer_src_ip_range": ",".join(outer_src_ip_range),
+                          "outer_dst_ip_range": ",".join(outer_dst_ip_range),
+                          "balancing_test_times": balancing_test_times,
+                          "balancing_range": balancing_range,
+                          "outer_encap_formats": OUTER_ENCAP_FORMATS,
+                          "nvgre_tni": NVGRE_TNI,
+                          "symmetric_hashing": symmetric_hashing}
+
+            duthost.shell("sonic-clear pbh statistics")
             ptf_runner(ptfhost,
                        "ptftests",
                        "inner_hash_test.InnerHashTest",
                        platform_dir="ptftests",
-                       params={"fib_info": FIB_INFO_FILE_DST,
-                               "router_mac": router_mac,
-                               "src_ports": vlan_ptf_ports,
-                               "hash_keys": hash_keys,
-                               "vxlan_port": VXLAN_PORT,
-                               "inner_src_ip_range": ",".join(inner_src_ip_range),
-                               "inner_dst_ip_range": ",".join(inner_dst_ip_range),
-                               "outer_src_ip_range": ",".join(outer_src_ip_range),
-                               "outer_dst_ip_range": ",".join(outer_dst_ip_range),
-                               "balancing_test_times": balancing_test_times,
-                               "balancing_range": balancing_range,
-                               "outer_encap_formats": OUTER_ENCAP_FORMATS,
-                               "nvgre_tni": NVGRE_TNI,
-                               "symmetric_hashing": symmetric_hashing},
+                       params=ptf_params,
                        log_file=log_file,
                        qlen=PTF_QLEN,
                        socket_recv_size=16384)
 
-        retry_call(check_pbh_counters,
-                   fargs=[duthost, outer_ipver, inner_ipver, balancing_test_times, symmetric_hashing, hash_keys],
-                   tries=5,
-                   delay=5)
+            retry_call(check_pbh_counters,
+                       fargs=[duthost, outer_ipver, inner_ipver, balancing_test_times, symmetric_hashing, hash_keys],
+                       tries=5,
+                       delay=5)
+
+        if update_outer_ipver == outer_ipver and update_inner_ipver == inner_ipver:
+            logging.info("Validate dynamic inner hash Edit Flow for outer {} and inner {} ip versions with"
+                         " symmetric_hashing set to {}".format(outer_ipver, inner_ipver, str(symmetric_hashing)))
+
+            swapped_outer_ipver = "ipv6" if outer_ipver == "ipv4" else "ipv4"
+            swapped_inner_ipver = "ipv6" if inner_ipver == "ipv4" else "ipv4"
+            with allure.step('Swap configuration of rules {}_{} with {}_{}'.format(outer_ipver,
+                                                                                   inner_ipver,
+                                                                                   swapped_outer_ipver,
+                                                                                   swapped_inner_ipver)):
+                request.getfixturevalue("update_rule")
+
+            with allure.step('Run again the ptf test InnerHashTest after updating the rules'):
+                duthost.shell("sonic-clear pbh statistics")
+                ptf_runner(ptfhost,
+                           "ptftests",
+                           "inner_hash_test.InnerHashTest",
+                           platform_dir="ptftests",
+                           params=ptf_params,
+                           log_file=log_file,
+                           qlen=PTF_QLEN,
+                           socket_recv_size=16384)
+
+            retry_call(check_pbh_counters,
+                       fargs=[duthost, swapped_outer_ipver, swapped_inner_ipver,
+                              balancing_test_times, symmetric_hashing, hash_keys],
+                       tries=5,
+                       delay=5)
 
 
 @pytest.mark.static_config
