2417f2ec17f8e33793da99b3fd3de5271b6b0f09
xwjiang2021 <96218837+xwjiang2021@users.noreply.github.com>
2022-04-02 12:46:48 +0800

    [Test gap] Inject route flapping and instability (#5204)
    * Fixes #3616
    
    What is the motivation for this PR?
    Need new test: Inject route flapping and instability
    
    How did you do it?
    1.choose one route, and ping it continiously
    2.withdraw announce some other routes at the same time, to see whether
    the route stable or not
    3. add the completeness level to set loop time
    
    How did you verify/test it?
    Run test in some loops
    
    Any platform specific information?
    Supported testbed topology if it's a new test case?

--- tests/route/conftest.py
+++ tests/route/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 # Pytest configuration used by the route tests.
 def pytest_addoption(parser):
     # Add options to pytest that are used by route tests
@@ -6,3 +8,7 @@ def pytest_addoption(parser):
 
     route_group.addoption("--num_routes", action="store", default=10000, type=int,
                      help="Number of routes for add/delete")
+
+@pytest.fixture(scope='module')
+def get_function_conpleteness_level(pytestconfig):
+    return pytestconfig.getoption("--completeness_level")
