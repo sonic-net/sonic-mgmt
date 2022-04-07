10374a5294e52bc82607548c96860c158b1c9158
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2022-04-07 16:32:21 +0800

    [dualtor] Add demo test template for `active-active` cable type (#5424)
    Approach
    What is the motivation for this PR?
    Modify testcase test_normal_op.py::test_normal_op_upstream to support parameterization based on cable type(active-active or active-standby).
    This PR acts as a roadmap on how to support cable type parameterization further for all dualtor_io testcases.
    
    Signed-off-by: Longxiang Lyu lolv@microsoft.com
    
    How did you do it?
    1. Add parameterization fixture cable_type.
    2. Let existing fixtures for dualtor active-standby selects ports in active-standby cable type to test.
    3. List out the basic APIs for active-active tests control/data plane fixtures.
    
    How did you verify/test it?
    Run the test test_normal_op.py::test_normal_op_upstream:

--- tests/dualtor_io/test_normal_op.py
+++ tests/dualtor_io/test_normal_op.py
@@ -3,10 +3,14 @@ import pytest
 from tests.common.config_reload import config_reload
 from tests.common.dualtor.control_plane_utils import verify_tor_states
 from tests.common.dualtor.data_plane_utils import send_t1_to_server_with_action, send_server_to_t1_with_action                                  # lgtm[py/unused-import]
+from tests.common.dualtor.dual_tor_common import cable_type                                                                                     # lgtm[py/unused-import]
+from tests.common.dualtor.dual_tor_common import CableType                                                                                      # lgtm[py/unused-import]
 from tests.common.dualtor.dual_tor_utils import upper_tor_host, lower_tor_host, force_active_tor                                                # lgtm[py/unused-import]
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_upper_tor                                                  # lgtm[py/unused-import]
+from tests.common.dualtor.nic_simulator_control import toggle_all_ports_both_tors_admin_forwarding_state_to_active                              # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import run_icmp_responder, run_garp_service, copy_ptftests_directory, change_mac_addresses             # lgtm[py/unused-import]
 from tests.common.dualtor.constants import MUX_SIM_ALLOWED_DISRUPTION_SEC, CONFIG_RELOAD_ALLOWED_DISRUPTION_SEC
+
 
 pytestmark = [
     pytest.mark.topology("dualtor")
@@ -15,9 +19,15 @@ pytestmark = [
 
 def test_normal_op_upstream(upper_tor_host, lower_tor_host,
                             send_server_to_t1_with_action,
-                            toggle_all_simulator_ports_to_upper_tor):
+                            toggle_all_simulator_ports_to_upper_tor,
+                            toggle_all_ports_both_tors_admin_forwarding_state_to_active,
+                            cable_type):
     """Send upstream traffic and confirm no disruption or switchover occurs"""
-    send_server_to_t1_with_action(upper_tor_host, verify=True, stop_after=60)
+    if cable_type == CableType.active_standby:
+        activehosts = upper_tor_host
+    elif cable_type == CableType.active_active:
+        activehosts = [upper_tor_host, lower_tor_host]
+    send_server_to_t1_with_action(activehosts, verify=True, stop_after=60)
     verify_tor_states(expected_active_host=upper_tor_host,
                       expected_standby_host=lower_tor_host)
 
