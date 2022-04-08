79582116bea23aed9b185ba67a3eeb746fe785f9
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2022-04-08 08:48:13 -0700

    Add DHCPv6 T1 CoPP Test (#5455)

--- ansible/roles/test/files/ptftests/copp_tests.py
+++ ansible/roles/test/files/ptftests/copp_tests.py
@@ -354,6 +354,57 @@ class DHCPTest(NoPolicyTest):
         return packet
 
 
+# SONIC configuration has no policer limiting for DHCPv6
+class DHCP6Test(NoPolicyTest):
+    def __init__(self):
+        NoPolicyTest.__init__(self)
+
+    def runTest(self):
+        self.log("DHCP6Test")
+        self.run_suite()
+
+    def contruct_packet(self, port_number):
+        src_mac = self.my_mac[port_number]
+
+        packet = testutils.simple_udpv6_packet(
+            pktlen=100,
+            eth_dst='33:33:00:01:00:02',
+            eth_src=src_mac,
+            ipv6_src='::1',
+            ipv6_dst='ff02::1:2',
+            udp_sport=546,
+            udp_dport=547
+        )
+
+        return packet
+    
+ #SONIC configuration has no packets to CPU for DHCPv6-T1 Topo
+class DHCP6TopoT1Test(PolicyTest):
+    def __init__(self):
+        PolicyTest.__init__(self)
+        # T1 DHCP6 no packet to packet to CPU so police rate is 0
+        self.PPS_LIMIT_MIN = 0
+        self.PPS_LIMIT_MAX = 0
+
+    def runTest(self):
+        self.log("DHCP6TopoT1Test")
+        self.run_suite()
+
+    def contruct_packet(self, port_number):
+        src_mac = self.my_mac[port_number]
+
+        packet = testutils.simple_udpv6_packet(
+            pktlen=100,
+            eth_dst='33:33:00:01:00:02',
+            eth_src=src_mac,
+            ipv6_src='::1',
+            ipv6_dst='ff02::1:2',
+            udp_sport=546,
+            udp_dport=547
+        )
+
+        return packet
+
 # SONIC configuration has no policer limiting for LLDP
 class LLDPTest(NoPolicyTest):
     def __init__(self):
