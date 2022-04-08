c4181c158020695bd90f9aa8ae91038b6e9596c0
Andrii-Yosafat Lozovyi <andrii-yosafatx.lozovyi@intel.com>
2022-04-08 16:16:59 +0200

    [NAT] Update PortChannel naming in NAT test suite (#5170)
    What is the motivation for this PR?
    Fix NAT test suite
    
    How did you do it?
    How did you verify/test it?
    Run NAT test cases:
    
    nat/test_static_nat.py::TestStaticNat::test_nat_static_basic[loopback-TCP] PASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_static_basic[loopback-UDP] PASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_static_basic_icmp[loopback] PASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_static_napt[loopback-TCP] PASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_static_napt[loopback-UDP] 32mPASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_clear_statistics_static_basic[loopback-TCP] PASSED
    nat/test_static_nat.py::TestStaticNat::test_nat_clear_statistics_static_basic[loopback-UDP] PASSED
    Any platform specific information?
    SONiC Software Version: SONiC.master.71470-dirty-20220210.130057
    Distribution: Debian 11.2
    Kernel: 5.10.0-8-2-amd64
    Build commit: 914a8a175
    Build date: Thu Feb 10 17:24:57 UTC 2022
    Built by: AzDevOps@sonic-build-workers-0015U1
    Platform: x86_64-arista_7170_64c
    HwSKU: Arista-7170-64C
    ASIC: barefoot
    
    Signed-off-by: Andrii-Yosafat Lozovyi <andrii-yosafatx.lozovyi@intel.com>

--- tests/nat/conftest.py
+++ tests/nat/conftest.py
@@ -9,6 +9,7 @@ from nat_helpers import GLOBAL_NAT_TIMEOUT
 from nat_helpers import GLOBAL_TCP_NAPT_TIMEOUT
 from nat_helpers import GLOBAL_UDP_NAPT_TIMEOUT
 from nat_helpers import FULL_CONE_TEST_SUBNET
+from nat_helpers import PORT_CHANNEL_TEMP
 from nat_helpers import conf_ptf_interfaces
 from nat_helpers import teardown_test_env
 from nat_helpers import exec_command
@@ -85,6 +86,7 @@ def setup_test_env(request, ptfhost, duthost, tbinfo):
     config_port_indices = cfg_facts['port_index_map']
     config_portchannels = cfg_facts.get('PORTCHANNEL', {})
     ptf_ports_available_in_topo = ptfhost.host.options['variable_manager'].extra_vars.get("ifaces_map")
+    port_channel_1_name = PORT_CHANNEL_TEMP.format(1)
     # Get outer port indices
     for port_id in config_portchannels.keys():
         port = config_portchannels[port_id]['members'][0]
@@ -118,8 +120,8 @@ def setup_test_env(request, ptfhost, duthost, tbinfo):
                          "indices_to_ports_config": indices_to_ports_config,
                          "ptf_ports_available_in_topo": ptf_ports_available_in_topo,
                          "config_portchannels": config_portchannels,
-                         "pch_ips": {"PortChannel0001": duthost.setup()['ansible_facts']['ansible_PortChannel0001']['ipv4']['address']},
-                         "pch_masks": {"PortChannel0001": duthost.setup()['ansible_facts']['ansible_PortChannel0001']['ipv4']['netmask']},
+                         "pch_ips": {port_channel_1_name: duthost.setup()['ansible_facts']['ansible_{}'.format(port_channel_1_name)]['ipv4']['address']},
+                         "pch_masks": {port_channel_1_name: duthost.setup()['ansible_facts']['ansible_{}'.format(port_channel_1_name)]['ipv4']['netmask']},
                          "outer_vrf": ["red"],
                          "inner_vrf": ["blue", "yellow"],
                          interface_type: {"vrf_conf": SETUP_CONF[interface_type]["vrf"],
