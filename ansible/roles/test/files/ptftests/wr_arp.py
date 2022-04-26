8cceddace58bacfd0b568fc13d5c16a9b1998c0b
Vaibhav Hemant Dixit <vaibhav.dixit@microsoft.com>
2022-04-26 00:15:40 +0000

    Change logs to indicate timeout for test_port_thr)

--- ansible/roles/test/files/ptftests/wr_arp.py
+++ ansible/roles/test/files/ptftests/wr_arp.py
@@ -307,9 +307,9 @@ class ArpTest(BaseTest):
 
         test_port_thr.join(timeout=self.how_long)
         if test_port_thr.isAlive():
-            self.log("Timed out waiting for warm reboot")
+            self.log("Timed out waiting for traffic-sender (test_port_thr thread)")
             self.req_dut('quit')
-            self.assertTrue(False, "Timed out waiting for warm reboot")
+            self.assertTrue(False, "Timed out waiting for traffic-sender (test_port_thr thread)")
 
         uptime_after = self.req_dut('uptime')
         if uptime_after.startswith('error'):
