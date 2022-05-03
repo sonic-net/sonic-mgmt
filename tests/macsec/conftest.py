8e284972f63f8e004ad4dddcca6ba9732e8305f3
Junhua Zhai <junhua.zhai@outlook.com>
2022-05-03 11:35:45 +0000

    Refactor macsec fixture

--- tests/macsec/conftest.py
+++ tests/macsec/conftest.py
@@ -1,42 +1,54 @@
 import pytest
-from . import macsec_profiles
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "macsec_required: mark test as MACsec required to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption("enable_macsec"):
+        skip_macsec = pytest.mark.skip(reason="macsec test cases")
+        for item in items:
+            if "macsec_required" in item.keywords:
+                item.add_marker(skip_macsec)
 
 
 @pytest.fixture(scope="module")
 def profile_name(macsec_profile):
-    return macsec_profile
+    return macsec_profile['name']
 
 
 @pytest.fixture(scope="module")
 def default_priority(macsec_profile):
-    return macsec_profiles[macsec_profile]['priority']
+    return macsec_profile['priority']
 
 
 @pytest.fixture(scope="module")
 def cipher_suite(macsec_profile):
-    return macsec_profiles[macsec_profile]['cipher_suite']
+    return macsec_profile['cipher_suite']
 
 
 @pytest.fixture(scope="module")
 def primary_ckn(macsec_profile):
-    return macsec_profiles[macsec_profile]['primary_ckn']
+    return macsec_profile['primary_ckn']
 
 
 @pytest.fixture(scope="module")
 def primary_cak(macsec_profile):
-    return macsec_profiles[macsec_profile]['primary_cak']
+    return macsec_profile['primary_cak']
 
 
 @pytest.fixture(scope="module")
 def policy(macsec_profile):
-    return macsec_profiles[macsec_profile]['policy']
+    return macsec_profile['policy']
 
 
 @pytest.fixture(scope="module")
 def send_sci(macsec_profile):
-    return macsec_profiles[macsec_profile]['send_sci']
+    return macsec_profile['send_sci']
 
 
 @pytest.fixture(scope="module")
 def rekey_period(macsec_profile):
-    return macsec_profiles[macsec_profile]['rekey_period']
+    return macsec_profile['rekey_period']
