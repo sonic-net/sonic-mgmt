56244498bfc52d3dab3cc5d3af279eba837dc58a
zhangyutong <doirszhang@tencent.com>
2022-05-05 15:18:49 +0800

    test idle timeout

---
+++ tests/dut_console/test_idle_timeout.py
@@ -0,0 +1,26 @@
+import pytest
+import pexpect
+import logging
+import time
+
+from tests.common.helpers.assertions import pytest_assert
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_TMOUT = "900"
+SET_TMOUT = "10"
+
+
+@pytest.fixture(scope="module")
+def test_timeout(duthost_console, duthost):
+    logger.info("Get default session idle timeout")
+    default_tmout = duthost_console.send_command('echo $TMOUT')
+    pytest_assert(default_tmout == DEFAULT_TMOUT, "default timeout on dut is not 900 seconds")
+
+    logger.info("Set session idle timeout")
+    duthost_console.send_command('export TMOUT={}'.format(SET_TMOUT))
+    set_tmout = duthost_console.send_command('echo $TMOUT')
+    pytest_assert(set_tmout == SET_TMOUT, "set timeout fail")
+
+    time.sleep(15)
+    duthost_console.send_command("\n", expect_string=r"str-s6000-on-6 login:")
