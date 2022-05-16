ab8a2267c572b92f2fb75997fc31befc46d7c03e
jsanghra <87666880+jsanghra@users.noreply.github.com>
2022-05-15 18:19:48 -0700

    Changes to testQosSaiPfcXonLimit for cisco-8000 (#5537)

--- tests/saitests/sai_qos_tests.py
+++ tests/saitests/sai_qos_tests.py
@@ -904,7 +904,6 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
             hysteresis = int(self.test_params['pkts_num_hysteresis'])
         else:
             hysteresis = 0
-        default_packet_length = 64
         hwsku = self.test_params['hwsku']
 
         # get a snapshot of counter values at recv and transmit ports
@@ -926,7 +925,12 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
 
         # create packet
         pkt_dst_mac = router_mac if router_mac != '' else dst_port_mac
-        pkt = construct_ip_pkt(default_packet_length,
+        if 'packet_size' in self.test_params:
+            packet_length = self.test_params['packet_size']
+        else:
+            packet_length = 64
+
+        pkt = construct_ip_pkt(packet_length,
                                pkt_dst_mac,
                                src_port_mac,
                                src_port_ip,
@@ -941,7 +945,7 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
 
         # create packet
         pkt_dst_mac2 = router_mac if router_mac != '' else dst_port_2_mac
-        pkt2 = construct_ip_pkt(default_packet_length,
+        pkt2 = construct_ip_pkt(packet_length,
                                 pkt_dst_mac2,
                                 src_port_mac,
                                 src_port_ip,
@@ -956,7 +960,7 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
 
         # create packet
         pkt_dst_mac3 = router_mac if router_mac != '' else dst_port_3_mac
-        pkt3 = construct_ip_pkt(default_packet_length,
+        pkt3 = construct_ip_pkt(packet_length,
                                 pkt_dst_mac3,
                                 src_port_mac,
                                 src_port_ip,
@@ -992,6 +996,12 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
                     self, src_port_id, pkt, 
                     pkts_num_egr_mem + pkts_num_leak_out + pkts_num_trig_pfc - pkts_num_dismiss_pfc - hysteresis
                 )
+            elif 'cisco-8000' in asic_type:
+                assert(fill_leakout_plus_one(self, src_port_id, dst_port_id, pkt, int(self.test_params['pg']), asic_type))
+                send_packet(
+                    self, src_port_id, pkt,
+                    pkts_num_leak_out + pkts_num_trig_pfc - pkts_num_dismiss_pfc - hysteresis - 1
+                )
             else:
                 send_packet(
                     self, src_port_id, pkt, 
@@ -1012,6 +1022,12 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
                     self, src_port_id, pkt2, 
                     pkts_num_egr_mem + pkts_num_leak_out + margin + pkts_num_dismiss_pfc - 1 + hysteresis
                 )
+            elif 'cisco-8000' in asic_type:
+                assert(fill_leakout_plus_one(self, src_port_id, dst_port_2_id, pkt2, int(self.test_params['pg']), asic_type))
+                send_packet(
+                    self, src_port_id, pkt2,
+                    pkts_num_leak_out + margin + pkts_num_dismiss_pfc - 2 + hysteresis
+                )
             else:
                 send_packet(
                     self, src_port_id, pkt2, 
@@ -1027,6 +1043,9 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
             )
             if hwsku == 'DellEMC-Z9332f-M-O16C64' or hwsku == 'DellEMC-Z9332f-O32':
                 send_packet(self, src_port_id, pkt3, pkts_num_egr_mem + pkts_num_leak_out + 1)
+            elif 'cisco-8000' in asic_type:
+                assert(fill_leakout_plus_one(self, src_port_id, dst_port_3_id, pkt3, int(self.test_params['pg']), asic_type))
+                send_packet(self, src_port_id, pkt3, pkts_num_leak_out)
             else:
                 send_packet(self, src_port_id, pkt3, pkts_num_leak_out + 1)
 
