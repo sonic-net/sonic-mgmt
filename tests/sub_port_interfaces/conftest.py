5342f2d138a0f4d44393fd40a02f41bd863a2cf5
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2022-05-30 14:21:13 +0800

    Add a new category 'asic_gen' in dut basic facts. (#5721)
    Description of PR
    Test cases under the tests/sub_port_interfaces only support td2 asic generation, we add a new category 'asic_gen' in dut basic facts to skip unsupported asic in advance.
    
    What is the motivation for this PR?
    Test cases under the tests/sub_port_interfaces only support td2 asic generation, we add a new category 'asic_gen' in dut basic facts to skip unsupported asic in advance.
    
    How did you do it?
    Get the asic generation infomation from ansible/group_vars/sonic/variables, and add it in dut basic facts. Also, modify the conditions in tests_mark_conditions.yaml to skip cases.
    
    How did you verify/test it?
    Running test cases on support/unsupport asic.
    
    Signed-off-by: Yutong Zhang <yutongzhang@microsoft.com>

--- tests/sub_port_interfaces/conftest.py
+++ tests/sub_port_interfaces/conftest.py
@@ -7,7 +7,6 @@ import pytest
 
 from tests.common import config_reload
 from tests.common.helpers.assertions import pytest_assert as py_assert
-from tests.common.utilities import get_host_visible_vars
 from tests.common.utilities import wait_until
 from tests.common.ptf_agent_updater import PtfAgentUpdater
 from tests.common import constants
@@ -52,19 +51,6 @@ def pytest_addoption(parser):
         default=4,
         help="Max numbers of sub-ports for test_max_numbers_of_sub_ports test case",
     )
-
-
-@pytest.fixture(scope='module', autouse=True)
-def skip_unsupported_asic_type(duthost):
-    SUBPORT_UNSUPPORTED_ASIC_LIST = ["th2"]
-    vendor = duthost.facts["asic_type"]
-    hostvars = get_host_visible_vars(duthost.host.options['inventory'], duthost.hostname)
-    for asic in SUBPORT_UNSUPPORTED_ASIC_LIST:
-        vendorAsic = "{0}_{1}_hwskus".format(vendor, asic)
-        if vendorAsic in hostvars.keys() and duthost.facts['hwsku'] in hostvars[vendorAsic]:
-            pytest.skip(
-                "Skipping test since subport is not supported on {0} {1} platforms".format(vendor, asic))
-
 
 @pytest.fixture(params=['port', 'port_in_lag'])
 def port_type(request):
