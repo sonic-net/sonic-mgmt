4ce78d23354836a9af2a3ffb53789d09bfbb8ec0
Perumal Venkatesh <pevenkat@cisco.com>
2022-05-31 16:00:18 -0700

    Revert "Committing config-change code."
    This reverts commit 0dd77b431e6b0a2f36c096967920cd5895c91d6b.

--- sonic-mgmt/tests/ixia/ecn/test_dequeue_ecn.py
+++ sonic-mgmt/tests/ixia/ecn/test_dequeue_ecn.py
@@ -7,7 +7,7 @@ from tests.common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_port
     ixia_api_serv_user, ixia_api_serv_passwd, ixia_api, ixia_testbed_config
 from tests.common.ixia.qos_fixtures import prio_dscp_map, lossless_prio_list
 
-from files.helper import run_ecn_test, is_ecn_marked, get_ecn_markings_dut, setup_ecn_markings_dut
+from files.helper import run_ecn_test, is_ecn_marked
 
 pytestmark = [ pytest.mark.topology('tgen') ]
 
@@ -16,7 +16,7 @@ def test_dequeue_ecn(request,
                      ixia_testbed_config,
                      conn_graph_facts,
                      fanout_graph_facts,
-                     duthosts, localhost,
+                     duthosts,
                      rand_one_dut_hostname,
                      rand_one_dut_portname_oper_up,
                      rand_one_dut_lossless_prio,
@@ -39,6 +39,9 @@ def test_dequeue_ecn(request,
     Returns:
         N/A
     """
+    disable_test = request.config.getoption("--disable_ecn_test")
+    if disable_test:
+        pytest.skip("test_dequeue_ecn is disabled")
 
     dut_hostname, dut_port = rand_one_dut_portname_oper_up.split('|')
     dut_hostname2, lossless_prio = rand_one_dut_lossless_prio.split('|')
@@ -48,18 +51,16 @@ def test_dequeue_ecn(request,
     testbed_config, port_config_list = ixia_testbed_config
     duthost = duthosts[rand_one_dut_hostname]
     lossless_prio = int(lossless_prio)
-    cisco_platform = (duthost.facts['asic_type'] == "cisco-8000")
 
     kmin = 50000
     kmax = 51000
     pmax = 100
+    pkt_size = 1024
     pkt_cnt = 100
     pkt_to_check = 0
-    pkt_size = 1024
-    if cisco_platform:
-        original_ecn_markings = get_ecn_markings_dut(duthost)
-        setup_ecn_markings_dut(duthost, localhost, dequeue = True, latency = False)
 
+    cisco_platform = (duthost.facts['asic_type'] == "cisco-8000")
+    if cisco_platform:
         oq_cell_count = 100      # Number of cells in OQ for this lossless priority 
         cell_size = 384
         cell_per_pkt = (pkt_size + cell_size - 1) // cell_size
@@ -69,34 +70,30 @@ def test_dequeue_ecn(request,
         pkt_to_check = pkt_to_oq + 1
     else:
         cell_size = 0
+    ip_pkts = run_ecn_test(api=ixia_api,
+                           testbed_config=testbed_config,
+                           port_config_list=port_config_list,
+                           conn_data=conn_graph_facts,
+                           fanout_data=fanout_graph_facts,
+                           duthost=duthost,
+                           dut_port=dut_port,
+                           kmin=kmin,
+                           kmax=kmax,
+                           pmax=pmax,
+                           pkt_size=pkt_size,
+                           pkt_cnt=pkt_cnt,
+                           lossless_prio=lossless_prio,
+                           prio_dscp_map=prio_dscp_map,
+                           iters=1)[0]
 
-    try:
-        ip_pkts = run_ecn_test(api=ixia_api,
-                               testbed_config=testbed_config,
-                               port_config_list=port_config_list,
-                               conn_data=conn_graph_facts,
-                               fanout_data=fanout_graph_facts,
-                               duthost=duthost,
-                               dut_port=dut_port,
-                               kmin=kmin,
-                               kmax=kmax,
-                               pmax=pmax,
-                               pkt_size=pkt_size,
-                               pkt_cnt=pkt_cnt,
-                               lossless_prio=lossless_prio,
-                               prio_dscp_map=prio_dscp_map,
-                               iters=1)[0]
 
-        """ Check if we capture all the packets """
-        pytest_assert(len(ip_pkts) == pkt_cnt,
-                      'Only capture {}/{} IP packets'.format(len(ip_pkts), pkt_cnt))
+    """ Check if we capture all the packets """
+    pytest_assert(len(ip_pkts) == pkt_cnt,
+                  'Only capture {}/{} IP packets'.format(len(ip_pkts), pkt_cnt))
 
-        """ Check if the first packet is marked """
-        pytest_assert(is_ecn_marked(ip_pkts[pkt_to_check]), "The {}th packet should be marked".format(pkt_to_check))
+    """ Check if the first packet is marked """
+    pytest_assert(is_ecn_marked(ip_pkts[pkt_to_check]), "The {}th packet should be marked".format(pkt_to_check))
 
-        """ Check if the last packet is not marked """
-        pytest_assert(not is_ecn_marked(ip_pkts[-1]),
-                      "The last packet should not be marked")
-    finally:
-        if cisco_platform:
-            setup_ecn_markings_dut(duthost, localhost, **original_ecn_markings)
+    """ Check if the last packet is not marked """
+    pytest_assert(not is_ecn_marked(ip_pkts[-1]),
+                  "The last packet should not be marked")
