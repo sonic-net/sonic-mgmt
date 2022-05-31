951ffc1bac6f05d560e779bb362e3d783b3fcf48
Anton Ptashnik <antonx.ptashnik@intel.com>
2022-05-31 04:30:04 -0700

    enabled syncd-rpc container swap for duration of PFC Asym tests

--- tests/pfc_asym/conftest.py
+++ tests/pfc_asym/conftest.py
@@ -4,6 +4,7 @@ from tests.common.fixtures.conn_graph_facts import fanout_graph_facts
 from tests.common.fixtures.pfc_asym import *
 from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import copy_saitests_directory   # lgtm[py/unused-import]
+from tests.common.utilities import str2bool
 
 
 @pytest.fixture(autouse=True)
@@ -11,3 +12,15 @@ def flush_neighbors(duthosts, rand_one_dut_hostname):
     """ Clear ARP table to make sure that neighbors learning will be triggered """
     duthost = duthosts[rand_one_dut_hostname]
     duthost.command("sonic-clear arp")
+
+def pytest_addoption(parser):
+
+    opts = parser.getgroup("QoS test suite options")
+
+    opts.addoption(
+        "--qos_swap_syncd",
+        action="store",
+        type=str2bool,
+        default=True,
+        help="Swap syncd container with syncd-rpc container",
+    )
