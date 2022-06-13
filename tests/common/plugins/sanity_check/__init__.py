c3596c45d9dcb75c3c32217b05a19e32f90a1769
Ze Gan <ganze718@gmail.com>
2022-06-13 03:51:04 -0700

    [sanity_check]: Fix sanity check with macsec (#5742)
    What is the motivation for this PR?
    In sanity check stage, the config_reload will be executed for recover DUT, but MACsec configuration wasn't written into config_db.json so that the MACsec configuration will be cleared after config_reload.
    
    How did you do it?
    Check whether the MACsec is enabled, if yes, call MACsec setup functions for recovering MACsec after config reload
    
    Signed-off-by: Ze Gan ganze718@gmail.com

--- tests/common/plugins/sanity_check/__init__.py
+++ tests/common/plugins/sanity_check/__init__.py
@@ -111,6 +111,7 @@ def sanity_check(localhost, duthosts, request, fanouthosts, nbrhosts, tbinfo):
     recover_method = "adaptive"
     pre_check_items = copy.deepcopy(SUPPORTED_CHECKS)  # Default check items
     post_check = False
+    enable_macsec = False
 
     customized_sanity_check = None
     for m in request.node.iter_markers():
@@ -152,6 +153,11 @@ def sanity_check(localhost, duthosts, request, fanouthosts, nbrhosts, tbinfo):
 
     if request.config.option.post_check:
         post_check = True
+
+    if request.config.option.enable_macsec:
+        enable_macsec = True
+        startup_macsec = request.getfixturevalue("startup_macsec")
+        start_macsec_service = request.getfixturevalue("start_macsec_service")
 
     cli_check_items = request.config.getoption("--check_items")
     cli_post_check_items = request.config.getoption("--post_check_items")
@@ -226,6 +232,10 @@ def sanity_check(localhost, duthosts, request, fanouthosts, nbrhosts, tbinfo):
                     neighbor_vm_restore(duthosts[dut_name], nbrhosts, tbinfo)
                 for action in infra_recovery_actions:
                     action()
+
+                if enable_macsec:
+                    start_macsec_service()
+                    startup_macsec()
 
                 logger.info("Run sanity check again after recovery")
                 new_check_results = do_checks(request, pre_check_items, stage=STAGE_PRE_TEST, after_recovery=True)
