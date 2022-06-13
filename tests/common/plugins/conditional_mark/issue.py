bba2015cc30698936c4933d42fc096d2333208b8
zhangyutong <doirszhang@tencent.com>
2022-06-13 14:12:43 +0800

    fix

--- tests/common/plugins/conditional_mark/issue.py
+++ tests/common/plugins/conditional_mark/issue.py
@@ -57,8 +57,8 @@ class GitHubIssueChecker(IssueCheckerBase):
                     self.user = github_creds.get('user', '')
                     self.api_token = github_creds.get('api_token', '')
                 else:
-                    self.user = os.environ["GIT_USER_NAME"]
-                    self.api_token = os.environ["GIT_API_TOKEN"]
+                    self.user = os.environ.get("GIT_USER_NAME")
+                    self.api_token = os.environ.get("GIT_API_TOKEN")
         except Exception as e:
             logger.error('Load credentials from {} failed with error: {}'.format(creds_file_path, repr(e)))
 
