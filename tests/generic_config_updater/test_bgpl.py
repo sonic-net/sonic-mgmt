01862c7581f5fb1b0ac234ec4f0f63d5061f66b6
jingwenxie <jingwenxie@microsoft.com>
2022-06-15 22:13:29 -0700

    [GCU] Add rollback validation test (#5469)
    Summary: Improve GCU test by adding rollback validation test
    
    What is the motivation for this PR?
    Cover test gap for GCU rollback
    
    How did you do it?
    Record config before test start, then compare if config is the same after rollback.
    
    How did you verify/test it?
    Run test of sonic-mgmt/tests/generic_config_updater/test_bgp*.py on KVM

--- tests/generic_config_updater/test_bgpl.py
+++ tests/generic_config_updater/test_bgpl.py
@@ -1,5 +1,6 @@
 import logging
 import pytest
+import re
 
 from netaddr import IPNetwork
 from tests.common.helpers.assertions import pytest_assert
@@ -14,6 +15,21 @@ pytestmark = [
 
 logger = logging.getLogger(__name__)
 
+def get_bgp_monitor_runningconfig(duthost):
+    """ Get bgp listener config
+    """
+    cmds = "show runningconfiguration bgp"
+    output = duthost.shell(cmds)
+    pytest_assert(not output['rc'],
+        "'{}' failed with rc={}".format(cmds, output['rc'])
+    )
+
+    # Sample:
+    # neighbor 11.0.0.1 description BGPMonitor
+    bgp_listener_pattern = r"\s+neighbor.*description BGPMonitor"
+    bgp_listener_config = re.findall(bgp_listener_pattern, output['stdout'])
+    return bgp_listener_config
+
 @pytest.fixture(autouse=True)
 def setup_env(duthosts, rand_one_dut_hostname):
     """
@@ -23,6 +39,7 @@ def setup_env(duthosts, rand_one_dut_hostname):
         rand_selected_dut: The fixture returns a randomly selected DuT.
     """
     duthost = duthosts[rand_one_dut_hostname]
+    original_bgp_listener_config = get_bgp_monitor_runningconfig(duthost)
     create_checkpoint(duthost)
 
     yield
@@ -30,6 +47,10 @@ def setup_env(duthosts, rand_one_dut_hostname):
     try:
         logger.info("Rolled back to original checkpoint")
         rollback_or_reload(duthost)
+        current_bgp_listener_config = get_bgp_monitor_runningconfig(duthost)
+        pytest_assert(set(original_bgp_listener_config) == set(current_bgp_listener_config),
+            "bgp listener config are not suppose to change after test"
+        )
     finally:
         delete_checkpoint(duthost)
 
