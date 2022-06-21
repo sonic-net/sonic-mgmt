9f7af402de9e8e16a33c81b31581c9e155e55430
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2022-06-21 18:00:17 +0800

    Fix junit xml parser failure in nightly test (#5844)
    * Fix junit xml parser failure in nightly test
    
    What is the motivation for this PR?
    We added start and end timestamp for testcases in test report, and we
    will parse start and end timestamp in junit xml parser.
    However, some tests will not contain start and end timestamp, but the
    parser will directly parse report file without checking, so it will fail
    
    How did you do it?
    Paser parses start and end timestamp only if they exist
    
    How did you verify/test it?
    Run a test
    
    Any platform specific information?
    Supported testbed topology if it's a new test case?
    
    * Fix expression error

--- test_reporting/junit_xml_parser.py
+++ test_reporting/junit_xml_parser.py
@@ -422,7 +422,8 @@ def _parse_test_cases(root):
             result[attribute] = test_case.get(attribute)
         for attribute in REQUIRED_TESTCASE_PROPERTIES:
             testcase_properties = _parse_testcase_properties(test_case)
-            result[attribute] = testcase_properties[attribute]
+            if attribute in testcase_properties:
+                result[attribute] = testcase_properties[attribute]
 
         # NOTE: "if failure" and "if error" does not work with the ETree library.
         failure = test_case.find("failure")
