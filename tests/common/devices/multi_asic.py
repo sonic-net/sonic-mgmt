26422604d185d28143e9cd442503ed4eeed2699c
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2022-07-05 09:08:22 -0700

    [MASIC] [chassis] Test default route with bgp flap support for multi-asic (#5780)
    * Update sonic_asic.py
    
    * Update test_default_route.py
    
    * Update test_default_route.py
    
    * Update test_default_route.py
    
    * Update multi_asic.py
    
    * Update test_default_route.py
    
    * Update multi_asic.py
    
    * Refine for single-asic case
    
    * Refine test_default_route for single-asics
    
    * Update test_default_route.py
    
    * Update test_default_route.py
    
    * Update sonic_asic.py
    
    to only check eth0 on multi_asic host

--- tests/common/devices/multi_asic.py
+++ tests/common/devices/multi_asic.py
@@ -672,3 +672,35 @@ class MultiAsicSonicHost(object):
         """Restart service on an asic passed or None(DEFAULT_ASIC_ID)"""
         self.asic_instance(asic_index).restart_service(service)
         
+    def get_bgp_name_to_ns_mapping(self):
+        """ This function returns mapping of bgp name -- namespace
+            e.g. {'ARISTAT2': 'asic0', ...}
+        """
+        mg_facts = self.sonichost.minigraph_facts(
+            host = self.sonichost.hostname
+        )['ansible_facts']
+        neighbors = mg_facts['minigraph_neighbors']
+        mapping = dict()
+        for neigh in neighbors.values():
+            mapping[neigh['name']] = neigh['namespace']        
+        return mapping
+
+    def get_default_route_from_app_db(self, af='ipv4'):
+        default_routes = dict()
+        if self.sonichost.is_multi_asic:
+            for front_asic in self.frontend_asics:
+                default_routes[front_asic.namespace] = front_asic.get_default_route_from_app_db(af)
+        else:
+            default_routes = self.asic_instance(0).get_default_route_from_app_db(af)
+        return default_routes
+    
+    def is_default_route_removed_from_app_db(self, uplink_asics = DEFAULT_NAMESPACE):
+        if self.sonichost.is_multi_asic:
+            for ns in uplink_asics:
+                if not self.asic_instance_from_namespace(ns).is_default_route_removed_from_app_db():
+                    return False
+        else:
+            if not self.asic_instance(0).is_default_route_removed_from_app_db():
+                return False
+        return True
+        
