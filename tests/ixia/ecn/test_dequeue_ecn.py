ae9d1c0dfa4ca8a7fb4b68e0671fea50a7746e5b
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2022-07-06 00:33:04 -0700

    Adding support for updating ecn config file based on testcase. (#5850)
    This PR is to add the following for cisco 8000 platform only:
    Add support for enabling the ECN marking configs in the DUT.
    Update the test_dequeue_ecn.py to call the above functions.
    Restore the configs after the test is done.

--- tests/ixia/ecn/test_dequeue_ecn.py
+++ tests/ixia/ecn/test_dequeue_ecn.py
@@ -8,6 +8,7 @@ from tests.common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_port
 from tests.common.ixia.qos_fixtures import prio_dscp_map, lossless_prio_list
 
 from files.helper import run_ecn_test, is_ecn_marked
+from tests.common.cisco_data import  get_ecn_markings_dut, setup_ecn_markings_dut
 
 pytestmark = [ pytest.mark.topology('tgen') ]
 
@@ -17,6 +18,7 @@ def test_dequeue_ecn(request,
                      conn_graph_facts,
                      fanout_graph_facts,
                      duthosts,
+                     localhost,
                      rand_one_dut_hostname,
                      rand_one_dut_portname_oper_up,
                      rand_one_dut_lossless_prio,
@@ -51,6 +53,7 @@ def test_dequeue_ecn(request,
     testbed_config, port_config_list = ixia_testbed_config
     duthost = duthosts[rand_one_dut_hostname]
     lossless_prio = int(lossless_prio)
+    cisco_platform = (duthost.facts['asic_type'] == "cisco-8000")
 
     kmin = 50000
     kmax = 51000
@@ -58,7 +61,21 @@ def test_dequeue_ecn(request,
     pkt_size = 1024
     pkt_cnt = 100
 
-    ip_pkts = run_ecn_test(api=ixia_api,
+    if cisco_platform:
+        original_ecn_markings = get_ecn_markings_dut(duthost)
+        setup_ecn_markings_dut(duthost, localhost, ecn_dequeue_marking = True, ecn_latency_marking = False)
+        oq_cell_count = 100      # Number of cells in OQ for this lossless priority 
+        cell_size = 384
+        cell_per_pkt = (pkt_size + cell_size - 1) // cell_size
+        margin_cells = 25
+        margin = margin_cells // cell_per_pkt
+        pkt_to_oq = (oq_cell_count//cell_per_pkt) + margin # Packets forwarded to OQ
+        pkt_to_check = pkt_to_oq + 1
+    else:
+        pkt_to_check = 0
+
+    try:
+        ip_pkts = run_ecn_test(api=ixia_api,
                            testbed_config=testbed_config,
                            port_config_list=port_config_list,
                            conn_data=conn_graph_facts,
@@ -75,13 +92,16 @@ def test_dequeue_ecn(request,
                            iters=1)[0]
 
 
-    """ Check if we capture all the packets """
-    pytest_assert(len(ip_pkts) == pkt_cnt,
-                  'Only capture {}/{} IP packets'.format(len(ip_pkts), pkt_cnt))
+        """ Check if we capture all the packets """
+        pytest_assert(len(ip_pkts) == pkt_cnt,
+                      'Only capture {}/{} IP packets'.format(len(ip_pkts), pkt_cnt))
 
-    """ Check if the first packet is marked """
-    pytest_assert(is_ecn_marked(ip_pkts[0]), "The first packet should be marked")
+        """ Check if the first packet is marked """
+        pytest_assert(is_ecn_marked(ip_pkts[pkt_to_check]), "The first packet should be marked")
 
-    """ Check if the last packet is not marked """
-    pytest_assert(not is_ecn_marked(ip_pkts[-1]),
-                  "The last packet should not be marked")
+        """ Check if the last packet is not marked """
+        pytest_assert(not is_ecn_marked(ip_pkts[-1]),
+                      "The last packet should not be marked")
+    finally:
+         if cisco_platform:
+            setup_ecn_markings_dut(duthost, localhost, **original_ecn_markings)
