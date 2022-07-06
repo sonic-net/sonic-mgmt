a042c7437e24786ac6f23f4b05b91dbc7e6ddd6a
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2022-07-06 00:33:04 -0700

    Adding support for updating ecn config file based on testcase. (#5850)
    This PR is to add the following for cisco 8000 platform only:
    Add support for enabling the ECN marking configs in the DUT.
    Update the test_dequeue_ecn.py to call the above functions.
    Restore the configs after the test is done.

--- tests/ixia/ecn/test_red_accuracy.py
+++ tests/ixia/ecn/test_red_accuracy.py
@@ -9,6 +9,7 @@ from tests.common.ixia.ixia_fixtures import ixia_api_serv_ip, ixia_api_serv_port
 from tests.common.ixia.qos_fixtures import prio_dscp_map, lossless_prio_list
 
 from files.helper import run_ecn_test, is_ecn_marked
+from tests.common.cisco_data import get_ecn_markings_dut, setup_ecn_markings_dut
 
 pytestmark = [ pytest.mark.topology('tgen') ]
 
@@ -18,6 +19,7 @@ def test_red_accuracy(request,
                       conn_graph_facts,
                       fanout_graph_facts,
                       duthosts,
+                      localhost,
                       rand_one_dut_hostname,
                       rand_one_dut_portname_oper_up,
                       rand_one_dut_lossless_prio,
@@ -53,6 +55,7 @@ def test_red_accuracy(request,
     testbed_config, port_config_list = ixia_testbed_config
     duthost = duthosts[rand_one_dut_hostname]
     lossless_prio = int(lossless_prio)
+    cisco_platform = (duthost.facts['asic_type'] == "cisco-8000")
 
     kmin = 500000
     kmax = 2000000
@@ -62,7 +65,12 @@ def test_red_accuracy(request,
     iters = 100
     result_file_name = 'result.txt'
 
-    ip_pkts_list = run_ecn_test(api=ixia_api,
+    if cisco_platform:
+        original_ecn_markings = get_ecn_markings_dut(duthost)
+        setup_ecn_markings_dut(duthost, localhost, dequeue = True, latency = False)
+
+    try:
+        ip_pkts_list = run_ecn_test(api=ixia_api,
                                 testbed_config=testbed_config,
                                 port_config_list=port_config_list,
                                 conn_data=conn_graph_facts,
@@ -78,31 +86,35 @@ def test_red_accuracy(request,
                                 prio_dscp_map=prio_dscp_map,
                                 iters=iters)
 
-    """ Check if we capture packets of all the rounds """
-    pytest_assert(len(ip_pkts_list) == iters,
-                  'Only capture {}/{} rounds of packets'.format(len(ip_pkts_list), iters))
+        """ Check if we capture packets of all the rounds """
+        pytest_assert(len(ip_pkts_list) == iters,
+                      'Only capture {}/{} rounds of packets'.format(len(ip_pkts_list), iters))
 
-    queue_mark_cnt = {}
-    for i in range(pkt_cnt):
-        queue_len = (pkt_cnt - i) * pkt_size
-        queue_mark_cnt[queue_len] = 0
+        queue_mark_cnt = {}
+        for i in range(pkt_cnt):
+            queue_len = (pkt_cnt - i) * pkt_size
+            queue_mark_cnt[queue_len] = 0
 
-    for i in range(iters):
-        ip_pkts = ip_pkts_list[i]
-        """ Check if we capture all the packets in each round """
-        pytest_assert(len(ip_pkts) == pkt_cnt,
-                      'Only capture {}/{} packets in round {}'.format(len(ip_pkts), pkt_cnt, i))
+        for i in range(iters):
+            ip_pkts = ip_pkts_list[i]
+            """ Check if we capture all the packets in each round """
+            pytest_assert(len(ip_pkts) == pkt_cnt,
+                          'Only capture {}/{} packets in round {}'.format(len(ip_pkts), pkt_cnt, i))
 
-        for j in range(pkt_cnt):
-            ip_pkt = ip_pkts[j]
-            queue_len = (pkt_cnt - j) * pkt_size
+            for j in range(pkt_cnt):
+                ip_pkt = ip_pkts[j]
+                queue_len = (pkt_cnt - j) * pkt_size
 
-            if is_ecn_marked(ip_pkt):
-                queue_mark_cnt[queue_len] += 1
+                if is_ecn_marked(ip_pkt):
+                    queue_mark_cnt[queue_len] += 1
 
-    """ Dump queue length vs. ECN marking probability into a file """
-    queue_mark_cnt = collections.OrderedDict(sorted(queue_mark_cnt.items()))
-    f = open(result_file_name, 'w')
-    for queue, mark_cnt in queue_mark_cnt.iteritems():
-        f.write('{} {}\n'.format(queue, float(mark_cnt)/iters))
-    f.close()
+        """ Dump queue length vs. ECN marking probability into a file """
+        queue_mark_cnt = collections.OrderedDict(sorted(queue_mark_cnt.items()))
+        f = open(result_file_name, 'w')
+        for queue, mark_cnt in queue_mark_cnt.iteritems():
+            f.write('{} {}\n'.format(queue, float(mark_cnt)/iters))
+        f.close()
+    finally:
+        if cisco_platform:
+            setup_ecn_markings_dut(duthost, localhost, **original_ecn_markings)
+
