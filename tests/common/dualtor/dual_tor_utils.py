7298e1e6b32d33e730176d61d82eff8e504bfa68
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2022-07-13 01:31:12 +0800

    Add 2 test cases for tunnel QoS remapping (#5923)
    This PR is to add two test cases for verifying tunnel QoS remapping.
    Test plan: #5508
    
    What is the motivation for this PR?
    This PR is to add two test cases for tunnel QoS remapping.
    * TC1 test_encap_dscp_rewrite
       The test is to verify the dscp rewriting of encapped packets.
    * TC2 test_bounced_back_traffic_in_expected_queue
       The test case is to verify the encapped packet is mapped to the correct queue
    
    How did you verify/test it?
    Verified on a dualtor testbed.
    
    collected 2 items                                                                                                                                                                                     
    
    qos/test_tunnel_qos_remap.py::test_encap_dscp_rewrite PASSED                                                                                                                                    [ 50%]
    qos/test_tunnel_qos_remap.py::test_bounced_back_traffic_in_expected_queue PASSED                                                                                                                [100%]

--- tests/common/dualtor/dual_tor_utils.py
+++ tests/common/dualtor/dual_tor_utils.py
@@ -1072,6 +1072,7 @@ def dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo):
     res = {}
     res['ptfhost'] = ptfhost
     res['standby_tor_mac'] = standby_tor.facts['router_mac']
+    res['active_tor_mac'] = active_tor.facts['router_mac']
     vlan_name = standby_tor_mg_facts['minigraph_vlans'].keys()[0]
     res['vlan_mac'] = standby_tor.get_dut_iface_mac(vlan_name)
     res['standby_tor_ip'] = _get_iface_ip(standby_tor_mg_facts, 'Loopback0')
@@ -1301,3 +1302,35 @@ def increase_linkmgrd_probe_interval(duthosts, tbinfo):
                     .format(probe_interval_ms))
     cmds.append("config save -y")
     duthosts.shell_cmds(cmds=cmds)
+
+def is_tunnel_qos_remap_enabled(duthost):
+    """
+    Check whether tunnel_qos_remap is enabled or not
+    """
+    try:
+        tunnel_qos_remap_status = duthost.shell('sonic-cfggen -d -v \'SYSTEM_DEFAULTS.tunnel_qos_remap.status\'', module_ignore_errors=True)["stdout_lines"][0].decode("utf-8")
+    except IndexError:
+        return False
+    return "enabled" == tunnel_qos_remap_status
+
+def is_port_with_4_lossless_queues(duthost, port, tbinfo):
+    """
+    Check whether the port has 4 lossless queues
+    If tunnel_qos_ramap is enables, there are 4 lossless queues for ports between T0 and T1
+    """
+    if not is_tunnel_qos_remap_enabled(duthost):
+        return False
+    config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    topo = tbinfo['topo']['type']
+    if 't1' in topo:
+        peer_name = 't0'
+    elif 't0' in topo:
+        peer_name = 't1'
+    else:
+        return False
+
+    if port in config_facts['DEVICE_NEIGHBOR'] and peer_name in config_facts['DEVICE_NEIGHBOR'][port]['name'].lower():
+        return True
+    return False
+   
+   
