ea2e4cbbbf0f4caf8b4a3237227b1cfd8ecd72da
Longxiang Lyu <lolv@microsoft.com>
2022-07-21 02:33:54 -0700

    Fix LGTM
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- ansible/dualtor/nic_simulator/__init__.py
+++ ansible/dualtor/nic_simulator/__init__.py
@@ -1,4 +1,4 @@
-from nic_simulator_grpc_service_pb2 import *
-from nic_simulator_grpc_service_pb2_grpc import *
-from nic_simulator_grpc_mgmt_service_pb2 import *
-from nic_simulator_grpc_mgmt_service_pb2_grpc import *
+from nic_simulator_grpc_service_pb2 import *                # lgtm[py/polluting-import]
+from nic_simulator_grpc_service_pb2_grpc import *           # lgtm[py/polluting-import]
+from nic_simulator_grpc_mgmt_service_pb2 import *           # lgtm[py/polluting-import]
+from nic_simulator_grpc_mgmt_service_pb2_grpc import *      # lgtm[py/polluting-import]
