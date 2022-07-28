7219dda0a4489204cee0dd0a8254a5e5f5a86ecc
Longxiang Lyu <lolv@microsoft.com>
2022-07-28 08:10:20 -0700

    Add dualtor common utilities
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- tests/common/dualtor/dual_tor_common.py
+++ tests/common/dualtor/dual_tor_common.py
@@ -6,7 +6,9 @@ import pytest
 __all__ = [
     'cable_type',
     'CableType',
-    'mux_config'
+    'mux_config',
+    'active_standby_port',
+    'active_active_ports'
 ]
 
 
@@ -20,6 +22,10 @@ class CableType(object):
 @pytest.fixture(params=[CableType.active_standby, CableType.active_active])
 def cable_type(request):
     """Dualtor cable type."""
+    has_enable_active_active_marker = bool([_ for _ in request.node.iter_markers() if _.name == "enable_active_active"])
+    if ((not has_enable_active_active_marker) and (request.param == CableType.active_active)):
+        pytest.skip("Skip cable type 'active-active'")
+
     return request.param
 
 
@@ -32,3 +38,29 @@ def mux_config(duthosts, tbinfo):
     duthost = duthosts[0]
     cmd = 'show mux config --json'
     return json.loads(duthost.shell(cmd)['stdout'])["MUX_CABLE"]["PORTS"]
+
+
+@pytest.fixture(scope="session")
+def active_active_ports(mux_config, tbinfo):
+    if 'dualtor' not in tbinfo['topo']['name']:
+        return []
+
+    active_active_ports = []
+    for port, port_config in mux_config.items():
+        if port_config["SERVER"].get("cable_type", CableType.default_type) == CableType.active_active:
+            active_active_ports.append(port)
+
+    return active_active_ports
+
+
+@pytest.fixture(scope="session")
+def active_standby_port(mux_config, tbinfo):
+    if 'dualtor' not in tbinfo['topo']['name']:
+        return []
+
+    active_standby_ports = []
+    for port, port_config in mux_config.items():
+        if port_config["SERVER"].get("cable_type", CableType.default_type) == CableType.active_standby:
+            active_active_ports.append(port)
+
+    return active_standby_ports
