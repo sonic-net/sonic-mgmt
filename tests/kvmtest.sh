8ac33c7c88c1e94ab34200df22ec38994c1803cf
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2022-08-01 11:11:07 -0700

    Add back -u to bypass util group

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -270,7 +270,7 @@ test_multi_asic_t1_lag() {
 
     pushd $SONIC_MGMT_DIR/tests
     # TODO: Remove disable of loganaler and sanity check once multi-asic testbed is stable.
-    ./run_tests.sh $MULTI_ASIC_CLI_OPTIONS -c "$tests" -p logs/$tgname -e --disable_loganalyzer
+    ./run_tests.sh $MULTI_ASIC_CLI_OPTIONS -u -c "$tests" -p logs/$tgname -e --disable_loganalyzer
     popd
 }
 
@@ -281,7 +281,7 @@ test_multi_asic_t1_lag_pr() {
 
     pushd $SONIC_MGMT_DIR/tests
     # TODO: Remove disable of loganaler and sanity check once multi-asic testbed is stable.
-    ./run_tests.sh $RUNTEST_CLI_COMMON_OPTS -c "$tests" -p logs/$tgname -e --disable_loganalyzer
+    ./run_tests.sh $RUNTEST_CLI_COMMON_OPTS -u -c "$tests" -p logs/$tgname -e --disable_loganalyzer
     popd
 }
 
