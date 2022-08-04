62d4471634becb41665cebfa41558ec07457c20b
Perumal Venkatesh <vperumal@gmail.com>
2022-08-04 12:00:13 -0700

    Update test_module.py

--- tests/platform_tests/api/test_module.py
+++ tests/platform_tests/api/test_module.py
@@ -237,13 +237,13 @@ class TestModuleApi(PlatformApiTestBase):
             # Ensure that we were able to obtain the minimum required type codes
             asic_type = duthost.facts["asic_type"]
             module_type = module.get_type(platform_api_conn, i)
-            if module_type == 'FABRIC-CARD' and 'cisco-8000' in duthost.facts["asic_type"]:
+            if module_type == 'FABRIC-CARD' and 'cisco-8000' in asic_type:
                 logger.info("This check is not applicable for Fabric card on cisco-8000 based chassis")
             else:
                 self.expect(set(MINIMUM_REQUIRED_TYPE_CODES_LIST) <= set(syseeprom_type_codes_list), "Module {}: Minimum required TlvInfo type codes not provided".format(i))
 
             # Ensure the base MAC address is sane
-            if module_type == 'FABRIC-CARD' and 'cisco-8000' in duthost.facts["asic_type"]:
+            if module_type == 'FABRIC-CARD' and 'cisco-8000' in asic_type:
 		logger.info("This check is not applicable for Fabric card on cisco-8000 based chassis")
             else:
                 base_mac = syseeprom_info_dict[ONIE_TLVINFO_TYPE_CODE_BASE_MAC_ADDR]
