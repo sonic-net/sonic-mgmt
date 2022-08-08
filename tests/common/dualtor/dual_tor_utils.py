e011ed0ac2ea7e062bf4f6d55177ecfd4e907569
Jing Zhang <zhangjing@microsoft.com>
2022-08-07 18:42:01 -0700

    Enable `test_normal_op` test cases on active-active dualtor interfaces  (#5984)
    Approach
    What is the motivation for this PR?
    To enable dualtor io tests on active-active dualtor ports.
    
    How did you do it?
    1. Added control utilities in nic_simulator_control, for toggling active-active interfaces to standby/active states on any or both duthosts. Toggles is triggered through cmd line, which is different to active-standby ports.
    2. Added active-active type in cable_type fixture.
    3. Update test_normal_op cases to adapt active-active interfaces. For some cases, disruption is not expected any more.
    4. Adjust test names, commets, to better suit today's usage.
    
    How did you verify/test it?
    Run test cases on mixed topology.

--- tests/common/dualtor/dual_tor_utils.py
+++ tests/common/dualtor/dual_tor_utils.py
@@ -30,7 +30,7 @@ from tests.common.helpers.generators import generate_ip_through_default_route
 from tests.common import constants
 
 
-__all__ = ['tor_mux_intf', 'tor_mux_intfs', 'ptf_server_intf', 't1_upper_tor_intfs', 't1_lower_tor_intfs', 'upper_tor_host', 'lower_tor_host', 'force_active_tor']
+__all__ = ['tor_mux_intf', 'tor_mux_intfs', 'ptf_server_intf', 't1_upper_tor_intfs', 't1_lower_tor_intfs', 'upper_tor_host', 'lower_tor_host', 'force_active_tor', 'force_standby_tor']
 
 logger = logging.getLogger(__name__)
 
@@ -293,6 +293,31 @@ def force_active_tor():
     for x in forced_intfs:
         x[0].shell("config muxcable mode auto {}; true".format(x[1]))
 
+
+@pytest.fixture
+def force_standby_tor():
+    """
+    @summary: Manually set dut host to the standby tor for intf
+    @param dut: The duthost for which to toggle mux
+    @param intf: One or a list of names of interface or 'all' for all interfaces
+    """
+    forced_intfs = []
+    def force_standby_tor_fn(dut, intf):
+        logger.info('Setting {} as standby for intfs {}'.format(dut, intf))
+        if type(intf) == str:
+            cmds = ["config muxcable mode standby {}; true".format(intf)]
+            forced_intfs.append((dut, intf))
+        else:
+            cmds = []
+            for i in intf:
+                forced_intfs.append((dut, i))
+                cmds.append("config muxcable mode standby {}; true".format(i))
+        dut.shell_cmds(cmds=cmds, continue_on_fail=True)
+
+    yield force_standby_tor_fn
+
+    for x in forced_intfs:
+        x[0].shell("config muxcable mode auto {}; true".format(x[1]))        
 
 
 def _get_tor_fanouthosts(tor_host, fanouthosts):
