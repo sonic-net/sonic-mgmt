sonic-net/sonic-mgmt

commit 98752da2c0410dd31696d400651c516facce62c2

Replace `pytest.skip` in test scripts with conditional marks.  (#5708)
Description of PR
When we use pytest.skip in test scripts, it will first execute some fixtures in test cases, which will waste some time. When using conditional marks to skip test cases, it will skip the case in the collect period, which will not execute fixtures in test cases and save some execute time. In this pr, we replace pytest.skip in test scripts with conditional marks to skip test case in advance and save execute time.

What is the motivation for this PR?
When we use pytest.skip in test scripts, it will first execute some fixtures in test cases, which will waste some time. When using conditional marks to skip test cases, it will skip the case in the collect period, which will not execute fixtures in test cases and save some running time. In this pr, we replace pytest.skip in test scripts with conditional marks to skip test case in advance and save running time.

How did you do it?
Replace pytest.skip in test scripts with conditional marks in tests_mark_conditions.yaml.

How did you verify/test it?
By running whole test cases and observe the running time.

Signed-off-by: Yutong Zhang <yutongzhang@microsoft.com>

ansible/library/dut_basic_facts.py
@@ -44,6 +44,8 @@ def main():
         results['is_multi_asic'] = device_info.is_multi_npu()
         results['num_asic'] = device_info.get_num_npus()
         results.update(device_info.get_sonic_version_info())
+        results['kernel_version'] = results['kernel_version'].split('-')[0]
+        results['is_supervisor'] = device_info.is_supervisor()
 
         # In case a image does not have /etc/sonic/sonic_release, guess release from 'build_version'
         if 'release' not in results or not results['release'] or results['release'] == 'none':

tests/acl/null_route/test_null_route_helper.py
@@ -58,13 +58,6 @@ TEST_DATA = [
     ("fc03:1000::1", "unblock {} fc03:1000::1/128".format(ACL_TABLE_NAME_V6), FORWARD), # Verify double-unblock doesn't cause issue
 ]
 
-
-@pytest.fixture(scope="module", autouse=True)
-def skip_on_dualtor_testbed(tbinfo):
-    if 'dualtor' in tbinfo['topo']['name']:
-        pytest.skip("Skip running on dualtor testbed")
-
-
 @pytest.fixture(scope="module", autouse=True)
 def remove_dataacl_table(rand_selected_dut):
     """

tests/bfd/test_bfd.py
@@ -7,17 +7,14 @@ from tests.common.fixtures.ptfhost_utils import change_mac_addresses, copy_arp_r
 from tests.common.dualtor.dual_tor_utils import get_t1_ptf_ports
 from tests.common.dualtor.mux_simulator_control import mux_server_url
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_rand_selected_tor_m
-from pkg_resources import parse_version
 
 pytestmark = [
     pytest.mark.topology('t1', 't1-lag', 't1-64-lag')
 ]
-def skip_201911_and_older(duthost):
-    """ Skip the current test if the DUT version is 201911 or older.
-    """
-    if parse_version(duthost.kernel_version) <= parse_version('4.9.0'):
-        pytest.skip("Test not supported for 201911 images or older. Skipping the test")
 
+def is_dualtor(tbinfo):
+    """Check if the testbed is dualtor."""
+    return "dualtor" in tbinfo["topo"]["name"]
 
 def get_t0_intfs(mg_facts):
     t0_intfs = []
@@ -235,7 +232,6 @@ def update_bfd_state(ptfhost, neighbor_addr, local_addr, state):
 def test_bfd_basic(request, rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports_to_rand_selected_tor_m, ipv6, dut_init_first):
     duthost = rand_selected_dut
     bfd_session_cnt =  int(request.config.getoption('--num_sessions'))
-    skip_201911_and_older(duthost)
     local_addrs, prefix_len, neighbor_addrs, neighbor_devs, neighbor_interfaces = get_neighbors(duthost, tbinfo, ipv6, count = bfd_session_cnt)
     try:
         add_dut_ip(duthost, neighbor_devs, local_addrs, prefix_len)
@@ -290,8 +286,6 @@ def test_bfd_basic(request, rand_selected_dut, ptfhost, tbinfo, toggle_all_simul
 def test_bfd_scale(rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports_to_rand_selected_tor_m, ipv6):
     duthost = rand_selected_dut
     bfd_session_cnt =  int(request.config.getoption('--num_sessions_scale'))
-  
-    skip_201911_and_older(duthost)
     local_addrs, prefix_len, neighbor_addrs, neighbor_devs, neighbor_interfaces = get_neighbors_scale(duthost, tbinfo, ipv6, scale_count = bfd_session_cnt)
 
     try:

tests/bgp/test_bgp_allow_list.py
@@ -62,9 +62,6 @@ ALLOW_LIST = {
 def setup(tbinfo, nbrhosts, duthosts, rand_one_dut_hostname):
     duthost = duthosts[rand_one_dut_hostname]
 
-    if tbinfo['topo']['type'] != 't1':
-        pytest.skip('Unsupported topology type: {}, supported: {}'.format(tbinfo['topo']['type'], 't1'))
-
     constants_stat = duthost.stat(path=CONSTANTS_FILE)
     if not constants_stat['stat']['exists']:
         pytest.skip('No file {} on DUT, BGP Allow List is not supported')

tests/bgp/test_bgp_bbr.py
@@ -91,8 +91,6 @@ def config_bbr_enabled(duthosts, setup, rand_one_dut_hostname, restore_bbr_defau
 @pytest.fixture(scope='module')
 def setup(duthosts, rand_one_dut_hostname, tbinfo, nbrhosts):
     duthost = duthosts[rand_one_dut_hostname]
-    if tbinfo['topo']['type'] != 't1':
-        pytest.skip('Unsupported topology type: {}, supported: {}'.format(tbinfo['topo']['type'], 't1'))
 
     constants_stat = duthost.stat(path=CONSTANTS_FILE)
     if not constants_stat['stat']['exists']:

tests/bgp/test_bgp_multipath_relax.py
@@ -9,13 +9,6 @@ pytestmark = [
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="module", autouse=True)
-def skip_test_multipath_relax_on_backend(tbinfo):
-    """Skip test_bgp_multipath_relax over storage backend topologies."""
-    if "backend" in tbinfo["topo"]["name"]:
-        pytest.skip("Skipping test_bgp_multipath_relax. Unsupported topology %s." % tbinfo["topo"]["name"])
-
-
 def get_t2_neigh(tbinfo):
     dut_t2_neigh = []
     for vm in tbinfo['topo']['properties']['topology']['VMs'].keys():

tests/bgp/test_bgpmon.py
@@ -23,13 +23,6 @@ ZERO_ADDR = r'0.0.0.0/0'
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="module", autouse=True)
-def skip_test_bgpmon_on_backend(tbinfo):
-    """Skip test_bgpmon over storage backend topologies."""
-    if "backend" in tbinfo["topo"]["name"]:
-        pytest.skip("Skipping test_bgpmon. Unsupported topology %s." % tbinfo["topo"]["name"])
-
-
 def get_default_route_ports(host, tbinfo):
     mg_facts = host.get_extended_minigraph_facts(tbinfo)
     route_info = json.loads(host.shell("show ip route {} json".format(ZERO_ADDR))['stdout'])

tests/common/plugins/conditional_mark/__init__.py
@@ -118,14 +118,14 @@ def read_asic_name(hwsku):
     except IOError as e:
         return None
 
-
-def load_dut_basic_facts(session, inv_name, dut_name):
+def load_dut_basic_facts(inv_name, dut_name):
     """Run 'ansible -m dut_basic_facts' command to get some basic DUT facts.
 
     The facts will be a 1 level dictionary. The dict keys can be used as variables in condition statements evaluation.
 
     Args:
-        session (obj): The pytest session object.
+        inv_name (str): The name of inventory.
+        dut_name (str): The name of dut.
 
     Returns:
         dict or None: Return the dut basic facts dict or None if something went wrong.
@@ -146,7 +146,6 @@ def load_dut_basic_facts(session, inv_name, dut_name):
         logger.error('Failed to load dut basic facts, exception: {}'.format(repr(e)))
 
     return results
-
 
 def get_basic_facts(session):
     testbed_name = session.config.option.testbed
@@ -170,6 +169,123 @@ def get_basic_facts(session):
             basic_facts = load_basic_facts(session)
             session.config.cache.set('BASIC_FACTS', basic_facts)
 
+
+def load_minigraph_facts(inv_name, dut_name):
+    """Run 'ansible -m minigraph_facts -a host={{hostname}}' command to get some basic minigraph facts.
+
+    The facts will be a 1 level dictionary. The dict keys can be used as variables in condition statements evaluation.
+
+    Args:
+        inv_name (str): The name of inventory.
+        dut_name (str): The name of dut.
+
+    Returns:
+        dict or None: Return the minigraph basic facts dict or None if something went wrong.
+    """
+    results = {}
+    logger.info('Getting minigraph basic facts')
+    try:
+        # get minigraph basic faces
+        ansible_cmd = "ansible -m minigraph_facts -i ../ansible/{0} {1} -a host={1}".format(inv_name, dut_name)
+        raw_output = subprocess.check_output(ansible_cmd.split()).decode('utf-8')
+        logger.debug('raw minigraph basic facts:\n{}'.format(raw_output))
+        output_fields = raw_output.split('SUCCESS =>', 1)
+        if len(output_fields) >= 2:
+            output_fields = json.loads(output_fields[1].strip())['ansible_facts']
+            results['minigraph_interfaces'] = output_fields['minigraph_interfaces']
+            results['minigraph_portchannels'] = output_fields['minigraph_portchannels']
+            results['minigraph_portchannel_interfaces'] = output_fields['minigraph_portchannel_interfaces']
+            results['minigraph_neighbors'] = output_fields['minigraph_neighbors']
+    except Exception as e:
+        logger.error('Failed to load minigraph basic facts, exception: {}'.format(repr(e)))
+
+    return results
+
+def load_config_facts(inv_name, dut_name):
+    """Run 'ansible -m config_facts -a 'host={{hostname}} source='persistent' ' command to get some basic config facts.
+
+    The facts will be a 1 level dictionary. The dict keys can be used as variables in condition statements evaluation.
+
+    Args:
+        inv_name (str): The name of inventory.
+        dut_name (str): The name of dut.
+
+    Returns:
+        dict or None: Return the minigraph basic facts dict or None if something went wrong.
+    """
+    results = {}
+    logger.info('Getting config basic facts')
+    try:
+        # get config basic faces
+        ansible_cmd = ['ansible', '-m', 'config_facts', '-i', '../ansible/{}'.format(inv_name), '{}'.format(dut_name), '-a', 'host={} source=\'persistent\''.format(dut_name)]
+        raw_output = subprocess.check_output(ansible_cmd).decode('utf-8')
+        logger.debug('raw config basic facts:\n{}'.format(raw_output))
+        output_fields = raw_output.split('SUCCESS =>', 1)
+        if len(output_fields) >= 2:
+            output_fields = json.loads(output_fields[1].strip())['ansible_facts']
+            results['VOQ_INBAND_INTERFACE'] = output_fields.get('VOQ_INBAND_INTERFACE', {})
+            results['BGP_VOQ_CHASSIS_NEIGHBOR'] = output_fields.get('BGP_VOQ_CHASSIS_NEIGHBOR', {})
+            results['INTERFACE'] = output_fields.get('INTERFACE', {})
+    except Exception as e:
+        logger.error('Failed to load config basic facts, exception: {}'.format(repr(e)))
+
+    return results
+
+def load_switch_capabilities_facts(inv_name, dut_name):
+    """Run 'ansible -m switch_capabilities_facts' command to get some basic config facts.
+
+    The facts will be a 1 level dictionary. The dict keys can be used as variables in condition statements evaluation.
+
+    Args:
+        inv_name (str): The name of inventory.
+        dut_name (str): The name of dut.
+
+    Returns:
+        dict or None: Return the minigraph basic facts dict or None if something went wrong.
+    """
+    results = {}
+    logger.info('Getting switch capabilities basic facts')
+    try:
+        # get switch capabilities basic faces
+        ansible_cmd = "ansible -m switch_capabilities_facts -i ../ansible/{} {}".format(inv_name, dut_name)
+        raw_output = subprocess.check_output(ansible_cmd.split()).decode('utf-8')
+        logger.debug('raw switch capabilities basic facts:\n{}'.format(raw_output))
+        output_fields = raw_output.split('SUCCESS =>', 1)
+        if len(output_fields) >= 2:
+            output_fields = json.loads(output_fields[1].strip())['ansible_facts']['switch_capabilities']
+            results['switch'] = output_fields.get('switch', {})
+    except Exception as e:
+        logger.error('Failed to load switch capabilities basic facts, exception: {}'.format(repr(e)))
+
+    return results
+
+def load_console_facts(inv_name, dut_name):
+    """Run 'ansible -m console_facts' command to get some basic console facts.
+
+    The facts will be a 1 level dictionary. The dict keys can be used as variables in condition statements evaluation.
+
+    Args:
+        inv_name (str): The name of inventory.
+        dut_name (str): The name of dut.
+
+    Returns:
+        dict or None: Return the minigraph basic facts dict or None if something went wrong.
+    """
+    results = {}
+    logger.info('Getting console basic facts')
+    try:
+        # get console basic faces
+        ansible_cmd = "ansible -m console_facts -i ../ansible/{} {}".format(inv_name, dut_name)
+        raw_output = subprocess.check_output(ansible_cmd.split()).decode('utf-8')
+        logger.debug('raw console basic facts:\n{}'.format(raw_output))
+        output_fields = raw_output.split('SUCCESS =>', 1)
+        if len(output_fields) >= 2:
+            output_fields = json.loads(output_fields[1].strip())['ansible_facts']['console_facts']
+            results = output_fields
+    except Exception as e:
+        logger.error('Failed to load console basic facts, exception: {}'.format(repr(e)))
+
+    return results
 
 def load_basic_facts(session):
     """Load some basic facts that can be used in condition statement evaluation.
@@ -201,7 +317,27 @@ def load_basic_facts(session):
         inv_name = 'lab'
 
     # Load DUT basic facts
-    _facts = load_dut_basic_facts(session, inv_name, dut_name)
+    _facts = load_dut_basic_facts(inv_name, dut_name)
+    if _facts:
+        results.update(_facts)
+
+    # Load minigraph basic facts
+    _facts = load_minigraph_facts(inv_name, dut_name)
+    if _facts:
+        results.update(_facts)
+
+    # Load config basic facts
+    _facts = load_config_facts(inv_name, dut_name)
+    if _facts:
+        results.update(_facts)
+
+    # Load switch capabilities basic facts
+    _facts = load_switch_capabilities_facts(inv_name, dut_name)
+    if _facts:
+        results.update(_facts)
+
+    # Load console basic facts
+    _facts = load_config_facts(inv_name, dut_name)
     if _facts:
         results.update(_facts)
 

commit c3f124f34a37e1fca93aab2e912b418cbb084841

Fixed dut_basic_facts ansible module to have support SONiC images which does not have attribute "is_supervisor" (#6118)
Description of PR
Fixed dut_basic_facts ansible module to have support SONiC images which does not have attribute "is_supervisor"

Previoulsy when we call dut_basic_facts ansible module on SONiC image which does not have attribute "is_supervisor"(for example: 202012) we received error: AttributeError("'module' object has no attribute 'is_supervisor'"
Now issue fixed - script will work on all SONiC branches
Issue introduced in PR: #5708

Summary: Fixed dut_basic_facts ansible module to have support SONiC images which does not have attribute "is_supervisor"

What is the motivation for this PR?
Fix AttributeError("'module' object has no attribute 'is_supervisor'"

How did you do it?
See code

How did you verify/test it?
Executed ansible module: dut_basic_facts

Signed-off-by: Petro Pikh <petrop@nvidia.com>

ansible/library/dut_basic_facts.py
@@ -45,7 +45,9 @@ def main():
         results['num_asic'] = device_info.get_num_npus()
         results.update(device_info.get_sonic_version_info())
         results['kernel_version'] = results['kernel_version'].split('-')[0]
-        results['is_supervisor'] = device_info.is_supervisor()
+        results['is_supervisor'] = False
+        if hasattr(device_info, 'is_supervisor'):
+            results['is_supervisor'] = device_info.is_supervisor()
 
         # In case a image does not have /etc/sonic/sonic_release, guess release from 'build_version'
         if 'release' not in results or not results['release'] or results['release'] == 'none':