941a18142f9f0509c26b0b24163af499d0a40832
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2022-08-10 16:33:16 +0800

    Restore tacacs_server after the module tacacs/test_accounting.py running. (#6117)
    Description of PR
    In module tacacs/test_accounting.py, the fixture check_tacacs use the function setup_tacacs_client to delete the default tacacs server, and set the ptf mgmt ip as tacacs sever ip. But it doesn't restore this config when the module finish running. We want to keep the config in consistent before and after the testcase running, so fix it.
    
    What is the motivation for this PR?
    In module tacacs/test_accounting.py, the fixture check_tacacs use the function setup_tacacs_client to delete the default tacacs server, and set the ptf mgmt ip as tacacs sever ip. But it doesn't restore this config when the module finish running. We want to keep the config in consistent before and after the testcase running, so fix it.
    
    How did you do it?
    Get the default tacacs server and put them into a list, when the module finish running, delete the ptf mgmt ipand restore the default tacacs server ip.
    
    How did you verify/test it?
    Running the test cases in this module and compare the tacacs server ip before and after running.
    
    Signed-off-by: Yutong Zhang <yutongzhang@microsoft.com>

--- tests/tacacs/conftest.py
+++ tests/tacacs/conftest.py
@@ -2,7 +2,7 @@ import os
 import logging
 import yaml
 import pytest
-from .utils import setup_tacacs_client, setup_tacacs_server, cleanup_tacacs
+from .utils import setup_tacacs_client, setup_tacacs_server, cleanup_tacacs, restore_tacacs_servers
 
 logger = logging.getLogger(__name__)
 TACACS_CREDS_FILE='tacacs_creds.yaml'
@@ -15,19 +15,18 @@ def tacacs_creds(creds_all_duts):
     creds_all_duts.update(hardcoded_creds)
     return creds_all_duts
 
-
 @pytest.fixture(scope="module")
 def check_tacacs(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds):
     logger.info('tacacs_creds: {}'.format(str(tacacs_creds)))
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     tacacs_server_ip = ptfhost.mgmt_ip
-    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
+    default_tacacs_servers = setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
     setup_tacacs_server(ptfhost, tacacs_creds, duthost)
 
     yield
 
     cleanup_tacacs(ptfhost, tacacs_creds, duthost)
-
+    restore_tacacs_servers(duthost, default_tacacs_servers, tacacs_server_ip)
 
 @pytest.fixture(scope="module")
 def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds):
