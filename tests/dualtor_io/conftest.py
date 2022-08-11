8bbbde456233e24b7b9db9261932bde3dc001a83
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2022-08-11 09:05:48 +0800

    [dualtor][active-active] Add BGP shutdown/startup testcases (#6143)
    Approach
    What is the motivation for this PR?
    Add testcase to cover upstream I/O scenario if one ToR goes through BGP shutdown/startup.
    
    test_active_tor_shutdown_bgp_sessions_upstream
    test_active_tor_shutdown_bgp_sessions_downstream
    
    Signed-off-by: Longxiang Lyu lolv@microsoft.com
    
    How did you do it?
    Add fixture shutdown_bgp_sessions to shutdown/startup bgp sessions.
    Use the same dst IP for upstream traffic if cable_type is active-active, this is to ensure those packets will be forwarded to the same ToR.
    
    How did you verify/test it?

--- tests/dualtor_io/conftest.py
+++ tests/dualtor_io/conftest.py
@@ -3,3 +3,7 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers", "enable_active_active: mark test to run with 'active_active' ports"
     )
+
+    config.addinivalue_line(
+        "markers", "skip_active_standby: mark test to skip running with 'active_standby' ports"
+    )
