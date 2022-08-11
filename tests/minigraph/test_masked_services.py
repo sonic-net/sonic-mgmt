399d27c32ced005adce9da0b44528d50fa5fc847
zbud-msft <zainbudhwani@microsoft.com>
2022-08-11 21:17:08 +0000

    Add changes per review comments

--- tests/minigraph/test_masked_services.py
+++ tests/minigraph/test_masked_services.py
@@ -1,75 +1,63 @@
 import logging
 import time
-
 from tests.common.helpers.assertions import pytest_assert
 
-def is_service_up(duthost, service):
-    output = duthost.shell("sudo systemctl show -p LoadState --value {}.service".format(service))
-    if output['stdout'] != "loaded":
+def is_service_loaded(duthost, service):
+    output = duthost.shell("systemctl show -p LoadState --value {}.service".format(service))
+    if output['rc'] != "0":
+        pytest.fail("Error showing load state of telemetry service")
         return False
-    return True
+    return output['stdout'] == "loaded"
 
 def change_service_state(duthost, service, enable):
-    if not enable:
-        duthost.shell("sudo systemctl stop {}.service".format(service))
-        duthost.shell("sudo systemctl disable {}.service".format(service))
-        duthost.shell("sudo systemctl mask {}.service".format(service))
+    outputs = []
+    if enable:
+        outputs = [
+            duthost.shell("systemctl unmask {}.service".format(service)),
+            duthost.shell("systemctl enable {}.service".format(service)),
+            duthost.shell("systemctl start {}.service".format(service))
+        ]
     else:
-        duthost.shell("sudo systemctl unmask {}.service".format(service))
-        duthost.shell("sudo systemctl enable {}.service".format(service))
-        duthost.shell("sudo systemctl start {}.service".format(service))
+        outputs = [
+            duthost.shell("systemctl stop {}.service".format(service)),
+            duthost.shell("systemctl disable {}.service".format(service)),
+            duthost.shell("systemctl mask {}.service".format(service))
+        ]
+    for output in outputs:
+        if output['rc'] != 0:
+            pytest.fail("Error starting or stopping service")
+            return
 
 def test_masked_services(duthosts, rand_one_dut_hostname):
     """
-    @summary: This test case will mask a running service, then test load_minigraph and check its success
+    @summary: This test case will mask telemetry service, then test load_minigraph and check its success
     """
-
     duthost = duthosts[rand_one_dut_hostname]
+    test_service = "telemetry"
 
-    logging.info("Bringing down a running service")
+    logging.info("Bringing down telemetry service")
+    service_status = duthost.critical_process_status("telemetry")
+    if not service_status:
+        pytest.fail("Make sure telemetry is up and running before calling this test")
 
-    test_service = ""
-    services = [
-        "telemetry",
-        "restapi",
-        "bgp",
-        "lldp",
-        "pmon",
-        "swss",
-        "nat"
-    ]
-   
-    for service in services:
-        service_status = duthost.critical_process_status(service)
-        if service_status:
-            test_service = service
-            break
-
-    if not test_service:
-        logging.error("No valid service to run test on.")
-        return
-
-    logging.info("Bringing down {}".format(test_service))
     change_service_state(duthost, test_service, False)
-
     logging.info("Wait until service is masked and inactive")
 
-    time.sleep(3)
+    time.sleep(3) #give adequate time for service to become masked and inactive
 
     logging.info("Check service status")
+    assert is_service_loaded(duthost, test_service) == False
 
-    assert is_service_up(duthost, test_service) == False
     logging.info("Starting load_minigraph")
+    load_minigraph_ret = duthost.shell("config load_minigraph -y")
+    load_minigraph_error_code = load_minigraph_ret['rc']
+    result = load_minigraph_error_code == "0"
 
-    duthost.shell("sudo config load_minigraph -y")
-    load_minigraph_error_code = duthost.shell("sudo echo $?")
-    result = load_minigraph_error_code['stdout'] == "0"
-
-    if not result:
+    if not result:  
         logging.info("Bring back service if not up")
         change_service_state(duthost, test_service, True)
         logging.info("Wait until service is unmasked and active")
         time.sleep(3)
         duthost.shell("sudo config reload")
-        time.sleep(3)
-        assert result == True
+        pytest.fail("Test failed as load_minigraph was not successful")
+
