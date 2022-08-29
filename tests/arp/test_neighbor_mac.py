37b9ade8f61f69efbc503decbd77de380f03423a
Yutong Zhang <yutongzhang@microsoft.com>
2022-08-29 17:43:17 +0800

    shutdown Ethernet0

--- tests/arp/test_neighbor_mac.py
+++ tests/arp/test_neighbor_mac.py
@@ -41,6 +41,7 @@ class TestNeighborMac:
 
         logger.info("Restore the DUT interface config, remove IP address")
         self.__configureInterfaceIp(duthost, action="remove")
+        self.__shutdownInterface(duthost)
 
     @pytest.fixture(params=[0, 1])
     def macIndex(self, request):
@@ -88,6 +89,24 @@ class TestNeighborMac:
             "config",
             "interface",
             "startup",
+            self.DUT_ETH_IF
+        ])
+
+    def __shutdownInterface(self, duthost):
+        """
+            Shutdown the interface on the DUT
+
+            Args:
+                duthost (AnsibleHost): Device Under Test (DUT)
+
+            Returns:
+                None
+        """
+        logger.info("Configure the interface '{0}' as DOWN".format(self.DUT_ETH_IF))
+        duthost.shell(argv=[
+            "config",
+            "interface",
+            "shutdown",
             self.DUT_ETH_IF
         ])
 
