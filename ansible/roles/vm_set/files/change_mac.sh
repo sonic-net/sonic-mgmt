d5d497a3df73478fa674ee107b593c2533127036
stephengzh <87261920+stephengao-ragilenetworks@users.noreply.github.com>
2022-09-01 14:46:15 +0800

    Update change_mac.sh
    When I adding topology, sometimes this file cannot be pointed to '../../test/files/helpers/change_mac.sh'.
    The reason shows "this file not found". 
    It is really not necessary to add this pointer. It is better to apply the "change ptf mac function" into this file directly.

--- ansible/roles/vm_set/files/change_mac.sh
+++ ansible/roles/vm_set/files/change_mac.sh
@@ -1,1 +1,18 @@
-../../test/files/helpers/change_mac.sh
+#!/bin/bash
+
+set -euo pipefail
+
+INTF_LIST=$(ls /sys/class/net | grep -E "^eth[0-9]+$")
+
+for INTF in ${INTF_LIST}; do
+    ADDR="$(cat /sys/class/net/${INTF}/address)"
+    PREFIX="$(cut -c1-15 <<< ${ADDR})"
+    SUFFIX="$(printf "%02x" ${INTF##eth})"
+    MAC="${PREFIX}${SUFFIX}"
+
+    echo "Update ${INTF} MAC address: ${ADDR}->$MAC"
+    # bringing the device down/up to trigger ipv6 link local address change
+    ip link set dev ${INTF} down
+    ip link set dev ${INTF} address ${MAC}
+    ip link set dev ${INTF} up
+done
