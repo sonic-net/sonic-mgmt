b91b9b98a4903a58d050d6220203a42f531e7cdb
siqbal1986 <shahzad.iqbal@microsoft.com>
2022-09-08 07:10:25 -0700

    Bfd test multihop (#6032) Cherry pick of PR #6032 for 202012

--- tests/bfd/test_bfd.py
+++ tests/bfd/test_bfd.py
@@ -14,15 +14,11 @@ from pkg_resources import parse_version
 from tests.common import constants
 
 pytestmark = [
-    pytest.mark.topology('t0', 't1', 't1-lag')
+   pytest.mark.topology('t1')
 ]
 
-def skip_201911_and_older(duthost):
-    """ Skip the current test if the DUT version is 201911 or older.
-    """
-    if parse_version(duthost.kernel_version) <= parse_version('4.9.0'):
-        pytest.skip("Test not supported for 201911 images or older. Skipping the test")
-
+BFD_RESPONDER_SCRIPT_SRC_PATH = '../ansible/roles/test/files/helpers/bfd_responder.py'
+BFD_RESPONDER_SCRIPT_DEST_PATH = '/opt/bfd_responder.py'
 
 def is_dualtor(tbinfo):
     """Check if the testbed is dualtor."""
@@ -96,6 +92,47 @@ def get_neighbors(duthost, tbinfo, ipv6=False, count=1):
             return [t1_ipv4_pattern.format(idx * 2) for idx in indices], 31, [t1_ipv4_pattern.format(idx * 2 + 1) for idx in indices], [t0_intfs[_] for _ in indices], [ptf_ports[_] for _ in indices]
 
 
+def get_loopback_intf(mg_facts, ipv6):
+    ipv6idx = 0 if mg_facts['minigraph_lo_interfaces'][0]['prefixlen'] == 128 else 1
+    if ipv6:
+        return mg_facts['minigraph_lo_interfaces'][ipv6idx]['addr']
+    else:
+         return mg_facts['minigraph_lo_interfaces'][(ipv6idx+1) %2]['addr']
+
+def get_neighbors_multihop(duthost, tbinfo, ipv6=False, count=1):
+    mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
+    t0_ipv4_pattern = '4.{}.{}.1'
+    t0_ipv6_pattern = '3000:3000:{:x}::3000'
+    t0_intfs = get_t0_intfs(mg_facts)
+    ptf_ports = [mg_facts['minigraph_ptf_indices'][port] for port in t0_intfs]
+    loopback_addr = get_loopback_intf( mg_facts, ipv6 )
+
+    index = random.sample(list(range(len(t0_intfs))), k=1)[0]
+    port_intf = t0_intfs[index]
+    ptf_intf = ptf_ports[index]
+    nexthop_ip = ""
+    neighbour_dev_name = mg_facts['minigraph_neighbors'][port_intf]['name']
+    for bgpinfo in  mg_facts['minigraph_bgp']:
+        if bgpinfo['name'] == neighbour_dev_name:
+            nexthop_ip =  bgpinfo['addr']
+            if  ipv6 and ":" not in nexthop_ip :
+                nexthop_ip = ""
+                continue
+            break
+    if nexthop_ip =="":
+        assert False
+    neighbor_addrs = []
+    idx2 =0
+    for idx in range(1, count):
+        if idx %250 ==0:
+            idx2 +=1
+        if ipv6:
+            neighbor_addrs.append(t0_ipv6_pattern.format(idx))
+        else:
+            neighbor_addrs.append(t0_ipv4_pattern.format((idx%250),idx2))
+    
+    return loopback_addr, ptf_intf, nexthop_ip, neighbor_addrs
+
 def init_ptf_bfd(ptfhost):
     ptfhost.shell("bfdd-beacon")
 
@@ -117,7 +154,13 @@ def del_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6=Fa
         if ipv6:
             ptfhost.shell("ip -6 addr del {}/{} dev eth{}".format(neighbor_addrs[idx], prefix_len, neighbor_interfaces[idx]), module_ignore_errors=True)
         else:
-            ptfhost.shell("ip addr del {}/{} dev eth{}".format(neighbor_addrs[idx], prefix_len, neighbor_interfaces[idx]), module_ignore_errors=True)
+            cmd_buffer += "ip addr del {}/{} dev eth{} ;".format(neighbor_addrs[idx], prefix_len,
+                                                                 neighbor_interfaces[idx])
+        if idx % 50 == 0:
+            ptfhost.shell(cmd_buffer, module_ignore_errors=True)
+            cmd_buffer = ""
+    if cmd_buffer != "":
+        ptfhost.shell(cmd_buffer, module_ignore_errors=True)
 
 
 def check_ptf_bfd_status(ptfhost, neighbor_addr, local_addr, expected_state):
@@ -157,8 +200,51 @@ def create_bfd_sessions(ptfhost, duthost, local_addrs, neighbor_addrs):
     if result['rc'] != 0:
         pytest.fail('Failed to apply BFD session configuration file: {}'.format(result['stderr']))
 
+def create_bfd_sessions_multihop(ptfhost, duthost, loopback_addr, ptf_intf, neighbor_addrs):
+    # Create a tempfile for BFD sessions
+    bfd_file_dir = duthost.shell('mktemp')['stdout']
+    ptf_file_dir = ptfhost.shell('mktemp')['stdout']
+    bfd_config = []
+    ptf_config = []
+    for neighbor_addr in neighbor_addrs:
+        bfd_config.append({
+            "BFD_SESSION_TABLE:default:default:{}".format(neighbor_addr): {
+                "local_addr": loopback_addr,
+                "multihop" : "true"
+            },
+            "OP": "SET"
+        })
+        ptf_config.append(
+            {
+                "neighbor_addr": loopback_addr,
+                "local_addr" : neighbor_addr,
+                "multihop" : "true",
+                "ptf_intf" : "eth{}".format(ptf_intf)
+            }
+        )
 
-def remove_bfd_sessions(duthost, local_addrs, neighbor_addrs):
+    # Copy json file to DUT
+    duthost.copy(content=json.dumps(bfd_config, indent=4), dest=bfd_file_dir, verbose=False)
+
+    # Apply BFD sessions with swssconfig
+    result = duthost.shell('docker exec -i swss swssconfig /dev/stdin < {}'.format(bfd_file_dir),
+                           module_ignore_errors=True)
+    if result['rc'] != 0:
+        pytest.fail('Failed to apply BFD session configuration file: {}'.format(result['stderr']))
+    # Copy json file to PTF
+    ptfhost.copy(content=json.dumps(ptf_config, indent=4), dest=ptf_file_dir, verbose=False)
+
+    ptfhost.copy(src=BFD_RESPONDER_SCRIPT_SRC_PATH, dest=BFD_RESPONDER_SCRIPT_DEST_PATH)
+
+    extra_vars = {"bfd_responder_args" : "-c {}".format(ptf_file_dir)}
+    ptfhost.host.options["variable_manager"].extra_vars.update(extra_vars)
+
+    ptfhost.template(src='templates/bfd_responder.conf.j2', dest='/etc/supervisor/conf.d/bfd_responder.conf')
+    ptfhost.command('supervisorctl reread')
+    ptfhost.command('supervisorctl update')
+    ptfhost.command('supervisorctl start bfd_responder')
+
+def remove_bfd_sessions(duthost, neighbor_addrs):
     # Create a tempfile for BFD sessions
     bfd_file_dir = duthost.shell('mktemp')['stdout']
     bfd_config = []
@@ -167,7 +253,6 @@ def remove_bfd_sessions(duthost, local_addrs, neighbor_addrs):
         duthost.shell("sonic-db-cli APPL_DB hmset 'BFD_SESSION_TABLE:default:default:{}' local_addr {}".format(neighbor_addr, local_addrs[idx]))
         bfd_config.append({
             "BFD_SESSION_TABLE:default:default:{}".format(neighbor_addr): {
-                "local_addr": local_addrs[idx]
             },
             "OP": "DEL"
         })
@@ -218,10 +303,9 @@ def test_bfd(rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports_to_r
                 check_ptf_bfd_status(ptfhost, neighbor_addr, local_addrs[idx], "Up")
     finally:
         stop_ptf_bfd(ptfhost)
-        del_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6=False)
-        remove_bfd_sessions(duthost, local_addrs, neighbor_addrs)
-        if 't1' in tbinfo['topo']['name']:
-            remove_dut_ip(duthost, neighbor_devs, local_addrs, prefix_len)
+        del_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6)
+        remove_bfd_sessions(duthost, neighbor_addrs)
+        remove_dut_ip(duthost, neighbor_devs, local_addrs, prefix_len)
 
 
 @pytest.mark.skip(reason="Test may currently fail due to lack of hardware support")
@@ -238,11 +322,12 @@ def test_bfd_ipv6(rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports
         add_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6=True)
         create_bfd_sessions(ptfhost, duthost, local_addrs, neighbor_addrs)
 
-        time.sleep(1)
-
-        for idx, neighbor_addr in enumerate(neighbor_addrs):
-            check_dut_bfd_status(duthost, neighbor_addr, "Up")
-            check_ptf_bfd_status(ptfhost, neighbor_addr, local_addrs[idx], "Up")
+       time.sleep(10)
+        bfd_state = ptfhost.shell("bfdd-control status")
+        dut_state = duthost.shell("show bfd summary")
+        for itr in local_addrs:
+            assert itr in bfd_state['stdout']
+            assert itr in dut_state['stdout']
 
         update_idx = random.choice(range(bfd_session_cnt))
         update_bfd_session_state(ptfhost, neighbor_addrs[update_idx], local_addrs[idx], "down")
@@ -256,7 +341,34 @@ def test_bfd_ipv6(rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports
                 check_ptf_bfd_status(ptfhost, neighbor_addr, local_addrs[idx], "Up")
     finally:
         stop_ptf_bfd(ptfhost)
-        del_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6=True)
-        remove_bfd_sessions(duthost, local_addrs, neighbor_addrs)
-        if 't1' in tbinfo['topo']['name']:
-            remove_dut_ip(duthost, neighbor_devs, local_addrs, prefix_len)
+        del_ipaddr(ptfhost, neighbor_addrs, prefix_len, neighbor_interfaces, ipv6)
+        remove_bfd_sessions(duthost, neighbor_addrs)
+        remove_dut_ip(duthost, neighbor_devs, local_addrs, prefix_len)
+
+
+@pytest.mark.parametrize('ipv6', [False, True], ids=['ipv4', 'ipv6'])
+def test_bfd_multihop(request, rand_selected_dut, ptfhost, tbinfo, toggle_all_simulator_ports_to_rand_selected_tor_m, ipv6):
+    duthost = rand_selected_dut
+
+    bfd_session_cnt = int(request.config.getoption('--num_sessions'))
+    loopback_addr, ptf_intf, nexthop_ip, neighbor_addrs = get_neighbors_multihop(duthost, tbinfo, ipv6, count = bfd_session_cnt)
+    try:
+        cmd_buffer = ""
+        for neighbor in neighbor_addrs:
+            cmd_buffer += 'sudo ip route add {} via {} ;'.format(neighbor, nexthop_ip)
+        duthost.shell(cmd_buffer)
+
+        create_bfd_sessions_multihop(ptfhost, duthost, loopback_addr, ptf_intf, neighbor_addrs)
+
+        time.sleep(1)
+        for neighbor_addr in neighbor_addrs:
+            check_dut_bfd_status(duthost, neighbor_addr, "Up")
+
+    finally:
+        remove_bfd_sessions(duthost, neighbor_addrs)
+        cmd_buffer = ""
+        for neighbor in neighbor_addrs:
+            cmd_buffer += 'sudo ip route delete {} via {} ;'.format(neighbor, nexthop_ip)
+        duthost.shell(cmd_buffer)
+        ptfhost.command('supervisorctl stop bfd_responder')
+        ptfhost.file(path=BFD_RESPONDER_SCRIPT_DEST_PATH, state="absent")
