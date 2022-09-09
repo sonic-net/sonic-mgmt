93f0dd0971641ef754e26f1244981b434422c943
Perumal Venkatesh <vperumal@gmail.com>
2022-09-09 00:01:13 -0700

    Minor fix in run_scripts.py

--- sonic-mgmt/tests/run_scripts.py
+++ sonic-mgmt/tests/run_scripts.py
@@ -287,6 +287,7 @@ def new_run_scripts(script_file,drop_version,log_dir,dut_name,topo_name,tstamp,c
             tc_name = tc_name + "_" + drop_version
 
         print("Executing: {}".format(tc))
+        current_result_file.write("Executing: {}".format(tc));current_result_file.flush()
 
         if collect_logs and dut_address is not None:
             cmd_list = list()
