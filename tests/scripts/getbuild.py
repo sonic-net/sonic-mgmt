be38c388a78372d339ed9eb0b8267d30e76e1f5d
Ye Jianquan <jianquanye@microsoft.com>
2022-09-10 21:12:50 +0800

    Add debug logs and retry function in getbuild.py (#6321)
    Approach
    What is the motivation for this PR?
    Several failures were found during getbuild.py in TestbedV2 pull request test run.
    Add some debug logs and retry function to fix and locate it.
    
    How did you do it?
    Add some debug logs and retry function.
    
    How did you verify/test it?
    The pull request test will verify it.
    
    Signed-off-by: jianquanye@microsoft.com

--- tests/scripts/getbuild.py
+++ tests/scripts/getbuild.py
@@ -10,6 +10,7 @@ _start_time = None
 _last_time = None
 artifact_size = 0
 NOT_FOUND_BUILD_ID = -999
+MAX_DOWNLOAD_TIMES = 3
 
 
 def reporthook(count, block_size, total_size):
@@ -93,13 +94,23 @@ def download_artifacts(url, content_type, platform, buildid, num_asic):
         filename = "{}.zip".format(platform)
 
     if url.startswith('http://') or url.startswith('https://'):
-        print('Downloading {} from build {}...'.format(filename, buildid))
         validate_url_or_abort(url)
-        try:
-            urlretrieve(url, filename, reporthook)
-        except Exception as e:
-            print("Download error", e)
-            sys.exit(1)
+        download_times = 0
+        while download_times < MAX_DOWNLOAD_TIMES:
+            try:
+                print('Downloading {} from build {}...'.format(filename, buildid))
+                download_times += 1
+                urlretrieve(url, filename, reporthook)
+                print('\nDownload finished!')
+                break
+            except Exception as e:
+                print("Download error", e)
+                if download_times < MAX_DOWNLOAD_TIMES:
+                    print('Download times: {}, sleep: {} seconds before retry.'.format(download_times, 30 * download_times))
+                    time.sleep(30 * download_times)
+                    continue
+                else:
+                    sys.exit(1)
 
 def find_latest_build_id(branch, success_flag = "succeeded"):
     """find latest successful build id for a branch"""
@@ -140,6 +151,7 @@ def main():
     if args.buildid is None:
         buildid_succ = find_latest_build_id(args.branch, "succeeded")
         buildid_partial = find_latest_build_id(args.branch, "partiallySucceeded")
+        print('Succeeded buildId:{}, PartiallySucceeded buildId {}'.format(buildid_succ, buildid_partial))
         if buildid_succ == NOT_FOUND_BUILD_ID and buildid_partial == NOT_FOUND_BUILD_ID:
             raise Exception("Can't find 'Succeeded' or 'partiallySucceeded' build result.")
         buildid = max(buildid_succ, buildid_partial)
