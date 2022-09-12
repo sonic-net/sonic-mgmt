8fcced9386bf31b0ce95bb59fb34978a2df5c347
rraghav-cisco <rraghav@cisco.com>
2022-09-12 07:04:34 +0000

    Trying a different path for ptf_utils import.

--- tests/ixia/ecn/test_dequeue_ecn.py
+++ tests/ixia/ecn/test_dequeue_ecn.py
@@ -9,7 +9,7 @@ from tests.common.ixia.qos_fixtures import prio_dscp_map, lossless_prio_list
 
 from files.helper import run_ecn_test, is_ecn_marked
 from tests.common.cisco_data import  get_markings_dut, setup_markings_dut
-from ptf_utils import get_sai_attributes
+from tests.ixia.ptf_utils import get_sai_attributes
 
 pytestmark = [ pytest.mark.topology('tgen') ]
 
