ca233fdde16f975024e0da989f08e9cb90a3f4ca
Roman Savchuk <romanx.savchuk@intel.com>
2022-09-20 05:26:14 -0700

    [ test_service_warm_restart ] added missed import

--- tests/platform_tests/test_service_warm_restart.py
+++ tests/platform_tests/test_service_warm_restart.py
@@ -1,6 +1,7 @@
 import pytest
 import logging
 
+from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # lgtm[py/unused-import]
 from tests.common.helpers.assertions import pytest_require
 from tests.common.utilities import skip_release
 from tests.platform_tests.verify_dut_health import verify_dut_health      # lgtm[py/unused-import]
