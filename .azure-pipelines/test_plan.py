2c7915b2cae897f6032e44e3c7848b79b54028c4
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2022-09-29 21:38:50 +0800

    Split Running job step in testbedV2 pipeline.  (#6338)
    Description of PR
    In testbed v2, we only have one Running test step to trace all status of a testplan. In this pr, we split this Running test into Lock testbed, Prepare testbed and Running test to trace the status more explicitly.
    
    What is the motivation for this PR?
    In testbed v2, we only have one Running test step to trace all status of a testplan. In this pr, we split this Running test into Lock testbed, Prepare testbed and Running test to trace the status more explicitly.
    
    How did you do it?
    Split Running test step into three steps.
    
    Signed-off-by: Yutong Zhang <yutongzhang@microsoft.com>

--- .azure-pipelines/test_plan.py
+++ .azure-pipelines/test_plan.py
@@ -140,7 +140,13 @@ class TestPlanManager(object):
         print("Result of cancelling test plan at {}:".format(tp_url))
         print(str(resp["data"]))
 
-    def poll(self, test_plan_id, interval=60, timeout=36000):
+    def poll(self, test_plan_id, interval=60, timeout=36000, expected_state=""):
+        '''
+        The states of testplan can be described as below:
+                                                                |-- FAILED
+        INIT -- LOCK_TESTBED -- PREPARE_TESTBED -- EXECUTING -- |-- CANCELLED
+                                                                |-- FINISHED
+        '''
 
         print("Polling progress and status of test plan at https://www.testbed-tools.org/scheduler/testplan/{}" \
               .format(test_plan_id))
@@ -154,17 +160,15 @@ class TestPlanManager(object):
         start_time = time.time()
         http_exception_times = 0
         while (time.time() - start_time) < timeout:
-            raw_resp = {}
             try:
-                raw_resp = requests.get(poll_url, headers=headers, timeout=10)
-                resp = raw_resp.json()
+                resp = requests.get(poll_url, headers=headers, timeout=10).json()
             except Exception as exception:
-                print("HTTP execute failure, url: {}, raw_resp: {}, exception: {}".format(poll_url, str(raw_resp),
+                print("HTTP execute failure, url: {}, raw_resp: {}, exception: {}".format(poll_url, resp,
                                                                                           str(exception)))
                 http_exception_times = http_exception_times + 1
                 if http_exception_times >= TOLERATE_HTTP_EXCEPTION_TIMES:
                     raise Exception("HTTP execute failure, url: {}, raw_resp: {}, exception: {}"
-                                    .format(poll_url, str(raw_resp), str(exception)))
+                                    .format(poll_url, resp, str(exception)))
                 else:
                     time.sleep(interval)
                     continue
@@ -186,13 +190,16 @@ class TestPlanManager(object):
                 else:
                     raise Exception("Test plan id: {}, status: {}, result: {}, Elapsed {:.0f} seconds" \
                                     .format(test_plan_id, status, result, time.time() - start_time))
-            print("Test plan id: {}, status: {}, progress: {}%, elapsed: {:.0f} seconds" \
-                  .format(test_plan_id, status, resp_data.get("progress", 0) * 100, time.time() - start_time))
-            time.sleep(interval)
+            elif status == expected_state:
+                return
+            else:
+                print("Test plan id: {}, status: {}, progress: {}%, elapsed: {:.0f} seconds" \
+                      .format(test_plan_id, status, resp_data.get("progress", 0) * 100, time.time() - start_time))
+                time.sleep(interval)
+
         else:
             raise Exception("Max polling time reached, test plan at {} is not successfully finished or cancelled" \
                             .format(poll_url))
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -270,7 +277,7 @@ if __name__ == "__main__":
     parser_poll = subparsers.add_parser("poll", help="Poll test plan status.")
     parser_cancel = subparsers.add_parser("cancel", help="Cancel running test plan.")
 
-    for p in [parser_poll, parser_cancel]:
+    for p in [parser_cancel, parser_poll]:
         p.add_argument(
             "-i", "--test-plan-id",
             type=int,
@@ -279,6 +286,15 @@ if __name__ == "__main__":
             help="Test plan id."
         )
 
+    parser_poll.add_argument(
+        "-e", "--expected-state",
+        type=str,
+        dest="expected_state",
+        required=False,
+        help="Expected state.",
+        choices = ["PREPARE_TESTBED", "EXECUTING", "FINISHED"],
+        default="FINISHED"
+    )
     parser_poll.add_argument(
         "--interval",
         type=int,
@@ -357,7 +373,7 @@ if __name__ == "__main__":
                 output=args.output
             )
         elif args.action == "poll":
-            tp.poll(args.test_plan_id, args.interval, args.timeout)
+            tp.poll(args.test_plan_id, args.interval, args.timeout, args.expected_state)
         elif args.action == "cancel":
             tp.cancel(args.test_plan_id)
         sys.exit(0)
