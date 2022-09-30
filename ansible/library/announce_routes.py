dc93cd07ac6d99cf784e4026425d77631b4f8637
stormliang <stormliang@microsoft.com>
2022-09-30 02:13:16 +0000

    fix announce_route

--- ansible/library/announce_routes.py
+++ ansible/library/announce_routes.py
@@ -206,8 +206,8 @@ def generate_routes(family, podset_number, tor_number, tor_subnet_number,
                     # Skip non podset 0 for T0
                     if podset != 0:
                         continue
-                    # Skip subnet 0 (vlan ip) for M0
-                    elif topo == "m0" and subnet == 0:
+                    # Skip subnet 0 (vlan ip)
+                    elif subnet == 0:
                         continue
                     elif tor != tor_index:
                         continue
