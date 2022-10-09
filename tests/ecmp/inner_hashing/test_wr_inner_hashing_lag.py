00899390448b2b5723efb0922e0630c9ac7c4a02
stormliang <stormliang@microsoft.com>
2022-10-09 15:03:02 +0000

    reduce time by add completeness level

--- tests/ecmp/inner_hashing/test_wr_inner_hashing_lag.py
+++ tests/ecmp/inner_hashing/test_wr_inner_hashing_lag.py
@@ -1,3 +1,4 @@
+from locale import normalize
 import logging
 import threading
 import pytest
@@ -6,6 +7,7 @@ import allure
 
 from datetime import datetime
 from tests.common import reboot
+from tests.common.plugins.test_completeness import normalize_levels
 from tests.ecmp.inner_hashing.conftest import get_src_dst_ip_range, FIB_INFO_FILE_DST, VXLAN_PORT,\
     PTF_QLEN, OUTER_ENCAP_FORMATS, NVGRE_TNI, setup_lag_config, config_pbh_lag
 from tests.ptf_runner import ptf_runner
@@ -29,7 +31,8 @@ class TestWRDynamicInnerHashingLag():
             config_pbh_lag(duthost, lag_port_map)
 
     def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
-                           vlan_ptf_ports, symmetric_hashing, localhost, lag_mem_ptf_ports_groups):
+                           vlan_ptf_ports, symmetric_hashing, localhost, lag_mem_ptf_ports_groups,
+                           get_function_completeness_level):
         logging.info("Executing warm boot dynamic inner hash test for outer {} and inner {} with symmetric_hashing"
                      " set to {}".format(outer_ipver, inner_ipver, str(symmetric_hashing)))
         timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
@@ -43,8 +46,14 @@ class TestWRDynamicInnerHashingLag():
         outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
         inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
 
-        balancing_test_times = 200
-        balancing_range = 0.3
+        normalize_level = get_function_completeness_level if get_function_completeness_level else 'thorough'
+        
+        if normalize_level == 'thorough':
+            balancing_test_times = 200
+            balancing_range = 0.3
+        else:
+            balancing_test_times = 100
+            balancing_range = 0.3
 
         reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm', 10, 0, 0, True, True,))
         reboot_thr.start()
