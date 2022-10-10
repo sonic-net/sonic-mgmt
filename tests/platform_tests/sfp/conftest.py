a984954417389fb60183c308fa2913f0a9a90fd9
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2022-10-10 18:21:06 +0800

    Revert [LogAnalyzer] Fixed sfp tests - to use common loganalyzer #5301 (#6493)
    What is the motivation for this PR?
    #5301 introduced the issue #6491,
    platform_tests.sfp.test_sfputil keeps failing on Mellanox platforms, since loganalyzer parameter is None.
    
    How did you do it?
    Revert #5301
    
    How did you verify/test it?
    Run platform_tests/sfp/test_sfputil.py
    
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

--- tests/platform_tests/sfp/conftest.py
+++ tests/platform_tests/sfp/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import logging
 import os
+from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer
 
 ans_host = None
 
@@ -12,17 +13,16 @@ def teardown_module():
 
 
 @pytest.fixture(autouse=True)
-def update_la_ignore_errors_list_for_mlnx(duthost, loganalyzer):
+def update_la_ignore_errors_list_for_mlnx(duthost):
     if duthost.facts["asic_type"] in ["mellanox"]:
-        for host in loganalyzer:
-            loganalyzer[host].ignore_regex.append("kernel.*Eeprom query failed*")
-            # Ignore PMPE error https://github.com/sonic-net/sonic-buildimage/issues/7163
-            loganalyzer[host].ignore_regex.append(r".*ERR pmon#xcvrd: Receive PMPE error event on module.*")
+        loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix='sfp_cfg')
+        loganalyzer.load_common_config()
+        loganalyzer.ignore_regex.append("kernel.*Eeprom query failed*")
+        # Ignore PMPE error https://github.com/sonic-net/sonic-buildimage/issues/7163
+        loganalyzer.ignore_regex.append(r".*ERR pmon#xcvrd: Receive PMPE error event on module.*")
+        marker = loganalyzer.init()
 
     yield
 
     if duthost.facts["asic_type"] in ["mellanox"]:
-        for host in loganalyzer:
-            loganalyzer[host].ignore_regex.remove("kernel.*Eeprom query failed*")
-            # Remove Ignore PMPE error https://github.com/sonic-net/sonic-buildimage/issues/7163
-            loganalyzer[host].ignore_regex.remove(r".*ERR pmon#xcvrd: Receive PMPE error event on module.*")
+        loganalyzer.analyze(marker)
