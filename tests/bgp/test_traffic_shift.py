a46b3195ebabed51b232c1a0e5a7b4a5caa611f4
stormliang <stormliang@microsoft.com>
2022-10-13 15:04:10 +0000

    fix error

--- tests/bgp/test_traffic_shift.py
+++ tests/bgp/test_traffic_shift.py
@@ -282,7 +282,7 @@ def test_TSA_B_C_with_no_neighbors(duthost, bgpmon_setup_teardown, nbrhosts, tbi
         pytest_assert(wait_until(300, 3, 0, verify_all_routes_announce_to_neighs, duthost, nbrhosts, routes_6, 6),
                       "Not all ipv6 routes are announced to neighbors")
 
-
+@pytest.mark.disable_loganalyzer
 def test_TSA_TSB_with_config_reload(duthost, ptfhost, nbrhosts, bgpmon_setup_teardown, traffic_shift_community):
     """
     Test TSA after config save and config reload
@@ -323,7 +323,7 @@ def test_TSA_TSB_with_config_reload(duthost, ptfhost, nbrhosts, bgpmon_setup_tea
         pytest_assert(verify_all_routes_announce_to_neighs(duthost, nbrhosts, parse_rib(duthost, 6), 6),
                       "Not all ipv6 routes are announced to neighbors")
 
-
+@pytest.mark.disable_loganalyzer
 def test_load_minigraph_with_traffic_shift_away(duthost, ptfhost, nbrhosts, bgpmon_setup_teardown,
                                                 traffic_shift_community):
     """
