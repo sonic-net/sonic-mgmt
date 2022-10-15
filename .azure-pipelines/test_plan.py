d30cd0a2c17b4052cff50ee7898e686c1a5d01a9
jianquanye <jianquanye@microsoft.com>
2022-10-15 16:11:05 +0800

    Fix format

--- .azure-pipelines/test_plan.py
+++ .azure-pipelines/test_plan.py
@@ -51,7 +51,8 @@ class TestPlanManager(object):
     def create(self, topology, test_plan_name="my_test_plan", deploy_mg_extra_params="", kvm_build_id="",
                min_worker=1, max_worker=2, pr_id="unknown", scripts=[], output=None):
         tp_url = "{}/test_plan".format(self.url)
-        print("Creating test plan, topology: {}, name: {}, build info:{} {} {}".format(topology, test_plan_name, repo_name, pr_id, build_id))
+        print("Creating test plan, topology: {}, name: {}, build info:{} {} {}".format(topology, test_plan_name,
+                                                                                       repo_name, pr_id, build_id))
         print("Test scripts to be covered in this test plan:")
         print(json.dumps(scripts, indent=4))
 
@@ -200,6 +201,7 @@ class TestPlanManager(object):
         else:
             raise Exception("Max polling time reached, test plan at {} is not successfully finished or cancelled" \
                             .format(poll_url))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
