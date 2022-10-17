eebf765bc396d6ce8ab5ce61d54d50265e6282a6
Anton Ptashnik <antonx.ptashnik@intel.com>
2022-10-17 11:19:42 +0300

    vxlan vnet tests: (#6063)
    Summary: enabled VxLAN VNET tests for Barefoot
    
    moved skip condition to a dedicated file tests_mark_conditions
    enabled tests for Barefoot

--- tests/vxlan/test_vnet_vxlan.py
+++ tests/vxlan/test_vnet_vxlan.py
@@ -24,7 +24,6 @@ logger = logging.getLogger(__name__)
 pytestmark = [
     pytest.mark.topology("t0"),
     pytest.mark.sanity_check(post_check=True),
-    pytest.mark.asic("mellanox"),
     pytest.mark.disable_loganalyzer
 ]
 
