2901fb34f1b3118f8d4a6263027b9997648f44a9
dojha <devojha@microsoft.com>
2022-10-24 18:49:38 +0000

    fixed pre-commit checks

--- tests/snmp/conftest.py
+++ tests/snmp/conftest.py
@@ -1,15 +1,18 @@
 import pytest
 from tests.common.utilities import wait_until
 
+
 @pytest.fixture(scope="module", autouse=True)
 def setup_check_snmp_ready(duthosts):
     for duthost in duthosts:
         assert wait_until(300, 20, 0, duthost.is_service_fully_started, "snmp"), "SNMP service is not running"
 
+
 @pytest.fixture(scope="module", autouse=True)
 def enable_queue_counterpoll_type(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     duthost.command('counterpoll queue enable')
+
 
 def pytest_addoption(parser):
     """
