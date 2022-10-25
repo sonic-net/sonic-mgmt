7722fd3611439c2a4aa9a9ab2cb361d6ea8b70d0
Azarack <jethro888222@yahoo.com>
2022-10-25 15:57:46 -0400

    BGP session flapping test case

--- tests/bgp/test_bgp_session_flap.py
+++ tests/bgp/test_bgp_session_flap.py
@@ -6,7 +6,6 @@ import logging
 import pytest
 import time
 from tests.common.utilities import InterruptableThread
-# from tests.common.utilities import join_all
 import textfsm
 import traceback
 
@@ -18,55 +17,51 @@ wait_time = 2
 stop_threads = False
 proc_textfsm = "./bgp/templates/show_proc_cpu.template"
 bgp_sum_textfsm = "./bgp/templates/bgp_summary.template"
+flap_threads = []
+skip_hosts = []
 
-# on current virtual testbed the sanity check is not working
-pytestmark = [pytest.mark.sanity_check(skip_sanity=True)]
+pytestmark = [
+    pytest.mark.topology('t1')
+]
 
 
 def get_cpu_stats(dut):
-    # cmd = 'vtysh -c "show processes cpu" '\
-    #     '-c "show processes memory" '\
-    #     '-c "show processes summary" '\
-    #     '-c "show ip bgp summary" '\
-    #     '-c "show ipv6 bgp summary"'
-    # logger.info(dut.shell(cmd, module_ignore_errors=True))
     proc_cpu = dut.shell("show processes cpu | head -n 20", module_ignore_errors=True)['stdout']
     proc_mem = dut.shell("show processes memory | head -n 20", module_ignore_errors=True)['stdout']
     proc_sum = dut.shell("show processes summary | grep -v '0.0 0.0'", module_ignore_errors=True)['stdout']
     bgp_cpu = dut.shell("show processes cpu | grep bgp", module_ignore_errors=True)['stdout']
     bgp_v4_sum = dut.shell("show ip bgp summary | grep memory", module_ignore_errors=True)['stdout']
     bgp_v6_sum = dut.shell("show ipv6 bgp summary | grep memory", module_ignore_errors=True)['stdout']
-    logger.info("CPU: {} Memory: {} Summary: {} BGP Memory: {} BGP IPv4: {} IPv6: {}"
+    logger.info("CPU:\n{}\nMemory:\n{}\nSummary:\n{}\nBGP Memory:\n{}\nBGP IPv4:\n{}\nIPv6:\n{}\n"
                 .format(proc_cpu, proc_mem, proc_sum, bgp_cpu, bgp_v4_sum, bgp_v6_sum))
     with open(proc_textfsm) as template:
         fsm = textfsm.TextFSM(template)
-        result = fsm.ParseText(proc_cpu)
-    logger.info(fsm.header)
-    logger.info(result)
+        parsed_cpu = fsm.ParseText(proc_cpu)[0]
+
+    with open(bgp_sum_textfsm) as template:
+        fsm = textfsm.TextFSM(template)
+        parsed_ipv4 = fsm.ParseText(bgp_v4_sum)[0]
+        parsed_ipv6 = fsm.ParseText(bgp_v6_sum)[0]
+    data = [float(parsed_cpu[0]), float(parsed_cpu[1]), float(parsed_cpu[2]),
+            float(parsed_ipv4[0]), float(parsed_ipv4[1]), float(parsed_ipv4[2]),
+            float(parsed_ipv6[0]), float(parsed_ipv6[1]), float(parsed_ipv6[2])]
+    return data
 
 
 @pytest.fixture(scope='module')
 def setup(tbinfo, nbrhosts, duthosts, rand_one_dut_hostname, enum_asic_index):
     duthost = duthosts[rand_one_dut_hostname]
-    dut_asn = tbinfo['topo']['properties']['configuration_properties']['common']['dut_asn']
 
-    tor_neighbors = natsorted([neighbor for neighbor in nbrhosts.keys() if neighbor.endswith('T0')])
-    tor1 = tor_neighbors[0]
+    tor_neighbors = dict()
+    tor1 = natsorted([neighbor for neighbor in nbrhosts.keys() if neighbor.endswith('T0')])[0]
 
     bgp_facts = duthost.bgp_facts(instance_id=enum_asic_index)['ansible_facts']
+    neigh_asn = dict()
     for k, v in bgp_facts['bgp_neighbors'].items():
-        if v['description'] == tor1:
-            if v['ip_version'] == 4:
-                neigh_ip_v4 = k
-                peer_group_v4 = v['peer group']
-            elif v['ip_version'] == 6:
-                neigh_ip_v6 = k
-                peer_group_v6 = v['peer group']
-            neigh_asn = v['remote AS']
+        if v['description'] not in skip_hosts:
+            neigh_asn[v['description']] = v['remote AS']
+            tor_neighbors[v['description']] = nbrhosts[v['description']]["host"]
             assert v['state'] == 'established'
-
-    dut_ip_v4 = tbinfo['topo']['properties']['configuration'][tor1]['bgp']['peers'][dut_asn][0]
-    dut_ip_v6 = tbinfo['topo']['properties']['configuration'][tor1]['bgp']['peers'][dut_asn][1]
 
     # verify sessions are established
     logger.info(duthost.shell('show ip bgp summary'))
@@ -74,16 +69,10 @@ def setup(tbinfo, nbrhosts, duthosts, rand_one_dut_hostname, enum_asic_index):
 
     setup_info = {
         'duthost': duthost,
-        'neighhost': nbrhosts[tor1]["host"],
-        'tor1': tor1,
-        'dut_asn': dut_asn,
-        'neigh_asn': neigh_asn,
-        'dut_ip_v4': dut_ip_v4,
-        'dut_ip_v6': dut_ip_v6,
-        'neigh_ip_v4': neigh_ip_v4,
-        'neigh_ip_v6': neigh_ip_v6,
-        'peer_group_v4': peer_group_v4,
-        'peer_group_v6': peer_group_v6
+        'neighhost': tor_neighbors[tor1],
+        'neigh_asn': neigh_asn[tor1],
+        'asn_dict':  neigh_asn,
+        'neighbors': tor_neighbors
     }
 
     logger.info("DUT BGP Config: {}".format(duthost.shell("show run bgp", module_ignore_errors=True)))
@@ -94,39 +83,14 @@ def setup(tbinfo, nbrhosts, duthosts, rand_one_dut_hostname, enum_asic_index):
     # get baseline BGP CPU and Memory Utilization
     get_cpu_stats(duthost)
 
-    return setup_info
+    yield setup_info
 
-
-def flap_neighbor_session(neigh, asn):
-    while(True):
-        # cmd = ["shutdown"]
-        # neigh.eos_config(lines=cmd, parents="router bgp {}".format(asn))
-        # neigh.eos_command(commands=["enable", "config", "router bgp {}".format(asn), "shutdown"],
-        #     answer="", prompt="\[confirm\]")
-        # neigh.eos_config(lines=["no shutdown"], parents="router bgp {}".format(asn))
-        neigh.kill_bgpd()
-        neigh.start_bgpd()
-        if stop_threads:
-            break
-
-
-def test_bgp_single_session_flaps(setup):
-    flap_threads = []
-
-    thread = InterruptableThread(
-        target=flap_neighbor_session,
-        args=(setup['neighhost'], setup['neigh_asn']))
-    thread.daemon = True
-    thread.start()
-    flap_threads.append(thread)
-
-    for i in range(10):
-        get_cpu_stats(setup['duthost'])
-        time.sleep(wait_time)
-
-    logger.info("Wait for all the threads to stop ...")
     global stop_threads
+    global flap_threads
     stop_threads = True
+    time.sleep(wait_time)
+
+    # ensure no threads are left running
     for thread in flap_threads:
         thread_exception = thread.join(timeout=0.1,
                                        suppress_exception=True)
@@ -135,5 +99,106 @@ def test_bgp_single_session_flaps(setup):
             logger.debug(
                 "".join(traceback.format_exception(*thread_exception))
             )
-    # join_all(flap_threads, max_wait)
+    flap_threads = []
+
+
+def flap_neighbor_session(neigh, asn):
+    while(True):
+        neigh.kill_bgpd()
+        neigh.start_bgpd()
+        if stop_threads:
+            logger.info("stop_threads now true, breaking loop")
+            break
+
+
+def test_bgp_single_session_flaps(setup):
+    global flap_threads
+    global stop_threads
+    stop_threads = False
+
+    # get baseline stat information
+    stats = []
+    stats.append(get_cpu_stats(setup['duthost']))
+
+    # start threads to flap neighbor sessions
+    thread = InterruptableThread(
+        target=flap_neighbor_session,
+        args=(setup['neighhost'], setup['neigh_asn']))
+    thread.daemon = True
+    thread.start()
+    flap_threads.append(thread)
+
+    for i in range(10):
+        stats.append(get_cpu_stats(setup['duthost']))
+        index = len(stats) - 1
+        assert stats[index][0] < (stats[index - 1][0] + 10)
+        assert stats[index][1] < (stats[index - 1][1] + 10)
+        assert stats[index][2] < (stats[index - 1][2] + 10)
+        assert stats[index][3] < (stats[index - 1][3] * 1.3)
+        assert stats[index][4] < (stats[index - 1][4] * 1.3)
+        assert stats[index][5] < (stats[index - 1][5] * 1.3)
+        assert stats[index][6] < (stats[index - 1][6] * 1.3)
+        assert stats[index][7] < (stats[index - 1][7] * 1.3)
+        assert stats[index][8] < (stats[index - 1][8] * 1.3)
+
+        time.sleep(wait_time)
+
+    stop_threads = True
+    time.sleep(wait_time)
+
+    for thread in flap_threads:
+        thread_exception = thread.join(timeout=0.1,
+                                       suppress_exception=True)
+        if thread_exception:
+            logger.debug("Exception in thread %r:", thread)
+            logger.debug(
+                "".join(traceback.format_exception(*thread_exception))
+            )
+    flap_threads = []
+
+
+def test_bgp_multiple_session_flaps(setup):
+    global flap_threads
+    global stop_threads
+    stop_threads = False
+
+    # get baseline stat information
+    stats = []
+    stats.append(get_cpu_stats(setup['duthost']))
+
+    # start threads to flap neighbor sessions
+    for neigh in setup['neighbors']:
+        thread = InterruptableThread(
+            target=flap_neighbor_session,
+            args=(neigh, setup['asn_dict'][str(neigh)]))
+        thread.daemon = True
+        thread.start()
+        flap_threads.append(thread)
+
+    for i in range(10):
+        stats.append(get_cpu_stats(setup['duthost']))
+        index = len(stats) - 1
+        assert stats[index][0] < (stats[index - 1][0] + 10)
+        assert stats[index][1] < (stats[index - 1][1] + 10)
+        assert stats[index][2] < (stats[index - 1][2] + 10)
+        assert stats[index][3] < (stats[index - 1][3] * 1.3)
+        assert stats[index][4] < (stats[index - 1][4] * 1.3)
+        assert stats[index][5] < (stats[index - 1][5] * 1.3)
+        assert stats[index][6] < (stats[index - 1][6] * 1.3)
+        assert stats[index][7] < (stats[index - 1][7] * 1.3)
+        assert stats[index][8] < (stats[index - 1][8] * 1.3)
+
+        time.sleep(wait_time)
+
+    stop_threads = True
+    time.sleep(wait_time)
+
+    for thread in flap_threads:
+        thread_exception = thread.join(timeout=0.1,
+                                       suppress_exception=True)
+        if thread_exception:
+            logger.debug("Exception in thread %r:", thread)
+            logger.debug(
+                "".join(traceback.format_exception(*thread_exception))
+            )
     flap_threads = []
