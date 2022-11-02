f3c5c996dd121566e5c884b3de0b991086ccb819
Xu Chen <xuchen3@microsoft.com>
2022-11-02 13:25:07 +0800

    fix frame DMAC addr of traffic for dual T0 scenario

--- tests/saitests/sai_qos_tests.py
+++ tests/saitests/sai_qos_tests.py
@@ -731,6 +731,13 @@ class PFCtest(sai_base_test.ThriftInterfaceDataPlane):
             packet_length = int(self.test_params['packet_size'])
         else:
             packet_length = 64
+
+        is_dualtor = bool(self.test_params.get('is_dualtor', False))
+        is_t0 = bool(self.test_params.get('is_t0', False))
+        def_vlan_mac = self.test_params.get('def_vlan_mac', None)
+        if is_dualtor and is_t0 and bool(def_vlan_mac):
+            pkt_dst_mac = def_vlan_mac
+
         pkt = construct_ip_pkt(packet_length,
                                pkt_dst_mac,
                                src_port_mac,
