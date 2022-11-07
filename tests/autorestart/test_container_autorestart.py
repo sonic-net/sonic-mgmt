5009492126eeda51d62a4de7ec5d6806b2bb9a03
Xichen96 <lukelin0907@gmail.com>
2022-11-07 15:15:18 +0800

    [autorestart] skip loganalizer error (#6415)
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>

--- tests/autorestart/test_container_autorestart.py
+++ tests/autorestart/test_container_autorestart.py
@@ -67,6 +67,14 @@ def ignore_expected_loganalyzer_exception(duthosts, enum_rand_one_per_hwsku_host
         Fifth, systemd would fire an error message:"ERR systemd[1]: Failed to start SNMP/TEAMD container." since
         SNMP/TEAMD container hits the limitation of restart. route_check.py also wrote an error message into syslog.
 
+        Sixth, after a process is killed, its network resources are not immediately released. So it might take some time
+        for the ports to be available again. The problem might be more pronounced with weak devices. So we expect some
+        failures with listening or binding to a socket. When encountering this problem, the process will be repeated
+        and it typically resolves by itself. So we skip "Unable to initialize team socket" in teamsyncd and "Failed to
+        bind socket" in dhcprelay.
+
+        Also invalid OID is more of a warning. So we skip messages with keyword "invalid OID".
+
     """
     swss_syncd_teamd_regex = [
             ".*ERR swss[0-9]*#orchagent.*removeLag.*",
@@ -77,6 +85,8 @@ def ignore_expected_loganalyzer_exception(duthosts, enum_rand_one_per_hwsku_host
             ".*ERR syncd[0-9]*#syncd.*SAI_API_SWITCH:sai_object_type_get_availability.*",
             ".*ERR syncd[0-9]*#syncd.*sendApiResponse: api SAI_COMMON_API_SET failed in syncd mode.*",
             ".*ERR syncd[0-9]*#syncd.*processQuadEvent.*",
+            ".*ERR syncd[0-9]*#syncd.*process_on_fdb_event: invalid OIDs in fdb notifications.*",
+            ".*ERR syncd[0-9]*#syncd.*process_on_fdb_event: FDB notification was not sent since it contain invalid OIDs.*",
             ".*ERR syncd[0-9]*#syncd.*saiGetMacAddress: failed to get mac address: SAI_STATUS_ITEM_NOT_FOUND.*",
             ".*ERR syncd[0-9]*#SDK.*mlnx_bridge_1d_oid_to_data: Unexpected bridge type 0 is not 1D.*",
             ".*ERR syncd[0-9]*#SDK.*mlnx_bridge_port_lag_or_port_get: Invalid port type - 2.*",
@@ -92,10 +102,12 @@ def ignore_expected_loganalyzer_exception(duthosts, enum_rand_one_per_hwsku_host
             ".*ERR swss[0-9]*#fdbsyncd.*readData.*netlink reports an error=-25 on reading a netlink socket.*",
             ".*ERR swss[0-9]*#portsyncd.*readData.*netlink reports an error=-33 on reading a netlink socket.*",
             ".*ERR teamd[0-9]*#teamsyncd.*readData.*netlink reports an error=-33 on reading a netlink socket.*",
+            ".*ERR teamd[0-9]*#teamsyncd.*readData.*Unable to initialize team socket.*",
             ".*ERR swss[0-9]*#orchagent.*set status: SAI_STATUS_ATTR_NOT_IMPLEMENTED_0.*",
             ".*ERR swss[0-9]*#orchagent.*setIntfVlanFloodType.*",
             ".*ERR swss[0-9]*#buffermgrd.*Failed to process invalid entry.*",
             ".*ERR snmp#snmpd.*",
+            ".*ERR dhcp_relay#dhcp6?relay.*bind: Failed to bind socket to link local ipv6 address on interface .* after [0-9]+ retries",
         ]
     ignore_regex_dict = {
         'common': [
