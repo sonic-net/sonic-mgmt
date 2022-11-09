d453be6f4ca456e3f70b7cdc5cea24284130eca5
Xichen96 <lukelin0907@gmail.com>
2022-11-09 16:09:32 +0800

    [202012][platfrom_tests] fix test_cpu_memory_usage (#6569) (#6785)
    What is the motivation for this PR?
    Celestica e1031 is a weak device with low memory. High memory consumption is expected. Also if we factor in the number of cores, CPU usage can be more accurate.
    
    How did you verify/test it?
    Run tests on celestica e1031

--- tests/platform_tests/test_cpu_memory_usage.py
+++ tests/platform_tests/test_cpu_memory_usage.py
@@ -28,14 +28,16 @@ def setup_thresholds(duthosts, enum_rand_one_per_hwsku_hostname):
     memory_threshold = 60
     high_cpu_consume_procs = {}
     is_asan = is_asan_image(duthosts, enum_rand_one_per_hwsku_hostname)
-    if duthost.facts['platform'] in ('x86_64-arista_7050_qx32', 'x86_64-kvm_x86_64-r0') or is_asan:
-        memory_threshold = 80
+    if duthost.facts['platform'] in ('x86_64-arista_7050_qx32', 'x86_64-kvm_x86_64-r0', 'x86_64-cel_e1031-r0') or is_asan:
+        memory_threshold = 90
     if duthost.facts['platform'] in ('x86_64-arista_7260cx3_64'):
         high_cpu_consume_procs['syncd'] = 80
     # The CPU usage of `sx_sdk` on mellanox is expected to be higher, and the actual CPU usage
     # is correlated with the number of ports. So we ignore the check of CPU for sx_sdk
     if duthost.facts["asic_type"] == 'mellanox':
         high_cpu_consume_procs['sx_sdk'] = 90
+    num_cpu = int(duthost.command('nproc --all')['stdout_lines'][0])
+    cpu_threshold = cpu_threshold * num_cpu
     return memory_threshold, cpu_threshold, high_cpu_consume_procs
 
 def test_cpu_memory_usage(duthosts, enum_rand_one_per_hwsku_hostname, setup_thresholds):
@@ -58,7 +60,7 @@ def test_cpu_memory_usage(duthosts, enum_rand_one_per_hwsku_hostname, setup_thre
         for proc in monit_result.processes:
             cpu_threshold = normal_cpu_threshold
             if high_cpu_consume_procs.has_key(proc['name']):
-                cpu_threshold = high_cpu_consume_procs[proc['name']]                
+                cpu_threshold = high_cpu_consume_procs[proc['name']]
             if proc['cpu_percent'] >= cpu_threshold:
                 logging.debug("process %s(%d) cpu usage exceeds %d%%.",
                               proc['name'], proc['pid'], cpu_threshold)
