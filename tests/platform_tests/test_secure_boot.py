bbc14f9e9d16c362fdffee7382119faf9740ef16
Azmy Ali <azmya@nvidia.com>
2022-11-09 11:59:01 +0200

    Adding new test case for non secure upgarde failure check: 	in this test case we validate non successful install of a given non secure image file adjusting existing upgrade test to be able to run on canonical setup by removing redundant imports

---
+++ tests/platform_tests/test_secure_boot.py
@@ -0,0 +1,48 @@
+"""
+this tests checks secure boot upgrade
+"""
+import logging
+import pytest
+from tests.common.errors import RunAnsibleModuleFail
+from tests.common import reboot
+from tests.common.helpers.assertions import pytest_assert
+from tests.upgrade_path.upgrade_helpers import check_services, check_sonic_version, \
+     install_sonic, check_reboot_cause
+from ansible.utils.unsafe_proxy import AnsibleUnsafeText
+from tests.upgrade_path.test_upgrade_path import upgrade_path_lists
+
+pytestmark = [
+    pytest.mark.topology('any'),
+    pytest.mark.device_type('physical'),
+    pytest.mark.disable_loganalyzer,
+]
+
+logger = logging.getLogger(__name__)
+
+
+def test_non_secure_boot_upgrade_failure(duthosts, enum_rand_one_per_hwsku_hostname, localhost, upgrade_path_lists, tbinfo, capsys):
+    """
+    @summary: This test case validates non successful upgrade of a given non secure image
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+    upgrade_type, _, non_secure_img, _ = upgrade_path_lists
+    logger.info("get current version installed")
+    current_version = duthost.image_facts()['ansible_facts']['ansible_image_facts']['current']
+    # install non secure image
+    logger.info("install non secure image - expect fail, image url = {}".format(non_secure_img))
+    result = "image install failure" # because we expect fail
+    try:
+        # in case of success result will return target image name
+        result = install_sonic(duthost, non_secure_img, tbinfo)
+    except RunAnsibleModuleFail as err:
+        err_msg = str(err.results._check_key("module_stdout"))
+        logger.info("Expected fail, msg : {}".format(err_msg))
+        pytest_assert("Failure: CMS signature verification failed" in str(err_msg), "failure was not due to security limitations")
+    finally:
+        pytest_assert(result=="image install failure", "install non secure image should not succeed")
+        logger.info("Cold reboot the DUT")
+        reboot(duthost, localhost)
+        logger.info("Check version has not changed after reboot")
+        check_sonic_version(duthost, current_version)
+        check_reboot_cause(duthost, upgrade_type)
+        check_services(duthost)
