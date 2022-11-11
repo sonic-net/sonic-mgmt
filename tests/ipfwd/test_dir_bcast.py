410d558305e844a25dddb2715b00192361e3a983
Yaqiang Zhu <zyq1512099831@gmail.com>
2022-11-11 13:42:30 +0800

    [mx] Add condition marker for mx (#6734)
    What is the motivation for this PR?
    Add condition marker for mx

--- tests/ipfwd/test_dir_bcast.py
+++ tests/ipfwd/test_dir_bcast.py
@@ -5,7 +5,7 @@ from tests.ptf_runner import ptf_runner
 from datetime import datetime
 
 pytestmark = [
-    pytest.mark.topology('t0', 'm0')
+    pytest.mark.topology('t0', 'm0', 'mx')
 ]
 
 def test_dir_bcast(duthosts, rand_one_dut_hostname, ptfhost, tbinfo):
