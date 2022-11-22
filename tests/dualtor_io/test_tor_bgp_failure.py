sonic-net/sonic-mgmt

commit 0e51b9183c75ff59ace05b9f444e550dbd1d4980

[dualtor][active-active] Fix `test_tor_bgp_failure` (#6369)
Approach
What is the motivation for this PR?
Correct the expected standby health to unhealthy.

Signed-off-by: Longxiang Lyu lolv@microsoft.com

How did you do it?

tests/dualtor_io/test_tor_bgp_failure.py
@@ -171,6 +171,7 @@ def test_active_tor_shutdown_bgp_sessions_upstream(
     verify_tor_states(
         expected_active_host=lower_tor_host,
         expected_standby_host=upper_tor_host,
+        expected_standby_health="unhealthy",
         cable_type=cable_type
     )
 
@@ -202,6 +203,7 @@ def test_active_tor_shutdown_bgp_sessions_downstream(
         verify_tor_states(
             expected_active_host=lower_tor_host,
             expected_standby_host=upper_tor_host,
+            expected_standby_health="unhealthy",
             cable_type=cable_type
         )
 

commit 514490bb17b0313a0fc26b128270f7d127f92056

[dualtor_io] Fix `test_active_tor_shutdown_bgp_sessions_upstream` (#6843)
Description of PR
Summary:
Fixes # (issue)

Fix regression introduced by #6369.

For active-standby interfaces, shutdown bgp on active side, it will turn in to standby, healthy.

Downstream test is currently disabled for active-standby so I didn't modify it.

sign-off: Jing Zhang zhangjing@microsoft.com

tests/dualtor_io/test_tor_bgp_failure.py
@@ -168,13 +168,20 @@ def test_active_tor_shutdown_bgp_sessions_upstream(
             action=lambda: shutdown_bgp_sessions(upper_tor_host)
         )
 
-    verify_tor_states(
-        expected_active_host=lower_tor_host,
-        expected_standby_host=upper_tor_host,
-        expected_standby_health="unhealthy",
-        cable_type=cable_type
-    )
+    if cable_type == CableType.active_active:
+        verify_tor_states(
+            expected_active_host=lower_tor_host,
+            expected_standby_host=upper_tor_host,
+            expected_standby_health="unhealthy",
+            cable_type=cable_type
+        )
 
+    if cable_type == CableType.active_standby:
+        verify_tor_states(
+            expected_active_host=lower_tor_host,
+            expected_standby_host=upper_tor_host,
+            cable_type=cable_type
+        )
 
 @pytest.mark.enable_active_active
 @pytest.mark.skip_active_standby