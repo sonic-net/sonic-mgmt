6c7ac4d198df38900122ebb5aed608226222f329
Chun'ang Li <39114813+lerry-lee@users.noreply.github.com>
2022-11-23 10:21:31 +0800

    Add support if use access token when creating test plans and downloading artifacts (#6859)
    What is the motivation for this PR?
    Add support if use access token when creating test plans and downloading artifacts.
    
    How did you do it?
    Add optional parameters access_token when creating test plans and downloading artifacts.
    
    How did you verify/test it?
    Use token manually and write test case to download artifacts, both passed.
    
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- tests/scripts/getbuild.py
+++ tests/scripts/getbuild.py
@@ -4,7 +4,8 @@ import json
 import time
 import sys
 import argparse
-from urllib.request import urlopen, urlretrieve
+from urllib.request import urlopen, urlretrieve, Request, build_opener, install_opener
+import base64
 
 _start_time = None
 _last_time = None
@@ -35,11 +36,12 @@ def reporthook(count, block_size, total_size):
         percent = int(count * block_size * 100 / total_size)
         time_left = (total_size - progress_size) / speed / 1024
         sys.stdout.write("\r...%d%%, %d(%d) MB, %d KB/s, %d seconds left..." %
-                     (percent, progress_size / (1024 * 1024), total_size / (1024 * 1024), speed, time_left))
+                         (percent, progress_size / (1024 * 1024), total_size / (1024 * 1024), speed, time_left))
     else:
         sys.stdout.write("\r...%d MB, %d KB/s, ..." %
-                     (progress_size / (1024 * 1024), speed))
+                         (progress_size / (1024 * 1024), speed))
     sys.stdout.flush()
+
 
 def validate_url_or_abort(url):
     # Attempt to retrieve HTTP response code
@@ -59,12 +61,19 @@ def validate_url_or_abort(url):
             print("Image file not found on remote machine. Aborting...")
             sys.exit(1)
 
-def get_download_url(buildid, artifact_name):
+
+def get_download_url(buildid, artifact_name, url_prefix, access_token):
     """get download url"""
 
-    artifact_url = "https://dev.azure.com/mssonic/build/_apis/build/builds/{}/artifacts?artifactName={}&api-version=5.0".format(buildid, artifact_name)
+    artifact_req = Request("https://dev.azure.com/{}/_apis/build/builds/{}/artifacts?artifactName={}&api-version=5.0"
+                           .format(url_prefix, buildid, artifact_name))
 
-    resp = urlopen(artifact_url)
+    # If access token is not empty, set headers
+    if access_token:
+        artifact_req.add_header('Authorization',
+                                'Basic {}'.format(base64.b64encode(access_token.encode('utf-8')).decode('utf-8')))
+
+    resp = urlopen(artifact_req)
 
     j = json.loads(resp.read().decode('utf-8'))
 
@@ -74,7 +83,7 @@ def get_download_url(buildid, artifact_name):
     return (download_url, artifact_size)
 
 
-def download_artifacts(url, content_type, platform, buildid, num_asic):
+def download_artifacts(url, content_type, platform, buildid, num_asic, access_token):
     """find latest successful build id for a branch"""
 
     if content_type == 'image':
@@ -100,22 +109,32 @@ def download_artifacts(url, content_type, platform, buildid, num_asic):
             try:
                 print('Downloading {} from build {}...'.format(filename, buildid))
                 download_times += 1
+                # If access token is not empty, set headers
+                if access_token:
+                    opener = build_opener()
+                    opener.addheaders = [
+                        ('Authorization',
+                         'Basic {}'.format(base64.b64encode(access_token.encode('utf-8')).decode('utf-8')))]
+                    install_opener(opener)
                 urlretrieve(url, filename, reporthook)
                 print('\nDownload finished!')
                 break
             except Exception as e:
                 print("Download error", e)
                 if download_times < MAX_DOWNLOAD_TIMES:
-                    print('Download times: {}, sleep: {} seconds before retry.'.format(download_times, 30 * download_times))
+                    print('Download times: {}, sleep: {} seconds before retry.'.format(download_times,
+                                                                                       30 * download_times))
                     time.sleep(30 * download_times)
                     continue
                 else:
                     sys.exit(1)
 
-def find_latest_build_id(branch, success_flag = "succeeded"):
+
+def find_latest_build_id(branch, success_flag="succeeded"):
     """find latest successful build id for a branch"""
 
-    builds_url = "https://dev.azure.com/mssonic/build/_apis/build/builds?definitions=1&branchName=refs/heads/{}&resultFilter={}&statusFilter=completed&api-version=6.0".format(branch, success_flag)
+    builds_url = "https://dev.azure.com/mssonic/build/_apis/build/builds?definitions=1&branchName=refs/heads/{}" \
+                 "&resultFilter={}&statusFilter=completed&api-version=6.0".format(branch, success_flag)
 
     resp = urlopen(builds_url)
 
@@ -130,6 +149,7 @@ def find_latest_build_id(branch, success_flag = "succeeded"):
 
     return latest_build_id
 
+
 def main():
     global artifact_size
 
@@ -137,14 +157,16 @@ def main():
     parser.add_argument('--buildid', metavar='buildid', type=int, help='build id')
     parser.add_argument('--branch', metavar='branch', type=str, help='branch name')
     parser.add_argument('--platform', metavar='platform', type=str,
-            choices=['broadcom', 'mellanox', 'vs'],
-            help='platform to download')
+                        choices=['broadcom', 'mellanox', 'vs'],
+                        help='platform to download')
     parser.add_argument('--content', metavar='content', type=str,
-            choices=['all', 'image'], default='image',
-            help='download content type [all|image(default)]')
+                        choices=['all', 'image'], default='image',
+                        help='download content type [all|image(default)]')
     parser.add_argument('--num_asic', metavar='num_asic', type=int,
-            default=1,
-            help='Specifiy number of asics')
+                        default=1,
+                        help='Specifiy number of asics')
+    parser.add_argument('--url_prefix', metavar='url_prefix', type=str, default='mssonic/build', help='url prefix')
+    parser.add_argument('--access_token', metavar='access_token', type=str, default='', help='access token')
 
     args = parser.parse_args()
 
@@ -160,9 +182,12 @@ def main():
 
     artifact_name = "sonic-buildimage.{}".format(args.platform)
 
-    (dl_url, artifact_size) = get_download_url(buildid, artifact_name)
+    (dl_url, artifact_size) = get_download_url(buildid, artifact_name,
+                                               url_prefix=args.url_prefix,
+                                               access_token=args.access_token)
 
-    download_artifacts(dl_url, args.content, args.platform, buildid, args.num_asic)
+    download_artifacts(dl_url, args.content, args.platform, buildid, args.num_asic, access_token=args.access_token)
+
 
 if __name__ == '__main__':
     main()
