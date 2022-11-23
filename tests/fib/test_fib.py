e3ca5ea5c89f762c059a33a40f1a690e872bfac1
Perumal Venkatesh <vperumal@gmail.com>
2022-11-23 14:00:04 -0800

    removing ip-proto from hash_keys for Cisco-8000 Distributed Chassis (#6791)
    In case of ip-proto hashing key in test_fib.py::test_hash, we have observed that the protocol by itself doesn’t contain enough entropy for proper distribution using the hashing logic. So for Cisco-8000 Distributed Chassis with T2 profile, we are skipping ip-proto hashing key.

--- tests/fib/test_fib.py
+++ tests/fib/test_fib.py
@@ -175,6 +175,10 @@ def hash_keys(duthost):
     if duthost.facts['asic_type'] in ["innovium"]:
         if 'ip-proto' in hash_keys:
             hash_keys.remove('ip-proto')
+    # removing ip-proto from hash_keys for Cisco-8000 Distributed Chassis
+    if duthost.facts['platform'] in ['x86_64-8800_rp_o-r0', 'x86_64-88_lc0_36fh_mo-r0', 'x86_64-8800_lc_48h_o-r0']:
+        if 'ip-proto' in hash_keys:
+            hash_keys.remove('ip-proto')
     # remove the ingress port from multi asic platform
     # In multi asic platform each asic has different hash seed,
     # the same packet coming in different asic
