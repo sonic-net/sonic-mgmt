6413afbeb1686ab9a076237f6e75610ad0b4d10a
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2022-11-25 12:31:28 +0800

    Enhance test_orchagent_standby_tor_downstream_enhance (#6870)
    What is the motivation for this PR?
    Currently, test_orchagent_standby_tor_downstream_enhance is running for more than 6 hours.
    Need to reduce running time.
    
    How did you do it?
    Each case in test_orchagent_standby_tor_downstream_enhance will call ptf script ansible/roles/test/files/ptftests/py3/ip_in_ip_tunnel_test.py, so far it will cost more than 6 mins to complete.
    That's because it verifies 100 negative packets and 10000 correct packets.
    
    Introduce completeness level, except Friday run, nightly uses confident level and it will run basic test in ip_in_ip_tunnel_test.py. It will verify 10 negative packets and 10 correct packets, if receive packet on every selected portchannel, test case will pass.
    For thorough level, verify 100 negative packets and then verify 100 correct packets every time, if fail to balance, will send 100 more packets and verify accumulated packets if balanced again.
    After this change, we can save around 20 mins for each case, then in total, we can save more than 4 hours for test_orchagent_standby_tor_downstream_enhance.
    
    How did you verify/test it?
    Run test_orchagent_standby_tor_downstream_enhance
    
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

--- tests/common/dualtor/dual_tor_utils.py
+++ tests/common/dualtor/dual_tor_utils.py
@@ -722,7 +722,8 @@ def mux_cable_server_ip(dut):
     return json.loads(mux_cable_config)
 
 
-def check_tunnel_balance(ptfhost, standby_tor_mac, vlan_mac, active_tor_ip, standby_tor_ip, selected_port, target_server_ip, target_server_ipv6, target_server_port, ptf_portchannel_indices, check_ipv6=False):
+def check_tunnel_balance(ptfhost, standby_tor_mac, vlan_mac, active_tor_ip, standby_tor_ip, selected_port, target_server_ip,
+                        target_server_ipv6, target_server_port, ptf_portchannel_indices, completeness_level, check_ipv6=False):
     """
     Function for testing traffic distribution among all avtive T1.
     A test script will be running on ptf to generate traffic to standby interface, and the traffic will be forwarded to
@@ -740,6 +741,7 @@ def check_tunnel_balance(ptfhost, standby_tor_mac, vlan_mac, active_tor_ip, stan
     Returns:
         None.
     """
+
     HASH_KEYS = ["src-port", "dst-port", "src-ip"]
     params = {
         "server_ip": target_server_ip,
@@ -749,7 +751,8 @@ def check_tunnel_balance(ptfhost, standby_tor_mac, vlan_mac, active_tor_ip, stan
         "active_tor_ip": active_tor_ip,
         "standby_tor_ip": standby_tor_ip,
         "ptf_portchannel_indices": ptf_portchannel_indices,
-        "hash_key_list": HASH_KEYS
+        "hash_key_list": HASH_KEYS,
+        "completeness_level": completeness_level
     }
     if check_ipv6:
         params["server_ip"] = target_server_ipv6
@@ -1059,7 +1062,7 @@ def get_crm_nexthop_counter(host):
     return crm_facts['resources']['ipv4_nexthop']['used']
 
 
-def dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo):
+def dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, get_function_completeness_level=None):
     """
     @summary: A helper function for collecting info of dualtor testbed.
     @param ptfhost: The ptf host fixture
@@ -1101,6 +1104,9 @@ def dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo):
     res['target_server_ip'] = servers[random_server_iface]['server_ipv4'].split('/')[0]
     res['target_server_ipv6'] = servers[random_server_iface]['server_ipv6'].split('/')[0]
     res['target_server_port'] = standby_tor_mg_facts['minigraph_ptf_indices'][random_server_iface]
+
+    normalize_level = get_function_completeness_level if get_function_completeness_level else 'thorough'
+    res['completeness_level'] = normalize_level
 
     logger.debug("dualtor info is generated {}".format(res))
     return res
