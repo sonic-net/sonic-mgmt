be509ddc8d2d30c1cadfad0186dd7b023e572b38
siqbal1986 <shahzad.iqbal@microsoft.com>
2022-11-29 17:38:00 -0800

    stress test to replicate production scenario (#6887)
    * added a test to replicate scenario in pilot testing.
    
    This test runs the following sequence to stress the restapi behavior.
    add 2 routes A,B
    loop 10 times
        verify A,B by reading routes.
        add 3 more routes C,D,E
        verify 5 routes by reading routes A, B, C, D, E 10 times.
        delete the 3 added routes. C, D, E.
    delete the last 2 routes A, B
    verify all routes deleted.

--- tests/restapi/conftest.py
+++ tests/restapi/conftest.py
@@ -120,13 +120,14 @@ def construct_url(duthosts, rand_one_dut_hostname):
         return endpoint
     return get_endpoint
 
+
 @pytest.fixture
 def vlan_members(duthosts, rand_one_dut_hostname, tbinfo):
     duthost = duthosts[rand_one_dut_hostname]
     VLAN_INDEX = 0
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
-    vlan_interfaces = mg_facts["minigraph_vlans"].values()[VLAN_INDEX]["members"]
-    if vlan_interfaces is not None:
-        return vlan_interfaces
-    else:
-        return []
+    if mg_facts["minigraph_vlans"] != {}:
+        vlan_interfaces = mg_facts["minigraph_vlans"].values()[VLAN_INDEX]["members"]
+        if vlan_interfaces is not None:
+            return vlan_interfaces
+    return []
