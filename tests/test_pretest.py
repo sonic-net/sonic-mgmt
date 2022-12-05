7dcc9b4a551d28ffeeb69fde575d22dea1b167cf
yaqiangz <zyq1512099831@gmail.com>
2022-12-05 03:06:16 +0000

    [dhcp_server] Skip dhcp_relay check in pretest if dhcp_server is enabled

--- tests/test_pretest.py
+++ tests/test_pretest.py
@@ -157,8 +157,13 @@ def test_disable_rsyslog_rate_limit(duthosts, enum_dut_hostname):
         # We don't want to fail here because it's an util
         logging.warn("Failed to retrieve feature status")
         return
+    config_facts = duthost.config_facts(host=duthost.hostname, source="running")
+    is_dhcp_server_enable = config_facts["ansible_facts"]["DEVICE_METADATA"]["localhost"]["dhcp_server"]
     for feature_name, state in features_dict.items():
         if 'enabled' not in state:
+            continue
+        # Skip dhcp_relay check if dhcp_server is enabled
+        if "enabled" in is_dhcp_server_enable and feature_name == "dhcp_relay":
             continue
         duthost.modify_syslog_rate_limit(feature_name, rl_option='disable')
 
