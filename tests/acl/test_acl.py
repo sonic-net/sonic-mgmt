ceb287f06d02048b39657bc4b550b8d0528e8bb1
Yaqiang Zhu <zyq1512099831@gmail.com>
2022-12-06 22:55:08 +0800

    [mx] Fix test_acl failed on mx topo (#6971)
    What is the motivation for this PR?
    Some of test_acl cases failed on mx topo because the mx topology has no port-channel. This PR is to fix that.
    
    How did you do it?
    Modify the method of obtaining upstream neighbors in mx topology
    
    How did you verify/test it?
    Run test_acl on mx topo

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -287,7 +287,7 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, ptf
             if namespace:
                 acl_table_ports[''] += port
 
-    if topo in ["t0", "m0", "mx"] or tbinfo["topo"]["name"] in ("t1-lag", "t1-64-lag", "t1-64-lag-clet"):
+    if topo in ["t0", "m0"] or tbinfo["topo"]["name"] in ("t1-lag", "t1-64-lag", "t1-64-lag-clet"):
         for k, v in port_channels.iteritems():
             acl_table_ports[v['namespace']].append(k)
             # In multi-asic we need config both in host and namespace.
