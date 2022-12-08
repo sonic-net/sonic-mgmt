cda8e549ef7244d1bd73ebd9d5240dd3cb2d2987
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2022-12-08 14:51:55 -0800

    is_intf_status_down() to check all ports together

--- tests/common/devices/eos.py
+++ tests/common/devices/eos.py
@@ -100,11 +100,37 @@ class EosHost(AnsibleHostBase):
             commands=['show interface %s' % interface_name])
         return 'Up' in show_int_result['stdout_lines'][0]
 
-    def is_intf_status_down(self, interface_name):
-        show_int_result = self.eos_command(commands=['show interface %s' % interface_name])
-        logging.info("Checking interface state: {}".format(show_int_result['stdout_lines'][0][0]))
-        # Either admin/opr status is down meaning link is down
-        return 'down' in show_int_result['stdout_lines'][0][0].lower()
+
+    def is_intf_status_down(self, ports):
+        """
+            Args: 
+                ports(set): all ports that connected to current fanout(self)
+            Return:
+                True: if all ports are down
+                False: if any port is up
+        """
+        show_int_result = self.eos_command(commands=['show interface status'])
+        for i in range(len(show_int_result['stdout_lines'][0])):
+            """ 
+            (Pdb) output_line
+            u'Et33/1     str2-7804-lc6-1-Ethernet0            notconnect   1134     full   100G   100GBASE-CR4    
+            (Pdb) output_line.split(' ')[0]
+            u'Et1/1'
+            """
+            output_line = show_int_result['stdout_lines'][0][i]
+            output_port = output_line.split(' ')[0].replace('Et','Ethernet')
+            # Only care about port that connect to current DUT
+            if output_port in ports:
+                # Note that here we have to check for 'notconnect',
+                # as 'connected' will be in both 'notconnect' and 'connected'
+                if 'notconnect' in output_line:
+                    logging.info("Interface %s is down" % (output_port))
+                    continue
+                else:
+                    logging.info("Interface %s is up" % (output_port))
+                    return False
+        return True
+
 
     def set_interface_lacp_rate_mode(self, interface_name, mode):
         out = self.eos_config(
