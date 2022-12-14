03f11080ccb30ae9b156ab3740523f10adc1166f
Jibin Bao <jbao@nvidia.com>
2022-12-14 11:40:49 +0800

    Fix pre-commit check issue

--- tests/qos/buffer_helpers.py
+++ tests/qos/buffer_helpers.py
@@ -1,12 +1,13 @@
 import ast
 
+
 class DutDbInfo:
     def __init__(self, duthost):
         self.duthost = duthost
         self.update_db_info()
 
     def get_asic_db(self):
-         return ast.literal_eval(self.duthost.shell('sonic-db-dump -n ASIC_DB -y')['stdout'])
+        return ast.literal_eval(self.duthost.shell('sonic-db-dump -n ASIC_DB -y')['stdout'])
 
     def get_appl_db(self):
         return ast.literal_eval(self.duthost.shell('sonic-db-dump -n APPL_DB -y')['stdout'])
@@ -30,7 +31,7 @@ class DutDbInfo:
         for key in self.asic_db.keys():
             if buffer_profile_oid in key:
                 return key
-        return exception("Not find the profile key for {}".format(buffer_profile_oid))
+        raise Exception("Not find the profile key for {}".format(buffer_profile_oid))
 
     def get_buffer_profile_info_from_asic_db(self, buffer_profile_key):
         return self.asic_db.get(buffer_profile_key).get("value")
@@ -39,5 +40,3 @@ class DutDbInfo:
         self.config_db = self.get_config_db()
         self.appl_db = self.get_appl_db()
         self.asic_db = self.get_asic_db()
-
-
