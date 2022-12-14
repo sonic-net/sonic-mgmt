7a03e8593f262319f92372e18133202bf7e09ff2
xwjiang2021 <xwjiang2021@github.com>
2022-12-14 04:30:58 +0000

    Fix test toggle mux overtime issue
    What is the motivation for this PR?
    Linke prober interval could be calculated by multiply of max backoff
    factor, retry count and heartbeat timeout interval.
    The default heartbeat timeout interval should be 100ms, but in fixture
    run_icmp_responder, it will be changed to 1000ms.
    For testscript test_toggle_mux.py, it doesn't use run_icmp_responder,
    and the heartbeat timeout interval may be changed by other tests, which
    could cause link prober interval too hight and case fail for timeout.
    
    How did you do it?
    Check if heartbeat timeout interval were changed, if not, follow
    previous routine, if changed, change to default value and change back
    after test.
    
    How did you verify/test it?
    Run testcase
    
    Any platform specific information?
    Supported testbed topology if it's a new test case?

--- tests/dualtor/test_toggle_mux.py
+++ tests/dualtor/test_toggle_mux.py
@@ -1,5 +1,5 @@
 import logging
-
+import json
 import pytest
 
 from tests.common.dualtor.constants import UPPER_TOR, LOWER_TOR
@@ -12,6 +12,8 @@ pytestmark = [
 ]
 
 logger = logging.getLogger(__name__)
+
+DEFAUL_INTERVAL_V4 = 100
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -27,24 +29,52 @@ def restore_mux_auto_mode(duthosts):
     duthosts.shell('config muxcable mode auto all')
 
 
+@pytest.fixture(scope="module")
+def get_interval_v4(duthosts):
+    mux_linkmgr_output = duthosts.shell('sonic-cfggen -d --var-json MUX_LINKMGR')
+    mux_linkmgr = list(mux_linkmgr_output.values())[0]['stdout']
+    if len(mux_linkmgr) != 0:
+        Cur_interval_v4 = json.loads(mux_linkmgr)['LINK_PROBER']['interval_v4']
+        return Cur_interval_v4
+    else:
+        return None
+
+
 @pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_simulator(duthosts, active_side, toggle_all_simulator_ports, get_mux_status, restore_mux_auto_mode):
+def test_toggle_mux_from_simulator(duthosts, active_side, toggle_all_simulator_ports,
+                                   get_mux_status, get_interval_v4, restore_mux_auto_mode):
     logger.info('Set all muxcable to manual mode on all ToRs')
     duthosts.shell('config muxcable mode manual all')
+
+    Cur_interval_v4 = get_interval_v4
+    if Cur_interval_v4 is not None:
+        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
+                       .format(DEFAUL_INTERVAL_V4))
+        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
 
     logger.info('Toggle mux active side from mux simulator')
     toggle_all_simulator_ports(active_side)
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
 
+    if Cur_interval_v4 is not None:
+        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
+                       .format(Cur_interval_v4))
+        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
+
     validate_check_result(check_result, duthosts, get_mux_status)
 
 
 @pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status, restore_mux_auto_mode):
-
+def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status, get_interval_v4, restore_mux_auto_mode):
     logger.info('Reset muxcable mode to auto for all ports on all DUTs')
     duthosts.shell('config muxcable mode auto all')
+
+    Cur_interval_v4 = get_interval_v4
+    if Cur_interval_v4 is not None:
+        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
+                       .format(DEFAUL_INTERVAL_V4))
+        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
 
     # Use cli to toggle muxcable active side
     if active_side == UPPER_TOR:
@@ -54,5 +84,10 @@ def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status, restore_mux_
     mux_active_dut.shell('config muxcable mode active all')
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
+
+    if Cur_interval_v4 is not None:
+        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
+                       .format(Cur_interval_v4))
+        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
 
     validate_check_result(check_result, duthosts, get_mux_status)
