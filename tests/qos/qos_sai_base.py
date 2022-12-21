4db2c1443a66cac109a3e1ee76fe31c5cfbb5289
Xu Chen <112069142+XuChen-MSFT@users.noreply.github.com>
2022-12-21 10:32:38 +0800

    correct test traffic's DMAC for dualtor qos test scenario (#7053)
    * correct test traffic's DMAC for dualtor qos test scenario
    
    * add arp proxy config info ouput for debug purpose

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -57,7 +57,7 @@ class QosBase:
         return self.buffer_model
 
     @pytest.fixture(scope='class', autouse=True)
-    def dutTestParams(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, dut_test_params):
+    def dutTestParams(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, dut_test_params, tbinfo):
         """
             Prepares DUT host test params
             Returns:
@@ -66,6 +66,19 @@ class QosBase:
         # update router mac
         if dut_test_params["topo"] in self.SUPPORTED_T0_TOPOS:
             dut_test_params["basicParams"]["router_mac"] = ''
+
+        # For dualtor qos test scenario, DMAC of test traffic is default vlan interface's MAC address.
+        # To reduce duplicated code, put "is_dualtor" and "def_vlan_mac" into dutTestParams['basicParams'].
+        if "dualtor" in tbinfo["topo"]["name"]:
+            dut_test_params["basicParams"]["is_dualtor"] = True
+            vlan_cfgs = tbinfo['topo']['properties']['topology']['DUT']['vlan_configs']
+            if vlan_cfgs and 'default_vlan_config' in vlan_cfgs:
+                default_vlan_name = vlan_cfgs['default_vlan_config']
+                if default_vlan_name:
+                    for vlan in vlan_cfgs[default_vlan_name].values():
+                        if 'mac' in vlan and vlan['mac']:
+                            dut_test_params["basicParams"]["def_vlan_mac"] = vlan['mac']
+                            break
 
         yield dut_test_params
 
@@ -895,6 +908,19 @@ class QosSaiBase(QosBase):
             duthost.command("docker exec syncd python /packets_aging.py enable")
             duthost.command("docker exec syncd rm -rf /packets_aging.py")
 
+    def dutArpProxyConfig(self, duthost):
+        # so far, only record ARP proxy config to logging for debug purpose
+        vlanInterface = {}
+        try:
+            vlanInterface = json.loads(duthost.shell('sonic-cfggen -d --var-json "VLAN_INTERFACE"')['stdout'])
+        except:
+            logger.info('Failed to read vlan interface config')
+        if not vlanInterface:
+            return
+        for key, value in vlanInterface.items():
+            if 'proxy_arp' in value:
+                logger.info('ARP proxy is {} on {}'.format(value['proxy_arp'], key))
+
     def dutBufferConfig(self, duthost):
         bufferConfig = {}
         try:
@@ -965,6 +991,8 @@ class QosSaiBase(QosBase):
         qosConfigs = dutConfig["qosConfigs"]
         dutAsic = dutConfig["dutAsic"]
         dutTopo = dutConfig["dutTopo"]
+
+        self.dutArpProxyConfig(duthost)
 
         if isMellanoxDevice(duthost):
             current_file_dir = os.path.dirname(os.path.realpath(__file__))
--- tests/saitests/py3/sai_qos_tests.py
+++ tests/saitests/py3/sai_qos_tests.py
@@ -953,10 +953,9 @@ class PFCtest(sai_base_test.ThriftInterfaceDataPlane):
         else:
             cell_occupancy = 1
 
-        is_dualtor = bool(self.test_params.get('is_dualtor', False))
-        is_t0 = bool(self.test_params.get('is_t0', False))
+        is_dualtor = self.test_params.get('is_dualtor', False)
         def_vlan_mac = self.test_params.get('def_vlan_mac', None)
-        if is_dualtor and is_t0 and bool(def_vlan_mac):
+        if is_dualtor and def_vlan_mac != None:
             pkt_dst_mac = def_vlan_mac
 
         pkt = construct_ip_pkt(packet_length,
@@ -1587,6 +1586,16 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
         else:
             cell_occupancy = 1
 
+        pkt_dst_mac2 = router_mac if router_mac != '' else dst_port_2_mac
+        pkt_dst_mac3 = router_mac if router_mac != '' else dst_port_3_mac
+
+        is_dualtor = self.test_params.get('is_dualtor', False)
+        def_vlan_mac = self.test_params.get('def_vlan_mac', None)
+        if is_dualtor and def_vlan_mac != None:
+            pkt_dst_mac = def_vlan_mac
+            pkt_dst_mac2 = def_vlan_mac
+            pkt_dst_mac3 = def_vlan_mac
+
         pkt = construct_ip_pkt(packet_length,
                                pkt_dst_mac,
                                src_port_mac,
@@ -1601,7 +1610,6 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
         )
 
         # create packet
-        pkt_dst_mac2 = router_mac if router_mac != '' else dst_port_2_mac
         pkt2 = construct_ip_pkt(packet_length,
                                 pkt_dst_mac2,
                                 src_port_mac,
@@ -1616,7 +1624,6 @@ class PFCXonTest(sai_base_test.ThriftInterfaceDataPlane):
         )
 
         # create packet
-        pkt_dst_mac3 = router_mac if router_mac != '' else dst_port_3_mac
         pkt3 = construct_ip_pkt(packet_length,
                                 pkt_dst_mac3,
                                 src_port_mac,
