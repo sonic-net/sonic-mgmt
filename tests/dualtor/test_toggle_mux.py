40a970c92535e6cdba719398f7a4bb6f0e1f444b
xwjiang-ms <xiaweijiang@microsoft.com>
2022-12-22 01:18:28 +0000

    Add a update linkprobe interval function in dual tor utils

--- tests/dualtor/test_toggle_mux.py
+++ tests/dualtor/test_toggle_mux.py
@@ -4,6 +4,7 @@ import pytest
 
 from tests.common.dualtor.constants import UPPER_TOR, LOWER_TOR
 from tests.common.dualtor.mux_simulator_control import check_mux_status, validate_check_result
+from tests.common.dualtor.dual_tor_utils import update_linkmgrd_probe_interval
 from tests.common.utilities import wait_until
 
 
@@ -34,47 +35,41 @@ def get_interval_v4(duthosts):
     mux_linkmgr_output = duthosts.shell('sonic-cfggen -d --var-json MUX_LINKMGR')
     mux_linkmgr = list(mux_linkmgr_output.values())[0]['stdout']
     if len(mux_linkmgr) != 0:
-        Cur_interval_v4 = json.loads(mux_linkmgr)['LINK_PROBER']['interval_v4']
-        return Cur_interval_v4
+        cur_interval_v4 = json.loads(mux_linkmgr)['LINK_PROBER']['interval_v4']
+        return cur_interval_v4
     else:
         return None
 
 
 @pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_simulator(duthosts, active_side, toggle_all_simulator_ports,
+def test_toggle_mux_from_simulator(duthosts, tbinfo, active_side, toggle_all_simulator_ports,
                                    get_mux_status, get_interval_v4, restore_mux_auto_mode):
     logger.info('Set all muxcable to manual mode on all ToRs')
     duthosts.shell('config muxcable mode manual all')
 
-    Cur_interval_v4 = get_interval_v4
-    if Cur_interval_v4 is not None:
-        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
-                       .format(DEFAUL_INTERVAL_V4))
-        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
+    cur_interval_v4 = get_interval_v4
+    if cur_interval_v4 is not None:
+        update_linkmgrd_probe_interval(duthosts, tbinfo, DEFAUL_INTERVAL_V4)
 
     logger.info('Toggle mux active side from mux simulator')
     toggle_all_simulator_ports(active_side)
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
 
-    if Cur_interval_v4 is not None:
-        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
-                       .format(Cur_interval_v4))
-        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
+    if cur_interval_v4 is not None:
+        update_linkmgrd_probe_interval(duthosts, tbinfo, cur_interval_v4)
 
     validate_check_result(check_result, duthosts, get_mux_status)
 
 
 @pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status, get_interval_v4, restore_mux_auto_mode):
+def test_toggle_mux_from_cli(duthosts, tbinfo, active_side, get_mux_status, get_interval_v4, restore_mux_auto_mode):
     logger.info('Reset muxcable mode to auto for all ports on all DUTs')
     duthosts.shell('config muxcable mode auto all')
 
-    Cur_interval_v4 = get_interval_v4
-    if Cur_interval_v4 is not None:
-        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
-                       .format(DEFAUL_INTERVAL_V4))
-        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
+    cur_interval_v4 = get_interval_v4
+    if cur_interval_v4 is not None:
+        update_linkmgrd_probe_interval(duthosts, tbinfo, DEFAUL_INTERVAL_V4)
 
     # Use cli to toggle muxcable active side
     if active_side == UPPER_TOR:
@@ -85,9 +80,7 @@ def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status, get_interval
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
 
-    if Cur_interval_v4 is not None:
-        duthosts.shell('sonic-db-cli CONFIG_DB HSET "MUX_LINKMGR|LINK_PROBER" "interval_v4" "{}"'
-                       .format(Cur_interval_v4))
-        duthosts.shell('docker exec -i mux supervisorctl restart linkmgrd')
+    if cur_interval_v4 is not None:
+        update_linkmgrd_probe_interval(duthosts, tbinfo, cur_interval_v4)
 
     validate_check_result(check_result, duthosts, get_mux_status)
