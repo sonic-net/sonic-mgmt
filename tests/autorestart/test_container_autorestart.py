cd2d41ead310810171b3c94cb39233b9558ca444
yaqiangz <zyq1512099831@gmail.com>
2023-01-06 10:02:42 +0000

    [pikez] Add syslog error ignore for pikez

--- tests/autorestart/test_container_autorestart.py
+++ tests/autorestart/test_container_autorestart.py
@@ -118,7 +118,15 @@ def ignore_expected_loganalyzer_exception(duthosts, enum_rand_one_per_hwsku_host
             ".*ERR snmp#snmpd.*",
             ".*ERR dhcp_relay#dhcp6?relay.*bind: Failed to bind socket to link local ipv6 address on interface .* "
             "after [0-9]+ retries",
-        ]
+            ".*ERR gbsyncd#syncd: :- updateNotificationsPointers: pointer for SAI_SWITCH_ATTR_REGISTER_READ is not "
+            "handled.*",
+            ".*ERR gbsyncd#syncd: :- updateNotificationsPointers: pointer for SAI_SWITCH_ATTR_REGISTER_WRITE is not "
+            "handled.*",
+            ".*ERR gbsyncd#syncd: :- diagShellThreadProc: Failed to enable switch shell: SAI_STATUS_NOT_SUPPORTED.*",
+            ".*ERR swss#orchagent: :- updateNotifications: pointer for SAI_SWITCH_ATTR_REGISTER_WRITE is not handled.*",
+            ".*ERR swss#orchagent: :- updateNotifications: pointer for SAI_SWITCH_ATTR_REGISTER_READ is not handled.*",
+            ".*WARNING kernel: \[.*\] Modules linked in: linux_ngbde\(OE\) linux_knet_cb\(OE-\) linux_bcm_knet\(OE\).*"
+    ]
     ignore_regex_dict = {
         'common': [
             ".*ERR monit.*",
