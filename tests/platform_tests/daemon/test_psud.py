2cc6f66e3dfadd54ab6f067c731c1cda16927f5e
sanmalho <sandeep.malhotra@nokia.com>
2023-01-20 14:14:57 -0500

    Fix for PID assertion issue - PSUD and CHASSID platform cases
    Fix for PID assertion - Typo Error
    
    Fixing up other chassisd and psud tests to poll for deamon to restart with bigger pid than before

--- tests/platform_tests/daemon/test_psud.py
+++ tests/platform_tests/daemon/test_psud.py
@@ -68,9 +68,12 @@ def check_daemon_status(duthosts, enum_supervisor_dut_hostname):
         duthost.start_pmon_daemon(daemon_name)
         time.sleep(10)
 
+def check_if_daemon_restarted(duthost, daemon_name, pre_daemon_pid):
+    daemon_status, daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
+    return (daemon_pid > pre_daemon_pid)
 
 def check_expected_daemon_status(duthost, expected_daemon_status):
-    daemon_status, _ = duthost.get_pmon_daemon_status(daemon_name)
+    daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
     return daemon_status == expected_daemon_status
 
 
@@ -173,8 +176,8 @@ def test_pmon_psud_stop_and_start_status(check_daemon_status, duthosts,
 
     duthost.start_pmon_daemon(daemon_name)
 
-    wait_until(50, 10, 0, check_expected_daemon_status,
-               duthost, expected_running_status)
+    wait_until(120, 10, 0, check_if_daemon_restarted, duthost, daemon_name, pre_daemon_pid)
+    wait_until(50, 10, 0, check_expected_daemon_status, duthost, expected_running_status)
 
     post_daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
     pytest_assert(post_daemon_status == expected_running_status,
@@ -204,8 +207,8 @@ def test_pmon_psud_term_and_start_status(check_daemon_status, duthosts,
 
     duthost.stop_pmon_daemon(daemon_name, SIG_TERM, pre_daemon_pid)
 
-    wait_until(50, 10, 5, check_expected_daemon_status,
-               duthost, expected_running_status)
+    wait_until(120, 10, 0, check_if_daemon_restarted, duthost, daemon_name, pre_daemon_pid)
+    wait_until(50, 10, 5, check_expected_daemon_status, duthost, expected_running_status)
 
     post_daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
     pytest_assert(post_daemon_status == expected_running_status,
@@ -234,8 +237,8 @@ def test_pmon_psud_kill_and_start_status(check_daemon_status, duthosts,
 
     duthost.stop_pmon_daemon(daemon_name, SIG_KILL, pre_daemon_pid)
 
-    wait_until(120, 10, 0, check_expected_daemon_status,
-               duthost, expected_running_status)
+    wait_until(120, 10, 0, check_if_daemon_restarted, duthost, daemon_name, pre_daemon_pid)
+    wait_until(120, 10, 0, check_expected_daemon_status, duthost, expected_running_status)
 
     post_daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
     pytest_assert(post_daemon_status == expected_running_status,
