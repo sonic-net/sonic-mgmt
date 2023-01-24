350a1ef3164f78414be1fb3d5e0eaa6bf1d3f1cb
SuvarnaMeenakshi <50386592+SuvarnaMeenakshi@users.noreply.github.com>
2023-01-24 08:47:22 -0800

    Fix generate_dut_backend_asics when multiple DUTs are selected (#7321)
    Fix the Error seen at the test collection stage when running all tests on a testbed which consists of multiple frontend HwSKUs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1207,12 +1207,12 @@ def generate_dut_backend_asics(request, duts_selected):
     metadata = get_testbed_metadata(request)
 
     if metadata is None:
-        return [[None]]
+        return [[None]]*len(duts_selected)
 
     for dut in duts_selected:
         mdata = metadata.get(dut)
         if mdata is None:
-            continue
+            dut_asic_list.append([None]) 
         dut_asic_list.append(mdata.get("backend_asics", [None]))
 
     return dut_asic_list
