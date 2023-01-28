af37dfb6cade06952eb3eeb6a17327b190f7adb0
Liping Xu <xuliping@microsoft.com>
2023-01-28 07:10:30 +0000

    Merged PR 1650: [nightly-hawk] update trusty image for testbed auto-recovery
    1: use trusty image to replace golden image
    2: update trusty images for Zinc
    3: add new trusty Library in NIGHTLY_HAWK group

--- test_analyzer/test_testbeds_auto_recovery_cfg.py
+++ test_analyzer/test_testbeds_auto_recovery_cfg.py
@@ -9,59 +9,66 @@ skip_testbeds_list = [
 
 
 golden_image_url = {
-    'vms2-t1-7260-7'         : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms3-t1-7280'           : '$(GOLDEN_IMAGE_BRCM_DNX_ABOOT_PUBLIC)',        # Arista
-    'vms6-t0-7060'           : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Arista
-    'vms6-t1-7060'           : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Arista
-    'vms7-t0-7260-1'         : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms7-t0-7260-2'         : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms18-t0-7050qx-acs-02' : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Arista
-    'vms18-t1-7050qx-acs-03' : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Arista
-    'vms20-t0-7050cx3-1'     : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms20-t0-7050cx3-2'     : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms20-t1-7050cx3-3'     : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms21-dual-t0-7050-3'   : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms21-dual-t0-7260'     : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms24-dual-t0-7050-1'   : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms24-dual-t0-7050-2'   : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms24-t0-7260-2'        : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms24-t1-7050qx-acs-01' : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Arista
-    'vms26-t2-7800-1'        :  None,                                          # Arista
-    'vms28-dual-t0-7260'     : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vms3-t1-dx010-1'        : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Celestica
-    'vms7-t0-dx010-4'        : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Celestica
-    'vms7-t0-dx010-5'        : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Celestica
-    'vms12-3-t0-e1031'       : '$(GOLDEN_IMAGE_BRCM_202012_SLIM)',             # Celestica
-    'vms12-9-t0-e1031'       : '$(GOLDEN_IMAGE_BRCM_202012_SLIM)',             # Celestica
-    'vms20-t1-dx010-6'       : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Celestica
-    'vms13-t1-n3164-2'       : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Cisco   #fixme
-    'vms20-t1-n3164-acs-4'   : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Cisco   #fixme
-    'vmsvc1-t1-n3164-acs-1'  : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Cisco   #fixme
-    'vms7-t0-s6100'          : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms7-t0-s6100-4'        : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms7-t1-s6100'          : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms11-t0-on-4'          : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms13-5-t1-lag'         : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms21-t0-z9332f-02'     : '$(GOLDEN_IMAGE_BRCM_202012)',                  # Dell
-    'vms1-8'                 : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms1-t1-2700'           : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms2-2-t0-2700'         : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms2-4-t0-2700'         : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms7-t0-4600c-2'        : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms12-t0-3800'          : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms18-t1-msn4600c-acs-1': '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms20-t0-sn3800-2'      : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms21-t0-2700'          : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms21-t1-2700-2'        : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms24-t0-3800-azd'      : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms28-t0-4600c-03'      : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms28-t0-4600c-04'      : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms11-2-t0'             : '$(GOLDEN_IMAGE_MLNX_202012)',                  # Mellanox
-    'vms18-t0-7215-acs-1'    : '$(GOLDEN_IMAGE_MARVELL_202012)',               # Nokia
-    'vms21-t0-7215-acs-3'    : '$(GOLDEN_IMAGE_MARVELL_202012)',               # Nokia
-    'vms29-t2-7250-1'        : None,                                           # Nokia
-    'vms29-t2-7250-2'        : None,                                           # Nokia 
-    'vms20-t0-ixia-2'        : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012_SLIM)',       # Rdma
-    'vmsvc1-dual-t0-7050-1'  : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)',            # Arista
-    'vmsvc1-dual-t0-7050-2'  : '$(GOLDEN_IMAGE_BRCM_ABOOT_202012)'             # Arista
+  'testbed-bjw-can-ec9516-1' : '$(BJW_TRUSTY_IMAGE_BFN_INTERNAL)',
+  'testbed-bjw-can-720dt-1'  : '$(BJW_TRUSTY_IMAGE_BRCM_ABOOT_202205)',
+  'testbed-bjw-can-720dt-2'  : '$(BJW_TRUSTY_IMAGE_BRCM_ABOOT_202205)',
+  'testbed-bjw-can-7215-1'   : '$(BJW_TRUSTY_IMAGE_MARVELL_202205)',
+  'testbed-bjw-can-2700-1'   : '$(BJW_TRUSTY_IMAGE_MLNX_202012)',
+  'vms20-t1-n3164-acs-4'     : '$(TRUSTY_IMAGE_BRCM_201911)',
+  'vmsvc1-t1-n3164-acs-1'    : '$(TRUSTY_IMAGE_BRCM_201911)',
+  'vms11-t0-on-4'            : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms13-4-t0'               : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms13-5-t1-lag'           : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms21-t0-z9332f-02'       : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms7-t0-dx010-4'          : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms7-t0-dx010-5'          : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms7-t0-s6100'            : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'vms7-t0-s6100-4'          : '$(TRUSTY_IMAGE_BRCM_202012)',
+  'testbed-str-e1031-acs-1'  : '$(TRUSTY_IMAGE_BRCM_202012_SLIM)',
+  'testbed-str-e1031-acs-3'  : '$(TRUSTY_IMAGE_BRCM_202012_SLIM)',
+  'vms20-t1-dx010-6'         : '$(TRUSTY_IMAGE_BRCM_202205)',
+  'vms3-t1-dx010-1'          : '$(TRUSTY_IMAGE_BRCM_202205)',
+  'vms7-t1-s6100'            : '$(TRUSTY_IMAGE_BRCM_202205)',
+  'vms2-t1-7260-7'           : '$(TRUSTY_IMAGE_BRCM_ABOOT_201911)',
+  'vms20-t0-ixia-2'          : '$(TRUSTY_IMAGE_BRCM_ABOOT_201911)',
+  'vms24-t1-7050qx-acs-01'   : '$(TRUSTY_IMAGE_BRCM_ABOOT_201911)',
+  'vms6-t1-7060'             : '$(TRUSTY_IMAGE_BRCM_ABOOT_201911)',
+  'vms20-t0-7050cx3-1'       : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms20-t0-7050cx3-2'       : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms21-dual-t0-7050-3'     : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms21-dual-t0-7260'       : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms24-dual-t0-7050-1'     : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms24-dual-t0-7050-2'     : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms24-t0-7260-2'          : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms28-dual-t0-7260'       : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms7-t0-7260-1'           : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms7-t0-7260-2'           : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012)',
+  'vms18-t0-7050qx-acs-02'   : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012_SLIM)',
+  'vms18-t1-7050qx-acs-03'   : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012_SLIM)',
+  'vms6-t0-7060'             : '$(TRUSTY_IMAGE_BRCM_ABOOT_202012_SLIM)',
+  'vms20-t1-7050cx3-3'       : '$(TRUSTY_IMAGE_BRCM_ABOOT_202205)',
+  'vmsvc1-dual-t0-7050-1'    : '$(TRUSTY_IMAGE_BRCM_ABOOT_202205)',
+  'vmsvc1-dual-t0-7050-2'    : '$(TRUSTY_IMAGE_BRCM_ABOOT_202205)',
+  'vms3-t1-7280'             : '$(TRUSTY_IMAGE_BRCM_ABOOT_DNX_CHASSIS_202205)',
+  'vms28-t0-7280-4'          : '$(TRUSTY_IMAGE_BRCM_DNX_ABOOT_PUBLIC)',
+  'vms21-t1-8102-01'         : '$(TRUSTY_IMAGE_CISCO_ABOOT_202012)',
+  'vms28-dual-t0-8102'       : '$(TRUSTY_IMAGE_CISCO_ABOOT_202012)',
+  'vms28-t1-8102-02'         : '$(TRUSTY_IMAGE_CISCO_ABOOT_202012)',
+  'vms21-t1-8101-02'         : '$(TRUSTY_IMAGE_CISCO_ABOOT_202205)',
+  'testbed-str2-7215-acs-1'  : '$(TRUSTY_IMAGE_MARVELL_202012)',
+  'testbed-str2-7215-acs-3'  : '$(TRUSTY_IMAGE_MARVELL_202205)',
+  'vms1-8'                   : '$(TRUSTY_IMAGE_MLNX_201911)',
+  'vms2-2-t0-2700'           : '$(TRUSTY_IMAGE_MLNX_201911)',
+  'vms2-4-t0-2700'           : '$(TRUSTY_IMAGE_MLNX_201911)',
+  'vms20-t0-sn3800-2'        : '$(TRUSTY_IMAGE_MLNX_201911)',
+  'vms11-2-t0'               : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms12-t0-3800'            : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms21-t0-2700'            : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms24-t0-3800-azd'        : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms28-t0-4600c-03'        : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms28-t0-4600c-04'        : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms7-t0-4600c-2'          : '$(TRUSTY_IMAGE_MLNX_202012)',
+  'vms1-t1-2700'             : '$(TRUSTY_IMAGE_MLNX_202205)',
+  'vms18-t1-msn4600c-acs-1'  : '$(TRUSTY_IMAGE_MLNX_202205)',
+  'vms21-t1-2700-2'          : '$(TRUSTY_IMAGE_MLNX_202205)'
 }
