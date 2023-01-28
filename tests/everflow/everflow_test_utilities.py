f446306e316e4eb747476e73bbc2fbde6666fef5
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-01-27 16:15:07 -0800

    add assertion to protect usage

--- tests/everflow/everflow_test_utilities.py
+++ tests/everflow/everflow_test_utilities.py
@@ -312,6 +312,7 @@ def setup_info(duthosts, rand_one_dut_hostname, tbinfo, request):
     if 't1' in topo or 't0' in topo or 'm0' in topo or 'mx' in topo or 'dualtor' in topo:
         downstream_duthost = upstream_duthost = duthost = duthosts[rand_one_dut_hostname]
     elif 't2' in topo:
+        pytest_assert(len(duthosts) > 1, "Test must run on whole chassis")
         downstream_duthost, upstream_duthost = get_t2_duthost(duthosts, tbinfo)
         
     setup_information = gen_setup_information(downstream_duthost, upstream_duthost, tbinfo)
