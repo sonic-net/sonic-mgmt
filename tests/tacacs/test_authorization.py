d028ad3caff8dd28a330985929e38d88c947339b
liuh-80 <liuh@microsoft.com>
2023-01-30 03:15:16 +0000

    Fix typo

--- tests/tacacs/test_authorization.py
+++ tests/tacacs/test_authorization.py
@@ -268,7 +268,7 @@ def test_bypass_authorization(duthosts, enum_rand_one_per_hwsku_hostname, tacacs
         Verify user can't run script with sh/python with following command.
             python ./testscript.py
 
-        NOTE: TACACS UT using tac_plus as server side, there is a bug that tac_plus can't handle an authorization messagecontains more than 10 attributes.
+        NOTE: TACACS UT using tac_plus as server side, there is a bug that tac_plus can't handle an authorization message contains more than 10 attributes.
               Because every command parameter will convert to a TACACS attribute, please don't using more than 5 command parameters in test case.
     """
     exit_code, stdout, stderr = ssh_run_command(remote_user_client, 'echo "" >> ./testscript.py')
