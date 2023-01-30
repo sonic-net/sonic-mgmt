7597ee7a3ec42b275fdfb298f14b3c0d3011fb26
abdosi <58047199+abdosi@users.noreply.github.com>
2023-01-30 11:02:54 -0800

    Enhance test_ip_packet for multi-asic scenario (#7304)
    Enhance test_ip_packet for multi-asic scenario.

--- tests/ip/test_ip_packet.py
+++ tests/ip/test_ip_packet.py
@@ -8,6 +8,7 @@ import ptf.testutils as testutils
 import pytest
 from ptf import mask, packet
 
+from collections import defaultdict
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.portstat_utilities import parse_column_positions
 from tests.common.portstat_utilities import parse_portstat
@@ -106,7 +107,8 @@ class TestIPPacket(object):
  
 
     @pytest.fixture(scope="class")
-    def common_param(self, duthost, tbinfo):
+    def common_param(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, tbinfo):
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
         mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
 
         # generate peer_ip and port channel pair, be like:[("10.0.0.57", "PortChannel0001")]
@@ -116,25 +118,50 @@ class TestIPPacket(object):
         pc_ports_map = {pair[1]: mg_facts["minigraph_portchannels"][pair[1]]["members"] for pair in
                         peer_ip_pc_pair}
 
-        if len(mg_facts["minigraph_interfaces"]) >= 2:
-            # generate peer_ip and interfaces pair,
-            # be like:[("10.0.0.57", ["Ethernet48"])]
-            peer_ip_ifaces_pair = [(intf["peer_addr"], [intf["attachto"]]) for intf in mg_facts["minigraph_interfaces"]
-                                   if
-                                   ipaddress.ip_address(intf['peer_addr']).version == 4]
-            selected_peer_ip_ifaces_pairs = random.sample(peer_ip_ifaces_pair, k=2)
-            rif_rx_ifaces = selected_peer_ip_ifaces_pairs[0][1][0]
+        # generate peer_ip and interfaces pair,
+        # be like:[("10.0.0.57", ["Ethernet48"])]
+        router_port_peer_ip_ifaces_pair = [(intf["peer_addr"], [intf["attachto"]], mg_facts["minigraph_neighbors"][intf["attachto"]]['namespace'])
+                               for intf in mg_facts["minigraph_interfaces"] if ipaddress.ip_address(intf['peer_addr']).version == 4]
+        # generate peer_ip and interfaces(port channel members) pair,
+        # be like:[("10.0.0.57", ["Ethernet48", "Ethernet52"])]
+        port_channel_peer_ip_ifaces_pair = [(pair[0], mg_facts["minigraph_portchannels"][pair[1]]["members"], 
+                                             mg_facts["minigraph_neighbors"][mg_facts["minigraph_portchannels"][pair[1]]["members"][0]]['namespace'])
+                                             for pair in peer_ip_pc_pair]
+
+
+        namespace_with_min_two_ip_interface=None
+        peer_ip_ifaces_pair = []
+        peer_ip_ifaces_pair_list = [router_port_peer_ip_ifaces_pair, port_channel_peer_ip_ifaces_pair]
+        namespace_neigh_cnt_map = defaultdict(list)
+        for idx, peer_ip_ifaces in enumerate(peer_ip_ifaces_pair_list):
+            for peer_idx, peer_info in enumerate(peer_ip_ifaces):
+                namespace_neigh_cnt_map[peer_info[2]].append((idx, peer_idx))
+                if len(namespace_neigh_cnt_map[peer_info[2]]) == 2:
+                    namespace_with_min_two_ip_interface = peer_info[2]
+                    break
+            if namespace_with_min_two_ip_interface is not None:
+                    break
+     
+        selected_peer_ip_ifaces_pairs = []
+        rif_rx_ifaces = None
+        if namespace_with_min_two_ip_interface is not None:
+            for v in namespace_neigh_cnt_map[namespace_with_min_two_ip_interface]:
+                selected_peer_ip_ifaces_pairs.append(peer_ip_ifaces_pair_list[v[0]][v[1]])
+                if not rif_rx_ifaces:
+                    if v[0]:
+                        rif_rx_ifaces = list(pc_ports_map.keys())[list(pc_ports_map.values()).index(selected_peer_ip_ifaces_pairs[0][1])]
+                    else:
+                        rif_rx_ifaces = selected_peer_ip_ifaces_pairs[0][1][0]
         else:
-            # generate peer_ip and interfaces(port channel members) pair,
-            # be like:[("10.0.0.57", ["Ethernet48", "Ethernet52"])]
-            peer_ip_ifaces_pair = [(pair[0], mg_facts["minigraph_portchannels"][pair[1]]["members"]) for pair in
-                                   peer_ip_pc_pair]
-            selected_peer_ip_ifaces_pairs = random.sample(peer_ip_ifaces_pair, k=2)
-            rif_rx_ifaces = list(pc_ports_map.keys())[list(pc_ports_map.values()).index(selected_peer_ip_ifaces_pairs[0][1])]
+            pytest.skip("Skip test as not enough neighbors/ports.")
 
         # use first port of first peer_ip_ifaces pair as input port
         # all ports in second peer_ip_ifaces pair will be output/forward port
         ptf_port_idx = mg_facts["minigraph_ptf_indices"][selected_peer_ip_ifaces_pairs[0][1][0]]
+        ptf_port_idx_namespace = namespace_with_min_two_ip_interface 
+        asic_id = duthost.get_asic_id_from_namespace(ptf_port_idx_namespace)
+        ingress_router_mac = duthost.asic_instance(asic_id).get_router_mac()
+ 
         # Some platforms do not support rif counter
         try:
             rif_counter_out = TestIPPacket.parse_rif_counters(duthost.command("show interfaces counters rif")["stdout_lines"])
@@ -144,16 +171,26 @@ class TestIPPacket(object):
             logger.info("Show rif counters failed with exception: {}".format(repr(e)))
             rif_support = False
 
-        yield selected_peer_ip_ifaces_pairs, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, mg_facts["minigraph_ptf_indices"]
+        for prefix in ["10.156.94.34/32", "10.156.190.188/32"]:
+            duthost.shell(duthost.get_vtysh_cmd_for_namespace("vtysh -c \"configure terminal\" -c \"ip route {} {} tag 1\"".format(
+                                                              prefix, selected_peer_ip_ifaces_pairs[1][0]), 
+                                                              ptf_port_idx_namespace))
+        yield selected_peer_ip_ifaces_pairs, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, mg_facts["minigraph_ptf_indices"], ingress_router_mac
 
-    def test_forward_ip_packet_with_0x0000_chksum(self, duthost, ptfadapter, common_param):
+        for prefix in ["10.156.94.34/32", "10.156.190.188/32"]:
+            duthost.shell(duthost.get_vtysh_cmd_for_namespace("vtysh -c \"configure terminal\" -c \"no ip route {} {} tag 1\"".format(
+                                                              prefix, selected_peer_ip_ifaces_pairs[1][0]), 
+                                                              ptf_port_idx_namespace))
+
+    def test_forward_ip_packet_with_0x0000_chksum(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet with checksum 0x0000(compute from scratch)
         # WHEN send the packet to DUT
         # THEN DUT should forward it as normal ip packet
 
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             pktlen=1246,
             ip_src="10.250.136.195",
@@ -205,14 +242,15 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
  
-    def test_forward_ip_packet_with_0xffff_chksum_tolerant(self, duthost, ptfadapter, common_param):
+    def test_forward_ip_packet_with_0xffff_chksum_tolerant(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet with checksum 0x0000(compute from scratch)
         # WHEN manually set checksum as 0xffff and send the packet to DUT
         # THEN DUT should tolerant packet with 0xffff, forward it as normal packet
 
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             pktlen=1246,
             ip_src="10.250.136.195",
@@ -264,14 +302,15 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
 
-    def test_forward_ip_packet_with_0xffff_chksum_drop(self, duthost, ptfadapter, common_param):
+    def test_forward_ip_packet_with_0xffff_chksum_drop(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet with checksum 0x0000(compute from scratch)
         # WHEN manually set checksum as 0xffff and send the packet to DUT
         # THEN DUT should drop packet with 0xffff and add drop count
 
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             pktlen=1246,
             ip_src="10.250.136.195",
@@ -323,7 +362,7 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt == 0, "DUT shouldn't forward packets, but forwarded {} packets, not in expected range".format(match_cnt))
 
-    def test_forward_ip_packet_recomputed_0xffff_chksum(self, duthost, ptfadapter, common_param):
+    def test_forward_ip_packet_recomputed_0xffff_chksum(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet, after forwarded(ttl-1) by DUT,
         #   it's checksum will be 0xffff after wrongly incrementally recomputed
         #   ref to https://datatracker.ietf.org/doc/html/rfc1624
@@ -331,9 +370,10 @@ class TestIPPacket(object):
         # WHEN send the packet to DUT
         # THEN DUT recompute new checksum correctly and forward packet as expected.
 
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             pktlen=1246,
             ip_src="10.250.40.40",
@@ -384,14 +424,15 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
 
-    def test_forward_ip_packet_recomputed_0x0000_chksum(self, duthost, ptfadapter, common_param):
+    def test_forward_ip_packet_recomputed_0x0000_chksum(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet, after forwarded(ttl-1) by DUT, it's checksum will be 0x0000 after recompute from scratch
         # WHEN send the packet to DUT
         # THEN DUT recompute new checksum as 0x0000 and forward packet as expected.
 
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             pktlen=1246,
             ip_src="10.250.136.195",
@@ -442,13 +483,14 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
 
-    def test_forward_normal_ip_packet(self, duthost, ptfadapter, common_param):
+    def test_forward_normal_ip_packet(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a random normal ip packet
         # WHEN send the packet to DUT
         # THEN DUT should forward it as normal ip packet, nothing change but ttl-1
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             ip_src=peer_ip_ifaces_pair[0][0],
             ip_dst=peer_ip_ifaces_pair[1][0])
@@ -493,13 +535,14 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
 
-    def test_drop_ip_packet_with_wrong_0xffff_chksum(self, duthost, ptfadapter, common_param):
+    def test_drop_ip_packet_with_wrong_0xffff_chksum(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a random normal ip packet, and manually modify checksum to 0xffff
         # WHEN send the packet to DUT
         # THEN DUT should drop it and add drop count
-        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices) = common_param
+        duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+        (peer_ip_ifaces_pair, rif_rx_ifaces, rif_support, ptf_port_idx, pc_ports_map, ptf_indices, ingress_router_mac) = common_param
         pkt = testutils.simple_ip_packet(
-            eth_dst=duthost.facts["router_mac"],
+            eth_dst=ingress_router_mac,
             eth_src=ptfadapter.dataplane.get_mac(0, ptf_port_idx),
             ip_src=peer_ip_ifaces_pair[0][0],
             ip_dst=peer_ip_ifaces_pair[1][0])
