255d216fed0af712ceeff412d2c6776aa726380f
guangyao6 <107662061+guangyao6@users.noreply.github.com>
2023-01-31 12:34:10 +0800

    [VM] enhance for wan isis dynamic hostname testcase (#7353)
    What is the motivation for this PR?
    Enhance is-is dynamic hostname case and add inter-connect duts auto-discovery for wan topology.
    
    How did you do it?
    1. Add a new testcase to check no dynamic hostname.
    2. Add wan topology for inter-connect duts auto-discovery, find inter-connect interface and add them to topo binding list.
    
    How did you verify/test it?
    1. Run is-is dynamic hostname case, case would pass.
    2. Run wan-2dut topo, all devices would setup is-is.

--- tests/wan/isis/conftest.py
+++ tests/wan/isis/conftest.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 import functools
 
@@ -14,23 +15,70 @@ def get_target_dut_port(mg_facts, dut_intf):
     return dut_port
 
 
-def get_dut_isis_dpg_topo(dut_host, nbrhosts, tbinfo):
+def parse_vm_vlan_port(vlan):
+    if isinstance(vlan, int):
+        dut_index = 0
+        vlan_index = vlan
+        ptf_index = vlan
+    else:
+        m = re.match(r"(\d+)\.(\d+)@(\d+)", vlan)
+        (dut_index, vlan_index, ptf_index) = (int(m.group(1)), int(m.group(2)), int(m.group(3)))
+    return (dut_index, vlan_index, ptf_index)
+
+
+def port_indice_to_name(dut_host, tbinfo, port_indice):
+    mg_facts = dut_host.get_extended_minigraph_facts(tbinfo)
+    indice_port_map = dict(zip(mg_facts['minigraph_port_indices'].values(), mg_facts['minigraph_port_indices'].keys()))
+    return indice_port_map[port_indice]
+
+
+def get_dut_isis_dpg_topo(dut_host, nbrhosts, duts_interconnects, tbinfo):
     connections = []
     mg_facts = dut_host.get_extended_minigraph_facts(tbinfo)
     for k, v in mg_facts['minigraph_neighbors'].items():
-        if v['name'] in nbrhosts.keys():
+        if v['name'] in nbrhosts:
             dut_port = get_target_dut_port(mg_facts, k)
             nbr_port = nbrhosts[v['name']]['host'].get_portchannel_by_member(v['port'])
             if nbr_port and dut_port:
                 connections.append((dut_host, dut_port, nbrhosts[v['name']]['host'], nbr_port))
+
+    for (dut1_host, dut1_port_index, dut2_host, dut2_port_index) in duts_interconnects:
+        if dut1_host == dut_host:
+            dut1_port = get_target_dut_port(
+                mg_facts,
+                port_indice_to_name(dut1_host, tbinfo, dut1_port_index))
+            dut2_port = get_target_dut_port(
+                dut2_host.get_extended_minigraph_facts(tbinfo),
+                port_indice_to_name(dut2_host, tbinfo, dut2_port_index))
+            connections.append((dut1_host, dut1_port, dut2_host, dut2_port))
+
     return connections
 
 
 @pytest.fixture(scope="session")
-def isis_dpg_topo(duthosts, nbrhosts, tbinfo):
+def duts_interconnects(duthosts, tbinfo):
+    connections = []
+    dut_names = [dut.hostname for dut in duthosts]
+    dut_index_map = dict(zip(tbinfo['duts_map'].values(), tbinfo['duts_map'].keys()))
+    if 'devices_interconnect_interfaces' in tbinfo['topo']['properties']['topology']:
+        for _, items in tbinfo['topo']['properties']['topology']['devices_interconnect_interfaces'].items():
+            assert(len(items) == 2)
+            connect = []
+            for item in items:
+                (dut_index, vlan_index, _) = parse_vm_vlan_port(item)
+                if dut_index not in dut_index_map or dut_index_map[dut_index] not in dut_names:
+                    break
+                connect.extend([duthosts[dut_index_map[dut_index]], vlan_index])
+            else:
+                connections.append(tuple(connect))
+    return connections
+
+
+@pytest.fixture(scope="session")
+def isis_dpg_topo(duthosts, nbrhosts, duts_interconnects, tbinfo):
     dpg_topo_list = []
     for dut_host in duthosts:
-        dpg_topo_list.extend(get_dut_isis_dpg_topo(dut_host, nbrhosts, tbinfo))
+        dpg_topo_list.extend(get_dut_isis_dpg_topo(dut_host, nbrhosts, duts_interconnects, tbinfo))
     return list(dict.fromkeys(dpg_topo_list))
 
 
