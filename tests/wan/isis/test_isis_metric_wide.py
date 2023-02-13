3609770c0610c9a7991785771798483136a63a8b
guangyao6 <107662061+guangyao6@users.noreply.github.com>
2023-02-13 16:08:03 +0800

    [VM] enhance isis_facts for wide metric and adapt IS-IS for frrcfgd mode (#7442)
    What is the motivation for this PR?
    1. Collect IS-IS database detail information in isis_facts is helpful for case development.
    2. Adapt IS-IS configuration for frrcfgd mode.
    
    How did you do it?
    1. Collect database detail information and store the information into isis_facts.
    2. Enable frrcfgd in setup and remove previous restart bgp container part.
    
    How did you verify/test it?
    1. Adapt wide metric case to new isis_facts and this case would pass.
    2. Run IS-IS level capability case and case would pass.

--- tests/wan/isis/test_isis_metric_wide.py
+++ tests/wan/isis/test_isis_metric_wide.py
@@ -6,6 +6,9 @@ from tests.common.utilities import wait_until
 from tests.common.helpers.assertions import pytest_assert
 from isis_helpers import config_device_isis
 from isis_helpers import add_dev_isis_attr, del_dev_isis_attr
+from isis_helpers import DEFAULT_ISIS_INSTANCE as isis_instance
+
+WIDE_MATRIC = '16777215'
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +24,7 @@ def isis_setup_teardown_wide_metric(isis_common_setup_teardown, request):
     selected_connections = isis_common_setup_teardown
 
     config_key = "wide_metric"
-    config_dict = {config_key: '16777215'}
+    config_dict = {config_key: WIDE_MATRIC}
     for (dut_host, _, _, _) in selected_connections:
         add_dev_isis_attr(dut_host, config_dict)
         target_devices.append(dut_host)
@@ -37,11 +40,10 @@ def isis_setup_teardown_wide_metric(isis_common_setup_teardown, request):
 
 def check_isis_wide_metric(duthost):
     isis_facts = duthost.isis_facts()["ansible_facts"]['isis_facts']
-    for lsp, metric in isis_facts['database_detail']['test'].items():
+    for lsp in isis_facts['database_detail'][isis_instance].keys():
         if duthost.hostname in lsp:
-            for _, value in metric.items():
-                if int(value) == 16777215:
-                    return True
+            for item in isis_facts['database_detail'][isis_instance][lsp]['extend_ip_reachability']:
+                return WIDE_MATRIC in item.values()
     return False
 
 
