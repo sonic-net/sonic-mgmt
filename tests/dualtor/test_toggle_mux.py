f2d5143d6ba40a09401196761ef15f2bdcfe892e
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2023-02-14 09:14:35 +0800

    [dualtor] Improve `test_toggle_mux` (#7431)
    Approach
    What is the motivation for this PR?
    Improve test_toggle_mux.
    
    Signed-off-by: Longxiang Lyu lolv@microsoft.com
    
    How did you do it?
    1. As the link prober interval reset is needed by all cases in test_toggle_mux.py, wrap this logic into a module-level fixture.
    2. Restarting linkmgrd to reduce the SONiC wait time to learn hardware mux change.
    
    How did you verify/test it?

--- tests/dualtor/test_toggle_mux.py
+++ tests/dualtor/test_toggle_mux.py
@@ -39,35 +39,45 @@ def get_interval_v4(duthosts):
         return None
 
 
-@pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_simulator(duthosts, tbinfo, active_side, toggle_all_simulator_ports,
-                                   get_mux_status, get_interval_v4, restore_mux_auto_mode):
-    logger.info('Set all muxcable to manual mode on all ToRs')
-    duthosts.shell('config muxcable mode manual all')
-
+@pytest.fixture(scope="module")
+def reset_link_prober_interval_v4(duthosts, get_interval_v4, tbinfo):
     cur_interval_v4 = get_interval_v4
     if cur_interval_v4 is not None:
         recover_linkmgrd_probe_interval(duthosts, tbinfo)
+
+    # NOTE: as there is no icmp_responder running, the device is stucked in consistently probing
+    # the mux status. If there is a previous case that has fixture run_icmp_responder called, the
+    # link prober interval is changed into 1000ms, the mux probing interval could be 384s at most.
+    # So after a hardware mux change, SONiC is only able to learn the change after 384s in worst case.
+    # To accelerate this, let's restarting linkmgrd to break out from the probing loop firstly and
+    # change the the probing interval back to 100ms to reduce the future probing interval maximum
+    # down to 38.4s.
+    duthosts.shell("docker exec mux supervisorctl restart linkmgrd")
+
+    yield
+
+    if cur_interval_v4 is not None:
+        update_linkmgrd_probe_interval(duthosts, tbinfo, cur_interval_v4)
+
+
+@pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
+def test_toggle_mux_from_simulator(duthosts, active_side, toggle_all_simulator_ports,
+                                   get_mux_status, reset_link_prober_interval_v4, restore_mux_auto_mode):
+    logger.info('Set all muxcable to manual mode on all ToRs')
+    duthosts.shell('config muxcable mode manual all')
 
     logger.info('Toggle mux active side from mux simulator')
     toggle_all_simulator_ports(active_side)
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
-
-    if cur_interval_v4 is not None:
-        update_linkmgrd_probe_interval(duthosts, tbinfo, cur_interval_v4)
-
     validate_check_result(check_result, duthosts, get_mux_status)
 
 
 @pytest.mark.parametrize("active_side", [UPPER_TOR, LOWER_TOR])
-def test_toggle_mux_from_cli(duthosts, tbinfo, active_side, get_mux_status, get_interval_v4, restore_mux_auto_mode):
+def test_toggle_mux_from_cli(duthosts, active_side, get_mux_status,
+                             reset_link_prober_interval_v4, restore_mux_auto_mode):
     logger.info('Reset muxcable mode to auto for all ports on all DUTs')
     duthosts.shell('config muxcable mode auto all')
-
-    cur_interval_v4 = get_interval_v4
-    if cur_interval_v4 is not None:
-        recover_linkmgrd_probe_interval(duthosts, tbinfo)
 
     # Use cli to toggle muxcable active side
     if active_side == UPPER_TOR:
@@ -77,8 +87,4 @@ def test_toggle_mux_from_cli(duthosts, tbinfo, active_side, get_mux_status, get_
     mux_active_dut.shell('config muxcable mode active all')
 
     check_result = wait_until(60, 5, 2, check_mux_status, duthosts, active_side)
-
-    if cur_interval_v4 is not None:
-        update_linkmgrd_probe_interval(duthosts, tbinfo, cur_interval_v4)
-
     validate_check_result(check_result, duthosts, get_mux_status)
