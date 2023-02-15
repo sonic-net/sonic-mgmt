3daff4242e64569817c27f98ce28cf52d80af486
Manouchehr Taheri <manouchehr.taheri@nokia.com>
2023-02-15 11:01:08 -0500

    Fixed test_get_transceiver_info testcase for ZR SFPs (manny: PR#7480)
    Moved code from outside QSFP_DD to inside

--- tests/platform_tests/api/test_sfp.py
+++ tests/platform_tests/api/test_sfp.py
@@ -244,13 +244,20 @@ class TestSfpApi(PlatformApiTestBase):
         'rxsigpowerlowwarning'
     ]
 
+    # EXPECTED_QSFPZR COMMON_INFO_KEYS
+    QSFPZR_EXPECTED_XCVR_INFO_KEYS = [
+        'supported_min_laser_freq',
+        'supported_max_laser_freq',
+        'supported_min_tx_power',
+        'supported_max_tx_power'
+    ]
+
     chassis_facts = None
     duthost_vars = None
 
     #
     # Helper functions
     #
-
     def is_xcvr_optical(self, xcvr_info_dict):
         """Returns True if transceiver is optical, False if copper (DAC)"""
         #For QSFP-DD specification compliance will return type as passive or active
@@ -362,13 +369,16 @@ class TestSfpApi(PlatformApiTestBase):
                     if duthost.sonic_release in ["201811", "201911", "202012", "202106", "202111"]:
                         UPDATED_EXPECTED_XCVR_INFO_KEYS = [key if key != 'vendor_rev' else 'hardware_rev' for key in
                             self.EXPECTED_XCVR_INFO_KEYS]
-                        #self.EXPECTED_XCVR_INFO_KEYS = EXPECTED_XCVR_INFO_KEYS
                     else:
-
                         if info_dict["type_abbrv_name"] == "QSFP-DD":
-                            UPDATED_EXPECTED_XCVR_INFO_KEYS = self.EXPECTED_XCVR_INFO_KEYS + \
-                                                           self.EXPECTED_XCVR_NEW_QSFP_DD_INFO_KEYS + \
-                                                           ["active_apsel_hostlane{}".format(i) for i in range(1, info_dict['host_lane_count'] + 1)]
+                            XCVR_INFO_KEYS = self.EXPECTED_XCVR_INFO_KEYS + \
+                                             self.EXPECTED_XCVR_NEW_QSFP_DD_INFO_KEYS + \
+                                             ["active_apsel_hostlane{}".format(i) for i in
+                                              range(1, info_dict['host_lane_count'] + 1)]
+                            if 'ZR' in info_dict['media_interface_code']:
+                                UPDATED_EXPECTED_XCVR_INFO_KEYS = XCVR_INFO_KEYS + self.QSFPZR_EXPECTED_XCVR_INFO_KEYS
+                            else:
+                                UPDATED_EXPECTED_XCVR_INFO_KEYS = XCVR_INFO_KEYS
                         else:
                             UPDATED_EXPECTED_XCVR_INFO_KEYS = self.EXPECTED_XCVR_INFO_KEYS
                     missing_keys = set(UPDATED_EXPECTED_XCVR_INFO_KEYS) - set(actual_keys)
