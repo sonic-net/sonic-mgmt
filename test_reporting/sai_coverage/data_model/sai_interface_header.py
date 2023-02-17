29127abd4f0784c326cdc018e15c90cce3c060a5
zhiyuan <liangzy512@gmail.com>
2023-02-17 15:43:14 +0800

    [SAI-PTF] explained parameters
    Signed-off-by: zhiyuan <liangzy512@gmail.com>

--- test_reporting/sai_coverage/data_model/sai_interface_header.py
+++ test_reporting/sai_coverage/data_model/sai_interface_header.py
@@ -1,6 +1,13 @@
 class SAIInterfaceHeader(object):
     """
     Structure of SAI interface header
+
+    Args:
+        intf_groupname: SAI interface groupname
+        intf_groupalias: SAI interface groupalias
+        intf_name: SAI interface name
+        intf_alias: SAI interface alias
+        file_name: file name
     """
     def __init__(self, intf_groupname, intf_groupalias, intf_name, intf_alias, file_name):
         self.intf_groupname = intf_groupname
