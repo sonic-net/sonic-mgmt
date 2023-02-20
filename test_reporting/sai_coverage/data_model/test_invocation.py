ea59eab0068d092e838626cd6a66a3b1b10fca9f
Zhiyuan Liang <39913941+zhiyuan0112@users.noreply.github.com>
2023-02-20 14:09:25 +0800

    [SAI-PTF] Fix SAI cases scanner (#7466)
    What is the motivation for this PR?
    Fix SAI header scanner and remove the extra operations in Kusto Explorer.
    
    How did you do it?
    Modify SAI header scanning code.
    Drop CaseInvocationReport table and integrate attributes into CaseInvocationStaging, so the extra operations in the Kusto Explorer can be omitted.
    
    How did you verify/test it?
    Tested locally.
    
    Signed-off-by: zhiyuan <liangzy512@gmail.com>
    Signed-off-by: zhiyuanliang <t-zhliang@microsoft.com>

--- test_reporting/sai_coverage/data_model/test_invocation.py
+++ test_reporting/sai_coverage/data_model/test_invocation.py
@@ -1,11 +1,36 @@
 class TestInvocation(object):
     """
     Structure of invocation
+
+    Args:
+        id: unique identifier for each record
+        file_name: file name
+        class_name: class name
+        case_name: method name
+        case_invoc: SAI interface
+        sai_header: file name
+        saiintf_id: SAI interface groupname
+        saiintf_method_table: SAI interface groupalias
+        saiintf_name: SAI interface name
+        saiintf_alias: SAI interface alias
+        test_set: distinguish test set ("t0" or "ptf")
+        test_platform: the platform used for running cases
+        platform_purpose_attr: platform
+        attr_name: Attribute name
+        attr_value: Attribute value
+        runnable: distinguish whether case runnable
+        sai_folder: folder name of the scanning file
+        upload_time: upload time
     """
 
-    def __init__(self, file_name, class_name, case_name, case_invoc, sai_header, saiintf_id,
+    def __init__(self, id, file_name, class_name, case_name, case_invoc, sai_header, saiintf_id,
                  saiintf_method_table, saiintf_name, saiintf_alias, test_set, test_platform,
                  platform_purpose_attr, attr_name, attr_value, runnable, sai_folder, upload_time):
+        sai_api = saiintf_name[4:4 + len(saiintf_name) - 7]
+        sai_feature = saiintf_method_table[4:4 + len(saiintf_method_table) - 10]
+
+        self.id = id
+        self.is_azure_used = False
         self.file_name = file_name
         self.case_name = case_name
         self.class_name = class_name
@@ -13,7 +38,8 @@ class TestInvocation(object):
         self.sai_header = sai_header
         self.saiintf_id = saiintf_id
         self.saiintf_method_table = saiintf_method_table
-        self.sai_api = saiintf_name
+        self.sai_feature = sai_feature.replace('_', '')
+        self.sai_api = sai_api[0:len(sai_api) - 10] if 'attribute' in sai_api else sai_api
         self.saiintf_alias = saiintf_alias
         self.test_set = test_set
         self.test_platform = test_platform
