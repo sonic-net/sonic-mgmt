f5d6428752633446c1de172db4faa71e6a02475b
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2023-02-20 01:33:51 -0800

    Changed the bidirectional traffic-flow check to unidirection in ixia pfcwd script for cisco-8000 (#7326)
    * fixing the a2a and m2o scripts for cisco-8000.
    
    * fixing the a2a and m2o scripts for cisco-8000.
    
    * Removing unnecessary import
    
    * Remove import in test_pfcwd_m2o.py

---
+++ tests/ixia/pfcwd/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from tests.common.cisco_data import is_cisco_device
+@pytest.fixture
+def setup_cgm_alpha_cisco(duthost):
+    if not is_cisco_device(duthost):
+        return
+    duthost.shell("mmuconfig -p pg_lossless_100000_300m_profile -a -6")
+    yield
+    duthost.shell("mmuconfig -p pg_lossless_100000_300m_profile -a -2")
+
+
