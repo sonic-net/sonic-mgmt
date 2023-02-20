523c91330e1e992030048ffb50623a7c2b4c5ab1
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2023-02-20 21:42:13 +0800

    [dualtor] Check before restore drop ports (#7515)
    Approach
    What is the motivation for this PR?
    Before making gRPC calls to restore the dropping ports, check first.
    
    Signed-off-by: Longxiang Lyu lolv@microsoft.com
    
    How did you do it?
    As the motivation.

--- tests/common/dualtor/nic_simulator_control.py
+++ tests/common/dualtor/nic_simulator_control.py
@@ -298,7 +298,8 @@ def set_drop_active_active(mux_config, nic_simulator_client):
             "Set drop recover on port %s, mux server %s, portid %s",
             interface_name, nic_address, portid,
         )
-    _call_set_drop_nic_simulator(_nic_addresses, _portids, _directions, recover=True)
+    if _nic_addresses:
+        _call_set_drop_nic_simulator(_nic_addresses, _portids, _directions, recover=True)
 
 
 @pytest.fixture(scope="function")
