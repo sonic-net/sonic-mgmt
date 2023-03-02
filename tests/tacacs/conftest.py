b74c3935fe51314ddb3cdf22e3813eb36f3d7b0c
Xin Wang <xiwang5@microsoft.com>
2023-03-02 18:39:40 +0800

    Add support of using lab tacacs server by default
    The current default configuration of DUTs does not support using
    lab tacacs server for authentication.
    
    This change added a new variable `tacacs_enabled_by_default` in
    group_vars file to control enablement of lab tacacs server.
    
    The ansible playbook for deploy minigraph was updated to support
    configuring lab tacacs server based on value of this variable.
    
    The tacacs test scripts are updated to support recover the lab
    tacacs server in a clean way after test is done.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- tests/tacacs/conftest.py
+++ tests/tacacs/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from .utils import setup_tacacs_client, setup_tacacs_server, cleanup_tacacs, restore_tacacs_servers
 
 logger = logging.getLogger(__name__)
-TACACS_CREDS_FILE='tacacs_creds.yaml'
+TACACS_CREDS_FILE = 'tacacs_creds.yaml'
 
 
 @pytest.fixture(scope="module")
@@ -15,21 +15,23 @@ def tacacs_creds(creds_all_duts):
     creds_all_duts.update(hardcoded_creds)
     return creds_all_duts
 
+
 @pytest.fixture(scope="module")
-def check_tacacs(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds):
+def check_tacacs(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds, request):
     logger.info('tacacs_creds: {}'.format(str(tacacs_creds)))
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     tacacs_server_ip = ptfhost.mgmt_ip
-    default_tacacs_servers = setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
+    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
     setup_tacacs_server(ptfhost, tacacs_creds, duthost)
 
     yield
 
     cleanup_tacacs(ptfhost, tacacs_creds, duthost)
-    restore_tacacs_servers(duthost, default_tacacs_servers, tacacs_server_ip)
+    restore_tacacs_servers(duthost, request)
+
 
 @pytest.fixture(scope="module")
-def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds):
+def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds, request):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     ptfhost_vars = ptfhost.host.options['inventory_manager'].get_host(ptfhost.hostname).vars
     if 'ansible_hostv6' not in ptfhost_vars:
@@ -41,3 +43,4 @@ def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_
     yield
 
     cleanup_tacacs(ptfhost, tacacs_creds, duthost)
+    restore_tacacs_servers(duthost, request)
