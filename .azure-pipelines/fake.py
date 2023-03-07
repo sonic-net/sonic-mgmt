5bc785eda131e941030b0e0041c6f4732ddb7476
Xin Wang <xiwang5@microsoft.com>
2023-03-07 17:42:55 +0800

    Remove fake script

--- .azure-pipelines/fake.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-if __name__ == "__main__":
-    print("a very loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog line")
