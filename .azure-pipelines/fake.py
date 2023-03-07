1c31c3d6acfa7eddebecff8cccbbfff48389ff86
Xin Wang <xiwang5@microsoft.com>
2023-03-07 17:39:50 +0800

    Add fake script

---
+++ .azure-pipelines/fake.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    print("a very loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog line")
