7083bef7616e80a6a689611284dda2979df2f407
Anant Kishor Sharma <ananshar@cisco.com>
2023-03-10 01:37:09 -0800

    fixing pre-commit error - tests/arp/test_unknown_mac.py:63:38: E127 continuation line over-indented for visual indent

--- tests/arp/test_unknown_mac.py
+++ tests/arp/test_unknown_mac.py
@@ -60,7 +60,7 @@ def unknownMacSetup(duthosts, rand_one_dut_hostname, tbinfo):
     # so we need to skip this test on Mellanox platform
     asic_type = duthost.facts["asic_type"]
     pytest_require(asic_type not in ["mellanox", "barefoot", "cisco-8000"],
-                                     "Test is not supported for platform {}".format(asic_type))
+                   "Test is not supported for platform {}".format(asic_type))
 
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
     is_backend_topology = mg_facts.get(constants.IS_BACKEND_TOPOLOGY_KEY, False)
