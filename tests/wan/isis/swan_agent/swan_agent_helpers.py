3b6e16dae44ae4caac2b10745d91f5835789c78f
Guangyao Zhao <guangyaozhao@microsoft.com>
2023-03-24 02:21:27 +0000

    Merged PR 2237: [VM]Adapt swan agent loading step
    What is the motivation for this PR?
    Adapt swan agent init and uninit process based on new image.
    
    How did you do it?
    Remove previous steps and unzip the tar file, using init and uninit script.
    
    How did you verify/test it?
    Run swan agent related testcase, testcase would pass.

--- tests/wan/isis/swan_agent/swan_agent_helpers.py
+++ tests/wan/isis/swan_agent/swan_agent_helpers.py
@@ -3,50 +3,78 @@ import re
 import glob
 
 
-DUT_SWAN_AGANT_PATH = '/tmp'
+DUT_SWAN_AGANT_PATH = '~'
 SWAN_AGENT_PATH = '/data/swan_agent'
 SWAN_AGENT_NAME = 'SwanAgentSonic'
+SWAN_AGENT_INIT = 'sonicswanagent_init.sh'
+SWAN_AGENT_UNINIT = 'sonicswanagent_uninit.sh'
+
+
+def get_latest_file(regex):
+    file_iter = glob.glob(os.path.join(SWAN_AGENT_PATH, regex))
+    return max(file_iter, key=os.path.getctime)
+
 
 def get_swan_agent_file():
     regex_swan_agent = "swanagentsonic-*.x64.tar"
-    file_iter = glob.glob(os.path.join(SWAN_AGENT_PATH, regex_swan_agent))
-    for item in file_iter:
-        return max(file_iter, key=os.path.getctime)
+    return get_latest_file(regex_swan_agent)
 
 
-def load_swan_agent(duthost):
+def extract_swan_agent_file(localhost):
+    regex_swan_agent_tar = "swanagentsonic-*.x86_64.tar"
+    target = get_latest_file(regex_swan_agent_tar)
+    localhost.shell("tar -xf {}".format(target))
+
+
+def get_containerid(dut_host):
+    regex_containerid = re.compile(r'^(\S+)\s+.*')
+    output = dut_host.shell("docker ps | grep '{}'".format(SWAN_AGENT_NAME))
+    return regex_containerid.match(output["stdout_lines"][0]).group(1)
+
+
+def load_swan_agent(dut_host, localhost):
+    extract_swan_agent_file(localhost)
+
     agent_file = get_swan_agent_file()
     dest_file = os.path.join(DUT_SWAN_AGANT_PATH, os.path.basename(agent_file))
+    init_script = os.path.join(DUT_SWAN_AGANT_PATH, SWAN_AGENT_INIT)
+    uninit_script = os.path.join(DUT_SWAN_AGANT_PATH, SWAN_AGENT_UNINIT)
 
-    duthost.file(path=dest_file, state='absent')
-    duthost.copy(src=agent_file, dest=dest_file)
-    duthost.shell("sudo docker load -i {}".format(dest_file))
+    dut_host.file(path=init_script, state='absent')
+    dut_host.copy(src=os.path.join(SWAN_AGENT_PATH, SWAN_AGENT_INIT), dest=init_script)
+    dut_host.shell("chmod +x {}".format(init_script))
 
-    output = duthost.shell("sudo docker images | grep 'swanagent'")["stdout_lines"]
+    dut_host.file(path=uninit_script, state='absent')
+    dut_host.copy(src=os.path.join(SWAN_AGENT_PATH, SWAN_AGENT_UNINIT), dest=uninit_script)
+    dut_host.shell("chmod +x {}".format(uninit_script))
+
+    dut_host.file(path=dest_file, state='absent')
+    dut_host.copy(src=agent_file, dest=dest_file)
+    dut_host.shell("sudo docker load -i {}".format(dest_file))
+
+    output = dut_host.shell("sudo docker images | grep 'swanagent'")["stdout_lines"]
     regex_imageid = re.compile(r'(\S+)\s+(\S+)\s+(\S+).*')
     if regex_imageid.match(output[0]):
-        duthost.shell("sudo docker run --name {} -itd --network host --cap-add=SYS_ADMIN {}".format(
+        dut_host.shell("sudo docker run --name {} -itd --network host --cap-add=SYS_ADMIN {}".format(
             SWAN_AGENT_NAME, regex_imageid.match(output[0]).group(3)))
         time.sleep(5)
-        regex_containerid = re.compile(r'^(\S+)\s+.*')
-        output = duthost.shell("docker ps | grep '{}'".format(SWAN_AGENT_NAME))
-        containerid = regex_containerid.match(output["stdout_lines"][0]).group(1)
-        duthost.shell("sudo docker cp {}:/go/src/grpcStub /tmp".format(containerid))
-        duthost.shell("nohup sudo python3 /tmp/grpcStub/server.py > /tmp/grpcStub.log 2>&1 &")
+        dut_host.shell("{}/{} {}".format(DUT_SWAN_AGANT_PATH, SWAN_AGENT_INIT, get_containerid(dut_host)))
         time.sleep(10)
 
 
-def remove_swan_agent(duthost):
+def remove_swan_agent(dut_host):
     agent_file = get_swan_agent_file()
     dest_file = os.path.join(DUT_SWAN_AGANT_PATH, os.path.basename(agent_file))
-    duthost.file(path=dest_file, state='absent')
+    dut_host.file(path=dest_file, state='absent')
 
-    duthost.shell("sudo docker stop {}".format(SWAN_AGENT_NAME))
-    time.sleep(3)
-    duthost.shell("sudo docker rm {}".format(SWAN_AGENT_NAME))
-    duthost.shell("sudo pkill -f /tmp/grpcStub/server.py", module_ignore_errors=True)
+    init_script = os.path.join(DUT_SWAN_AGANT_PATH, SWAN_AGENT_INIT)
+    dut_host.file(path=init_script, state='absent')
 
-    output = duthost.shell("sudo docker images | grep 'swanagent'")["stdout_lines"]
+    dut_host.shell("{}/{} {}".format(DUT_SWAN_AGANT_PATH, SWAN_AGENT_UNINIT, get_containerid(dut_host)))
+    uninit_script = os.path.join(DUT_SWAN_AGANT_PATH, SWAN_AGENT_UNINIT)
+    dut_host.file(path=uninit_script, state='absent')
+
+    output = dut_host.shell("sudo docker images | grep 'swanagent'")["stdout_lines"]
     regex_imageid = re.compile(r'(\S+)\s+(\S+)\s+(\S+).*')
     if regex_imageid.match(output[0]):
-        duthost.shell("sudo docker rmi {}".format(regex_imageid.match(output[0]).group(3)))
+        dut_host.shell("sudo docker rmi {}".format(regex_imageid.match(output[0]).group(3)))
