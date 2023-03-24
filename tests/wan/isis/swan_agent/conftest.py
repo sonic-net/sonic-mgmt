00ce5ef9041c3bb3b078f92bbb4484a77cce88aa
Guangyao Zhao <guangyaozhao@microsoft.com>
2023-03-24 03:13:30 +0000

    Merged PR 2239: [VM]Adapt swan agent loading step2
    What is the motivation for this PR?
    Adapt swan agent init and uninit process based on new image.
    
    How did you do it?
    Remove previous steps and unzip the tar file, using init and uninit script.
    
    How did you verify/test it?
    Run swan agent related testcase, testcase would pass.

--- tests/wan/isis/swan_agent/conftest.py
+++ tests/wan/isis/swan_agent/conftest.py
@@ -1,6 +1,8 @@
 import pytest
+import functools
 from swan_agent_helpers import get_swan_agent_file
 from swan_agent_helpers import load_swan_agent, remove_swan_agent
+
 
 def pytest_collection_modifyitems(config, items):
     if not config.getoption("--swan_agent"):
@@ -16,11 +18,10 @@ def pytest_collection_modifyitems(config, items):
 
 
 @pytest.fixture(scope="module")
-def swan_agent_setup_teardown(isis_common_setup_teardown, request):
-    target_devices = []
+def swan_agent_setup_teardown(isis_common_setup_teardown, request, localhost):
     selected_connections = isis_common_setup_teardown
     (dut_host, dut_port, nbr_host, nbr_port) = selected_connections[0]
 
-    load_swan_agent(dut_host)
+    load_swan_agent(dut_host, localhost)
     request.addfinalizer(functools.partial(remove_swan_agent, dut_host))
     yield dut_host
