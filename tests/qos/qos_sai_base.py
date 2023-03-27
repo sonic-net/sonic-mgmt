a3751775dcb58f9cc36921e3fa643f7a2ddee1d3
Xu Chen <112069142+XuChen-MSFT@users.noreply.github.com>
2023-03-27 11:24:00 +0800

    stop radvd/arp_update service to reduce background traffic during qos sai test (#7820)

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -844,8 +844,10 @@ class QosSaiBase(QosBase):
         services = [
             {"docker": dut_asic.get_docker_name("lldp"), "service": "lldp-syncd"},
             {"docker": dut_asic.get_docker_name("lldp"), "service": "lldpd"},
-            {"docker": dut_asic.get_docker_name("bgp"),  "service": "bgpd"},
-            {"docker": dut_asic.get_docker_name("bgp"),  "service": "bgpmon"},
+            {"docker": dut_asic.get_docker_name("bgp"), "service": "bgpd"},
+            {"docker": dut_asic.get_docker_name("bgp"), "service": "bgpmon"},
+            {"docker": dut_asic.get_docker_name("radv"), "service": "radvd"},
+            {"docker": dut_asic.get_docker_name("swss"), "service": "arp_update"}
         ]
 
         feature_list = ['lldp', 'bgp', 'syncd', 'swss']
