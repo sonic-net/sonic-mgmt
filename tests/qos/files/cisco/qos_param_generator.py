75e3e90306dbd2fabc562b3feb9686bb427994ba
Randall Pittman <randallbpittman@gmail.com>
2023-03-28 17:49:01 +0000

    pre-commit formatting on new file.

--- tests/qos/files/cisco/qos_param_generator.py
+++ tests/qos/files/cisco/qos_param_generator.py
@@ -31,7 +31,7 @@ class QosParamCisco(object):
         self.qos_params[testcase]["skip"] = reason
 
     def __define_shared_reservation_size(self):
-        if self.ingress_pool_size == None or self.ingress_pool_headroom == None:
+        if self.ingress_pool_size is None or self.ingress_pool_headroom is None:
             skip_reason = "ingress_lossless_pool not defined, nothing to test"
             self.__mark_skip("shared_res_size_1", skip_reason)
             self.__mark_skip("shared_res_size_2", skip_reason)
@@ -41,14 +41,16 @@ class QosParamCisco(object):
                      "queues": [0, 0, 0, 0, 1, 1, 1, 1, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4],
                      "src_port_i": [0, 1, 2, 3, 0, 1, 2, 3, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
                      "dst_port_i": [6, 7, 8, 9, 6, 7, 8, 9, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11],
-                     "pkt_counts": [3822, 3822, 3822, 3822, 3822, 3822, 3822, 3822, 2595, 2595, 2595, 2595, 2038, 2038, 1014, 1014, 1014, 1014, 64, 1],
+                     "pkt_counts": [3822, 3822, 3822, 3822, 3822, 3822, 3822, 3822, 2595, 2595, 2595, 2595,
+                                    2038, 2038, 1014, 1014, 1014, 1014, 64, 1],
                      "shared_limit_bytes": 75497472}
             res_2 = {"dscps": [3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3],
                      "pgs": [3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3],
                      "queues": [3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3],
                      "src_port_i": [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8],
                      "dst_port_i": [9, 9, 10, 10, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6],
-                     "pkt_counts": [3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 2052, 2052, 1286, 1286, 1286, 238, 1],
+                     "pkt_counts": [3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 3549, 2052, 2052,
+                                    1286, 1286, 1286, 238, 1],
                      "shared_limit_bytes": 67109376}
         else:
             res_1 = {"dscps": [8, 8, 8, 8, 8, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4],
@@ -56,7 +58,8 @@ class QosParamCisco(object):
                      "queues": [0, 0, 0, 0, 0, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4],
                      "src_port_i": [0, 1, 2, 3, 4, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
                      "dst_port_i": [6, 7, 8, 9, 10, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11],
-                     "pkt_counts": [3413, 3413, 3413, 3413, 3413, 2389, 2389, 2389, 1526, 1526, 1392, 415, 415, 415, 415, 42, 1] ,
+                     "pkt_counts": [3413, 3413, 3413, 3413, 3413, 2389, 2389, 2389, 1526, 1526, 1392, 415,
+                                    415, 415, 415, 42, 1],
                      "shared_limit_bytes": 46661760}
             res_2 = {"dscps": [3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3],
                      "pgs": [3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3],
