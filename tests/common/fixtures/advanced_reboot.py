32bbfb52507e94b5bb9cbedd4c47aac5f85c5131
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2023-04-02 23:48:40 -0700

    [advanced-reboot] Listen on peer dut during dualtor server->t1 traffic and fix loopback ping (#7532)

--- tests/common/fixtures/advanced_reboot.py
+++ tests/common/fixtures/advanced_reboot.py
@@ -15,6 +15,8 @@ from tests.common.helpers.sad_path import SadOperation
 from tests.ptf_runner import ptf_runner
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import InterruptableThread
+from tests.common.dualtor.data_plane_utils import get_peerhost
+from tests.common.dualtor.dual_tor_utils import show_muxcable_status
 
 logger = logging.getLogger(__name__)
 
@@ -93,6 +95,12 @@ class AdvancedReboot:
         self.lagMemberCnt = 0
         self.vlanMaxCnt = 0
         self.hostMaxCnt = HOST_MAX_COUNT
+        if "dualtor" in self.getTestbedType():
+            self.dual_tor_mode = True
+            peer_duthost = get_peerhost(duthosts, duthost)
+            self.peer_duthost = peer_duthost
+        else:
+            self.dual_tor_mode = False
 
         self.__buildTestbedData(tbinfo)
 
@@ -164,6 +172,8 @@ class AdvancedReboot:
         """
 
         self.mgFacts = self.duthost.get_extended_minigraph_facts(tbinfo)
+        if self.dual_tor_mode:
+            self.peer_mgFacts = self.peer_duthost.get_extended_minigraph_facts(tbinfo)
 
         self.rebootData['arista_vms'] = [
             attr['mgmt_addr'] for dev, attr in self.mgFacts['minigraph_devices'].items() if attr['hwsku'] == 'Arista-VM'
@@ -181,6 +191,7 @@ class AdvancedReboot:
         if vlan_table:
             vlan_name = list(vlan_table.keys())[0]
             vlan_mac = vlan_table[vlan_name].get('mac', self.rebootData['dut_mac'])
+
         self.rebootData['vlan_mac'] = vlan_mac
         self.rebootData['lo_prefix'] = "%s/%s" % (self.mgFacts['minigraph_lo_interfaces'][0]['addr'],
                                                   self.mgFacts['minigraph_lo_interfaces'][0]['prefixlen'])
@@ -570,7 +581,7 @@ class AdvancedReboot:
         return self.runRebootTest()
 
     def __setupRebootOper(self, rebootOper):
-        if "dualtor" in self.getTestbedType():
+        if self.dual_tor_mode:
             for device in self.duthosts:
                 device.shell("config mux mode manual all")
 
@@ -595,6 +606,13 @@ class AdvancedReboot:
             'neigh_port_info': copy.deepcopy(self.mgFacts['minigraph_neighbors']),
         }
 
+        if self.dual_tor_mode:
+            dualtor_testData = {
+                'peer_ports': copy.deepcopy(self.peer_mgFacts['minigraph_ptf_indices']),
+                'dut_mux_status': copy.deepcopy(show_muxcable_status(self.duthost)),
+            }
+            testData.update(dualtor_testData)
+
         if isinstance(rebootOper, SadOperation):
             logger.info('Running setup handler for reboot operation {}'.format(rebootOper))
             rebootOper.setup(testData)
@@ -615,7 +633,7 @@ class AdvancedReboot:
             rebootOper.verify()
 
     def __revertRebootOper(self, rebootOper):
-        if "dualtor" in self.getTestbedType():
+        if self.dual_tor_mode:
             for device in self.duthosts:
                 device.shell("config mux mode auto all")
 
@@ -662,6 +680,12 @@ class AdvancedReboot:
             "service_list": None if self.rebootType != 'service-warm-restart' else self.service_list,
             "service_data": None if self.rebootType != 'service-warm-restart' else self.service_data,
         }
+
+        if self.dual_tor_mode:
+            params.update({
+            "peer_ports_file": self.rebootData['peer_ports_file'],
+            "dut_mux_status": self.rebootData['dut_mux_status_file'],
+            })
 
         if not isinstance(rebootOper, SadOperation):
             # Non-routing neighbor/dut lag/bgp, vlan port up/down operation is performed before dut reboot process
@@ -777,7 +801,6 @@ class AdvancedReboot:
                 self.__restorePrevDockerImage()
         else:
             logger.info('Stay in new image')
-
 
 @pytest.fixture
 def get_advanced_reboot(request, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhost, localhost, tbinfo,
