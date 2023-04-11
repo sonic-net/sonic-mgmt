322ca65022f318f592f73d36aedb475d94953689
Sudharsan Dhamal Gopalarathnam <dgsudharsan@users.noreply.github.com>
2023-04-10 18:38:02 -0700

    [config reload]Modifying config reload tests to align with latest config reload enhancement (#7558)
    ### Approach
    #### What is the motivation for this PR?
    Modifying config reload tests to align with latest config reload enhancement according to HLD https://github.com/sonic-net/SONiC/pull/1203
    
    #### How did you do it?
    Removed the timer based checks
    
    #### How did you verify/test it?
    Run the tests with config reload enhamcenet changes
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/config_reload.py
+++ tests/common/config_reload.py
@@ -43,16 +43,6 @@ def config_system_checks_passed(duthost):
         if int(out['stdout'].strip()) < 120:
             return False
 
-    logging.info("Checking if delayed services are up")
-    out = duthost.shell("systemctl list-dependencies sonic-delayed.target --plain |sed '1d'")
-    status = duthost.shell("systemctl is-enabled {}".format(out['stdout'].replace("\n", " ")))
-    services = [line.strip() for line in out['stdout'].splitlines()]
-    state = [line.strip() for line in status['stdout'].splitlines()]
-    for service in services:
-        if state[services.index(service)] == "enabled":
-            out1 = duthost.shell("systemctl show {} --property=LastTriggerUSecMonotonic --value".format(service))
-            if out1['stdout'].strip() == "0":
-                return False
     logging.info("All checks passed")
     return True
 
