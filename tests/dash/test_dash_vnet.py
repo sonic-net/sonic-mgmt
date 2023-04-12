9ad5e6d654a80b558830ca4476e366ee17999d5f
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2023-04-12 10:30:38 +0800

    [pre-commit] Fix style issues in test scripts under `tests/dash`, `tests/decap`, `tests/drop_packets` folder(#8001)
    What is the motivation for this PR?
    pre-commit is a static analysis tool introduced recently. This tool is
    not able to do diff-only check. It checks the whole files touched by PR.
    We can't blame PR author for legacy issues. That's why currently the
    pre-commit check is only optional.
    
    To ensure that we can make pre-commit a mandatory check for PRs
    submitted to this repository, we need to fix all the legacy issues
    complained by pre-commit.
    
    How did you do it?
    This change fixes the style issues of test scripts under `tests/dash`,
    `tests/decap`, `tests/drop_packets` folder

--- tests/dash/test_dash_vnet.py
+++ tests/dash/test_dash_vnet.py
@@ -6,7 +6,9 @@ import ptf.packet as scapy
 from ptf.mask import Mask
 import ptf.testutils as testutils
 
-from constants import *
+from constants import VM_VNI, VNET2_VNI, REMOTE_CA_IP, LOCAL_CA_IP, REMOTE_ENI_MAC,\
+    LOCAL_ENI_MAC, LOOPBACK_IP, DUT_MAC, LOCAL_PA_IP, LOCAL_PTF_INTF, LOCAL_PTF_MAC,\
+    REMOTE_PA_IP, REMOTE_PTF_INTF, REMOTE_PTF_MAC
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +16,7 @@ pytestmark = [
     pytest.mark.topology('appliance'),
     pytest.mark.disable_loganalyzer
 ]
+
 
 @pytest.fixture
 def inbound_vnet_packets(dash_config_info):
@@ -55,6 +58,7 @@ def inbound_vnet_packets(dash_config_info):
 
     return inner_packet, pa_match_vxlan_packet, pa_mismatch_vxlan_packet, masked_exp_packet
 
+
 @pytest.fixture
 def outbound_vnet_packets(dash_config_info):
     inner_packet = testutils.simple_udp_packet(
@@ -91,6 +95,7 @@ def outbound_vnet_packets(dash_config_info):
     masked_exp_packet.set_do_not_care_scapy(scapy.UDP, "chksum")
     return inner_packet, vxlan_packet, masked_exp_packet
 
+
 def test_outbound_vnet(ptfadapter, apply_vnet_configs, outbound_vnet_packets, dash_config_info):
     """
     Send VXLAN packets from the VM VNI
@@ -98,7 +103,8 @@ def test_outbound_vnet(ptfadapter, apply_vnet_configs, outbound_vnet_packets, da
     _, vxlan_packet, expected_packet = outbound_vnet_packets
     testutils.send(ptfadapter, dash_config_info[LOCAL_PTF_INTF], vxlan_packet, 1)
     testutils.verify_packet(ptfadapter, expected_packet, dash_config_info[REMOTE_PTF_INTF])
-    
+
+
 def test_inbound_vnet_pa_validate(ptfadapter, apply_vnet_configs, inbound_vnet_packets, dash_config_info):
     """
     Send VXLAN packets from the remote VNI with PA validation enabled
