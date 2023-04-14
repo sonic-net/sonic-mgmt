b26808742ab4e882e86ac98e9041c8222584e3d7
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-04-14 12:12:38 -0700

    fix argument

--- tests/route/test_route_flap.py
+++ tests/route/test_route_flap.py
@@ -211,7 +211,7 @@ def test_route_flap(duthosts, tbinfo, ptfhost, ptfadapter,
     
     for route_prefix in iproute_info:
         if "/{}".format(route_prefix_len) in route_prefix:
-            has_ext_route = has_external_route(duthost, asichost, route_prefix)
+            has_ext_route = has_external_route(asichost, route_prefix)
             if not has_ext_route:
                 continue
             # multi-asics can have more than 1 routes in iproute_info[route_prefix], even single-asics have only 1
