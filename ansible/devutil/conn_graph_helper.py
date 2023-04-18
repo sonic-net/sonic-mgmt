ee67b78d1a7e49e88f5773a77572ee85f29a37a3
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2023-04-18 12:50:02 +0800

    [pre-commit] Fix style issues in test scripts under `ansible/c*` to (#8017)
    What is the motivation for this PR?
    pre-commit is a static analysis tool introduced recently. This tool is not able to do diff-only check. It checks the whole files touched by PR. We can't blame PR author for legacy issues. That's why currently the pre-commit check is only optional.
    
    To ensure that we can make pre-commit a mandatory check for PRs submitted to this repository, we need to fix all the legacy issues complained by pre-commit.
    
    How did you do it?
    This change fixes the style issues of test scripts under ansible/c* to ansible/h*` folder
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- ansible/devutil/conn_graph_helper.py
+++ ansible/devutil/conn_graph_helper.py
@@ -5,6 +5,7 @@ import imp
 
 CONN_GRAPH_LOG = "/tmp/conn_graph_debug.txt"
 
+
 def get_conn_graph_facts(hostnames):
     """
     @summary: Load conn_graph_facts from conn_graph_facts.xml
@@ -12,16 +13,20 @@ def get_conn_graph_facts(hostnames):
     @return: A dict, conn_graph_facts
     """
     filename = inspect.getframeinfo(inspect.currentframe()).filename
-    ansible_path = os.path.join(os.path.dirname(os.path.abspath(filename)), '../')
+    ansible_path = os.path.join(
+        os.path.dirname(os.path.abspath(filename)), '../')
     if ansible_path not in sys.path:
         sys.path.append(ansible_path)
 
-    utils = imp.load_source('conn_graph_utils', os.path.join(ansible_path, 'library/conn_graph_facts.py'))
-    utils.LAB_GRAPHFILE_PATH = os.path.join(ansible_path, utils.LAB_GRAPHFILE_PATH)
+    utils = imp.load_source('conn_graph_utils', os.path.join(
+        ansible_path, 'library/conn_graph_facts.py'))
+    utils.LAB_GRAPHFILE_PATH = os.path.join(
+        ansible_path, utils.LAB_GRAPHFILE_PATH)
     utils.debug_fname = CONN_GRAPH_LOG
 
     lab_graph = utils.find_graph(hostnames=hostnames, part=True)
-    succeed, results = utils.build_results(lab_graph=lab_graph, hostnames=hostnames, ignore_error=True)
+    succeed, results = utils.build_results(
+        lab_graph=lab_graph, hostnames=hostnames, ignore_error=True)
     if not succeed:
         print("Parse conn graph failes msg = {}".format(results))
         return {'device_pdu_info': {}, 'device_pdu_links': {}}
