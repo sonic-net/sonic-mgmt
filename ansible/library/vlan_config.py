87cd3764a5a4dda69f078b123ed836aab864b294
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2023-04-18 17:08:30 +0800

    [pre-commit] Fix style issues in test scripts under `ansilbe/l*` folder (#8033)
    * [pre-commit] Fix style issues in test scripts under `ansilbe/l*` folder
    
    What is the motivation for this PR?
    pre-commit is a static analysis tool introduced recently. This tool is
    not able to do diff-only check. It checks the whole files touched by PR.
    We can't blame PR author for legacy issues. That's why currently the
    pre-commit check is only optional.
    
    To ensure that we can make pre-commit a mandatory check for PRs
    submitted to this repository, we need to fix all the legacy issues
    complained by pre-commit.
    
    How did you do it?
    This change fixes the style issues of test scripts under `ansilbe/l*`
    folder

--- ansible/library/vlan_config.py
+++ ansible/library/vlan_config.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-import yaml
+from ansible.module_utils.basic import AnsibleModule
 import traceback
 
 DOCUMENTATION = '''
 module: vlan_config.py
 Ansible_version_added:  2.0.0.2
-short_description: Gather all vlan info related to servers' interfaces 
+short_description: Gather all vlan info related to servers' interfaces
 Description:
-       When deploy testbed topology with server connected to TOR SONiC, 
+       When deploy testbed topology with server connected to TOR SONiC,
        gather server vlan interfaces info for generating SONiC minigraph file
  arguments:
     vm_topo_config: Topology file; required: True
@@ -16,16 +16,17 @@ Description:
     vlan_config:  vlan config name to use; required: False
 
 Ansible_facts:
-    'vlan_configs': all Vlans Configuration 
+    'vlan_configs': all Vlans Configuration
 '''
 
 EXAMPLES = '''
     - name: find all vlan configurations for T0 topology
-      vlan_config: 
+      vlan_config:
         vm_topo_config: "{{ vm_topo_config }}"
         port_alias: "{{ port_alias }}"
         vlan_config: "{{ vlan_config | default(None) }}"
 '''
+
 
 def main():
     module = AnsibleModule(
@@ -47,13 +48,15 @@ def main():
 
         vlans = m_args['vm_topo_config']['DUT']['vlan_configs'][vlan_config]
         for vlan, vlan_param in vlans.items():
-            vlan_configs.update({vlan : {}})
+            vlan_configs.update({vlan: {}})
             vlan_configs[vlan]['id'] = vlan_param['id']
             vlan_configs[vlan]['tag'] = vlan_param['tag']
             vlan_configs[vlan]['prefix'] = vlan_param['prefix']
             vlan_configs[vlan]['prefix_v6'] = vlan_param['prefix_v6']
-            vlan_configs[vlan]['intfs'] = [port_alias[i] for i in vlan_param['intfs']]
-            vlan_configs[vlan]['portchannels'] = vlan_param.get('portchannels', [])
+            vlan_configs[vlan]['intfs'] = [port_alias[i]
+                                           for i in vlan_param['intfs']]
+            vlan_configs[vlan]['portchannels'] = vlan_param.get(
+                'portchannels', [])
 
             if 'mac' in vlan_param:
                 vlan_configs[vlan]['mac'] = vlan_param['mac']
@@ -61,11 +64,11 @@ def main():
             if 'type' in vlan_param:
                 vlan_configs[vlan]['type'] = vlan_param['type']
 
-    except Exception as e:
-        module.fail_json(msg = traceback.format_exc())
+    except Exception:
+        module.fail_json(msg=traceback.format_exc())
     else:
-        module.exit_json(ansible_facts={'vlan_configs' : vlan_configs})
+        module.exit_json(ansible_facts={'vlan_configs': vlan_configs})
 
-from ansible.module_utils.basic import *
+
 if __name__ == "__main__":
     main()
