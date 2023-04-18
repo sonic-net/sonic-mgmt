f34d62d23ea76615cd71133634b7819798423783
Kelly Chen <kelly_chen@edge-core.com>
2023-04-18 12:41:57 +0800

    Change topology mark for test_snmp_default_route and test_snmp_loopback (#7620)
    What is the motivation for this PR?
    How did you do it?
    -- test_snmp_default_route.py
    The original topology mark is 'any', but there is no default route in the 'ptf' topology.
    -- test_snmp_loopback.py
    Use "nbrhosts" module to get the VM information.
    But it will cause the error if the topology does not have VM(ex: ptf32).
    
    How did you verify/test it?
    Can skip two test cases on ptf32 topology.

--- tests/snmp/test_snmp_loopback.py
+++ tests/snmp/test_snmp_loopback.py
@@ -7,7 +7,7 @@ except ImportError:  # python2
     from pipes import quote
 
 pytestmark = [
-    pytest.mark.topology('any'),
+    pytest.mark.topology('t0', 't1', 't2', 'm0', 'mx'),
     pytest.mark.device_type('vs')
 ]
 
