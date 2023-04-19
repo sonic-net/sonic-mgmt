00c8973c863c68c5e974e27eec0103587d6eda14
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2023-04-19 08:09:16 +0800

    [pre-commit] Fix style issues in test scripts under `ansible/*` folder (#8064)
    What is the motivation for this PR?
    pre-commit is a static analysis tool introduced recently. This tool is not able to do diff-only check. It checks the whole files touched by PR. We can't blame PR author for legacy issues. That's why currently the pre-commit check is only optional.
    
    To ensure that we can make pre-commit a mandatory check for PRs submitted to this repository, we need to fix all the legacy issues complained by pre-commit.
    
    How did you do it?
    This change fixes the style issues of test scripts under ansible/* folder
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- ansible/terminal_plugins/aos.py
+++ ansible/terminal_plugins/aos.py
@@ -49,10 +49,12 @@ class TerminalModule(TerminalBase):
             self._exec_cli_command(to_bytes(json.dumps(cmd), errors='surrogate_or_strict'))
             prompt = self._get_prompt()
             if prompt is None or not prompt.endswith(b'#'):
-                raise AnsibleConnectionFailure('failed to elevate privilege to enable mode still at prompt [%s]' % prompt)
+                raise AnsibleConnectionFailure(
+                    'failed to elevate privilege to enable mode still at prompt [%s]' % prompt)
         except AnsibleConnectionFailure as e:
             prompt = self._get_prompt()
-            raise AnsibleConnectionFailure('unable to elevate privilege to enable mode, at prompt [%s] with error: %s' % (prompt, e.message))
+            raise AnsibleConnectionFailure(
+                'unable to elevate privilege to enable mode, at prompt [%s] with error: %s' % (prompt, e.message))
 
     def on_unbecome(self):
         prompt = self._get_prompt()
