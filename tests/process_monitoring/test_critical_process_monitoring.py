0307d15ddbdd2f2067b4347b45828ed289096336
Ye Jianquan <jianquanye@microsoft.com>
2023-04-20 11:34:28 +0800

    [test_critical_process_monitor]Prolong wait time to 90s (#8082)
    * [test_critical_process_monitor]Prolong wait time to 90s
    Summary:
    Fixes the flaky failure of test_critical_process_monitor:
    
    Expected Messages that are missing:
    .Process 'syncd' is not running in namespace 'host'.
    
    In the test module, it waits 70s for the process exit logs, but the syncd exit log sometimes arrives around 70-90s, then the test module fails.
    Prolong the wait time to 90s to fix the flaky issue
    
    details:
    
    On dut:
    admin@vlab-03:~$ date && docker exec syncd kill -SIGKILL 20
    Wed Apr 19 12:09:49 UTC 2023
    Syslog:
    Apr 19 12:10:15.473993 vlab-03 INFO syncd#supervisord 2023-04-19 12:10:15,472 INFO exited: syncd (terminated by SIGKILL; not expected)
    
    It spends 25s the supervisor detects syncd process exited, and 1 minute later, supervisor-proc-exit-listener print the expected log, 85s totally.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/process_monitoring/test_critical_process_monitoring.py
+++ tests/process_monitoring/test_critical_process_monitoring.py
@@ -586,9 +586,9 @@ def test_monitoring_critical_processes(duthosts, rand_one_dut_hostname, tbinfo, 
 
     stop_critical_processes(duthost, containers_in_namespaces)
 
-    # Wait for 70 seconds such that Supervisord/Monit has a chance to write alerting message into syslog.
-    logger.info("Sleep 70 seconds to wait for the alerting messages in syslog...")
-    time.sleep(70)
+    # Wait for 90 seconds such that Supervisord/Monit has a chance to write alerting message into syslog.
+    logger.info("Sleep 90 seconds to wait for the alerting messages in syslog...")
+    time.sleep(90)
 
     logger.info("Checking the alerting messages from syslog...")
     loganalyzer.analyze(marker)
