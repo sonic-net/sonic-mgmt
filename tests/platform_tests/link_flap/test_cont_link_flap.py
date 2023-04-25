de4ec102bb6e8d022b2b0d2194fc8ef040ecb184
ShiyanWangMS <shiyanwang@microsoft.com>
2023-04-25 13:59:31 +0800

    Fix test failure in test_cont_link_flap.py (#8128)
    Fixes test failure in test_cont_link_flap.py caused by #7968
    
    What is the motivation for this PR?
    Test failed with error:
    File "/azp/_work/6/s/tests/platform_tests/link_flap/test_cont_link_flap.py", line 145, in test_cont_link_flap
    incr_redis_memory = float(end_time_redis_memory) - float(start_time_redis_memory)
    ValueError: could not convert string to float: '\1'
    
    How did you do it?
    Correct reg express.
    
    How did you verify/test it?
    Manually run test case: test_cont_link_flap.py
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/platform_tests/link_flap/test_cont_link_flap.py
+++ tests/platform_tests/link_flap/test_cont_link_flap.py
@@ -54,7 +54,7 @@ class TestContLinkFlap(object):
 
         # Record Redis Memory at start
         start_time_redis_memory = duthost.shell(
-            r"redis-cli info memory | grep used_memory_human | sed -e 's/.*:\(.*\)M/\\1/'")["stdout"]
+            r"redis-cli info memory | grep used_memory_human | sed -e 's/.*:\(.*\)M/\1/'")["stdout"]
         logging.info("Redis Memory: %s M", start_time_redis_memory)
 
         # Record ipv4 route counts at start
@@ -137,7 +137,7 @@ class TestContLinkFlap(object):
 
         # Record Redis Memory at end
         end_time_redis_memory = duthost.shell(
-            r"redis-cli info memory | grep used_memory_human | sed -e 's/.*:\(.*\)M/\\1/'")["stdout"]
+            r"redis-cli info memory | grep used_memory_human | sed -e 's/.*:\(.*\)M/\1/'")["stdout"]
         logging.info("Redis Memory at start: %s M", start_time_redis_memory)
         logging.info("Redis Memory at end: %s M", end_time_redis_memory)
 
