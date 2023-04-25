3439a0b8c5ac355bfb6dcc9bae3cef17cf8fbd07
sharonl <slutati@nvidia.com>
2023-04-25 09:52:36 +0300

    Update advanced-reboot ptf test to support 70K packets

--- ansible/roles/test/files/ptftests/advanced-reboot.py
+++ ansible/roles/test/files/ptftests/advanced-reboot.py
@@ -207,7 +207,7 @@ class ReloadTest(BaseTest):
         #   Inter-packet interval, to be used in send_in_background method.
         #   Improve this interval to gain more precision of disruptions.
         self.send_interval = 0.0035
-        self.packets_to_send = min(int(self.time_to_listen / (self.send_interval + 0.0015)), 45000) # How many packets to be sent in send_in_background method
+        self.packets_to_send = min(int(self.time_to_listen / (self.send_interval + 0.0015)), 70000) # How many packets to be sent in send_in_background method
 
         # Thread pool for background watching operations
         self.pool = ThreadPool(processes=3)
