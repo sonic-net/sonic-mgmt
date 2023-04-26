f650593cd97dd354c3a7bcb93dd050faa9584400
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2023-04-26 17:24:56 +0800

    Add UT for flush_unused_database (#8032)
    **What I did**
    Add new UT for flush_unused_database scripts.
    
    **Why I did it**
    The flush_unused_database script not cover by UT.
    
    **How I verified it**
    Pass all UT.
    
    **Details if related**
    This UT depends on PR: https://github.com/sonic-net/sonic-buildimage/pull/14632
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -173,7 +173,8 @@ test_t0() {
       process_monitoring/test_critical_process_monitoring.py \
       show_techsupport/test_techsupport_no_secret.py \
       system_health/test_system_status.py \
-      radv/test_radv_ipv6_ra.py"
+      radv/test_radv_ipv6_ra.py \
+      database/test_db_scripts.py"
 
       pushd $SONIC_MGMT_DIR/tests
       ./run_tests.sh $RUNTEST_CLI_COMMON_OPTS -c "$tests" -p logs/$tgname
