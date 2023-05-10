2276705cccf8b9c5626de522c9f29bb2e940b1fb
Ying Xie <ying.xie@microsoft.com>
2023-05-10 06:34:51 +0000

    [add-topo] add option to disble updategraph service during testbed deployment
    If SONiC image was built with ENABLE_DHCP_GRAPH_SERVICE=y option, the image
    will boot up waiting for configuraion to be pushed down from DHCP. When
    starting such image, waiting for swss to become active would timeout and
    thus fail the testbed deployment.
    
    Adding option to disable updategraph service during testbed deployment.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- ansible/roles/vm_set/library/sonic_kickstart.py
+++ ansible/roles/vm_set/library/sonic_kickstart.py
@@ -84,9 +84,20 @@ class SerialSession(object):
 
 
 def session(new_params):
-    seq = [
-        ('while true; do if [ $(systemctl is-active swss) == "active" ]; then break; fi; '
-         'echo $(systemctl is-active swss); sleep 1; done', [r'#'], 180),
+    if new_params['disable_updategraph']:
+        seq = [
+            ('while true; do if [ $(systemctl is-active swss) == "active" ]; then break; fi; '
+             'echo $(systemctl is-active swss); '
+             'sed -i -e "s/enabled=true/enabled=false/" /etc/sonic/updategraph.conf; '
+             'systemctl restart updategraph; sleep 1; done', [r'#'], 180),
+        ]
+    else:
+        seq = [
+            ('while true; do if [ $(systemctl is-active swss) == "active" ]; then break; fi; '
+             'echo $(systemctl is-active swss); sleep 1; done', [r'#'], 180),
+        ]
+
+    seq.extend([
         ('pkill dhclient', [r'#']),
         ('hostname %s' % str(new_params['hostname']), [r'#']),
         ('sed -i s:sonic:%s: /etc/hosts' %
@@ -98,7 +109,7 @@ def session(new_params):
         ('ip route', [r'#']),
         ('echo %s:%s | chpasswd' %
          (str(new_params['login']), str(new_params['new_password'])), [r'#']),
-    ]
+    ])
     # For multi-asic VS there is no default config generated.
     # interfaces-config service will not add eth0 IP address as there
     # no default config. Multiple SWSS service will not start until
@@ -125,14 +136,15 @@ def core(module):
 def main():
 
     module = AnsibleModule(argument_spec=dict(
-        telnet_port=dict(required=True),
-        login=dict(required=True),
-        passwords=dict(required=True, type='list'),
-        hostname=dict(required=True),
-        mgmt_ip=dict(required=True),
-        mgmt_gw=dict(required=True),
-        new_password=dict(required=True),
-        num_asic=dict(required=True),
+        telnet_port = dict(required=True),
+        login = dict(required=True),
+        passwords = dict(required=True, type='list'),
+        hostname = dict(required=True),
+        mgmt_ip = dict(required=True),
+        mgmt_gw = dict(required=True),
+        new_password = dict(required=True),
+        num_asic = dict(required=True),
+        disable_updategraph = dict(required=True, type='bool'),
     ))
 
     try:
