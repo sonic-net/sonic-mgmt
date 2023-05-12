a3ba817f05882e2a9d9333256278853b2523c5c2
Raghavendran Ramanathan <rraghav@cisco.com>
2023-05-11 17:12:17 -0700

    Update to use topo-any for single asic tests only for cisco-8000.

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -875,7 +875,12 @@ class QosSaiBase(QosBase):
 
         dutTopo = "topo-"
 
-        if dutTopo + topo in qosConfigs['qos_params'].get(dutAsic, {}):
+        if dutAsic == "gb" and topo == "t2":
+            if get_src_dst_asic_and_duts['src_asic'] == get_src_dst_asic_and_duts['dst_asic']:
+                dutTopo = dutTopo + "any"
+            else:
+                dutTopo = dutTopo + topo
+        elif dutTopo + topo in qosConfigs['qos_params'].get(dutAsic, {}):
             dutTopo = dutTopo + topo
         else:
             # Default topo is any
