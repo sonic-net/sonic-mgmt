b9f739072a3c32008f15d8b7d6916023bf124a04
Hantien <han_tien@edge-core.com>
2023-05-15 15:40:26 +0800

    Add new test item to test mtu change and boundary.
    What is the motivation for this PR?
    Add tests for MTU change and boundary to improve test coverage.
    
    How did you do it?
    These tests simulate different MTU values and verify if the system can handle the change and boundary correctly.
    
    Supported testbed topology if it's a new test case?
    T1 and T2 topology is supported.

--- tests/ipfwd/test_mtu.py
+++ tests/ipfwd/test_mtu.py
@@ -1,9 +1,8 @@
 import pytest
-import time
 import logging
 
-from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # lgtm[py/unused-import]
-from tests.common.fixtures.ptfhost_utils import set_ptf_port_mapping_mode
+from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory     # noqa F401
+from tests.common.fixtures.ptfhost_utils import set_ptf_port_mapping_mode   # noqa F401
 from tests.ptf_runner import ptf_runner
 from datetime import datetime
 
@@ -34,8 +33,8 @@ def setup_port_mtu(duthost, get_dut_port_name_list, gather_facts):
     for port in gather_facts['src_port_ids']:
         duthost.shell('config interface mtu {} {}'.format(DUT_PORT_NAME_LIST[port],dut_def_mtu))
 
-@pytest.mark.parametrize("mtu", [1514,9114])
-def test_mtu(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhost, mtu, gather_facts):
+@pytest.mark.parametrize("mtu", [1514, 9114])
+def test_mtu(tbinfo, ptfhost, mtu, gather_facts):
     """
     Before the test starts, it is given that the mtu of the ports used in the test is set as
     default value.(L3 MTU=9100, L2 MTU=9014)
@@ -44,14 +43,7 @@ def test_mtu(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhos
     All of the values listed in parametrized arg 'mtu' must be smaller or equal to the default mtu
     size.
     """
-    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-
-    topo_type = tbinfo['topo']['type']
-    if topo_type not in ('t1', 't2'):
-        pytest.skip("Unsupported topology")
-
     testbed_type = tbinfo['topo']['name']
-    router_mac = duthost.shell('sonic-cfggen -d -v \'DEVICE_METADATA.localhost.mac\'')["stdout_lines"][0].decode("utf-8")
 
     log_file = "/tmp/mtu_test.{}-{}.log".format(mtu,datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
 
@@ -64,7 +56,7 @@ def test_mtu(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhos
                "mtu_test.MtuTest",
                platform_dir="ptftests",
                params={"testbed_type": testbed_type,
-                       "router_mac": router_mac,
+                       "router_mac": gather_facts['src_router_mac'],
                        "testbed_mtu": mtu,
                        "src_host_ip": gather_facts['src_host_ipv4'],
                        "src_router_ip": gather_facts['src_router_ipv4'],
@@ -80,7 +72,7 @@ def test_mtu(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhos
                socket_recv_size=16384)
 
 @pytest.mark.parametrize("mtu", [1514,1515,9114])
-def test_mtu_change(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhost, mtu, gather_facts):
+def test_mtu_change(tbinfo,duthost, ptfhost, mtu, gather_facts):
     """
     The test set the l2 mtu of the ports used in the test is set as 1514.
     Invoke ptf test 'mtu_test.MtuTest' to inject packets with size 'mtu'(l2 mtu) which is set as
@@ -89,15 +81,8 @@ def test_mtu_change(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
     that the ptf test resut is falied. For the values listed in parametrized arg 'mtu' that are less
     than or equal to 1514, it is expected the ptf test is passed.
     """
-    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-
-    topo_type = tbinfo['topo']['type']
-    if topo_type not in ('t1', 't2'):
-        pytest.skip("Unsupported topology")
 
     testbed_type = tbinfo['topo']['name']
-    router_mac = duthost.shell('sonic-cfggen -d -v \'DEVICE_METADATA.localhost.mac\'')["stdout_lines"][0].decode("utf-8")
-
     log_file = "/tmp/mtu_change_test.{}-{}.log".format(mtu,datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
 
     logging.info("Starting MTU test. PTF log file: %s" % log_file)
@@ -118,7 +103,7 @@ def test_mtu_change(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
                "mtu_test.MtuTest",
                platform_dir="ptftests",
                params={"testbed_type": testbed_type,
-                       "router_mac": router_mac,
+                       "router_mac": gather_facts['src_router_mac'],
                        "testbed_mtu": mtu,
                        "src_host_ip": gather_facts['src_host_ipv4'],
                        "src_router_ip": gather_facts['src_router_ipv4'],
@@ -135,7 +120,7 @@ def test_mtu_change(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
 
 
 @pytest.mark.parametrize("mtu_boundary, boundary_type", [(1500, "lower"), (9216,"upper")])
-def test_mtu_boundary(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhost, gather_facts, mtu_boundary, boundary_type):
+def test_mtu_boundary(tbinfo, duthost, ptfhost, gather_facts, mtu_boundary, boundary_type):
     """
         Set the value mtu_boundary as mtu on the ports used in this test.
         Invoke ptf test 'mtu_test.MtuTest' to inject packets with size
@@ -150,14 +135,7 @@ def test_mtu_boundary(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostnam
             It is expected that all of the executed commands to set mtu are failed on all of the ports
             used in this test.
     """
-    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-
-    topo_type = tbinfo['topo']['type']
-    if topo_type not in ('t1', 't2'):
-        pytest.skip("Unsupported topology")
-
     testbed_type = tbinfo['topo']['name']
-    router_mac = duthost.shell('sonic-cfggen -d -v \'DEVICE_METADATA.localhost.mac\'')["stdout_lines"][0].decode("utf-8")
 
     log_file = "/tmp/mtu_change_test.{}-{}.log".format(mtu_boundary,datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
 
@@ -184,7 +162,7 @@ def test_mtu_boundary(tbinfo, duthosts, enum_rand_one_per_hwsku_frontend_hostnam
                "mtu_test.MtuTest",
                platform_dir="ptftests",
                params={"testbed_type": testbed_type,
-                       "router_mac": router_mac,
+                       "router_mac": gather_facts['src_router_mac'],
                        "testbed_mtu": mtu_boundary,
                        "src_host_ip": gather_facts['src_host_ipv4'],
                        "src_router_ip": gather_facts['src_router_ipv4'],
