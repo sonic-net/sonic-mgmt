5220eb2964637ece1ae17a86599e73c73a436cb6
liuh-80 <liuh@microsoft.com>
2023-05-15 02:22:00 +0000

    Fix code format

--- tests/system_health/test_watchdog.py
+++ tests/system_health/test_watchdog.py
@@ -1,3 +1,4 @@
+import logging
 import pytest
 import time
 from tests.common.helpers.assertions import pytest_assert
@@ -11,10 +12,13 @@ logger = logging.getLogger(__name__)
 
 SLEEP_TIME = 10
 
+
 @pytest.fixture
 def pause_orchagent(duthost):
     # find orchagent pid
-    pid = duthost.shell(r"ps -ef | grep orchagent | grep -v grep | awk '{print $2}'", module_ignore_errors=True)['stdout']
+    pid = duthost.shell(
+                    r"ps -ef | grep orchagent | grep -v grep | awk '{print $2}'",
+                    module_ignore_errors=True)['stdout']
     logger.info('Get orchagent pid: {}'.format(pid))
     
     # pause orchagent and clear syslog
@@ -31,24 +35,28 @@ def test_orchagent_watchdog(duthosts, enum_rand_one_per_hwsku_hostname, pause_or
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
 
     result = duthost.shell(
-                        r"docker exec -i swss sh -c 'test -f /usr/bin/supervisor-proc-watchdog-listener && echo exist'"
-                        , module_ignore_errors=True)['stdout']
+                        r"docker exec -i swss sh -c 'test -f /usr/bin/supervisor-proc-watchdog-listener && echo exist'",
+                        module_ignore_errors=True)['stdout']
     logger.info('Check watchdog exist: {}'.format(result))
     if result != 'exist':
         pytest.skip("Skip orchagent watchdog test.")
-    
+
     # wait watchdog emit alert
     WATCHDOG_TIMEOUT = 120
     current_attempt = 0
     while (True):
         time.sleep(SLEEP_TIME)
-        alert = duthost.shell(r"sudo cat /var/log/syslog | grep 'is stuck in namespace'", module_ignore_errors=True)['stdout']
+        alert = duthost.shell(
+                            r"sudo cat /var/log/syslog | grep 'is stuck in namespace'",
+                            module_ignore_errors=True)['stdout']
         logger.info('Get alert from host: {}'.format(alert))
         if "orchagent" in str(alert):
             return
         else:
             # orchagent watchdog timeout is 60 seconds
             if current_attempt >= WATCHDOG_TIMEOUT/SLEEP_TIME:
-                pytest_assert(False, "orchagent watchdog did not been trigger after {} seconds".format(WATCHDOG_TIMEOUT))
+                pytest_assert(
+                            False,
+                            "orchagent watchdog did not been trigger after {} seconds".format(WATCHDOG_TIMEOUT))
             else:
                 current_attempt += 1
