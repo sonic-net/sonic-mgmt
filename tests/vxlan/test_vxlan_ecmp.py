ca3b8f0934d2a2fa01dd0719e916833db77ee15d
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2023-05-17 14:48:52 -0700

    Enable test_vxlan_ecmp on 4600c platform (#8339)
    * Enable test_vxlan_ecmp on 4600c platform
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/vxlan/test_vxlan_ecmp.py
+++ tests/vxlan/test_vxlan_ecmp.py
@@ -146,7 +146,7 @@ def fixture_setUp(duthosts,
 
     data = {}
     asic_type = duthosts[rand_one_dut_hostname].facts["asic_type"]
-    if asic_type == "cisco-8000":
+    if asic_type in ["cisco-8000", "mellanox"]:
         data['tolerance'] = 0.03
     else:
         raise RuntimeError("Pls update this script for your platform.")
