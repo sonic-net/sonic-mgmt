1a80aa478f254074f97db21f91a8a68c4c2e4696
ganglyu <ganglv@microsoft.com>
2023-05-18 14:53:58 +0800

    Fix precommit error

--- tests/common/helpers/portchannel_to_vlan.py
+++ tests/common/helpers/portchannel_to_vlan.py
@@ -386,9 +386,8 @@ def setup_po2vlan(duthosts, ptfhost, rand_one_dut_hostname, rand_selected_dut, p
         create_checkpoint(duthost, SETUP_ENV_CP)
         dut_lag_map, ptf_lag_map, src_vlan_id = setup_dut_ptf(ptfhost, duthost, tbinfo, vlan_intfs_dict)
 
-        vlan_ports_list = 
-            running_vlan_ports_list(duthosts, rand_one_dut_hostname, rand_selected_dut, tbinfo, ports_list)
-        populate_fdb(ptfadapter, vlan_ports_list, vlan_intfs_dict)
+        vp_list = running_vlan_ports_list(duthosts, rand_one_dut_hostname, rand_selected_dut, tbinfo, ports_list)
+        populate_fdb(ptfadapter, vp_list, vlan_intfs_dict)
         bind_acl_table(duthost, tbinfo)
         apply_acl_rules(duthost, tbinfo)
     # --------------------- Testing -----------------------
