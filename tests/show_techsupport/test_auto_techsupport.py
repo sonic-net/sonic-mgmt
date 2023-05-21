55826be771aa3a672e39c4012ad21d9f135573d1
ppikh <70200079+ppikh@users.noreply.github.com>
2023-05-21 09:47:48 +0300

    Changed logic which doing counting for free/used space to use 1Mb = 1000Kb (#8086)
    Originally we used logic which expected 1Mb = 1024Kb, but seems like it's wrong and we need to use 1Mb = 1000Kb
    
    Auto-techsupport used shutil python lib to get used/free space
    
    If we use on the same host "shutil" and "df" we will see next output:
    >>> import shutil
    >>> shutil.disk_usage('/')
    usage(total=11491549184, used=5826863104, free=5057351680)
    
    df linux utils shows next output:
    [petrop@mtr-vdi-077 sonic-mgmt]$ df /
    Filesystem              1K-blocks    Used Available Use% Mounted on
    /dev/mapper/centos-root  11222216 5690296   4938820  54% /
    
    Test used "df" in case when get total space.
    If use 1Mb = 1024 Kb - in result we will have: 10 959 Mb
    If use 1Mb = 1000 Kb - in result we will have: 11 222 Mb
    
    shutil.disk_usage returned 11491549184 which is close to 'df' output divided by 1000.
    
    Fixed auto-techsupport test case - issue "dd: invalid number:... "
    
    When run cmd "dd" with args(for example): sudo dd if=/dev/zero of=/var/core/bash.1679904321.1060.core.gz bs=1M count=1427.2072
    It will fail with error: "dd: invalid number: ‘1427.2072’"
    Int values only supported by dd.
    Changed test logic to use int values instead of float in dd command arguments
    
    Signed-off-by: Petro Pikh <petrop@nvidia.com>

--- tests/show_techsupport/test_auto_techsupport.py
+++ tests/show_techsupport/test_auto_techsupport.py
@@ -32,6 +32,7 @@ DEFAULT_MAX_TECHSUPPORT_LIMIT = 10
 DEFAULT_MAX_CORE_LIMIT = 5
 DEFAULT_SINCE = '2 days ago'
 
+KB_SIZE = 1000  # We use 1000 to have the same value as in shutil.disk_usage() method which used in SONiC code
 CMD_GET_AUTO_TECH_SUPPORT_HISTORY_REDIS_KEYS = 'sudo redis-cli --raw -n 6  KEYS AUTO_TECHSUPPORT*'
 TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
 SAI_CALL_TEMPLATE_FILE_PATH = os.path.join(TEMPLATES_DIR, 'sai_call_fail_config.j2')
@@ -1056,11 +1057,10 @@ def get_partition_usage_info(duthost, partition='/'):
     """
     with allure.step('Getting HDD partition {} usage'.format(partition)):
         output = duthost.shell('sudo df {}'.format(partition))['stdout_lines']
-        kb = 1024
         _, total, used, avail, used_percent, _ = output[-1].split()
-        total_mb = int(total) / kb
-        used_mb = int(used) / kb
-        avail_mb = int(avail) / kb
+        total_mb = int(total) / KB_SIZE
+        used_mb = int(used) / KB_SIZE
+        avail_mb = int(avail) / KB_SIZE
         used_percent = int(used_percent.strip('%'))
 
     return total_mb, used_mb, avail_mb, used_percent
@@ -1078,8 +1078,7 @@ def get_used_space(duthost, path_to_file_folder):
         directory_usage_line = -1
         memory_usage_line = 0
         used_by_folder = du_output[directory_usage_line].split()[memory_usage_line]
-        kb = 1024
-        used_by_folder_mb = int(used_by_folder) / kb
+        used_by_folder_mb = int(used_by_folder) / KB_SIZE
 
     return used_by_folder_mb
 
@@ -1127,7 +1126,7 @@ def create_stub_file(duthost, path_to_file, size_in_mb):
     :param path_to_file: full path to file which should be created
     :param size_in_mb: size of file in mb
     """
-    cmd = 'sudo dd if=/dev/zero of={} bs=1M count={}'.format(path_to_file, size_in_mb)
+    cmd = 'sudo dd if=/dev/zero of={} bs=1M count={}'.format(path_to_file, int(size_in_mb))
     duthost.shell(cmd)
 
 
