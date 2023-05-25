4af7fe25f9a8591b2dd8d266fb1191986d87ce45
abdosi <58047199+abdosi@users.noreply.github.com>
2023-05-24 17:22:53 -0700

    Fix the  ptf_test_port_map.json for chassis/multi-asic (#8384)
    What I did:
    
    Fix the ptf_test_port_map_active_active and ptf_test_port_map to get correct ptf_port to asic_idx mapping by using only the Front panel ports (ignoring all backplane and special ports)

--- tests/common/fixtures/ptfhost_utils.py
+++ tests/common/fixtures/ptfhost_utils.py
@@ -468,18 +468,24 @@ def ptf_test_port_map(ptfhost, tbinfo, duthosts, mux_server_url, duts_running_co
             target_dut_index = int(list(dut_intf_map.keys())[0])
             target_dut_port = int(list(dut_intf_map.values())[0])
             router_mac = router_macs[target_dut_index]
+            dut_port = None
             if len(duts_minigraph_facts[duthosts[target_dut_index].hostname]) > 1:
                 for list_idx, mg_facts_tuple in enumerate(duts_minigraph_facts[duthosts[target_dut_index].hostname]):
                     idx, mg_facts = mg_facts_tuple
-                    if target_dut_port in list(mg_facts['minigraph_port_indices'].values()):
-                        router_mac = duts_running_config_facts[duthosts[target_dut_index].hostname][list_idx][1]\
-                        ['DEVICE_METADATA']['localhost']['mac'].lower()
-                        asic_idx = idx
-                        break
+                    for a_dut_port, a_dut_port_index in list(mg_facts['minigraph_port_indices'].items()):
+                        if a_dut_port_index == target_dut_port and "Ethernet-Rec" not in a_dut_port and \
+                           "Ethernet-IB" not in a_dut_port and "Ethernet-BP" not in a_dut_port:
+                            dut_port = a_dut_port
+                            router_mac = \
+                                duts_running_config_facts[duthosts[target_dut_index].hostname][list_idx][1][
+                                         'DEVICE_METADATA']['localhost']['mac'].lower()
+                            asic_idx = idx
+                            break
             ports_map[ptf_port] = {
                 'target_dut': target_dut_index,
                 'target_dest_mac': router_mac,
                 'target_src_mac': router_mac,
+                'dut_port': dut_port,
                 'asic_idx': asic_idx
             }
 
@@ -557,11 +563,15 @@ def ptf_test_port_map_active_active(ptfhost, tbinfo, duthosts, mux_server_url, d
                 # Dealing with multi-asic target dut
                 for list_idx, mg_facts_tuple in enumerate(duts_minigraph_facts[target_hostname]):
                     idx, mg_facts = mg_facts_tuple
-                    if target_dut_port in list(mg_facts['minigraph_port_indices'].values()):
-                        router_mac = duts_running_config_facts[target_hostname][list_idx][1]\
-                        ['DEVICE_METADATA']['localhost']['mac'].lower()
-                        asic_idx = idx
-
+                    for a_dut_port, a_dut_port_index in list(mg_facts['minigraph_port_indices'].items()):
+                        if a_dut_port_index == target_dut_port and "Ethernet-Rec" not in a_dut_port and \
+                           "Ethernet-IB" not in a_dut_port and "Ethernet-BP" not in a_dut_port:
+                            dut_port = a_dut_port
+                            router_mac = \
+                                duts_running_config_facts[duthosts[target_dut_index].hostname][list_idx][1][
+                                         'DEVICE_METADATA']['localhost']['mac'].lower()
+                            asic_idx = idx
+                            break
             ports_map[ptf_port] = {
                 'target_dut': [target_dut_index],
                 'target_dest_mac': router_mac,
