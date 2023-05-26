cc9e25b06564d49de91973909a81b1a43e5f9f62
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-05-26 10:37:50 -0700

    Update multi_asic.py

--- tests/common/devices/multi_asic.py
+++ tests/common/devices/multi_asic.py
@@ -458,9 +458,6 @@ class MultiAsicSonicHost(object):
                     services.append(service_name)
 
         for docker in services:
-            # TODO: https://github.com/sonic-net/sonic-mgmt/issues/5970
-            if self.sonichost.is_multi_asic and docker == "gbsyncd":
-                continue
             cmd_disable_rate_limit = (
                 r"docker exec -i {} sed -i "
                 r"'s/^\$SystemLogRateLimit/#\$SystemLogRateLimit/g' "
