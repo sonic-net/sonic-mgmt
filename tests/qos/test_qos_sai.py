2390140a981451e3136ebbf012650bb2446969f4
Randall Pittman <randallbpittman@gmail.com>
2023-05-30 13:45:28 +0000

    Cisco only.

--- tests/qos/test_qos_sai.py
+++ tests/qos/test_qos_sai.py
@@ -1616,8 +1616,9 @@ class TestQosSai(QosSaiBase):
             "src_port_ip": dutConfig["testPorts"]["src_port_ip"],
         })
 
-        src_port_name = dutConfig["dutInterfaces"][testParams["src_port_id"]]
-        testParams['dscp_to_pg_map'] = load_dscp_to_pg_map(duthost, src_port_name, dut_qos_maps)
+        if dutTestParams["basicParams"]["sonic_asic_type"] == 'cisco-8000':
+            src_port_name = dutConfig["dutInterfaces"][testParams["src_port_id"]]
+            testParams['dscp_to_pg_map'] = load_dscp_to_pg_map(duthost, src_port_name, dut_qos_maps)
 
         if "platform_asic" in dutTestParams["basicParams"]:
             testParams["platform_asic"] = dutTestParams["basicParams"]["platform_asic"]
