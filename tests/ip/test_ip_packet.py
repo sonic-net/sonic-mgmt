9d985d9918392987890a087f876438ecc4c59a8d
sanjair-git <114024719+sanjair-git@users.noreply.github.com>
2023-05-30 02:32:39 -0400

    Fix for sonic-mgmt issue #7954  - test_ip_packet.py (#8414)

--- tests/ip/test_ip_packet.py
+++ tests/ip/test_ip_packet.py
@@ -302,7 +302,8 @@ class TestIPPacket(object):
         pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
         pytest_assert(match_cnt >= self.PKT_NUM_MIN, "DUT forwarded {} packets, but {} packets matched expected format, not in expected range".format(tx_ok, match_cnt))
 
-    def test_forward_ip_packet_with_0xffff_chksum_drop(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
+    def test_forward_ip_packet_with_0xffff_chksum_drop(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
+                                                       ptfadapter, common_param, tbinfo):
         # GIVEN a ip packet with checksum 0x0000(compute from scratch)
         # WHEN manually set checksum as 0xffff and send the packet to DUT
         # THEN DUT should drop packet with 0xffff and add drop count
@@ -356,11 +357,24 @@ class TestIPPacket(object):
         tx_drp = TestIPPacket.sum_ifaces_counts(portstat_out, out_ifaces, "tx_drp")
         tx_err = TestIPPacket.sum_ifaces_counts(rif_counter_out, out_rif_ifaces, "tx_err") if rif_support else 0
 
-        pytest_assert(rx_ok >= self.PKT_NUM_MIN, "Received {} packets in rx, not in expected range".format(rx_ok))
-        pytest_assert(max(rx_drp, rx_err) >= self.PKT_NUM_MIN, "Dropped {} packets in rx, not in expected range".format(rx_err))
-        pytest_assert(tx_ok <= self.PKT_NUM_ZERO, "Forwarded {} packets in tx, not in expected range".format(tx_ok))
-        pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO, "Dropped {} packets in tx, not in expected range".format(tx_err))
-        pytest_assert(match_cnt == 0, "DUT shouldn't forward packets, but forwarded {} packets, not in expected range".format(match_cnt))
+        # For t2 max topology, increase the tolerance value from 0.1 to 0.2
+        # Set the tolerance value to 0.2 if the topology is T2 max, PKT_NUM_ZERO would be set to 200
+        vms_num = len(tbinfo['topo']['properties']['topology']['VMs'])
+        if tbinfo['topo']['type'] == "t2" and vms_num > 8:
+            logger.info("Setting PKT_NUM_ZERO for t2 max topology with 0.2 tolerance")
+            self.PKT_NUM_ZERO = self.PKT_NUM * 0.2
+
+        pytest_assert(rx_ok >= self.PKT_NUM_MIN,
+                      "Received {} packets in rx, not in expected range".format(rx_ok))
+        pytest_assert(max(rx_drp, rx_err) >= self.PKT_NUM_MIN,
+                      "Dropped {} packets in rx, not in expected range".format(rx_err))
+        pytest_assert(tx_ok <= self.PKT_NUM_ZERO,
+                      "Forwarded {} packets in tx, not in expected range".format(tx_ok))
+        pytest_assert(max(tx_drp, tx_err) <= self.PKT_NUM_ZERO,
+                      "Dropped {} packets in tx, not in expected range".format(tx_err))
+        pytest_assert(match_cnt == 0,
+                      "DUT shouldn't forward packets, but forwarded {} packets, not in expected range"
+                      .format(match_cnt))
 
     def test_forward_ip_packet_recomputed_0xffff_chksum(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfadapter, common_param):
         # GIVEN a ip packet, after forwarded(ttl-1) by DUT,
