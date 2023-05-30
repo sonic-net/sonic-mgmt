c76417440cbd4bae260f8101f2e13297b6ccb336
cmf-ragilenetworks <121847352+cmf-ragilenetworks@users.noreply.github.com>
2023-05-30 17:24:03 +0800

    Update test_telemetry.py
    Update test_telemetry.py so that it can test the Dut whose panel port serial number starts from 1 instead of 0

--- tests/telemetry/test_telemetry.py
+++ tests/telemetry/test_telemetry.py
@@ -84,8 +84,12 @@ def test_telemetry_ouput(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost,
             "Skipping test as no Ethernet0 frontpanel port on supervisor")
     logger.info('start telemetry output testing')
     dut_ip = duthost.mgmt_ip
-    cmd = 'python ' + gnxi_path + 'gnmi_cli_py/py_gnmicli.py -g -t {0} -p {1} -m get -x COUNTERS/Ethernet0 -xt COUNTERS_DB \
-           -o "ndastreamingservertest"'.format(dut_ip, TELEMETRY_PORT)
+    cfg_facts = duthost.get_running_config_facts()
+    up_ports = [p for p, v in cfg_facts['PORT'].items() if v.get('admin_status', None) == 'up']
+    v_port = sorted(up_ports)[0]
+    logging.warning("v_port={}".format(v_port))
+    cmd = 'python ' + gnxi_path + 'gnmi_cli_py/py_gnmicli.py -g -t {0} -p {1} -m get -x COUNTERS/{2} -xt COUNTERS_DB \
+           -o "ndastreamingservertest"'.format(dut_ip, TELEMETRY_PORT, v_port)
     show_gnmi_out = ptfhost.shell(cmd)['stdout']
     logger.info("GNMI Server output")
     logger.info(show_gnmi_out)
@@ -171,6 +175,7 @@ def test_virtualdb_table_streaming(duthosts, enum_rand_one_per_hwsku_hostname, p
     skip_201911_and_older(duthost)
     cmd = generate_client_cli(
         duthost=duthost, gnxi_path=gnxi_path, method=METHOD_SUBSCRIBE, update_count=3)
+    logging.warning("cmd={}".format(cmd))
     show_gnmi_out = ptfhost.shell(cmd)['stdout']
     result = str(show_gnmi_out)
 
