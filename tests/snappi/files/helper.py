73d377da7119a31bbadaaf6778bb2833b977e6b3
Dev Ojha <47282568+developfast@users.noreply.github.com>
2023-05-30 17:02:27 -0700

    [Snappi] Skip warm and fast reboot RDMA test cases for Cisco devices (#8415)
    Summary: Cisco devices do not support warm or fast reboot operations. If such an operation is performed, it leaves the switch in a bad state, resulting in failures. This change skips all cases with warm and fast reboots for cisco devices.
    
    What is the motivation for this PR?
    Lots of failures related to ARP were seen with warm and fast reboot RDMA cases in cisco switches. Cisco switches currently do not support warm and fast reboots, so these failures are expected. Hence, these test cases will be skipped if it is a cisco switch,
    
    How did you do it?
    Check device's asic type and skip the appropriate cases.
    
    How did you verify/test it?
    Test cases are skipped on lab switch.
    
    Any platform specific information?
    Cisco-8000

--- tests/snappi/files/helper.py
+++ tests/snappi/files/helper.py
@@ -1,10 +1,11 @@
 from tests.common.broadcom_data import is_broadcom_device
 from tests.common.helpers.assertions import pytest_require
+from tests.common.cisco_data import is_cisco_device
 
 
 def skip_warm_reboot(duthost, reboot_type):
     """
-    Skip warm reboot tests for TD2 asics
+    Skip warm/fast reboot tests for TD2 asics and Cisco devices
 
     Args:
         duthost (pytest fixture): device under test
@@ -15,5 +16,10 @@ def skip_warm_reboot(duthost, reboot_type):
     """
     SKIP_LIST = ["td2"]
     asic_type = duthost.get_asic_name()
-    pytest_require(not (is_broadcom_device(duthost) and asic_type in SKIP_LIST and "warm" in reboot_type),
-                   "Warm reboot is not supported on {}".format(asic_type))
+    reboot_case_supported = True
+    if (reboot_type == "warm" or reboot_type == "fast") and is_cisco_device(duthost):
+        reboot_case_supported = False
+    elif is_broadcom_device(duthost) and asic_type in SKIP_LIST and "warm" in reboot_type:
+        reboot_case_supported = False
+    pytest_require(reboot_case_supported, "Reboot type {} is not supported on {} switches".
+                   format(reboot_type, duthost.facts['asic_type']))
