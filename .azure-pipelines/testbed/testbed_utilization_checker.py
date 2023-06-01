10f8b51b47f207e88a3d5a80cb9669c8b051e35a
Xiawei Jiang <xiaweijiang@microsoft.com>
2023-06-01 08:24:30 +0000

    Merged PR 2789: Poll testbed utilization date from elastic testbed
    Elastic testbed is synced with tbshare now, and tbshare will be redirect to elastic testbed soon, so we need to poll testbed utilization date from elastic testbed

--- .azure-pipelines/testbed/testbed_utilization_checker.py
+++ .azure-pipelines/testbed/testbed_utilization_checker.py
@@ -8,6 +8,12 @@ import logging
 from datetime import datetime
 from collections import defaultdict
 
+_self_dir = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.realpath(os.path.join(_self_dir, ".."))
+if base_path not in sys.path:
+    sys.path.append(base_path)
+from nightly.templates.lock_release import get_token_from_testbedV2
+
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
@@ -25,25 +31,6 @@ INVENTORY_POOL = ["../../ansible/str",
                   "../../ansible/str3", 
                   "../../ansible/strsvc", 
                   "../../ansible/bjw"]
-
-
-def get_token(client_id, client_secret, proxies):
-    token_url = 'https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token'
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    payload = {
-        'resource': 'https://tbshare.azurewebsites.net',
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret
-    }
-    try:
-        resp = requests.post(token_url, headers=headers, data=payload, proxies=proxies, timeout=10).json()
-        return resp['access_token']
-    except Exception as e:
-        print('Get token failed with exception: {}'.format(repr(e)))
-    return None
 
 
 def get_testbeds_list():
@@ -89,8 +76,8 @@ def get_testbed_hwsku(testbed_utilization):
     return testbed_utilization
 
 
-def get_testbed_status(testbed, token, proxies):
-    url = 'https://tbshare.azurewebsites.net/api/testbed/{}'.format(testbed)
+def get_tbshare_resp(token, proxies):
+    url = 'https://sonic-elastictest-prod-management-webapp.azurewebsites.net/api/v1/testbeds/query_by_keyword?keyword=&testbed_type=PHYSICAL&page=1&page_size=2000'
     headers = {
         'Authorization': 'Bearer ' + token
     }
@@ -110,54 +97,27 @@ def get_testbed_status(testbed, token, proxies):
     return result
 
 
-def parse_testbed_status_result(testbed_status):
-    """
-    Parse poll results of tbshare.
-    The data looks like this:
-    {
-        "testbed-bjw-can-7215-1": {
-            "failed": false,
-            "testbed": {
-                "topo": "m0",
-                "lock_time": "2023-03-07T01:42:42.040050+00:00",
-                "lock_reason": "NightlyTest",
-                "Timestamp": "Tue, 07 Mar 2023 01:42:42 GMT",
-                "duts": "bjw-can-7215-1",
-                "locked_by": "testbed-bjw-can-7215-1.202205_229718",
-                "absolute_lock": true,
-                "nightly": false,
-                "PartitionKey": "switch",
-                "etag": "W/\"datetime'2023-03-07T01%3A42%3A42.1181954Z'\"",
-                "RowKey": "testbed-bjw-can-7215-1",
-                "release_time": "2023-03-08T13:42:42.040050+00:00"
-            }
-        }
-    }
-    """
+def parse_testbed_status_result(tbshare_resp, skip_testbeds):
     testbed_utilization = []
-    for testbed_name in testbed_status:
+    if "data" not in tbshare_resp:
+        return testbed_utilization
+    upload_info = {}
+    for testbed_info in tbshare_resp["data"]:
         upload_info = {}
-        upload_info["TestbedName"] = testbed_name
-        if "testbed" in testbed_status[testbed_name]:
-            testbed_info = testbed_status[testbed_name]["testbed"]
-        else:
-            testbed_info = {}
-        if not testbed_status[testbed_name]["failed"]:
-            upload_info["DutName"] = testbed_info["duts"]
-            if testbed_info["locked_by"]:
+        if testbed_info["name"] not in skip_testbeds:
+            upload_info["TestbedName"] = testbed_info["name"]
+            duts = []
+            for dut in testbed_info["dut"]:
+                duts.append(dut)
+            upload_info["DutName"] = ";".join(duts)
+            if testbed_info["locked_by"] is not None:
                 upload_info["LockedBy"] = testbed_info["locked_by"]
                 if testbed_info["lock_reason"]:
                     upload_info["LockReason"] = testbed_info["lock_reason"]
                 upload_info["LockTime"] = testbed_info["lock_time"]
                 upload_info["ReleaseTime"] = testbed_info["release_time"]
-        else:
-            if "duts" in testbed_info:
-                upload_info["DutName"] = testbed_info["duts"]
-            else:
-                upload_info["DutName"] = ""
-            upload_info["LockedBy"] = "unknown"
-        upload_info["Timestamp"] = testbed_status[testbed_name]["timestamp"]
-        testbed_utilization.append(upload_info)
+            upload_info["Timestamp"] = str(datetime.utcnow())
+            testbed_utilization.append(upload_info)
 
     testbed_utilization = get_testbed_hwsku(testbed_utilization)
     return testbed_utilization
@@ -172,26 +132,16 @@ if __name__ == '__main__':
     if skip_testbeds or skip_testbeds != '':
         skip_testbeds = skip_testbeds.split(',')
 
-    client_id = os.environ.get('TBSHARE_AAD_CLIENT_ID')
-    client_secret = os.environ.get('TBSHARE_AAD_CLIENT_SECRET')
-
-    if not client_id or not client_secret:
-        print('Need environment variables: TBSHARE_AAD_CLIENT_ID, TBSHARE_AAD_CLIENT_SECRET')
-        sys.exit(1)
-
     proxies = {
         'http': os.environ.get('http_proxy'),
         'https': os.environ.get('http_proxy')
     }
 
-    token = get_token(client_id, client_secret, proxies)
+    token = get_token_from_testbedV2()
+    tbshare_resp = defaultdict(list)
+    tbshare_resp = get_tbshare_resp(token, proxies)
+    testbed_utilization = parse_testbed_status_result(tbshare_resp, skip_testbeds)
 
-    testbed_list = get_testbeds_list()
-    testbed_status = defaultdict(list)
-    for testbed in testbed_list:
-        if testbed not in skip_testbeds:
-            testbed_status[testbed] = get_testbed_status(testbed, token, proxies)
-    testbed_utilization = parse_testbed_status_result(testbed_status)
-
-    with open("testbed_utilization.json", "w") as f:
-        json.dump(testbed_utilization, f)
+    if len(testbed_utilization) > 0:
+        with open("testbed_utilization.json", "w") as f:
+            json.dump(testbed_utilization, f)
