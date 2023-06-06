a09e2eb1c3bb95833f17b9f81c6c1ec0b6770b7c
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-06-06 13:24:40 -0700

    Update utilities.py

--- tests/common/utilities.py
+++ tests/common/utilities.py
@@ -9,7 +9,6 @@ import json
 import logging
 import os
 import re
-import random
 import six
 import sys
 import threading
@@ -76,19 +75,6 @@ def skip_release_for_platform(duthost, release_list, platform_list):
             any(platform in duthost.facts['platform'] for platform in platform_list):
         pytest.skip("DUT has version {} and platform {} and test does not support {} for {}".format(
                     duthost.os_version, duthost.facts['platform'], ", ".join(release_list), ", ".join(platform_list)))
-
-
-def get_sup_node_or_random_node(duthosts):
-    # accomodate for T2 chassis, which only SUP has pdu info
-    # try to find sup node in multi-dut
-    for dut in duthosts:
-        if dut.is_supervisor_node():
-            return dut
-    # if not chassis, it's dualtor or single-dut, return random node or itself
-    if len(duthosts) > 1:
-        duthosts = random.sample(duthosts, 1)
-    logger.info("Randomly select dut {} for testing".format(duthosts[0]))
-    return duthosts[0]
 
 
 def wait(seconds, msg=""):
