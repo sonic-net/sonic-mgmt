8c466e0efd3e5d60ff4c4f58af4122545d56ef04
Soumya Velamala <87676006+svelamal@users.noreply.github.com>
2023-06-14 11:34:52 -0700

    Update test_ecn_config_update.py

--- tests/generic_config_updater/test_ecn_config_update.py
+++ tests/generic_config_updater/test_ecn_config_update.py
@@ -54,7 +54,8 @@ def ensure_application_of_updated_config(duthost, configdb_field, values):
         values: expected value(s) of configdb_field
     """
     def _confirm_value_in_asic_db():
-        table_name = duthost.shell('sonic-db-cli ASIC_DB keys *WRED*')["stdout"]
+        table_name = [duthost.shell('sonic-db-cli ASIC_DB keys *WRED*')["stdout"]][0]
+        table_name = table_name.split("\n")[0]
         wred_data = duthost.shell('sonic-db-cli ASIC_DB hgetall {}'.format(table_name))["stdout"]
         wred_data = ast.literal_eval(wred_data)
         for field, value in zip(configdb_field.split(','), values.split(',')):
