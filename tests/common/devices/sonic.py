b52e3bf9e34d44f3d4265ccc6d82f3eb1a0ef77a
Cong Hou <97947969+congh-nvidia@users.noreply.github.com>
2023-06-22 00:05:17 +0800

    [Mellanox] Update dscp remapping cases for Nvidia platforms (#8317)
    This PR contains the fix for two test cases in DSCP remapping test: test_tunnel_decap_dscp_to_pg_mapping and test_xoff_for_pcbb.
    The reason that we need this fix:
    
    Nvidia's implementation for DSCP remapping is different from Community. We need some additional mapping and the tunnel mode is pipe. For details please check the PR: [Mellanox] Support DSCP remapping in dual ToR topo on T0 switch sonic-buildimage#12605.
    Nvidia's cell size is 144.
    Need to stop packet aging in buffer.
    Need to add qos parameters for pcbb xoff test case.

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -1663,6 +1663,8 @@ Totals               6450                 6449
             asic = "th3"
         elif "Cisco Systems Inc Device a001" in output:
             asic = "gb"
+        elif "Mellanox Technologies" in output:
+            asic = "spc"
 
         return asic
 
