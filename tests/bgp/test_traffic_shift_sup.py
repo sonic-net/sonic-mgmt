419defaa94281164970b2cbc620db6c85506275d
Tejaswini Chadaga <tchadaga@microsoft.com>
2023-06-23 12:22:19 -0700

    whitespace fixes

--- tests/bgp/test_traffic_shift_sup.py
+++ tests/bgp/test_traffic_shift_sup.py
@@ -2,7 +2,6 @@ import logging
 import pexpect
 import pytest
 import time
-import pkgutil
 from tests.common.helpers.assertions import pytest_assert
 from tests.common import config_reload
 from test_traffic_shift import get_traffic_shift_state
@@ -22,6 +21,14 @@ TS_NO_NEIGHBORS = "System Mode: No external neighbors"
 This test file is specific to T2 chassis topology
 It tests TSA/B functionality from supervisor
 """
+
+
+@pytest.fixture(scope="module")
+def check_support(duthosts, enum_supervisor_dut_hostname):
+    duthost = duthosts[enum_supervisor_dut_hostname]
+    rcli_path = duthost.shell("python3 -c \"import pkgutil ; print(pkgutil.find_loader('rcli'))\"")['stdout']
+    if str(rcli_path) == "None":
+        pytest.skip("rcli package not installed. TSA/B/C from supervisor is not supported in this image")
 
 
 class TestTrafficShiftOnSup:
@@ -47,7 +54,7 @@ class TestTrafficShiftOnSup:
     def run_cmd_on_sup(self, cmd):
         try:
             # Issue TSA on DUT
-            client = pexpect.spawn("ssh {}@{}".format(self.dutuser, self.dutip))
+            client = pexpect.spawn("ssh {}@{}".format(self.dutuser, self.dutip), timeout=300)
             sel = client.expect(["The authenticity .*", "admin@{}'s password:".format(self.dutip)])
             if sel == 0:
                 client.sendline("yes")
@@ -65,14 +72,11 @@ class TestTrafficShiftOnSup:
         except Exception as e:
             logger.error("Exception caught while executing cmd {}. Error message: {}".format(cmd, e))
 
-    def test_TSA(self, duthosts, enum_supervisor_dut_hostname, creds):
+    def test_TSA(self, duthosts, enum_supervisor_dut_hostname, check_support, creds):
         """
         Test TSA
         Verify all linecards transition to maintenance state after TSA on supervisor
         """
-        if pkgutil.find_loader('rcli') is None:
-            pytest.skip("rcli package not installed. TSA from supervisor is not supported in this image")
-
         self.setup_dutinfo(duthosts, enum_supervisor_dut_hostname, creds)
         try:
             # Issue TSA on DUT
@@ -86,14 +90,11 @@ class TestTrafficShiftOnSup:
             # Issue TSB on DUT to recover the chassis
             self.run_cmd_on_sup("sudo TSB")
 
-    def test_TSB(self, duthosts, enum_supervisor_dut_hostname, creds):
+    def test_TSB(self, duthosts, enum_supervisor_dut_hostname, check_support, creds):
         """
         Test TSB
         Verify all linecards transition back to normal state from maintenance after TSB on supervisor
         """
-        if pkgutil.find_loader('rcli') is None:
-            pytest.skip("rcli package not installed. TSA from supervisor is not supported in this image")
-
         self.setup_dutinfo(duthosts, enum_supervisor_dut_hostname, creds)
         try:
             # Issue TSA on DUT to move chassis to maintenance
@@ -109,15 +110,12 @@ class TestTrafficShiftOnSup:
             logger.error("Exception caught in TSB test. Error message: {}".format(e))
 
     @pytest.mark.disable_loganalyzer
-    def test_TSA_TSB_chassis_with_config_reload(self, duthosts, enum_supervisor_dut_hostname, creds):
+    def test_TSA_TSB_chassis_with_config_reload(self, duthosts, enum_supervisor_dut_hostname, check_support, creds):
         """
         Test TSA/TSB with config reload
         Verify all linecards remain in Maintenance state after TSA and config reload on supervisor
         Verify all linecards remain in Normal state after TSB and config reload on supervisor
         """
-        if pkgutil.find_loader('rcli') is None:
-            pytest.skip("rcli package not installed. TSA from supervisor is not supported in this image")
-
         self.setup_dutinfo(duthosts, enum_supervisor_dut_hostname, creds)
         try:
             # Issue TSA on DUT to move chassis to maintenance
