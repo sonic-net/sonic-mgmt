16de018cec23195bb539895ace76e8be881a2d47
sanjair-git <114024719+sanjair-git@users.noreply.github.com>
2023-06-23 13:52:52 -0400

    Fix for PID assertion issue - PSUD and CHASSID platform cases (#7781)
    Fix for PID assertion issue - PSUD and CHASSID platform cases
    Fixing up other chassisd and psud tests to poll for deamon to restart with bigger pid than before
    ---------
    
    Co-authored-by: sanmalho <sandeep.malhotra@nokia.com>

--- tests/platform_tests/daemon/test_chassisd.py
+++ tests/platform_tests/daemon/test_chassisd.py
@@ -66,7 +66,7 @@ def check_daemon_status(duthosts, enum_rand_one_per_hwsku_hostname):
 
 
 def check_expected_daemon_status(duthost, expected_daemon_status):
-    daemon_status, _ = duthost.get_pmon_daemon_status(daemon_name)
+    daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
     return daemon_status == expected_daemon_status
 
 
@@ -157,6 +157,7 @@ def test_pmon_chassisd_stop_and_start_status(check_daemon_status, duthosts,
 
     duthost.start_pmon_daemon(daemon_name)
 
+    wait_until(120, 10, 0, check_if_daemon_restarted, duthost, daemon_name, pre_daemon_pid)
     wait_until(50, 25, 0, check_expected_daemon_status, duthost, expected_running_status)
 
     post_daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(daemon_name)
@@ -215,6 +216,7 @@ def test_pmon_chassisd_kill_and_start_status(check_daemon_status, duthosts,
 
     duthost.stop_pmon_daemon(daemon_name, SIG_KILL, pre_daemon_pid)
 
+    wait_until(120, 10, 0, check_if_daemon_restarted, duthost, daemon_name, pre_daemon_pid)
     wait_until(120, 10, 0, check_expected_daemon_status, duthost, expected_running_status)
 
     post_daemon_status, post_daemon_pid = duthost.get_pmon_daemon_status(
