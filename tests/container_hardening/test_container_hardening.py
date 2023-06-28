8cf6217a0e136fc1a8dc8465c2395ade9f6d0514
Mai Bui <maibui@microsoft.com>
2023-06-28 21:45:14 +0000

    address comments
    Signed-off-by: Mai Bui <maibui@microsoft.com>

--- tests/container_hardening/test_container_hardening.py
+++ tests/container_hardening/test_container_hardening.py
@@ -13,6 +13,11 @@ def test_bgp_dev(duthost):
     """
     Test bgp container has no access to /dev/vda* or /dev/sda*
     """
-    device = duthost.shell("docker exec bgp bash -c 'df -h | grep /etc/hosts' | awk '{print $1}'")['stdout']
+    cmd = duthost.shell("docker exec bgp bash -c 'df -h | grep /etc/hosts' | awk '{print $1}'")
+    rc, device = cmd['rc'], cmd['stdout']
+    if rc != 0:
+        pytest_assert(False, 'Failed to get the device name.')
+    if not device.startswith('/dev/'):
+        pytest_assert(False, 'Invalid device {}.'.format(device))
     output = duthost.shell("docker exec bgp bash -c 'ls {}'".format(device), module_ignore_errors=True)['stdout']
     pytest_assert(not output, 'The partition {} exists.'.format(device))
