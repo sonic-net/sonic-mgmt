ad7f696047fad0dc010e2b7b73d02615e86a7471
Alon Navarro <alonn@nvidia.com>
2023-06-29 15:54:51 +0300

    fix conftest

--- tests/clock/conftest.py
+++ tests/clock/conftest.py
@@ -16,10 +16,11 @@ def ntp_server(request):
     @summary: Return NTP server's ip if given, otherwise skip the test
     """
     ntp_server_ip = request.config.getoption("ntp_server")
-    logging.info(f'NTP server ip from execution parameter: {ntp_server_ip}')
     if ntp_server_ip is None:
         logging.info('IP of NTP server was not given. Skipping the test')
         pytest.skip("IP of NTP server was not given. Skipping the test")
+    else:
+        logging.info(f'NTP server ip from execution parameter: {ntp_server_ip}')
     return ntp_server_ip
 
 
