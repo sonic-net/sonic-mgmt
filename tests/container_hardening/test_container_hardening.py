5ba3fa6b53b4c6491252864ae4a70849fe5418d8
Mai Bui <maibui@microsoft.com>
2023-07-05 17:22:18 +0000

    make test generic
    Signed-off-by: Mai Bui <maibui@microsoft.com>

--- tests/container_hardening/test_container_hardening.py
+++ tests/container_hardening/test_container_hardening.py
@@ -9,15 +9,17 @@ pytestmark = [
 logger = logging.getLogger(__name__)
 
 
-def test_bgp_dev(duthost):
+def test_container_privileged(duthost):
     """
-    Test bgp container has no access to /dev/vda* or /dev/sda*
+    Test container without --privileged flag has no access to /dev/vda* or /dev/sda*
     """
-    cmd = duthost.shell("docker exec bgp bash -c 'df -h | grep /etc/hosts' | awk '{print $1}'")
-    rc, device = cmd['rc'], cmd['stdout']
-    if rc != 0:
-        pytest_assert(False, 'Failed to get the device name.')
-    if not device.startswith('/dev/'):
-        pytest_assert(False, 'Invalid device {}.'.format(device))
-    output = duthost.shell("docker exec bgp bash -c 'ls {}'".format(device), module_ignore_errors=True)['stdout']
-    pytest_assert(not output, 'The partition {} exists.'.format(device))
+    container_names = duthost.shell(r'docker ps -a --format \{\{.Names\}\}')['stdout_lines']
+    for container_name in container_names:
+        if container_name == 'bgp':
+            docker_exec_cmd = 'docker exec {} bash -c '.format(container_name)
+            cmd = duthost.shell(docker_exec_cmd + "'df -h | grep /etc/hosts' | awk '{print $1}'")
+            rc, device = cmd['rc'], cmd['stdout']
+            pytest_assert(rc == 0, 'Failed to get the device name.')
+            pytest_assert(device.startswith('/dev/'), 'Invalid device {}.'.format(device))
+            output = duthost.shell(docker_exec_cmd + "'ls {}'".format(device), module_ignore_errors=True)['stdout']
+            pytest_assert(not output, 'The partition {} exists.'.format(device))
