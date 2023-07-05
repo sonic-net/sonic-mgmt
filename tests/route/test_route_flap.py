23be9b411a1900677da6b6074f21b0c169005f58
Veronica Cojocaru <vcojocaru@arista.com>
2023-07-05 12:10:03 -0700

    Skip over routes for the IB interface when selecting the route to ping. This was the previous selection behavior before the recent addition of filtering for external nexthops.

--- tests/route/test_route_flap.py
+++ tests/route/test_route_flap.py
@@ -27,7 +27,7 @@ LOOP_TIMES_LEVEL_MAP = {
     'diagnose': 50
 }
 
-WAIT_EXPECTED_PACKET_TIMEOUT = 5
+WAIT_EXPECTED_PACKET_TIMEOUT = 15
 EXABGP_BASE_PORT = 5000
 NHIPV4 = '10.10.246.254'
 WITHDRAW = 'withdraw'
@@ -230,11 +230,15 @@ def get_dev_port_and_route(duthost, asichost, dst_prefix_set):
                         continue
                     if per_hop['ip'] in internal_bgp_ips:
                         continue
+                    if 'IB' in per_hop['interfaceName']:
+                        continue
                     dev_port = per_hop['interfaceName']
         else:
             dev = json.loads(asichost.run_vtysh(cmd)['stdout'])
             for per_hop in dev[route_to_ping][0]['nexthops']:
                 if 'interfaceName' not in per_hop.keys():
+                    continue
+                if 'IB' in per_hop['interfaceName']:
                     continue
                 dev_port = per_hop['interfaceName']
     pytest_assert(dev_port, "dev_port not exist")
