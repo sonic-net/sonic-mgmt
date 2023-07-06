868b0b6dd9597dee08029be8685cc90134711545
Shiyan <shiyanwang@microsoft.com>
2023-07-06 16:35:41 +0800

    First commit

--- ansible/roles/vm_set/files/mux_simulator.py
+++ ansible/roles/vm_set/files/mux_simulator.py
@@ -809,6 +809,22 @@ def reset_flow_handler(vm_set):
     return g_muxes.reset_flows()
 
 
+@app.route('/mux/<vm_set>/output', methods=['POST'])
+def output_flow_handler(vm_set):
+    _validate_vm_set(vm_set)
+    data = _validate_out_sides(request)
+    app.logger.info('===== {} POST {} with {} ====='.format(request.remote_addr, request.url, json.dumps(data)))
+    return g_muxes.update_flows('output', data['out_sides'])
+
+
+@app.route('/mux/<vm_set>/drop', methods=['POST'])
+def drop_flow_handler(vm_set):
+    _validate_vm_set(vm_set)
+    data = _validate_out_sides(request)
+    app.logger.info('===== {} POST {} with {} ====='.format(request.remote_addr, request.url, json.dumps(data)))
+    return g_muxes.update_flows('drop', data['out_sides'])
+
+
 @app.route('/mux/<vm_set>/<int:port_index>/flap_counter', methods=['GET'])
 def flap_counter_port(vm_set, port_index):
     """
