e86c4444a31a0818997bfeb6fbbf863610ea5a6b
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-07-10 15:57:12 -0700

    Update test_power_off_reboot.py

--- tests/platform_tests/test_power_off_reboot.py
+++ tests/platform_tests/test_power_off_reboot.py
@@ -2,7 +2,7 @@ import logging
 import pytest
 import time
 
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts     # noqa F401
+import tests.common.fixtures.conn_graph_facts
 from tests.common.reboot import wait_for_startup, REBOOT_TYPE_POWEROFF
 from tests.common.platform.processes_utils import wait_critical_processes, check_critical_processes
 from tests.common.helpers.assertions import pytest_assert
