4530c13a30ccc111e5e72fe637a709684f7a9cf9
SuvarnaMeenakshi <50386592+SuvarnaMeenakshi@users.noreply.github.com>
2023-07-10 11:47:55 -0700

    Use device loopback IP address to send SNMP query from neighboring ceos or vsonic (#8802)
    What is the motivation for this PR?
    sonic-net/sonic-buildimage#15487 modifies snmpd in SONiC to listen on management and loopback ip by default instead of listening on any IP.
    test_interop_protocol.py::test_snmp sends a query to the neighbor device using the link ip address.
    After the above change to listen on management and loopback ip, the snmp query will fail.
    Hence, modifying the test to send SNMP query to SONiC DUT from neighboring vsonic/eos using Loopback IP of SONiC DUT.
    
    How did you do it?
    Get loopback IPv4 address DUT and use that to query from neighbor device.
    Added a generic helped function to send SNMP query to DUT from a neighbor.
    
    (cherry picked from commit d66965a96dc6d80c2074a845e8f4132ca6b50659)

--- tests/macsec/test_interop_protocol.py
+++ tests/macsec/test_interop_protocol.py
@@ -1,13 +1,12 @@
 from time import sleep
 import pytest
 import logging
-import re
 import scapy.all as scapy
 import ptf.testutils as testutils
 from collections import Counter
+import ipaddress
 
 from tests.common.utilities import wait_until
-from tests.common.devices.eos import EosHost
 from tests.common import config_reload
 from macsec_helper import *
 from macsec_config_helper import *
@@ -121,24 +120,28 @@ class TestInteropProtocol():
             assert wait_until(BGP_TIMEOUT, BGP_KEEPALIVE, BGP_HOLDTIME,
                               check_bgp_established, ctrl_port, upstream_links[ctrl_port])
 
-    def test_snmp(self, duthost, ctrl_links, upstream_links, creds, wait_mka_establish):
+    def test_snmp(self, duthost, ctrl_links, upstream_links, creds_all_duts, wait_mka_establish):
         '''
         Verify SNMP request/response works across interface with macsec configuration
         '''
         if duthost.is_multi_asic:
             pytest.skip("The test is for Single ASIC devices")
 
-        for ctrl_port, nbr in ctrl_links.items():
-            if isinstance(nbr["host"], EosHost):
-                result = nbr["host"].eos_command(
-                    commands=['show snmp community | include name'])
-                community = re.search(r'Community name: (\S+)',
-                                      result['stdout'][0]).groups()[0]
-            else:  # vsonic neighbour
-                community = creds['snmp_rocommunity']
-
-            up_link = upstream_links[ctrl_port]
+        loopback0_ips = duthost.config_facts(host=duthost.hostname,
+                                             source="running")[
+                                             "ansible_facts"].get(
+                                             "LOOPBACK_INTERFACE",
+                                             {}).get('Loopback0', {})
+        for ip in loopback0_ips:
+            if isinstance(ipaddress.ip_network(ip),
+                          ipaddress.IPv4Network):
+                dut_loip = ip.split('/')[0]
+                break
+        else:
+            pytest.fail("No Loopback0 IPv4 address for {}".
+                        format(duthost.hostname))
+        for ctrl_port, nbr in list(ctrl_links.items()):
             sysDescr = ".1.3.6.1.2.1.1.1.0"
-            command = "docker exec snmp snmpwalk -v 2c -c {} {} {}".format(
-                community, up_link["local_ipv4_addr"], sysDescr)
-            assert not duthost.command(command)["failed"]
+            result = get_snmp_output(dut_loip, duthost, nbr,
+                                     creds_all_duts, sysDescr)
+            assert not result["failed"]
