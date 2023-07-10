2893a6026319eab0c8808159a2446e5596ecf6e2
ganglyu <ganglv@microsoft.com>
2023-07-10 01:25:22 +0000

    Revert "Use GNMI API for DASH test"
    This reverts commit dcca5858ae19efdd459a3b9686e366ccaaa5f9f8.

--- tests/dash/conftest.py
+++ tests/dash/conftest.py
@@ -7,11 +7,8 @@ from constants import ENI, VM_VNI, VNET1_VNI, VNET2_VNI, REMOTE_CA_IP, LOCAL_CA_
     REMOTE_PA_IP, REMOTE_PTF_INTF, REMOTE_PTF_MAC, REMOTE_PA_PREFIX, VNET1_NAME, VNET2_NAME, ROUTING_ACTION, \
     ROUTING_ACTION_TYPE, LOOKUP_OVERLAY_IP
 from dash_utils import render_template_to_host, apply_swssconfig_file
-from gnmi_utils import generate_gnmi_cert, apply_gnmi_cert, recover_gnmi_cert, apply_gnmi_file
 
 logger = logging.getLogger(__name__)
-
-ENABLE_GNMI_API = False
 
 
 def pytest_addoption(parser):
@@ -129,7 +126,7 @@ def dash_config_info(duthost, config_facts, minigraph_facts):
 
 
 @pytest.fixture(scope="function")
-def apply_config(duthost, localhost, skip_config, skip_cleanup):
+def apply_config(duthost, skip_config, skip_cleanup):
     configs = []
     op = "SET"
 
@@ -143,10 +140,7 @@ def apply_config(duthost, localhost, skip_config, skip_cleanup):
         template_name = "{}.j2".format(config)
         dest_path = "/tmp/{}.json".format(config)
         render_template_to_host(template_name, duthost, dest_path, config_info, op=op)
-        if ENABLE_GNMI_API == True:
-            apply_gnmi_file(duthost, localhost, dest_path)
-        else:
-            apply_swssconfig_file(duthost, dest_path)
+        apply_swssconfig_file(duthost, dest_path)
 
     yield _apply_config
 
@@ -208,17 +202,3 @@ def apply_direct_configs(dash_outbound_configs, apply_config):
     del dash_outbound_configs[VNET2_NAME]
 
     apply_config(dash_outbound_configs)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def setup_gnmi_server(duthosts, rand_one_dut_hostname, localhost):
-    if ENABLE_GNMI_API == False:
-        yield
-        return
-
-    duthost = duthosts[rand_one_dut_hostname]
-    duthost.shell("docker exec gnmi rm /usr/local/yang-models/sonic-dash.yang", module_ignore_errors=True)
-    generate_gnmi_cert(localhost, duthost)
-    apply_gnmi_cert(duthost)
-    yield
-    recover_gnmi_cert(duthost)
