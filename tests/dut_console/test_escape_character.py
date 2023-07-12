7e6d72c806d013fe7a76a8e90490710a007e21ee
sanjair-git <sanjai.rajendran@nokia.com>
2023-07-12 12:48:50 -0400

    Fix precommit errors

--- tests/dut_console/test_escape_character.py
+++ tests/dut_console/test_escape_character.py
@@ -13,6 +13,6 @@ pytestmark = [
 
 def test_console_escape(duthost_console):
     duthost_console.send_command("ping 127.0.0.1 -c {} -i 1".format(TOTAL_PACKETS),
-                                               expect_string=r"icmp_seq={}".format(packet_number))
+                                 expect_string=r"icmp_seq={}".format(packet_number))
     duthost_console.send_command("\x03",
                                  expect_string=r"{} packets transmitted".format(packet_number), max_loops=300)
