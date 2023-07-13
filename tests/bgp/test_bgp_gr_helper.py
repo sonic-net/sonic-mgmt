fb997d80cad7f4cd28d968efdf9efa07c29db4b6
jcaiMR <jcai@microsoft.com>
2023-07-13 06:35:45 +0000

    cherry pick #8084 limit parallel_run cct tasks number to avoid connection issue to 202205

--- tests/bgp/test_bgp_gr_helper.py
+++ tests/bgp/test_bgp_gr_helper.py
@@ -17,7 +17,8 @@ pytestmark = [
 logger = logging.getLogger(__name__)
 
 
-def test_bgp_gr_helper_routes_perserved(duthosts, rand_one_dut_hostname, nbrhosts, setup_bgp_graceful_restart, tbinfo):
+def test_bgp_gr_helper_routes_perserved(duthosts, rand_one_dut_hostname, nbrhosts,
+                                        setup_bgp_graceful_restart, tbinfo, cct=8):
     """Verify that routes received from one neighbor are all preserved during peer graceful restart."""
 
     def _find_test_bgp_neighbors(test_neighbor_name, bgp_neighbors):
@@ -122,7 +123,9 @@ def test_bgp_gr_helper_routes_perserved(duthosts, rand_one_dut_hostname, nbrhost
         test_interface = ifnames_common[0]
     else:
         # if default route is not present, randomly select a neighbor to test
-        test_interface = random.sample([k for k, v in dev_nbrs.items() if not v['name'].startswith("Server")], 1)[0]
+        test_interface = random.sample(
+            [k for k, v in dev_nbrs.items() if not v['name'].startswith("Server")], 1
+        )[0]
 
     # get neighbor device connected ports
     nbr_ports = []
@@ -139,7 +142,10 @@ def test_bgp_gr_helper_routes_perserved(duthosts, rand_one_dut_hostname, nbrhost
     # get neighbor BGP peers
     test_bgp_neighbors = _find_test_bgp_neighbors(test_neighbor_name, bgp_neighbors)
 
-    logging.info("Select neighbor %s to verify that all bgp routes are preserved during graceful restart", test_neighbor_name)
+    logging.info(
+        "Select neighbor %s to verify that all bgp routes are preserved during graceful restart",
+        test_neighbor_name
+    )
 
     # get all routes received from neighbor before GR
     all_neighbor_routes_before_gr = {}
