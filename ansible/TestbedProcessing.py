00a51b99ce2633f37a388578e1634d29715b9fdd
Patrick MacArthur <pmacarthur@arista.com>
2023-07-13 17:14:09 -0400

    TestbedProcessing: Add model, serial, and base_mac to veos output

--- ansible/TestbedProcessing.py
+++ ansible/TestbedProcessing.py
@@ -862,6 +862,9 @@ def makeVeos(data, veos, devices, outfile):
                             entry += "\ttype=" + dev.get("type")
                             entry += "\thwsku=" + dev.get("hwsku")
                             entry += "\tcard_type=" + dev.get("card_type")
+                            entry += "\tmodel=" + dev.get("model")
+                            entry += "\tserial=" + dev.get("serial")
+                            entry += "\tbase_mac=" + dev.get("base_mac")
                     except Exception:
                         try:
                             ansible_host = veos.get(key).get(
