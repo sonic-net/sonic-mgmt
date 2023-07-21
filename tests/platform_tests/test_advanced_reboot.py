8c7d06d75ee1994cfb7c6fde40467dc746aecdbe
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2023-07-20 23:13:41 -0700

    Add import random

--- tests/platform_tests/test_advanced_reboot.py
+++ tests/platform_tests/test_advanced_reboot.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import random
 
 from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # lgtm[py/unused-import]
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses      # lgtm[py/unused-import]
