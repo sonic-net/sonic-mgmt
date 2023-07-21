53a3b3820cf9db0b075def0bc18ff0720bb24bef
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-07-21 11:29:30 -0700

    Update test_memory_exhaustion.py

--- tests/platform_tests/test_memory_exhaustion.py
+++ tests/platform_tests/test_memory_exhaustion.py
@@ -50,9 +50,8 @@ class TestMemoryExhaustion:
                     check_interfaces_and_services(lc, conn_graph_facts["device_conn"][lc.hostname],
                                                   xcvr_skip_list)
 
-
     def test_memory_exhaustion(self, duthosts, enum_rand_one_per_hwsku_hostname,
-                               localhost, xcvr_skip_list):
+                               conn_graph_facts, localhost, xcvr_skip_list):
         duthost = duthosts[enum_rand_one_per_hwsku_hostname]
         dut_ip = duthost.mgmt_ip
         hostname = duthost.hostname
