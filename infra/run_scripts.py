079ba313802cca4f7b14ad0663160982c0044de6
Akshay Ghosh <akghosh@cisco.com>
2023-07-22 22:14:55 -0700

    Reverted Makefile & added dut_name to run_scripts.py

--- infra/run_scripts.py
+++ infra/run_scripts.py
@@ -422,6 +422,8 @@ def main():
 
     if device_type == 'sherman':
         dut_name = 'sherman-01'
+    elif device_type == 'churchill':
+        dut_name = 'churchill-01'
     else:
         dut_name = 'mathilda-01'
 
