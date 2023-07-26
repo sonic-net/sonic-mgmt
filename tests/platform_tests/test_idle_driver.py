5977d595f7fe9e3cd1d4aa907d79382c38dce93b
Xichen Lin <lukelin0907@gmail.com>
2023-07-26 03:09:25 +0000

    remove restriction on intel cpu

--- tests/platform_tests/test_idle_driver.py
+++ tests/platform_tests/test_idle_driver.py
@@ -16,9 +16,6 @@ pytestmark = [
 
 def test_idle_driver(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
-    is_intel = 'Intel' in duthost.shell('lscpu | grep Vendor')['stdout']
-    if not is_intel:
-        pytest.skip('CPU vendor is not Intel')
     idle_driver = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver')['stdout']
     pytest_assert(idle_driver == 'none', 'Idle driver {} is active on this m0 device'.format(idle_driver))
     cpuidle = duthost.command('ls /sys/devices/system/cpu/cpu0/cpuidle/', module_ignore_errors=True)['rc']
