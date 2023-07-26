0fd5f0bd1cd0316ffa6cfd9bbee446e1bee56c9b
Xichen Lin <lukelin0907@gmail.com>
2023-07-26 05:38:31 +0000

    check cstate not more than 1

--- tests/platform_tests/test_idle_driver.py
+++ tests/platform_tests/test_idle_driver.py
@@ -17,6 +17,7 @@ pytestmark = [
 def test_idle_driver(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     idle_driver = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver')['stdout']
-    pytest_assert(idle_driver == 'none', 'Idle driver {} is active on this m0 device'.format(idle_driver))
-    cpuidle = duthost.command('ls /sys/devices/system/cpu/cpu0/cpuidle/', module_ignore_errors=True)['rc']
-    pytest_assert(cpuidle == 2, "cpuidle directory should not exist: rc {}".format(cpuidle))
+    if idle_driver != "none":
+        cstates = duthost.shell('sed -n "s/.*C\([0-9]*\).*/\\1/p" /sys/devices/system/cpu/cpu*/cpuidle/state*/name')['stdout'].split()
+        max_cstate = max([int(cstate) for cstate in cstates])
+        pytest_assert(max_cstate <= 1, "When idle driver is present, cstates higher than 1 is not allowed: max_cstate {}".format(max_cstate))
