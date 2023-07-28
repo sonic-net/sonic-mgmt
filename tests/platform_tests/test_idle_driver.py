fcb9a46b4c21c8cf21ea9a14e9e20cacd29b0f75
Xichen Lin <lukelin0907@gmail.com>
2023-07-28 04:24:40 +0000

    fix styling issue
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>

--- tests/platform_tests/test_idle_driver.py
+++ tests/platform_tests/test_idle_driver.py
@@ -14,10 +14,12 @@ pytestmark = [
     pytest.mark.topology('m0', 'mx'),
 ]
 
+
 def test_idle_driver(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     idle_driver = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver')['stdout']
     if idle_driver != "none":
         cstates = duthost.shell('sed -n "s/.*C\([0-9]*\).*/\\1/p" /sys/devices/system/cpu/cpu*/cpuidle/state*/name')['stdout'].split()
         max_cstate = max([int(cstate) for cstate in cstates])
-        pytest_assert(max_cstate <= 1, "When idle driver is present, cstates higher than 1 is not allowed: max_cstate {}".format(max_cstate))
+        pytest_assert(max_cstate <= 1,
+                "When idle driver is present, cstate higher than 1 is not allowed: max_cstate {}".format(max_cstate))
