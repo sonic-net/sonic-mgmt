a78201a87ac44fc3a49fbdd3b8a00c5314b47973
Xichen Lin <lukelin0907@gmail.com>
2023-07-28 06:14:45 +0000

    fix styling issue

--- tests/platform_tests/test_idle_driver.py
+++ tests/platform_tests/test_idle_driver.py
@@ -19,7 +19,9 @@ def test_idle_driver(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     idle_driver = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver')['stdout']
     if idle_driver != "none":
-        cstates = duthost.shell('sed -n "s/.*C\([0-9]*\).*/\\1/p" /sys/devices/system/cpu/cpu*/cpuidle/state*/name')['stdout'].split()
+        cstates = duthost.shell('sed -n "s/.*C\\([0-9]*\\).*/\\1/p" '
+                                '/sys/devices/system/cpu/cpu*/cpuidle/state*/name')['stdout'].split()
         max_cstate = max([int(cstate) for cstate in cstates])
         pytest_assert(max_cstate <= 1,
-                "When idle driver is present, cstate higher than 1 is not allowed: max_cstate {}".format(max_cstate))
+                      "When idle driver is present, cstate>1 is not allowed: max_cstate {}"
+                      .format(max_cstate))
