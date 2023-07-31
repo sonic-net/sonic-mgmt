439bb7a62fdf9059f605cea9d833f051bfa53995
ShiyanWangMS <shiyanwang@microsoft.com>
2023-07-31 10:57:04 +0800

    [202205] Change tool scripts to use Python2 explicitly
    Summary:
    This is a part of Python3 migration. The incoming new sonic-mgmt-docker will set Python3 as default.
    Old branch will use Python2 explicitly.
    
    Approach
    What is the motivation for this PR?
    This is a part of Python3 migration. The incoming new sonic-mgmt-docker will set Python3 as default.
    Old branch will use Python2 explicitly.
    
    How did you do it?
    In tool scripts, add head: #!/usr/bin/env python2
    In any caller of tool scripts, use "./" to call the scripts.
    
    co-authorized by: jianquanye@microsoft.com

--- ansible/files/check_testbed_and_inventory_file.py
+++ ansible/files/check_testbed_and_inventory_file.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 """Tool for checking testbed file and related inventory file.
 
 When we may make some mistakes while adding or updating testbed configurations in testbed file and inventory files.
