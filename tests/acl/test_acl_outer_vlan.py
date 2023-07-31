65a5eb362d8d06ebf05c47345240854f14f3a2c3
Yaqiang Zhu <yaqiangzhu@microsoft.com>
2023-07-31 14:30:08 +0800

    [acl][fanout] Optimize skip condition for SONiC fanout in test_acl_outer_vlan (#9170)
    What is the motivation for this PR?
    Currently some SONiC fanout supports tagged packet test. No need to skip for those devices.
    
    How did you do it?
    Optimize skip condition for SONiC fanout in test_acl_outer_vlan.
    
    How did you verify/test it?
    Run tests with Nokia 7215 / Broadcom SONiC fanouts.
    
    Signed-off-by: Yaqiang Zhu <yaqiangzhu@microsoft.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/acl/test_acl_outer_vlan.py
+++ tests/acl/test_acl_outer_vlan.py
@@ -18,6 +18,7 @@ from tests.common.fixtures.ptfhost_utils import change_mac_addresses    # noqa F
 from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer, LogAnalyzerError
 from abc import abstractmethod
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_rand_selected_tor_m    # noqa F401
+from tests.common.utilities import check_skip_release
 
 logger = logging.getLogger(__name__)
 
@@ -626,7 +627,14 @@ def skip_sonic_leaf_fanout(fanouthosts):
     """
     for fanouthost in list(fanouthosts.values()):
         if fanouthost.get_fanout_os() == 'sonic':
-            pytest.skip("Not supporteds on SONiC leaf-fanout")
+            # Skips this test if the SONiC image installed on fanout is < 202205
+            is_skip, _ = check_skip_release(fanouthost, ["201811", "201911", "202012", "202106", "202111"])
+            if is_skip:
+                pytest.skip("OS Version of fanout is older than 202205, unsupported")
+            asic_type = fanouthost.facts['asic_type']
+            platform = fanouthost.facts["platform"]
+            if not (asic_type in ["broadcom"] or platform in ["armhf-nokia_ixs7215_52x-r0"]):
+                pytest.skip("Not supporteds on SONiC leaf-fanout platform")
 
 
 class TestAclVlanOuter_Ingress(AclVlanOuterTest_Base):
