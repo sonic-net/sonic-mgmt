ef574d738a9d0338fb914924d14019745b7804e2
Soumya Velamala <87676006+svelamal@users.noreply.github.com>
2023-08-02 02:22:27 -0700

    Ignoring API connection failure to mux simulator (#8995)
    Ignoring API connection failure to mux simulator because this is ought to happen when mvrf/test_mgmtvrf.py is run and currently it throws a loganalyzer error
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/mvrf/test_mgmtvrf.py
+++ tests/mvrf/test_mgmtvrf.py
@@ -21,6 +21,17 @@ SONIC_SSH_REGEX = "OpenSSH_[\\w\\.]+ Debian"
 SONIC_SSH_PORT = 22
 
 
+@pytest.fixture(autouse=True)
+def _ignore_mux_errlogs(rand_one_dut_hostname, loganalyzer):
+    """Ignore expected failures logs during test execution."""
+    if loganalyzer:
+        loganalyzer[rand_one_dut_hostname].ignore_regex.extend(
+            [
+                ".*ERR pmon#CCmisApi.*y_cable_port.*GET http.*"
+            ])
+    return
+
+
 def restore_config_db(duthost):
     # Restore the original config_db to override the config_db with mgmt vrf config
     duthost.shell("mv /etc/sonic/config_db.json.bak /etc/sonic/config_db.json")
