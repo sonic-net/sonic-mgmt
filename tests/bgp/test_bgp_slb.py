02b5e0dee21e9d0609246f88a7ba524cb2614174
jcaiMR <jcai@microsoft.com>
2023-08-03 02:31:50 +0000

    fix python pre-commit check issue

--- tests/bgp/test_bgp_slb.py
+++ tests/bgp/test_bgp_slb.py
@@ -2,9 +2,8 @@ import pytest
 
 from tests.common import reboot
 from tests.common.helpers.bgp import BGPNeighbor
-from tests.common.dualtor.mux_simulator_control import (
+from tests.common.dualtor.mux_simulator_control import \
     toggle_all_simulator_ports_to_enum_rand_one_per_hwsku_frontend_host_m  # noqa F401
-)
 from tests.common.utilities import wait_until, delete_running_config
 
 
