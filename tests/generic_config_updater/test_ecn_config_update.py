187bc00eb20402926240cd6c2720d004c88f6002
CHARUDATTA CHITALE <cchitale@cisco.com>
2023-08-04 11:28:57 -0700

    cisco change for ecn-config-update

--- tests/generic_config_updater/test_ecn_config_update.py
+++ tests/generic_config_updater/test_ecn_config_update.py
@@ -54,13 +54,27 @@ def ensure_application_of_updated_config(duthost, configdb_field, values):
         values: expected value(s) of configdb_field
     """
     def _confirm_value_in_asic_db():
-        table_name = duthost.shell('sonic-db-cli ASIC_DB keys *WRED*')["stdout"]
-        wred_data = duthost.shell('sonic-db-cli ASIC_DB hgetall {}'.format(table_name))["stdout"]
-        wred_data = ast.literal_eval(wred_data)
-        for field, value in zip(configdb_field.split(','), values.split(',')):
-            if value != wred_data[WRED_MAPPING[field]]:
-                return False
-        return True
+        if(duthost.facts['asic_type'] == 'cisco-8000'):
+            wred_objects = duthost.shell('sonic-db-cli ASIC_DB keys *WRED*')["stdout"]
+            wred_objects = wred_objects.split("\n")
+            for wred_object in wred_objects:
+                wred_data = duthost.shell('sonic-db-cli ASIC_DB hgetall {}'.format(wred_object))["stdout"]
+                if('NULL' in wred_data):
+                    continue
+                wred_data = ast.literal_eval(wred_data)
+                for field, value in zip(configdb_field.split(','), values.split(',')):
+                    if value != wred_data[WRED_MAPPING[field]]:
+                        return False
+                return True
+            return False
+        else:
+            table_name = duthost.shell('sonic-db-cli ASIC_DB keys *WRED*')["stdout"]
+            wred_data = duthost.shell('sonic-db-cli ASIC_DB hgetall {}'.format(table_name))["stdout"]
+            wred_data = ast.literal_eval(wred_data)
+            for field, value in zip(configdb_field.split(','), values.split(',')):
+                if value != wred_data[WRED_MAPPING[field]]:
+                    return False
+            return True
 
     logger.info("Validating fields in ASIC DB...")
     pytest_assert(
