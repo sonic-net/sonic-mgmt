3db674d000b82828468c4d27814f01121606da00
Yutong Zhang <yutongzhang@microsoft.com>
2023-08-07 16:58:53 +0800

    Ignore key "DATABASE" in table "VERSIONS" in config checking.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2069,6 +2069,11 @@ def core_dump_and_config_check(duthosts, tbinfo, request):
             else:
                 EXCLUDE_CONFIG_KEY_NAMES = []
 
+            # 2. DB version issue #2839 #2870.
+            # This difference will cause additional reload.
+            # We will revert this change after this issue is fixed.
+            EXCLUDE_CONFIG_KEY_NAMES.append('VERSIONS|DATABASE')
+
             def _remove_entry(table_name, key_name, config):
                 if table_name in config and key_name in config[table_name]:
                     config[table_name].pop(key_name)
