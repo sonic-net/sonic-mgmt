d7e757bbaeffd4a0af4d0dd0d3a4740a59002791
rbpittman <randallbpittman@gmail.com>
2023-08-11 15:36:39 -0400

    ARP resolution during get_rx_port to avoid flaky LAG ID (#9408)
    Summary:
    When running isolated tests, the first packet sent on a host can egress out a different member due to missing ARP table entries. Send a preliminary packet to ensure ARP is fully resolved before returning the LAG ID.
    
    How did you do it?
    Observed issue when running isolated XoffLimit test case.
    
    How did you verify/test it?
    Cisco8000 TB.
    Reproduced issue by clearing arp tables on DUT. Running the xoff test case would then fail due to incorrectly blocked egress because the get_rx_port function returned the wrong port ID.
    This fix works on this test when clearing ARP tables, it logs the correction.

--- tests/saitests/py3/sai_qos_tests.py
+++ tests/saitests/py3/sai_qos_tests.py
@@ -187,13 +187,24 @@ def get_rx_port(dp, device_number, src_port_id, dst_mac, dst_ip, src_ip, src_vla
     src_port_mac = dp.dataplane.get_mac(device_number, src_port_id)
     pkt = construct_ip_pkt(64, dst_mac, src_port_mac,
                            src_ip, dst_ip, 0, src_vlan, ip_id=ip_id)
+    # Send initial packet for any potential ARP resolution, which may cause the LAG
+    # destination to change. Can occur especially when running tests in isolation on a
+    # first test attempt.
+    send_packet(dp, src_port_id, pkt, 1)
+    # Observed experimentally this sleep needs to be at least 0.02 seconds. Setting higher.
+    time.sleep(1)
     send_packet(dp, src_port_id, pkt, 1)
 
     masked_exp_pkt = construct_ip_pkt(
         48, dst_mac, src_port_mac, src_ip, dst_ip, 0, src_vlan, ip_id=ip_id, exp_pkt=True)
 
+    pre_result = dp.dataplane.poll(
+        device_number=0, exp_pkt=masked_exp_pkt, timeout=3)
     result = dp.dataplane.poll(
         device_number=0, exp_pkt=masked_exp_pkt, timeout=3)
+    if pre_result.port != result.port:
+        logging.debug("During get_rx_port, corrected LAG destination from {} to {}".format(
+            pre_result.port, result.port))
     if isinstance(result, dp.dataplane.PollFailure):
         dp.fail("Expected packet was not received. Received on port:{} {}".format(
             result.port, result.format()))
