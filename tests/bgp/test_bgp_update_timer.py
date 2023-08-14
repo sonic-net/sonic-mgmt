40ad4568a4739776d42fedfa3dc1b7cdaf0aeea5
jcaiMR <jcai@microsoft.com>
2023-08-14 15:09:03 +0000

    fix tcpdump issue in bgp update timer testing

--- tests/bgp/test_bgp_update_timer.py
+++ tests/bgp/test_bgp_update_timer.py
@@ -42,6 +42,11 @@ NEIGHBOR_PORT0 = 11000
 NEIGHBOR_PORT1 = 11001
 WAIT_TIMEOUT = 120
 
+def is_tcpdump_running(duthost, cmd):
+    check_cmd = "ps u -C tcpdump | grep '%s'" % cmd
+    if cmd in duthost.shell(check_cmd)['stdout']:
+        return True
+    return False
 
 @contextlib.contextmanager
 def log_bgp_updates(duthost, iface, save_path, ns):
@@ -58,10 +63,13 @@ def log_bgp_updates(duthost, iface, save_path, ns):
         duthost.asic_instance_from_namespace(ns).ns_arg,
         start_pcap,
     )
-    start_pcap = "nohup {}{} &".format(
+    start_pcap_cmd = "nohup {}{} &".format(
         duthost.asic_instance_from_namespace(ns).ns_arg, start_pcap
     )
-    duthost.shell(start_pcap)
+    duthost.shell(start_pcap_cmd)
+    if not wait_until(WAIT_TIMEOUT, 5, 20, lambda: is_tcpdump_running(duthost, start_pcap),):
+        pytest.fail("Could not start tcpdump")
+
     try:
         yield
     finally:
