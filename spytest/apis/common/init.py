acd698b61dca8daa29a68b3585a1cc51f9b31113
Rama Sasthri, Kristipati <rama.kristipati@gmail.com>
2023-08-15 04:33:04 +0530

    Spytest 2.2 feature apis common hooks base config (#9433)
    * refactor hooks as sonic hooks to support additional types
    
    * spytest base config update for sonic-vs
    
    ---------
    
    Co-authored-by: Rama Sasthri, Kristipati <rama.kristipati@broadcom.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- spytest/apis/common/init.py
+++ spytest/apis/common/init.py
@@ -1,12 +1,32 @@
 # this file is imported from framework and hence
 # we can't import framework API globally here
 # import them with in functions
-#from spytest import st
+# from spytest import st
+
+from utilities.tracer import Tracer
+
+
+def _callback(event, data, fpath, func, *args, **kwargs):
+    if data == "infra":
+        kwargs["_func_"] = func
+        from apis.common.instrument import apis_instrument as apis_instrument_impl
+        return apis_instrument_impl("infra-call-prolog", *args, **kwargs)
+
 
 def apis_register():
+    # Tracer.register(_callback, "infra", include="spytest/infra.py")
     from apis.common.hooks import Hooks
     return Hooks()
 
-def apis_instrument(scope, data):
+
+def apis_instrument(scope, *args, **kwargs):
     from apis.common.instrument import apis_instrument as apis_instrument_impl
-    return apis_instrument_impl(scope, data)
+    return apis_instrument_impl(scope, *args, **kwargs)
+
+
+def apis_trace_register(callback, data):
+    exclude = ["common/sonic_prompts.py", "common/hooks.py",
+               "common/checks.py", "common/support.py",
+               "common/init.py", "common/instrument.py",
+               "common/sonic_hooks.py", "common/sonic_features.py"]
+    Tracer.register(callback, data, exclude=exclude)
