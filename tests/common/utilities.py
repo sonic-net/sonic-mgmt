6b500caa528f0bc270fc959dfe9ba3bbfdb1208b
Anshul Dubela <anshuldubela2009@gmail.com>
2023-08-16 10:31:14 +0530

    Update utilities.py
    Fixed Below pre-commit issue:
    trim trailing whitespace.................................................Failed
    - hook id: trailing-whitespace
    - exit code: 1
    - files were modified by this hook
    
    Fixing tests/common/utilities.py

--- tests/common/utilities.py
+++ tests/common/utilities.py
@@ -511,7 +511,7 @@ def compare_crm_facts(left, right):
     for k, v in list(left_acl_group.items()):
         lv = v
         rv = right_acl_group[k]
-    
+
         # For Cisco-8000 devices, hardware counters are statistical-based with +/- 1 entry tolerance.
         # Hence, the available counter may not increase as per initial value.
         if abs(int(lv['available']) - int(rv['available'])) > 1 or abs(int(lv['used']) - int(rv['used'])) > 1:
