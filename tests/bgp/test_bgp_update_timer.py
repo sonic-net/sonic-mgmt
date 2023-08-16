c107b569007a1c5a33bc3d2436db34985b2642de
jcaiMR <111116206+jcaiMR@users.noreply.github.com>
2023-08-17 07:56:31 +0800

    fix tcpdump issue in bgp_update_timer script (#9481)
    Manually cherry pick #9446 into 202012 because of conflict

--- tests/bgp/test_bgp_update_timer.py
+++ tests/bgp/test_bgp_update_timer.py
@@ -34,6 +34,14 @@ NEIGHBOR_ASN1 = 61001
 NEIGHBOR_PORT0 = 11000
 NEIGHBOR_PORT1 = 11001
 WAIT_TIMEOUT = 120
+TCPDUMP_WAIT_TIMEOUT = 20
+
+
+def is_tcpdump_running(duthost, cmd):
+    check_cmd = "ps u -C tcpdump | grep '%s'" % cmd
+    if cmd in duthost.shell(check_cmd)['stdout']:
+        return True
+    return False
 
 
 @contextlib.contextmanager
@@ -48,8 +56,14 @@ def log_bgp_updates(duthost, iface, save_path, ns):
         start_pcap = "tcpdump -i %s -w %s port 179" % (iface, save_path)
     # for multi-asic dut, add 'ip netns exec asicx' to the beggining of tcpdump cmd 
     stop_pcap = "sudo pkill -f '%s%s'" % (duthost.asic_instance_from_namespace(ns).ns_arg, start_pcap)
-    start_pcap = "nohup {}{} &".format(duthost.asic_instance_from_namespace(ns).ns_arg, start_pcap)
-    duthost.shell(start_pcap)
+    start_pcap_cmd = "nohup {}{} &".format(duthost.asic_instance_from_namespace(ns).ns_arg, start_pcap)
+    duthost.shell(start_pcap_cmd)
+    # wait until tcpdump process created
+    if not wait_until(WAIT_TIMEOUT, 5, 1, lambda: is_tcpdump_running(duthost, start_pcap),):
+        pytest.fail("Could not start tcpdump")
+    # sleep and wait for tcpdump ready to sniff packets
+    time.sleep(TCPDUMP_WAIT_TIMEOUT)
+
     try:
         yield
     finally:
