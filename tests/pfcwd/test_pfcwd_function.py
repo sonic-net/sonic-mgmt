eef0d87b3a2ea541d0971bd7bca7cc2643d3bf03
Shiyan <shiyanwang@microsoft.com>
2023-08-17 10:44:19 +0800

    first commit

--- tests/pfcwd/test_pfcwd_function.py
+++ tests/pfcwd/test_pfcwd_function.py
@@ -493,7 +493,7 @@ class SendVerifyTraffic():
         log_format = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         log_file = "/tmp/pfc_wd.PfcWdTest.{}.log".format(log_format)
         ptf_runner(self.ptf, "ptftests", "pfc_wd.PfcWdTest", "ptftests", params=ptf_params,
-                   log_file=log_file)
+                   log_file=log_file, is_python3=True)
 
     def verify_rx_ingress(self, action):
         """
@@ -524,7 +524,7 @@ class SendVerifyTraffic():
         log_format = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         log_file = "/tmp/pfc_wd.PfcWdTest.{}.log".format(log_format)
         ptf_runner(self.ptf, "ptftests", "pfc_wd.PfcWdTest", "ptftests", params=ptf_params,
-                   log_file=log_file)
+                   log_file=log_file, is_python3=True)
 
     def verify_other_pfc_queue(self):
         """
@@ -557,7 +557,7 @@ class SendVerifyTraffic():
         log_format = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         log_file = "/tmp/pfc_wd.PfcWdTest.{}.log".format(log_format)
         ptf_runner(self.ptf, "ptftests", "pfc_wd.PfcWdTest", "ptftests", params=ptf_params,
-                   log_file=log_file)
+                   log_file=log_file, is_python3=True)
 
     def verify_other_pfc_pg(self):
         """
@@ -590,7 +590,7 @@ class SendVerifyTraffic():
         log_format = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         log_file = "/tmp/pfc_wd.PfcWdTest.{}.log".format(log_format)
         ptf_runner(self.ptf, "ptftests", "pfc_wd.PfcWdTest", "ptftests", params=ptf_params,
-                   log_file=log_file)
+                   log_file=log_file, is_python3=True)
 
     def fill_buffer(self):
         """
@@ -613,7 +613,7 @@ class SendVerifyTraffic():
         log_format = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         log_file = "/tmp/pfc_wd.PfcWdTest.{}.log".format(log_format)
         ptf_runner(self.ptf, "ptftests", "pfc_wd.PfcWdTest", "ptftests", params=ptf_params,
-                   log_file=log_file)
+                   log_file=log_file, is_python3=True)
 
     def verify_wd_func(self, action, rx_action, tx_action):
         """
