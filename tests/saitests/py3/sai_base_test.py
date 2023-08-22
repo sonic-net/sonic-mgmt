096b766f416def086041d6847648a97a3e9bd683
ansrajpu-git <113939367+ansrajpu-git@users.noreply.github.com>
2023-08-22 13:41:13 -0400

    PR to cherry pick Improvising qos tests by tunning the qos params for single_asic, single_dut_multi_asic and  multi_dut (#8222) from master to 202205
    * QoS tunning for LossyQueueTest
    * Making assert specific to dnx for DscpQueueMapping qos_test
    fix flake8 errors
    flake8 issue resolved
    * Recommiting the changes of qos.yml to qos_params_j2c.yml
    * Reverting dscp to 7 as this is fixed in BRCM CSP
    * Revert "Making assert specific to dnx for DscpQueueMapping qos_test"
    
    This reverts commit dcbf90fa1bbdb3778d9ef86a202c30d86bd08114.
    * changing dscp value back to 8
    * tx_enable/disable retry check corrected
    * Flake8 errore fixed
    * Making check to run only for chassis in LossyQueue
    * Adding check for sai_base for tx_enable/disable

--- tests/saitests/py3/sai_base_test.py
+++ tests/saitests/py3/sai_base_test.py
@@ -14,6 +14,7 @@ from ptf import config
 import ptf.testutils as testutils
 import json
 import socket
+import time
 
 ################################################################
 #
@@ -174,6 +175,11 @@ class ThriftInterface(BaseTest):
         return stdOut, stdErr, retValue
 
     def sai_thrift_port_tx_enable(self, client, asic_type, port_list, target='dst', last_port=True):
+        count = 0
+        if asic_type == 'mellanox':
+            self.enable_mellanox_egress_data_plane(port_list)
+        else:
+            sai_thrift_port_tx_enable(client, asic_type, port_list, target=target)
         if self.platform_asic and self.platform_asic == "broadcom-dnx" and last_port:
             # need to enable watchdog on the source asic using cint script
             cmd = "bcmcmd -n {} \"BCMSAI credit-watchdog enable\"".format(self.src_asic_index)
@@ -181,15 +187,23 @@ class ThriftInterface(BaseTest):
                                                             self.test_params['dut_username'],
                                                             self.test_params['dut_password'],
                                                             cmd)
-            assert 'Success rv = 0' in stdOut[1], "enable wd failed '{}' on asic '{}' on '{}'".format(cmd, self.src_asic_index,
-                                                                                            self.src_server_ip)
-
-        if asic_type == 'mellanox':
-            self.enable_mellanox_egress_data_plane(port_list)
-        else:
-            sai_thrift_port_tx_enable(client, asic_type, port_list, target=target)
+            if retValue != 0 or 'Success rv = 0' not in stdOut[1]:
+                # Retry credit-wd command max 3 times on failure
+                while count < 3:
+                    print("Retrying credit_wd_enable")
+                    time.sleep(5)
+                    stdOut, stdErr, retValue = self.exec_cmd_on_dut(self.src_server_ip,
+                                                                    self.test_params['dut_username'],
+                                                                    self.test_params['dut_password'],
+                                                                    cmd)
+                    if stdOut and 'Success rv = 0' in stdOut[1]:
+                        break
+                    count += 1
+            assert 'Success rv = 0' in stdOut[1] if stdOut else retValue == 0,\
+                "enable wd failed '{}' on asic '{}' on '{}'".format(cmd, self.src_asic_index, self.src_server_ip)
 
     def sai_thrift_port_tx_disable(self, client, asic_type, port_list, target='dst'):
+        count = 0
         if self.platform_asic and self.platform_asic == "broadcom-dnx":
             # need to enable watchdog on the source asic using cint script
             cmd = "bcmcmd -n {} \"BCMSAI credit-watchdog disable\"".format(self.src_asic_index)
@@ -197,8 +211,21 @@ class ThriftInterface(BaseTest):
                                                             self.test_params['dut_username'],
                                                             self.test_params['dut_password'],
                                                             cmd)
-            assert 'Success rv = 0' in stdOut[1], "disable wd failed '{}' on asic '{}' on '{}'".format(cmd, self.src_asic_index,
-                                                                                        self.src_server_ip)
+            if retValue != 0 or 'Success rv = 0' not in stdOut[1]:
+                # Retry credit-wd command max 3 times on failure
+                while count < 3:
+                    print("Retrying credit_wd_enable")
+                    time.sleep(5)
+                    stdOut, stdErr, retValue = self.exec_cmd_on_dut(self.src_server_ip,
+                                                                    self.test_params['dut_username'],
+                                                                    self.test_params['dut_password'],
+                                                                    cmd)
+                    if stdOut and 'Success rv = 0' in stdOut[1]:
+                        break
+                    count += 1
+            assert 'Success rv = 0' in stdOut[1] if stdOut else retValue == 0, \
+                "disable wd failed '{}' on asic '{}' on '{}'".format(cmd, self.src_asic_index, self.src_server_ip)
+                        
         if asic_type == 'mellanox':
             self.disable_mellanox_egress_data_plane(port_list)
         else:
@@ -246,7 +273,6 @@ class ThriftInterface(BaseTest):
                 self.exec_cmd_on_dut(
                     self.server, self.test_params['dut_username'],
                     self.test_params['dut_password'], cmd_recover_q_scheduler_config)
-
 
 class ThriftInterfaceDataPlane(ThriftInterface):
     """
