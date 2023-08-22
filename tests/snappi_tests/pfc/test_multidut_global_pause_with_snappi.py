fd7bc58ea0d5062cc25f769b54d5e06876f9fc7b
selldinesh <dinesh.sellappan@keysight.com>
2023-08-22 21:28:22 +0000

    fixing import

--- tests/snappi_tests/pfc/test_multidut_global_pause_with_snappi.py
+++ tests/snappi_tests/pfc/test_multidut_global_pause_with_snappi.py
@@ -7,9 +7,9 @@ from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
 from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port,\
     snappi_api, snappi_dut_base_config, get_tgen_peer_ports, get_multidut_snappi_ports,\
     get_multidut_tgen_peer_port_set, cleanup_config                                                 # noqa: F401
-from tests.common.snappi_tests.qos_fixtures import lossless_prio_list, prio_dscp_map                      # noqa: F401
+from tests.common.snappi_tests.qos_fixtures import lossless_prio_list, prio_dscp_map                # noqa: F401
 from tests.snappi_tests.variables import config_set, line_card_choice
-from files.multidut_helper import run_pfc_test
+from tests.snappi_tests.pfc.files.multidut_helper import run_pfc_test                               # noqa: F401
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
 
 logger = logging.getLogger(__name__)
