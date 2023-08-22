aa0fde99b814529d20e154f7922011f3a1aa3989
maunnikr-cisco <142842429+maunnikr-cisco@users.noreply.github.com>
2023-08-22 11:27:32 -0700

    compressing while loop using wait_until
    replacing while loop in get_crm_facts with a wait_until function

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -17,7 +17,7 @@ from ansible.plugins.loader import connection_loader
 from tests.common.devices.base import AnsibleHostBase
 from tests.common.devices.constants import ACL_COUNTERS_UPDATE_INTERVAL_IN_SEC
 from tests.common.helpers.dut_utils import is_supervisor_node
-from tests.common.utilities import get_host_visible_vars
+from tests.common.utilities import get_host_visible_vars, wait_until
 from tests.common.cache import cached
 from tests.common.helpers.constants import DEFAULT_ASIC_ID, DEFAULT_NAMESPACE
 from tests.common.helpers.platform_api.chassis import is_inband_port
@@ -1815,6 +1815,11 @@ Totals               6450                 6449
             section_id = 0
             for line in output:
                 if not_ready_prompt in line:
+                    if time_total > 0:
+                        logging.warning("CRM counters are not ready yet, will retry after 10 seconds")
+                        time_total -= 10
+                    else:
+                        logging.warning("CRM counters are not ready yet, will not retry - time exceeded")
                     return False
                 if len(line.strip()) != 0:
                     if not in_section:
@@ -1849,15 +1854,8 @@ Totals               6450                 6449
             return True
         # Retry until crm resources are ready
         timeout = crm_facts['polling_interval'] + 10
-        while timeout >= 0:
-            ret = _show_and_parse_crm_resources()
-            if ret:
-                break
-            logging.warning("CRM counters are not ready yet, will retry after 10 seconds")
-            time.sleep(10)
-            timeout -= 10
-        assert(timeout >= 0)
-
+        time_total = timeout
+        wait_until(timeout, 10, 10, lambda: _show_and_parse_crm_resources())
         return crm_facts
 
     def start_service(self, service_name, docker_name):
