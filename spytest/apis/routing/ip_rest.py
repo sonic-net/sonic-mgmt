fabbf2b7c49dcd75588dc011f44402aa33f025bd
Chandra-BS <chandra.singh@broadcom.com>
2023-08-30 03:59:32 -0700

    Add support for GNMI and improvements

--- spytest/apis/routing/ip_rest.py
+++ spytest/apis/routing/ip_rest.py
@@ -1,7 +1,8 @@
 from spytest import st
 from apis.system.rest import get_rest
 
-non_physical_ports = ['vlan', 'portchannel']
+non_physical_ports = ['vlan', 'portchannel', 'loopback']
+
 
 def get_subinterface_index(dut, port):
     """
@@ -11,11 +12,16 @@ def get_subinterface_index(dut, port):
     :return:
     :rtype
     """
+    if len(port.split(".")) == 2:
+        return port.split(".")[-1]
+    else:
+        return '0'
+    # Following part of code is not needed. Will remove it after a complete run
     if any(intf in port.lower() for intf in non_physical_ports):
         return "0"
     rest_urls = st.get_datastore(dut, "rest_urls")
     url = rest_urls['sub_interface_config'].format(port)
-    output = get_rest(dut, rest_url = url)
+    output = get_rest(dut, rest_url=url)
     try:
         index = output['output']['openconfig-interfaces:subinterfaces']['subinterface'][0]['index']
         return str(index)
@@ -23,4 +29,3 @@ def get_subinterface_index(dut, port):
         st.log("{} exception occurred".format(e))
         st.log("The actual output is: {}".format(output))
         return False
-
