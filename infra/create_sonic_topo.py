030f12d3af51eb825c2aa284c193f1e47d532268
Perumal Venkatesh <vperumal@gmail.com>
2023-09-02 15:52:09 -0700

    Adding support for t1-28-lag

--- infra/create_sonic_topo.py
+++ infra/create_sonic_topo.py
@@ -509,6 +509,7 @@ def upload_tb_files(data,topo_type,base_topo_file,device_type):
         ftp_client.put('t1-spine.j2','golden-code/sonic-test/sonic-mgmt/ansible/roles/eos/templates/t1-spine.j2')
         ftp_client.put('t1-tor.j2','golden-code/sonic-test/sonic-mgmt/ansible/roles/eos/templates/t1-tor.j2')
         ftp_client.put('topo_t1.yml', 'golden-code/sonic-test/sonic-mgmt/ansible/vars/topo_t1.yml')
+    elif topo_type == 't1-28-lag':
         ftp_client.put('t1-28-lag-spine.j2','golden-code/sonic-test/sonic-mgmt/ansible/roles/eos/templates/t1-28-lag-spine.j2')
         ftp_client.put('t1-28-lag-tor.j2','golden-code/sonic-test/sonic-mgmt/ansible/roles/eos/templates/t1-28-lag-tor.j2')
         ftp_client.put('topo_t1-28-lag.yml', 'golden-code/sonic-test/sonic-mgmt/ansible/vars/topo_t1-28-lag.yml')    
