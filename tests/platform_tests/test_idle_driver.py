1ddc501d5dadda875cce10749537f1ea76d67a0e
Xichen96 <lukelin0907@gmail.com>
2023-09-05 13:44:25 +0800

    fix getting current idle driver error (#9799)
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/platform_tests/test_idle_driver.py
+++ tests/platform_tests/test_idle_driver.py
@@ -17,8 +17,8 @@ pytestmark = [
 
 def test_idle_driver(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
-    idle_driver = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver')['stdout']
-    if idle_driver != "none":
+    idle_driver_result = duthost.shell('cat /sys/devices/system/cpu/cpuidle/current_driver', module_ignore_errors=True)
+    if idle_driver_result['rc'] == 0 and idle_driver_result['stdout'] != "none":
         cstates = duthost.shell('sed -n "s/.*C\\([0-9]*\\).*/\\1/p" '
                                 '/sys/devices/system/cpu/cpu*/cpuidle/state*/name')['stdout'].split()
         max_cstate = max([int(cstate) for cstate in cstates])
