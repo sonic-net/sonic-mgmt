b38d89503877c107262b080fd6bf8c773f32c87a
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2023-09-08 09:32:49 -0700

    Add container stopped and container down events test (#9632)

--- tests/telemetry/events/host_events.py
+++ tests/telemetry/events/host_events.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
 import logging
-
+import time
 from run_events_test import run_test
 from event_utils import backup_monit_config, customize_monit_config, restore_monit_config
 from telemetry_utils import trigger_logger
+from tests.common.helpers.dut_utils import is_container_running
 
 logger = logging.getLogger(__name__)
 tag = "sonic-events-host"
@@ -14,6 +15,11 @@ def test_event(duthost, gnxi_path, ptfhost, data_dir, validate_yang):
     logger.info("Beginning to test host events")
     run_test(duthost, gnxi_path, ptfhost, data_dir, validate_yang, trigger_kernel_event,
              "event_kernel.json", "sonic-events-host:event-kernel", tag, False)
+    run_test(duthost, gnxi_path, ptfhost, data_dir, validate_yang, restart_container,
+             "event_stopped_ctr.json", "sonic-events-host:event-stopped-ctr", tag, False)
+    run_test(duthost, gnxi_path, ptfhost, data_dir, validate_yang, mask_container,
+             "event_down_ctr.json", "sonic-events-host:event-down-ctr", tag, False)
+
     backup_monit_config(duthost)
     customize_monit_config(
         duthost,
@@ -44,3 +50,39 @@ def trigger_kernel_event(duthost):
     logger.info("Invoking logger for kernel events")
     # syslog at github.com/torvalds/linux/blob/master/fs/squashfs/decompressor_multi.c#L193
     trigger_logger(duthost, "zlib decompression failed, data probably corrupt", "kernel")
+
+
+def get_running_container(duthost):
+    logger.info("Check if acms or snmp container is running")
+    container = "acms"
+    container_running = is_container_running(duthost, container)
+    if not container_running:
+        container = "snmp"
+    else:
+        return container
+    container_running = is_container_running(duthost, container)
+    if not container_running:
+        return ""
+    return container
+
+
+def restart_container(duthost):
+    logger.info("Stopping container for event stopped event")
+    container = get_running_container(duthost)
+    assert container != "", "No available container for testing"
+
+    duthost.shell("systemctl restart {}".format(container))
+
+
+def mask_container(duthost):
+    logger.info("Masking container for event down event")
+    container = get_running_container(duthost)
+    assert container != "", "No available container for testing"
+
+    duthost.shell("systemctl mask {}".format(container))
+    duthost.shell("docker stop {}".format(container))
+
+    time.sleep(30)  # Wait 30 seconds for container_checker to fire event
+
+    duthost.shell("systemctl unmask {}".format(container))
+    duthost.shell("systemctl restart {}".format(container))
