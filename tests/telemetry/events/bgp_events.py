576e910451e8f160729283d0d85dfee5ce3248dd
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2023-09-18 13:19:11 -0700

    Add bgp notification event (#9862)

--- tests/telemetry/events/bgp_events.py
+++ tests/telemetry/events/bgp_events.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import logging
+import time
 
 from run_events_test import run_test
 
@@ -9,12 +10,32 @@ tag = "sonic-events-bgp"
 
 
 def test_event(duthost, gnxi_path, ptfhost, data_dir, validate_yang):
-    logger.info("Beginning to test bgp-state event")
+    run_test(duthost, gnxi_path, ptfhost, data_dir, validate_yang, drop_tcp_packets,
+             "bgp_notification.json", "sonic-events-bgp:notification", tag)
     run_test(duthost, gnxi_path, ptfhost, data_dir, validate_yang, shutdown_bgp_neighbors,
              "bgp_state.json", "sonic-events-bgp:bgp-state", tag)
 
 
+def drop_tcp_packets(duthost):
+    logger.info("Adding rule to drop TCP packets to test bgp-notification")
+
+    ret = duthost.shell("iptables -I INPUT -p tcp --dport 179 -j DROP")
+    assert ret["rc"] == 0, "Unable to add DROP rule to iptables"
+
+    ret = duthost.shell("iptables -I INPUT -p tcp --sport 179 -j DROP")
+    assert ret["rc"] == 0, "Unable to add DROP rule to iptables"
+
+    time.sleep(10)  # Give time for hold timer expiry notif to fire, val from config db
+
+    ret = duthost.shell("iptables -D INPUT -p tcp --dport 179 -j DROP")
+    assert ret["rc"] == 0, "Unable to remove DROP rule from iptables"
+
+    ret = duthost.shell("iptables -D INPUT -p tcp --sport 179 -j DROP")
+    assert ret["rc"] == 0, "Unable to remove DROP rule from iptables"
+
+
 def shutdown_bgp_neighbors(duthost):
+    logger.info("Shutting down bgp neighbors to test bgp-state event")
     assert duthost.is_service_running("bgpcfgd", "bgp") is True and duthost.is_bgp_state_idle() is False
     logger.info("Start all bgp sessions")
     ret = duthost.shell("config bgp startup all")
