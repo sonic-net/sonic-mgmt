4b16ecdbc6a106b6c7cde3b59a7feca1b5aa59e2
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-09-25 12:28:58 -0700

    Update utilities.py

--- tests/override_config_table/utilities.py
+++ tests/override_config_table/utilities.py
@@ -1,12 +1,10 @@
 import json
 import logging
+import pytest
 
 from tests.common.config_reload import config_reload
 
 logger = logging.getLogger(__name__)
-
-GOLDEN_CONFIG = "/etc/sonic/golden_config_db.json"
-
 
 def backup_config(duthost, config, config_backup):
     logger.info("Backup {} to {} on {}".format(
@@ -26,7 +24,16 @@ def get_running_config(duthost, asic=None):
 
 
 def reload_minigraph_with_golden_config(duthost, json_data):
-    duthost.copy(content=json.dumps(json_data, indent=4), dest=GOLDEN_CONFIG)
+    """
+    for multi-asic devices, we copy config db from host and asics
+    for single-asic devices, we copy only config_db.json
+    """
+    if duthost.is_multi_asic:
+        for asic_id in duthost.get_frontend_asic_ids():
+            golden_config = "/etc/sonic/golden_config_db{}.json".format(asic_id)
+            duthost.copy(content=json.dumps(json_data, indent=4), dest=golden_config)
+    golden_config = "/etc/sonic/golden_config_db.json"
+    duthost.copy(content=json.dumps(json_data, indent=4), dest=golden_config)
     config_reload(duthost, config_source="minigraph", safe_reload=True, override_config=True)
 
 
