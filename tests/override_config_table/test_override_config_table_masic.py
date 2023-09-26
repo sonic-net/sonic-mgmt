9460b636530a3d9c01708e2ec27d099cf8a32e62
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-09-25 18:29:50 -0700

    Update test_override_config_table_masic.py

--- tests/override_config_table/test_override_config_table_masic.py
+++ tests/override_config_table/test_override_config_table_masic.py
@@ -233,12 +233,17 @@ def load_minigraph_with_golden_empty_table_removal(duthost):
     )
 
 
-def test_load_minigraph_with_golden_config(duthost, setup_env):
+def test_load_minigraph_with_golden_config(duthosts, setup_env,
+                                           enum_rand_one_per_hwsku_frontend_hostname):
     """
     Test Golden Config override during load minigraph
     Note: Skip full config override for multi-asic duts for now, because we
     don't have CLI to get new golden config that contains 'localhost' and 'asicxx'
     """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    if not duthost.is_multi_asic:
+        pytest.skip("Skip override-config-table multi-asic testing on single-asic platforms,\
+                    test provided golden config format is not compatible with single-asics")
     load_minigraph_with_golden_empty_input(duthost)
     load_minigraph_with_golden_partial_config(duthost)
     load_minigraph_with_golden_new_feature(duthost)
