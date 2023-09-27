4db295e6bcaeb5978f9402ed8646f8748083fe93
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-09-27 09:13:06 -0700

    Update utilities.py

--- tests/override_config_table/utilities.py
+++ tests/override_config_table/utilities.py
@@ -25,13 +25,8 @@ def get_running_config(duthost, asic=None):
 
 def reload_minigraph_with_golden_config(duthost, json_data):
     """
-    for multi-asic devices, we copy config db from host and asics
-    for single-asic devices, we copy only config_db.json
+    for multi-asic/single-asic devices, we only have 1 golden_config_db.json
     """
-    if duthost.is_multi_asic:
-        for asic_id in duthost.get_frontend_asic_ids():
-            golden_config = "/etc/sonic/golden_config_db{}.json".format(asic_id)
-            duthost.copy(content=json.dumps(json_data, indent=4), dest=golden_config)
     golden_config = "/etc/sonic/golden_config_db.json"
     duthost.copy(content=json.dumps(json_data, indent=4), dest=golden_config)
     config_reload(duthost, config_source="minigraph", safe_reload=True, override_config=True)
