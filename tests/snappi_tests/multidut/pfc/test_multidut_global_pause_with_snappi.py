d1ee037d7164152b823e683d99c556bfdfdad95c
selldinesh <dinesh.sellappan@keysight.com>
2023-10-11 21:37:23 +0000

    pytest marker change

--- tests/snappi_tests/multidut/pfc/test_multidut_global_pause_with_snappi.py
+++ tests/snappi_tests/multidut/pfc/test_multidut_global_pause_with_snappi.py
@@ -14,7 +14,7 @@ from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
 
 logger = logging.getLogger(__name__)
 
-pytestmark = [pytest.mark.topology('tgen')]
+pytestmark = [pytest.mark.topology('multidut-tgen')]
 
 
 @pytest.mark.parametrize('line_card_choice', [line_card_choice])
