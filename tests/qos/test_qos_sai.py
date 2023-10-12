fb83dbaebe1139c282e631e5731a20a2a23adc9d
Zhixin Zhu <zhixzhu@cisco.com>
2023-10-12 13:23:24 -0700

    Update LossyQueueVoqTest to handle that dst_port in port-channel
    Signed-off-by: Zhixin Zhu <zhixzhu@cisco.com>

--- tests/qos/test_qos_sai.py
+++ tests/qos/test_qos_sai.py
@@ -1074,11 +1074,11 @@ class TestQosSai(QosSaiBase):
             Raises:
                 RunAnsibleModuleFail if ptf test fails
         """
-        if dutTestParams["basicParams"]["sonic_asic_type"] != "cisco-8000":
-            pytest.skip("Lossy Queue Voq test is not supported")
+        if not get_src_dst_asic_and_duts['single_asic_test']:
+            pytest.skip("Lossy Queue Voq test is only supported on cisco-8000 single-asic")
         if "lossy_queue_voq_1" in LossyVoq:
-            if ('modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and
-                get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True"):
+            if 'modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and\
+              get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True":
                 pytest.skip("LossyQueueVoq: This test is skipped since cisco-8000 T2 "
                             "doesn't support split-voq.")
         elif "lossy_queue_voq_2" in LossyVoq:
