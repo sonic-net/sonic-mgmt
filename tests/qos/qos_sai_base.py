7d55efa199e57994d1c1cdc3f86e120b17d4b8d6
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2023-10-12 17:11:38 -0700

    Add skip for 400G for PGDrop. (#9540)
    Add skip for 400G for PGDrop (64 byte packets)
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -2000,6 +2000,21 @@ class QosSaiBase(QosBase):
 
         return dualtor_ports_set
 
+    @pytest.fixture(autouse=False)
+    def _check_ingress_speed_gte_400g(
+            self,
+            get_src_dst_asic_and_duts,
+            dutQosConfig):
+        portSpeedCableLength = dutQosConfig["portSpeedCableLength"]
+        m = re.search("([0-9]+)_([0-9]+m)", portSpeedCableLength)
+        if not m:
+            raise RuntimeError(
+                "Format error in portSpeedCableLength:{}".
+                format(portSpeedCableLength))
+        speed = int(m.group(1))
+        if speed >= 400000:
+            pytest.skip("PGDrop test is not supported for 400G port speed.")
+
     def select_port_ids_for_mellnaox_device(self, duthost, mgFacts, testPortIds):
         """
         For Nvidia devices, the tested ports must have the same cable length and speed.
