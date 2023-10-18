8d08a950445bb3d9b1b765738d31d779cadcfb65
Suyash Prakash <suypraka@cisco.com>
2023-10-18 13:16:09 -0700

    Added platform specific check

--- ansible/roles/test/files/ptftests/py3/copp_tests.py
+++ ansible/roles/test/files/ptftests/py3/copp_tests.py
@@ -44,7 +44,13 @@ class ControlPlaneBaseTest(BaseTest):
     MAX_PORTS = 128
     PPS_LIMIT = 600
     PPS_LIMIT_MIN = PPS_LIMIT * 0.9
-    PPS_LIMIT_MAX = PPS_LIMIT * 1.4
+    if (duthost.facts["hwsku"] == "Cisco-8111-O64" or
+        duthost.facts["hwsku"] == "Cisco-8111-O32" or
+        duthost.facts["hwsku"] == "Cisco-8111-C32" or
+        duthost.facts["hwsku"] == "Cisco-8111-O62C2"):
+        PPS_LIMIT_MAX = PPS_LIMIT * 1.4
+    else:
+        PPS_LIMIT_MAX = PPS_LIMIT * 1.3
     NO_POLICER_LIMIT = PPS_LIMIT * 1.4
     TARGET_PORT = "3"  # Historically we have port 3 as a target port
     TASK_TIMEOUT = 600  # Wait up to 10 minutes for tasks to complete
