cd75346fd57f45cdbe5d9662b471d67779ff0632
Zhixin Zhu <zhixzhu@cisco.com>
2023-10-24 10:03:22 -0700

    update skip_src_dst_different_asic to check asic type, add lossy_queue_voq_2 to qos_params.pac.yaml
    Signed-off-by: Zhixin Zhu <zhixzhu@cisco.com>

--- tests/qos/test_qos_sai.py
+++ tests/qos/test_qos_sai.py
@@ -1082,17 +1082,18 @@ class TestQosSai(QosSaiBase):
             Raises:
                 RunAnsibleModuleFail if ptf test fails
         """
+        if not get_src_dst_asic_and_duts['single_asic_test']:
+            pytest.skip("Lossy Queue Voq test is only supported on cisco-8000 single-asic")
         if "lossy_queue_voq_1" in LossyVoq:
-            if 'modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and \
-                get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True":
+            if 'modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and\
+              get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True":
                 pytest.skip("LossyQueueVoq: This test is skipped since cisco-8000 T2 "
                             "doesn't support split-voq.")
         elif "lossy_queue_voq_2" in LossyVoq:
-            if not ('modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and \
-                get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True"):
-                pytest.skip(
-                    "LossyQueueVoq: lossy_queue_voq_2 test is not applicable "
-                    "for split-voq.")
+            if not ('modular_chassis' in get_src_dst_asic_and_duts['src_dut'].facts and
+                    get_src_dst_asic_and_duts['src_dut'].facts["modular_chassis"] == "True"):
+                pytest.skip("LossyQueueVoq: lossy_queue_voq_2 test is not applicable "
+                            "for split-voq.")
         portSpeedCableLength = dutQosConfig["portSpeedCableLength"]
         qosConfig = dutQosConfig["param"][portSpeedCableLength]
         flow_config = qosConfig[LossyVoq]["flow_config"]
