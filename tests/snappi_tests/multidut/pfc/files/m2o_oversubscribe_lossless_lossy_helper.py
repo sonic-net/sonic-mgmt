2d72fbd0c7d0937e83a15df5be03dcc41698dda1
selldinesh <dinesh.sellappan@keysight.com>
2023-10-24 17:57:29 +0000

    flake8 fixes

--- tests/snappi_tests/multidut/pfc/files/m2o_oversubscribe_lossless_lossy_helper.py
+++ tests/snappi_tests/multidut/pfc/files/m2o_oversubscribe_lossless_lossy_helper.py
@@ -9,7 +9,7 @@ import logging                                                                  
 from tests.common.helpers.assertions import pytest_assert, pytest_require               # noqa: F401
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts  # noqa: F401
 from tests.common.snappi_tests.snappi_helpers import get_dut_port_id                     # noqa: F401
-from tests.common.snappi_tests.common_helpers import pfc_class_enable_vector,\
+from tests.common.snappi_tests.common_helpers import pfc_class_enable_vector, \
     stop_pfcwd, disable_packet_aging                                                    # noqa: F401
 from tests.common.snappi_tests.port import select_ports                                 # noqa: F401
 from tests.common.snappi_tests.snappi_helpers import wait_for_arp
@@ -67,7 +67,8 @@ def run_pfcwd_multi_node_test(api,
     rx_port = snappi_extra_params.multi_dut_params.multi_dut_ports[0]
     rx_port_id_list = [rx_port["port_id"]]
     duthost2 = snappi_extra_params.multi_dut_params.duthost2
-    tx_port = [snappi_extra_params.multi_dut_params.multi_dut_ports[1], snappi_extra_params.multi_dut_params.multi_dut_ports[2]]
+    tx_port = [snappi_extra_params.multi_dut_params.multi_dut_ports[1],
+               snappi_extra_params.multi_dut_params.multi_dut_ports[2]]
     tx_port_id_list = [tx_port[0]["port_id"], tx_port[1]["port_id"]]
 
     pytest_assert(testbed_config is not None, 'Fail to get L2/3 testbed config')
