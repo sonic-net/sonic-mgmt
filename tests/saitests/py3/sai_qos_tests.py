ad0b42c2a4a6a80eeb80b4f4b4123953a5fd8562
ansrajpu <anshu.rajput@nokia.com>
2023-10-25 16:27:12 -0400

    Updating the condition to correct queue value_testQosSaiDscpToPgMapping

--- tests/saitests/py3/sai_qos_tests.py
+++ tests/saitests/py3/sai_qos_tests.py
@@ -997,7 +997,7 @@ class DscpToPgMapping(sai_base_test.ThriftInterfaceDataPlane):
         dst_port_id = get_rx_port(
             self, 0, src_port_id, pkt_dst_mac, dst_port_ip, src_port_ip)
         print("actual dst_port_id: %d" % (dst_port_id), file=sys.stderr)
-
+        time.sleep(3)
         try:
             for pg, dscps in list(pg_dscp_map.items()):
                 pg_cntrs_base = sai_thrift_read_pg_counters(
@@ -1032,13 +1032,13 @@ class DscpToPgMapping(sai_base_test.ThriftInterfaceDataPlane):
                     # CPU sends LACP/LLDP/BGP control pkts to queue7
                     # So for those queues the count could be more
                     if i == pg:
-                        if i == 0 or i == 4 or i == 7:
+                        if i == 7:
                             assert (pg_cntrs[pg] >= pg_cntrs_base[pg] + len(dscps))
                         else:
                             assert (pg_cntrs[pg] ==
                                     pg_cntrs_base[pg] + len(dscps))
                     else:
-                        if i == 0 or i == 4 or i == 7:
+                        if i == 7:
                             assert(pg_cntrs[i] >= pg_cntrs_base[i])
                         else:
                             assert (pg_cntrs[i] == pg_cntrs_base[i])
