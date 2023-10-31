5114da339bd33dbfed315d1ff3dcb65ee0e3b2d1
ganglyu <ganglv@microsoft.com>
2023-10-31 11:07:55 +0800

    Address review comment

--- tests/common/helpers/gnmi_utils.py
+++ tests/common/helpers/gnmi_utils.py
@@ -1,27 +1,29 @@
 from functools import lru_cache
 import pytest
 
+
 @lru_cache(maxsize=None)
 class GNMIEnvironment(object):
     TELEMETRY_MODE = 0
     GNMI_MODE = 1
+
     def __init__(self, duthost, mode):
         if mode == self.TELEMETRY_MODE:
             ret = self.generate_telemetry_config(duthost)
-            if ret == True:
+            if ret:
                 return
             ret = self.generate_gnmi_config(duthost)
-            if ret == True:
+            if ret:
                 return
         elif mode == self.GNMI_MODE:
             ret = self.generate_gnmi_config(duthost)
-            if ret == True:
+            if ret:
                 return
             ret = self.generate_telemetry_config(duthost)
-            if ret == True:
+            if ret:
                 return
         pytest.fail("Can't generate GNMI/TELEMETRY configuration, mode %d" % mode)
-    
+
     def generate_gnmi_config(self, duthost):
         cmd = "docker images | grep -w sonic-gnmi"
         if duthost.shell(cmd, module_ignore_errors=True)['rc'] == 0:
@@ -35,7 +37,7 @@ class GNMIEnvironment(object):
             else:
                 pytest.fail("GNMI is not running")
         return False
-    
+
     def generate_telemetry_config(self, duthost):
         cmd = "docker images | grep -w sonic-telemety"
         if duthost.shell(cmd, module_ignore_errors=True)['rc'] == 0:
