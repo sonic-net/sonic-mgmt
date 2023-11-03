f935c33554a08005c0299650f8a4f86eac489d68
Chun'ang Li <chunangli@microsoft.com>
2023-11-03 07:24:00 +0000

    Merged PR 3948: [Refine] Enhance testbed health check scripts
    **what**
    [Refine] Enhance testbed health check scripts.
    - Add interfaces status check
    - Add critical containers check
    - Fix and improve BGP sessions check
    - Refine output result and logs
    
    **check result after enhanced**
    - unhealthy
    ![image (17).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2817%29.png)
    ![image (18).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2818%29.png)
    
    - host unreachable
    ![image (11).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2811%29.png)
    ![image (14).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2814%29.png)
    
    - healthy
    ![image (16).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2816%29.png)
    ![image (15).png](https://dev.azure.com/mssonic/12b9cbf4-b1d3-4768-8e49-669345c32e5d/_apis/git/repositories/5380e8f7-6e2a-4154-8dee-f3be7b096894/pullRequests/3948/attachments/image%20%2815%29.png)
    
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- .azure-pipelines/testbed_health_check.py
+++ .azure-pipelines/testbed_health_check.py
@@ -6,7 +6,7 @@
         ./testbed_health_check.py -t vms01 -i ../ansible/str --tbfile ../ansible/testbed.yaml
         -o testbed_health_check_result_vms01 --log-level info
 
-    It will print the basic testbed health check result to the console,
+    It will log the basic testbed health check result to the console,
     if you specify an output file, it will write to it as well.
 """
 import argparse
@@ -29,8 +29,21 @@ from devutil.devices.factory import init_localhost, init_testbed_sonichosts  # n
 logger = logging.getLogger(__name__)
 
 
+def get_timestamp_utcnow():
+    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+
 class ElastictestCommonResponse:
-    def __init__(self, code: int, timestamp: str, errmsg: str, data: object):
+    def __init__(self, code: int, timestamp: str = get_timestamp_utcnow(), errmsg: list = None, data: object = None):
+        """
+        Initialize an instance of the ElastictestCommonResponse class.
+
+        Args:
+            code: int. The return code. '0' indicates success, while any other value indicates failure.
+            timestamp: str. The timestamp when this response object was generated. Default is utcnow().
+            errmsg: list. A list of error messages if the check failed.
+            data: object. The check result.
+        """
         self.code = code
         self.timestamp = timestamp
         self.errmsg = errmsg
@@ -41,87 +54,207 @@ class TestbedCheckResult(ElastictestCommonResponse):
     pass
 
 
-def check_bgp_session_state(sonichosts, state="established"):
+class TestbedHealthCheck:
     """
-    Check if the current BGP session state equals the target state.
-    Args:
-        sonichosts (list): List of SonicHost objects representing the hosts to check.
-        state (str, optional): The target state to compare the BGP session state against. Defaults to "established".
-    Returns:
-        tuple: A tuple containing the following:
-            - bool: True if all BGP sessions are in the target state, False otherwise.
-            - dict or None: The BGP facts dictionary if there are sessions not in the target state, None otherwise.
-            - str or None: An error log message if there are sessions not in the target state, None otherwise.
+    Testbed health check class
     """
 
-    bgp_facts = sonichosts[0].bgp_facts()['ansible_facts']
+    def __init__(self, sonichosts, output_file: str = None):
+        """
+        Initialize an instance of the TestbedHealthCheck class.
 
-    print(json.dumps(bgp_facts, indent=4))
+        Args:
+            sonichosts (list): A list of Sonic hosts to perform health checks on.
+            output_file (str, optional): The output file to store the health check results. Defaults to None.
+        """
+        self.sonichosts = sonichosts
+        self.check_result = TestbedCheckResult(code=0, errmsg=[], data={})
+        self.output_file = output_file
 
-    neigh_not_ok = []
-    for k, v in list(bgp_facts['bgp_neighbors'].items()):
-        if v['state'] != state:
-            neigh_not_ok.append(f"{k}, {v['state']}")
+    def run_check(self):
+        try:
 
-    errlog = "bgp neighbors that not established: {}".format(neigh_not_ok)
+            # Check if critical containers are running
+            self.check_critical_containers_running()
+            if self.check_result.code != 0:
+                logger.info("Check finished. Testbed is unhealthy: {}".format(self.check_result.errmsg))
+                return
 
-    print(errlog)
+            # Check bgp sessions
+            self.check_bgp_session_state()
+            if self.check_result.code != 0:
+                logger.info("Check finished. Testbed is unhealthy: {}".format(self.check_result.errmsg))
+                return
 
-    if len(neigh_not_ok) > 0:
-        return False, bgp_facts, errlog
+            # Check interfaces status
+            self.check_interface_status_of_up_ports()
+            if self.check_result.code != 0:
+                logger.info("Check finished. Testbed is unhealthy: {}".format(self.check_result.errmsg))
+                return
 
-    return True, None, None
+            logger.info("Check finished. Testbed is healthy.")
 
+        except Exception as e:
+            logger.info("Exception occurred during checking: {}".format(e))
+            # todo: currently, host unreachable will raise exception, but others run ansible module fail as well.
+            #  Should figure out and distinguish them in the future.
+            self.check_result.code = 2
+            self.check_result.errmsg = ["Host unreachable."]
+            self.check_result.data = str(e)
 
-def check(sonichosts, output=None):
-    """
-    Perform essential health checks on the given SONiC hosts.
-    Args:
-        sonichosts (list): List of SONiC hosts to check.
-        output (str, optional): Path to the output file where the checking results will be written. Defaults to None.
-    Raises:
-        Exception: If an error occurs during the checking process.
-    """
-    try:
-        # Define variable to record result
-        code = 0
-        data = None
-        errmsg = ""
+        finally:
+            # If output file is specified, write result to it.
+            if self.output_file:
+                with open(self.output_file, "w") as f:
+                    f.write(json.dumps(self.check_result.__dict__, separators=(",", ":")))
 
-        # Testbed health check
-        # todo: currently, simply check bgp session state
-        bgp_session_state, bgp_facts, errlog = check_bgp_session_state(sonichosts)
+    def check_bgp_session_state(self, state="established"):
+        """
+        Check if the current BGP session state equals the target state.
 
-        # If testbed is unhealthy
-        if not bgp_session_state:
-            code = 1
-            data = bgp_facts
-            errmsg = errlog
-            logger.info("Testbed is unhealthy. {}".format(errmsg))
-        else:
-            logger.info("Testbed is healthy.")
+        Args:
+            state: str. The target state to compare the BGP session state against. Defaults to "established".
+        """
 
-        testbedCheckResult = TestbedCheckResult(code=code, timestamp=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                                                errmsg=errmsg, data=data)
+        check_passed = True
+        bgp_facts_on_hosts = {}
 
-        # If output file is specified, write result to it.
-        if output:
-            with open(output, "w") as f:
-                f.write(json.dumps(testbedCheckResult.__dict__, separators=(",", ":")))
+        logger.info("======================= check_bgp_session_state starts =======================")
 
-    except Exception as e:
-        logger.error("Failed to check. {}".format(e))
-        # todo: currently, both host unreachable and bgp container down will come here, mark them as unhealthy now.
-        #  Should figure out and distinguish them in the future.
-        testbedCheckResult = TestbedCheckResult(code=1, timestamp=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                                                data=None,
-                                                errmsg="Unhealthy: host unreachable or bgp container was down")
+        for sonichost in self.sonichosts:
 
-        # If output file is specified, write result to it.
-        if output:
-            with open(output, "w") as f:
-                f.write(json.dumps(testbedCheckResult.__dict__, separators=(",", ":")))
-        # sys.exit(1)
+            hostname = sonichost.hostname
+            logger.info("----------------------- check_bgp_session_state on [{}] -----------------------".format(
+                hostname))
+
+            # Retrieve BGP facts for the Sonic host
+            bgp_facts = sonichost.bgp_facts()['ansible_facts']
+
+            bgp_facts_on_hosts[hostname] = bgp_facts
+
+            # Check BGP session state for each neighbor
+            neigh_not_ok = []
+            for k, v in list(bgp_facts['bgp_neighbors'].items()):
+                if v['state'] != state:
+                    neigh_not_ok.append(f"{k}, {v['state']}")
+
+            errlog = "BGP neighbors that not established on {}: {}".format(hostname, neigh_not_ok)
+
+            logger.info(errlog)
+
+            if len(neigh_not_ok) > 0:
+                # Set the flag check_passed to False if any BGP neighbors are not established
+                check_passed = False
+                # Add errlog to check result errmsg
+                self.check_result.errmsg.append(errlog)
+
+        # Set the check result
+        self.check_result.code = 0 if check_passed else 1
+        self.check_result.data["bgp_facts_on_hosts"] = bgp_facts_on_hosts
+
+        logger.info("======================= check_bgp_session_state ends =======================")
+
+    def check_interface_status_of_up_ports(self):
+        """
+        Check the status of up ports on a list of SonicHost objects representing the DUTs.
+        """
+
+        check_passed = True
+        interface_facts_on_hosts = {}
+
+        logger.info("======================= check_interface_status_of_up_ports starts =======================")
+
+        for sonichost in self.sonichosts:
+
+            hostname = sonichost.hostname
+            logger.info(
+                "----------------------- check_interface_status_of_up_ports on [{}] -----------------------".format(
+                    hostname))
+
+            # Retrieve the basic facts of the DUT
+            dut_basic_facts = sonichost.dut_basic_facts()["ansible_facts"]["dut_basic_facts"]
+
+            # todo: Skip multi_asic check on multi_asic dut now because currently not support get asic object
+            if dut_basic_facts["is_multi_asic"]:
+                logger.info('Not support to check interfaces status on multi-asic DUT now.')
+                interface_facts_on_hosts[hostname] = 'Not support to check interfaces status on multi-asic DUT now.'
+                continue
+
+            # Retrieve the configuration facts for the DUT
+            cfg_facts = sonichost.config_facts(host=hostname, source='running')['ansible_facts']
+
+            # Get a list of up ports from the configuration facts
+            up_ports = [p for p, v in list(cfg_facts['PORT'].items()) if v.get('admin_status', None) == 'up']
+
+            logger.info('up_ports: {}'.format(up_ports))
+
+            # Retrieve the interface facts for the up ports
+            interface_facts = sonichost.interface_facts(up_ports=up_ports)['ansible_facts']
+
+            interface_facts_on_hosts[hostname] = interface_facts
+
+            errlog = 'ansible_interface_link_down_ports on {}: {}'.format(
+                hostname, interface_facts['ansible_interface_link_down_ports'])
+
+            logger.info(errlog)
+
+            # Check if there are any link down ports in the interface facts
+            if len(interface_facts['ansible_interface_link_down_ports']) > 0:
+                # Set the flag check_passed to False if any BGP neighbors are not established
+                check_passed = False
+                # Add errlog to check result errmsg
+                self.check_result.errmsg.append(errlog)
+
+        # Set the check result
+        self.check_result.code = 0 if check_passed else 1
+        self.check_result.data["interface_facts_on_hosts"] = interface_facts_on_hosts
+
+        logger.info("======================= check_interface_status_of_up_ports ends =======================")
+
+    def check_critical_containers_running(self, critical_containers: list = None):
+        """
+        Check if critical containers are running on a list of SonicHost objects representing the DUTs.
+
+        Args:
+            critical_containers: list. A list of critical container names to check. Default is None.
+        """
+
+        # Set default critical containers to check
+        if not critical_containers:
+            critical_containers = ["syncd", "swss", "bgp"]
+
+        check_passed = True
+
+        logger.info("======================= check_critical_containers_running starts =======================")
+
+        for sonichost in self.sonichosts:
+
+            hostname = sonichost.hostname
+            logger.info(
+                "----------------------- check_critical_containers_running on [{}] -----------------------".format(
+                    hostname))
+
+            # Get the list of running containers on the host
+            running_containers = sonichost.shell(r"docker ps -f 'status=running' --format \{\{.Names\}\}")[
+                'stdout_lines']
+
+            for critical_container in critical_containers:
+
+                # If the critical container is not running, add an error log
+                if critical_container not in running_containers:
+                    # Set the flag check_passed to False if any critical container is not running
+                    check_passed = False
+
+                    errlog = "{} is not running on {}.".format(critical_container, hostname)
+                    logger.info(errlog)
+
+                    # Add errlog to check result errmsg
+                    self.check_result.errmsg.append(errlog)
+
+        # Set the check result
+        self.check_result.code = 0 if check_passed else 1
+
+        logger.info("======================= check_critical_containers_running ends =======================")
 
 
 def validate_args(args):
@@ -153,7 +286,8 @@ def main(args):
         sys.exit(1)
 
     logger.info("Checking")
-    check(sonichosts, args.output)
+    testbedHealthCheck_instance = TestbedHealthCheck(sonichosts=sonichosts, output_file=args.output)
+    testbedHealthCheck_instance.run_check()
 
 
 if __name__ == "__main__":
