9b13ec9d17e567fd049150ec9403736b965119e2
Azarack <jethro888222@yahoo.com>
2023-11-03 19:33:10 +0000

    adjusting waiting

--- tests/macsec/test_chassis_reboot.py
+++ tests/macsec/test_chassis_reboot.py
@@ -1,4 +1,5 @@
-'''This script is to test the the chassis reboot behavior for SONiC.
+'''
+This script is to test the the chassis reboot behavior for SONiC.
 
 Step 1: Configure Macsec between neighbor and DUT
 Step 2: Save config on DUT and reboot
@@ -8,6 +9,7 @@ Step 3: Verify macsec connection is re-established after reboot
 
 import logging
 import pytest
+from time import sleep
 from tests.common import reboot
 from tests.common.helpers.assertions import pytest_assert
 
@@ -24,7 +26,8 @@ def test_chassis_reboot(duthost, enum_rand_one_frontend_asic_index, duthosts, lo
     dut_lldp_table = duthost.shell("show lldp table")['stdout'].split("\n")[3].split()
     dut_to_neigh_int = dut_lldp_table[0]
     rphost.shell("config save -y")
-    reboot(rphost, localhost, wait=240)
+    reboot(rphost, localhost, wait=200)
+    sleep(60)  # wait extra for macsec to establish
 
     space_var = ""
     if asic_index is not None:
