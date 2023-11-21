04ccf5e964682a55b3ce2c3ce8130ab00d05eced
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2023-11-20 22:40:25 -0800

    Update test_memory_checker to support gnmi and telemetry containers (#10748)

--- tests/memory_checker/test_memory_checker.py
+++ tests/memory_checker/test_memory_checker.py
@@ -256,19 +256,20 @@ def remove_and_restart_container(memory_checker_dut_and_container):
     container.post_check()
 
 
-@pytest.fixture(params=['telemetry'])
-def enum_memory_checker_container(request):
-    return request.param
+def get_test_container(duthost):
+    test_container = "telemetry"
+    cmd = "docker images | grep -w sonic-gnmi"
+    if duthost.shell(cmd, module_ignore_errors=True)['rc'] == 0:
+        test_container = "gnmi"
+    return test_container
 
 
 @pytest.fixture
-def memory_checker_dut_and_container(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
-                                     enum_memory_checker_container):
+def memory_checker_dut_and_container(duthosts, enum_rand_one_per_hwsku_frontend_hostname):
     """Perform some checks and return applicable duthost and container name
 
     Args:
         duthosts: The fixture returns list of DuTs.
-        enum_memory_checker_container: Fixture returning the name of the container to test
         enum_rand_one_per_hwsku_frontend_hostname: The fixture randomly pick up
           a frontend DuT from testbed.
 
@@ -276,7 +277,8 @@ def memory_checker_dut_and_container(duthosts, enum_rand_one_per_hwsku_frontend_
         (duthost, container)
     """
     duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-    container_name = enum_memory_checker_container
+
+    container_name = get_test_container(duthost)
     container = MemoryCheckerContainer(container_name, duthost)
 
     pytest_require("Celestica-E1031" not in duthost.facts["hwsku"]
@@ -399,6 +401,7 @@ class MemoryCheckerContainer(object):
     # NOTE: these limits could be computed by reading the monit_$container config
     MEMORY_LIMITS = {
         'telemetry': 400 * 1024 * 1024,
+        'gnmi': 400 * 1024 * 1024,
     }
 
     def __init__(self, name, duthost):
@@ -461,6 +464,8 @@ class MemoryCheckerContainer(object):
 
     def get_restart_expected_logre(self):
         cap_name = self.name.capitalize()
+        if self.name == "gnmi":
+            cap_name = "GNMI"
         return [
             r".*restart_service.*Restarting service '{}'.*".format(self.name),
             r".*Stopping {} container.*".format(cap_name),
