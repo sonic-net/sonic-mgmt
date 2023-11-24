0d5fe09f8b74a7aadb4020bf0abad8773f536dd0
Anshul Dubela <andubela@cisco.com>
2023-11-24 11:10:40 -0800

    MIGSOFTWAR-9879 test_snmp_fdb_dmac_nmatch
    Signed-off-by: Anshul Dubela <andubela@cisco.com>

--- sonic-mgmt/tests/snmp/test_snmp_fdb.py
+++ sonic-mgmt/tests/snmp/test_snmp_fdb.py
@@ -104,7 +104,7 @@ def test_snmp_fdb_send_tagged(ptfadapter, duthosts, rand_one_dut_hostname,      
     # Flush dataplane
     ptfadapter.dataplane.flush()
 
-    time.sleep(10)
+    time.sleep(20)
     hostip = duthost.host.options['inventory_manager'].get_host(
         duthost.hostname).vars['ansible_host']
     snmp_facts = get_snmp_facts(
