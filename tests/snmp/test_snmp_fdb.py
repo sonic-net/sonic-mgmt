4757bf20d6e465a541dcf444ae9d0cef5bfb9764
Anshul Dubela <anshuldubela2009@gmail.com>
2023-11-25 11:57:28 +0530

    Update test_snmp_fdb.py
    Removed the empty line

--- tests/snmp/test_snmp_fdb.py
+++ tests/snmp/test_snmp_fdb.py
@@ -105,7 +105,6 @@ def test_snmp_fdb_send_tagged(ptfadapter, duthosts, rand_one_dut_hostname,      
     ptfadapter.dataplane.flush()
 
     time.sleep(20)
-
     hostip = duthost.host.options['inventory_manager'].get_host(
         duthost.hostname).vars['ansible_host']
     snmp_facts = get_snmp_facts(
