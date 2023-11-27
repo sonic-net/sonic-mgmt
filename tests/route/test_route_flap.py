beee88713b2ffd0d986908574e30b39b0cef90bd
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2023-11-27 08:27:41 -0800

    Update test_route_flap.py (#10814)

--- tests/route/test_route_flap.py
+++ tests/route/test_route_flap.py
@@ -255,7 +255,7 @@ def get_dev_port_and_route(duthost, asichost, dst_prefix_set):
                     if per_hop['interfaceName'] in internal_intfs:
                         continue
                     port = per_hop['interfaceName']
-                    neigh = duthost.shell("show ip int | grep -w {}".format(port))['stdout']
+                    neigh = duthost.shell("show ip int | grep -w {}".format(port), module_ignore_errors=True)['stdout']
                     if neigh == '':
                         logger.info("{} is still internal interface, skipping".format(port))
                     else:
