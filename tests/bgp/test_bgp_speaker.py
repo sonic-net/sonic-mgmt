4dc1b13835d6c5174589eca98d85f06f139cd3e8
Chuan Wu <103085864+echuawu@users.noreply.github.com>
2023-11-27 20:58:23 +0800

    Use python3 when invoking ptf_runner in test_bgp_speaker (#10847)
    In the test_bgp_speaker.py, there is no is_python3=True option when invoking ptf_runner function.
    Add is_python3=True option in ptf_runner function invoking.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/bgp/test_bgp_speaker.py
+++ tests/bgp/test_bgp_speaker.py
@@ -337,7 +337,8 @@ def bgp_speaker_announce_routes_common(common_setup_teardown, tbinfo, duthost,
                            "asic_type": asic_type,
                            "test_balancing": False},
                    log_file="/tmp/bgp_speaker_test.FibTest.log",
-                   socket_recv_size=16384)
+                   socket_recv_size=16384,
+                   is_python3=True)
 
     logger.info("Withdraw routes")
     withdraw_route(ptfip, lo_addr, prefix, nexthop_ips[1].ip, port_num[0])
