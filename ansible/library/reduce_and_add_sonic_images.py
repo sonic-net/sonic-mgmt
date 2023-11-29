f0b55494c3175061b79d285549d84a02336351da
Xin Wang <xiwang5@microsoft.com>
2023-11-29 14:01:49 +0800

    Fix style issue

--- ansible/library/reduce_and_add_sonic_images.py
+++ ansible/library/reduce_and_add_sonic_images.py
@@ -212,7 +212,6 @@ def install_new_sonic_image(module, new_image_url, save_as=None):
         )
 
 
-
 def work_around_for_slow_disks(module):
     # Increase hung task timeout to 600 seconds to avoid kernel panic
     # while writing lots of data to a slow disk.
