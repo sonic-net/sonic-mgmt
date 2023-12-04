52201e75cd5f034cb17eb2ff2998fa5c8906fb6e
Alpesh S Patel <alpesh@cisco.com>
2023-12-04 17:58:42 +0000

    review comments
    Signed-off-by: Alpesh S Patel <alpesh@cisco.com>

--- sonic-mgmt/tests/cisco/qos/test_cisco8000_buf_qos.py
+++ sonic-mgmt/tests/cisco/qos/test_cisco8000_buf_qos.py
@@ -8,10 +8,10 @@ import logging
 pid_sku = [{"x86_64-8102_64h_o-r0" : ["Cisco-8102-C64"]},
            {"x86_64-8101_32h_o-r0" : ["32x100Gb"]},
            {"x86_64-8111_32eh_o-r0": ["Cisco-8111-O32", "Cisco-8111-O64", "Cisco-8111-O62C2"]},
-           {"x86_64-88_lc0_36fh_mo-r0" : [{"Cisco-88-LC0-36FH-M-O36": [0,1,2]}]},
-           {"x86_64-88_lc0_36fh_m-r0" : [{"Cisco-88-LC0-36FH-M-O36": [0,1,2]}]},
-           {"x86_64-88_lc0_36fh_o-r0" : [{"Cisco-88-LC0-36FH-O36": [0,1,2]}]},
-           {"x86_64-88_lc0_36fh-r0" : [{"Cisco-88-LC0-36FH-O36": [0,1,2]}]},
+           {"x86_64-88_lc0_36fh_mo-r0" : [("Cisco-88-LC0-36FH-M-O36", [0,1,2])]},
+           {"x86_64-88_lc0_36fh_m-r0" : [("Cisco-88-LC0-36FH-M-O36", [0,1,2])]},
+           {"x86_64-88_lc0_36fh_o-r0" : [("Cisco-88-LC0-36FH-O36", [0,1,2])]},
+           {"x86_64-88_lc0_36fh-r0" : [("Cisco-88-LC0-36FH-O36", [0,1,2])]},
            {"x86_64-8101_32fh_o-r0" : ["32x400Gb", "Cisco-8101-C64", "Cisco-8101-O32", "Cisco-8101-O8C48", "Cisco-DSF-8101-32FH"]},
            {"x86_64-8800_rp_o-r0" : [""]}
            ]
@@ -23,41 +23,33 @@ def print_rc(cmd, rc):
         logging.error("Failed :{}".format(cmd))
 
 
+def get_pid_sku(pid_sku):
+    for pid in pid_sku:
+        for sku in pid_sku[pid]:
+            if isinstance(sku, tuple):
+                t2_sku, t2_asics = sku
+                for asic in t2_asics:
+                    path = "{}/{}/{}".format(pid, t2_sku, str(asic))
+                    logging.info("path {} sku {} asic {}".format(path, t2_sku, str(asic)))
+                    yield path, t2_sku
+            else:
+                path = "{}/{}".format(pid, sku)
+                logging.info("path {} sku {}".format(path, sku))
+                yield path, sku
+
+
 def run_mmu_config(duthost, raw_pid_sku, mmucommand):
-    def get_pid_sku(pid_sku):
-        for pid in (pid_sku):
-            v =  pid_sku[pid]
-            if list(v):
-                for sku in v:
-                    if isinstance(sku, dict):
-                        t2_sku, t2_asic = next(iter(sku.items()))
-                        for asic in t2_asic:
-                            path="{}/{}/{}".format(pid, t2_sku, str(asic))
-                            logging.info("path {} sku {} asic {}".format(path, t2_sku, asic))
-                            yield path, t2_sku
-                    else:
-                        path="{}/{}".format(pid, str(sku))
-                        logging.info("path {} sku {}".format(path, sku))
-                        yield path, sku
-    #
-    prefix='/usr/share/sonic/device'
-    output_file='/tmp/_cfggen_'
-    for item in get_pid_sku(raw_pid_sku):
-        pidpath = item[0]
-        sku = item[1]
-        if sku:
-            cfggen = 'sonic-cfggen -t {}/{}/{} -k {} > {}'.format(prefix, pidpath, mmucommand, sku, output_file)
-        else:
-            cfggen = 'sonic-cfggen -t {}/{}/{} -k {} > {}'.format(prefix, pidpath, mmucommand, '""', output_file)
-        #
+    prefix = '/usr/share/sonic/device'
+    output_file = '/tmp/_cfggen_'
+    for pidpath, sku in get_pid_sku(raw_pid_sku):
+        cfggen = 'sonic-cfggen -t {}/{}/{} -k {} > {}'.format(prefix, pidpath, mmucommand, sku if sku else '""', output_file)
         logging.info("executing {} for pid/sku {}".format(cfggen, pidpath))
         try:
             rc = duthost.shell(cfggen)
             print_rc(cfggen, rc)
         except:
             pytest.fail("{} failed for pid/sku {}".format(mmucommand, pidpath))
-        # 
-        json_cmd='jq < {}'.format(output_file)
+        json_cmd = 'jq < {}'.format(output_file)
         logging.info("executing {} for pid/sku {}".format(json_cmd, pidpath))
         try:
             rc = duthost.shell(json_cmd)
@@ -86,21 +78,21 @@ aiml_special_cfg = {'DEVICE_METADATA|localhost':
 
 class AIML_Config:
   def __init__(self, duthost, aiml_special_cfg, mmucommand):
-      for t in (aiml_special_cfg):
+      for t in aiml_special_cfg:
           valdict = aiml_special_cfg[t]
           for k in valdict:
-              getcmd="redis-cli -n 4 HGET '{}' {}".format(t, k)
+              getcmd = "redis-cli -n 4 HGET '{}' {}".format(t, k)
               logging.info("executing {} ".format(getcmd))
               try:
                   rc = duthost.shell(getcmd)
                   print_rc(getcmd, rc)
               except:
                   pytest.fail("{} failed ".format(getcmd))
-              v_orig=rc['stdout']
+              v_orig = rc['stdout']
               setattr(self, k, v_orig)
               logging.info("for key '{}' saving value '{}'".format(k, v_orig))
-              new_val=valdict[k]
-              setcmd="redis-cli -n 4 HSET '{}' {} '{}'".format(t, k, new_val)
+              new_val = valdict[k]
+              setcmd = "redis-cli -n 4 HSET '{}' {} '{}'".format(t, k, new_val)
               duthost.shell(setcmd) # first set return an error
               logging.info("executing {} ".format(setcmd))
               try:
@@ -112,12 +104,11 @@ class AIML_Config:
 
   def reset_config(self, duthost, aiml_special_cfg):
       # reset the original config
-      for t in (aiml_special_cfg):
-          v = aiml_special_cfg[t]
-          for k in v:
+      for t in aiml_special_cfg:
+          for k in aiml_special_cfg[t]:
               val = getattr(self, k)
               logging.info("for key '{}' setting retrieved value '{}'".format(k, val))
-              setcmd="redis-cli -n 4 HSET '{}' {} '{}'".format(t, k, val)
+              setcmd = "redis-cli -n 4 HSET '{}' {} '{}'".format(t, k, val)
               logging.info("executing {} ".format(setcmd))
               try:
                   rc = duthost.shell(setcmd)
