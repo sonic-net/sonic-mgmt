80dba4e704960b829b3cd5020ac66d2973367eef
Thushar Gowda <thgowda@cisco.com>
2023-12-06 15:50:04 -0800

    Add verification

--- sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_l3vni_basic.py
+++ sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_l3vni_basic.py
@@ -16,7 +16,16 @@ def box_service_module_hooks(request):
 def box_service_func_hooks(request):
     yield
 
+# TODO: Parameterize the configs. For now, use static configs
 CONFIGS_FILE = 'vxlan_l3vni_configs.yaml'
+
+LEAF0_VXLAN_IP = '10.200.200.200'
+LEAF1_VXLAN_IP = '10.200.200.201'
+SPINE0_VXLAN_IP = '10.200.200.210'
+
+LEAF0_BGP_RTRID = '2363073663'
+LEAF1_BGP_RTRID = '2363033449'
+SPINE0_BGP_RTRID = '2363070545'
 
 def setup_node(node, config, type=''):
     if type:
@@ -44,6 +53,7 @@ def setup_node(node, config, type=''):
 #                                                                  #
 ####################################################################
 def test_l3vni_basic_config():
+    VNI = '1000'
 
     vars = st.get_testbed_vars()
 
@@ -61,9 +71,44 @@ def test_l3vni_basic_config():
             setup_node(nodes[node], config['sonic'], '')
             setup_node(nodes[node], config['bgp'], 'vtysh')
 
+    # Make sure links are up by pinging, sometimes packet exchange doesn't happen on sim till pings are initiated
     count = 5
     st.show(nodes['leaf0'], "sudo ping -c {} {} -q".format(count, '11.11.11.1'))
     st.show(nodes['leaf1'], "sudo ping -c {} {} -q".format(count, '11.11.12.1'))
 
-    
-    st.report_pass("test_case_passed")
+    leaf0_vrf_prefix = '100.100.100.0'
+    leaf1_vrf_prefix = '100.100.101.0'
+
+    leaf0_output = st.show(nodes['leaf0'], "show bgp l2vpn evpn {}".format(leaf1_vrf_prefix), type="vtysh")
+
+    leaf0_parsed = st.parse_show(nodes['leaf0'], "show bgp l2vpn evpn {}".format(leaf1_vrf_prefix),
+                                 leaf0_output, "show_bgp_l2vpn_evpn_prefix.tmpl")
+
+    leaf1_output = st.show(nodes['leaf1'], "show bgp l2vpn evpn {}".format(leaf0_vrf_prefix), type="vtysh")
+
+    leaf1_parsed = st.parse_show(nodes['leaf1'], "show bgp l2vpn evpn {}".format(leaf0_vrf_prefix),
+                                 leaf1_output, "show_bgp_l2vpn_evpn_prefix.tmpl")
+
+    if len(leaf0_parsed) == 0:
+        st.report_fail("Found no prefixes advertised to Leaf0")
+
+    if len(leaf1_parsed) == 0:
+        st.report_fail("Found no prefixes advertised to Leaf1")
+
+    for path in leaf0_parsed:
+        if path['valid'] != 'valid':
+            st.report_fail("Invalid path found in leaf0")
+        if path['pathevpntype'] != '5':
+            st.report_fail("Invalid evpn type {} found in leaf0".format(path['evpntype']))
+        if path['vni'] != '1000':
+            st.report_fail("Incorrect vni found in leaf0")
+
+    for path in leaf1_parsed:
+        if path['valid'] != 'valid':
+            st.report_fail("Invalid path found in leaf0")
+        if path['pathevpntype'] != '5':
+            st.report_fail("Invalid evpn type {} found in leaf0".format(path['evpntype']))
+        if path['vni'] != '1000':
+            st.report_fail("Incorrect vni found in leaf0")
+
+    st.report_pass("Basic L3VNI test passed")
