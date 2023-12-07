1943f5bbdcff57db626d6c9d67f337da2c468945
bshpigel <152057803+bshpigel@users.noreply.github.com>
2023-12-07 09:09:59 +0200

    Update loganalyzer.py
    According to Roy Sror's comment - the error file name will contain the time and test name

--- tests/common/plugins/loganalyzer/loganalyzer.py
+++ tests/common/plugins/loganalyzer/loganalyzer.py
@@ -163,7 +163,8 @@ class LogAnalyzer:
 
             tmp_folder = "/tmp/loganalyzer/{}".format(self.ansible_host.hostname)
             os.makedirs(tmp_folder, exist_ok=True)
-            file_path = os.path.join(tmp_folder, "log_error.json")
+            cur_time = time.strftime("%d_%m_%Y_%H_%M_%S", time.gmtime())
+            file_path = os.path.join(tmp_folder, f"log_error_{self.marker_prefix}_{cur_time}.json")
             logging.info("Log errors will be saved in file: {}".format(file_path))
             data = {'log_errors': log_errors}
             with open(file_path, "w+") as file:
