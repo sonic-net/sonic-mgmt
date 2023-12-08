6ba6ea149ffed0abb6541cb1c38d0686e20ef04a
mannytaheri <86314901+mannytaheri@users.noreply.github.com>
2023-12-07 20:12:15 -0500

    Add docker0's IPv6 address since it was removed when disabling IPv6 (#10651)
    * Add docker0's IPv6 address since it was removed when disabling IPv6
    
    * Add docker0's IPv6 address since it was removed when disabling IPv6
    
    * Add docker0's IPv6 address since it was removed when disabling IPv6

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -25,6 +25,7 @@ from tests.common.utilities import wait_until
 from tests.ptf_runner import ptf_runner
 from tests.common.system_utils import docker
 from tests.common.errors import RunAnsibleModuleFail
+from tests.common import config_reload
 
 logger = logging.getLogger(__name__)
 
@@ -1539,12 +1540,18 @@ class QosSaiBase(QosBase):
     @pytest.fixture(scope='class', autouse=True)
     def dut_disable_ipv6(self, duthosts, get_src_dst_asic_and_duts, tbinfo, lower_tor_host):
         for duthost in get_src_dst_asic_and_duts['all_duts']:
+            docker0_ipv6_addr = \
+                duthost.shell("sudo ip -6  addr show dev docker0 | grep global" + " | awk '{print $2}'")[
+                    "stdout_lines"][0]
             duthost.shell("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
 
         yield
 
         for duthost in get_src_dst_asic_and_duts['all_duts']:
             duthost.shell("sysctl -w net.ipv6.conf.all.disable_ipv6=0")
+            logger.info("Adding docker0's IPv6 address since it was removed when disabing IPv6")
+            duthost.shell("ip -6 addr add {} dev docker0".format(docker0_ipv6_addr))
+            config_reload(duthost, config_source='config_db', safe_reload=True, check_intf_up_ports=True)
 
     @pytest.fixture(scope='class', autouse=True)
     def sharedHeadroomPoolSize(
