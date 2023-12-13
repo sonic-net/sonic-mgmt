ab8e3471222d9357146e80601e891ba8e84e8f6e
Azarack <jethro888222@yahoo.com>
2023-12-13 16:04:03 +0000

    change to random frontend dut

--- tests/bgp/test_ipv6_nlri_over_ipv4.py
+++ tests/bgp/test_ipv6_nlri_over_ipv4.py
@@ -20,11 +20,11 @@ pytestmark = [
 
 
 @pytest.fixture(scope='module')
-def setup(tbinfo, nbrhosts, duthosts, rand_one_dut_hostname, request):
+def setup(tbinfo, nbrhosts, duthosts, enum_frontend_dut_hostname, request):
     # verify neighbors are type sonic
     if request.config.getoption("neighbor_type") != "sonic":
         pytest.skip("Neighbor type must be sonic")
-    duthost = duthosts[rand_one_dut_hostname]
+    duthost = duthosts[enum_frontend_dut_hostname]
     dut_asn = tbinfo['topo']['properties']['configuration_properties']['common']['dut_asn']
 
     lldp_table = duthost.shell("show lldp table")['stdout'].split("\n")[3].split()
