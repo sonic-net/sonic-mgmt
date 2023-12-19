7bec9d0aa4618e10f4cda39d8cc8db491998f8cd
Sai Raghavendra Nitin Kalavala <nkalaval@cisco.com>
2023-12-19 10:08:58 -0800

    Adding deconfig flow and cleanup of existing test cases

--- sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_vxlan_l2vni_v6_vtep_basic.py
+++ sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_vxlan_l2vni_v6_vtep_basic.py
@@ -17,6 +17,7 @@ import apis.system.box_services as boxserv_obj
 
 V6_VTEP_CONFIG_FILE = 'vxlan_v6_vtep_configs.yaml'
 
+REMOTE_VTEP_COUNT = '1'
 SPINE0_VTEP_IP = 'fd27::2cb:8b5a:196'
 SPINE1_VTEP_IP = 'fd27::234:377f:6b3'
 LEAF0_VTEP_IP  = 'fd27::280:10f1:25f'
@@ -30,9 +31,65 @@ def box_service_module_hooks(request):
     dut_list = [vars.D1, vars.D2, vars.D3, vars.D4]
     yield
 
+
 @pytest.fixture(scope="function", autouse=True)
 def box_service_func_hooks(request):
     yield
+
+
+def config_node(node, config, type='', skip_errors=False):
+    if type:
+        st.config(node, config, type=type, skip_error_check = skip_errors, conf=True)
+    else:
+        st.config(node, config, skip_error_check = skip_errors, conf=True)
+
+
+def report_fail(dut, msg=''):
+    st.log(msg, dut)
+    st.error(msg, dut)
+    st.report_fail(msg, dut)
+
+
+def config_static(node, config_domain, add=True):
+    vars = st.get_testbed_vars()
+
+    nodes = {}
+    nodes['leaf0'] = vars.D1
+    nodes['leaf1'] = vars.D2
+    nodes['spine0'] = vars.D3
+    nodes['spine1'] = vars.D4
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    domain = ''
+    if config_domain == 'bgp':
+        domain = 'vtysh'
+
+    with open(dir_path + '/' + V6_VTEP_CONFIG_FILE) as c:
+        config_list = yaml.load(c, Loader=yaml.FullLoader)
+        if add:
+            config_node(nodes[node], config_list[node][config_domain]['config'], domain)
+        else:
+            config_node(nodes[node], config_list[node][config_domain]['deconfig'], domain,  skip_errors=True)
+
+
+@pytest.fixture()
+def setup_and_teardown():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    with open(dir_path + '/' + V6_VTEP_CONFIG_FILE) as c:
+        config_list = yaml.load(c, Loader=yaml.FullLoader)
+        for node, config in config_list.items():
+            config_static(node, 'sonic')
+            config_static(node, 'bgp')
+
+    yield 'setup_and_teardown'
+
+    with open(dir_path + '/' + V6_VTEP_CONFIG_FILE) as c:
+        config_list = yaml.load(c, Loader=yaml.FullLoader)
+        for node, config in config_list.items():
+            config_static(node, 'bgp', add=False)
+            config_static(node, 'sonic', add=False)
 
 
 def verify_vtep_state(dut, expected_sip, expected_dip):
@@ -46,81 +103,52 @@ def verify_vtep_state(dut, expected_sip, expected_dip):
     Total count : 1
 
     '''
-    output = st.show(dut, "show vxlan remotevtep")
-    output_parsed = st.parse_show(dut, "show vxlan remotevtep", output, "show_vxlan_remotevtep.tmpl")
+    output = st.config(dut, "show vxlan remotevtep")
+    output_parsed = st.parse_show(dut, "show vxlan remotevtep", output, "show_vxlan_remote.tmpl")
 
     for vtep in output_parsed:
         # Test 1: Verify if the State is UP - oper_up
         if vtep['tun_status'] == 'oper_up':
-            st.log("Tunnel State oper_up UP", dut)
+            st.log("Tunnel State is up. Status : oper_up", dut)
         else:
-            st.log("Tunnel State Error: NOT oper_up",dut)
-            st.error("Tunnel State Error: NOT oper_up",dut)
-            st.report_fail("test_case_failed",dut)
+            report_fail(dut, msg='Tunnel State is not up. Status : oper_down')
 
         # Test 2: Verify SIP and DIP
         if vtep['src_vtep'] == expected_sip:
             st.log("Source vtep validated", dut)
         else:
-            st.log("Source vtep expected {} found {} ".format(expected_sip, vtep['src_vtep']), dut)
-            st.error("Source vtep expected {} found {} ".format(expected_sip, vtep['src_vtep']), dut)
-            st.report_fail("test_case_failed",dut)
-
+            report_fail(dut, msg='Source vtep is not as expected. Found {} Expected {}'.format(vtep['src_vtep'], expected_sip))
         if vtep['dst_vtep'] == expected_dip:
             st.log("Destination vtep validated", dut)
         else:
-            st.log("Destination vtep expected {} found {} ".format(expected_dip, vtep['dst_vtep']), dut)
-            st.error("Destination vtep expected {} found {} ".format(expected_dip, vtep['dst_vtep']), dut)
-            st.report_fail("test_case_failed",dut)
+            report_fail(dut, msg='Source vtep is not as expected. Found {} Expected {}'.format(vtep['dst_vtep'], expected_dip))
 
         # Test 3: Verify if the Total Count is 1
-        if vtep['total_count'] == 1:
+        if vtep['total_count'] == REMOTE_VTEP_COUNT:
             st.log("All remote VTEPs detected", dut)
         else:
-            st.log("All remote VTEPs have not been discovered. Discovered {} VTEPs".format(vtep['total_count']), dut)
-            st.error("All remote VTEPs have not been discovered. Discovered {} VTEPs".format(vtep['total_count']), dut)
-            st.report_fail("test_case_failed",dut)
+            report_fail(dut, msg='Remote Vteps discovered count not as expected. Found {} Expected {}'.format(vtep['total_count'], REMOTE_VTEP_COUNT))
 
-
-def setup_node(node, config, type=''):
-    if type:
-        st.config(node, config, type=type, skip_error_check=False, conf=True)
-    else:
-        st.config(node, config, skip_error_check=False, conf=True)
-    st.wait(2)
-
-def cleanup_node(node, config, type=''):
-    if type:
-        st.config(node, config, type=type, skip_error_check=False, conf=True)
-    else:
-        st.config(node, config, skip_error_check=False, conf=True)
-    st.wait(2)
 
 @pytest.mark.system_box
 @pytest.mark.community
 @pytest.mark.community_pass
-def test_v6_vtep_basic():
-    vars   = st.get_testbed_vars()
+def test_v6_vtep_basic(setup_and_teardown):
+    vars = st.get_testbed_vars()
 
     nodes = {}
-    nodes['leaf0']  = vars.D1
-    nodes['leaf1']  = vars.D2
+    nodes['leaf0'] = vars.D1
+    nodes['leaf1'] = vars.D2
     nodes['spine0'] = vars.D3
     nodes['spine1'] = vars.D4
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # sleep for 40 seconds for BGP to converge
+    st.wait(40)
 
-    with open(dir_path + '/' + V6_VTEP_CONFIG_FILE) as c:
-        config_list = yaml.load(c, Loader=yaml.FullLoader)
-        for node, config in config_list.items():
-            setup_node(nodes[node], config['sonic'], '')
-            st.wait(10)
-            setup_node(nodes[node], config['bgp'], 'vtysh')
-            st.wait(10)
-
-    st.wait(30)
+    # Test remote vtep status on LEAF0
     verify_vtep_state(nodes['leaf0'], LEAF0_VTEP_IP, LEAF1_VTEP_IP)
-    st.report_pass("VTEP check on LEAF0 passed", dut)
+    st.report_pass("test_case_passed", "for leaf0")
 
+    # Test remote vtep status on LEAF1
     verify_vtep_state(nodes['leaf1'], LEAF1_VTEP_IP, LEAF0_VTEP_IP)
-    st.report_pass("VTEP check on LEAF1 passed", dut)
+    st.report_pass("test_case_passed", "for leaf1")
