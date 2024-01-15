ab12fe2a08d689ba037fb58c69e2e2029317e1c0
liuh-80 <liuh@microsoft.com>
2024-01-15 09:01:52 +0000

    Improve code

--- tests/tacacs/test_authorization.py
+++ tests/tacacs/test_authorization.py
@@ -651,15 +651,15 @@ def test_fallback_to_local_authorization_with_config_reload(
     tacacs_passkey = tacacs_creds[duthost.hostname]['tacacs_passkey']
     override_config = {
         "AAA": {
-            "authentication": { "login": "tacacs+" },
-            "accounting": { "login": "tacacs+,local" },
-            "authorization": { "login": "tacacs+,local" }
+            "authentication": {"login": "tacacs+"},
+            "accounting": {"login": "tacacs+,local"},
+            "authorization": {"login": "tacacs+,local"}
         },
         "TACPLUS": {
-            "global": { "auth_type": "login", "passkey": tacacs_passkey }
+            "global": {"auth_type": "login", "passkey": tacacs_passkey}
         },
         "TACPLUS_SERVER": {
-            tacacs_server_ip: { "priority": "60", "tcp_port": "49", "timeout": "2" }
+            tacacs_server_ip: {"priority": "60", "tcp_port": "49", "timeout": "2"}
         }
     }
     reload_minigraph_with_golden_config(duthost, override_config)
