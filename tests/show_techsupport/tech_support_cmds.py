5dd273e19d9c184bdb1bc83668d4d8d9783cda31
suresh.vemula <115137866+vesuresh537@users.noreply.github.com>
2024-01-16 15:10:46 +0530

    Update tech_support_cmds.py
    ading show command "show interface transceiver lpmode"

--- tests/show_techsupport/tech_support_cmds.py
+++ tests/show_techsupport/tech_support_cmds.py
@@ -147,6 +147,7 @@ misc_show_cmds = [
     "show version",
     "show interface status -d all",
     "show interface transceiver presence",
+    "show interface transceiver lpmode",
     "show interface transceiver eeprom --dom",
     "show ip interface",
     "show interface counters",
