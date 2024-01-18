523f7d2e3fa0024a28c56d4830fdf0ef17508c80
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-01-18 09:44:58 +0800

    Support recovering Nexus devices via console (#11264)
    What is the motivation for this PR?
    In PR #10806, we support auto recovery of testbeds via console. But at that time, we didn't support nexus testbeds.
    In Nexus testbeds, they first enter into loader, and after rebooting in loader, they enter into ONIE. So in this PR, we enhance the function which will do recover in ONIE to cover this sistuation.
    
    How did you do it?
    Enhance the function which will do recover in ONIE to cover Nexus testbeds. If the type of testbeds is Nexus, first do reboot, and then, recover in ONIE.

---
+++ .azure-pipelines/recover_testbed/constants.py
@@ -0,0 +1,48 @@
+RC_SSH_SUCCESS = 0
+RC_SSH_FAILED = 1
+RC_PASSWORD_FAILED = 2
+
+# Here we will get a screen like
+#
+#                              GNU GRUB  version 2.02
+#
+#  +----------------------------------------------------------------------------+
+#  |*SONiC-OS-20220531.48                                                       |
+#  | ONIE                                                                       |
+#  |                                                                            |
+#  |                                                                            |
+#  +----------------------------------------------------------------------------+
+#       Use the ^ and v keys to select which entry is highlighted.
+#       Press enter to boot the selected OS, `e' to edit the commands
+#       before booting or `c' for a command-line.
+
+# The buffer maybe small and can not hold all characters
+# So we select typical characters
+OS_VERSION_IN_GRUB = "-OS-"
+ONIE_ENTRY_IN_GRUB = "*ONIE"
+
+# After enter into ONIE, we we get the screen like
+#
+#                              GNU GRUB  version 2.02
+#
+#  +----------------------------------------------------------------------------+
+#  |*ONIE: Install OS                                                           |
+#  | ONIE: Rescue                                                               |
+#  | ONIE: Uninstall OS                                                         |
+#  | ONIE: Update ONIE                                                          |
+#  | ONIE: Embed ONIE                                                           |
+#  |                                                                            |
+#  |                                                                            |
+#  +----------------------------------------------------------------------------+
+#       Use the ^ and v keys to select which entry is highlighted.
+#       Press enter to boot the selected OS, `e' to edit the commands
+#       before booting or `c' for a command-line.
+
+INSTALL_OS_IN_ONIE = "Install OS"
+
+# After enter into the installation in ONIE, it will discover some configuration
+# And finally, we will get the string "ONIE: Starting ONIE Service Discovery"
+ONIE_START_TO_DISCOVERY = "Discovery"
+
+# At last, if installation successes in ONIE, we will get the prompt
+SONIC_PROMPT = "sonic login:"
