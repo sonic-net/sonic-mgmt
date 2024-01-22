f1205baaebd3cb455b315ad74bda46c255b72e4a
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-01-22 10:00:35 +0800

    Support recovering Nokia devices via console (#11329)
    What is the motivation for this PR?
    In PR #10806, we support auto recovery of testbeds via console. But at that time, we didn't support nokia testbeds.
    In Nokia testbeds, they first enter into u-boot, and we can enter into ONIE through u-boot. So in this PR, we enhance the function which will do recover in ONIE to cover this sistuation.
    
    How did you do it?
    Enhance the function which will do recover in ONIE to cover Nokia testbeds. If the type of testbeds is Nokia, enter into ONIE through u-boot, and then, recover in ONIE.
    
    How did you verify/test it?
    Recover Nokia testbeds.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- .azure-pipelines/recover_testbed/recover_testbed.py
+++ .azure-pipelines/recover_testbed/recover_testbed.py
@@ -46,6 +46,8 @@ def recover_via_console(sonichost, conn_graph_facts, localhost, mgmt_ip, image_u
             posix_shell_onie(dut_console, mgmt_ip, image_url, is_nexus=True)
         elif device_type in ["mellanox", "cisco", "acs"]:
             posix_shell_onie(dut_console, mgmt_ip, image_url)
+        elif device_type in ["nokia"]:
+            posix_shell_onie(dut_console, mgmt_ip, image_url, is_nokia=True)
         else:
             return
 
