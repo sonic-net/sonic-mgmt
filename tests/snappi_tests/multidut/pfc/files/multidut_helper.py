491c527e82816c1980ce3e8726ae9c89ffdf3acf
selldinesh <dinesh.sellappan@keysight.com>
2024-01-24 09:42:59 -0800

    precommit fixes

--- tests/snappi_tests/multidut/pfc/files/multidut_helper.py
+++ tests/snappi_tests/multidut/pfc/files/multidut_helper.py
@@ -3,14 +3,14 @@ import time
 
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
-    fanout_graph_facts # noqa F401
+    fanout_graph_facts  # noqa F401
 from tests.common.snappi_tests.common_helpers import pfc_class_enable_vector,\
     get_lossless_buffer_size, get_pg_dropped_packets,\
     stop_pfcwd, disable_packet_aging, sec_to_nanosec,\
     get_pfc_frame_count, packet_capture, config_capture_pkt,\
     traffic_flow_mode, calc_pfc_pause_flow_rate      # noqa F401
-from tests.common.snappi_tests.port import select_ports, select_tx_port # noqa F401
-from tests.common.snappi_tests.snappi_helpers import wait_for_arp # noqa F401
+from tests.common.snappi_tests.port import select_ports, select_tx_port  # noqa F401
+from tests.common.snappi_tests.snappi_helpers import wait_for_arp  # noqa F401
 from tests.common.snappi_tests.traffic_generation import setup_base_traffic_config, generate_test_flows, \
     generate_background_flows, generate_pause_flows, run_traffic, verify_pause_flow, verify_basic_test_flow, \
     verify_background_flow, verify_pause_frame_count_dut, verify_egress_queue_frame_count, \
@@ -90,7 +90,7 @@ def run_pfc_test(api,
     global DATA_FLOW_DURATION_SEC
     global data_flow_delay_sec
 
-    dut_port = snappi_extra_params.multi_dut_params.multi_dut_ports[0]['peer_port']
+    #dut_port = snappi_extra_params.multi_dut_params.multi_dut_ports[0]['peer_port']
 
     port_id = 0
 
