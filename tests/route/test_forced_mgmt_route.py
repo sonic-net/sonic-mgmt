f86f6722ae141a27e775dcbcf16c2c86f7957ea3
liuh-80 <liuh@microsoft.com>
2024-01-29 05:24:53 +0000

    Fix warning

--- tests/route/test_forced_mgmt_route.py
+++ tests/route/test_forced_mgmt_route.py
@@ -34,7 +34,7 @@ def backup_restore_config(duthosts, enum_rand_one_per_hwsku_hostname):
 
 
 def get_interface_reload_timestamp(duthost):
-    timestamp = duthost.command("sudo systemctl show --no-pager interfaces-config"\
+    timestamp = duthost.command("sudo systemctl show --no-pager interfaces-config"
                                 " -p ExecMainExitTimestamp --value")["stdout"]
     logger.info("interfaces config timestamp {}".format(timestamp))
 
