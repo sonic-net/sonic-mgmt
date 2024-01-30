d822babe301878d63b2d63dba4abf94e504e0668
Sreejith Sreekumaran <sreesree@cisco.com>
2024-01-30 05:35:06 -0800

    Addressed review comments and introduced polling for count check

--- sonic-mgmt/tests/cisco/qos/test_q7_counters.py
+++ sonic-mgmt/tests/cisco/qos/test_q7_counters.py
@@ -6,7 +6,6 @@ taking the UC7 queue
 import logging
 import pytest
 import time
-from tests.common.cisco_data import is_cisco_device
 import json
 
 
@@ -61,14 +60,10 @@ def clear_queue_counters(asichost):
 def test_verify_q7_counters(duthosts, enum_frontend_dut_hostname, enum_frontend_asic_index, tbinfo, request):
 
     """
-    @summary: Verify output of `show queue counters for UC7 increases as expected`
+    @summary: Verify output of `show queue counters` for UC7 increases as expected
     """
 
     duthost = duthosts[enum_frontend_dut_hostname]
-
-    if not is_cisco_device(duthost):
-        pytest.skip("Skipping as not a Cisco device")
-
     asichost = duthost.asic_instance(enum_frontend_asic_index)
 
     port_channels_data = asichost.get_portchannels_and_members_in_ns(tbinfo)
@@ -80,12 +75,14 @@ def test_verify_q7_counters(duthosts, enum_frontend_dut_hostname, enum_frontend_
       1. Iterate over all port channel.
       2. Clear sonic-queuecounters
       3. Get begin queue counter for UC7 as sum of counters per memeber link
-      4. Ping 100 packets to the peer IP
+      4. Ping 50 packets to the peer IP
       5. Get end queue counter for UC7 as sum of counters per member link
       6. Check the delta between end and begin counter is greater than the pinged count
     """
 
-    PING_COUNT = 100
+    PING_COUNT = 50
+    POLL_INTERVAL = 2
+    POLL_COUNT = 4
 
     for portchannel, portchannel_members in port_channels_data.items():
         for neighbor in mg_facts['minigraph_portchannel_interfaces']:
@@ -102,8 +99,12 @@ def test_verify_q7_counters(duthosts, enum_frontend_dut_hostname, enum_frontend_
                 result = asichost.command(ping_cmd)
                 verify_command_result(result, ping_cmd)
 
-                time.sleep(10)
-                end_counter = get_queue_counter(asichost, portchannel_members)
+                for _ in range(POLL_COUNT):
+                    time.sleep(POLL_INTERVAL)
+                    end_counter = get_queue_counter(asichost, portchannel_members)
+
+                    if end_counter - start_counter >= PING_COUNT:
+                        break
 
                 assert (end_counter - start_counter >= PING_COUNT), \
                        "Insufficient UC7 counter increase of {}. Expected at least {} increase.".format(
