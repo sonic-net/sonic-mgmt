814c629f8d0e122df4c648d12d199b39b102ad34
Yun Li <yunli1@microsoft.com>
2024-02-01 00:21:56 +0800

    Fix code format issues

--- tests/generic_config_updater/test_kubernetes_config.py
+++ tests/generic_config_updater/test_kubernetes_config.py
@@ -17,21 +17,86 @@ logger = logging.getLogger(__name__)
 
 # K8S config
 K8SEMPTYCONFIG = []
-K8SHALFCONFIG = ['"KUBERNETES_MASTER": {\n        "SERVER": {}\n    }']
-K8SFULLCONFIG = ['"KUBERNETES_MASTER": {\n        "SERVER": {\n            "disable": "false",\n            "ip": "k8svip.ap.gbl"\n        }\n    }']
-K8SFULLCONFIG2 = ['"KUBERNETES_MASTER": {\n        "SERVER": {\n            "disable": "false",\n            "ip": "k8svip2.ap.gbl"\n        }\n    }']
+K8SHALFCONFIG = [
+    '"KUBERNETES_MASTER": {\n'
+    '    "SERVER": {}\n'
+    '}'
+]
+K8SFULLCONFIG = [
+    '"KUBERNETES_MASTER": {\n'
+    '    "SERVER": {\n'
+    '        "disable": "false",\n'
+    '        "ip": "k8svip.ap.gbl"\n'
+    '    }\n'
+    '}'
+]
+K8SFULLCONFIG2 = [
+    '"KUBERNETES_MASTER": {\n'
+    '    "SERVER": {\n'
+    '        "disable": "false",\n'
+    '        "ip": "k8svip2.ap.gbl"\n'
+    '    }\n'
+    '}'
+]
 
 
 # K8S config patch
-K8SEMPTYTOHALFPATCH = [{"op": "add", "path": "/KUBERNETES_MASTER", "value": {"SERVER": {}}}]
-K8SHALFTOFULLPATCH = [{"op": "add", "path": "/KUBERNETES_MASTER/SERVER/disable", "value": "false"},
-                    {"op": "add", "path": "/KUBERNETES_MASTER/SERVER/ip", "value": "k8svip.ap.gbl"}]
-K8SFULLTOHALFPATCH = [{"op": "remove", "path": "/KUBERNETES_MASTER/SERVER/ip"},
-                    {"op": "remove", "path": "/KUBERNETES_MASTER/SERVER/disable"}]
-K8SHALFTOEMPTYPATCH = [{"op": "remove", "path": "/KUBERNETES_MASTER"}]
-K8SEMPTYTOFULLPATCH = [{"op": "add", "path": "/KUBERNETES_MASTER", "value": {"SERVER": {"disable": "false", "ip": "k8svip.ap.gbl"}}}]
-K8SFULLTOFULLPATCH = [{"op": "replace", "path": "/KUBERNETES_MASTER/SERVER/ip", "value": "k8svip2.ap.gbl"}]
-K8SFULLTOEMPTYPATCH = [{"op": "remove", "path": "/KUBERNETES_MASTER"}]
+K8SEMPTYTOHALFPATCH = [
+    {
+        "op": "add",
+        "path": "/KUBERNETES_MASTER",
+        "value": {"SERVER": {}}
+    }
+]
+K8SHALFTOFULLPATCH = [
+    {
+        "op": "add",
+        "path": "/KUBERNETES_MASTER/SERVER/disable",
+        "value": "false"
+    },
+    {   
+        "op": "add",
+        "path": "/KUBERNETES_MASTER/SERVER/ip",
+        "value": "k8svip.ap.gbl"
+    }
+]
+K8SFULLTOHALFPATCH = [
+    {
+        "op": "remove",
+        "path": "/KUBERNETES_MASTER/SERVER/ip"
+    },
+    {
+        "op": "remove",
+        "path": "/KUBERNETES_MASTER/SERVER/disable"
+    }
+]
+K8SHALFTOEMPTYPATCH = [
+    {
+        "op": "remove",
+        "path": "/KUBERNETES_MASTER"
+    }
+]
+K8SEMPTYTOFULLPATCH = [
+    {
+        "op": "add",
+        "path": "/KUBERNETES_MASTER",
+        "value": {"SERVER": {"disable": "false", "ip": "k8svip.ap.gbl"}}
+    }
+]
+K8SFULLTOFULLPATCH = [
+    {
+        "op": "replace",
+        "path": "/KUBERNETES_MASTER/SERVER/ip",
+        "value": "k8svip2.ap.gbl"
+    }
+]
+K8SFULLTOEMPTYPATCH = [
+    {
+        "op": "remove",
+        "path": "/KUBERNETES_MASTER"
+    }
+]
+
 
 @pytest.fixture(autouse=True)
 def setup_env(duthosts, rand_one_dut_hostname):
@@ -56,8 +121,10 @@ def setup_env(duthosts, rand_one_dut_hostname):
             "k8s config are not suppose to change after test org: {}, cur: {}"
             .format(original_k8s_config, current_k8s_config)
         )
+
     finally:
         delete_checkpoint(duthost)
+
 
 def get_k8s_runningconfig(duthost):
     """ Get k8s config from running config
@@ -70,6 +137,7 @@ def get_k8s_runningconfig(duthost):
 
     k8s_pattern = r'"KUBERNETES_MASTER":\s*{\s*.*(?:\s*.*\s*.*\s*)?\s*}\s*}'
     k8s_config = re.findall(k8s_pattern, output['stdout'])
+
     return k8s_config
 
 
@@ -118,5 +186,3 @@ def test_k8s_tc1_test_config(rand_selected_dut):
     """
     k8s_config_cleanup(rand_selected_dut)
     k8s_config_update(rand_selected_dut)
-
-
