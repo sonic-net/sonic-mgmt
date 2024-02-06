c0795ea25b1cdedb40952c7f04fb63fbb3e2069d
julius-bcm <66816195+julius-bcm@users.noreply.github.com>
2024-02-06 17:27:12 +0530

    upstream changes in API connection.py (#11613)
    * upstream changes in API boot_up.py
    
    Signed-off-by: julius-bcm <julius.mariyan@broadcom.com>
    
    * upstream changes in API connection.py
    
    Signed-off-by: julius-bcm <julius.mariyan@broadcom.com>
    
    ---------
    
    Signed-off-by: julius-bcm <julius.mariyan@broadcom.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- spytest/apis/system/connection.py
+++ spytest/apis/system/connection.py
@@ -1,23 +1,41 @@
-from spytest.access.connection import DeviceConnection, DeviceConnectionTimeout
+from spytest.access.connection import DeviceConnectionTimeout
 from spytest import st
 
-def connect_to_device(ip, username, password, protocol='ssh', port=22,retry=0, alt_password=None, sudo=True):
+
+class DryRunConnection(object):
+    def __init__(self, **kwargs):
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+
+    def find_prompt(self):
+        return "#"
+
+    def send_command(self, command, **kwargs):
+        return "#"
+
+    def disconnect(self):
+        pass
+
+
+def connect_to_device(ip, username, password, protocol='ssh', port=22,
+                      retry=0, alt_password=None, sudo=True, retry_wait=2):
 
     if st.is_dry_run():
-        return None
+        return DryRunConnection(username=username)
 
     if protocol == "telnet":
         type = 'sonic_terminal'
     else:
         type = 'sonic_ssh'
 
+    blocking_timeout = 30
     device = {
         'access_model': type,
         'ip': ip,
         'port': port,
         'username': username,
         'password': password,
-        'blocking_timeout': 30,
+        'blocking_timeout': blocking_timeout,
     }
 
     connected = False
@@ -26,7 +44,8 @@ def connect_to_device(ip, username, password, protocol='ssh', port=22,retry=0, a
     while True:
         try:
             st.log("Trying %d.." % count)
-            net_connect = DeviceConnection(**device)
+            net_connect = st.do_ssh(ipaddress=ip, username=username, password=password, altpassword=alt_password,
+                                    port=port, blocking_timeout=blocking_timeout, access_model=type)
             connected = True
             break
         except DeviceConnectionTimeout:
@@ -34,6 +53,8 @@ def connect_to_device(ip, username, password, protocol='ssh', port=22,retry=0, a
             count += 1
             if count > retry:
                 break
+            if retry_wait > 0:
+                st.wait(retry_wait)
         except Exception:
             st.log("Exception: Cannot connect..")
             if alt_password:
@@ -42,7 +63,8 @@ def connect_to_device(ip, username, password, protocol='ssh', port=22,retry=0, a
                 device['password'] = alt_password
                 try:
                     st.log("Trying %d.." % count)
-                    net_connect = DeviceConnection(**device)
+                    net_connect = st.do_ssh(ipaddress=ip, username=username, password=password, altpassword=alt_password,
+                                            port=port, blocking_timeout=blocking_timeout, access_model=type)
                     connected = True
                     break
                 except DeviceConnectionTimeout:
@@ -50,21 +72,29 @@ def connect_to_device(ip, username, password, protocol='ssh', port=22,retry=0, a
                     count += 1
                     if count > retry:
                         break
+                    if retry_wait > 0:
+                        st.wait(retry_wait)
                 except Exception:
                     st.log("Except2: Cannot connect..")
                     break
             break
 
-    if connected:
+    if net_connect and connected:
         st.log("Connected ...")
         prompt = net_connect.find_prompt()
         st.log("Detected prompt - {}".format(prompt))
         if sudo:
             command = "sudo su \n{}".format(password)
             net_connect.send_command(command, r"#|\?|$")
+
+        # set cols to avoid echo on new line
+        command = "stty cols 5000"
+        net_connect.send_command(command, r"#|\?|$")
+
         return net_connect
 
-    st.log("Cannot connect..")
+    connstr = "/".join(["{}={}".format(k, v) for k, v in device.items()])
+    st.log("Cannot connect {}".format(connstr))
     return None
 
 
@@ -79,11 +109,14 @@ def make_sudo(ssh_conn_obj, password):
 
 
 def execute_command(ssh_obj, command):
+    if not ssh_obj:
+        st.warn("Invalid connection handle")
+        return None
     try:
         prompt = ssh_obj.find_prompt()
-        result = ssh_obj.send_command(command, expect_string="{}|#".format(prompt))
+        result = ssh_obj.send_command(command, expect_string="{}|:|#".format(prompt))
         st.log(result)
         return result
     except Exception as e:
-        st.log("Exception : {}".format(e))
+        st.warn("Exception : {}".format(e))
         return None
