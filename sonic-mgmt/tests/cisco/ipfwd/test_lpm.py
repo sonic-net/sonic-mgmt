b23955b786589b346f0b64a469114ddf3b92db22
Rajendra Kumar Thirumurthi (rthirumu) <rthirumu@cisco.com>
2024-02-13 13:50:52 -0800

    MIGSOFTWAR-12246 LPM Basic test Verify LPM output (#31)

--- sonic-mgmt/tests/cisco/ipfwd/test_lpm.py
+++ sonic-mgmt/tests/cisco/ipfwd/test_lpm.py
@@ -9,7 +9,14 @@ from collections import defaultdict
 from tests.common.helpers.assertions import pytest_require, pytest_assert
 from tests.common.cisco_data import is_cisco_device
 from tests.common.utilities import wait_until
+
 from tests.cisco.common.utils import skip_if_sim
+from tests.cisco.common.utils import Arp
+from tests.cisco.common.utils import IPRoutes
+from tests.cisco.common.utils import get_crm_info
+from tests.cisco.common.utils import combinations
+
+CISCO_NHOP_GROUP_FILL_PERCENTAGE = 0.92
 
 pytestmark = [
     pytest.mark.topology('t1', 't2')
@@ -70,3 +77,96 @@ def test_lpm_infra(duthost, tbinfo, skip_if_sim):
 
     for x in result:
         print("Resource: {} Usage: {}". format(x, result[x]))
+
+
+def test_basic_lpm_output(duthost, tbinfo, loganalyzer):
+    """
+    Test validate basic LPM output
+    - Add test IP address to an active IP interface
+    - Add static ARPs
+    - Create unique next hop groups
+    - Add IP route and nexthop
+    - Enable debug shell and check LPM output
+    - clean up
+    """
+
+    default_max_nhop_paths = 2
+    polling_interval = 1
+    sleep_time = 380
+    sleep_time_sync_before = 120
+
+    asic = duthost.asic_instance()
+
+    # find out an active IP port
+    ip_ifaces = list(asic.get_active_ip_interfaces(tbinfo).keys())
+    pytest_assert(len(ip_ifaces), "No IP interfaces found")
+    eth_if = ip_ifaces[0]
+
+    # Generate ARP entries
+    arp_count = 8
+    arplist = Arp(duthost, asic, arp_count, eth_if)
+    #arplist.arps_add()
+
+    # indices
+    indices = list(range(arp_count))
+    ip_indices = combinations(indices, default_max_nhop_paths)
+    ip_prefix = ipaddr.IPAddress("192.168.0.0")
+
+    crm_before = get_crm_info(duthost, asic)
+
+
+    # Enable debug shell
+    #duthost.command("sudo config platform cisco sdk-debug enable")
+
+    # increase CRM polling time
+    asic.command("crm config polling interval {}".format(polling_interval))
+
+    # Waiting for ARP routes to be synced and programmed
+    time.sleep(sleep_time_sync_before)
+    crm_stat = get_crm_info(duthost, asic)
+    nhop_group_count = crm_stat["available_nhop_grp"]
+    nhop_group_mem_count = crm_stat["available_nhop_grp_mem"]
+    nhop_group_count = int(nhop_group_count * CISCO_NHOP_GROUP_FILL_PERCENTAGE)
+    # Consider both available nhop_grp and nhop_grp_mem before creating nhop_groups
+    nhop_group_mem_count = int((nhop_group_mem_count) / default_max_nhop_paths * CISCO_NHOP_GROUP_FILL_PERCENTAGE)
+    nhop_group_count = min(nhop_group_mem_count, nhop_group_count)
+
+    logger.info("Adding {} next hops on {}".format(nhop_group_count, eth_if))
+    # create nexthop group
+    nhop = IPRoutes(duthost, asic)
+    try:
+        for i, indx_list in zip(list(range(nhop_group_count)), ip_indices):
+            # get a list of unique group of next hop IPs
+            ips = [arplist.ip_mac_list[x].ip for x in indx_list]
+
+            ip_route = "{}/31".format(ip_prefix + (2*i))
+
+            # add IP route with the next hop group created
+            nhop.add_ip_route(ip_route, ips)
+
+        nhop.program_routes()
+        # wait for routes to be synced and programmed
+        time.sleep(sleep_time)
+        crm_after = get_crm_info(duthost, asic)
+
+        # generate LPM report
+        lpm = LPM(duthost, asic)
+        result = lpm.get_lpm_resource_usage_stats()
+
+        ipv4_entries = result["IPv4 Entries"]
+        ipv6_entries = result["IPv6 Entries"]
+
+    finally:
+        nhop.delete_routes()
+        arplist.clean_up()
+        asic.command(
+            "crm config polling interval {}".format(crm_before["polling"])
+        )
+        #duthost.command("sudo config platform cisco sdk-debug disable")
+        time.sleep(sleep_time)
+
+
+    pytest_assert(ipv4_entries != 0 and ipv6_entries != 0,
+            "ipv4_entries {} or ipv6_entries {} are not updated correctly".format(ipv4_entries, ipv6_entries))
+
+
