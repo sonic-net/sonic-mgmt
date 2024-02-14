9c749206344989e5810a75dfa4eaf97aa520caf7
Rajendra Kumar Thirumurthi (rthirumu) <rthirumu@cisco.com>
2024-02-14 00:52:10 -0800

    LPM verify L1, L2 MIGSOFTWAR-11870 (#35)

--- sonic-mgmt/tests/cisco/ipfwd/test_lpm.py
+++ sonic-mgmt/tests/cisco/ipfwd/test_lpm.py
@@ -79,7 +79,7 @@ def test_lpm_infra(duthost, tbinfo, skip_if_sim):
         print("Resource: {} Usage: {}". format(x, result[x]))
 
 
-def test_basic_lpm_output(duthost, tbinfo, loganalyzer):
+def test_basic_lpm_output(duthost, tbinfo, skip_if_sim):
     """
     Test validate basic LPM output
     - Add test IP address to an active IP interface
@@ -168,5 +168,95 @@ def test_basic_lpm_output(duthost, tbinfo, loganalyzer):
 
     pytest_assert(ipv4_entries != 0 and ipv6_entries != 0,
             "ipv4_entries {} or ipv6_entries {} are not updated correctly".format(ipv4_entries, ipv6_entries))
+    
+    
+def test_lpm_update_with_new_prefix(duthost, tbinfo, skip_if_sim):
+    """
+    Test validate basic LPM output
+    - Add test IP address to an active IP interface
+    - Add static ARPs
+    - Create unique next hop groups
+    - Add IP route and nexthop
+    - Enable debug shell and check LPM output
+    - clean up
+    """
 
+    default_max_nhop_paths = 2
+    polling_interval = 1
+    sleep_time = 180
+    sleep_time_sync_before = 120
+
+    asic = duthost.asic_instance()
+
+    # find out an active IP port
+    ip_ifaces = list(asic.get_active_ip_interfaces(tbinfo).keys())
+    pytest_assert(len(ip_ifaces), "No IP interfaces found")
+    eth_if = ip_ifaces[0]
+
+    # Generate ARP entries
+    arp_count = 128
+    arplist = Arp(duthost, asic, arp_count, eth_if)
+    arplist.arps_add()
+
+    # indices
+    indices = list(range(arp_count))
+    ip_indices = combinations(indices, default_max_nhop_paths)
+    ip_prefix = ipaddr.IPAddress("192.168.0.0")
+
+    crm_before = get_crm_info(duthost, asic)
+
+    # increase CRM polling time
+    asic.command("crm config polling interval {}".format(polling_interval))
+
+    # Waiting for ARP routes to be synced and programmed
+    time.sleep(sleep_time_sync_before)
+    crm_stat = get_crm_info(duthost, asic)
+    nhop_group_count = crm_stat["available_nhop_grp"]
+    nhop_group_mem_count = crm_stat["available_nhop_grp_mem"]
+    nhop_group_count = int(nhop_group_count * CISCO_NHOP_GROUP_FILL_PERCENTAGE)
+    # Consider both available nhop_grp and nhop_grp_mem before creating nhop_groups
+    nhop_group_mem_count = int((nhop_group_mem_count) / default_max_nhop_paths * CISCO_NHOP_GROUP_FILL_PERCENTAGE)
+    nhop_group_count = min(nhop_group_mem_count, nhop_group_count)
+
+    # generate LPM report
+    lpm = LPM(duthost, asic)
+
+    result = lpm.get_lpm_resource_usage_stats()
+
+    before_ipv4_entries = result["IPv4 Entries"]
+    before_l2_rows = result["L2 SRAM Rows"]
+    before_l1_rows = result["L1 Rows"]
+
+    logger.info("Adding {} next hops on {}".format(nhop_group_count, eth_if))
+    # create nexthop group
+    nhop = IPRoutes(duthost, asic)
+    try:
+        for i, indx_list in zip(list(range(nhop_group_count)), ip_indices):
+            # get a list of unique group of next hop IPs
+            ips = [arplist.ip_mac_list[x].ip for x in indx_list]
+
+            ip_route = "{}/31".format(ip_prefix + (8*i))
+
+            # add IP route with the next hop group created
+            nhop.add_ip_route(ip_route, ips)
+
+        nhop.program_routes()
+        # wait for routes to be synced and programmed
+        time.sleep(sleep_time)
+
+        result = lpm.get_lpm_resource_usage_stats()
+        after_ipv4_entries = result["IPv4 Entries"]
+        after_l2_rows = result["L2 SRAM Rows"]
+        after_l1_rows = result["L1 Rows"]
+
+    finally:
+        nhop.delete_routes()
+        arplist.clean_up()
+        asic.command(
+            "crm config polling interval {}".format(crm_before["polling"])
+        )
+
+    pytest_assert(after_ipv4_entries > before_ipv4_entries and after_l2_rows > before_l2_rows and after_l1_rows > before_l1_rows,
+            "after_ipv4_entries {}, before_ipv4_entries {}, after_l2_rows {}, before_l2_rows {}, after_l1_rows {}, before_l1_rows {}".
+            format(after_ipv4_entries, before_ipv4_entries, after_l2_rows, before_l2_rows, after_l1_rows, before_l1_rows))
 
