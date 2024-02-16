eab50e3d4b283eb8d106988ffad9754d22440485
Yucai Gu (yucgu) <yucgu@cisco.com>
2024-02-16 13:39:28 -0800

    Adding more BUM traffic test in L2VNI (#34)

--- sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_l2vni.py
+++ sonic-mgmt/spytest/tests/cisco/tortuga/vxlan/test_l2vni.py
@@ -33,12 +33,12 @@ data.remote = None
 
 data.d3t1_ip_addr = "1.1.1.1"
 data.t1d3_ip_addr = "1.1.1.2"
-data.t1d3_mac_addr = "00:00:00:00:00:01"
+data.t1d3_mac_addr = "00:0a:01:00:11:01"
 
 data.d3t4_ip_addr = "1.1.1.1"
 data.t1d4_ip_addr = "1.1.1.3"
-data.t1d4_mac_addr = "00:00:00:00:00:02"
-data.pkts_per_burst = "1000"
+data.t1d4_mac_addr = "00:0a:01:00:12:01"
+data.pkts_per_burst = "500"
 data.mask = "24"
 data.counters_threshold = 10
 data.tgen_stats_threshold = 20
@@ -127,6 +127,14 @@ def setup_teardown_l2vni():
     # Make sure links are up by pinging, sometimes packet exchange doesn't happen on sim till pings are initiated
     st.wait(5)
     count = 5
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '11.11.11.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '11.11.13.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '11.11.12.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '11.11.14.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.210'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.211'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.210'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.211'), skip_tmpl=True, skip_error_check=True)
     st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.201'), skip_tmpl=True, skip_error_check=True)
     st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.200'), skip_tmpl=True, skip_error_check=True)
 
@@ -204,7 +212,7 @@ def test_l2vni_vtep_setup (setup_teardown_l2vni):
     st.wait(60)
 
     verify_vtep_state(nodes)
-    ## Run Traffic: Bi-directional Ping and Burst of 1000 Packets
+    ## Run Traffic: Bi-directional Ping and Burst of 500 Packets
     run_traffic_test(nodes)
 
     st.report_pass('test_case_passed', nodes['leaf0'])
@@ -225,7 +233,6 @@ def test_l2vni_vtep_delete_add (setup_teardown_l2vni):
     st.wait(60)
 
     verify_vtep_state(nodes)
-    #run_traffic_test(nodes)
 
     test_node = 'leaf0'
     config_static(test_node, 'bgp', add=False)
@@ -237,13 +244,17 @@ def test_l2vni_vtep_delete_add (setup_teardown_l2vni):
 
     # Make sure links are up by pinging, sometimes packet exchange doesn't happen on sim till pings are initiated
     count = 5
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '11.11.11.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '11.11.13.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '11.11.12.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '11.11.14.1'), skip_tmpl=True, skip_error_check=True)
     st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.201'), skip_tmpl=True, skip_error_check=True)
     st.show(nodes['leaf1'], 'sudo ping -c {} {} -q'.format(count, '10.200.200.200'), skip_tmpl=True, skip_error_check=True)
 
     st.wait(30)
 
     verify_vtep_state(nodes)
-    #run_traffic_test(nodes)
+    run_traffic_test(nodes)
 
     st.report_pass('test_case_passed', nodes['leaf0'])
     st.report_pass('test_case_passed', nodes['leaf1'])
@@ -268,9 +279,8 @@ def clear_counters():
 
 ##
 ## Spirent Traffic Mode:  Ping : 5 Pings between TG1<->TG2
-##                        Burst: 1000 Single Burst Between TG1<->TG2
 ##
-def traffic_allow(_mode):
+def traffic_test_ping():
     data.my_dut_list = st.get_dut_names()
  
     clear_counters()
@@ -278,27 +288,21 @@ def traffic_allow(_mode):
     tg_handler = tgapi.get_handles_byname("T1D3P1", "T1D4P1")
     tg = tg_handler["tg"]
 
-    #tg.tg_traffic_control(action="reset", port_handle=tg_handler["tg_ph_list"])
-    #tg.tg_traffic_control(action="clear_stats", port_handle=tg_handler["tg_ph_list"])
-
     vars = st.get_testbed_vars()
     dut_lists = [vars.D3, vars.D4]
 
     (tg1, tg2, tg_ph_1, tg_ph_2) = get_handles()
 
-    #tg1.tg_traffic_control(action='reset', port_handle=tg_ph_1)
-    #tg2.tg_traffic_control(action='reset', port_handle=tg_ph_2)
-
     tg.tg_traffic_control(action="reset", port_handle=tg_handler["tg_ph_list"])
     tg.tg_traffic_control(action="clear_stats", port_handle=tg_handler["tg_ph_list"])
 
     res=tg1.tg_interface_config(port_handle=tg_ph_1, mode='config', intf_ip_addr=data.t1d3_ip_addr,
-    gateway=data.t1d4_ip_addr, src_mac_addr='00:0a:01:00:11:01', arp_send_req='2', enable_ping_response=1)
+    gateway=data.t1d4_ip_addr, src_mac_addr=data.t1d3_mac_addr, arp_send_req='2', enable_ping_response=1)
     st.log("INTFCONF: "+str(res))
     handle1 = res['handle']
 
     res=tg2.tg_interface_config(port_handle=tg_ph_2, mode='config', intf_ip_addr=data.t1d4_ip_addr,
-    gateway=data.t1d3_ip_addr, src_mac_addr='00:0a:01:00:12:01', arp_send_req='2',enable_ping_response=1)
+    gateway=data.t1d3_ip_addr, src_mac_addr=data.t1d4_mac_addr, arp_send_req='2',enable_ping_response=1)
     st.log("INTFCONF: "+str(res))
     handle2 = res['handle']
     st.wait(5)
@@ -307,6 +311,8 @@ def traffic_allow(_mode):
     for _dut in dut_lists:
         st.wait(2)
         st.show(_dut, "sudo show mac")
+        st.show(_dut, "sudo show vxlan remotemac all", skip_tmpl=True, skip_error_check=True)
+        st.show(_dut, "sudo show vxlan counter", skip_tmpl=True, skip_error_check=True)
 
     st.banner("Ping from TG1(D3) to TG2(D4)")
 
@@ -321,50 +327,100 @@ def traffic_allow(_mode):
     print(ping_res2)
 
     ## Update Ping Result for Ping Test:
-    if _mode == "_ping":
-      if ping_res1:
-         st.log("5 Ping from TG1(D3) to TG2(D4) succeeded.")
-      else:
-         st.warn("5 Ping TG1(D3) to TG2(D4) failed.")
-         st.log("5 Ping TG1(D3) to TG2(D4) failed.")
-         st.report_fail("test_case_failed", "5 Ping TG1(D3) to TG2(D4) failed.")
+    if ping_res1:
+       st.log("5 Ping from TG1(D3) to TG2(D4) succeeded.")
+    else:
+       st.warn("5 Ping TG1(D3) to TG2(D4) failed.")
+       st.log("5 Ping TG1(D3) to TG2(D4) failed.")
+       st.report_fail("test_case_failed", "5 Ping TG1(D3) to TG2(D4) failed.")
 
-      st.report_pass("test_case_passed", "5 Ping TG1(D3) to TG2(D4) Passed")
+    st.report_pass("test_case_passed", "5 Ping TG1(D3) to TG2(D4) Passed")
 
-      # Ping from tgen2 to tgen1
-      for _dut in dut_lists:
-         st.show(_dut,"sudo show mac")
-         st.wait(2)
-         st.show(_dut,"sudo show mac")
-      st.banner("Ping from TG1(D3) to TG2(D4)")
+    # Ping from tgen2 to tgen1
+    for _dut in dut_lists:
+       st.show(_dut,"sudo show mac")
+       st.wait(2)
+       st.show(_dut,"sudo show mac")
+       #st.show(_dut, "sudo show vxlan remotemac all", skip_tmpl=True, skip_error_check=True)
+       #st.show(_dut, "sudo show vxlan counter", skip_tmpl=True, skip_error_check=True)
 
-      if ping_res2:
-         st.log("5 Ping from TG2(D4) to TG1(D3) succeeded.")
-      else:
-         st.warn("5 Ping TG2(D4) to TG1(D3) failed.")
-         st.report_fail("test_case_failed", "5 Ping TG2(D4) to TG1(D3) failed.")
+    st.banner("Ping from TG1(D3) to TG2(D4)")
 
-      st.report_pass("test_case_passed", "5 Ping TG2(D4) to TG1(D3) Passed")
+    if ping_res2:
+       st.log("5 Ping from TG2(D4) to TG1(D3) succeeded.")
+    else:
+       st.warn("5 Ping TG2(D4) to TG1(D3) failed.")
+       st.report_fail("test_case_failed", "5 Ping TG2(D4) to TG1(D3) failed.")
 
-      for _dut in dut_lists:
-         st.show(_dut,"sudo show mac")
-         st.wait(2)
-         st.show(_dut,"sudo show mac")
-      st.banner("Ping from TG2(D4) to TG1(D3)")
+    st.report_pass("test_case_passed", "5 Ping TG2(D4) to TG1(D3) Passed")
 
-    ## Update Traffic Result for Burst  Test:
+    for _dut in dut_lists:
+       st.show(_dut,"sudo show mac")
+       st.wait(2)
+       st.show(_dut,"sudo show mac")
+    st.banner("Ping from TG2(D4) to TG1(D3)")
+
+## Spirent Traffic : Bi-Directional Burst of 500 Packets
+##
+## Spirent Traffic type:  unicast: 500 Single unicast Burst Between TG1<->TG2
+##                        broadcast: 500 Single broadcast Burst Between TG1<->TG2
+##                        unknown unicast: 500 Single Burst Between TG1<->TG2
+##                        multicast: 500 Single multicast Burst Between TG1<->TG2
+##
+##
+def traffic_test_burst(_mode):
+    data.my_dut_list = st.get_dut_names()
+ 
+    clear_counters()
+
+    tg_handler = tgapi.get_handles_byname("T1D3P1", "T1D4P1")
+    tg = tg_handler["tg"]
+
+    vars = st.get_testbed_vars()
+    dut_lists = [vars.D3, vars.D4]
+
+    (tg1, tg2, tg_ph_1, tg_ph_2) = get_handles()
+
+    tg.tg_traffic_control(action="reset", port_handle=tg_handler["tg_ph_list"])
+    tg.tg_traffic_control(action="clear_stats", port_handle=tg_handler["tg_ph_list"])
+
+    res=tg1.tg_interface_config(port_handle=tg_ph_1, mode='config', intf_ip_addr=data.t1d3_ip_addr,
+    gateway=data.t1d4_ip_addr, src_mac_addr=data.t1d3_mac_addr, arp_send_req='2', enable_ping_response=1)
+    st.log("INTFCONF: "+str(res))
+    handle1 = res['handle']
+
+    res=tg2.tg_interface_config(port_handle=tg_ph_2, mode='config', intf_ip_addr=data.t1d4_ip_addr,
+    gateway=data.t1d3_ip_addr, src_mac_addr=data.t1d4_mac_addr, arp_send_req='2',enable_ping_response=1)
+    st.log("INTFCONF: "+str(res))
+    handle2 = res['handle']
+    st.wait(5)
+
+    ## Update Traffic Result for Burst Test:
      
     for _dut in dut_lists:
       papi.clear_interface_counters(_dut)
       st.config(_dut, "sudo sonic-clear counters")
 
-    tr1=tg1.tg_traffic_config(port_handle=tg_ph_1, mode='create', transmit_mode='single_burst', length_mode='fixed',
-            pkts_per_burst=data.pkts_per_burst, l3_length=data.tgen_l3_len, rate_pps=data.tgen_rate_pps,
+    if _mode == "unicast":
+      dst_mac1 = data.t1d4_mac_addr
+      dst_mac2 = data.t1d3_mac_addr
+    if _mode == "broadcast":
+      dst_mac1 = "ff:ff:ff:ff:ff:ff"
+      dst_mac2 = "ff:ff:ff:ff:ff:ff"
+    if _mode == "multicast":
+      dst_mac1 = "01:00:5e:44:44:44"
+      dst_mac2 = "01:00:5e:33:33:33"
+    if _mode == "unknownunicast":
+      dst_mac1 = "00:44:44:44:44:44"
+      dst_mac2 = "00:33:33:33:33:33"
+
+    data.tr1=tg1.tg_traffic_config(port_handle=tg_ph_1, mode='create', transmit_mode='single_burst', length_mode='fixed',
+            pkts_per_burst=data.pkts_per_burst, mac_dst=dst_mac1, l3_length=data.tgen_l3_len, rate_pps=data.tgen_rate_pps,
             emulation_src_handle=handle1, emulation_dst_handle=handle2)
     st.wait(5)
 
-    tr2=tg2.tg_traffic_config(port_handle=tg_ph_2, mode='create', transmit_mode='single_burst', length_mode='fixed',
-            pkts_per_burst=data.pkts_per_burst, l3_length=data.tgen_l3_len, rate_pps=data.tgen_rate_pps,
+    data.tr2=tg2.tg_traffic_config(port_handle=tg_ph_2, mode='create', transmit_mode='single_burst', length_mode='fixed',
+            pkts_per_burst=data.pkts_per_burst, mac_dst=dst_mac2, l3_length=data.tgen_l3_len, rate_pps=data.tgen_rate_pps,
             emulation_src_handle=handle2, emulation_dst_handle=handle1)
 
     st.wait(5)
@@ -376,7 +432,7 @@ def traffic_allow(_mode):
 
     tg1.tg_traffic_control(action='clear_stats', port_handle=tg_ph_1)
     tg2.tg_traffic_control(action='clear_stats', port_handle=tg_ph_2)
-    st.log("TRAFCONF - TR1: " + str(tr1) + " TR2: " + str(tr2))
+    st.log("TRAFCONF - TR1: " + str(data.tr1) + " TR2: " + str(data.tr2))
     for _dut in dut_lists:
       papi.clear_interface_counters(_dut)
       st.config(_dut, "sudo sonic-clear counters")
@@ -410,48 +466,54 @@ def traffic_allow(_mode):
     st.banner("Tgen Sent Packets on D3T1P1: {} and Received Packets on D4T1P1: {}".format(total_tg1_tx, total_tg2_rx))
     st.banner("Tgen Sent Packets on D4T1P1: {} and Received Packets on D3T1P1: {}".format(total_tg2_tx, total_tg1_rx))
 
-    if _mode == "_burst":
-      if (int(total_tg1_tx) == 0) | (int(total_tg2_tx) == 0):
-        st.log("Traffic Validation Failed")
-        st.report_fail("test_case_failed", "Single Burst of 1000 Packets Test  Failed")
-      elif (abs(int(total_tg1_tx)-int(total_tg2_rx)) > data.tgen_stats_threshold):
-        st.log("Traffic Validation Failed")
-        #st.report_fail("test_case_failed")
-        st.report_fail("test_case_failed", "Single Burst of 1000 Packets Test  Failed")
-      elif (abs(int(total_tg2_tx)-int(total_tg1_rx)) > data.tgen_stats_threshold):
-        st.log("Traffic Validation Failed")
-        #st.report_fail("test_case_failed")
-        st.report_fail("test_case_failed", "Single Burst of 1000 Packets Test  Failed")
+    if (int(total_tg1_tx) == 0) | (int(total_tg2_tx) == 0):
+      st.log("Traffic Validation Failed")
+      st.report_fail("test_case_failed", "Single Burst of 500 Packets Test  Failed")
+    elif (abs(int(total_tg1_tx)-int(total_tg2_rx)) > data.tgen_stats_threshold):
+      st.log("Traffic Validation Failed")
+      #st.report_fail("test_case_failed")
+      st.report_fail("test_case_failed", "Single Burst of 500 Packets Test  Failed")
+    elif (abs(int(total_tg2_tx)-int(total_tg1_rx)) > data.tgen_stats_threshold):
+      st.log("Traffic Validation Failed")
+      #st.report_fail("test_case_failed")
+      st.report_fail("test_case_failed", "Single Burst of 500 Packets Test  Failed")
 
 
-      #Getting interfaces counter values on DUT
-      DUT_rx_value = papi.get_interface_counters(vars.D3, vars.D3T1P1, "rx_ok")
-      DUT_tx_value = papi.get_interface_counters(vars.D4, vars.D4T1P1, "tx_ok")
+    #Getting interfaces counter values on DUT
+    DUT_rx_value = papi.get_interface_counters(vars.D3, vars.D3T1P1, "rx_ok")
+    DUT_tx_value = papi.get_interface_counters(vars.D4, vars.D4T1P1, "tx_ok")
 
-      for i in DUT_rx_value:
-        p1_rcvd = i['rx_ok']
-        p1_rcvd = p1_rcvd.replace(",","")
+    for i in DUT_rx_value:
+      p1_rcvd = i['rx_ok']
+      p1_rcvd = p1_rcvd.replace(",","")
 
-      for i in DUT_tx_value:
-        p2_txmt = i['tx_ok']
-        p2_txmt = p2_txmt.replace(",","")
+    for i in DUT_tx_value:
+      p2_txmt = i['tx_ok']
+      p2_txmt = p2_txmt.replace(",","")
 
-      st.log("rx_ok counter value on DUT Ingress port: {} and tx_ok xounter value on DUT Egress port : {}".format(p1_rcvd, p2_txmt))
-      st.banner("rx_ok counter value on DUT Ingress port: {} and tx_ok xounter value on DUT Egress port : {}".format(p1_rcvd, p2_txmt))
+    for _dut in dut_lists:
+      st.show(_dut, "sudo show vxlan remotemac all", skip_tmpl=True, skip_error_check=True)
+      st.show(_dut, "sudo show vxlan counter", skip_tmpl=True, skip_error_check=True)
 
-      st.report_pass("test_case_passed",  "Single Burst of 1000 Packets Test Passed")
+    st.log("rx_ok counter value on DUT Ingress port: {} and tx_ok xounter value on DUT Egress port : {}".format(p1_rcvd, p2_txmt))
+    st.banner("rx_ok counter value on DUT Ingress port: {} and tx_ok xounter value on DUT Egress port : {}".format(p1_rcvd, p2_txmt))
 
-## Spirent Traffic : Bi-Directional Ping and Burst of 1000 Packets
+    st.report_pass("test_case_passed",  "Single Burst of 500 Packets Test Passed")
+
+## Spirent Traffic : Bi-Directional Ping and Burst of 500 Packets
 def run_traffic_test (nodes):
 
     # ping test
-    traffic_allow("_ping") 
+    traffic_test_ping() 
     st.wait(1)
 
     # traffic test
-    traffic_allow("_burst") 
-    st.wait(1)
+    traffic_test_burst("unicast") 
+    st.wait(5)
+    traffic_test_burst("broadcast") 
+    st.wait(5)
+    traffic_test_burst("unknownunicast") 
+    st.wait(5)
+    traffic_test_burst("multicast") 
+    st.wait(5)
 
-    # BUM test
-    #st.wait(1)
-    #report_fail(nodes['leaf0'], msg='Traffic test failed')
