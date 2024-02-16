d12296a256cc75ca72200001e0f4ac9ee3baff00
Mihir Patel <patelmi@microsoft.com>
2024-02-16 01:40:12 +0000

    Modify test_get_transceiver_info to handle get_transceiver_info_firmware_versions API
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>

--- tests/common/helpers/platform_api/sfp.py
+++ tests/common/helpers/platform_api/sfp.py
@@ -65,6 +65,8 @@ def is_replaceable(conn, index):
 def get_transceiver_info(conn, index):
     return sfp_api(conn, index, 'get_transceiver_info')
 
+def get_transceiver_info_firmware_versions(conn, index):
+    return sfp_api(conn, index, 'get_transceiver_info_firmware_versions')
 
 def get_transceiver_bulk_status(conn, index):
     return sfp_api(conn, index, 'get_transceiver_bulk_status')
