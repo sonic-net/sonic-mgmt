fa933b08bea1d8f01cf1f1e58b96063a98064a50
jingwenxie <jingwenxie@microsoft.com>
2024-02-22 09:35:34 +0000

    update com

--- tests/generic_config_updater/test_aaa.py
+++ tests/generic_config_updater/test_aaa.py
@@ -157,8 +157,6 @@ def parse_tacacs_server(duthost):
 def aaa_tc1_add_config(duthost):
     """ Test AAA add initial config for its sub type
     """
-    # Recent AAA YANG update that passkey in TACPLUS must exist first for authorization tacacs+
-    tacacs_global_tc2_add_config(duthost)
     aaa_config = {
         "accounting": {
             "login": "tacacs+,local"
@@ -306,6 +304,9 @@ def test_tc1_aaa_suite(rand_selected_dut):
         contian AAA table. So we remove AAA config at first.
     """
     aaa_add_init_config_without_table(rand_selected_dut)
+    # Recent AAA YANG update that passkey in TACPLUS must exist first for authorization tacacs+
+    # Since tc2 it will clean and retest TACPLUS table, we don't care TACPLUS residue after tc1
+    tacacs_global_tc2_add_config(duthost)
     aaa_tc1_add_config(rand_selected_dut)
     aaa_tc1_replace(rand_selected_dut)
     aaa_tc1_add_duplicate(rand_selected_dut)
