6db4cf403d64d7de1db8bd1589321400a99cb7dc
liuh-80 <liuh@microsoft.com>
2024-02-27 06:30:52 +0000

    Start TACACS server before check reachable

--- tests/tacacs/conftest.py
+++ tests/tacacs/conftest.py
@@ -29,8 +29,8 @@ def check_tacacs(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_cre
     logger.info('tacacs_creds: {}'.format(str(print_tacacs_creds)))
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     tacacs_server_ip = ptfhost.mgmt_ip
-    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
     setup_tacacs_server(ptfhost, tacacs_creds, duthost)
+    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
 
     yield
 
