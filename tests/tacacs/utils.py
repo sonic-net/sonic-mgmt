67437f0325f02d64d71c75ff018fcc27418ad8b7
liuh-80 <liuh@microsoft.com>
2024-02-27 06:28:24 +0000

    Improve IPV6 address reachable check

--- tests/tacacs/utils.py
+++ tests/tacacs/utils.py
@@ -73,10 +73,10 @@ def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip):
     """setup tacacs client"""
 
     # UT should failed when set reachable TACACS server with this setup_tacacs_client
-    ping_result = duthost.shell("ping {} -c 1 -W 3".format(tacacs_server_ip))['stdout']
-    logger.info("TACACS server ping result: {}".format(ping_result))
-    if "100% packet loss" in ping_result:
-        pytest_assert(False, "TACACS server not reachable: {}".format(ping_result))
+    check_result = duthost.shell("(echo >/dev/tcp/{}/49) &>/dev/null && echo \"open\" || echo \"close\"".format(tacacs_server_ip))['stdout']
+    logger.warning("TACACS server {} check result: {}".format(tacacs_server_ip, check_result))
+    if "close" in check_result:
+        pytest_assert(False, "TACACS server {} not reachable: {}".format(tacacs_server_ip, check_result))
 
     # configure tacacs client
     default_tacacs_servers = []
