f19797f165fe73f13451c46005fdf0f6deb78ef6
vkuma82 <kumarvinod82@yahoo.com>
2024-03-05 10:56:46 -0800

    fix typo

--- tests/snappi_tests/multidut/ecn/test_multidut_dequeue_ecn_with_snappi.py
+++ tests/snappi_tests/multidut/ecn/test_multidut_dequeue_ecn_with_snappi.py
@@ -11,7 +11,7 @@ from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, lossless_prio_list      # noqa F401
 
 from tests.snappi_tests.variables import config_set, line_card_choice
-from tests.snappi_tests.dinesh_response_time_pr.files.multidut_helper import run_ecn_test
+from tests.snappi_tests.multidut.ecn.files.multidut_helper import run_ecn_test
 from tests.common.snappi_tests.read_pcap import is_ecn_marked
 from tests.snappi_tests.files.helper import skip_ecn_tests
 from tests.common.snappi_tests.common_helpers import packet_capture
