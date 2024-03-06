def9df039102d1344668d11c347ec18e4734776e
Yutong Zhang <yutongzhang@microsoft.com>
2024-03-06 05:53:21 +0000

    Merged PR 4863: [bugfix] Fix the conflict of merging
    Fix the conflict of merging

--- .azure-pipelines/recover_testbed/recover_testbed.py
+++ .azure-pipelines/recover_testbed/recover_testbed.py
@@ -45,11 +45,7 @@ def recover_via_console(sonichost, conn_graph_facts, localhost, mgmt_ip, image_u
             posix_shell_aboot(dut_console, mgmt_ip, image_url)
         elif device_type in ["nexus"]:
             posix_shell_onie(dut_console, mgmt_ip, image_url, is_nexus=True)
-<<<<<<< HEAD
-        elif device_type in ["mellanox", "cisco", "acs", "celestica"]:
-=======
         elif device_type in ["mellanox", "cisco", "acs", "celestica", "force10"]:
->>>>>>> public/master
             is_celestica = device_type in ["celestica"]
             posix_shell_onie(dut_console, mgmt_ip, image_url, is_celestica=is_celestica)
         elif device_type in ["nokia"]:
