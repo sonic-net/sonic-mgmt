f19293f3317dccb31b9dec0bb959883d32c375ce
ecsonic <ecsonic@edge-core.com>
2024-03-12 08:06:00 +0000

    Convert python2 solution to python3.

--- tests/platform_tests/cli/test_show_platform.py
+++ tests/platform_tests/cli/test_show_platform.py
@@ -245,7 +245,7 @@ def test_show_platform_psustatus(duthosts, enum_supervisor_dut_hostname):
     num_psu_ok = 0
 
     for line in psu_status_output_lines[2:]:
-        if isinstance(line, unicode):
+        if isinstance(line, str):
             line = line.encode('ascii', 'ignore').decode('ascii')
         logging.info("line is '{}'".format(line))
         psu_match = psu_line_pattern.match(line)
