1d7070173dd5423a9d986196aea184be45db717f
w1nda <32250288+w1nda@users.noreply.github.com>
2024-03-12 13:03:16 +0800

    Update sonic.py

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -1768,14 +1768,14 @@ Totals               6450                 6449
         Sample output:
             {
                 "Vlan1000": {
-                    "interface_ipv4": [],
-                    "interface_ipv6": [],
-                    "members": ['Ethernet0', 'Ethernet1']
+                    "interface_ipv4": [ "192.168.0.1/24" ],
+                    "interface_ipv6": [ "fc02:1000::1/64" ],
+                    "members": ["Ethernet0", "Ethernet1"]
                 },
                 "Vlan2000": {
-                    "interface_ipv4": [],
-                    "interface_ipv6": [],
-                    "members": ['Ethernet3', 'Ethernet4']
+                    "interface_ipv4": [ "192.168.1.1/24" ],
+                    "interface_ipv6": [ "fc02:1001::1/64" ],
+                    "members": ["Ethernet3", "Ethernet4"]
                 }
             }
         """
