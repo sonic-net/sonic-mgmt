114c02a112a71f80bcf6623527bdbddf090db503
Yaqiang Zhu <yaqiangzhu@microsoft.com>
2024-03-14 18:16:07 +0800

    [image_download] Add ipv6 only mgmt image download test (#11936)
    What is the motivation for this PR?
    Add ipv6 only mgmt image download test case.
    
    How did you do it?
    Add ipv6 only mgmt image download test case.
    
    How did you verify/test it?
    Run tests

--- tests/ip/test_mgmt_ipv6_only.py
+++ tests/ip/test_mgmt_ipv6_only.py
@@ -1,7 +1,7 @@
 import pytest
 
 from tests.common.fixtures.duthost_utils import convert_and_restore_config_db_to_ipv6_only  # noqa F401
-from tests.common.helpers.assertions import pytest_assert
+from tests.common.helpers.assertions import pytest_assert, pytest_require
 
 pytestmark = [
     pytest.mark.topology('any'),
@@ -58,3 +58,23 @@ def test_show_features_ipv6_only(duthosts, enum_dut_hostname, convert_and_restor
                                     .format(cmd_key), module_ignore_errors=False)['stdout']
         pytest_assert(redis_value.lower() == cmd_value.lower(),
                       "'{}' is '{}' which does not match with config_db".format(cmd_key, cmd_value))
+
+
+def test_image_download_ipv6_only(creds, duthosts, enum_dut_hostname,
+                                  convert_and_restore_config_db_to_ipv6_only): # noqa F811
+    """
+    Test image download in mgmt ipv6 only scenario
+    """
+    duthost = duthosts[enum_dut_hostname]
+    image_url = creds.get("test_image_url", {}).get("ipv6", "")
+    pytest_require(len(image_url) != 0, "Cannot get image url")
+    cfg_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    mgmt_interfaces = cfg_facts.get("MGMT_INTERFACE", {}).keys()
+    for mgmt_interface in mgmt_interfaces:
+        output = duthost.shell("curl --fail --interface {} {}".format(mgmt_interface, image_url),
+                               module_ignore_errors=True)
+        if output["rc"] == 0:
+            break
+    else:
+        pytest.fail("Failed to download image from image_url {} via any of {}"
+                    .format(image_url, list(mgmt_interfaces)))
