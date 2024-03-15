436426900f368de98ec360d7f6a67ef674bd3f55
Ye Jianquan <jianquanye@microsoft.com>
2024-03-15 09:48:02 +0800

    Revert IPv6 conversion fixture (#11995)
    * Revert "[image_download] Add ipv6 only mgmt image download test (#11936)"
    
    This reverts commit f5cdac4d57a2beaf6f48896f5fab56bcb6a22a15.
    
    * Revert "[IPv6 only]Add a fixture to convert the DUT to IPv6 only, and enhance the connection plugin to retry with IPv6 addr if IPv4 addr is unavailable (#11957)"
    
    This reverts commit afd7fcebc0a0219c84c81e905fad0d23ee1bc346.
    
    
    Approach
    What is the motivation for this PR?
    There's a performance impact introduced by the IPv6 fixture, the test time is getting longer.
    Revert and will create another PR after the performance issue is resolved.

--- ansible/plugins/connection/multi_passwd_ssh.py
+++ ansible/plugins/connection/multi_passwd_ssh.py
@@ -1,12 +1,10 @@
 import imp
 import os
-import logging
 
 from functools import wraps
-from ansible.errors import AnsibleAuthenticationFailure, AnsibleConnectionFailure
+from ansible.errors import AnsibleAuthenticationFailure
 from ansible.plugins import connection
 
-logger = logging.getLogger(__name__)
 
 # HACK: workaround to import the SSH connection plugin
 _ssh_mod = os.path.join(os.path.dirname(connection.__file__), "ssh.py")
@@ -28,15 +26,7 @@ DOCUMENTATION += """
           vars:
               - name: ansible_altpasswords
               - name: ansible_ssh_altpasswords
-      hostv6:
-          description: IPv6 address
-          vars:
-              - name: ansible_hostv6
 """.lstrip("\n")
-
-# A sample error message that host unreachable:
-# 'Failed to connect to the host via ssh: ssh: connect to host 192.168.0.2 port 22: Connection timed out'
-CONNECTION_TIMEOUT_ERR_FLAG = "Connection timed out"
 
 
 def _password_retry(func):
@@ -47,38 +37,6 @@ def _password_retry(func):
     """
     @wraps(func)
     def wrapped(self, *args, **kwargs):
-
-        # If the host have an IPv6 address, try connect IPv4 address first,
-        # If IPv4 host unavailable, fall back to use IPv6 address
-        try:
-            hostv6 = self.get_option("hostv6")
-        except KeyError:
-            hostv6 = None
-
-        if hostv6:
-            try:
-                return func(self, *args, **kwargs)
-            except AnsibleConnectionFailure as e:
-                logger.info("First connection failed: {}".format(str(e)))
-                if CONNECTION_TIMEOUT_ERR_FLAG in e.message:
-                    self._play_context.remote_addr = hostv6
-                    # args sample:
-                    # ( [b'sshpass', b'-d18', b'ssh', b'-o', b'ControlMaster=auto', b'-o', b'ControlPersist=120s', b'-o', b'UserKnownHostsFile=/dev/null', b'-o', b'StrictHostKeyChecking=no', b'-o', b'StrictHostKeyChecking=no', b'-o', b'User="admin"', b'-o', b'ConnectTimeout=60', b'-o', b'ControlPath="/home/user/.ansible/cp/376bdcc730"', 'fc00:1234:5678:abcd::2', b'/bin/sh -c \'echo PLATFORM; uname; echo FOUND; command -v \'"\'"\'python3.10\'"\'"\'; command -v \'"\'"\'python3.9\'"\'"\'; command -v \'"\'"\'python3.8\'"\'"\'; command -v \'"\'"\'python3.7\'"\'"\'; command -v \'"\'"\'python3.6\'"\'"\'; command -v \'"\'"\'python3.5\'"\'"\'; command -v \'"\'"\'/usr/bin/python3\'"\'"\'; command -v \'"\'"\'/usr/libexec/platform-python\'"\'"\'; command -v \'"\'"\'python2.7\'"\'"\'; command -v \'"\'"\'/usr/bin/python\'"\'"\'; command -v \'"\'"\'python\'"\'"\'; echo ENDFOUND && sleep 0\''], None) # noqa: E501
-                    # args[0] are the parameters of ssh connection
-                    ssh_args = args[0]
-                    # Change the IPv4 host in the ssh_args to IPv6
-                    for idx in range(len(ssh_args)):
-                        if type(ssh_args[idx]) == bytes and ssh_args[idx].decode() == self.host:
-                            ssh_args[idx] = hostv6
-                    self.host = hostv6
-                    self.set_option("host", hostv6)
-            except BaseException as e:
-                # Only catch the connection error, won't block the multi-password functionality
-                logger.info("First connection failed: {}".format(str(e)))
-
-            # Reset the sshpass_pipe for the new connections to be created
-            self.sshpass_pipe = os.pipe()
-
         password = self.get_option("password") or self._play_context.password
         conn_passwords = [password]
         altpassword = self.get_option("altpassword")
