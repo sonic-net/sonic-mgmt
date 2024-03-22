2580a96819f5037b4f561a94a9947613516944f6
liuh-80 <liuh@microsoft.com>
2024-03-22 01:29:06 +0000

    Add TACACS fixture for enabled TACACS in all test case

--- tests/tacacs/utils.py
+++ tests/tacacs/utils.py
@@ -1,8 +1,12 @@
+import binascii
+import copy
 import crypt
 import logging
+import os
+import pytest
 import re
 import binascii
-import pytest
+import yaml
 
 from tests.common.errors import RunAnsibleModuleFail
 from tests.common.utilities import wait_until, check_skip_release, delete_running_config
@@ -69,7 +73,8 @@ def setup_local_user(duthost, tacacs_creds):
     duthost.shell('sudo echo "{}:{}" | chpasswd'.format(tacacs_creds['local_user'], tacacs_creds['local_user_passwd']))
 
 
-def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip):
+def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip,
+                        tacacs_server_passkey, authorization="local"):
     """setup tacacs client"""
 
     # UT should failed when set reachable TACACS server with this setup_tacacs_client
@@ -80,7 +85,7 @@ def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip):
 
     # configure tacacs client
     default_tacacs_servers = []
-    duthost.shell("sudo config tacacs passkey %s" % tacacs_creds[duthost.hostname]['tacacs_passkey'])
+    duthost.shell("sudo config tacacs passkey %s" % tacacs_server_passkey)
 
     # get default tacacs servers
     config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
@@ -95,7 +100,7 @@ def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip):
 
     (skip, _) = check_skip_release(duthost, per_command_authorization_skip_versions)
     if not skip:
-        duthost.shell("sudo config aaa authorization local")
+        duthost.shell("sudo config aaa authorization {}".format(authorization))
 
     (skip, _) = check_skip_release(duthost, per_command_accounting_skip_versions)
     if not skip:
@@ -104,6 +109,13 @@ def setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip):
     # setup local user
     setup_local_user(duthost, tacacs_creds)
     return default_tacacs_servers
+
+
+def convert_db_config_to_cli_parameter(cfg):
+    if cfg == "tacacs+,local":
+        return "\"tacacs+ local\""
+
+    return cfg
 
 
 def restore_tacacs_servers(duthost):
@@ -116,6 +128,7 @@ def restore_tacacs_servers(duthost):
     aaa_config = config_facts.get("AAA", {})
     if aaa_config:
         cfg = aaa_config.get("authentication", {}).get("login", "")
+        cfg = convert_db_config_to_cli_parameter(cfg)
         if cfg:
             cmds.append("config aaa authentication login %s" % cfg)
 
@@ -126,10 +139,12 @@ def restore_tacacs_servers(duthost):
             cmds.append("config aaa authentication failthrough disable")
 
         cfg = aaa_config.get("authorization", {}).get("login", "")
+        cfg = convert_db_config_to_cli_parameter(cfg)
         if cfg:
             cmds.append("config aaa authorization %s" % cfg)
 
         cfg = aaa_config.get("accounting", {}).get("login", "")
+        cfg = convert_db_config_to_cli_parameter(cfg)
         if cfg:
             cmds.append("config aaa accounting %s" % cfg)
 
@@ -208,6 +223,30 @@ def setup_tacacs_server(ptfhost, tacacs_creds, duthost):
                   'tacacs_jit_user': tacacs_creds['tacacs_jit_user'],
                   'tacacs_jit_user_passwd': crypt.crypt(tacacs_creds['tacacs_jit_user_passwd'], 'abc'),
                   'tacacs_jit_user_membership': tacacs_creds['tacacs_jit_user_membership']}
+
+    dut_options = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars
+    logger.warning("setup_tacacs_server: dut_options:{}".format(dut_options))
+    if 'ansible_user' in dut_options and 'ansible_password' in dut_options:
+        duthost_admin_user = dut_options['ansible_user']
+        duthost_admin_passwd = dut_options['ansible_password']
+        logger.debug("setup_tacacs_server: update extra_vars with duthost_admin_user and duthost_admin_passwd.")
+        extra_vars['duthost_admin_user'] = duthost_admin_user
+        extra_vars['duthost_admin_passwd'] = crypt.crypt(duthost_admin_passwd, 'abc')
+    else:
+        logger.warning("setup_tacacs_server: duthost options does not contains config for duthost_admin_user.")
+        # extra_vars['duthost_admin_user'] = 'admin'
+        # extra_vars['duthost_admin_passwd'] = crypt.crypt('YourPaSsWoRd', 'abc')
+        extra_vars['duthost_admin_user'] = tacacs_creds[duthost.hostname]['sonic_login']
+        extra_vars['duthost_admin_passwd'] = crypt.crypt(tacacs_creds[duthost.hostname]['sonic_password'], 'abc')
+
+    if 'ansible_ssh_user' in dut_options and 'ansible_ssh_pass' in dut_options:
+        duthost_ssh_user = dut_options['ansible_ssh_user']
+        duthost_ssh_passwd = dut_options['ansible_ssh_pass']
+        logger.debug("setup_tacacs_server: update extra_vars with duthost_ssh_user and duthost_ssh_passwd.")
+        extra_vars['duthost_ssh_user'] = duthost_ssh_user
+        extra_vars['duthost_ssh_passwd'] = crypt.crypt(duthost_ssh_passwd, 'abc')
+    else:
+        logger.warning("setup_tacacs_server: duthost options does not contains config for ansible_ssh_user.")
 
     ptfhost.host.options['variable_manager'].extra_vars.update(extra_vars)
     ptfhost.template(src="tacacs/tac_plus.conf.j2", dest="/etc/tacacs+/tac_plus.conf")
@@ -338,3 +377,20 @@ def change_and_wait_aaa_config_update(duthost, command, last_timestamp=None, tim
 
     exist = wait_until(timeout, 1, 0, log_exist, duthost)
     pytest_assert(exist, "Not found aaa config update log: {}".format(command))
+
+
+def print_tacacs_creds(tacacs_creds):
+    print_tacacs_creds = copy.deepcopy(tacacs_creds)
+    if isinstance(print_tacacs_creds, dict):
+        for tacacs_creds_msg in print_tacacs_creds.values():
+            if isinstance(tacacs_creds_msg, dict):
+                if tacacs_creds_msg.get("docker_registry_password"):
+                    tacacs_creds_msg["docker_registry_password"] = "******"
+
+    logger.info('tacacs_creds: {}'.format(str(print_tacacs_creds)))
+
+
+def load_tacacs_creds():
+    TACACS_CREDS_FILE = 'tacacs_creds.yaml'
+    creds_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), TACACS_CREDS_FILE)
+    return yaml.safe_load(open(creds_file_path).read())
