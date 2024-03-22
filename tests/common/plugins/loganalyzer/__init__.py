fa1fcf7bb67cb5c1780928dcac187b2398040db8
ganglyu <ganglv@microsoft.com>
2024-03-22 10:41:16 +0800

    Support AnsibleConnectionFailure

--- tests/common/plugins/loganalyzer/__init__.py
+++ tests/common/plugins/loganalyzer/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import pytest
 
+from ansible.errors import AnsibleConnectionFailure
 from .loganalyzer import LogAnalyzer, DisableLogrotateCronContext
 from tests.common.errors import RunAnsibleModuleFail
 from tests.common.helpers.parallel import parallel_run, reset_ansible_local_tmp
@@ -52,17 +53,15 @@ def loganalyzer(duthosts, request):
         mark_file = "/etc/sonic/disable_loganalyzer_mark"
         for duthost in duthosts:
             try:
-                duthost.shell("touch %s" % mark_file, module_ignore_errors=True)
-            except RunAnsibleModuleFail:
-                # Ignore possible ansible error
-                pass
+                duthost.shell("touch %s" % mark_file)
+            except RunAnsibleModuleFail, AnsibleConnectionFailure as e:
+                logging.warning("command failed: \n", str(e))
         yield
         for duthost in duthosts:
             try:
-                duthost.shell("rm -f %s" % mark_file, module_ignore_errors=True)
-            except RunAnsibleModuleFail:
-                # Ignore possible ansible error
-                pass
+                duthost.shell("rm -f %s" % mark_file)
+            except RunAnsibleModuleFail, AnsibleConnectionFailure as e:
+                logging.warning("command failed: \n", str(e))
         return
 
     # Analyze all the duts
