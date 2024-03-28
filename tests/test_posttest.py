75d2bca1b797f2a4987347367ea7a73a809d24e8
xwjiang-ms <xiaweijiang@microsoft.com>
2024-03-28 03:34:38 +0000

    Rename dump file in posttest but no extract

--- tests/test_posttest.py
+++ tests/test_posttest.py
@@ -2,9 +2,6 @@ import pytest
 import logging
 import time
 import os
-import glob
-import tarfile
-import gzip
 from tests.common.helpers.assertions import pytest_require
 
 logger = logging.getLogger(__name__)
@@ -18,43 +15,6 @@ pytestmark = [
 ]
 
 LOG_SAVE_PATH = 'logs/'
-
-
-def extract_tar_gz_file(tar_file_path, extract_path):
-    logger.info("Extracting {} to {}".format(tar_file_path, extract_path))
-    with tarfile.open(tar_file_path, 'r') as tar:
-        members = tar.getmembers()
-        for member in members:
-            try:
-                logger.info("Extract {}".format(member.name))
-                tar.extract(member, path=extract_path)
-            except Exception as e:
-                logger.info("Error extracting {}: {}".format(member.name, e))
-
-
-def extract_gz_file(gz_file_path):
-    extracted_file_path = os.path.splitext(gz_file_path)[0]
-    logger.info("Extracting {} to {}".format(gz_file_path, extracted_file_path))
-    with gzip.open(gz_file_path, 'rb') as f_in:
-        with open(extracted_file_path, 'wb') as f_out:
-            f_out.write(f_in.read())
-    os.remove(gz_file_path)
-
-
-def extract_dump_and_syslog(tar_file_path, dump_path, dir_name):
-    logger.info("Extracting tar.gz dump file {}".format(tar_file_path))
-    extract_tar_gz_file(tar_file_path, dump_path)
-    # rename dump folder
-    os.rename(tar_file_path.split('.tar.gz')[0], os.path.join(dump_path, dir_name))
-    # renmae .tar.gz dump file
-    os.rename(tar_file_path, os.path.join(dump_path, dir_name + '.tar.gz'))
-
-    syslog_path = dump_path + dir_name + '/log/'
-    syslog_gz_files = glob.glob(os.path.join(syslog_path, 'syslog*.gz'))
-    logger.info("Extracting syslog gz files: {}".format(syslog_gz_files))
-    if len(syslog_gz_files) > 0:
-        for syslog_gz in syslog_gz_files:
-            extract_gz_file(syslog_gz)
 
 
 def test_collect_techsupport(request, duthosts, enum_dut_hostname):
@@ -83,8 +43,7 @@ def test_collect_techsupport(request, duthosts, enum_dut_hostname):
         if not log_dir:
             log_dir = tar_file.split('/')[-1].split('.tar.gz')[0]
         tar_file_path = os.path.join(dump_path, tar_file_name)
-        logger.info("tar_file_path: {}, dump_path: {}".format(tar_file_path, dump_path))
-        extract_dump_and_syslog(tar_file_path, dump_path, log_dir)
+        os.rename(tar_file_path, os.path.join(dump_path, log_dir + '.tar.gz'))
 
     assert True
 
