a2cea4bb089b7e8e2ad19e9626a036b69b2c78b5
Cong Hou <congh@nvidia.com>
2024-04-01 14:53:23 +0800

    Fix community copp test for bookworm
    Need to remove the python-setuptools from the package installation command. All test cases passed without python-setuptools.

--- tests/copp/copp_utils.py
+++ tests/copp/copp_utils.py
@@ -222,7 +222,23 @@ def _install_nano(dut, creds,  syncd_docker_name):
         https_proxy = creds.get('proxy_env', {}).get('https_proxy', '')
         check_cmd = "docker exec -i {} bash -c 'cat /etc/os-release'".format(syncd_docker_name)
         # Change the permission of /tmp to 777 to workaround issue #16034
-        if "bullseye" in dut.shell(check_cmd)['stdout'].lower():
+        if "bookworm" in dut.shell(check_cmd)['stdout'].lower():
+            cmd = '''docker exec -e http_proxy={} -e https_proxy={} {} bash -c " \
+                    chmod 777 /tmp \
+                    && rm -rf /var/lib/apt/lists/* \
+                    && apt-get update \
+                    && apt-get install -y python3-pip build-essential libssl-dev libffi-dev \
+                    python3-dev wget cmake python-is-python3 \
+                    && wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz \
+                    && tar xzf 1.0.0.tar.gz && cd nanomsg-1.0.0 \
+                    && mkdir -p build && cmake . && make install && ldconfig && cd .. && rm -rf nanomsg-1.0.0 \
+                    && rm -f 1.0.0.tar.gz && pip3 install cffi && pip3 install --upgrade cffi && pip3 install nnpy \
+                    && mkdir -p /opt && cd /opt && wget \
+                    https://raw.githubusercontent.com/p4lang/ptf/master/ptf_nn/ptf_nn_agent.py \
+                    && mkdir ptf && cd ptf && wget \
+                    https://raw.githubusercontent.com/p4lang/ptf/master/src/ptf/afpacket.py && touch __init__.py \
+                    " '''.format(http_proxy, https_proxy, syncd_docker_name)
+        elif "bullseye" in dut.shell(check_cmd)['stdout'].lower():
             cmd = '''docker exec -e http_proxy={} -e https_proxy={} {} bash -c " \
                     chmod 777 /tmp \
                     && rm -rf /var/lib/apt/lists/* \
