7530c00f0d73c8928f86c1ad87dbff0d2f862575
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-04-03 17:12:58 +0800

    Fix the issue in `test_ssh_limit.py` due to password rotation (#12255)
    Fix the issue in test_ssh_limit.py due to password rotation
    In this PR, we provide a public function connecting to device through paramiko. It can try either several potential passwords or a single password. We will use this new function to connect device.

--- tests/common/utilities.py
+++ tests/common/utilities.py
@@ -1126,18 +1126,40 @@ def capture_and_check_packet_on_dut(
         duthost.file(path=pcap_save_path, state="absent")
 
 
-def duthost_ssh(duthost, sonic_username, sonic_passwords, sonic_ip):
-    for password in sonic_passwords:
+def _paramiko_ssh(ip_address, username, passwords):
+    """
+    Connect to the device via ssh using paramiko
+    Args:
+        ip_address (str): The ip address of device
+        username (str): The username of device
+        passwords (str or list): Potential passwords of device
+            this argument can be either a string or a list of string
+    Returns:
+        AuthResult: the ssh session of device
+    """
+    if isinstance(passwords, str):
+        candidate_passwords = [passwords]
+    elif isinstance(passwords, list):
+        candidate_passwords = passwords
+    else:
+        raise Exception("The passwords argument must be either a string or a list of string.")
+
+    for password in candidate_passwords:
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(sonic_ip, username=sonic_username, password=password,
+            ssh.connect(ip_address, username=username, password=password,
                         allow_agent=False, look_for_keys=False, timeout=10)
-            return ssh
+            return ssh, password
         except paramiko.AuthenticationException:
             continue
         except Exception as e:
-            logging.info("Cannot access DUT {} via ssh, error: {}".format(duthost.hostname, e))
+            logging.info("Cannot access device {} via ssh, error: {}".format(ip_address, e))
             raise e
-    logging.info("Cannot access DUT {} via ssh, error: Password incorrect".format(duthost.hostname))
+    logging.info("Cannot access device {} via ssh, error: Password incorrect".format(ip_address))
     raise paramiko.AuthenticationException
+
+
+def paramiko_ssh(ip_address, username, passwords):
+    ssh, pwd = _paramiko_ssh(ip_address, username, passwords)
+    return ssh
