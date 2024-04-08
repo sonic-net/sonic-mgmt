0866b98cfc166bff132e19c7c0327152c4e9dc49
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2024-04-08 14:21:50 +0800

    Fix ptf can't ssh dut issue due to password rotation (#12303)
    What is the motivation for this PR?
    Some testcases failed for ptf can't login ssh dut due to password rotation
    
    How did you do it?
    Get dut current password and overwrite default one in creds

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,6 +44,7 @@ from tests.common.utilities import get_host_visible_vars
 from tests.common.utilities import get_test_server_host
 from tests.common.utilities import str2bool
 from tests.common.utilities import safe_filename
+from tests.common.utilities import get_dut_current_passwd
 from tests.common.helpers.dut_utils import is_supervisor_node, is_frontend_node
 from tests.common.cache import FactsCache
 from tests.common.config_reload import config_reload
@@ -767,6 +768,9 @@ def creds_on_dut(duthost):
     creds["console_password"] = {}
 
     creds["ansible_altpasswords"] = []
+
+    passwords = creds["ansible_altpasswords"] + [creds["sonicadmin_password"]]
+    creds['sonicadmin_password'] = get_dut_current_passwd(duthost.mgmt_ip, creds['sonicadmin_user'], passwords)
 
     for k, v in list(console_login_creds.items()):
         creds["console_user"][k] = v["user"]
