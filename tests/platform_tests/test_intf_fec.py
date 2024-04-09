9a66d4f34dfb68db5648bd7f63bcca6c744e605b
vvolam <vvolam@microsoft.com>
2024-04-09 15:03:27 -0700

    Add test_intf_fec.py

---
+++ tests/platform_tests/test_intf_fec.py
@@ -0,0 +1,33 @@
+import logging
+import pytest
+
+from tests.common.utilities import skip_release
+
+pytestmark = [
+    pytest.mark.disable_loganalyzer,  # disable automatic loganalyzer
+    pytest.mark.topology('any')
+]
+
+SUPPORTED_PLATFORMS = [
+    "mlnx_msn4700"
+]
+
+def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostname, enum_frontend_asic_index, conn_graph_facts):
+    """
+    @Summary: Check the FEC operational mode, if links are up using 'show interface status'
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    
+    if any(platform in duthost.facts['platform'] for platform in SUPPORTED_PLATFORMS):
+        #Not supported on 202305 and older releases
+        skip_release(duthost, ['201811', '201911', '202012', '202205', '202211', '202305'])
+    else:
+        pytest.skip("DUT has platform {}, test is not supported".format(duthost.facts['platform']))
+
+    logging.info("Get output of '{}'".format("show interface status"))
+    intf_status = duthost.show_and_parse("show interface status")
+
+    for intf in intf_status:
+        if intf['oper'].lower() == "up":
+            if intf['fec'].lower() == "n/a":
+                pytest.fail("FEC status is N/A for interface {}".format(intf['interface']))
