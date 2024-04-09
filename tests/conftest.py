113c6b31ff1a698f9ab1b3e23606852a83e81c55
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-04-09 14:41:06 +0800

    [202012] Fix issues due to password rotation (#12352)
    This PR is a combination of #12253, #12255 and #12264

--- tests/conftest.py
+++ tests/conftest.py
@@ -649,6 +649,8 @@ def creds_on_dut(duthost):
     creds["console_user"] = {}
     creds["console_password"] = {}
 
+    creds["ansible_altpasswords"] = []
+
     for k, v in console_login_creds.items():
         creds["console_user"][k] = v["user"]
         creds["console_password"][k] = v["passwd"]
