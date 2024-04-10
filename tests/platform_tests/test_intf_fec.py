a1c49f06dc1907e37e5b22dcf928fe19904c3c3e
vvolam <vvolam@microsoft.com>
2024-04-10 15:41:46 -0700

    Add more supported platforms and fix the indentation

--- tests/platform_tests/test_intf_fec.py
+++ tests/platform_tests/test_intf_fec.py
@@ -9,17 +9,21 @@ pytestmark = [
 ]
 
 SUPPORTED_PLATFORMS = [
-    "mlnx_msn4700"
+    "mlnx_msn",
+    "8101",
+    "8111"
 ]
 
-def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostname, enum_frontend_asic_index, conn_graph_facts):
+
+def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
+                              enum_frontend_asic_index, conn_graph_facts):
     """
     @Summary: Check the FEC operational mode, if links are up using 'show interface status'
     """
     duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-    
+  
     if any(platform in duthost.facts['platform'] for platform in SUPPORTED_PLATFORMS):
-        #Not supported on 202305 and older releases
+        # Not supported on 202305 and older releases
         skip_release(duthost, ['201811', '201911', '202012', '202205', '202211', '202305'])
     else:
         pytest.skip("DUT has platform {}, test is not supported".format(duthost.facts['platform']))
