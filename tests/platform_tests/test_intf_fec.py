3dff5c9d05eeccaa4f99a1cdd82dcfa7d66d927d
vvolam <vvolam@microsoft.com>
2024-04-10 16:29:19 -0700

    Add more information on Cisco platforms

--- tests/platform_tests/test_intf_fec.py
+++ tests/platform_tests/test_intf_fec.py
@@ -10,8 +10,8 @@ pytestmark = [
 
 SUPPORTED_PLATFORMS = [
     "mlnx_msn",
-    "8101",
-    "8111"
+    "8101_32fh",
+    "8111_32eh"
 ]
 
 
@@ -21,7 +21,7 @@ def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostnam
     @Summary: Check the FEC operational mode, if links are up using 'show interface status'
     """
     duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-  
+ 
     if any(platform in duthost.facts['platform'] for platform in SUPPORTED_PLATFORMS):
         # Not supported on 202305 and older releases
         skip_release(duthost, ['201811', '201911', '202012', '202205', '202211', '202305'])
