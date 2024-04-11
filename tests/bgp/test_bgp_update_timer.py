89b96fd4eabebd6515fc657a025f10d0cec3212c
Anant Kishor Sharma <ananshar@cisco.com>
2024-04-11 02:19:28 -0700

    fix the issue with update interval threshold

--- tests/bgp/test_bgp_update_timer.py
+++ tests/bgp/test_bgp_update_timer.py
@@ -487,7 +487,9 @@ def test_bgp_update_timer_session_down(
         bgp_pcap = BGP_DOWN_LOG_TMPL
         with log_bgp_updates(duthost, "any", bgp_pcap, n0.namespace):
             duthost.shell("config bgp shutdown neighbor {}".format(n0.name))
-            current_time = time.time()
+            # use dut time to avoid delta in dut and mgmt docker time
+            cmd_dut_time = duthost.shell("date +%s.%6N", module_ignore_errors=True)
+            current_time = cmd_dut_time.get('stdout', None)
             time.sleep(constants.sleep_interval)
 
         if constants.log_dir:
