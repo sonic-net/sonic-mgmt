4307af067580fea4c989bc6b651427444db638bc
vvolam <vvolam@microsoft.com>
2024-04-11 15:11:01 -0700

    Address review comment.
    Add a check for speed and sfp presence as well.

--- tests/platform_tests/test_intf_fec.py
+++ tests/platform_tests/test_intf_fec.py
@@ -14,11 +14,16 @@ SUPPORTED_PLATFORMS = [
     "8111_32eh"
 ]
 
+SUPPORTED_SPEEDS = [
+    "100G", "200G", "400G", "800G", "1600G"
+]
+
 
 def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
                               enum_frontend_asic_index, conn_graph_facts):
     """
-    @Summary: Check the FEC operational mode, if links are up using 'show interface status'
+    @Summary: Verify the FEC operational mode is valid, for all the interfaces with
+    SFP present, supported speeds and link is up using 'show interface status'
     """
     duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
 
@@ -32,6 +37,14 @@ def test_verify_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostnam
     intf_status = duthost.show_and_parse("show interface status")
 
     for intf in intf_status:
-        if intf['oper'].lower() == "up":
-            if intf['fec'].lower() == "n/a":
-                pytest.fail("FEC status is N/A for interface {}".format(intf['interface']))
+        sfp_presence = duthost.show_and_parse("sudo sfpshow presence -p {}"
+                                              .format(intf['interface']))
+        if sfp_presence:
+            presence = sfp_presence[0].get('presence', '').lower()
+            oper = intf.get('oper', '').lower()
+            speed = intf.get('speed', '')
+            fec = intf.get('fec', '').lower()
+
+            if presence == "present" and oper == "up" and speed in SUPPORTED_SPEEDS:
+                if fec == "n/a":
+                    pytest.fail("FEC status is N/A for interface {}".format(intf['interface']))
