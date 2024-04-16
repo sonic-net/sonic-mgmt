18830bace82e007794627db39b53abe35c4a3168
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-04-16 15:33:02 +0800

    Add pytest mark in test_copp.py (#12462)
    What is the motivation for this PR?
    In order to run this module, we need a special RPC syncd image but this image doesn't suppot vs testbed. So I add the pytest mark pytest.mark.device_type('physical') in this module to make it clearly.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/copp/test_copp.py
+++ tests/copp/test_copp.py
@@ -43,7 +43,8 @@ from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # noqa
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses      # noqa F401
 
 pytestmark = [
-    pytest.mark.topology("t0", "t1", "t2", "m0", "mx")
+    pytest.mark.topology("t0", "t1", "t2", "m0", "mx"),
+    pytest.mark.device_type('physical')
 ]
 
 _COPPTestParameters = namedtuple("_COPPTestParameters",
