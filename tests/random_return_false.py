154231ff251560d0e4aae9a99267cc250cf7cbf2
chunangli <chunang_li@163.com>
2024-04-24 19:40:02 +0800

    debug
    Signed-off-by: chunangli <chunangli@microsoft.com>

--- tests/random_return_false.py
+++ tests/random_return_false.py
@@ -2,10 +2,10 @@ import random
 
 
 def test_random_return_false():
-    # Generate a random number between 1 and 5
-    rand_num = random.randint(1, 5)
+    # Generate a random number between 1 and 10
+    rand_num = random.randint(1, 10)
 
-    # Return False with a probability of 1/5 (20%)
+    # Return False with a probability of 1/10 (10%)
     if rand_num == 1:
         assert False
     else:
