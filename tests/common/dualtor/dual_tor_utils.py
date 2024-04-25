7f76430892ef72341ecf51790c0ab8c49e1bcacd
Jing Zhang <zhangjing@microsoft.com>
2024-04-25 09:21:20 -0700

    Add retry logic for fanout to ToR interface shutdown (#12557)

--- tests/common/dualtor/dual_tor_utils.py
+++ tests/common/dualtor/dual_tor_utils.py
@@ -58,6 +58,9 @@ ARP_RESPONDER_PY = "arp_responder.py"
 SCRIPTS_SRC_DIR = "scripts/"
 OPT_DIR = "/opt"
 
+EOS_RETRY_MAX = 3
+RETRY_TIMEOUT_SECONDS = 5
+
 
 def get_tor_mux_intfs(duthost):
     return sorted(duthost.get_vlan_intfs(), key=lambda intf: int(intf.replace('Ethernet', '')))
@@ -451,7 +454,46 @@ def _shutdown_fanout_tor_intfs(tor_host, tor_fanouthosts, tbinfo, dut_intfs=None
         fanout_host.shutdown(intf_list)
         fanout_intfs_to_recover[fanout_host].extend(intf_list)
 
+    oper_up_dut_intf = _oper_up_dut_intfs(tor_host, dut_intfs)
+    retry_cnt = 0
+
+    while oper_up_dut_intf and retry_cnt < EOS_RETRY_MAX:
+
+        retry_fanout_shut_intfs = defaultdict(list)
+        for dut_intf in oper_up_dut_intf:
+            encoded_dut_intf = encode_dut_port_name(tor_host.hostname, dut_intf)
+            if encoded_dut_intf in full_dut_fanout_port_map:
+                fanout_host = full_dut_fanout_port_map[encoded_dut_intf]['fanout_host']
+                fanout_intf = full_dut_fanout_port_map[encoded_dut_intf]['fanout_intf']
+                retry_fanout_shut_intfs[fanout_host].append(fanout_intf)
+
+        for fanout_host, intf_list in list(retry_fanout_shut_intfs.items()):
+            fanout_host.shutdown(intf_list)
+            fanout_intfs_to_recover[fanout_host].extend(intf_list)
+
+        retry_cnt += 1
+        time.sleep(RETRY_TIMEOUT_SECONDS)
+
+        oper_up_dut_intf = _oper_up_dut_intfs(tor_host, dut_intfs)
+
     return fanout_shut_intfs
+
+
+def _oper_up_dut_intfs(tor_host, dut_intfs):
+    """Helper function for checking if fanout interfaces that are connected to specified DUT
+    are shutdown.
+    """
+
+    logger.debug("dut_intfs: {}".format(dut_intfs))
+
+    intfs_status = tor_host.show_and_parse("show interface status")
+    logger.debug("show interface status: {}".format(intfs_status))
+
+    up_dut_intfs = [intf['interface'] for intf in intfs_status
+                    if intf['interface'] in dut_intfs and intf['oper'] == 'up']
+    logger.debug("up_dut_intfs: {}".format(up_dut_intfs))
+
+    return up_dut_intfs
 
 
 @pytest.fixture
