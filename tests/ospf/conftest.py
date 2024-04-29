94befb1a1261e7c8b9b9448b334f0e15b78e1057
Asad Raza <araza07@gmail.com>
2024-04-29 15:16:49 +0500

    Add test case code for OSPF testing

--- tests/ospf/conftest.py
+++ tests/ospf/conftest.py
@@ -4,11 +4,12 @@ Conftest file for OSPF tests
 
 import pytest
 import time
+import re
 from tests.common.config_reload import config_reload
 
 
 @pytest.fixture(scope='module')
-def ospf_setup(duthosts, rand_one_dut_hostname, ptfhost, nbrhosts, tbinfo, request):
+def ospf_setup(duthosts, rand_one_dut_hostname, nbrhosts, tbinfo, request):
 
     # verify neighbors are type sonic
     if request.config.getoption("neighbor_type") != "sonic":
@@ -16,19 +17,27 @@ def ospf_setup(duthosts, rand_one_dut_hostname, ptfhost, nbrhosts, tbinfo, reque
 
     duthost = duthosts[rand_one_dut_hostname]
 
-    setup_info = {}
+    setup_info = {'nbr_addr': {}, 'bgp_routes': []}
 
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
 
-    for a_bgp_nbr in mg_facts['minigraph_bgp']:
-        setup_info[a_bgp_nbr['name']] = a_bgp_nbr['addr']
+    for bgp_nbr in mg_facts['minigraph_bgp']:
+        setup_info['nbr_addr'][bgp_nbr['name']] = bgp_nbr['addr']
+
+    cmd = "show ip route bgp"
+    bgp_routes = duthost.shell(cmd)['stdout']
+    bgp_routes_pattern = re.compile(r'B>\*(\d+\.\d+\.\d+\.\d+/\d+)')
+    original_prefixes = bgp_routes_pattern.findall(bgp_routes).sort()
+    setup_info['bgp_routes'] = original_prefixes
 
     for neigh_name in list(nbrhosts.keys()):
         ip_addr = None
+        asn = None
         neigh_mg_facts = nbrhosts[neigh_name]["host"].minigraph_facts(host=nbrhosts[neigh_name]["host"].hostname)
         for neigh_bgp_nbr in neigh_mg_facts['minigraph_bgp']:
             if neigh_bgp_nbr['name'] == duthost.hostname:
                 ip_addr = neigh_bgp_nbr['addr']
+                asn = neigh_bgp_nbr['asn']
                 break
         cmd_list = [
             'docker exec -it bgp bash',
@@ -37,9 +46,12 @@ def ospf_setup(duthosts, rand_one_dut_hostname, ptfhost, nbrhosts, tbinfo, reque
             'exit',
             'vtysh',
             'config t',
-            'no router bgp',
+            'router bgp',
+            'no neighbor {} remote-as {}'.format(str(ip_addr), str(asn)),
+            'exit',
             'router ospf',
             'network {}/31 area 0'.format(str(ip_addr)),
+            'redistribute bgp',
             'do write',
             'end',
             'exit'
