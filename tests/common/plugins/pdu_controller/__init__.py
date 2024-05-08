1e76b4cfa1af5af27bf641f7d97d64c84e6bd74c
Yutong Zhang <yutongzhang@microsoft.com>
2024-05-08 14:25:44 +0800

    Remove unnecessary code

--- tests/common/plugins/pdu_controller/__init__.py
+++ tests/common/plugins/pdu_controller/__init__.py
@@ -1,20 +1,9 @@
 import logging
 
 import pytest
-import os
-import sys
 from .pdu_manager import pdu_manager_factory
 from tests.common.utilities import get_host_visible_vars, get_sup_node_or_random_node
 
-SELF_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.realpath(os.path.join(SELF_DIR, ".."))
-if BASE_DIR not in sys.path:
-    sys.path.append(BASE_DIR)
-ANSIBLE_DIR = os.path.realpath(os.path.join(SELF_DIR, "../../../../ansible"))
-if ANSIBLE_DIR not in sys.path:
-    sys.path.append(ANSIBLE_DIR)
-
-from devutil.inv_helpers import HostManager            # noqa E402
 
 logger = logging.getLogger(__name__)
 
@@ -50,9 +39,6 @@ def _get_pdu_controller(duthost, conn_graph_facts, tbinfo):
         inv_mgr = duthost.host.options["inventory_manager"]
         pdu_host = inv_mgr.get_host(duthost.hostname).get_vars().get("pdu_host")
 
-        hostmgr = HostManager(os.path.join(ANSIBLE_DIR, tbinfo['inv_name']))
-        hosts = hostmgr.get_host_list('all', pdu_host)
-
         pdu_links = {}
         pdu_info = {}
         pdu_vars = {}
@@ -62,23 +48,22 @@ def _get_pdu_controller(duthost, conn_graph_facts, tbinfo):
 
         index = 1
         for ph in pdu_host.split(','):
-            if ph in hosts:
-                host_vars = hosts[ph]
-                pdu_links['PSU{}'.format(index)] = {
-                    'N/A': {
-                        'peerdevice': ph,
-                        'peerport': 'probing',
-                        'feed': 'N/A',
-                    }
+            host_vars = inv_mgr.get_host(ph).get_vars()
+            pdu_links['PSU{}'.format(index)] = {
+                'N/A': {
+                    'peerdevice': ph,
+                    'peerport': 'probing',
+                    'feed': 'N/A',
                 }
-                pdu_info[ph] = {
-                    'Hostname': ph,
-                    'Protocol': host_vars['protocol'],
-                    'ManagementIp': host_vars['ansible_host'],
-                    'Type': 'Pdu',
-                }
-                pdu_vars[ph] = host_vars
-                index = index + 1
+            }
+            pdu_info[ph] = {
+                'Hostname': ph,
+                'Protocol': host_vars['protocol'],
+                'ManagementIp': host_vars['ansible_host'],
+                'Type': 'Pdu',
+            }
+            pdu_vars[ph] = host_vars
+            index = index + 1
     else:
         pdu_links = device_pdu_links[hostname]
         pdu_info = device_pdu_info[hostname]
