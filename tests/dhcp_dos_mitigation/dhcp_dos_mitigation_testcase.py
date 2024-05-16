1b5f9e5c2fee28c1a487757e97ac7fdb99377faa
Ghulam-Bahoo <ghulam.bahoo@xflowresearch.com>
2024-05-16 15:35:06 +0500

    Corrected Indentation

--- tests/dhcp_dos_mitigation/dhcp_dos_mitigation_testcase.py
+++ tests/dhcp_dos_mitigation/dhcp_dos_mitigation_testcase.py
@@ -37,8 +37,8 @@ class TestDhcpRateLimit:
     def simulate_excessive_dhcp_traffic(self, ptfhost, port, packet_rate):
         """Simulate excessive DHCP traffic on the specified port."""
         ptfhost.shell(f'python3 -c "import scapy.all as scapy;\
-                      \ scapy.sendp(scapy.Ether()/scapy.IP()/scapy.UDP(dport=67),\
-                      \ iface=\'{port}\', count={packet_rate*2}, inter=0.001)"')
+                    scapy.sendp(scapy.Ether()/scapy.IP()/scapy.UDP(dport=67),\
+                    iface=\'{port}\', count={packet_rate*2}, inter=0.001)"')
 
     def verify_dhcp_packets_dropped(self, duthost, port):
         """Verify that DHCP packets are being dropped as per the rate limit."""
