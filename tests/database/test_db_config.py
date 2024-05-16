c46f3e417ac1e99d65bc589bae799dfba968ba1e
Saikrishna Arcot <sarcot@microsoft.com>
2024-05-16 13:10:18 -0700

    Fix precommit errors
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- tests/database/test_db_config.py
+++ tests/database/test_db_config.py
@@ -13,6 +13,7 @@ pytestmark = [
     pytest.mark.topology('any')
 ]
 
+
 def test_redis_save_disabled(duthosts, rand_one_dut_hostname):
     """
     @summary: Test that the database isn't being saved to disk
@@ -21,4 +22,5 @@ def test_redis_save_disabled(duthosts, rand_one_dut_hostname):
 
     save_config_json = duthost.command(argv=["redis-cli", "--json", "CONFIG", "GET", "save"])["stdout"]
     save_config = json.loads(save_config_json)
-    pytest_assert(save_config["save"] == "", "Redis should not be persisting contents to disk, save config is {}".format(save_config["save"]))
+    pytest_assert(save_config["save"] == "", "Redis should not be persisting contents to disk, save config is {}"
+                  .format(save_config["save"]))
