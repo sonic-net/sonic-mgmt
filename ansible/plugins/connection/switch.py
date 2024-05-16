sonic-net/sonic-mgmt

commit 55713dca49166ae93d921466e22ef9ec7b115d85

Enhance the "switch" connection plugin to hand Passwordless Login (#12318)
What is the motivation for this PR?
Case failed with error message "msg": "Establish connection to server failed after tried 3 times."

How did you do it?
Ansible uses the option "'ControlPersist=60s'" for SSH connection, it would allow the master connection to remain open after the initial SSH session has been established. This option helps to speed up subsequent SSH connections to the same host by reusing the existing connection.
But in the current script, the code always waits for the '[Pp]assword:' prompt, causing a timeout in passwordless login.

Break if the prompt is " '>', '#'" to handle the passwordless logdin

How did you verify/test it?
Run the original case.

Signed-off-by: xuliping <xuliping@microsoft.com>

ansible/plugins/connection/switch.py
@@ -81,8 +81,8 @@ class Connection(ConnectionBase):
                     client = pexpect.spawn(' '.join(cmd), env={
                                            'TERM': 'dumb'}, timeout=self.timeout)
                     i = client.expect(
-                        ['[Pp]assword:', pexpect.EOF, pexpect.TIMEOUT])
-                    if i == 0:
+                        ['[Pp]assword:', '>', '#', pexpect.EOF, pexpect.TIMEOUT])
+                    if i in [0, 1, 2]:
                         break
                     else:
                         self._display.vvv(
@@ -95,11 +95,19 @@ class Connection(ConnectionBase):
                     raise AnsibleError(
                         "Establish connection to server failed after tried %d times." % max_retries)
 
+            # if "'>', '#'" means Passwordless login, no need to send password
+            if i in [1, 2]:
+                self._display.vvv(
+                    "Establish connection to server successful without requiring a password.", host=self.host)
+                break
+
             self._display.vvv("Try password %s..." %
                               login_passwd[0:4], host=self.host)
             client.sendline(login_passwd)
             i = client.expect(['>', '#', '[Pp]assword:', pexpect.EOF])
             if i < 2:
+                self._display.vvv(
+                    "Establish connection to server successful with password.", host=self.host)
                 break
             elif i == 3:
                 last_user = None

commit c58a773851de2f918a76789f72656c93760d4d26

Fix switch.py for password based login (#12754)
What is the motivation for this PR?
Tests using tests/scripts/exec_template.yml fail when fanout has a password set.

Regression introduced by: #12318

There is a retry logic in switch.py for different passwords a fanout switch can have in the lab: https://github.com/sonic-net/sonic-mgmt/blob/202305/ansible/group_vars/all/labinfo.json#L21-L25. The issue here is that if the first attenpt fails the value of i being returned is 2 which in the second iteration is being wrongly interpreted as ssh connection being established: https://github.com/sonic-net/sonic-mgmt/blob/202305/ansible/plugins/connection/switch.py#L99-L102. This later results in error when the script tries to run CLI commands on the fanout.

How did you do it?
Proposed solution is to make the collection of expected regexes consistent at both of these places:

https://github.com/sonic-net/sonic-mgmt/blob/202305/ansible/plugins/connection/switch.py#L84
https://github.com/sonic-net/sonic-mgmt/blob/202305/ansible/plugins/connection/switch.py#L107
How did you verify/test it?
Verified by running the affected pfcwd tests on a testbed where fanouts had password set.

ansible/plugins/connection/switch.py
@@ -81,7 +81,7 @@ class Connection(ConnectionBase):
                     client = pexpect.spawn(' '.join(cmd), env={
                                            'TERM': 'dumb'}, timeout=self.timeout)
                     i = client.expect(
-                        ['[Pp]assword:', '>', '#', pexpect.EOF, pexpect.TIMEOUT])
+                        ['>', '#', '[Pp]assword:', pexpect.EOF, pexpect.TIMEOUT])
                     if i in [0, 1, 2]:
                         break
                     else:
@@ -96,7 +96,7 @@ class Connection(ConnectionBase):
                         "Establish connection to server failed after tried %d times." % max_retries)
 
             # if "'>', '#'" means Passwordless login, no need to send password
-            if i in [1, 2]:
+            if i < 2:
                 self._display.vvv(
                     "Establish connection to server successful without requiring a password.", host=self.host)
                 break