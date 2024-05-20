0d765c8b03c981065a017af02e0451aea96f250f
Wenda Chu <32250288+w1nda@users.noreply.github.com>
2024-05-20 21:32:56 +0800

    Update test_container_autorestart.py

--- tests/autorestart/test_container_autorestart.py
+++ tests/autorestart/test_container_autorestart.py
@@ -29,13 +29,14 @@ PROGRAM_STATUS = "RUNNING"
 
 
 @pytest.fixture(autouse=True, scope='module')
-def config_reload_after_tests(duthosts, selected_rand_one_per_hwsku_hostname):
-    if tbinfo["topo"]["type"] == "mx":
-        duthost.shell("config feature state dhcp_server enabled")
-        duthost.shell("sudo systemctl restart dhcp_relay.service")
+def config_reload_after_tests(duthosts, selected_rand_one_per_hwsku_hostname, tbinfo):
     # Enable autorestart for all features before the test begins
     for hostname in selected_rand_one_per_hwsku_hostname:
         duthost = duthosts[hostname]
+        # Enable dhcp_server feature for mx topo
+        if tbinfo["topo"]["type"] == "mx":
+            duthost.shell("config feature state dhcp_server enabled")
+            duthost.shell("sudo systemctl restart dhcp_relay.service")
         feature_list, _ = duthost.get_feature_status()
         for feature, status in list(feature_list.items()):
             if status == 'enabled':
@@ -44,10 +45,11 @@ def config_reload_after_tests(duthosts, selected_rand_one_per_hwsku_hostname):
     # Config reload should set the auto restart back to state before test started
     for hostname in selected_rand_one_per_hwsku_hostname:
         duthost = duthosts[hostname]
+        # Disable dhcp_server feature for mx topo
+        if tbinfo["topo"]["type"] == "mx":
+            duthost.shell("config feature state dhcp_server disabled")
+            duthost.shell("sudo systemctl restart dhcp_relay.service")
         config_reload(duthost, config_source='config_db', safe_reload=True)
-    if tbinfo["topo"]["type"] == "mx":
-        duthost.shell("config feature state dhcp_server disabled")
-        duthost.shell("sudo systemctl restart dhcp_relay.service")
 
 
 def enable_autorestart(duthost):
