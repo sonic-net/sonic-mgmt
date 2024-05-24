d093ef4c1c70d76ea98e920744a1237c44149478
Lawrence Lee <lawlee@microsoft.com>
2024-05-23 18:14:25 -0700

    Limit IP addr flushes to global scope IPs (#12556)
    What is the motivation for this PR?
    Applying the fix in #11779 to all tests
    
    How did you do it?
    How did you verify/test it?

--- ansible/roles/test/files/helpers/remove_ip.sh
+++ ansible/roles/test/files/helpers/remove_ip.sh
@@ -6,5 +6,5 @@ INTF_LIST=$(ls /sys/class/net | grep -E "^eth[0-9]+(\.[0-9]+)?$")
 
 for INTF in ${INTF_LIST}; do
     echo "Flush ${INTF} IP address"
-    ip addr flush dev ${INTF}
+    ip addr flush dev ${INTF} scope global
 done
