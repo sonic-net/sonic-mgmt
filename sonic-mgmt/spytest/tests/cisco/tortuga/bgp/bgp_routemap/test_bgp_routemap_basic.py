eef4462ac0b1e3899735915e5b0e6fd1bd263229
Ramanan Natarajan (ramannat) <ramannat@cisco.com>
2024-05-29 05:22:18 +0530

    Ramannat bgp rmap cicd (#196)

--- sonic-mgmt/spytest/tests/cisco/tortuga/bgp/bgp_routemap/test_bgp_routemap_basic.py
+++ sonic-mgmt/spytest/tests/cisco/tortuga/bgp/bgp_routemap/test_bgp_routemap_basic.py
@@ -12,11 +12,13 @@ import yaml
 import pytest
 from spytest import st
 
+import tortuga_common_utils as common_obj
+
 import apis.routing.ip as ipapi
 # TODO: to be restored after the upstream bgpapi has been addressed and
 # the local bgp.py is removed.
 # import apis.routing.bgp as bgpapi
-import tests.cisco.tortuga.bgp.bgp as bgpapi
+import bgp_common_utils as bgpapi
 
 pytest.fixture(scope='module', autouse=True)
 def box_service_module_hooks(request):
@@ -38,77 +40,41 @@ def box_service_func_hooks(request):
 
 CONFIGS_FILE = 'bgp_cfg.yaml'
 
-def config_node(node, config, type=''):
-    if type:
-        st.config(node, config, type=type, skip_error_check=False, conf=True)
-    else:
-        st.config(node, config, skip_error_check=False, conf=True)
-
 ######################################################################
 #                                                                    #
-#  SD1 ---default--- SD3 ---default--- SD2                           #
+#  spine0 ---default--- leaf0 ---default--- spine1                   #
 #                                                                    #
 ######################################################################
-
-def config_static(node, config_domain, add=True):
-    vars = st.get_testbed_vars()
-
-    nodes = {}
-    nodes['SD1'] = vars.D1
-    nodes['SD3'] = vars.D3
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    domain = ''
-    if config_domain == 'bgp':
-        domain = 'vtysh'
-
-    with open(dir_path + '/' + CONFIGS_FILE) as c:
-        config_list = yaml.load(c, Loader=yaml.FullLoader)
-        if add:
-            config_node(nodes[node], config_list[node][config_domain]['config'], domain)
-        else:
-            config_node(nodes[node], config_list[node][config_domain]['deconfig'], domain)
 
 @pytest.fixture(scope="module", autouse=True)
 def setup_teardown_bgp():
     vars = st.get_testbed_vars()
 
     nodes = {}
-    nodes['SD1'] = vars.D1
-    nodes['SD3'] = vars.D3
+    nodes['spine0'] = vars.D1
+    nodes['leaf0'] = vars.D3
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    update_path = common_obj.modify_config_file(dir_path + '/' + CONFIGS_FILE, vars)
 
     with open(dir_path + '/' + CONFIGS_FILE) as c:
         config_list = yaml.load(c, Loader=yaml.FullLoader)
         for node, config in config_list.items():
-            st.config(nodes[node], "systemctl stop drake", skip_error_check=False, conf=True)
-            st.config(nodes[node], "no router bgp", type='vtysh', skip_error_check=False, conf=True)
-            '''
-            # Disabling drake so that there are no automatic underlay configs
-            # Check drake agent is running before disabling it.
-            cmd = 'systemctl status drake'
-            cmd_output = st.config(nodes[node], cmd)
-            if "Active:  active (running)" in str(cmd_output.encode('ascii','ignore')):
-                st.config(nodes[node], "systemctl stop drake", skip_error_check=False, conf=True)
-                st.config(nodes[node], "no router bgp", type='vtysh', skip_error_check=False, conf=True)
-            '''
-
-            config_static(node, 'sonic')
-            config_static(node, 'bgp')
+            common_obj.config_static(node, 'bgp', True, update_path)
+            common_obj.config_static(node, 'sonic', True, update_path)
 
     count = 5    
-    st.show(nodes['SD1'], 'sudo ping -c {} {} -q'.format(count, '10.1.3.3'), skip_tmpl=True, skip_error_check=True)
-    st.show(nodes['SD3'], 'sudo ping -c {} {} -q'.format(count, '10.1.3.1'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['spine0'], 'sudo ping -c {} {} -q'.format(count, '10.1.3.3'), skip_tmpl=True, skip_error_check=True)
+    st.show(nodes['leaf0'], 'sudo ping -c {} {} -q'.format(count, '10.1.3.1'), skip_tmpl=True, skip_error_check=True)
 
     yield 'setup_teardown_bgp'
 
     with open(dir_path + '/' + CONFIGS_FILE) as c:
         config_list = yaml.load(c, Loader=yaml.FullLoader)
         for node, config in config_list.items():
-            config_static(node, 'bgp', add=False)
-            config_static(node, 'sonic', add=False)
+            common_obj.config_static(node, 'bgp', False, update_path)
+            common_obj.config_static(node, 'sonic', False, update_path)
 
 
 ######################################################################
@@ -128,14 +94,14 @@ def test_ft_bgp_rmap(setup_teardown_bgp):
     vars = st.get_testbed_vars()
 
     nodes = {}
-    nodes['SD1'] = vars.D1
-    nodes['SD3'] = vars.D3
+    nodes['spine0'] = vars.D1
+    nodes['leaf0'] = vars.D3
 
     # Getting topo info
     # NOTE: D1 is spine D3 is leaf by default
 
-    leaf_name  = nodes['SD3']
-    spine_name = nodes['SD1']
+    leaf_name  = nodes['leaf0']
+    spine_name = nodes['spine0']
     leaf_as    = 3003
     spine_as   = 1001
 
@@ -190,8 +156,8 @@ def test_bgp_route_map_with_community(setup_teardown_bgp):
     vars = st.get_testbed_vars()
 
     nodes = {}
-    nodes['SD1'] = vars.D1
-    nodes['SD3'] = vars.D3
+    nodes['spine0'] = vars.D1
+    nodes['leaf0'] = vars.D3
     dut1_as = 1001
     dut3_as = 3003
 
@@ -201,20 +167,20 @@ def test_bgp_route_map_with_community(setup_teardown_bgp):
     static_rt_mlen = '32'
     rmap_community = '100:100'
 
-    ipapi.config_route_map(nodes['SD3'], route_map='rmap1', config='yes',
+    ipapi.config_route_map(nodes['leaf0'], route_map='rmap1', config='yes',
                            sequence='10', community=rmap_community)
-    ipapi.create_static_route(nodes['SD1'], next_hop='blackhole', static_ip=static_rt)
+    ipapi.create_static_route(nodes['spine0'], next_hop='blackhole', static_ip=static_rt)
 
-    bgpapi.config_bgp(dut=nodes['SD1'], local_as=dut1_as, config='yes',
+    bgpapi.config_bgp(dut=nodes['spine0'], local_as=dut1_as, config='yes',
                       config_type_list=["redist"], redistribute='static', cli_type="")
-    bgpapi.config_bgp(dut=nodes['SD3'], local_as=dut3_as, 
+    bgpapi.config_bgp(dut=nodes['leaf0'], local_as=dut3_as, 
                       neighbor=dut1_addr_ipv4,
                       addr_family='ipv4', config='yes',
                       config_type_list=["routeMap"], routeMap='rmap1',
                       diRection='in', cli_type="")
 
     # Check the show command in leaf
-    output = bgpapi.show_bgp_ipvx_prefix(nodes['SD3'], prefix=static_rt_ip, masklen=static_rt_mlen, family='ipv4')
+    output = bgpapi.show_bgp_ipvx_prefix(nodes['leaf0'], prefix=static_rt_ip, masklen=static_rt_mlen, family='ipv4')
     st.log(output)
     # there is only one record
     for x in output or {}:
@@ -224,9 +190,9 @@ def test_bgp_route_map_with_community(setup_teardown_bgp):
         else:
             result = False
 
-    ipapi.config_route_map(dut=nodes['SD3'], route_map='rmap1', config='no',
+    ipapi.config_route_map(dut=nodes['leaf0'], route_map='rmap1', config='no',
                            sequence='10')
-    ipapi.delete_static_route(dut=nodes['SD1'], next_hop='blackhole', static_ip=static_rt)
+    ipapi.delete_static_route(dut=nodes['spine0'], next_hop='blackhole', static_ip=static_rt)
     if result:
         st.report_pass("operation_successful")
     else:
