557555dd2ed0036753d96b8d28d692483b67437d
jongorel <156464693+jongorel@users.noreply.github.com>
2024-05-29 12:13:36 -0400

    Fix GCU Dynamic ACL ARP bug caused by stale ip config in PTFhost (#13014)
    * Remove ip addresses from ptfhost before running tests, add arp logging

--- tests/generic_config_updater/test_dynamic_acl.py
+++ tests/generic_config_updater/test_dynamic_acl.py
@@ -25,6 +25,7 @@ import ptf.testutils as testutils
 from ipaddress import ip_network, IPv6Network, IPv4Network
 from tests.arp.arp_utils import increment_ipv6_addr, increment_ipv4_addr
 
+from tests.common.fixtures.ptfhost_utils import remove_ip_addresses  # noqa F401
 from tests.generic_config_updater.gu_utils import expect_op_success, expect_op_failure
 from tests.generic_config_updater.gu_utils import create_checkpoint, delete_checkpoint, rollback_or_reload
 from tests.generic_config_updater.gu_utils import format_and_apply_template, load_and_apply_json_patch
@@ -1077,10 +1078,14 @@ def test_gcu_acl_arp_rule_creation(rand_selected_dut,
         show_cmd = "show arp"
         ipv6_ping_option = ""
         dynamic_acl_create_arp_forward_rule(rand_selected_dut, setup)
+        output = rand_selected_dut.show_and_parse("show arp")
+        logger.info("Result of show arp is: " + str(output))
     else:
         show_cmd = "nbrshow -6 -ip"
         ipv6_ping_option = "-6"
         dynamic_acl_create_ndp_forward_rule(rand_selected_dut, setup)
+        output = rand_selected_dut.show_and_parse("nbrshow -6")
+        logger.info("Result of nbrshow -6 is: " + str(output))
 
     dynamic_acl_create_secondary_drop_rule(rand_selected_dut, setup, port_name)
 
