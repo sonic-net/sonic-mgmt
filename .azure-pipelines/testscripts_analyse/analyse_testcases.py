57a9500c9c761b0cdf9b66cfec5d339e0fc69814
Yutong Zhang <yutongzhang@microsoft.com>
2024-06-04 14:04:24 +0800

    Fix comment

--- .azure-pipelines/testscripts_analyse/analyse_testcases.py
+++ .azure-pipelines/testscripts_analyse/analyse_testcases.py
@@ -10,7 +10,7 @@ The return value is formatted as below:
         'testscript': 'acl/custom_acl_table/test_custom_acl_table.py',
         'topology': 't0',
         'trackid': '3aa57f0f-8f18-4cf7-ae1e-0a18973a0b86',
-        'scantime': '2024-05-3106: 53: 40.826349',
+        'scantime': '2024-05-31 06:53:40.826349',
         'category': 'data',
         'covered': False
     },
@@ -18,7 +18,7 @@ The return value is formatted as below:
         'testscript': 'bgp/test_bgp_allow_list.py',
         'topology': 't1',
         'trackid': '3aa57f0f-8f18-4cf7-ae1e-0a18973a0b86',
-        'scantime': '2024-05-3106: 53: 40.826349',
+        'scantime': '2024-05-31 06:53:40.826349',
         'category': 'control',
         'covered': False
     }
@@ -30,6 +30,7 @@ import re
 import os
 import sys
 import uuid
+import logging
 
 from natsort import natsorted
 from datetime import datetime
@@ -51,26 +52,35 @@ def collect_all_scripts():
     # Open each file and search for regex pattern
     pattern = re.compile(r"[^@]pytest\.mark\.topology\(([^\)]*)\)")
     test_scripts = []
+
     for f in files:
         # Remove prefix from file name:
         filename = f[len(location) + 1:]
-        with open(f, 'r') as file:
-            for line in file:
-                match = pattern.search(line)
-                if match:
-                    for topology in match.group(1).split(","):
-                        result = {
-                            "testscript": filename,
-                            "topology": topology.strip().strip('"').strip('\'')
-                        }
-                        test_scripts.append(result)
+        try:
+            with open(f, 'r') as file:
+                for line in file:
+                    match = pattern.search(line)
+                    if match:
+                        for topology in match.group(1).split(","):
+                            result = {
+                                "testscript": filename,
+                                "topology": topology.strip().strip('"').strip('\'')
+                            }
+                            test_scripts.append(result)
+        except Exception as e:
+            logging.error('Failed to load file {}, error {}'.format(f, e))
+
     return test_scripts
 
 
 def check_PRChecker_coverd(test_scripts):
     pr_test_scripts_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../", "pr_test_scripts.yaml")
-    with open(pr_test_scripts_file) as f:
-        pr_test_scripts = yaml.safe_load(f)
+
+    try:
+        with open(pr_test_scripts_file) as f:
+            pr_test_scripts = yaml.safe_load(f)
+    except Exception as e:
+        logging.error('Failed to load file {}, error {}'.format(f, e))
 
     for test_script in test_scripts:
         topo_type = test_script["topology"]
