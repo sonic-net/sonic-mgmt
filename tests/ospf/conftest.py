0cd6ec4fe703e5316f897ce962ec10c816cb82b9
Asad Raza <araza07@gmail.com>
2024-06-10 23:11:14 +0500

    OSPF Test Cases (#12647)
    Motivation for this PR
    To add PyTest test case code in accordance with previously contributed OSPF test plan
    
    How I did it
    Added a new folder directory 'sonic-mgmt/tests/ospf/' containing the following files:
    
    conftest.py containing setup/teardown plugins
    test_ospf.py containing 2 test cases:
    Test OSPF Neighborship
    Test OSPF Dynamic Routing
    
    Platform specific information
    These test cases have been written to run on testing topologies with SONiC-type neighbors. Once the code is tested and verified, code to support Arista-type neighbors will be developed as well.

--- tests/ospf/conftest.py
+++ tests/ospf/conftest.py
@@ -1,3 +1,7 @@
+'''
+Conftest file for OSPF tests
+'''
+
 import pytest
 import re
 from tests.common.config_reload import config_reload
@@ -73,3 +77,63 @@ def get_ospf_neighbor_interface(host):
                 ':')[1]  # Return the interface name
     # Return None if interface name is not found or not PortChannels.
     return nbr_int_info
+
+
+@pytest.fixture(scope='module')
+def ospf_setup(duthosts, rand_one_dut_hostname, nbrhosts, tbinfo, request):
+
+    # verify neighbors are type sonic
+    if request.config.getoption("neighbor_type") != "sonic":
+        pytest.skip("Neighbor type must be sonic")
+
+    duthost = duthosts[rand_one_dut_hostname]
+
+    setup_info = {'nbr_addr': {}, 'bgp_routes': []}
+
+    mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
+
+    for bgp_nbr in mg_facts['minigraph_bgp']:
+        setup_info['nbr_addr'][bgp_nbr['name']] = bgp_nbr['addr']
+
+    # gather original BGP routes
+    cmd = "show ip route bgp"
+    bgp_routes = duthost.shell(cmd)['stdout']
+    bgp_routes_pattern = re.compile(r'B>\*(\d+\.\d+\.\d+\.\d+/\d+)')
+    original_prefixes = bgp_routes_pattern.findall(bgp_routes).sort()
+    setup_info['bgp_routes'] = original_prefixes
+
+    for neigh_name in list(nbrhosts.keys()):
+        ip_addr = None
+        asn = None
+        neigh_mg_facts = nbrhosts[neigh_name]["host"].minigraph_facts(host=nbrhosts[neigh_name]["host"].hostname)
+        for neigh_bgp_nbr in neigh_mg_facts['minigraph_bgp']:
+            if neigh_bgp_nbr['name'] == duthost.hostname:
+                ip_addr = neigh_bgp_nbr['addr']
+                asn = neigh_bgp_nbr['asn']
+                break
+        cmd_list = [
+            'docker exec -it bgp bash',
+            'cd /usr/lib/frr',
+            './ospfd &',
+            'exit',
+            'vtysh',
+            'config t',
+            'router bgp',
+            'no neighbor {} remote-as {}'.format(str(ip_addr), str(asn)),
+            'exit',
+            'router ospf',
+            'network {}/31 area 0'.format(str(ip_addr)),
+            'redistribute bgp',
+            'do write',
+            'end',
+            'exit'
+        ]
+        nbrhosts[neigh_name]["host"].shell_cmds(cmd_list)
+
+    yield setup_info
+
+    # restore config to original state on both DUT and neighbor
+    config_reload(duthost, safe_reload=True)
+    time.sleep(10)
+    for neigh_name in list(nbrhosts.keys()):
+        config_reload(nbrhosts[neigh_name]["host"], is_dut=False)
