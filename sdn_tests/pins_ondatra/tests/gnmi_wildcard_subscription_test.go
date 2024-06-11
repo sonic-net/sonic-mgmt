94bfed17e41fde3f1a473a7293e6fa00149e08db
vsuryaprasad-hcl <v.suryaprasad@hcl.com>
2024-06-11 12:30:48 -0400

    [sdn_tests]:Adding Wild Card tests to pins_ondatra.

--- sdn_tests/pins_ondatra/tests/gnmi_wildcard_subscription_test.go
+++ sdn_tests/pins_ondatra/tests/gnmi_wildcard_subscription_test.go
@@ -590,12 +590,625 @@ func TestWCOnChangeIntegratedCircuitNodeId(t *testing.T) {
 	}
 }
 
+func TestWCOnChangeComponentOperStatus(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID("794672b0-e15f-4a72-8619-f5a0bbb45e9b").Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Collect every component with an oper-status through a GET to be compared to the SUBSCRIBE.
+	wantUpdates := make(map[string]int)
+	vals := gnmi.LookupAll(t, dut, gnmi.OC().ComponentAny().OperStatus().State())
+	for _, val := range vals {
+		if !val.IsPresent() {
+			continue
+		}
+		component, err := fetchPathKey(val.Path, componentKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		wantUpdates[component]++
+	}
+
+	// Collect components through Subscription to be compared to the previous GET.
+	initialValues := gnmi.CollectAll(t, gnmiOpts(t, dut, gpb.SubscriptionMode_ON_CHANGE), gnmi.OC().
+		ComponentAny().
+		OperStatus().State(), shortWait).
+		Await(t)
+
+	gotUpdates := make(map[string]int)
+	for _, val := range initialValues {
+		component, err := fetchPathKey(val.Path, intfKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		if val.IsPresent() {
+			gotUpdates[component]++
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got):\n%v", diff)
+	}
+}
+
+type counterSubscription struct {
+	dut *ondatra.DUTDevice
+	t   *testing.T
+}
+
+func (c counterSubscription) subToInUnicastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InUnicastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInBroadcastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InBroadcastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInMulticastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InMulticastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutUnicastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutUnicastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutBroadcastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutBroadcastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutMulticastPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutMulticastPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInOctets() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InOctets().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutOctets() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutOctets().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInDiscards() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InDiscards().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutDiscards() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutDiscards().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInErrors() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InErrors().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToOutErrors() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		OutErrors().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToInFcsErrors() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		InterfaceAny().
+		Counters().
+		InFcsErrors().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToTransmitPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		Qos().
+		InterfaceAny().
+		Output().
+		QueueAny().
+		TransmitPkts().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToTransmitOctets() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		Qos().
+		InterfaceAny().
+		Output().
+		QueueAny().
+		TransmitOctets().State(), longWait).
+		Await(c.t)
+}
+
+func (c counterSubscription) subToDroppedPkts() []*ygnmi.Value[uint64] {
+	return gnmi.CollectAll(c.t, gnmiOpts(c.t, c.dut, gpb.SubscriptionMode_TARGET_DEFINED), gnmi.OC().
+		Qos().
+		InterfaceAny().
+		Output().
+		QueueAny().
+		DroppedPkts().State(), longWait).
+		Await(c.t)
+}
+
+type counterTest struct {
+	uuid   string
+	subfun func() []*ygnmi.Value[uint64]
+}
+
+func TestWcTargetDefinedCounters(t *testing.T) {
+	dut := ondatra.DUT(t, "DUT")
+	testCases := []struct {
+		name     string
+		function func(*testing.T)
+	}{
+		{
+			name: "InUnicastPkts",
+			function: counterTest{
+				uuid: "488f9daa-9b8d-455f-b580-5dd5491d64b5",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInUnicastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InBroadcastPkts",
+			function: counterTest{
+				uuid: "edffb8d9-9040-4188-b9d3-bdb083a61f27",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInBroadcastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InMulticastPkts",
+			function: counterTest{
+				uuid: "1b20f216-1bb5-4ed6-b2ed-5a09942a2eee",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInMulticastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutUnicastPkts",
+			function: counterTest{
+				uuid: "712042e8-b057-4f0c-bd0b-cde2861a3555",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutUnicastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutBroadcastPkts",
+			function: counterTest{
+				uuid: "06cf226d-36ad-4dec-958e-89a6c2d42506",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutBroadcastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutMulticastPkts",
+			function: counterTest{
+				uuid: "4b81e22d-5dd2-4ea2-95bd-25d3655978a3",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutMulticastPkts,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InOctets",
+			function: counterTest{
+				uuid: "30e25f1b-f79c-4824-adac-4fa6feba2f02",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInOctets,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutOctets",
+			function: counterTest{
+				uuid: "9a55189e-c1a4-42c9-a02d-eec3d8ec5d1b",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutOctets,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InDiscards",
+			function: counterTest{
+				uuid: "744c4e37-e6d2-400f-999b-adb6a81b461d",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInDiscards,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutDiscards",
+			function: counterTest{
+				uuid: "69075f31-d825-4eb2-9bbe-e9bfb95b36a6",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutDiscards,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InErrors",
+			function: counterTest{
+				uuid: "b416b679-f336-4d3c-9e70-cc907508cda1",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInErrors,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "OutErrors",
+			function: counterTest{
+				uuid: "76b515f6-9464-42ee-aa31-2210c5c9fc29",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToOutErrors,
+			}.targetDefinedCounterTest,
+		},
+		{
+			name: "InFcsErrors",
+			function: counterTest{
+				uuid: "6e6aac39-eaa4-4d10-890b-aec13e981733",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToInFcsErrors,
+			}.targetDefinedCounterTest,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, testCase.function)
+	}
+}
+
+func TestWcTargetDefinedQosCountersWc(t *testing.T) {
+	dut := ondatra.DUT(t, "DUT")
+
+	testCases := []struct {
+		name     string
+		function func(*testing.T)
+	}{
+		{
+			name: "TransmitPkts",
+			function: counterTest{
+				uuid: "7b1133b0-b934-4948-b087-d63108418dcb",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToTransmitPkts,
+			}.targetDefinedQosCounterWcTest,
+		},
+		{
+			name: "TransmitOctets",
+			function: counterTest{
+				uuid: "9c36c0ca-2551-4a7d-a7de-d47fef53d158",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToTransmitOctets,
+			}.targetDefinedQosCounterWcTest,
+		},
+		{
+			name: "DroppedPkts",
+			function: counterTest{
+				uuid: "88985776-2b1e-4afc-8e7b-dd1114f7070c",
+				subfun: counterSubscription{
+					dut: dut,
+					t:   t,
+				}.subToDroppedPkts,
+			}.targetDefinedQosCounterWcTest,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, testCase.function)
+	}
+}
+
+func (c counterTest) targetDefinedCounterTest(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID(c.uuid).Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Collect every interface through a GET to be compared to the SUBSCRIBE.
+	wantUpdates := make(map[string]int)
+	ports := gnmi.GetAll(t, dut, gnmi.OC().InterfaceAny().Name().State())
+	for _, port := range ports {
+		if strings.Contains(port, intfPrefix) {
+			wantUpdates[port] = 2 // initial subscription plus one timed update.
+		}
+	}
+
+	// Collect interfaces through subscription to be compared to the previous GET.
+	subcriptionValues := c.subfun()
+
+	gotUpdates := make(map[string]int)
+	for _, val := range subcriptionValues {
+		port, err := fetchPathKey(val.Path, intfKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		if val.IsPresent() && strings.Contains(port, intfPrefix) {
+			gotUpdates[port]++
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got):\n%v", diff)
+	}
+}
+
+// UMF default queue name is interface-id:queueName
+// This name is used for the queue name without proper number->string mapping
+func isQueueConfigured(qname, iname string) bool {
+	return !strings.HasPrefix(qname, iname)
+}
+
+func (c counterTest) targetDefinedQosCounterWcTest(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID(c.uuid).Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Collect every interface through a GET to be compared to the SUBSCRIBE.
+	wantUpdates := make(map[string]uint64)
+	qosInterfaces := gnmi.Get(t, dut, gnmi.OC().Qos().State()).Interface
+	for intf := range qosInterfaces {
+		queueNames := gnmi.GetAll(t, dut, gnmi.OC().Qos().Interface(intf).Output().QueueAny().Name().State())
+		for _, queueName := range queueNames {
+			if !isQueueConfigured(queueName, intf) {
+				continue
+			}
+			wantUpdates[intf+","+queueName] = 2 // initial subscription plus one timed update.
+		}
+	}
+
+	// Collect interfaces through subscription to be compared to the previous GET.
+	subcriptionValues := c.subfun()
+
+	gotUpdates := make(map[string]uint64)
+	for _, val := range subcriptionValues {
+		intfQueue, err := fetchQosKey(val.Path)
+		if err != nil {
+			if !strings.Contains(err.Error(), "unconfigured") {
+				t.Logf("fetchQosKey() failed: %v", err)
+			}
+			continue
+		}
+
+		interfaceID, queueName := intfQueue[0], intfQueue[1]
+		if val.IsPresent() {
+			gotUpdates[interfaceID+","+queueName]++
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got):\n%v", diff)
+	}
+}
+
+func fetchQosKey(path *gpb.Path) ([]string, error) {
+	if path == nil {
+		return nil, errors.New("received nil path")
+	}
+	pathStr, err := ygot.PathToString(path)
+	if err != nil {
+		return nil, errors.Errorf("ygot.PathToString() failed: %v", err)
+	}
+
+	if len(path.GetElem()) != 8 {
+		return nil, errors.Errorf("no valid interface id or queue name from path: %v", pathStr)
+	}
+
+	interfaceEle := path.GetElem()[2].GetKey()
+	if interfaceEle == nil {
+		return nil, errors.Errorf("no valid interface id from path: %v", pathStr)
+	}
+	interfaceKey, ok := interfaceEle[intfIDKey]
+	if !ok {
+		return nil, errors.Errorf("no valid interface id from path: %v", pathStr)
+	}
+
+	queueEle := path.GetElem()[5].GetKey()
+	if queueEle == nil {
+		return nil, errors.Errorf("no valid queue name from path: %v", pathStr)
+	}
+	queueName, ok := queueEle[queueKey]
+	if !ok {
+		return nil, errors.Errorf("no valid queue name from path: %v", pathStr)
+	}
+	if !isQueueConfigured(queueName, interfaceKey) {
+		return nil, errors.Errorf("unconfigured queue found: %v", pathStr)
+	}
+
+	return []string{interfaceKey, queueName}, nil
+}
+
+func TestWCOnChangeSoftwareModuleModuleType(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID("ff05e9c6-b57b-4128-9535-e8543dc5aedc").Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Collect every software module component through a GET to be compared to the SUBSCRIBE.
+	wantUpdates := make(map[string]int)
+	components := gnmi.GetAll(t, dut, gnmi.OC().ComponentAny().Name().State())
+	for _, component := range components {
+		if component == "" {
+			continue
+		}
+		compTypeVal, present := testhelper.LookupComponentTypeOCCompliant(t, dut, component)
+		if !present || compTypeVal != "SOFTWARE_MODULE" {
+			continue
+		}
+		wantUpdates[component]++
+	}
+	if len(wantUpdates) == 0 {
+		t.Fatal("No software module components found")
+	}
+
+	// Collect interfaces through subscription to be compared to the previous GET.
+	initialValues := gnmi.CollectAll(t, gnmiOpts(t, dut, gpb.SubscriptionMode_ON_CHANGE), gnmi.OC().
+		ComponentAny().
+		SoftwareModule().
+		ModuleType().State(), shortWait).
+		Await(t)
+
+	gotUpdates := make(map[string]int)
+	for _, val := range initialValues {
+		component, err := fetchPathKey(val.Path, componentKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		if val.IsPresent() {
+			gotUpdates[component]++
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got):\n%v", diff)
+	}
+}
+
+func TestWCOnChangeHardwarePort(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID("acfc84d1-b76f-45b3-bb8f-267abca3b2d2").Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Collect every interface through a GET to be compared to the SUBSCRIBE.
+	wantUpdates := make(map[string]int)
+	ports := gnmi.GetAll(t, dut, gnmi.OC().InterfaceAny().Name().State())
+	for _, port := range ports {
+		if strings.Contains(port, intfPrefix) {
+			wantUpdates[port]++
+		}
+	}
+
+	// Collect interfaces through subscription to be compared to the previous GET.
+	initialValues := gnmi.CollectAll(t, gnmiOpts(t, dut, gpb.SubscriptionMode_ON_CHANGE), gnmi.OC().
+		InterfaceAny().
+		HardwarePort().State(), shortWait).
+		Await(t)
+
+	gotUpdates := make(map[string]int)
+	for _, val := range initialValues {
+		port, err := fetchPathKey(val.Path, intfKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		if val.IsPresent() && strings.Contains(port, intfPrefix) {
+			gotUpdates[port]++
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got): %v", diff)
+	}
+}
+
+func TestWCOnChangeComponentType(t *testing.T) {
+	defer testhelper.NewTearDownOptions(t).WithID("e07ea34c-b217-4aef-99ac-2516b0b5c393").Teardown(t)
+	dut := ondatra.DUT(t, "DUT")
+
+	// Determine that updates are received from all expected components.
+	wantUpdates := make(map[string]int)
+	components := gnmi.GetAll(t, dut, gnmi.OC().ComponentAny().State())
+	for _, component := range components {
+		if component.GetName() == "" {
+			continue
+		}
+
+		if _, present := testhelper.LookupComponentTypeOCCompliant(t, dut, component.GetName()); !present {
+			continue
+		}
+
+		wantUpdates[component.GetName()]++
+	}
+
+	// Collect components updates from ON_CHANGE subscription.
+	initialValues := gnmi.CollectAll(t, gnmiOpts(t, dut, gpb.SubscriptionMode_ON_CHANGE), gnmi.OC().
+		ComponentAny().
+		Type().State(), shortWait).
+		Await(t)
+
+	gotUpdates := make(map[string]int)
+	for _, val := range initialValues {
+		component, err := fetchPathKey(val.Path, componentKey)
+		if err != nil {
+			t.Errorf("fetchPathKey() failed: %v", err)
+			continue
+		}
+		if val.IsPresent() {
+			gotUpdates[component] = 1
+		}
+	}
+
+	if diff := cmp.Diff(wantUpdates, gotUpdates); diff != "" {
+		t.Errorf("Update notifications comparison failed! (-want +got):\n%v", diff)
+	}
+}
+
 func gnmiOpts(t *testing.T, dut *ondatra.DUTDevice, mode gpb.SubscriptionMode) *gnmi.Opts {
-        client, err := dut.RawAPIs().BindingDUT().DialGNMI(context.Background())
-        if err != nil {
-                t.Fatalf("DialGNMI() failed: %v", err)
-        }
-        return dut.GNMIOpts().
-                WithClient(client).
-                WithYGNMIOpts(ygnmi.WithSubscriptionMode(mode))
+	client, err := dut.RawAPIs().BindingDUT().DialGNMI(context.Background())
+	if err != nil {
+		t.Fatalf("DialGNMI() failed: %v", err)
+	}
+	return dut.GNMIOpts().
+		WithClient(client).
+		WithYGNMIOpts(ygnmi.WithSubscriptionMode(mode))
 }
