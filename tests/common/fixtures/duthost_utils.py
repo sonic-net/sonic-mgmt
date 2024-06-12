bdc905b6c8e3cf79639415d50284ab07b986d7d5
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2024-06-12 14:15:59 +1000

    waiting for services to be up in ipv6_fixture (#13241)
    Description of PR
    Summary:
    Fixes MSFT ADO 28332318
    
    Approach
    What is the motivation for this PR?
    Fix case error with the following error message:
     failed on setup with "Failed: <ipv6 address> not appeared in xxx netstat"
    
    On some physical testbed, the snmpd process took around 3 minutes to come up.
    Current convert_and_restore_config_db_to_ipv6_only needs to check all process to be up before proceeding with SNMP checking.
    
    How did you do it?
    wait_critical_processes() checks for the critical process which includes snmpd.
    
    How did you verify/test it?
    Tested it locally.
    
    co-authorized by: jianquanye@microsoft.com

--- tests/common/fixtures/duthost_utils.py
+++ tests/common/fixtures/duthost_utils.py
@@ -16,6 +16,7 @@ from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import wait_until
 from jinja2 import Template
 from netaddr import valid_ipv4, valid_ipv6
+from tests.common.platform.processes_utils import wait_critical_processes
 
 
 logger = logging.getLogger(__name__)
@@ -743,6 +744,12 @@ def convert_and_restore_config_db_to_ipv6_only(duthosts):
             logger.info(f"config changed. Doing config reload for {duthost.hostname}")
             config_reload(duthost, wait=120)
     duthosts.reset()
+
+    for duthost in duthosts.nodes:
+        if config_db_modified[duthost.hostname]:
+            # Wait until all critical processes are up,
+            # especially snmpd as it needs to be up for SNMP status verification
+            wait_critical_processes(duthost)
 
     # Verify mgmt-interface status
     mgmt_intf_name = "eth0"
