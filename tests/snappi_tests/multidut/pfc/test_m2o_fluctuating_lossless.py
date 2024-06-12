c8ef8d5cf8b138256369a14e47597c4be6175edd
dks0692 <32043794+dks0692@users.noreply.github.com>
2024-06-12 16:48:05 -0700

    [Snappi] Adding Many To One Fluctuating Lossless Testcase (#8026)
    Tests the dynamic response of lossy and lossless streams destined to a single egress port when the lossless streams cause periods of over-subscription.
    
    Co-authored-by: Dinesh Kumar Sellappan <dinesh.sellappan@keysight.com>

---
+++ tests/snappi_tests/multidut/pfc/test_m2o_fluctuating_lossless.py
@@ -0,0 +1,101 @@
+import pytest
+import random
+import logging
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts, \
+    fanout_graph_facts                                                                       # noqa: F401
+from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port, \
+    snappi_api, snappi_dut_base_config, get_tgen_peer_ports, get_multidut_snappi_ports, \
+    get_multidut_tgen_peer_port_set                                         # noqa: F401
+from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, \
+    lossless_prio_list                                                                      # noqa: F401
+from tests.snappi_tests.variables import config_set, line_card_choice
+from tests.snappi_tests.multidut.pfc.files.m2o_fluctuating_lossless_helper import run_m2o_fluctuating_lossless_test
+from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
+logger = logging.getLogger(__name__)
+pytestmark = [pytest.mark.topology('multidut-tgen')]
+
+
+@pytest.mark.parametrize('line_card_choice', [line_card_choice])
+@pytest.mark.parametrize('linecard_configuration_set', [config_set])
+def test_m2o_fluctuating_lossless(snappi_api,                  # noqa: F811
+                                  conn_graph_facts,            # noqa: F811
+                                  fanout_graph_facts,          # noqa: F811
+                                  line_card_choice,
+                                  duthosts,
+                                  prio_dscp_map,                # noqa: F811
+                                  lossless_prio_list,           # noqa: F811
+                                  linecard_configuration_set,   # noqa: F811
+                                  get_multidut_snappi_ports,):  # noqa: F811
+
+    """
+    Run PFC Fluctuating Lossless Traffic Congestion with many to one traffic pattern
+
+    Args:
+        snappi_api (pytest fixture): SNAPPI session
+        snappi_testbed_config (pytest fixture): testbed configuration information
+        conn_graph_facts (pytest fixture): connection graph
+        fanout_graph_facts (pytest fixture): fanout graph
+        duthosts (pytest fixture): list of DUTs
+        lossy_prio_list (pytest fixture): list of lossy priorities
+        prio_dscp_map (pytest fixture): priority vs. DSCP map (key = priority)
+        line_card_choice: Line card choice to be mentioned in the variable.py file
+        linecard_configuration_set : Line card classification, (min 1 or max 2  hostnames and asics to be given)
+
+    Brief Description:
+        This test uses the m2o_fluctuating_lossless_helper.py file and generates 4 Background traffic and
+        2 Test flow traffic. The background traffic will include four lossy traffic streams, with any priorities
+        0..2 and 5..6, each having 20% bandwidth for a total of 80% of the port line rate. The test data traffic
+        will include two lossless traffic flows, with the SONiC default lossless priorities of 3 and 4. Each of
+        lossless traffic flows will be shaped to have line rate of 20% and 10%, so that there are periods where
+        both lossless flows contribute a bandwidth of 30% (which should cause over-subscription on the egress port).
+        The __gen_traffic() generates the flows. run_traffic() starts the flows and returns the flows stats.
+        The verify_m2o_oversubscribtion_results() takes in the flows stats and verifies the loss criteria
+        mentioned in the flag. Ex: 'loss': '10' means the flows tohave 10% loss, 'loss': '0' means there shouldn't
+        be any loss
+
+    Returns:
+        N/A
+    """
+    if line_card_choice not in linecard_configuration_set.keys():
+        pytest_assert(False, "Invalid line_card_choice value passed in parameter")
+    if (len(linecard_configuration_set[line_card_choice]['hostname']) == 2):
+        dut_list = random.sample(duthosts, 2)
+        duthost1, duthost2 = dut_list
+    elif (len(linecard_configuration_set[line_card_choice]['hostname']) == 1):
+        dut_list = [dut for dut in duthosts if
+                    linecard_configuration_set[line_card_choice]['hostname'] == [dut.hostname]]
+        duthost1, duthost2 = dut_list[0], dut_list[0]
+    else:
+        pytest_assert(False, "Hostname can't be an empty list")
+    snappi_port_list = get_multidut_snappi_ports(line_card_choice=line_card_choice,
+                                                 line_card_info=linecard_configuration_set[line_card_choice])
+    if len(snappi_port_list) < 3:
+        pytest_assert(False, "Need Minimum of 3 ports for the test")
+    snappi_ports = get_multidut_tgen_peer_port_set(line_card_choice, snappi_port_list, config_set, 3)
+
+    testbed_config, port_config_list, snappi_ports = snappi_dut_base_config(dut_list,
+                                                                            snappi_ports,
+                                                                            snappi_api)
+
+    all_prio_list = prio_dscp_map.keys()
+    test_prio_list = lossless_prio_list
+    pause_prio_list = test_prio_list
+    bg_prio_list = [x for x in all_prio_list if x not in pause_prio_list]
+
+    snappi_extra_params = SnappiTestParams()
+    snappi_extra_params.multi_dut_params.duthost1 = duthost1
+    snappi_extra_params.multi_dut_params.duthost2 = duthost2
+    snappi_extra_params.multi_dut_params.multi_dut_ports = snappi_ports
+
+    run_m2o_fluctuating_lossless_test(api=snappi_api,
+                                      testbed_config=testbed_config,
+                                      port_config_list=port_config_list,
+                                      conn_data=conn_graph_facts,
+                                      fanout_data=fanout_graph_facts,
+                                      dut_port=snappi_ports[0]['peer_port'],
+                                      pause_prio_list=pause_prio_list,
+                                      test_prio_list=test_prio_list,
+                                      bg_prio_list=bg_prio_list,
+                                      prio_dscp_map=prio_dscp_map,
+                                      snappi_extra_params=snappi_extra_params)
