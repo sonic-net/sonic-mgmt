866f2a06d56d175e2ae145ca4461ef115cc5714e
Chris <156943338+ccroy-arista@users.noreply.github.com>
2024-06-12 20:47:58 -0700

    sonic-mgmt: add th5 asic type for qos tests (#13230)
    See aristanetworks/sonic-qual.msft/issues/137
    (ASIC type is incorrectly identified as jr2).
    
    In conjunction with the addition of the
    Quicksilver HW SKUs (occurring in a separate
    pull request) the TH5 ASIC type is being
    added to the qos base test file here so it
    can be properly identified during testing,
    such as in the case of test_qos_sai.py.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -41,7 +41,7 @@ class QosBase:
     SUPPORTED_T1_TOPOS = ["t1-lag", "t1-64-lag", "t1-56-lag", "t1-backend", "t1-28-lag", "t1-32-lag"]
     SUPPORTED_PTF_TOPOS = ['ptf32', 'ptf64']
     SUPPORTED_ASIC_LIST = ["pac", "gr", "gb", "td2", "th", "th2", "spc1", "spc2", "spc3", "spc4", "td3", "th3",
-                           "j2c+", "jr2"]
+                           "j2c+", "jr2", "th5"]
 
     BREAKOUT_SKUS = ['Arista-7050-QX-32S']
 
