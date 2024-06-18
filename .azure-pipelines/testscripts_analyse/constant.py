7857940bf9451fe9fa583689a634b40fa960679c
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-06-18 09:35:38 +0800

    Add pfcwd into dataplane (#13322)

--- .azure-pipelines/testscripts_analyse/constant.py
+++ .azure-pipelines/testscripts_analyse/constant.py
@@ -1,7 +1,7 @@
 DATAPLANE_FEATURES = {
         "acl", "arp", "bfd", "copp", "crm", "dash", "decap", "drop_packets", "dualtor", "dualtor_io",
         "ecmp", "everflow", "fdb", "fib", "flow_counter", "ip", "ipfwd", "ixia", "macsec", "mclag",
-        "mpls", "nat", "pfc_asym", "qos", "radv", "read_mac", "route", "sai_qualify", "sflow",
+        "mpls", "nat", "pfc_asym", "pfcwd", "qos", "radv", "read_mac", "route", "sai_qualify", "sflow",
         "snappi_tests", "span", "stress", "upgrade_path", "vlan", "voq", "vrf", "vs_voq_cfgs", "vxlan", "wan"
 }
 
