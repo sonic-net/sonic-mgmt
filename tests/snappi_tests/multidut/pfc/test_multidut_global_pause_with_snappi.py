0dcf086fa065a3ff88f0f35a43ae5e9f3e25b0e7
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2024-06-28 09:38:20 +1000

    random.sample exception in multidut snappi test cases. (#13460)
    Fixes random.sample error in multidut test cases.

--- tests/snappi_tests/multidut/pfc/test_multidut_global_pause_with_snappi.py
+++ tests/snappi_tests/multidut/pfc/test_multidut_global_pause_with_snappi.py
@@ -49,7 +49,7 @@ def test_global_pause(snappi_api,                                   # noqa: F811
     pytest_require(len(linecard_configuration_set[line_card_choice]['hostname']) != 0,
                    "Hostname can't be an empty list")
     if (len(linecard_configuration_set[line_card_choice]['hostname']) == 2):
-        dut_list = random.sample(duthosts, 2)
+        dut_list = random.sample(list(duthosts), 2)
         duthost1, duthost2 = dut_list
     elif (len(linecard_configuration_set[line_card_choice]['hostname']) == 1):
         dut_list = [dut for dut in duthosts if linecard_configuration_set[line_card_choice]['hostname'] == [dut.hostname]]      # noqa: E501
