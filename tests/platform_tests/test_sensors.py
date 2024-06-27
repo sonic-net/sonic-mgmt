5e872dd7c894449b57a58baf6d3cad9dd2a13463
mhen1 <mhen@nvidia.com>
2024-06-27 12:13:15 +0300

    Add support to test_sensors for Dynamic changes of PSUs (#12519)
    What is the motivation for this PR?
    Allow the sensors checks of sensors related to the PSU to be dependent on the PSUs installed on the DUT and not the platform.
    
    How did you do it?
    Created a static mapping between each PSU model the feature supports to the sensors of that PSU (generalized to be independent on the slot number the psu is installed in).
    
    How did you verify/test it?
    Ran the test on a platform with a new PSU to verify it checks the correct sensors.

--- tests/platform_tests/test_sensors.py
+++ tests/platform_tests/test_sensors.py
@@ -3,9 +3,10 @@ import logging
 import os
 import pytest
 import yaml
-
 from tests.common.helpers.assertions import pytest_assert
 from tests.common import mellanox_data
+
+from tests.platform_tests.sensors_utils.psu_sensor_utils import SensorHelper
 
 pytestmark = [
     pytest.mark.topology('any')
@@ -18,17 +19,55 @@ def to_json(obj):
     return json.dumps(obj, indent=4)
 
 
+def to_yaml(obj):
+    return yaml.dump(obj, indent=4)
+
+
 @pytest.fixture(scope='module')
 def sensors_data():
-    sensors_data_file_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), SENSORS_DATA_FILE)
-    return yaml.safe_load(open(sensors_data_file_fullpath).read())
+    """
+    Parses SENSORS_DATA_FILE yaml.
+    """
+    sensors_data_file_full_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), SENSORS_DATA_FILE)
+    return yaml.safe_load(open(sensors_data_file_full_path).read())
+
+
+def update_sensors_checks(duthost, sensors_checks, hardware_version):
+    """
+    This function will update the update_sensors_checks dynamically according to the sensors of the PSUs installed on
+    the dut, specified in psu-sensors-data.yml. In order to do that, we first remove all PSU related sensors from
+    sensors_checks variable and then add the correct ones from psu-sensors-data.yml.
+    @param duthost: duthost fixture
+    @param sensors_checks: the sensors data fetched from sensors-sku-data.yml file
+    @param hardware_version: hardware version as retrieved from mellanox_data.get_hardware_version
+    """
+    sensor_helper = SensorHelper(duthost)
+    if sensor_helper.platform_supports_dynamic_psu():
+        # Remove sensors of PSUs that aren't installed on the dut
+        missing_psu_indexes = sensor_helper.get_missing_psus()
+        if missing_psu_indexes:
+            sensor_helper.remove_psu_checks(sensors_checks, missing_psu_indexes)
+
+        psu_models_to_replace = sensor_helper.get_psu_index_model_dict()
+        # We only replace psus that are covered by psu-sensors-data.yml
+        if psu_models_to_replace:
+            logging.info(f"Fetching PSU sensors for PSUS: {psu_models_to_replace}\n")
+            if sensor_helper.get_uncovered_psus():
+                logging.warning(f"Unsupported PSUs in psu-sensors-data.yml: {sensor_helper.get_uncovered_psus()}\n")
+
+            sensor_helper.remove_psu_checks(sensors_checks, set(psu_models_to_replace.keys()))
+
+            sensor_helper.update_psu_sensors(sensors_checks, psu_models_to_replace, hardware_version)
+        else:
+            logging.warning(f"PSU sensors not covered by psu-sensors-data.yml. "
+                            f"Unsupported PSUs: {sensor_helper.get_uncovered_psus()}\n")
 
 
 def test_sensors(duthosts, rand_one_dut_hostname, sensors_data):
     duthost = duthosts[rand_one_dut_hostname]
     # Get platform name
     platform = duthost.facts['platform']
-
+    hardware_version = ""
     if mellanox_data.is_mellanox_device(duthost):
         hardware_version = mellanox_data.get_hardware_version(duthost, platform)
         if hardware_version:
@@ -40,6 +79,7 @@ def test_sensors(duthosts, rand_one_dut_hostname, sensors_data):
     if platform not in list(sensors_checks.keys()):
         pytest.skip("Skip test due to not support check sensors for current platform({})".format(platform))
 
+    update_sensors_checks(duthost, sensors_checks[platform], hardware_version)
     logging.info("Sensor checks:\n{}".format(to_json(sensors_checks[platform])))
 
     # Gather sensor facts
