d72ff02087b9cb9012179bfd84c787ec6e59a319
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2024-06-28 09:20:06 +1000

    [Snappi] Fixes for T2 ixia test cases: Traffic MAC address, peer_ports, SnappiFanout  (#13470)
    Summary:
    Fixes :
    
    Chose gateway MAC based on port, as the gateway MAC maybe different on packet chassis.
    change peer_ports calculation in get_multidut_tgen_peer_port_set to cover all the dut/asic combination.
    SnappiFanoutManager only supports Snappi chassis, so ignore other type of fanout switches during initialization.
    change the default prefix length to /31 (ipv4), /126(ipv6). as these are the default prefix length that will be advertised in SONiC BGP for connected routes.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/snappi_tests/snappi_helpers.py
+++ tests/common/snappi_tests/snappi_helpers.py
@@ -80,7 +80,9 @@ class SnappiFanoutManager():
         self.ip_address = '0.0.0.0'
 
         for fanout in list(fanout_data.keys()):
-            self.fanout_list.append(fanout_data[fanout])
+            # only snappi fanout, skip other fanout type(eos, sonic, etc.)
+            if fanout_data[fanout]['device_info']['HwSku'] in ('SNAPPI-tester', 'IXIA-tester'):
+                self.fanout_list.append(fanout_data[fanout])
 
     def __parse_fanout_connections__(self):
         device_conn = self.last_device_connection_details
@@ -94,7 +96,7 @@ class SnappiFanoutManager():
                 format(self.ip_address, fanout_port, peer_port, peer_device, speed)
             retval.append(string)
 
-        return(retval)
+        return (retval)
 
     def get_fanout_device_details(self, device_number):
         """With the help of this function you can select the chassis you want
@@ -145,7 +147,7 @@ class SnappiFanoutManager():
         Returns:
             Details of the chassis connection as dictionary format.
         """
-        return(self.last_device_connection_details)
+        return (self.last_device_connection_details)
 
     def get_chassis_ip(self):
         """This function returns IP address of a particular chassis
