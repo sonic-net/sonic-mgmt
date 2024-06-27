5e6fe9de84ae7155db454aa7236142782f10e0bf
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2024-06-28 09:20:06 +1000

    [Snappi] Fixes for T2 ixia test cases: Traffic MAC address, peer_ports, SnappiFanout  (#13470)
    Summary:
    Fixes :
    
    Chose gateway MAC based on port, as the gateway MAC maybe different on packet chassis.
    change peer_ports calculation in get_multidut_tgen_peer_port_set to cover all the dut/asic combination.
    SnappiFanoutManager only supports Snappi chassis, so ignore other type of fanout switches during initialization.
    change the default prefix length to /31 (ipv4), /126(ipv6). as these are the default prefix length that will be advertised in SONiC BGP for connected routes.

--- tests/snappi_tests/variables.py
+++ tests/snappi_tests/variables.py
@@ -49,13 +49,13 @@ config_set = {
                 }
             }
 
-dut_ip_start = '20.1.1.1'
-snappi_ip_start = '20.1.1.2'
-prefix_length = 8
+dut_ip_start = '20.1.1.0'
+snappi_ip_start = '20.1.1.1'
+prefix_length = 31
 
 dut_ipv6_start = '2000:1::1'
 snappi_ipv6_start = '2000:1::2'
-v6_prefix_length = 16
+v6_prefix_length = 126
 
 pfcQueueGroupSize = 8  # can have values 4 or 8
 pfcQueueValueDict = {0: 0,
