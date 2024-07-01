c5deb9095ac20e6566a2fa57bf540f39cdf9d4fc
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2024-07-01 12:15:49 +0800

    Add ecmp inner hash test to PR test (#13472)
    Approach
    What is the motivation for this PR?
    Elastictest performs well in distribute running PR test in multiple KVMs, which support us to add more test scripts to PR checker.
    But some traffic test using ptfadapter can't be tested on KVM platform, we need to skip traffic test if needed
    
    How did you do it?
    Add test to t0 job and skip traffic test
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/ecmp/inner_hashing/test_wr_inner_hashing.py
+++ tests/ecmp/inner_hashing/test_wr_inner_hashing.py
@@ -9,13 +9,13 @@ from tests.common import reboot
 from tests.ecmp.inner_hashing.conftest import get_src_dst_ip_range, FIB_INFO_FILE_DST, VXLAN_PORT,\
     PTF_QLEN, OUTER_ENCAP_FORMATS, NVGRE_TNI, config_pbh
 from tests.ptf_runner import ptf_runner
+from tests.common.fixtures.ptfhost_utils import skip_traffic_test   # noqa F401
 
 logger = logging.getLogger(__name__)
 
 pytestmark = [
     pytest.mark.disable_loganalyzer,
-    pytest.mark.topology('t0'),
-    pytest.mark.asic('mellanox')
+    pytest.mark.topology('t0')
 ]
 
 
@@ -29,7 +29,7 @@ class TestWRDynamicInnerHashing():
 
     def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
                            vlan_ptf_ports, symmetric_hashing, localhost, lag_mem_ptf_ports_groups,
-                           get_function_completeness_level):
+                           get_function_completeness_level, skip_traffic_test):     # noqa F811
         logging.info("Executing warm boot dynamic inner hash test for outer {} and inner {} with symmetric_hashing"
                      " set to {}".format(outer_ipver, inner_ipver, str(symmetric_hashing)))
         with allure.step('Run ptf test InnerHashTest and warm-reboot in parallel'):
@@ -57,6 +57,52 @@ class TestWRDynamicInnerHashing():
             reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm', 10, 0, 0, True, True,))
             reboot_thr.start()
 
+            if not skip_traffic_test:
+                ptf_runner(ptfhost,
+                           "ptftests",
+                           "inner_hash_test.InnerHashTest",
+                           platform_dir="ptftests",
+                           params={"fib_info": FIB_INFO_FILE_DST,
+                                   "router_mac": router_mac,
+                                   "src_ports": vlan_ptf_ports,
+                                   "exp_port_groups": lag_mem_ptf_ports_groups,
+                                   "hash_keys": hash_keys,
+                                   "vxlan_port": VXLAN_PORT,
+                                   "inner_src_ip_range": ",".join(inner_src_ip_range),
+                                   "inner_dst_ip_range": ",".join(inner_dst_ip_range),
+                                   "outer_src_ip_range": ",".join(outer_src_ip_range),
+                                   "outer_dst_ip_range": ",".join(outer_dst_ip_range),
+                                   "balancing_test_times": balancing_test_times,
+                                   "balancing_range": balancing_range,
+                                   "outer_encap_formats": outer_encap_format,
+                                   "nvgre_tni": NVGRE_TNI,
+                                   "symmetric_hashing": symmetric_hashing},
+                           log_file=log_file,
+                           qlen=PTF_QLEN,
+                           socket_recv_size=16384,
+                           is_python3=True)
+            reboot_thr.join()
+
+
+@pytest.mark.static_config
+class TestWRStaticInnerHashing():
+
+    def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
+                           vlan_ptf_ports, symmetric_hashing, localhost, lag_mem_ptf_ports_groups, skip_traffic_test):   # noqa F811
+        logging.info("Executing static inner hash test for outer {} and inner {} with symmetric_hashing set to {}"
+                     .format(outer_ipver, inner_ipver, str(symmetric_hashing)))
+        timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+        log_file = "/tmp/wr_inner_hash_test.StaticInnerHashTest.{}.{}.{}.log"\
+                   .format(outer_ipver, inner_ipver, timestamp)
+        logging.info("PTF log file: %s" % log_file)
+
+        outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
+        inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
+
+        reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm', 10, 0, 0, True, True,))
+        reboot_thr.start()
+
+        if not skip_traffic_test:
             ptf_runner(ptfhost,
                        "ptftests",
                        "inner_hash_test.InnerHashTest",
@@ -71,54 +117,10 @@ class TestWRDynamicInnerHashing():
                                "inner_dst_ip_range": ",".join(inner_dst_ip_range),
                                "outer_src_ip_range": ",".join(outer_src_ip_range),
                                "outer_dst_ip_range": ",".join(outer_dst_ip_range),
-                               "balancing_test_times": balancing_test_times,
-                               "balancing_range": balancing_range,
-                               "outer_encap_formats": outer_encap_format,
-                               "nvgre_tni": NVGRE_TNI,
+                               "outer_encap_formats": OUTER_ENCAP_FORMATS,
                                "symmetric_hashing": symmetric_hashing},
                        log_file=log_file,
                        qlen=PTF_QLEN,
                        socket_recv_size=16384,
                        is_python3=True)
-            reboot_thr.join()
-
-
-@pytest.mark.static_config
-class TestWRStaticInnerHashing():
-
-    def test_inner_hashing(self, duthost, hash_keys, ptfhost, outer_ipver, inner_ipver, router_mac,
-                           vlan_ptf_ports, symmetric_hashing, localhost, lag_mem_ptf_ports_groups):
-        logging.info("Executing static inner hash test for outer {} and inner {} with symmetric_hashing set to {}"
-                     .format(outer_ipver, inner_ipver, str(symmetric_hashing)))
-        timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-        log_file = "/tmp/wr_inner_hash_test.StaticInnerHashTest.{}.{}.{}.log"\
-                   .format(outer_ipver, inner_ipver, timestamp)
-        logging.info("PTF log file: %s" % log_file)
-
-        outer_src_ip_range, outer_dst_ip_range = get_src_dst_ip_range(outer_ipver)
-        inner_src_ip_range, inner_dst_ip_range = get_src_dst_ip_range(inner_ipver)
-
-        reboot_thr = threading.Thread(target=reboot, args=(duthost, localhost, 'warm', 10, 0, 0, True, True,))
-        reboot_thr.start()
-
-        ptf_runner(ptfhost,
-                   "ptftests",
-                   "inner_hash_test.InnerHashTest",
-                   platform_dir="ptftests",
-                   params={"fib_info": FIB_INFO_FILE_DST,
-                           "router_mac": router_mac,
-                           "src_ports": vlan_ptf_ports,
-                           "exp_port_groups": lag_mem_ptf_ports_groups,
-                           "hash_keys": hash_keys,
-                           "vxlan_port": VXLAN_PORT,
-                           "inner_src_ip_range": ",".join(inner_src_ip_range),
-                           "inner_dst_ip_range": ",".join(inner_dst_ip_range),
-                           "outer_src_ip_range": ",".join(outer_src_ip_range),
-                           "outer_dst_ip_range": ",".join(outer_dst_ip_range),
-                           "outer_encap_formats": OUTER_ENCAP_FORMATS,
-                           "symmetric_hashing": symmetric_hashing},
-                   log_file=log_file,
-                   qlen=PTF_QLEN,
-                   socket_recv_size=16384,
-                   is_python3=True)
         reboot_thr.join()
