5fbe64aa3bf331bc477f97063db00274305c90e4
ganglyu <ganglv@microsoft.com>
2024-07-04 11:21:43 +0800

    Fix precommit error

--- tests/dns/test_dns_resolv_conf.py
+++ tests/dns/test_dns_resolv_conf.py
@@ -38,29 +38,6 @@ def test_dns_resolv_conf(duthost):
                   "Mismatch between expected and current nameservers! Expected: [{}]. Current: [{}].".format(
                   " ".join(expected_nameservers), " ".join(current_nameservers)))
 
-
-def test_dns_resolv_conf_container(duthost):
-    """verify that /etc/resolv.conf in container contains the expected nameservers
-
-    Args:
-        duthost: AnsibleHost instance for DUT
-    """
-
-    # If running a public image with sonic-mgmt-int, we can't say for sure whether we should expect there to not be any
-    # DNS servers configured in /etc/resolv.conf, or whether the internal DNS should be configured. This is becuase
-    # pretest configures the internal DNS server, but if there's some system reload/reboot, then this may go away/be
-    # overwritten.
-    #
-    # There are some changes that might improve the state of things (resolvconf, config_db support), but for now, just
-    # skip this combination.
-    if get_image_type(duthost) == "public":
-        pytest.skip("Inconsistent expectations for DNS server when running a public image with sonic-mgmt-int")
-
-    # Check SONiC image type and get expected nameservers in /etc/resolv.conf
-    expected_nameservers = set(RESOLV_CONF_NAMESERVERS[get_image_type(duthost=duthost)])
-
-    logger.info("expected nameservers: [{}]".format(" ".join(expected_nameservers)))
-
     containers = duthost.get_running_containers()
     for container in containers:
         resolv_conf = duthost.shell("docker exec %s cat /etc/resolv.conf" % container, module_ignore_errors=True)
@@ -76,5 +53,5 @@ def test_dns_resolv_conf_container(duthost):
         logger.info("{} container, current nameservers: [{}]".format(container, " ".join(current_nameservers)))
 
         pytest_assert(not (current_nameservers ^ expected_nameservers),
-                    "Mismatch between expected and current nameservers for {}! Expected: [{}]. Current: [{}].".format(
-                    container, " ".join(expected_nameservers), " ".join(current_nameservers)))
+                      "Mismatch between expected and current nameservers for {}! Expected: [{}]. Current: [{}].".format(
+                      container, " ".join(expected_nameservers), " ".join(current_nameservers)))
