6ba6dbf7a3c075434de8b0e3dd51ba3687f6345e
bingwang <bingwang@microsoft.com>
2024-07-05 22:55:16 +0000

    Change shell to command

--- tests/upgrade_path/test_upgrade_path.py
+++ tests/upgrade_path/test_upgrade_path.py
@@ -89,9 +89,9 @@ def setup_upgrade_test(duthost, localhost, from_image, to_image, tbinfo,
     # Remove config_db.json before cold-reboot to target image
     # to ensure the new config_db.json is generated from minigraph
     if "cold" in reboot_type \
-            and duthost.shell("ls /host/old_config/minigraph.xml", module_ignore_errors=True)['rc'] == 0:
+            and duthost.command("ls /host/old_config/minigraph.xml", module_ignore_errors=True)['rc'] == 0:
         logger.info("Remove config_db.json before cold reboot to target image")
-        duthost.shell("rm -f /host/old_config/config_db.json", module_ignore_errors=True)
+        duthost.command("rm -f /host/old_config/config_db.json", module_ignore_errors=True)
 
     if allow_fail and modify_reboot_script:
         # add fail step to reboot script
