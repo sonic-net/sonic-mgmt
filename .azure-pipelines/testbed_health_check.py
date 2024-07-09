2809ab13fa617e5bd5e60137b6cbb6ade33a9263
Jianquan Ye <jianquanye@microsoft.com>
2024-07-09 11:15:36 +0000

    Merged PR 6402: Skip Elastictest health check on 8800 testbeds
    Skip Elastictest health check on 8800 testbeds

--- .azure-pipelines/testbed_health_check.py
+++ .azure-pipelines/testbed_health_check.py
@@ -99,6 +99,15 @@ class TestbedHealthChecker:
 
         logger.info("======================= init_hosts starts =======================")
 
+        skip_healthy_check_testbed_list = ["vms68-t2-8800-1", "vms68-t2-8800-2",
+                                           "vms29-t2-8800-2", "vms29-t2-8800-3",
+                                           "vmsvc5-t2-8800-1", "vmsvc5-t2-8800-2"]
+
+        if self.testbed_name in skip_healthy_check_testbed_list:
+            errmsg = "Skipping perform checks on {}.".format(self.testbed_name)
+            logger.info(errmsg)
+            raise SkipCurrentTestbed(errmsg)
+
         # Init localhost
         self.localhost = init_localhost(self.inventory, options={"verbosity": self.log_verbosity})
         if not self.localhost:
