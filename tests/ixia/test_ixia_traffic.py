88351ee2f40a4032c2895f8163202db1449861b9
xwjiang-ms <96218837+xwjiang-ms@users.noreply.github.com>
2024-07-10 09:47:37 +0800

    Add ixia and wan test to pr skip yml (#13598)
    What is the motivation for this PR?
    Ixia test do not support on kvm testbed, and wan test do not support in PR test, need to add those test to pr test skip yml to explicitly skip in PR testing.
    
    How did you do it?
    Add ixia/wan test to PR test skip yml, and add pytest marker in ixia test
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/ixia/test_ixia_traffic.py
+++ tests/ixia/test_ixia_traffic.py
@@ -26,8 +26,12 @@ from tests.common.ixia.common_helpers import get_vlan_subnet, get_addrs_in_subne
     get_peer_ixia_chassis
 
 
-@pytest.mark.disable_loganalyzer
-@pytest.mark.topology("tgen")
+pytestmark = [
+    pytest.mark.topology('tgen'),
+    pytest.mark.disable_loganalyzer
+]
+
+
 def test_testbed(conn_graph_facts, duthosts, rand_one_dut_hostname, fanout_graph_facts,     # noqa F811
                  ixia_api_server_session, fanouthosts):                                     # noqa F811
     duthost = duthosts[rand_one_dut_hostname]
