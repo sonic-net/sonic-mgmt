6b10abf078e622d7c0b54be43405fa3bff77c0fe
selldinesh <dinesh.sellappan@keysight.com>
2024-07-16 02:32:25 +0000

    fixing trailing whitespace

--- tests/snappi_tests/multidut/bgp/files/bgp_outbound_helper.py
+++ tests/snappi_tests/multidut/bgp/files/bgp_outbound_helper.py
@@ -228,7 +228,7 @@ def duthost_bgp_config(duthosts,
     duthosts[0].copy(src="/tmp/temp_config.json", dest="/etc/sonic/config_db.json")
 
     logger.info('Reloading config_db.json to apply IP and BGP configuration on {}'.format(duthosts[0].hostname))
-    pytest_assert('Error' not in duthosts[0].shell("sudo config reload -f -y \n")['stderr'], 
+    pytest_assert('Error' not in duthosts[0].shell("sudo config reload -f -y \n")['stderr'],
                   'Error while reloading config in {} !!!!!'.format(duthosts[0].hostname))
     logger.info('Config Reload Successful in {} !!!'.format(duthosts[0].hostname))
 
