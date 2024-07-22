64a5bf1c37f80434dde4d5a113ae699bf77412f0
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2024-07-22 14:21:46 +1000

    [Snappi] Fix duthost port mapping in multidut snappi tests (PFCWD, ECN) (#13541)
    Description of PR
    Summary:
    Fixes #13443
    
    Approach
    What is the motivation for this PR?
    Fix duthost and port mapping mismatch issue in multidut PFCWD and ECN test cases. PFC test cases will be changed in another PR to avoid large commit.
    
    How did you do it?
    Add 'duthost' into get_multidut_snappi_ports, so that we can retrieve duthost based on port information.
    
    How did you verify/test it?
    Test it manually. all PFCWD test cases can run successfully. There are test failures but are not related to this change.
    
    Any platform specific information?
    Supported testbed topology if it's a new test case?
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/snappi_tests/snappi_fixtures.py
+++ tests/common/snappi_tests/snappi_fixtures.py
@@ -630,6 +630,7 @@ def get_multidut_snappi_ports(duthosts, conn_graph_facts, fanout_graph_facts):  
                         if port["peer_port"] in asic_port_map[asic] and hostname in port['peer_device']:
                             port['asic_value'] = asic
                             port['asic_type'] = host.facts["asic_type"]
+                            port['duthost'] = host
                             ports.append(port)
         return ports
     return _get_multidut_snappi_ports
