230fb9c50056d0080aba3ca99de7b8f06285b20e
Xin Wang <xiwang5@microsoft.com>
2024-07-23 15:42:11 +0800

    Fix az login timeout issue while running test_plan.py (#13736)
    What is the motivation for this PR?
    The test_plan.py may run for over 24 hours polling test plan status if the test plan is in EXECUTING.
    
    The test_plan.py script is wrapped in AzureCLI task which takes cares of az login. However, the az login session may expire after 24 hours. In this case, the test_plan.py script will fail to get access token for accessing Elastictest API to poll test plan status.
    
    How did you do it?
    The workaround is run "az login" every 12 hours in test_plan.py to ensure that long running tests won't fail with getting access token.
    
    To support the az login session, need to pass some more environment variables to the test_plan.py. The run elastictest pipeline yaml has been updated accordingly.
    
    New variable SONIC_AUTOMATION_SERVICE_PRINCIPAL has been added to variable group SONiC-Elastictest on Azure pipeline platform.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- .azure-pipelines/test_plan.py
+++ .azure-pipelines/test_plan.py
@@ -131,18 +131,6 @@ class FinishStatus(AbstractStatus):
         super(FinishStatus, self).__init__(TestPlanStatus.FINISHED)
 
 
-# def get_scope(elastictest_url):
-#     scope = "api://sonic-testbed-tools-dev/.default"
-#     if elastictest_url in [
-#         "http://sonic-testbed2-scheduler-backend.azurewebsites.net",
-#         "https://sonic-testbed2-scheduler-backend.azurewebsites.net",
-#         "http://sonic-elastictest-prod-scheduler-backend-webapp.azurewebsites.net",
-#         "https://sonic-elastictest-prod-scheduler-backend-webapp.azurewebsites.net"
-#     ]:
-#         scope = "api://sonic-testbed-tools-prod/.default"
-#     return scope
-
-
 def parse_list_from_str(s):
     # Since Azure Pipeline doesn't support to receive an empty parameter,
     # We use ' ' as a magic code for empty parameter.
@@ -159,6 +147,7 @@ def parse_list_from_str(s):
 class TestPlanManager(object):
 
     def __init__(self, url, frontend_url, client_id=None):
+        self.last_login_time = datetime.now()
         self.url = url
         self.frontend_url = frontend_url
         self.client_id = client_id
@@ -181,7 +170,34 @@ class TestPlanManager(object):
 
         return stdout, stderr, return_code
 
+    def az_run(self, cmd):
+        stdout, stderr, retcode = self.cmd(cmd.split())
+        if retcode != 0:
+            raise Exception(f'Command {cmd} execution failed, rc={retcode}, error={stderr}')
+        return stdout, stderr, retcode
+
     def get_token(self):
+
+        # Success of "az account get-access-token" depends on "az login". However, the "az login" session may expire
+        # after 24 hours. So we re-login every 12 hours to ensure success of "az account get-access-token".
+        if datetime.now() - self.last_login_time > timedelta(hours=12):
+            cmd = "az login --service-principal -u {} --tenant {} --allow-no-subscriptions --federated-token {}"\
+                .format(
+                    os.environ.get("SONIC_AUTOMATION_SERVICE_PRINCIPAL"),
+                    os.environ.get("ELASTICTEST_MSAL_TENANT_ID"),
+                    os.environ.get("SYSTEM_ACCESS_TOKEN")
+                )
+            attempt = 0
+            while (attempt < MAX_GET_TOKEN_RETRY_TIMES):
+                try:
+                    stdout, _, _ = self.az_run(cmd)
+                    self.last_login_time = datetime.now()
+                    break
+                except Exception as exception:
+                    attempt += 1
+                    print("Failed to login with exception: {}. Retry {} times to login."
+                          .format(repr(exception), MAX_GET_TOKEN_RETRY_TIMES - attempt))
+
         token_is_valid = \
             self._token_expires_on is not None and \
             (self._token_expires_on - datetime.now()) > timedelta(hours=TOKEN_EXPIRE_HOURS)
@@ -192,10 +208,8 @@ class TestPlanManager(object):
         cmd = 'az account get-access-token --resource {}'.format(self.client_id)
         attempt = 0
         while (attempt < MAX_GET_TOKEN_RETRY_TIMES):
-            stdout, stderr, return_code = self.cmd(cmd.split())
             try:
-                if return_code != 0:
-                    raise Exception("Failed to get token: rc: {}, error: {}".format(return_code, stderr))
+                stdout, _, _ = self.az_run(cmd)
 
                 token = json.loads(stdout.decode("utf-8"))
                 self._token = token.get("accessToken", None)
@@ -213,30 +227,6 @@ class TestPlanManager(object):
                 print("Failed to get token with exception: {}".format(repr(exception)))
 
         raise Exception("Failed to get token after {} attempts".format(MAX_GET_TOKEN_RETRY_TIMES))
-
-        # token_url = "https://login.microsoftonline.com/{}/oauth2/v2.0/token".format(self.tenant_id)
-        # headers = {
-        #     "Content-Type": "application/x-www-form-urlencoded"
-        # }
-
-        # payload = {
-        #     "grant_type": "client_credentials",
-        #     "client_id": self.client_id,
-        #     "client_secret": self.client_secret,
-        #     "scope": get_scope(self.url)
-        # }
-        # attempt = 0
-        # while (attempt < MAX_GET_TOKEN_RETRY_TIMES):
-        #     try:
-        #         resp = requests.post(token_url, headers=headers, data=payload, timeout=10).json()
-        #         self._token = resp["access_token"]
-        #         self._token_generate_time = datetime.utcnow()
-        #         return self._token
-        #     except Exception as exception:
-        #         attempt += 1
-        #         print("Get token failed with exception: {}. Retry {} times to get token."
-        #               .format(repr(exception), MAX_GET_TOKEN_RETRY_TIMES - attempt))
-        # raise Exception("Failed to get token after {} attempts".format(MAX_GET_TOKEN_RETRY_TIMES))
 
     def create(self, topology, test_plan_name="my_test_plan", deploy_mg_extra_params="", kvm_build_id="",
                min_worker=None, max_worker=None, pr_id="unknown", output=None,
