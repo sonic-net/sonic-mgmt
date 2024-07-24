e59767e7256f7e63c25d5c24a4c5a1a6136a5a0a
Scott Pilkey <spilkey@cisco.com>
2024-07-24 11:32:47 -0700

    Increase post-PDU state change delay to allow all PSUs to react

--- tests/platform_tests/test_platform_info.py
+++ tests/platform_tests/test_platform_info.py
@@ -27,6 +27,8 @@ CMD_PLATFORM_PSUSTATUS_JSON = "{} --json".format(CMD_PLATFORM_PSUSTATUS)
 CMD_PLATFORM_FANSTATUS = "show platform fan"
 CMD_PLATFORM_TEMPER = "show platform temperature"
 
+PDU_WAIT_TIME = 20
+
 THERMAL_CONTROL_TEST_WAIT_TIME = 65
 THERMAL_CONTROL_TEST_CHECK_INTERVAL = 5
 
@@ -209,8 +211,7 @@ def turn_all_outlets_on(pdu_ctrl):
     for outlet in all_outlet_status:
         if not outlet["outlet_on"]:
             pdu_ctrl.turn_on_outlet(outlet)
-            time.sleep(5)
-    time.sleep(5)
+    time.sleep(PDU_WAIT_TIME)
 
 
 def check_all_psu_on(dut, psu_test_results):
@@ -296,7 +297,7 @@ def test_turn_on_off_psu_and_check_psustatus(duthosts,
 
         logging.info("Turn off outlet {}".format(outlet))
         pdu_ctrl.turn_off_outlet(outlet)
-        time.sleep(10)
+        time.sleep(PDU_WAIT_TIME)
 
         cli_psu_status = duthost.command(CMD_PLATFORM_PSUSTATUS)
         for line in cli_psu_status["stdout_lines"][2:]:
@@ -310,7 +311,7 @@ def test_turn_on_off_psu_and_check_psustatus(duthosts,
 
         logging.info("Turn on outlet {}".format(outlet))
         pdu_ctrl.turn_on_outlet(outlet)
-        time.sleep(10)
+        time.sleep(PDU_WAIT_TIME)
 
         cli_psu_status = duthost.command(CMD_PLATFORM_PSUSTATUS)
         for line in cli_psu_status["stdout_lines"][2:]:
