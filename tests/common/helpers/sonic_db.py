69ef7796e2c6d402505f42ac756e5dd0976b3d7f
Sai Kiran <110003254+opcoder0@users.noreply.github.com>
2024-07-29 22:36:25 +1000

    Add ASIC DB validation to ACL tests (#13574)
    This PR extends AsicDbCli, SonicDbCli and adds fixtures for tests to be able to perform ASIC DB validation. The PR includes the following changes -
    
    Extend AsicDbCli with ACL, Next hop and route entry methods
    Extend SonicDbCli with hget_all
    Add ASIC DB fixtures for tests
    Add ASIC DB validation tests to ACL tests
    Fix port numbers to match configured range for tests test_l4_dport_range_match_dropped and test_l4_sport_range_match_dropped ACL test cases.
    Add README
    
    What is the motivation for this PR?
    Performing ASIC DB validation for test cases improves the depth of the coverage of tests.
    
    How did you do it?
    By extending AsicDbCli and SonicDbCli. Added methods to access ASIC DB entries for route entry table, next hop table, ACL entries. Added ASIC DB validation to ACL tests.
    
    How did you verify/test it?
    Ran ACL tests on s6100 and 8111 DUTs.
    
    Any platform specific information?
    None
    
    Supported testbed topology if it's a new test case?
    Not applicable
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/helpers/sonic_db.py
+++ tests/common/helpers/sonic_db.py
@@ -1,3 +1,5 @@
+import ast
+import ipaddress
 import logging
 import json
 import six
@@ -92,6 +94,32 @@ class SonicDbCli(object):
                 return result['stdout'].decode('unicode-escape')
             else:
                 return result['stdout']
+
+    def hget_all(self, key):
+        """
+        Executes a sonic-db-cli HGETALL command.
+
+        Args:
+            key: full name of the key to get.
+
+        Returns:
+            The corresponding value of the key.
+        Raises:
+            SonicDbKeyNotFound: If the key is not found.
+        """
+
+        cmd = self._cli_prefix() + "HGETALL {}".format(key)
+        result = self._run_and_check(cmd)
+        if result == {}:
+            raise SonicDbKeyNotFound("Key: %s not found in sonic-db cmd: %s" % (key, cmd))
+        else:
+            v = None
+            if six.PY2:
+                v = result['stdout'].decode('unicode-escape')
+            else:
+                v = result['stdout']
+            v_dict = ast.literal_eval(v)
+            return v_dict
 
     def get_and_check_key_value(self, key, value, field=None):
         """
@@ -192,6 +220,10 @@ class AsicDbCli(SonicDbCli):
     ASIC_LAG_MEMBER_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER"
     ASIC_ROUTERINTF_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE"
     ASIC_NEIGH_ENTRY_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_NEIGHBOR_ENTRY"
+    ASIC_ROUTE_ENTRY_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY"
+    ASIC_NEXT_HOP_TABLE = "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP"
+    ASIC_ACL_ENTRY = "ASIC_STATE:SAI_OBJECT_TYPE_ACL_ENTRY"
+    ASIC_ACL_RANGE = "ASIC_STATE:SAI_OBJECT_TYPE_ACL_RANGE"
 
     def __init__(self, host):
         """
@@ -204,11 +236,18 @@ class AsicDbCli(SonicDbCli):
         self.system_port_key_list = []
         self.port_key_list = []
         self.lagid_key_list = []
+        self.acl_entries = []
+        self.acl_range_key_to_value = {}
 
     def get_switch_key(self):
         """Returns a list of keys in the switch table"""
         cmd = self._cli_prefix() + "KEYS %s*" % AsicDbCli.ASIC_SWITCH_TABLE
         return self._run_and_raise(cmd)["stdout_lines"][0]
+
+    def get_switch_key_value(self):
+        """Returns the value of the switch key"""
+        switch_key = self.get_switch_key()
+        return self.hget_all(switch_key)
 
     def get_system_port_key_list(self, refresh=False):
         """Returns a list of keys in the system port table"""
@@ -257,6 +296,166 @@ class AsicDbCli(SonicDbCli):
         """Returns a list of keys in the neighbor table"""
         cmd = self._cli_prefix() + "KEYS %s:*" % AsicDbCli.ASIC_NEIGH_ENTRY_TABLE
         return self._run_and_raise(cmd)["stdout_lines"]
+
+    def get_route_entries(self):
+        """Returns a list of route entries"""
+        cmd = self._cli_prefix() + "KEYS %s:*" % AsicDbCli.ASIC_ROUTE_ENTRY_TABLE
+        return self._run_and_raise(cmd)['stdout_lines']
+
+    def get_route_entries_by_dest_ip(self, dest_ip=None):
+        """
+        Returns a list of route entries based on destination IP address.
+        dest_ip can be a full IP address or partial address. With values
+                like '192.168.0.0/21', '192.168' or 'fc02:1000::' and so on.
+        """
+        if dest_ip is None:
+            return self.get_route_entries()
+
+        cmd = self._cli_prefix() + "KEYS %s:{\"dest\":\"%s*" % (AsicDbCli.ASIC_ROUTE_ENTRY_TABLE, dest_ip)
+        return self._run_and_raise(cmd)['stdout_lines']
+
+    def get_next_hop_entries(self):
+        """Returns all next hop entries"""
+        cmd = self._cli_prefix() + "KEYS %s:*" % AsicDbCli.ASIC_NEXT_HOP_TABLE
+        return self._run_and_raise(cmd)['stdout_lines']
+
+    def get_acl_entries(self, refresh=False):
+        """Returns all ACL entries"""
+        if self.acl_entries != [] and refresh is False:
+            return self.acl_entries
+        cmd = self._cli_prefix() + "KEYS %s:*" % AsicDbCli.ASIC_ACL_ENTRY
+        entry_keys = self._run_and_raise(cmd)['stdout_lines']
+        for k in entry_keys:
+            self.acl_entries.append(self.hget_all(k))
+        return self.acl_entries
+
+    def get_acl_range_entries(self, refresh=False):
+        """Returns all ACL range entries"""
+        if self.acl_range_key_to_value and refresh is False:
+            return self.acl_range_key_to_value
+        cmd = self._cli_prefix() + "KEYS %s:*" % AsicDbCli.ASIC_ACL_RANGE
+        range_keys = self._run_and_raise(cmd)['stdout_lines']
+        for k in range_keys:
+            self.acl_range_key_to_value[k] = self.hget_all(k)
+        return self.acl_range_key_to_value
+
+    def find_acl_by(self, **kwargs):
+        """
+        Returns ACL entries matching the following key values. Supported
+        keys include: ether_type, ip_protocol, tcp_flags, packet_action,
+        src_ip, dst_ip l4_src_port, l4_dst_port, range_type.
+        range_type recognizes 'l4_src_port' or 'l4_dst_port' values. A corresponding
+        l4_src_port or l4_dst_port value is expected.
+
+        If more than one key is specified the find function assumes 'and' operation
+        to match an entry.
+        """
+
+        # kwargs keys to ACL_ENTRY field names
+        key_to_field_name = {
+            'ether_type': 'SAI_ACL_ENTRY_ATTR_FIELD_ETHER_TYPE',
+            'ip_protocol': 'SAI_ACL_ENTRY_ATTR_FIELD_IP_PROTOCOL',
+            'ipv6_next_header': 'SAI_ACL_ENTRY_ATTR_FIELD_IPV6_NEXT_HEADER',
+            'icmp_type': 'SAI_ACL_ENTRY_ATTR_FIELD_ICMP_TYPE',
+            'icmp_code': 'SAI_ACL_ENTRY_ATTR_FIELD_ICMP_CODE',
+            'icmpv6_type': 'SAI_ACL_ENTRY_ATTR_FIELD_ICMPV6_TYPE',
+            'icmpv6_code': 'SAI_ACL_ENTRY_ATTR_FIELD_ICMPV6_CODE',
+            'tcp_flags': 'SAI_ACL_ENTRY_ATTR_FIELD_TCP_FLAGS',
+            'packet_action': 'SAI_ACL_ENTRY_ATTR_ACTION_PACKET_ACTION',
+            'src_ip': 'SAI_ACL_ENTRY_ATTR_FIELD_SRC_IP',
+            'src_ipv6': 'SAI_ACL_ENTRY_ATTR_FIELD_SRC_IPV6',
+            'dst_ip': 'SAI_ACL_ENTRY_ATTR_FIELD_DST_IP',
+            'dst_ipv6': 'SAI_ACL_ENTRY_ATTR_FIELD_DST_IPV6',
+            'l4_src_port': 'SAI_ACL_ENTRY_ATTR_FIELD_L4_SRC_PORT',
+            'l4_dst_port': 'SAI_ACL_ENTRY_ATTR_FIELD_L4_DST_PORT',
+            'range_type': 'SAI_ACL_ENTRY_ATTR_FIELD_ACL_RANGE_TYPE'
+        }
+
+        # ACL_RANGE keys
+        l4_src_port_range_key = 'SAI_ACL_RANGE_TYPE_L4_SRC_PORT_RANGE'
+        l4_dst_port_range_key = 'SAI_ACL_RANGE_TYPE_L4_DST_PORT_RANGE'
+        acl_range_attr_type_key = 'SAI_ACL_RANGE_ATTR_TYPE'
+        acl_range_limit_key = 'SAI_ACL_RANGE_ATTR_LIMIT'
+
+        q = {}
+        for k, v in kwargs.items():
+            if v is not None:
+                # reset key from src_ip -> src_ipv6, dst_ip -> dst_ipv6
+                # based on IP version
+                if k == 'src_ip' or k == 'dst_ip':
+                    ip_ver = ipaddress.ip_address(v).version
+                    logger.debug(f'k is {k} and ip_ver = {ip_ver}')
+                    if ip_ver == 6:
+                        if k == 'src_ip':
+                            q[key_to_field_name['src_ipv6']] = v
+                        if k == 'dst_ip':
+                            q[key_to_field_name['dst_ipv6']] = v
+                    else:
+                        q[key_to_field_name[k]] = v
+                    continue
+                q[key_to_field_name[k]] = v
+
+        if not q:
+            return []
+
+        if self.acl_entries == []:
+            logger.debug('Cannot find acl entry; empty acl_entries')
+            return []
+
+        l4_port, l4_port_key = '', ''
+        if key_to_field_name['range_type'] in q:
+            if key_to_field_name['l4_src_port'] not in q and key_to_field_name['l4_dst_port'] not in q:
+                raise ValueError('range type requires a src or dst port number')
+            if q[key_to_field_name['range_type']] == 'l4_src_port':
+                l4_port, l4_port_key = q[key_to_field_name['l4_src_port']], l4_src_port_range_key
+                del q[key_to_field_name['l4_src_port']]
+            elif q[key_to_field_name['range_type']] == 'l4_dst_port':
+                l4_port, l4_port_key = q[key_to_field_name['l4_dst_port']], l4_dst_port_range_key
+                del q[key_to_field_name['l4_dst_port']]
+            else:
+                raise ValueError('unknown range type')
+            logger.debug(f'ACL Range table {self.acl_range_key_to_value}')
+
+        def range_matched(range_oid):
+            # uses l4_port and l4_port_key
+            k = f'ASIC_STATE:SAI_OBJECT_TYPE_ACL_RANGE:oid:{range_oid}'
+            v = self.acl_range_key_to_value.get(k)
+            if v is None:
+                return False
+            if v.get(acl_range_attr_type_key) != l4_port_key:
+                return False
+            start_port, end_port = v[acl_range_limit_key].split(',')
+            # l4_port can be hex starting with 0x or a decimal but not
+            # hex without 0x prefix
+            if int(l4_port, 0) >= int(start_port) and int(l4_port, 0) <= int(end_port):
+                return True
+            return False
+
+        def entry_matched(entry):
+            # k - search condition, v - value passed
+            for k, v in q.items():
+                entry_val = entry.get(k)
+                if entry_val is None:
+                    return False
+                if k == key_to_field_name['range_type']:
+                    range_oid = entry_val[entry_val.find('0x'):]
+                    if range_matched(range_oid) is False:
+                        return False
+                else:
+                    # remove '&mask'
+                    pos = entry_val.find('&')
+                    n = len(entry_val) if pos == -1 else pos
+                    v_clean = entry_val[:n]
+                    if v != v_clean:
+                        return False
+            return True
+
+        entries = []
+        for acl_entry in self.acl_entries:
+            if entry_matched(acl_entry):
+                entries.append(acl_entry)
+
+        return entries
 
     def get_neighbor_key_by_ip(self, ipaddr):
         """Returns the key in the neighbor table that is for a specific IP neighbor
