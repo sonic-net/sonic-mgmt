8b9249a1580cec7b0760608f35dc7668b4b11d19
rbpittman <rapittma@cisco.com>
2024-08-02 20:23:00 -0400

    Add Cisco GR2 hwsku, setup test markings for appropriate support (#13825)
    * Add GR2 hwsku, setup test markings for appropriate support.
    
    * Add hwsku for 8122
    
    * Add gr2 to supported asic list for test qos sai.

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -40,7 +40,7 @@ class QosBase:
                           "t0-standalone-64", "t0-standalone-128", "t0-standalone-256"]
     SUPPORTED_T1_TOPOS = ["t1-lag", "t1-64-lag", "t1-56-lag", "t1-backend", "t1-28-lag", "t1-32-lag"]
     SUPPORTED_PTF_TOPOS = ['ptf32', 'ptf64']
-    SUPPORTED_ASIC_LIST = ["pac", "gr", "gb", "td2", "th", "th2", "spc1", "spc2", "spc3", "spc4", "td3", "th3",
+    SUPPORTED_ASIC_LIST = ["pac", "gr", "gr2", "gb", "td2", "th", "th2", "spc1", "spc2", "spc3", "spc4", "td3", "th3",
                            "j2c+", "jr2", "th5"]
 
     BREAKOUT_SKUS = ['Arista-7050-QX-32S']
