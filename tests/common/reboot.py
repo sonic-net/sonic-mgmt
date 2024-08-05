3afa347fc86567c25d0b1dbf9827fe14bd7466fc
Yutong Zhang <yutongzhang@microsoft.com>
2024-08-05 11:19:00 +0800

    test

--- tests/common/reboot.py
+++ tests/common/reboot.py
@@ -353,13 +353,16 @@ def check_reboot_cause(dut, reboot_cause_expected):
     @param dut: The AnsibleHost object of DUT.
     @param reboot_cause_expected: The expected reboot cause.
     """
-    # For kvm testbed, command `show reboot-cause` will return Unknown.
-    # So, overwrite the reboot_cause_expected as `Unknown`
-    if dut.facts["asic_type"] == "vs":
-        reboot_cause_expected = "Unknown"
-
     reboot_cause_got = get_reboot_cause(dut)
     logger.debug("dut {} last reboot-cause {}".format(dut.hostname, reboot_cause_got))
+
+    # For kvm testbed, the expected output of command `show reboot-cause`
+    # is such like "User issued 'reboot' command [User: admin, Time: Sun Aug  4 06:43:19 PM UTC 2024]"
+    # So, overwrite the reboot_cause_expected
+    if dut.facts["asic_type"] == "vs":
+        reboot_cause_expected = "User issued \'reboot\' command"
+        return reboot_cause_expected in reboot_cause_got
+
     return reboot_cause_got == reboot_cause_expected
 
 
