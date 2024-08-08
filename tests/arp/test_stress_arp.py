853558a4f6de241263ffa859f86eaa96ad310839
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-08-08 11:10:32 +0800

    Tolerate hash collision when adding entries in `test_stress_arp.py` (#13861)
    What is the motivation for this PR?
    In script test_stress_arp.py, we will add as much ipv4 arp entries and ipv6 nd entries as we can. We will get this number from crm. But in fact, there is a probability of hash collision, which means we can not add such number entries as we expected. So in this PR, we tolerate this possible and set the percentage of hash collision 1%
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/arp/test_stress_arp.py
+++ tests/arp/test_stress_arp.py
@@ -87,7 +87,12 @@ def test_ipv4_arp(duthost, garp_enabled, ip_and_intf_info, intfs_for_test,
         try:
             add_arp(ptf_intf_ipv4_hosts, intf1_index, ptfadapter)
             if not skip_traffic_test:
-                pytest_assert(wait_until(20, 1, 0, lambda: get_fdb_dynamic_mac_count(duthost) >= arp_avaliable),
+                # There is a certain probability of hash collision, we set the percentage as 1% here
+                # The entries we add will not exceed 10000, so the number we tolerate is 100
+                logger.debug("Expected route number: {}, real route number {}"
+                             .format(arp_avaliable, get_fdb_dynamic_mac_count(duthost)))
+                pytest_assert(wait_until(20, 1, 0,
+                                         lambda: abs(arp_avaliable - get_fdb_dynamic_mac_count(duthost)) < 100),
                               "ARP Table Add failed")
         finally:
             clear_dut_arp_cache(duthost)
@@ -161,7 +166,12 @@ def test_ipv6_nd(duthost, ptfhost, config_facts, tbinfo, ip_and_intf_info,
         try:
             add_nd(ptfadapter, ip_and_intf_info, ptf_intf_index, nd_avaliable)
             if not skip_traffic_test:
-                pytest_assert(wait_until(20, 1, 0, lambda: get_fdb_dynamic_mac_count(duthost) >= nd_avaliable),
+                # There is a certain probability of hash collision, we set the percentage as 1% here
+                # The entries we add will not exceed 10000, so the number we tolerate is 100
+                logger.debug("Expected route number: {}, real route number {}"
+                             .format(nd_avaliable, get_fdb_dynamic_mac_count(duthost)))
+                pytest_assert(wait_until(20, 1, 0,
+                                         lambda: abs(nd_avaliable - get_fdb_dynamic_mac_count(duthost)) < 100),
                               "Neighbor Table Add failed")
         finally:
             clear_dut_arp_cache(duthost)
