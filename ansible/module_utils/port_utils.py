4a3a80134cde066a6d334b4ca7b8092bdc17952f
TANMAYA UDUPA <143127557+tudupa@users.noreply.github.com>
2024-08-07 21:52:44 -0400

    Added Nokia-7215-A1 hwsku to port_utils (#14020)
    What is the motivation for this PR?
    This PR supports adding new hwsku (Nokia-7215-A1) to port utils.
    
    How did you do it?
    Added Nokia-7215-A1 to port utils.
    
    How did you verify/test it?
    Ran the add-topo, gen-mg to see if everything is correctly working.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -285,13 +285,13 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
         elif hwsku == "Seastone-DX010":
             for i in range(1, 33):
                 port_alias_to_name_map["Eth%d" % i] = "Ethernet%d" % ((i - 1) * 4)
-        elif hwsku in ["Celestica-E1031-T48S4", "Nokia-7215", "Nokia-M0-7215"]:
+        elif hwsku in ["Celestica-E1031-T48S4", "Nokia-7215", "Nokia-M0-7215", "Nokia-7215-A1"]:
             for i in range(1, 53):
                 port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1))
         elif hwsku == "et6448m":
             for i in range(0, 52):
                 port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
-        elif hwsku in ["rd98DX35xx_cn9131", "rd98DX35xx", "Nokia-7215-A1"]:
+        elif hwsku in ["rd98DX35xx_cn9131", "rd98DX35xx"]:
             for i in range(0, 32):
                 port_alias_to_name_map["oneGigE%d" % i] = "Ethernet%d" % i
             for i in range(32, 48):
