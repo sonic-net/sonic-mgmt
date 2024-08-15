7a05a1982888c367f9ecc8fd9fc48a512eecd242
Zain Budhwani <zainbudhwani@microsoft.com>
2024-08-15 22:08:36 +0000

    Fix test_events flakiness

--- tests/telemetry/test_events.py
+++ tests/telemetry/test_events.py
@@ -54,46 +54,7 @@ def test_events(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_strea
             logger.info("Completed test file: {}".format(os.path.join(EVENTS_TESTS_PATH, file)))
 
 
-@pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
-@pytest.mark.disable_loganalyzer
-def test_events_cache(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_streaming_telemetry, gnxi_path):
-    """Create expected o/p file of events with N events. Call event-publisher tool to publish M events (M<N). Publish
-    remainder of events. Verify o/p file that N events were received"""
-    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
-    logger.info("Start events cache testing")
-
-    skip_201911_and_older(duthost)
-    reset_event_counters(duthost)
-    restart_eventd(duthost)
-    current_published_counter = read_event_counters(duthost)[1]
-
-    M = 20
-    N = 30
-
-    received_op_file = os.path.join(DATA_DIR, "received_op_file")
-
-    create_ip_file(duthost, DATA_DIR, "first_part_ip_file", 1, M)
-    create_ip_file(duthost, DATA_DIR, "second_part_ip_file", M + 1, N)
-
-    # Publish first M events
-    event_publish_tool(duthost, "first_part_ip_file")
-
-    event_thread = InterruptableThread(target=listen_for_events, args=(duthost, gnxi_path, ptfhost,
-                                       "test-event-source:test", received_op_file, 30, N, N-1))
-    event_thread.start()
-
-    # Publish second batch of events
-    event_publish_tool(duthost, "second_part_ip_file")
-
-    event_thread.join(30)
-
-    # Verify received output
-    verify_received_output(received_op_file, N)
-
-    verify_counter_increase(duthost, current_published_counter, N, PUBLISHED)
-
-
-@pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
+pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
 @pytest.mark.disable_loganalyzer
 def test_events_cache_overflow(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_streaming_telemetry,
                                gnxi_path):
