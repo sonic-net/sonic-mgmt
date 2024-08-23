fa5b7383796cfd50e6481d99b062efeeaeacbfef
Nana@Nvidia <78413612+nhe-NV@users.noreply.github.com>
2024-08-24 04:57:49 +0800

    Move the skip for expired ttl test case logic to the conditional yaml file (#13863)
    If the skip logic is in the test body, the fixture still need to be done, in this test case, even the test need to be skipped,still need to finish the teardown of configure_copp_drop_for_ttl_error, and in the fixture, it has config reload which will take 2 mins to finish, since the test case need to be skipped, no need to waste 2 mins on it.
    
    At the same time, change the config reload to use safe reload
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/drop_packets/drop_packets.py
+++ tests/drop_packets/drop_packets.py
@@ -146,7 +146,7 @@ EOF
     yield
 
     duthost.command("rm {} {}".format(copp_trap_group_json, copp_trap_rule_json))
-    config_reload(duthost)
+    config_reload(duthost, safe_reload=True)
 
 
 def is_mellanox_devices(hwsku):
@@ -943,9 +943,6 @@ def test_ip_pkt_with_expired_ttl(duthost, do_test, ptfadapter, setup, tx_dut_por
     """
     @summary: Create an IP packet with TTL=0.
     """
-    if "x86_64-mlnx_msn" in duthost.facts["platform"] or "x86_64-nvidia_sn" in duthost.facts["platform"]:
-        pytest.skip("Not supported on Mellanox devices")
-
     log_pkt_params(ports_info["dut_iface"], ports_info["dst_mac"], ports_info["src_mac"],
                    pkt_fields["ipv4_dst"], pkt_fields["ipv4_src"])
 
