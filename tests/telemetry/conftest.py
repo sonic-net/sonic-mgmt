7eb0551c42dacdc23ed87c05c7a50c3f5f301917
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2024-08-27 11:28:21 -0700

    Add dhcp-relay structured events tests (#13504)
    #### What is the motivation for this PR?
    
    Add support for dhcp-relay-discard event, which we use ptfadapter to and ptf.testutils to send a dhcp discover packet to a port that has no ipv4 address assigned.
    
    Add support for dhcp-relay-disparity event which uses same repro for dhcp-relay-disparity but at greater intervals and packets sent due to dhcpmon configuration
    
    #### How did you do it?
    
    Added new testcase that triggered sending dhcp discover packet which will trigger event.
    
    #### How did you verify/test it?
    
    Pipeline/Manual test
    
    run_events_test.run_test                 L0027 INFO   | events received:
    ```json
        {
            "sonic-events-dhcp-relay:dhcp-relay-discard": {
                "ifname": "Vlan1000",
                "timestamp": "2024-07-09T01:27:30.395216Z"
            }
        }
    ```
    run_events_test.run_test                 L0027 INFO   | events received:
    ```json
        {
            "sonic-events-dhcp-relay:dhcp-relay-disparity": {
                "duration": "198",
                "timestamp": "2024-07-09T01:30:43.711312Z",
                "vlan": "Agg-Vlan1000"
            }
        }
    ```
    run_events_test.run_test                 L0027 INFO   | events received:
    ```json
        {
            "sonic-events-dhcp-relay:dhcp-relay-bind-failure": {
                "timestamp": "2024-07-09T01:32:27.667714Z",
                "type": "local",
                "vlan": "Vlan1000"
            }
        }
    ```
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/telemetry/conftest.py
+++ tests/telemetry/conftest.py
@@ -164,7 +164,8 @@ def do_init(duthost):
 
 
 @pytest.fixture(scope="module")
-def test_eventd_healthy(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_streaming_telemetry, gnxi_path):
+def test_eventd_healthy(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, ptfadapter,
+                        setup_streaming_telemetry, gnxi_path):
     """
     @summary: Test eventd heartbeat before testing all testcases
     """
@@ -183,6 +184,6 @@ def test_eventd_healthy(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, set
 
     py_assert(wait_until(100, 10, 0, duthost.is_service_fully_started, "eventd"), "eventd not started.")
 
-    module.test_event(duthost, gnxi_path, ptfhost, DATA_DIR, None)
+    module.test_event(duthost, gnxi_path, ptfhost, ptfadapter, DATA_DIR, None)
 
     logger.info("Completed test file: {}".format("eventd_events test completed."))
--- tests/telemetry/test_events.py
+++ tests/telemetry/test_events.py
@@ -37,7 +37,7 @@ def validate_yang(duthost, op_file="", yang_file=""):
 
 @pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
 @pytest.mark.disable_loganalyzer
-def test_events(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_streaming_telemetry, gnxi_path,
+def test_events(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, ptfadapter, setup_streaming_telemetry, gnxi_path,
                 test_eventd_healthy):
     """ Run series of events inside duthost and validate that output is correct
     and conforms to YANG schema"""
@@ -50,7 +50,7 @@ def test_events(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost, setup_strea
     for file in os.listdir(EVENTS_TESTS_PATH):
         if file.endswith("_events.py") and not file.endswith("eventd_events.py"):
             module = __import__(file[:len(file)-3])
-            module.test_event(duthost, gnxi_path, ptfhost, DATA_DIR, validate_yang)
+            module.test_event(duthost, gnxi_path, ptfhost, ptfadapter, DATA_DIR, validate_yang)
             logger.info("Completed test file: {}".format(os.path.join(EVENTS_TESTS_PATH, file)))
 
 
