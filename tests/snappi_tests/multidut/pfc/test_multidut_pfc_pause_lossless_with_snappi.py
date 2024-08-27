0e59517adc8774e4557b17ce58590dc445580c7c
Dashuai Zhang <dashuaizhang@microsoft.com>
2024-08-27 13:02:02 +1000

    make snappi multidut work for both T1 and T2 testbed update intf_config_changed flag add tgen topo errors, comments and function rename get_snappi_ports_multi_dut -> get_snappi_ports filter multidut fanout to have snappi only

--- tests/snappi_tests/multidut/pfc/test_multidut_pfc_pause_lossless_with_snappi.py
+++ tests/snappi_tests/multidut/pfc/test_multidut_pfc_pause_lossless_with_snappi.py
@@ -1,9 +1,11 @@
 import pytest
 from tests.common.helpers.assertions import pytest_require, pytest_assert                   # noqa: F401
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts_multidut     # noqa: F401
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts, \
+    fanout_graph_facts_multidut     # noqa: F401
 from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port, \
-    snappi_api, snappi_dut_base_config, get_snappi_ports_for_rdma, cleanup_config, \
-    get_snappi_ports                                        # noqa: F401
+    snappi_api, snappi_dut_base_config, get_snappi_ports_for_rdma, cleanup_config, get_snappi_ports_multi_dut, \
+    snappi_testbed_config, get_snappi_ports_single_dut, \
+    get_snappi_ports, is_snappi_multidut                                        # noqa: F401
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, all_prio_list, lossless_prio_list,\
     lossy_prio_list                         # noqa F401
 from tests.snappi_tests.variables import MULTIDUT_PORT_INFO, MULTIDUT_TESTBED
@@ -15,11 +17,12 @@ from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
 from tests.snappi_tests.files.helper import skip_warm_reboot
 logger = logging.getLogger(__name__)
 
-pytestmark = [pytest.mark.topology('multidut-tgen')]
+pytestmark = [pytest.mark.topology('multidut-tgen', 'tgen')]
 
 
 @pytest.mark.parametrize("multidut_port_info", MULTIDUT_PORT_INFO[MULTIDUT_TESTBED])
 def test_pfc_pause_single_lossless_prio(snappi_api,                     # noqa: F811
+                                        snappi_testbed_config,
                                         conn_graph_facts,               # noqa: F811
                                         fanout_graph_facts_multidut,             # noqa: F811
                                         duthosts,
@@ -49,30 +52,35 @@ def test_pfc_pause_single_lossless_prio(snappi_api,                     # noqa: 
     Returns:
         N/A
     """
-    for testbed_subtype, rdma_ports in multidut_port_info.items():
-        tx_port_count = 1
-        rx_port_count = 1
-        snappi_port_list = get_snappi_ports
-        pytest_assert(MULTIDUT_TESTBED == tbinfo['conf-name'],
-                      "The testbed name from testbed file doesn't match with MULTIDUT_TESTBED in variables.py ")
-        pytest_assert(len(snappi_port_list) >= tx_port_count + rx_port_count,
-                      "Need Minimum of 2 ports defined in ansible/files/*links.csv file")
+    snappi_port_list = get_snappi_ports
+    if is_snappi_multidut(duthosts):
+        for testbed_subtype, rdma_ports in multidut_port_info.items():
+            tx_port_count = 1
+            rx_port_count = 1
+            snappi_port_list = get_snappi_ports
+            pytest_assert(MULTIDUT_TESTBED == tbinfo['conf-name'],
+                          "The testbed name from testbed file doesn't match with MULTIDUT_TESTBED in variables.py ")
+            pytest_assert(len(snappi_port_list) >= tx_port_count + rx_port_count,
+                          "Need Minimum of 2 ports defined in ansible/files/*links.csv file")
 
-        pytest_assert(len(rdma_ports['tx_ports']) >= tx_port_count,
-                      'MULTIDUT_PORT_INFO doesn\'t have the required Tx ports defined for \
-                      testbed {}, subtype {} in variables.py'.
-                      format(MULTIDUT_TESTBED, testbed_subtype))
+            pytest_assert(len(rdma_ports['tx_ports']) >= tx_port_count,
+                          'MULTIDUT_PORT_INFO doesn\'t have the required Tx ports defined for \
+                          testbed {}, subtype {} in variables.py'.
+                          format(MULTIDUT_TESTBED, testbed_subtype))
 
-        pytest_assert(len(rdma_ports['rx_ports']) >= rx_port_count,
-                      'MULTIDUT_PORT_INFO doesn\'t have the required Rx ports defined for \
-                      testbed {}, subtype {} in variables.py'.
-                      format(MULTIDUT_TESTBED, testbed_subtype))
-        logger.info('Running test for testbed subtype: {}'.format(testbed_subtype))
-        snappi_ports = get_snappi_ports_for_rdma(snappi_port_list, rdma_ports,
-                                                 tx_port_count, rx_port_count, MULTIDUT_TESTBED)
-        testbed_config, port_config_list, snappi_ports = snappi_dut_base_config(duthosts,
-                                                                                snappi_ports,
-                                                                                snappi_api)
+            pytest_assert(len(rdma_ports['rx_ports']) >= rx_port_count,
+                          'MULTIDUT_PORT_INFO doesn\'t have the required Rx ports defined for \
+                          testbed {}, subtype {} in variables.py'.
+                          format(MULTIDUT_TESTBED, testbed_subtype))
+            logger.info('Running test for testbed subtype: {}'.format(testbed_subtype))
+            snappi_ports = get_snappi_ports_for_rdma(snappi_port_list, rdma_ports,
+                                                     tx_port_count, rx_port_count, MULTIDUT_TESTBED)
+            testbed_config, port_config_list, snappi_ports = snappi_dut_base_config(duthosts,
+                                                                                    snappi_ports,
+                                                                                    snappi_api)
+    else:
+        snappi_ports = get_snappi_ports
+        testbed_config, port_config_list = snappi_testbed_config
 
     _, lossless_prio = enum_dut_lossless_prio.split('|')
     lossless_prio = int(lossless_prio)
