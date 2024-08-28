4bf4bd341067e0b087a98d6a0280e68cc7773463
Illia Kotvitskyi <ikotvitskyi@nvidia.com>
2024-08-28 17:32:47 +0300

    update datetime restoration in clock tests use ntpdate instead of a temporary adding of NTP server check NTP server reachability on the setup stage
    the motivation is to avoid waiting for the next poll and prevent the situation when the polling interval changes after adding the NTP server
    
    Change-Id: I78ab7bab5609ae0ecad31887225abe152f7a39e8

--- tests/clock/test_clock.py
+++ tests/clock/test_clock.py
@@ -31,6 +31,9 @@ class ClockConsts:
     CMD_SHOW_CLOCK_TIMEZONES = "show clock timezones"
     CMD_CONFIG_CLOCK_TIMEZONE = "config clock timezone"
     CMD_CONFIG_CLOCK_DATE = "config clock date"
+    CMD_NTP_STOP = 'service ntp stop'
+    CMD_NTP_START = 'service ntp start'
+    CMD_NTPDATE = 'ntpdate'
 
     # expected outputs
     OUTPUT_CMD_SUCCESS = ''
@@ -60,7 +63,7 @@ class ClockConsts:
 
 class ClockUtils:
     @staticmethod
-    def run_cmd(duthosts, cmd, param=''):
+    def run_cmd(duthosts, cmd, param='', raise_err=False):
         """
         @summary:
             Run a given command and return its output.
@@ -81,6 +84,8 @@ class ClockUtils:
                 err = cmd_err.results["stderr"]
                 cmd_output = output if output else err
                 logging.info(f'Command Error!\nError message: "{cmd_output}"')
+                if raise_err:
+                    raise Exception(cmd_output)
 
             cmd_output = str(cmd_output)
             logging.info(f'Output: {cmd_output}')
