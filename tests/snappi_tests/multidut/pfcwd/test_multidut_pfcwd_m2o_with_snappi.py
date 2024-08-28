2bb5632bd8b959481cea7a84a87ae83ff1b172c5
selldinesh <dinesh.sellappan@keysight.com>
2024-08-28 21:35:17 +0000

    [Fix] Accomodating the infra change for multidut ECN and PFCWD cases

--- tests/snappi_tests/multidut/pfcwd/test_multidut_pfcwd_m2o_with_snappi.py
+++ tests/snappi_tests/multidut/pfcwd/test_multidut_pfcwd_m2o_with_snappi.py
@@ -1,30 +1,29 @@
 import pytest
 import random
-from tests.common.helpers.assertions import pytest_require
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts     # noqa: F401
+import logging
+from tests.common.helpers.assertions import pytest_require, pytest_assert                               # noqa: F401
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts_multidut     # noqa: F401
 from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port, \
-    snappi_api, snappi_dut_base_config, get_tgen_peer_ports, get_multidut_snappi_ports, \
-    get_multidut_tgen_peer_port_set, cleanup_config                                         # noqa: F401
+    snappi_api, snappi_dut_base_config, get_snappi_ports, get_snappi_ports_for_rdma, cleanup_config      # noqa: F401
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, all_prio_list,\
     lossless_prio_list, lossy_prio_list     # noqa F401
-from tests.snappi_tests.variables import config_set, line_card_choice
+from tests.snappi_tests.variables import MULTIDUT_PORT_INFO, MULTIDUT_TESTBED
 from tests.snappi_tests.multidut.pfcwd.files.pfcwd_multidut_multi_node_helper import run_pfcwd_multi_node_test
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
-
+logger = logging.getLogger(__name__)
 pytestmark = [pytest.mark.topology('multidut-tgen')]
 
 
 @pytest.mark.parametrize("trigger_pfcwd", [True])
-@pytest.mark.parametrize('line_card_choice', [line_card_choice])
-@pytest.mark.parametrize('linecard_configuration_set', [config_set])
+@pytest.mark.parametrize("multidut_port_info", MULTIDUT_PORT_INFO[MULTIDUT_TESTBED])
 def test_pfcwd_many_to_one(snappi_api,              # noqa: F811
                            conn_graph_facts,        # noqa: F811
-                           fanout_graph_facts,      # noqa: F811
+                           fanout_graph_facts_multidut,      # noqa: F811
                            duthosts,
-                           rand_one_dut_lossless_prio,
-                           line_card_choice,
-                           linecard_configuration_set,
-                           get_multidut_snappi_ports,   # noqa: F811
+                           lossless_prio_list,    # noqa: F811
+                           get_snappi_ports,    # noqa: F811
+                           tbinfo,      # noqa: F811
+                           multidut_port_info,
                            trigger_pfcwd,
                            prio_dscp_map,               # noqa: F811
                            lossy_prio_list,):           # noqa: F811
@@ -35,55 +34,53 @@ def test_pfcwd_many_to_one(snappi_api,              # noqa: F811
     Args:
         snappi_api (pytest fixture): SNAPPI session
         conn_graph_facts (pytest fixture): connection graph
-        fanout_graph_facts (pytest fixture): fanout graph
+        fanout_graph_facts_multidut (pytest fixture): fanout graph
         duthosts (pytest fixture): list of DUTs
         rand_one_dut_lossless_prio (str): lossless priority to test, e.g., 's6100-1|3'
         lossy_prio_list (pytest fixture): list of lossy priorities
         prio_dscp_map (pytest fixture): priority vs. DSCP map (key = priority)
         trigger_pfcwd (bool): if PFC watchdog is expected to be triggered
-        line_card_choice: Line card choice to be mentioned in the variable.py file
-        linecard_configuration_set : Line card classification, (min 1 or max 2  hostnames and asics to be given)
+        tbinfo (pytest fixture): fixture provides information about testbed
+        get_snappi_ports (pytest fixture): gets snappi ports and connected DUT port info and returns as a list
 
     Returns:
         N/A
     """
-    if line_card_choice not in linecard_configuration_set.keys():
-        pytest_require(False, "Invalid line_card_choice value passed in parameter")
+    for testbed_subtype, rdma_ports in multidut_port_info.items():
+        tx_port_count = 2
+        rx_port_count = 1
+        snappi_port_list = get_snappi_ports
+        pytest_assert(MULTIDUT_TESTBED == tbinfo['conf-name'],
+                      "The testbed name from testbed file doesn't match with MULTIDUT_TESTBED in variables.py ")
+        pytest_assert(len(snappi_port_list) >= tx_port_count + rx_port_count,
+                      "Need Minimum of 3 ports defined in ansible/files/*links.csv file")
 
-    if (len(linecard_configuration_set[line_card_choice]['hostname']) == 2):
-        dut_list = random.sample(duthosts.frontend_nodes, 2)
-        duthost1, duthost2 = dut_list
-    elif (len(linecard_configuration_set[line_card_choice]['hostname']) == 1):
-        dut_list = [dut for dut in duthosts.frontend_nodes if
-                    linecard_configuration_set[line_card_choice]['hostname'] == [dut.hostname]]
-        duthost1, duthost2 = dut_list[0], dut_list[0]
-    else:
-        pytest_require(False, "Hostname can't be an empty list")
+        pytest_assert(len(rdma_ports['tx_ports']) >= tx_port_count,
+                      'MULTIDUT_PORT_INFO doesn\'t have the required Tx ports defined for \
+                      testbed {}, subtype {} in variables.py'.
+                      format(MULTIDUT_TESTBED, testbed_subtype))
 
-    snappi_port_list = get_multidut_snappi_ports(line_card_choice=line_card_choice,
-                                                 line_card_info=linecard_configuration_set[line_card_choice])
-    if len(snappi_port_list) < 3:
-        pytest_require(False, "Need Minimum of 3 ports for the test")
+        pytest_assert(len(rdma_ports['rx_ports']) >= rx_port_count,
+                      'MULTIDUT_PORT_INFO doesn\'t have the required Rx ports defined for \
+                      testbed {}, subtype {} in variables.py'.
+                      format(MULTIDUT_TESTBED, testbed_subtype))
+        logger.info('Running test for testbed subtype: {}'.format(testbed_subtype))
+        snappi_ports = get_snappi_ports_for_rdma(snappi_port_list, rdma_ports,
+                                                 tx_port_count, rx_port_count, MULTIDUT_TESTBED)
+        testbed_config, port_config_list, snappi_ports = snappi_dut_base_config(duthosts,
+                                                                                snappi_ports,
+                                                                                snappi_api)
 
-    snappi_ports = get_multidut_tgen_peer_port_set(line_card_choice, snappi_port_list, config_set, 3)
-
-    testbed_config, port_config_list, snappi_ports = snappi_dut_base_config(dut_list,
-                                                                            snappi_ports,
-                                                                            snappi_api)
-
-    _, lossless_prio = rand_one_dut_lossless_prio.split('|')
-    lossless_prio = int(lossless_prio)
+    lossless_prio = random.sample(lossless_prio_list, 1)
 
     snappi_extra_params = SnappiTestParams()
-    snappi_extra_params.multi_dut_params.duthost1 = duthost1
-    snappi_extra_params.multi_dut_params.duthost2 = duthost2
     snappi_extra_params.multi_dut_params.multi_dut_ports = snappi_ports
 
     run_pfcwd_multi_node_test(api=snappi_api,
                               testbed_config=testbed_config,
                               port_config_list=port_config_list,
                               conn_data=conn_graph_facts,
-                              fanout_data=fanout_graph_facts,
+                              fanout_data=fanout_graph_facts_multidut,
                               dut_port=snappi_ports[0]['peer_port'],
                               pause_prio_list=[lossless_prio],
                               test_prio_list=[lossless_prio],
@@ -93,4 +90,4 @@ def test_pfcwd_many_to_one(snappi_api,              # noqa: F811
                               pattern="many to one",
                               snappi_extra_params=snappi_extra_params)
 
-    cleanup_config(dut_list, snappi_ports)
+    cleanup_config(duthosts, snappi_ports)
