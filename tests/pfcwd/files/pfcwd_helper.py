246b80293ced803f0f15fe799a4907b33f74e653
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2024-09-03 08:41:30 -0700

    Stabilize test_pfcwd_timer_accuracy (#14335)
    * Stabilize test_pfcwd_timer_accuracy
    
    * Move sleep into send_background_traffic
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/pfcwd/files/pfcwd_helper.py
+++ tests/pfcwd/files/pfcwd_helper.py
@@ -4,6 +4,7 @@ import sys
 import random
 import pytest
 import contextlib
+import time
 
 from tests.ptf_runner import ptf_runner
 from tests.common import constants
@@ -491,6 +492,8 @@ def send_background_traffic(duthost, ptfhost, storm_hndle, selected_test_ports, 
                                                                        selected_test_ports,
                                                                        test_ports_info)
         background_traffic_log = _send_background_traffic(ptfhost, background_traffic_params)
+        # Ensure the background traffic is running before moving on
+        time.sleep(1)
     yield
     if is_mellanox_device(duthost):
         _stop_background_traffic(ptfhost, background_traffic_log)
