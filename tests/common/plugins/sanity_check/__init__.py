8a2a0fa93bd25185d1b8b84811d6c3952ed93e9d
chunangli <chunang_li@163.com>
2024-09-03 10:43:24 +0800

    debug fix
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- tests/common/plugins/sanity_check/__init__.py
+++ tests/common/plugins/sanity_check/__init__.py
@@ -119,7 +119,7 @@ def do_checks(request, check_items, *args, **kwargs):
     return check_results
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def sanity_check_full(localhost, duthosts, request, fanouthosts, nbrhosts, tbinfo):
     logger.info("Prepare sanity check")
 
