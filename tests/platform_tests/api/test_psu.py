b817d8ee4a26f849721356f98647a3d26e19829a
smagarwal <smagarwal@arista.com>
2024-09-09 15:52:17 +0000

    Retry test_power calculated power above 10%
    If the test fails on `abs(power - (voltage*current)) < power*0.1` then retry the test. Test is repeated for a maximum of three times until it passes, else it is failed.
    This change is introduced to account for the stringent tolerance level of 10% and the errors that might be caused due to time differences reading each of the parameters.

--- tests/platform_tests/api/test_psu.py
+++ tests/platform_tests/api/test_psu.py
@@ -32,6 +32,7 @@ STATUS_LED_COLOR_GREEN = "green"
 STATUS_LED_COLOR_AMBER = "amber"
 STATUS_LED_COLOR_RED = "red"
 STATUS_LED_COLOR_OFF = "off"
+MAX_ATTEMPTS = 3
 
 
 class TestPsuApi(PlatformApiTestBase):
@@ -206,66 +207,74 @@ class TestPsuApi(PlatformApiTestBase):
         skip_release_for_platform(duthost, ["202012", "201911", "201811"], ["arista"])
 
         for psu_id in range(self.num_psus):
-            name = psu.get_name(platform_api_conn, psu_id)
-            if name in self.psu_skip_list:
-                logger.info("skipping check for {}".format(name))
-            else:
-                voltage = None
-                voltage_supported = self.get_psu_facts(duthost, psu_id, True, "voltage")
-                if voltage_supported:
-                    voltage = psu.get_voltage(platform_api_conn, psu_id)
-                    if self.expect(voltage is not None, "Failed to retrieve voltage of PSU {}".format(psu_id)):
-                        self.expect(isinstance(voltage, float), "PSU {} voltage appears incorrect".format(psu_id))
-                current = None
-                current_supported = self.get_psu_facts(duthost, psu_id, True, "current")
-                if current_supported:
-                    current = psu.get_current(platform_api_conn, psu_id)
-                    if self.expect(current is not None, "Failed to retrieve current of PSU {}".format(psu_id)):
-                        self.expect(isinstance(current, float), "PSU {} current appears incorrect".format(psu_id))
-                power = None
-                power_supported = self.get_psu_facts(duthost, psu_id, True, "power")
-                if power_supported:
-                    power = psu.get_power(platform_api_conn, psu_id)
-                    if self.expect(power is not None, "Failed to retrieve power of PSU {}".format(psu_id)):
-                        self.expect(isinstance(power, float), "PSU {} power appears incorrect".format(psu_id))
-                max_supp_power = None
-                max_power_supported = self.get_psu_facts(duthost, psu_id, True, "max_power")
-                if max_power_supported:
-                    max_supp_power = psu.get_maximum_supplied_power(platform_api_conn, psu_id)
-                    if self.expect(max_supp_power is not None,
-                                   "Failed to retrieve maximum supplied power power of PSU {}".format(psu_id)):
-                        self.expect(isinstance(max_supp_power, float),
-                                    "PSU {} maximum supplied power appears incorrect".format(psu_id))
+            for i in range(0,MAX_ATTEMPTS):
+                name = psu.get_name(platform_api_conn, psu_id)
+                if name in self.psu_skip_list:
+                    logger.info("skipping check for {}".format(name))
+                else:
+                    voltage = None
+                    voltage_supported = self.get_psu_facts(duthost, psu_id, True, "voltage")
+                    if voltage_supported:
+                        voltage = psu.get_voltage(platform_api_conn, psu_id)
+                        if self.expect(voltage is not None, "Failed to retrieve voltage of PSU {}".format(psu_id)):
+                            self.expect(isinstance(voltage, float), "PSU {} voltage appears incorrect".format(psu_id))
+                    current = None
+                    current_supported = self.get_psu_facts(duthost, psu_id, True, "current")
+                    if current_supported:
+                        current = psu.get_current(platform_api_conn, psu_id)
+                        if self.expect(current is not None, "Failed to retrieve current of PSU {}".format(psu_id)):
+                            self.expect(isinstance(current, float), "PSU {} current appears incorrect".format(psu_id))
+                    power = None
+                    power_supported = self.get_psu_facts(duthost, psu_id, True, "power")
+                    if power_supported:
+                        power = psu.get_power(platform_api_conn, psu_id)
+                        if self.expect(power is not None, "Failed to retrieve power of PSU {}".format(psu_id)):
+                            self.expect(isinstance(power, float), "PSU {} power appears incorrect".format(psu_id))
+                    max_supp_power = None
+                    max_power_supported = self.get_psu_facts(duthost, psu_id, True, "max_power")
+                    if max_power_supported:
+                        max_supp_power = psu.get_maximum_supplied_power(platform_api_conn, psu_id)
+                        if self.expect(max_supp_power is not None,
+                                    "Failed to retrieve maximum supplied power of PSU {}".format(psu_id)):
+                            self.expect(isinstance(max_supp_power, float),
+                                        "PSU {} maximum supplied power appears incorrect".format(psu_id))
 
-                if current is not None and voltage is not None and power is not None:
-                    self.expect(abs(power - (voltage*current)) < power*0.1, "PSU {} reading does not make sense \
-                        (power:{}, voltage:{}, current:{})".format(psu_id, power, voltage, current))
+                    if current is not None and voltage is not None and power is not None:
+                        is_within_tolerance = abs(power - (voltage*current)) < power*0.1
+                        if not is_within_tolerance and i < MAX_ATTEMPTS - 1:
+                            logger.info("Retrying test for {} as readings not within tolerance level".format(name))
+                            continue
+                        self.expect(is_within_tolerance, "PSU {} reading does not make sense \
+                            (power:{}, voltage:{}, current:{})".format(psu_id, power, voltage, current))
 
-                powergood_status = psu.get_powergood_status(platform_api_conn, psu_id)
-                if self.expect(powergood_status is not None,
-                               "Failed to retrieve operational status of PSU {}".format(psu_id)):
-                    self.expect(powergood_status is True, "PSU {} is not operational".format(psu_id))
+                    powergood_status = psu.get_powergood_status(platform_api_conn, psu_id)
+                    if self.expect(powergood_status is not None,
+                                "Failed to retrieve operational status of PSU {}".format(psu_id)):
+                        self.expect(powergood_status is True, "PSU {} is not operational".format(psu_id))
 
-                high_threshold = None
-                voltage_high_threshold_supported = self.get_psu_facts(duthost, psu_id, True, "voltage_high_threshold")
-                if voltage_high_threshold_supported:
-                    high_threshold = psu.get_voltage_high_threshold(platform_api_conn, psu_id)
-                    if self.expect(high_threshold is not None,
-                                   "Failed to retrieve the high voltage threshold of PSU {}".format(psu_id)):
-                        self.expect(isinstance(high_threshold, float),
-                                    "PSU {} voltage high threshold appears incorrect".format(psu_id))
-                low_threshold = None
-                voltage_low_threshold_supported = self.get_psu_facts(duthost, psu_id, True, "voltage_low_threshold")
-                if voltage_low_threshold_supported:
-                    low_threshold = psu.get_voltage_low_threshold(platform_api_conn, psu_id)
-                    if self.expect(low_threshold is not None,
-                                   "Failed to retrieve the low voltage threshold of PSU {}".format(psu_id)):
-                        self.expect(isinstance(low_threshold, float),
-                                    "PSU {} voltage low threshold appears incorrect".format(psu_id))
-                if high_threshold is not None and low_threshold is not None:
-                    self.expect(voltage < high_threshold and voltage > low_threshold,
-                                "Voltage {} of PSU {} is not in between {} and {}"
-                                .format(voltage, psu_id, low_threshold, high_threshold))
+                    high_threshold = None
+                    voltage_high_threshold_supported = self.get_psu_facts(duthost, psu_id, True, "voltage_high_threshold")
+                    if voltage_high_threshold_supported:
+                        high_threshold = psu.get_voltage_high_threshold(platform_api_conn, psu_id)
+                        if self.expect(high_threshold is not None,
+                                    "Failed to retrieve the high voltage threshold of PSU {}".format(psu_id)):
+                            self.expect(isinstance(high_threshold, float),
+                                        "PSU {} voltage high threshold appears incorrect".format(psu_id))
+                    low_threshold = None
+                    voltage_low_threshold_supported = self.get_psu_facts(duthost, psu_id, True, "voltage_low_threshold")
+                    if voltage_low_threshold_supported:
+                        low_threshold = psu.get_voltage_low_threshold(platform_api_conn, psu_id)
+                        if self.expect(low_threshold is not None,
+                                    "Failed to retrieve the low voltage threshold of PSU {}".format(psu_id)):
+                            self.expect(isinstance(low_threshold, float),
+                                        "PSU {} voltage low threshold appears incorrect".format(psu_id))
+                    if high_threshold is not None and low_threshold is not None:
+                        self.expect(voltage < high_threshold and voltage > low_threshold,
+                                    "Voltage {} of PSU {} is not in between {} and {}"
+                                    .format(voltage, psu_id, low_threshold, high_threshold))
+                
+                break
+            
         self.assert_expectations()
 
     def test_temperature(self, duthosts, enum_rand_one_per_hwsku_hostname, localhost, platform_api_conn):
