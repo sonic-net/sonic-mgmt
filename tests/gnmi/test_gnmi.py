47447f1597fa4829c9521594a7c6de851d33c426
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2024-09-11 02:14:40 +0800

    Add gnmi client cname authorize test case (#13133)
    Add gnmi client cname authorize test case 
    
    #### Why I did it
    GNMI add client cert cname validation feature, to protect this feature, add this test case.
    
    ### How I did it
    Create cert with cname and validate GNMI service can accept/reject cert with cert cname config.
    
    #### How to verify it
    Pass all test case.
    
    ### Description for the changelog
    Add gnmi client cname authorize test case.

--- tests/gnmi/test_gnmi.py
+++ tests/gnmi/test_gnmi.py
@@ -1,7 +1,7 @@
 import pytest
 import logging
 
-from .helper import gnmi_capabilities
+from .helper import gnmi_capabilities, gnmi_set, add_gnmi_client_common_name, del_gnmi_client_common_name
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +20,45 @@ def test_gnmi_capabilities(duthosts, rand_one_dut_hostname, localhost):
     assert ret == 0, msg
     assert "sonic-db" in msg, msg
     assert "JSON_IETF" in msg, msg
+
+
+@pytest.fixture(scope="function")
+def setup_invalid_client_cert_cname(duthosts, rand_one_dut_hostname):
+    duthost = duthosts[rand_one_dut_hostname]
+    del_gnmi_client_common_name(duthost, "test.client.gnmi.sonic")
+    add_gnmi_client_common_name(duthost, "invalid.cname")
+
+    keys = duthost.shell('sudo sonic-db-cli CONFIG_DB keys GNMI*')["stdout_lines"]
+    logger.debug("GNMI client cert keys: {}".format(keys))
+
+    yield
+
+    del_gnmi_client_common_name(duthost, "invalid.cname")
+    add_gnmi_client_common_name(duthost, "test.client.gnmi.sonic")
+
+
+def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
+                                                  rand_one_dut_hostname,
+                                                  ptfhost,
+                                                  setup_invalid_client_cert_cname):
+    '''
+    Verify GNMI native write, incremental config for configDB
+    GNMI set request with invalid path
+    '''
+    duthost = duthosts[rand_one_dut_hostname]
+
+    file_name = "vnet.txt"
+    text = "{\"Vnet1\": {\"vni\": \"1000\", \"guid\": \"559c6ce8-26ab-4193-b946-ccc6e8f930b2\"}}"
+    with open(file_name, 'w') as file:
+        file.write(text)
+    ptfhost.copy(src=file_name, dest='/root')
+    # Add DASH_VNET_TABLE
+    update_list = ["/sonic-db:APPL_DB/localhost/DASH_VNET_TABLE:@/root/%s" % (file_name)]
+    msg = ""
+    try:
+        gnmi_set(duthost, ptfhost, [], update_list, [])
+    except Exception as e:
+        logger.info("Failed to set: " + str(e))
+        msg = str(e)
+
+    assert "Unauthenticated" in msg
