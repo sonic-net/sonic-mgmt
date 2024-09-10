e07e31de5c71f35e73cce37b87e89649973389e7
Nishanth Sampath Kumar <nissampa@cisco.com>
2024-09-10 15:39:50 -0700

    flake8 error resolved for platform_tests/test_reload_dpu.py

--- tests/smartswitch/platform_tests/test_reload_dpu.py
+++ tests/smartswitch/platform_tests/test_reload_dpu.py
@@ -1,27 +1,28 @@
 """
-Tests for the `reboot and reload ...` commands in DPU 
+Tests for the `reboot and reload ...` commands in DPU
 """
 
 import logging
 import pytest
 import time
 from tests.common.helpers.assertions import pytest_assert
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts     
-from tests.common.platform.interface_utils import check_interface_status_of_up_ports
+from tests.common.platform.interface_utils \
+     import check_interface_status_of_up_ports
 from tests.common.utilities import wait_until
 from tests.common.platform.processes_utils import wait_critical_processes
-from tests.common.reboot import reboot,wait_for_startup,REBOOT_TYPE_COLD
-from tests.common.config_reload import config_force_option_supported, config_system_checks_passed
-from tests.smartswitch.common.platform.device_utils_dpu import *
+from tests.common.reboot import reboot, wait_for_startup, REBOOT_TYPE_COLD
+from tests.common.config_reload import config_force_option_supported, config_system_checks_passed  # noqa: F401, E501
+from tests.smartswitch.common.platform.device_utils_dpu import *  # noqa: F403
 from tests.common.helpers.platform_api import chassis, module
-from tests.platform_tests.api.conftest import *
+from tests.platform_tests.api.conftest import *  # noqa: F403
 
 pytestmark = [
     pytest.mark.topology('t1')
 ]
 
 
-def test_dpu_ping_after_reboot(duthosts, enum_rand_one_per_hwsku_hostname, localhost, platform_api_conn):
+def test_dpu_ping_after_reboot(duthosts, enum_rand_one_per_hwsku_hostname,
+                               localhost, platform_api_conn):
     """
     @summary: Verify output of `config chassis modules startup <DPU_Number>`
     """
@@ -29,32 +30,39 @@ def test_dpu_ping_after_reboot(duthosts, enum_rand_one_per_hwsku_hostname, local
     ip_address_list = []
     num_modules = int(chassis.get_num_modules(platform_api_conn))
 
-    reboot(duthost, localhost, reboot_type=REBOOT_TYPE_COLD, wait_for_ssh=False)
+    reboot(duthost, localhost, reboot_type=REBOOT_TYPE_COLD,
+           wait_for_ssh=False)
     wait_for_startup(duthost, localhost, 10, 300)
-    pytest_assert(wait_until(300, 5, 0, check_interface_status_of_up_ports, duthost),
-                          "Not all ports that are admin up on are operationally up")
+    pytest_assert(wait_until(300, 5, 0, check_interface_status_of_up_ports,
+                  duthost),
+                  "Not all ports that are admin up on are operationally up")
     logging.info("Interfaces are up")
 
     for index in range(num_modules):
-        ip_address_list.append(module.get_midplane_ip(platform_api_conn, index))
+        ip_address_list.append(
+                module.get_midplane_ip(platform_api_conn, index))
         dpu = module.get_name(platform_api_conn, index)
-        duthosts.shell("config chassis modules startup %s"%dpu)
+        duthosts.shell("config chassis modules startup %s" % (dpu))
         time.sleep(2)
 
-    pytest_assert(wait_until(120, 30, 0, check_dpu_ping_status, duthost, ip_address_list),
-                          "Not all DPUs operationally up")
+    pytest_assert(wait_until(120, 30, 0, check_dpu_ping_status,  # noqa: F405
+                  duthost, ip_address_list),
+                  "Not all DPUs operationally up")
 
 
-def test_show_ping_int_after_reload(duthosts, enum_rand_one_per_hwsku_hostname, localhost, platform_api_conn):
+def test_show_ping_int_after_reload(duthosts, enum_rand_one_per_hwsku_hostname,
+                                    localhost, platform_api_conn):
     """
-    @summary: To Check Ping between NPU and DPU after configuration reload on NPU
+    @summary: To Check Ping between NPU and DPU
+              after configuration reload on NPU
     """
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     num_modules = int(chassis.get_num_modules(platform_api_conn))
     ip_address_list = []
 
     for index in range(num_modules):
-        ip_address_list.append(module.get_midplane_ip(platform_api_conn, index))
+        ip_address_list.append(
+                module.get_midplane_ip(platform_api_conn, index))
 
     logging.info("Reload configuration")
     duthost.shell("sudo config reload -y &>/dev/null", executable="/bin/bash")
@@ -62,5 +70,6 @@ def test_show_ping_int_after_reload(duthosts, enum_rand_one_per_hwsku_hostname, 
     logging.info("Wait until all critical services are fully started")
     wait_critical_processes(duthost)
 
-    pytest_assert(wait_until(30, 10, 0, check_dpu_ping_status, duthost, ip_address_list),
-                          "Not all DPUs operationally up")
+    pytest_assert(wait_until(30, 10, 0, check_dpu_ping_status,  # noqa: F405
+                  duthost, ip_address_list),
+                  "Not all DPUs operationally up")
