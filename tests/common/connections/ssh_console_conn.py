94afc20d41c2d6c80dc66727d2d78443f3d3865d
Matt Soulsby <mattsoulsby@microsoft.com>
2024-09-11 10:23:56 +1000

    tests-common: Handle SSHConsoleConn being closed prematurely
    * Add error describing a blocked console port
    * Add check to session_preparation, throwing error if console port is blocked

--- tests/common/connections/ssh_console_conn.py
+++ tests/common/connections/ssh_console_conn.py
@@ -2,7 +2,7 @@ import time
 import re
 from .base_console_conn import BaseConsoleConn, CONSOLE_SSH
 from netmiko.ssh_exception import NetMikoAuthenticationException
-
+from paramiko.ssh_exception import SSHException
 
 class SSHConsoleConn(BaseConsoleConn):
     def __init__(self, **kwargs):
@@ -26,7 +26,12 @@ class SSHConsoleConn(BaseConsoleConn):
         super(SSHConsoleConn, self).__init__(**kwargs)
 
     def session_preparation(self):
-        self._test_channel_read()
+        session_init_msg = self._test_channel_read()
+        self.logger.debug(session_init_msg)
+        
+        if re.search(r"Port is in use. Closing connection...", session_init_msg, flags=re.M):
+            raise PortInUseException(f"Host aborted connection, as console port '{self.username.split(':')[-1]}' is currently occupied.")
+
         if (self.menu_port):
             # For devices logining via menu port, 2 additional login are needed
             # Since we have attempted all passwords in __init__ of base class until successful login
@@ -151,3 +156,9 @@ class SSHConsoleConn(BaseConsoleConn):
         # and any other login is prevented
         self.remote_conn.close()
         del self.remote_conn
+
+
+class PortInUseException(SSHException):
+    '''Exception to denote a console port is in use.'''
+    pass
+
