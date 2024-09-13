62ea27e2b572b5a41062c10a8792138eb8b0a8d1
Jianquan Ye <jianquanye@microsoft.com>
2024-09-13 10:30:02 +1000

    Refactor test_acl.py with multi-thread for multi-dut scenario (#14545)
    Description of PR
    Summary:
    Refactor test_acl.py to better fit for chassis test.
    It reboot/config_reload the dut one by one, on Cisco Chassis, test_acl spend 12 hours.
    Use multi-thread to speed up the test.
    
    Approach
    What is the motivation for this PR?
    Refactor test_acl.py to better fit for chassis test.
    It reboot/config_reload the dut one by one, on Cisco Chassis, test_acl spend 12 hours.
    Use multi-thread to speed up the test.
    
    How did you do it?
    Use multi-thread to speed up the test.
    
    How did you verify/test it?
    Run on physical testbed
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/port_toggle.py
+++ tests/common/port_toggle.py
@@ -80,7 +80,7 @@ def port_toggle(duthost, tbinfo, ports=None, wait_time_getter=None, wait_after_p
 
         if not startup_ok:
             down_ports = __get_down_ports()
-            startup_err_msg = "Some ports did not come up as expected: {}".format(str(down_ports))
+            startup_err_msg = "{}: Some ports did not come up as expected: {}".format(duthost.hostname, str(down_ports))
     except Exception as e:
         startup_err_msg = "Startup ports failed with exception: {}".format(repr(e))
 
