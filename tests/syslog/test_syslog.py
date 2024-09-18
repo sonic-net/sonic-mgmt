aba038a20235984dc9c2ca92e4e3ef6c868ea5b3
Yutong Zhang <yutongzhang@microsoft.com>
2024-09-18 09:56:41 +0800

    fix

--- tests/syslog/test_syslog.py
+++ tests/syslog/test_syslog.py
@@ -1,6 +1,6 @@
 import logging
 import pytest
-from tests.common.helpers.syslog_helpers import run_syslog
+from tests.common.helpers.syslog_helpers import run_syslog, check_default_route   # noqa F401
 
 logger = logging.getLogger(__name__)
 
@@ -15,5 +15,7 @@ pytestmark = [
                           ("7.0.80.165", "7.0.80.166"),
                           ("fd82:b34f:cc99::100", "7.0.80.166"),
                           ("fd82:b34f:cc99::100", "fd82:b34f:cc99::200")])
-def test_syslog(rand_selected_dut, dummy_syslog_server_ip_a, dummy_syslog_server_ip_b, check_default_route):
+def test_syslog(rand_selected_dut, dummy_syslog_server_ip_a, dummy_syslog_server_ip_b,
+                check_default_route      # noqa: F811
+                ):
     run_syslog(rand_selected_dut, dummy_syslog_server_ip_a, dummy_syslog_server_ip_b, check_default_route)
