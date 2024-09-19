112dc096dba6f1b0906a645c7835f3e8f526e312
Ryan Garthwaite <rgarthwaite@microsoft.com>
2024-09-19 05:47:20 +0000

    Merged PR 7273: Fix forced management routes config issue in 201811
    When 201811.105 is installed on a device (tested on 7260 device) it has the following MGMT_INTERFACE config:
    ```
    "MGMT_INTERFACE": {
           "eth0|10.212.69.35/27": {
               "gwaddr": "10.212.69.33"
           },
           "eth0|2603:10e2:140:3000::43/122": {
               "forced_mgmt_routes": [
                   "10.212.69.2",
                   "10.212.69.3",
                   "10.201.148.32/28",
                   "10.1.84.0/24",
                   "10.212.69.0/24",
                   "10.212.65.0/24",
                   "10.64.5.0/24",
                   "10.64.6.0/24",
                   "10.20.8.129",
                   "10.20.8.130",
                   "2603:10b0:11d:1::400/118",
                   "2603:10b0:b17:18::400/118",
                   "2603:10e2:140:3000::101/122"
               ],
               "gwaddr": "2603:10e2:140:3000::41"
           }
       }
    ```
    This isn't an issue until the device warm-reboots a few hops later onto 202305 where it can no longer route to the trusty8 server.
    
    Fixed by moving the IPv4 forced_mgmt_routes out of the IPv6 section and into the IPv4 section.
    
    Here's a green multi-hop run showing that it can reach trusty8 to download the 202311 image and complete this upgrade hop of the path: https://dev.azure.com/mssonic/internal/_build/results?buildId=646399&view=logs&j=76acabad-01e9-5c52-6fe6-d396d63e85d2&t=55864d99-7fe9-5504-0078-bfbb010fc228
    
    ADO: https://msazure.visualstudio.com/One/_workitems/edit/29330440
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Bug fix for forced management routes configuration issue in the 201811 version.
    
    #### PR Summary
    This pull request addresses a configuration issue with forced management routes in the 201811 version.
    - `tests/metadata-scripts/utilities.py`: Added a new function `fix_forced_mgmt_routes_config` to correct the forced management routes for both IPv4 and IPv6.
    - Integrated the new function into the `set_base_image_a` function to ensure the fix is applied when the target version is 201811.

--- tests/metadata-scripts/utilities.py
+++ tests/metadata-scripts/utilities.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+from ipaddress import ip_network, IPv4Network, IPv6Network
 from tests.common.errors import RunAnsibleModuleFail
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.helpers.dut_utils import patch_rsyslog
@@ -8,6 +9,70 @@ from tests.common.reboot import REBOOT_TYPE_COLD, REBOOT_TYPE_SOFT
 from tests.upgrade_path.upgrade_helpers import install_sonic, check_sonic_version, reboot
 
 logger = logging.getLogger(__name__)
+
+
+def fix_forced_mgmt_routes_config(duthost):
+    """
+    Fixes the 201811 forced management routes configuration in the Redis DB where IPv4 CIDRs are mixed with IPv6 CIDRs
+    by partitioning them into the expected lists.
+    No config save or load is required as future upgrades before the issue manifests occur and do the config
+    sync in the process.
+    """
+
+    logger.info("Checking forced management routes configuration...")
+
+    # Fetch the interface IP keys e.g. 'MGMT_INTERFACE|eth0|2603:10e2:140:3000::43/122'
+    mgmt_if_ip_keys = duthost.command("redis-cli -n 4 KEYS MGMT_INTERFACE\\|eth0\\*")["stdout_lines"]
+
+    assert len(mgmt_if_ip_keys) == 2, \
+        "Expected 2 keys for the management interface - One for IPv4 and one for IPv6 but got: {}" \
+        .format(mgmt_if_ip_keys)
+
+    # Work out which is the IPv4 and which is the IPv6
+    key0_cidr = ip_network(mgmt_if_ip_keys[0].split("|")[-1], strict=False)
+    if isinstance(key0_cidr, IPv4Network):
+        ipv4_key = mgmt_if_ip_keys[0]
+        ipv6_key = mgmt_if_ip_keys[1]
+    elif isinstance(key0_cidr, IPv6Network):
+        ipv4_key = mgmt_if_ip_keys[1]
+        ipv6_key = mgmt_if_ip_keys[0]
+    else:
+        assert False, "Unexpected IP network type: {}".format(key0_cidr)
+
+    # Get the current forced management routes
+    get_forced_mgmt_routes_cmd_tmpl = "redis-cli -n 4 HGET '{}' 'forced_mgmt_routes@'"
+    orig_ipv4_forced_mgmt_routes = duthost.command(get_forced_mgmt_routes_cmd_tmpl.format(ipv4_key))["stdout"]
+    orig_ipv4_forced_mgmt_routes = orig_ipv4_forced_mgmt_routes.split(",") if orig_ipv4_forced_mgmt_routes else []
+    orig_ipv6_forced_mgmt_routes = duthost.command(get_forced_mgmt_routes_cmd_tmpl.format(ipv6_key))["stdout"]
+    orig_ipv6_forced_mgmt_routes = orig_ipv6_forced_mgmt_routes.split(",") if orig_ipv6_forced_mgmt_routes else []
+
+    # Partition based on IP
+    corrected_ipv4_forced_mgmt_routes = []
+    corrected_ipv6_forced_mgmt_routes = []
+    for cidr_str in orig_ipv4_forced_mgmt_routes + orig_ipv6_forced_mgmt_routes:
+        cidr = ip_network(cidr_str, strict=False)
+        if isinstance(cidr, IPv4Network):
+            corrected_ipv4_forced_mgmt_routes.append(cidr_str)
+        elif isinstance(cidr, IPv6Network):
+            corrected_ipv6_forced_mgmt_routes.append(cidr_str)
+        else:
+            raise TypeError("Unexpected IP network type: {}".format(cidr))
+
+    # Write them back out
+    set_forced_mgmt_routes_cmd_tmpl = "redis-cli -n 4 HSET '{}' 'forced_mgmt_routes@' '{}'"
+    if corrected_ipv4_forced_mgmt_routes != orig_ipv4_forced_mgmt_routes:
+        # IPv4 forced management routes have changed - write them back out
+        duthost.command(set_forced_mgmt_routes_cmd_tmpl.format(ipv4_key, ",".join(corrected_ipv4_forced_mgmt_routes)))
+        logger.info("Fixed IPv4 forced management routes from {} to {}".format(orig_ipv4_forced_mgmt_routes,
+                                                                               corrected_ipv4_forced_mgmt_routes))
+    if corrected_ipv6_forced_mgmt_routes != orig_ipv6_forced_mgmt_routes:
+        # IPv6 forced management routes have changed - write them back out
+        duthost.command(set_forced_mgmt_routes_cmd_tmpl.format(ipv6_key, ",".join(corrected_ipv6_forced_mgmt_routes)))
+        logger.info("Fixed IPv6 forced management routes from {} to {}".format(orig_ipv6_forced_mgmt_routes,
+                                                                               corrected_ipv6_forced_mgmt_routes))
+
+    logger.info("Forced management routes configuration fixed.")
+
 
 def set_base_image_a(duthost, localhost, base_image, tbinfo):
     """
@@ -37,6 +102,8 @@ def set_base_image_a(duthost, localhost, base_image, tbinfo):
     reboot_type = 'hard' if "s6100" in duthost.facts["platform"] else 'cold'
     reboot(duthost, localhost, reboot_type=reboot_type)
     check_sonic_version(duthost, target_version)
+    if "201811" in target_version:
+        fix_forced_mgmt_routes_config(duthost)
 
 
 def cleanup_prev_images(duthost):
