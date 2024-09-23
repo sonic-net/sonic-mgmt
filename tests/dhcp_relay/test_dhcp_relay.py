53384691866ebb4f5d997b98891593b65ec6eba7
Yaqiang Zhu <yaqiangzhu@microsoft.com>
2024-09-23 11:35:09 +0800

    [dhcp_relay] Add stress test for restarting dhcp_relay (#14641)
    What is the motivation for this PR?
    Address test gap for this change: sonic-net/sonic-buildimage#20021
    Prevously, dhcrelay would hit the issue that it wouldn't relay any packets if there are packets come when dhcrelay startup. This issue has been fixed from image side by sonic-net/sonic-buildimage#20021. This PR is to add test for it.
    
    How did you do it?
    Add stress test with dhcp_relay restart:
    Keep sending DHCP packets
    Restart dhcp_relay
    Check socket buffer
    Run general dhcp relay test.
    
    How did you verify/test it?
    Run test on m0/t0/dualtor topos, all passed

--- tests/dhcp_relay/test_dhcp_relay.py
+++ tests/dhcp_relay/test_dhcp_relay.py
@@ -1,4 +1,3 @@
-import ipaddress
 import pytest
 import random
 import time
@@ -16,6 +15,7 @@ from tests.common.utilities import skip_release
 from tests.common import config_reload
 from tests.common.platform.processes_utils import wait_critical_processes
 from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer, LogAnalyzerError
+from tests.dhcp_relay.dhcp_relay_utils import check_routes_to_dhcp_server, restart_dhcp_service
 
 pytestmark = [
     pytest.mark.topology('t0', 'm0'),
@@ -42,171 +42,6 @@ def ignore_expected_loganalyzer_exceptions(rand_one_dut_hostname, loganalyzer):
         loganalyzer[rand_one_dut_hostname].ignore_regex.extend(ignoreRegex)
 
     yield
-
-
-@pytest.fixture(scope="module")
-def dut_dhcp_relay_data(duthosts, rand_one_dut_hostname, ptfhost, tbinfo):
-    """ Fixture which returns a list of dictionaries where each dictionary contains
-        data necessary to test one instance of a DHCP relay agent running on the DuT.
-        This fixture is scoped to the module, as the data it gathers can be used by
-        all tests in this module. It does not need to be run before each test.
-    """
-    duthost = duthosts[rand_one_dut_hostname]
-    dhcp_relay_data_list = []
-
-    mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
-
-    switch_loopback_ip = mg_facts['minigraph_lo_interfaces'][0]['addr']
-
-    # SONiC spawns one DHCP relay agent per VLAN interface configured on the DUT
-    vlan_dict = mg_facts['minigraph_vlans']
-    for vlan_iface_name, vlan_info_dict in list(vlan_dict.items()):
-        # Filter(remove) PortChannel interfaces from VLAN members list
-        vlan_members = [port for port in vlan_info_dict['members'] if 'PortChannel' not in port]
-
-        # Gather information about the downlink VLAN interface this relay agent is listening on
-        downlink_vlan_iface = {}
-        downlink_vlan_iface['name'] = vlan_iface_name
-
-        for vlan_interface_info_dict in mg_facts['minigraph_vlan_interfaces']:
-            if vlan_interface_info_dict['attachto'] == vlan_iface_name:
-                downlink_vlan_iface['addr'] = vlan_interface_info_dict['addr']
-                downlink_vlan_iface['mask'] = vlan_interface_info_dict['mask']
-                break
-
-        # Obtain MAC address of the VLAN interface
-        res = duthost.shell('cat /sys/class/net/{}/address'.format(vlan_iface_name))
-        downlink_vlan_iface['mac'] = res['stdout']
-
-        downlink_vlan_iface['dhcp_server_addrs'] = mg_facts['dhcp_servers']
-
-        # We choose the physical interface where our DHCP client resides to be index of first interface
-        # with alias (ignore PortChannel) in the VLAN
-        client_iface = {}
-        for port in vlan_members:
-            if port in mg_facts['minigraph_port_name_to_alias_map']:
-                break
-        else:
-            continue
-        client_iface['name'] = port
-        client_iface['alias'] = mg_facts['minigraph_port_name_to_alias_map'][client_iface['name']]
-        client_iface['port_idx'] = mg_facts['minigraph_ptf_indices'][client_iface['name']]
-
-        # Obtain uplink port indicies for this DHCP relay agent
-        uplink_interfaces = []
-        uplink_port_indices = []
-        for iface_name, neighbor_info_dict in list(mg_facts['minigraph_neighbors'].items()):
-            if neighbor_info_dict['name'] in mg_facts['minigraph_devices']:
-                neighbor_device_info_dict = mg_facts['minigraph_devices'][neighbor_info_dict['name']]
-                if 'type' in neighbor_device_info_dict and neighbor_device_info_dict['type'] in \
-                        ['LeafRouter', 'MgmtLeafRouter']:
-                    # If this uplink's physical interface is a member of a portchannel interface,
-                    # we record the name of the portchannel interface here, as this is the actual
-                    # interface the DHCP relay will listen on.
-                    iface_is_portchannel_member = False
-                    for portchannel_name, portchannel_info_dict in list(mg_facts['minigraph_portchannels'].items()):
-                        if 'members' in portchannel_info_dict and iface_name in portchannel_info_dict['members']:
-                            iface_is_portchannel_member = True
-                            if portchannel_name not in uplink_interfaces:
-                                uplink_interfaces.append(portchannel_name)
-                            break
-                    # If the uplink's physical interface is not a member of a portchannel,
-                    # add it to our uplink interfaces list
-                    if not iface_is_portchannel_member:
-                        uplink_interfaces.append(iface_name)
-                    uplink_port_indices.append(mg_facts['minigraph_ptf_indices'][iface_name])
-
-        other_client_ports_indices = []
-        for iface_name in vlan_members:
-            if mg_facts['minigraph_ptf_indices'][iface_name] == client_iface['port_idx']:
-                pass
-            else:
-                other_client_ports_indices.append(mg_facts['minigraph_ptf_indices'][iface_name])
-
-        dhcp_relay_data = {}
-        dhcp_relay_data['downlink_vlan_iface'] = downlink_vlan_iface
-        dhcp_relay_data['client_iface'] = client_iface
-        dhcp_relay_data['other_client_ports'] = other_client_ports_indices
-        dhcp_relay_data['uplink_interfaces'] = uplink_interfaces
-        dhcp_relay_data['uplink_port_indices'] = uplink_port_indices
-        dhcp_relay_data['switch_loopback_ip'] = str(switch_loopback_ip)
-
-        # Obtain MAC address of an uplink interface because vlan mac may be different than that of physical interfaces
-        res = duthost.shell('cat /sys/class/net/{}/address'.format(uplink_interfaces[0]))
-        dhcp_relay_data['uplink_mac'] = res['stdout']
-        dhcp_relay_data['default_gw_ip'] = mg_facts['minigraph_mgmt_interface']['gwaddr']
-
-        dhcp_relay_data_list.append(dhcp_relay_data)
-
-    return dhcp_relay_data_list
-
-
-def check_routes_to_dhcp_server(duthost, dut_dhcp_relay_data):
-    """Validate there is route on DUT to each DHCP server
-    """
-    default_gw_ip = dut_dhcp_relay_data[0]['default_gw_ip']
-    dhcp_servers = set()
-    for dhcp_relay in dut_dhcp_relay_data:
-        dhcp_servers |= set(dhcp_relay['downlink_vlan_iface']['dhcp_server_addrs'])
-
-    for dhcp_server in dhcp_servers:
-        rtInfo = duthost.get_ip_route_info(ipaddress.ip_address(dhcp_server))
-        nexthops = rtInfo["nexthops"]
-        if len(nexthops) == 0:
-            logger.info("Failed to find route to DHCP server '{0}'".format(dhcp_server))
-            return False
-        if len(nexthops) == 1:
-            # if only 1 route to dst available - check that it's not default route via MGMT iface
-            route_index_in_list = 0
-            ip_dst_index = 0
-            route_dst_ip = nexthops[route_index_in_list][ip_dst_index]
-            if route_dst_ip == ipaddress.ip_address(default_gw_ip):
-                logger.info("Found route to DHCP server via default GW(MGMT interface)")
-                return False
-    return True
-
-
-@pytest.fixture(scope="module")
-def validate_dut_routes_exist(duthosts, rand_one_dut_hostname, dut_dhcp_relay_data):
-    """Fixture to valid a route to each DHCP server exist
-    """
-    pytest_assert(wait_until(120, 5, 0, check_routes_to_dhcp_server, duthosts[rand_one_dut_hostname],
-                             dut_dhcp_relay_data), "Failed to find route for DHCP server")
-
-
-def restart_dhcp_service(duthost):
-    duthost.shell('systemctl reset-failed dhcp_relay')
-    duthost.shell('systemctl restart dhcp_relay')
-    duthost.shell('systemctl reset-failed dhcp_relay')
-
-    for retry in range(5):
-        time.sleep(30)
-        dhcp_status = duthost.shell('docker container top dhcp_relay | grep dhcrelay | cat')["stdout"]
-        if dhcp_status != "":
-            break
-    else:
-        assert False, "Failed to restart dhcp docker"
-
-    time.sleep(30)
-
-
-def get_subtype_from_configdb(duthost):
-    # HEXISTS returns 1 if the key exists, otherwise 0
-    subtype_exist = int(duthost.shell('redis-cli -n 4 HEXISTS "DEVICE_METADATA|localhost" "subtype"')["stdout"])
-    subtype_value = ""
-    if subtype_exist:
-        subtype_value = duthost.shell('redis-cli -n 4 HGET "DEVICE_METADATA|localhost" "subtype"')["stdout"]
-    return subtype_exist, subtype_value
-
-
-@pytest.fixture(scope="module")
-def testing_config(duthosts, rand_one_dut_hostname, tbinfo):
-    duthost = duthosts[rand_one_dut_hostname]
-
-    if 'dualtor' in tbinfo['topo']['name']:
-        yield DUAL_TOR_MODE, duthost
-    else:
-        yield SINGLE_TOR_MODE, duthost
 
 
 def check_interface_status(duthost):
