4d2107e9a6d482b09cffdfee363b4ee3f09ddbf5
Illia <37450862+illia-kotvitskyi@users.noreply.github.com>
2024-09-23 20:22:20 +0300

    ensure flex_db counters match expected number on setup of pfcwd_status tests (#14274)
    move number of flex_db counters per port to FLEXDB_COUNTERS_PER_PORT
    
    Change-Id: I0d63e2c4de5de424dd7ba993706e0569a5a9f6d4
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/generic_config_updater/test_pfcwd_status.py
+++ tests/generic_config_updater/test_pfcwd_status.py
@@ -20,6 +20,7 @@ logger = logging.getLogger(__name__)
 
 READ_FLEXDB_TIMEOUT = 20
 READ_FLEXDB_INTERVAL = 5
+FLEXDB_COUNTERS_PER_PORT = 3
 
 
 @pytest.fixture(autouse=True)
@@ -72,7 +73,7 @@ def set_default_pfcwd_config(duthost):
 
 
 @pytest.fixture
-def ensure_dut_readiness(duthost):
+def ensure_dut_readiness(duthost, extract_pfcwd_config):
     """
     Verify dut health/create and rollback checkpoint
 
@@ -81,6 +82,10 @@ def ensure_dut_readiness(duthost):
     """
     verify_orchagent_running_or_assert(duthost)
     create_checkpoint(duthost)
+
+    pfcwd_config = extract_pfcwd_config
+    number_of_ports = len(pfcwd_config)
+    check_config_update(duthost, number_of_ports * FLEXDB_COUNTERS_PER_PORT)
 
     yield
 
@@ -190,9 +195,10 @@ def test_stop_pfcwd(duthost, extract_pfcwd_config, ensure_dut_readiness, port):
         4. Validates that orchagent is running fine pre and post test
     """
     pfcwd_config = extract_pfcwd_config
+    initial_count = len(pfcwd_config) * FLEXDB_COUNTERS_PER_PORT
 
     if port == 'single':
-        expected_count = get_flex_db_count(duthost) - 3
+        expected_count = initial_count - FLEXDB_COUNTERS_PER_PORT
     else:
         expected_count = 0
     json_patch = list()
@@ -232,9 +238,9 @@ def test_start_pfcwd(duthost, extract_pfcwd_config, ensure_dut_readiness, stop_p
     pfcwd_config = extract_pfcwd_config
 
     if port == 'single':
-        expected_count = 3
+        expected_count = FLEXDB_COUNTERS_PER_PORT
     else:
-        expected_count = len(pfcwd_config) * 3
+        expected_count = len(pfcwd_config) * FLEXDB_COUNTERS_PER_PORT
     json_patch = list()
     exp_str = 'Ethernet'
     op = 'add'
