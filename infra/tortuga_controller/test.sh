614696d1424ae32aa32aeed5714e05679787f884
Yucai Gu (yucgu) <yucgu@cisco.com>
2024-09-24 19:40:12 -0700

    Update controller test to 0120 (#480)

--- infra/tortuga_controller/test.sh
+++ infra/tortuga_controller/test.sh
@@ -15,20 +15,7 @@ FABRIC_NAME=tortuga-1x3
 PYVXR_HOST=tortuga-1x3.cisco.com
 HOST_PORTS=40409,39855,36427,41851,49665,56787,35027,56005,51299
 LEAF_PORTS=43039,45263,52467
-SPINE_COUNT=1
-TEST_NAME="${1}"
-HOST_USER="vxr"
-
-# Vrf40000 is automatically created by Tortuga when a L2VNI + SAG
-# is added to the fabric.
-SUBINFS1x3="Vrf40000|*|Ethernet1_9|eth1|lo|1"
-RELAYS1x3="Vrf40000|relay1|5.1.30.1|5010#5020"
-RELAYS1x3="*"
-BGPPEERS1x3="Vrf40000|bgp1#5.1.30.1#4000#65200"
-BGPPEERS1x3="*"
-ROUTES1x3="Vrf40000|5.1.1.0/24|41.220.1.1|leaf1|Ethernet1_32_1"
-ROUTES1x3="${ROUTES1x3},Vrf40000|6.6.6.0/24|blackhole"
-PORTS1x3="leaf1|Ethernet1_32_1#41.220.1.2/24#Vrf40000"
+SPINE_PORTS=1
 
 CONFIG_GEN=./config-gen
 os=$(uname)
@@ -36,26 +23,42 @@ if [[ "${os}" == "Darwin" ]]; then
   CONFIG_GEN=./sandbox/gobin/config-gen
 fi
 
+# Common static routes, sub-interfaces and routed port configs for PyVxr setups.
+# Vrf40000 is automatically created by Tortuga when a L2VNI + SAG is added.
+PYVXR_ROUTES="Vrf12000000|6.6.6.0/24|blackhole"
+PYVXR_PORTS="leaf0|Ethernet1_32_1#41.230.10.2/24#Vrf12000000"
+PYVXR_SUBINFS="Vrf40000|*|Ethernet1_9|eth1|lo|2"
+PYVXR_RELAYS="*"
+PYVXR_BGPPEERS="*"
+PYVXR_CHANNELS="*"
+PYVXR_VRFS="*"
 CLOUD_URL=https://tortuga-k8s-a.cisco.com:32398
 START_TIME=$(date +%s)
 TEST_TAGS="sonic-test,beta2,ipv4,ipv6,loopback"
 CGEN_TEST=extended
 ORG_NAME="Test"
+HOST_USER="vxr"
+LAG=true
 
 # Disable SSH based pre/post checks in prod mode.
-if [[ "${TEST_NAME}" == "prod" ]]; then
+if [[ "${1}" == "-prod" ]]; then
   TEST_TAGS="${TEST_TAGS},no-ssh"
-  TEST_NAME=""
+elif [[ "${1}" == "-nolag" ]]; then
+  LAG=false
+fi
+
+if [[ "${LAG}" == true ]]; then
+  PYVXR_CHANNELS="PortChannel0|leaf0:Ethernet1_7#leaf0:Ethernet1_10|10|false|eth1#eth2"
+  LENGTH=$(echo "${LEAF_PORTS}" | tr -cd , | wc -c)
+  if [[ ${LENGTH} -gt 0 ]]; then
+    PYVXR_CHANNELS="${PYVXR_CHANNELS},PortChannel10|leaf1:Ethernet1_7#leaf1:Ethernet1_10|10|false|eth1#eth2"
+  fi
+  if [[ ${LENGTH} -gt 1 ]]; then
+    PYVXR_CHANNELS="${PYVXR_CHANNELS},PortChannel20|leaf2:Ethernet1_7#leaf2:Ethernet1_10|10|false|eth1#eth2"
+  fi
 fi
 
 set -euo pipefail
-
-function cleanup() {
-  "${CONFIG_GEN}" --cloud "${CLOUD_URL}" --reset --fabric "${FABRIC_NAME}" --orgName "${ORG_NAME}" --timeout "90s"
-  echo
-  echo "-------------------------Running ${1}-------------------------"
-  echo
-}
 
 # SONiC regression test flow:
 # 1) Create three L2VNI with SAG.
@@ -67,28 +70,36 @@ function cleanup() {
 # 5) Adds Loopbacks to all leaf switches.
 # 6) Adds multiple SubInterfaces to all leaves from host1.
 # 7) Adds static routes to host's loopbacks.
-cleanup "SONiC regression tests"
+function run_pyvxr() {
+  "${CONFIG_GEN}" \
+    --lldp \
+    --auto \
+    --prefix \
+    --verify \
+    --orgName "${ORG_NAME}" \
+    --hostUser "${HOST_USER}" \
+    --test "${CGEN_TEST}" \
+    --cloud "${CLOUD_URL}" \
+    --fabric "${FABRIC_NAME}" \
+    --pyvxr "${PYVXR_HOST}" \
+    --spines "${SPINE_PORTS}" \
+    --leaves "${LEAF_PORTS}" \
+    --hosts "${HOST_PORTS}" \
+    --ports "${PYVXR_PORTS}" \
+    --routes "${PYVXR_ROUTES}" \
+    --dhcpRelays "${PYVXR_RELAYS}" \
+    --bgpPeers "${PYVXR_BGPPEERS}" \
+    --subInterfaces "${PYVXR_SUBINFS}" \
+    --portChannels "${PYVXR_CHANNELS}" \
+    --vrfs "${PYVXR_VRFS}" \
+    --tags "${TEST_TAGS}"
+}
 
-"${CONFIG_GEN}" \
-  --lldp \
-  --auto \
-  --prefix \
-  --verify \
-  --orgName "${ORG_NAME}" \
-  --hostUser "${HOST_USER}" \
-  --test "${CGEN_TEST}" \
-  --cloud "${CLOUD_URL}" \
-  --fabric "${FABRIC_NAME}" \
-  --pyvxr "${PYVXR_HOST}" \
-  --spines "${SPINE_COUNT}" \
-  --leaves "${LEAF_PORTS}" \
-  --hosts "${HOST_PORTS}" \
-  --dhcpRelays "${RELAYS1x3}" \
-  --bgpPeers "${BGPPEERS1x3}" \
-  --subInterfaces "${SUBINFS1x3}" \
-  --ports "${PORTS1x3}" \
-  --routes "${ROUTES1x3}" \
-  --tags "${TEST_TAGS}"
+echo
+echo "-------------------------SONiC regression tests-------------------------"
+echo
+"${CONFIG_GEN}" --cloud "${CLOUD_URL}" --reset --fabric "${FABRIC_NAME}" --orgName "${ORG_NAME}" --timeout "90s"
+run_pyvxr
 
 end=$(date +%s)
 stm=$((end-START_TIME))
