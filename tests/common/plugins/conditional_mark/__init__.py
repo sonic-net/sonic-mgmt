5c74d9483a261eb59dab72dd446c7de1eedf9be3
wumiao <wu.miao@nokia.com>
2024-09-26 16:16:28 -0400

    Fix:

--- tests/common/plugins/conditional_mark/__init__.py
+++ tests/common/plugins/conditional_mark/__init__.py
@@ -273,8 +273,7 @@ def load_config_facts(inv_name, dut_name):
             results['VOQ_INBAND_INTERFACE'] = output_fields.get('VOQ_INBAND_INTERFACE', {})
             results['BGP_VOQ_CHASSIS_NEIGHBOR'] = output_fields.get('BGP_VOQ_CHASSIS_NEIGHBOR', {})
             results['INTERFACE'] = output_fields.get('INTERFACE', {})
-            if ('DEVICE_METADATA' in output_fields and 'localhost' in output_fields['DEVICE_METADATA']
-                    and 'switch_type' in output_fields['DEVICE_METADATA']['localhost']):
+            if 'switch_type' in output_fields['DEVICE_METADATA']['localhost']:
                 results['switch_type'] = output_fields['DEVICE_METADATA']['localhost']['switch_type']
 
     except Exception as e:
