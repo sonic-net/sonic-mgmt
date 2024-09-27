sonic-net/sonic-mgmt

commit 81ed1325515a9dca9d697e41c565d664447add48

Minimize cross-module dependencies in test_upgrade_path.py (#14672)
What is the motivation for this PR?
In the test_upgrade_path.py script, several functions are imported from other directories. To reduce cross-module dependencies, we have relocated these shared functions to a common directory.

tests/conftest.py
@@ -66,9 +66,9 @@ try:
 except ImportError as e:
     logging.error(e)
 
-from tests.platform_tests.args.advanced_reboot_args import add_advanced_reboot_args
-from tests.platform_tests.args.cont_warm_reboot_args import add_cont_warm_reboot_args
-from tests.platform_tests.args.normal_reboot_args import add_normal_reboot_args
+from tests.common.platform.args.advanced_reboot_args import add_advanced_reboot_args
+from tests.common.platform.args.cont_warm_reboot_args import add_cont_warm_reboot_args
+from tests.common.platform.args.normal_reboot_args import add_normal_reboot_args
 from ptf import testutils
 from ptf.mask import Mask
 

tests/common/helpers/upgrade_helpers.py
@@ -10,7 +10,7 @@ from tests.common import reboot
 from tests.common.reboot import get_reboot_cause, reboot_ctrl_dict
 from tests.common.reboot import REBOOT_TYPE_WARM, REBOOT_TYPE_COLD
 from tests.common.utilities import wait_until, setup_ferret
-from tests.platform_tests.verify_dut_health import check_neighbors
+from tests.common.platform.device_utils import check_neighbors
 
 SYSTEM_STABILIZE_MAX_TIME = 300
 logger = logging.getLogger(__name__)

tests/common/platform/args/advanced_reboot_args.py
@@ -1,5 +1,5 @@
 from tests.common.utilities import str2bool
-from tests.platform_tests.warmboot_sad_cases import SAD_CASE_LIST
+from tests.common.platform.warmboot_sad_cases import SAD_CASE_LIST
 
 
 def add_advanced_reboot_args(parser):

commit 3466d276069d636767374f44030a13b193198e74

Merged PR 7468: [Bugfix] Fix import error cause by PR #14672
Fix import error cause by PR #14672

----
#### AI description  (iteration 1)
#### PR Classification
Bug fix

#### PR Summary
This pull request addresses an import error caused by a previous pull request (#14672) and introduces a new log analysis feature for advanced reboot tests.
- `/tests/platform_tests/conftest.py`: Added `advanceboot_loganalyzer_factory` and `multihop_advanceboot_loganalyzer_factory` functions for advanced reboot log analysis.
- `/tests/platform_tests/templates/expect_boot_messages`: Added a new template file for expected boot messages.
- `/tests/metadata-scripts/test_metadata_upgrade_path.py`: Updated imports to fix the import error and reorganized utility functions.
- `/tests/metadata-scripts/test_multi_hop_upgrade_path.py`: Updated imports to fix the import error and reorganized utility functions.

tests/metadata-scripts/test_metadata_upgrade_path.py
@@ -7,10 +7,9 @@ from tests.common import reboot
 from tests.common.helpers.upgrade_helpers import install_sonic, upgrade_test_helper, add_pfc_storm_table
 from tests.common.fixtures.advanced_reboot import get_advanced_reboot
 from tests.common.fixtures.duthost_utils import backup_and_restore_config_db
-from tests.platform_tests.conftest import advanceboot_loganalyzer, advanceboot_neighbor_restore
+from tests.common.platform.device_utils import advanceboot_loganalyzer, advanceboot_neighbor_restore, verify_dut_health
 from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # noqa F401
-from tests.platform_tests.warmboot_sad_cases import get_sad_case_list, SAD_CASE_LIST
-from tests.platform_tests.verify_dut_health import verify_dut_health        # lgtm[py/unused-import]
+from tests.common.platform.warmboot_sad_cases import get_sad_case_list, SAD_CASE_LIST
 from tests.platform_tests.verify_dut_health import add_fail_step_to_reboot  # lgtm[py/unused-import]
 
 pytestmark = [

tests/metadata-scripts/test_multi_hop_upgrade_path.py
@@ -8,7 +8,7 @@ from tests.common.utilities import wait_until
 from tests.common.helpers.upgrade_helpers import SYSTEM_STABILIZE_MAX_TIME, check_copp_config, check_reboot_cause, \
     check_services, install_sonic, multi_hop_warm_upgrade_test_helper
 from tests.platform_tests.conftest import multihop_advanceboot_loganalyzer_factory
-from tests.platform_tests.verify_dut_health import verify_dut_health, check_neighbors   # lgtm[py/unused-import]
+from tests.common.platform.device_utils import verify_dut_health, check_neighbors
 from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory   # noqa F401
 from postupgrade_helper import run_postupgrade_actions
 from utilities import cleanup_prev_images, set_base_image_a, sonic_update_firmware

tests/platform_tests/conftest.py
@@ -3,9 +3,14 @@ import pytest
 import os
 import logging
 from tests.common.mellanox_data import is_mellanox_device
+from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer
+from tests.common.platform.device_utils import get_current_sonic_version, overwrite_script_to_backup_logs, \
+    get_kexec_time, analyze_log_file, analyze_sairedis_rec, _parse_timestamp, get_data_plane_report, \
+    get_report_summary, verify_mac_jumping, verify_required_events, LOGS_ON_TMPFS_PLATFORMS
 from .args.counterpoll_cpu_usage_args import add_counterpoll_cpu_usage_args
 from tests.common.helpers.mellanox_thermal_control_test_helper import suspend_hw_tc_service, resume_hw_tc_service
 
+TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates")
 
 @pytest.fixture(autouse=True, scope="module")
 def skip_on_simx(duthosts, rand_one_dut_hostname):
@@ -198,3 +203,206 @@ def dpu_npu_port_list(duthosts):
                     dpu_npu_port_list[dut.hostname].append(port_key.split("|")[-1])
     logging.info(f"dpu npu port list: {dpu_npu_port_list}")
     return dpu_npu_port_list
+
+
+def advanceboot_loganalyzer_factory(duthost, request, marker_postfix=None):
+    """Create pre-reboot and post-reboot analysis functions via `LogAnalyzer` with optional marker postfix"""
+    test_name = request.node.name
+    if "upgrade_path" in test_name:
+        reboot_type_source = request.config.getoption("--upgrade_type")
+    else:
+        reboot_type_source = test_name
+    if "warm" in reboot_type_source:
+        reboot_type = "warm"
+    elif "fast" in reboot_type_source:
+        reboot_type = "fast"
+    else:
+        reboot_type = "unknown"
+    platform = duthost.facts["platform"]
+    logs_in_tmpfs = list()
+
+    marker_prefix = "test_advanced_reboot_{}".format(test_name) if not marker_postfix else \
+        "test_advanced_reboot_{}_{}".format(test_name, marker_postfix)
+    loganalyzer = LogAnalyzer(
+        ansible_host=duthost, marker_prefix=marker_prefix)
+    base_os_version = list()
+
+    def bgpd_log_handler(preboot=False):
+        # check current OS version post-reboot. This can be different than preboot OS version in case of upgrade
+        current_os_version = get_current_sonic_version(duthost)
+        if preboot:
+            if 'SONiC-OS-201811' in current_os_version:
+                bgpd_log = "/var/log/quagga/bgpd.log"
+            else:
+                bgpd_log = "/var/log/frr/bgpd.log"
+            additional_files = {'/var/log/swss/sairedis.rec': '', bgpd_log: ''}
+            loganalyzer.additional_files = list(additional_files.keys())
+            loganalyzer.additional_start_str = list(additional_files.values())
+            return bgpd_log
+        else:
+            # log_analyzer may start with quagga and end with frr, and frr.log might still have old logs.
+            # To avoid missing preboot log, or analyzing old logs, combine quagga and frr log into new file
+            duthost.shell("cat {} {} | sort -n > {}".format(
+                "/var/log/quagga/bgpd.log", "/var/log/frr/bgpd.log", "/var/log/bgpd.log"), module_ignore_errors=True)
+            loganalyzer.additional_files = [
+                '/var/log/swss/sairedis.rec', '/var/log/bgpd.log']
+
+    def pre_reboot_analysis():
+        log_filesystem = duthost.shell(
+            "df --output=fstype -h /var/log")['stdout']
+        logs_in_tmpfs.append(
+            True if (log_filesystem and "tmpfs" in log_filesystem) else False)
+        base_os_version.append(get_current_sonic_version(duthost))
+        bgpd_log = bgpd_log_handler(preboot=True)
+        if platform in LOGS_ON_TMPFS_PLATFORMS or (len(logs_in_tmpfs) > 0 and logs_in_tmpfs[0] is True):
+            # For small disk devices, /var/log in mounted in tmpfs.
+            # Hence, after reboot the preboot logs are lost.
+            # For log_analyzer to work, it needs logs from the shutdown path
+            # Below method inserts a step in reboot script to back up logs to /host/
+            overwrite_script_to_backup_logs(duthost, reboot_type, bgpd_log)
+        marker = loganalyzer.init()
+        loganalyzer.load_common_config()
+
+        ignore_file = os.path.join(TEMPLATES_DIR, "ignore_boot_messages")
+        expect_file = os.path.join(TEMPLATES_DIR, "expect_boot_messages")
+        ignore_reg_exp = loganalyzer.parse_regexp_file(src=ignore_file)
+        expect_reg_exp = loganalyzer.parse_regexp_file(src=expect_file)
+
+        loganalyzer.ignore_regex.extend(ignore_reg_exp)
+        loganalyzer.expect_regex = []
+        loganalyzer.expect_regex.extend(expect_reg_exp)
+        loganalyzer.match_regex = []
+        return marker
+
+    def post_reboot_analysis(marker, event_counters=None, reboot_oper=None, log_dir=None):
+        bgpd_log_handler()
+        if platform in LOGS_ON_TMPFS_PLATFORMS or (len(logs_in_tmpfs) > 0 and logs_in_tmpfs[0] is True):
+            restore_backup = "mv /host/syslog.99 /var/log/; " + \
+                             "mv /host/sairedis.rec.99 /var/log/swss/; " + \
+                             "mv /host/swss.rec.99 /var/log/swss/; " + \
+                             "mv /host/bgpd.log.99 /var/log/"
+            duthost.shell(restore_backup, module_ignore_errors=True)
+            # find the fast/warm-reboot script path
+            reboot_script_path = duthost.shell('which {}'.format(
+                "{}-reboot".format(reboot_type)))['stdout']
+            # restore original script. If the ".orig" file does not exist (upgrade path case), ignore the error.
+            duthost.shell("mv {} {}".format(reboot_script_path + ".orig", reboot_script_path),
+                          module_ignore_errors=True)
+        # For mac jump test, the log message we care about is uaually combined with other messages in one line,
+        # which makes the length of the line longer than 1000 and get dropped by Logananlyzer. So we need to increase
+        # the max allowed length.
+        # The regex library in Python 2 takes very long time (over 10 minutes) to process long lines. In our test,
+        # most of the combined log message for mac jump test is around 5000 characters. So we set the max allowed
+        # length to 6000.
+        result = loganalyzer.analyze(marker, fail=False, maximum_log_length=6000)
+        analyze_result = {"time_span": dict(), "offset_from_kexec": dict()}
+        offset_from_kexec = dict()
+
+        # Parsing sairedis shall happen after parsing syslog because FDB_AGING_DISABLE is required
+        # when analysing sairedis.rec log, so we need to sort the keys
+        key_list = ["syslog", "bgpd.log", "sairedis.rec"]
+        for i in range(0, len(key_list)):
+            for message_key in list(result["expect_messages"].keys()):
+                if key_list[i] in message_key:
+                    key_list[i] = message_key
+                    break
+
+        for key in key_list:
+            messages = result["expect_messages"][key]
+            if "syslog" in key:
+                get_kexec_time(duthost, messages, analyze_result)
+                reboot_start_time = analyze_result.get(
+                    "reboot_time", {}).get("timestamp", {}).get("Start")
+                if not reboot_start_time or reboot_start_time == "N/A":
+                    logging.error("kexec regex \"Rebooting with /sbin/kexec\" not found in syslog. " +
+                                  "Skipping log_analyzer checks..")
+                    return
+                syslog_messages = messages
+            elif "bgpd.log" in key:
+                bgpd_log_messages = messages
+            elif "sairedis.rec" in key:
+                sairedis_rec_messages = messages
+
+        # analyze_sairedis_rec() use information from syslog and must be called after analyzing syslog
+        analyze_log_file(duthost, syslog_messages,
+                         analyze_result, offset_from_kexec)
+        analyze_log_file(duthost, bgpd_log_messages,
+                         analyze_result, offset_from_kexec)
+        analyze_sairedis_rec(sairedis_rec_messages,
+                             analyze_result, offset_from_kexec)
+
+        for marker, time_data in list(analyze_result["offset_from_kexec"].items()):
+            marker_start_time = time_data.get("timestamp", {}).get("Start")
+            reboot_start_time = analyze_result.get(
+                "reboot_time", {}).get("timestamp", {}).get("Start")
+            if reboot_start_time and reboot_start_time != "N/A" and marker_start_time:
+                time_data["time_taken"] = (_parse_timestamp(marker_start_time) -
+                                           _parse_timestamp(reboot_start_time)).total_seconds()
+            else:
+                time_data["time_taken"] = "N/A"
+
+        if reboot_oper and not isinstance(reboot_oper, str):
+            reboot_oper = type(reboot_oper).__name__
+        get_data_plane_report(analyze_result, reboot_type,
+                              log_dir, reboot_oper)
+        result_summary = get_report_summary(
+            duthost, analyze_result, reboot_type, reboot_oper, base_os_version)
+        logging.info(json.dumps(analyze_result, indent=4))
+        logging.info(json.dumps(result_summary, indent=4))
+        if reboot_oper:
+            report_file_name = request.node.name + "_" + reboot_oper + "_report.json"
+            summary_file_name = request.node.name + "_" + reboot_oper + "_summary.json"
+        else:
+            report_file_name = request.node.name + "_report.json"
+            summary_file_name = request.node.name + "_summary.json"
+
+        report_file_dir = os.path.realpath((os.path.join(os.path.dirname(__file__),
+                                                         "../logs/platform_tests/")))
+        report_file_path = report_file_dir + "/" + report_file_name
+        summary_file_path = report_file_dir + "/" + summary_file_name
+        if not os.path.exists(report_file_dir):
+            os.makedirs(report_file_dir)
+        with open(report_file_path, 'w') as fp:
+            json.dump(analyze_result, fp, indent=4)
+        with open(summary_file_path, 'w') as fp:
+            json.dump(result_summary, fp, indent=4)
+
+        # After generating timing data report, do some checks on the timing data
+        verification_errors = list()
+        verify_mac_jumping(test_name, analyze_result, verification_errors)
+        if duthost.facts['platform'] != 'x86_64-kvm_x86_64-r0':
+            # TBD: expand this verification to KVM - extra port events in KVM which need to be filtered
+            verify_required_events(
+                duthost, event_counters, analyze_result, verification_errors)
+        return verification_errors
+
+    return pre_reboot_analysis, post_reboot_analysis
+
+
+@pytest.fixture()
+def multihop_advanceboot_loganalyzer_factory(duthosts, enum_rand_one_per_hwsku_frontend_hostname, request):
+    """
+    Advance reboot log analysis involving multiple hops.
+    This fixture returns a factory function requiring the hop_index to be supplied.
+    Then, it starts log analysis at the beginning of the test. At the end,
+    the collected expect messages are verified and timing of start/stop is calculated.
+
+    Args:
+        duthosts : List of DUT hosts
+        enum_rand_one_per_hwsku_frontend_hostname: hostname of a randomly selected DUT
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    # Currently, advanced reboot test would skip for kvm platform if the test has no device_type marker for vs.
+    # Doing the same skip logic in this fixture to avoid running loganalyzer without the test executed
+    if duthost.facts['platform'] == 'x86_64-kvm_x86_64-r0':
+        device_marks = [arg for mark in request.node.iter_markers(
+            name='device_type') for arg in mark.args]
+        if 'vs' not in device_marks:
+            pytest.skip('Testcase not supported for kvm')
+
+    def _multihop_advanceboot_loganalyzer_factory(hop_index):
+        pre_reboot_analysis, post_reboot_analysis = advanceboot_loganalyzer_factory(
+            duthost, request, marker_postfix="hop-{}".format(hop_index))
+        return pre_reboot_analysis, post_reboot_analysis
+
+    yield _multihop_advanceboot_loganalyzer_factory

tests/platform_tests/templates/expect_boot_messages
@@ -1,0 +1,60 @@
+r, ".* NOTICE (?:admin|root): Rebooting with /sbin/kexec -e to.*..."
+
+r, ".* systemd.* (Stopping|Stopped|Starting|Started).* Database container.*"
+r, ".* NOTICE admin: (Stopping|Stopped|Starting|Started).* (?!syncd|swss|gbsyncd).* service.*"
+r, ".* NOTICE admin: Stopped.* swss.* service.*"
+r, ".* NOTICE root: (Stopping|Stopped|Starting|Started).* (swss|syncd|gbsyncd).* service.*"
+r, ".* root: WARMBOOT_FINALIZER : Wait for database to become ready.*"
+r, ".* NOTICE root: WARMBOOT_FINALIZER : Finalizing warmboot.*"
+r, ".* NOTICE root: WARMBOOT_FINALIZER : warmboot is not enabled.*"
+
+r, ".* NOTICE swss#orchagent.*initPort: Initialized port.*"
+r, ".* NOTICE swss#orchagent.*updatePortOperStatus: Port.*oper state set.*to up.*"
+r, ".* NOTICE teamd#tlm_teamd.*try_add_lag: The LAG 'PortChannel.*' has been added.*"
+r, ".* NOTICE swss#orchagent.*notifySyncd.*sending syncd: INIT_VIEW.*"
+r, ".* NOTICE swss#orchagent.*sai_redis_notify_syncd: switched ASIC to INIT VIEW.*"
+r, ".* NOTICE swss#orchagent.*notifySyncd.*sending syncd: APPLY_VIEW.*"
+r, ".* NOTICE swss#orchagent.*sai_redis_notify_syncd: switched ASIC to APPLY VIEW.*"
+r, ".* NOTICE syncd#syncd.*performWarmRestart: switches defined in warm restart.*"
+r, ".* NOTICE syncd#syncd.*performWarmRestartSingleSwitch: Warm boot: create switch VID.*"
+r, ".* NOTICE bgp#fpmsyncd.*main: Warm-Restart timer started.*.*"
+r, ".* NOTICE bgp#fpmsyncd.*main: Warm-Restart reconciliation processed..*"
+r, ".* INFO syncd#syncd.*SAI_API_FDB:_brcm_sai_fdb_event_cb.*fdbEvent: (delete \(0\)|0) for mac.*"
+r, ".* NOTICE swss#orchagent.*setAgingFDB: Set switch.*fdb_aging_time 0 sec"
+r, ".* NOTICE swss#orchagent.*do.*Task: Set switch attribute fdb_aging_time to 600"
+
+# sairedis.rec messages
+r, ".*\|c\|SAI_OBJECT_TYPE_SWITCH.*"
+r, ".*\|g\|SAI_OBJECT_TYPE_SWITCH.*SAI_SWITCH_ATTR_DEFAULT_VIRTUAL_ROUTER_ID.*"
+r, ".*\|(S|s)\|SAI_OBJECT_TYPE_ROUTE_ENTRY.*0\.0\.0\.0/0.*SAI_ROUTE_ENTRY_ATTR_PACKET_ACTION=SAI_PACKET_ACTION_FORWARD.*"
+r, ".*\|c\|SAI_OBJECT_TYPE_NEIGHBOR_ENTRY.*"
+r, ".*\|c\|SAI_OBJECT_TYPE_FDB_ENTRY.*"
+
+# bgpd.log messages
+r, ".*ADJCHANGE: neighbor .* in vrf default Up.*"
+r, ".*rcvd End-of-RIB for .* Unicast from.*"
+
+# mlnx specific syslog regexes
+r, ".* syncd.*mlnx_sai_switch.*mlnx_create_switch: Create switch.*INIT_SWITCH=true.*"
+r, ".* syncd#SDK.*mlnx_sai_switch.*mlnx_create_switch.*Created switch Switch ID.*"
+
+# mlnx specific sairedis regexes
+r, ".*\|n\|fdb_event.*fdb_entry.*mac.*fdb_event.*[SAI_FDB_EVENT_MOVE|SAI_FDB_EVENT_LEARNED].*SAI_FDB_ENTRY_ATTR_TYPE.*SAI_FDB_ENTRY_TYPE_DYNAMIC.*SAI_FDB_ENTRY_ATTR_PACKET_ACTION.*SAI_PACKET_ACTION_FORWARD.*"
+
+# 201911 specific regexes
+r, ".* NOTICE admin: Stopping radv ....*"
+r, ".* INFO systemd.*Stopped Router advertiser container.*"
+r, ".* INFO systemd.*Starting Router advertiser container.*"
+r, ".* INFO systemd.*Started Router advertiser container.*"
+r, ".* NOTICE admin: Stopping bgp.*"
+r, ".* NOTICE admin: Stopped  bgp.*"
+r, ".* INFO systemd.*Started BGP container.*"
+r, ".* NOTICE admin: Stopping teamd.*"
+r, ".* NOTICE admin: Stopped teamd.*"
+r, ".* INFO systemd.*Starting TEAMD container.*"
+r, ".* INFO systemd.*Started TEAMD container.*"
+r, ".* swss#orchagent.*sai_redis_notify_syncd.*sending syncd.*INIT view.*"
+r, ".* swss#orchagent.*initSaiRedis.*Notify syncd INIT_VIEW.*"
+r, ".* swss#orchagent.*sai_redis_notify_syncd.*sending syncd.*APPLY view.*"
+r, ".* syncd#SDK.*notifySyncd.*setting very first run to FALSE, op = APPLY_VIEW.*"
+r, ".* teamd#teammgrd.*setLagAdminStatus.*Set port channel PortChannel.*admin status to up.*"