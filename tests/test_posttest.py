41a06734e76f0ee88dd03d2277f7637d6f946725
augusdn <augustinelee@microsoft.com>
2024-09-30 17:04:54 +1000

    add ptfhost check statement to prevent failure on test_posttest.py (#14783)
    PTFHost can be None in some cases (eg. ixia) and this was not handled correctly in test_collect_ptf_logs. Add ptfhost check to prevent error caused by None value.
    
    PTFHost can be None in some cases (eg. ixia) and this was not handled correctly in test_collect_ptf_logs. Add ptfhost check to prevent error caused by None value.
    
    Description of PR
    Summary:
    PTFHost can be None in some cases (eg. ixia) and this was not handled correctly in test_collect_ptf_logs. Add ptfhost check to prevent error caused by None value.
    
    Approach
    What is the motivation for this PR?
    Resolve unexpected error during log collection
    
    How did you do it?
    check for None value
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/test_posttest.py
+++ tests/test_posttest.py
@@ -92,6 +92,8 @@ def test_enable_startup_tsa_tsb_service(duthosts, localhost):
 
 
 def test_collect_ptf_logs(ptfhost):
+    if ptfhost is None:
+        return
     log_files = ptfhost.shell('ls /tmp/*.log')['stdout'].split()
     if not os.path.exists('logs/ptf'):
         os.makedirs('logs/ptf')
