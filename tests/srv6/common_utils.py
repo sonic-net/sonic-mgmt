116e8f63d35e3368e6e39e831c5a066af5721eec
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2024-10-03 19:45:19 -0700

    enable 4 more test cases

---
+++ tests/srv6/common_utils.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+import re
+import ipaddress
+import logging
+import sys
+import argparse
+import logging
+import getpass
+
+logger = logging.getLogger(__name__)
+
+#
+# Flags used at run time
+#
+run_inside_docker = False
+debug_flag = False
+
+
+def set_debug_flag(flag):
+    global debug_flag
+    debug_flag = flag
+
+
+def get_debug_flag():
+    return debug_flag
+
+
+def set_run_inside_docker(flag):
+    global run_inside_docker
+    run_inside_docker  = flag
+
+
+def get_run_inside_docker():
+    global run_inside_docker
+    return run_inside_docker
+
+
+#
+# This is the IP to accessing host from sonic-mgmt
+#
+def get_hostip_and_user():
+    hostip, hostuser = "172.17.0.1", getpass.getuser()
+    return  hostip, hostuser
+
+
+#
+# Debug print util function for printing out debug information
+#
+def debug_print(msg, force = False):
+    if not get_debug_flag() and not force:
+        return
+    logger.info(msg)
+    print(msg)
+
+
+#
+# a util function to run command. add ssh if it is running inside sonic-mgmt docker.
+#
+def run_command_with_return(cmd, force = False):
+    if get_run_inside_docker():
+        # add host access
+        hostip, user = get_hostip_and_user()
+        cmd = "ssh  -q -o \"UserKnownHostsFile=/dev/null\" -o \"StrictHostKeyChecking=no\" {}@{} \"{}\"".format(user, hostip, cmd)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    output, stderr = process.communicate()
+    if stderr != "" and stderr != None:
+        # It is an error, use force print
+        debug_print("{} : get error {}".format(cmd, stderr), force = True)
+
+    debug_print("cmd : {}, stderr : {}, output : {}".format(cmd, stderr, output), force)
+    return output, stderr
+
+
+#
+# Goal is to run the following command to set up tcpdump
+# For example
+# ssh ubuntu@172.17.0.1 "nohup tcpdump -i VM0100-t0 -w /tmp/Vm0100-t0.pcap > /tmp/tcpdump.log 2>&1 &"
+#
+def enable_tcpdump(intf_list, file_loc, prefix, use_docker=False, set_debug=False):
+    # Enable flags baased on input flags
+    set_run_inside_docker(use_docker)
+    set_debug_flag(set_debug)
+    for intf in intf_list:
+        cmd = "tcpdump -i {} -w {}/{}_{}.pcap > /tmp/{}_{}.log 2>&1 &".format(intf, file_loc, prefix, intf, prefix, intf)
+        if get_run_inside_docker():
+            cmd = "nohup {}".format(cmd)
+        debug_print("Run {}".format(cmd), force = True)
+        run_command_with_return(cmd)
+        run_command_with_return("ps aux | grep tcpdump", force = True)
+    # Disable flags
+    set_debug_flag(False)
+    set_run_inside_docker(False)
+
+
+#
+# Remove all existing tcpdump sessions
+#
+def disable_tcpdump(use_docker=False, set_debug=False):
+    set_run_inside_docker(use_docker)
+    run_command_with_return("pkill tcpdump")
+    set_run_inside_docker(False)
--- tests/srv6/srv6_utils.py
+++ tests/srv6/srv6_utils.py
@@ -7,6 +7,11 @@ from tests.common.helpers.assertions import pytest_assert
 
 logger = logging.getLogger(__name__)
 
+#
+# log directory inside each vsonic. vsonic starts with admin as user.
+#
+test_log_dir = "/home/admin/testlogs/"
+
 
 #
 # Helper func for print a set of lines
@@ -38,7 +43,7 @@ def change_route(operation, ptfip, neighbor, route, nexthop, port):
 # Skip some BGP neighbor check
 #
 def skip_bgp_neighbor_check(neighbor):
-    skip_addresses = ['2064:100::1d', '2064:200::1e', '2064:300::1f']
+    skip_addresses = []
     for addr in skip_addresses:
         if neighbor == addr:
             return True
@@ -139,3 +144,142 @@ def runSendReceive(pkt, src_port, exp_pkt, dst_ports, pkt_expected, ptfadapter):
         logger.debug('Received: ' + str(received))
     logger.debug('Passed: ' + str(passed))
     return passed
+
+
+#
+# Helper func to check if a list of IPs go via a given set of next hop
+#
+def check_routes_func(nbrhost, ips, nexthops, vrf="", is_v6=False):
+    # Check remote learnt dual homing routes
+    vrf_str = ""
+    if vrf != "":
+        vrf_str = "vrf {}".format(vrf)
+    ip_str = "ip"
+    if is_v6:
+        ip_str = "ipv6"
+    for ip in ips:
+        cmd = "show {} route {} {} nexthop-group".format(ip_str, vrf_str, ip)
+        res = nbrhost.command(cmd)["stdout_lines"]
+        print_lines(res)
+        found = 0
+        for nexthop in nexthops:
+            for line in res:
+                if nexthop in line:
+                    found = found +1
+        if len(nexthops) != found:
+            return False
+    return True
+
+
+#
+# check if a list of IPs go via a given set of next hop
+#
+def check_routes(nbrhost, ips, nexthops, vrf="", is_v6=False):
+    # Add retry for debugging purpose
+    count = 0
+    ret = False
+
+    #
+    # Sleep 10 sec before retrying
+    #
+    sleep_duration_for_retry = 10
+
+    # retry 3 times before claiming failure
+    while count < 3 and ret == False:
+        ret = check_routes_func(nbrhost, ips, nexthops, vrf, is_v6)
+        if not ret:
+            count = count + 1
+            # sleep make sure all forwarding structures are settled down.
+            time.sleep(sleep_duration_for_retry)
+            logger.info("Sleep {} seconds to retry round {}".format(sleep_duration_for_retry, count))
+
+    pytest_assert(ret)
+
+
+#
+# Record fwding chain to a file
+#
+def recording_fwding_chain(nbrhost, fname, comments):
+
+    filename = "{}{}".format(test_log_dir, fname)
+
+    cmd = "mkdir -p {}".format(test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "sudo touch /etc/sonic/frr/vtysh.conf"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+    cmd = "date >> {} ".format(comments, filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "echo ' {}' >> {} ".format(comments, filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show bgp summary' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ip route vrf Vrf1 192.100.1.0 nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ipv6 route fd00:201:201:fff1:11:: nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ipv6 route fd00:202:202:fff2:22:: nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+    cmd = "echo '' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+
+#
+# Debug commands for FRR zebra
+#
+debug_cmds= [
+    'debug zebra events',
+    'debug zebra rib',
+    'debug zebra rib detailed',
+    'debug zebra nht',
+    'debug zebra nht detailed',
+    'debug zebra dplane',
+    'debug zebra nexthop',
+    'debug zebra nexthop detail',
+    'debug zebra packet',
+    'debug zebra packet detail'
+]
+
+
+#
+# Turn on/off FRR debug to a file
+#
+def turn_on_off_frr_debug(duthosts, rand_one_dut_hostname, nbrhosts, filename, vm, is_on = True):
+    nbrhost = nbrhosts[vm]['host']
+    # save frr log to a file
+    pfxstr = " "
+    if not is_on:
+        pfxstr = " no "
+
+    cmd = "vtysh -c 'configure terminal' -c '{} log file {}'".format(pfxstr, filename)
+    nbrhost.command(cmd)
+
+    #
+    # Change frr debug flags
+    #
+    for dcmd in debug_cmds:
+        cmd = "vtysh -c '"+ pfxstr + dcmd +"'"
+        nbrhost.command(cmd)
+
+    #
+    # Check debug flags
+    #
+    cmd = "vtysh -c 'show debug'"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    #
+    # Check log file
+    #
+    cmd = "vtysh -c 'show run' | grep log"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+
+#
+# Collect file from bgp docker
+#
+def collect_frr_debugfile(duthosts, rand_one_dut_hostname, nbrhosts, filename, vm):
+    nbrhost = nbrhosts[vm]['host']
+    cmd = "mkdir -p {}".format(test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "docker cp bgp:{} {}".format(filename, test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
