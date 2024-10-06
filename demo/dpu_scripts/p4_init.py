58892e15bf6471b33c6ed31d6e9319ff097e976c
r12f <r12f.code@gmail.com>
2024-10-06 02:19:04 +0000

    Underlay initialization using p4runtime shell python module.

---
+++ demo/dpu_scripts/p4_init.py
@@ -0,0 +1,53 @@
+import p4runtime_sh.p4runtime as p4sh_rt
+import p4runtime_sh.shell as p4sh
+import logging
+import sys
+import os
+
+
+# The verbose toggle in P4Runtime shell is not published to pip, so we need to disable it manually.
+def disable_print():
+    sys.stdout = open(os.devnull, 'w')
+
+
+def enable_print():
+    sys.stdout = sys.__stdout__
+
+
+def init_shell():
+    p4sh.setup(
+        device_id=0,
+        grpc_addr='127.0.0.1:9559',
+        election_id=(0, 1),
+    )
+
+
+def insert_underlay_entry(entry, match: str, action: str, next_hop_id: str):
+    try:
+        logging.info(f"Inserting underlay entry: {match} -> (Action:{action}, NextHopId: {next_hop_id})")
+
+        disable_print()
+        entry.match["meta.dst_ip_addr"] = match
+        entry.action["packet_action"] = action
+        entry.action["next_hop_id"] = next_hop_id
+        entry.insert()
+    except p4sh_rt.P4RuntimeWriteException as e:
+        logging.error(f"Failed to insert underlay entry: {e}")
+    finally:
+        enable_print()
+
+
+def init_underlay():
+    logging.info("Inserting underlay entries ...")
+
+    underlay_entry = p4sh.TableEntry("dash_ingress.underlay.underlay_routing")(action="dash_ingress.underlay.pkt_act")
+    insert_underlay_entry(underlay_entry, "::10.0.0.37/128", action="1", next_hop_id="0")
+    insert_underlay_entry(underlay_entry, "::10.0.0.39/128", action="1", next_hop_id="2")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    # sh.global_options["verbose"] = False
+
+    init_shell()
+    init_underlay()
--- demo/install_scripts.sh
+++ demo/install_scripts.sh
@@ -16,14 +16,21 @@ if ! which sshpass > /dev/null; then
     sudo apt-get install -y sshpass
 fi
 
+#
+# Installing all contents to NPU
+#
 echo "Installing all scripts to NPU..."
 $NPU_PW $SCP npu_scripts/* admin@$NPU_IP:
 
+#
+# Installing all contents to DPU
+#
 echo "Pushing minigraph to DPU ..."
 $DPU_PW $SCP ../ansible/minigraph/SONIC01DPU.xml* admin@$DPU_IP:
 
 echo "Installing all scripts to DPU..."
 $DPU_PW $SCP dpu_scripts/* admin@$DPU_IP:
+$DPU_PW $SSH admin@$DPU_IP "docker cp p4_*.py dash_engine:/behavioral-model"
 
 echo "Pushing p4runtime shell to DPU..."
 if [ ! -f p4runtime-sh.tar ]; then
@@ -32,18 +39,28 @@ if [ ! -f p4runtime-sh.tar ]; then
 fi
 $DPU_PW $SCP p4runtime-sh.tar admin@$DPU_IP:
 $DPU_PW $SSH admin@$DPU_IP "docker load -i p4runtime-sh.tar"
+
 echo ""
 
+#
+# Installing all contents to PTF
+#
 echo "Installing all scripts to PTF..."
 docker cp ptf_scripts/* ptf_vms6-1:/root/
 echo ""
 
+#
+# Installing all contents to mgmt container
+#
 echo "Pushing dash api to mgmt container"
 docker cp $DEMO_BUILDIMAGE_DIR/target/debs/bookworm/libdashapi_1.0.0_amd64.deb mgmt:/home/$USER/
 echo "Installing dash api to mgmt container"
 docker exec mgmt sudo dpkg -i /home/$USER/libdashapi_1.0.0_amd64.deb
 echo ""
 
+#
+# Installing all contents to host machine
+#
 echo "Installing SONiC gnmi agent docker image..."
 docker load -i $DEMO_SONICVPP_DIR/docker-sonic-gnmi-agent.gz
 echo ""
