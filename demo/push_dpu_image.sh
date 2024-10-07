ac9c810b7303337123a178cd15e62d5f4d81fd71
r12f <r12f.code@gmail.com>
2024-10-07 03:58:32 +0000

    Push DPU image and container scripts.

--- demo/push_dpu_image.sh
+++ demo/push_dpu_image.sh
@@ -3,19 +3,6 @@
 source ./demo_env.sh
 
 echo "Pushing DASH container to DPU ..."
-$DPU_PW $SSH admin@$DPU_IP "rm -f docker-dash-engine.gz"
-$DPU_PW $SCP $DEMO_BUILDIMAGE_DIR/target/docker-dash-engine.gz admin@$DPU_IP:
+$DPU_PW $SSH admin@$DPU_IP "rm -f sonic-vs.bin"
+$DPU_PW $SCP $DEMO_BUILDIMAGE_DIR/target/sonic-vs.bin admin@$DPU_IP:
 echo ""
-
-echo "Stopping DASH engine ..."
-$DPU_PW $SSH admin@$DPU_IP "sudo systemctl stop dash-engine"
-$DPU_PW $SSH admin@$DPU_IP "docker rm dash_engine"
-echo ""
-
-echo "Loading new DASH engine ..."
-$DPU_PW $SSH admin@$DPU_IP "docker load -i docker-dash-engine.gz"
-$DPU_PW $SSH admin@$DPU_IP "sudo systemctl start dash-engine"
-echo ""
-
-echo "Reload config ..."
-$DPU_PW $SSH admin@$DPU_IP "sudo config reload -y"
