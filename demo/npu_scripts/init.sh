71cb5bb13742b9ce82d3e51c10a176286a3a22a2
r12f <r12f.code@gmail.com>
2024-10-07 04:37:15 +0000

    Update NPU side init.sh.

--- demo/npu_scripts/init.sh
+++ demo/npu_scripts/init.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Setup gNMI certs
 echo "Create TLS certificate for gNMI server..."
 sudo mkdir /etc/sonic/tls
 if [ ! -f /etc/sonic/tls/server.key ]; then
     sudo openssl req -x509 -newkey rsa:4096 -keyout /etc/sonic/tls/server.key -out /etc/sonic/tls/server.crt -days 365 -nodes -subj "/C=US/ST=California/L=San-Jose/O=Cisco"
 fi
-cat /etc/sonic/config_db.json | jq '. + { GNMI: { certs: { server_crt: "/etc/sonic/tls/server.crt", server_key: "/etc/sonic/tls/server.key" } } }' > config_db.json.tmp
-sudo cp config_db.json.tmp /etc/sonic/config_db.json
+
+sonic-db-cli CONFIG_DB hset "DEVICE_METADATA|localhost" subtype SmartSwitch
+sonic-db-cli CONFIG_DB hset 'GNMI|certs' server_crt /etc/sonic/tls/server.crt
+sonic-db-cli CONFIG_DB hset 'GNMI|certs' server_key /etc/sonic/tls/server.key
+sudo config save -y
+
+docker exec gnmi sed -i 's|if \[ ! -z $CA_CRT \]; then|if [ -n "$CA_CRT" ] \&\& [ "$CA_CRT" != "null" ]; then|' /usr/bin/gnmi-native.sh
+docker exec swss sed -i 's/tcp:\/\/${mgmt_ip}:8100/tcp:\/\/0.0.0.0:8100/g' /usr/bin/orchagent.sh
 
 # Reload config and gNMI server
 sudo config reload -y
