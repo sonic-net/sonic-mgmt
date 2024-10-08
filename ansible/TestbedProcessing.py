31c209a8d7ca1b43e500b36f36df1cb756647328
vdahiya12 <67608553+vdahiya12@users.noreply.github.com>
2024-10-07 23:56:07 -0700

    [ansible][minigraph] add support for adding autonegotiation in minigraph templates and fanout EOS (#13990)
    These changes add a support for adding auto negotiation to specific testbed based on variables defined in ansible
    
    Two things are required for autoneg support
    
    1. autoneg_enabled : True
    must be enabled in inventory files
    2. t0 topo file must have a port list for autoneg
    Example:
    intfs : [1, 2, 3, 4, 5, 6, 7, 8]
    With these changes the PR includes changes to pick the port numbering from topo file and apply minigraph parsing changes such that during deploy-mg or gen-mg the required ports have autoneg and deployment is clean with link up
    
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- ansible/TestbedProcessing.py
+++ ansible/TestbedProcessing.py
@@ -311,7 +311,7 @@ error handling: checks if attribute values are None type or string "None"
 
 
 def makeSonicLabLinks(data, outfile):
-    csv_columns = "StartDevice,StartPort,EndDevice,EndPort,BandWidth,VlanID,VlanMode,SlotId"
+    csv_columns = "StartDevice,StartPort,EndDevice,EndPort,BandWidth,VlanID,VlanMode,AutoNeg,SlotId"
     topology = data
     csv_file = outfile
 
@@ -331,6 +331,7 @@ def makeSonicLabLinks(data, outfile):
                     bandWidth = element.get("Bandwidth")
                     vlanID = element.get("VlanID")
                     vlanMode = element.get("VlanMode")
+                    AutoNeg = element.get("AutoNeg")
                     slotId = element.get("SlotId")
 
                     # catch empty values
@@ -346,10 +347,13 @@ def makeSonicLabLinks(data, outfile):
                         vlanMode = ""
                     if not slotId:
                         slotId = ""
+                    if not AutoNeg:
+                        AutoNeg = ""
 
                     row = startDevice + "," + startPort + "," + endDevice + "," + \
                         endPort + "," + str(bandWidth) + \
                         "," + str(vlanID) + "," + vlanMode + \
+                        "," + str(AutoNeg) + \
                         "," + str(slotId)
                     f.write(row + "\n")
     except IOError:
