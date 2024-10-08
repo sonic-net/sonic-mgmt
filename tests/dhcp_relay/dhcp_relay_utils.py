f80227cd14f05c893b77917a849379d644731adb
Yaqiang Zhu <yaqiangzhu@microsoft.com>
2024-10-08 15:24:07 +0800

    [dhcp_relay] Add delay in checking dhcp_relay status (#14739)
    What is the motivation for this PR?
    In some scenario, checking process status would happen before dhcp_relay starting. This PR is to fix it.
    
    How did you do it?
    Add delay and modify check logic
    
    How did you verify/test it?
    PR test
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/dhcp_relay/dhcp_relay_utils.py
+++ tests/dhcp_relay/dhcp_relay_utils.py
@@ -39,7 +39,7 @@ def restart_dhcp_service(duthost):
     def _is_dhcp_relay_ready():
         output = duthost.shell('docker exec dhcp_relay supervisorctl status | grep dhcp | awk \'{print $2}\'',
                                module_ignore_errors=True)
-        return (not output['rc'] and output['stderr'] == '' and
+        return (not output['rc'] and output['stderr'] == '' and len(output['stdout_lines']) != 0 and
                 all(element == 'RUNNING' for element in output['stdout_lines']))
 
-    pytest_assert(wait_until(60, 1, 0, _is_dhcp_relay_ready), "dhcp_relay is not ready after restarting")
+    pytest_assert(wait_until(60, 1, 10, _is_dhcp_relay_ready), "dhcp_relay is not ready after restarting")
