3a6b5e797e152318e7aa8d74843f55de9ea8f043
Abhishek Dosi <abdosi@microsoft.com>
2024-10-08 20:56:45 +0000

    Fix pre test failure
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- tests/bgp/test_bgp_azng_migration.py
+++ tests/bgp/test_bgp_azng_migration.py
@@ -10,18 +10,19 @@ pytestmark = [
     pytest.mark.topology('t2')
 ]
 
+
 def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
 
     duthost = duthosts[enum_upstream_dut_hostname]
-    
+
     if get_image_type(duthost) == "public":
         pytest.skip("AZNG Migration is not supported on public image")
 
-    config_facts  = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
+    config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
 
     peer_device_namespace = DEFAULT_NAMESPACE
     peer_device_ip_set = set()
-    
+
     for peer_device, peer_device_info in config_facts['DEVICE_NEIGHBOR_METADATA'].items():
         if peer_device_info['type'] == "AZNGHub":
             for peer_device_ip, peer_device_bgp_data in config_facts['BGP_NEIGHBOR'].items():
@@ -43,26 +44,26 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
     bgp_fact_info = asichost.bgp_facts()
 
     for ip in peer_device_ip_set:
-       assert bgp_fact_info['ansible_facts']['bgp_neighbors'][ip]['state'] == 'established' 
+        assert bgp_fact_info['ansible_facts']['bgp_neighbors'][ip]['state'] == 'established'
 
     adv_cmd_list = []
     recv_cmd_list = []
-    
+
     for ip in peer_device_ip_set:
         if ipaddress.IPNetwork(ip).version == 4:
             bgp_nbr_recv_cmd = "sudo vtysh -c 'show ip bgp neighbors {} received-routes json'".format(
                 ip)
             recv_cmd_list.append(bgp_nbr_recv_cmd)
-            
+
             bgp_nbr_advertise_cmd = "sudo vtysh -c 'show ip bgp neighbors {} advertised-routes json'".format(
                 ip)
-            
+
             adv_cmd_list.append(bgp_nbr_advertise_cmd)
         else:
             bgp_nbr_recv_cmd = "sudo vtysh -c 'show bgp ipv6 neighbors {} received-routes json'".format(
                 ip)
             recv_cmd_list.append(bgp_nbr_recv_cmd)
-            
+
             bgp_nbr_advertise_cmd = "sudo vtysh -c 'show bgp ipv6 neighbors {} advertised-routes json'".format(
                 ip)
             adv_cmd_list.append(bgp_nbr_advertise_cmd)
@@ -85,37 +86,36 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
 
             if "ipv6" in bgp_nbr_recv_cmd:
                 original_ipv6_route_recv_count = routes_json['totalPrefixCounter']
-                original_ipv6_route_recv_filter_count =  routes_json['filteredPrefixCounter']
+                original_ipv6_route_recv_filter_count = routes_json['filteredPrefixCounter']
             else:
                 original_ipv4_route_recv_count = routes_json['totalPrefixCounter']
-                original_ipv4_route_recv_filter_count =  routes_json['filteredPrefixCounter']
+                original_ipv4_route_recv_filter_count = routes_json['filteredPrefixCounter']
 
             assert routes_json['totalPrefixCounter'] > 0
             assert routes_json['filteredPrefixCounter'] < routes_json['totalPrefixCounter']
-                
+
         for bgp_nbr_adv_cmd in adv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_adv_cmd, peer_device_namespace))
             routes_json = json.loads(res['stdout'])
 
             if "ipv6" in bgp_nbr_adv_cmd:
                 original_ipv6_route_adv_count = routes_json['totalPrefixCounter']
-                original_ipv6_route_adv_filter_count =  routes_json['filteredPrefixCounter']
+                original_ipv6_route_adv_filter_count = routes_json['filteredPrefixCounter']
             else:
                 original_ipv4_route_adv_count = routes_json['totalPrefixCounter']
-                original_ipv4_route_adv_filter_count =  routes_json['filteredPrefixCounter']
+                original_ipv4_route_adv_filter_count = routes_json['filteredPrefixCounter']
 
             assert routes_json['totalPrefixCounter'] > 0
             assert routes_json['filteredPrefixCounter'] == 0
 
         rc = duthost.shell('sudo azng_migration -r')
-        pytest_assert(rc['failed'] == False, "AZNG Migration Rollback failed")
+        pytest_assert(not rc['failed'], "AZNG Migration Rollback failed")
 
         for bgp_nbr_recv_cmd in recv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_recv_cmd, peer_device_namespace))
             routes_json = json.loads(res['stdout'])
             assert routes_json['totalPrefixCounter'] > 0
             assert routes_json['filteredPrefixCounter'] == routes_json['totalPrefixCounter']
-                
 
         for bgp_nbr_adv_cmd in adv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_adv_cmd, peer_device_namespace))
@@ -124,8 +124,8 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
             assert routes_json['filteredPrefixCounter'] == 0
 
         rc = duthost.shell('sudo azng_migration -d')
-        
-        pytest_assert(rc['failed'] == False, "AZNG Migration Deny Route-map apply failed")
+
+        pytest_assert(not rc['failed'], "AZNG Migration Deny Route-map apply failed")
 
         for bgp_nbr_recv_cmd in recv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_recv_cmd, peer_device_namespace))
@@ -140,7 +140,7 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
             assert routes_json['filteredPrefixCounter'] == routes_json['totalPrefixCounter']
 
         rc = duthost.shell('sudo azng_migration -i')
-        pytest_assert(rc['failed'] == False, "AZNG Migration Outbound Route-map permit apply failed")
+        pytest_assert(not rc['failed'], "AZNG Migration Outbound Route-map permit apply failed")
 
         for bgp_nbr_recv_cmd in recv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_recv_cmd, peer_device_namespace))
@@ -155,14 +155,14 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
             assert routes_json['filteredPrefixCounter'] == 0
 
             if "ipv6" in bgp_nbr_adv_cmd:
-                assert  routes_json['totalPrefixCounter'] == original_ipv6_route_adv_count
-                assert  routes_json['filteredPrefixCounter'] == original_ipv6_route_adv_filter_count
+                assert routes_json['totalPrefixCounter'] == original_ipv6_route_adv_count
+                assert routes_json['filteredPrefixCounter'] == original_ipv6_route_adv_filter_count
             else:
-                assert  routes_json['totalPrefixCounter'] == original_ipv4_route_adv_count
-                assert  routes_json['filteredPrefixCounter'] == original_ipv4_route_adv_filter_count
+                assert routes_json['totalPrefixCounter'] == original_ipv4_route_adv_count
+                assert routes_json['filteredPrefixCounter'] == original_ipv4_route_adv_filter_count
 
         rc = duthost.shell('sudo azng_migration -o')
-        pytest_assert(rc['failed'] == False, "AZNG Migration Inbound Route-map permit apply failed")
+        pytest_assert(not rc['failed'], "AZNG Migration Inbound Route-map permit apply failed")
 
         for bgp_nbr_recv_cmd in recv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_recv_cmd, peer_device_namespace))
@@ -171,24 +171,24 @@ def test_bgp_azng_migration(duthosts, enum_upstream_dut_hostname):
             assert routes_json['filteredPrefixCounter'] < routes_json['totalPrefixCounter']
 
             if "ipv6" in bgp_nbr_recv_cmd:
-                assert  routes_json['totalPrefixCounter'] == original_ipv6_route_recv_count
-                assert  routes_json['filteredPrefixCounter'] == original_ipv6_route_recv_filter_count
+                assert routes_json['totalPrefixCounter'] == original_ipv6_route_recv_count
+                assert routes_json['filteredPrefixCounter'] == original_ipv6_route_recv_filter_count
             else:
-                assert  routes_json['totalPrefixCounter'] == original_ipv4_route_recv_count
-                assert  routes_json['filteredPrefixCounter'] == original_ipv4_route_recv_filter_count
+                assert routes_json['totalPrefixCounter'] == original_ipv4_route_recv_count
+                assert routes_json['filteredPrefixCounter'] == original_ipv4_route_recv_filter_count
 
         for bgp_nbr_adv_cmd in adv_cmd_list:
             res = duthost.shell(duthost.get_vtysh_cmd_for_namespace(bgp_nbr_adv_cmd, peer_device_namespace))
             routes_json = json.loads(res['stdout'])
             assert routes_json['totalPrefixCounter'] > 0
             assert routes_json['filteredPrefixCounter'] == 0
-        
+
         rc = duthost.shell('sudo azng_migration -p')
-        if rc['failed'] == True:
+        if rc['failed']:
             recover_via_minigraph = True
 
-        pytest_assert(recover_via_minigraph == False, "AZNG Migration Production set failed")
-        success = True 
+        pytest_assert(not recover_via_minigraph, "AZNG Migration Production set failed")
+        success = True
     finally:
         if not success:
             if recover_via_minigraph:
