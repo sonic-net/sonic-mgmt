1ca924e372cf9d2894bed69ce9ebe7328a46b806
Chris <156943338+ccroy-arista@users.noreply.github.com>
2024-10-10 00:47:12 -0700

    sonic-mgmt: Initial support for Arista-7060X6-64PE-C256S2 (#14835)
    What is the motivation for this PR?
    Support for Arista-7060X6-64PE-C256S2 HwSku.
    
    How did you do it?
    Added port configuration and th5 asic entry for Arista-7060X6-64PE-C256S2.
    
    How did you verify/test it?
    Verified that a testbed can be initialized under a t0 topo and sonic-mgmt tests can be run.
    
    Any platform specific information?
    Specific to the Arista-7060X6-64PE-C256S2 HwSku.

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -124,6 +124,12 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                     port_alias_to_name_map["Ethernet%d/%d" % (i, j)] = "Ethernet%d" % ((i - 1) * 8 + j - 1)
             port_alias_to_name_map["Ethernet65"] = "Ethernet512"
             port_alias_to_name_map["Ethernet66"] = "Ethernet513"
+        elif hwsku == "Arista-7060X6-64PE-C256S2":
+            for i in range(1, 65, 2):  # This hwsku uses every second OSFP port.
+                for j in range(1, 9):
+                    port_alias_to_name_map["Ethernet%d/%d" % (i, j)] = "Ethernet%d" % ((i - 1) * 8 + j - 1)
+            port_alias_to_name_map["Ethernet65"] = "Ethernet512"
+            port_alias_to_name_map["Ethernet66"] = "Ethernet513"
         elif hwsku == "Arista-7050QX32S-Q32":
             for i in range(5, 29):
                 port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 5) * 4)
