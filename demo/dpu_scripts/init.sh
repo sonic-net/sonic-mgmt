3317558444d7e960c0bed94be9d149d291ecb7d8
r12f <r12f.code@gmail.com>
2024-10-11 07:01:02 +0000

    Fix DPU init scripts.

--- demo/dpu_scripts/init.sh
+++ demo/dpu_scripts/init.sh
@@ -5,10 +5,10 @@ sudo  mkdir /etc/sonic/tls
 sudo openssl req -x509 -newkey rsa:4096 -keyout /etc/sonic/tls/server.key -out /etc/sonic/tls/server.crt -days 365 -nodes -subj "/C=US/ST=California/L=San-Jose/O=Cisco"
 
 echo "Updating config-db ..."
+sonic-db-cli CONFIG_DB hset 'DEVICE_METADATA|localhost' switch_type dpu
 sonic-db-cli CONFIG_DB hset "DEVICE_METADATA|localhost" subtype SmartSwitch
 sonic-db-cli CONFIG_DB hset 'GNMI|certs' server_crt /etc/sonic/tls/server.crt
 sonic-db-cli CONFIG_DB hset 'GNMI|certs' server_key /etc/sonic/tls/server.key
-sonic-db-cli CONFIG_DB hset 'DEVICE_METADATA|localhost' switch_type dpu
 sudo config save -y
 
 echo "Switch syncd to DASH ..."
@@ -24,7 +24,7 @@ docker exec gnmi sed -i 's|if \[ ! -z $CA_CRT \]; then|if [ -n "$CA_CRT" ] \&\& 
 docker commit gnmi docker-sonic-gnmi:latest
 /usr/bin/gnmi.sh stop
 /usr/bin/gnmi.sh start
-docker exec swss sed -i 's/tcp:\/\/${mgmt_ip}:8100/tcp:\/\/0.0.0.0:8100/g' /usr/bin/orchagent.sh
+docker exec swss sed -i 's/tcp:\/\/\${mgmt_ip}:8100/tcp:\/\/0.0.0.0:8100/g' /usr/bin/orchagent.sh
 docker commit swss docker-orchagent:latest
 /usr/bin/swss.sh stop
 /usr/bin/swss.sh start
