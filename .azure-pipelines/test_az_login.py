912c2f6beb17449303fc657718517e3bb3d94aef
chunangli <chunang_li@163.com>
2024-10-18 10:36:55 +0800

    debug
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- .azure-pipelines/test_az_login.py
+++ .azure-pipelines/test_az_login.py
@@ -4,11 +4,7 @@ if __name__ == '__main__':
     # Success of "az account get-access-token" depends on "az login". However, the "az login" session may expire
     # after 24 hours. So we re-login every 12 hours to ensure success of "az account get-access-token".
 
-    cmd = "az login --service-principal -u {} --tenant {} --allow-no-subscriptions --federated-token {}".format(
-        os.environ.get("SONIC_AUTOMATION_SERVICE_PRINCIPAL"),
-        os.environ.get("ELASTICTEST_MSAL_TENANT_ID"),
-        os.environ.get("SYSTEM_ACCESS_TOKEN")
-    )
+    cmd = "az login --identity --username e41c8a84-4662-48b0-b8a6-3f52230bcc5e"
 
     print(os.system(cmd))
 
