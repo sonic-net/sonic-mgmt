4de623d1b627428840b62ec1a0d91a87b377c91f
chunangli <chunang_li@163.com>
2024-10-21 11:26:22 +0800

    debug
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- .azure-pipelines/test_az_login.py
+++ .azure-pipelines/test_az_login.py
@@ -4,10 +4,14 @@ if __name__ == '__main__':
     # Success of "az account get-access-token" depends on "az login". However, the "az login" session may expire
     # after 24 hours. So we re-login every 12 hours to ensure success of "az account get-access-token".
 
-    cmd = "az login --identity --username e41c8a84-4662-48b0-b8a6-3f52230bcc5e"
+    cmd1 = "az login"
 
-    print(os.system(cmd))
+    print(os.system(cmd1))
 
-    cmd = 'az account get-access-token --resource {}'.format(os.environ.get("ELASTICTEST_MSAL_CLIENT_ID"))
+    cmd2 = "az login --identity --username e41c8a84-4662-48b0-b8a6-3f52230bcc5e"
 
-    print(os.system(cmd))
+    print(os.system(cmd2))
+
+    cmd3 = 'az account get-access-token --resource {}'.format(os.environ.get("ELASTICTEST_MSAL_CLIENT_ID"))
+
+    print(os.system(cmd3))
