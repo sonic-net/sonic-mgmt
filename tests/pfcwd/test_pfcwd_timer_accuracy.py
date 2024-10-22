601805989e65a38a63e91ec48e3b9bfd05a8452f
Austin Pham <austinpham@microsoft.com>
2024-10-23 00:23:57 +1100

    Revert "chore: fix PR review"
    This reverts commit 0aca11159b2c3b31d8bc14412fdc56883feeb7a4.

--- tests/pfcwd/test_pfcwd_timer_accuracy.py
+++ tests/pfcwd/test_pfcwd_timer_accuracy.py
@@ -28,7 +28,7 @@ def pfc_queue_idx(pfcwd_timer_setup_restore):
     yield pfcwd_timer_setup_restore['storm_handle'].pfc_queue_idx
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='class')
 def stop_pfcwd(duthosts, enum_rand_one_per_hwsku_frontend_hostname):
     """
     Fixture that stops PFC Watchdog before each test run
@@ -67,7 +67,7 @@ def ignore_loganalyzer_exceptions(enum_rand_one_per_hwsku_frontend_hostname, log
     yield
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='class', autouse=True)
 def pfcwd_timer_setup_restore(setup_pfc_test, enum_fanout_graph_facts, duthosts,        # noqa F811
                               enum_rand_one_per_hwsku_frontend_hostname, fanouthosts, stop_pfcwd):
     """
