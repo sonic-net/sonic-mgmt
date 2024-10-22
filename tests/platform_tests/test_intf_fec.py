7e8535db5cab87adc78d1f5c9b56d4594a452d63
Vasundhara Volam <vvolam@microsoft.com>
2024-10-22 16:57:55 +0000

    Increase the delay to 7 secs after FEC configuration

--- tests/platform_tests/test_intf_fec.py
+++ tests/platform_tests/test_intf_fec.py
@@ -80,7 +80,7 @@ def test_config_fec_oper_mode(duthosts, enum_rand_one_per_hwsku_frontend_hostnam
         config_status = duthost.command("sudo config interface fec {} rs"
                                         .format(intf['interface']))
         if config_status:
-            duthost.command("sleep 2")
+            duthost.command("sleep 7")
             # Verify the FEC operational mode is restored
             logging.info("Get output of '{} {}'".format("show interfaces fec status", intf['interface']))
             fec_status = duthost.show_and_parse("show interfaces fec status {}".format(intf['interface']))
