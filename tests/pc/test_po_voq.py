bc25189437ebe30d141ecc4a1252eb81bcf85f2e
Javier-Tan <47554099+Javier-Tan@users.noreply.github.com>
2024-10-24 01:54:05 +0000

    [pc/test_po_voq.py]: Add testcase for LAG member flap DB sync
    * Add testcase test_voq_po_member_down_update to test bringing down LAG member is synced successfully across all DBs
    
    Signed-off-by: Javier Tan javiertan@microsoft.com

--- tests/pc/test_po_voq.py
+++ tests/pc/test_po_voq.py
@@ -1,6 +1,8 @@
 import pytest
+import random
 import tests.common.helpers.voq_lag as voq_lag
 from tests.common.helpers.voq_helpers import verify_no_routes_from_nexthop
+from tests.common.platform.device_utils import fanout_switch_port_lookup
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import wait_until
 import logging
@@ -170,3 +172,65 @@ def test_voq_po_down_via_cli_update(duthosts, enum_rand_one_per_hwsku_frontend_h
         duthost.shell("config interface {} startup {}".format(asic.cli_ns_option, voq_lag.TMP_PC))
         pytest_assert(wait_until(30, 5, 0, lambda: duthost.check_intf_link_state(voq_lag.TMP_PC)),
                       "{} is not enabled".format(voq_lag.TMP_PC))
+
+
+@pytest.mark.parametrize("flap_method", ["local", "remote"])
+def test_voq_po_member_down_update(duthosts, enum_rand_one_per_hwsku_frontend_hostname, setup_teardown, fanouthosts, flap_method):
+    """
+    Test to verify when a LAG member goes down on an ASIC, it is synced across all DBs
+
+    All DBs = local app db, chassis app db, local & remote asic db
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    remote_duthosts = [dut_host for dut_host in duthosts.frontend_nodes if dut_host != duthost]
+    asic, portchannel_ip, portchannel_members = setup_teardown
+    tmp_lag_id = voq_lag.get_lag_id_from_chassis_db(duthosts)
+
+    # Make sure LAG is up across all DBs (all PC members are up across all DBs)
+    for portchannel_member in portchannel_members:
+        voq_lag.verify_lag_member_status_in_app_db(asic, portchannel_member, enabled=True)
+        voq_lag.verify_lag_member_status_in_chassis_db(duthosts, portchannel_member, enabled=True)
+    # For checking LAG member status in ASIC_DB,
+    # we check how many members are disabled in a LAG since we can't identify individual members
+    voq_lag.verify_lag_member_status_in_asic_db(duthost.asics, tmp_lag_id, exp_disabled=0)
+    for remote_duthost in remote_duthosts:
+        voq_lag.verify_lag_member_status_in_asic_db(remote_duthost.asics, tmp_lag_id, exp_disabled=0)
+
+    # Choose a random LAG member to bring down, check that LAG member down is synced across all DBs
+    down_pc_member = random.choice(portchannel_members)
+    fanout, fanout_port = fanout_switch_port_lookup(fanouthosts, duthost.hostname, down_pc_member)
+    up_pc_members = [pc_member for pc_member in portchannel_members if pc_member != down_pc_member]
+    try:
+        if flap_method == "local":
+            logging.info("Disabling {} via CLI".format(down_pc_member))
+            duthost.shell("config interface {} shutdown {}".format(asic.cli_ns_option, down_pc_member))
+        else:
+            logging.info("Disabling {} via fanout to simulate external flapping".format(down_pc_member))
+            logging.info("Disabling {} on {}".format(fanout_port, fanout.hostname))
+            fanout.shell("config interface shutdown {}".format(fanout_port))
+
+        pytest_assert(wait_until(30, 5, 0, lambda: not duthost.check_intf_link_state(down_pc_member)),
+                      "{} is not disabled".format(down_pc_member))
+
+        # Verify other LAG members are still up
+        for up_pc_member in up_pc_members:
+            voq_lag.verify_lag_member_status_in_app_db(asic, up_pc_member, enabled=True)
+            voq_lag.verify_lag_member_status_in_chassis_db(duthosts, up_pc_member, enabled=True)
+
+        voq_lag.verify_lag_member_status_in_app_db(asic, down_pc_member, enabled=False)
+        voq_lag.verify_lag_member_status_in_chassis_db(duthosts, down_pc_member, enabled=False)
+        voq_lag.verify_lag_member_status_in_asic_db(duthost.asics, tmp_lag_id, exp_disabled=1)
+        for remote_duthost in remote_duthosts:
+            voq_lag.verify_lag_member_status_in_asic_db(remote_duthost.asics, tmp_lag_id, exp_disabled=1)
+    finally:
+        # Bring LAG member back up
+        if flap_method == "local":
+            logging.info("Enabling {} via CLI".format(down_pc_member))
+            duthost.shell("config interface {} startup {}".format(asic.cli_ns_option, down_pc_member))
+        else:
+            logging.info("Enabling {} via fanout".format(down_pc_member))
+            logging.info("Enabling {} on {}".format(fanout_port, fanout.hostname))
+            fanout.shell("config interface startup {}".format(fanout_port))
+
+        pytest_assert(wait_until(30, 5, 0, lambda: duthost.check_intf_link_state(down_pc_member)),
+                      "{} is not enabled".format(down_pc_member))
