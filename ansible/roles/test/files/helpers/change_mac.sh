138e0ee8a1621ab27353be5659c72d1e5b2f987e
Christopher Croy <ccroy@arista.com>
2024-10-25 22:57:08 +0000

    sonic-mgmt: fix t0-isolated-d128u128s2.yml typo
    The field 'bp_interfaces' in the
    t0-isolated-d128u128s2 yml file should be
    'bp_interface' i.e. w/o the 's'.

--- ansible/roles/test/files/helpers/change_mac.sh
+++ ansible/roles/test/files/helpers/change_mac.sh
@@ -7,7 +7,7 @@ INTF_LIST=$(ls /sys/class/net | grep -E "^eth[0-9]+$")
 for INTF in ${INTF_LIST}; do
     ADDR="$(cat /sys/class/net/${INTF}/address)"
     PREFIX="$(cut -c1-15 <<< ${ADDR})"
-    SUFFIX="$(printf "%02x" ${INTF##eth})"
+    SUFFIX="$(printf "%02x" $((${INTF##eth}%256)))"
     MAC="${PREFIX}${SUFFIX}"
 
     echo "Update ${INTF} MAC address: ${ADDR}->$MAC"
