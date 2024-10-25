d76cf8fb1aac22b40242d0b0d089147f81157fc1
Judy Joseph <jujoseph@microsoft.com>
2024-10-25 02:21:33 +0000

    Add new scale test test_scale_rekey

--- tests/macsec/test_deployment.py
+++ tests/macsec/test_deployment.py
@@ -13,6 +13,8 @@ pytestmark = [
 
 
 class TestDeployment():
+    MKA_TIMEOUT = 6
+
     @pytest.mark.disable_loganalyzer
     def test_config_reload(self, duthost, ctrl_links, policy, cipher_suite, send_sci, wait_mka_establish):
         # Save the original config file
@@ -23,3 +25,46 @@ class TestDeployment():
         assert wait_until(300, 6, 12, check_appl_db, duthost, ctrl_links, policy, cipher_suite, send_sci)
         # Recover the original config file
         duthost.shell("sudo mv /tmp/config_db*.json /etc/sonic")
+
+
+    @pytest.mark.disable_loganalyzer
+    def test_scale_rekey(self, duthost, ctrl_links, rekey_period, wait_mka_establish):
+        dut_egress_sa_table_orig = {}
+        dut_ingress_sa_table_orig = {}
+        dut_egress_sa_table_current = {}
+        dut_ingress_sa_table_current = {}
+        new_dut_egress_sa_table = {}
+        new_dut_ingress_sa_table = {}
+
+        # Shut the interface and wait for all macsec sessions to be down
+        for dut_port, nbr in ctrl_links.items():
+            _, _, _, dut_egress_sa_table_orig[dut_port], dut_ingress_sa_table_orig[dut_port]= get_appl_db(
+                duthost, dut_port, nbr["host"], nbr["port"])
+            intf_asic = duthost.get_port_asic_instance(dut_port)
+            duthost.shell("config interface {} shutdown {}".format(intf_asic.cli_ns_option, dut_port))
+
+        sleep(TestDeployment.MKA_TIMEOUT)
+
+        #Unshut the interfaces so that macsec sessions come back up
+        for dut_port, nbr in ctrl_links.items():
+            intf_asic = duthost.get_port_asic_instance(dut_port)
+            duthost.shell("config interface {} startup {}".format(intf_asic.cli_ns_option, dut_port))
+
+        for dut_port, nbr in ctrl_links.items():
+            def check_new_mka_session():
+                _, _, _, dut_egress_sa_table_current[dut_port], dut_ingress_sa_table_current[dut_port] = get_appl_db(
+                    duthost, dut_port, nbr["host"], nbr["port"])
+                assert dut_egress_sa_table_orig[dut_port] != dut_egress_sa_table_current[dut_port]
+                assert dut_ingress_sa_table_orig[dut_port] != dut_ingress_sa_table_current[dut_port]
+                return True
+            assert wait_until(30, 2, 2, check_new_mka_session)
+
+
+        # Wait for rekey and make sure all sessions are present
+        sleep(rekey_period)
+
+        for dut_port, nbr in ctrl_links.items():
+            _, _, _, new_dut_egress_sa_table[dut_port], new_dut_ingress_sa_table[dut_port]= get_appl_db(
+                duthost, dut_port, nbr["host"], nbr["port"])
+            assert dut_egress_sa_table_current[dut_port] != new_dut_egress_sa_table[dut_port]
+            assert dut_ingress_sa_table_current[dut_port] != new_dut_ingress_sa_table[dut_port]
