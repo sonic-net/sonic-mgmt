809f7cc4875e17921a31f63c5c59e2127212fa45
Sai Kiran <110003254+opcoder0@users.noreply.github.com>
2024-10-25 11:45:17 +1100

    [dualtor] Add test to simulate server reboot (#14690)
    Add test to simulate server reboot via restart of icmp_responder and verify that the TORs get back to healthy status after the restart is complete.

--- tests/dualtor_mgmt/test_server_failure.py
+++ tests/dualtor_mgmt/test_server_failure.py
@@ -3,7 +3,8 @@ import pytest
 import random
 
 from tests.common.dualtor.mux_simulator_control import toggle_simulator_port_to_upper_tor, \
-                                                       simulator_flap_counter, simulator_server_down    # noqa F401
+                                                       simulator_flap_counter, simulator_server_down, \
+                                                       toggle_all_simulator_ports                       # noqa F401
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.dualtor.dual_tor_utils import show_muxcable_status                                    # noqa: F401
 from tests.common.dualtor.dual_tor_common import active_active_ports                                    # noqa F401
@@ -13,11 +14,17 @@ from tests.common.dualtor.dual_tor_common import CableType
 from tests.common.dualtor.dual_tor_utils import validate_active_active_dualtor_setup                    # noqa F401
 from tests.common.dualtor.dual_tor_utils import upper_tor_host                                          # noqa F401
 from tests.common.dualtor.dual_tor_utils import lower_tor_host                                          # noqa F401
+from tests.common.dualtor.dual_tor_utils import lower_tor_fanouthosts, fanout_lower_tor_port_control    # noqa F401
+from tests.common.dualtor.dual_tor_utils import upper_tor_fanouthosts, fanout_upper_tor_port_control    # noqa F401
 from tests.common.dualtor.nic_simulator_control import simulator_server_down_active_active              # noqa F401
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses, run_garp_service, \
                                                 run_icmp_responder                                      # noqa: F401
 from tests.common.utilities import wait_until
-
+from tests.common.dualtor.icmp_responder_control import shutdown_icmp_responder                         # noqa: F401
+from tests.common.dualtor.icmp_responder_control import start_icmp_responder                            # noqa: F401
+from tests.common.dualtor.control_plane_utils import verify_tor_states
+from tests.common.platform.interface_utils import expect_interface_status
+from tests.common.dualtor.constants import UPPER_TOR
 
 pytestmark = [
     pytest.mark.topology('dualtor'),
@@ -90,3 +97,90 @@ def test_server_down(cable_type, duthosts, tbinfo, active_active_ports, active_s
                       "mux_cable status is unexpected. Should be (standby, unhealthy)")
         pytest_assert(wait_until(30, 1, 0, lower_tor_mux_state_verfication, 'standby', 'unhealthy'),
                       "mux_cable status is unexpected. Should be (standby, unhealthy)")
+
+
+@pytest.mark.enable_active_active
+def test_server_reboot(request, cable_type, tbinfo,                                # noqa: F811
+                       start_icmp_responder, shutdown_icmp_responder,              # noqa: F811
+                       active_standby_ports, active_active_ports,                  # noqa: F811
+                       upper_tor_host, lower_tor_host,                             # noqa: F811
+                       toggle_all_simulator_ports,                                 # noqa: F811
+                       fanout_upper_tor_port_control,                              # noqa: F811
+                       fanout_lower_tor_port_control):                             # noqa: F811
+
+    """
+    Test verifies that TOR health returns back to healthy status after a server reboot.
+    """
+    if cable_type == CableType.active_standby:
+        interface_name = random.choice(active_standby_ports)
+        # Set upper_tor as active
+        toggle_all_simulator_ports(UPPER_TOR)
+        verify_tor_states(expected_active_host=upper_tor_host,
+                          expected_standby_host=lower_tor_host, cable_type=cable_type)
+
+        pytest_assert(expect_interface_status(upper_tor_host, interface_name, 'up'),
+                      f'{interface_name} on upper ToR must be up')
+        pytest_assert(expect_interface_status(lower_tor_host, interface_name, 'up'),
+                      f'{interface_name} on lower ToR must be up')
+        shutdown_icmp_responder()
+
+        # simulate server reboot by turning off all fanout ports on both the ToRs
+        shutdown_upper, restart_upper = fanout_upper_tor_port_control
+        shutdown_lower, restart_lower = fanout_lower_tor_port_control
+        shutdown_upper()
+        shutdown_lower()
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, upper_tor_host, interface_name, 'down'),
+                      f'{interface_name} on upper ToR is expected to be down after server shutdown')
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, lower_tor_host, interface_name, 'down'),
+                      f'{interface_name} on lower ToR is expected to be down after server shutdown')
+        restart_upper()
+        restart_lower()
+
+        # fanout ports are back on
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, upper_tor_host, interface_name, 'up'),
+                      f'{interface_name} on upper ToR is expected to be down after server shutdown')
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, lower_tor_host, interface_name, 'up'),
+                      f'{interface_name} on lower ToR is expected to be down after server shutdown')
+
+        start_icmp_responder()
+        # The ToRs must then reconcile to a consistent state
+        # Upper ToR switches to standby and Lower to active.
+        verify_tor_states(expected_active_host=lower_tor_host,
+                          expected_standby_host=upper_tor_host, cable_type=cable_type)
+    elif cable_type == CableType.active_active:
+        interface_name = random.choice(active_active_ports)
+
+        verify_tor_states(expected_active_host=[upper_tor_host, lower_tor_host],
+                          expected_standby_host=None, cable_type=cable_type)
+
+        pytest_assert(expect_interface_status(upper_tor_host, interface_name, 'up'),
+                      f'{interface_name} on upper ToR must be up')
+        pytest_assert(expect_interface_status(lower_tor_host, interface_name, 'up'),
+                      f'{interface_name} on lower ToR must be up')
+        shutdown_icmp_responder()
+
+        verify_tor_states(expected_active_host=None,
+                          expected_standby_host=[upper_tor_host, lower_tor_host],
+                          expected_standby_health='unhealthy')
+
+        # simulate server reboot by turning off all fanout ports on both the ToRs
+        shutdown_upper, restart_upper = fanout_upper_tor_port_control
+        shutdown_lower, restart_lower = fanout_lower_tor_port_control
+        shutdown_upper()
+        shutdown_lower()
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, upper_tor_host, interface_name, 'down'),
+                      f'{interface_name} on upper ToR is expected to be down after server shutdown')
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, lower_tor_host, interface_name, 'down'),
+                      f'{interface_name} on lower ToR is expected to be down after server shutdown')
+        restart_upper()
+        restart_lower()
+
+        # fanout ports are back on
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, upper_tor_host, interface_name, 'up'),
+                      f'{interface_name} on upper ToR is expected to be down after server shutdown')
+        pytest_assert(wait_until(30, 1, 0, expect_interface_status, lower_tor_host, interface_name, 'up'),
+                      f'{interface_name} on lower ToR is expected to be down after server shutdown')
+
+        start_icmp_responder()
+        verify_tor_states(expected_active_host=[upper_tor_host, lower_tor_host],
+                          expected_standby_host=None, cable_type=cable_type)
