32ccb491b81029e9faf1947c5c429414dd77f666
yatishkoul <yatishkoul@gmail.com>
2024-10-25 19:09:39 -0700

    Added new testcase for checking console connections from supervisor to linecards for T2 Chassis

--- tests/common/helpers/console_helper.py
+++ tests/common/helpers/console_helper.py
@@ -1,5 +1,6 @@
 import pytest
 import pexpect
+import re
 
 
 def assert_expect_text(client, text, target_line, timeout_sec=0.1):
@@ -22,3 +23,39 @@ def create_ssh_client(ip, user, pwd):
 def ensure_console_session_up(client, line):
     client.expect_exact('Successful connection to line [{}]'.format(line))
     client.expect_exact('Press ^A ^X to disconnect')
+
+
+def get_target_lines(duthost):
+    """
+    retrieve the indices of online line cards.
+    Returns a list of indices of the line cards that are online.
+    """
+    result = duthost.shell("show chassis module status", module_ignore_errors=True)
+    lines = result['stdout'].splitlines()
+    linecards = []
+    
+    # Pattern to match lines that have a "LINE-CARD" entry and "Online" in the Oper-Status column
+    linecard_pattern = re.compile(r"^\s*(LINE-CARD\d+)\s+.*?\s+\d+\s+Online\s+up\s+\S+")
+    
+    for line in lines:
+        match = linecard_pattern.match(line)
+        if match:
+            linecard_name = match.group(1)
+            index = linecard_name.split("LINE-CARD")[1]
+            linecards.append(index)
+    
+    if not linecards:
+        pytest.fail("No line cards are online.")
+        
+    return linecards   
+
+def handle_pexpect_exceptions(target_line):
+    """Handle pexpect exceptions during console interactions."""
+    try:
+        yield  
+    except pexpect.exceptions.EOF:
+        pytest.fail(f"EOF reached during console interaction for line {target_line}.")
+    except pexpect.exceptions.TIMEOUT:
+        pytest.fail(f"Timeout reached during console interaction for line {target_line}.")
+    except Exception as e:
+        pytest.fail(f"Error occured during console interaction for line {target_line}: {e}") 
