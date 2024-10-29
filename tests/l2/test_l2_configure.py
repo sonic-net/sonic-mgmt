c1aebcdab9262c40aacafc7ecd641186a845e398
hdwhdw <dwhuang9@gmail.com>
2024-10-29 00:02:10 +0000

    Remove upgrade from the test workflow.
    The test will now simply configure L2 and then check config db.
    
    TESTED:
    passed on 202012.66 (image with db_migrator fix)
    failed on 202012.76 (image without fix)
    failed at master head (with fixes, so indicates some other bugs)

--- tests/l2/test_l2_configure.py
+++ tests/l2/test_l2_configure.py
@@ -1,84 +1,99 @@
 """
 Tests related to L2 configuration
 """
+
 import logging
 import pytest
 
-from six.moves.urllib.parse import urlparse
-from tests.common import reboot, config_reload
+from tests.common import config_reload
 from tests.common.platform.processes_utils import wait_critical_processes
 from tests.common.helpers.assertions import pytest_assert
-from tests.common.helpers.upgrade_helpers import install_sonic
 
-CONFIG_DB = '/etc/sonic/config_db.json'
-CONFIG_DB_BAK = '/etc/sonic/config_db.json.bak'
-DUT_IMG_PATH = '/tmp/dut-sonic-img.bin'
-LOCALHOST_IMG_PATH = '/tmp/localhost-sonic-img.bin'
+CONFIG_DB = "/etc/sonic/config_db.json"
+CONFIG_DB_BAK = "/etc/sonic/config_db.json.bak"
+DUT_IMG_PATH = "/tmp/dut-sonic-img.bin"
+LOCALHOST_IMG_PATH = "/tmp/localhost-sonic-img.bin"
 
 logger = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.topology('any'),
+    pytest.mark.topology("any"),
     pytest.mark.sanity_check(skip_sanity=True),
     pytest.mark.disable_loganalyzer,
-    pytest.mark.skip_check_dut_health
+    pytest.mark.skip_check_dut_health,
 ]
 
 
-def simple_install_sonic(duthost, localhost, where):
+@pytest.fixture(autouse=True)
+def setup_env(duthosts, rand_one_dut_hostname):
     """
-    @summary: Install an image onto DUT and activate it.
-    """
-    localhost_img = where
-    if urlparse(where).scheme in ('http', 'https'):
-        # Download image if provided url.
-        localhost.get_url(url=where, dest=LOCALHOST_IMG_PATH)
-        localhost_img = LOCALHOST_IMG_PATH
-    duthost.copy(src=localhost_img, dest=DUT_IMG_PATH)
-    duthost.shell("sudo sonic-installer install {} -y".format(DUT_IMG_PATH))
-
-
-def test_l2_config_and_upgrade(request, duthosts, rand_one_dut_hostname, localhost, tbinfo):
-    """
-    @summary: A testcase that verifies DB migrator does not add unnecessary data during upgrade.
+    Setup/teardown fixture for each loopback interface test.
+    rollback to check if it goes back to starting config
 
     Args:
-        request: From pytest.
-        duthosts: set of DUTs.
-        rand_one_duthostname: sample a random DUT.
-        localhost: localhost object.
-        tbinfo: testbed info
+        duthosts: list of DUTs.
+        rand_selected_dut: The fixture returns a randomly selected DuT.
+    """
+    duthost = duthosts[rand_one_dut_hostname]
+    duthost.shell("sudo cp {} {}".format(CONFIG_DB, CONFIG_DB_BAK))
+
+    yield
+
+    duthost.shell("sudo cp {} {}".format(CONFIG_DB_BAK, CONFIG_DB))
+    config_reload(duthost)
+    wait_critical_processes(duthost)
+    duthost.shell("sudo rm -f {}".format(CONFIG_DB_BAK))
+
+
+def is_table_empty(duthost, table):
+    """
+    @summary: Verify a table is empty.
+
+    Args:
+        duthost: DUT host object.
+        table: Table name to verify.
+    """
+    # grep returns 1 when there is no match, use || true to override that.
+    count = int(
+        duthost.shell(
+            'sonic-db-cli CONFIG_DB KEYS "{}|*" | grep -c {} || true'.format(
+                table, table
+            )
+        )["stdout"]
+    )
+    return count == 0
+
+
+def test_no_hardcoded_minigraph(duthosts, rand_one_dut_hostname, tbinfo):
+    """
+    @summary: A testcase asserts no hardcoded minigraph config is imported to config_db during L2 configuration.
+
+    Args:
+        duthosts: list of DUTs.
+        rand_one_dut_hostname: The fixture returns a randomly selected DuT.
+        tbinfo: The testbed information. Needed for configuring management interface.
+
     """
     # Setup.
     duthost = duthosts[rand_one_dut_hostname]
+    if is_table_empty(duthost, "TELEMETRY") or is_table_empty(duthost, "RESTAPI"):
+        pytest.skip("TELEMETRY or RESTAPI table is empty. Please load minigraph first.")
+
     hwsku = duthost.facts["hwsku"]
     mgmt_fact = duthost.get_extended_minigraph_facts(tbinfo)["minigraph_mgmt_interface"]
-    source_image = request.config.getoption('source_image', default=None)
-    target_image = request.config.getoption('target_image', default=None)
-    if target_image is None:
-        pytest.skip("Must specify a target image through --target_image. Skipping the test.")
-
-    # Step 1: (Install source image and) reboot
-    if source_image:
-        install_sonic(duthost, source_image, tbinfo)
-    reboot(duthost, localhost, reboot_type="cold")
-
-    def _verify_config_db(duthost):
-        for table in ["TELEMETRY", "RESTAPI"]:
-            # grep returns 1 when there is no match, use || true to override that.
-            count = int(duthost.shell('grep -c {} /etc/sonic/config_db.json || true'.format(table))['stdout'])
-            pytest_assert(count == 0, "{} table is not empty!".format(table))
 
     # Step 2: Configure DUT into L2 mode.
     # Save original config
     duthost.shell("sudo cp {} {}".format(CONFIG_DB, CONFIG_DB_BAK))
     # Perform L2 configuration
-    L2_INIT_CFG_FILE = '/tmp/init_l2_cfg.json'
-    MGMT_CFG_FILE = '/tmp/mgmt_cfg.json'
-    L2_CFG_FILE = '/tmp/l2_cfg.json'
-    gen_l2_cfg = "sudo sonic-cfggen --preset l2 -p -H -k {} > {}".format(hwsku, L2_INIT_CFG_FILE)
+    L2_INIT_CFG_FILE = "/tmp/init_l2_cfg.json"
+    MGMT_CFG_FILE = "/tmp/mgmt_cfg.json"
+    L2_CFG_FILE = "/tmp/l2_cfg.json"
+    gen_l2_cfg = "sudo sonic-cfggen --preset l2 -p -H -k {} > {}".format(
+        hwsku, L2_INIT_CFG_FILE
+    )
     duthost.shell(gen_l2_cfg)
-    gen_mgmt_cfg = '''
+    gen_mgmt_cfg = """
     echo '
     {{
         "MGMT_INTERFACE": {{
@@ -98,25 +113,25 @@ def test_l2_config_and_upgrade(request, duthosts, rand_one_dut_hostname, localho
             }}
         }}
     }}' > {}
-    '''.format(mgmt_fact["addr"], mgmt_fact["prefixlen"], mgmt_fact["gwaddr"], duthost.hostname, MGMT_CFG_FILE)
+    """.format(
+        mgmt_fact["addr"],
+        mgmt_fact["prefixlen"],
+        mgmt_fact["gwaddr"],
+        duthost.hostname,
+        MGMT_CFG_FILE,
+    )
     duthost.shell(gen_mgmt_cfg)
-    duthost.shell("jq -s '.[0] * .[1]' {} {} > {}".format(L2_INIT_CFG_FILE, MGMT_CFG_FILE, L2_CFG_FILE))
+    duthost.shell(
+        "jq -s '.[0] * .[1]' {} {} > {}".format(
+            L2_INIT_CFG_FILE, MGMT_CFG_FILE, L2_CFG_FILE
+        )
+    )
     duthost.shell("sudo cp {} {}".format(L2_CFG_FILE, CONFIG_DB))
     config_reload(duthost)
     wait_critical_processes(duthost)
-    try:
-        _verify_config_db(duthost)
-    except pytest.fail.Exception:
-        pytest.skip("Unable to clear minigraph table when setting up L2 config for current image. Skipping the test.")
 
-    # Step 3: Install target image.
-    if target_image:
-        # Use simple_install_sonic to avoid messing with config logic.
-        simple_install_sonic(duthost, target_image)
-        _verify_config_db(duthost)
-
-    # Step 4: Reboot to target image.
-    reboot(duthost, localhost, reboot_type="cold")
-
-    # Step 5: Verifies no config from minigraph is written into ConfigDB.
-    _verify_config_db(duthost)
+    # Verify no minigraph config is present.
+    for table in ["TELEMETRY", "RESTAPI"]:
+        pytest_assert(
+            is_table_empty(duthost, table), "{} table is not empty!".format(table)
+        )
