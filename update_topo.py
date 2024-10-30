4b319be49f10971218dfd25939d718022b15a7a0
Ko Outlaw-Spruell (koutlaws) <koutlaws@cisco.com>
2024-10-29 17:39:03 -0700

    Update update_topo.py to take npl related info (#581)

--- update_topo.py
+++ update_topo.py
@@ -5,6 +5,7 @@ import json
 import sys
 
 TOPO_PLATFORM_FILE_MAP = 'topo_and_platform_to_filename_map.json'
+DEFAULT_SONIC_IMAGE_PATH = '../../sonic-cisco-8000.bin'
 
 platform_set = set()
 
@@ -20,6 +21,9 @@ parser.add_argument("-p", "--platform", help = "type of the dut platform ", narg
 parser.add_argument('-f', '--topo_yaml', type=str, help='topo yaml file', required=False,default=None)
 parser.add_argument("--dut-username", help = "username for the dut ", nargs='?', const='', default = 'cisco', required=False)
 parser.add_argument("--dut-password", help = "password for the dut ", nargs='?', const='', default = 'cisco123', required=False)
+parser.add_argument("--onie-install", help = "path to use for onie install image", nargs='?', const='', default = '', required=False)
+parser.add_argument("--npl-path", help = "npl path", nargs='?', const='', default = '', required=False)
+parser.add_argument("--npl-suite-ver", help = "npl suite version", nargs='?', const='', default = '', required=False)
 args = parser.parse_args()
 
 topology_file = args.topo_yaml
@@ -46,13 +50,32 @@ with open(topology_file, "r") as fd:
     for device in topo["devices"]:
         if "onie-install" not in topo["devices"][device]:
             continue
-
-        topo["devices"][device]["onie-install"] = "../../sonic-cisco-8000.bin"
-        if "vxr_sim_config" not in topo["devices"][device]:
-            topo["devices"][device]["vxr_sim_config" ] = {}
             
+        print(f"Modifying settings for device {device}...")
+        #specify onie install image, use default if not set
+        if args.onie_install:
+            print(f"set onie-install to: '{args.onie_install}'")
+            topo["devices"][device]["onie-install"] = args.onie_install
+        else:
+            print(f"onie-install path not specified. Setting to default path: '{DEFAULT_SONIC_IMAGE_PATH}'")
+            topo["devices"][device]["onie-install"] = DEFAULT_SONIC_IMAGE_PATH
+
+        #populate npl suite version and path. Not going to populate by default
+        if args.npl_path and args.npl_suite_ver:
+            if "vxr_sim_config" not in topo["devices"][device]:
+                topo["devices"][device]["vxr_sim_config" ] = {}
+                topo["devices"][device]["vxr_sim_config" ]["shelf"] = {}
+
+            print(f"setting ConfigS1NpsuiteVer to '{args.npl_suite_ver}'")
+            print(f"setting ConfigS1NplPath to '{args.npl_path}'")
+            topo["devices"][device]["vxr_sim_config"]["shelf"]["ConfigS1NpsuiteVer"] = args.npl_suite_ver
+            topo["devices"][device]["vxr_sim_config"]["shelf"]["ConfigS1NplPath"] = args.npl_path
+        
+        #populate dut password, default cisco/cisco123
+        print(f"set DUT username/pass to '{args.dut_username}/{args.dut_password}'")
         topo["devices"][device]["linux_username"] = args.dut_username
         topo["devices"][device]["linux_password"] = args.dut_password
+        print(f"Modify settings for device {device} done.")
 
 with open(topology_file, "w") as fd:
     yaml.safe_dump(topo, fd)
