f99f60aaef9a6357af7e7268b4d8991c0cf3a239
yatishkoul <yatishkoul@microsoft.com>
2024-11-04 18:08:08 +0000

    pre-commit fix for console_hepler.py

--- tests/common/helpers/console_helper.py
+++ tests/common/helpers/console_helper.py
@@ -33,21 +33,22 @@ def get_target_lines(duthost):
     result = duthost.shell("show chassis module status", module_ignore_errors=True)
     lines = result['stdout'].splitlines()
     linecards = []
-    
+
     # Pattern to match lines that have a "LINE-CARD" entry and "Online" in the Oper-Status column
     linecard_pattern = re.compile(r"^\s*(LINE-CARD\d+)\s+.*?\s+\d+\s+Online\s+up\s+\S+")
-    
+
     for line in lines:
         match = linecard_pattern.match(line)
         if match:
             linecard_name = match.group(1)
             index = linecard_name.split("LINE-CARD")[1]
             linecards.append(index)
-    
+
     if not linecards:
         pytest.fail("No line cards are online.")
-        
+
     return linecards
+
 
 def handle_pexpect_exceptions(target_line):
     """Handle pexpect exceptions during console interactions."""
