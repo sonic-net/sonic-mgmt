c76ff86cd038cff1ac248b6e2e32f5e4bed2cab8
Nishanth Sampath Kumar <nissampa@cisco.com>
2024-11-05 09:09:35 -0800

    modified the skip reason

--- tests/smartswitch/common/device_utils_dpu.py
+++ tests/smartswitch/common/device_utils_dpu.py
@@ -38,7 +38,7 @@ def check_smartswitch_and_dark_mode(duthosts,
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
 
     if "DPUS" not in duthost.facts:
-        pytest.skip("Test is not supported for this testbed and os version")
+        pytest.skip("Test is not supported for this testbed")
 
     darkmode = is_dark_mode_enabled(duthost, platform_api_conn,
                                     num_dpu_modules)
