53b0284b2ccb1f5a2892c4aff18543266461d643
bhagrane <bhagrane@cisco.com>
2024-11-06 14:23:44 -0800

    MIGSOFTWAR18824.mtucheck (#575)

---
+++ sonic-mgmt/tests/cisco/ipfwd/test_mtu.py
@@ -0,0 +1,51 @@
+import pytest
+import logging
+import re
+
+from tests.common.fixtures.ptfhost_utils import copy_ptftests_directory     # noqa F401
+from tests.common.fixtures.ptfhost_utils import set_ptf_port_mapping_mode   # noqa F401
+from tests.ptf_runner import ptf_runner
+from datetime import datetime
+
+EXPECTED_MTU_VALUE = 9216
+
+pytestmark = [
+    pytest.mark.topology('t1', 't2'),
+    pytest.mark.device_type('vs')
+]
+
+@pytest.mark.parametrize("mtu", [1514, 9114])
+def test_mtu(tbinfo, vmhost, ptfhost, mtu, gather_facts):
+
+    testbed_type = tbinfo['topo']['name']
+
+    output = vmhost.shell("ifconfig {} | grep mtu".format(vmhost.external_port))['stdout']
+    match = re.search(r'mtu (\d+)', output)
+    if match:
+        mtu_value_set = int(match.group(1))
+
+    if mtu == 9114 and  mtu_value_set < EXPECTED_MTU_VALUE:
+        pytest.fail(f'MTU on UCS interface is set to {mtu_value_set}, must be set to {EXPECTED_MTU_VALUE}')
+
+    log_file = "/tmp/mtu_test.{}-{}.log".format(mtu, datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
+
+    logging.info("Starting MTU test. PTF log file: %s" % log_file)
+    ptf_runner(ptfhost,
+               "ptftests",
+               "mtu_test.MtuTest",
+               platform_dir="ptftests",
+               params={"testbed_type": testbed_type,
+                       "router_mac": gather_facts['src_router_mac'],
+                       "testbed_mtu": mtu,
+                       "src_host_ip": gather_facts['src_host_ipv4'],
+                       "src_router_ip": gather_facts['src_router_ipv4'],
+                       "dst_host_ip": gather_facts['dst_host_ipv4'],
+                       "src_host_ipv6": gather_facts['src_host_ipv6'],
+                       "src_router_ipv6": gather_facts['src_router_ipv6'],
+                       "dst_host_ipv6": gather_facts['dst_host_ipv6'],
+                       "src_ptf_port_list": gather_facts['src_port_ids'],
+                       "dst_ptf_port_list": gather_facts['dst_port_ids']
+                       },
+               log_file=log_file,
+               socket_recv_size=16384,
+               is_python3=True)
