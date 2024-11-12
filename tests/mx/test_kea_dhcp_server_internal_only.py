4009d6c9d142f9a4f00ee7c728805ab28a38a3cd
Yaqiang Zhu <yaqiangzhu@microsoft.com>
2024-11-12 04:00:58 +0000

    Merged PR 8172: [mx][dhcp_server] Don't remove all vlan when testing golden config db
    Vlan configuraion would be overridden when load golden config db, hence no need to manually removed
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Bug fix
    
    #### PR Summary
    This pull request addresses an issue with VLAN removal during golden config DB testing for MX.
    - `tests/mx/test_kea_dhcp_server_internal_only.py`: Moved the `remove_all_vlans(duthost)` call to ensure VLANs are removed only when using the GCU config tool.

--- tests/mx/test_kea_dhcp_server_internal_only.py
+++ tests/mx/test_kea_dhcp_server_internal_only.py
@@ -97,10 +97,10 @@ def test_dhcp_server_ip_assignment(
     #  We don't restore vlan config after tests for mx, we remove all vlans before tests
     loganalyzer[duthost.hostname].ignore_regex.append(".*processFlexCounterEvent: port VID oid:.*, " +
                                                       "was not found \(probably port was removed\/splitted\).*")
-    remove_all_vlans(duthost)
     dhcp_server_config = json.load(open(MX_VLAN_AND_DHCP_SERVER_CONF_PATH, "r")).get(vlan_count, [])
     loganalyzer[duthost.hostname].add_start_ignore_mark()
     if config_tool == DHCP_SERVER_CONFIG_TOOL_GCU:
+        remove_all_vlans(duthost)
         gcu_patch = jsonpatch.make_patch({}, dhcp_server_config).patch
         apply_dhcp_server_config_gcu(duthost, gcu_patch)
     elif config_tool == DHCP_SERVER_CONFIG_TOOL_GOLDEN:
