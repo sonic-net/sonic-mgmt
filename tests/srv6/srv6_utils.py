ca9638cfbcf7044fa6708c06707d88f4b3bdad9e
Eddie Ruan <119699263+eddieruan-alibaba@users.noreply.github.com>
2024-11-26 00:27:10 -0800

    Add SRv6 test cases on 7 nodes testbed (#15723)
    This PR is to add some SRv6 test cases listed in https://github.com/sonic-net/sonic-mgmt/blob/master/docs/testplan/srv6/SRv6-phoenixwing-ptf-testplan.md. These test cases are running on the 7 node testbed.
    
    The infra changes are in #15349
    
    What is the motivation for this PR?
    Add couple SRv6 Test cases listed in https://github.com/sonic-net/sonic-mgmt/blob/master/docs/testplan/srv6/SRv6-phoenixwing-ptf-testplan.md.
    
    How did you do it?
    Add 5 more test cases, including ptf traffic test and link flapping test cases.
    
    How did you verify/test it?
    Via daily jenkins run
    
    The test log could be found at http://phoenixwing.com.cn/vsonic
    
    Any platform specific information?
    Only on 7 node vsonic testbed.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/srv6/srv6_utils.py
+++ tests/srv6/srv6_utils.py
@@ -1,8 +1,17 @@
 import logging
+import time
 import requests
+import ptf.packet as scapy
+import ptf.testutils as testutils
+
 from tests.common.helpers.assertions import pytest_assert
 
 logger = logging.getLogger(__name__)
+
+#
+# log directory inside each vsonic. vsonic starts with admin as user.
+#
+test_log_dir = "/home/admin/testlogs/"
 
 
 #
@@ -35,7 +44,7 @@ def change_route(operation, ptfip, neighbor, route, nexthop, port):
 # Skip some BGP neighbor check
 #
 def skip_bgp_neighbor_check(neighbor):
-    skip_addresses = ['2064:100::1d', '2064:200::1e', '2064:300::1f']
+    skip_addresses = []
     for addr in skip_addresses:
         if neighbor == addr:
             return True
@@ -101,3 +110,178 @@ def find_node_interfaces(nbrhost):
             found = found + 1
 
     return found, hwsku
+
+
+#
+# Send receive packets
+#
+def runSendReceive(pkt, src_port, exp_pkt, dst_ports, pkt_expected, ptfadapter):
+    """
+    @summary Send packet and verify it is received/not received on the expected ports
+    @param pkt: The packet that will be injected into src_port
+    @param src_ports: The port into which the pkt will be injected
+    @param exp_pkt: The packet that will be received on one of the dst_ports
+    @param dst_ports: The ports on which the exp_pkt may be received
+    @param pkt_expected: Indicated whether it is expected to receive the exp_pkt on one of the dst_ports
+    @param ptfadapter: The ptfadapter fixture
+    """
+    # Send the packet and poll on destination ports
+    testutils.send(ptfadapter, src_port, pkt, 1)
+    logger.debug("Sent packet: " + pkt.summary())
+    (index, rcv_pkt) = testutils.verify_packet_any_port(ptfadapter, exp_pkt, dst_ports)
+    received = False
+    if rcv_pkt:
+        received = True
+    pytest_assert(received is True)
+    logger.debug('index=%s, received=%s' % (str(index), str(received)))
+    if received:
+        logger.debug("Received packet: " + scapy.Ether(rcv_pkt).summary())
+    if pkt_expected:
+        logger.debug('Expected packet on dst_ports')
+        passed = True if received else False
+        logger.debug('Received: ' + str(received))
+    else:
+        logger.debug('No packet expected on dst_ports')
+        passed = False if received else True
+        logger.debug('Received: ' + str(received))
+    logger.debug('Passed: ' + str(passed))
+    return passed
+
+
+#
+# Helper func to check if a list of IPs go via a given set of next hop
+#
+def check_routes_func(nbrhost, ips, nexthops, vrf="", is_v6=False):
+    # Check remote learnt dual homing routes
+    vrf_str = ""
+    if vrf != "":
+        vrf_str = "vrf {}".format(vrf)
+    ip_str = "ip"
+    if is_v6:
+        ip_str = "ipv6"
+    for ip in ips:
+        cmd = "show {} route {} {} nexthop-group".format(ip_str, vrf_str, ip)
+        res = nbrhost.command(cmd)["stdout_lines"]
+        print_lines(res)
+        found = 0
+        for nexthop in nexthops:
+            for line in res:
+                if nexthop in line:
+                    found = found + 1
+        if len(nexthops) != found:
+            return False
+    return True
+
+
+#
+# check if a list of IPs go via a given set of next hop
+#
+def check_routes(nbrhost, ips, nexthops, vrf="", is_v6=False):
+    # Add retry for debugging purpose
+    count = 0
+    ret = False
+
+    #
+    # Sleep 10 sec before retrying
+    #
+    sleep_duration_for_retry = 10
+
+    # retry 3 times before claiming failure
+    while count < 3 and not ret:
+        ret = check_routes_func(nbrhost, ips, nexthops, vrf, is_v6)
+        if not ret:
+            count = count + 1
+            # sleep make sure all forwarding structures are settled down.
+            time.sleep(sleep_duration_for_retry)
+            logger.info("Sleep {} seconds to retry round {}".format(sleep_duration_for_retry, count))
+
+    pytest_assert(ret)
+
+
+#
+# Record fwding chain to a file
+#
+def recording_fwding_chain(nbrhost, fname, comments):
+
+    filename = "{}{}".format(test_log_dir, fname)
+
+    cmd = "mkdir -p {}".format(test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "sudo touch /etc/sonic/frr/vtysh.conf"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+    cmd = "date >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "echo ' {}' >> {} ".format(comments, filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show bgp summary' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ip route vrf Vrf1 192.100.1.0 nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ipv6 route fd00:201:201:fff1:11:: nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "vtysh -c 'show ipv6 route fd00:202:202:fff2:22:: nexthop-group' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+    cmd = "echo '' >> {} ".format(filename)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+
+#
+# Debug commands for FRR zebra
+#
+debug_cmds = [
+    'debug zebra events',
+    'debug zebra rib',
+    'debug zebra rib detailed',
+    'debug zebra nht',
+    'debug zebra nht detailed',
+    'debug zebra dplane',
+    'debug zebra nexthop',
+    'debug zebra nexthop detail',
+    'debug zebra packet',
+    'debug zebra packet detail'
+]
+
+
+#
+# Turn on/off FRR debug to a file
+#
+def turn_on_off_frr_debug(duthosts, rand_one_dut_hostname, nbrhosts, filename, vm, is_on=True):
+    nbrhost = nbrhosts[vm]['host']
+    # save frr log to a file
+    pfxstr = " "
+    if not is_on:
+        pfxstr = " no "
+
+    cmd = "vtysh -c 'configure terminal' -c '{} log file {}'".format(pfxstr, filename)
+    nbrhost.command(cmd)
+
+    #
+    # Change frr debug flags
+    #
+    for dcmd in debug_cmds:
+        cmd = "vtysh -c '" + pfxstr + dcmd + "'"
+        nbrhost.command(cmd)
+
+    #
+    # Check debug flags
+    #
+    cmd = "vtysh -c 'show debug'"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    #
+    # Check log file
+    #
+    cmd = "vtysh -c 'show run' | grep log"
+    nbrhost.shell(cmd, module_ignore_errors=True)
+
+
+#
+# Collect file from bgp docker
+#
+def collect_frr_debugfile(duthosts, rand_one_dut_hostname, nbrhosts, filename, vm):
+    nbrhost = nbrhosts[vm]['host']
+    cmd = "mkdir -p {}".format(test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
+    cmd = "docker cp bgp:{} {}".format(filename, test_log_dir)
+    nbrhost.shell(cmd, module_ignore_errors=True)
