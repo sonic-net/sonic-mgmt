49b8360d2f9ef1193eaa7e772a9f9a4ffa7d3ffa
Xichen Lin <lukelin0907@gmail.com>
2024-11-28 14:36:11 +0000

    WIP 13

--- tests/wol/test_wol.py
+++ tests/wol/test_wol.py
@@ -148,70 +148,38 @@ def test_send_to_single_specific_interface_udp(
 
 def test_send_to_vlan(
     duthost,
-    ptfhost,
+    ptfadapter,
     get_connected_dut_intf_to_ptf_index,
     loganalyzer
 ):
     loganalyzer[duthost.hostname].ignore_regex.append(VLAN_MEMBER_CHANGE_ERR)
-    connected_dut_intf_to_ptf_index = get_connected_dut_intf_to_ptf_index
+
     dut_ptf_int_map = dict(get_connected_dut_intf_to_ptf_index)
-    connected_dut_intf = [dut_intf for dut_intf, _ in connected_dut_intf_to_ptf_index]
+    connected_dut_intf = [dut_intf for dut_intf, _ in get_connected_dut_intf_to_ptf_index]
     dut_mac = duthost.facts['router_mac']
     target_mac = "1a:2b:3c:d1:e2:f1"
+
     vlan_brief = duthost.get_vlan_brief()
     vlan_names = list(vlan_brief.keys())
     random_vlan = random.choice(vlan_names)
     vlan_members = vlan_brief[random_vlan]['members']
-    connected_vlan_members = [member for member in vlan_members if member in connected_dut_intf]
-    random_member_to_remove = random.choice(connected_vlan_members)
-    random_vlan_members = [member for member in connected_vlan_members if member != random_member_to_remove]
+    connected_vlan_members = list(filter(lambda member: member in connected_dut_intf, vlan_members))
+    random_member_to_remove = connected_vlan_members.pop(random.randrange(len(connected_vlan_members)))
     logging.info("Test with random vlan %s, members %s and member to remove %s"
-                 % (random_vlan, random_vlan_members, random_member_to_remove))
+                 % (random_vlan, connected_vlan_members, random_member_to_remove))
 
     duthost.del_member_from_vlan(vlan_n2i(random_vlan), random_member_to_remove)
 
-    try:
-        tcpdump_cmd = 'nohup tcpdump -i %s -w %s %s >/dev/null 2>&1 & echo $!' % \
-            (random_member_to_remove, generate_pcap_file_path(random_member_to_remove), WOL_ETHER_PKT_FILTER)
-        tcpdump_pid = ptfhost.shell(tcpdump_cmd)["stdout"]
-        for member in random_vlan_members + [random_member_to_remove]:
-            ptf_int = 'eth' + str(dut_ptf_int_map[member])
-            tcpdump_cmd = 'nohup tcpdump -i %s -w %s %s >/dev/null 2>&1 & echo $!' % \
-                (ptf_int, generate_pcap_file_path(ptf_int), WOL_ETHER_PKT_FILTER)
-            tcpdump_pid = ptfhost.shell(tcpdump_cmd)["stdout"]
-            cmd_check_if_process_running = "ps -p %s | grep %s |grep -v grep | wc -l" % (tcpdump_pid, tcpdump_pid)
-            success = ptfhost.shell(cmd_check_if_process_running)["stdout"] == "1"
-            if not success:
-                ptfhost.shell('killall tcpdump', module_ignore_errors=True)
-                pytest.fail("Failed to start tcpdump on %s" % member)
+    pkt = Ether(src=dut_mac, dst=target_mac, type=0x0842)
+    pkt /= Raw(load=build_magic_packet_payload(target_mac))
 
-        def validate_wol_packets(pkts):
-            pytest_assert(len(pkts) == 1, "Unexpected pkts count %s" % len(pkts))
-            pkt = pkts[0]
-            pytest_assert(pkt.dst == target_mac, "Unexpected dst mac %s" % pkt.dst)
-            pytest_assert(pkt.src == dut_mac, "Unexpected src mac %s" % pkt.src)
-            pytest_assert(pkt.type == ETHER_TYPE_WOL_DEC)
-            pytest_assert(pkt.load == build_magic_packet_payload(target_mac))
+    duthost.shell("wol {} {}".format(random_vlan, target_mac))
 
-        duthost.shell("wol %s %s" % (random_vlan, target_mac))
+    testutils.verify_packets(ptfadapter, pkt, list(map(lambda member: dut_ptf_int_map[member], connected_vlan_members)))
+    testutils.verify_no_packet_any(ptfadapter, pkt,
+                                   list(map(lambda member: dut_ptf_int_map[member], [random_member_to_remove])))
 
-        time.sleep(1)
-        ptfhost.shell('killall tcpdump')
-        time.sleep(1)
-
-        ptf_int = 'eth' + str(dut_ptf_int_map[random_member_to_remove])
-        with tempfile.NamedTemporaryFile() as temp_pcap:
-            ptfhost.fetch(src=generate_pcap_file_path(ptf_int), dest=temp_pcap.name, flat=True)
-            pytest_assert(len(scapy_sniff(offline=temp_pcap.name)) == 0)
-
-        for member in random_vlan_members:
-            ptf_int = 'eth' + str(dut_ptf_int_map[member])
-            with tempfile.NamedTemporaryFile() as temp_pcap:
-                ptfhost.fetch(src=generate_pcap_file_path(ptf_int), dest=temp_pcap.name, flat=True)
-                validate_wol_packets(scapy_sniff(offline=temp_pcap.name))
-
-    finally:
-        duthost.add_member_to_vlan(vlan_n2i(random_vlan), random_member_to_remove, False)
+    duthost.add_member_to_vlan(vlan_n2i(random_vlan), random_member_to_remove, False)
 
 
 def test_send_broadcast_to_single_interface(
