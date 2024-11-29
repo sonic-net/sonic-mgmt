562cf57e3e05a006dc0d9319bc9506a037a018bc
Xichen Lin <lukelin0907@gmail.com>
2024-11-29 14:09:41 +0000

    WIP 24

--- tests/wol/conftest.py
+++ tests/wol/conftest.py
@@ -49,12 +49,15 @@ def dst_ip(request, duthost, ptfhost, vlan_brief, random_vlan, random_intf_pair)
     if ip:
         vlan_intf = ipaddress.ip_interface(vlan_brief[random_vlan]["interface_" + ip][0])
         ip = random_ip_from_network(vlan_intf.network)
+        logging.info("Test with ip {} from vlan interface {}".format(ip, vlan_intf))
         ptfhost.shell("ip addr add {} dev eth{}".format(ip, random_intf_pair[1]))
+        logging.info("Configure ip {} on eth{} of ptf".format(ip, random_intf_pair[1]))
 
     yield ip
 
     if ip:
         ptfhost.shell("ip addr del {} dev eth{}".format(ip, random_intf_pair[1]))
+        logging.info("Remove ip {} on eth{} of ptf".format(ip, random_intf_pair[1]))
 
 
 def vlan_n2i(vlan_name):
@@ -67,18 +70,22 @@ def vlan_n2i(vlan_name):
 @pytest.fixture(scope="function")
 def get_connected_intf_pair_under_vlan(get_connected_dut_intf_to_ptf_index, vlan_brief, random_vlan):
     vlan_members = vlan_brief[random_vlan]['members']
-    items_in_vlan = filter(lambda member: member in vlan_members, get_connected_dut_intf_to_ptf_index)
+    items_in_vlan = list(filter(lambda member: member in vlan_members, get_connected_dut_intf_to_ptf_index))
+    logging.info("Intf pair under vlan {}: {}".format(random_vlan, items_in_vlan))
     return list(items_in_vlan)
 
 
 @pytest.fixture(scope="function")
-def random_intf_pair_to_remove_under_vlan(duthost, get_connected_intf_pair_under_vlan):
+def random_intf_pair_to_remove_under_vlan(duthost, random_vlan, get_connected_intf_pair_under_vlan):
     intf_pair_to_remove = random.choice(get_connected_intf_pair_under_vlan)
+    logging.info("Intf pair to remove under vlan {}: {}".format(random_vlan, intf_pair_to_remove))
     duthost.del_member_from_vlan(vlan_n2i(random_vlan), intf_pair_to_remove[0])
+    logging.info("Intf pair {} removed from vlan {}".format(intf_pair_to_remove, random_vlan))
 
     yield intf_pair_to_remove
 
     duthost.add_member_to_vlan(vlan_n2i(random_vlan), intf_pair_to_remove[0], False)
+    logging.info("Intf pair {} added back to vlan {}".format(intf_pair_to_remove, random_vlan))
 
 
 @pytest.fixture(scope="function")
