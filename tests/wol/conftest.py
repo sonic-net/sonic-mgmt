4f253e31e1177fd8dab0ce43846b48d6c958986f
Xichen Lin <lukelin0907@gmail.com>
2024-11-29 14:44:47 +0000

    WIP 26

--- tests/wol/conftest.py
+++ tests/wol/conftest.py
@@ -68,7 +68,7 @@ def vlan_n2i(vlan_name):
 
 
 @pytest.fixture(scope="function")
-def get_connected_intf_pair_under_vlan(get_connected_dut_intf_to_ptf_index, vlan_brief, random_vlan):
+def get_intf_pair_under_vlan(get_connected_dut_intf_to_ptf_index, vlan_brief, random_vlan):
     vlan_members = vlan_brief[random_vlan]['members']
     items_in_vlan = list(filter(lambda item: item[0] in vlan_members, get_connected_dut_intf_to_ptf_index))
     logging.info("Intf pair under vlan {}: {}".format(random_vlan, items_in_vlan))
@@ -76,8 +76,8 @@ def get_connected_intf_pair_under_vlan(get_connected_dut_intf_to_ptf_index, vlan
 
 
 @pytest.fixture(scope="function")
-def random_intf_pair_to_remove_under_vlan(duthost, random_vlan, get_connected_intf_pair_under_vlan):
-    intf_pair_to_remove = random.choice(get_connected_intf_pair_under_vlan)
+def random_intf_pair_to_remove_under_vlan(duthost, random_vlan, get_intf_pair_under_vlan):
+    intf_pair_to_remove = random.choice(get_intf_pair_under_vlan)
     logging.info("Intf pair to remove under vlan {}: {}".format(random_vlan, intf_pair_to_remove))
     duthost.del_member_from_vlan(vlan_n2i(random_vlan), intf_pair_to_remove[0])
     logging.info("Intf pair {} removed from vlan {}".format(intf_pair_to_remove, random_vlan))
@@ -89,10 +89,10 @@ def random_intf_pair_to_remove_under_vlan(duthost, random_vlan, get_connected_in
 
 
 @pytest.fixture(scope="function")
-def remaining_intf_pair_under_vlan(get_connected_intf_pair_under_vlan, random_intf_pair_to_remove_under_vlan):
-    return list(filter(lambda item: item != random_intf_pair_to_remove_under_vlan, get_connected_intf_pair_under_vlan))
+def remaining_intf_pair_under_vlan(get_intf_pair_under_vlan, random_intf_pair_to_remove_under_vlan):
+    return list(filter(lambda item: item != random_intf_pair_to_remove_under_vlan, get_intf_pair_under_vlan))
 
 
 @pytest.fixture(scope="function")
-def get_connected_intf_pair_not_under_vlan(get_connected_dut_intf_to_ptf_index, remaining_intf_pair_under_vlan):
+def get_intf_pair_not_under_vlan(get_connected_dut_intf_to_ptf_index, remaining_intf_pair_under_vlan):
     return list(filter(lambda item: item not in remaining_intf_pair_under_vlan, get_connected_dut_intf_to_ptf_index))
