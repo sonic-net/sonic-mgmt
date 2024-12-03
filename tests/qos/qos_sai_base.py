4764e0298e3d27a7eed4d0353f4b6814eef3ef51
rick-arista <148895369+rick-arista@users.noreply.github.com>
2024-12-02 23:06:51 -0800

    Add qos_params.th5.yaml (#14250)
    What is the motivation for this PR?
    Adds a baseline of parameters, which fixes the headroom pool size test.
    
    How did you do it?
    Values were generated by updated version of the generator script.
    
    How did you verify/test it?
    Manual test runs.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -147,7 +147,7 @@ class QosBase:
             qlen=10000,
             is_python3=True,
             relax=relax,
-            timeout=1200,
+            timeout=1850,
             socket_recv_size=16384,
             custom_options=custom_options,
             pdb=pdb
@@ -1623,6 +1623,9 @@ class QosSaiBase(QosBase):
         elif 'broadcom' in duthost.facts['asic_type'].lower():
             if 'platform_asic' in duthost.facts and duthost.facts['platform_asic'] == 'broadcom-dnx':
                 logger.info("THDI_BUFFER_CELL_LIMIT_SP is not valid for broadcom DNX - ignore dynamic buffer config")
+                qosParams = qosConfigs['qos_params'][dutAsic][dutTopo]
+            elif dutAsic == 'th5':
+                logger.info("Generator script not implemented for TH5")
                 qosParams = qosConfigs['qos_params'][dutAsic][dutTopo]
             else:
                 bufferConfig = self.dutBufferConfig(duthost, dut_asic)
