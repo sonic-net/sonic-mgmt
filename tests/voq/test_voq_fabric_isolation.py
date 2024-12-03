bbabe3c50244a044a07c6268ea884566153d2554
Nathan Wolfe <nwolfe@arista.com>
2024-12-03 22:48:12 +0000

    Handle AUTO_ISOLATED attribute missing from table
    AUTO_ISOLATED is only added to the FABRIC_PORT_TABLE once the fabric
    port has been isolated once, so we should treat this attribute missing
    from this table the same as AUTO_ISOLATED=0

--- tests/voq/test_voq_fabric_isolation.py
+++ tests/voq/test_voq_fabric_isolation.py
@@ -109,5 +109,8 @@ def check_fabric_link_status(host, asicName, port, state):
     auto_isolated = cmd_output[0]
     if auto_isolated == state:
         return True
+    elif auto_isolated == '' and state == '0':
+        # AUTO_ISOLATED attribute may be missing from the table if it's the first time it's been isolated, missing means not isolated
+        return True
     else:
         return False
