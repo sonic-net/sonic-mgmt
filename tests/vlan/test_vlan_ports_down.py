0a900498e3aab67aeb7e6e1694be2fb078d41269
Mahdi Ramezani <mramezani@microsoft.com>
2024-12-04 23:41:46 +0000

    Skipped IP-in-IP decap test if the ASIC type is 'vs'. Also, added some logging statements to the test.
    Signed-off-by: Mahdi Ramezani <mramezani@microsoft.com>

--- tests/vlan/test_vlan_ports_down.py
+++ tests/vlan/test_vlan_ports_down.py
@@ -30,10 +30,12 @@ def vlan_ports_setup(duthosts, rand_one_dut_hostname):
     vlan_members = vlan_brief[vlan_name]["members"]
     ifs_status = duthost.get_interfaces_status()
     vlan_up_members = [port for port in vlan_members if ifs_status[port]["admin"] == "up"]
+    logger.info(f"Bringing down all member ports of {vlan_name}...")
     for vlan_up_port in vlan_up_members:
         duthost.shell(f"sudo config interface shutdown {vlan_up_port}")
     time.sleep(5)  # Sleep for 5 seconds to ensure T1 switches update their routing table
     yield vlan_name
+    logger.info(f"Restoring the previous admin state of all member ports of {vlan_name}...")
     for vlan_port in vlan_up_members:
         duthost.shell(f"sudo config interface startup {vlan_port}")
 
@@ -49,6 +51,7 @@ def test_vlan_ports_down(vlan_ports_setup, duthosts, rand_one_dut_hostname, nbrh
     vlan_name = vlan_ports_setup
     ip_interfaces = duthost.show_ip_interface()["ansible_facts"]["ip_interfaces"]
     vlan_info = ip_interfaces[vlan_name]
+    logger.info(f"Checking if {vlan_name} is oper UP...")
     # check if the VLAN interface is operationally Up (IPv4)
     pytest_assert(vlan_info["oper_state"] == "up", f"{vlan_name} is operationally down.")
 
@@ -57,30 +60,39 @@ def test_vlan_ports_down(vlan_ports_setup, duthosts, rand_one_dut_hostname, nbrh
     # check if the VLAN interface is operationally Up (IPv6)
     pytest_assert(vlan_info_ipv6["oper"] == "up", f"{vlan_name} is operationally down.")
 
+    logger.info("Checking BGP routes on T1 neighbors...")
     vlan_subnet = str(IPNetwork(f"{vlan_info['ipv4']}/{vlan_info['prefix_len']}", flags=NOHOST))
     vlan_subnet_ipv6 = str(IPNetwork(vlan_info_ipv6["ipv6 address/mask"], flags=NOHOST))
     nbrcount = 0
     for nbrname, nbrhost in nbrhosts.items():
         nbrhost = nbrhost["host"]
         # check IPv4 routes on nbrhost
+        logger.info(f"Checking IPv4 routes on {nbrname}...")
         try:
             vlan_route = nbrhost.get_route(vlan_subnet)["vrfs"]["default"]
         except Exception:
             # nbrhost might be unreachable. Skip it.
+            logger.info(f"{nbrname} might be unreachable.")
             continue
         pytest_assert(vlan_route["bgpRouteEntries"],
                       f"{vlan_name}'s IPv4 subnet is not advertised to the T1 neighbor {nbrname}.")
         # check IPv6 routes on nbrhost
+        logger.info(f"Checking IPv6 routes on {nbrname}...")
         try:
             vlan_route_ipv6 = nbrhost.get_route(vlan_subnet_ipv6)["vrfs"]["default"]
         except Exception:
             # nbrhost might be unreachable. Skip it.
+            logger.info(f"{nbrname} might be unreachable.")
             continue
         pytest_assert(vlan_route_ipv6["bgpRouteEntries"],
                       f"{vlan_name}'s IPv6 subnet is not advertised to the T1 neighbor {nbrname}.")
         nbrcount += 1
     if nbrcount == 0:
         pytest.skip("Could not get routing info from any T1 neighbors.")
+    if duthost.facts["asic_type"].lower() == "vs":
+        logger.info("Skipping IP-in-IP decapsulation test for the 'vs' ASIC type.")
+        return
+    logger.info("Starting the IP-in-IP decapsulation test...")
     mg_facts = duthost.get_extended_minigraph_facts(tbinfo)
     # Use the first Ethernet port associated with the first portchannel to send test packets to the DUT
     portchannel_info = next(iter(mg_facts["minigraph_portchannels"].values()))
@@ -110,5 +122,7 @@ def test_vlan_ports_down(vlan_ports_setup, duthosts, rand_one_dut_hostname, nbrh
     exp_pkt.set_do_not_care_packet(IP, "ttl")
     exp_pkt.set_do_not_care_packet(IP, "chksum")
     exp_pkt.set_do_not_care_packet(IP, "tos")
+    logger.info("Sending the IP-in-IP packet...")
     testutils.send(ptfadapter, ptf_src_port_index, outer_pkt)
+    logger.info("IP-in-IP packet sent.")
     testutils.verify_packet_any_port(ptfadapter, exp_pkt, ports=ptf_dst_port_indices)
