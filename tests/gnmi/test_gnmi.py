9313f19f2b271cf54e281c8a46f1925795f66177
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2024-12-06 02:58:17 +0800

    Add gnmi CRL test case (#15823)
    Add gnmi CRL test case.
    
    Why I did it
    Add test case for gnmi CRL feature.
    
    How I did it
    Add gnmi CRL test case.
    
    How to verify it
    Pass all test case.

--- tests/gnmi/test_gnmi.py
+++ tests/gnmi/test_gnmi.py
@@ -1,7 +1,8 @@
 import pytest
 import logging
 
-from .helper import gnmi_capabilities, gnmi_set, add_gnmi_client_common_name, del_gnmi_client_common_name
+from .helper import gnmi_capabilities, gnmi_set, add_gnmi_client_common_name, del_gnmi_client_common_name, dump_gnmi_log
+from tests.common.utilities import wait_until
 
 logger = logging.getLogger(__name__)
 
@@ -37,16 +38,7 @@ def setup_invalid_client_cert_cname(duthosts, rand_one_dut_hostname):
     add_gnmi_client_common_name(duthost, "test.client.gnmi.sonic")
 
 
-def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
-                                                  rand_one_dut_hostname,
-                                                  ptfhost,
-                                                  setup_invalid_client_cert_cname):
-    '''
-    Verify GNMI native write, incremental config for configDB
-    GNMI set request with invalid path
-    '''
-    duthost = duthosts[rand_one_dut_hostname]
-
+def gnmi_create_vnet(duthost, ptfhost, cert=None):
     file_name = "vnet.txt"
     text = "{\"Vnet1\": {\"vni\": \"1000\", \"guid\": \"559c6ce8-26ab-4193-b946-ccc6e8f930b2\"}}"
     with open(file_name, 'w') as file:
@@ -56,9 +48,71 @@ def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
     update_list = ["/sonic-db:APPL_DB/localhost/DASH_VNET_TABLE:@/root/%s" % (file_name)]
     msg = ""
     try:
-        gnmi_set(duthost, ptfhost, [], update_list, [])
+        gnmi_set(duthost, ptfhost, [], update_list, [], cert)
     except Exception as e:
         logger.info("Failed to set: " + str(e))
         msg = str(e)
 
+    gnmi_log = dump_gnmi_log(duthost)
+
+    return msg, gnmi_log
+
+
+def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
+                                                  rand_one_dut_hostname,
+                                                  ptfhost,
+                                                  setup_invalid_client_cert_cname):
+    '''
+    Verify GNMI native write, incremental config for configDB
+    GNMI set request with invalid path
+    '''
+    duthost = duthosts[rand_one_dut_hostname]
+    msg, gnmi_log = gnmi_create_vnet(duthost, ptfhost)
+
     assert "Unauthenticated" in msg
+    assert "Failed to retrieve cert common name mapping" in gnmi_log
+
+
+@pytest.fixture(scope="function")
+def setup_crl_server_on_ptf(ptfhost):
+    ptfhost.shell('rm -f /root/crl.log')
+    ptfhost.shell('nohup python /root/crl_server.py &')
+    logger.warning("crl server started")
+
+    # Wait untill HTTP server ready
+    def server_ready_log_exist(ptfhost):
+        res = ptfhost.shell("sed -n '/Ready handle request/p'  /root/crl.log", module_ignore_errors=True)
+        logger.debug("crl.log: {}".format(res["stdout_lines"]))
+        return len(res["stdout_lines"]) > 0
+
+    wait_until(60, 1, 0, server_ready_log_exist, ptfhost)
+    logger.warning("crl server ready")
+
+    yield
+
+    # pkill will use the kill signal -9 as exit code, need ignore error
+    ptfhost.shell("pkill -9 -f 'python /root/crl_server.py'", module_ignore_errors=True)
+
+
+def test_gnmi_authorize_failed_with_revoked_cert(duthosts,
+                                                 rand_one_dut_hostname,
+                                                 ptfhost,
+                                                 setup_crl_server_on_ptf):
+    '''
+    Verify GNMI native write, incremental config for configDB
+    GNMI set request with invalid path
+    '''
+    duthost = duthosts[rand_one_dut_hostname]
+
+    retry = 3
+    msg = ""
+    gnmi_log = ""
+    while retry > 0:
+        retry -= 1
+        msg, gnmi_log = gnmi_create_vnet(duthost, ptfhost, "gnmiclient.revoked")
+        # retry when download crl failed, ptf device network not stable
+        if "desc = Peer certificate revoked" in gnmi_log:
+            break
+
+    assert "Unauthenticated" in msg
+    assert "desc = Peer certificate revoked" in gnmi_log
