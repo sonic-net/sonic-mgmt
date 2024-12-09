3667f29a2b0a0f6135cea8745a6a19d77534fcec
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2024-12-09 14:23:43 +0800

    Update test_gnmi.py

--- tests/gnmi/test_gnmi.py
+++ tests/gnmi/test_gnmi.py
@@ -39,7 +39,7 @@ def setup_invalid_client_cert_cname(duthosts, rand_one_dut_hostname):
 
 def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
                                                   rand_one_dut_hostname,
-                                                  ptfhost,
+                                                  localhost,
                                                   setup_invalid_client_cert_cname):
     '''
     Verify GNMI native write, incremental config for configDB
@@ -51,14 +51,8 @@ def test_gnmi_authorize_failed_with_invalid_cname(duthosts,
     text = "{\"Vnet1\": {\"vni\": \"1000\", \"guid\": \"559c6ce8-26ab-4193-b946-ccc6e8f930b2\"}}"
     with open(file_name, 'w') as file:
         file.write(text)
-    ptfhost.copy(src=file_name, dest='/root')
     # Add DASH_VNET_TABLE
     update_list = ["/sonic-db:APPL_DB/localhost/DASH_VNET_TABLE:@/root/%s" % (file_name)]
-    msg = ""
-    try:
-        gnmi_set(duthost, ptfhost, [], update_list, [])
-    except Exception as e:
-        logger.info("Failed to set: " + str(e))
-        msg = str(e)
+    ret, msg = gnmi_set(duthost, localhost, [], update_list, [])
 
     assert "Unauthenticated" in msg
