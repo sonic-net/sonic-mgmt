a09e9a2b118f7190de16a281f18125480a99ba21
Judy Joseph <jujoseph@microsoft.com>
2024-12-09 05:53:06 +0000

    deploy-mg changes 2

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -6,6 +6,7 @@
 
 
 import copy
+import jinja2
 import json
 import os
 
@@ -32,12 +33,14 @@ SONIC_MGMT_DIR = os.path.dirname(ANSIBLE_DIR)
 class GenerateGoldenConfigDBModule(object):
     def __init__(self):
         self.module = AnsibleModule(argument_spec=dict(topo_name=dict(required=True, type='str'),
-                                                       port_index_map=dict(require=False, type='dict', default=None),
-                                                       macsec_profile=dict(require=False, type='str', default=None)),
+                                    port_index_map=dict(require=False, type='dict', default=None),
+                                    macsec_profile=dict(require=False, type='str', default=None),
+                                    num_asics=dict(require=False, type='int', default=1)),
                                     supports_check_mode=True)
         self.topo_name = self.module.params['topo_name']
         self.port_index_map = self.module.params['port_index_map']
         self.macsec_profile = self.module.params['macsec_profile']
+        self.num_asics = self.module.params['num_asics']
 
     def generate_mgfx_golden_config_db(self):
         rc, out, err = self.module.run_command("sonic-cfggen -H -m -j /etc/sonic/init_cfg.json --print-data")
@@ -125,13 +128,25 @@ class GenerateGoldenConfigDBModule(object):
             for k, v in list(macsec_profiles.items()):
                 if k == self.macsec_profile:
                     profile = v
+                    # Update the macsec profile name in the profile context
+                    profile['macsec_profile'] = k
                     break
-            
+
+            # Update the profile context with the asic count
+            profile['asic_cnt'] = self.num_asics
+
             # Render the template using the profile
+            ANSIBLE_TEMPLATES_DIR = ANSIBLE_DIR + '/templates'
+            j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(ANSIBLE_TEMPLATES_DIR))
+            j2_template = j2_env.get_template('golden_config_db_t2.j2')
+            rendered_json = j2_template.render(profile)
+
+            return rendered_json
 
     def generate(self):
         if self.topo_name == "mx":
             config = self.generate_mx_golden_config_db()
+            self.module.run_command("sudo rm -f {}".format(TEMP_DHCP_SERVER_CONFIG_PATH))
         else if self.topo_name == "t2":
             config = self.generate_t2_golden_config_db()
         else:
@@ -139,8 +154,6 @@ class GenerateGoldenConfigDBModule(object):
 
         with open(GOLDEN_CONFIG_DB_PATH, "w") as temp_file:
             temp_file.write(config)
-        self.module.run_command("sudo rm -f {}".format(TEMP_DHCP_SERVER_CONFIG_PATH))
-        self.module.run_command("sudo rm -f {}".format(TEMP_SMARTSWITCH_CONFIG_PATH))
         self.module.exit_json(change=True, msg="Success to generate golden_config_db.json")
 
 
