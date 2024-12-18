f000f7e8a0b452ebe64304fc16c9b90291651cd9
Yutong Zhang <yutongzhang@microsoft.com>
2024-12-18 14:16:10 +0800

    Modify as comment

--- .azure-pipelines/markers_check/markers_check.py
+++ .azure-pipelines/markers_check/markers_check.py
@@ -6,17 +6,17 @@ import logging
 from natsort import natsorted
 
 
-def collect_scripts_and_markers():
-    '''
-    This function collects all test scripts under the folder 'tests/' and get the topology type marked in the script.
-    If there is no such marker, we will exit with status code 1.
-    If there are some exceptions occurred, we will exit with status code 2.
-    Otherwise, we will exit with status code 0.
-    '''
+def check_scripts_and_markers():
+    """
+    This function collects all test scripts under the folder 'tests/' and check the topology type marked in the script.
+
+    Returns:
+        List: A list of test scripts without the topology type marker.
+    """
     location = sys.argv[1]
 
-    # Recursively find all files starting with "test_" and ending with ".py"
-    # Note: The full path and name of files are stored in a list named "files"
+    # Recursively find all scripts starting with "test_" and ending with ".py"
+    # Note: The full path and name of files are stored in a list named "scripts"
     scripts = []
     for root, dirs, script in os.walk(location):
         for s in script:
@@ -24,7 +24,7 @@ def collect_scripts_and_markers():
                 scripts.append(os.path.join(root, s))
     scripts = natsorted(scripts)
 
-    # Open each file and search for regex pattern
+    # Open each script and search for regex pattern
     pattern = re.compile(r"[^@]pytest\.mark\.topology\(([^\)]*)\)")
 
     scripts_without_marker = []
@@ -36,28 +36,33 @@ def collect_scripts_and_markers():
         try:
             with open(s, 'r') as script:
                 for line in script:
-                    # Get topology type of script from mark `pytest.mark.topology`
+                    # Get topology type of script from marker `pytest.mark.topology`
                     match = pattern.search(line)
                     if match:
                         has_markers = True
                         break
 
             if not has_markers:
-                print("\033[31mPlease add mark `pytest.mark.topology` in script {}\033[0m".format(script_name))
                 scripts_without_marker.append(script_name)
 
         except Exception as e:
-            logging.error('Exception occurred while trying to get marker in {}, error {}'.format(s, e))
-            sys.exit(2)
+            raise Exception('Exception occurred while trying to get marker in {}, error {}'.format(s, e))
 
-    if scripts_without_marker:
-        sys.exit(1)
-
-    sys.exit(0)
+    return scripts_without_marker
 
 
 def main():
-    collect_scripts_and_markers()
+    try:
+        scripts_without_marker = check_scripts_and_markers()
+
+        for script in scripts_without_marker:
+            print("\033[31mPlease add mark `pytest.mark.topology` in script {}\033[0m".format(script))
+            sys.exit(1)
+
+        sys.exit(0)
+    except Exception as e:
+        logging.error(e)
+        sys.exit(2)
 
 
 if __name__ == '__main__':
