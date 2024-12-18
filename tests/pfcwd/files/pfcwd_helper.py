sonic-net/sonic-mgmt

commit e8ad4a806d9f821e604be88e9f3b7032e6043d64

Fix #14711 cherry pick from #15850 (#15946)
* add test case for cli show pfcwd stat (#14711)

What is the motivation for this PR?
#11169

How did you do it?
Add a new test case to cover "pfcwd show stats" cmd

Trigger PFC storm with action drop or forward
send_tx_egress and send_rx_ingress
execute CMD 'show pfcwd stat' and ensure the output is expected
How did you verify/test it?
https://elastictest.org/scheduler/testplan/66f268ae4216a91fd43d97b6
https://elastictest.org/scheduler/testplan/66f268880369ccd340b3ea60

* chore: fix conflict with #14580

Signed-off-by: Austin Pham <austinpham@microsoft.com>

---------

Signed-off-by: Austin Pham <austinpham@microsoft.com>
Co-authored-by: Liping Xu <108326363+lipxu@users.noreply.github.com>

tests/pfcwd/files/pfcwd_helper.py
@@ -565,15 +565,60 @@ def has_neighbor_device(setup_pfc_test):
     return True
 
 
-def check_pfc_storm_state(dut, port, queue):
+def check_pfc_storm_state(dut, port, queue, expected_state):
     """
     Helper function to check if PFC storm is detected/restored on a given queue
     """
-    pfcwd_stats = dut.show_and_parse("show pfcwd stats")
-    queue_name = str(port) + ":" + str(queue)
-    for entry in pfcwd_stats:
-        if entry["queue"] == queue_name:
-            logger.info("PFCWD status on queue {} stats: {}".format(queue_name, entry))
-            return entry['storm detected/restored']
-    logger.info("PFCWD not triggered on queue {}".format(queue_name))
-    return None
+    pfcwd_stat = parser_show_pfcwd_stat(dut, port, queue)
+    if expected_state == "storm":
+        if ("storm" in pfcwd_stat[0]['status']) and \
+                int(pfcwd_stat[0]['storm_detect_count']) > int(pfcwd_stat[0]['restored_count']):
+            return True
+    else:
+        if ("storm" not in pfcwd_stat[0]['status']) and \
+                int(pfcwd_stat[0]['storm_detect_count']) == int(pfcwd_stat[0]['restored_count']):
+            return True
+    return False
+
+
+def parser_show_pfcwd_stat(dut, select_port, select_queue):
+    """
+    CLI "show pfcwd stats" output:
+    admin@bjw-can-7060-1:~$ show pfcwd stats
+            QUEUE    STATUS    STORM DETECTED/RESTORED    TX OK/DROP    RX OK/DROP    TX LAST OK/DROP    RX LAST OK/DROP # noqa: E501
+    -------------  --------  -------------------------  ------------  ------------  -----------------  ----------------- # noqa: E501
+    Ethernet112:4       N/A                        2/2       100/100       100/100              100/0              100/0 # noqa: E501
+    admin@bjw-can-7060-1:~$
+    """
+    logger.info("port {} queue {}".format(select_port, select_queue))
+    pfcwd_stat_output = dut.show_and_parse('show pfcwd stat')
+
+    pfcwd_stat = []
+    for item in pfcwd_stat_output:
+        port, queue = item['queue'].split(':')
+        if port != select_port or int(queue) != int(select_queue):
+            continue
+        storm_detect_count, restored_count = item['storm detected/restored'].split('/')
+        tx_ok_count, tx_drop_count = item['tx ok/drop'].split('/')
+        rx_ok_count, rx_drop_count = item['rx ok/drop'].split('/')
+        tx_last_ok_count, tx_last_drop_count = item['tx last ok/drop'].split('/')
+        rx_last_ok_count, rx_last_drop_count = item['rx last ok/drop'].split('/')
+
+        parsed_dict = {
+            'port': port,
+            'queue': queue,
+            'status': item['status'],
+            'storm_detect_count': storm_detect_count,
+            'restored_count': restored_count,
+            'tx_ok_count': tx_ok_count,
+            'tx_drop_count': tx_drop_count,
+            'rx_ok_count': rx_ok_count,
+            'rx_drop_count': rx_drop_count,
+            'tx_last_ok_count': tx_last_ok_count,
+            'tx_last_drop_count': tx_last_drop_count,
+            'rx_last_ok_count': rx_last_ok_count,
+            'rx_last_drop_count': rx_last_drop_count
+        }
+        pfcwd_stat.append(parsed_dict)
+
+    return pfcwd_stat

tests/pfcwd/test_pfcwd_function.py
@@ -721,10 +721,6 @@ class TestPfcwdFunc(SetupPfcwdFunc):
         test_ports_info = {self.pfc_wd['rx_port'][0]: self.pfc_wd}
         queues = [self.storm_hndle.pfc_queue_idx]
 
-        if dut.facts['asic_type'] == "mellanox":
-            PFC_STORM_TIMEOUT = 30
-            pfcwd_stats_before_test = check_pfc_storm_state(dut, port, self.storm_hndle.pfc_queue_idx)
-
         with send_background_traffic(dut, self.ptf, queues, selected_test_ports, test_ports_info):
             if action != "dontcare":
                 start_wd_on_ports(dut, port, restore_time, detect_time, action)
@@ -744,9 +740,14 @@ class TestPfcwdFunc(SetupPfcwdFunc):
             if dut.facts['asic_type'] == "mellanox":
                 # On Mellanox platform, more time is required for PFC storm being triggered
                 # as PFC pause sent from Non-Mellanox leaf fanout is not continuous sometimes.
-                pytest_assert(wait_until(PFC_STORM_TIMEOUT, 2, 0,
-                                        lambda: check_pfc_storm_state(dut, port, self.storm_hndle.pfc_queue_idx) != pfcwd_stats_before_test),  # noqa: E501, E128
-                                        "PFC storm state did not change as expected")  # noqa: E127
+                PFC_STORM_TIMEOUT = 30
+                pytest_assert(
+                    wait_until(
+                        PFC_STORM_TIMEOUT, 2, 5,
+                        check_pfc_storm_state, dut, port, self.storm_hndle.pfc_queue_idx, "storm"
+                    ),
+                    "PFC storm state did not change as expected"
+                )
             else:
                 time.sleep(5)
 

commit 77c2f898b15d7ef05f7a58140c1fb28aec5f50eb

Fix test_pfcwd_function on Mellanox platform (#16117)
Description of PR
Summary:
This PR is to fix test issue caused by PR #15946

The code below is to ensure there is PFCWD triggered on new queues before checking syslog.

sonic-mgmt/tests/pfcwd/test_pfcwd_function.py

Lines 739 to 741 in 15bf903

 pytest_assert(wait_until(PFC_STORM_TIMEOUT, 2, 0, 
                         lambda: check_pfc_storm_state(dut, port, self.storm_hndle.pfc_queue_idx) != pfcwd_stats_before_test),  # noqa: E501, E128 
                         "PFC storm state did not change as expected")  # noqa: E127 
The test is flaky after change in #15946 because the wait time is not enough for PFCWD to be triggered on new queues.

Approach
What is the motivation for this PR?
This PR is to fix test issue caused by PR #15946

How did you do it?
Change the logic back while maintain compatibility with chassis.

How did you verify/test it?
The change is verified on a Mellanox platform.

collected 5 items                                                                                                                                                                                     

pfcwd/test_pfcwd_function.py::TestPfcwdFunc::test_pfcwd_actions[str2-msn4600c-acs-04]  ^H ^H ^H ^HPASSED                                                                                                    [ 20%]
pfcwd/test_pfcwd_function.py::TestPfcwdFunc::test_pfcwd_multi_port[str2-msn4600c-acs-04]  ^H ^H ^HPASSED                                                                                                 [ 40%]
pfcwd/test_pfcwd_function.py::TestPfcwdFunc::test_pfcwd_mmu_change[str2-msn4600c-acs-04]  ^H ^HPASSED                                                                                                 [ 60%]

tests/pfcwd/files/pfcwd_helper.py
@@ -566,9 +566,23 @@ def has_neighbor_device(setup_pfc_test):
     return True
 
 
-def check_pfc_storm_state(dut, port, queue, expected_state):
+def check_pfc_storm_state(dut, port, queue):
     """
     Helper function to check if PFC storm is detected/restored on a given queue
+    """
+    pfcwd_stats = dut.show_and_parse("show pfcwd stats")
+    queue_name = str(port) + ":" + str(queue)
+    for entry in pfcwd_stats:
+        if entry["queue"] == queue_name:
+            logger.info("PFCWD status on queue {} stats: {}".format(queue_name, entry))
+            return entry['storm detected/restored']
+    logger.info("PFCWD not triggered on queue {}".format(queue_name))
+    return None
+
+
+def verify_pfc_storm_in_expected_state(dut, port, queue, expected_state):
+    """
+    Helper function to verify if PFC storm on a specific queue is in expected state
     """
     pfcwd_stat = parser_show_pfcwd_stat(dut, port, queue)
     if expected_state == "storm":

tests/pfcwd/test_pfcwd_cli.py
@@ -12,7 +12,7 @@ from tests.ptf_runner import ptf_runner
 from tests.common import constants
 from tests.common.dualtor.dual_tor_utils import is_tunnel_qos_remap_enabled, dualtor_ports # noqa F401
 from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_enum_rand_one_per_hwsku_frontend_host_m # noqa F401, E501
-from .files.pfcwd_helper import send_background_traffic, check_pfc_storm_state, parser_show_pfcwd_stat
+from .files.pfcwd_helper import send_background_traffic, verify_pfc_storm_in_expected_state, parser_show_pfcwd_stat
 from tests.common.utilities import wait_until
 
 pytestmark = [
@@ -297,7 +297,7 @@ class TestPfcwdFunc(SetupPfcwdFunc):
 
         logger.info("Verify if PFC storm is detected on port {}".format(port))
         pytest_assert(
-            wait_until(30, 2, 5, check_pfc_storm_state, dut, port, self.storm_hndle.pfc_queue_idx, "storm"),
+            wait_until(30, 2, 5, verify_pfc_storm_in_expected_state, dut, port, self.storm_hndle.pfc_queue_idx, "storm"), # noqa E501
             "PFC storm state did not change as expected"
         )
 
@@ -316,7 +316,7 @@ class TestPfcwdFunc(SetupPfcwdFunc):
         # storm restore
         logger.info("Verify if PFC storm is restored on port {}".format(port))
         pytest_assert(
-            wait_until(30, 2, 5, check_pfc_storm_state, dut, port, self.storm_hndle.pfc_queue_idx, "restore"),
+            wait_until(30, 2, 5, verify_pfc_storm_in_expected_state, dut, port, self.storm_hndle.pfc_queue_idx, "restore"), # noqa E501
             "PFC storm state did not change as expected"
         )
 

tests/pfcwd/test_pfcwd_function.py
@@ -721,6 +721,10 @@ class TestPfcwdFunc(SetupPfcwdFunc):
         test_ports_info = {self.pfc_wd['rx_port'][0]: self.pfc_wd}
         queues = [self.storm_hndle.pfc_queue_idx]
 
+        if dut.facts['asic_type'] == "mellanox":
+            PFC_STORM_TIMEOUT = 30
+            pfcwd_stats_before_test = check_pfc_storm_state(dut, port, self.storm_hndle.pfc_queue_idx)
+
         with send_background_traffic(dut, self.ptf, queues, selected_test_ports, test_ports_info):
             if action != "dontcare":
                 start_wd_on_ports(dut, port, restore_time, detect_time, action)
@@ -740,14 +744,9 @@ class TestPfcwdFunc(SetupPfcwdFunc):
             if dut.facts['asic_type'] == ["mellanox", "cisco-8000"]:
                 # On Mellanox platform, more time is required for PFC storm being triggered
                 # as PFC pause sent from Non-Mellanox leaf fanout is not continuous sometimes.
-                PFC_STORM_TIMEOUT = 30
-                pytest_assert(
-                    wait_until(
-                        PFC_STORM_TIMEOUT, 2, 5,
-                        check_pfc_storm_state, dut, port, self.storm_hndle.pfc_queue_idx, "storm"
-                    ),
-                    "PFC storm state did not change as expected"
-                )
+                pytest_assert(wait_until(PFC_STORM_TIMEOUT, 2, 0,
+                                        lambda: check_pfc_storm_state(dut, port, self.storm_hndle.pfc_queue_idx) != pfcwd_stats_before_test),  # noqa: E501, E128
+                                        "PFC storm state did not change as expected")  # noqa: E127
             else:
                 time.sleep(5)
 