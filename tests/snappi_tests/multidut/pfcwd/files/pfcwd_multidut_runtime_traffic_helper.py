78f668683757e9b92940700bfd98953411119f5e
selldinesh <dinesh.sellappan@keysight.con>
2024-12-18 18:11:53 +0000

    Adding changes to accommodate new snappi model

--- tests/snappi_tests/multidut/pfcwd/files/pfcwd_multidut_runtime_traffic_helper.py
+++ tests/snappi_tests/multidut/pfcwd/files/pfcwd_multidut_runtime_traffic_helper.py
@@ -195,9 +195,9 @@ def __run_traffic(api, config, duthost, port, all_flow_names, pfcwd_start_delay_
     wait_for_arp(api, max_attempts=10, poll_interval_sec=2)
 
     logger.info('Starting transmit on all flows ...')
-    ts = api.transmit_state()
-    ts.state = ts.START
-    api.set_transmit_state(ts)
+    ts = api.control_state()
+    ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.START
+    api.set_control_state(ts)
 
     time.sleep(pfcwd_start_delay_sec)
     start_pfcwd(duthost, port['asic_value'])
@@ -229,9 +229,9 @@ def __run_traffic(api, config, duthost, port, all_flow_names, pfcwd_start_delay_
     rows = api.get_metrics(request).flow_metrics
 
     logger.info('Stop transmit on all flows ...')
-    ts = api.transmit_state()
-    ts.state = ts.STOP
-    api.set_transmit_state(ts)
+    ts = api.control_state()
+    ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.STOP
+    api.set_control_state(ts)
 
     return rows
 
