9bab128bfe620a95758062548623980845d9d6fb
Anton Hryshchuk <76687950+AntonHryshchuk@users.noreply.github.com>
2024-12-18 11:50:30 +0200

    [lag_member] add missing imports required to test (#16021)
    During movement test to Python 3 (#14944), were deleted necessary imports of fixtures with "autouse=True":
    copy_ptftests_directory
    copy_arp_responder_py
    
    copy_arp_responder_py:
    The fixture copied required configuration files. The arp responder configurations used in the test by method setup_arp_responder
    
    Signed-off-by: AntonHryshchuk <antonh@nvidia.com>

--- tests/pc/test_lag_member.py
+++ tests/pc/test_lag_member.py
@@ -10,7 +10,7 @@ from collections import Counter
 from tests.common.helpers.assertions import pytest_assert, pytest_require
 from tests.ptf_runner import ptf_runner
 from tests.common.utilities import wait_until
-from tests.common.fixtures.ptfhost_utils import copy_acstests_directory # noqa F401
+from tests.common.fixtures.ptfhost_utils import copy_acstests_directory, copy_ptftests_directory, copy_arp_responder_py # noqa F401
 from tests.common.config_reload import config_reload
 
 logger = logging.getLogger(__name__)
