8c0862e9b6366567291ee723c565bc68c961a2bd
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-12-19 14:10:39 +0800

    Transite to conditional marks for skipping T0 test scripts (#16083)
    What is the motivation for this PR?
    Previously, we used the pr_test_skip_scripts.yaml file to maintain a list of test scripts that were temporarily excluded from PR testing. In the new PR testing model, we are moving away from hardcoded scripts and will instead use conditional markers to skip tests as needed. Since there are numerous scripts to address, we will handle them incrementally by dividing them across different PRs based on their respective topologies.
    
    How did you do it?
    In the new PR testing model, we are moving away from hardcoded scripts and will instead use conditional markers to skip tests as needed. Since there are numerous scripts to address, we will handle them incrementally by dividing them across different PRs based on their respective topologies.
    
    How did you verify/test it?
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/platform_tests/api/test_psu.py
+++ tests/platform_tests/api/test_psu.py
@@ -25,7 +25,6 @@ logger = logging.getLogger(__name__)
 
 pytestmark = [
     pytest.mark.topology('any'),
-    pytest.mark.device_type('physical'),
     pytest.mark.disable_loganalyzer  # disable automatic loganalyzer
 ]
 
