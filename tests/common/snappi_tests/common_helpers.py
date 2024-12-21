f5d0d675514895762948276fc3d9e32bda8c0c83
sreejithsreekumaran <60534136+sreejithsreekumaran@users.noreply.github.com>
2024-12-21 02:39:10 +0000

    ECN probabilistic marking with PFC induced congestion (#15909)
    Description of PR
    Summary:
    Fixes # (issue)
    
    Approach
    What is the motivation for this PR?
    Add IXIA based test case to demonstrate ECN marking. The test will inject XOFF frame(s) to congest a queue that results in ECN marking.
    
    How did you do it?
    Using Snappi infra
    
    How did you verify/test it?
    setup continous traffic at 99.98% line rate with 1350B packets
    Test sends a single XOFF frame to the egress port to create congestion in the queue3.
    A pfc frame is sent with quanta set to values between 500 and 65000, one at a time -
    read the base ECN counter using serviceability CLI
    inject XOFF-
    read the ECN counters
    At the end of the iteration, once the data is collected, ensure that for each case, with increase in quanta, the marked count at index (n+1) is >= count observed with collection at index (n). Further if the marked count at index (n) > 0, verify that once marking happens, the marking probability increases with an increase in queue occupancy caused by increase in quanta
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/common/snappi_tests/common_helpers.py
+++ tests/common/snappi_tests/common_helpers.py
@@ -438,7 +438,7 @@ def get_wred_profiles(host_ans, asic_value=None):
         return None
 
 
-def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
+def config_wred(host_ans, kmin, kmax, pmax, kdrop=None, profile=None, asic_value=None):
     """
     Config a WRED/ECN profile of a SONiC switch
     Args:
@@ -456,10 +456,11 @@ def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
     asic_type = str(host_ans.facts["asic_type"])
     if not isinstance(kmin, int) or \
        not isinstance(kmax, int) or \
-       not isinstance(pmax, int):
+       not isinstance(pmax, int) or \
+       (kdrop is not None and not isinstance(kdrop, int)):
         return False
 
-    if kmin < 0 or kmax < 0 or pmax < 0 or pmax > 100 or kmin > kmax:
+    if kmin < 0 or kmax < 0 or pmax < 0 or pmax > 100 or kmin > kmax or (kdrop and (kdrop < 0 or kdrop > 100)):
         return False
     profiles = get_wred_profiles(host_ans, asic_value)
     """ Cannot find any WRED/ECN profiles """
@@ -478,6 +479,7 @@ def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
 
     kmax_arg = '-{}max'.format(color[0])
     kmin_arg = '-{}min'.format(color[0])
+    kdrop_arg = '-{}drop'.format(color[0])
 
     for p in profiles:
         """ This is not the profile to configure """
@@ -486,6 +488,7 @@ def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
 
         kmin_old = int(profiles[p]['{}_min_threshold'.format(color)])
         kmax_old = int(profiles[p]['{}_max_threshold'.format(color)])
+        kdrop_old = int(profiles[p]['{}_drop_probability'.format(color)])
 
         if kmin_old > kmax_old:
             return False
@@ -494,10 +497,12 @@ def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
 
         kmax_cmd = ' '.join(['sudo ecnconfig -p {}', kmax_arg, '{}'])
         kmin_cmd = ' '.join(['sudo ecnconfig -p {}', kmin_arg, '{}'])
+        kdrop_cmd = ' '.join(['sudo ecnconfig -p {}', kdrop_arg, '{}'])
 
         if asic_value is not None:
             kmax_cmd = ' '.join(['sudo ip netns exec', asic_value, 'ecnconfig -p {}', kmax_arg, '{}'])
             kmin_cmd = ' '.join(['sudo ip netns exec', asic_value, 'ecnconfig -p {}', kmin_arg, '{}'])
+            kdrop_cmd = ' '.join(['sudo ip netns exec', asic_value, 'ecnconfig -p {}', kdrop_arg, '{}'])
             if asic_type == 'broadcom':
                 disable_packet_aging(host_ans, asic_value)
 
@@ -507,6 +512,9 @@ def config_wred(host_ans, kmin, kmax, pmax, profile=None, asic_value=None):
         else:
             host_ans.shell(kmin_cmd.format(p, kmin))
             host_ans.shell(kmax_cmd.format(p, kmax))
+
+        if kdrop and kdrop != kdrop_old:
+            host_ans.shell(kdrop_cmd.format(p, kdrop))
 
     return True
 
