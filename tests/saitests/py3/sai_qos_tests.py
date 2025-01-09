828e97eb41ff8a621b3ee35890718f888671233a
Raghavendran Ramanathan <rraghav@cisco.com>
2025-01-08 20:04:22 -0800

    Adding the new logic from PR:16315 to this PR.

--- tests/saitests/py3/sai_qos_tests.py
+++ tests/saitests/py3/sai_qos_tests.py
@@ -3819,9 +3819,14 @@ class WRRtest(sai_base_test.ThriftInterfaceDataPlane):
                 recv_pkt = scapy.Ether(received.packet)
 
         if asic_type == 'cisco-8000':
-            cmd_opt = ""
-            if 'dst_asic_index' in self.test_params:
-                cmd_opt = "-n asic{}".format(self.test_params['dst_asic_index'])
+            out, err, ret = self.exec_cmd_on_dut(
+                self.dst_server_ip,
+                self.test_params['dut_username'],
+                self.test_params['dut_password'],
+                "show platform summary | egrep 'ASIC Count' | awk -F: '{print $2}'")
+            cmd_opt = "-n asic{}".format(self.test_params['dst_asic_index'])
+            if out[0].strip() == "1":
+                cmd_opt = ""
             cmd = "sudo show platform npu script {} -s set_scheduler.py".format(cmd_opt)
             out, err, ret = self.exec_cmd_on_dut(
                 self.dst_server_ip,
@@ -3829,19 +3834,9 @@ class WRRtest(sai_base_test.ThriftInterfaceDataPlane):
                 self.test_params['dut_password'],
                 cmd)
             if err and out == []:
-                if "Invalid value for " in " ".join(err):
-                    cmd = "sudo show platform npu script -s set_scheduler.py"
-                    out, err, ret = self.exec_cmd_on_dut(
-                        self.dst_server_ip,
-                        self.test_params['dut_username'],
-                        self.test_params['dut_password'],
-                        cmd)
-                if err and out == []:
-                    raise RuntimeError("cmd({}) might have failed in the DUT. Error:{}".format(cmd, err))
-                else:
-                    print("Success in setting scheduler in sAsic DUT.", file=sys.stderr)
+                raise RuntimeError("cmd({}) might have failed in the DUT. Error:{}".format(cmd, err))
             else:
-                print("Success in setting scheduler in mAsic DUT.", file=sys.stderr)
+                print("Success in setting scheduler in DUT.", file=sys.stderr)
         else:
             # Release port
             self.sai_thrift_port_tx_enable(
