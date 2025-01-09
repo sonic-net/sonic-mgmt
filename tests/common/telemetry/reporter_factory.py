6fcf3552b62339f4ddf7ee920a69391d631f3586
sm-xu <xumei@microsoft.com>
2025-01-09 20:43:34 +0000

    update 01/09

--- tests/common/telemetry/reporter_factory.py
+++ tests/common/telemetry/reporter_factory.py
@@ -6,8 +6,10 @@ class TelemetryReporterFactory:
     def __init__(self):
         return
 
+    @staticmethod
     def create_periodic_metrics_reporter(common_labels: Dict[str, str]):
         return (PeriodicMetricsReporter(common_labels))
 
+    @staticmethod
     def create_final_metrics_reporter(common_labels: Dict[str, str]):
         return (FinalMetricsReporter(common_labels))
