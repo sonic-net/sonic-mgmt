721e8de87cf82c514c7ba739513573bb3bae8138
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-01-11 05:52:19 +0800

    Enhance Read-only disk test case, verify remote user can do TSA/TSB after monit script mitigate issue. (#16378)
    Enhance Read-only disk test case, verify remote user can do TSA/TSB after monit script mitigate issue.
    
    Why I did it
    Verify after monit fix login issue ,remote user can run TSA/TSB command.
    
    How I did it
    Check remote user can do TSA/TSB after monit script mitigate issue.
    
    How to verify it
    Pass all test case.
    
    Description for the changelog
    Enhance Read-only disk test case, verify remote user can do TSA/TSB after monit script mitigate issue.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/tacacs/test_ro_disk.py
+++ tests/tacacs/test_ro_disk.py
@@ -258,6 +258,14 @@ def test_ro_disk(localhost, ptfhost, duthosts, enum_rand_one_per_hwsku_hostname,
 
         chk_ssh_remote_run(localhost, dutip, rw_user, rw_pass, "sudo find /home -ls")
 
+        # Verify after monit fix login issue ,remote user can run TSA/TSB command.
+        res = ssh_remote_run(localhost, dutip, rw_user, rw_pass, "sudo TSA")
+        logger.debug("TSA res={}".format(res))
+        assert "System Mode: Normal -> Maintenance" in res["stdout_lines"], "Failed to TSA"
+        res = ssh_remote_run(localhost, dutip, rw_user, rw_pass, "sudo TSB")
+        logger.debug("TSB res={}".format(res))
+        assert "System Mode: Maintenance -> Normal" in res["stdout_lines"], "Failed to TSB"
+
         if not os.path.exists(DATA_DIR):
             os.makedirs(DATA_DIR)
 
