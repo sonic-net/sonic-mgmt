20a0974640511292dd05fc74e85256e7ec42c8b5
Dave Thelen <dt@nexthop.ai>
2025-01-13 03:04:35 +0000

    fixes #16471

---
+++ tests/common/fixtures/radius.py
@@ -0,0 +1,13 @@
+import logging
+import pytest
+
+from tests.radius.utils import load_radius_creds
+
+logger = logging.getLogger(__name__)
+
+@pytest.fixture(scope="module")
+def radius_creds(creds_all_duts):
+    test_creds = load_radius_creds()
+    creds_all_duts.update(test_creds)
+    return creds_all_duts
+
