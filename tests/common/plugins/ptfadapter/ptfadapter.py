69831f4f8d5e94b571e44644351b3ee2c69d0a81
Wenda Chu <32250288+w1nda@users.noreply.github.com>
2025-01-21 14:29:18 +0800

    fix topo

--- tests/common/plugins/ptfadapter/ptfadapter.py
+++ tests/common/plugins/ptfadapter/ptfadapter.py
@@ -110,7 +110,7 @@ class PtfTestAdapter(BaseTest):
         ptf.dataplane_instance.port_to_device = lambda port: ptf.dataplane_instance.port_device_map[port]
         ptf.dataplane_instance.port_to_tuple = lambda port: (ptf.dataplane_instance.port_device_map[port], port)
         ptf.dataplane_instance._poll = ptf.dataplane_instance.poll
-        ptf.dataplane_instanec.poll = lambda device_number, port_number=None, timeout=None, exp_pkt=None, filters=[]: \
+        ptf.dataplane_instance.poll = lambda device_number, port_number=None, timeout=None, exp_pkt=None, filters=[]: \
             ptf.dataplane_instance._poll(
                 ptf.dataplane_instance.port_to_device(port_number) if port_number else None,
                 port_number,
