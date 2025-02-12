950193b90b6e1b474b44cfaef5a493c463ce81e7
selldinesh <dinesh.sellappan@keysight.com>
2025-02-12 04:17:18 +0000

    Adding changes to run on different lanes

--- tests/snappi_tests/dataplane/test_fec_error_injection.py
+++ tests/snappi_tests/dataplane/test_fec_error_injection.py
@@ -2,7 +2,15 @@ from tests.snappi_tests.dataplane.imports import *        # noqa: F401
 from tests.common.snappi_tests.traffic_generation import setup_base_traffic_config     # noqa: F401
 from tests.common.snappi_tests.common_helpers import traffic_flow_mode      # noqa: F401
 from tests.common.snappi_tests.snappi_helpers import wait_for_arp, fetch_snappi_flow_metrics      # noqa: F401
+from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port, \
+    snappi_api, get_snappi_ports, is_snappi_multidut, \
+    get_snappi_ports_single_dut, \
+    get_snappi_ports_multi_dut, snappi_dut_base_config, cleanup_config, get_snappi_ports_for_rdma  # noqa: F401
 logger = logging.getLogger(__name__)
+from tests.common.snappi_tests.variables import dut_ip_start, snappi_ip_start, \
+    prefix_length, dut_ipv6_start, snappi_ipv6_start, v6_prefix_length
+from tests.snappi_tests.variables import create_ip_list
+from snappi_tests.reboot.files.reboot_helper import get_macs
 pytestmark = [pytest.mark.topology('tgen')]
 
 ErrorTypes = ['codeWords',
@@ -20,127 +28,200 @@ def get_ti_stats(ixnet):
     return tmp
 
 
-@pytest.fixture(scope="function", autouse=True)
-def setup_config(snappi_api,
-                 snappi_testbed_config,
-                 conn_graph_facts,
-                 fanout_graph_facts,
-                 duthosts,
-                 rand_one_dut_portname_oper_up,
-                 rand_one_dut_hostname):
-    """
-    Fixture to initialize resources for the test.
-    """
-    dut_hostname, dut_port = rand_one_dut_portname_oper_up.split('|')
-    testbed_config, port_config_list = snappi_testbed_config
-    duthost = duthosts[rand_one_dut_hostname]
-    api = snappi_api
-    conn_data = conn_graph_facts
-    fanout_data = fanout_graph_facts
-    snappi_extra_params = None    
+def get_fanout_port_groups(snappi_ports, fanout_per_port):
+    num_groups = int(len(snappi_ports)/fanout_per_port) 
+    group_list = []
+    for i in range(fanout_per_port):  
+        group=[]
+        for j in range(num_groups):
+            group.append(snappi_ports[i+j*fanout_per_port])
+        pytest_assert(len(group)%2==0,'Must have Even number of front panel ports to have equal Tx and Rx ports')
+        group_list.append(tuple(group))
+    return group_list
 
-    if snappi_extra_params is None:
-        snappi_extra_params = SnappiTestParams()
-    port_id = get_dut_port_id(duthost.hostname,
-                              dut_port,
-                              conn_data,
-                              fanout_data)
 
-    pytest_assert(port_id is not None,
-                'Fail to get ID for port {}'.format(dut_port))
-    snappi_extra_params.base_flow_config = setup_base_traffic_config(testbed_config=testbed_config,
-                                                                     port_config_list=port_config_list,
-                                                                     port_id=port_id)
-    base_flow = snappi_extra_params.base_flow_config
-    test_flow = testbed_config.flows.flow(name='IPv4 Traffic')[-1]
-    test_flow.tx_rx.device.tx_names = [testbed_config.devices[0].name]
-    test_flow.tx_rx.device.rx_names = [testbed_config.devices[1].name]
+def configure_dut_interface(duthost, fanout_port_group):
+    dut_ip_list = create_ip_list(dut_ip_start, len(fanout_port_group), mask=prefix_length)
+    snappi_ip_list = create_ip_list(snappi_ip_start, len(fanout_port_group), mask=prefix_length)
+    ports = [port for port in fanout_port_group if port['peer_device'] == duthost.hostname]
+    for index, port in enumerate(ports):
+        logger.info('Configuring port {} with IP {}/{}'.
+                    format(port['peer_port'], dut_ip_list[index], prefix_length))
+        duthost.command('sudo config interface ip add {} {}/{} \n'.
+                        format(port['peer_port'], dut_ip_list[index], prefix_length))
+
+
+def cleanup_dut_interface(duthost, fanout_port_group):
+    dut_ip_list = create_ip_list(dut_ip_start, len(fanout_port_group), mask=prefix_length)
+    snappi_ip_list = create_ip_list(snappi_ip_start, len(fanout_port_group), mask=prefix_length)
+    ports = [port for port in fanout_port_group if port['peer_device'] == duthost.hostname]
+    for index, port in enumerate(ports):
+        logger.info('Removing {}/{} from port {}'.
+                    format(dut_ip_list[index], prefix_length, port['peer_port']))
+        duthost.command('sudo config interface ip remove {} {}/{} \n'.
+                        format(port['peer_port'], dut_ip_list[index], prefix_length))
+
+
+def create_snappi_config(snappi_api, fanout_port_group):
+    half_ports = int(len(fanout_port_group)/2)
+    tx_ports = fanout_port_group[:half_ports]
+    rx_ports = fanout_port_group[half_ports:]
+    config = snappi_api.config()
+
+    for index, tx_port in enumerate(tx_ports):
+        config.ports.port(name='Tx_%d' %
+                          index, location=tx_port['location'])
+        
+    for index, rx_port in enumerate(rx_ports):
+        config.ports.port(name='Rx_%d' %
+                          index, location=rx_port['location'])
+
+    config.options.port_options.location_preemption = True
+    layer1 = config.layer1.layer1()[-1]
+    layer1.name = 'port settings'
+    layer1.port_names = [port.name for port in config.ports]
+    layer1.ieee_media_defaults = False
+    layer1.auto_negotiation.rs_fec = False
+    layer1.auto_negotiation.link_training = False
+    layer1.speed = 'speed_'+str(int(int(fanout_port_group[0]['speed'])/1000))+'_gbps'
+    layer1.auto_negotiate = False
+    # Tx
+    macs_tx = get_macs("101700000011", len(tx_ports))
+    macs_rx = get_macs("001700000011", len(rx_ports))
+    dut_ip_list = create_ip_list(dut_ip_start, len(fanout_port_group), mask=prefix_length)
+    snappi_ip_list = create_ip_list(snappi_ip_start, len(fanout_port_group), mask=prefix_length)
+    tx_flow_name = []
+    for index, tx_port in enumerate(tx_ports):
+        d1 = config.devices.device(name='Tx Topology {}'.format(index))[-1]
+        eth = d1.ethernets.add()
+        eth.connection.port_name = 'Tx_%d' % index
+        eth.name = 'Tx_Ethernet_%d' % index
+        eth.mac = macs_tx[index]
+        ipv4 = eth.ipv4_addresses.add()
+        ipv4.name = 'Tx_IPv4_%d' % index
+        ipv4.address = snappi_ip_list[:half_ports][index]
+        ipv4.gateway = dut_ip_list[:half_ports][index]
+        ipv4.prefix = prefix_length
+        tx_flow_name.append(d1.name)
+
+    # Rx
+    rx_flow_name = []
+    for index, rx_port in enumerate(rx_ports):
+        d2 = config.devices.device(name='Rx Topology {}'.format(index))[-1]
+        eth = d2.ethernets.add()
+        eth.connection.port_name = 'Rx_%d' % index
+        eth.name = 'Rx_Ethernet_%d' % index
+        eth.mac = macs_rx[index]
+        ipv4 = eth.ipv4_addresses.add()
+        ipv4.name = 'Rx_IPv4_%d' % index
+        ipv4.address = snappi_ip_list[half_ports:][index]
+        ipv4.gateway = dut_ip_list[half_ports:][index]
+        ipv4.prefix = prefix_length
+        rx_flow_name.append(d2.name)
+
+    test_flow = config.flows.flow(name='IPv4 Traffic')[-1]
+    test_flow.tx_rx.device.tx_names = tx_flow_name
+    test_flow.tx_rx.device.rx_names = rx_flow_name
     test_flow.metrics.enable = True
     test_flow.metrics.loss = True
-    test_flow.size.fixed = 64
-    test_flow.rate.percentage = 10
-    api.set_config(testbed_config)
-    test_platform = TestPlatform(snappi_api._address)
-    test_platform.Authenticate(snappi_api._username, snappi_api._password)
-    id = test_platform.Sessions.find()[-1].Id
-    session_assistant = SessionAssistant(IpAddress=snappi_api._address,
-                                         RestPort=snappi_api._port,
-                                         SessionId=id,
-                                         UserName=snappi_api._username,
-                                         Password=snappi_api._password)
-    yield api, port_config_list, session_assistant
+    test_flow.size.fixed = 512
+    test_flow.rate.percentage = 100
+    return config
 
 
+@pytest.mark.parametrize('fanout_per_port', [2])
 @pytest.mark.parametrize('error_type', ErrorTypes)
-def test_fec_error_injection(duthosts,
-                             error_type,
-                             setup_config):
+def test_fec_error_injection(duthost,
+                             snappi_api,
+                             get_snappi_ports,
+                             fanout_graph_facts_multidut,
+                             fanout_per_port,
+                             error_type):
     """
     Test to check if packets get dropped on injecting fec errors
     """
-    api, port_config_list, session_assistant = setup_config
-    ixnet = api._ixnetwork
-    logger.info("Wait for Arp to Resolve ...")
-    wait_for_arp(api, max_attempts=30, poll_interval_sec=2)
+    snappi_extra_params = SnappiTestParams()
+    snappi_ports = get_snappi_ports
+    fanout_port_group_list = get_fanout_port_groups(snappi_ports,fanout_per_port)
+    for iteration ,fanout_port_group in enumerate(fanout_port_group_list):
+        logger.info('|----------------------------------------|')
+        logger.info('Iteration: {} | Using Fanout Ports :- \n'.format(iteration+1))
+        for port in fanout_port_group:
+            logger.info(port['peer_port'] +' : ' + port['location'] + ' : ' + port['snappi_speed_type'])
+        logger.info('|----------------------------------------|\n')
+        configure_dut_interface(duthost, fanout_port_group )
+        snappi_config= create_snappi_config(snappi_api, fanout_port_group)
+        snappi_api.set_config(snappi_config)
+        
+        ixnet = snappi_api._ixnetwork
+        logger.info("Wait for Arp to Resolve ...")
+        wait_for_arp(snappi_api, max_attempts=30, poll_interval_sec=2)
 
-    port1 = ixnet.Vport.find()[port_config_list[0].id]
-    logger.info('|----------------------------------------|')
-    logger.info('| Setting FEC Error Type to : {} |'.format(error_type))
-    logger.info('|----------------------------------------|')
-    port1.L1Config.FecErrorInsertion.ErrorType = error_type
-    if error_type == 'codeWords':
-        port1.L1Config.FecErrorInsertion.PerCodeword = 16
-    port1.L1Config.FecErrorInsertion.Continuous = True
-
-    logger.info('Starting Traffic ...')
-    ts = api.control_state()
-    ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.START
-    api.set_control_state(ts)
-    wait(10, "For traffic to start")
-    try:
-        logger.info('Starting FEC Error Insertion')
-        port1.StartFecErrorInsertion()
-        wait(15, "For error insertion to start")
-        logger.info('Dumping Traffic Item statistics :\n {}'.format(tabulate(get_ti_stats(ixnet), headers='keys', tablefmt='psql')))
-        if error_type == 'minConsecutiveUncorrectableWithLossOfLink' or error_type == 'codeWords' or error_type == 'laneMarkers':
-            pytest_assert(duthosts[0].links_status_down(port_config_list[0].peer_port) is True,
-                          "FAIL: Link is still up after injecting FEC Error")
-            logger.info('|----------------------------------------|')
-            logger.info("PASS: Link Went down after injecting FEC Error: {}".format(error_type))
-            logger.info('|----------------------------------------|')
-        elif error_type == 'maxConsecutiveUncorrectableWithoutLossOfLink':
-            pytest_assert(duthosts[0].links_status_down(port_config_list[0].peer_port) is False,
-                          "FAIL: Link went down after injecting FEC Error")
-            logger.info('|----------------------------------------|')
-            logger.info("PASS: Link didn't go down after injecting FEC Error: {}".format(error_type))
-            logger.info('|----------------------------------------|')
-
-        flow_metrics = fetch_snappi_flow_metrics(api, ['IPv4 Traffic'])[0]
-        pytest_assert(flow_metrics.frames_tx > 0 and int(flow_metrics.loss) > 0,
-                    "FAIL: Rx Port did not stop receiving packets after starting FEC Error Insertion")
-        logger.info('PASS : Rx Port stopped receiving packets after starting FEC Error Insertion')
-        logger.info('Stopping FEC Error Insertion')
-        port1.StopFecErrorInsertion()
-        wait(15, "For error insertion to stop")
-        if error_type == 'minConsecutiveUncorrectableWithLossOfLink' or error_type == 'laneMarkers' or error_type == 'codeWords':
-            pytest_assert(duthosts[0].links_status_down(port_config_list[0].peer_port) is False,
-                          "FAIL: Link is still down after stopping FEC Error")
-            logger.info('|----------------------------------------|')
-            logger.info("PASS: Link is up after stopping FEC Error injection: {}".format(error_type))
-            logger.info('|----------------------------------------|')
-        ixnet.ClearStats()
-        wait(10, "For clear stats operation to complete")
-        logger.info('Dumping Traffic Item statistics :\n {}'.
-                    format(tabulate(get_ti_stats(ixnet), headers='keys', tablefmt='psql')))
-        flow_metrics = fetch_snappi_flow_metrics(api, ['IPv4 Traffic'])[0]
-        pytest_assert(int(flow_metrics.frames_rx_rate) > 0 and int(flow_metrics.loss) == 0,
-                      "FAIL: Rx Port did not resume receiving packets after stopping FEC Error Insertion")
-        logger.info('PASS : Rx Port resumed receiving packets after stopping FEC Error Insertion')
-        logger.info('Stopping Traffic ...')
-        ts = api.control_state()
-        ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.STOP
-        api.set_control_state(ts)
-        wait(10, "For traffic to stop")
-    finally:
-        logger.info('....Finally Block')
-        port1.StopFecErrorInsertion()
+        tx_ports, rx_ports = [], []
+        for port in ixnet.Vport.find():
+            if 'Tx' in port.Name:
+                tx_ports.append(port)
+            else:
+                rx_ports.append(port)
+        logger.info('\n\n')
+        logger.info('\t Setting FEC Error Type to : {} on Snappi ports :-'.format(error_type))
+        for port in tx_ports:
+            port.L1Config.FecErrorInsertion.ErrorType = error_type
+            logger.info(port.Name)
+            if error_type == 'codeWords':
+                port.L1Config.FecErrorInsertion.PerCodeword = 16
+            port.L1Config.FecErrorInsertion.Continuous = True
+        logger.info('|----------------------------------------|')
+        logger.info('Starting Traffic ...')
+        ts = snappi_api.control_state()
+        ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.START
+        snappi_api.set_control_state(ts)
+        wait(10, "For traffic to start")
+        try:
+            logger.info('Starting FEC Error Insertion')
+            [port.StartFecErrorInsertion() for port in tx_ports]
+            wait(15, "For error insertion to start")
+            logger.info('Dumping Traffic Item statistics :\n {}'.
+                        format(tabulate(get_ti_stats(ixnet), headers='keys', tablefmt='psql')))
+            for snappi_port in tx_ports:
+                for port in fanout_port_group:
+                    if port['location'] == snappi_port.Location:
+                        if error_type == 'minConsecutiveUncorrectableWithLossOfLink' or error_type == 'codeWords' or error_type == 'laneMarkers':
+                            pytest_assert(duthost.links_status_down(port['peer_port']) is True,
+                                    "FAIL: {} is still up after injecting FEC Error".format(port['peer_port']))
+                            logger.info("PASS: {} Went down after injecting FEC Error: {}".format(port['peer_port'], error_type))
+                        elif error_type == 'maxConsecutiveUncorrectableWithoutLossOfLink':
+                            pytest_assert(duthost.links_status_down(port['peer_port']) is False,
+                                        "FAIL: {} went down after injecting FEC Error".format(port['peer_port']))
+                            logger.info("PASS: {} didn't go down after injecting FEC Error: {}".format(port['peer_port'], error_type))
+            flow_metrics = fetch_snappi_flow_metrics(snappi_api, ['IPv4 Traffic'])[0]
+            pytest_assert(flow_metrics.frames_tx > 0 and int(flow_metrics.loss) > 0,
+                        "FAIL: Rx Port did not drop packets after starting FEC Error Insertion")
+            logger.info('PASS : Snappi Rx Port observed packet drop after starting FEC Error Insertion')
+            logger.info('Stopping FEC Error Insertion')
+            [port.StopFecErrorInsertion() for port in tx_ports]
+            wait(20, "For error insertion to stop")
+            for snappi_port in tx_ports:
+                for port in fanout_port_group:
+                    if port['location'] == snappi_port.Location:
+                        if error_type == 'minConsecutiveUncorrectableWithLossOfLink' or error_type == 'codeWords' or error_type == 'laneMarkers':
+                            pytest_assert(duthost.links_status_down(port['peer_port']) is False,
+                                        "FAIL: {} is still down after stopping FEC Error".format(port['peer_port']))
+                            logger.info("PASS: {} is up after stopping FEC Error injection: {}".format(port['peer_port'], error_type))
+            ixnet.ClearStats()
+            wait(10, "For clear stats operation to complete")
+            logger.info('Dumping Traffic Item statistics :\n {}'.
+                        format(tabulate(get_ti_stats(ixnet), headers='keys', tablefmt='psql')))
+            flow_metrics = fetch_snappi_flow_metrics(snappi_api, ['IPv4 Traffic'])[0]
+            pytest_assert(int(flow_metrics.frames_rx_rate) > 0 and int(flow_metrics.loss) == 0,
+                        "FAIL: Rx Port did not resume receiving packets after stopping FEC Error Insertion")
+            logger.info('PASS : Rx Port resumed receiving packets after stopping FEC Error Insertion')
+            logger.info('Stopping Traffic ...')
+            ts = snappi_api.control_state()
+            ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.STOP
+            snappi_api.set_control_state(ts)
+            wait(10, "For traffic to stop")
+        finally:
+            logger.info('....Finally Block')
+            [port.StopFecErrorInsertion() for port in tx_ports]
+            cleanup_dut_interface(duthost, fanout_port_group)
