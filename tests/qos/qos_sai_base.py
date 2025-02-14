22c31f9d214b84b7a4f90db4302269ed77ef616d
echuawu <chuanw@nvidia.com>
2025-02-14 20:09:44 +0800

    Add t0-isolated-d16u16s1 and t0-isolated-d16u16s2 into qos sai test
    Add t0-isolated-d16u16s1 and t0-isolated-d16u16s2 into qos sai test
    
    Change-Id: I15fe4dd646e1a072354119b0cf3564daf463404a

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -40,7 +40,7 @@ class QosBase:
     SUPPORTED_T0_TOPOS = ["t0", "t0-56", "t0-56-po2vlan", "t0-64", "t0-116", "t0-35", "dualtor-56", "dualtor-64",
                           "dualtor-120", "dualtor", "dualtor-64-breakout", "t0-120", "t0-80", "t0-backend",
                           "t0-56-o8v48", "t0-8-lag", "t0-standalone-32", "t0-standalone-64", "t0-standalone-128",
-                          "t0-standalone-256", "t0-28"]
+                          "t0-standalone-256", "t0-28", "t0-isolated-d16u16s1", "t0-isolated-d16u16s2"]
     SUPPORTED_T1_TOPOS = ["t1-lag", "t1-64-lag", "t1-56-lag", "t1-backend", "t1-28-lag", "t1-32-lag",
                           "t1-isolated-d28u1"]
     SUPPORTED_PTF_TOPOS = ['ptf32', 'ptf64']
