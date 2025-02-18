9aefb503c73526a0e30e9734270fe1a3870c7d09
Matthew Soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-02-18 12:25:13 +1100

    [202012] Fix drop counter test (#10634) (#16919)
    Description of PR
    Summary:
    Cherry pick #10634
    Additionally, a few extra small fixes from master have been migrated to this branch manually (which were too difficult to cherry pick properly due to newer commits having been cherry picked to this branch already).

--- tests/drop_packets/drop_packets.py
+++ tests/drop_packets/drop_packets.py
@@ -121,14 +121,17 @@ def pkt_fields(duthosts, rand_one_dut_hostname, tbinfo):
     return test_pkt_data
 
 
-def expected_packet_mask(pkt):
+def expected_packet_mask(pkt, ip_ver):
     """ Return mask for sniffing packet """
     exp_pkt = pkt.copy()
     exp_pkt = mask.Mask(exp_pkt)
     exp_pkt.set_do_not_care_scapy(packet.Ether, 'dst')
     exp_pkt.set_do_not_care_scapy(packet.Ether, 'src')
-    exp_pkt.set_do_not_care_scapy(packet.IP, 'ttl')
-    exp_pkt.set_do_not_care_scapy(packet.IP, 'chksum')
+    if ip_ver == "ipv4" and pkt.haslayer(packet.IP):
+        exp_pkt.set_do_not_care_scapy(packet.IP, 'ttl')
+        exp_pkt.set_do_not_care_scapy(packet.IP, 'chksum')
+    elif ip_ver == "ipv6" and pkt.haslayer(packet.IPv6):
+        exp_pkt.set_do_not_care_scapy(packet.IPv6, 'hlim')
     return exp_pkt
 
 
@@ -602,7 +605,8 @@ def test_src_ip_is_multicast_addr(do_test, ptfadapter, setup, tx_dut_ports, pkt_
 
     log_pkt_params(ports_info["dut_iface"], ports_info["dst_mac"], ports_info["src_mac"], pkt_fields["ipv4_dst"], ip_src)
 
-    do_test("L3", pkt, ptfadapter, ports_info, setup["neighbor_sniff_ports"], tx_dut_ports)
+    group = "L3"
+    do_test(group, pkt, ptfadapter, ports_info, setup["neighbor_sniff_ports"], tx_dut_ports, ip_ver=ip_addr)
 
 
 def test_src_ip_is_class_e(do_test, ptfadapter, duthosts, rand_one_dut_hostname, setup, tx_dut_ports, pkt_fields, ports_info):
@@ -673,7 +677,13 @@ def test_ip_is_zero_addr(do_test, ptfadapter, setup, tx_dut_ports, pkt_fields, a
 
     logger.info(pkt_params)
 
-    do_test("L3", pkt, ptfadapter, ports_info, setup["dut_to_ptf_port_map"].values(), tx_dut_ports)
+    group = "L3"
+    # Src IP zero drops not supported on DNX platform
+    if setup.get("platform_asic") == "broadcom-dnx" and addr_direction == "src":
+        pytest.skip("Src IP zero packets are not dropped on Broadcom DNX platform currently")
+
+    do_test(group, pkt, ptfadapter, ports_info, list(setup["dut_to_ptf_port_map"].values()), tx_dut_ports,
+            ip_ver=addr_type)
 
 
 def test_dst_ip_link_local(do_test, ptfadapter, duthosts, rand_one_dut_hostname, setup, tx_dut_ports, pkt_fields, ports_info):
@@ -765,7 +775,7 @@ def test_broken_ip_header(do_test, ptfadapter, setup, tx_dut_ports, pkt_fields, 
         tcp_sport=pkt_fields["tcp_sport"],
         tcp_dport=pkt_fields["tcp_dport"]
         )
-    setattr(pkt[testutils.scapy.scapy.all.IP], pkt_field, value)
+    setattr(pkt[packet.IP], pkt_field, value)
 
     do_test("L3", pkt, ptfadapter, ports_info, setup["neighbor_sniff_ports"], tx_dut_ports, skip_counter_check=sai_acl_drop_adj_enabled)
 
@@ -785,8 +795,8 @@ def test_absent_ip_header(do_test, ptfadapter, setup, tx_dut_ports, pkt_fields, 
         tcp_sport=pkt_fields["tcp_sport"],
         tcp_dport=pkt_fields["tcp_dport"]
         )
-    tcp = pkt[testutils.scapy.scapy.all.TCP]
-    del pkt[testutils.scapy.scapy.all.IP]
+    tcp = pkt[packet.TCP]
+    del pkt[packet.IP]
     pkt.type = 0x800
     pkt = pkt/tcp
 
@@ -852,8 +862,8 @@ def test_non_routable_igmp_pkts(do_test, ptfadapter, setup, fanouthost, tx_dut_p
         pytest.skip("Test case requires explicit fanout support")
 
     from scapy.contrib.igmp import IGMP
-    Ether = testutils.scapy.Ether
-    IP = testutils.scapy.IP
+    Ether = packet.Ether
+    IP = packet.IP
 
     if "vlan" in tx_dut_ports[ports_info["dut_iface"]].lower() and msg_type == "membership_report":
         pytest.skip("Test case is not supported on VLAN interface")
