20e87e47f07592f81e81a1b846b50f9e45e38989
Feng Pan <fenpan@microsoft.com>
2025-02-19 10:08:09 +0000

    keep this PR as kvm test

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -11,7 +11,7 @@ import json
 import re
 
 from ansible.module_utils.basic import AnsibleModule
-from sonic_py_common import device_info
+from sonic_py_common import device_info, multi_asic
 
 DOCUMENTATION = '''
 module: generate_golden_config_db.py
@@ -112,6 +112,10 @@ class GenerateGoldenConfigDBModule(object):
         return gold_config_db
 
     def check_bmp_version(self):
+        # skip multi_asic first
+        if multi_asic.is_multi_asic():
+            return False
+
         output_version = device_info.get_sonic_version_info()
         build_version = output_version['build_version']
 
@@ -155,8 +159,8 @@ class GenerateGoldenConfigDBModule(object):
             "auto_restart": "enabled",
             "check_up_status": "false",
             "delayed": "False",
-            "has_global_scope": "False",
-            "has_per_asic_scope": "True",
+            "has_global_scope": "True",
+            "has_per_asic_scope": "False",
             "high_mem_alert": "disabled",
             "set_owner": "local",
             "state": "enabled",
