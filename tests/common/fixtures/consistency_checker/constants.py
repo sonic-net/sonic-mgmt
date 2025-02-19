1421113df3609935afcd23c2f34be7592a1dfce7
Ryan Garthwaite <ryangwaite@gmail.com>
2025-02-19 01:00:15 +0000

    Renamed ARISTA_KEYS to BROADCOM_KEYS
    This clarifies what the array actually resembles.

--- tests/common/fixtures/consistency_checker/constants.py
+++ tests/common/fixtures/consistency_checker/constants.py
@@ -10,7 +10,7 @@ class ConsistencyCheckQueryKey:
     attributes: List[str]
 
 
-ARISTA_KEYS: List[ConsistencyCheckQueryKey] = [
+BROADCOM_KEYS: List[ConsistencyCheckQueryKey] = [
     ConsistencyCheckQueryKey("ASIC_STATE:SAI_OBJECT_TYPE_BUFFER_POOL:*", attributes=[ALL_ATTRIBUTES]),
     ConsistencyCheckQueryKey("ASIC_STATE:SAI_OBJECT_TYPE_BUFFER_PROFILE:*", attributes=[ALL_ATTRIBUTES]),
     ConsistencyCheckQueryKey("ASIC_STATE:SAI_OBJECT_TYPE_SWITCH:*", attributes=[ALL_ATTRIBUTES]),
@@ -41,11 +41,11 @@ ARISTA_KEYS: List[ConsistencyCheckQueryKey] = [
 # The list of platforms and versions that have been tested to work with the consistency checker
 SUPPORTED_PLATFORMS_AND_VERSIONS = {
     "x86_64-arista_7060_cx32s": {
-        "202305": ARISTA_KEYS,
-        "202311": ARISTA_KEYS,
+        "202305": BROADCOM_KEYS,
+        "202311": BROADCOM_KEYS,
     },
     "x86_64-arista_7260cx3_64": {
-        "202305": ARISTA_KEYS,
-        "202311": ARISTA_KEYS,
+        "202305": BROADCOM_KEYS,
+        "202311": BROADCOM_KEYS,
     },
 }
