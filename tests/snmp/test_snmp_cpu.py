b58d454a2f2dcb044c3a8e4ad74a4bafaa44d50c
Dashuai Zhang <dashuaizhang@microsoft.com>
2025-02-23 22:04:52 +1100

    update default timeout to 5s for high cpu case.

--- tests/snmp/test_snmp_cpu.py
+++ tests/snmp/test_snmp_cpu.py
@@ -52,9 +52,12 @@ def test_snmp_cpu(duthosts, enum_rand_one_per_hwsku_hostname, localhost, creds_a
         time.sleep(20)
 
         # Gather facts with SNMP version 2
+        # use 5 seconds timeout to align with snmpd.conf in
+        # https://github.com/sonic-net/sonic-buildimage/blob/7a21cab07dbd0ace80833a57e391dec0ebde9978/dockers/docker-snmp/snmpd.conf.j2#L197
         snmp_facts = get_snmp_facts(
             duthost, localhost, host=hostip, version="v2c",
-            community=creds_all_duts[duthost.hostname]["snmp_rocommunity"], is_dell=True, wait=True)['ansible_facts']
+            community=creds_all_duts[duthost.hostname]["snmp_rocommunity"], is_dell=True, wait=True,
+            snmp_timeout=5)['ansible_facts']
 
         # Pull CPU utilization via shell
         # Explanation: Run top command with 2 iterations, 5sec delay.
