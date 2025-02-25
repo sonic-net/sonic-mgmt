a803a15a72654ba4b2e2216b5525a842b87d6329
hdwhdw <dwhuang9@gmail.com>
2025-02-25 01:25:04 +0000

    remove generated files and compile proto files dynamically.

--- tests/gnmi/test_gnoi_system_grpc.py
+++ tests/gnmi/test_gnoi_system_grpc.py
@@ -1,20 +1,28 @@
-
 import pytest
 import logging
 import grpc
 import time
+import os
+import sys
 
-from tests.gnmi.protos.gnoi.system import system_pb2_grpc, system_pb2
 from tests.common.helpers.gnmi_utils import GNMIEnvironment
 
-pytestmark = [
-    pytest.mark.topology('any')
-]
+pytestmark = [pytest.mark.topology("any")]
 
 
 """
 This module contains tests for the gNOI System Services, using gRPC python API.
 """
+
+
+def _get_gnoi_stubs():
+    PROTO_ROOT = "gnmi/protos"
+    sys.path.append(os.path.abspath(PROTO_ROOT))
+    from gnoi.system import system_pb2_grpc, system_pb2
+    return system_pb2_grpc, system_pb2
+
+
+system_pb2_grpc, system_pb2 = _get_gnoi_stubs()
 
 
 def test_gnoi_system_time(duthosts, rand_one_dut_hostname):
@@ -41,7 +49,7 @@ def test_gnoi_system_time(duthosts, rand_one_dut_hostname):
     credentials = grpc.ssl_channel_credentials(
         root_certificates=root_certificates,
         private_key=client_key,
-        certificate_chain=client_certificate
+        certificate_chain=client_certificate,
     )
 
     # Create gRPC channel
@@ -69,5 +77,6 @@ def test_gnoi_system_time(duthosts, rand_one_dut_hostname):
         current_time_ns = int(time.time() * 1e9)
         reasonable_interval_ns = 60 * 1e9  # 60 seconds in nanoseconds
 
-        assert abs(response.time - current_time_ns) < reasonable_interval_ns, \
-            f"System time {response.time} is not within the reasonable interval of current time {current_time_ns}"
+        assert (
+            abs(response.time - current_time_ns) < reasonable_interval_ns
+        ), f"System time {response.time} is not within the reasonable interval of current time {current_time_ns}"
