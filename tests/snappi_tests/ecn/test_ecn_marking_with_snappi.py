677ec6e454886db8306201acf1e9459a5697319b
Austin (Thang Pham) <austinpham@microsoft.com>
2025-02-25 12:25:13 +1100

    chore: migrate ECN multidiut (#16866)
    Description of PR
    Summary:
    
    As part of test merge plan, we're merging tests/snappi_tests/multidut to tests/snappi_tests for ECN folder.
    
    This PR simply moving over the same code in tests/snappi_tests/multidut/ecn to tests/snappi_tests/ecn as it and resolve a few conflicts.
    
    Fixes # (issue) 31217327
    
    Approach
    What is the motivation for this PR?
    Described above.
    
    How did you do it?
    All the tests in tests/snappi_tests/multidut/ecn folder are ran nightly on t0 to verify the status
    Compare each file in tests/snappi_tests/multidut/ecn folder with tests/snappi_tests/ecn folder to find the differences
    Resolve the differences and modify so that it fits with t0 platform
    Ran regression on t0 platforms and verify if it works
    How did you verify/test it?
    We run regression on T0 platform and T2 platforms to verify if this works. Verification includes 2 main process
    
    Nightly verification trigger on before-merge branch multidut for regression
    Verification trigger on original tests/snappi_tests/ecn folder for baseline comparison
    Verification trigger on after-merge branch to compare new process. Only failures existed in baseline are acceptable.
    
    Signed-off-by: Austin Pham <austinpham@microsoft.com>

--- tests/snappi_tests/ecn/test_ecn_marking_with_snappi.py
+++ tests/snappi_tests/ecn/test_ecn_marking_with_snappi.py
@@ -10,7 +10,7 @@ from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, \
     lossless_prio_list, disable_pfcwd   # noqa F401
 from tests.snappi_tests.files.helper import multidut_port_info, setup_ports_and_dut, enable_debug_shell  # noqa: F401
-from tests.snappi_tests.multidut.ecn.files.multidut_helper import run_ecn_marking_test, \
+from tests.snappi_tests.ecn.files.helper import run_ecn_marking_test, \
     run_ecn_marking_port_toggle_test, run_ecn_marking_ect_marked_pkts
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
 from tests.common.cisco_data import is_cisco_device
