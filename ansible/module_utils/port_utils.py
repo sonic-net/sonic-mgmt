adf38d856656f39d0f975afc8753364837e4049d
roman_savchuk <rsavchuk@nvidia.com>
2025-02-26 20:26:24 +0100

    Added new Mellanox SN5610N-C256S2 and SN5610N-C224O8 hwskus (#16196)

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -443,7 +443,7 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                     alias = "etp{}{}".format(i, split_alias)
                     eth_name = "Ethernet{}".format((i - 1) * 8 + idx * 2)
                     port_alias_to_name_map[alias] = eth_name
-        elif hwsku == "Mellanox-SN5600-C256S1":
+        elif hwsku in ["Mellanox-SN5600-C256S1", "Mellanox-SN5610N-C256S2"]:
             split_alias_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
             for i in range(1, 65, 2):
                 for idx, split_alias in enumerate(split_alias_list):
@@ -451,7 +451,9 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                     eth_name = "Ethernet{}".format((i - 1) * 8 + idx)
                     port_alias_to_name_map[alias] = eth_name
             port_alias_to_name_map['etp65'] = "Ethernet512"
-        elif hwsku == "Mellanox-SN5600-C224O8":
+            if hwsku == "Mellanox-SN5610N-C256S2":
+                port_alias_to_name_map['etp66'] = "Ethernet520"
+        elif hwsku in ["Mellanox-SN5600-C224O8", "Mellanox-SN5610N-C224O8"]:
             split_alias_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
             split_alias_list_1 = ["a", "b"]
             split_2_port_indexs = [13, 17, 45, 49]
@@ -467,6 +469,8 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                         eth_name = "Ethernet{}".format((i - 1) * 8 + idx)
                         port_alias_to_name_map[alias] = eth_name
             port_alias_to_name_map['etp65'] = "Ethernet512"
+            if hwsku == "Mellanox-SN5610N-C224O8":
+                port_alias_to_name_map['etp66'] = "Ethernet520"
         elif hwsku == "Arista-7060DX5-32":
             for i in range(1, 33):
                 port_alias_to_name_map["Ethernet%d/1" % i] = "Ethernet%d" % ((i - 1) * 8)
