301009ceb09abe432df20654d6133374bbb2494a
Malavika Unnikrishnan <142842429+maunnikr-cisco@users.noreply.github.com>
2025-03-05 10:22:15 -0800

    Removing redundant space

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -16,7 +16,6 @@ from ansible.plugins.loader import connection_loader
 
 from tests.common.devices.base import AnsibleHostBase
 from tests.common.devices.constants import ACL_COUNTERS_UPDATE_INTERVAL_IN_SEC
-
 from tests.common.helpers.dut_utils import is_supervisor_node, is_macsec_capable_node
 from tests.common.str_utils import str2bool
 from tests.common.utilities import get_host_visible_vars
