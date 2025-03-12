e037522f87f5c2e258a5d580fc7fabe67e7ebfbe
yatishkoul <yatishkoul@gmail.com>
2025-03-12 13:57:29 -0700

    Resolving Comments

--- tests/bgp/test_ping_bgp_neighbor.py
+++ tests/bgp/test_ping_bgp_neighbor.py
@@ -13,17 +13,17 @@ def test_ping_bgp_neighbor(duthosts, enum_frontend_dut_hostname, enum_asic_index
     duthost = duthosts[enum_frontend_dut_hostname]
     bgp_facts = duthost.bgp_facts(instance_id=enum_asic_index)['ansible_facts']
     namespace = duthost.get_namespace_from_asic_id(enum_asic_index)
-    is_multi_asic = namespace is not None
 
     for neighbor_ip, neighbor_data in bgp_facts['bgp_neighbors'].items():
         # Verify that the BGP session is established
         error_msg = f"BGP session with {neighbor_ip} is not 'established'"
         assert neighbor_data['state'] == 'established', error_msg
-        logging.info(f"Pinging BGP neighbor {neighbor_ip} from {duthost.hostname} (ASIC {enum_asic_index})")
-        if is_multi_asic:
-            ping_cmd = f"sudo ip netns exec {namespace} ping -c 3 {neighbor_ip}"
+        asic_info = f"(namespace: {namespace})" if namespace else ""
+        logging.info(f"Pinging BGP neighbor {neighbor_ip} from {duthost.hostname} {asic_info}")
+        if namespace:
+            ping_cmd = f"sudo ip netns exec {namespace} ping -c 1 {neighbor_ip}"
         else:
-            ping_cmd = f"ping -c 3 {neighbor_ip}"
+            ping_cmd = f"ping -c 1 {neighbor_ip}"
 
         result = duthost.shell(ping_cmd, module_ignore_errors=True)
         # Checking if the ping was successful
