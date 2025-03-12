beac74361d28e2d2ec197bd9d83cfc04f4aa6ef2
Dashuai Zhang <dashuaizhang@microsoft.com>
2025-03-12 09:31:39 +0000

    clear debug message

--- tests/common/helpers/platform_api/scripts/platform_api_server.py
+++ tests/common/helpers/platform_api/scripts/platform_api_server.py
@@ -95,7 +95,7 @@ class PlatformAPITestService(BaseHTTPRequestHandler):
         except NotImplementedError:
             syslog.syslog(syslog.LOG_WARNING, "API '{}' not implemented".format(api))
         except Exception as e:
-            syslog.syslog(syslog.LOG_ERR, "Error executing API '{}': {}".format(api, str(e)))
+            syslog.syslog(syslog.LOG_ERR, "Error executing API '{}': {}".format(api, repr(e)))
 
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
