d72aa5d4ad83ad198c2410f49e2629fb2db7e90b
Chuan Wu <103085864+echuawu@users.noreply.github.com>
2025-03-20 16:48:06 +0800

    Unify the ipv6 address to upper case in generic hash case (#17426)
    - What is the motivation for this PR?
    Use upper case ipv6 address during add or remove ipv6 address, it would avoid config db recognizing lower case and upper case address as two different address. In this scenario, it may lead to intf creation or deletion issue.
    
    - How did you do it?
    Use upper case ipv6 address during add or remove ipv6 address
    
    - How did you verify/test it?
    Run it in local testbed.

--- tests/hash/generic_hash_helper.py
+++ tests/hash/generic_hash_helper.py
@@ -142,7 +142,7 @@ def restore_configuration(duthost):
         # Re-config ip interface
         for ip_interface in ip_interface_to_restore:
             formatted_ip_addr = format_ip_mask(f"{ip_interface['addr']}/{ip_interface['mask']}")
-            duthost.shell(f"config interface ip add {ip_interface['attachto']} {formatted_ip_addr}")
+            duthost.shell(f"config interface ip add {ip_interface['attachto']} {formatted_ip_addr.upper()}")
         # Re-config vlan interface
         if vlan_member_to_restore:
             duthost.shell(f"config vlan member add {vlan_member_to_restore['vlan_id']} "
@@ -609,7 +609,7 @@ def remove_ip_interface_and_config_vlan(duthost, mg_facts, tbinfo, downlink_inte
         for ip_interface in mg_facts['minigraph_interfaces']:
             if ip_interface['attachto'] == downlink_interface:
                 formatted_ip_addr = format_ip_mask(f"{ip_interface['addr']}/{ip_interface['mask']}")
-                duthost.shell(f"config interface ip remove {ip_interface['attachto']} {formatted_ip_addr}")
+                duthost.shell(f"config interface ip remove {ip_interface['attachto']} {formatted_ip_addr.upper()}")
                 ip_interface_to_restore.append(ip_interface)
                 l2_ports.add(downlink_interface)
         for portchannel in mg_facts['minigraph_portchannels'].values():
@@ -619,14 +619,15 @@ def remove_ip_interface_and_config_vlan(duthost, mg_facts, tbinfo, downlink_inte
                         formatted_ip_addr = format_ip_mask(
                             f"{portchannel_ip_interface['addr']}/{portchannel_ip_interface['mask']}")
                         duthost.shell(
-                            f"config interface ip remove {portchannel_ip_interface['attachto']} {formatted_ip_addr}")
+                            f"config interface ip remove {portchannel_ip_interface['attachto']} "
+                            f"{formatted_ip_addr.upper()}")
                         ip_interface_to_restore.append(portchannel_ip_interface)
                         l2_ports.add(portchannel_ip_interface['attachto'])
     # re-config the uplink interfaces, remove the ip address on the egress portchannel interfaces
     for ip_interface in mg_facts['minigraph_portchannel_interfaces']:
         if ip_interface['attachto'] in uplink_interfaces:
             formatted_ip_addr = format_ip_mask(f"{ip_interface['addr']}/{ip_interface['mask']}")
-            duthost.shell(f"config interface ip remove {ip_interface['attachto']} {formatted_ip_addr}")
+            duthost.shell(f"config interface ip remove {ip_interface['attachto']} {formatted_ip_addr.upper()}")
             ip_interface_to_restore.append(ip_interface)
             l2_ports.add(ip_interface['attachto'])
     # Configure VLANs for VLAN_ID test
