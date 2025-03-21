e659d01019d5753288843f1ec858eccafd9901a6
judyjoseph <53951155+judyjoseph@users.noreply.github.com>
2025-03-21 00:04:32 -0700

    Address fixes to make sure t2 topology deploy works both with and without macsec_enabled (#17530)
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- ansible/library/get_macsec_profile.py
+++ ansible/library/get_macsec_profile.py
@@ -5,7 +5,24 @@ import json
 from ansible.module_utils.basic import AnsibleModule
 
 
-def get_macsec_profile(module, macsec_profile):
+def convert_to_eos(cipher_name):
+    # Set the cipher suite as 256 xpn by default
+    eos_cipher_name = 'aes256-gcm-xpn'
+
+    if cipher_name == 'GCM-AES-XPN-256':
+        eos_cipher_name = 'aes256-gcm-xpn'
+    elif cipher_name == 'GCM-AES-128':
+        eos_cipher_name = 'aes128-gcm'
+    elif cipher_name == 'GCM-AES-256':
+        eos_cipher_name = 'aes256-gcm'
+    elif cipher_name == 'GCM-AES-XPN-128':
+        eos_cipher_name = 'aes128-gcm-xpn'
+
+    return eos_cipher_name
+
+
+# This API support EoS based templates now
+def get_macsec_profile(module, macsec_profile, vm_type):
     with open('/tmp/profile.json') as f:
         macsec_profiles = json.load(f)
 
@@ -13,14 +30,23 @@ def get_macsec_profile(module, macsec_profile):
         if profile:
             profile['macsec_profile'] = macsec_profile
 
+            # Currently handling ceos, add more cases for vsonic etc
+            if vm_type == 'ceos':
+                # Get the cipher suite in eos terminology
+                eos_cipher_suite_name = convert_to_eos(profile['cipher_suite'])
+                profile['cipher_suite'] = eos_cipher_suite_name
+
     return profile
 
 
 def main():
-    module = AnsibleModule(argument_spec=dict(macsec_profile=dict(required=True, type='str')))
+    module = AnsibleModule(argument_spec=dict(
+                           macsec_profile=dict(required=True, type='str'),
+                           vm_type=dict(required=True, type='str')))
 
     macsec_profile = module.params['macsec_profile']
-    module.exit_json(profile=get_macsec_profile(module, macsec_profile), changed=False)
+    vm_type = module.params['vm_type']
+    module.exit_json(profile=get_macsec_profile(module, macsec_profile, vm_type), changed=False)
 
 
 if __name__ == "__main__":
