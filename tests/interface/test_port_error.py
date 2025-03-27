fecf035d2d51ab653f1ace113bf845184cdb2815
Vaibhav Dahiya <vdahiya@microsoft.com>
2025-03-27 00:21:37 +0000

    fix the test
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- tests/interface/test_port_error.py
+++ tests/interface/test_port_error.py
@@ -16,7 +16,7 @@ pytestmark = [
 
 SUPPORTED_PLATFORMS = ["arista_7060x6"]
 
-class MACFaultTest:
+class  TestMACFault(object):
     @pytest.fixture(autouse=True)
     def is_supported_platform(self, duthost):
         if any(platform in duthost.facts['platform'] for platform in SUPPORTED_PLATFORMS):
@@ -54,12 +54,12 @@ class MACFaultTest:
         local_fault_before = self.get_mac_fault_count(dut, interface, "mac local fault")
         logging.info("Initial MAC local fault count on {}: {}".format(interface, local_fault_before))
 
-        dut.shell("sudo sfputil debug rx-output disable {}".format(interface))
+        dut.shell("sudo sfputil debug rx-output {} disable".format(interface))
         time.sleep(5)
-        dut.shell("sudo sfputil debug rx-output enable {}".format(interface))
         pytest_assert(self.get_interface_status(dut, interface) == "down", 
                       "Interface {} did not go down after enabling low-power mode".format(interface))
 
+        dut.shell("sudo sfputil debug rx-output {} enable".format(interface))
         time.sleep(20)
         pytest_assert(self.get_interface_status(dut, interface) == "up", 
                       "Interface {} did not come up after disabling low-power mode".format(interface))
@@ -70,23 +70,22 @@ class MACFaultTest:
         pytest_assert(local_fault_after > local_fault_before, 
                       "MAC local fault count did not increment after toggling low-power mode")
 
-    def test_mac_remote_fault_increment(self, select_random_interface, fanouthosts):
+    def test_mac_remote_fault_increment(self, select_random_interface):
         dut, interface = select_random_interface
-
-        remote_host = fanouthosts[fanout]
-        logging.info("DUT interface: {} connected to fanout switch: {} on port: {}".format(interface, fanout, fanout_port))
 
         remote_fault_before = self.get_mac_fault_count(dut, interface, "mac remote fault")
         logging.info("Initial MAC remote fault count on {}: {}".format(interface, remote_fault_before))
 
-        dut.shell("sudo sfputil debug tx-output disable {}".format(interface))
+        dut.shell("sudo sfputil debug tx-output {} disable".format(interface))
         time.sleep(5)
-        dut.shell("sudo sfputil debug tx-output enable {}".format(interface))
+        dut.shell("sudo sfputil debug tx-output {} enable".format(interface))
         time.sleep(20)
+
+        pytest_assert(self.get_interface_status(dut, interface) == "up", 
+                      "Interface {} did not come up after disabling low-power mode".format(interface))
 
         remote_fault_after = self.get_mac_fault_count(dut, interface, "mac remote fault")
         logging.info("MAC remote fault count after toggling low-power mode on {}: {}".format(interface, remote_fault_after))
 
         pytest_assert(remote_fault_after > remote_fault_before, 
                       "MAC remote fault count did not increment after toggling low-power mode on remote device")
-
