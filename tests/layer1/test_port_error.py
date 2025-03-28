3945c2d429e9cb3dbe80668ab931330046b8e060
Vaibhav Dahiya <vdahiya@microsoft.com>
2025-03-28 23:09:03 +0000

    fix all
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -32,7 +32,7 @@ class TestMACFault(object):
             if error_info['port errors'] == fault_type:
                 fault_count = int(error_info['count'])
                 break
-        
+
         logging.info("{} count on {}: {}".format(fault_type, interface, fault_count))
         return fault_count
 
@@ -54,19 +54,19 @@ class TestMACFault(object):
 
         dut.shell("sudo sfputil debug rx-output {} disable".format(interface))
         time.sleep(5)
-        pytest_assert(self.get_interface_status(dut, interface) == "down", 
+        pytest_assert(self.get_interface_status(dut, interface) == "down",
                       "Interface {} did not go down after enabling low-power mode".format(interface))
 
         dut.shell("sudo sfputil debug rx-output {} enable".format(interface))
         time.sleep(20)
-        pytest_assert(self.get_interface_status(dut, interface) == "up", 
+        pytest_assert(self.get_interface_status(dut, interface) == "up",
                       "Interface {} did not come up after disabling low-power mode".format(interface))
 
         local_fault_after = self.get_mac_fault_count(dut, interface, "mac local fault")
         logging.info("MAC local fault count after toggling low-power mode on " +
                      "{}: {}".format(interface, local_fault_after))
 
-        pytest_assert(local_fault_after > local_fault_before, 
+        pytest_assert(local_fault_after > local_fault_before,
                       "MAC local fault count did not increment after toggling low-power mode")
 
     def test_mac_remote_fault_increment(self, select_random_interface):
@@ -80,12 +80,12 @@ class TestMACFault(object):
         dut.shell("sudo sfputil debug tx-output {} enable".format(interface))
         time.sleep(20)
 
-        pytest_assert(self.get_interface_status(dut, interface) == "up", 
+        pytest_assert(self.get_interface_status(dut, interface) == "up",
                       "Interface {} did not come up after disabling low-power mode".format(interface))
 
         remote_fault_after = self.get_mac_fault_count(dut, interface, "mac remote fault")
         logging.info("MAC remote fault count after toggling low-power mode on " +
                      "{}: {}".format(interface, remote_fault_after))
 
-        pytest_assert(remote_fault_after > remote_fault_before, 
+        pytest_assert(remote_fault_after > remote_fault_before,
                       "MAC remote fault count did not increment after toggling low-power mode on remote device")
