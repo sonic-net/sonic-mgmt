198dfc2215f360f3960536f3e309b7468ae75530
rraghav-cisco <58446052+rraghav-cisco@users.noreply.github.com>
2025-03-28 16:04:54 -0700

    T2:Fixing the static-route function for other plats. (#17628)
    Description of PR
    Summary:
    Fixes # 17611
    
    Approach
    What is the motivation for this PR?
    The recent addition for static route in snappi tests for cisco-8000 broke other platform runs. This PR attempts to fix the new function for the other platforms.
    
    How did you do it?
    Converted the runtimeError to a warning, and return the same address.
    
    How did you verify/test it?
    Ran it with my TB, with both == cisco-8000, and != cisco-8000 checks.
    
    and verified that the issue in the above git issue is not seen(global_pause is unsupported in cisco-8000):
    
    ___________________________________________________________________________________________ test_global_pause[multidut_port_info1] ___________________________________________________________________________________________
    --------------------------------------------------------------------------- generated xml file: /run_logs/ixia/pretest/2025-03-20-07-23-24/tr.xml ----------------------------------------------------------------------------
    INFO:root:Can not get Allure report URL. Please check logs
    ================================================================================================== short test summary info ===================================================================================================
    PASSED snappi_tests/pfc/test_global_pause_with_snappi.py::test_global_pause[multidut_port_info1]
    ERROR snappi_tests/pfc/test_global_pause_with_snappi.py::test_global_pause[multidut_port_info3] - Failed: Processes "['analyze_logs--<MultiAsicSonicHost yy39top-lc2>']" failed with exit code "1"
    FAILED snappi_tests/pfc/test_global_pause_with_snappi.py::test_global_pause[multidut_port_info0] - Failed: Background Flow Prio 1 should not have any dropped packet
    FAILED snappi_tests/pfc/test_global_pause_with_snappi.py::test_global_pause[multidut_port_info2] - Failed: Background Flow Prio 1 should not have any dropped packet
    FAILED snappi_tests/pfc/test_global_pause_with_snappi.py::test_global_pause[multidut_port_info3] - Failed: Background Flow Prio 1 should not have any dropped packet
    =============================================================================== 3 failed, 1 passed, 8 warnings, 1 error in 2068.50s (0:34:28) ================================================================================
    sonic@snappi-sonic-mgmt-msft-t2:/data/tests$ vi common/plugins/conditional_mark/tests_mark_conditions.yaml
    
    co-authorized by: jianquanye@microsoft.com

--- tests/common/snappi_tests/snappi_fixtures.py
+++ tests/common/snappi_tests/snappi_fixtures.py
@@ -1312,11 +1312,14 @@ def static_routes_cisco_8000(addr, dut=None, intf=None, namespace=None, setup=Tr
     global DEST_TO_GATEWAY_MAP
     if dut is None:
         if addr not in DEST_TO_GATEWAY_MAP:
-            raise RuntimeError(f"Request for dest addr: {addr} without setting it in advance.")
+            logger.warn(f"Request for dest addr: {addr} without setting it in advance.")
+            return addr
         return DEST_TO_GATEWAY_MAP[addr]['dest']
 
     if (dut.facts['asic_type'] != "cisco-8000" or
             not dut.get_facts().get("modular_chassis", None)):
+        DEST_TO_GATEWAY_MAP[addr] = {}
+        DEST_TO_GATEWAY_MAP[addr]['dest'] = addr
         return addr
 
     if setup:
