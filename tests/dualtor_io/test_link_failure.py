b3b5660d556213e88829d3f148adddc15e42adb9
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2025-03-29 00:15:18 +0800

    [dualtor_io] Collect syslog to debug (#17722)
    * [test_link_failure] Collect syslog to debug
    
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/dualtor_io/test_link_failure.py
+++ tests/dualtor_io/test_link_failure.py
@@ -422,17 +422,17 @@ def test_active_link_admin_down_config_reload_downstream(
             upper_tor_host.shell("config save -y")
 
 
-@pytest.mark.disable_loganalyzer
 @pytest.mark.enable_active_active
 @pytest.mark.skip_active_standby
 def test_active_link_admin_down_config_reload_link_up_upstream(
     upper_tor_host, lower_tor_host, send_server_to_t1_with_action,      # noqa F811
-    cable_type, active_active_ports                                     # noqa F811
+    cable_type, active_active_ports, setup_loganalyzer                  # noqa F811
 ):
     """
     Send traffic from server to T1 and unshut the active-active mux ports.
     Verify switchover and disruption.
     """
+    setup_loganalyzer(upper_tor_host, collect_only=True)
     if cable_type == CableType.active_active:
         try:
             config_interface_admin_status(upper_tor_host, active_active_ports, "down")
@@ -475,17 +475,17 @@ def test_active_link_admin_down_config_reload_link_up_upstream(
             upper_tor_host.shell("config save -y")
 
 
-@pytest.mark.disable_loganalyzer
 @pytest.mark.enable_active_active
 @pytest.mark.skip_active_standby
 def test_active_link_admin_down_config_reload_link_up_downstream_standby(
     upper_tor_host, lower_tor_host, send_t1_to_server_with_action,      # noqa F811
-    cable_type, active_active_ports                                     # noqa F811
+    cable_type, active_active_ports, setup_loganalyzer                  # noqa F811
 ):
     """
     Send traffic from T1 to standby ToR and unshut the active-active mux ports.
     Verify switchover and disruption.
     """
+    setup_loganalyzer(upper_tor_host, collect_only=True)
     if cable_type == CableType.active_active:
         try:
             config_interface_admin_status(upper_tor_host, active_active_ports, "down")
