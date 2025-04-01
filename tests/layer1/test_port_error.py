5c9513b33a19b13ce8c2905245956a2e2797d044
Vaibhav Dahiya <vdahiya@microsoft.com>
2025-04-01 20:02:55 +0000

    fix
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -16,7 +16,10 @@ SUPPORTED_PLATFORMS = ["arista_7060x6"]
 
 class TestMACFault(object):
     @pytest.fixture(autouse=True)
-    def is_supported_platform(self, duthost):
+    def is_supported_platform(self, duthost, tbinfo):
+        if 'ptp' not in tbinfo['topo']['name']:
+            pytest.skip("Skipping test: Not applicable for PTP topology")
+        
         if any(platform in duthost.facts['platform'] for platform in SUPPORTED_PLATFORMS):
             skip_release(duthost, ["201811", "201911", "202012", "202205", "202211", "202305", "202405"])
         else:
@@ -55,19 +58,19 @@ class TestMACFault(object):
         dut.shell("sudo sfputil debug rx-output {} disable".format(interface))
         time.sleep(5)
         pytest_assert(self.get_interface_status(dut, interface) == "down",
-                      "Interface {} did not go down after enabling low-power mode".format(interface))
+                      "Interface {} did not go down after disabling rx-output using sfputil".format(interface))
 
         dut.shell("sudo sfputil debug rx-output {} enable".format(interface))
         time.sleep(20)
         pytest_assert(self.get_interface_status(dut, interface) == "up",
-                      "Interface {} did not come up after disabling low-power mode".format(interface))
+                      "Interface {} did not come up after enabling rx-output using sfputil".format(interface))
 
         local_fault_after = self.get_mac_fault_count(dut, interface, "mac local fault")
-        logging.info("MAC local fault count after toggling low-power mode on " +
+        logging.info("MAC local fault count after disabling/enabling rx-output using sfputil" +
                      "{}: {}".format(interface, local_fault_after))
 
         pytest_assert(local_fault_after > local_fault_before,
-                      "MAC local fault count did not increment after toggling low-power mode")
+                      "MAC local fault count did not increment after disabling/enabling tx-output on the device")
 
     def test_mac_remote_fault_increment(self, select_random_interface):
         dut, interface = select_random_interface
@@ -77,15 +80,18 @@ class TestMACFault(object):
 
         dut.shell("sudo sfputil debug tx-output {} disable".format(interface))
         time.sleep(5)
+        pytest_assert(self.get_interface_status(dut, interface) == "down",
+                      "Interface {} did not go down after disabling rx-output using sfputil".format(interface))
         dut.shell("sudo sfputil debug tx-output {} enable".format(interface))
         time.sleep(20)
 
         pytest_assert(self.get_interface_status(dut, interface) == "up",
-                      "Interface {} did not come up after disabling low-power mode".format(interface))
+                      "Interface {} did not come up after disabling tx-output using sfputil".format(interface))
 
         remote_fault_after = self.get_mac_fault_count(dut, interface, "mac remote fault")
-        logging.info("MAC remote fault count after toggling low-power mode on " +
+        logging.info("MAC remote fault count after disabling/enabling rx-output using sfputil" +
                      "{}: {}".format(interface, remote_fault_after))
 
         pytest_assert(remote_fault_after > remote_fault_before,
-                      "MAC remote fault count did not increment after toggling low-power mode on remote device")
+                      "MAC remote fault count did not increment after disabling/enabling tx-output on the device")
+
