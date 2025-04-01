7adfdc9964e5f2dbc2337f32aa3bd9129c761367
Chuan Wu <103085864+echuawu@users.noreply.github.com>
2025-04-02 00:27:14 +0800

    Add SRv6 forwarding plane test script (#17453)
    Test coverage:
    1.Cover DIP shift function
    2.Cover uSID container copy and segment left reduce function
    3.Cover USD function including pipe mode as MSFT requested
    4.Cover Config reload / cold reboot
    5.Cover SAI SDK dump validation
    
    Change-Id: I1da20cb7f0cb124422336820d7546b71333f5116

--- tests/common/helpers/dut_utils.py
+++ tests/common/helpers/dut_utils.py
@@ -610,3 +610,48 @@ def duthost_clear_console_port(
     duthost_config_menu.disconnect()
     logger.info(f"Successfully cleared console port {console_port}, sleeping for 5 seconds")
     time.sleep(5)
+
+
+def get_available_tech_support_files(duthost):
+    """
+    Get available techsupport files list
+    :param duthost: duthost object
+    :return: list of available techsupport files
+    """
+    try:
+        available_tech_support_files = duthost.shell('ls /var/dump/*.tar.gz')['stdout_lines']
+    except RunAnsibleModuleFail:
+        available_tech_support_files = []
+    return available_tech_support_files
+
+
+def get_new_techsupport_files_list(duthost, available_tech_support_files):
+    """
+    Get list of new created techsupport files
+    :param duthost: duthost object
+    :param available_tech_support_files: list of already available techsupport files
+    :return: list of new techsupport files
+    """
+    try:
+        duthost.shell('ls -lh /var/dump/')  # print into logs full folder content(for debug purpose)
+        new_available_tech_support_files = duthost.shell('ls /var/dump/*.tar.gz')['stdout_lines']
+    except RunAnsibleModuleFail:
+        new_available_tech_support_files = []
+    new_techsupport_files_list = list(set(new_available_tech_support_files) - set(available_tech_support_files))
+
+    return new_techsupport_files_list
+
+
+def extract_techsupport_tarball_file(duthost, tarball_name):
+    """
+    Extract techsupport tar file and return path to data extracted from archive
+    :param duthost: duthost object
+    :param tarball_name: path to tar file, example: /var/dump/sonic_dump_DUT_NAME_20210901_22140.tar.gz
+    :return: path to folder with techsupport data, example: /tmp/sonic_dump_DUT_NAME_20210901_22140
+    """
+    with allure.step('Extracting techsupport file: {}'.format(tarball_name)):
+        dst_folder = '/tmp/'
+        duthost.shell('tar -xf {} -C {}'.format(tarball_name, dst_folder))
+        techsupport_folder = tarball_name.split('.')[0].split('/var/dump/')[1]
+        techsupport_folder_full_path = '{}{}'.format(dst_folder, techsupport_folder)
+    return techsupport_folder_full_path
