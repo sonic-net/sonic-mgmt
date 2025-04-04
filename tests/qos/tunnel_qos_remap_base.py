12cbbecf9aca65c3bf6de81df221b1ddc39f5775
bingwang-ms <66248323+bingwang-ms@users.noreply.github.com>
2025-04-04 09:10:23 -0700

    Restore fixture disable_packet_aging (#17800)

--- tests/qos/tunnel_qos_remap_base.py
+++ tests/qos/tunnel_qos_remap_base.py
@@ -310,6 +310,30 @@ def qos_config(rand_selected_dut, tbinfo, dut_config):
     return speed_cable_to_params[speed_cable]
 
 
+@pytest.fixture(scope='module', autouse=True)
+def disable_packet_aging(duthosts):
+    """
+        For Nvidia(Mellanox) platforms, packets in buffer will be aged after a timeout. Need to disable this
+        before any buffer tests.
+    """
+    for duthost in duthosts:
+        asic = duthost.get_asic_name()
+        if 'spc' in asic:
+            logger.info("Disable Mellanox packet aging")
+            duthost.copy(src="qos/files/mellanox/packets_aging.py", dest="/tmp")
+            duthost.command("docker cp /tmp/packets_aging.py syncd:/")
+            duthost.command("docker exec syncd python /packets_aging.py disable")
+
+    yield
+
+    for duthost in duthosts:
+        asic = duthost.get_asic_name()
+        if 'spc' in asic:
+            logger.info("Enable Mellanox packet aging")
+            duthost.command("docker exec syncd python /packets_aging.py enable")
+            duthost.command("docker exec syncd rm -rf /packets_aging.py")
+
+
 def _create_ssh_tunnel_to_syncd_rpc(duthost):
     dut_asic = duthost.asic_instance()
     dut_asic.create_ssh_tunnel_sai_rpc()
