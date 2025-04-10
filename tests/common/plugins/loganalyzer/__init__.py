79c1e46c7f80926bd8a19608a2c9fd4d0095194a
chunangli <chunang_li@163.com>
2025-04-10 14:13:53 +0800

    debug timeout
    Signed-off-by: Chun'ang Li <chunangli@microsoft.com>

--- tests/common/plugins/loganalyzer/__init__.py
+++ tests/common/plugins/loganalyzer/__init__.py
@@ -99,5 +99,6 @@ def loganalyzer(duthosts, request, log_rotate_modular_chassis):
             "rep_setup" in request.node.__dict__ and request.node.rep_setup.skipped:
         return
     logging.info("Starting to analyse on all DUTs")
-    parallel_run(analyze_logs, [analyzers, markers], {'fail_test': fail_test, 'store_la_logs': store_la_logs},
-                 duthosts, timeout=240)
+    parallel_run(analyze_logs, [analyzers, markers],
+                 {'fail_test': fail_test, 'store_la_logs': store_la_logs},
+                 duthosts, timeout=600)
