ac1422ac05a6e80b34c0df943d4887d66c7ddbf0
Soumya Mishra <soumyamishra@microsoft.com>
2025-04-11 16:56:22 -0700

    Fix test failure

--- tests/acl/test_src_mac_rewrite.py
+++ tests/acl/test_src_mac_rewrite.py
@@ -10,13 +10,17 @@ import ipaddress
 import json
 import ptf.testutils as testutils
 from ptf import mask
-from ptf.packet import Ether, IP, UDP, VXLAN, simple_vxlan_packet
+from ptf.packet import Ether, IP, UDP, VXLAN
+import ptf.packet as scapy
+from scapy.all import Ether
 
 from tests.common.utilities import wait_until
 from tests.common.config_reload import config_reload
 from tests.common.helpers.assertions import pytest_assert, pytest_require
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses        # lgtm[py/unused-import]
 from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer, LogAnalyzerError
+from tests.common.utilities import get_neighbor_ptf_port_list
+from tests.common.helpers.constants import UPSTREAM_NEIGHBOR_MAP
 
 from abc import abstractmethod
 
@@ -49,30 +53,16 @@ ACL_TABLE_TYPE = "INNER_SRC_MAC_REWRITE_TYPE"
 LOG_EXPECT_ACL_TABLE_CREATE_RE = ".*Created ACL table.*"
 LOG_EXPECT_ACL_TABLE_REMOVE_RE = ".*Successfully deleted ACL table.*"
 
-@pytest.fixture(scope="module")
-def setup_ports_and_portchannels(duthost):
-    # Ensure all ports and PortChannels are up
-    ports = ["Ethernet0", "Ethernet4", "Ethernet8", "Ethernet12"]  # Example ports
-    portchannels = ["PortChannel1", "PortChannel2"]  # Example PortChannels
-    
-    for port in ports + portchannels:
-        duthost.shell("sudo config interface startup {port}")
-    
-
-def cleanup_ports_and_portchannels(duthost):
-    # Cleanup PortChannels after test (optional)
-    portchannels = ["PortChannel1", "PortChannel2"]
-    for po in portchannels:
-        duthost.shell("sudo config portchannel del {po}")
-
 def setup_acl_table(duthost):
     table_name = ACL_TABLE_NAME
-    ports = ["Ethernet0", "Ethernet4", "Ethernet8", "Ethernet12"]
-    portchannels = ["PortChannel1", "PortChannel2"]
+    ports = list(mg_facts['minigraph_portchannels'])
+    dut_port = ports[0] if ports else None
+
     cmd = "config acl add table {} {} -s {} -p {}".format(
             ACL_TABLE_NAME,
             ACL_TABLE_TYPE,
-            "egress"
+            "egress",
+            dut_port
         )
     
     logger.info("Creating ACL table {} for testing".format(table_name))
@@ -85,16 +75,9 @@ def setup_acl_table(duthost):
         # Todo: cleanup
         pytest.fail("Failed to create ACL table {}".format(table_name))
 
-    # Bind ACL table to specific ports and PortChannels
-    
-    for port in ports + portchannels:
-        duthost.shell("sudo config acl bind table table_name {port}")
-
 def remove_acl_table(duthost):
     table_name = ACL_TABLE_NAME
     cmd = "config acl remove table {}".format(table_name)
-    ports = ["Ethernet0", "Ethernet4", "Ethernet8", "Ethernet12"]
-    portchannels = ["PortChannel1", "PortChannel2"]
 
     logger.info("Removing ACL table {}".format(table_name))
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="TestAclSrcMacRewrite")
@@ -106,9 +89,6 @@ def remove_acl_table(duthost):
     except LogAnalyzerError:
         # Todo: cleanup
         pytest.fail("Failed to remove ACL table {}".format(table_name))   
-
-    for port in ports + portchannels:
-        duthost.shell(f"sudo config acl unbind table TEST_TABLE {port}")
 
 def setup_acl_rules(duthost, inner_src_ip, vni, action, new_src_mac):
     table_name = ACL_TABLE_NAME
@@ -130,56 +110,80 @@ def setup_acl_rules(duthost, inner_src_ip, vni, action, new_src_mac):
     if duthost.facts['asic_type'] != 'vs':
         pytest_assert(wait_until(60, 2, 0, check_rule_counters, duthost), "Acl rule counters are not ready")
 
-def remove_acl_rules(self, duthost, stage, ip_ver):
+def remove_acl_rules(self, duthost):
     table_name = ACL_TABLE_NAME
     duthost.copy(src=os.path.join(FILES_DIR, ACL_REMOVE_RULES_FILE), dest=TMP_DIR)
     remove_rules_dut_path = os.path.join(TMP_DIR, ACL_REMOVE_RULES_FILE)
     duthost.command("acl-loader update full {} --table_name {}".format(remove_rules_dut_path, table_name))
     time.sleep(5)
 
-def test_modify_inner_src_mac_egress(duthost, ptfadapter, setup_acl_rule):
+def test_modify_inner_src_mac_egress(duthost, ptfadapter, tbinfo):
     # Define test parameters
-    inner_src_ip = "192.168.1.1"
+    inner_src_ip = "192.168.0.2"
+    inner_dst_ip = "192.168.0.1"
     vni_id = 5000
     original_inner_src_mac = "00:66:77:88:99:aa"
     modified_inner_src_mac = "00:11:22:33:44:55"
     outer_src_mac = "00:11:22:33:44:66"
-    outer_dst_mac = "00:aa:bb:cc:dd:ee"
+    outer_dst_mac = duthost.facts['router_mac']    # MAC address should be router_mac rather than ptf mac
     outer_src_ip = "10.1.1.1"
     outer_dst_ip = "20.1.1.1"
 
+    setup_acl_rules(duthost, inner_src_ip, vni_id, ACTION_FORWARD, modified_inner_src_mac)
     # Create VXLAN-encapsulated packet sent by server
-    pkt = simple_vxlan_packet(
+    inner_pkt = testutils.simple_udp_packet(
+            eth_dst=duthost.facts['router_mac'],
+            eth_src=original_inner_src_mac,
+            ip_src=inner_src_ip,
+            ip_dst=inner_dst_ip,
+            ip_id=0,
+            ip_ihl=5,
+            udp_sport=1234,
+            udp_dport=4321,
+            ip_ttl=121)
+    pkt = testutils.simple_vxlan_packet(
         eth_dst=outer_dst_mac,
         eth_src=outer_src_mac,
         ip_src=outer_src_ip,
         ip_dst=outer_dst_ip,
         udp_sport=1234,
         udp_dport=4789,
-        vni=vni_id,
-        inner_frame=Ether(src=original_inner_src_mac, dst='00:bb:cc:dd:ee:ff')/IP(src=inner_src_ip, dst='192.168.2.1')
+        vxlan_vni=vni_id,
+        inner_frame=inner_pkt
     )
 
-    # Define expected modified packet on egress
-    expected_pkt = simple_vxlan_packet(
-        eth_dst=outer_dst_mac,
-        eth_src=outer_src_mac,
-        ip_src=outer_src_ip,
-        ip_dst=outer_dst_ip,
-        udp_sport=1234,
-        udp_dport=4789,
-        vni=vni_id,
-        inner_frame=Ether(src=modified_inner_src_mac, dst='00:bb:cc:dd:ee:ff')/IP(src=inner_src_ip, dst='192.168.2.1')
-    )
+    expected_pkt = mask.Mask(pkt)
+
+    # Mask outer Ethernet
+    expected_pkt.set_do_not_care_scapy(Ether, 'dst')
+    expected_pkt.set_do_not_care_scapy(Ether, 'src')
 
     # Send packet from server into the DUT
-    send(ptfadapter, 0, pkt)
+    testutils.send(ptfadapter, 0, pkt)
 
     time.sleep(2)
 
     # Verify the modified packet is received at egress
-    verify_packet(ptfadapter, expected_pkt, 1)
+    #testutils.verify_packet(ptfadapter, expected_pkt, 0)
+
+    result = testutils.dp_poll(ptfadapter, exp_pkt=expected_pkt, port_number=0, timeout=2)
+
+    # Check and extract inner source MAC
+    if result:
+        actual_pkt = scapy.Ether(result.packet)
+
+        # Get the second Ethernet header (inner Ethernet)
+        inner_eth = actual_pkt.getlayer(scapy.Ether, 1)
+        inner_src_mac = inner_eth.src if inner_eth else None
+
+        print(f"Inner source MAC: {inner_src_mac}")
+
+        assert inner_src_mac == modified_inner_src_mac, f"Expected inner src MAC {modified_inner_src_mac}, got {inner_src_mac}"
+    else:
+        assert False, "No packet received on port 0"
 
     # Verify ACL counter incremented, confirming rule was applied
     result = duthost.shell("show acl counter TEST_TABLE RULE_MODIFY_INNER_SRC_MAC")
     assert "1" in result['stdout'], "ACL counter not incremented as expected"
+    remove_acl_rules(duthost)
+    remove_acl_table(duthost)
