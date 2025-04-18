e51651adbc20af3f4afe5aee8a0dadbdfedbc9f2
BYGX-wcr <wcr@live.cn>
2025-04-18 18:38:16 +0000

    one more note in comment

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -363,6 +363,7 @@ class GenerateGoldenConfigDBModule(object):
         config = self.update_dns_config(config)
 
         # To enable bmp feature when the image version is >= 202411 and the device is not supervisor
+        # Note: the Chassis supervisor is not holding any BGP sessions so the BMP feature is not needed
         if self.check_version_for_bmp() is True and device_info.is_supervisor() is False:
             if multi_asic.is_multi_asic():
                 config = self.overwrite_feature_golden_config_db_multiasic(config, "bmp")
