5d6569265a54fe8127b6cfb0a6794a24e14b18b3
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-04-21 17:36:15 +0800

    Update test_authorization.py

--- tests/tacacs/test_authorization.py
+++ tests/tacacs/test_authorization.py
@@ -730,8 +730,10 @@ def test_send_remote_address(
     # Send a authorization packet to TACACS server
     ssh_run_command(remote_rw_user_client, "show version")
 
-    # Remote address is first part of SSH_CONNECTION: '10.250.0.1 47462 10.250.0.101 22'
+    exit_code, stdout_stream, stderr_stream = ssh_run_command(remote_rw_user_client, "echo $SSH_CONNECTION")
+    pytest_assert(exit_code == 0)
 
-    stdout = duthost.shell("echo $SSH_CONNECTION")["stdout"]
+    # Remote address is first part of SSH_CONNECTION: '10.250.0.1 47462 10.250.0.101 22'
+    stdout = stdout_stream.readlines()
     remote_address = stdout[0].split(" ")[0]
     check_server_received(ptfhost, remote_address)
