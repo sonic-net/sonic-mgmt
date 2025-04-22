38045c9b821aa65174004238b045cfda254a506a
sm-xu <xumei@microsoft.com>
2025-04-22 04:55:41 +0000

    update 04/21

--- test_reporting/telemetry/examples.py
+++ test_reporting/telemetry/examples.py
@@ -1,26 +1,23 @@
+import os
 from datetime import datetime
+from typing import Dict
 from reporter_factory import TelemetryReporterFactory
-from metric_definitions import *    #noqa: F401
+from metric_definitions import *    # noqa: F401
 from metrics import GaugeMetric
 
 
-def main():
-    """
-
-    PSU    Model            Serial             HW Rev    Voltage (V)    Current (A)    Power (W)  Status    LED
-    -----  ---------------  ---------------  --------  -------------  -------------  -----------  --------  -----
-    PSU 1  PWR-ABCD         1Z011010112349Q        01          12.09          18.38       222.00  OK        green
-    PSU 2  PWR-ABCD         1Z011010156787X        01          12.01          17.72       214.00  OK        green
-
-    """
-
+def MetricReporter():
     # user provided common labels
+
+    # In this example, the test case is hardcoded. In the actual test script, we can use
+    # request.node.originalname to dynamically retrieve the name of the running test case.
     common_labels = {
         METRIC_LABEL_TEST_CASE: "mock-case",
-        METRIC_LABEL_TEST_FILE: "mock-test.py",
     }
 
     # infra provided common labels
+    file_name = os.path.basename(__file__)
+    common_labels[METRIC_LABEL_TEST_FILE] = file_name
     common_labels[METRIC_LABEL_TESTBED] = "TB-XYZ"
     common_labels[METRIC_LABEL_TEST_BUILD] = "2024.1103"
 
@@ -31,85 +28,172 @@ def main():
 
     # Create a MetricReporterFactory and build a PeriodicMetricsReporter
     periodic_reporter = TelemetryReporterFactory.create_periodic_metrics_reporter(common_labels)
+    final_reporter = TelemetryReporterFactory.create_final_metrics_reporter(common_labels)
 
-    metric_labels = {METRIC_LABEL_DEVICE_ID: "switch-A"}
+    return periodic_reporter, final_reporter
+
+
+def psuMetrics(metrics_reporter) -> Dict[str, GaugeMetric]:
 
     # Create metrics
     voltage = GaugeMetric(name=METRIC_NAME_PSU_VOLTAGE,
                           description="Power supply unit voltage reading",
                           unit="V",
-                          reporter=periodic_reporter)
-
+                          reporter=metrics_reporter)
     current = GaugeMetric(name=METRIC_NAME_PSU_CURRENT,
                           description="Power supply unit current reading",
                           unit="A",
-                          reporter=periodic_reporter)
-
+                          reporter=metrics_reporter)
     power = GaugeMetric(name=METRIC_NAME_PSU_POWER,
                         description="Power supply unit power reading",
                         unit="W",
-                        reporter=periodic_reporter)
-
+                        reporter=metrics_reporter)
     status = GaugeMetric(name=METRIC_NAME_PSU_STATUS,
-                          description="Power supply unit status",
-                          unit="N/A",
-                          reporter=periodic_reporter)
-
+                         description="Power supply unit status",
+                         unit="N/A",
+                         reporter=metrics_reporter)
     led = GaugeMetric(name=METRIC_NAME_PSU_LED,
-                        description="Power supply unit LED state",
-                        unit="N/A",
-                        reporter=periodic_reporter)
+                      description="Power supply unit LED state",
+                      unit="N/A",
+                      reporter=metrics_reporter)
 
-    # Pass metrics to the reporter
+    return {
+        METRIC_NAME_PSU_VOLTAGE: voltage,
+        METRIC_NAME_PSU_CURRENT: current,
+        METRIC_NAME_PSU_POWER: power,
+        METRIC_NAME_PSU_STATUS: status,
+        METRIC_NAME_PSU_LED: led,
+    }
+
+
+def latencyMetrics(metrics_reporter) -> Dict[str, GaugeMetric]:
+
+    # Create metrics
+    bgp_port_restart = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_TIME_PORT_RESTART,
+                                   description="BGP convergence time when a port is shut down and start up",
+                                   unit="s",
+                                   reporter=metrics_reporter)
+    bgp_container_restart = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_TIME_CONTAINER_RESTART,
+                                        description="BGP convergence time when the bgp container restarts",
+                                        unit="s",
+                                        reporter=metrics_reporter)
+    bgp_nh_change = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_TIME_NEXTHOP_CHANGE,
+                                description="BGP convergence time when nexthops change",
+                                unit="s",
+                                reporter=metrics_reporter)
+
+    return {
+        METRIC_NAME_BGP_CONVERGENCE_TIME_PORT_RESTART: bgp_port_restart,
+        METRIC_NAME_BGP_CONVERGENCE_TIME_CONTAINER_RESTART: bgp_container_restart,
+        METRIC_NAME_BGP_CONVERGENCE_TIME_NEXTHOP_CHANGE: bgp_nh_change,
+    }
+
+
+def test_telemetry_example1(metrics_reporter, psu_metrics):
+    """
+
+    PSU    Model            Serial             HW Rev    Voltage (V)    Current (A)    Power (W)  Status    LED
+    -----  ---------------  ---------------  --------  -------------  -------------  -----------  --------  -----
+    PSU 1  PWR-ABCD         1Z011010112349Q        01          12.09          18.38       222.00  OK        green
+    PSU 2  PWR-ABCD         1Z011010156787X        01          12.01          17.72       214.00  OK        green
+
+    """
+    # Set shared metric labels
+    metric_labels = {METRIC_LABEL_DEVICE_ID: "switch-A"}
+
+    # Set non-shared metric labels
     metric_labels[METRIC_LABEL_DEVICE_PSU_ID] = "PSU 1"
     metric_labels[METRIC_LABEL_DEVICE_PSU_MODEL] = "PWR-ABCD"
     metric_labels[METRIC_LABEL_DEVICE_PSU_SERIAL] = "1Z011010112349Q"
-    voltage.record(metric_labels, 12.09)
-    current.record(metric_labels, 18.38)
-    power.record(metric_labels, 222.00)
-    status.record(metric_labels, PSU_STATUS.OK)
-    led.record(metric_labels, LED_STATE.GREEN)
 
-    # Pass metrics to the reporter
+    # Set metric values
+    psu_metrics[METRIC_NAME_PSU_VOLTAGE].record(metric_labels, 12.09)
+    psu_metrics[METRIC_NAME_PSU_CURRENT].record(metric_labels, 18.38)
+    psu_metrics[METRIC_NAME_PSU_POWER].record(metric_labels, 222.00)
+    psu_metrics[METRIC_NAME_PSU_STATUS].record(metric_labels, PSU_STATUS.OK)
+    psu_metrics[METRIC_NAME_PSU_LED].record(metric_labels, LED_STATE.GREEN)
+
+    # Set non-shared metric labels
     metric_labels[METRIC_LABEL_DEVICE_PSU_ID] = "PSU 2"
     metric_labels[METRIC_LABEL_DEVICE_PSU_MODEL] = "PWR-ABCD"
     metric_labels[METRIC_LABEL_DEVICE_PSU_SERIAL] = "1Z011010156787X"
-    voltage.record(metric_labels, 12.01)
-    current.record(metric_labels, 17.72)
-    power.record(metric_labels, 214.00)
-    status.record(metric_labels, PSU_STATUS.OK)
-    led.record(metric_labels, LED_STATE.GREEN)
 
-    # Report periodic metrics
-    periodic_reporter.report()
+    # Set metric values
+    psu_metrics[METRIC_NAME_PSU_VOLTAGE].record(metric_labels, 12.01)
+    psu_metrics[METRIC_NAME_PSU_CURRENT].record(metric_labels, 17.72)
+    psu_metrics[METRIC_NAME_PSU_POWER].record(metric_labels, 214.00)
+    psu_metrics[METRIC_NAME_PSU_STATUS].record(metric_labels, PSU_STATUS.OK)
+    psu_metrics[METRIC_NAME_PSU_LED].record(metric_labels, LED_STATE.GREEN)
 
-    # Build a FinalMetricsReporter
-    final_reporter = TelemetryReporterFactory.create_final_metrics_reporter(common_labels)
+    # Pass metrics to the reporter
+    metrics_reporter.report()
 
-    # Create metrics
-    bgp_port_restart = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_PORT_RESTART,
-                          description="BGP convergence time when a port is shut down and start up",
-                          unit="s",
-                          reporter=final_reporter)
-    bgp_container_restart = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_CONTAINER_RESTART,
-                               description="BGP convergence time when the bgp container restarts",
-                               unit="s",
-                               reporter=final_reporter)
-    bgp_nh_change = GaugeMetric(name=METRIC_NAME_BGP_CONVERGENCE_NEXTHOP_CHANGE,
-                       description="BGP convergence time when nexthops change",
-                       unit="s",
-                       reporter=final_reporter)
 
-    # reset metric_labels
-    metric_labels = []
+def test_telemetry_example2(metrics_reporter, psu_metrics):
+    """
+
+    PSU    Model            Serial             HW Rev    Voltage (V)    Current (A)    Power (W)  Status    LED
+    -----  ---------------  ---------------  --------  -------------  -------------  -----------  --------  -----
+    PSU 1  PWR-DECD         1Z0110101ASDF9Q        01          12.08          19.12       231.00  OK        green
+    PSU 2  PWR-DECD         1Z011010153FE7X        01          12.07          18.75       226.00  OK        green
+
+    """
+    # Set shared metric labels
+    metric_labels = {METRIC_LABEL_DEVICE_ID: "switch-Z"}
+
+    # Set non-shared metric labels
+    metric_labels[METRIC_LABEL_DEVICE_PSU_ID] = "PSU 1"
+    metric_labels[METRIC_LABEL_DEVICE_PSU_MODEL] = "PWR-DECD"
+    metric_labels[METRIC_LABEL_DEVICE_PSU_SERIAL] = "1Z0110101ASDF9Q"
+
+    # Set metric values
+    psu_metrics[METRIC_NAME_PSU_VOLTAGE].record(metric_labels, 12.08)
+    psu_metrics[METRIC_NAME_PSU_CURRENT].record(metric_labels, 19.12)
+    psu_metrics[METRIC_NAME_PSU_POWER].record(metric_labels, 231.00)
+    psu_metrics[METRIC_NAME_PSU_STATUS].record(metric_labels, PSU_STATUS.OK)
+    psu_metrics[METRIC_NAME_PSU_LED].record(metric_labels, LED_STATE.GREEN)
+
+    # Set non-shared metric labels
+    metric_labels[METRIC_LABEL_DEVICE_PSU_ID] = "PSU 2"
+    metric_labels[METRIC_LABEL_DEVICE_PSU_MODEL] = "PWR-DECD"
+    metric_labels[METRIC_LABEL_DEVICE_PSU_SERIAL] = "1Z011010153FE7X"
+
+    # Set metric values
+    psu_metrics[METRIC_NAME_PSU_VOLTAGE].record(metric_labels, 12.07)
+    psu_metrics[METRIC_NAME_PSU_CURRENT].record(metric_labels, 18.75)
+    psu_metrics[METRIC_NAME_PSU_POWER].record(metric_labels, 226.00)
+    psu_metrics[METRIC_NAME_PSU_STATUS].record(metric_labels, PSU_STATUS.OK)
+    psu_metrics[METRIC_NAME_PSU_LED].record(metric_labels, LED_STATE.GREEN)
+
+    # Pass metrics to the reporter
+    metrics_reporter.report()
+
+
+def test_telemetry_example3(metrics_reporter, latency_metrics):
+
+    # Set shared metric labels
     metric_labels = {METRIC_LABEL_DEVICE_ID: "switch-A"}
 
-    bgp_port_restart.record(metric_labels, 15)
-    bgp_container_restart.record(metric_labels, 72)
-    bgp_nh_change.record(metric_labels, 60)
+    # Set metric values
+    latency_metrics[METRIC_NAME_BGP_CONVERGENCE_TIME_PORT_RESTART].record(metric_labels, 15)
+    latency_metrics[METRIC_NAME_BGP_CONVERGENCE_TIME_CONTAINER_RESTART].record(metric_labels, 72)
+    latency_metrics[METRIC_NAME_BGP_CONVERGENCE_TIME_NEXTHOP_CHANGE].record(metric_labels, 60)
 
-    # Report final metrics
-    final_reporter.report()
+    # Pass metrics to the reporter
+    metrics_reporter.report()
+
+
+def main():
+
+    # Mimic pytest object resolution to create the metrics reporter and metrics automatically before running the tests.
+    periodic_reporter, final_reporter = MetricReporter()
+    psu_metrics = psuMetrics(periodic_reporter)
+    latency_metrics = latencyMetrics(final_reporter)
+
+    # Report telemetry metrics
+    test_telemetry_example1(periodic_reporter, psu_metrics)
+    test_telemetry_example2(periodic_reporter, psu_metrics)
+    test_telemetry_example3(final_reporter, latency_metrics)
 
 
 if __name__ == '__main__':
--- test_reporting/telemetry/metric_definitions.py
+++ test_reporting/telemetry/metric_definitions.py
@@ -5,45 +5,45 @@ from typing import Final
 from enum import Enum, unique
 
 # allowed metric labels
-METRIC_LABEL_TESTBED: Final[str] = "test.testbed"                   # testbed name/ID
-METRIC_LABEL_TEST_BUILD: Final[str] = "test.os.version"             # Software/build version
-METRIC_LABEL_TEST_CASE: Final[str] = "test.testcase"                # test case name/ID
-METRIC_LABEL_TEST_FILE: Final[str] = "test.file"                    # test file name
-METRIC_LABEL_TEST_JOBID: Final[str] = "test.job.id"                 # test job ID
-METRIC_LABEL_DEVICE_ID: Final[str] = "device.id"                    # device refers to the level of switch
+METRIC_LABEL_TESTBED: Final[str] = "test.testbed"                    # testbed name/ID
+METRIC_LABEL_TEST_BUILD: Final[str] = "test.os.version"              # Software/build version
+METRIC_LABEL_TEST_CASE: Final[str] = "test.testcase"                 # test case name/ID
+METRIC_LABEL_TEST_FILE: Final[str] = "test.file"                     # test file name
+METRIC_LABEL_TEST_JOBID: Final[str] = "test.job.id"                  # test job ID
+METRIC_LABEL_DEVICE_ID: Final[str] = "device.id"                     # device refers to the level of switch
 METRIC_LABEL_DEVICE_PORT_ID: Final[str] = "device.port.id"
 METRIC_LABEL_DEVICE_PSU_ID: Final[str] = "device.psu.id"
 METRIC_LABEL_DEVICE_PSU_MODEL: Final[str] = "device.psu.model"
 METRIC_LABEL_DEVICE_PSU_SERIAL: Final[str] = "device.psu.serial"
-METRIC_LABEL_DEVICE_PSU_HW_REV: Final[str] = "device.psu.hw_rev"    # hardware revision
+METRIC_LABEL_DEVICE_PSU_HW_REV: Final[str] = "device.psu.hw_rev"     # hardware revision
 METRIC_LABEL_DEVICE_QUEUE_ID: Final[str] = "device.queue.id"
-METRIC_LABEL_DEVICE_QUEUE_CAST: Final[str] = "device.queue.cast"    # unicast or multicast
+METRIC_LABEL_DEVICE_QUEUE_CAST: Final[str] = "device.queue.cast"     # unicast or multicast
 METRIC_LABEL_DEVICE_SENSOR_ID: Final[str] = "device.sensor.id"
-METRIC_LABEL_DEVICE_PG_ID: Final[str] = "device.pg.id"              # priority group
+METRIC_LABEL_DEVICE_PG_ID: Final[str] = "device.pg.id"               # priority group
 METRIC_LABEL_DEVICE_BUFFER_POOL_ID: Final[str] = "device.buffer_pool.id"
 METRIC_LABEL_DEVICE_INGRESS_PORT_ID: Final[str] = "device.ingress_port.id"
 METRIC_LABEL_DEVICE_EGRESS_PORT_ID: Final[str] = "device.egress_port.id"
-METRIC_LABEL_TRAFFIC_RATE: Final[str] = "traffic.rate"               # Measured as a percentage of the line rate
-METRIC_LABEL_TRAFFIC_PACKET_SIZE: Final[str] = "traffic.packet_size" # Measured in bytes
-METRIC_LABEL_TRAFFIC_RFC2889_FLAG: Final[str] = "traffic.rfc2889_flag"
+METRIC_LABEL_DEVICE_TG_TRAFFIC_RATE: Final[str] = "tg.traffic_rate"  # Measured as a percentage of the line rate
+METRIC_LABEL_DEVICE_TG_FRAME_BYTES: Final[str] = "tg.frame_bytes"    # Frame size measured in bytes
+METRIC_LABEL_DEVICE_TG_RFC2889_ENABLED: Final[str] = "tg.rfc2889.enabled"
 
-# allowed metric names
-# 1) metric names of periodic reports
+# Allowed metric names, grouped into two categories:
+# 1) Metrics included in periodic reports
 METRIC_NAME_PORT_STATE: Final[str] = "port.state"
 METRIC_NAME_PORT_RX_BPS: Final[str] = "port.rx.bps"
-METRIC_NAME_PORT_RX_UTILIZATION: Final[str] = "port.rx.utilization"
-METRIC_NAME_PORT_RX_OK_COUNTER: Final[str] = "port.rx.ok_counter"
-METRIC_NAME_PORT_RX_ERR_COUNTER: Final[str] = "port.rx.err_counter"
-METRIC_NAME_PORT_RX_DROP_COUNTER: Final[str] = "port.rx.drop_counter"
-METRIC_NAME_PORT_RX_OVERRUN_COUNTER: Final[str] = "port.rx.overrun_counter"
+METRIC_NAME_PORT_RX_UTIL_PCT: Final[str] = "port.rx.util.pct"   # utilization percentage
+METRIC_NAME_PORT_RX_PACKETS_OK: Final[str] = "port.rx.packets.ok"
+METRIC_NAME_PORT_RX_PACKETS_ERR: Final[str] = "port.rx.packets.err"
+METRIC_NAME_PORT_RX_PACKETS_DROP: Final[str] = "port.rx.packets.drop"
+METRIC_NAME_PORT_RX_PACKETS_OVERRUN: Final[str] = "port.rx.packets.overrun"
 METRIC_NAME_PORT_TX_BPS: Final[str] = "port.tx.bps"
-METRIC_NAME_PORT_TX_UTILIZATION: Final[str] = "port.tx.utilization"
-METRIC_NAME_PORT_TX_OK_COUNTER: Final[str] = "port.tx.ok_counter"
-METRIC_NAME_PORT_TX_ERR_COUNTER: Final[str] = "port.tx.err_counter"
-METRIC_NAME_PORT_TX_DROP_COUNTER: Final[str] = "port.tx.drop_counter"
-METRIC_NAME_PORT_TX_OVERRUN_COUNTER: Final[str] = "port.tx.overrun_counter"
+METRIC_NAME_PORT_TX_UTIL_PCT: Final[str] = "port.tx.util.pct"   # utilization percentage
+METRIC_NAME_PORT_TX_PACKETS_OK: Final[str] = "port.tx.packets.ok"
+METRIC_NAME_PORT_TX_PACKETS_ERR: Final[str] = "port.tx.packets.err"
+METRIC_NAME_PORT_TX_PACKETS_DROP: Final[str] = "port.tx.packets.drop"
+METRIC_NAME_PORT_TX_PACKETS_OVERRUN: Final[str] = "port.tx.packets.overrun"
 
-METRIC_NAME_QUEUE_WATERMARK: Final[str] = "queue.watermark"
+METRIC_NAME_QUEUE_WATERMARK_BYTES: Final[str] = "queue.watermark.bytes"
 
 METRIC_NAME_PSU_VOLTAGE: Final[str] = "psu.voltage"
 METRIC_NAME_PSU_CURRENT: Final[str] = "psu.current"
@@ -58,16 +58,19 @@ METRIC_NAME_TEMPERATURE_CRIT_HIGH_TH: Final[str] = "temperature.crit_high_th"
 METRIC_NAME_TEMPERATURE_CRIT_LOW_TH: Final[str] = "temperature.crit_low_th"
 METRIC_NAME_TEMPERATURE_WARNING: Final[str] = "temperature.warning"
 
-# 2) metric names of final status reports
-METRIC_NAME_LATENCY_MIN: Final[str] = "latency.min"
-METRIC_NAME_LATENCY_MAX: Final[str] = "latency.max"
-METRIC_NAME_LATENCY_AVG: Final[str] = "latency.avg"
+# 2) Metrics included in final status reports
 
-METRIC_NAME_NO_LOSS_MAX_RATE: Final[str] = "no_loss_max_rate" # Measured as a percentage of line rate
+# Latency are measured nano-seconds
+METRIC_NAME_LATENCY_L3_MIN_NS: Final[str] = "latency.l3.min.ns"
+METRIC_NAME_LATENCY_L3_MAX_NS: Final[str] = "latency.l3.max.ns"
+METRIC_NAME_LATENCY_L3_AVG_NS: Final[str] = "latency.l3.avg.ns"
 
-METRIC_NAME_BGP_CONVERGENCE_PORT_RESTART: Final[str] = "bgp.convergence.port_restart"
-METRIC_NAME_BGP_CONVERGENCE_CONTAINER_RESTART: Final[str] = "bgp.convergence.container_restart"
-METRIC_NAME_BGP_CONVERGENCE_NEXTHOP_CHANGE: Final[str] = "bgp.convergence.nexthop_change"
+METRIC_NAME_NO_LOSS_MAX_RATE: Final[str] = "no_loss_max_rate"     # Measured as a percentage of line rate
+
+# convergence time and route recover time are measured in seconds
+METRIC_NAME_BGP_CONVERGENCE_TIME_PORT_RESTART: Final[str] = "bgp.convergence_time.port_restart"
+METRIC_NAME_BGP_CONVERGENCE_TIME_CONTAINER_RESTART: Final[str] = "bgp.convergence_time.container_restart"
+METRIC_NAME_BGP_CONVERGENCE_TIME_NEXTHOP_CHANGE: Final[str] = "bgp.convergence_time.nexthop_change"
 
 METRIC_NAME_ROUTE_RECOVERY_PORT_RESTART: Final[str] = "route.recovery.port_restart"
 METRIC_NAME_ROUTE_RECOVERY_CONTAINER_RESTART: Final[str] = "route.recovery.container_restart"
@@ -76,9 +79,9 @@ METRIC_NAME_ROUTE_RECOVERY_NEXTHOP_CHANGE: Final[str] = "route.recovery.nexthop_
 METRIC_NAME_ECN_EGRESS_MARKING: Final[str] = "ecn.egress_marking"
 METRIC_NAME_ECN_ACCURACY_MARKING: Final[str] = "ecn.accuracy_marking"
 
-METRIC_NAME_FAILURE_TEST_SINGLE_LINK: Final[str] = "failure_test.single_link"
-METRIC_NAME_FAILURE_TEST_ALL_LINKS: Final[str] = "failure_test.all_links"
-METRIC_NAME_FAILURE_TEST_CONTAINER: Final[str] = "failure_test.container"
+METRIC_NAME_FAILURE_TEST_SINGLE_LINK_DOWN: Final[str] = "failure_test.single_link_down"
+METRIC_NAME_FAILURE_TEST_ALL_LINKS_DOWN: Final[str] = "failure_test.all_links_down"
+METRIC_NAME_FAILURE_TEST_CONTAINER_CRIPPLE: Final[str] = "failure_test.container_cripple"
 METRIC_NAME_FAILURE_TEST_DUT_REBOOT: Final[str] = "failure_test.DUT_reboot"
 METRIC_NAME_FAILURE_TEST_NEIGHBOR_REBOOT: Final[str] = "failure_test.neighbor_reboot"
 METRIC_NAME_FAILURE_TEST_ROUTE_WITHDRAWAL: Final[str] = "failure_test.route_withdrawal"
@@ -91,25 +94,37 @@ METRIC_NAME_PFC_LOSSY: Final[str] = "pfc.lossy"
 METRIC_NAME_QOS_DWRR: Final[str] = "qos.dwrr"
 
 # constant assignments
+
+
 @unique
 class ADMIN_STATUS(Enum):
     DOWN = 0
     UP = 1
+
 
 @unique
 class OPER_STATUS(Enum):
     DOWN = 0
     UP = 1
 
+
+@unique
+class FLAG(Enum):
+    ON = 0
+    OFF = 1
+
+
 @unique
 class WARNING_STATUS(Enum):
     FALSE = 0
     TRUE = 1
 
+
 @unique
 class FINAL_STATUS(Enum):
     PASS = 0
     FAIL = 1
+
 
 @unique
 class PSU_STATUS(Enum):
@@ -121,19 +136,22 @@ class PSU_STATUS(Enum):
     UNPOWERED = 5
     UNKNOWN = 6
 
+
 @unique
 class LED_STATE(Enum):
     GREEN = 1
     YELLOW = 2
     RED = 3
 
+
 @unique
 class QUEUE_TYPE(Enum):
     UNICAST = 1
     MULTICAST = 2
 
+
 @unique
-class METRIC_GROUP:
+class METRIC_GROUP(Enum):
     PORT_METRICS = "PORT_METRICS"
     PSU_METRICS = "PSU_METRICS"
     QUEUE_METRICS = "QUEUE_METRICS"
