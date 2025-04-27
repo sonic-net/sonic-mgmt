4b188ced110731c04ebf7afe4e63c8673e7bf1d3
karavasi <olympia.karavasili_arapogianni@nokia.com>
2025-04-27 13:17:33 +0200

    Fix conflicts

--- tests/generic_config_updater/test_cacl.py
+++ tests/generic_config_updater/test_cacl.py
@@ -48,7 +48,7 @@ def get_iptable_rules(duthost, ip_netns_namespace_prefix):
 
 @pytest.fixture(scope="module", autouse=True)
 def restore_test_env(duthosts, rand_one_dut_front_end_hostname):
-    duthost = duthosts[rand_one_dut_hostname]
+    duthost = duthosts[rand_one_dut_front_end_hostname]
     config_reload(duthost, config_source="minigraph", safe_reload=True)
     yield
 
