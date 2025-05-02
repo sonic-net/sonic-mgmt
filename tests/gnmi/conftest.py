97f37a230fb206e4b433bae384fb6b7c7c63b8e6
Verma-Anukul <anukulverma2013@gmail.com>
2025-05-02 00:23:36 -0700

    Addressed review comments

--- tests/gnmi/conftest.py
+++ tests/gnmi/conftest.py
@@ -10,7 +10,7 @@ from grpc_tools import protoc
 from tests.common.helpers.assertions import pytest_require as pyrequire
 from tests.common.helpers.dut_utils import check_container_state
 from tests.gnmi.helper import gnmi_container, apply_cert_config, recover_cert_config, create_ext_conf, create_ca_conf
-from tests.gnmi.helper import GNMI_SERVER_START_WAIT_TIME
+from tests.gnmi.helper import GNMI_SERVER_START_WAIT_TIME, check_ntp_sync_status
 from tests.common.gu_utils import create_checkpoint, rollback
 from tests.common.helpers.gnmi_utils import GNMIEnvironment
 from tests.common.helpers.ntp_helper import setup_ntp_context
@@ -48,7 +48,12 @@ def setup_gnmi_ntp_client_server(duthosts, rand_one_dut_hostname, ptfhost):
     duthost = duthosts[rand_one_dut_hostname]
 
     if duthost.facts['platform'] == 'x86_64-kvm_x86_64-r0':
-        pytest.skip("check_system_time_sync is skipped for this platform, so skip ntp setup")
+        logger.info("check_system_time_sync is skipped for this platform, so skip ntp setup")
+        return
+
+    if check_ntp_sync_status(duthost) == True:
+        logger.info("DUT is already in sycn with NTP server, so skip ntp setup")
+        return
 
     with setup_ntp_context(ptfhost, duthost, False):
         yield
