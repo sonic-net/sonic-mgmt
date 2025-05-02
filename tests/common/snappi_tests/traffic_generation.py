319b6f0a2a93dfc1871c1c68d4d2d773bc2394ee
shwnaik <shwnaik@cisco.com>
2025-05-02 00:27:58 -0700

    Check for Gr2 in single lossy priority case (#17769)
    Description of PR
    Summary:
    Fixes # (issue)
    
    Approach
    What is the motivation for this PR?
    Following tests were failing on Gr2:
    snappi_tests.pfc.test_pfc_pause_lossy_with_snappi#test_pfc_pause_single_lossy_prio::test_pfc_pause_single_lossy_prio
    Error:
    Failed: PFC pause frames should be received and counted in RX PFC counters for priority 2
    
    How did you do it?
    How did you verify/test it?
    Ran the test with changes and test passed
    
    Any platform specific information?
    Only for cisco-8000
    
    co-authorized by: jianquanye@microsoft.com
    
    Signed-off-by: opcoder0 <110003254+opcoder0@users.noreply.github.com>

--- tests/common/snappi_tests/traffic_generation.py
+++ tests/common/snappi_tests/traffic_generation.py
@@ -724,7 +724,9 @@ def verify_pause_frame_count_dut(rx_dut,
                 pytest_assert(pfc_pause_rx_frames == 0,
                               "PFC pause frames with no bit set in the class enable vector should be dropped")
             else:
-                if len(prios) > 1 and is_cisco_device(tx_dut) and not test_traffic_pause:
+                if ((len(prios) > 1 and is_cisco_device(tx_dut) and not test_traffic_pause) or
+                    (len(prios) == 1 and is_cisco_device(tx_dut) and
+                     "x86_64-8122" in tx_dut.facts['platform'] and not test_traffic_pause)):
                     pytest_assert(pfc_pause_rx_frames == 0,
                                   "PFC pause frames should not be counted in RX PFC counters for priority {}"
                                   .format(prios))
@@ -732,7 +734,6 @@ def verify_pause_frame_count_dut(rx_dut,
                     pytest_assert(pfc_pause_rx_frames > 0,
                                   "PFC pause frames should be received and counted in RX PFC counters for priority {}"
                                   .format(prio))
-
     for peer_port, prios in dut_port_config[0].items():  # PFC pause frames sent by DUT's ingress port to TGEN
         for prio in prios:
             pfc_pause_tx_frames = get_pfc_frame_count(rx_dut, peer_port, prio, is_tx=True)
