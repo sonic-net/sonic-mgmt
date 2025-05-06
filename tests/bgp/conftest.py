8f5c7ab7425cd7994dd064a084b821b2f5512138
Zhijian Li <zhijianli@microsoft.com>
2025-05-06 15:13:12 +0800

    [M1] Fix test_bgp_update_timer on M1 topo (#18222)
    What is the motivation for this PR?
    Fix test_bgp_update_timer on M1 topo
    
    How did you verify/test it?
    Verified on Arista-7050CX3 M1-48 testbed.
    
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/bgp/conftest.py
+++ tests/bgp/conftest.py
@@ -472,7 +472,7 @@ def setup_interfaces(duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhos
         setup_func = _setup_interfaces_dualtor
     elif tbinfo["topo"]["type"] in ["t0", "mx"]:
         setup_func = _setup_interfaces_t0_or_mx
-    elif tbinfo["topo"]["type"] in set(["t1", "t2"]):
+    elif tbinfo["topo"]["type"] in set(["t1", "t2", "m1"]):
         setup_func = _setup_interfaces_t1_or_t2
     elif tbinfo["topo"]["type"] == "m0":
         if topo_scenario == "m0_l3_scenario":
