0c58476d4a620c8f7cca3f5db3ee712fb25a356a
Zhixin Zhu <zhixzhu@cisco.com>
2025-05-06 10:35:00 -0700

    add import d0 in dshell script
    Signed-off-by: Zhixin Zhu <zhixzhu@cisco.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -2814,6 +2814,7 @@ def set_port_cir(interface, rate):
 
     def copy_set_voq_watchdog_script_cisco_8000(self, dut, asic="", enable=True):
         dshell_script = '''
+from common import d0
 def set_voq_watchdog(enable):
     d0.set_bool_property(sdk.la_device_property_e_VOQ_WATCHDOG_ENABLED, enable)
 set_voq_watchdog({})
