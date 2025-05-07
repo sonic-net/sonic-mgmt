11a9fd0e788fcc1c1f9ccfecf29164e77bf7fb1c
Anukul Verma <163091262+Verma-Anukul@users.noreply.github.com>
2025-05-07 11:44:12 +0530

    Changes to setup NTP before executing gnmi suite (#18204)
    * Adding ntp setup fixture for gnmi
    
    * Indentation fix
    
    * Fix for x86_64-kvm_x86_64-r0 platform to skip fixture
    
    * Addressed review comments
    
    * Fixed indentation issues
    
    * Pre-commit sanity fix
    
    * To retrigger pre-commit
    
    * Pre-sanity fix
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/gnmi/helper.py
+++ tests/gnmi/helper.py
@@ -146,20 +146,17 @@ def recover_cert_config(duthost):
     assert wait_until(300, 3, 0, check_gnmi_status, duthost), "GNMI service failed to start"
 
 
-def check_system_time_sync(duthost):
+def check_ntp_sync_status(duthost):
     """
     Checks if the DUT's time is synchronized with the NTP server.
-    If not synchronized, it attempts to restart the NTP service.
     """
 
     ntp_daemon = get_ntp_daemon_in_use(duthost)
 
     if ntp_daemon == NtpDaemon.CHRONY:
         ntp_status_cmd = "chronyc -c tracking"
-        restart_ntp_cmd = "sudo systemctl restart chrony"
     else:
         ntp_status_cmd = "ntpstat"
-        restart_ntp_cmd = "sudo systemctl restart ntp"
 
     ntp_status = duthost.command(ntp_status_cmd, module_ignore_errors=True)
     if (ntp_daemon == NtpDaemon.CHRONY and "Not synchronised" not in ntp_status["stdout"]) or \
@@ -167,18 +164,37 @@ def check_system_time_sync(duthost):
         logger.info("DUT %s is synchronized with NTP server.", duthost)
         return True
     else:
-        logger.info("DUT %s is NOT synchronized. Restarting NTP service...", duthost)
-        duthost.command(restart_ntp_cmd)
-        time.sleep(5)
-        # Rechecking status after restarting NTP
-        ntp_status = duthost.command(ntp_status_cmd, module_ignore_errors=True)
-        if (ntp_daemon == NtpDaemon.CHRONY and "Not synchronised" not in ntp_status["stdout"]) or \
-                (ntp_daemon != NtpDaemon.CHRONY and "synchronized" in ntp_status["stdout"]):
-            logger.info("DUT %s is now synchronized with NTP server.", duthost)
-            return True
-        else:
-            logger.error("DUT %s: NTP synchronization failed. Please check manually.", duthost)
-            return False
+        logger.info("DUT %s is NOT synchronized.", duthost)
+        return False
+
+
+def check_system_time_sync(duthost):
+    """
+    Checks if the DUT's time is synchronized with the NTP server.
+    If not synchronized, it attempts to restart the NTP service.
+    """
+
+    if check_ntp_sync_status(duthost) is True:
+        return True
+
+    ntp_daemon = get_ntp_daemon_in_use(duthost)
+
+    if ntp_daemon == NtpDaemon.CHRONY:
+        restart_ntp_cmd = "sudo systemctl restart chrony"
+    else:
+        restart_ntp_cmd = "sudo systemctl restart ntp"
+
+    logger.info("DUT %s is NOT synchronized. Restarting NTP service...", duthost)
+    duthost.command(restart_ntp_cmd)
+    time.sleep(5)
+    # Rechecking status after restarting NTP
+    ntp_status = check_ntp_sync_status(duthost)
+    if ntp_status is True:
+        logger.info("DUT %s is now synchronized with NTP server.", duthost)
+        return True
+    else:
+        logger.error("DUT %s: NTP synchronization failed. Please check manually.", duthost)
+        return False
 
 
 def gnmi_capabilities(duthost, localhost):
