dcaf24dae9bd7c2753b7869a7c32f1553256fde7
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2025-05-12 04:19:59 +0000

    Rename variable file to script

--- .azure-pipelines/impacted_area_testing/analyze_impact.py
+++ .azure-pipelines/impacted_area_testing/analyze_impact.py
@@ -22,9 +22,9 @@ def find_python_files(directory):
     """
     python_files = []
     for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".py"):
-                python_files.append(os.path.join(root, file))
+        for script in files:
+            if script.endswith(".py"):
+                python_files.append(os.path.join(root, script))
     return python_files
 
 
