b508e35840ac0a942fb927ae600748756d845bb6
Soumya Mishra <soumyamishra@microsoft.com>
2025-05-12 23:41:09 -0700

    update the ports used for the acl table

--- tests/acl/test_src_mac_rewrite.py
+++ tests/acl/test_src_mac_rewrite.py
@@ -31,8 +31,6 @@ ACL_REMOVE_RULES_FILE = "acl_rules_del.json"
 ACL_ADD_RULES_FILE = "acltb_test_rules_srcmac_rewrite.j2"
 ACL_RULES_FILE = 'acl_config.json'
 TMP_DIR = '/tmp'
-ACTION_FORWARD = 'FORWARD'
-ACTION_DROP = 'DROP'
 INGRESS = 'ingress'
 EGRESS = 'egress'
 IPV4 = 'ipv4'
@@ -92,50 +90,76 @@ def get_acl_counter(duthost, table_name, rule_name, timeout=ACL_COUNTERS_UPDATE_
 
     pytest.fail("ACL rule {} not found in table {}".format(rule_name, table_name))
 
+@pytest.fixture(scope='module')
+def get_portchannel_for_eth_ports(rand_selected_dut, tbinfo):
+    """
+    Returns a list of tuples: (eth_port, portchannel_name, ptf_port)
+    Selects the first two Ethernet ports and their associated PortChannels (if any).
+    """
+    mg_facts = rand_selected_dut.get_extended_minigraph_facts(tbinfo)
+    eth_to_portchannel = {}
+
+    for pc_name, pc_data in mg_facts["minigraph_portchannels"].items():
+        for member in pc_data["members"]:
+            eth_to_portchannel[member] = pc_name
+
+    # Pick two distinct Ethernet ports with ptf indices
+    eth_ports = list(mg_facts["minigraph_ptf_indices"].keys())
+    assert len(eth_ports) >= 3, "Need at least two Ethernet ports"
+
+    selected_ports = eth_ports[:3]  # Use the first two ports
+    result = []
+
+    for eth_port in selected_ports:
+        ptf_port = mg_facts["minigraph_ptf_indices"][eth_port]
+        pc_name = eth_to_portchannel.get(eth_port)
+        result.append((eth_port, pc_name, ptf_port))
+
+    logger.info("Selected ports and their mappings:")
+    for eth_port in selected_ports:
+        ptf_port = mg_facts["minigraph_ptf_indices"][eth_port]
+        pc_name = eth_to_portchannel.get(eth_port)
+        logger.info("  DUT port: %s | PortChannel: %s | PTF port: %s", eth_port, pc_name, ptf_port)
+        result.append((eth_port, pc_name, ptf_port))
+
+    return result
 
 @pytest.fixture(scope='module')
-def prepare_test_port(rand_selected_dut, tbinfo):
-    mg_facts = rand_selected_dut.get_extended_minigraph_facts(tbinfo)
+def prepare_test_ports(get_portchannel_for_eth_ports):
+    """
+    Returns: (ptf_port_1, ptf_port_2, test_port_1, test_port_2)
+    Each test_port is either a PortChannel or Ethernet port, depending on availability.
+    """
+    ports = get_portchannel_for_eth_ports
+    assert len(ports) == 3, "Expected exactly two test ports"
 
-    ports = list(mg_facts['minigraph_portchannels'])
-    if not ports:
-        ports = mg_facts["minigraph_acls"]["DataAcl"]
+    eth1, pc1, ptf1 = ports[1]
+    eth2, pc2, ptf2 = ports[2]
 
-    dut_port = ports[0] if ports else None
+    test_port_1 = pc1 if pc1 else eth1
+    test_port_2 = pc2 if pc2 else eth2
 
-    if not dut_port:
-        pytest.skip('No portchannels nor dataacl ports found')
-    if "Ethernet" in dut_port:
-        dut_eth_port = dut_port
-    elif "PortChannel" in dut_port:
-        dut_eth_port = mg_facts["minigraph_portchannels"][dut_port]["members"][0]
-    ptf_src_port = mg_facts["minigraph_ptf_indices"][dut_eth_port]
+    logger.info("Selected test ports:")
+    logger.info("  ptf_port_1: %s, dut_port_1: %s (PC: %s)", ptf1, eth1, pc1)
+    logger.info("  ptf_port_2: %s, dut_port_2: %s (PC: %s)", ptf2, eth2, pc2)
+    logger.info("  Using test_port_1: %s, test_port_2: %s", test_port_1, test_port_2)
 
-    topo = tbinfo["topo"]["type"]
-    # Get the list of upstream ports
-    upstream_ports = defaultdict(list)
-    upstream_port_ids = []
-    for interface, neighbor in list(mg_facts["minigraph_neighbors"].items()):
-        port_id = mg_facts["minigraph_ptf_indices"][interface]
-        if (topo == "t0" and "T1" in neighbor["name"]):
-            upstream_ports[neighbor['namespace']].append(interface)
-            upstream_port_ids.append(port_id)
-
-    return ptf_src_port, upstream_port_ids, dut_port
+    return ptf1, ptf2, test_port_1, test_port_2
 
 
-def setup_acl_table(duthost, prepare_test_port):
-    table_name = ACL_TABLE_NAME
-    _, _, dut_port = prepare_test_port
+def setup_acl_table(duthost, prepare_test_ports):
+    ptf_port_1, ptf_port_2, dut_port_1, dut_port_2 = prepare_test_ports
+    ports = [dut_port_1, dut_port_2]
+    duthost.shell(f"config acl remove table {ACL_TABLE_NAME}", module_ignore_errors=True)
 
     cmd = "config acl add table {} {} -s {} -p {}".format(
             ACL_TABLE_NAME,
             ACL_TABLE_TYPE,
             "egress",
-            dut_port
+            ",".join(ports)
         )
 
-    logger.info("Creating ACL table {} for testing".format(table_name))
+    logger.info("Creating ACL table {} for testing".format(ACL_TABLE_NAME))
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="TestAclSrcMacRewrite")
     loganalyzer.expect_regex = [LOG_EXPECT_ACL_TABLE_CREATE_RE]
     try:
@@ -143,14 +167,13 @@ def setup_acl_table(duthost, prepare_test_port):
             duthost.shell(cmd)
     except LogAnalyzerError:
         # Todo: cleanup
-        pytest.fail("Failed to create ACL table {}".format(table_name))
+        pytest.fail("Failed to create ACL table {}".format(ACL_TABLE_NAME))
 
 
 def remove_acl_table(duthost):
-    table_name = ACL_TABLE_NAME
-    cmd = "config acl remove table {}".format(table_name)
+    cmd = "config acl remove table {}".format(ACL_TABLE_NAME)
 
-    logger.info("Removing ACL table {}".format(table_name))
+    logger.info("Removing ACL table {}".format(ACL_TABLE_NAME))
     loganalyzer = LogAnalyzer(ansible_host=duthost, marker_prefix="TestAclSrcMacRewrite")
     loganalyzer.expect_regex = [LOG_EXPECT_ACL_TABLE_REMOVE_RE]
 
@@ -159,35 +182,39 @@ def remove_acl_table(duthost):
             duthost.shell(cmd)
     except LogAnalyzerError:
         # Todo: cleanup
-        pytest.fail("Failed to remove ACL table {}".format(table_name))
+        pytest.fail("Failed to remove ACL table {}".format(ACL_TABLE_NAME))
 
 
-def setup_acl_rules(duthost, inner_src_ip, vni, action, new_src_mac):
-    table_name = ACL_TABLE_NAME
-
+def setup_acl_rules(duthost, inner_src_ip, vni, new_src_mac):
     extra_vars = {
-        'table_name': table_name,
+        'table_name': ACL_TABLE_NAME,
         'vni': vni,
         'inner_src_ip': inner_src_ip,
-        'action': action,
         'new_src_mac': new_src_mac
         }
     dest_path = os.path.join(TMP_DIR, ACL_RULES_FILE)
     duthost.host.options['variable_manager'].extra_vars.update(extra_vars)
     duthost.file(path=dest_path, state='absent')
     duthost.template(src=os.path.join(TEMPLATES_DIR, ACL_ADD_RULES_FILE), dest=dest_path)
-    logger.info("Creating ACL rule matching src_ip {} action {}".format(inner_src_ip, action))
+    logger.info("Creating ACL rule matching src_ip {}".format(inner_src_ip))
     duthost.shell("config load -y {}".format(dest_path))
+
+    logger.info("Waiting for ACL rule to be applied")
+
+    time.sleep(5)
+
+    logger.info("Waiting for ACL rule to become active...")
+    output = duthost.shell("show acl rule")["stdout"]
+    logger.info("ACL rule dump:\n%s", output)
 
     if duthost.facts['asic_type'] != 'vs':
         pytest_assert(wait_until(60, 2, 0, check_rule_counters, duthost), "Acl rule counters are not ready")
 
 
 def remove_acl_rules(self, duthost):
-    table_name = ACL_TABLE_NAME
     duthost.copy(src=os.path.join(FILES_DIR, ACL_REMOVE_RULES_FILE), dest=TMP_DIR)
     remove_rules_dut_path = os.path.join(TMP_DIR, ACL_REMOVE_RULES_FILE)
-    duthost.command("acl-loader update full {} --table_name {}".format(remove_rules_dut_path, table_name))
+    duthost.command("acl-loader update full {} --table_name {}".format(remove_rules_dut_path, ACL_TABLE_NAME))
     time.sleep(5)
 
 
@@ -207,7 +234,7 @@ def test_modify_inner_src_mac_egress(duthost, ptfadapter, prepare_test_port):
 
     ptf_src_port, ptf_dst_ports, dut_port = prepare_test_port
 
-    setup_acl_rules(duthost, inner_src_ip, vni_id, ACTION_FORWARD, modified_inner_src_mac)
+    setup_acl_rules(duthost, inner_src_ip, vni_id, modified_inner_src_mac)
     # Create VXLAN-encapsulated packet sent by server
     inner_pkt = testutils.simple_udp_packet(
             eth_dst=duthost.facts['router_mac'],
@@ -256,14 +283,11 @@ def test_modify_inner_src_mac_egress(duthost, ptfadapter, prepare_test_port):
         inner_eth = actual_pkt.getlayer(scapy.Ether, 1)
         inner_src_mac = inner_eth.src if inner_eth else None
 
-        print(f"Inner source MAC: {inner_src_mac}")
+        logger.info("Inner source MAC: {inner_src_mac}")
 
         assert inner_src_mac == modified_inner_src_mac, f"Expected {modified_inner_src_mac},got {inner_src_mac}"
     else:
         assert False, "No packet received on port 0"
 
-    # Verify ACL counter incremented, confirming rule was applied
-    result = duthost.shell("show acl counter TEST_TABLE RULE_MODIFY_INNER_SRC_MAC")
-    assert "1" in result['stdout'], "ACL counter not incremented as expected"
     remove_acl_rules(duthost)
     remove_acl_table(duthost)
