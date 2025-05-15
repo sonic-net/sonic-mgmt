3000f1463aaf0990e241f591900431c6bcde9c40
Justin Wong <51811017+justin-wong-ce@users.noreply.github.com>
2025-05-14 18:44:57 -0700

    Add PTF parameter for ceos neighbor lacp multiplier (#18215)
    What is the motivation for this PR?
    To be able to have different LACP multipliers when running tests on different platforms.
    
    How did you do it?
    Mirror --neighbor_type param changes.
    
    How did you verify/test it?
    Tested with a Arista 7050-CX3 with and without the parameter. Can observe that the LACP multiplier is properly set on the ceos neighbors when parameter is given, and see that there are no changes when the parameter is not set.
    
    Tested wtih:
    
    sudo ./run_tests.sh -n ardut -u -x -c 'upgrade_path/test_upgrade_path.py::test_upgrade_path' -e "--base_image_list LINK-TO-20240510 --target_image_list LINK-TO-20241110 --ceos_neighbor_lacp_multiplier=5"

--- ansible/roles/test/files/ptftests/py3/advanced-reboot.py
+++ ansible/roles/test/files/ptftests/py3/advanced-reboot.py
@@ -180,6 +180,7 @@ class ReloadTest(BaseTest):
         self.check_param('asic_type', '', required=False)
         self.check_param('logfile_suffix', None, required=False)
         self.check_param('neighbor_type', 'eos', required=False)
+        self.check_param('ceos_neighbor_lacp_multiplier', 3, required=False)
         self.check_param('port_channel_intf_idx', [], required=False)
         if not self.test_params['preboot_oper'] or self.test_params['preboot_oper'] == 'None':
             self.test_params['preboot_oper'] = None
@@ -1405,9 +1406,9 @@ class ReloadTest(BaseTest):
         self.assertTrue(is_good, errors)
 
     def runTest(self):
-        # Set LACP timer multiplier to 5 for cEOS peers
-        if self.test_params['neighbor_type'] == "eos":
-            self.ceos_set_lacp_all_neighs(5)
+        # Set LACP timer multiplier for cEOS peers when it is not default (3)
+        if self.test_params['neighbor_type'] == "eos" and self.test_params['ceos_neighbor_lacp_multiplier'] != 3:
+            self.ceos_set_lacp_all_neighs(self.test_params['ceos_neighbor_lacp_multiplier'])
 
         self.pre_reboot_test_setup()
         try:
@@ -1463,7 +1464,7 @@ class ReloadTest(BaseTest):
             self.fails['dut'].add(traceback_msg)
         finally:
             # Restore cEOS LACP timer multiplier to default (3)
-            if self.test_params['neighbor_type'] == "eos":
+            if self.test_params['neighbor_type'] == "eos" and self.test_params['ceos_neighbor_lacp_multiplier'] != 3:
                 self.ceos_set_lacp_all_neighs(3)
 
             self.handle_post_reboot_test_reports()
