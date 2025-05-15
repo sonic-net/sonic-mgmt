6390c7dc9de8e033c3d22b6509b6497e063adcab
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2025-05-15 16:56:59 +1000

    [202412][PR: 18350] disable default pfcwd for all lossy DUT (#271)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    
    Summary:
    Update default_pfcwd_status to disable for some platform which only have
    lossy traffic.
    
    Note: This PR needs the image fix
    https://github.com/sonic-net/sonic-utilities/pull/3880 for it to work.
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [ ] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    
    ### Back port request
    - [ ] 202012
    - [ ] 202205
    - [ ] 202305
    - [ ] 202311
    - [ ] 202405
    - [x] 202412
    
    ### Approach
    #### What is the motivation for this PR?
    Update the default pfcwd status in golden_config for lossy platform
    
    #### How did you do it?
    Update the default pfcwd status in golden_config for lossy platform
    
    #### How did you verify/test it?
    Verified on physical platform. 
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -31,14 +31,28 @@ DUMMY_QUOTA = "dummy_single_quota"
 
 logger = logging.getLogger(__name__)
 
+LOSSY_HWSKU = frozenset({'Arista-7060X6-64PE-C256S2', 'Arista-7060X6-64PE-C224O8',
+                         'Mellanox-SN5600-C256S1', 'Mellanox-SN5600-C224O8',
+                         'Arista-7060X6-64PE-B-C512S2', 'Arista-7060X6-64PE-B-C448O16',
+                         'Mellanox-SN5640-C512S2', 'Mellanox-SN5640-C448O16'})
+
+
+def is_full_lossy_hwsku(hwsku):
+    """
+    Return True if the platform is lossy-only and PFCWD should default to ‘disable’.
+    """
+    return hwsku in LOSSY_HWSKU
+
 
 class GenerateGoldenConfigDBModule(object):
     def __init__(self):
         self.module = AnsibleModule(argument_spec=dict(topo_name=dict(required=True, type='str'),
-                                                       port_index_map=dict(require=False, type='dict', default=None)),
+                                                       port_index_map=dict(require=False, type='dict', default=None),
+                                                       hwsku=dict(require=False, type='str', default=None)),
                                     supports_check_mode=True)
         self.topo_name = self.module.params['topo_name']
         self.port_index_map = self.module.params['port_index_map']
+        self.hwsku = self.module.params['hwsku']
 
     def generate_mgfx_golden_config_db(self):
         rc, out, err = self.module.run_command("sonic-cfggen -H -m -j /etc/sonic/init_cfg.json --print-data")
@@ -96,6 +110,23 @@ class GenerateGoldenConfigDBModule(object):
 
         gold_config_db.update(dhcp_server_config_obj)
         return gold_config_db
+
+    def generate_full_lossy_golden_config_db(self):
+        rc, out, err = self.module.run_command("sonic-cfggen -H -m -j /etc/sonic/init_cfg.json --print-data")
+        if rc != 0:
+            self.module.fail_json(msg="Failed to get config from minigraph: {}".format(err))
+
+        # Generate config table from init_cfg.ini
+        ori_config_db = json.loads(out)
+
+        golden_config_db = {}
+        if "DEVICE_METADATA" in ori_config_db:
+            golden_config_db["DEVICE_METADATA"] = ori_config_db["DEVICE_METADATA"]
+            if ("localhost" in golden_config_db["DEVICE_METADATA"] and
+               "default_pfcwd_status" in golden_config_db["DEVICE_METADATA"]["localhost"]):
+                golden_config_db["DEVICE_METADATA"]["localhost"]["default_pfcwd_status"] = "disable"
+
+        return json.dumps(golden_config_db, indent=4)
 
     def check_version_for_bmp(self):
         output_version = device_info.get_sonic_version_info()
@@ -251,11 +282,17 @@ class GenerateGoldenConfigDBModule(object):
         return json.dumps(gold_config_db, indent=4)
 
     def generate(self):
+        module_msg = "Success to generate golden_config_db.json"
         # topo check
         if self.topo_name == "mx" or "m0" in self.topo_name:
             config = self.generate_mgfx_golden_config_db()
+            module_msg = module_msg + " for mgfx"
         elif self.topo_name == "t1-28-lag":
             config = self.generate_smartswitch_golden_config_db()
+            module_msg = module_msg + " for smartswitch"
+        elif self.hwsku and is_full_lossy_hwsku(self.hwsku):
+            module_msg = module_msg + " for full lossy hwsku"
+            config = self.generate_full_lossy_golden_config_db()
         else:
             config = "{}"
 
@@ -270,7 +307,7 @@ class GenerateGoldenConfigDBModule(object):
             temp_file.write(config)
         self.module.run_command("sudo rm -f {}".format(TEMP_DHCP_SERVER_CONFIG_PATH))
         self.module.run_command("sudo rm -f {}".format(TEMP_SMARTSWITCH_CONFIG_PATH))
-        self.module.exit_json(change=True, msg="Success to generate golden_config_db.json")
+        self.module.exit_json(change=True, msg=module_msg)
 
 
 def main():
