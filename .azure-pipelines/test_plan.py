015a696b7364ee1a49cd7ee7de60d65de0dd49ce
Chun'ang Li <39114813+lerry-lee@users.noreply.github.com>
2025-05-20 11:43:32 +0800

    [CI]Fix az login command change (#18475)
    [CI]Fix az login command change
    https://dev.azure.com/mssonic/build/_build/results?buildId=852194&view=logs&j=405cb77f-2a5c-554e-d5bf-3ae925ae989c&t=7d9d9900-e19c-536b-7c71-5dab0ae5924a&s=0a3b3f7d-0d77-5854-5590-307191428571
    Failed to az login with exception: Exception("Command az login --identity --username bc5d2320-ffa3-403d-92c0-7b27a8ae415e execution failed, rc=1, error=b'ERROR: Passing the managed identity ID with --username is no longer supported. Use --client-id, --object-id or --resource-id instead.\\n'").
    
    Official AZ CLI release note:
    https://learn.microsoft.com/en-us/cli/azure/upcoming-breaking-changes?view=azure-cli-latest&utm_source=chatgpt.com#login
    
    * Fix az login
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- .azure-pipelines/test_plan.py
+++ .azure-pipelines/test_plan.py
@@ -207,7 +207,8 @@ class TestPlanManager(object):
     def get_token(self):
 
         # 1. Run az login with re-try
-        az_login_cmd = f"az login --identity --username {self.managed_identity_id}"
+        print("az login --identity --client-id")
+        az_login_cmd = f"az login --identity --client-id {self.managed_identity_id}"
         az_login_attempts = 0
         while az_login_attempts < MAX_GET_TOKEN_RETRY_TIMES:
             try:
@@ -378,7 +379,7 @@ class TestPlanManager(object):
         }
         raw_resp = {}
         try:
-            raw_resp = requests.post(tp_url, headers=headers, data=json.dumps(payload), timeout=10)
+            raw_resp = requests.post(tp_url, headers=headers, data=json.dumps(payload), timeout=20)
             resp = raw_resp.json()
         except Exception as exception:
             raise Exception(f"HTTP execute failure, url: {tp_url}, raw_resp: {raw_resp}, exception: {str(exception)}")
