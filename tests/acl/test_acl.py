b2769fccfb6758b16e90fa8271b3b18b17041f2a
Zhijian Li <zhijianli@microsoft.com>
2025-05-22 12:08:28 +0800

    [M1] Adapt ACL test for M1 topo (#18514)
    What is the motivation for this PR?
    Adapt ACL test on M1 topo.
    
    How did you verify/test it?
    Verified on Arista-7050CX3 M1-48 testbed.

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -414,9 +414,10 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_selected_front_end_dut, ran
     # TODO: We should make this more robust (i.e. bind all active front-panel ports)
     acl_table_ports = defaultdict(list)
 
-    if topo in ["t0", "mx", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1", "t1-lag", "t1-28-lag",
-                                                                              "t1-isolated-d28u1", "t1-isolated-d28",
-                                                                              "t1-isolated-d128"):
+    if topo in ["t0", "mx", "m0_vlan", "m0_l3", "m1"] \
+            or tbinfo["topo"]["name"] in ("t1", "t1-lag", "t1-28-lag",
+                                          "t1-isolated-d28u1", "t1-isolated-d28",
+                                          "t1-isolated-d128"):
         for namespace, port in list(downstream_ports.items()):
             acl_table_ports[namespace] += port
             # In multi-asic we need config both in host and namespace.
