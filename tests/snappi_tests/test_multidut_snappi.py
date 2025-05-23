31311bee9779e253c2d7deb48a11e430abf2547a
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2025-05-23 12:11:10 +1000

    fix test_snappi_multidut.py (#18523)
    Description of PR
    Summary:
    Update test_snappi_multidut.py to adopt the new snappi version.
    
    Type of change
     Bug fix
     Testbed and Framework(new/improvement)
     New Test case
     Skipped for non-supported platforms
     Test case improvement
    Back port request
     202012
     202205
     202305
     202311
     202405
     202411
    Approach
    What is the motivation for this PR?
    To support new snappi model changes
    
    How did you do it?
    use new API to start/stop traffic.
    
    How did you verify/test it?
    verified on physical testbed.
    
    Any platform specific information?
    Supported testbed topology if it's a new test case?
    
    co-authorized by: jianquanye@microsoft.com

--- tests/snappi_tests/test_multidut_snappi.py
+++ tests/snappi_tests/test_multidut_snappi.py
@@ -153,9 +153,9 @@ def test_snappi(request,
     wait_for_arp(snappi_api, max_attempts=30, poll_interval_sec=2)
 
     # """ Start traffic """
-    ts = snappi_api.transmit_state()
-    ts.state = ts.START
-    snappi_api.set_transmit_state(ts)
+    cs = snappi_api.control_state()
+    cs.traffic.flow_transmit.state = cs.traffic.flow_transmit.START
+    snappi_api.set_control_state(cs)
 
     # """ Wait for traffic to finish """
     time.sleep(duration_sec)
@@ -187,9 +187,9 @@ def test_snappi(request,
     request.flow.flow_names = all_flow_names
     rows = snappi_api.get_metrics(request).flow_metrics
 
-    ts = snappi_api.transmit_state()
-    ts.state = ts.STOP
-    snappi_api.set_transmit_state(ts)
+    cs = snappi_api.control_state()
+    cs.traffic.flow_transmit.state = cs.traffic.flow_transmit.STOP
+    snappi_api.set_control_state(cs)
 
     """ Analyze traffic results """
     for row in rows:
