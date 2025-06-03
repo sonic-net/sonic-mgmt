e6980ab82829ce217e05168d57bd5e73dad630f9
StormLiangMS <89824293+StormLiangMS@users.noreply.github.com>
2025-06-03 08:44:45 +0800

    [CRM AVAILABLE] To enhance the crm tests for TD3 and Cisco devices (#18733)
    For TD3, the maximum number of nexthop groups is 255, which is below the minimum requirement of 256, resulting in test failures. To address this, TD3 should be added to SKU_NEXTHOP_THRESHOLDS.
    
    Additionally, some HWSKUs require more time for the crm command to become operational. To accommodate this, the wait time has been increased from 90 seconds to 360 seconds in worst case.
    
    What is the motivation for this PR?
    To fix the flaky failure on Cisco devices and the consistent failure on TD3
    
    How did you do it?
    For TD3, add 255 as a threshold.
    Extend maximum wait time from 90 seconds to 360 seconds.
    
    How did you verify/test it?
    Run test on TD3 and Cisco devices.
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/crm/test_crm_available.py
+++ tests/crm/test_crm_available.py
@@ -15,6 +15,9 @@ SKU_NEXTHOP_THRESHOLDS = {
     'nokia-m0-7215': 126,
     'nokia-7215-a1': 126,
     'nokia-7215': 126,
+    'arista-7050cx3-32s-c28s4': 255,
+    'Arista-7050CX3-32S-C32': 255,
+    'arista-7050cx3-32s-s128': 255,
 }
 
 DEFAULT_NEXTHOP_THRESHOLD = 256
@@ -29,8 +32,8 @@ def test_crm_next_hop_group(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
     duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
 
     hwsku = duthost.facts["hwsku"].lower()
-
-    nexthop_group_threshold = SKU_NEXTHOP_THRESHOLDS.get(hwsku, DEFAULT_NEXTHOP_THRESHOLD)
+    lower_sku_nexthop_thresholds = {k.lower(): v for k, v in SKU_NEXTHOP_THRESHOLDS.items()}
+    nexthop_group_threshold = lower_sku_nexthop_thresholds.get(hwsku, DEFAULT_NEXTHOP_THRESHOLD)
 
     resource_name = "nexthop_group"
     if resource_name in crm_resources:
