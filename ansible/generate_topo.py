a99fe0e3992da75253d4dff349c57cb54fbb00b7
Dashuai Zhang <dashuaizhang@microsoft.com>
2025-06-03 00:46:44 +0000

    fix pre-commit

--- ansible/generate_topo.py
+++ ansible/generate_topo.py
@@ -36,7 +36,7 @@ roles_cfg = {
         "asn": 65100,
         "asn_v6": 4200000000,
         "downlink": None,
-        "uplink": {"role": "t1", "asn": 64600, "asn_v6": 4200100000, "asn_increment": 0,},
+        "uplink": {"role": "t1", "asn": 64600, "asn_v6": 4200100000, "asn_increment": 0},
         "peer": {"role": "pt0", "asn": 65100, "asn_v6": 4200000000, "asn_increment": 1},
     },
     "t1": {
