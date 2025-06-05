5fcd219d55d5499122c9827c5928cd25634ef33d
zitingguo-ms <zitingguo@microsoft.com>
2025-06-05 21:41:37 +0800

    [ACL] Include service port into upstream neighbors (#18671)
    Summary:
    Fixes # (issue)
    In the topology with service ports, like t0-d18u8s4, t0-isolated-d16u16s1, t0-isolated-d32u32s2. we should include the PT0 neighbor port in the upstream neighbor list. With the enhancement in #18516, we can achieve this by adding pt0 in UPSTREAM_ALL_NEIGHBOR_MAP.

--- tests/acl/test_stress_acl.py
+++ tests/acl/test_stress_acl.py
@@ -193,7 +193,8 @@ def prepare_test_port(rand_selected_dut, tbinfo):
     upstream_port_neighbor_ips = {}
     for interface, neighbor in list(mg_facts["minigraph_neighbors"].items()):
         port_id = mg_facts["minigraph_ptf_indices"][interface]
-        if (topo == "t1" and "T2" in neighbor["name"]) or (topo == "t0" and "T1" in neighbor["name"]) or \
+        if (topo == "t1" and "T2" in neighbor["name"]) or \
+                (topo == "t0" and ("T1" in neighbor["name"] or "PT0" in neighbor["name"])) or \
                 (topo == "m0" and "M1" in neighbor["name"]) or (topo == "mx" and "M0" in neighbor["name"]) or \
                 (topo_name in ("t1-isolated-d32", "t1-isolated-d128") and "T0" in neighbor["name"]):
             upstream_ports[neighbor['namespace']].append(interface)
