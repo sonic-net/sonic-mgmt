b24b0c5f55d6730bdc326153c0e6488982dd51fe
yatishkoul <yatishkoul@gmail.com>
2025-06-06 10:27:35 -0700

    Resolving Merge Conflicts 01

--- tests/snappi_tests/pfc/test_lossless_response_to_throttling_pause_storms.py
+++ tests/snappi_tests/pfc/test_lossless_response_to_throttling_pause_storms.py
@@ -7,11 +7,12 @@ from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi
     get_snappi_ports_multi_dut, is_snappi_multidut, snappi_port_selection, tgen_port_info, \
     snappi_api, snappi_dut_base_config, get_snappi_ports, get_snappi_ports_for_rdma, cleanup_config  # noqa: F401
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, \
-    lossless_prio_list, disable_pfcwd                                                                # noqa: F401
+    lossless_prio_list, disable_pfcwd                                                           # noqa: F401
 from tests.snappi_tests.pfc.files.lossless_response_to_throttling_pause_storms_helper import (
     run_lossless_response_to_throttling_pause_storms_test)
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
-from tests.common.snappi_tests.variables import pfcQueueGroupSize, pfcQueueValueDict                 # noqa: F401
+from tests.common.snappi_tests.variables import pfcQueueGroupSize, pfcQueueValueDict     # noqa: F401
+from tests.snappi_tests.cisco.helper import disable_voq_watchdog                  # noqa: F401
 logger = logging.getLogger(__name__)
 pytestmark = [pytest.mark.topology('multidut-tgen', 'tgen')]
 
