4ae8ac58e7c5291181a1d56ad0beb5b229e0886a
Guy Shemesh <gshemesh@nvidia.com>
2025-06-11 14:58:41 +0300

    Adding WA to for Port Error Test
    1. adding reeboot before running the test to clean up the db for reset
       the faults counters
    2. adding command that bringup all the ports od the device
    3. addding condition for the picked ports, that they are up and
       connected to optical module.
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -5,6 +5,7 @@ import time
 
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import skip_release
+from tests.common.platform.transceiver_utils import parse_sfp_eeprom_infos
 
 pytestmark = [
     pytest.mark.disable_loganalyzer,  # disable automatic loganalyzer
@@ -44,23 +45,46 @@ class TestMACFault(object):
     def get_interface_status(dut, interface):
         return dut.show_and_parse("show interfaces status {}".format(interface))[0].get("oper", "unknown")
 
-    @pytest.fixture
+    @staticmethod
+    def verify_ports_startup(dut):
+        dut.shell('show interface status | awk \'NR>2 {print $1}\' | xargs -I{} sudo config interface startup {}')
+        time.sleep(30)
+
+    #TODO: Reboot the switch before test as a WA of #4384527, remove this after the issue is fixed
+    @pytest.fixture(scope="class", autouse=True)
+    def reboot_dut(self, duthosts, localhost, enum_rand_one_per_hwsku_frontend_hostname):
+        from infra.tools.redmine.redmine_api import is_redmine_issue_active
+        from tests.common.reboot import reboot
+        if is_redmine_issue_active([4384527])[0]:
+            reboot(duthosts[enum_rand_one_per_hwsku_frontend_hostname],
+                   localhost, safe_reboot=True, check_intf_up_ports=False)
+
+    @pytest.fixture(scope="class")
     def select_random_interfaces(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname):
         dut = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
-        interfaces = list(dut.show_and_parse("show interfaces status"))
+
+        self.verify_ports_startup(dut)
 
         sfp_presence = dut.command(cmd_sfp_presence)
         parsed_presence = {line.split()[0]: line.split()[1] for line in sfp_presence["stdout_lines"][2:]}
 
-        available_interfaces = [
-            intf["interface"] for intf in interfaces
-            if parsed_presence.get(intf["interface"]) == "Present"
-        ]
+        eeprom_infos = dut.command("sudo sfputil show eeprom -d")['stdout']
+        eeprom_infos = parse_sfp_eeprom_infos(eeprom_infos)
 
-        pytest_assert(available_interfaces, "No interfaces with SFP detected. Cannot proceed with tests.")
+        available_optical_interfaces = []
+        for port_name, eeprom_info in eeprom_infos.items():
+            if parsed_presence.get(port_name) == "Present" and \
+                    "SFP EEPROM detected" in eeprom_info[port_name] \
+                    and "COPPER" not in eeprom_info.get("Media Interface Technology", "COPPER").upper() \
+                    and self.get_interface_status(dut, interface) == "up":
+                available_optical_interfaces.append(port_name)
+
+        pytest_assert(available_optical_interfaces, "No interfaces with SFP detected. Cannot proceed with tests.")
+        logging.info("Available Optical interfaces for tests: {}".format(available_optical_interfaces))
 
         # Select 5 random interfaces (or fewer if not enough available)
-        selected_interfaces = random.sample(available_interfaces, min(5, len(available_interfaces)))
+        selected_interfaces = random.sample(available_optical_interfaces, min(5, len(available_optical_interfaces)))
+        logging.info("Selected interfaces for tests: {}".format(selected_interfaces))
 
         return dut, selected_interfaces
 
@@ -77,19 +101,21 @@ class TestMACFault(object):
             dut.shell("sudo sfputil debug rx-output {} disable".format(interface))
             time.sleep(5)
             pytest_assert(self.get_interface_status(dut, interface) == "down",
-                          "Interface {} did not go down after disabling rx-output using sfputil".format(interface))
+                          "Interface {iface} did not go down after 'sudo sfputil debug rx-output {iface} disable'"
+                          .format(iface=interface))
 
             dut.shell("sudo sfputil debug rx-output {} enable".format(interface))
             time.sleep(20)
             pytest_assert(self.get_interface_status(dut, interface) == "up",
-                          "Interface {} did not come up after enabling rx-output using sfputil".format(interface))
+                          "Interface {iface} did not come up after 'sudo sfputil debug rx-output {iface} enable'"
+                          .format(iface=interface))
 
             local_fault_after = self.get_mac_fault_count(dut, interface, "mac local fault")
             logging.info("MAC local fault count after disabling/enabling rx-output using sfputil {}: {}".format(
                 interface, local_fault_after))
 
             pytest_assert(local_fault_after > local_fault_before,
-                          "MAC local fault count did not increment after disabling/enabling tx-output on the device")
+                          "MAC local fault count did not increment after disabling/enabling rx-output on the device")
 
     def test_mac_remote_fault_increment(self, select_random_interfaces):
         dut, interfaces = select_random_interfaces
@@ -104,17 +130,20 @@ class TestMACFault(object):
             dut.shell("sudo sfputil debug tx-output {} disable".format(interface))
             time.sleep(5)
             pytest_assert(self.get_interface_status(dut, interface) == "down",
-                          "Interface {} did not go down after disabling rx-output using sfputil".format(interface))
+                          "Interface {iface} did not go down after 'sudo sfputil debug tx-output {iface} disable'"
+                          .format(iface=interface))
 
             dut.shell("sudo sfputil debug tx-output {} enable".format(interface))
             time.sleep(20)
 
             pytest_assert(self.get_interface_status(dut, interface) == "up",
-                          "Interface {} did not come up after disabling tx-output using sfputil".format(interface))
+                          "Interface {iface} did not come up after 'sudo sfputil debug tx-output {iface} enable'"
+                          .format(iface=interface))
 
             remote_fault_after = self.get_mac_fault_count(dut, interface, "mac remote fault")
-            logging.info("MAC remote fault count after disabling/enabling rx-output using sfputil {}: {}".format(
+            logging.info("MAC remote fault count after disabling/enabling tx-output using sfputil {}: {}".format(
                 interface, remote_fault_after))
 
             pytest_assert(remote_fault_after > remote_fault_before,
                           "MAC remote fault count did not increment after disabling/enabling tx-output on the device")
+
