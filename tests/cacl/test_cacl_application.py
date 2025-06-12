2b0af7c193e2376a88e56ebc6f035eade9dbdf8f
Yawen <yawenni@microsoft.com>
2025-06-12 12:29:05 +1000

    [DualToR] follow-up to #18766: add missing iptables rule (#18920)
    What is the motivation for this PR?
    This is a follow-up to #18766, which enforced same loopback ip and drop bgp pkts on loopback1 for dualtor.
    However, I noticed that one commit related to appending the iptables/ip6tables rule was not included in the previous PR. This PR adds the missing iptables rule to ensure that is correctly applied.
    
    How did you do it?
    Append the missing commit.
    
    How did you verify/test it?
    It was verified in #18766

--- tests/cacl/test_cacl_application.py
+++ tests/cacl/test_cacl_application.py
@@ -580,11 +580,11 @@ def generate_expected_rules(duthost, tbinfo, docker_network, asic_index, expecte
         rules_to_expect_for_dualtor = [
                                        "-A INPUT -p udp -m udp --dport 67 -j DHCP",
                                        "-A DHCP -j RETURN",
-                                       "-I INPUT 1 -d 10.1.0.34 -p tcp --dport 179 -j DROP",
-                                       " ip6tables -I INPUT 1 -d fc00:1:0:34:: -p tcp --dport 179 -j DROP",
+                                       "-A INPUT -d 10.1.0.34/32 -p tcp -m tcp --dport 179 -j DROP",
                                        "-N DHCP"
                                       ]
         iptables_rules.extend(rules_to_expect_for_dualtor)
+        ip6tables_rules.append("-A INPUT -d fc00:1:0:34::/128 -p tcp -m tcp --dport 179 -j DROP")
 
     # On standby tor, it has expected dhcp mark iptables rules.
     if expected_dhcp_rules_for_standby:
