b9112e69a34a79b12e44898d3230bd149e9af04c
sonmathu <sonmathu@cisco.com>
2025-06-12 17:28:00 -0700

    MIGSOFTWAR-22760: Fixing error in test_verify_cpu_port_config on T2 (#1294)

--- sonic-mgmt/tests/cisco/qos/test_cpu_port_config.py
+++ sonic-mgmt/tests/cisco/qos/test_cpu_port_config.py
@@ -92,79 +92,66 @@ def test_verify_cpu_port_config(duthosts, rand_one_dut_hostname, pg_to_test, req
     if 'hbm_usage' in data:
         hbm_present = True
 
-    asic_facts = get_asic_facts(duthost)
-    asic_namespace_string = ""
-    asics = []
+    show_command = "sudo show platform npu voq cgm_profile -i CPU -t {} -d"
 
-    asics = asic_facts if duthost.is_multi_asic else ['']
+    logging.info("Checking CPU Port")
+    cmd = show_command.format(pg_to_test)
+    result = duthost.command(cmd)
+    verify_command_result(result, cmd)
 
-    for asic in asics:
-        if not asic:
-            asic_namespace_string = ''
-            asic = 'asic0'
-        else:
-            asic_namespace_string = " -n " + str(asic)
+    json_str = result["stdout"].strip()
+    try:
+        data = json.loads(json_str)
+    except Exception as e:
+        pytest.fail("JSON load error: {}".format(e))
 
-        show_command = "sudo show platform npu voq cgm_profile -i CPU -t {}{} -d"
-
-        logging.info("Checking CPU Port")
-        cmd = show_command.format(pg_to_test, asic_namespace_string)
-        result = duthost.command(cmd)
-        verify_command_result(result, cmd)
-
-        json_str = result["stdout"].strip()
-        try:
-            data = json.loads(json_str)
-        except Exception as e:
-            pytest.fail("JSON load error: {}".format(e))
-
-        voq_drop_data = None
-        if "voq_drop_prob_g" in data:
-            voq_drop_data = data["voq_drop_prob_g"]
-            if voq_drop_data:
-                sms_quant_len = len(voq_drop_data)
-                voq_quant_len = len(voq_drop_data[0])
-                age_quant_len = len(voq_drop_data[0][1])
-
-        if hbm_present:
-            voq_evict_data = None
-            if "voq_evict_prob_g" not in data:
-                logging.info("Eviction data unavailable for CPU Port PG {}."
-                             " Please check".format(pg_to_test))
-            else:
-                voq_evict_data = data["voq_evict_prob_g"]
-                if not voq_drop_data and voq_evict_data:
-                    sms_quant_len = len(voq_evict_data)
-                    voq_quant_len = len(voq_evict_data[0])
-                    age_quant_len = len(voq_evict_data[0][1])
-
-                    ''' Verify evict is 0 for all quant'''
-                    if voq_drop_data:
-                        for g_idx in range(sms_quant_len):
-                            for voq_idx in range(voq_quant_len):
-                                for age_idx in range(age_quant_len):
-                                    actual_value = voq_evict_data[g_idx][voq_idx][age_idx]
-                                    expected_value = 0
-                                    assert (
-                                            actual_value == expected_value
-                                    ), '''
-                                            Eviction not expected for CPU Port PG {} at
-                                            SMS/VoQ/Age region {}/{}/{} Expected: {} Actual: {}
-                                         '''.format(pg_to_test, g_idx, voq_idx,
-                                                    age_idx, expected_value, actual_value)
-
-        ''' Verify drop is 7 for last quant only'''
-        DROP_PROB_100 = 7
+    voq_drop_data = None
+    if "voq_drop_prob_g" in data:
+        voq_drop_data = data["voq_drop_prob_g"]
         if voq_drop_data:
-            for g_idx in range(sms_quant_len):
-                for voq_idx in range(voq_quant_len):
-                    for age_idx in range(age_quant_len):
-                        actual_value = voq_drop_data[g_idx][voq_idx][age_idx]
-                        expected_value = DROP_PROB_100 if voq_idx == (voq_quant_len - 1) else 0
-                        assert (
-                                actual_value == expected_value
-                        ), '''
-                                Drop Probability not as expected for CPU Port PG {} at
-                                SMS/VoQ/Age region {}/{}/{} Expected: {} Actual: {}
-                             '''.format(pg_to_test, g_idx, voq_idx,
-                                        age_idx, expected_value, actual_value)
+            sms_quant_len = len(voq_drop_data)
+            voq_quant_len = len(voq_drop_data[0])
+            age_quant_len = len(voq_drop_data[0][1])
+
+    if hbm_present:
+        voq_evict_data = None
+        if "voq_evict_prob_g" not in data:
+            logging.info("Eviction data unavailable for CPU Port PG {}."
+                            " Please check".format(pg_to_test))
+        else:
+            voq_evict_data = data["voq_evict_prob_g"]
+            if not voq_drop_data and voq_evict_data:
+                sms_quant_len = len(voq_evict_data)
+                voq_quant_len = len(voq_evict_data[0])
+                age_quant_len = len(voq_evict_data[0][1])
+
+                ''' Verify evict is 0 for all quant'''
+                if voq_drop_data:
+                    for g_idx in range(sms_quant_len):
+                        for voq_idx in range(voq_quant_len):
+                            for age_idx in range(age_quant_len):
+                                actual_value = voq_evict_data[g_idx][voq_idx][age_idx]
+                                expected_value = 0
+                                assert (
+                                        actual_value == expected_value
+                                ), '''
+                                        Eviction not expected for CPU Port PG {} at
+                                        SMS/VoQ/Age region {}/{}/{} Expected: {} Actual: {}
+                                        '''.format(pg_to_test, g_idx, voq_idx,
+                                                age_idx, expected_value, actual_value)
+
+    ''' Verify drop is 7 for last quant only'''
+    DROP_PROB_100 = 7
+    if voq_drop_data:
+        for g_idx in range(sms_quant_len):
+            for voq_idx in range(voq_quant_len):
+                for age_idx in range(age_quant_len):
+                    actual_value = voq_drop_data[g_idx][voq_idx][age_idx]
+                    expected_value = DROP_PROB_100 if voq_idx == (voq_quant_len - 1) else 0
+                    assert (
+                            actual_value == expected_value
+                    ), '''
+                            Drop Probability not as expected for CPU Port PG {} at
+                            SMS/VoQ/Age region {}/{}/{} Expected: {} Actual: {}
+                            '''.format(pg_to_test, g_idx, voq_idx,
+                                    age_idx, expected_value, actual_value)
