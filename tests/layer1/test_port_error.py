9472d6c7803b2b1b0407f846d9e66570b1241ea9
gshemesh2 <gshemesh@nvidia.com>
2025-06-17 16:03:57 +0300

    Update test_port_error.py
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -45,16 +45,12 @@ class TestMACFault(object):
     def get_interface_status(dut, interface):
         return dut.show_and_parse("show interfaces status {}".format(interface))[0].get("oper", "unknown")
 
+    #TODO: Reboot the switch before test as a WA of #22205, remove this after the issue is fixed
     @pytest.fixture(scope="class", autouse=True)
     def reboot_dut(self, duthosts, localhost, enum_rand_one_per_hwsku_frontend_hostname, request):
-        from tests.common.plugins.loganalyzer_dynamic_errors_ignore.la_dynamic_errors_ignore import (
-            GitHubDynamicErrorsIgnore
-        )
         from tests.common.reboot import reboot
-        github_checker = GitHubDynamicErrorsIgnore(conditions_dict={}, pytest_item_obj=request)
-        if github_checker.is_github_issue_active("https://github.com/sonic-net/sonic-buildimage/issues/22205"):
-            reboot(duthosts[enum_rand_one_per_hwsku_frontend_hostname],
-                   localhost, safe_reboot=True, check_intf_up_ports=True)
+        reboot(duthosts[enum_rand_one_per_hwsku_frontend_hostname],
+              localhost, safe_reboot=True, check_intf_up_ports=True)
 
     @pytest.fixture(scope="class")
     def select_random_interfaces(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname):
