29cc6b28a7e5f5636fde4523c12c0bf7b45179e3
Austin (Thang Pham) <austinpham@microsoft.com>
2025-06-18 16:18:04 +1000

    feat: update port alias for 64pe-b-p32o64 (#18949)
    Signed-off-by: Austin Pham <austinpham@microsoft.com>
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -130,6 +130,12 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                     port_alias_to_name_map["Ethernet%d/%d" % (i, j)] = "Ethernet%d" % ((i - 1) * 8 + j - 1)
             port_alias_to_name_map["Ethernet65"] = "Ethernet512"
             port_alias_to_name_map["Ethernet66"] = "Ethernet513"
+        elif hwsku == "Arista-7060X6-64PE-B-P32O64":
+            for i in range(1, 33):
+                port_alias_to_name_map["etp%d" % (i)] = "Ethernet%d" % ((i - 1) * 8)
+            for i in range(33, 65):
+                for x, j in zip([1, 5], ["a", "b"]):
+                    port_alias_to_name_map["etp%d%s" % (i, j)] = "Ethernet%d" % ((i - 1) * 8 + x - 1)
         elif hwsku == "Arista-7060X6-64PE-256x200G":
             for i in range(1, 65):
                 for j in [1, 3, 5, 7]:
