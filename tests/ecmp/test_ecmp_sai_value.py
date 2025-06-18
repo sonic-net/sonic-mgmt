3dd3c41e45705bf9a2a743843a52d6d753d06aab
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2025-06-18 16:10:38 +0800

    Skip frr_bmp during container autorestart check in test_ecmp_sai_value (#19042)
    What is the motivation for this PR?
    Fix the error:
    failed on setup with "KeyError: 'frr_bmp'"
    
    frr_bmp shows up in show feature status but it isn't a real docker container so we should skip it.
    
    How did you do it?
    Skip frr_bmp during container autorestart check in test_ecmp_sai_value
    
    How did you verify/test it?
    Run test_ecmp_sai_value on testbed.
    
    -------------------------------------------------------------------------------------------------- live log sessionfinish --------------------------------------------------------------------------------------------------
    02:15:51 __init__.pytest_terminal_summary         L0067 INFO   | Can not get Allure report URL. Please check logs
    ======================================================================================= 10 passed, 9 warnings in 1039.59s (0:17:19) ========================================================================================
    DEBUG:tests.conftest:[log_custom_msg] item: <Function test_ecmp_offset_value[bjw2-can-7260-12-warm-reboot]>
    INFO:root:Can not get Allure report URL. Please check logs

--- tests/ecmp/test_ecmp_sai_value.py
+++ tests/ecmp/test_ecmp_sai_value.py
@@ -54,12 +54,18 @@ def enable_container_autorestart(duthosts, enum_rand_one_per_hwsku_frontend_host
     container_autorestart_states = duthost.get_container_autorestart_states()
     for feature, status in list(feature_list.items()):
         # Enable container autorestart only if the feature is enabled and container autorestart is disabled.
+        if feature == "frr_bmp":
+            # Skip frr_bmp since it's not container just bmp option used by bgpd
+            continue
         if status == 'enabled' and container_autorestart_states[feature] == 'disabled':
             duthost.shell("sudo config feature autorestart {} enabled".format(feature))
 
     yield
     for feature, status in list(feature_list.items()):
         # Disable container autorestart back if it was initially disabled.
+        if feature == "frr_bmp":
+            # Skip frr_bmp since it's not container just bmp option used by bgpd
+            continue
         if status == 'enabled' and container_autorestart_states[feature] == 'disabled':
             duthost.shell("sudo config feature autorestart {} disabled".format(feature))
 
