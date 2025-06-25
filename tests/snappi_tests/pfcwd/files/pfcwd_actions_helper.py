6768dd91ee3e422f38f24e8415d38aad037162cf
v-jessgeorge <v-jessgeorge@microsoft.com>
2025-06-25 10:44:15 +0530

    Fixing pre-commit errors in folder snappi_tests with 41 folders (#19162)
    Replaced noqa with noqa: and pre-commit spacing issues has been addressed in folder: tests/snappi_tests

--- tests/snappi_tests/pfcwd/files/pfcwd_actions_helper.py
+++ tests/snappi_tests/pfcwd/files/pfcwd_actions_helper.py
@@ -3,15 +3,15 @@ import time
 
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
-    fanout_graph_facts  # noqa F401
+    fanout_graph_facts  # noqa: F401
 from tests.common.snappi_tests.common_helpers import pfc_class_enable_vector,\
     get_lossless_buffer_size, get_pg_dropped_packets,\
     stop_pfcwd, disable_packet_aging, sec_to_nanosec,\
     get_pfc_frame_count, packet_capture, config_capture_pkt,\
     start_pfcwd, enable_packet_aging, start_pfcwd_fwd, \
-    traffic_flow_mode, calc_pfc_pause_flow_rate      # noqa F401
-from tests.common.snappi_tests.port import select_ports, select_tx_port  # noqa F401
-from tests.common.snappi_tests.snappi_helpers import wait_for_arp  # noqa F401
+    traffic_flow_mode, calc_pfc_pause_flow_rate      # noqa: F401
+from tests.common.snappi_tests.port import select_ports, select_tx_port  # noqa: F401
+from tests.common.snappi_tests.snappi_helpers import wait_for_arp  # noqa: F401
 from tests.common.snappi_tests.traffic_generation import verify_pause_flow, \
     verify_basic_test_flow, verify_background_flow, verify_pause_frame_count_dut, \
     run_traffic_and_collect_stats, multi_base_traffic_config, verify_egress_queue_frame_count, \
