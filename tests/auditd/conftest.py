473d6ef283e3d89d58d5eef19ef63eb35a22c510
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-06-25 10:00:28 +0800

    Update conftest.py

--- tests/auditd/conftest.py
+++ tests/auditd/conftest.py
@@ -6,6 +6,11 @@ from tests.common.helpers.assertions import pytest_assert as py_assert
 logger = logging.getLogger(__name__)
 
 
+def pytest_addoption(parser):
+    parser.addoption("--container_test", action="store", default="",
+                     help="This flag indicate the test running by conntainer test")
+
+
 @pytest.fixture(scope="module")
 def check_auditd(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
