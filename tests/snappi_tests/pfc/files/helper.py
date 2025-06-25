a4db1767e880e11e781ace8b238c15c2fcf7aa64
v-jessgeorge <v-jessgeorge@microsoft.com>
2025-06-25 10:44:15 +0530

    Fixing pre-commit errors in folder snappi_tests with 41 folders (#19162)
    Replaced noqa with noqa: and pre-commit spacing issues has been addressed in folder: tests/snappi_tests
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/snappi_tests/pfc/files/helper.py
+++ tests/snappi_tests/pfc/files/helper.py
@@ -4,14 +4,14 @@ import time
 from tests.common.cisco_data import is_cisco_device
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
-    fanout_graph_facts  # noqa F401
+    fanout_graph_facts  # noqa: F401
 from tests.common.snappi_tests.common_helpers import pfc_class_enable_vector,\
     get_lossless_buffer_size, get_pg_dropped_packets,\
     disable_packet_aging, enable_packet_aging, sec_to_nanosec,\
     get_pfc_frame_count, packet_capture, config_capture_pkt,\
-    traffic_flow_mode, calc_pfc_pause_flow_rate, get_tx_frame_count      # noqa F401
-from tests.common.snappi_tests.port import select_ports, select_tx_port  # noqa F401
-from tests.common.snappi_tests.snappi_helpers import get_dut_port_id, wait_for_arp  # noqa F401
+    traffic_flow_mode, calc_pfc_pause_flow_rate, get_tx_frame_count      # noqa: F401
+from tests.common.snappi_tests.port import select_ports, select_tx_port  # noqa: F401
+from tests.common.snappi_tests.snappi_helpers import get_dut_port_id, wait_for_arp  # noqa: F401
 from tests.common.snappi_tests.traffic_generation import setup_base_traffic_config, generate_test_flows, \
     generate_background_flows, generate_pause_flows, run_traffic, verify_pause_flow, verify_basic_test_flow, \
     verify_background_flow, verify_pause_frame_count_dut, verify_egress_queue_frame_count, \
