1b44d99a55bf411e917d3bf72e1543d43400ed27
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-06-25 10:00:36 +0800

    Update conftest.py

--- tests/bmp/conftest.py
+++ tests/bmp/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption("--container_test", action="store", default="",
                      help="This flag indicate the test running by conntainer test")
