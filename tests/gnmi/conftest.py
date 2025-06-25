26399d56198944a8be87172b1de3c2eb2ad4abde
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-06-25 15:30:02 +0800

    Update conftest.py

--- tests/gnmi/conftest.py
+++ tests/gnmi/conftest.py
@@ -20,11 +20,6 @@ logger = logging.getLogger(__name__)
 SETUP_ENV_CP = "test_setup_checkpoint"
 
 
-def pytest_addoption(parser):
-    parser.addoption("--container_test", action="store", default="",
-                     help="This flag indicates that the test is being run by the container test.")
-
-
 @pytest.fixture(scope="function", autouse=True)
 def skip_non_x86_platform(duthosts, rand_one_dut_hostname):
     """
