8a07351a6e0408c5682c59d686aa9a345c1eb744
v-jessgeorge <v-jessgeorge@microsoft.com>
2025-06-25 10:43:06 +0530

    Fixing pre-commit errors in folder under  tests/t* tests/u* and tests/v*, tests/w* with 24 files (#19164)
    Fixing the pre-commit errors and replaced noqa with noqa: in the folders:
    
    tests/tacacs
    tests/telemetry
    tests/testbed_setup
    tests/upgrade_path
    tests/vlan
    tests/voq
    tests/vrf
    tests/vxlan
    tests/wan
    tests/wol
    
    Signed-off-by: Yael Tzur <ytzur@nvidia.com>

--- tests/tacacs/conftest.py
+++ tests/tacacs/conftest.py
@@ -1,6 +1,6 @@
 import logging
 import pytest
-from tests.common.fixtures.tacacs import tacacs_creds     # noqa F401
+from tests.common.fixtures.tacacs import tacacs_creds     # noqa: F401
 from tests.common.helpers.tacacs.tacacs_helper import tacacs_v6_context
 
 logger = logging.getLogger(__name__)
@@ -19,6 +19,6 @@ def skip_in_container_test(request):
 
 
 @pytest.fixture(scope="module")
-def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds): # noqa F811
+def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_creds):  # noqa: F811
     with tacacs_v6_context(ptfhost, duthosts[enum_rand_one_per_hwsku_hostname], tacacs_creds) as result:
         yield result
