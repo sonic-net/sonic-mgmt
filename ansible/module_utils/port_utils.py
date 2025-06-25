85832367437f610fbbbc75b61a290fa3de974461
Dakota Crozier <dakotac@arista.com>
2025-06-25 22:24:27 +0000

    Add missing space

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -138,7 +138,7 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                     port_alias_to_name_map["etp%d%s" % (i, j)] = "Ethernet%d" % ((i - 1) * 8 + x - 1)
         elif hwsku == "Arista-7060X6-64PE-B-P64":
             for i in range(1, 65):
-               port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1) * 8)
+                port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1) * 8)
         elif hwsku == "Arista-7060X6-64PE-256x200G":
             for i in range(1, 65):
                 for j in [1, 3, 5, 7]:
