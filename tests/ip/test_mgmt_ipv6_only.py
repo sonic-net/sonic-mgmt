612f33d10feda6813bfc3f942a6c8b75b7c2869b
ganglv <88995770+ganglyu@users.noreply.github.com>
2025-06-26 11:55:32 +0800

    Update user_auth and restart gnmi server (#19185)
    What is the motivation for this PR?
    We will enable user_auth by default, and then telemetry test will be blocked.
    
    How did you do it?
    Set user_auth as none and restart gnmi server before the telemetry test, and recover user_auth after the telemetry test.
    
    How did you verify/test it?
    Run ip/test_mgmt_ipv6_only.py
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/ip/test_mgmt_ipv6_only.py
+++ tests/ip/test_mgmt_ipv6_only.py
@@ -2,6 +2,7 @@ import logging
 
 import pytest
 import re
+import time
 
 from tests.common.helpers.constants import DEFAULT_ASIC_ID
 from tests.common.helpers.multi_thread_utils import SafeThreadPoolExecutor
@@ -230,10 +231,16 @@ def test_telemetry_output_ipv6_only(request, duthosts_ipv6_mgmt_only, localhost,
     log_eth0_interface_info(duthosts_ipv6_mgmt_only)
 
     def verify_telemetry_output_ipv6_only(dut):
+        # Wait 15 seconds after starting GNMI server
+        GNMI_SERVER_START_WAIT_TIME = 15
         with setup_streaming_telemetry_context(True, dut, localhost, ptfhost, gnxi_path):
             env = GNMIEnvironment(dut, GNMIEnvironment.TELEMETRY_MODE)
+            # Set up telemetry server
             dut.shell('sonic-db-cli CONFIG_DB hset "%s|gnmi" user_auth none' % (env.gnmi_config_table),
                       module_ignore_errors=False)
+            dut.shell('docker exec %s supervisorctl reload' % (env.gnmi_container),
+                      module_ignore_errors=False)
+            time.sleep(GNMI_SERVER_START_WAIT_TIME)
             dut_ip = get_mgmt_ipv6(dut)
             cmd = "~/gnmi_get -xpath_target COUNTERS_DB -xpath COUNTERS/Ethernet0 -target_addr \
                 [%s]:%s -logtostderr -insecure" % (dut_ip, env.gnmi_port)
