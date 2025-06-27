a1d5642bd0dbdb1addfefdebfb71560ad3ddae4d
ksravani-hcl <kanasani.sravani@hcltech.com>
2025-06-27 20:49:06 +0530

    [ondatra] Skip some validations for Alpine virtual switches.ondatra: get-config function cleanup. (#18128)
    Co-authored-by: kishanps <kishanps@google.com>
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- sdn_tests/pins_ondatra/tests/thinkit/arbitration_test.cc
+++ sdn_tests/pins_ondatra/tests/thinkit/arbitration_test.cc
@@ -1,9 +1,20 @@
 #include "tests/forwarding/arbitration_test.h"
 
 #include <memory>
+#include <string>
+#include <utility>
 
+#include "absl/flags/flag.h"
 #include "gtest/gtest.h"
 #include "infrastructure/thinkit/ondatra_params.h"
+#include "p4/v1/p4runtime.pb.h"
+#include "p4_pdpi/p4_runtime_session.h"
+#include "thinkit/mirror_testbed.h"
+#include "thinkit/mirror_testbed_fixture.h"
+
+ABSL_FLAG(
+    std::string, p4info_file, "",
+    "Path to the file containing the textproto of the P4Info to be pushed");
 
 namespace pins {
 namespace {
