4276be3241692e628ca7fcc1f0c3ea3a2693c73b
prabhataravind <108555774+prabhataravind@users.noreply.github.com>
2025-06-27 18:27:00 -0700

    Support t1-48-lag topo for acl tests (#19236)
    Description of PR
    In t1-48-lag topo, the downstream links are physical ports, the upstream links are portchannels.
    This change is needed to add the links to the acl_table_ports and prevent test failure.
    
    Summary:
    Fixes # (issue)
    
    Type of change
     Bug fix

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -397,7 +397,7 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, ptf
     # TODO: We should make this more robust (i.e. bind all active front-panel ports)
     acl_table_ports = defaultdict(list)
 
-    if topo in ["t0", "mx", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1", "t1-lag", "t1-28-lag"):
+    if topo in ["t0", "mx", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1", "t1-lag", "t1-28-lag", "t1-48-lag"):
         for namespace, port in list(downstream_ports.items()):
             acl_table_ports[namespace] += port
             # In multi-asic we need config both in host and namespace.
@@ -409,7 +409,7 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, ptf
             topo in ["t0", "m0_vlan", "m0_l3"]
             or tbinfo["topo"]["name"] in (
                 "t1-lag", "t1-64-lag", "t1-64-lag-clet",
-                "t1-56-lag", "t1-28-lag", "t1-32-lag"
+                "t1-56-lag", "t1-28-lag", "t1-32-lag", "t1-48-lag"
             )
             or 't1-isolated' in tbinfo["topo"]["name"]
         )
