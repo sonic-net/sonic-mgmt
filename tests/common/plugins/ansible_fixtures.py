85fed0a5296125147cfd6f25d6d57bdd661cba59
Yutong Zhang <yutongzhang@microsoft.com>
2025-07-01 14:57:07 +0800

    modify

--- tests/common/plugins/ansible_fixtures.py
+++ tests/common/plugins/ansible_fixtures.py
@@ -1,7 +1,9 @@
 """ This module provides few pytest-ansible fixtures overridden """
 import pytest
-from pytest_ansible.host_manager import get_host_manager
-
+try:
+    from pytest_ansible.host_manager import get_host_manager
+except ImportError:
+    from pytest_ansible.host_manager.utils import get_host_manager
 
 # Here we override ansible_adhoc fixture from pytest-ansible plugin to overcome
 # scope limitation issue; since we want to be able to use ansible_adhoc in module/class scope
