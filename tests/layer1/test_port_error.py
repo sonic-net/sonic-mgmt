7b1491e17e781930d2b93f984c83e6044ce8309e
gshemesh2 <gshemesh@nvidia.com>
2025-07-02 15:08:05 +0300

    Update test_port_error.py
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -62,7 +62,7 @@ class TestMACFault(object):
     def get_interface_status(dut, interface):
         return dut.show_and_parse("show interfaces status {}".format(interface))[0].get("oper", "unknown")
 
-    #TODO: Reboot the switch before test as a WA of #22205, remove this after the issue is fixed
+    # TODO: Reboot the switch before test as a WA of #22205, remove this after the issue is fixed
     @pytest.fixture(scope="class", autouse=True)
     def reboot_dut(self, duthosts, localhost, enum_rand_one_per_hwsku_frontend_hostname):
         from tests.common.reboot import reboot
