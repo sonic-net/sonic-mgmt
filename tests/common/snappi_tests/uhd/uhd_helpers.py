b5719848b724d519f2476697867390e3a512d125
Alberto Villarreal <alberto.villarreal@keysight.com>
2025-07-02 17:54:08 -0500

    Updates to uhd_helper to support a privatelink network for testing

--- tests/common/snappi_tests/uhd/uhd_helpers.py
+++ tests/common/snappi_tests/uhd/uhd_helpers.py
@@ -17,12 +17,14 @@ class NetworkConfigSettings:
         self.ENI_STEP = 1
         self.ENI_L2R_STEP = 1000
 
+        self.VTEP_IP = self.ipp("221.0.0.1")
         self.PAL = self.ipp("221.1.0.1")
         self.PAR = self.ipp("221.2.0.1")
 
         self.ACL_TABLE_MAC_STEP = '00:00:00:02:00:00'
         self.ACL_POLICY_MAC_STEP = '00:00:00:00:00:32'
 
+        self.ENI_VNI_NVGRE_START = 1000
         self.ACL_RULES_NSG = 1000
         self.ACL_TABLE_COUNT = 5
 
@@ -55,6 +57,19 @@ class NetworkConfigSettings:
         self.l47_tg_servermac = ''
         self.first_staticArpMac = ''
         self.dut_mac = ''
+
+        # PrivateLink
+        self.NVGRE_COUNT = 32
+        self.NVGRE_SUBMASK = 48
+        self.NVGRE_VSID = 100
+        self.NVGRE_IP = self.ipp('2603:100:3e8::0:0')
+        self.NVGRE_IP_STEP = self.ipp(ipaddress.ip_address('0:0:1::0:0'))
+        self.PAL_PL = self.ipp("221.2.0.0")
+        self.PAR_PL = self.ipp("221.1.0.0")
+
+    def dec2hex(self, dec):
+        hex_str = hex(dec)[2:]
+        return hex_str
 
     def set_mac_addresses(self, clientmac, servermac, dutmac):
 
@@ -241,6 +256,224 @@ def create_front_panel_ports(count, config, cards_dict):
     fp_list.append(dpu_port_1)
 
     return fp_list
+
+
+def create_arp_bypass_pl(fp_ports_list, ip_list, config, cards_dict, subnet_mask):
+
+    connections_list = []
+    num_cps_cards = cards_dict['num_cps_cards']
+    first_cps_card, first_tcpbg_card = set_first_stateful_cards(cards_dict)
+    vrange_count = config.ENI_COUNT//num_cps_cards
+    vlan_start = (config.ENI_L2R_STEP + 1) - vrange_count
+
+    """
+    if cards_dict['num_cps_cards'] > 0:
+        first_cps_card = 1
+        if cards_dict['num_tcpbg_cards'] > 0:
+            first_tcpbg_card = cards_dict['num_cps_cards'] + 1
+        else:
+            first_tcpbg_card = 0
+    """
+
+    vlan_start_tmp = vlan_start
+    for port in range(num_cps_cards):
+
+        vlan_start_tmp += vrange_count
+        arp_bypass_dict = {
+            'name': "ARP Bypass {}".format(port+1),
+            'functions': [{"choice": "connect_arp", "connect_arp": {}}],
+            'endpoints': []
+        }
+
+        # Test role will all use client instead of one client and one server
+        client_role = 'c'
+
+        arp_bypass_dict['endpoints'].append(
+            {'choice': 'front_panel', 'front_panel': {'port_name': 'l47_port_{}{}'.format(9, client_role),
+                                                      'vlan': {'choice': 'vlan_range', 'vlan_range':
+                                                          {'start': vlan_start_tmp, 'count': vrange_count,  # noqa: E128
+                                                           'step': 1}}}}
+        )
+        arp_bypass_dict['endpoints'].append(
+            {'choice': 'front_panel', 'front_panel': {'port_name': 'l47_port_{}{}'.format(port+1, client_role),
+                                                      'vlan': {'choice': 'vlan_range', 'vlan_range':
+                                                          {'start': vlan_start_tmp,  # noqa: E128
+                                                           'count': vrange_count, 'step': 1}}}}
+        )
+
+        connections_list.append(arp_bypass_dict)
+
+    return connections_list
+
+
+def create_connections_pl(fp_ports_list, ip_list, subnet_mask, config, cards_dict, arp_bypass_list):
+
+    connections_list = arp_bypass_list
+
+    num_cps_cards = cards_dict['num_cps_cards']
+
+    first_cps_card, first_tcpbg_card = set_first_stateful_cards(cards_dict)
+    """
+    if cards_dict['num_cps_cards'] > 0:
+        first_cps_card = 1
+        first_tcpbg_card = cards_dict['num_cps_cards'] + 1
+    """
+
+    # TODO loopback IP need updated for multiple DPUs for example: 'dst_ip': {'choice': 'ipv4', 'ipv4': '221.0.0.1'}
+    # ip6 = int(config.NVGRE_IP)  # noqa: F841
+    ip6_step = int(config.NVGRE_IP_STEP)
+    nvgre_count = config.NVGRE_COUNT
+    nvgre_eni = config.ENI_START
+    vtep_ip = config.VTEP_IP
+    vtep_ip_tmp = 0
+    underlay_ip = config.PAL_PL
+    underlay_ip_tmp = 0
+    vlan_endpoint_ip = config.PAR_PL
+    vlanEP_ip_tmp = 0
+    # ip_start = config.IP_L_START  # noqa: F841
+    ip_tmp = 0
+    client_vlan_start = config.ENI_L2R_STEP + 1
+    client_vlan_tmp = 0
+    # vxlan_vni_start = config.ENI_L2R_STEP * 2
+    vxlan_vni_start = config.ENI_L2R_STEP
+    vxlan_vni_tmp = 0
+    nvgre_ip = "2603:100:%s:0::0" % (config.dec2hex(config.ENI_VNI_NVGRE_START))
+    nvgre_ip_tmp = 0
+
+    for port in range(num_cps_cards):
+
+        server_dict_temp = {
+            'name': 'l47 Server {}'.format(port+1),
+            'functions': [],
+            'endpoints': []
+        }
+
+        client_dict_temp = {
+            'name': 'l47 Client {}'.format(port+1),
+            'functions': [],
+            'endpoints': []
+        }
+
+        # client_vlan = build_node_vlan(eni, config, nodetype="client")
+        # server_vlan = build_node_vlan(eni, config, nodetype="server")
+
+        # client_card, test_role = find_card_slot(first_cps_card, first_tcpbg_card, server_vlan)
+        # client_cps_port = find_port(num_cps_cards, first_cps_card, first_tcpbg_card, server_vlan, test_role)
+        # server_card, test_role = find_card_slot(first_cps_card, first_tcpbg_card, server_vlan)
+
+        # TODO needed when there are multiple DPU Ports
+        # dpu_port = 1 if server_vlan <= 128 else 2
+        dpu_port = 1
+
+        # Server side
+        """
+        if server_vlan == 256:
+            overlay_ip_addr = 0
+        else:
+            overlay_ip_addr = eni+1
+        """
+        # overlay_ip_addr = eni
+
+        # if server_vlan <= 128:
+        #    lb_ip = 1
+        # else:
+        #    lb_ip = 2
+
+        # client_role, server_role = find_testrole(test_role, server_vlan)
+
+        # TODO VNIs need to be +1000 for production
+        production = True  # turn ON for now
+        if production is True:
+            vni_index = 1000
+        else:
+            vni_index = 0  # noqa: F841
+
+        nvgre_ip_tmp = str(ipaddress.ip_address(nvgre_ip) + (ip6_step * (port * nvgre_count)))
+        vtep_ip_tmp = vtep_ip + (1 * port)
+        underlay_ip_tmp = underlay_ip + (config.NVGRE_COUNT * port)
+        server_conn_tmp = {
+            "choice": "connect_vlan_nvgre",
+            "connect_vlan_nvgre": {
+                "vlan_endpoint_settings": {
+                    "outgoing_nvgre_header": {
+                        "src_mac": {"choice": "mac", "mac": "{}".format(config.l47_tg_servermac)},
+                        "dst_mac": {"choice": "mac", "mac": "{}".format(config.dut_mac)},
+                        "src_ip": {"choice": "ipv4_range", "ipv4_range": {'start': "{}".format(underlay_ip_tmp),
+                                                                          'count': nvgre_count, 'step': '0.0.0.1'}},
+                        "dst_ip": {"choice": "ipv4", "ipv4": "{}".format(vtep_ip_tmp)},
+                    }
+                },
+                "nvgre_endpoint_settings": {
+                    "vsid": {"choice": "vsid", "vsid": config.NVGRE_VSID},
+                    "protocols": {"accept": ["tcp"]}, "routing_method": "ip_routing",  # noqa: E128
+                    "ip_routing": {"destination_ips": {"choice": "ipv6_range",
+                                                       "ipv6_range": {'start': "{}".format(
+                                                           ipaddress.ip_address(nvgre_ip_tmp)),
+                                                           'subnet_bits': config.NVGRE_SUBMASK, 'count': nvgre_count,
+                                                           'step': "{}".format(str(ipaddress.ip_address(ip6_step)))}}}}
+            }}
+        server_dict_temp['functions'].append(server_conn_tmp)
+        nvgre_eni_temp = nvgre_eni + (nvgre_count * port)
+        server_dict_temp['endpoints'].append(
+            {"choice": "front_panel", "front_panel": {
+                "port_name": "l47_port_{}s".format(port + 1),  # noqa: E122
+                "vlan": {"choice": "vlan_range", "vlan_range": {"start": nvgre_eni_temp, "count": 32, "step": 1}}},
+             "tags": ["vlan"]},
+        )
+        dpu_port = 2
+        server_dict_temp['endpoints'].append(
+            {"choice": "front_panel", "front_panel": {"port_name": "dpu_port_{}".format(dpu_port)},
+             "tags": ["nvgre"]}
+        )
+
+        # Client Side
+        # TODO vni_index
+        client_start = config.IP_L_START
+        ip_tmp = client_start + (int(ipaddress.ip_address('8.0.0.0')) * port)
+        vlanEP_ip_tmp = vlan_endpoint_ip + (nvgre_count * port)
+        vxlan_vni_tmp = vxlan_vni_start + (nvgre_count * port)
+
+        client_conn_tmp = {"choice": "connect_vlan_vxlan", "connect_vlan_vxlan": {
+            "vlan_endpoint_settings": {
+                "outgoing_vxlan_header": {
+                    "src_mac": {"choice": "mac", "mac": "{}".format(config.l47_tg_clientmac)},
+                    "dst_mac": {"choice": "mac", "mac": "{}".format(config.dut_mac)},
+                    "src_ip": {"choice": "ipv4_range", "ipv4_range": {'start': "{}".format(vlanEP_ip_tmp),
+                                                                      'count': nvgre_count, 'step': '0.0.0.1'}},
+                    "dst_ip": {"choice": "ipv4", "ipv4": "{}".format(vtep_ip_tmp)},
+                }
+            },
+            "vxlan_endpoint_settings": {"vni": {"choice": "vni_range", "vni_range": {'start': vxlan_vni_tmp,
+                                                                                     'count': nvgre_count, 'step': 1}},
+                                        "protocols": {"accept": ["tcp"]}, "routing_method": "ip_routing",
+                                        "ip_routing": {"destination_ips": {"choice": "ipv4_range",
+                                                                           "ipv4_range": {  # noqa: E128
+                                                                               "start": "{}".format(ip_tmp),
+                                                                               "count": config.NVGRE_COUNT,
+                                                                               "step": "0.64.0.0"
+                                                                           }}}}
+        }}
+
+        client_vlan_tmp = client_vlan_start + (nvgre_count * port)
+        client_dict_temp['functions'].append(client_conn_tmp)
+        client_dict_temp['endpoints'].append(
+            {"choice": "front_panel", "front_panel": {
+                "port_name": "l47_port_{}c".format(port+1),
+                "vlan": {"choice": "vlan_range", "vlan_range": {'start': client_vlan_tmp, 'count': nvgre_count,
+                                                                'step': 1}}}, "tags": ["vlan"]},
+        )
+
+        dpu_port = 1
+        client_dict_temp['endpoints'].append(
+            {"choice": "front_panel", "front_panel": {"port_name": "dpu_port_{}".format(dpu_port)},
+             "tags": ["vxlan"]}
+        )
+
+        # Add server and client settings to connections_list
+        connections_list.append(server_dict_temp)
+        connections_list.append(client_dict_temp)
+
+    return connections_list
 
 
 def create_arp_bypass(fp_ports_list, ip_list, config, cards_dict, subnet_mask):
