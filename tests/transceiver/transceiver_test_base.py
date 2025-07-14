f574787b4dd3be552dbe52eafb57c222b44c0889
mihirpat1 <112018033+mihirpat1@users.noreply.github.com>
2025-07-14 07:16:32 -0700

    CMIS CDB firmware management and infra for copying to DUT (#19232)
    * CMIS CDB firmware management and infra for copying to DUT
    
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>
    
    * Relaxes requirements for firmwary binary naming
    
    * Addressed PR comments and ensuring that 3 FW binaries are preset during FW upgrade testing
    
    * Added more info on firmware selection criteria
    
    ---------
    
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>

--- tests/transceiver/transceiver_test_base.py
+++ tests/transceiver/transceiver_test_base.py
@@ -12,7 +12,6 @@ import pytest
 import logging
 
 from tests.common.platform.interface_utils import get_dev_conn
-from tests.transceiver.utils.inventory import get_dev_transceiver_details
 
 logger = logging.getLogger(__name__)
 
@@ -22,17 +21,18 @@ logger = logging.getLogger(__name__)
 class TransceiverTestBase:
     @pytest.fixture(scope="class", autouse=True)
     def setup(self, request, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
-              enum_frontend_asic_index, conn_graph_facts, transceiver_inventory, get_lport_to_pport_mapping):
+              enum_frontend_asic_index, conn_graph_facts, get_dev_transceiver_details,
+              get_transceiver_common_attributes, get_lport_to_pport_mapping):
         """
         Fixture to set up the test environment.
         """
         duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
         _, dev_conn = get_dev_conn(duthost, conn_graph_facts, enum_frontend_asic_index)
-        dev_transceiver_details = get_dev_transceiver_details(duthost, transceiver_inventory)
-        logger.info("Dev transceiver details: {}".format(dev_transceiver_details))
+        logger.info("DUT transceiver details: {}".format(get_dev_transceiver_details))
         if request.cls is not None:
             request.cls.duthost = duthost
             request.cls.enum_frontend_asic_index = enum_frontend_asic_index
             request.cls.dev_conn = dev_conn
-            request.cls.dev_transceiver_details = dev_transceiver_details
+            request.cls.transceiver_common_attributes = get_transceiver_common_attributes
+            request.cls.dev_transceiver_details = get_dev_transceiver_details
             request.cls.lport_to_pport_mapping = get_lport_to_pport_mapping
