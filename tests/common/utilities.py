c5f026926f4cd6f5049dbb14e2565f99b97641b1
Yan Mo <andymo96@gmail.com>
2025-07-14 15:16:32 -0700

    Enable pretest with ipv6 testbeds (#19584)
    * Enable pretest with ipv6 testbeds
    
    Signed-off-by: Yael Tzur <ytzur@nvidia.com>

--- tests/common/utilities.py
+++ tests/common/utilities.py
@@ -545,6 +545,16 @@ def is_ipv4_address(ip_address):
         return False
 
 
+def is_ipv6_address(ip_address):
+    """Check if ip address is ipv6."""
+    ip_address = ip_address.encode().decode()
+    try:
+        ipaddress.IPv6Address(ip_address)
+        return True
+    except ipaddress.AddressValueError:
+        return False
+
+
 def get_mgmt_ipv6(duthost):
     config_facts = duthost.get_running_config_facts()
     mgmt_interfaces = config_facts.get("MGMT_INTERFACE", {})
