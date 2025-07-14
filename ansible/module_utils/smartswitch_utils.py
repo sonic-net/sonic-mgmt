7f08bdc48575e06566bd209f0d68246fd38f1a34
Cong Hou <97947969+congh-nvidia@users.noreply.github.com>
2025-07-14 18:30:11 +0800

    Update the smartswitch golden config for Mellanox platform (#18930)
    Signed-off-by: Yael Tzur <ytzur@nvidia.com>

--- ansible/module_utils/smartswitch_utils.py
+++ ansible/module_utils/smartswitch_utils.py
@@ -8,6 +8,7 @@ smartswitch_hwsku_config = {
     "Mellanox-SN4280-O28": {
         "dpu_num": 4,
         "port_key": "Ethernet{}",
+        "interface_key": "Ethernet{}|18.{}.202.0/31",
         "base": 224,
         "step": 8,
         "dpu_key": "dpu{}"
