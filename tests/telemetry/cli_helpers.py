1bbdd9cb4b9f079e56c4d47835996cc36afb5b4a
Zain Budhwani <zainbudhwani@microsoft.com>
2025-07-16 19:24:32 +0000

    Initial commit

---
+++ tests/telemetry/cli_helpers.py
@@ -0,0 +1,66 @@
+import json
+from tests.common.reboot import reboot
+
+def get_json_from_gnmi_output(stdout):
+    marker_pos = stdout.find("The GetResponse is below")
+    start_pos = stdout.find("{", marker_pos)
+
+    assert start_pos > 0, "JSON not found in GetResponse"
+
+    json_blob = stdout[start_pos:]
+    data = json.loads(json_blob)
+
+
+def reboot_device(duthost, localhost):
+    reboot(duthost, localhost)
+
+
+def transform_reboot_cause_output(reboot_cause_dict):
+    reboot_cause = reboot_cause_dict.get("cause", "Unknown")
+    reboot_user = reboot_cause_dict.get("user", "N/A")
+    reboot_time = reboot_cause_dict.get("time", "N/A")
+
+    reboot_cause_str = ""
+
+    reboot_cause = output.get("cause", "Unknown")
+    reboot_user = output.get("user", "N/A")
+    reboot_time = output.get("time", "N/A")
+
+    if reboot_user != "N/A":
+        reboot_cause_str = "User issued '{}' command".format(reboot_cause)
+    else:
+        reboot_cause_str = reboot_cause
+
+    if reboot_user != "N/A" or reboot_time != "N/A":
+        reboot_cause_str += " ["
+
+        if reboot_user != "N/A":
+            reboot_cause_str += "User: {}".format(reboot_user)
+            if reboot_time != "N/A":
+                reboot_cause_str += ", "
+
+        if reboot_time != "N/A":
+            reboot_cause_str += "Time: {}".format(reboot_time)
+
+        reboot_cause_str += "]"
+    return reboot_cause_str
+
+
+def check_reboot_cause(duthost, output):
+    cmd = "show reboot-cause"
+    result = duthost.shell(cmd)["stdout"]
+
+    reboot_cause_str = transform_reboot_cause_output(output)
+
+    failure_message = "{} no match parsed gnmi output {} for SHOW/reboot-cause path".format(result, reboot_cause_str)
+    assert result == reboot_cause_str, failure_message
+
+
+def check_reboot_cause_history(duthost, output):
+    cmd = "show reboot-cause history"
+    result = duthost.show_and_parse(cmd)
+
+    cmd_data = json.loads(result)
+
+    failure_message = "cmd result {} does not match gnmi output {} for SHOW/reboot-cause/history path".format(cmd_data, output)
+    assert cmd_data == output, failure_message
---
+++ tests/telemetry/test_telemetry_show_cli.py
@@ -0,0 +1,63 @@
+import logging
+import pytest
+import re
+from tests.common.helpers.assertions import pytest_assert
+from tests.common.utilities import wait_until
+from telemetry_utils import generate_client_cli, check_gnmi_cli_running
+from tests.common.utilities import InterruptableThread
+
+pytestmark = [
+    pytest.mark.topology('any')
+]
+
+logger = logging.getLogger(__name__)
+
+METHOD_GET = "get"
+METHOD_SUBSCRIBE = "subscribe"
+
+
+@pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
+def test_telemetry_show_non_get(duthosts, enum_rand_one_per_hwsku_hostname, ptfhost,
+                                setup_streaming_telemetry, gnxi_path):
+    """
+    Test non-get mode from APPL_DB and query a non existing table and key, ensure no errors
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+    logger.info('Start telemetry SHOW testing')
+    cmd = generate_client_cli(duthost=duthost, gnxi_path=gnxi_path, method=METHOD_SUBSCRIBE,
+                              xpath="reboot-cause", target="SHOW")
+    ptf_result = ptfhost.shell(cmd)
+    pytest_assert(ptf_result['rc'] != 0, "SHOW command for non GET operation should fail".format(cmd))
+
+
+@pytest.mark.parametrize('setup_streaming_telemetry', [False], indirect=True)
+def test_telemetry_show_get(duthosts, localhost, enum_rand_one_per_hwsku_hostname, ptfhost,
+                            setup_streaming_telemetry, gnxi_path, request):
+    """
+    Test poll mode from APPL_DB and query a non existing table and key, ensure no errors
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_hostname]
+    logger.info('Start telemetry SHOW testing')
+
+    with open(show_paths_file, 'r') as show_paths_file:
+        show_paths_data = json.load(show_paths_file)
+
+    for path, test_config in show_paths_data.items():
+        # Do any setup that is required before executing query
+        if test_config["setup"]:
+            setup_fixtures = [request.getfixturevalue(fixture) for fixture in test_config["setup_fixtures"]]
+            setup_args = test_config["setup_args"]
+            getattr(helper, test_config["setup"])(*setup_fixtures, *setup_args)
+
+        cmd = generate_client_cli(duthost=duthost, gnxi_path=gnxi_path, method=METHOD_GET,
+                                  xpath=path, target="SHOW")
+        ptf_result = ptfhost.shell(cmd)
+        pytest_assert(ptf_result['rc'] == 0, "ptf cmd command {} failed".format(cmd))
+        show_gnmi_out = ptf_result['stdout']
+        logger.info("GNMI Server output: {}".format(show_gnmi_out))
+
+        if test_config["verify"]:
+            output = str(show_gnmi_out)
+            verify_fixtures = [request.getfixturevalue(fixture) for fixture in test_config["verify_fixtures"]]
+            verify_args = test_config["verify_args"]
+            getattr(helper, test_config["verify"])(*verify_fixtures, *verify_args)
