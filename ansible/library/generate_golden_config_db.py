c1290b8a282fbd89c1c8600bbfeb883367912718
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-07-21 09:43:28 +0800

    Update generate_golden_config_db.py

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -404,9 +404,9 @@ class GenerateGoldenConfigDBModule(object):
         if "localhost" not in ori_config_db["DEVICE_METADATA"]:
             ori_config_db["DEVICE_METADATA"]["localhost"] = {}
 
-        # hack docker_routing_config_mode issue, multiasic test failed because this field is 'None', which break yang validation.
+        # multiasic test failed because this field is 'None', which break yang validation.
         if "docker_routing_config_mode" not in ori_config_db["DEVICE_METADATA"]["localhost"]:
-                ori_config_db["DEVICE_METADATA"]["localhost"]["docker_routing_config_mode"] = "unified"
+            ori_config_db["DEVICE_METADATA"]["localhost"]["docker_routing_config_mode"] = "unified"
 
         # Older version image may not support ZMQ feature flag
         rc, out, err = self.module.run_command("sudo cat /usr/local/yang-models/sonic-device_metadata.yang")
