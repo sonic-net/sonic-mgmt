e1ffd16cf0509e33c2fce5bf42727a70dbd9b991
chunangli <chunang_li@163.com>
2025-07-22 11:33:49 +0800

    add sonic-vpp elastictest nightly test based impacted area to get full test scripts

--- .azure-pipelines/impacted_area_testing/constant.py
+++ .azure-pipelines/impacted_area_testing/constant.py
@@ -6,7 +6,7 @@
 # - t0-2vlans
 # - t0-sonic
 # - t1- lag
-PR_TOPOLOGY_TYPE = ["t0_checker", "t0-2vlans_checker", "t0-sonic_checker", "t1_checker", "t1_lag_vpp_checker",
+PR_TOPOLOGY_TYPE = ["t0_checker", "t0-2vlans_checker", "t0-sonic_checker", "t1_checker",
                     "t1-multi-asic_checker", "dpu_checker", "dualtor_checker"]
 
 EXCLUDE_TEST_SCRIPTS = [
@@ -20,7 +20,6 @@ PR_CHECKER_TOPOLOGY_NAME = {
     "t0-2vlans": ["t0", "_kvmtest-t0-2vlans_"],
     "t0-sonic": ["t0-64-32", "_kvmtest-t0-sonic_"],
     "t1": ["t1-lag", "_kvmtest-t1-lag_"],
-    "t1-lag-vpp": ["t1-lag-vpp", "_kvmtest-t1-lag-vpp_"],
     "t1-multi-asic": ["t1-8-lag", "_kvmtest-multi-asic-t1-lag_"],
     "dpu": ["dpu", "_kvmtest-dpu_"],
     "dualtor": ["dualtor", "_kvmtest-dualtor-t0_"]
