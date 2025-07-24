7f787883727e50f2b7c617bc86ca64f9bf61e3f1
weguo-NV <weguo@nvidia.com>
2025-07-24 21:57:40 +0800

    Split packet trimming casee into symmetric and asymmetric
    Change-Id: Iac6609e39dac9e48da3aa7d8d8ca606ad18d8472

--- tests/generic_config_updater/conftest.py
+++ tests/generic_config_updater/conftest.py
@@ -171,3 +171,22 @@ def ignore_expected_loganalyzer_exceptions(duthosts, selected_dut_hostname, loga
             ".*ERR ctrmgrd.py: Join failed.*"
         ]
         loganalyzer[duthost.hostname].ignore_regex.extend(ignoreRegex)
+
+
+@pytest.fixture(scope="session")
+def skip_if_packet_trimming_not_supported(duthost):
+    """
+    Check if the current device supports packet trimming feature.
+    """
+    platform = duthost.facts["platform"]
+    logger.info(f"Checking packet trimming support for platform: {platform}")
+
+    # Check if the SWITCH_TRIMMING_CAPABLE capability is true
+    trimming_capable = duthost.command('redis-cli -n 6 HGET "SWITCH_CAPABILITY|switch" "SWITCH_TRIMMING_CAPABLE"')[
+        'stdout'].strip()
+    if trimming_capable.lower() != 'true':
+        pytest.skip("Packet trimming is not supported")
+
+    # For Nvidia SPC1/2/3 platforms, skip the test
+    elif any(platform_id in platform.lower() for platform_id in ["sn2", "sn3", "sn4"]):
+        pytest.skip(f"Packet trimming is not supported on {platform}")
