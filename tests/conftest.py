6ada73e3c294a186bc273d469bea371be7b32dd1
Riff <r12f.code@gmail.com>
2025-07-24 13:58:10 -0700

    Update tools for running test on NUT testbeds. (#19241)
    What is the motivation for this PR?
    With the support of network under test (NUT) testbed being added, we need to update the tools for running the tests.
    
    How did you do it?
    Since the only difference between these 2 testbeds is the testbed definition, and the ixia tests uses very minimum set fields, we can easily create some adapting code to make the existing tool supporting the new type of testbeds.
    
    How did you verify/test it?
    Running test locally.
    
    Signed-off-by: opcoder0 <110003254+opcoder0@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -792,6 +792,8 @@ def ptfhosts(enhance_inventory, ansible_adhoc, tbinfo, duthost, request):
     _hosts = []
     if 'ptp' in tbinfo['topo']['name']:
         return None
+    if tbinfo['topo']['name'].startswith("nut-"):
+        return None
     if "ptf_image_name" in tbinfo and "docker-keysight-api-server" in tbinfo["ptf_image_name"]:
         return None
     if "ptf" in tbinfo:
@@ -937,13 +939,18 @@ def nbrhosts(enhance_inventory, ansible_adhoc, tbinfo, creds, request):
 
 
 @pytest.fixture(scope="module")
-def fanouthosts(enhance_inventory, ansible_adhoc, conn_graph_facts, creds, duthosts):      # noqa: F811
+def fanouthosts(enhance_inventory, ansible_adhoc, tbinfo, conn_graph_facts, creds, duthosts):      # noqa: F811
     """
     Shortcut fixture for getting Fanout hosts
     """
 
     dev_conn = conn_graph_facts.get('device_conn', {})
     fanout_hosts = {}
+
+    if tbinfo['topo']['name'].startswith('nut-'):
+        # Nut topology has no fanout
+        return fanout_hosts
+
     # WA for virtual testbed which has no fanout
     for dut_host, value in list(dev_conn.items()):
         duthost = duthosts[dut_host]
