e7b8d3626616b34e6581ddaab48035d2b8d2bf2f
Jing Zhang <zhangjing@microsoft.com>
2025-07-31 11:47:33 -0700

    add new topo (#19974)

--- ansible/module_utils/smartswitch_utils.py
+++ ansible/module_utils/smartswitch_utils.py
@@ -5,6 +5,12 @@ smartswitch_hwsku_config = {
         "interface_key": "Ethernet-BP{}|18.{}.202.0/31",
         "dpu_key": "dpu{}"
     },
+    "Cisco-8102-28FH-DPU-O": {
+        "dpu_num": 8,
+        "port_key": "Ethernet-BP{}",
+        "interface_key": "Ethernet-BP{}|18.{}.202.0/31",
+        "dpu_key": "dpu{}"
+    },
     "Mellanox-SN4280-O28": {
         "dpu_num": 4,
         "port_key": "Ethernet{}",
