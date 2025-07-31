609b6bd1a8671c3adf5ef631c519b52f8865bd04
yawenni <yawenni@microsoft.com>
2025-07-31 16:47:22 +1000

    remove unused import of utilities in the pretest

--- tests/test_pretest.py
+++ tests/test_pretest.py
@@ -11,7 +11,6 @@ from tests.common.helpers.port_utils import get_common_supported_speeds
 
 from collections import defaultdict
 
-from tests.common import utilities
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.helpers.assertions import pytest_require
 from tests.common.helpers.dut_utils import verify_features_state
