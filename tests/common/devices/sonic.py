e7875c0162868a622c0128b883be9b2ab06457d4
Wei Guo <weguo@nvidia.com>
2025-08-05 10:47:47 +0300

    Add packet trimming counter testcase
    Change-Id: I553bae33fb05730f0e80685e26e565801485c70e

--- tests/common/devices/sonic.py
+++ tests/common/devices/sonic.py
@@ -2787,6 +2787,7 @@ Totals               6450                 6449
         counter_type_cli_map = {
             'QUEUE_STAT': 'queue',
             'PORT_STAT': 'port',
+            'SWITCH_STAT': 'switch',
             'PORT_BUFFER_DROP': 'port-buffer-drop',
             'RIF_STAT': 'rif',
             'QUEUE_WATERMARK_STAT': 'watermark',
