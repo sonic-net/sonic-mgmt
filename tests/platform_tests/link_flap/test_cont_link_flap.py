efac3ac023482bb7bac2e8235514047bf3d845bf
zitingguo <zitingguo@microsoft.com>
2025-08-12 02:14:30 +0000

    Use get_vtysh_cmd_for_namespace instead
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>

--- tests/platform_tests/link_flap/test_cont_link_flap.py
+++ tests/platform_tests/link_flap/test_cont_link_flap.py
@@ -38,29 +38,14 @@ class TestContLinkFlap(object):
         frr_daemon_memory_per_asics = {}
 
         for asic in duthost.asics:
-            frr_daemon_memory_output = asic.run_vtysh(f'-c "show memory {daemon}"')["stdout"]
+            frr_daemon_memory_output = duthost.shell(duthost.get_vtysh_cmd_for_namespace(
+                f'vtysh -c "show memory {daemon}"', asic.namespace))["stdout"]
+            logging.info(f"{daemon} memory status: \n%s", frr_daemon_memory_output)
 
-            logging.info(
-                f"{daemon}{('-' + asic.namespace) if asic.namespace else ''} memory status: \n%s",
-                frr_daemon_memory_output
-            )
-            found = False
-            for line in frr_daemon_memory_output.splitlines():
-                if "Used ordinary blocks" in line:
-                    try:
-                        frr_daemon_memory = line.split()[-2]
-                        frr_daemon_memory_per_asics[asic.asic_index] = frr_daemon_memory
-                        found = True
-                    except IndexError:
-                        raise ValueError(
-                            f"Malformed memory line for {daemon} on ASIC {asic.asic_index}: '{line}'"
-                        )
-                    break
-
-            if not found:
-                raise ValueError(
-                    f"Missing 'Used ordinary blocks' line in memory output for {daemon} on ASIC {asic.asic_index}"
-                )
+            output = duthost.shell(duthost.get_vtysh_cmd_for_namespace(
+                f'vtysh -c "show memory {daemon}" | grep "Used ordinary blocks"', asic.namespace))["stdout"]
+            frr_daemon_memory = output.split()[-2]
+            frr_daemon_memory_per_asics[asic.index] = frr_daemon_memory
 
         return frr_daemon_memory_per_asics
 
