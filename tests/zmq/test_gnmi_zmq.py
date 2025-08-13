642f2a05b2f34fd5e2aaa9102742e336011acb16
xuliping <xuliping@microsoft.com>
2025-08-13 02:46:52 +0000

    fix flake8

--- tests/zmq/test_gnmi_zmq.py
+++ tests/zmq/test_gnmi_zmq.py
@@ -58,7 +58,7 @@ def enable_zmq(duthost):
     save_reload_config(duthost)
 
     pytest_assert(wait_until(360, 10, 120, duthost.critical_services_fully_started),
-                    "Not all critical services are fully started")
+                  "Not all critical services are fully started")
 
     config_facts = duthost.config_facts(host=duthost.hostname, source="running")['ansible_facts']
     bgp_neighbors = config_facts.get('BGP_NEIGHBOR', {})
