75046173be487cdb5948f7d867e61b2c77f069bc
Alberto Villarreal <alberto.villarreal@keysight.com>
2025-08-15 07:24:16 -0500

    update to match testcase active/standby dpu

--- tests/snappi_tests/dash/ha/ha_helper.py
+++ tests/snappi_tests/dash/ha/ha_helper.py
@@ -876,12 +876,12 @@ def run_linkloss(duthost, tbinfo, api, dpu_if_ips, initial_cps_value, ha_test_ca
         dpu_midplane_ip = dpu_if_ips['dpu1']['if_midplane_ip']
     elif ha_test_case == "linkloss6":
         npu_ip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
-        dpu_ip = dpu_if_ips['dpu1']['if_ip']
-        dpu_midplane_ip = dpu_if_ips['dpu1']['if_midplane_ip']
+        dpu_ip = dpu_if_ips['dpu2']['if_ip']
+        dpu_midplane_ip = dpu_if_ips['dpu2']['if_midplane_ip']
     elif ha_test_case == "linkloss7":
         npu_ip = duthost.host.options['inventory_manager'].get_host(duthost.hostname).vars['ansible_host']
-        dpu_ip = dpu_if_ips['dpu1']['if_ip']
-        dpu_midplane_ip = dpu_if_ips['dpu1']['if_midplane_ip']
+        dpu_ip = dpu_if_ips['dpu2']['if_ip']
+        dpu_midplane_ip = dpu_if_ips['dpu2']['if_midplane_ip']
     else:
         return None
 
