d3934c2249fd6f961aa22dd747a3d4c6fb2a9dd3
Xu Chen <xuchen3@microsoft.com>
2025-08-19 11:09:42 +0800

    remove dangerous fixture disable_ipv6 for ptf container

--- tests/common/fixtures/ptfhost_utils.py
+++ tests/common/fixtures/ptfhost_utils.py
@@ -691,18 +691,3 @@ def iptables_drop_ipv6_tx(ptfhost):
     yield
     ptfhost.shell("ip6tables -P OUTPUT ACCEPT")
 
-
-@pytest.fixture(scope='function')
-def disable_ipv6(ptfhost):
-    default_ipv6_status = ptfhost.shell("sysctl -n net.ipv6.conf.all.disable_ipv6")["stdout"]
-    changed = False
-    # Disable IPv6 on all interfaces in PTF container
-    if default_ipv6_status != "1":
-        ptfhost.shell("echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6")
-        changed = True
-
-    yield
-
-    # Restore the original IPv6 setting on all interfaces in the PTF container
-    if changed:
-        ptfhost.shell("echo {} > /proc/sys/net/ipv6/conf/all/disable_ipv6".format(default_ipv6_status))
