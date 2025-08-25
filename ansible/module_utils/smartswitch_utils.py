c4ba86041ced2a4bf202cc4bbf3c7a648ab8e8e4
nnelluri-cisco <nnelluri@cisco.com>
2025-08-25 13:46:58 -0700

    Generate new golden config db for cisco smartswitch platforms. (#20314)
    * added cisco hwsku support for golden config
    
    * fixing pre-commit failures
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- ansible/module_utils/smartswitch_utils.py
+++ ansible/module_utils/smartswitch_utils.py
@@ -1,30 +1,35 @@
-smartswitch_hwsku_config = {
-    "Cisco-8102-28FH-DPU-O-T1": {
-        "dpu_num": 8,
-        "port_key": "Ethernet-BP{}",
-        "interface_key": "Ethernet-BP{}|18.{}.202.0/31",
-        "dpu_key": "dpu{}"
-    },
-    "Cisco-8102-28FH-DPU-O": {
-        "dpu_num": 8,
-        "port_key": "Ethernet-BP{}",
-        "interface_key": "Ethernet-BP{}|18.{}.202.0/31",
-        "dpu_key": "dpu{}"
-    },
-    "Mellanox-SN4280-O28": {
-        "dpu_num": 4,
-        "port_key": "Ethernet{}",
-        "interface_key": "Ethernet{}|18.{}.202.0/31",
-        "base": 224,
-        "step": 8,
-        "dpu_key": "dpu{}"
-    },
-    "Mellanox-SN4280-O8C40": {
-        "dpu_num": 4,
-        "port_key": "Ethernet{}",
-        "interface_key": "Ethernet{}|18.{}.202.0/31",
-        "base": 224,
-        "step": 8,
-        "dpu_key": "dpu{}"
-    }
+common_cisco_hwsku_config = {
+    "dpu_num": 8,
+    "port_key": "Ethernet{}",
+    "interface_key": "Ethernet{}|18.{}.202.0/31",
+    "base": 224,
+    "step": 8,
+    "dpu_key": "dpu{}"
 }
+
+common_mellanox_hwsku_config = {
+    "dpu_num": 4,
+    "port_key": "Ethernet{}",
+    "interface_key": "Ethernet{}|18.{}.202.0/31",
+    "base": 224,
+    "step": 8,
+    "dpu_key": "dpu{}"
+}
+
+smartswitch_hwsku_config = {
+    # Cisco SKUs
+    hwsku: common_cisco_hwsku_config.copy() for hwsku in [
+        "Cisco-8102-28FH-DPU-O",
+        "Cisco-8102-28FH-DPU-C28",
+        "Cisco-8102-28FH-DPU-O8C20",
+        "Cisco-8102-28FH-DPU-O12C16",
+        "Cisco-8102-28FH-DPU-O8C40",
+        "Cisco-8102-28FH-DPU-O8V40"
+    ]
+}
+
+# Mellanox SKUs
+smartswitch_hwsku_config.update({
+    "Mellanox-SN4280-O28": common_mellanox_hwsku_config.copy(),
+    "Mellanox-SN4280-O8C40": common_mellanox_hwsku_config.copy()
+})
