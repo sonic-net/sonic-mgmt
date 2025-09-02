69105a848f2d80932a7d45674f1f638bc687f479
nnelluri-cisco <nnelluri@cisco.com>
2025-09-01 21:56:48 -0700

    HA ovs rules fix

--- ansible/roles/vm_set/library/vm_topology.py
+++ ansible/roles/vm_set/library/vm_topology.py
@@ -1224,6 +1224,11 @@ class VMTopology(object):
                         (br_name, dut_iface_id, vm_iface_id, injected_iface_id))
             bind_helper("ovs-ofctl add-flow %s table=0,priority=10,ipv6,in_port=%s,nw_proto=89,action=output:%s,%s" %
                         (br_name, dut_iface_id, vm_iface_id, injected_iface_id))
+            #added ovs rules for HA 
+            bind_helper("ovs-ofctl add-flow %s table=0,priority=999,udp,in_port=%s,action=output:%s" %
+                        (br_name, dut_iface_id, injected_iface_id))
+            bind_helper("ovs-ofctl add-flow %s table=0,priority=999,tcp,in_port=%s,action=output:%s" %
+                        (br_name, dut_iface_id, injected_iface_id))
 
         # Add flow for BFD Control packets (UDP port 3784)
             bind_helper("ovs-ofctl add-flow %s 'table=0,priority=10,udp,in_port=%s,\
