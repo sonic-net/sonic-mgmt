04629db0979a4e2936698892a3eb483d9249ba55
Zain Budhwani <zainbudhwani@microsoft.com>
2025-09-03 18:19:13 +0000

    Skip m1

--- tests/telemetry/events/swss_events.py
+++ tests/telemetry/events/swss_events.py
@@ -30,7 +30,7 @@ WAIT_TIME = 3
 
 
 def test_event(duthost, gnxi_path, ptfhost, ptfadapter, data_dir, validate_yang):
-    if duthost.topo_type.lower() in ["m0", "mx", "m1", "m2", "m3"]:
+    if duthost.topo_type.lower() in ["m0", "mx", "m1"]:
         logger.info("Skipping swss events test on MGFX topologies")
         return
     logger.info("Beginning to test swss events")
