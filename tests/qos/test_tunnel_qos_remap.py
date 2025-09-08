cd72c607f8d66f77713666a632486163957cf7e0
rbpittman <rapittma@cisco.com>
2025-09-08 02:49:55 -0400

    Disable Cisco VOQ WD for dualtor QOS, improve PG test debuggability (#20502)
    * Disable Cisco VOQ watchdog for dualtor tunnel tests. Improve debuggability and speed of the tunnel PG map test.
    
    * Fix VOQ watchdog test.
    
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/qos/test_tunnel_qos_remap.py
+++ tests/qos/test_tunnel_qos_remap.py
@@ -29,7 +29,8 @@ from .tunnel_qos_remap_base import build_testing_packet, check_queue_counter,\
     dut_config, qos_config, tunnel_qos_maps, run_ptf_test, toggle_mux_to_host,\
     setup_module, update_docker_services, swap_syncd, counter_poll_config                               # noqa: F401
 from .tunnel_qos_remap_base import leaf_fanout_peer_info, start_pfc_storm, \
-    stop_pfc_storm, get_queue_counter, get_queue_watermark, disable_packet_aging                        # noqa: F401
+    stop_pfc_storm, get_queue_counter, get_queue_watermark, disable_packet_aging, \
+    disable_voq_watchdog_dualtor  # noqa: F401
 from ptf import testutils
 from ptf.testutils import simple_tcp_packet
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts, fanout_graph_facts     # noqa: F401
