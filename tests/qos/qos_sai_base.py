b9f40c7dba02af086beb8ecdbcc10ae7c086b940
rick-arista <148895369+rick-arista@users.noreply.github.com>
2025-09-07 21:11:41 -0700

    Add qos params for Arista-7060X6-16PE-384C-B-O128S2 (#20546)
    Description of PR
    This adds QoS parameters and the necessary test case changes to use them. This generalizes the handling of pkts_num_egr_mem to be applied to all Arista-7060X6* SKUs and updates its value for TH5-512.
    
    Changes were manually tested and verified for 100% pass rate of the qos/test_qos_sai.py tests.
    
    signed-off-by: jianquanye@microsoft.com
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -3048,7 +3048,8 @@ def set_queue_pir(interface, queue, rate):
     @pytest.fixture(scope='class', autouse=True)
     def is_supported_per_dir(self, get_src_dst_asic_and_duts, tbinfo):  # noqa F811
         supported_per_dir_platform = ["Mellanox-SN5640-C448O16", "Mellanox-SN5640-C512S2",
-                                      "Mellanox-SN5600-C224O8", "Mellanox-SN5600-C256S1"]
+                                      "Mellanox-SN5600-C224O8", "Mellanox-SN5600-C256S1",
+                                      "Arista-7060X6-16PE-384C-B-O128S2"]
         is_supported_per_dir = \
             get_src_dst_asic_and_duts["src_asic"].sonichost.facts["hwsku"] in supported_per_dir_platform
         logging.info(f"is_supported_per_dir: {is_supported_per_dir}")
