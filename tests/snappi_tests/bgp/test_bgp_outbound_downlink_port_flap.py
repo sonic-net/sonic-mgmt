8a7f1afaf116d549f66a0825eac96b6fdbda4525
Austin (Thang Pham) <austinpham@microsoft.com>
2025-09-09 10:21:01 +1000

    feat: add record-property for bgp convergence (#20291)
    Description of PR
    Summary: Add record_property for bgp convergence
    Fixes # (issue) 34417012
    
    Type of change
     Bug fix
     Testbed and Framework(new/improvement)
     New Test case
     Skipped for non-supported platforms
     Test case improvement
    
    Approach
    What is the motivation for this PR?
    Add record_property so that we can have it logged to kusto
    
    Signed-off-by: Austin Pham <austinpham@microsoft.com>
    Signed-off-by: yenlu <yenlu@cisco.com>

--- tests/snappi_tests/bgp/test_bgp_outbound_downlink_port_flap.py
+++ tests/snappi_tests/bgp/test_bgp_outbound_downlink_port_flap.py
@@ -33,7 +33,8 @@ def test_bgp_outbound_downlink_port_flap(snappi_api,                            
                                          conn_graph_facts,                             # noqa: F811
                                          fanout_graph_facts_multidut,                           # noqa: F811
                                          duthosts,
-                                         creds):
+                                         creds,
+                                         record_property):
     """
     Gets the packet loss duration on flapping the interconnected port between T1 and downlink in T1 side
 
@@ -83,4 +84,5 @@ def test_bgp_outbound_downlink_port_flap(snappi_api,                            
     snappi_extra_params.multi_dut_params.hw_platform = hw_platform
     run_bgp_outbound_link_flap_test(api=snappi_api,
                                     creds=creds,
-                                    snappi_extra_params=snappi_extra_params)
+                                    snappi_extra_params=snappi_extra_params,
+                                    record_property=record_property)
