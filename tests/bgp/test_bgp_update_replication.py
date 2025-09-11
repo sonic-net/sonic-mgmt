93eef4b20dc5070a3f75b658dd83b290014dc4a7
Matt Soulsby <83053424+matthew-soulsby@users.noreply.github.com>
2025-09-12 09:50:34 +1000

    Loosen intervals, remove threshold calculations

--- tests/bgp/test_bgp_update_replication.py
+++ tests/bgp/test_bgp_update_replication.py
@@ -60,9 +60,6 @@ def measure_stats(dut):
     bgp_sum_template = "./bgp/templates/bgp_summary_extended.textfsm"
     # Time in seconds commands should execute within
     responsive_threshold = 2
-    # Percentage thresholds
-    cpu_threshold = 100.0
-    mem_threshold = 100.0
 
     time_before_cmd = time.process_time()
 
@@ -109,18 +106,6 @@ def measure_stats(dut):
     })
 
     logger.debug(stats)
-
-    # Check that CPU usage isn't excessive
-    pytest_assert(
-        cpu_threshold >= cpu_usage,
-        f"CPU utilisation has reached {cpu_usage}, which is above threshold of {cpu_threshold}"
-    )
-
-    # Check that memory usage isn't excessive
-    pytest_assert(
-        mem_threshold >= mem_usage,
-        f"Memory utilisation has reached {mem_usage}, which is above threshold of {mem_threshold}"
-    )
 
     return stats
 
@@ -229,7 +214,7 @@ def test_bgp_update_replication(
     prev_num_rib = int(results[0]["num_rib"])
 
     # Inject and withdraw routes with a specified interval in between iterations
-    for interval in [3.0, 1.0, 0.5]:
+    for interval in [4, 3, 2]:
         # Repeat 20 times
         for _ in range(20):
             # Inject 10000 routes
