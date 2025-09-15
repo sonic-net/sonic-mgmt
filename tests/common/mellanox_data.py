5678930f82bd817abd9beb40457f92efec2b11c3
gshemesh2 <gshemesh@nvidia.com>
2025-09-15 14:18:33 +0300

    Update mellanox_data.py
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/common/mellanox_data.py
+++ tests/common/mellanox_data.py
@@ -1,12 +1,15 @@
 import functools
 
 
-SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "Mellanox-SN2700-D48C8","Mellanox-SN2700-C28D8", "Mellanox-SN2700-A1-C28D8"]
-SPC2_HWSKUS = ["ACS-MSN3700", "ACS-MSN3700C", "ACS-MSN3420"]
-SPC3_HWSKUS = ["ACS-MSN4700", "Mellanox-SN4700-O28", "ACS-SN4280", "Mellanox-SN4280-O28"]
+SPC1_HWSKUS = ["ACS-MSN2700", "Mellanox-SN2700", "Mellanox-SN2700-D48C8", "ACS-MSN2740", "ACS-MSN2100", "ACS-MSN2410",
+               "ACS-MSN2010", "ACS-SN2201", "Mellanox-SN2700-C28D8", "Mellanox-SN2700-A1-C28D8"]
+SPC2_HWSKUS = ["ACS-MSN3700", "ACS-MSN3700C", "ACS-MSN3800", "Mellanox-SN3800-D112C8", "ACS-MSN3420"]
+SPC3_HWSKUS = ["ACS-MSN4700", "Mellanox-SN4700-O28", "ACS-MSN4600C", "ACS-MSN4410", "ACS-MSN4600",
+               "Mellanox-SN4600C-D112C8", "Mellanox-SN4600C-C64", "ACS-SN4280", "Mellanox-SN4280-O28",
+               "Mellanox-SN4280-O8C40", "Mellanox-SN4280-C48", "Mellanox-SN4280-O8V40", "Mellanox-SN4280-O8C80"]
 SPC4_HWSKUS = ["ACS-SN5600", "Mellanox-SN5600-V256", "Mellanox-SN5600-C256S1", "Mellanox-SN5600-C224O8",
-               "Mellanox-SN5610N-C256S2", "Mellanox-SN5610N-C224O8"]
-SPC5_HWSKUS = ["ACS-SN5640", "Mellanox-SN5640-C512S2", "Mellanox-SN5640-C448O16"]
+               'Mellanox-SN5610N-C256S2', 'Mellanox-SN5610N-C224O8']
+SPC5_HWSKUS = ["Mellanox-SN5640-C512S2", "Mellanox-SN5640-C448O16"]
 SWITCH_HWSKUS = SPC1_HWSKUS + SPC2_HWSKUS + SPC3_HWSKUS + SPC4_HWSKUS + SPC5_HWSKUS
 
 LOSSY_ONLY_HWSKUS = ['Mellanox-SN5600-C256S1', 'Mellanox-SN5600-C224O8', 'Mellanox-SN5640-C512S2',
@@ -17,6 +20,7 @@ PSU_CAPABILITIES = [
     ['psu{}_curr', 'psu{}_curr_in', 'psu{}_power', 'psu{}_power_in', 'psu{}_volt', 'psu{}_volt_in', 'psu{}_volt_out2']
 ]
 MULTI_HARDWARE_TYPE_PLATFORMS = ['x86_64-mlnx_msn4700-r0',
+                                 'x86_64-mlnx_msn4410-r0',
                                  'x86_64-mlnx_msn4600c-r0',
                                  'x86_64-mlnx_msn3700-r0',
                                  'x86_64-mlnx_msn3700c-r0']
@@ -145,19 +149,19 @@ SWITCH_MODELS = {
             }
         }
     },
-    "x86_64-nvidia_sn5610n-r0": {
-        "chip_type": "spectrum4",
+    "x86_64-nvidia_sn2201-r0": {
+        "chip_type": "spectrum1",
         "reboot": {
             "cold_reboot": True,
             "fast_reboot": True,
             "warm_reboot": True
         },
         "fans": {
-            "number": 5,
+            "number": 4,
             "hot_swappable": True
         },
         "psus": {
-            "number": 4,
+            "number": 2,
             "hot_swappable": True,
             "capabilities": PSU_CAPABILITIES[1]
         },
@@ -165,91 +169,29 @@ SWITCH_MODELS = {
             "number": 1
         },
         "cpu_cores": {
-            "number": 0
+            "number": 2
         },
         "ports": {
-            "number": 64
+            "number": 52
         },
         "thermals": {
             "cpu_core": {
                 "start": 0,
-                "number": 6
-            },
-            "module": {
-                "start": 1,
-                "number": 65
-            },
-            "psu": {
-                "start": 1,
                 "number": 2
             },
-            "cpu_pack": {
-                "number": 1
-            },
-            "cpu_ambient": {
-                "number": 1
-            },
-            "asic_ambient": {
-                "number": 1
-            },
-            "port_ambient": {
-                "number": 1
-            },
-            "fan_ambient": {
-                "number": 1
-            },
-            "pch": {
-                "number": 1
-            },
-            "sodimm": {
-                "start": 1,
-                "number": 2
-            }
-        }
-    },
-    "x86_64-nvidia_sn5640_simx-r0": {
-        "chip_type": "spectrum5",
-        "reboot": {
-            "cold_reboot": True,
-            "fast_reboot": True,
-            "warm_reboot": True
-        },
-        "fans": {
-            "number": 10,
-            "hot_swappable": True
-        },
-        "psus": {
-            "number": 4,
-            "hot_swappable": True,
-            "capabilities": PSU_CAPABILITIES[1]
-        },
-        "cpu_pack": {
-            "number": 0
-        },
-        "cpu_cores": {
-            "number": 0
-        },
-        "ports": {
-            "number": 512
-        },
-        "thermals": {
-            "cpu_core": {
-                "start": 0,
-                "number": 6
-            },
             "module": {
-                "start": 1,
-                "number": 512
-            },
-            "psu": {
-                "start": 1,
+                "start": 49,
                 "number": 4
             },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
             "cpu_pack": {
-                "number": 0
+                "number": 1
             },
             "cpu_ambient": {
-                "number": 0
+                "number": 1
             },
             "asic_ambient": {
                 "number": 1
@@ -260,12 +202,9 @@ SWITCH_MODELS = {
             "fan_ambient": {
                 "number": 1
             },
-            "pch": {
-                "number": 1
-            },
             "sodimm": {
                 "start": 1,
-                "number": 2
+                "number": 1
             }
         }
     },
@@ -425,6 +364,217 @@ SWITCH_MODELS = {
                 "number": 1
             },
             "fan_ambient": {
+                "number": 1
+            }
+        }
+    },
+    "x86_64-mlnx_msn2410-r0": {
+        "chip_type": "spectrum1",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True,
+            "capabilities": PSU_CAPABILITIES[0]
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 2
+        },
+        "ports": {
+            "number": 56
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 2
+            },
+            "module": {
+                "start": 1,
+                "number": 56
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "sodimm": {
+                "start": 1,
+                "number": 1
+            }
+        }
+    },
+    "x86_64-mlnx_msn2010-r0": {
+        "chip_type": "spectrum1",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": False
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": False
+        },
+        "cpu_pack": {
+            "number": 0
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 22
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 22
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "sodimm": {
+                "start": 1,
+                "number": 2
+            }
+        }
+    },
+    "x86_64-mlnx_msn2100-r0": {
+        "chip_type": "spectrum1",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 4,
+            "hot_swappable": False
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": False
+        },
+        "cpu_pack": {
+            "number": 0
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 16
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 16
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "sodimm": {
+                "start": 1,
+                "number": 1
+            }
+        }
+    },
+    "x86_64-mlnx_msn3800-r0": {
+        "chip_type": "spectrum2",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": False,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 3,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True,
+            "capabilities": PSU_CAPABILITIES[1]
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 64
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 64
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "gearbox": {
+                "start": 1,
+                "number": 32
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "comex_ambient": {
                 "number": 1
             }
         }
@@ -759,6 +909,61 @@ SWITCH_MODELS = {
                 }
             }
     },
+    "x86_64-mlnx_msn4600c-r0": {
+        "chip_type": "spectrum3",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": False,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 1,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True,
+            "capabilities": PSU_CAPABILITIES[1]
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 64
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 64
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "comex_ambient": {
+                "number": 1
+            }
+        }
+    },
     "x86_64-mlnx_msn3420-r0": {
         "chip_type": "spectrum2",
         "reboot": {
@@ -792,6 +997,116 @@ SWITCH_MODELS = {
             "module": {
                 "start": 1,
                 "number": 60
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "comex_ambient": {
+                "number": 1
+            }
+        }
+    },
+    "x86_64-mlnx_msn4410-r0": {
+        "chip_type": "spectrum3",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": False,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 6,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True,
+            "capabilities": PSU_CAPABILITIES[1]
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 32
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 32
+            },
+            "psu": {
+                "start": 1,
+                "number": 2
+            },
+            "cpu_pack": {
+                "number": 1
+            },
+            "asic_ambient": {
+                "number": 1
+            },
+            "port_ambient": {
+                "number": 1
+            },
+            "fan_ambient": {
+                "number": 1
+            },
+            "comex_ambient": {
+                "number": 1
+            }
+        }
+    },
+    "x86_64-mlnx_msn4600-r0": {
+        "chip_type": "spectrum3",
+        "reboot": {
+            "cold_reboot": True,
+            "fast_reboot": True,
+            "warm_reboot": True
+        },
+        "fans": {
+            "number": 3,
+            "hot_swappable": True
+        },
+        "psus": {
+            "number": 2,
+            "hot_swappable": True,
+            "capabilities": PSU_CAPABILITIES[1]
+        },
+        "cpu_pack": {
+            "number": 1
+        },
+        "cpu_cores": {
+            "number": 4
+        },
+        "ports": {
+            "number": 64
+        },
+        "thermals": {
+            "cpu_core": {
+                "start": 0,
+                "number": 4
+            },
+            "module": {
+                "start": 1,
+                "number": 64
             },
             "psu": {
                 "start": 1,
@@ -860,7 +1175,7 @@ SWITCH_MODELS = {
                 "number": 1
             }
         }
-    },
+    }
 }
 
 
@@ -908,7 +1223,7 @@ def get_hardware_version(duthost, platform):
     if platform in MULTI_HARDWARE_TYPE_PLATFORMS:
         config1 = duthost.command('cat /run/hw-management/system/config1', module_ignore_errors=True)
         config3 = duthost.command('cat /run/hw-management/system/config3', module_ignore_errors=True)
-        if platform in ('x86_64-mlnx_msn4700-r0'):
+        if platform in ('x86_64-mlnx_msn4700-r0', 'x86_64-mlnx_msn4410-r0'):
             return 'a1' if config1['rc'] == 0 and config1['stdout'] == '1' else ''
         elif platform == 'x86_64-mlnx_msn4600c-r0':
             if config1['rc'] == 0:
