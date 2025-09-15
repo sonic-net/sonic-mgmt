e25a46ca3013ac4f0d17a56ca50279a8821a0d96
gshemesh2 <gshemesh@nvidia.com>
2025-09-15 12:13:12 +0300

    Update conftest.py
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -296,7 +296,6 @@ def pytest_addoption(parser):
     parser.addoption("--container_test", action="store", default="",
                      help="This flag indicates that the test is being run by the container test.")
 
-
     #################################
     #   Port error test options     #
     #################################
