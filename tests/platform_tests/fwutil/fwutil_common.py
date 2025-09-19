fcbf66f7134bc8f00c403d0b7dea76c0e89fb40e
echuawu <chuanw@nvidia.com>
2025-09-19 17:57:34 +0300

    Add BMC platform api test
    Including cases:
    test_get_name()
    test_get_presence()
    test_get_model()
    test_get_serial()
    test_get_revision()
    test_get_status()
    test_is_replaceable()
    test_get_eeprom()
    test_get_version()
    test_reset_root_password()
    test_bmc_dump()
    test_bmc_firmware_update()
    
    Add test coverage for BMC firmware install and update command
    
    Add completeness_level to control test scope
        1. If completeness_level set to 'basic', the script would randomly
           choose install or update command to do CLI test
        2. If completeness_level set to other values, the script would
           execute test_bmc_firmware_update case 2 times to cover the two
           types of firmware command
    
    Change-Id: I8dd3e334439aa3239641738ef3365c1f0b1a0914
    Signed-off-by: echuawu <chuanw@nvidia.com>

--- tests/platform_tests/fwutil/fwutil_common.py
+++ tests/platform_tests/fwutil/fwutil_common.py
@@ -4,12 +4,12 @@ import os
 import json
 import logging
 import allure
-import re
 
 from copy import deepcopy
 
 from tests.common.utilities import wait_until
 from tests.common.reboot import SONIC_SSH_REGEX
+from tests.common.helpers.firmware_helper import show_firmware
 
 logger = logging.getLogger(__name__)
 
@@ -126,33 +126,6 @@ def complete_install(duthost, localhost, boot_type, res, pdu_ctrl, component, au
             localhost.wait_for(host=hn, port=22, state='started', delay=10, timeout=150)
             wait_until(300, 30, 0, duthost.critical_services_fully_started)
             time.sleep(60)
-
-
-def show_firmware(duthost):
-    out = duthost.command("fwutil show status")
-    num_spaces = 2
-    curr_chassis = ""
-    output_data = {"chassis": {}}
-    status_output = out['stdout']
-    separators = re.split(r'\s{2,}', status_output.splitlines()[1])  # get separators
-    output_lines = status_output.splitlines()[2:]
-
-    for line in output_lines:
-        data = []
-        start = 0
-
-        for sep in separators:
-            curr_len = len(sep)
-            data.append(line[start:start+curr_len].strip())
-            start += curr_len + num_spaces
-
-        if data[0].strip() != "":
-            curr_chassis = data[0].strip()
-            output_data["chassis"][curr_chassis] = {"component": {}}
-
-        output_data["chassis"][curr_chassis]["component"][data[2]] = data[3]
-
-    return output_data
 
 
 def get_install_paths(request, duthost, defined_fw, versions, chassis, target_component):
