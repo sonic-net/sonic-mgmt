60c437b0ae53eecf60db74805acf85dbe62721be
echuawu <chuanw@nvidia.com>
2025-09-19 17:57:34 +0300

    Add BMC platform api test
    Including cases:
    test_get_name()
    test_get_presence()
    test_get_model()
    test_get_serial()
    test_get_revision()
    test_get_status()
    test_is_replaceable()
    test_get_eeprom()
    test_get_version()
    test_reset_root_password()
    test_bmc_dump()
    test_bmc_firmware_update()
    
    Change-Id: I3491eb538cc6a504200f12614fce24cafec33355

---
+++ tests/common/helpers/firmware_helper.py
@@ -0,0 +1,27 @@
+import re
+
+def show_firmware(duthost):
+    out = duthost.command("fwutil show status")
+    num_spaces = 2
+    curr_chassis = ""
+    output_data = {"chassis": {}}
+    status_output = out['stdout']
+    separators = re.split(r'\s{2,}', status_output.splitlines()[1])  # get separators
+    output_lines = status_output.splitlines()[2:]
+
+    for line in output_lines:
+        data = []
+        start = 0
+
+        for sep in separators:
+            curr_len = len(sep)
+            data.append(line[start:start+curr_len].strip())
+            start += curr_len + num_spaces
+
+        if data[0].strip() != "":
+            curr_chassis = data[0].strip()
+            output_data["chassis"][curr_chassis] = {"component": {}}
+
+        output_data["chassis"][curr_chassis]["component"][data[2]] = data[3]
+
+    return output_data
