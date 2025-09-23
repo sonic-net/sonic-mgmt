590a5d3cc0541de9d0e3dc2c87636cd38fb21f56
Xin Wang <xiwang5@microsoft.com>
2025-09-23 09:50:53 +0800

    Improve setup-container.sh for Ubuntu 24.04 based docker-sonic-mgmt (#20761)
    To support docker-sonic-mgmt based on Ubuntu 24.04, the setup-container.sh needs some minor improvements:
    
    1. The Ubuntu 24.04 docker image as account "ubuntu" by default. Its uid could conflict with the user's uid. This change added code to best effort deleting this default "ubuntu" account.
    2. In old docker-sonic-mgmt, python3 is installed in virtual environment. In 2024, the docker-sonic-mgmt was updated to install python3 in global environment. It's no longer necessary to hack the virtual environment. This change adjusted the sequence of conditions for hacking venv, so that the code can fail faster to skip this step. In the future, this piece of code should can be deleted.
    
    Signed-off-by: Xin Wang <xiwang5@microsoft.com>

--- setup-container.sh
+++ setup-container.sh
@@ -191,6 +191,11 @@ FROM {{ IMAGE_ID }}
 
 USER root
 
+# Remove possible default ubuntu user of Ubuntu 24.04
+RUN if getent passwd ubuntu; \
+then userdel -r ubuntu; \
+fi
+
 # Group configuration
 RUN if getent group {{ GROUP_NAME }}; \
 then groupmod -o -g {{ GROUP_ID }} {{ GROUP_NAME }}; \
@@ -252,9 +257,11 @@ WORKDIR ${HOME}
 # 2. The user is not AzDevOps. By default python3 virtual env is installed for AzDevOps user.
 #    No need to install it again when current user is AzDevOps.
 # 3. The python3 virtual env is not installed for AzDevOps. Then, it is not required for other users either.
-RUN if ! pip3 list | grep -c pytest >/dev/null && \
-[ '{{ USER_NAME }}' != 'AzDevOps' ] && \
-[ -d /var/AzDevOps/env-python3 ]; then \
+# As of 2025, python3 is installed globally in the docker-sonic-mgmt image. So, this step is not really required.
+# Adjust the conditions to fail faster to skip this step.
+RUN if [ -d /var/AzDevOps/env-python3 ] \
+ && [ '{{ USER_NAME }}' != 'AzDevOps' ] \
+ && ! pip3 list | grep -c pytest >/dev/null; then \
 /bin/bash -c 'python3 -m venv ${HOME}/env-python3'; \
 /bin/bash -c '${HOME}/env-python3/bin/pip install pip --upgrade'; \
 /bin/bash -c '${HOME}/env-python3/bin/pip install wheel'; \
