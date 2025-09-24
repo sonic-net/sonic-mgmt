770472e5cde81daed2c6ba337e11ffec61f31a9e
Wenda Chu <32250288+w1nda@users.noreply.github.com>
2025-09-24 15:55:01 +0800

    Update announce_routes.py

--- ansible/library/announce_routes.py
+++ ansible/library/announce_routes.py
@@ -1625,12 +1625,11 @@ def filterout_subnet(aggregate_routes, candidate_routes):
 
 
 def convert_routes_to_str(topo_routes):
-    for vm in topo_routes.keys():
-        for ip_version in topo_routes[vm].keys():
+    for vm in topo_routes:
+        for ip_version in topo_routes[vm]:
             topo_routes[vm][ip_version] = \
                 [(str(r[0]) if r[0] else None, str(r[1]) if r[1] else None, str(r[2]) if r[2] else None)
                  for r in topo_routes[vm][ip_version]]
-    return topo_routes
 
 
 def main():
