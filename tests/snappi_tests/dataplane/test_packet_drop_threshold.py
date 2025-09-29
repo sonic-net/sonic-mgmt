a96b3252085dcf7cf2c88cf3ef264a199deb04f3
vikumarks <vikumar7ks@gmail.com>
2025-09-29 10:14:43 -0700

    Use snappi api insted of restpy
    Signed-off-by: vikumarks <vikumar7ks@gmail.com>

--- tests/snappi_tests/dataplane/test_packet_drop_threshold.py
+++ tests/snappi_tests/dataplane/test_packet_drop_threshold.py
@@ -1,20 +1,15 @@
-from tests.snappi_tests.dataplane.imports import *
+from tests.snappi_tests.dataplane.imports import *  # noqa F403
+from snappi_tests.dataplane.files.helper import *  # noqa F403
+from tests.common.telemetry.metrics import GaugeMetric
+from tests.common.telemetry.constants import (
+    METRIC_LABEL_DEVICE_ID,
+    METRIC_LABEL_TG_FRAME_BYTES,
+    METRIC_LABEL_TG_RFC2889_ENABLED,
+    UNIT_PERCENT,
+)
 
 logger = logging.getLogger(__name__)
-
 pytestmark = [pytest.mark.topology("tgen")]
-
-import pytest
-import snappi
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import json
-
-# Test Parameters
-traffic_run_duration = 10  # Configurable traffic run duration
-frame_sizes = [66, 86, 128, 256, 512, 1024, 2048, 4096, 8192]  # Frame Sizes
-frame_ordering = ["RFC2889", "none"]
 
 test_results = pd.DataFrame(
     columns=[
@@ -23,133 +18,123 @@ test_results = pd.DataFrame(
         "Line Rate (%)",
         "Tx Frames",
         "Rx Frames",
-        "Frames Delta",
         "Loss %",
         "Status",
         "Duration (s)",
     ]
 )
 
-
-@pytest.fixture(scope="session", autouse=True)
-def session_teardown(request):
-    yield
-    request.session.ixnet_session.remove()
-    logger.info("\n=== Custom session teardown in Test Script ===")
-
-
-def boundary_check(ixnet, hls, line_rate):
-    """Tests if the given line rate results in frame loss."""
-    logger.info(f"Updating percentLineRate to: {line_rate}")
-    frame_size = hls.FrameSize.FixedSize
-    frame_rate_hljson = [
-        {
-            "xpath": f"/traffic/trafficItem[1]/highLevelStream[{index+1}]",
-            "frameRate": {"xpath": f"/traffic/trafficItem[1]/highLevelStream[{index+1}]/frameRate","rate":line_rate},
-        }
-        for index in range(len(hls))
-    ]
-
-    ixnet.ResourceManager.ImportConfig(json.dumps(frame_rate_hljson), False)        
-
-    start_traffic(ixnet)
-    wait_with_message("Running traffic for", traffic_run_duration)
-    stop_traffic(ixnet)
-    wait_with_message("Waiting for stats to settle", 5)
-
-    # Fetch traffic stats inline using Pandas
-    view = StatViewAssistant(ixnet, "Traffic Item Statistics")
-    df = pd.DataFrame(view.Rows.RawData, columns=view.ColumnHeaders)
-
-    # Select only necessary columns and convert them to numeric
-    df = df[["Traffic Item", "Tx Frames", "Rx Frames", "Frames Delta", "Loss %"]]
-    df[["Loss %"]] = pd.to_numeric(df["Loss %"], errors="coerce")
-
-    # Check if loss occurred (Loss % > 0)
-    df["Status"] = (df["Loss %"] == 0).map({True: "PASS", False: "FAIL"})
-    # Print the DataFrame for results
-    logger.info(
-        f'Dumping Frame Size/Rate: {frame_size}/{line_rate} Traffic Item Stats:\n{tabulate(df, headers="keys", tablefmt="psql",showindex=False)}'
-    )  # .transpose()
-
-    loss = df["Loss %"].max()  # Get max loss in case of multiple traffic items
-    ixnet.ClearStats()
-
-    # Create a DataFrame for the current test result directly
-    global test_results
-    result_df = pd.DataFrame(
-        [
-            {
-                "Frame Ordering": ixnet.Traffic.FrameOrderingMode,
-                "Frame Size": hls.FrameSize.FixedSize,
-                "Line Rate (%)": line_rate,
-                "Tx Frames": df["Tx Frames"].sum(),
-                "Rx Frames": df["Rx Frames"].sum(),
-                "Frames Delta": df["Frames Delta"].sum(),
-                "Loss %": df["Loss %"].max(),
-                "Status": df["Status"].iloc[0],
-                "Duration (s)": traffic_run_duration,
-            }
-        ]
-    )
-
-    # Append the new result to the global test_results DataFrame
-    test_results = pd.concat([test_results, result_df], ignore_index=True)
-
-    return loss == 0  # True if no loss, False if loss occurs
+route_ranges = {"IPv6": [["777:777:777::1", 64, 5000]], "IPv4": [["100.1.1.1", 24, 5000]]}
 
 
 pytestmark = [pytest.mark.topology("tgen")]
 
 
+@pytest.mark.parametrize("ip_version", ["IPv6"])
+@pytest.mark.parametrize("frame_bytes", [256])  # [86, 128, 256, 512, 1024, 1518]
+@pytest.mark.parametrize("rfc2889_enabled", [True, False])
 def test_packet_drop_threshold(
     request,
-    snappi_api,  # noqa F811
-    setup_snappi_port_configs,
-    conn_graph_facts,  # noqa F811
-    fanout_graph_facts,  # noqa F811
     duthosts,
+    snappi_api,  # noqa F811
     get_snappi_ports,
-    setup_and_teardown,
+    fanout_graph_facts_multidut,  # noqa F811
+    set_primary_chassis,
+    create_snappi_config,
+    rfc2889_enabled,
+    frame_bytes,
+    ip_version,
+    db_reporter,
 ):
     """
     Test to measure latency introduced by the switch under fully loaded conditions.
     """
-    ixnet, session_assistant = setup_and_teardown
-    hls = ixnet.Traffic.TrafficItem.find().HighLevelStream.find()
-    frame_size_hljson_template = [
+    no_loss_max_rate = GaugeMetric("no_loss_max_rate", "No Loss Max Rate", UNIT_PERCENT, db_reporter)
+    snappi_extra_params = SnappiTestParams()
+    snappi_ports = get_duthost_bgp_details(duthosts, get_snappi_ports, ip_version)
+    port_distrbution = (slice(0, len(snappi_ports) // 2), slice(len(snappi_ports) // 2, None))
+    tx_ports, rx_ports = snappi_ports[port_distrbution[0]], snappi_ports[port_distrbution[1]]
+    snappi_extra_params.protocol_config = {
+        "Tx": {
+            "route_ranges": route_ranges[ip_version],
+            "network_group": False,
+            "protocol_type": "bgp",
+            "ports": tx_ports,
+            "subnet_type": ip_version,
+            "is_rdma": False,
+        },
+        "Rx": {
+            "route_ranges": route_ranges[ip_version],
+            "network_group": False,
+            "protocol_type": "bgp",
+            "ports": rx_ports,
+            "subnet_type": ip_version,
+            "is_rdma": False,
+        },
+    }
+
+    snappi_config, snappi_obj_handles = create_snappi_config(snappi_extra_params)
+    frame_rate = 100  # Start with 100% line rate
+    snappi_extra_params.traffic_flow_config = [
         {
-            "xpath": f"/traffic/trafficItem[1]/highLevelStream[{index+1}]",
-            "frameSize": {"xpath": f"/traffic/trafficItem[1]/highLevelStream[{index+1}]/frameSize"},
+            "line_rate": frame_rate,
+            "frame_size": frame_bytes,
+            "is_rdma": False,
+            "flow_name": "packet_drop_threshold",
+            "tx_names": snappi_obj_handles["Tx"]["ip"] + snappi_obj_handles["Rx"]["ip"],
+            "rx_names": snappi_obj_handles["Rx"]["ip"] + snappi_obj_handles["Tx"]["ip"],
+            "mesh_type": "mesh",
         }
-        for index in range(len(hls))
     ]
-    for ordering in frame_ordering:
-        ixnet.Traffic.FrameOrderingMode = ordering
-        ixnet.Traffic.TrafficItem.find().Generate()
-        ixnet.Traffic.Apply()
-        logger.info(f"FrameOrderingMode: {ordering}")
-        for frame_size in frame_sizes:
-            logger.info(f"Finding Packet Drop Threshold for FrameOrderingMode: {ordering} Frame Size: {frame_size}")
-            hljson = list(map(lambda hl: {**hl, "frameSize": {**hl["frameSize"], "fixedSize": frame_size}}, frame_size_hljson_template))
-            ixnet.ResourceManager.ImportConfig(json.dumps(hljson), False)
-            """ Uses binary search to determine the max line rate without loss. """
-            if boundary_check(ixnet, hls, 100):
-                continue
+    snappi_config = create_traffic_items(snappi_config, snappi_extra_params)
+    snappi_api.set_config(snappi_config)
+    start_stop(snappi_api, operation="start", op_type="protocols")
+    # ***************************************************************************
+    # Using RestPy Code
+    ixnet_traffic_params = {"BiDirectional": True, "SrcDestMesh": "fullMesh"}
+    ixnet = snappi_api._ixnetwork
+    ixnet.Traffic.TrafficItem.find().update(**ixnet_traffic_params)
+    ixnet.Traffic.FrameOrderingMode = "RFC2889" if rfc2889_enabled else "none"
+    start_stop(snappi_api, operation="start", op_type="traffic")
+    start_stop(snappi_api, operation="stop", op_type="traffic")
+    # ***************************************************************************
 
-            low, high, best_rate = 1, 100, 1
-            while high - low > 0.5:  # Stop when precision is within 0.5%
-                mid = round((low + high) / 2, 2)
-                logger.info("=" * 50)
-                logger.info(f"Testing {mid}% Line Rate   Range: {low}% - {high}%")
-                logger.info("=" * 50)
-                if boundary_check(ixnet, hls, mid):
-                    best_rate, low = mid, mid
-                else:
-                    high = mid  # Decrease rate if loss
+    req = snappi_api.config_update().flows
+    req.property_names = [req.RATE]
+    update_flow = snappi_config.flows[0]
+    req.flows.append(update_flow)
+    best_rate = 100
+    """ Uses binary search to determine the max line rate without loss. """
+    if not boundary_check(snappi_api, snappi_config, frame_bytes, best_rate, rfc2889_enabled):
+        low, high, best_rate = 1, 100, 1
+        while high - low > 0.1:  # Stop when precision is within 0.5%
+            mid = round((low + high) / 2, 2)
+            logger.info("=" * 50)
+            logger.info(f"Testing {mid}% Line Rate   Range: {low}% - {high}%")
+            logger.info("=" * 50)
+            update_flow.rate.percentage = mid
+            snappi_api.update_flows(req)
 
-            logger.info(f"Final Maximum Line Rate Without Loss for FrameOrderingMode: {ordering} Frame Size:{frame_size} is : {best_rate}%")
+            if boundary_check(snappi_api, snappi_config, frame_bytes, mid, rfc2889_enabled):
+                best_rate, low = mid, mid
+            else:
+                high = mid  # Decrease rate if loss
 
+        logger.info(
+            f"Final Maximum Line Rate Without Loss for FrameOrderingMode: {rfc2889_enabled}, "
+            f"Frame Size: {frame_bytes} is: {best_rate}%"
+        )
+    """
+    max_line_rate = test_results[
+        (test_results['Frame Ordering'] == rfc2889_enabled) &
+        (test_results['Frame Size'] == frame_bytes) &
+        (test_results['Loss %'] == 0.0)
+    ]['Line Rate (%)'].max()
+    """
+    no_loss_max_rate.record(
+        best_rate, {"tg.ip_version": ip_version, "tg.frame_bytes": frame_bytes, "tg.rfc2889.enabled": rfc2889_enabled}
+    )
+    db_reporter.report()
     for ordering_mode, group in test_results.groupby("Frame Ordering"):
         summary = f"""
         Summary for Frame Ordering Mode: {ordering_mode}
@@ -159,131 +144,50 @@ def test_packet_drop_threshold(
         """
         logger.info(summary.strip())
 
-def wait_with_message(message, duration):
-    """Displays a countdown while waiting."""
-    for remaining in range(duration, 0, -1):
-        logger.info(f"{message} {remaining} seconds remaining.")
-        # sys.stdout.flush()
-        time.sleep(1)
-    logger.info("")  # Ensure line break after countdown.
 
+def boundary_check(snappi_api, snappi_config, frame_bytes, line_rate, rfc2889_enabled):
+    """Tests if the given line rate results in frame loss."""
+    logger.info(f"Updating percentLineRate to: {line_rate}")
 
-def start_traffic(ixnet):
-    """Starts the traffic and ensures frames are being transmitted."""
-    logger.info("\tStarting traffic...")
+    # ***************************************************************************
+    # Using RestPy Code
+    ixnet = snappi_api._ixnetwork
     ixnet.Traffic.StartStatelessTrafficBlocking()
-    ti = StatViewAssistant(ixnet, "Traffic Item Statistics")
+    wait_with_message("Running traffic for", 10)
+    start_stop(snappi_api, operation="stop", op_type="traffic")
+    df = get_stats(snappi_api, "Traffic Item Statistics", columns=None, return_type="df")
 
-    if not ti.CheckCondition("Tx Frames", StatViewAssistant.GREATER_THAN, 0):
-        raise Exception("Traffic did not start properly.")
-    logger.info("\tTraffic started successfully.")
+    # Select only necessary columns and convert them to numeric
+    df = df[["name", "frames_tx", "frames_rx", "loss"]]
+    df[["loss"]] = pd.to_numeric(df["loss"], errors="coerce")
 
-
-def stop_traffic(ixnet, timeout=30, interval=3):
-    """Stops traffic and ensures it is fully stopped within the timeout period."""
-    logger.info("\tStopping traffic...")
-    ixnet.Traffic.StopStatelessTrafficBlocking()
-
-    for _ in range(0, timeout, interval):
-        if not ixnet.Traffic.IsTrafficRunning:
-            logger.info("\tTraffic successfully stopped.")
-            return
-        time.sleep(interval)
-
-    raise TimeoutError("\tTraffic did not stop within the timeout period.")
-
-
-@pytest.fixture(scope="function", autouse=True)
-def setup_and_teardown(request, snappi_api, setup_snappi_port_configs):
-    """
-    Fixture to initialize and cleanup resources for the test.
-    """
-    # Setup: Initialize session_assistant and ixnet
-    session_assistant = SessionAssistant(
-        IpAddress=snappi_api._address,
-        RestPort=snappi_api._port,
-        UserName=snappi_api._username,
-        Password=snappi_api._password,
-        SessionName="LatencyMeasurement",
-        ClearConfig=True,
+    # Check if loss occurred (Loss % > 0)
+    df["Status"] = (df["loss"] == 0).map({True: "PASS", False: "FAIL"})
+    # Print the DataFrame for results
+    logger.info(
+        f"Dumping Frame Size/Rate: {frame_bytes}/{line_rate} RFC2889 Enabled: {rfc2889_enabled} Traffic Item Stats:\n"
+        f"{tabulate(df, headers='keys', tablefmt='psql', showindex=False)}"
     )
-    ixnet = session_assistant.Ixnetwork
-    request.session.ixnet_session = session_assistant.Session
-    # ixnet.Locations.add(Hostname="10.36.84.31", ChainTopology="star")
-    # ixnet.Locations.add(Hostname="10.36.84.34", PrimaryDevice="10.36.84.31")
+    loss = df["loss"].max()  # Get max loss in case of multiple traffic items
 
-    logger.info(f"Starting IxNetwork Session Id {session_assistant.Session}")
-    logger.info("Session and ixnetwork initialized.")
-    port_config_list = setup_snappi_port_configs
-    logger.info("Connect the virtual ports to test ports")
-    port_list = [
-        {"xpath": f"/vport[{i+1}]", "location": port['location'], "name": f"Port-{i:02d}"}
-        for i, port in enumerate(port_config_list)
-    ]
-    # Import configuration and assign ports
-    ixnet.ResourceManager.ImportConfig(json.dumps(port_list), False)
-
-    ixnet.AssignPorts(True)
-
-    # Assign IP addresses and gateways
-    def assign_addresses(ipv4_device, ips, gateways):
-        ipv4_device.Address.ValueList(ips)
-        ipv4_device.GatewayIp.ValueList(gateways)
-
-    vports, half_ports = ixnet.Vport.find(), len(port_config_list) // 2
-    logger.info("Creating IxNetwork Topology")
-    ipv4_w = (
-        ixnet.Topology.add(Vports=vports[:half_ports])
-        .DeviceGroup.add(Name="Device West", Multiplier="1")
-        .Ethernet.add()
-        .Ipv4.add(Name=f"Ipv4 West")
-    )
-    ipv4_e = (
-        ixnet.Topology.add(Vports=vports[half_ports:])
-        .DeviceGroup.add(Name="Device East", Multiplier="1")
-        .Ethernet.add()
-        .Ipv4.add(Name=f"Ipv4 East")
+    # Create a DataFrame for the current test result directly
+    global test_results
+    result_df = pd.DataFrame(
+        [
+            {
+                "Frame Ordering": rfc2889_enabled,
+                "Frame Size": frame_bytes,
+                "Line Rate (%)": line_rate,
+                "Tx Frames": df["frames_tx"].sum(),
+                "Rx Frames": df["frames_rx"].sum(),
+                "Loss %": df["loss"].max(),
+                "Status": df["Status"].iloc[0],
+                "Duration (s)": 30,
+            }
+        ]
     )
 
-    ip, gw = map(list, zip(*[[pc['ipAddress'], pc['ipGateway']] for pc in port_config_list]))
+    # Append the new result to the global test_results DataFrame
+    test_results = pd.concat([test_results, result_df], ignore_index=True)
 
-    assign_addresses(ipv4_w, ip[:half_ports], gw[:half_ports])
-    assign_addresses(ipv4_e, ip[half_ports:], gw[half_ports:])
-
-    logger.info("Create Traffic Item")
-    ixnet.Traffic.FrameOrderingMode = "RFC2889"
-    trafficItem = ixnet.Traffic.TrafficItem.add(
-        Name="TestTraffic",
-        BiDirectional=True,
-        SrcDestMesh="fullMesh",
-        TrafficType="ipv4",
-    )
-    logger.info("Add endpoint flow group")
-    trafficItem.EndpointSet.add(
-        Sources=ixnet.Topology.find(), Destinations=ixnet.Topology.find()
-    )
-    logger.info("Configuring config elements")
-    configElement = trafficItem.ConfigElement.find()[0]
-    configElement.FrameRate.update(Rate=100, Type="percentLineRate")
-    configElement.TransmissionControl.update(Duration=20, Type="continous")
-    configElement.FrameRateDistribution.PortDistribution = "applyRateToAll"
-    configElement.FrameSize.FixedSize = 512
-    tracking = trafficItem.Tracking.find()[0]
-    tracking.TrackBy = ["sourceDestPortPair0"]
-
-    logger.info("Start All Protocols")
-    ixnet.StartAllProtocols(Arg1="sync")
-    try:
-        logger.info("Verify protocol sessions")
-        protocolsSummary = StatViewAssistant(ixnet, "Protocols Summary")
-        protocolsSummary.CheckCondition(
-            "Sessions Not Started", StatViewAssistant.EQUAL, 0, 180
-        )
-        protocolsSummary.CheckCondition(
-            "Sessions Down", StatViewAssistant.EQUAL, 0, 180
-        )
-    except Exception as e:
-        logger.info("ERROR:Protocols session are down.")
-        raise Exception(str(e))
-
-    yield ixnet, session_assistant
+    return loss == 0  # True if no loss, False if loss occurs
