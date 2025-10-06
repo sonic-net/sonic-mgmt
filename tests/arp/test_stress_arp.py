4ee35ba6c075d547a79f840e42e523469320aa39
gshemesh2 <gshemesh@nvidia.com>
2025-10-06 11:50:09 +0300

    Update test_stress_arp.py

--- tests/arp/test_stress_arp.py
+++ tests/arp/test_stress_arp.py
@@ -22,7 +22,8 @@ TEST_INCOMPLETE_NEIGHBOR_CNT = 10
 logger = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.topology('t0')
+    pytest.mark.topology('t0'),
+    pytest.mark.dualtor_active_standby_toggle_to_enum_tor
 ]
 
 LOOP_TIMES_LEVEL_MAP = {
@@ -90,14 +91,15 @@ def genrate_ipv4_ip():
     ptf_intf_ipv4_hosts = ipv4_addr.hosts()
     return list(ptf_intf_ipv4_hosts)
 
-
-def test_ipv4_arp(duthost, garp_enabled, ip_and_intf_info, intfs_for_test,
+def test_ipv4_arp(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
+                  garp_enabled, ip_and_intf_info, intfs_for_test,
                   ptfadapter, get_function_completeness_level):
     """
     Send gratuitous ARP (GARP) packet sfrom the PTF to the DUT
 
     The DUT should learn the (previously unseen) ARP info from the packet
     """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
     normalized_level = get_function_completeness_level
     if normalized_level is None:
         normalized_level = "debug"
@@ -107,6 +109,11 @@ def test_ipv4_arp(duthost, garp_enabled, ip_and_intf_info, intfs_for_test,
     pytest_assert(ipv4_available > 0 and fdb_available > 0, "Entries have been filled")
 
     arp_available = min(min(ipv4_available, fdb_available), ENTRIES_NUMBERS)
+    # Limit ARP scale based on available NH entries
+    asic_type = duthost.facts["asic_type"]
+    if 'cisco-8000' in asic_type:
+        ipv4_nh_available = get_crm_resources(duthost, "ipv4_nexthop", "available")
+        arp_available = min(arp_available, ipv4_nh_available)
 
     pytest_require(garp_enabled, 'Gratuitous ARP not enabled for this device')
     ptf_intf_ipv4_hosts = genrate_ipv4_ip()
@@ -182,8 +189,10 @@ def add_nd(ptfadapter, ip_and_intf_info, ptf_intf_index, nd_available):
     logger.info("Sending {} ipv6 neighbor entries".format(nd_available))
 
 
-def test_ipv6_nd(duthost, ptfhost, config_facts, tbinfo, ip_and_intf_info,
+def test_ipv6_nd(duthosts, enum_rand_one_per_hwsku_frontend_hostname,
+                 ptfhost, config_facts, tbinfo, ip_and_intf_info,
                  ptfadapter, get_function_completeness_level, proxy_arp_enabled):
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
     is_ipv6_only = is_ipv6_only_topology(tbinfo)
     _, _, ptf_intf_ipv6_addr, _, ptf_intf_index = ip_and_intf_info
     ptf_intf_ipv6_addr = increment_ipv6_addr(ptf_intf_ipv6_addr)
@@ -200,7 +209,10 @@ def test_ipv6_nd(duthost, ptfhost, config_facts, tbinfo, ip_and_intf_info,
     pytest_assert(ipv6_available > 0 and fdb_available > 0, "Entries have been filled")
 
     nd_available = min(min(ipv6_available, fdb_available), ENTRIES_NUMBERS)
-
+    asic_type = duthost.facts["asic_type"]
+    if 'cisco-8000' in asic_type:
+        ipv6_nh_available = get_crm_resources(duthost, "ipv6_nexthop", "available")
+        nd_available = min(nd_available, ipv6_nh_available)
     while loop_times > 0:
         loop_times -= 1
         try:
@@ -289,7 +301,7 @@ def test_ipv6_nd_incomplete(duthost, ptfhost, config_facts, tbinfo, ip_and_intf_
         conntrack_cnt_post = int(duthost.command("cat /proc/sys/net/netfilter/nf_conntrack_count")["stdout"])
         logger.info("nf_conntrack_count post test: {}".format(conntrack_cnt_post))
 
-        pytest_assert((conntrack_cnt_post - conntrack_cnt_pre) < tgt_conntrack_cnt * 0.1,
+        pytest_assert((conntrack_cnt_post - conntrack_cnt_pre) < tgt_conntrack_cnt,
                       "{} echo requests cause large increase in conntrack entries".format(tgt_conntrack_cnt))
 
         pytest_assert("[UNREPLIED]" not in duthost.command("conntrack -f ipv6 -L dying")["stdout"],
