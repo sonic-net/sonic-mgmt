4366d9bbeb902a213c1f6fd937af72749de71964
Yue Gao <yuega2@cisco.com>
2025-10-07 16:33:12 -0400

    Enable traffic test on vpp

--- tests/ptf_runner.py
+++ tests/ptf_runner.py
@@ -53,6 +53,18 @@ def get_dut_type(host):
         logger.info("DUT type file doesn't exist.")
     return "Unknown"
 
+def get_asic_type(host):
+    asic_type_stat = host.stat(path="/sonic/asic_type.txt")
+    if asic_type_stat["stat"]["exists"]:
+        asic_type = host.shell("cat /sonic/asic_type.txt")["stdout"]
+        if asic_type:
+            logger.info("ASIC type is {}".format(asic_type))
+            return asic_type.lower()
+        else:
+            logger.warning("ASIC type file is empty.")
+    else:
+        logger.info("ASIC type file doesn't exist.")
+    return "Unknown"
 
 def get_ptf_image_type(host):
     """
@@ -110,8 +122,9 @@ def ptf_runner(host, testdir, testname, platform_dir=None, params={},
                device_sockets=[], timeout=0, custom_options="",
                module_ignore_errors=False, is_python3=None, async_mode=False, pdb=False):
     dut_type = get_dut_type(host)
+    asic_type = get_asic_type(host)
     kvm_support = params.get("kvm_support", False)
-    if dut_type == "kvm" and kvm_support is False:
+    if dut_type == "kvm" and asic_type != "vpp" and kvm_support is False:
         logger.info("Skip test case {} for not support on KVM DUT".format(testname))
         return True
 
