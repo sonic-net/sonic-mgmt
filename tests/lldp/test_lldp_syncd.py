74f2d8599d037e74c663228cfbc33e7ed4354f84
wrideout-arista <wrideout@arista.com>
2025-10-23 14:10:57 -0400

    Initialize lldpctl_interface in ansible/tests/lldp/test_lldpsyncd.py in order to (#20635)
    Initialize lldpctl_interface in ansible/tests/lldp/test_lldpsyncd.py in order to prevent potential UnboundLocalError.

--- tests/lldp/test_lldp_syncd.py
+++ tests/lldp/test_lldp_syncd.py
@@ -256,6 +256,7 @@ def verify_each_interface_lldp_content(db_instance, interface, lldpctl_interface
     wait_until(20, 1, 0, get_lldp_entry_content_with_retry)
 
     logger.debug("Interface {}, entry_content:{}".format(interface, entry_content))
+    lldpctl_interface = None
     if isinstance(lldpctl_interfaces, dict):
         lldpctl_interface = lldpctl_interfaces.get(interface)
     elif isinstance(lldpctl_interfaces, list):
