27aa0684dc6507c4b1d6b0da56c5237f4ead584b
Dayou Liu <113053330+dayouliu1@users.noreply.github.com>
2025-10-29 21:40:18 -0700

    Fix test gnmi mgmt ipv6

--- tests/common/helpers/gnmi_utils.py
+++ tests/common/helpers/gnmi_utils.py
@@ -319,12 +319,15 @@ def verify_tcp_port(localhost, ip, port):
     logger.info("TCP: " + res['stdout'] + res['stderr'])
 
 
-def gnmi_capabilities(duthost, localhost):
+def gnmi_capabilities(duthost, localhost, duthost_mgmt_ip):
     env = GNMIEnvironment(duthost, GNMIEnvironment.GNMI_MODE)
     ip = duthost.mgmt_ip
     port = env.gnmi_port
     # Run gnmi_cli in gnmi container as workaround
-    cmd = "docker exec %s gnmi_cli -client_types=gnmi -a %s:%s " % (env.gnmi_container, ip, port)
+    if duthost_mgmt_ip['version'] == 'v6':
+        cmd = "docker exec %s gnmi_cli -client_types=gnmi -a [%s]:%s " % (env.gnmi_container, ip, port)
+    else:
+        cmd = "docker exec %s gnmi_cli -client_types=gnmi -a %s:%s " % (env.gnmi_container, ip, port)
     cmd += "-client_crt /etc/sonic/telemetry/gnmiclient.crt "
     cmd += "-client_key /etc/sonic/telemetry/gnmiclient.key "
     cmd += "-ca_crt /etc/sonic/telemetry/gnmiCA.pem "
