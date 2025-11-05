85c74f563c37df769ed0de6c5545a646cdf5d443
Boyang Yu <byu@arista.com>
2025-11-05 22:03:32 +0000

    Fix static check

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -42,8 +42,8 @@ class QosBase:
     """
     Common APIs
     """
-    SUPPORTED_T0_TOPOS = ["t0", "t0-56", "t0-56-po2vlan", "t0-64", "t0-116", "t0-118", "t0-35", "t0-d18u8s4", "dualtor-56",
-                          "dualtor-64", "dualtor-120", "dualtor", "dualtor-64-breakout", "dualtor-aa",
+    SUPPORTED_T0_TOPOS = ["t0", "t0-56", "t0-56-po2vlan", "t0-64", "t0-116", "t0-118", "t0-35", "t0-d18u8s4",
+                          "dualtor-56", "dualtor-64", "dualtor-120", "dualtor", "dualtor-64-breakout", "dualtor-aa",
                           "dualtor-aa-64-breakout", "t0-120", "t0-80", "t0-backend", "t0-56-o8v48", "t0-8-lag",
                           "t0-standalone-32", "t0-standalone-64", "t0-standalone-128", "t0-standalone-256", "t0-28"]
     SUPPORTED_T1_TOPOS = ["t1-lag", "t1-64-lag", "t1-56-lag", "t1-backend", "t1-28-lag", "t1-32-lag", "t1-48-lag"]
