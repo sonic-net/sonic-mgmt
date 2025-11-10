7fe8ffbc317134018c0b2ea1132dc6fce210640b
gshemesh2 <gshemesh@nvidia.com>
2025-11-10 11:05:11 +0200

    Refactor import for check_pmon_uptime_minutes
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -3,7 +3,7 @@ import pytest
 
 from tests.common.utilities import wait_until
 from tests.common.helpers.assertions import pytest_assert
-from tests.platform_tests.conftest import check_pmon_uptime_minutes
+from tests.common.platform.processes_utils import check_pmon_uptime_minutes
 
 pytestmark = [
     pytest.mark.topology('any', 't1-multi-asic'),
