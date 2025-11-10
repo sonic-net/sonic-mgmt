b44382554a51a1c30a6c03b863d0cf6667cf385f
yue-fred-gao <132678244+yue-fred-gao@users.noreply.github.com>
2025-11-09 20:09:17 -0600

    Issue #21205: Fix swss memory usage check failure in test_bgp_stress_link_flap in sonic-vpp (#21206)
    Description of PR
    Summary:
    Fixes #21205
    
    Type of change
     Bug fix
     Testbed and Framework(new/improvement)
     New Test case
     Skipped for non-supported platforms
     Test case improvement
    
    Approach
    What is the motivation for this PR?
    Makes test_bgp_stress_link_flap more robust in sonic-vpp
    
    How did you do it?
    Increase delay for sonic-vpp before teardown.
    
    How did you verify/test it?
    Run the test multiple times and it passed
    
    Any platform specific information?
    specific to sonic-vpp platform
    
    signed-off-by: jianquanye@microsoft.com
    Signed-off-by: Guy Shemesh <gshemesh@nvidia.com>

--- tests/bgp/test_bgp_stress_link_flap.py
+++ tests/bgp/test_bgp_stress_link_flap.py
@@ -243,10 +243,10 @@ def test_bgp_stress_link_flap(duthosts, rand_one_dut_hostname, setup, nbrhosts, 
 
     # Skip the test on Virtual Switch due to fanout switch dependency and warm reboot
     asic_type = duthost.facts['asic_type']
-    if asic_type == "vs" and (test_type == "fanout" or test_type == "all"):
+    if (asic_type == "vs" or asic_type == "vpp") and (test_type == "fanout" or test_type == "all"):
         pytest.skip("Stress link flap test is not supported on Virtual Switch")
 
-    if asic_type != "vs":
+    if asic_type != "vs" and asic_type != "vpp":
         delay_time = SLEEP_DURATION
     else:
         delay_time = SLEEP_DURATION * 100
@@ -318,7 +318,11 @@ def test_bgp_stress_link_flap(duthosts, rand_one_dut_hostname, setup, nbrhosts, 
 
     asyncio.run(flap_interfaces())
 
-    logger.info("Test Completed, waiting for 60 seconds to stabilize the system")
-    time.sleep(60)
+    if asic_type == "vpp":
+        sleep_time = 180
+    else:
+        sleep_time = 60
+    logger.info("Test Completed, waiting for {} seconds to stabilize the system".format(sleep_time))
+    time.sleep(sleep_time)
 
     return
