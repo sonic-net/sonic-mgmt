3b3cd43554cc9b1cf465fc662a99db30ca506355
Nikolay Mirin <nikolay.a.mirin@gmail.com>
2025-11-20 00:52:31 -0800

    autogenerated CODEOWNERS (#21371)
    What is the motivation for this PR?
    Improved reviewers assignment procedure
    
    How did you do it?
    Ran the codeowners script
    https://github.com/sonic-net/sonic-pipelines/tree/main/scripts/code-owners
    
    How did you verify/test it?
    Confirmed the assigned CODEOWNERS exist in Github and are relevant.
    
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- .code-owners/run.sh
+++ .code-owners/run.sh
@@ -1,3 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-codeowners-cli --repo ../ --contributors_file contributors.yaml --folder_presets_file folder_presets.yaml > ../CODEOWNERS
+# codeowners-cli Documentation
+# https://github.com/sonic-net/sonic-pipelines/blob/main/scripts/code-owners/README.md
+#
+
+
+CODEOWNERS_SCRIPTS="/labhome/nmirin/workspace/repo/sonic-pipelines/scripts/code-owners"
+# clone from
+# https://github.com/sonic-net/sonic-pipelines/
+
+
+CURRENT_SCRIPT=$(readlink -f "$0")
+CODEOWNERS_METADIR=$(dirname "${CURRENT_SCRIPT}")
+REPO_DIR=$(dirname "${CODEOWNERS_METADIR}")
+
+cat "${CODEOWNERS_METADIR}/CODEOWNERS.header"
+# Example command
+uv --project "${CODEOWNERS_SCRIPTS}" \
+      	run codeowners-cli --repo "${REPO_DIR}" \
+                      	   --contributors_file "${CODEOWNERS_METADIR}/contributors.yaml" \
+			   --folder_presets_file "${CODEOWNERS_METADIR}/folder_presets.yaml"
