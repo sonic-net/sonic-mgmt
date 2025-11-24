ebb6d809a552334574152b1afbadb9d0aff10618
Vinod Kumar <119973184+vikumarks@users.noreply.github.com>
2025-11-24 15:44:52 -0800

    Import added for fixtuer is_pfc_enabled (#21413)

--- tests/snappi_tests/qos/test_ipip_packet_reorder_with_snappi.py
+++ tests/snappi_tests/qos/test_ipip_packet_reorder_with_snappi.py
@@ -6,7 +6,7 @@ from tests.common.helpers.assertions import pytest_require
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
     fanout_graph_facts  # noqa: F401
 from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port,\
-    snappi_api, snappi_testbed_config  # noqa: F401
+    snappi_api, snappi_testbed_config, is_pfc_enabled  # noqa: F401
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map  # noqa: F401
 
 logger = logging.getLogger(__name__)
