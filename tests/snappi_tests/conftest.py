25754e7ade1e910348b5ed357c6770af56dba6ae
Raghavendran Ramanathan <rraghav@cisco.com>
2025-11-24 16:15:48 -0800

    Adding the script based on conversion instead of depending on deploy-mg.
    Signed-off-by: Raghavendran Ramanathan <rraghav@cisco.com>

--- tests/snappi_tests/conftest.py
+++ tests/snappi_tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 import random
 from tests.common.snappi_tests.common_helpers import enable_packet_aging, start_pfcwd
 from tests.conftest import generate_priority_lists
-
+from tests.common.helpers.parallel import parallel_run
+from tests.common.helpers.xml_utils import modify_minigraph
+from tests.common.config_reload import config_reload
 
 @pytest.fixture(autouse=True, scope="module")
 def rand_lossless_prio(request):
@@ -77,6 +79,64 @@ def enable_packet_aging_after_test(duthosts, rand_one_dut_hostname):
     duthost = duthosts[rand_one_dut_hostname]
     enable_packet_aging(duthost)
 
+@pytest.fixture(autouse=True, scope="session")
+def convert_to_rsb(duthosts, duts_minigraph_facts):
+    minigraph_xml = "/etc/sonic/minigraph.xml"
+    local_xml = "/tmp/minigraph.xml"
+
+    def convert_dut_to_rsb(node, duts_minigraph_facts, results):
+        if node.is_supervisor_node():
+            return
+        node.shell(f"rm /etc/sonic/running_golden*", module_ignore_errors=True)
+        filename = node.fetch(src=minigraph_xml, dest="/tmp")['dest']
+        modify_minigraph(filename, duts_minigraph_facts[node.hostname][0][1])
+        backup_config_files(node, "full_configs")
+        copy_and_load_minigraph_to_dut(node, filename)
+        backup_config_files(node, "rsb_configs")
+        prepare_gcu_patches(node, "full_configs", "rsb_configs")
+        copy_minigraph_back(node, "full_configs")
+
+    #for dut in duthosts:
+    #    convert_dut_to_rsb(dut, duts_minigraph_facts)
+    parallel_run(convert_dut_to_rsb, [], {'duts_minigraph_facts':duts_minigraph_facts}, duthosts)
+
+    yield
+
+    for dut in duthosts:
+        config_reload(dut, config_source="minigraph", start_bgp=True)
+        dut.shell(f"rm /etc/sonic/running_golden*", module_ignore_errors=True)
+        dut.shell("config save -y")
+
+def backup_config_files(dut, path):
+    dut.shell(cmd=f"mkdir {path}", module_ignore_errors=True)
+    dut.shell(cmd=f"cp /etc/sonic/config_db*json {path}")
+    dut.shell(cmd=f"cp /etc/sonic/minigraph.xml {path}")
+
+def copy_and_load_minigraph_to_dut(dut, path):
+    dut.copy(src=path, dest="/etc/sonic/minigraph.xml")
+    config_reload(dut, config_source="minigraph", start_bgp=True)
+    dut.shell("config save -y")
+
+def prepare_gcu_patches(duthost, full_configs, rsb_configs, gcu_patches="gcu_patches"):
+    duthost.shell(f"mkdir {gcu_patches}", module_ignore_errors=True)
+    for asic in range(3):
+        cmds = [
+            f'''jsondiff --indent 2 {rsb_configs}/config_db{asic}.json {full_configs}/config_db{asic}.json > /tmp/f; mv /tmp/f {gcu_patches}/patch{asic}.json''',
+            f'''jq 'map(select(.op != "remove"))' {gcu_patches}/patch{asic}.json > /tmp/f; mv /tmp/f {gcu_patches}/patch{asic}.json''',
+            f'''jq 'map(select(.op != "move"))' {gcu_patches}/patch{asic}.json > /tmp/f; mv /tmp/f {gcu_patches}/patch{asic}.json''',
+            f'''jq 'map(select(.path | contains("BUFFER_PROFILE") | not))' {gcu_patches}/patch{asic}.json > /tmp/f; mv /tmp/f {gcu_patches}/patch{asic}.json''',
+            f'''jq 'map(select(.path | contains("BUFFER_POOL") | not))' {gcu_patches}/patch{asic}.json > /tmp/f; mv /tmp/f {gcu_patches}/patch{asic}.json''',
+            f'''jq 'map(select(.path | contains("BUFFER_PG") | not))' {gcu_patches}/patch{asic}.json > /tmp/f; mv /tmp/f  {gcu_patches}/patch{asic}.json''',
+            f'''sed -i 's@path": "@path": "/asic'{asic}'@' {gcu_patches}/patch{asic}.json''',
+            f'''sed -i 's@from": "@from": "/asic'{asic}'@' {gcu_patches}/patch{asic}.json'''
+        ]
+        for cmd in cmds:
+            duthost.shell(cmd=cmd)
+
+def copy_minigraph_back(duthost, source):
+    duthost.shell(f"cp {source}/minigraph.xml /etc/sonic/minigraph.xml")
+ 
+
 
 @pytest.fixture(autouse=True,  scope="module")
 def load_gcu_config(duthosts):
@@ -90,8 +150,7 @@ def load_gcu_config(duthosts):
             return False
 
     for dut in duthosts:
-        home = dut.shell(cmd="echo $HOME")['stdout']
-        path = f"{home}/gcu_patches/"
+        path = "gcu_patches"
         if dut.stat(path=path)['stat']['exists']:
             if dut.is_multi_asic:
                 for asic in range(16):
@@ -103,5 +162,6 @@ def load_gcu_config(duthosts):
             else:
                 filename = f"{path}/patch.json"
                 load_file(dut, filename)
+
     yield
     return
