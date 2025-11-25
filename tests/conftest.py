af817bcfb077d60c19ab7e6bf5f35e48a5ef8003
Liping Xu <xuliping@microsoft.com>
2025-11-25 00:46:33 +0000

    for debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -3715,3 +3715,40 @@ def yang_validation_check(request, duthosts):
             error_summary.append(f"{host}: {result['error']}")
 
         pt_assert(False, "post-test YANG validation failed:\n" + "\n".join(error_summary))
+
+
+@pytest.fixture(scope='function', autouse=True)
+def check_pfcwd_config(duthosts, enum_rand_one_per_hwsku_frontend_hostname):
+    """
+    Fixture that stops PFC Watchdog before each test run
+
+    Args:
+        duthost (AnsibleHost): DUT instance
+    """
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+
+    cmd = "show run all | grep -E 'detection_time|POLL_INTERVAL'"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("before pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
+
+    cmd = "cat /etc/sonic/config_db.json | grep -E 'detection_time|POLL_INTERVAL'"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("before pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
+
+    cmd = "show pfcwd config"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("before pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
+
+    yield
+
+    cmd = "show run all | grep -E 'detection_time|POLL_INTERVAL'"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("after pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
+
+    cmd = "cat /etc/sonic/config_db.json | grep -E 'detection_time|POLL_INTERVAL'"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("after pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
+
+    cmd = "show pfcwd config"
+    pfcwd_cmd_response = duthost.shell(cmd, module_ignore_errors=True)
+    logger.info("after pfcwd_cmd {} response: {}".format(cmd, pfcwd_cmd_response.get('stdout', None)))
