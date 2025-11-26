7d84e7331feb1b72835d1f4f5253c271d35dfbbb
vikumarks <vikumar7ks@gmail.com>
2025-11-26 11:19:49 -0800

    Unifying code
    Read running config to get L1 settings and apply those to snappi.
    Unified snappi_dut_base_config and snappi_multi_base_config
    
    Unified get_snappi_ports_single_dut, get_snappi_ports_multi_dut and multidut_snappi_ports_for_bgp

--- tests/snappi_tests/reboot/files/reboot_helper.py
+++ tests/snappi_tests/reboot/files/reboot_helper.py
@@ -245,10 +245,10 @@ def __tgen_bgp_config(snappi_api, ):
     layer1.name = 'port settings'
     layer1.port_names = [port.name for port in config.ports]
     layer1.ieee_media_defaults = False
-    layer1.auto_negotiation.rs_fec = False
-    layer1.auto_negotiation.link_training = False
+    layer1.auto_negotiation.rs_fec = temp_tg_port[1].get('fec', False)
+    layer1.auto_negotiation.link_training = temp_tg_port[1].get('link_training', False)
     layer1.speed = temp_tg_port[1]['speed']
-    layer1.auto_negotiate = False
+    layer1.auto_negotiate = temp_tg_port[1].get('autoneg', False)
 
     conf_values = dict()
     num_of_devices = 1000
