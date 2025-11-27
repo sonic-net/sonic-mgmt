f07430adc6806c773fcc45065775157822ea9ae9
Riff <r12f.code@gmail.com>
2025-11-27 03:13:04 -0800

    Redesign and implement the telemetry framework (#20387)
    What is the motivation for this PR?
    The current telemetry framework has many serious problems:
    
    Hard to import, because it is outside of the tests folder
    No implementation, hard to try out
    Hard to use and extend, because the interface is too primitive
    No tests and can break easily
    How did you do it?
    This PR add a few things to improve the current telemetry framework:
    
    Add new design doc with example code provided in the doc.
    Implement both TS (timeseries) and DB (database) reporter, so we can try the feature easily.
    How did you verify/test it?
    Run all tests locally and passed.
    
    Any platform specific information?
    N/A.
    
    Fix TS reporters.
    
    Fix TSRepoter.
    
    Fix TS reporter.
    
    minor fix.
    
    Fix example.
    
    Reverse relationship of metrics and reporter to support more types of metrics in future.
    
    Remove bydesign not working test.
    
    Fix example.
    
    Update tests.
    
    Update TS reporter and DB repoter.
    
    Update minor things.
    
    Remove no required code.
    
    Revert unexpected change.
    
    Rename and update doc.
    
    * Get os version and job id (#1)
    
    Get os version and elastictest job id from correct place.
    
    * Revert OS version change.
    
    * Remove duplicated mock reporter fixture.
    
    ---------
    
    Co-authored-by: Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -84,6 +84,8 @@ from tests.common.platform.args.normal_reboot_args import add_normal_reboot_args
 from ptf import testutils
 from ptf.mask import Mask
 
+from tests.common.telemetry.fixtures import db_reporter, ts_reporter                        # noqa: F401
+
 
 logger = logging.getLogger(__name__)
 cache = FactsCache()
