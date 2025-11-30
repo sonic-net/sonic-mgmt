740913fa40b59e1d0957428b2fba68065fa27a6f
Carmine Scarpitta <cscarpit@cisco.com>
2025-11-30 13:25:12 -0600

    Fix BGP configuration
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- tests/srv6/test_srv6_usid_bgp_l3vpn.py
+++ tests/srv6/test_srv6_usid_bgp_l3vpn.py
@@ -54,126 +54,135 @@ def run_srv6_usid_bgp_l3vpn(enum_frontend_dut_hostname, hosts):
     # configure bgp on neighbor
     cmd = "config vrf add Vrf10"
     nbr['host'].shell(cmd)
-    cmd = "config loopback add Loopback1"
-    nbr['host'].shell(cmd)
-    cmd = "config interface vrf bind Loopback1 Vrf10"
-    nbr['host'].shell(cmd)
-    cmd = "config interface ip add Loopback1 2001:db8:1::1"
+    cmd = "config interface ip add Loopback0 fcbb:bbbb:1::1/48"
     nbr['host'].shell(cmd)
     cmd = (
         "vtysh"
         " -c 'configure'"
-        " -c 'ipv6 route {}/{} Loopback1'"
+        " -c 'no ipv6 protocol bgp route-map RM_SET_SRC6'"
         " -c 'segment-routing'"
         " -c 'srv6'"
         " -c 'locators'"
         " -c 'locator MAIN'"
         " -c 'prefix fcbb:bbbb:1::/48 block-len 32 node-len 16 func-bits 16'"
+        " -c 'behavior usid'"
         " -c 'exit'"
         " -c 'exit'"
         " -c 'exit'"
         " -c 'exit'"
-        " -c 'router bgp 64600'"
-        " -c 'neighbor 2001:db8:2::1 remote-as 65100'"
+        " -c 'router bgp {}'"
+        " -c 'bgp disable-ebgp-connected-route-check'"
         " -c 'address-family ipv6 unicast'"
-        " -c 'network {}/{}'"
-        " -c 'exit'"
+        " -c 'network fcbb:bbbb:1::/48'"
+        " -c 'exit-address-family'"
         " -c 'address-family ipv6 vpn'"
-        " -c 'neighbor 2001:db8:2::1 activate'"
+        " -c 'neighbor {} activate'"
         " -c 'exit-address-family'"
         " -c 'segment-routing srv6'"
         " -c 'locator MAIN'"
         " -c 'exit'"
-        " -c 'router bgp 64600 vrf Vrf10'"
+        " -c 'router bgp {} vrf Vrf10'"
+        " -c 'no bgp network import-check'"
         " -c 'address-family ipv6 unicast'"
+        " -c 'network {}/{}'"
         " -c 'sid vpn export auto'"
         " -c 'rd vpn export 1:10'"
         " -c 'rt vpn both 99:99'"
         " -c 'import vpn'"
         " -c 'export vpn'"
-        " -c 'redistribute connected'"
         " -c 'exit'"
-        " -c 'exit'").format(V6_PREFIX_NBR, V6_MASK_NBR, V6_PREFIX_NBR, V6_MASK_NBR)
+        " -c 'exit'").format(
+            nbr['conf']['bgp']['asn'],
+            nbr['conf']['bgp']['peers'][next(iter(nbr['conf']['bgp']['peers']))][1],
+            nbr['conf']['bgp']['asn'],
+            V6_PREFIX_NBR,
+            V6_MASK_NBR
+        )
     nbr['host'].shell(cmd)
     Logger.info("Route %s added", V6_PREFIX_NBR)
 
     # configure bgp on dut
     cmd = "config vrf add Vrf10"
     duthost.shell(cmd)
-    cmd = "config loopback add Loopback1"
-    duthost.shell(cmd)
-    cmd = "config interface vrf bind Loopback1 Vrf10"
-    duthost.shell(cmd)
-    cmd = "config interface ip add Loopback1 2001:db8:2::1"
+    cmd = "config interface ip add Loopback0 fcbb:bbbb:2::1/48"
     duthost.shell(cmd)
     cmd = (
         "vtysh"
         " -c 'configure'"
-        " -c 'ipv6 route {}/{} Loopback1'"
+        " -c 'no ipv6 protocol bgp route-map RM_SET_SRC6'"
         " -c 'segment-routing'"
         " -c 'srv6'"
         " -c 'locators'"
         " -c 'locator MAIN'"
         " -c 'prefix fcbb:bbbb:2::/48 block-len 32 node-len 16 func-bits 16'"
+        " -c 'behavior usid'"
         " -c 'exit'"
         " -c 'exit'"
         " -c 'exit'"
         " -c 'exit'"
-        " -c 'router bgp 65100'"
-        " -c 'neighbor 2001:db8:1::1 remote-as 64600'"
+        " -c 'router bgp {}'"
+        " -c 'bgp disable-ebgp-connected-route-check'"
         " -c 'address-family ipv6 unicast'"
-        " -c 'network {}/{}'"
-        " -c 'exit'"
+        " -c 'network fcbb:bbbb:2::/48'"
+        " -c 'exit-address-family'"
         " -c 'address-family ipv6 vpn'"
-        " -c 'neighbor 2001:db8:1::1 activate'"
+        " -c 'neighbor {} activate'"
         " -c 'exit-address-family'"
         " -c 'segment-routing srv6'"
         " -c 'locator MAIN'"
         " -c 'exit'"
-        " -c 'router bgp 65100 vrf Vrf10'"
+        " -c 'router bgp {} vrf Vrf10'"
+        " -c 'no bgp network import-check'"
+        " -c 'address-family ipv6 unicast'"
+        " -c 'network {}/{}'"
         " -c 'address-family ipv6 unicast'"
         " -c 'sid vpn export auto'"
         " -c 'rd vpn export 2:10'"
         " -c 'rt vpn both 99:99'"
         " -c 'import vpn'"
         " -c 'export vpn'"
-        " -c 'redistribute connected'"
         " -c 'exit'"
-        " -c 'exit'").format(V6_PREFIX_DUT, V6_MASK_DUT, V6_PREFIX_DUT, V6_MASK_DUT)
+        " -c 'exit'").format(
+            list(nbr['conf']['bgp']['peers'].keys())[0],
+            nbr['conf']['interfaces']['Port-Channel1']['ipv6'].split('/')[0],
+            list(nbr['conf']['bgp']['peers'].keys())[0],
+            V6_PREFIX_DUT,
+            V6_MASK_DUT
+        )
     duthost.shell(cmd)
     Logger.info("Route %s added to :duthost", V6_PREFIX_DUT)
 
     # check ROUTE_TABLE
     Logger.info("checking  DUT for route %s", V6_PREFIX_NBR)
-    cmd = "redis-cli -n 0  hgetall \"ROUTE_TABLE:Vrf10:{}\"".format(V6_PREFIX_NBR)
+    cmd = "sonic-db-cli APPL_DB hgetall \"ROUTE_TABLE:Vrf10:{}/{}\"".format(V6_PREFIX_NBR, V6_MASK_NBR)
     result = duthost.shell(cmd)
     result = result['stdout']
     Logger.info("Routes found: %s", result)
     py_assert(result != "", "The DUT did not program the SRv6 steering route")
-    py_assert("segments: Vrf10:2001:db8::/64" not in result,
+    py_assert("'segments': 'fcbb:bbbb:1:1::'" in result,
               "The DUT did not program the SRv6 steering route correctly, missing 'segments' field")
-    py_assert("source: fc00:0:1::1" not in result,
-              "The DUT did not program the SRv6 steering route correctly, missing 'source' field")
+    py_assert("'seg_src'" in result,
+              "The DUT did not program the SRv6 steering route correctly, missing 'seg_src' field")
 
     # check SRV6_SID_LIST_TABLE
     Logger.info("checking  DUT for SID list %s", V6_PREFIX_NBR)
-    cmd = "redis-cli -n 0  hgetall \"SRV6_SID_LIST_TABLE:Vrf10:{}\"".format(V6_PREFIX_NBR)
+    cmd = "sonic-db-cli APPL_DB hgetall \"SRV6_SID_LIST_TABLE:fcbb:bbbb:1:1::\"".format(V6_PREFIX_NBR, V6_MASK_NBR)
     result = duthost.shell(cmd)
     result = result['stdout']
     Logger.info("SID Lists found: %s", result)
     py_assert(result != "", "The DUT did not program the SRv6 SID list")
-    py_assert("path: fcbb:bbbb:1:1::" not in result,
+    py_assert("'path': 'fcbb:bbbb:1:1::'" in result,
               "The DUT did not program the SRv6 SID list correctly, missing 'path' field")
 
     # check SRV6_MY_SID_TABLE
     Logger.info("checking  DUT for MY_SID fcbb:bbbb:2:1::")
-    cmd = "redis-cli -n 0  hgetall \"SRV6_MY_SID_TABLE:32:16:16:0:fcbb:bbbb:2:1::\""
+    cmd = "sonic-db-cli APPL_DB hgetall \"SRV6_MY_SID_TABLE:32:16:16:0:fcbb:bbbb:2:1::\""
     result = duthost.shell(cmd)
     result = result['stdout']
     Logger.info("MY SIDs found: %s", result)
     py_assert(result != "", "The DUT did not program SRv6 MySid entry")
-    py_assert("action: udt6" not in result, "The DUT did not program SRv6 MySid entry, missing 'action' field")
-    py_assert("vrf: Vrf10" not in result, "The DUT did not program SRv6 MySid entry correcly, missing 'vrf' field")
+    py_assert("'action': 'udt6'" in result, "The DUT did not program SRv6 MySid entry, missing 'action' field")
+    py_assert("'vrf': 'Vrf10'" in result, "The DUT did not program SRv6 MySid entry correcly, missing 'vrf' field")
 
 
 def test_srv6_usid_bgp_l3vpn(enum_frontend_dut_hostname, setUp):
