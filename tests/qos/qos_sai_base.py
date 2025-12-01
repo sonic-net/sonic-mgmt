ea4d844f042c088db14bf4f15f3e216a717fa416
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2025-12-02 03:36:54 +1100

    enable test_acl (#21472)
    enable test_acl and test_qos_sai for t0-f2-d40u8, t1-f2-d10u8 topo
    
    Signed-off-by: Yael Tzur <ytzur@nvidia.com>

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -50,10 +50,11 @@ class QosBase:
         "dualtor-120", "dualtor", "dualtor-64-breakout", "dualtor-aa", "dualtor-aa-56", "dualtor-aa-64-breakout",
         "t0-120", "t0-80", "t0-backend", "t0-56-o8v48", "t0-8-lag", "t0-standalone-32", "t0-standalone-64",
         "t0-standalone-128", "t0-standalone-256", "t0-28", "t0-isolated-d16u16s1", "t0-isolated-d16u16s2",
-        "t0-isolated-d96u32s2", "t0-isolated-d32u32s2",
-        "t0-88-o8c80"
+        "t0-isolated-d96u32s2",  "t0-isolated-d32u32s2",
+        "t0-88-o8c80", "t0-f2-d40u8"
     ]
     SUPPORTED_T1_TOPOS = ["t1-lag", "t1-64-lag", "t1-56-lag", "t1-backend", "t1-28-lag", "t1-32-lag", "t1-48-lag",
+                          "t1-f2-d10u8",
                           "t1-isolated-d28u1", "t1-isolated-v6-d28u1", "t1-isolated-d56u2", "t1-isolated-v6-d56u2",
                           "t1-isolated-d56u1-lag", "t1-isolated-v6-d56u1-lag", "t1-isolated-d128", "t1-isolated-d32",
                           "t1-isolated-d448u15-lag", "t1-isolated-v6-d448u15-lag"]
