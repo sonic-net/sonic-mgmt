a006eea60b281a06aceeb462542b8e3c0519166a
Ryan Garofano <rgarofano@arista.com>
2025-12-02 15:01:52 -0800

    Support multi-chip in packet trimming tests with refactor (#21318)
    * Support multi-chip in packet trimming tests

--- tests/packet_trimming/test_packet_trimming_asymmetric.py
+++ tests/packet_trimming/test_packet_trimming_asymmetric.py
@@ -3,9 +3,10 @@ import logging
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.plugins.allure_wrapper import allure_step_wrapper as allure
 from tests.packet_trimming.constants import (
-    TRIM_SIZE, TRIM_DSCP, DEFAULT_PACKET_SIZE, DEFAULT_DSCP, TRIM_SIZE_MAX, JUMBO_PACKET_SIZE, TRIM_QUEUE, ASYM_TC,
-    ASYM_PORT_1_DSCP, ASYM_PORT_2_DSCP, VALID_TRIMMING_CONFIGS_ASYM, INVALID_TRIMMING_CONFIGS_ASYM, MODE_TOGGLE_COUNT,
-    NORMAL_PACKET_DSCP)
+    DEFAULT_PACKET_SIZE, DEFAULT_DSCP, JUMBO_PACKET_SIZE,
+    ASYM_PORT_1_DSCP, ASYM_PORT_2_DSCP, MODE_TOGGLE_COUNT,
+    NORMAL_PACKET_DSCP, PACKET_COUNT)
+from tests.packet_trimming.packet_trimming_config import PacketTrimmingConfig
 from tests.packet_trimming.packet_trimming_helper import (
     configure_trimming_global, verify_trimming_config, configure_trimming_action, verify_trimmed_packet,
     remove_tc_to_dscp_map, configure_tc_to_dscp_map, verify_normal_packet, verify_packet_trimming)
@@ -22,11 +23,15 @@ logger = logging.getLogger(__name__)
 class TestPacketTrimmingAsymmetric(BasePacketTrimming):
     trimming_mode = "asymmetric"
 
-    def configure_trimming_global_by_mode(self, duthost, size=TRIM_SIZE):
+    def configure_trimming_global_by_mode(self, duthost, size=None):
         """
         Configure trimming global by trimming mode
         """
-        configure_trimming_global(duthost, size=size, queue=TRIM_QUEUE, dscp='from-tc', tc=ASYM_TC)
+        if size is None:
+            size = PacketTrimmingConfig.get_trim_size(duthost)
+        queue = PacketTrimmingConfig.get_trim_queue(duthost)
+        asym_tc = PacketTrimmingConfig.get_asym_tc(duthost)
+        configure_trimming_global(duthost, size=size, queue=queue, dscp='from-tc', tc=asym_tc)
 
     def get_extra_trimmed_packet_kwargs(self):
         return dict(
@@ -42,12 +47,12 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
         Test Case: Verify Trimming Configuration
         """
         with allure.step(f"Testing {self.trimming_mode} DSCP valid configurations"):
-            for size, dscp, queue, tc in VALID_TRIMMING_CONFIGS_ASYM:
+            for size, dscp, queue, tc in PacketTrimmingConfig.get_valid_trim_configs(duthost, asymmetric=True):
                 logger.info(f"Testing valid config: size={size}, dscp={dscp}, queue={queue}, tc={tc}")
                 pytest_assert(configure_trimming_global(duthost, size=size, queue=queue, dscp=dscp, tc=tc))
 
         with allure.step(f"Testing {self.trimming_mode} DSCP invalid configurations"):
-            for size, dscp, queue, tc in INVALID_TRIMMING_CONFIGS_ASYM:
+            for size, dscp, queue, tc in PacketTrimmingConfig.get_invalid_trim_configs(duthost, asymmetric=True):
                 logger.info(f"Testing invalid config: size={size}, dscp={dscp}, queue={queue}, tc={tc}")
                 pytest_assert(not configure_trimming_global(duthost, size=size, queue=queue, dscp=dscp, tc=tc))
 
@@ -67,36 +72,36 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
                 configure_trimming_action(duthost, test_params['trim_buffer_profiles'][buffer_profile], "on")
 
         with allure.step("Verify trimming packet"):
-            kwargs = self.get_verify_trimmed_packet_kwargs({**test_params})
-            kwargs.update({'duthost': duthost, 'ptfadapter': ptfadapter})
+            kwargs = self.get_verify_trimmed_packet_kwargs(duthost, ptfadapter, {**test_params})
             verify_trimmed_packet(**kwargs)
 
-        with allure.step(f"Configure trimming in {self.trimming_mode} mode and update trim size to {TRIM_SIZE_MAX}"):
-            self.configure_trimming_global_by_mode(duthost, TRIM_SIZE_MAX)
+        max_trim_size = PacketTrimmingConfig.get_max_trim_size(duthost)
+        with allure.step(f"Configure trimming in {self.trimming_mode} mode and update trim size to {max_trim_size}"):
+            self.configure_trimming_global_by_mode(duthost, max_trim_size)
 
         with allure.step("Send packets and verify trimming works after config update"):
-            kwargs = self.get_verify_trimmed_packet_kwargs({**test_params})
+            kwargs = self.get_verify_trimmed_packet_kwargs(duthost, ptfadapter, {**test_params})
             kwargs.update({
-                'duthost': duthost,
-                'ptfadapter': ptfadapter,
                 'send_pkt_size': JUMBO_PACKET_SIZE,
-                'recv_pkt_size': TRIM_SIZE_MAX
+                'recv_pkt_size': max_trim_size
             })
             verify_trimmed_packet(**kwargs)
 
+        trim_size = PacketTrimmingConfig.get_trim_size(duthost)
+        trim_queue = PacketTrimmingConfig.get_trim_queue(duthost)
+        asym_tc = PacketTrimmingConfig.get_asym_tc(duthost)
+
         with allure.step("Verify setting TC value while missing TC_TO_DSCP_MAP attached to the egress port"):
             remove_tc_to_dscp_map(duthost)
-            configure_trimming_global(duthost, size=TRIM_SIZE, queue=TRIM_QUEUE, dscp='from-tc', tc=ASYM_TC)
-            verify_trimming_config(duthost, size=TRIM_SIZE, queue=TRIM_QUEUE, dscp='from-tc', tc=ASYM_TC)
+            configure_trimming_global(duthost, size=trim_size, queue=trim_queue, dscp='from-tc', tc=asym_tc)
+            verify_trimming_config(duthost, size=trim_size, queue=trim_queue, dscp='from-tc', tc=asym_tc)
 
         with allure.step("Verify that trimming is configured before configuring tc_to_dscp_map"):
             configure_tc_to_dscp_map(duthost, test_params['egress_ports'])
-            kwargs = self.get_verify_trimmed_packet_kwargs({**test_params})
+            kwargs = self.get_verify_trimmed_packet_kwargs(duthost, ptfadapter, {**test_params})
             kwargs.update({
-                'duthost': duthost,
-                'ptfadapter': ptfadapter,
                 'send_pkt_size': DEFAULT_PACKET_SIZE,
-                'recv_pkt_size': TRIM_SIZE
+                'recv_pkt_size': trim_size
             })
             verify_trimmed_packet(**kwargs)
 
@@ -108,9 +113,14 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
             for buffer_profile in test_params['trim_buffer_profiles']:
                 configure_trimming_action(duthost, test_params['trim_buffer_profiles'][buffer_profile], "on")
 
+        trim_size = PacketTrimmingConfig.get_trim_size(duthost)
+        trim_queue = PacketTrimmingConfig.get_trim_queue(duthost)
+        asym_tc = PacketTrimmingConfig.get_asym_tc(duthost)
+        trim_dscp = PacketTrimmingConfig.DSCP
+
         for i in range(MODE_TOGGLE_COUNT):
             with allure.step(f"Round {i+1}: Configure trimming in Symmetric DSCP mode"):
-                configure_trimming_global(duthost, size=TRIM_SIZE, queue=TRIM_QUEUE, dscp=TRIM_DSCP)
+                configure_trimming_global(duthost, size=trim_size, queue=trim_queue, dscp=trim_dscp)
                 for egress_port in test_params['egress_ports']:
                     verify_packet_trimming(
                         duthost=duthost,
@@ -120,12 +130,13 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
                         block_queue=test_params['block_queue'],
                         send_pkt_size=DEFAULT_PACKET_SIZE,
                         send_pkt_dscp=DEFAULT_DSCP,
-                        recv_pkt_size=TRIM_SIZE,
-                        recv_pkt_dscp=TRIM_DSCP
+                        recv_pkt_size=trim_size,
+                        recv_pkt_dscp=trim_dscp,
+                        packet_count=PACKET_COUNT
                     )
 
             with allure.step(f"Round {i+1}: Configure trimming in Asymmetric DSCP mode"):
-                configure_trimming_global(duthost, size=TRIM_SIZE, queue=TRIM_QUEUE, dscp='from-tc', tc=ASYM_TC)
+                configure_trimming_global(duthost, size=trim_size, queue=trim_queue, dscp='from-tc', tc=asym_tc)
                 verify_trimmed_packet(
                     duthost=duthost,
                     ptfadapter=ptfadapter,
@@ -134,7 +145,7 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
                     block_queue=test_params['block_queue'],
                     send_pkt_size=DEFAULT_PACKET_SIZE,
                     send_pkt_dscp=DEFAULT_DSCP,
-                    recv_pkt_size=TRIM_SIZE,
+                    recv_pkt_size=trim_size,
                     recv_pkt_dscp_port1=ASYM_PORT_1_DSCP,
                     recv_pkt_dscp_port2=ASYM_PORT_2_DSCP
                 )
@@ -147,8 +158,12 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
             for buffer_profile in test_params['trim_buffer_profiles']:
                 configure_trimming_action(duthost, test_params['trim_buffer_profiles'][buffer_profile], "on")
 
+        trim_size = PacketTrimmingConfig.get_trim_size(duthost)
+        trim_queue = PacketTrimmingConfig.get_trim_queue(duthost)
+        asym_tc = PacketTrimmingConfig.get_asym_tc(duthost)
+
         with allure.step("Configure trimming in Asymmetric DSCP mode"):
-            configure_trimming_global(duthost, size=TRIM_SIZE, queue=TRIM_QUEUE, dscp='from-tc', tc=ASYM_TC)
+            configure_trimming_global(duthost, size=trim_size, queue=trim_queue, dscp='from-tc', tc=asym_tc)
 
         with allure.step("Verify trimming in Asymmetric DSCP mode"):
             verify_trimmed_packet(
@@ -159,7 +174,7 @@ class TestPacketTrimmingAsymmetric(BasePacketTrimming):
                 block_queue=test_params['block_queue'],
                 send_pkt_size=DEFAULT_PACKET_SIZE,
                 send_pkt_dscp=DEFAULT_DSCP,
-                recv_pkt_size=TRIM_SIZE,
+                recv_pkt_size=trim_size,
                 recv_pkt_dscp_port1=ASYM_PORT_1_DSCP,
                 recv_pkt_dscp_port2=ASYM_PORT_2_DSCP
             )
