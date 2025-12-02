174038f3456556f5382c3cf6bdc8e70109b8e86e
Kaz Kylheku <kazinator@arista.com>
2025-12-02 20:40:10 +0000

    override config tests: ignore certain tables in empty test
    In the test case test_load_minigraph_with_golden_config, the function
    load_minigraph_with_golden_empty_input asserts. This function expects all the
    tables to be identical after the golden config is overridden to be empty.  But
    it looks as if the golden config is the source of several configurations: the
    "BMP" table, and "fec" : "rs" entries in the "PORT".  Thus, when the golden
    config is replaced with an empty configuration, the entire "BMP" table,
    well as those "fec" properties in the "PORT" table, disappear.
    
    We propose a variable GOLDEN_OVERRIDDEN_TABLES for additional tables
    which that function should skip. This is similar to the existing variable
    NON_USER_CONFIG_TABLES which non-user-config tables that need to be skipped.
    
    tests/override_config_table/test_override_config_table.py
    (GOLDEN_OVERRIDDEN_TABLES): New variable.
    (load_minigraph_with_golden_empty_input): Skip tables found in
    GOLDEN_OVERRIDDEN_TABLES.
    
    tests/override_config_table/test_override_config_table_masic.py:
    Same changes as in test_override_config_table.py.
    
    Signed-Off-By: Kaz Kylheku <kazinator@arista.com>

--- tests/override_config_table/test_override_config_table_masic.py
+++ tests/override_config_table/test_override_config_table_masic.py
@@ -9,6 +9,10 @@ from tests.common.utilities import backup_config, restore_config, get_running_co
     reload_minigraph_with_golden_config, file_exists_on_dut, compare_dicts_ignore_list_order, \
     NON_USER_CONFIG_TABLES
 
+# Tables known to be overriden in run-time config, which will appear different
+# if the golden config is overridden empty.
+GOLDEN_OVERRRIDDEN_TABLES = ["FEATURE", "PORT"]
+
 GOLDEN_CONFIG = "/etc/sonic/golden_config_db.json"
 GOLDEN_CONFIG_BACKUP = "/etc/sonic/golden_config_db.json_before_override"
 CONFIG_DB = "/etc/sonic/config_db.json"
@@ -98,7 +102,7 @@ def load_minigraph_with_golden_empty_input(duthost):
     # Test host running config override
     host_current_config = get_running_config(duthost)
     for table in initial_host_config:
-        if table in NON_USER_CONFIG_TABLES:
+        if table in NON_USER_CONFIG_TABLES or table in GOLDEN_OVERRRIDDEN_TABLES:
             continue
 
         if table == "ACL_TABLE":
@@ -112,7 +116,7 @@ def load_minigraph_with_golden_empty_input(duthost):
     # Test asic0 running config override
     asic0_current_config = get_running_config(duthost, "asic0")
     for table in initial_asic0_config:
-        if table in NON_USER_CONFIG_TABLES:
+        if table in NON_USER_CONFIG_TABLES or table in GOLDEN_OVERRRIDDEN_TABLES:
             continue
 
         if table == "ACL_TABLE":
