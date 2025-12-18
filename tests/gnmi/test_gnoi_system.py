3c24e6bfe523ed74174dac4e987684641f6e3b21
Ravali Yeluri (WIPRO LIMITED) <v-ryeluri@microsoft.com>
2025-12-17 21:37:28 -0800

    Adding gnxi/preload code

--- tests/gnmi/test_gnoi_system.py
+++ tests/gnmi/test_gnoi_system.py
@@ -16,25 +16,6 @@ This module contains tests for the gNOI System API.
 """
 
 
-def test_gnoi_system_time(duthosts, rand_one_dut_hostname, localhost):
-    """
-    Verify the gNOI System Time API returns the current system time in valid JSON format.
-    """
-    duthost = duthosts[rand_one_dut_hostname]
-
-    # Get current time
-    ret, msg = gnoi_request(duthost, localhost, "System", "Time", "")
-    pytest_assert(ret == 0, "System.Time API reported failure (rc = {}) with message: {}".format(ret, msg))
-    logging.info("System.Time API returned msg: {}".format(msg))
-    # Message should contain a json substring like this {"time":1735921221909617549}
-    # Extract JSON part from the message
-    msg_json = extract_first_json_substring(msg)
-    if not msg_json:
-        pytest.fail("Failed to extract JSON from System.Time API response")
-    logging.info("Extracted JSON: {}".format(msg_json))
-    pytest_assert("time" in msg_json, "System.Time API did not return time")
-
-
 def extract_first_json_substring(s):
     """
     Extract the first JSON substring from a given string.
