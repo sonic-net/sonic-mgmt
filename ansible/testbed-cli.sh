b366fdcae071d27ac35493e57ecfeea4ec7e18fc
prathyushabathula <130686114+prathyushabathula@users.noreply.github.com>
2025-12-18 11:44:59 -0600

    Feature/csonic support (#20665)
    Description of PR
    Added code to support the csonic image as a neighboring device for the testbed
    This PR introduces support for cSONiC (Containerized SONiC), a new VM type that runs SONiC virtual switch instances in Docker containers. This enables lightweight, fast-deployment testing environments similar to how cEOS provides containerized Arista EOS support.
    
    Command 1: add-topo - Deploy Topology
    Deploy the cSONiC topology to a testbed:
    cd /data/sonic-mgmt/ansible
    ./testbed-cli.sh -t vtestbed.yaml -m veos_vtb -k csonic add-topo vms-kvm-t0-csonic password.txt
    
    Command 2: deploy-mg - Deploy Minigraph
    Deploy or update the minigraph configuration to the DUT:
    ./testbed-cli.sh -t vtestbed.yaml -m veos_vtb deploy-mg vms-kvm-t0-csonic veos_vtb password.txt
    
    Signed-off-by: Yael Tzur <ytzur@nvidia.com>

--- ansible/testbed-cli.sh
+++ ansible/testbed-cli.sh
@@ -25,7 +25,7 @@ function usage
   echo "Options:"
   echo "    -t <tbfile>     : testbed CSV file name (default: 'testbed.yaml')"
   echo "    -m <vmfile>     : virtual machine file name (default: 'veos')"
-  echo "    -k <vmtype>     : vm type (veos|ceos|vsonic|vcisco) (default: 'ceos')"
+  echo "    -k <vmtype>     : vm type (veos|ceos|vsonic|vcisco|csonic) (default: 'ceos')"
   echo "    -n <vm_num>     : vm num (default: 0)"
   echo "    -s <msetnumber> : master set identifier on specified <k8s-server-name> (default: 1)"
   echo "    -d <dir>        : sonic vm directory (default: $HOME/sonic-vm)"
