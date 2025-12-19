2701cb99b2083e9d22d9c296984f9274a626cc74
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2025-12-19 02:00:42 +0000

    Fix script python path
    Signed-off-by: opcoder0 <110003254+opcoder0@users.noreply.github.com>

--- tests/common/plugins/conditional_mark/unit_test/run_tests.sh
+++ tests/common/plugins/conditional_mark/unit_test/run_tests.sh
@@ -11,9 +11,9 @@ echo ""
 
 echo "1. Unit Tests (Programmatic):"
 echo "----------------------------------------------------------------------"
-/home/sagummaraj/.pyenv/versions/dev/bin/python unittest_category_expiry.py -v
+python unittest_category_expiry.py -v
 
 echo ""
 echo "2. Unit Tests (YAML-based):"
 echo "----------------------------------------------------------------------"
-/home/sagummaraj/.pyenv/versions/dev/bin/python unittest_yaml_skip_categories.py -v
+python unittest_yaml_skip_categories.py -v
