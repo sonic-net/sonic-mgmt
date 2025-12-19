1b71665a297057eeef3d6064ca9c0ffd2e6bf339
karavasi <olympia.karavasili_arapogianni@nokia.com>
2025-12-19 17:00:24 +0100

    Displaying ip route table for all duthosts Signed-off-by: karavasi <olympia.karavasili_arapogianni@nokia.com>

--- tests/generic_config_updater/add_cluster/test_add_cluster.py
+++ tests/generic_config_updater/add_cluster/test_add_cluster.py
@@ -954,8 +954,9 @@ def setup_add_cluster(tbinfo,
     )
     initial_buffer_pg_info = get_cfg_info_from_dut(duthost, 'BUFFER_PG', enum_rand_one_asic_namespace)
     with allure.step("Data Verification before removing cluster"):
-        logger.info(duthost.shell('show ip bgp summary -d all'))
-        logger.info(duthost.shell('show ipv6 bgp summary -d all'))
+        for host_device in duthosts:
+            logger.info(host_device.shell('show ip bgp summary -d all'))
+            logger.info(host_device.shell('show ipv6 bgp summary -d all'))
         send_and_verify_traffic(tbinfo, duthost_src, duthost, asic_id_src, asic_id,
                                 ptfadapter, dst_ip=STATIC_DST_IP, count=10, expect_error=False)
 
