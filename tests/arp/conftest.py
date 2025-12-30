17407f5cb8dbb0e024c89fae6815bde4352d0cf1
Justin Wong <jvwong@arista.com>
2025-12-30 19:19:46 +0000

    Remove packets_for_test fixture dependency for proxy arp enabled
    Fixing issue:
    https://github.com/sonic-net/sonic-mgmt/issues/21801
    
    Determine the ip version from the test name to remove teh dependency on
    packets_for_test.
    
    Signed-off-by: Justin Wong <jvwong@arista.com>

--- tests/arp/conftest.py
+++ tests/arp/conftest.py
@@ -282,7 +282,7 @@ def ip_and_intf_info(config_facts, intfs_for_test, ptfhost, ptfadapter):
 
 
 @pytest.fixture
-def proxy_arp_enabled(packets_for_test, rand_selected_dut, config_facts):
+def proxy_arp_enabled(request, rand_selected_dut, config_facts):
     """
     Tries to enable proxy ARP for each VLAN on the ToR
 
@@ -316,7 +316,7 @@ def proxy_arp_enabled(packets_for_test, rand_selected_dut, config_facts):
         new_proxy_arp_res = duthost.shell(proxy_arp_check_cmd.format(vid))
         new_proxy_arp_vals.append(new_proxy_arp_res['stdout'])
 
-    if ip_version == 'v6':
+    if 'ipv6' in request.node.name:
         # Allow time for ndppd to reset and startup
         time.sleep(30)
 
