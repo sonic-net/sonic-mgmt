eec727dd3abcd3de03522a232204db4a774149a7
Mai Bui <maibui@microsoft.com>
2026-01-13 15:09:22 +0000

    fix precommit
    Signed-off-by: Mai Bui <maibui@microsoft.com>

--- tests/auditd/conftest.py
+++ tests/auditd/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from tests.common.helpers.dut_utils import is_container_running
 
+
 @pytest.fixture(scope="module")
 def verify_auditd_containers_running(duthosts, enum_rand_one_per_hwsku_hostname):
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
