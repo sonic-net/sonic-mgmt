d6b574a8d50aec6e3ce843173ea668b811e63821
BenTsayeg211 <82679746+BenTsayeg211@users.noreply.github.com>
2026-01-21 12:17:03 +0200

    Update test_intf_fec.py
    Signed-off-by: Ben Tsayeg <btsayeg@nvidia.com>

--- tests/platform_tests/test_intf_fec.py
+++ tests/platform_tests/test_intf_fec.py
@@ -163,7 +163,7 @@ def test_verify_fec_stats_counters(duthosts, toggles_num, enum_rand_one_per_hwsk
         speed_gbps = f"{int(speed) // 1000}G"
         if speed_gbps not in SUPPORTED_SPEEDS:
             continue
-        
+
         # Removes commas from "show interfaces counters fec-stats" (i.e. 12,354 --> 12354) to allow int conversion
         fec_corr = intf.get('fec_corr', '').replace(',', '').lower()
         fec_uncorr = intf.get('fec_uncorr', '').replace(',', '').lower()
