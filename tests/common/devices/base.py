f17e356a6d1fd6859ad2381fa80d3f2d161d30b5
Ryangwaite <ryangwaite@gmail.com>
2026-01-27 20:03:34 +1000

    Fix Python Interpreter Not Found Errors after Device Reboot in upgrade_path tests (#22123)
    What is the motivation for this PR?
    Fix a bug when upgrading/downgrading across SONiC versions that have different python versions
    
    How did you do it?
    Clear the discovered ansible fact cache before ansible goes to use it after the device has come up after rebooting. This makes it first rediscover the current python ansible interpreter.
    
    How did you verify/test it?
    Tested test_upgrade_path from 202311->202505 upgrade path. Two scenarios one where the device starts on 202311 at test start time (so no inital downgrade happens), and the other where it starts on 202505 so it first needs to be downgraded to 202311 during the test before advanced reboot starts. Tested on 202511 sonic-mgmt branch.
    
    Any platform specific information?
    No
    
    Supported testbed topology if it's a new test case?
    N/A
    
    Signed-off-by: Ryan Garthwaite <ryangwaite@gmail.com>
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/common/devices/base.py
+++ tests/common/devices/base.py
@@ -3,6 +3,7 @@ import json
 import logging
 import collections
 from multiprocessing.pool import ThreadPool
+from pytest_ansible.results import AdHocResult, ModuleResult
 
 from tests.common.errors import RunAnsibleModuleFail
 
@@ -104,8 +105,16 @@ class AnsibleHostBase(object):
 
         module_args = json.loads(json.dumps(module_args, cls=AnsibleHostBase.CustomEncoder))
         complex_args = json.loads(json.dumps(complex_args, cls=AnsibleHostBase.CustomEncoder))
-        res = self.module(*module_args, **complex_args)[self.hostname]
-        res.encoder = AnsibleHostBase.CustomEncoder
+
+        adhoc_res: AdHocResult = self.module(*module_args, **complex_args)
+
+        if self.module_name == "meta":
+            # The meta module is special in Ansible - it doesn't execute on remote hosts, it controls Ansible's behavior
+            # There are no per-host ModuleResults contained within it
+            return
+
+        hostname_res: ModuleResult = adhoc_res[self.hostname]
+        hostname_res.encoder = AnsibleHostBase.CustomEncoder
 
         if verbose:
             logger.debug(
@@ -114,7 +123,7 @@ class AnsibleHostBase(object):
                     function_name,
                     line_number,
                     self.hostname,
-                    self.module_name, json.dumps(res, cls=AnsibleHostBase.CustomEncoder)
+                    self.module_name, json.dumps(hostname_res, cls=AnsibleHostBase.CustomEncoder)
                 )
             )
         else:
@@ -125,15 +134,15 @@ class AnsibleHostBase(object):
                     line_number,
                     self.hostname,
                     self.module_name,
-                    res.is_failed,
-                    res.get('rc', None)
+                    hostname_res.is_failed,
+                    hostname_res.get('rc', None)
                 )
             )
 
-        if (res.is_failed or 'exception' in res) and not module_ignore_errors:
-            raise RunAnsibleModuleFail("run module {} failed".format(self.module_name), res)
+        if (hostname_res.is_failed or 'exception' in hostname_res) and not module_ignore_errors:
+            raise RunAnsibleModuleFail("run module {} failed".format(self.module_name), hostname_res)
 
-        return res
+        return hostname_res
 
 
 class NeighborDevice(dict):
