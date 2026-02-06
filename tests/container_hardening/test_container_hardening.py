068fbf86abcff65972f0874ceff0ae0567ae3c6a
Nate White <nate@nexthop.ai>
2026-02-06 15:26:00 +0000

    Update test_container_hardening to check privilege via docker
    Previously this test was checking whether the block device that
    /etc/hosts was mounted on was mounted in the container to determine
    whether the container was privleged. This isn't really effective.
    
    There was also some checking of just some dockers based on random
    inputs.
    
    Instead, we'll get a list of all the containers running on the dut and
    check all of them against the allow-list of privileged containers. We'll
    check with the docker config to determine which containers are
    privileged which is a more concrete check.
    
    Signed-off-by: Nate White <nate@nexthop.ai>

--- tests/container_hardening/test_container_hardening.py
+++ tests/container_hardening/test_container_hardening.py
@@ -1,8 +1,7 @@
 import re
 import pytest
 import logging
-from tests.common.helpers.assertions import pytest_assert, pytest_require
-from tests.common.helpers.dut_utils import is_container_running, get_disabled_container_list
+from tests.common.helpers.assertions import pytest_assert
 
 pytestmark = [
     pytest.mark.topology('any'),
@@ -11,9 +10,7 @@ pytestmark = [
 logger = logging.getLogger(__name__)
 
 CONTAINER_NAME_REGEX = r"([a-zA-Z_-]+)(\d*)([a-zA-Z_-]+)(\d*)$"
-# Skip testing on following containers
-# The following containers cannot be privileged hardening
-# syncd, gbsyncd, and swss cannot be privileged hardening
+# The following containers are allowed to run in privileged mode
 PRIVILEGED_CONTAINERS = [
     "syncd",
     "gbsyncd",
@@ -23,29 +20,51 @@ PRIVILEGED_CONTAINERS = [
 ]
 
 
-def test_container_privileged(duthosts, enum_rand_one_per_hwsku_hostname, enum_rand_one_asic_index, enum_dut_feature):
+def get_base_container_name(container_name):
     """
-    Test container without --privileged flag has no access to /dev/vda* or /dev/sda*
+    Extract the base feature name from a container name.
+    e.g., bgp0 -> bgp, syncd1 -> syncd, p4rt -> p4rt
+    """
+    match = re.match(CONTAINER_NAME_REGEX, container_name)
+    if match:
+        return ''.join(match.groups()[:-1])
+    return container_name
+
+
+def test_container_privileged(duthosts, enum_rand_one_per_hwsku_hostname):
+    """
+    Test that no containers are running in privileged mode except those explicitly allowed.
+
+    Uses 'docker inspect' to check the privileged status of all running containers
+    and fails if any container not in PRIVILEGED_CONTAINERS is running with --privileged.
     """
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
-    asic = duthost.asic_instance(enum_rand_one_asic_index)
-    container_name = asic.get_docker_name(enum_dut_feature)
-    disabled_containers = get_disabled_container_list(duthost)
 
-    skip_condition = disabled_containers[:]
-    skip_condition.extend(PRIVILEGED_CONTAINERS)
-    # bgp0 -> bgp, bgp -> bgp, p4rt -> p4rt
-    feature_name = ''.join(re.match(CONTAINER_NAME_REGEX, container_name).groups()[:-1])
-    pytest_require(feature_name not in skip_condition, "Skipping test for container {}".format(feature_name))
+    # Get all running containers
+    running_containers = duthost.shell(
+        r"docker ps -f 'status=running' --format \{\{.Names\}\}"
+    )['stdout_lines']
+    pytest_assert(running_containers, "No running containers found on DUT")
 
-    is_running = is_container_running(duthost, container_name)
-    pytest_assert(is_running, "Container '{}' is not running. Exiting...".format(container_name))
+    # Check privileged status for each container using docker inspect
+    unauthorized_privileged = []
+    for container_name in running_containers:
+        result = duthost.shell(
+            r"docker inspect --format \{\{.HostConfig.Privileged\}\} " + container_name
+        )
+        is_privileged = result['stdout'].strip().lower() == 'true'
 
-    docker_exec_cmd = 'docker exec {} bash -c '.format(container_name)
-    cmd = duthost.shell(docker_exec_cmd + "'mount | grep /etc/hosts' | awk '{print $1}'")
-    rc, device = cmd['rc'], cmd['stdout']
-    output = duthost.shell(docker_exec_cmd + "'ls {}'".format(device), module_ignore_errors=True)['stdout']
+        if is_privileged:
+            base_name = get_base_container_name(container_name)
+            if base_name not in PRIVILEGED_CONTAINERS:
+                logger.error("Container '{}' is running in privileged mode but is not allowed".format(container_name))
+                unauthorized_privileged.append(container_name)
+            else:
+                logger.info("Container '{}' is privileged (as expected)".format(container_name))
 
-    pytest_assert(rc == 0, 'Failed to get the device name.')
-    pytest_assert(device.startswith('/dev/'), 'Invalid device {}.'.format(device))
-    pytest_assert(not output, 'The partition {} exists.'.format(device))
+    pytest_assert(
+        not unauthorized_privileged,
+        "The following containers are running in privileged mode but are not allowed: {}".format(
+            unauthorized_privileged
+        )
+    )
