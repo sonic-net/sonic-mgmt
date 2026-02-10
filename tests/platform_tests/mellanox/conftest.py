070e5fb26c219d00851e676ce2247cfbbf3043c5
echuawu <chuanw@nvidia.com>
2026-02-10 08:55:21 +0200

    Enhance test_check_sfp_presence.py to support CPO type
    1. Update fixture definition of is_cpo_supported
    2. Update test_check_sfp_presence.py to support CPO type
    
    Change-Id: I846b1e312f1a47e7eb1f622fb7e89d5b54d2ba95
    Signed-off-by: echuawu <chuanw@nvidia.com>

--- tests/platform_tests/mellanox/conftest.py
+++ tests/platform_tests/mellanox/conftest.py
@@ -1,7 +1,11 @@
 import pytest
+import json
 
 from tests.platform_tests.mellanox.software_control_helper import sc_supported, sc_ms_sku, get_ports_supporting_sc, \
     check_sc_sai_attribute_value
+
+HWSKU_JSON_PATH = "/usr/share/sonic/device/{}/{}/hwsku.json"
+CPO_PORT_TYPE = "CPO"
 
 
 def pytest_addoption(parser):
@@ -33,3 +37,21 @@ def get_sw_control_ports(duthost, is_sw_control_feature_enabled, conn_graph_fact
     if is_sw_control_feature_enabled:
         sw_ports = get_ports_supporting_sc(duthost)
         return sw_ports
+
+
+@pytest.fixture(scope="module")
+def is_cpo_supported(duthosts, rand_one_dut_hostname):
+    duthost = duthosts[rand_one_dut_hostname]
+    platform = duthost.facts["platform"]
+    hwsku = duthost.facts['hwsku']
+    f_path = HWSKU_JSON_PATH.format(platform, hwsku)
+    if not duthost.stat(path=f_path)["stat"]["exists"]:
+        return False
+
+    output = duthost.command(f"cat {f_path}")["stdout"]
+    hwsku_info = json.loads(output)
+    if hwsku_info.get('interfaces'):
+        for intf in hwsku_info['interfaces']:
+            if hwsku_info['interfaces'][intf].get('port_type') == CPO_PORT_TYPE:
+                return True
+    return False
