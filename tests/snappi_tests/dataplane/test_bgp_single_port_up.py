06899c4ba4453f673ba24ba791a24774fa035480
selldinesh <dinesh.sellappan@keysight.com>
2026-02-11 10:27:44 -0800

    review comment changes
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/snappi_tests/dataplane/test_bgp_single_port_up.py
+++ tests/snappi_tests/dataplane/test_bgp_single_port_up.py
@@ -1,5 +1,5 @@
 from tests.common.telemetry import UNIT_SECONDS       # noqa: F401, F403, F405, E402
-from tests.common.telemetry.constants import METRIC_LABEL_TG_TRAFFIC_RATE,\
+from tests.common.telemetry.constants import METRIC_LABEL_TG_TRAFFIC_RATE, \
     METRIC_LABEL_TG_FRAME_BYTES  # noqa: F401, F403, F405, E402
 from tests.snappi_tests.dataplane.imports import *  # noqa F403
 from snappi_tests.dataplane.files.helper import *  # noqa F403
@@ -28,10 +28,10 @@ ROUTE_RANGES = {
 }
 
 
-@pytest.mark.parametrize("subnet_type", ["IPv6"])
+@pytest.mark.parametrize("ip_version", ["IPv6"])
 @pytest.mark.parametrize("frame_rate", [10])
-@pytest.mark.parametrize("frame_size", [64, 128, 256, 512, 1024, 1518])
-@pytest.mark.parametrize("event_type", ["Port Flap Up", "Route Injection"])
+@pytest.mark.parametrize("frame_size", [64, 128, 256, 512, 1024, 4096, 8192])
+@pytest.mark.parametrize("event_type", ["t0_port_startup", "route_injection"])
 def test_bgp_sessions(
     duthosts,
     snappi_api,
@@ -39,27 +39,21 @@ def test_bgp_sessions(
     fanout_graph_facts_multidut,
     set_primary_chassis,   # noqa: F811, F401
     create_snappi_config,  # noqa: F811, F401
-    subnet_type,
+    ip_version,
     db_reporter,
     tbinfo,
     frame_rate,
     frame_size,
     event_type
 ):
-    """
-    Test to check if packets get dropped on injecting fec errors
-    Note: fanout_per_port is the number of fanouts per fron panel port
-          Example: For running the test on 400g fanout mode of a 800g port,
-          fanout_per_port is 2
-    """
     snappi_extra_params = SnappiTestParams()
     pytest_assert(
-        subnet_type in ROUTE_RANGES, "Failing test as no route ranges are provided for {}".format(subnet_type)
+        ip_version in ROUTE_RANGES, "Failing test as no route ranges are provided for {}".format(ip_version)
     )
     snappi_extra_params.ROUTE_RANGES = ROUTE_RANGES
-    snappi_ports = get_duthost_bgp_details(duthosts, get_snappi_ports, subnet_type)
-    tx_ports = snappi_ports[::2]
-    rx_ports = snappi_ports[1::2]
+    snappi_ports = get_duthost_bgp_details(duthosts, get_snappi_ports, ip_version)
+    port_distrbution = (slice(0, len(snappi_ports) // 2), slice(len(snappi_ports) // 2, None))
+    tx_ports, rx_ports = snappi_ports[port_distrbution[0]], snappi_ports[port_distrbution[1]]
     snappi_extra_params.FLAP_DETAILS = {
         "device_name": rx_ports[0]['peer_device'],
         "port_name": rx_ports[0]['peer_port'],
@@ -68,14 +62,14 @@ def test_bgp_sessions(
         "Tx": {
             "protocol_type": "bgp",
             "ports": tx_ports,
-            "subnet_type": subnet_type,
+            "subnet_type": ip_version,
             "is_rdma": False,
         },
         "Rx": {
-            "route_ranges": ROUTE_RANGES[subnet_type] * len(rx_ports),
+            "route_ranges": ROUTE_RANGES[ip_version] * len(rx_ports),
             "protocol_type": "bgp",
             "ports": rx_ports,
-            "subnet_type": subnet_type,
+            "subnet_type": ip_version,
             "is_rdma": False,
         },
     }
@@ -97,7 +91,7 @@ def test_bgp_sessions(
         snappi_config,
         tx_ports,
         rx_ports,
-        subnet_type,
+        ip_version,
         snappi_obj_handles,
         event_type,
         db_reporter,
@@ -111,7 +105,7 @@ def get_convergence_for_single_session_flap(
     snappi_config,
     tx_ports,
     rx_ports,
-    subnet_type,
+    ip_version,
     snappi_obj_handles,
     event_type,
     db_reporter,
@@ -124,7 +118,7 @@ def get_convergence_for_single_session_flap(
                                              "convergence time for port up/Route Injection event",
                                              UNIT_SECONDS,
                                              db_reporter)
-    if event_type == "Port Flap Up":
+    if event_type == "t0_port_startup":
         logger.info("Starting Single Port Flap (Up) Test")
         snappi_api.set_config(snappi_config)
         flap_dut_obj = next(
@@ -133,7 +127,7 @@ def get_convergence_for_single_session_flap(
         )
         try:
             start_stop(snappi_api, operation="start", op_type="protocols")
-            check_bgp_state(snappi_api, subnet_type)
+            check_bgp_state(snappi_api, ip_version)
             logger.info(
                 "Shutting down {} port of {} dut before starting traffic ".format(
                     snappi_extra_params.FLAP_DETAILS["port_name"],
@@ -162,30 +156,20 @@ def get_convergence_for_single_session_flap(
             )
             # calculate pld
             wait(20, "For statistics to be collected")
-            tmp = get_stats(
-                snappi_api,
-                "Data Plane Port Statistics",
-                columns=['Port', 'Tx Frame Rate', 'Rx Frame Rate'],
-                return_type='df'
-            )
             tmp['port_num'] = tmp['Port'].str.extract(r'(\d+)').astype(int)
-            even_ports = tmp.loc[tmp['port_num'] % 2 == 0, :]
+            tmp = tmp.sort_values('port_num')
+            mid = len(tmp) // 2
+            second_half_ports = tmp.iloc[mid:, :]
             # Check if 'Rx Frame Rate' values for Rx ports are greater than atleast 1000
             # to ensure all Rx ports are receiving traffic
-            all_close = (even_ports['Rx Frame Rate'] > 1000).all()
+            all_close = (second_half_ports['Rx Frame Rate'] > 1000).all()
             pytest_assert(all_close, "Not all Rx ports are having frame rate atleast 1000")
             flow_stats = get_stats(snappi_api, "Traffic Item Statistics")
             delta_frames = flow_stats[0].frames_tx - flow_stats[0].frames_rx
             pkt_loss_duration = 1000 * (delta_frames / flow_stats[0].frames_tx_rate)
             logger.info("Delta Frames : {}".format(delta_frames))
             pytest_assert(int(delta_frames) != 0, "Delta Frames is 0 after flap, which means no packet drop occurred")
-            snappi_api._ixnetwork.ClearStats()
-            wait(20, "For statistics to be Cleared")
-            flow_stats = get_stats(snappi_api, "Traffic Item Statistics")
-            pytest_assert(
-                int(flow_stats[0].loss) == 0,
-                "Total Tx Rx Rates are not equal after link flap",
-            )
+            pytest_assert(is_traffic_converged(snappi_api), "Traffic did not converge after link down")
             logger.info('Traffic has converged back after link flap Up')
             logger.info('--------------------------   Convergence Numbers   ----------------------------------')
             logger.info("Convergence Time for Single Port Flap Up: {} (ms)".format(pkt_loss_duration))
@@ -193,10 +177,10 @@ def get_convergence_for_single_session_flap(
             start_stop(snappi_api, operation="stop", op_type="traffic")
             test_labels = {
                 METRIC_LABEL_TEST_PARAMS_EVENT_TYPE: event_type,
-                METRIC_LABEL_TEST_PARAMS_ROUTE_SCALE: ROUTE_RANGES[subnet_type][0][0][-1],
+                METRIC_LABEL_TEST_PARAMS_ROUTE_SCALE: ROUTE_RANGES[ip_version][0][0][-1],
                 METRIC_LABEL_TG_TRAFFIC_RATE: snappi_extra_params.traffic_flow_config[0]['line_rate'],
                 METRIC_LABEL_TG_FRAME_BYTES: snappi_extra_params.traffic_flow_config[0]['frame_size'],
-                METRIC_LABEL_TG_IP_VERSION: subnet_type,
+                METRIC_LABEL_TG_IP_VERSION: ip_version,
             }
             convergence_dataplane_time.record(pkt_loss_duration, test_labels)
             db_reporter.report()
@@ -215,18 +199,17 @@ def get_convergence_for_single_session_flap(
             flap_dut_obj.command(
                 "sudo config interface startup {}\n".format(snappi_extra_params.FLAP_DETAILS["port_name"])
             )
-    elif event_type == "Route Injection":
+    elif event_type == "route_injection":
         logger.info("Starting Single Port (Route Injection) Test")
         dut_obj = rx_ports[0]['duthost']
         table_name = "AI_ACL_TABLE"
         try:
             snappi_api.set_config(snappi_config)
             start_stop(snappi_api, operation="start", op_type="protocols")
-            # start_stop(snappi_api, operation="start", op_type="traffic")
             logger.info('\n')
             logger.info("Configuring ACL for packet drop on one of the BGP peer")
-            destination_ips = ROUTE_RANGES[subnet_type]
-            acl_dict = configure_acl_for_route_withdrawl(destination_ips, table_name)
+            destination_ip_list = [list(item) for item in ROUTE_RANGES[ip_version][0]]
+            acl_dict = configure_acl_for_route_withdrawl(destination_ip_list, table_name)
             dut_obj.command("sudo config acl add table {} l3v6".format(json.dumps(acl_dict)))
             logger.info("sudo config acl add table {} l3v6".format(table_name))
             cmd = "sudo config acl add table {} L3v6 -p {} -s egress".format(table_name, rx_ports[1]['peer_port'])
@@ -264,14 +247,7 @@ def get_convergence_for_single_session_flap(
             pytest_assert(int(delta_frames) != 0, "Delta Frames is 0 after removing applied acl and route Injection,\
                         which means no packet drop occurred")
             logger.info("PACKET LOSS DURATION After Route Injection (ms): {}".format(pkt_loss_duration))
-            snappi_api._ixnetwork.ClearStats()
-            wait(20, "For clear stats")
-            flow_stats = get_stats(snappi_api, "Traffic Item Statistics")
-            pytest_assert(
-                int(flow_stats[0].loss) == 0,
-                "Tx Rx Rates are not equal after route Injection",
-            )
-            logger.info('Total Tx and Rx Rates are equal after route injection')
+            pytest_assert(is_traffic_converged(snappi_api), "Traffic did not converge after route injection")
             dut_obj.command("sudo config acl remove table {}".format(table_name))
             logger.info('\n')
             logger.info('--------------------------   Convergence Numbers   ----------------------------------')
@@ -283,10 +259,10 @@ def get_convergence_for_single_session_flap(
             # Create metrics
             test_labels = {
                 METRIC_LABEL_TEST_PARAMS_EVENT_TYPE: event_type,
-                METRIC_LABEL_TEST_PARAMS_ROUTE_SCALE: ROUTE_RANGES[subnet_type][0][0][-1],
+                METRIC_LABEL_TEST_PARAMS_ROUTE_SCALE: ROUTE_RANGES[ip_version][0][0][-1],
                 METRIC_LABEL_TG_TRAFFIC_RATE: snappi_extra_params.traffic_flow_config[0]['line_rate'],
                 METRIC_LABEL_TG_FRAME_BYTES: snappi_extra_params.traffic_flow_config[0]['frame_size'],
-                METRIC_LABEL_TG_IP_VERSION: subnet_type,
+                METRIC_LABEL_TG_IP_VERSION: ip_version,
             }
             convergence_dataplane_time.record(pkt_loss_duration, test_labels)
             db_reporter.report()
