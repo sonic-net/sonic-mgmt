b4dbd89a7fb7ee73ced030ec7d3a3a8f1e958c38
Sarabjeet Singh [Marvell] <sarabjeets@marvell.com>
2026-02-19 05:50:24 +0530

    read_mac (#18455)
    Signed-off-by: sarabjeets <sarabjeets@marvell.com>
    Signed-off-by: Mihut Aronovici <aronovic@cisco.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -164,6 +164,13 @@ def pytest_addoption(parser):
     # FWUtil options
     parser.addoption('--fw-pkg', action='store', help='Firmware package file')
 
+    # read_mac options
+    parser.addoption('--image1', action='store', type=str, help='1st image to download and install')
+    parser.addoption('--image2', action='store', type=str, help='2nd image to download and install')
+    parser.addoption('--iteration', action='store', type=int, help='Number of image installing iterations')
+    parser.addoption('--minigraph1', action='store', type=str, help='path to the minigraph1')
+    parser.addoption('--minigraph2', action='store', type=str, help='path to the minigraph2')
+
     #####################################
     # dash, vxlan, route shared options #
     #####################################
