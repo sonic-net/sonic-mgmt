07edc8f77e50539bcc4fc0fd4dd70d0c88d78711
Mihut Aronovici <aronovic@cisco.com>
2026-02-19 14:09:07 -0800

    fix PR comments

--- tests/ha/ha_dash_flow_utils.py
+++ tests/ha/ha_dash_flow_utils.py
@@ -4,7 +4,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def parse_flow_data(output):
+def get_flow_array(flow_table):
+    flow_array = []
+    for records in flow_table.values():
+        flow_array.extend(records)
+    return flow_array
+
+
+def parse_pdsctl_show_flow_output(output):
     keys = [
         "Session", "LookupId", "Dir", "SIP", "DIP",
         "Proto", "Sport", "Dport", "Role", "Action"
@@ -30,44 +37,33 @@ def parse_flow_data(output):
     if current_table_id is not None and current_table_data:
         flow_tables[current_table_id] = current_table_data
 
-    return flow_tables
+    flow_array = get_flow_array(flow_tables)
+    sorted_flow_array = sorted(flow_array, key=lambda x: int(x['Session']))
+
+    return sorted_flow_array
 
 
-def get_flow_array(flow_table):
-    flow_array = []
-    for records in flow_table.values():
-        flow_array.extend(records)
-    return flow_array
-
-
-def compare_flow_tables(dpuhost1, dpuhost2):
+def compare_flow_tables_pdsctl(dpuhost1, dpuhost2):
     if 'pensando' not in dpuhost1.facts['asic_type'] or 'pensando' not in dpuhost2.facts['asic_type']:
         logger.warning("Only Pensando is supported for this function")
         return None
 
     output1 = dpuhost1.shell("pdsctl show flow")["stdout"]
     output2 = dpuhost2.shell("pdsctl show flow")["stdout"]
-    flow_table1 = parse_flow_data(output1)
-
+    flow_table1 = parse_pdsctl_show_flow_output(output1)
     if (flow_table1 is None):
         logger.warning(f" flows table for {dpuhost1.hostname} is empty")
         return False
 
-    flow_table2 = parse_flow_data(output2)
+    flow_table2 = parse_pdsctl_show_flow_output(output2)
     if (flow_table2 is None):
         logger.warning(f" flows table for {dpuhost2.hostname} is empty")
         return False
 
-    flow_array1 = get_flow_array(flow_table1)
-    flow_array2 = get_flow_array(flow_table2)
+    logger.debug(f"flows on primary: {flow_table1}")
+    logger.debug(f"flows on standby: {flow_table2}")
 
-    sorted_flow_array1 = sorted(flow_array1, key=lambda x: int(x['Session']))
-    sorted_flow_array2 = sorted(flow_array2, key=lambda x: int(x['Session']))
-
-    logger.debug(f"flows on primary: {sorted_flow_array1}")
-    logger.debug(f"flows on standby: {sorted_flow_array2}")
-
-    if sorted_flow_array1 == sorted_flow_array2:
+    if flow_table1 == flow_table2:
         logger.info(f" flows for {dpuhost1.hostname} and {dpuhost2.hostname} are identical")
         return True
     else:
