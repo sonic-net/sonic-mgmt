adfa0636ff06c3fa3e76111b9d344501d16b0e96
Vasundhara Volam <vvolam@microsoft.com>
2026-02-25 02:16:28 +0000

    Remove unused import init_testbed_sonichosts in testbed_health_check.py
    Signed-off-by: Vasundhara Volam <vvolam@microsoft.com>

--- .azure-pipelines/testbed_health_check.py
+++ .azure-pipelines/testbed_health_check.py
@@ -27,7 +27,7 @@ ansible_path = os.path.realpath(os.path.join(_self_dir, "../ansible"))
 if ansible_path not in sys.path:
     sys.path.append(ansible_path)
 
-from devutil.devices.factory import init_host, init_localhost, init_testbed_sonichosts, init_sonichosts  # noqa: E402
+from devutil.devices.factory import init_host, init_localhost, init_sonichosts  # noqa: E402
 from devutil.devices.ansible_hosts import HostsUnreachable, RunAnsibleModuleFailed  # noqa: E402
 
 logger = logging.getLogger(__name__)
