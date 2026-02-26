9d7285a99d10159be016cd543ca2c8e7c461a80a
Ravali Yeluri (WIPRO LIMITED) <v-ryeluri@microsoft.com>
2026-02-25 22:37:17 -0800

    Removing test_gnoi_killprocess_signal_types function

--- tests/gnmi/test_gnoi_killprocess.py
+++ tests/gnmi/test_gnoi_killprocess.py
@@ -142,4 +142,3 @@ def test_invalid_signal(duthosts, rand_one_dut_hostname, ptf_gnoi):
         duthost.critical_services_fully_started,
         "System unhealthy after gNOI API request",
     )
-    
