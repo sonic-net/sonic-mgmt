28df5493472840005759f1750b62e173c78fb35e
Xichen96 <lukelin0907@gmail.com>
2026-02-28 08:42:40 +1100

    merge dhcp_relay_utils in 2 locations (#22538)
    What is the motivation for this PR
    Having similar functions in two locations is messy.
    
    How did you do it
    Moved the dhcp_relay_utils functions into a single location.
    
    How did you verify/test it
    Ran the test suite.
    
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>
    (cherry picked from commit 0b63f4df19e1e4cd7ee1129748e40e6b526a5636)
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>

--- tests/common/dhcp_relay_utils.py
+++ tests/common/dhcp_relay_utils.py
@@ -1,9 +1,11 @@
 import re
+import ipaddress
+import time
+import json
+import logging
 import pytest
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import wait_until
-import json
-import logging
 import ptf.packet as scapy
 
 
@@ -476,3 +478,61 @@ def sonic_dhcp_relay_unconfig(duthost, dut_dhcp_relay_data):
         for dhcp_relay in dut_dhcp_relay_data:
             vlan = str(dhcp_relay['downlink_vlan_iface']['name'])
             duthost.shell(f'config dhcpv4_relay del {vlan}', module_ignore_errors=True)
+
+
+def check_routes_to_dhcp_server(duthost, dut_dhcp_relay_data):
+    """Validate there is route on DUT to each DHCP server
+    """
+    output = duthost.shell("show ip bgp sum", module_ignore_errors=True)
+    logger.info("bgp state: {}".format(output["stdout"]))
+    output = duthost.shell("show int po", module_ignore_errors=True)
+    logger.info("portchannel state: {}".format(output["stdout"]))
+    default_gw_ip = dut_dhcp_relay_data[0]['default_gw_ip']
+    dhcp_servers = set()
+    for dhcp_relay in dut_dhcp_relay_data:
+        dhcp_servers |= set(dhcp_relay['downlink_vlan_iface']['dhcp_server_addrs'])
+
+    for dhcp_server in dhcp_servers:
+        rtInfo = duthost.get_ip_route_info(ipaddress.ip_address(dhcp_server))
+        nexthops = rtInfo["nexthops"]
+        if len(nexthops) == 0:
+            logger.info("Failed to find route to DHCP server '{0}'".format(dhcp_server))
+            return False
+        if len(nexthops) == 1:
+            # if only 1 route to dst available - check that it's not default route via MGMT iface
+            route_index_in_list = 0
+            ip_dst_index = 0
+            route_dst_ip = nexthops[route_index_in_list][ip_dst_index]
+            if default_gw_ip and route_dst_ip == ipaddress.ip_address(default_gw_ip):
+                logger.info("Found route to DHCP server via default GW(MGMT interface)")
+                return False
+    return True
+
+
+def check_dhcp_stress_status(duthost, test_duration_seconds):
+    # Monitor DHCP status during the test
+    start_time = time.time()
+    sleep_time = 30
+    while time.time() - start_time < test_duration_seconds - sleep_time:
+        # Check the status of the DHCP container
+        dhcp_container_status = duthost.shell('docker ps | grep dhcp_relay')["stdout"]
+        if dhcp_container_status == "":
+            assert False, "DHCP container is NOT running."
+
+        # Check CPU usage of the DHCP process
+        dhcp_cpu_usage = duthost.shell('show processes cpu --verbose | grep dhc | awk \'{print $9}\'')["stdout"]
+        if dhcp_cpu_usage:
+            dhcp_cpu_usage_lines = dhcp_cpu_usage.splitlines()
+            for cpu_usage in dhcp_cpu_usage_lines:
+                cpu_usage_float = float(cpu_usage)
+            assert cpu_usage_float < 50.0, "DHCP CPU usage is too high: {}%".format(cpu_usage_float)
+
+        # Check the status of multiple DHCP processes inside the container
+        dhcp_process_status = duthost.shell(
+             'docker exec dhcp_relay supervisorctl status | grep dhcp | grep -v dhcp6')["stdout"]
+        if dhcp_process_status:
+            dhcp_process_status_lines = dhcp_process_status.splitlines()
+            for dhcp_process_status_line in dhcp_process_status_lines:
+                process_name, process_status = dhcp_process_status_line.split()[0], dhcp_process_status_line.split()[1],
+                assert process_status == "RUNNING", "{} is not running!".format(process_name)
+    time.sleep(sleep_time)
