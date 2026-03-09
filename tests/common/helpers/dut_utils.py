c4f27ceea2356f7779b0293fa22319ccdefbabca
Dashuai Zhang <164845223+sdszhang@users.noreply.github.com>
2026-03-09 12:41:24 +1100

    add authtype for consoleserver (#22679)
    Summary:
    support specifying authentication credential choice for console server (e.g. TACACS or AAD),
    
    With the following device:
    
    Hostname,ManagementIp,HwSku,Type,Protocol,Os,AuthType
    console-1,192.168.10.1/23,Cisco,ConsoleServer,ssh,sonic,,
    console-2,192.168.10.2/23,Cisco,ConsoleServer,ssh,sonic,,tacacs
    console-1 will use the default credentials in secrets.yaml "console_login".
    console-2 will use more specific credentials in secrets.yaml "console_login_options['tacacs']"
    
    What is the motivation for this PR?
    user can use different credential for console server.
    
    How did you do it?
    introduced new console_type, CONSOLE_SSH_TACACS
    
    How did you verify/test it?
    Verified locally.
    
    Signed-off-by: Dashuai Zhang <dashuaizhang@microsoft.com>
    Signed-off-by: ssithaia-ebay <ssithaian@ebay.com>

--- tests/common/helpers/dut_utils.py
+++ tests/common/helpers/dut_utils.py
@@ -13,7 +13,7 @@ from tests.common.errors import RunAnsibleModuleFail
 from collections import defaultdict
 from tests.common.connections.console_host import ConsoleHost, CONSOLE_LINECARD
 from tests.common.connections.linecard_console_conn import UnsupportedPlatformError
-from tests.common.utilities import get_dut_current_passwd
+from tests.common.utilities import get_dut_current_passwd, update_console_creds
 from tests.common.connections.base_console_conn import (
     CONSOLE_SSH_CISCO_CONFIG,
     CONSOLE_SSH_DIGI_CONFIG,
@@ -559,11 +559,13 @@ def create_duthost_console(duthost, localhost, conn_graph_facts, creds):  # noqa
         console_host = console_host.split("/")[0]
     console_port = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['peerport']
     console_type = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['type']
+    console_auth_type = conn_graph_facts['device_console_info'][dut_hostname].get('AuthType', "")
     console_menu_type = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['menu_type']
     console_username = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['proxy']
     console_device = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['peerdevice']
 
     console_type = f"console_{console_type}"
+    update_console_creds(creds, console_auth_type)
 
     if console_menu_type and console_menu_type.lower() != "n/a":
         console_menu_type = f"{console_type}_{console_menu_type}"
@@ -655,7 +657,10 @@ def creds_on_dut(duthost):
     for cred_var in cred_vars:
         if cred_var in creds:
             creds[cred_var] = jinja2.Template(creds[cred_var]).render(**hostvars)
-    # load creds for console
+
+    creds["console_login_options"] = hostvars.get("console_login_options", {})
+
+    # load default creds for console
     if "console_login" not in list(hostvars.keys()):
         console_login_creds = {}
     else:
