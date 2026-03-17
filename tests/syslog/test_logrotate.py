641e3c4f34649bcdc1f2d58ff2bd5d8547410393
Minbo Fan: mfan-upscale <mfan@upscaleai.com>
2026-03-17 13:37:54 -0700

    Fix issue #23051 test_logrotate_normal_size handle corner case file number reach limit
    Signed-off-by: Minbo Fan: mfan-upscale <mfan@upscaleai.com>

--- tests/syslog/test_logrotate.py
+++ tests/syslog/test_logrotate.py
@@ -19,7 +19,7 @@ LOG_FOLDER = '/var/log'
 SMALL_VAR_LOG_PARTITION_SIZE = '300M'
 FAKE_IP = '10.20.30.40'
 FAKE_MAC = 'aa:bb:cc:dd:11:22'
-
+FILE_NUM_FOR_VALIDATE = 10
 
 @pytest.fixture(scope='module', autouse=True)
 def disable_logrotate_cron_job(rand_selected_dut):
@@ -113,6 +113,26 @@ def create_temp_syslog_file(duthost, size):
     logger.info('Create a temp syslog file as {}'.format(size))
     duthost.shell('sudo fallocate -l {} /var/log/syslog'.format(size))
 
+def get_tail_syslog_files(duthost, num):
+    files = duthost.shell('sudo ls -lt --full-time /var/log/syslog.* | tail -n {}'.format(num),
+                        module_ignore_errors=True)['stdout']
+    logger.debug(files)
+    return files
+
+def files_time_shifted(before, after):
+    fl1 = before.splitlines()
+    fl2 = after.splitlines()
+    # get syslog files timestamp, when the file number limit is reached.
+    # after log rotation, the total log file numner does not change, but
+    # timestamp of those files should be shifted one position.
+    time_fl1 = [item.split()[6] for item in fl1]
+    time_fl2 = [item.split()[6] for item in fl2]
+    logger.debug("=========== Syslog file imestamps before rotate =======")
+    logger.debug(time_fl1)
+    logger.debug("=========== Syslog file timestamps after rotate =======")
+    logger.debug(time_fl2)
+    # Return true if sure oldest syslog files timestamp shifted
+    return time_fl1[:-1] == time_fl2[1:]
 
 def run_logrotate(duthost, force=False):
     """
@@ -140,7 +160,7 @@ def multiply_with_unit(logrotate_threshold, num):
     return str(int(logrotate_threshold[:-1]) * num) + logrotate_threshold[-1]
 
 
-def validate_logrotate_function(duthost, logrotate_threshold, small_size):
+def validate_logrotate_function(duthost, logrotate_threshold, small_size, num_of_file=10):
     """
     Validate logrotate function
     :param duthost: DUT host object
@@ -165,13 +185,22 @@ def validate_logrotate_function(duthost, logrotate_threshold, small_size):
 
     with allure.step('There will be logrotate process when rsyslog size is larger than threshold {}'.format(
             logrotate_threshold)):
+        # When the total number of syslog file smaller than num_of_file to list, set to smaller value
+        if syslog_number_origin < num_of_file:
+            num_of_file = syslog_number_origin
+        tail_files_before_rotate = get_tail_syslog_files(duthost, num_of_file)
         create_temp_syslog_file(duthost, multiply_with_unit(logrotate_threshold, 1.1))
         run_logrotate(duthost)
         syslog_number_with_rotate = get_syslog_file_count(duthost)
         logger.info('There are {} syslog gz files after running logrotate'.format(syslog_number_with_rotate))
-        assert syslog_number_origin + 1 == syslog_number_with_rotate, \
-            'No logrotate happens, there should be one time logrotate executed'
-
+        # Handle the corner case when number of syslog files reach its configured limit
+        if syslog_number_origin  == syslog_number_with_rotate:
+            tail_files_after_rotate = get_tail_syslog_files(duthost, num_of_file)
+            assert files_time_shifted(tail_files_before_rotate, tail_files_after_rotate) == 1, \
+            	'No logrotate happens, both syslog file number and timestamp are the same'
+        else:
+            assert syslog_number_origin + 1 == syslog_number_with_rotate, \
+            	'No logrotate happens, there should be one time logrotate executed'
 
 def get_threshold_based_on_memory(duthost):
     """
@@ -210,7 +239,7 @@ def test_logrotate_normal_size(rand_selected_dut):
         if get_var_log_size(duthost) < 200 * 1024:
             pytest.skip('{} size is lower than 200MB, skip this test'.format(LOG_FOLDER))
     rotate_large_threshold = get_threshold_based_on_memory(duthost)
-    validate_logrotate_function(duthost, rotate_large_threshold, False)
+    validate_logrotate_function(duthost, rotate_large_threshold, False, FILE_NUM_FOR_VALIDATE)
 
 
 @pytest.mark.disable_loganalyzer
@@ -233,7 +262,7 @@ def test_logrotate_small_size(rand_selected_dut, simulate_small_var_log_partitio
     """
     duthost = rand_selected_dut
     rotate_small_threshold = get_threshold_based_on_memory(duthost)
-    validate_logrotate_function(duthost, rotate_small_threshold, True)
+    validate_logrotate_function(duthost, rotate_small_threshold, True, FILE_NUM_FOR_VALIDATE)
 
 
 def get_pending_entries(duthost, ignore_list=None):
