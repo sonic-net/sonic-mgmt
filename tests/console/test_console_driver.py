9530a5e3051b68bf771254a695d3d8d8ff154602
Xichen96 <lukelin0907@gmail.com>
2026-03-25 11:32:37 +1100

    [console] add console test for c0 c0-lo (#22209)
    What is the motivation for this PR?
    Add tests for console devices with c0 and c0-lo topo
    
    How did you do it?
    Add test
    
    How did you verify/test it?
    Run tests on device
    
    Signed-off-by: Xichen Lin <lukelin0907@gmail.com>
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/console/test_console_driver.py
+++ tests/console/test_console_driver.py
@@ -3,22 +3,21 @@ import pytest
 from tests.common.helpers.assertions import pytest_assert
 
 pytestmark = [
-    pytest.mark.topology('any')
+    pytest.mark.topology('c0', 'c0-lo')
 ]
 
 
-def test_console_driver(duthost):
+def test_console_driver(duthost, tbinfo, setup_c0):
     """
     Test console driver are well installed.
     Verify ttyUSB(0-47) are presented in DUT
+    Both c0 and c0-lo have 48 console lines
     """
-    out = duthost.shell('ls /dev/ttyUSB*', module_ignore_errors=True)['stdout']
-    ttys = set(out.split())
-    pytest_assert(len(ttys) > 0, "No virtual tty devices been created by console driver")
+    topo_console_intfs = tbinfo["topo"]["properties"]["topology"]["console_interfaces"]
 
-    out = list(duthost.console_facts()["ansible_facts"]["console_facts"]["lines"].keys())
-    for i in range(0, len(out)):
-        expected_virtual_tty = "/dev/ttyUSB{}".format(i)
-        pytest_assert(
-            expected_virtual_tty in ttys,
-            "Expected virtual tty device [{}] not found.".format(expected_virtual_tty))
+    ls_out = duthost.shell('ls {}*'.format(duthost._get_serial_device_prefix()), module_ignore_errors=True)['stdout']
+    num_ttys = len(ls_out.split())
+    pytest_assert(num_ttys > 0, "No virtual tty devices been created by console driver")
+    pytest_assert(num_ttys >= len(topo_console_intfs),
+                  "Number of virtual tty devices [{}] is less than expected [{}]"
+                  .format(num_ttys, len(topo_console_intfs)))
