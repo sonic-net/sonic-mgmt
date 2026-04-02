5695c039af0d3cbce67dc3b21c49cabeb2e3ad13
Mihut Aronovici <aronovic@cisco.com>
2026-04-02 12:42:50 -0700

    updated based on latest commits

--- tests/ha/ha_dpu_utils.py
+++ tests/ha/ha_dpu_utils.py
@@ -1,37 +1,41 @@
 import logging
-from tests.common.platform.device_utils import platform_api_conn  # noqa: F401,F403
-from tests.common.helpers.platform_api import module
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import wait_until
 
 PING_MAX_TIMEOUT = 180
 PING_MAX_TIME_INT = 60
+midplane_prefix = "169.254.200."
+
+logger = logging.getLogger(__name__)
 
 
-def dpu_power_on_index(duthost, platform_api_conn, dpu_index):    # noqa F811
+
+def dpu_power_on_for_index(duthost, dpu_index):    # noqa F811
     """
     Executes power on for a specific DPU
     """
+    try:
+        duthost.shell(f"sudo config chassis module startup DPU{dpu_index}")
+    except Exception as e:
+        logger.error(f"Error powering on dpu{dpu_index}: {e}")
 
-    dpu = module.get_name(platform_api_conn, dpu_index)
-    ip_address = module.get_midplane_ip(platform_api_conn, dpu_index)
-    duthost.shell("sudo config chassis modules startup %s" % (dpu))
-
+    ip_address = f"{midplane_prefix}{dpu_index + 1}"
     pytest_assert(wait_until(PING_MAX_TIMEOUT, PING_MAX_TIME_INT, 0,
                   check_dpu_ping_status,
                   duthost, ip_address),
                   "DPU is not operationally UP")
 
 
-def dpu_power_off_index(duthost, platform_api_conn, dpu_index):    # noqa F811
+def dpu_power_off_for_index(duthost, dpu_index):    # noqa F811
     """
     Executes power off a specific DPU
     """
+    try:
+        duthost.shell(f"sudo config chassis module shutdown DPU{dpu_index}")
+    except Exception as e:
+        logger.error(f"Error powering off dpu{dpu_index}: {e}")
 
-    dpu = module.get_name(platform_api_conn, dpu_index)
-    ip_address = module.get_midplane_ip(platform_api_conn, dpu_index)
-    duthost.shell("sudo config chassis modules shutdown %s" % (dpu))
-
+    ip_address = f"{midplane_prefix}{dpu_index + 1}"
     pytest_assert(wait_until(PING_MAX_TIMEOUT, PING_MAX_TIME_INT, 0,
                   check_dpu_not_pingable,
                   duthost, ip_address),
@@ -47,13 +51,16 @@ def check_dpu_ping_status(duthost, ip_address):
     Returns:
         Returns True or False based on Ping is successfull or not
     """
-
-    output_ping = duthost.command("ping -c 3 %s" % (ip_address))
-    logging.info("Ping output: '{}'".format(output_ping))
-    if "0% packet loss" in output_ping["stdout"]:
-        return True
-    else:
-        logging.error("Ping failed for '{}'".format(ip_address))
+    try:
+        output_ping = duthost.command("ping -c 3 %s" % (ip_address))
+        logging.info("Ping output: '{}'".format(output_ping))
+        if "0% packet loss" in output_ping["stdout"]:
+            return True
+        else:
+            logging.warn("Ping failed for '{}'".format(ip_address))
+            return False
+    except Exception as e:
+        logging.info(f"Ping output exception {e}")
         return False
 
 
@@ -66,11 +73,15 @@ def check_dpu_not_pingable(duthost, ip_address):
     Returns:
         Returns True or False if Ping failed or not
     """
-    output_ping = duthost.command("ping -c 3 %s" % (ip_address), module_ignore_errors=True)
-    logging.info("Ping output: '{}'".format(output_ping))
-    if "100% packet loss" in output_ping["stdout"]:
-        logging.info("Ping is not working for '{}'".format(ip_address))
+    try:
+        output_ping = duthost.command("ping -c 3 %s" % (ip_address), module_ignore_errors=True)
+        logging.info("Ping output: '{}'".format(output_ping))
+        if "100% packet loss" in output_ping["stdout"]:
+            logging.info("Ping is not working for '{}'".format(ip_address))
+            return True
+        else:
+            logging.error("Ping still work for '{}'".format(ip_address))
+            return False
+    except Exception as e:
+        logging.info(f"Ping output exception {e}")
         return True
-    else:
-        logging.error("Ping still work for '{}'".format(ip_address))
-        return False
--- tests/ha/test_ha_dpu_power_down.py
+++ tests/ha/test_ha_dpu_power_down.py
@@ -8,21 +8,28 @@ import threading
 import queue
 from constants import (
     LOCAL_PTF_INTF,
-    REMOTE_PTF_RECV_INTF,
-    REMOTE_PTF_SEND_INTF
+    REMOTE_PTF_RECV_INTF
 )
 from gnmi_utils import apply_messages
-from packets import outbound_pl_packets, inbound_pl_packets
+from packets import outbound_pl_packets
 from tests.common.config_reload import config_reload
-from tests.common.platform.device_utils import platform_api_conn
-from ha_dpu_utils import dpu_power_off_index, dpu_power_on_index
+from ha_dpu_utils import dpu_power_off_for_index, dpu_power_on_for_index
 
 logger = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.topology('t1-smartswitch-ha'),
-    pytest.mark.skip_check_dut_health
+    pytest.mark.topology('t1-smartswitch-ha')
 ]
+
+
+THRESHOLD_LOSS_PERCENT = 2.0
+RATE_PPS = 20
+INITIAL_SEND_COUNT = 100
+
+
+def reload_config_for_host(dpuhost):
+    logger.info(f"config reload on {dpuhost.hostname}")
+    config_reload(dpuhost, safe_reload=True, yang_validate=False)
 
 
 @pytest.fixture(autouse=True, scope="function")
@@ -85,7 +92,11 @@ def common_setup_teardown(
         apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpuhost.dpu_index)
 
     yield
-    config_reload(dpuhost, safe_reload=True, yang_validate=False)
+    '''
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(dpuhosts)) as executor:
+        # Map the reload_config_for_host function to the dpuhosts list
+        executor.map(reload_config_for_host, dpuhosts)
+    '''
 
 
 """
@@ -94,8 +105,8 @@ We are testing 4 scenarios:
     2. Traffic to Primary and Standby DPU failure
     3. Traffic to Standby and Primary DPU Failure
     4. Traffic to Standby and Standby DPU Failure
-    When DPU failure is on standby not traffic loss should be observed
-    When DPU failure is on primary less than 1% traffic loss should be observed
+    DPU failure on standby: no traffic loss expected
+    DPU failure on primary: less than a specified percentage traffic loss expected
 """
 
 
@@ -113,24 +124,20 @@ def test_ha_dpu_failure(
     dpuhosts,
     activate_dash_ha_from_json,
     dash_pl_config,
-    standby_link_fail,
     standby_dpu_fail,
     traffic_to_standby
 ):
     encap_proto = "vxlan"
-    rate_pps = 20
-    initial_send_count = 100
+    rate_pps = RATE_PPS
+    initial_send_count = INITIAL_SEND_COUNT
     delay = 1.0 / rate_pps
     rcv_outbound_pl_ports = dash_pl_config[0][REMOTE_PTF_RECV_INTF] + dash_pl_config[1][REMOTE_PTF_RECV_INTF]
 
     if traffic_to_standby:
         vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config[1], encap_proto)
-        pe_to_dpu_pkt, exp_dpu_to_vm_pkt = inbound_pl_packets(dash_pl_config[1])
     else:
         vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config[0], encap_proto)
-        pe_to_dpu_pkt, exp_dpu_to_vm_pkt = inbound_pl_packets(dash_pl_config[0])
 
-    _, exp_dpu_to_vm_pkt_standby = inbound_pl_packets(dash_pl_config[1])
     packet_sending_flag = queue.Queue(1)
 
     send_count = 0
@@ -142,10 +149,10 @@ def test_ha_dpu_failure(
             time.sleep(0.2)
         if standby_dpu_fail:
             logger.info(f"Simulate standby DPU failure, pkt sent {send_count}")
-            dpu_power_off_index(duthosts[1], platform_api_conn, 0)
+            dpu_power_off_for_index(duthosts[1], 0)
         else:
             logger.info(f"Simulate primary DPU failure, pkt sent {send_count}")
-            dpu_power_off_index(duthosts[0], platform_api_conn, 0)
+            dpu_power_off_for_index(duthosts[0], 0)
         logger.info(f"After DPU failure, pkt sent {send_count}")
 
     t = threading.Thread(target=dpu_ha_action, name="dpu_ha_action_thread")
@@ -154,7 +161,7 @@ def test_ha_dpu_failure(
     reached_max_time = False
     ptfadapter.dataplane.flush()
     time.sleep(1)
-    rcv_inbound_pl_ports = [dash_pl_config[0][LOCAL_PTF_INTF], dash_pl_config[1][LOCAL_PTF_INTF]]
+
     while not reached_max_time:
         # After we send initial_send_count packets, awake link_ha_action thread
         if send_count == initial_send_count:
@@ -164,52 +171,24 @@ def test_ha_dpu_failure(
         try:
             if traffic_to_standby:
                 if send_count == 0:
-                    logger.info("Send first outbound packet to standby")
+                    logger.info("Send first packet to standby")
                 testutils.send(ptfadapter, dash_pl_config[1][LOCAL_PTF_INTF], vm_to_dpu_pkt, 1)
                 testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_pe_pkt, rcv_outbound_pl_ports)
                 if send_count == 0:
-                    logger.info("First outbound packet received")
-                    logger.info("Send first inbound packet to standby")
-                try:
-                    testutils.send(ptfadapter, dash_pl_config[1][REMOTE_PTF_SEND_INTF], pe_to_dpu_pkt, 1)
-                    if failed_count > 0:
-                        testutils.verify_packet(ptfadapter, exp_dpu_to_vm_pkt_standby,
-                                                dash_pl_config[1][LOCAL_PTF_INTF])
-                    else:
-                        testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_vm_pkt, rcv_inbound_pl_ports)
-                except Exception as e:
-                    if failed_count < 3:
-                        logger.info(f"inbound pkt dropped: {e}")
-                    failed_count += 1
-                if send_count == 0:
-                    logger.info("First packets verified to standby")
+                    logger.info("First packet received")
             else:
                 if send_count == 0:
-                    logger.info("Send first outbound packet to primary")
+                    logger.info("Send first packet to primary")
                 testutils.send(ptfadapter, dash_pl_config[0][LOCAL_PTF_INTF], vm_to_dpu_pkt, 1)
                 testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_pe_pkt, rcv_outbound_pl_ports)
                 if send_count == 0:
                     logger.info("First outbound packet received")
-                    logger.info("Send first inbound packet to primary")
-                try:
-                    testutils.send(ptfadapter, dash_pl_config[0][REMOTE_PTF_SEND_INTF], pe_to_dpu_pkt, 1)
-                    if failed_count > 0:
-                        testutils.verify_packet(ptfadapter, exp_dpu_to_vm_pkt_standby,
-                                                dash_pl_config[1][LOCAL_PTF_INTF])
-                    else:
-                        testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_vm_pkt, rcv_inbound_pl_ports)
-                    if send_count == 0:
-                        logger.info("First inbound packet received")
-                except Exception as e:
-                    if failed_count < 3:
-                        logger.info(f"inbound pkt dropped: {e}")
-                    failed_count += 1
         except Exception as e:
             if failed_count == 0:
-                logger.info(f"pkt dropped after {send_count} pkts, exception {e}")
+                logger.info(f"pkt dropped after {send_count} pkts")
                 if send_count == 0:
-                    logger.error(f"pkt dropped exception {e}")
-                    pytest.fail("HA link fail test error: no packets were received")
+                    logger.error(f"first pkt dropped exception {e}")
+                    pytest.fail("HA DPU reboot test error: no packets received")
             failed_count += 1
 
         send_count += 1
@@ -218,11 +197,11 @@ def test_ha_dpu_failure(
 
     t.join()
     time.sleep(2)
-    breakpoint()
+    # bring back up the DPU
     if standby_dpu_fail:
-        dpu_power_on_index(duthosts[1], platform_api_conn, 0)
+        dpu_power_on_for_index(duthosts[1], 0)
     else:
-        dpu_power_on_index(duthosts[0], platform_api_conn, 0)
+        dpu_power_on_for_index(duthosts[0], 0)
 
     traffic = "traffic to standby" if traffic_to_standby else "traffic to primary"
     if standby_dpu_fail:
@@ -232,7 +211,7 @@ def test_ha_dpu_failure(
         else:
             logger.info(f"Standby DPU fail with {traffic} test OK. All {send_count} packets sent were received.")
     else:
-        threshold_loss = 1.0
+        threshold_loss = THRESHOLD_LOSS_PERCENT
         percentage_loss = (failed_count / send_count) * 100
         if (percentage_loss < threshold_loss):
             logger.info(f"Primary DPU fail with {traffic} test OK. Sent: {send_count},"
