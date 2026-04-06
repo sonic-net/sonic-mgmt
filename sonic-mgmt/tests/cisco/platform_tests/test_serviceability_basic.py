adb62ecb59c3a96c90cbe52ad3ae12d164038fae
Malavika Unnikrishnan (maunnikr) <maunnikr@cisco.com>
2026-04-06 13:59:00 -0700

    MIGSOFTWAR-23583: Removing skip for udump CLI UT (#2367)

--- sonic-mgmt/tests/cisco/platform_tests/test_serviceability_basic.py
+++ sonic-mgmt/tests/cisco/platform_tests/test_serviceability_basic.py
@@ -250,7 +250,6 @@ def test_show_platform_npu_user_mode_cli(duthosts, enum_rand_one_per_hwsku_hostn
     assert not result_list, "One or more show platform npu commands failed {}".format(result_list)
 
 
-@pytest.mark.skip(reason="Disabled: broken testcase: MIGSOFTWAR-35140")
 def test_show_platform_npu_udump(duthosts, enum_rand_one_per_hwsku_hostname, enum_rand_one_asic_index):
     """
     @summary: Verify output of 'sudo udump --force -t <sw, full>'
