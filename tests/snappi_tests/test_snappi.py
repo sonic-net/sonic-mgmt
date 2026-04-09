e17d31c9a7b14eea6a859ff65de511065fcc3abe
xiongxc <xuecheng.xiong@viavisolutions.com>
2026-04-09 09:24:57 +0800

    [snappi] Import is_pfc_enabled fixture in test_snappi.py
    Add is_pfc_enabled to the imports from snappi_fixtures so it is
    available for use in test_snappi.py.
    
    Signed-off-by: xiongxc <xuecheng.xiong@viavisolutions.com>

--- tests/snappi_tests/test_snappi.py
+++ tests/snappi_tests/test_snappi.py
@@ -5,7 +5,7 @@ from tests.common.helpers.assertions import pytest_assert, pytest_require
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
     fanout_graph_facts                  # noqa: F401
 from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port,\
-    snappi_api, snappi_testbed_config   # noqa: F401
+    snappi_api, snappi_testbed_config, is_pfc_enabled  # noqa: F401
 from tests.common.snappi_tests.snappi_helpers import wait_for_arp
 from tests.common.snappi_tests.port import select_ports
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map  # noqa: F401
