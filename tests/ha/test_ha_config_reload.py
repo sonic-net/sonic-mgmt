ae4ca914ab0494c73ef202b08af1cc277b3a67c9
aronovic <166534786+aronovic@users.noreply.github.com>
2026-04-09 00:43:19 -0400

    [HA] [smartswitch] implement module 8 of the HA testplan (#23220)
    Added the tests from HA test plan module 8:
    1. Shutdown/Startup BGP sessions from NPU
    2. Config reload on T1
    These tests are parametrized based on the traffic being sent to Primary
    or Standby and also the operation being done on either Primary or
    Standby.
    
    Signed-off-by: Mihut Aronovici <aronovic@cisco.com>
    Signed-off-by: Raghavendran Ramanathan <rraghav@cisco.com>

---
+++ tests/ha/test_ha_config_reload.py
@@ -0,0 +1,204 @@
+import logging
+
+import configs.privatelink_config as pl
+import ptf.testutils as testutils
+import pytest
+import time
+import threading
+import queue
+from constants import (
+    LOCAL_PTF_INTF,
+    REMOTE_PTF_RECV_INTF
+)
+from gnmi_utils import apply_messages
+from packets import outbound_pl_packets
+from tests.common.config_reload import config_reload
+
+logger = logging.getLogger(__name__)
+
+pytestmark = [
+    pytest.mark.topology('t1-smartswitch-ha'),
+    pytest.mark.skip_check_dut_health
+]
+
+TRAFFIC_LOSS_THRESHOLD_PERCENTAGE = 2.0
+
+
+@pytest.fixture(autouse=True, scope="function")
+def common_setup_teardown(
+    localhost,
+    duthosts,
+    ptfhost,
+    dpu_index,
+    skip_config,
+    dpuhosts,
+    setup_ha_config,
+    setup_dash_ha_from_json,
+    setup_gnmi_server,
+    set_vxlan_udp_sport_range,
+    setup_npu_dpu  # noqa: F811
+):
+    if skip_config:
+        return
+
+    for i in range(len(duthosts)):
+        duthost = duthosts[i]
+        dpuhost = dpuhosts[i]
+        base_config_messages = {
+            **pl.APPLIANCE_CONFIG,
+            **pl.ROUTING_TYPE_PL_CONFIG,
+            **pl.VNET_CONFIG,
+            **pl.ROUTE_GROUP1_CONFIG,
+            **pl.METER_POLICY_V4_CONFIG
+        }
+        logger.info(f"Starting DASH configuration on {duthost.hostname} dpu {dpuhost.dpu_index} "
+                    f"with {base_config_messages}")
+
+        apply_messages(localhost, duthost, ptfhost, base_config_messages, dpuhost.dpu_index)
+
+        route_and_mapping_messages = {
+            **pl.PE_VNET_MAPPING_CONFIG,
+            **pl.PE_SUBNET_ROUTE_CONFIG,
+            **pl.VM_SUBNET_ROUTE_CONFIG
+        }
+
+        if 'bluefield' in dpuhost.facts['asic_type']:
+            route_and_mapping_messages.update({
+                **pl.INBOUND_VNI_ROUTE_RULE_CONFIG
+            })
+
+        logger.info(route_and_mapping_messages)
+        apply_messages(localhost, duthost, ptfhost, route_and_mapping_messages, dpuhost.dpu_index)
+
+        meter_rule_messages = {
+            **pl.METER_RULE1_V4_CONFIG,
+            **pl.METER_RULE2_V4_CONFIG,
+        }
+        logger.info(meter_rule_messages)
+        apply_messages(localhost, duthost, ptfhost, meter_rule_messages, dpuhost.dpu_index)
+
+        logger.info(pl.ENI_CONFIG)
+        apply_messages(localhost, duthost, ptfhost, pl.ENI_CONFIG, dpuhost.dpu_index)
+
+        logger.info(pl.ENI_ROUTE_GROUP1_CONFIG)
+        apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpuhost.dpu_index)
+
+    yield
+
+    config_reload(dpuhost, safe_reload=True, yang_validate=False)
+
+
+"""
+We are testing 4 scenarios:
+    1. Traffic to Primary and Primary DUT reboot
+    2. Traffic to Primary and Standby DUT reboot
+    3. Traffic to Standby and Primary DUT reboot
+    4. Traffic to Standby and Standby DUT reboot
+    When config is realoaded on standby DUT not traffic loss is expected
+    When config is reloaded on primary less than 1% traffic loss is expected
+"""
+
+
+@pytest.mark.parametrize(
+    "standby_config_reload", [True, False],
+    ids=["Standby Config Reload", "Primary Config Reload"]
+)
+@pytest.mark.parametrize(
+    "traffic_to_standby", [True, False],
+    ids=["Standby Traffic", "Primary Traffic"]
+)
+def test_ha_dut_config_reload(
+    localhost,
+    ptfadapter,
+    duthosts,
+    dpuhosts,
+    activate_dash_ha_from_json,
+    dash_pl_config,
+    standby_config_reload,
+    traffic_to_standby
+):
+    encap_proto = "vxlan"
+    rate_pps = 20  # packets per second
+    initial_send_count = 100
+    delay = 1.0 / rate_pps
+    rcv_outbound_pl_ports = dash_pl_config[0][REMOTE_PTF_RECV_INTF] + dash_pl_config[1][REMOTE_PTF_RECV_INTF]
+
+    if traffic_to_standby:
+        vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config[1], encap_proto)
+    else:
+        vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config[0], encap_proto)
+
+    packet_sending_flag = queue.Queue(1)
+
+    send_count = 0
+    failed_count = 0
+
+    def config_reload_ha_action():
+        # wait for packets sending started, then reload the DUT
+        while packet_sending_flag.empty() or (not packet_sending_flag.get()):
+            time.sleep(0.2)
+        if standby_config_reload:
+            logger.info(f"Standby DUT config reload, pkt sent {send_count}")
+            config_reload(dpuhosts[1], yang_validate=False)
+        else:
+            logger.info(f"Primary DUT config reload, pkt sent {send_count}")
+            config_reload(dpuhosts[0], yang_validate=False)
+        logger.info(f"After config reload, pkt sent {send_count}")
+
+    t = threading.Thread(target=config_reload_ha_action, name="config_reload_ha_action_thread")
+    t.start()
+    t_max = time.time() + 60
+    reached_max_time = False
+    ptfadapter.dataplane.flush()
+    time.sleep(1)
+    while not reached_max_time:
+        # After we send initial_send_count packets, awake config_reload_ha_action thread
+        if send_count == initial_send_count:
+            logger.info("Awake config reload HA action thread")
+            packet_sending_flag.put(True)
+
+        try:
+            if traffic_to_standby:
+                if send_count == 0:
+                    logger.info("Send first outbound packet to standby")
+                testutils.send(ptfadapter, dash_pl_config[1][LOCAL_PTF_INTF], vm_to_dpu_pkt, 1)
+                testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_pe_pkt, rcv_outbound_pl_ports)
+                if send_count == 0:
+                    logger.info("First packet verified on standby")
+            else:
+                if send_count == 0:
+                    logger.info("Send first outbound packet to primary")
+                testutils.send(ptfadapter, dash_pl_config[0][LOCAL_PTF_INTF], vm_to_dpu_pkt, 1)
+                testutils.verify_packet_any_port(ptfadapter, exp_dpu_to_pe_pkt, rcv_outbound_pl_ports)
+                if send_count == 0:
+                    logger.info("First packet verified on primary")
+        except Exception as e:
+            if failed_count == 0:
+                logger.info(f"pkt dropped after {send_count} pkts, exception {e}")
+                if send_count == 0:
+                    logger.error(f"pkt dropped exception {e}")
+                    pytest.fail("HA config reload test error: no packets were received")
+            failed_count += 1
+
+        send_count += 1
+        time.sleep(delay)
+        reached_max_time = time.time() > t_max
+
+    t.join()
+    time.sleep(2)
+    traffic = "traffic to standby" if traffic_to_standby else "traffic to primary"
+    if standby_config_reload:
+        if failed_count > 0:
+            pytest.fail(f"Standby config reload with {traffic} test error: sent: {send_count} "
+                        f"lost: {failed_count}.")
+        else:
+            logger.info(f"Standby config reload with {traffic} test OK. All {send_count} packets sent were received.")
+    else:
+        threshold_loss = TRAFFIC_LOSS_THRESHOLD_PERCENTAGE
+        percentage_loss = (failed_count / send_count) * 100
+        if (percentage_loss < threshold_loss):
+            logger.info(f"Primary DUT config reload with {traffic} test OK. Sent: {send_count},"
+                        f" lost: {failed_count}, loss percentage: {percentage_loss}, threshold: {threshold_loss}")
+        else:
+            pytest.fail(f"Primary DUT config reload with {traffic} test error. Sent: {send_count},"
+                        f" lost: {failed_count} loss percentage: {percentage_loss}, threshold: {threshold_loss}")
