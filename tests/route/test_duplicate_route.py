66e5fb4a7850930f43558f74d4b9f6ad7f08ffd3
Illia <37450862+illia-kotvitskyi@users.noreply.github.com>
2026-04-09 11:07:05 +0300

    add expected error log in test_duplicate_routes (#21984)
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/route/test_duplicate_route.py
+++ tests/route/test_duplicate_route.py
@@ -105,6 +105,7 @@ def verify_expected_loganalyzer_logs(
         ".*ERR.* mlnx_create_route_async.* Entry Already Exists.",
         ".*ERR.* object key SAI_OBJECT_TYPE_ROUTE_ENTRY:.* already exists.*",  # TODO move to expectRegex
         ".*ERR.* addRoutePost: Failed to create route.*",  # TODO move to expectRegex
+        r".*ERR pidof\[\d+\]: can't read from \d+\/stat.*",
         ".*ERR.* ipv4_route_bulk_updates API returned.*Key already exists in table.*",
         ".*ERR syncd#SDK:.*mlnx_route_pre_create: Route entry already exists in the Route DB.*",
         ".*ERR syncd#SDK:.*mlnx_route_bulk_set_impl: Failed to prepare route data for bulk operation. index:.*",
