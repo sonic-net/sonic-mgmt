00fbc7af03bf52370bcab17c11fc4b78e82abe61
selldinesh <dinesh.sellappan@keysight.com>
2026-04-20 13:49:58 -0700

    removing logger
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/snappi_tests/bgp/test_bgp_scalability.py
+++ tests/snappi_tests/bgp/test_bgp_scalability.py
@@ -5,8 +5,6 @@ from tests.snappi_tests.bgp.files.bgp_test_gap_helper import run_bgp_scalability
 from tests.common.fixtures.conn_graph_facts import (                        # noqa: F401
     conn_graph_facts, fanout_graph_facts)
 import pytest
-import logging
-logger = logging.getLogger(__name__)
 
 pytestmark = [pytest.mark.topology('tgen')]
 
