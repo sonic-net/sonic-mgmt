42da71832276b2c6245210dc8a7f4db541f6eb54
P Sai Yasaswini <sai-yasaswini.p@hpe.com>
2026-04-21 15:10:20 +0530

    Update port_utils.py
    modifying range of interfaces

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -120,7 +120,7 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
             port_alias_to_name_map["etp65"] = "Ethernet512"
             port_alias_to_name_map["etp66"] = "Ethernet513"
         elif hwsku == "Juniper-QFX5241-64-OD":
-            for i in range(0, 63):
+            for i in range(0, 64):
                 port_alias_to_name_map["et-0/0/%d" % i] = "Ethernet%d" % (i * 8)
             port_alias_to_name_map["et-0/0/64"] = "Ethernet512"
             port_alias_to_name_map["et-0/0/65"] = "Ethernet513"
