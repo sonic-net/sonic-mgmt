03019dea86b182828cbdfd493f15474115b1cbe4
Chuan Wu <103085864+echuawu@users.noreply.github.com>
2026-05-04 12:20:51 +0800

    Update test_mmu_dynamic_threshold_config_update.py to avoid memory utilization failure (#24120)
    - What is the motivation for this PR?
    Memory utilization failure after the running of test_mmu_dynamic_threshold_config_update.py
    
    - How did you do it?
    Check port and bgp status in config reload operation to avoid memory utilization validation failure in the next script
    
    - How did you verify/test it?
    Run it locally
    
    Signed-off-by: echuawu <chuanw@nvidia.com>
    Signed-off-by: ssithaia-ebay <ssithaian@ebay.com>

--- tests/common/gu_utils.py
+++ tests/common/gu_utils.py
@@ -363,7 +363,7 @@ def rollback_or_reload(duthost, cp=DEFAULT_CHECKPOINT_NAME):
     output = rollback(duthost, cp)
 
     if output['rc'] or "Config rolled back successfully" not in output['stdout']:
-        config_reload(duthost)
+        config_reload(duthost, safe_reload=True, check_intf_up_ports=True, wait_for_bgp=True)
         pytest.fail("config rollback failed. Restored by config_reload")
 
 
