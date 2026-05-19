c447e0121296407aabbb8f8c1285c406a4af1644
Austin (Thang Pham) <austinpham@microsoft.com>
2026-05-19 17:13:28 +1000

    chore: add ft2-o128 to supported golden config db generation (#24692)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    
    Summary: Add support for ft2-o128 to golden config db
    Fixes # (issue) 3796896
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [ ] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    ### Back port request
    - [ ] 202205
    - [ ] 202305
    - [ ] 202311
    - [ ] 202405
    - [ ] 202411
    - [ ] 202505
    - [ ] 202511
    
    ### Approach
    #### What is the motivation for this PR?
    From #24572, we have ft2-o128 topo generation. This is to add the
    ft2-o128 to golden config db support.
    
    #### How did you do it?
    
    #### How did you verify/test it?
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->
    
    Signed-off-by: Austin (Ngoc Thang) Pham <austinpham@microsoft.com>
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- ansible/library/generate_golden_config_db.py
+++ ansible/library/generate_golden_config_db.py
@@ -974,7 +974,7 @@ class GenerateGoldenConfigDBModule(object):
         """
         Generate golden_config for FT2 to enable FEC and set BGP confed.
         """
-        SUPPORTED_TOPO = ["ft2-64", "lt2-p32o64", "lt2-o128"]
+        SUPPORTED_TOPO = ["ft2-64", "lt2-p32o64", "lt2-o128", "ft2-o128"]
         if self.topo_name not in SUPPORTED_TOPO:
             return "{}"
         SUPPORTED_PORT_SPEED = ["200000", "400000", "800000"]
