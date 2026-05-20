65927067b6c51682935c2fb8ab0def92e467c9b1
selldinesh <dinesh.sellappan@keysight.com>
2026-05-20 15:40:51 -0700

    adding dscp_to_tc map check
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/snappi_tests/dataplane/test_ecn_marking.py
+++ tests/snappi_tests/dataplane/test_ecn_marking.py
@@ -1,13 +1,16 @@
+import random
 from tests.snappi_tests.dataplane.imports import *  # noqa: F401, F403, F405
 from snappi_tests.dataplane.files.helper import get_duthost_interface_details, create_snappi_config, \
     get_snappi_stats, set_primary_chassis, create_traffic_items, start_stop  # noqa: F401, F403, F405, E402
 from tests.common.snappi_tests.snappi_helpers import wait_for_arp
-
+from tests.common.snappi_tests.common_helpers import config_wred, enable_ecn, \
+    stop_pfcwd, disable_packet_aging, config_capture_pkt   # noqa: F401
 pytestmark = [pytest.mark.topology("nut")]
 logger = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize("subnet_type", ["IPv4"])
+@pytest.mark.parametrize("lossy_prio", [0, 1, 2, 5, 6])
 def test_ecn_marking(
     duthosts,
     snappi_api,                   # noqa: F811
@@ -15,17 +18,35 @@ def test_ecn_marking(
     set_primary_chassis,   # noqa: F811
     create_snappi_config,    # noqa: F811
     subnet_type,
+    lossy_prio,
     fanout_graph_facts_multidut,
 ):
     """
     Test to check if packets are getting ECN marked on congestion
     """
+
     snappi_extra_params = SnappiTestParams()
     snappi_ports = get_duthost_interface_details(duthosts, get_snappi_ports, subnet_type, protocol_type="IP")
     pytest_assert(len(snappi_ports) >= 3, "Not enough ports for the test, Need at least 3 ports")
-    tx_ports = snappi_ports[:2]
-    rx_ports = [snappi_ports[2]]
-    snappi_ports = tx_ports + rx_ports
+    tx_ports = snappi_ports[1:2]
+    rx_ports = [snappi_ports[0]]
+    egress_duthost = rx_ports[0]['duthost']
+
+    config_facts = egress_duthost.config_facts(host=egress_duthost.hostname, source="running")['ansible_facts']
+    if 'DSCP_TO_TC_MAP' not in config_facts:
+        pytest_assert(False, "DSCP_TO_TC_MAP is not configured on the DUT")
+    if str(lossy_prio) not in config_facts['DSCP_TO_TC_MAP']['AZURE'].values():
+        pytest_assert(False, "Lossy priority {} is not mapped to any DSCP in DSCP_TO_TC_MAP".format(lossy_prio))
+
+    dscp_values = [int(dscp) for dscp, tc in config_facts['DSCP_TO_TC_MAP']['AZURE'].items() if int(tc) == lossy_prio]
+    snappi_extra_params.ecn_params = {'kmin': 500000, 'kmax': 910000, 'pmax': 50}
+    logger.info("Stopping PFC watchdog")
+    stop_pfcwd(egress_duthost, rx_ports[0]['asic_value'])
+    logger.info("Disabling packet aging if necessary")
+    disable_packet_aging(egress_duthost)
+    # Enabling ECN on the DUT port QUEUE
+    pytest_assert(enable_ecn(host_ans=egress_duthost, prio=lossy_prio), 'Unable to enable ecn')
+
     snappi_extra_params.protocol_config = {
         "Tx": {"protocol_type": "ip", "ports": tx_ports,
                "subnet_type": subnet_type, 'is_rdma': True},
@@ -36,13 +57,15 @@ def test_ecn_marking(
     api = snappi_api
     snappi_extra_params.traffic_flow_config = [
         {
-            "line_rate": 55,
+            "line_rate": 60,
             "frame_size": 1024,
             "is_rdma": True,
             "flow_name": "Traffic Flow",
             "tx_names": snappi_obj_handles["Tx"]["ip"],
             "rx_names": snappi_obj_handles["Rx"]["ip"],
-            "traffic_duration_fixed_seconds": 10,
+            "traffic_duration_fixed_seconds": 60,
+            "lossy_prio": lossy_prio,
+            "dscp_value": random.choice(dscp_values),
         },
     ]
     config = create_traffic_items(config, snappi_extra_params)
@@ -117,6 +140,7 @@ def test_ecn_marking(
     logger.info("Total packets Captured: {}".format(len(ip_pkts)))
     logger.info("Total packets marked: {}".format(count))
     logger.info("Percentage of packets marked: {}".format(count / len(ip_pkts) * 100))
+
     # Check if the first packet is ECN marked
     pytest_assert(is_ecn_marked(ip_pkts[0]), "The first packet should be marked")
     # Check if the last packet is not ECN marked
