53c97e13ad3330666216c92743c7bcc9fcc2f402
Nathan Wolfe <nwolfe@arista.com>
2026-05-27 17:39:49 +0000

    Fixed linter error
    Signed-off-by: Nathan Wolfe <nwolfe@arista.com>

--- tests/platform_tests/counterpoll/test_counterpoll_watermark.py
+++ tests/platform_tests/counterpoll/test_counterpoll_watermark.py
@@ -173,8 +173,8 @@ def test_counterpoll_queue_watermark_pg_drop(duthosts, localhost, enum_rand_one_
             num_expected_counters = len(counters_maps[MAPS])
             pytest_assert(wait_until(120, 5, 0, check_counters_populated, duthost,
                                      MAPS_LONG_PREFIX.format(prefix), num_expected_counters),
-                                     "Expected {} entries matching {}".format(
-                                     num_expected_counters, MAPS_LONG_PREFIX.format(prefix)))
+                          "Expected {} entries matching {}".format(
+                              num_expected_counters, MAPS_LONG_PREFIX.format(prefix)))
     # verify QUEUE or PG maps are generated into COUNTERS_DB after enabling relevant counterpoll
     with allure.step("Verifying MAPS in COUNTERS_DB on {}...".format(duthost.hostname)):
         maps_dict = RELEVANT_MAPS[tested_counterpoll]
