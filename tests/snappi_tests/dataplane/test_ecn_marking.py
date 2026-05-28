0febde14592e36081d8ac6e4ef088628c72c67eb
selldinesh <dinesh.sellappan@keysight.com>
2026-05-28 13:29:58 -0700

    Added changes for ecn response time with egress tracking
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/snappi_tests/dataplane/test_ecn_marking.py
+++ tests/snappi_tests/dataplane/test_ecn_marking.py
@@ -1,149 +1,153 @@
 import random
 from tests.snappi_tests.dataplane.imports import *  # noqa: F401, F403, F405
 from snappi_tests.dataplane.files.helper import get_duthost_interface_details, create_snappi_config, \
-    get_snappi_stats, set_primary_chassis, create_traffic_items, start_stop  # noqa: F401, F403, F405, E402
+    get_snappi_stats, set_primary_chassis, create_traffic_items, start_stop, \
+    wait_with_message, get_stats  # noqa: F401, F403, F405, E402
 from tests.common.snappi_tests.snappi_helpers import wait_for_arp
 from tests.common.snappi_tests.common_helpers import config_wred, enable_ecn, \
-    stop_pfcwd, disable_packet_aging, config_capture_pkt   # noqa: F401
+    stop_pfcwd, disable_packet_aging, config_capture_pkt  # noqa: F401
+
 pytestmark = [pytest.mark.topology("nut")]
 logger = logging.getLogger(__name__)
 
+ip_version = "IPv4"
 
-@pytest.mark.parametrize("subnet_type", ["IPv4"])
+
+@pytest.fixture(scope="module")
+def port_groups(duthosts, get_snappi_ports):
+    snappi_ports = get_duthost_interface_details(
+        duthosts, get_snappi_ports, ip_version, protocol_type="IP"
+    )
+    pytest_assert(len(snappi_ports) % 3 == 0,
+                  "Number of ports should be a multiple of 3 to create port groups of 3 ports each")
+    pg = []
+    for i in range(0, len(snappi_ports), 3):
+        pg.append(snappi_ports[i:i + 3])
+    return pg
+
+
+@pytest.mark.parametrize("subnet_type", [ip_version])
 @pytest.mark.parametrize("lossy_prio", [0, 1, 2, 5, 6])
 def test_ecn_marking(
-    duthosts,
-    snappi_api,                   # noqa: F811
-    get_snappi_ports,             # noqa: F811
-    set_primary_chassis,   # noqa: F811
-    create_snappi_config,    # noqa: F811
-    subnet_type,
-    lossy_prio,
-    fanout_graph_facts_multidut,
+        duthosts,
+        snappi_api,  # noqa: F811
+        get_snappi_ports,  # noqa: F811
+        set_primary_chassis,  # noqa: F811
+        create_snappi_config,  # noqa: F811
+        subnet_type,
+        lossy_prio,
+        port_groups,
+        fanout_graph_facts_multidut,
 ):
     """
     Test to check if packets are getting ECN marked on congestion
     """
-
     snappi_extra_params = SnappiTestParams()
-    snappi_ports = get_duthost_interface_details(duthosts, get_snappi_ports, subnet_type, protocol_type="IP")
-    pytest_assert(len(snappi_ports) >= 3, "Not enough ports for the test, Need at least 3 ports")
-    tx_ports = snappi_ports[1:2]
-    rx_ports = [snappi_ports[0]]
-    egress_duthost = rx_ports[0]['duthost']
+    # snappi_ports = get_duthost_interface_details(duthosts, get_snappi_ports, subnet_type, protocol_type="IP")
+    for snappi_ports in port_groups:
+        logger.info('\n')
+        logger.info("Snappi ports used for the test:")
+        for port in snappi_ports:
+            logger.info('{}: {}'.format(port['peer_port'], port['location']))
+        pytest_assert(len(snappi_ports) >= 3, "Not enough ports for the test, Need at least 3 ports")
+        tx_ports = snappi_ports[:2]
+        rx_ports = [snappi_ports[2]]
+        egress_duthost = rx_ports[0]['duthost']
 
-    config_facts = egress_duthost.config_facts(host=egress_duthost.hostname, source="running")['ansible_facts']
-    if 'DSCP_TO_TC_MAP' not in config_facts:
-        pytest_assert(False, "DSCP_TO_TC_MAP is not configured on the DUT")
-    if str(lossy_prio) not in config_facts['DSCP_TO_TC_MAP']['AZURE'].values():
-        pytest_assert(False, "Lossy priority {} is not mapped to any DSCP in DSCP_TO_TC_MAP".format(lossy_prio))
+        config_facts = egress_duthost.config_facts(host=egress_duthost.hostname, source="running")['ansible_facts']
+        if 'DSCP_TO_TC_MAP' not in config_facts:
+            pytest_assert(False, "DSCP_TO_TC_MAP is not configured on the DUT")
+        if str(lossy_prio) not in config_facts['DSCP_TO_TC_MAP']['AZURE'].values():
+            pytest_assert(False, "Lossy priority {} is not mapped to any DSCP in DSCP_TO_TC_MAP".format(lossy_prio))
 
-    dscp_values = [int(dscp) for dscp, tc in config_facts['DSCP_TO_TC_MAP']['AZURE'].items() if int(tc) == lossy_prio]
-    snappi_extra_params.ecn_params = {'kmin': 500000, 'kmax': 910000, 'pmax': 50}
-    logger.info("Stopping PFC watchdog")
-    stop_pfcwd(egress_duthost, rx_ports[0]['asic_value'])
-    logger.info("Disabling packet aging if necessary")
-    disable_packet_aging(egress_duthost)
-    # Enabling ECN on the DUT port QUEUE
-    pytest_assert(enable_ecn(host_ans=egress_duthost, prio=lossy_prio), 'Unable to enable ecn')
+        dscp_values = [int(dscp) for dscp, tc in config_facts['DSCP_TO_TC_MAP']['AZURE'].items() if
+                       int(tc) == lossy_prio]
 
-    snappi_extra_params.protocol_config = {
-        "Tx": {"protocol_type": "ip", "ports": tx_ports,
-               "subnet_type": subnet_type, 'is_rdma': True},
-        "Rx": {"protocol_type": "ip",
-               "ports": rx_ports, "subnet_type": subnet_type, 'is_rdma': True},
-    }
-    config, snappi_obj_handles = create_snappi_config(snappi_extra_params)
-    api = snappi_api
-    snappi_extra_params.traffic_flow_config = [
-        {
-            "line_rate": 60,
-            "frame_size": 1024,
-            "is_rdma": True,
-            "flow_name": "Traffic Flow",
-            "tx_names": snappi_obj_handles["Tx"]["ip"],
-            "rx_names": snappi_obj_handles["Rx"]["ip"],
-            "traffic_duration_fixed_seconds": 60,
-            "lossy_prio": lossy_prio,
-            "dscp_value": random.choice(dscp_values),
-        },
-    ]
-    config = create_traffic_items(config, snappi_extra_params)
-    packet_capture_file = "ECN_capture"
-    logger.info("Packet capture file: {}.pcapng".format(packet_capture_file))
-    packet_capture_ports = ['Port_3']
-    config_capture_pkt(
-        testbed_config=config,
-        port_names=packet_capture_ports,
-        capture_type=packet_capture.IP_CAPTURE,
-        capture_name=packet_capture_file,
-    )
-    api.set_config(config)
-    logger.info("Starting All protocols")
-    start_stop(snappi_api, operation="start", op_type="protocols")
-    logger.info("Wait for Arp to Resolve ...")
-    wait_for_arp(api, max_attempts=30, poll_interval_sec=2)
-    # Starting Capture
-    logger.info("Starting packet capture ...")
-    cs = api.control_state()
-    cs.port.capture.port_names = packet_capture_ports
-    cs.port.capture.state = cs.port.capture.START
-    api.set_control_state(cs)
-    wait(5, "To start capture")
-    # Starting Traffic
-    logger.info("Starting transmit on all flows ...")
-    ts = api.control_state()
-    ts.traffic.flow_transmit.state = ts.traffic.flow_transmit.START
-    api.set_control_state(ts)
-    wait(60, "For Fixed duration traffic to stop and stats to be generated")
-    logger.info("\n")
-    logger.info(
-        tabulate(
-            get_snappi_stats(
-                api._ixnetwork,
-                "Traffic Item Statistics",
-                [
-                    'Tx Frames',
-                    'Rx Frames',
-                    'Frames Delta',
-                    'Loss %',
-                    'Tx Frame Rate',
-                    'Rx Frame Rate'
-                ]
-            ),
-            headers="keys",
-            tablefmt="psql"
-        )
-    )
-    # Stopping Capture
-    logger.info("Stopping packet capture ...")
-    request = api.capture_request()
-    request.port_name = packet_capture_ports[0]
-    cs = api.control_state()
-    cs.port.capture.state = cs.port.capture.STOP
-    api.set_control_state(cs)
-    wait(20, "To stop capture")
-    logger.info(
-        "Retrieving and saving packet capture to {}.pcapng".format(packet_capture_file)
-    )
-    wait(20, "To load capture")
-    pcap_bytes = api.get_capture(request)
-    with open(packet_capture_file + ".pcapng", "wb") as fid:
-        fid.write(pcap_bytes.getvalue())
-    # Analyzing captured packets for ECN markings
-    ip_pkts = get_ipv4_pkts(packet_capture_file + ".pcapng")
-    count = 0
-    for pkt in ip_pkts:
-        if is_ecn_marked(pkt):
-            count += 1
-    pytest_assert(count > 0, "No packets are ECN marked")
-    logger.info("Total packets Captured: {}".format(len(ip_pkts)))
-    logger.info("Total packets marked: {}".format(count))
-    logger.info("Percentage of packets marked: {}".format(count / len(ip_pkts) * 100))
+        logger.info("Stopping PFC watchdog")
+        stop_pfcwd(egress_duthost, rx_ports[0]['asic_value'])
+        logger.info("Disabling packet aging if necessary")
+        disable_packet_aging(egress_duthost)
+        pytest_assert(enable_ecn(host_ans=egress_duthost, prio=lossy_prio), 'Unable to enable ecn')
+        snappi_extra_params.protocol_config = {
+            "Tx": {"protocol_type": "ip", "ports": tx_ports,
+                   "subnet_type": subnet_type, 'is_rdma': True},
+            "Rx": {"protocol_type": "ip",
+                   "ports": rx_ports, "subnet_type": subnet_type, 'is_rdma': True},
+        }
+        config, snappi_obj_handles = create_snappi_config(snappi_extra_params)
+        api = snappi_api
+        snappi_extra_params.traffic_flow_config = [
+            {
+                "line_rate": 60,
+                "frame_size": 1024,
+                "is_rdma": True,
+                "flow_name": "Traffic Flow",
+                "tx_names": snappi_obj_handles["Tx"]["ip"],
+                "rx_names": snappi_obj_handles["Rx"]["ip"],
+                "traffic_duration_fixed_seconds": 1000,
+                "lossy_prio": lossy_prio,
+                "dscp_value": random.choice(dscp_values),
+            },
+        ]
+        config = create_traffic_items(config, snappi_extra_params)
+        api.set_config(config)
 
-    # Check if the first packet is ECN marked
-    pytest_assert(is_ecn_marked(ip_pkts[0]), "The first packet should be marked")
-    # Check if the last packet is not ECN marked
-    pytest_assert(
-        not is_ecn_marked(ip_pkts[-1]), "The last packet should not be marked"
-    )
+        logger.info("Starting All protocols")
+        start_stop(snappi_api, operation="start", op_type="protocols")
+        logger.info("Wait for Arp to Resolve ...")
+        wait_for_arp(api, max_attempts=30, poll_interval_sec=2)
+        ixnet = api._ixnetwork
+        trafficItem = ixnet.Traffic.TrafficItem.find()
+        trafficItem.EgressEnabled = 'True'
+        eg = trafficItem.EgressTracking.find()
+        eg.Encapsulation = 'Any: Use Custom Settings'
+        eg.Offset = 'Custom'
+        eg.CustomOffsetBits = 126
+        eg.CustomWidthBits = 2
+        logger.info("PASSED: Egress tracking configured successfully")
+        # Start Traffic
+        logger.info('Generating Traffic Item')
+        trafficItem.Generate()
+        logger.info('Applying Traffic')
+        ixnet.Traffic.Apply()
+        logger.info('Starting Traffic')
+        ixnet.Traffic.StartStatelessTrafficBlocking()
+        time.sleep(30)
+        columns = ["frames_tx", "frames_rx", "loss", "frames_tx_rate", "frames_rx_rate"]
+        get_stats(snappi_api, "Traffic Item Statistics", columns, 'print')
+        TI_Statistics = StatViewAssistant(ixnet, 'Traffic Item Statistics')
+        pytest_assert(int(float(TI_Statistics.Rows[0]['Loss %'])) > 0,
+                      "Loss must be observed when oversubscribed traffic is running")
+        # Drill Down
+        tiview = ixnet.Statistics.View.find(Caption='Traffic Item Statistics')[0]
+        pytest_assert(len(tiview) == 1, "No statistics rows found in Traffic Item Statistics view")
+        drill_down = tiview.DrillDown.find()
+        drill_down.TargetRowIndex = 0
+        logger.info("Using AvailableDrillDownOptions: {}".format(drill_down.AvailableDrillDownOptions[3]))
+        drill_down.TargetDrillDownOption = 'Custom: (2 bits at offset 126)'
+        drill_down.DoDrillDown()
+        wait_with_message("For drill down operation to complete:", 30)
+        logger.info("Drill Down Finshed")
+        # Stop one of the high level streams and note down its respective time stamp
+        stream1 = trafficItem.HighLevelStream.find(Name="Traffic Flow-EndpointSet-1 - Flow Group 0001")
+        UD_Statistics = StatViewAssistant(ixnet, 'User Defined Statistics')
+        pytest_assert(int(float(UD_Statistics.Rows[2]['Rx Frame Rate'])) > 0,
+                      "Flow is not receiving marked packets when traffic is running")
+        logger.info("PASSED:Flow is receiving marked packets when traffic is running")
+        stream1.StopStatelessTrafficBlocking()
+        wait_with_message("For egress port buffer to drain:", 30)
+        flow_Statistics = StatViewAssistant(ixnet, 'Flow Statistics')
+        last_time_stamp_flow_1 = float(flow_Statistics.Rows[0]["Last TimeStamp"].split(':')[-1])
+        # Note down the last timestamp of the ECN flow bit row 3
+        UD_Statistics = StatViewAssistant(ixnet, 'User Defined Statistics')
+        # When we stop the flow stream Egress tracking for Row 3 should stop receiving marked packets
+        pytest_assert(int(float(UD_Statistics.Rows[2]['Rx Frame Rate'])) == 0,
+                      "Flow is still receiving markedpackets after stopping one of the stream")
+        logger.info("PASSED: Traffic is not receiving marked packets after stopping one of the stream")
+        Egress_3 = float(UD_Statistics.Rows[2]["Last TimeStamp"].split(':')[-1])
+        ECN_RESPONSE_TIME = round((Egress_3 - last_time_stamp_flow_1) * 1000, 3)
+        logger.info('\n')
+        logger.info("ECN Response Time is: {} milliseconds".format(ECN_RESPONSE_TIME))
+        logger.info('\n')
+        # Stop Traffic
+        trafficItem.StopStatelessTrafficBlocking()
