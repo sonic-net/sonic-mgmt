f1566baf8902cb065fd450281aea5949e9ebad73
yaopingzhu-arista <yaopingzhu@arista.com>
2026-05-27 23:06:17 -0400

    Fix the Broadcom supported cmds list in techsupport (#24770)
    Due to the changes in
    [PR#4542](https://github.com/sonic-net/sonic-utilities/pull/4542), the
    show_techsupport test needs to check Broadcom commands based on the chip
    module.
    
    The fix separates non-TH5 and TH5 commands into different lists
    (broadcom_cmd_bcmcmd_xgs_soc and broadcom_cmd_bcmcmd_xgs_th5) and
    combines with the common commands list (broadcom_cmd_bcmcmd_xgs) before
    checking.
    
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    
    Summary:
    Fixes # (issue)
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [x] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    ### Back port request
    - [ ] 202311
    - [ ] 202405
    - [ ] 202411
    - [ ] 202505
    - [x] 202511
    - [ ] 202512
    - [ ] 202605
    
    ### Approach
    #### What is the motivation for this PR?
    To address show_techsupport test failure after
    [PR#4542](https://github.com/sonic-net/sonic-utilities/pull/4542) is
    merged.
    
    #### How did you do it?
    
    #### How did you verify/test it?
    Run show_techsupport test on the DUTs with Broadcom non-TH5 and TH5
    chipsets, and verify no failed cases.
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->
    
    Signed-off-by: yaopingzhu <yaopingzhu@arista.com>
    Signed-off-by: selldinesh <dinesh.sellappan@keysight.com>

--- tests/show_techsupport/test_techsupport.py
+++ tests/show_techsupport/test_techsupport.py
@@ -534,6 +534,30 @@ def commands_to_check(duthosts, enum_rand_one_per_hwsku_hostname):
             asic_cmds = cmds.broadcom_cmd_bcmcmd_dnx
         else:
             asic_cmds = cmds.broadcom_cmd_bcmcmd_xgs
+
+            # Check if soc commands should be supported
+            soc_supported = None
+            try:
+                soc_supported = duthost.shell(r'bcmcmd bsh -c SOC')
+            except Exception:
+                pass
+            else:
+                if (soc_supported and soc_supported['rc'] == 0
+                        and 'Unknown command: SOC' not in ' '.join(soc_supported["stdout_lines"])):
+                    asic_cmds += cmds.broadcom_cmd_bcmcmd_xgs_soc
+                else:
+                    asic_cmds += cmds.broadcom_cmd_bcmcmd_xgs_th5
+
+            # Check if nat commands should be supported
+            nat_supported = None
+            try:
+                nat_supported = duthost.shell(r'bcmcmd "show feature" | grep -i nat')
+            except Exception:
+                pass
+            else:
+                if (nat_supported and nat_supported['rc'] == 0):
+                    asic_cmds += cmds.broadcom_cmd_bcmcmd_xgs_nat
+
         cmds_to_check.update(
             {
                 "broadcom_cmd_bcmcmd":
