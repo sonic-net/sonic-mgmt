sonic-net/sonic-mgmt

commit d303ed9e8d8bdffcd9f722856b9c805bd59b299d

Fix KeyError in counterpoll restore by adding missing counter types to COUNTERPOLL_MAPPING (#24026)
<!--
Please make sure you've read and understood our contributing guidelines;
https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
-->
### Description of PR

Summary:
On SONiC images (202511.17+), `counterpoll show` reports additional
counter types (`PHY`, `WRED_ECN_PORT`, `FLOW_CNT_ROUTE`, `SRV6`,
`SWITCH`) introduced by
[sonic-net/sonic-utilities#4106](https://github.com/sonic-net/sonic-utilities/pull/4106)
(cherry-picked to 202511 as
[#4291](https://github.com/sonic-net/sonic-utilities/pull/4291)). These
types were not present in `CounterpollConstants.COUNTERPOLL_MAPPING`,
causing a `KeyError: 'PHY'` during teardown of
`test_cpu_memory_usage_counterpoll` when `restore_counterpoll_status()`
attempts to look up each type. This issue primarily affects Mellanox
platforms.

This PR adds the missing counter type constants and their corresponding
entries in `COUNTERPOLL_MAPPING`.

- **Related ADO**:
[37209542](https://msazure.visualstudio.com/One/_workitems/edit/37209542)
- **Failed test**: `platform_tests/test_cpu_memory_usage.py`
- **Verification**: [ElasticTest test
plan](https://elastictest.org/scheduler/testplan/69decd28a2ac2651eecd1de7?leftSideViewMode=detail&testcase=platform_tests%2ftest_cpu_memory_usage.py&type=console)
- re-ran the failed test and it passed after this fix.

### Type of change

- [x] Bug fix
- [ ] Testbed and Framework(new/improvement)
- [ ] New Test case
    - [ ] Skipped for non-supported platforms
- [ ] Test case improvement

### Back port request
- [ ] 202205
- [ ] 202305
- [ ] 202311
- [ ] 202405
- [ ] 202411
- [ ] 202505
- [x] 202511

### Approach
#### What is the motivation for this PR?
SONiC 202511.17 introduced new counterpoll types (PHY, WRED_ECN_PORT,
FLOW_CNT_ROUTE, SRV6, SWITCH) via
[sonic-net/sonic-utilities#4106](https://github.com/sonic-net/sonic-utilities/pull/4106)
(cherry-picked to 202511 as

tests/common/constants.py
@@ -51,6 +51,16 @@ class CounterpollConstants:
     ACL_TYPE = "ACL"
     WRED_ECN_QUEUE_STAT_TYPE = 'WRED_ECN_QUEUE_STAT'
     WRED_QUEUE = 'wredqueue'
+    WRED_ECN_PORT_STAT_TYPE = 'WRED_ECN_PORT_STAT'
+    WRED_PORT = 'wredport'
+    PHY_TYPE = 'PHY'
+    PHY = 'phy'
+    FLOW_CNT_ROUTE_STAT_TYPE = 'FLOW_CNT_ROUTE_STAT'
+    FLOW_CNT_ROUTE = 'flowcnt-route'
+    SRV6_STAT_TYPE = 'SRV6_STAT'
+    SRV6 = 'srv6'
+    SWITCH_STAT_TYPE = 'SWITCH_STAT'
+    SWITCH = 'switch'
     COUNTERPOLL_MAPPING = {PG_DROP_STAT_TYPE: PG_DROP,
                            QUEUE_STAT_TYPE: QUEUE,
                            PORT_STAT_TYPE: PORT,
@@ -61,7 +71,12 @@ class CounterpollConstants:
                            QUEUE_WATERMARK_STAT_TYPE: WATERMARK,
                            PG_WATERMARK_STAT_TYPE: WATERMARK,
                            ACL_TYPE: ACL,
-                           WRED_ECN_QUEUE_STAT_TYPE: WRED_QUEUE}
+                           WRED_ECN_QUEUE_STAT_TYPE: WRED_QUEUE,
+                           WRED_ECN_PORT_STAT_TYPE: WRED_PORT,
+                           PHY_TYPE: PHY,
+                           FLOW_CNT_ROUTE_STAT_TYPE: FLOW_CNT_ROUTE,
+                           SRV6_STAT_TYPE: SRV6,
+                           SWITCH_STAT_TYPE: SWITCH}
     PORT_BUFFER_DROP_INTERVAL = '10000'
     COUNTERPOLL_INTERVAL = {PORT_BUFFER_DROP: 10000}
     SX_SDK = 'sx_sdk'

commit 56b188dbd345d54d4ff668451305c6766f3017f4

Skip unsupported counterpoll types in disable/enable helpers (#24933)
### Description of PR

Summary:
After PR #24026 added new counter types (`flowcnt-route`, `srv6`,
`switch`, `phy`, `wredport`) to `COUNTERPOLL_MAPPING`,
`test_counterpoll_watermark` started failing on platforms that don't
support all counter types (e.g., Pensando-elba, Arista-7060CX-32S).

The root cause is that `counterpoll flowcnt-route disable` returns
"Route flow counter is not supported on this platform" with `rc=1`,
which makes `disable_counterpoll()` raise an unhandled exception.

This fix makes `disable_counterpoll()` and `enable_counterpoll()`
tolerant of unsupported counter types by:
- Using `module_ignore_errors=True` to avoid hard failures
- Logging a warning when a platform reports "not supported"
- Only raising exceptions for genuine unexpected errors

Regression introduced by:
https://github.com/sonic-net/sonic-mgmt/pull/24026

Address this error:
```
    raise RunAnsibleModuleFail("run module {} failed".format(module_name), hostname_res)
tests.common.errors.RunAnsibleModuleFail: run module command failed, Ansible Results =>
failed = True
changed = True
rc = 1
cmd = ['counterpoll', 'flowcnt-route', 'disable']
start = 2026-05-12 12:52:02.836322
end = 2026-05-12 12:52:03.081859
delta = 0:00:00.245537
msg = non-zero return code
invocation = {'module_args': {'_raw_params': 'counterpoll flowcnt-route disable', '_uses_shell': False, 'expand_argument_vars': True, 'stdin_add_newline': True, 'strip_empty_ends': True, 'argv': None, 'chdir': None, 'executable': None, 'creates': None, 'removes': None, 'stdin': None}}
_ansible_no_log = False
stdout =
Route flow counter is not supported on this platformstderr =
```

ADO:37940567




### Type of change

- [x] Bug fix
- [ ] Testbed and Framework(new/improvement)
- [ ] New Test case
    - [ ] Skipped for non-supported platforms
- [ ] Test case improvement

### Back port request
- [ ] 202205
- [ ] 202305
- [ ] 202311
- [ ] 202405
- [ ] 202411

tests/common/helpers/counterpoll_helper.py
@@ -1,3 +1,5 @@
+import logging
+
 from tests.common.constants import CounterpollConstants
 
 
@@ -39,14 +41,67 @@ class ConterpollHelper:
                         counterpoll_before[counterpoll][CounterpollConstants.STATUS]))
 
     @staticmethod
+    def _run_command_ignore_errors(duthost, cmd):
+        """Run command handling both SonicHost and SonicAsic interfaces.
+
+        SonicAsic.command() does not support module_ignore_errors, so
+        we try that kwarg first and fall back to catching the exception.
+        """
+        try:
+            return duthost.command(cmd, module_ignore_errors=True)
+        except TypeError:
+            # SonicAsic.command() doesn't accept module_ignore_errors
+            try:
+                return duthost.command(cmd)
+            except Exception as e:
+                # Return a synthetic failed result
+                return {'rc': 1, 'stdout': str(e), 'stderr': str(e)}
+
+    @staticmethod
     def disable_counterpoll(duthost, counter_type_list):
         for counterpoll_type in counter_type_list:
-            duthost.command(CounterpollConstants.COUNTERPOLL_DISABLE.format(counterpoll_type))
+            result = ConterpollHelper._run_command_ignore_errors(
+                duthost,
+                CounterpollConstants.COUNTERPOLL_DISABLE.format(
+                    counterpoll_type))
+            if result.get('rc', 0) != 0:
+                stdout = result.get('stdout', '').lower()
+                stderr = result.get('stderr', '').lower()
+                if 'not supported' in stdout or \
+                        'not supported' in stderr:
+                    logging.warning(
+                        "Counterpoll type '%s' not supported "
+                        "on this platform, skipping",
+                        counterpoll_type)
+                else:
+                    raise Exception(
+                        "Failed to disable counterpoll "
+                        "'{}': rc={}".format(
+                            counterpoll_type,
+                            result.get('rc', -1)))
 
     @staticmethod
     def enable_counterpoll(duthost, counter_type_list):
         for counterpoll_type in counter_type_list:
-            duthost.command(CounterpollConstants.COUNTERPOLL_ENABLE.format(counterpoll_type))
+            result = ConterpollHelper._run_command_ignore_errors(
+                duthost,
+                CounterpollConstants.COUNTERPOLL_ENABLE.format(
+                    counterpoll_type))
+            if result.get('rc', 0) != 0:
+                stdout = result.get('stdout', '').lower()
+                stderr = result.get('stderr', '').lower()
+                if 'not supported' in stdout or \
+                        'not supported' in stderr:
+                    logging.warning(
+                        "Counterpoll type '%s' not supported "
+                        "on this platform, skipping",
+                        counterpoll_type)
+                else:
+                    raise Exception(
+                        "Failed to enable counterpoll "
+                        "'{}': rc={}".format(
+                            counterpoll_type,
+                            result.get('rc', -1)))
 
     @staticmethod
     def set_counterpoll_interval(duthost, counterpoll_type, interval):