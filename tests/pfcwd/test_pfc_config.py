77b89080cbfc32a68f21477f04cd49c4ccf1a4ab
Yatish Koul <yatishkoul@microsoft.com>
2026-06-04 21:03:51 +0000

    pfcwd/test_pfc_config: revert override_config=True for test_default_cfg_after_load_mg
    This test verifies pfcwd's default configuration after a plain
    'config load_minigraph'. Overlaying golden_config_db.json (via -o)
    strips the PFC_WD section on KVM, so the test then fails with
    'Failed to start pfcwd after load_minigraph'. Reverting to the
    upstream behavior.
    
    Signed-off-by: Yatish Koul <yatishkoul@microsoft.com>

--- tests/pfcwd/test_pfc_config.py
+++ tests/pfcwd/test_pfc_config.py
@@ -329,7 +329,7 @@ class TestDefaultPfcConfig(object):
         # We can safely ignore them as they are not critical to the test.
         safe_reload_ignored_dockers = ['dhcp_server', 'dhcp_relay']
         config_reload(duthost, config_source='minigraph', safe_reload=True,
-                      safe_reload_ignored_dockers=safe_reload_ignored_dockers, override_config=True)
+                      safe_reload_ignored_dockers=safe_reload_ignored_dockers)
         # sleep 20 seconds to make sure configuration is loaded
         time.sleep(20)
         res = duthost.command('pfcwd show config')
