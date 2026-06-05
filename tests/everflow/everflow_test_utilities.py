b1dcec12aa396f1f3720064bf2244af337fa649c
Javier-Tan <47554099+Javier-Tan@users.noreply.github.com>
2026-06-05 17:49:03 +1000

    [everflow] Minor everflow changes
    Signed-off-by: Javier-Tan <47554099+Javier-Tan@users.noreply.github.com>

--- tests/everflow/everflow_test_utilities.py
+++ tests/everflow/everflow_test_utilities.py
@@ -487,12 +487,9 @@ def setup_info(duthosts, rand_one_dut_hostname, tbinfo, request, topo_scenario):
     """
     duthost = None
     topo = tbinfo['topo']['name']
-    if 't2' in topo:
-        if len(duthosts) == 1:
-            downstream_duthost = upstream_duthost = duthost = duthosts[rand_one_dut_hostname]
-        else:
-            pytest_assert(len(duthosts) > 2, "Test must run on whole chassis")
-            downstream_duthost, upstream_duthost = get_t2_duthost(duthosts, tbinfo)
+    if 't2' in topo and len(duthosts) > 1:
+        pytest_assert(len(duthosts) > 2, "Test must run on whole chassis")
+        downstream_duthost, upstream_duthost = get_t2_duthost(duthosts, tbinfo)
     else:
         downstream_duthost = upstream_duthost = duthost = duthosts[rand_one_dut_hostname]
 
@@ -630,7 +627,7 @@ def validate_acl_rules_in_asic_db(duthost):
         asic_rules = asic.shell(asic_cmd)['stdout_lines']
 
         if len(config_rules) != len(asic_rules):
-            logging.error("ACL rules count mismatch in ASIC {}: CONFIG_DB={}, ASIC_DB={}".format(
+            logging.warning("ACL rules count mismatch in ASIC {}: CONFIG_DB={}, ASIC_DB={}".format(
                 asic.asic_index if duthost.is_multi_asic else "single",
                 len(config_rules), len(asic_rules)))
             return False
@@ -654,6 +651,7 @@ def wait_for_acl_rules_in_asic_db(duthost):
         wait_until(acl_rule_wait_time, 10, 0, validate_acl_rules_in_asic_db, duthost),
         "ACL rules in ASIC DB did not match CONFIG DB within {} seconds".format(acl_rule_wait_time)
     )
+    logging.info("Successfully validated ACL rules")
 
 
 # TODO: This should be refactored to some common area of sonic-mgmt.
