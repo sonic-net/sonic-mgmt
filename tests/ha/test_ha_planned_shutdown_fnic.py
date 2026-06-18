c9c37d54da09fe30e20b8fdd531d91a61b807f4f
Jing Zhang <zhangjing@microsoft.com>
2026-06-18 10:58:41 -0700

    [ha]: Apply DASH configs in dependency order (#25363)
    Summary:
    Apply the shared `apply_dash_configs` helper from #25252 to HA tests so
    DASH APP_DB writes follow the same dependency order as DASH tests.
    
    Signed-off-by: Jing Zhang <zhangjing@microsoft.com>
    Signed-off-by: ssithaia-ebay <ssithaian@ebay.com>

--- tests/ha/test_ha_planned_shutdown_fnic.py
+++ tests/ha/test_ha_planned_shutdown_fnic.py
@@ -10,9 +10,9 @@ import threading
 import queue
 from tests.common.helpers.assertions import pytest_assert
 from constants import LOCAL_PTF_INTF, REMOTE_PTF_RECV_INTF
-from gnmi_utils import apply_messages
 from packets import outbound_pl_packets
 from tests.common.config_reload import config_reload
+from tests.ha.conftest import apply_dash_pl_pipeline_config
 from ha_dash_flow_utils import compare_flow_tables, compare_flow_tables_pdsctl
 from ha_utils import activate_primary_dash_ha, activate_secondary_dash_ha, \
          verify_ha_state, set_dash_ha_scope, set_dead_dash_ha_scope
@@ -52,53 +52,7 @@ def common_setup_teardown(
     if skip_config:
         return
 
-    for i in range(len(duthosts)):
-        duthost = duthosts[i]
-        dpuhost = dpuhosts[i]
-        base_config_messages = {
-            **pl.APPLIANCE_FNIC_CONFIG,
-            **pl.ROUTING_TYPE_PL_CONFIG,
-            **pl.ROUTING_TYPE_VNET_CONFIG,
-            **pl.VNET_CONFIG,
-            **pl.ROUTE_GROUP1_CONFIG,
-            **pl.METER_POLICY_V4_CONFIG,
-            **pl.TUNNEL1_CONFIG,
-        }
-        logger.info(f"configure on {duthost.hostname} dpu {dpuhost.dpu_index} {base_config_messages}")
-
-        apply_messages(localhost, duthost, ptfhost, base_config_messages, dpuhost.dpu_index)
-
-        route_and_mapping_messages = {
-            **pl.PE_VNET_MAPPING_CONFIG,
-            **pl.PE_SUBNET_ROUTE_CONFIG,
-            **pl.VM_VNET_MAPPING_CONFIG,
-            **pl.VM_SUBNET_ROUTE_WITH_TUNNEL_SINGLE_ENDPOINT,
-        }
-
-        logger.info(route_and_mapping_messages)
-        apply_messages(localhost, duthost, ptfhost, route_and_mapping_messages, dpuhost.dpu_index)
-
-        if 'pensando' not in dpuhost.facts['asic_type']:
-            route_rule_messages = {
-                **pl.VM_VNI_ROUTE_RULE_CONFIG,
-                **pl.INBOUND_VNI_ROUTE_RULE_CONFIG,
-                **pl.TRUSTED_VNI_ROUTE_RULE_CONFIG,
-            }
-            logger.info(route_rule_messages)
-            apply_messages(localhost, duthost, ptfhost, route_rule_messages, dpuhost.dpu_index)
-
-        meter_rule_messages = {
-            **pl.METER_RULE1_V4_CONFIG,
-            **pl.METER_RULE2_V4_CONFIG,
-        }
-        logger.info(meter_rule_messages)
-        apply_messages(localhost, duthost, ptfhost, meter_rule_messages, dpuhost.dpu_index)
-
-        logger.info(pl.ENI_FNIC_CONFIG)
-        apply_messages(localhost, duthost, ptfhost, pl.ENI_FNIC_CONFIG, dpuhost.dpu_index)
-
-        logger.info(pl.ENI_ROUTE_GROUP1_CONFIG)
-        apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpuhost.dpu_index)
+    apply_dash_pl_pipeline_config(localhost, duthosts, dpuhosts, ptfhost, floating_nic=True)
 
     yield
 
