2937bcd697233fb6efb695f5db3460a6ca39141e
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2026-06-23 11:36:37 +0800

    srv6: refresh 7-node configdb jsons and run sanity on cisco/force10
    Update the 7-node SRv6 testbed assets so test_srv6_basic_sanity.py can
    run on both ciscovs-7nodes and force10-7nodes topologies:
    
    - ansible/vars/configdb_jsons/7nodes_cisco/{P1..P4,PE1..PE3}.json:
      refresh per-node config_db.json (ports, BGP/SRv6 data plane).
    - ansible/vars/configdb_jsons/7nodes_force10/{P1..P4,PE1..PE3}.json:
      align force10 7-node configs with the cisco set so the same SRv6
      sanity flow applies.
    - tests/common/testbed.py, tests/srv6/test_srv6_basic_sanity.py:
      recognize the 7-node force10 topology alongside ciscovs-7nodes.
    
    Signed-off-by: Eddie Ruan <eddie.ruan@alibaba-inc.com>

--- tests/srv6/test_srv6_basic_sanity.py
+++ tests/srv6/test_srv6_basic_sanity.py
@@ -213,10 +213,9 @@ def test_check_routes(duthosts, rand_one_dut_hostname, nbrhosts):
 # Test Case : Traffic check in Normal Case
 #
 def test_traffic_check_normal(tbinfo, duthosts, rand_one_dut_hostname, ptfhost, nbrhosts, ptfadapter):
-    #
-    # Create a packet sending to 192.100.0.1
-    #
-    # establish_and_configure_bfd(nbrhosts)
+    if tbinfo["topo"]["name"] not in ["ciscovs-7nodes"]:
+        pytest.skip("SRv6 data plane only available on ciscovs topologies")
+
     tcp_pkt0 = simple_tcp_packet(
         ip_src="192.200.0.1",
         ip_dst="192.100.0.1",
