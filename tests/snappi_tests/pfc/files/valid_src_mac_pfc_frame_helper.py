4e7d80a92cc816b26fa72c789eb4adf7f81e66ac
yongjiao.liu <yongjiao.liu@viavisolutions.com>
2026-06-23 10:24:35 +0800

    pfc: fix import alias collision in valid_src_mac_pfc_frame_helper
    Rename imported validate_pfc_frame to pcap_validate_pfc_frame to avoid
    shadowing by the local validate_pfc_frame variable, and remove unused
    validate_pfc_frame_cisco import.
    
    Signed-off-by: yongjiao.liu <yongjiao.liu@viavisolutions.com>

--- tests/snappi_tests/pfc/files/valid_src_mac_pfc_frame_helper.py
+++ tests/snappi_tests/pfc/files/valid_src_mac_pfc_frame_helper.py
@@ -15,7 +15,7 @@ from tests.common.snappi_tests.snappi_helpers import wait_for_arp  # noqa: F401
 from tests.common.snappi_tests.traffic_generation import setup_base_traffic_config, generate_test_flows, \
     generate_pause_flows, run_traffic, verify_basic_test_flow
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
-from tests.common.snappi_tests.read_pcap import validate_pfc_frame, validate_pfc_frame_cisco
+from tests.common.snappi_tests.read_pcap import validate_pfc_frame as pcap_validate_pfc_frame
 from tests.common.cisco_data import is_cisco_device
 
 logger = logging.getLogger(__name__)
@@ -148,10 +148,10 @@ def run_pfc_valid_src_mac_test(
 
     if snappi_extra_params.packet_capture_type == packet_capture.PFC_CAPTURE:
         # PFC pause frame capture is requested
-        _validate_pfc_frame = True
+        validate_pfc_frame = True
     else:
         # PFC pause frame capture is not requested
-        _validate_pfc_frame = False
+        validate_pfc_frame = False
 
     # Generate test flow config
     generate_test_flows(testbed_config=testbed_config,
@@ -192,12 +192,12 @@ def run_pfc_valid_src_mac_test(
     pfc.pfc_delay = 0
 
     # Verify PFC pause frames
-    if _validate_pfc_frame:
+    if validate_pfc_frame:
         peer_mac_addr = snappi_extra_params.base_flow_config["rx_port_config"].gateway_mac
         # Use Cisco-specific validation for Cisco devices, generic validation for others
         # Both functions validate source MAC address when peer_mac_addr is provided
        
-        is_valid_pfc_frame, error_msg = validate_pfc_frame(
+        is_valid_pfc_frame, error_msg = pcap_validate_pfc_frame(
                             snappi_extra_params.packet_capture_file + ".pcapng",
                             peer_mac_addr=peer_mac_addr)
         pytest_assert(is_valid_pfc_frame, error_msg)
