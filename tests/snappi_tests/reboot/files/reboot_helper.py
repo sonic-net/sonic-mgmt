fd976fb2ea7adc63aa7e575efcb87c634cbccda4
yongjiao.liu <yongjiao.liu@viavisolutions.com>
2026-06-24 17:17:25 +0800

    snappi/reboot: fix ping result string comparison in wait loops
    `responses[-1].result` returns OTG enum strings "succeeded" or "failed".
    The original code used `not x == y` which, while logically equivalent,
    is ambiguous and triggers linter warnings. Replace all occurrences with
    explicit `!= "succeeded"` / `== "succeeded"` comparisons for clarity.
    
    Signed-off-by: Yongjiao Liu <yongjiao.liu@viavisolutions.com>
    Signed-off-by: yongjiao.liu <yongjiao.liu@viavisolutions.com>

--- tests/snappi_tests/reboot/files/reboot_helper.py
+++ tests/snappi_tests/reboot/files/reboot_helper.py
@@ -412,7 +412,7 @@ def wait_for_bgp_and_lb_soft(snappi_api, ping_req, ):
     found_lb_state = False
     while True:
         responses = ping_loopback_if(snappi_api, ping_req)
-        if not found_lb_state and not responses[-1].result == "succeeded":
+        if not found_lb_state and responses[-1].result != "succeeded":
             loopback_down_start_timer = time.time()
             found_lb_state = True
             logger.info('!!!!!!! 1. loopback timer started {} !!!!!!'.format(
@@ -460,13 +460,13 @@ def wait_for_bgp_and_lb(snappi_api, ping_req, ):
             found_bgp_state = True
             logger.info('!!! 1. bgp is down time started {} !!!'.format(
                 bgp_down_start_timer))
-        if not found_lb_state and not responses[-1].result == "succeeded":
+        if not found_lb_state and responses[-1].result != "succeeded":
             loopback_down_start_timer = time.time()
             found_lb_state = True
             logger.info('!!! 1. loopback timer started {} !!!'.format(
                 loopback_down_start_timer))
-        if bgpv4_metrics[-1].session_state in "down" and not \
-                responses[-1].result == "succeeded" and found_bgp_state and \
+        if bgpv4_metrics[-1].session_state in "down" and \
+                responses[-1].result != "succeeded" and found_bgp_state and \
                 found_lb_state:
             logger.info('BGP Control And LoopBack I/F Down')
             break
@@ -490,8 +490,8 @@ def wait_for_bgp_and_lb(snappi_api, ping_req, ):
             logger.info(' ')
             logger.info('2. loopback up end time {} !!!'.format(
                 loopback_up_start_timer))
-        if bgpv4_metrics[-1].session_state in "up" and responses[-1].result == "succeeded" \
-                and found_bgp_state and found_lb_state:
+        if bgpv4_metrics[-1].session_state in "up" and \
+                responses[-1].result == "succeeded" and found_bgp_state and found_lb_state:
             logger.info('BGP Control And LoopBack I/F Up')
             break
 
