8d9fad4d3e7b98c09998fd93a6eea78b718f1293
sutter <sutter.lum@pinewave.com>
2026-07-01 14:29:19 -0700

    flake8 fixes for prh
    Signed-off-by: sutter <sutter.lum@pinewave.com>

--- tests/transceiver/common/process_restart_helpers.py
+++ tests/transceiver/common/process_restart_helpers.py
@@ -1,11 +1,13 @@
 import tests.transceiver.attribute_parser.attribute_keys as at_keys
 
-#NOTE TEMP FILE - need to move to established files per file_organization.md
+# NOTE TEMP FILE - need to move to established files per file_organization.md
 
-############################# CONSTANTS #############################
+# ############################ CONSTANTS ############################
 # from docs/testplan/transceiver/test_plan.md
 XCVRD_RESTART = r"docker exec pmon supervisorctl restart xcvrd"
-XCVRD_UPTIME = r"docker exec pmon supervisorctl status xcvrd | awk '{print $NF}'"
+XCVRD_UPTIME = (
+    r"docker exec pmon supervisorctl status xcvrd | awk '{print $NF}'"
+)
 PMON_RESTART = r"sudo systemctl restart pmon"
 SWSS_RESTART = r"sudo systemctl restart swss"
 SYNCD_RESTART = r"sudo systemctl restart syncd"
@@ -20,50 +22,64 @@ DEFAULT_PMON_SETTLE_SEC = 120
 DEFAULT_SWSS_SETTLE_SEC = 180
 DEFAULT_SYNCD_SETTLE_SEC = 240
 
-############################# GENERIC HELPERS #############################
+# ########################## GENERIC HELPERS ##########################
+
+
 def sys_attr(port_attrs, name, default):
-    """Extract system attribute from port attributes dict with default fallback."""
+    """Extract system attribute from port attrs, falling back to default."""
     return port_attrs.get(at_keys.SYSTEM_ATTRIBUTES_KEY, {}).get(name, default)
 
+
 def restart_process(duthost, process):
-    if process == "xcvrd": cmd = XCVRD_RESTART
-    elif process == "pmon": cmd = PMON_RESTART
-    elif process == "swss": cmd = SWSS_RESTART  
-    elif process == "syncd": cmd = SYNCD_RESTART
+    if process == "xcvrd":
+        cmd = XCVRD_RESTART
+    elif process == "pmon":
+        cmd = PMON_RESTART
+    elif process == "swss":
+        cmd = SWSS_RESTART
+    elif process == "syncd":
+        cmd = SYNCD_RESTART
     if process:
         duthost.shell(cmd)
         return True
 
+
 def get_xcvrd_uptime(duthost):
     return duthost.shell(XCVRD_UPTIME)
+
 
 def get_lldp_neighbors(duthost):
     return duthost.shell(LLDP_NEIGHBORS)
 
+
 def get_corefiles(duthost):
     return duthost.shell(SEARCH_COREFILES)
+
 
 def get_db_port_table(duthost, port, namespace=None, attr_filter=None):
     """
     Example of UNFILTERED Output:
         {'admin_status': 'up',
-        'alias': 'etp5a', 
-        'autoneg': 'off', 
-        'fec': 'rs', 
-        'index': '5', 
-        'lanes': '57', 
-        'mtu': '9100', 
-        'speed': '100000', 
-        'subport': '1', 
-        'description': '', 
-        'oper_status': 'up', 
-        'flap_count': '1', 
+        'alias': 'etp5a',
+        'autoneg': 'off',
+        'fec': 'rs',
+        'index': '5',
+        'lanes': '57',
+        'mtu': '9100',
+        'speed': '100000',
+        'subport': '1',
+        'description': '',
+        'oper_status': 'up',
+        'flap_count': '1',
         'last_up_time': 'Wed Jun 24 17:31:05 2026'}
     """
     cmd = ''
-    if namespace: #MULTI-ASIC
-        cmd = f'sonic-db-cli -n "{namespace}" APPL_DB hgetall "PORT_TABLE:{port}" '
-    else: #SINGLE ASIC
+    if namespace:  # MULTI-ASIC
+        cmd = (
+            f'sonic-db-cli -n "{namespace}" APPL_DB '
+            f'hgetall "PORT_TABLE:{port}" '
+        )
+    else:  # SINGLE ASIC
         cmd = f'sonic-db-cli APPL_DB hgetall "PORT_TABLE:{port}"'
 
     if attr_filter:
