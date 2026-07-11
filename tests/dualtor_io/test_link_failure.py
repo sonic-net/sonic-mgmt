4fb16a54a20803853759f496d36cd91bea3f5951
xwjiang-ms <xiaweijiang@microsoft.com>
2026-07-11 17:05:41 +1000

    Fix dualtor link down standby duplication tolerance
    Signed-off-by: xwjiang-ms <xiaweijiang@microsoft.com>

--- tests/dualtor_io/test_link_failure.py
+++ tests/dualtor_io/test_link_failure.py
@@ -115,15 +115,19 @@ def test_active_link_down_downstream_active(
 def test_active_link_down_downstream_standby(
     upper_tor_host, lower_tor_host, send_t1_to_server_with_action,      # noqa: F811
     toggle_all_simulator_ports_to_upper_tor,                            # noqa: F811
-    shutdown_fanout_upper_tor_intfs                                     # noqa: F811
+    shutdown_fanout_upper_tor_intfs,                                    # noqa: F811
+    link_down_downstream_active_duplication_setting                     # noqa: F811
 ):
     """
     Send traffic from T1 to standby ToR and shutdown the active ToR link.
     Verify switchover and disruption lasts < 1 second
     """
+    allowed_duplication, merge_duplications = link_down_downstream_active_duplication_setting
     send_t1_to_server_with_action(
         lower_tor_host, verify=True, delay=MUX_SIM_ALLOWED_DISRUPTION_SEC,
-        allowed_disruption=3, action=shutdown_fanout_upper_tor_intfs
+        allowed_disruption=3, allowed_duplication=allowed_duplication,
+        action=shutdown_fanout_upper_tor_intfs,
+        merge_duplications_into_disruptions=merge_duplications
     )
     verify_tor_states(
         expected_active_host=lower_tor_host,
