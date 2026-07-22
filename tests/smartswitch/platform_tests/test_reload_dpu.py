39ebf868bc41f423d1961066b9f8d962d789004c
Cong Hou <congh@nvidia.com>
2026-07-22 12:25:33 +0300

    Fix comments
    1. Restore the sleep time in the kernel panic command.
    2. Get the DPU uptimes before triggering the kernel panic.
    
    Signed-off-by: Cong Hou <congh@nvidia.com>

--- tests/smartswitch/platform_tests/test_reload_dpu.py
+++ tests/smartswitch/platform_tests/test_reload_dpu.py
@@ -28,7 +28,7 @@ pytestmark = [
     pytest.mark.topology('smartswitch')
 ]
 
-kernel_panic_cmd = "sudo nohup bash -c 'sleep 10 && echo c > /proc/sysrq-trigger' &"
+kernel_panic_cmd = "sudo nohup bash -c 'sleep 5 && echo c > /proc/sysrq-trigger' &"
 memory_exhaustion_cmd = "sudo nohup bash -c 'sleep 5 && tail /dev/zero' &"
 DUT_ABSENT_TIMEOUT_FOR_KERNEL_PANIC = 100
 DUT_ABSENT_TIMEOUT_FOR_MEMORY_EXHAUSTION = 240
@@ -246,6 +246,9 @@ def test_dpu_status_post_dpu_kernel_panic(duthosts, dpuhosts,
                                                  platform_api_conn,
                                                  num_dpu_modules)
 
+    logging.info("Recording DPU boot times before DPU kernel panic")
+    pre_boot_times = get_all_dpu_uptimes(dpuhosts, dpu_on_list)
+
     triggered_dpu_on_list = []
     triggered_ip_list = []
     for index in range(len(dpu_on_list)):
@@ -261,9 +264,6 @@ def test_dpu_status_post_dpu_kernel_panic(duthosts, dpuhosts,
         triggered_ip_list.append(ip_address_list[index])
 
     pytest_assert(triggered_dpu_on_list, "No DPUs were triggered; all skipped due to missing dpuhosts")
-
-    logging.info("Recording DPU boot times before DPU kernel panic")
-    pre_boot_times = get_all_dpu_uptimes(dpuhosts, triggered_dpu_on_list)
 
     logging.info("Checking DPUs are not pingable")
     check_dpus_are_not_pingable(duthost, triggered_ip_list)
