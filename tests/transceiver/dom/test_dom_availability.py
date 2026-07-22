fab22dc429bf0b276f33b0acd19bff22c98b56e6
david <david.huang@eoptolink.com>
2026-07-22 21:22:17 +0800

    Fix DOM TC1 freshness validation ownership
    Signed-off-by: david <david.huang@eoptolink.com>

--- tests/transceiver/dom/test_dom_availability.py
+++ tests/transceiver/dom/test_dom_availability.py
@@ -7,7 +7,7 @@ def test_dom_data_availability_verification(
     dom_port_context,
     dom_sensor_by_port,
     dom_availability_plan_by_port,
-    parse_dom_update_time,
+    dom_freshness_failures,
     dom_now_utc,
 ):
     """Verify configured DOM sensor data is present and fresh in STATE_DB."""
@@ -29,11 +29,9 @@ def test_dom_data_availability_verification(
         if max_age_min is not None or expected_fields or field_failures:
             has_configured_checks = True
 
+        field_failures.extend(dom_freshness_failures(sensor_data, max_age_min, now_utc))
+
         if not sensor_data:
-            if max_age_min is not None:
-                field_failures.append(
-                    "missing TRANSCEIVER_DOM_SENSOR data for last_update_time freshness check"
-                )
             for field in expected_fields:
                 field_failures.append(
                     "missing TRANSCEIVER_DOM_SENSOR data for expected field {}".format(field)
@@ -41,29 +39,6 @@ def test_dom_data_availability_verification(
             if field_failures:
                 all_failures.append("{}:\n  {}".format(port, "\n  ".join(field_failures)))
             continue
-
-        if max_age_min is not None:
-            try:
-                max_age = float(max_age_min)
-            except (TypeError, ValueError):
-                field_failures.append(
-                    "invalid data_max_age_min={!r} in DOM_ATTRIBUTES".format(max_age_min)
-                )
-            else:
-                parsed_time = parse_dom_update_time(sensor_data.get("last_update_time"))
-                if parsed_time is None:
-                    field_failures.append(
-                        "last_update_time missing or unparsable while data_max_age_min is configured"
-                    )
-                else:
-                    age_minutes = (now_utc - parsed_time).total_seconds() / 60.0
-                    if age_minutes > max_age:
-                        field_failures.append(
-                            "last_update_time too old (age_min={:.2f}, limit={})".format(
-                                age_minutes,
-                                max_age_min,
-                            )
-                        )
 
         for field in expected_fields:
             if field not in sensor_data:
