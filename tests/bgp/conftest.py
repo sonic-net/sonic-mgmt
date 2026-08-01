c046e6f6675c77daab0f9f2bb7ea815fe4067305
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2026-08-01 01:36:15 -0700

    support UMA/LMA in tests and bgp conftest (#26723)
    support UMA/LMA in tests and bgp conftest
    
    co-authorized by: jianquanye@microsoft.com

--- tests/bgp/conftest.py
+++ tests/bgp/conftest.py
@@ -623,7 +623,7 @@ def setup_interfaces(duthosts, enum_rand_one_per_hwsku_frontend_hostname, ptfhos
         setup_func = _setup_interfaces_dualtor
     elif tbinfo["topo"]["type"] in ["t0", "mx"]:
         setup_func = _setup_interfaces_t0_or_mx
-    elif tbinfo["topo"]["type"] in set(["t1", "t2", "m1", "lt2", "ft2", "c0", "lrh", "urh"]):
+    elif tbinfo["topo"]["type"] in set(["t1", "t2", "m1", "lt2", "ft2", "c0", "lrh", "urh", "lma", "uma"]):
         setup_func = _setup_interfaces_t1_t2_drh
     elif tbinfo["topo"]["type"] == "m0":
         if topo_scenario == "m0_l3_scenario":
