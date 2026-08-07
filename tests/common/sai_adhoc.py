42322679f0f2f117fe1ba6ed284063a0ecc19f9d
Ariz Zubair <arizzubair@microsoft.com>
2026-08-07 17:17:55 +0000

    Fix linting error.
    Signed-off-by: Ariz Zubair <arizzubair@microsoft.com>

--- tests/common/sai_adhoc.py
+++ tests/common/sai_adhoc.py
@@ -24,7 +24,8 @@ def get_info_helper(conf_name):
         param:
             conf_name : configuration name in testbed.yaml
     """
-    ansible_config_path = pathlib.Path(os.getenv("ANSIBLE_CONFIG", pathlib.Path(__file__).resolve().parent.joinpath("../ansible")))
+    ansible_config_path = pathlib.Path(os.getenv("ANSIBLE_CONFIG",
+                                                 pathlib.Path(__file__).resolve().parent.joinpath("../ansible")))
     with open(ansible_config_path.joinpath("testbed.yaml")) as f:
         testbed_infos = yaml.load(f, Loader=SafeLoader)
         for testbed_info in testbed_infos:
