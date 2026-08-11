85a7032a9311433e07bacdc1bcc6c4949c5ff00d
sutter <sutter.lum@pinewave.com>
2026-08-11 15:34:38 -0700

    added orchagent expected pid
    Signed-off-by: sutter <sutter.lum@pinewave.com>

--- tests/transceiver/system/process_restart/test_syncd_restart.py
+++ tests/transceiver/system/process_restart/test_syncd_restart.py
@@ -67,6 +67,7 @@ def test_system_syncd_restart(
     expected_pid_changes.add("xcvrd")
     expected_pid_changes.add("syncd")
     expected_pid_changes.add("swss")
+    expected_pid_changes.add("orchagent")
     ports = sorted(port_attributes_dict.keys())
     assert ports, "port_attributes_dict is empty - nothing to validate"
     failures = []
