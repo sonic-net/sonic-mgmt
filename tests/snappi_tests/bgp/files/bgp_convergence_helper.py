211adb3f39ceafd0120763fcb005e63f64d8d1f8
yongjiao.liu <yongjiao.liu@viavisolutions.com>
2026-09-08 10:53:18 +0800

    Revert rx_frame_rate assertion fix in get_rib_in_convergence
    Traffic should not be received yet at this point since routes are
    still withdrawn; assert rx_frame_rate == 0 before advertising routes.
    
    Signed-off-by: yongjiao.liu <yongjiao.liu@viavisolutions.com>

--- tests/snappi_tests/bgp/files/bgp_convergence_helper.py
+++ tests/snappi_tests/bgp/files/bgp_convergence_helper.py
@@ -876,9 +876,7 @@ def get_rib_in_convergence(snappi_api,
             tx_frame_rate = flow_stats[0].frames_tx_rate
             rx_frame_rate = flow_stats[0].frames_rx_rate
             assert tx_frame_rate != 0, "Traffic has not started"
-            # Routes are withdrawn but traffic still flows via existing forwarding;
-            # convergence is measured from here until rx catches up after ADVERTISE.
-            assert rx_frame_rate != 0, "Traffic is not received"
+            assert rx_frame_rate == 0
 
             """ Advertise All Routes """
             logger.info('Advertising all Routes from {}'.format(route_names))
