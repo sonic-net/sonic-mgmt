bac54aad8430e1230f3e7fcf7a58fefec0649333
donghaolicd <leedonhom@gmail.com>
2026-09-08 18:43:02 +0000

    [gnmi] Share COUNTERS_DB TLS per module
    Signed-off-by: donghaolicd <leedonhom@gmail.com>

--- tests/gnmi/test_gnmi_countersdb.py
+++ tests/gnmi/test_gnmi_countersdb.py
@@ -10,6 +10,9 @@ from tests.common.pygnmi_client import SubscribeMode
 logger = logging.getLogger(__name__)
 
 pytestmark = [
+    pytest.mark.parametrize(
+        "gnmi_tls", ["tls"], indirect=True, scope="module"
+    ),
     pytest.mark.topology('any'),
     pytest.mark.disable_loganalyzer,
     pytest.mark.usefixtures(
