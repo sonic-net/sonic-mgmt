72ffcc20e210411f54c7b500ef9a9f96267876ed
mssonicbld <79238446+mssonicbld@users.noreply.github.com>
2026-09-17 01:13:38 +1000

    [action] [PR:26707] Improve interlock in `check_appl_db` for `EosHost` nbrs (#27112)
    ### Description of PR
    Without this interlock test_config_reload ends up finishing the test before the ports come back up causing issues on subsequent tests
    
    Summary:
    Fixes #26706 
    
    ### Type of change
    
    - [x] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
     - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    
    ### Back port request
    - [ ] 202311
    - [ ] 202405
    - [ ] 202411
    - [ ] 202505
    - [x] 202511
    - [ ] 202512
    - [x] 202605
    
    ### Tested branch
    - [ ] master
    - [ ] 202311
    - [ ] 202405
    - [ ] 202411
    - [ ] 202505
    - [x] 202511
    - [ ] 202512
    - [ ] 202605
    - [ ] N/A
    
    ### Test result
    202511 - `macsec/test_deployment.py` passes now
    
    ### Approach
    #### What is the motivation for this PR?
    `macsec/test_deployment.py::test_config_reload` completing early causes `macsec/test_deployment.py::test_scale_rekey` to fail due to ports still being down.
    
    #### How did you do it?
    Improve the interlock in `macsec/test_deployment.py::test_config_reload` to make sure the macsec ports are up even on `EosHost` nbrs.
    
    #### How did you verify/test it?
    Saw `macsec/test_deployment.py` passing all testlets on 202511.
    
    #### Any platform specific information?
    N/A
    
    #### Supported testbed topology if it's a new test case?
    N/A
    
    ### Documentation
    N/A

--- tests/common/macsec/macsec_helper.py
+++ tests/common/macsec/macsec_helper.py
@@ -16,7 +16,7 @@ import scapy.contrib.macsec as scapy_macsec
 
 from tests.common.macsec.macsec_platform_helper import sonic_db_cli
 from tests.common.devices.eos import EosHost
-from tests.common.utilities import convert_scapy_packet_to_bytes
+from tests.common.utilities import convert_scapy_packet_to_bytes, wait_until
 
 __all__ = [
     'check_wpa_supplicant_process',
@@ -198,6 +198,9 @@ def check_appl_db(duthost, ctrl_links, policy, cipher_suite, send_sci):
     procs = []
     for port_name, nbr in list(ctrl_links.items()):
         if isinstance(nbr["host"], EosHost):
+            assert wait_until(300, 3, 0,
+                              lambda: duthost.iface_macsec_ok(port_name) and
+                              nbr["host"].iface_macsec_ok(nbr["port"]))
             continue
         proc = submit_async_task(
             __check_appl_db,
