5572e82d938852683fa34fc1834c2d79bf8fd0f6
Pratik Dam <pdam@arista.com>
2026-09-22 20:26:49 +0530

    Fix drop counters ASIC type DUT
    Signed-off-by: Pratik Dam <pdam@arista.com>

--- tests/drop_packets/test_drop_counters.py
+++ tests/drop_packets/test_drop_counters.py
@@ -324,7 +324,9 @@ def check_if_skip():
 
 
 @pytest.fixture(scope='module')
-def do_test(duthosts):
+def do_test(duthosts, enum_rand_one_per_hwsku_frontend_hostname):
+    duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+
     def do_counters_test(discard_group, pkt, ptfadapter, ports_info, sniff_ports, tx_dut_ports=None,    # noqa: F811
                          comparable_pkt=None, skip_counter_check=False, drop_information=None, ip_ver='ipv4'):
         """
@@ -338,7 +340,7 @@ def do_test(duthosts):
         @param ip_ver: A string, ipv4 or ipv6
         """
         check_if_skip()
-        asic_type = duthosts[0].facts["asic_type"]
+        asic_type = duthost.facts["asic_type"]
         if asic_type == "vs":
             skip_counter_check = True
 
