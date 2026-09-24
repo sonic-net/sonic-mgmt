23c9d2f20e761ea2327eccb943b257d10db8bcf4
Deeksha Rao <deerao@microsoft.com>
2026-09-24 00:55:47 +0000

    Revert "[dut_console]: Allow configured BMC console paths"
    This reverts commit df842732481916c3b773520699b941836eb16a95.

--- tests/dut_console/test_console_baud_rate.py
+++ tests/dut_console/test_console_baud_rate.py
@@ -54,17 +54,14 @@ def console_client_setup_teardown(duthost, conn_graph_facts, creds):
         console_host = console_host.split("/")[0]
     console_type = conn_graph_facts['device_console_link'][dut_hostname]["ConsolePort"]["type"]
     pytest_require(console_type == "ssh", "Unsupported console type: {}".format(console_type))
-    host_console_command = get_host_console_command(duthost)
-    pytest_require(
-        host_console_command or is_sonic_console(conn_graph_facts, dut_hostname),
-        "Unsupported non-SONiC console switch without a host console command.",
-    )
+    pytest_require(is_sonic_console(conn_graph_facts, dut_hostname), "Unsupport non-sonic console swith.")
     console_port = conn_graph_facts['device_console_link'][dut_hostname]['ConsolePort']['peerport']
     console_user = creds['console_user']['console_ssh']
     console_passwords = creds['console_password']['console_ssh']
     sonic_passwords = [creds['sonicadmin_password']] + creds.get('ansible_altpasswords', [])
     sonic_passwords.extend(console_passwords)
     sonic_passwords = list(dict.fromkeys(password for password in sonic_passwords if password))
+    host_console_command = get_host_console_command(duthost)
 
     client = None
     for console_password in console_passwords:
